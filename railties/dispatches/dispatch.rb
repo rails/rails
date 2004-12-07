a6f718cfa0b633ed343a641bf6a3942476e26802
David Heinemeier Hansson <david@loudthinking.com>
2004-12-07 12:03:57 +0000

    Make the dispatch.rb behave after being run by the WEBrick dispatcher
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@64 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/dispatches/dispatch.rb
+++ railties/dispatches/dispatch.rb
@@ -1,6 +1,6 @@
 #!/usr/local/bin/ruby
 
-require File.dirname(__FILE__) + "/../config/environment"
+require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
 
 # If you're using RubyGems and mod_ruby, this require should be changed to an absolute path one, like:
 # "/usr/local/lib/ruby/gems/1.8/gems/rails-0.8.0/lib/dispatcher" -- otherwise performance is severely impaired
