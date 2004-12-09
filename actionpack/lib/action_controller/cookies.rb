ad354b207a5600fbe7c784f370cf30446925f13c
David Heinemeier Hansson <david@loudthinking.com>
2004-12-09 00:15:58 +0000

    Added CookieJar#delete and fixed CookieJar[] to just call first, so you get a string instead of an array back. This limits each cookie to one value, which I consider a mighty fine restriction
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@92 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_controller/cookies.rb
+++ actionpack/lib/action_controller/cookies.rb
@@ -36,7 +36,7 @@ module ActionController #:nodoc:
     # Returns the value of the cookie by +name+ -- or nil if no such cookie exist. You set new cookies using either the cookie method
     # or cookies[]= (for simple name/value cookies without options).
     def [](name)
-      @cookies[name.to_s].value if @cookies[name.to_s] && @cookies[name.to_s].respond_to?(:value)
+      @cookies[name.to_s].value.first if @cookies[name.to_s] && @cookies[name.to_s].respond_to?(:value)
     end
     
     def []=(name, options)
@@ -48,6 +48,11 @@ module ActionController #:nodoc:
       end
       
       set_cookie(name, options)
+    end
+    
+    # Removes the cookie on the client machine by setting the value to an empty string.
+    def delete(name)
+      set_cookie(name, "value" => "")
     end
 
     private
