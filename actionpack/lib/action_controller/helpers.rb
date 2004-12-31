2dd2b56482a4e102251258e3c2458618de8897c2
David Heinemeier Hansson <david@loudthinking.com>
2004-12-31 16:48:02 +0000

    Auto-requiring of helpers should stay silent when it isn't found for whatever reason
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@290 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_controller/helpers.rb
+++ actionpack/lib/action_controller/helpers.rb
@@ -96,7 +96,7 @@ module ActionController #:nodoc:
           inherited_without_helper(child)
           begin
             child.helper(child.controller_name)
-          rescue LoadError
+          rescue LoadError, StandardError
             # No default helper available for this controller
           end
         end        
