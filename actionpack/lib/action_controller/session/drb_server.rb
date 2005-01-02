ddcc67115c8863d053f8286efe21db74e1678b44
David Heinemeier Hansson <david@loudthinking.com>
2005-01-02 16:00:50 +0000

    Added thread-safety to the DRbStore #66, #389 [Ben Stiglitz]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@315 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_controller/session/drb_server.rb
+++ actionpack/lib/action_controller/session/drb_server.rb
@@ -5,5 +5,28 @@
                                                                                 
 require 'drb'
 
-DRb.start_service('druby://127.0.0.1:9192', Hash.new)
+session_hash = Hash.new
+session_hash.instance_eval { @mutex = Mutex.new }
+
+class <<session_hash
+  def []=(key, value)
+    @mutex.synchronize do
+      super(key, value)
+    end
+  end
+  
+  def [](key)
+    @mutex.synchronize do
+      super(key)
+    end
+  end
+  
+  def delete(key)
+    @mutex.synchronize do
+      super(key)
+    end
+  end
+end
+
+DRb.start_service('druby://127.0.0.1:9192', session_hash)
 DRb.thread.join
