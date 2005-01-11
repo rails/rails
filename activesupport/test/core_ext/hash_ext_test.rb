876a1a1fa304ae99f22ee1a8d4962fc1173d4372
David Heinemeier Hansson <david@loudthinking.com>
2005-01-11 21:37:02 +0000

    Added Byte operations to Numeric, so 5.5.megabytes + 200.kilobytes #461 [Marcel Molina]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@393 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/test/core_ext/hash_ext_test.rb
+++ activesupport/test/core_ext/hash_ext_test.rb
@@ -1,7 +1,38 @@
 require 'test/unit'
-require File.dirname(__FILE__) + '/../../lib/core_ext/hash_ext'
+require File.dirname(__FILE__) + '/../../lib/core_ext/hash'
 
 class HashExtTest < Test::Unit::TestCase
+
+  def setup
+    @strings = { 'a' => 1, 'b' => 2 }
+    @symbols = { :a  => 1, :b  => 2 }
+    @mixed   = { :a  => 1, 'b' => 2 }
+  end
+
+  def test_methods
+    h = {}
+    assert_respond_to h, :symbolize_keys
+    assert_respond_to h, :symbolize_keys!
+    assert_respond_to h, :to_options
+    assert_respond_to h, :to_options!
+  end
+
+  def test_symbolize_keys
+    assert_equal @symbols, @symbols.symbolize_keys
+    assert_equal @symbols, @strings.symbolize_keys
+    assert_equal @symbols, @mixed.symbolize_keys
+
+    assert_raises(NoMethodError) { { [] => 1 }.symbolize_keys }
+  end
+
+  def test_symbolize_keys!
+    assert_equal @symbols, @symbols.dup.symbolize_keys!
+    assert_equal @symbols, @strings.dup.symbolize_keys!
+    assert_equal @symbols, @mixed.dup.symbolize_keys!
+
+    assert_raises(NoMethodError) { { [] => 1 }.symbolize_keys! }
+  end
+
   def test_assert_valid_keys
     assert_nothing_raised do
       { :failure => "stuff", :funny => "business" }.assert_valid_keys([ :failure, :funny ])
--- activesupport/test/misc_test.rb
+++ activesupport/test/misc_test.rb
@@ -17,35 +17,3 @@ class MiscTest < Test::Unit::TestCase
     end
   end
 end
-
-class HashOptionsTest < Test::Unit::TestCase
-  def setup
-    @strings = { 'a' => 1, 'b' => 2 }
-    @symbols = { :a  => 1, :b  => 2 }
-    @mixed   = { :a  => 1, 'b' => 2 }
-  end
-
-  def test_methods
-    h = {}
-    assert_respond_to h, :symbolize_keys
-    assert_respond_to h, :symbolize_keys!
-    assert_respond_to h, :to_options
-    assert_respond_to h, :to_options!
-  end
-
-  def test_symbolize_keys
-    assert_equal @symbols, @symbols.symbolize_keys
-    assert_equal @symbols, @strings.symbolize_keys
-    assert_equal @symbols, @mixed.symbolize_keys
-
-    assert_raises(NoMethodError) { { [] => 1 }.symbolize_keys }
-  end
-
-  def test_symbolize_keys!
-    assert_equal @symbols, @symbols.dup.symbolize_keys!
-    assert_equal @symbols, @strings.dup.symbolize_keys!
-    assert_equal @symbols, @mixed.dup.symbolize_keys!
-
-    assert_raises(NoMethodError) { { [] => 1 }.symbolize_keys! }
-  end
-end
