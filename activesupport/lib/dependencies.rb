d8805988a125720a315580050b7195537fb6e77a
David Heinemeier Hansson <david@loudthinking.com>
2005-01-15 16:46:17 +0000

    Missing something from #475
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@413 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/dependencies.rb
+++ activesupport/lib/dependencies.rb
@@ -32,6 +32,7 @@ module Dependencies
     old_loaded.each do |file_name| 
       begin
         silence_warnings { load("#{file_name}.rb") } unless loaded.include?(file_name)
+        loaded << file_name
       rescue LoadError
         # The association didn't reside in its own file, so we assume it was required by other means
       end
