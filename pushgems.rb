bb817f1ed474b91e24ff7b661cd1d8c0635786d8
David Heinemeier Hansson <david@loudthinking.com>
2005-02-18 16:52:40 +0000

    Get Action Service (soon to be renamed Action Web Service) in the loop
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@662 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- pushgems.rb
+++ pushgems.rb
@@ -2,7 +2,7 @@
 
 build_number = `svn log -q -rhead http://dev.rubyonrails.org/svn/rails`.scan(/r([0-9]*)/).first.first.to_i
 
-(%w( actionmailer actionpack activerecord railties activesupport) - ARGV).each do |pkg|
+(%w( actionservice actionmailer actionpack activerecord railties activesupport) - ARGV).each do |pkg|
   puts "Pushing: #{pkg} (#{build_number})"
   `cd #{pkg} && PKG_BUILD=#{build_number} rake pgem && cd ..`
 end
