8c40759b10ec6a2be2e12a9c160cb64085d490b4
David Heinemeier Hansson <david@loudthinking.com>
2005-02-20 10:55:08 +0000

    Allow downloading of WSDL for an AWS controller as a proper file #674
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@710 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/configs/routes.rb
+++ railties/configs/routes.rb
@@ -5,6 +5,10 @@ ActionController::Routing::Routes.draw do |map|
   # Here's a sample route:
   # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
+
+  # Allow downloading Web Service WSDL as a file with an extension
+  # instead of a file named 'wsdl'
+  map.connect ':controller/service.wsdl', :action => 'wsdl'
   
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id'
