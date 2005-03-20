2c918ed9b5fba4802654a4e9792519391fd687d6
David Heinemeier Hansson <david@loudthinking.com>
2005-03-20 23:39:07 +0000

    Added note about routing the site root
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@950 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/configs/routes.rb
+++ railties/configs/routes.rb
@@ -6,10 +6,14 @@ ActionController::Routing::Routes.draw do |map|
   # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
 
+  # You can have the root of your site routed by hooking up '' 
+  # -- just remember to delete public/index.html.
+  # map.connect '', :controller => "welcome"
+
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
   map.connect ':controller/service.wsdl', :action => 'wsdl'
-  
+
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id'
 end
