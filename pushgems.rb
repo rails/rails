fd95719c98c413e2d200e2c175ca85dae843d45d
David Heinemeier Hansson <david@loudthinking.com>
2005-03-22 13:29:22 +0000

    Releasing 0.11.0
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@977 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- pushgems.rb
+++ pushgems.rb
@@ -1,8 +1,14 @@
 #!/usr/local/bin/ruby
 
-build_number = `svn log -q -rhead http://dev.rubyonrails.org/svn/rails`.scan(/r([0-9]*)/).first.first.to_i
+unless ARGV.first == "no_build"
+  build_number = `svn log -q -rhead http://dev.rubyonrails.org/svn/rails`.scan(/r([0-9]*)/).first.first.to_i
+end
 
-(%w( actionwebservice actionmailer actionpack activerecord railties activesupport) - ARGV).each do |pkg|
+%w( actionwebservice actionmailer actionpack activerecord railties activesupport).each do |pkg|
   puts "Pushing: #{pkg} (#{build_number})"
-  `cd #{pkg} && rm -rf pkg && PKG_BUILD=#{build_number} rake pgem && cd ..`
+  if build_number
+    `cd #{pkg} && rm -rf pkg && PKG_BUILD=#{build_number} rake pgem && cd ..`
+  else
+    `cd #{pkg} && rm -rf pkg && rake pgem && cd ..`
+  end
 end
