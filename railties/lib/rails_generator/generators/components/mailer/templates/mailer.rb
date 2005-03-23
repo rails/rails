5f5f19b06d8a4db4a61055280660dc57180ff47c
David Heinemeier Hansson <david@loudthinking.com>
2005-03-23 14:16:46 +0000

    _at is better for Time.now than _on
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@988 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/rails_generator/generators/components/mailer/templates/mailer.rb
+++ railties/lib/rails_generator/generators/components/mailer/templates/mailer.rb
@@ -1,12 +1,12 @@
 class <%= class_name %> < ActionMailer::Base
 <% for action in actions -%>
 
-  def <%= action %>(sent_on = Time.now)
+  def <%= action %>(sent_at = Time.now)
     @subject    = '<%= class_name %>#<%= action %>'
     @body       = {}
     @recipients = ''
     @from       = ''
-    @sent_on    = sent_on
+    @sent_on    = sent_at
     @headers    = {}
   end
 <% end -%>
