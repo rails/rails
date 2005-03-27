1b17cddc3c82a33c17d48df220d2d97a77dcce2a
David Heinemeier Hansson <david@loudthinking.com>
2005-03-27 15:07:27 +0000

    Released 0.11.1
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@1024 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

---
+++ release.rb
@@ -0,0 +1,30 @@
+#!/usr/local/bin/ruby
+
+PACKAGES = %w( activesupport activerecord actionpack actionmailer actionwebservice )
+
+unless ruby_forge_password = ARGV.first
+  print "rubyforge.org's password: "
+  ruby_forge_password = STDIN.gets.chomp
+end
+
+# Checkout source
+`rm -rf release* && svn export http://dev.rubyonrails.org/svn/rails/trunk release`
+
+# Create Rails packages
+`cd release/railties && rake template=jamis package`
+
+# Upload documentation
+`cd release/rails/doc/api && scp -r * davidhh@comox.textdrive.com:public_html/rails`
+PACKAGES.each do |p| 
+  `cd release/#{p} && echo "Publishing documentation for #{p}" && rake template=jamis pdoc`
+end
+
+# Upload packages
+(PACKAGES + %w(railties)).each do |p| 
+  `cd release/#{p} && echo "Releasing #{p}" && RUBY_FORGE_PASSWORD=#{ruby_forge_password} rake release`
+end
+
+# Upload rails tgz/zip
+
+# Create SVN tag
+
