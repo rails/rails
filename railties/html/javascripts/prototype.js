b0c0c9c2ef908606c03e03c7db422fde703eaa99
David Heinemeier Hansson <david@loudthinking.com>
2005-03-28 10:22:44 +0000

    Added Effect.Fade which smoothly turns opacity from 100 to 0 and then hides the element #960 [thomas@fesch.at]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@1032 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/html/javascripts/prototype.js
+++ railties/html/javascripts/prototype.js
@@ -452,3 +452,33 @@ Effect.Highlight.prototype = {
     element.style.backgroundColor = "#ffff" + current.toColorPart();
   }
 }
+
+
+Effect.Fade = Class.create();
+Effect.Fade.prototype = {
+  initialize: function(element) {
+    this.element = $(element);
+    this.start  = 100;
+    this.finish = 0;
+    this.current = this.start;
+    this.fade();
+  },
+  
+  fade: function() {
+    if (this.isFinished()) { this.element.style.display = 'none'; return; }
+    if (this.timer) clearTimeout(this.timer);
+    this.setOpacity(this.element, this.current);
+    this.current -= 10;
+    this.timer = setTimeout(this.fade.bind(this), 100);
+  },
+  
+  isFinished: function() {
+    return this.current <= this.finish;
+  },
+  
+  setOpacity: function(element, opacity) {
+    opacity = (opacity == 100) ? 99.999 : opacity;
+    element.style.filter = "alpha(opacity:"+opacity+")";
+    element.style.opacity = opacity/100;
+  }
+}
