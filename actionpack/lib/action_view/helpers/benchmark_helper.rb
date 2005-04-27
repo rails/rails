fbd86c201722ac319818dc339d12d9716e4d13d9
David Heinemeier Hansson <david@loudthinking.com>
2005-04-27 08:17:25 +0000

    Added BenchmarkHelper that can measure the execution time of a block in a template and reports the result to the log
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@1240 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

---
+++ actionpack/lib/action_view/helpers/benchmark_helper.rb
@@ -0,0 +1,22 @@
+require 'benchmark'
+
+module ActionView
+  module Helpers
+    module BenchmarkHelper
+      # Measures the execution time of a block in a template and reports the result to the log. Example:
+      #
+      #  <% benchmark "Notes section" do %>
+      #    <%= expensive_notes_operation %>
+      #  <% end %>
+      #
+      # Will add something like "Notes section (0.345234)" to the log.
+      def benchmark(message = "Benchmarking", &block)
+        bm = Benchmark.measure do
+          block.call
+        end
+        
+        @logger.info("#{message} (#{bm.real})")
+      end
+    end
+  end
+end
