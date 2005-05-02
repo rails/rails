3135ec40a3304eb79284c4447b0ffc79867f6224
David Heinemeier Hansson <david@loudthinking.com>
2005-05-02 11:41:24 +0000

    Added silencing to the default Logger class
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@1271 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/clean_logger.rb
+++ activesupport/lib/active_support/clean_logger.rb
@@ -1,6 +1,16 @@
 require 'logger'
 
 class Logger #:nodoc:
+  # Silences the logger for the duration of the block.
+  def silence
+    result = nil
+    old_logger_level = level
+    self.level = Logger::ERROR
+    result = yield
+    self.level = old_logger_level
+    return result
+  end
+
   private
     remove_const "Format"
     Format = "%s\n"
