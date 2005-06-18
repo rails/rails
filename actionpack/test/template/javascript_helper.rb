a49a784dc7e8c45065d072aa99f1cda18c9322bb
David Heinemeier Hansson <david@loudthinking.com>
2005-06-18 05:17:39 +0000

    More tests for JavascriptHelper #1460
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@1451 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/test/template/javascript_helper.rb
+++ actionpack/test/template/javascript_helper.rb
@@ -2,8 +2,60 @@ require File.dirname(__FILE__) + '/../abstract_unit'
 
 class JavascriptHelperTest < Test::Unit::TestCase
   include ActionView::Helpers::JavascriptHelper
+  
+  include ActionView::Helpers::UrlHelper
+  include ActionView::Helpers::TagHelper
+  
+  def setup
+    @controller = Class.new do
+      def url_for(options, *parameters_for_method_reference)
+        url =  "http://www.example.com/"
+        url << options[:action].to_s if options and options[:action]
+        url
+      end
+    end
+    @controller = @controller.new
+  end
 
   def test_escape_javascript
     assert_equal %(This \\"thing\\" is really\\n netos\\'), escape_javascript(%(This "thing" is really\n netos'))
   end
+  
+  def test_link_to_function
+    assert_equal %(<a href="#" onclick="alert('Hello world!'); return false;">Greeting</a>), 
+      link_to_function("Greeting", "alert('Hello world!')")
+  end
+  
+  def test_link_to_remote
+    assert_equal %(<a class="fine" href="#" onclick="new Ajax.Request('http://www.example.com/whatnot', {asynchronous:true}); return false;">Remote outpost</a>),
+      link_to_remote("Remote outpost", { :url => { :action => "whatnot"  }}, { :class => "fine"  })
+    assert_equal %(<a href="#" onclick="new Ajax.Request('http://www.example.com/whatnot', {onComplete:function(request){alert(request.reponseText)}, asynchronous:true}); return false;">Remote outpost</a>),
+      link_to_remote("Remote outpost", :complete => "alert(request.reponseText)", :url => { :action => "whatnot"  })
+  end
+  
+  def test_periodically_call_remote
+    assert_equal %(<script>new PeriodicalExecuter(function() {new Ajax.Updater('schremser_bier', 'http://www.example.com/mehr_bier', {asynchronous:true})}, 10)</script>),
+      periodically_call_remote(:update => "schremser_bier", :url => { :action => "mehr_bier" })
+  end
+  
+  def test_form_remote_tag
+    assert_equal %(<form onsubmit="new Ajax.Updater('glass_of_beer', 'http://www.example.com/fast', {parameters:Form.serialize(this), asynchronous:true}); return false;">),
+      form_remote_tag(:update => "glass_of_beer", :url => { :action => :fast  })
+  end
+  
+  def test_submit_to_remote
+    assert_equal %(<input name="More beer!" onclick="new Ajax.Updater('empty_bottle', 'http://www.example.com/', {parameters:Form.serialize(this.form), asynchronous:true}); return false;" type="button" value="1000000" />),
+      submit_to_remote("More beer!", 1_000_000, :update => "empty_bottle")
+  end
+  
+  def test_observe_field
+    assert_equal %(<script type="text/javascript">new Form.Element.Observer('glass', 300, function(element, value) {new Ajax.Request('http://www.example.com/reorder_if_empty', {asynchronous:true})})</script>),
+      observe_field("glass", :frequency => 5.minutes, :url => { :action => "reorder_if_empty" })
+  end
+  
+  def test_observe_form
+    assert_equal %(<script type="text/javascript">new Form.Observer('cart', 2, function(element, value) {new Ajax.Request('http://www.example.com/cart_changed', {asynchronous:true})})</script>),
+      observe_form("cart", :frequency => 2, :url => { :action => "cart_changed" })
+  end
+  
 end
