aa49feb5886bd2821e425c39dc1e3562e484817e
David Heinemeier Hansson <david@loudthinking.com>
2005-06-21 05:58:42 +0000

    Proper order in test #1329
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@1457 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activerecord/test/column_alias_test.rb
+++ activerecord/test/column_alias_test.rb
@@ -8,11 +8,11 @@ class TestColumnAlias < Test::Unit::TestCase
     if ActiveRecord::ConnectionAdapters.const_defined? :OracleAdapter
       if ActiveRecord::Base.connection.instance_of?(ActiveRecord::ConnectionAdapters::OracleAdapter)
         records = topic.connection.select_all("SELECT id AS pk FROM topics WHERE ROWNUM < 2")
-        assert_equal(records[0].keys[0], "pk")
+        assert_equal("pk", records[0].keys[0])
       end
     else
       records = topic.connection.select_all("SELECT id AS pk FROM topics")
-      assert_equal(records[0].keys[0], "pk")
+      assert_equal("pk", records[0].keys[0])
     end
   end
   
