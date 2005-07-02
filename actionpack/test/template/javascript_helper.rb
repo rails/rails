b94e8af9a4596723b654129b650da490783f209a
David Heinemeier Hansson <david@loudthinking.com>
2005-07-02 18:20:13 +0000

    Update for JavaScript helper tests #1584 [Thomas Fuchs]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@1611 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/test/template/javascript_helper.rb
+++ actionpack/test/template/javascript_helper.rb
@@ -5,6 +5,8 @@ class JavaScriptHelperTest < Test::Unit::TestCase
   
   include ActionView::Helpers::UrlHelper
   include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::FormHelper
   
   def setup
     @controller = Class.new do
@@ -32,52 +34,84 @@ class JavaScriptHelperTest < Test::Unit::TestCase
   end
   
   def test_link_to_remote
-    assert_equal %(<a class="fine" href="#" onclick="new Ajax.Request('http://www.example.com/whatnot', {asynchronous:true}); return false;">Remote outpost</a>),
+    assert_equal %(<a class=\"fine\" href=\"#\" onclick=\"new Ajax.Request('http://www.example.com/whatnot', {evalScripts:true, asynchronous:true}); return false;\">Remote outpost</a>),
       link_to_remote("Remote outpost", { :url => { :action => "whatnot"  }}, { :class => "fine"  })
-    assert_equal %(<a href="#" onclick="new Ajax.Request('http://www.example.com/whatnot', {onComplete:function(request){alert(request.reponseText)}, asynchronous:true}); return false;">Remote outpost</a>),
+    assert_equal %(<a href=\"#\" onclick=\"new Ajax.Request('http://www.example.com/whatnot', {onComplete:function(request){alert(request.reponseText)}, evalScripts:true, asynchronous:true}); return false;\">Remote outpost</a>),
       link_to_remote("Remote outpost", :complete => "alert(request.reponseText)", :url => { :action => "whatnot"  })
   end
   
   def test_periodically_call_remote
-    assert_equal %(<script>new PeriodicalExecuter(function() {new Ajax.Updater('schremser_bier', 'http://www.example.com/mehr_bier', {asynchronous:true})}, 10)</script>),
+    assert_equal %(<script>new PeriodicalExecuter(function() {new Ajax.Updater({success:'schremser_bier'}, 'http://www.example.com/mehr_bier', {evalScripts:true, asynchronous:true})}, 10)</script>),
       periodically_call_remote(:update => "schremser_bier", :url => { :action => "mehr_bier" })
   end
   
   def test_form_remote_tag
-    assert_equal %(<form action="http://www.example.com/fast" method="post" onsubmit="new Ajax.Updater('glass_of_beer', 'http://www.example.com/fast', {parameters:Form.serialize(this), asynchronous:true}); return false;">),
+    assert_equal %(<form action=\"http://www.example.com/fast\" method=\"post\" onsubmit=\"new Ajax.Updater({success:'glass_of_beer'}, 'http://www.example.com/fast', {parameters:Form.serialize(this), evalScripts:true, asynchronous:true}); return false;\">),
       form_remote_tag(:update => "glass_of_beer", :url => { :action => :fast  })
   end
   
   def test_submit_to_remote
-    assert_equal %(<input name="More beer!" onclick="new Ajax.Updater('empty_bottle', 'http://www.example.com/', {parameters:Form.serialize(this.form), asynchronous:true}); return false;" type="button" value="1000000" />),
+    assert_equal %(<input name=\"More beer!\" onclick=\"new Ajax.Updater({success:'empty_bottle'}, 'http://www.example.com/', {parameters:Form.serialize(this.form), evalScripts:true, asynchronous:true}); return false;\" type=\"button\" value=\"1000000\" />),
       submit_to_remote("More beer!", 1_000_000, :update => "empty_bottle")
   end
   
   def test_observe_field
-    assert_equal %(<script type="text/javascript">new Form.Element.Observer('glass', 300, function(element, value) {new Ajax.Request('http://www.example.com/reorder_if_empty', {asynchronous:true})})</script>),
+    assert_equal %(<script type=\"text/javascript\">new Form.Element.Observer('glass', 300, function(element, value) {new Ajax.Request('http://www.example.com/reorder_if_empty', {evalScripts:true, asynchronous:true})})</script>),
       observe_field("glass", :frequency => 5.minutes, :url => { :action => "reorder_if_empty" })
   end
   
   def test_observe_form
-    assert_equal %(<script type="text/javascript">new Form.Observer('cart', 2, function(element, value) {new Ajax.Request('http://www.example.com/cart_changed', {asynchronous:true})})</script>),
+    assert_equal %(<script type=\"text/javascript\">new Form.Observer('cart', 2, function(element, value) {new Ajax.Request('http://www.example.com/cart_changed', {evalScripts:true, asynchronous:true})})</script>),
       observe_form("cart", :frequency => 2, :url => { :action => "cart_changed" })
   end
   
-  def test_remote_autocomplete
-    assert_equal %(<script type="text/javascript">new Ajax.Autocompleter('some_input', 'some_input_autocomplete', 'http://www.example.com/autocomplete', {})</script>),
-      remote_autocomplete("some_input", :url => { :action => "autocomplete" });    
-  end 
+  def test_auto_complete_field
+    assert_equal %(<script type=\"text/javascript\">new Ajax.Autocompleter('some_input', 'some_input_auto_complete', 'http://www.example.com/autocomplete', {})</script>),
+      auto_complete_field("some_input", :url => { :action => "autocomplete" });    
+  end
+  
+  def test_auto_complete_result
+    result = [ { :title => 'test1'  }, { :title => 'test2'  } ]
+    assert_equal %(<ul><li>test1</li><li>test2</li></ul>), 
+      auto_complete_result(result, :title)
+    assert_equal %(<ul><li>t<strong class=\"highlight\">est</strong>1</li><li>t<strong class=\"highlight\">est</strong>2</li></ul>), 
+      auto_complete_result(result, :title, "est")
+  end
+  
+  def test_text_field_with_auto_complete
+    assert_match "<style>",
+      text_field_with_auto_complete(:message, :recipient)
+    assert_equal %(<input autocomplete=\"off\" id=\"message_recipient\" name=\"message[recipient]\" size=\"30\" type=\"text\" value=\"\" /><div class=\"auto_complete\" id=\"message_recipient_auto_complete\"></div><script type=\"text/javascript\">new Ajax.Autocompleter('message_recipient', 'message_recipient_auto_complete', 'http://www.example.com/auto_complete_for_message_recipient', {})</script>),
+      text_field_with_auto_complete(:message, :recipient, {}, :skip_style => true)
+  end
   
   def test_effect
     assert_equal "new Effect.Highlight('posts',{});", visual_effect(:highlight, "posts")
     assert_equal "new Effect.Highlight('posts',{});", visual_effect("highlight", :posts)
     assert_equal "new Effect.Highlight('posts',{});", visual_effect(:highlight, :posts)    
     assert_equal "new Effect.Fade('fademe',{duration:4.0});", visual_effect(:fade, "fademe", :duration => 4.0)
+    assert_equal "new Effect.Shake(element,{});", visual_effect(:shake)
   end
   
-  def test_remote_sortable
-    assert_equal %(<script type="text/javascript">Sortable.create('mylist',{onUpdate:function(){new Ajax.Request('http://www.example.com/order', {parameters:Sortable.serialize('mylist'), asynchronous:true})}})</script>), 
-      remote_sortable("mylist", :url => { :action => "order" })
+  def test_sortable_element
+    assert_equal %(<script type=\"text/javascript\">Sortable.create('mylist', {onUpdate:function(){new Ajax.Request('http://www.example.com/order', {parameters:Sortable.serialize('mylist'), evalScripts:true, asynchronous:true})}})</script>), 
+      sortable_element("mylist", :url => { :action => "order" })
+  end
+  
+  def test_draggable_element
+    assert_equal %(<script type=\"text/javascript\">new Draggable('product_13', {})</script>),
+      draggable_element('product_13')
+    assert_equal %(<script type=\"text/javascript\">new Draggable('product_13', {revert:true})</script>),
+      draggable_element('product_13', :revert => true)
+  end
+  
+  def test_drop_receiving_element
+    assert_equal %(<script type=\"text/javascript\">Droppables.add('droptarget1', {onDrop:function(element){new Ajax.Request('http://www.example.com/', {parameters:'id=' + encodeURIComponent(element.id), evalScripts:true, asynchronous:true})}})</script>),
+      drop_receiving_element('droptarget1')
+    assert_equal %(<script type=\"text/javascript\">Droppables.add('droptarget1', {onDrop:function(element){new Ajax.Request('http://www.example.com/', {parameters:'id=' + encodeURIComponent(element.id), evalScripts:true, asynchronous:true})}, accept:'products'})</script>),
+      drop_receiving_element('droptarget1', :accept => 'products')
+    assert_equal %(<script type=\"text/javascript\">Droppables.add('droptarget1', {onDrop:function(element){new Ajax.Updater({success:'infobox'}, 'http://www.example.com/', {parameters:'id=' + encodeURIComponent(element.id), evalScripts:true, asynchronous:true})}, accept:'products'})</script>),
+      drop_receiving_element('droptarget1', :accept => 'products', :update => 'infobox')
   end
   
 end
