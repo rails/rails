8b1b72bb0d3f70b2a840ce6863775ef91f63ae14
Jamis Buck <jamis@37signals.com>
2005-07-22 09:39:33 +0000

    Remove duplicated file contents
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@1884 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionmailer/lib/action_mailer/adv_attr_accessor.rb
+++ actionmailer/lib/action_mailer/adv_attr_accessor.rb
@@ -25,32 +25,3 @@ module ActionMailer
     end
   end
 end
-
-module ActionMailer
-  module AdvAttrAccessor #:nodoc:
-    def self.append_features(base)
-      super
-      base.extend(ClassMethods)
-    end
-
-    module ClassMethods #:nodoc:
-      def adv_attr_accessor(*names)
-        names.each do |name|
-          define_method("#{name}=") do |value|
-            instance_variable_set("@#{name}", value)
-          end
-
-          define_method(name) do |*parameters|
-            raise ArgumentError, "expected 0 or 1 parameters" unless parameters.length <= 1
-            if parameters.empty?
-              instance_variable_get("@#{name}")
-            else
-              instance_variable_set("@#{name}", parameters.first)
-            end
-          end
-        end
-      end
-
-    end
-  end
-end
