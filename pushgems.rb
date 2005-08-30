e6a6b3fc4897be4d63d11cf454deb28406c466d0
Jamis Buck <jamis@37signals.com>
2005-08-30 20:55:42 +0000

    More rails-switchtower decoupling
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@2075 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- pushgems.rb
+++ pushgems.rb
@@ -4,7 +4,7 @@ unless ARGV.first == "no_build"
   build_number = "1962"
 end
 
-%w( actionwebservice actionmailer actionpack activerecord railties activesupport switchtower ).each do |pkg|
+%w( actionwebservice actionmailer actionpack activerecord railties activesupport ).each do |pkg|
   puts "Pushing: #{pkg} (#{build_number})"
   if build_number
     `cd #{pkg} && rm -rf pkg && PKG_BUILD=#{build_number} rake pgem && cd ..`
