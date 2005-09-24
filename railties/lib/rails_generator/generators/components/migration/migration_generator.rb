6d336753b9535617de105ef5494ffc3199d079b3
David Heinemeier Hansson <david@loudthinking.com>
2005-09-24 08:42:38 +0000

    Added prevention of duplicate migrations from the generator #2240 [fbeausoleil@ftml.net]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@2320 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/rails_generator/generators/components/migration/migration_generator.rb
+++ railties/lib/rails_generator/generators/components/migration/migration_generator.rb
@@ -2,6 +2,8 @@ class MigrationGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
       m.directory File.join('db/migrate')
+      existing_migrations = Dir.glob("db/migrate/[0-9]*_#{file_name}.rb")
+      raise "Another migration already exists with the same name" unless existing_migrations.empty?
       next_migration_number = Dir.glob("db/migrate/[0-9]*.*").size + 1
       m.template 'migration.rb', File.join('db/migrate', "#{next_migration_number}_#{file_name}.rb")
     end
