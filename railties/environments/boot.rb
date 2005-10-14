f2c7634db58b52f8d0415a7002976089fea3191c
Nicholas Seckar <nseckar@gmail.com>
2005-10-14 21:54:12 +0000

    Added cleaning of RAILS_ROOT to useless elements such as '../non-dot-dot/'. Provides cleaner backtraces and error messages.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@2592 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/environments/boot.rb
+++ railties/environments/boot.rb
@@ -1,4 +1,5 @@
-RAILS_ROOT = File.join(File.dirname(__FILE__), '..') unless defined?(RAILS_ROOT)
+require 'pathname'
+RAILS_ROOT = Pathname.new(File.join(File.dirname(__FILE__), '..')).cleanpath.to_s + '/' unless defined?(RAILS_ROOT)
 
 if File.directory?("#{RAILS_ROOT}/vendor/rails")
   require "#{RAILS_ROOT}/vendor/rails/railties/lib/initializer"
