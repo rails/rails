ccd18dc65008ebb44bfb0e72a8267991838f9744
Scott Barron <scott@elitists.net>
2005-10-15 17:05:55 +0000

    Should be RAILS_ROOT/test/fixtures.
    Closes #2476
    
    
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@2616 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/test_help.rb
+++ railties/lib/test_help.rb
@@ -6,8 +6,8 @@ require 'action_controller/test_process'
 require 'action_web_service/test_invoke'
 require 'breakpoint'
 
-Test::Unit::TestCase.fixture_path = RAILS_ROOT + "/fixtures/"
+Test::Unit::TestCase.fixture_path = RAILS_ROOT + "/test/fixtures/"
 
 def create_fixtures(*table_names)
-  Fixtures.create_fixtures(RAILS_ROOT + "/fixtures", table_names)
+  Fixtures.create_fixtures(RAILS_ROOT + "/test/fixtures", table_names)
 end
