6027d376b9e68a41f9b3da5abd21087fd965c421
David Heinemeier Hansson <david@loudthinking.com>
2005-10-15 01:10:12 +0000

    Cleaned up some docs
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@2598 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/helpers/application.rb
+++ railties/helpers/application.rb
@@ -1,4 +1,4 @@
-# The filters added to this controller will be run for all controllers in the application.
-# Likewise will all the methods added be available for all controllers.
+# Filters added to this controller will be run for all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
 end
