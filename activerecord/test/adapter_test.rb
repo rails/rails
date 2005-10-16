cd2b092f0c5baa0b3d766055842c031188e50c9f
Jeremy Kemper <jeremy@bitsweat.net>
2005-10-16 04:05:15 +0000

    r3957@asus:  jeremy | 2005-10-16 00:01:55 -0700  more sequence tests from #2292
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@2640 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activerecord/test/adapter_test.rb
+++ activerecord/test/adapter_test.rb
@@ -35,4 +35,24 @@ class AdapterTest < Test::Unit::TestCase
   ensure
     @connection.remove_index :accounts, :firm_id rescue nil
   end
+
+  # test resetting sequences in odd tables in postgreSQL
+  if ActiveRecord::Base.connection.respond_to?(:reset_pk_sequence!)
+    require 'fixtures/movie'
+    require 'fixtures/subscriber'
+    def test_reset_empty_table_with_custom_pk
+      Movie.delete_all
+      Movie.connection.reset_pk_sequence! 'movies'
+      assert_equal 1, Movie.create(:name => 'fight club').id
+    end
+
+    def test_reset_table_with_non_integer_pk
+      Subscriber.delete_all
+      Subscriber.connection.reset_pk_sequence! 'subscribers'
+
+      sub = Subscriber.new(:name => 'robert drake')
+      sub.id = 'bob drake'
+      assert sub.save!
+    end
+  end
 end
