46f76ea549cdf75cc43b9e16a9e571b787968c10
Nicholas Seckar <nseckar@gmail.com>
2005-10-16 18:37:11 +0000

    Improve line number detection for template errors
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@2655 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_view/template_error.rb
+++ actionpack/lib/action_view/template_error.rb
@@ -47,7 +47,7 @@ module ActionView
   
     def line_number
       if file_name
-        regexp = /#{Regexp.escape file_name}:(\d+)\s*$/
+        regexp = /#{Regexp.escape File.basename(file_name)}:(\d+)\s*$/
         [@original_exception.message, @original_exception.clean_backtrace].flatten.each do |line|
           return $1.to_i if regexp =~ line
         end
