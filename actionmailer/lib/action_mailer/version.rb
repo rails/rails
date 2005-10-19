e3e5d60bcd639da34333514562f82f89fd0c00f0
David Heinemeier Hansson <david@loudthinking.com>
2005-10-19 14:01:40 +0000

    Really ready for push
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@2686 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionmailer/lib/action_mailer/version.rb
+++ actionmailer/lib/action_mailer/version.rb
@@ -2,7 +2,7 @@ module ActionMailer
   module Version #:nodoc:
     MAJOR = 1
     MINOR = 1
-    TINY  = 0
+    TINY  = 1
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
--- actionwebservice/lib/action_web_service/version.rb
+++ actionwebservice/lib/action_web_service/version.rb
@@ -2,7 +2,7 @@ module ActionWebService
   module Version
     MAJOR = 0
     MINOR = 9
-    TINY  = 0
+    TINY  = 1
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
