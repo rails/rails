e0332f59e446c43cd9815dfa70a590b7471e016e
Nicholas Seckar <nseckar@gmail.com>
2005-10-27 17:00:25 +0000

    Back off cleanpath to be symlink friendly. Closes #2533
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@2768 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/environments/boot.rb
+++ railties/environments/boot.rb
@@ -2,7 +2,7 @@ unless defined?(RAILS_ROOT)
   root_path = File.join(File.dirname(__FILE__), '..')
   unless RUBY_PLATFORM =~ /mswin32/
     require 'pathname'
-    root_path = Pathname.new(root_path).cleanpath.to_s
+    root_path = Pathname.new(root_path).cleanpath(true).to_s
   end
   RAILS_ROOT = root_path
 end
