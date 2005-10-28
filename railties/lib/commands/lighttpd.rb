419794d8ba1915d63126584546dfa8c9c084f4a0
David Heinemeier Hansson <david@loudthinking.com>
2005-10-28 20:42:00 +0000

    Work, but better!
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@2792 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/commands/lighttpd.rb
+++ railties/lib/commands/lighttpd.rb
@@ -5,12 +5,12 @@ if RUBY_PLATFORM !~ /mswin/ && `lighttpd -version 2>/dev/null`.size > 0
     puts "=> Configure in config/lighttpd.conf"
     detach = true
   else
-    puts "=> Call with -d to detach; Configure in config/lighttpd.conf"
+    puts "=> Call with -d to detach (requires absolute paths in config/lighttpd.conf)"
     puts "=> Ctrl-C to shutdown server (see config/lighttpd.conf for options)"
     detach = false
   end
 
-  `lighttpd -f #{File.dirname(__FILE__) + "/../"}/config/lighttpd.conf`
+  `lighttpd #{!detach ? "-D " : ""}-f #{RAILS_ROOT}/config/lighttpd.conf`
 else
   puts "lighttpd is not available on your system (or not in your path)"
 end
