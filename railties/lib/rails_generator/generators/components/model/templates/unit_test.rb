a52132af587efa9bc9cefe3394499a681418c37e
David Heinemeier Hansson <david@loudthinking.com>
2005-11-02 10:15:31 +0000

    Added demonstration of fixture use to the test case generated by the model generator [DHH]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@2846 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/rails_generator/generators/components/model/templates/unit_test.rb
+++ railties/lib/rails_generator/generators/components/model/templates/unit_test.rb
@@ -3,12 +3,8 @@ require File.dirname(__FILE__) + '/../test_helper'
 class <%= class_name %>Test < Test::Unit::TestCase
   fixtures :<%= table_name %>
 
-  def setup
-    @<%= singular_name %> = <%= class_name %>.find(1)
-  end
-
   # Replace this with your real tests.
   def test_truth
-    assert_kind_of <%= class_name %>,  @<%= singular_name %>
+    assert_kind_of <%= class_name %>, <%= table_name %>(:first)
   end
 end
