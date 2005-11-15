08fd13d17c0e3f0de4403bd9b3280fe26fc75acb
Jeremy Kemper <jeremy@bitsweat.net>
2005-11-15 07:51:09 +0000

    Handle cookie parsing irregularity for certain Nokia phones.  Closes #2530.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@3039 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_controller/cgi_ext/cookie_performance_fix.rb
+++ actionpack/lib/action_controller/cgi_ext/cookie_performance_fix.rb
@@ -108,7 +108,7 @@ class CGI #:nodoc:
       cookies = Hash.new([])
 
       if raw_cookie
-        raw_cookie.split(/; /).each do |pairs|
+        raw_cookie.split(/; ?/).each do |pairs|
           name, values = pairs.split('=',2)
           next unless name and values
           name = CGI::unescape(name)
--- actionpack/test/controller/cgi_test.rb
+++ actionpack/test/controller/cgi_test.rb
@@ -313,6 +313,8 @@ end
 class CGIRequestTest < Test::Unit::TestCase
   def setup
     @request_hash = {"HTTP_MAX_FORWARDS"=>"10", "SERVER_NAME"=>"glu.ttono.us:8007", "FCGI_ROLE"=>"RESPONDER", "HTTP_X_FORWARDED_HOST"=>"glu.ttono.us", "HTTP_ACCEPT_ENCODING"=>"gzip, deflate", "HTTP_USER_AGENT"=>"Mozilla/5.0 (Macintosh; U; PPC Mac OS X; en) AppleWebKit/312.5.1 (KHTML, like Gecko) Safari/312.3.1", "PATH_INFO"=>"", "HTTP_ACCEPT_LANGUAGE"=>"en", "HTTP_HOST"=>"glu.ttono.us:8007", "SERVER_PROTOCOL"=>"HTTP/1.1", "REDIRECT_URI"=>"/dispatch.fcgi", "SCRIPT_NAME"=>"/dispatch.fcgi", "SERVER_ADDR"=>"207.7.108.53", "REMOTE_ADDR"=>"207.7.108.53", "SERVER_SOFTWARE"=>"lighttpd/1.4.5", "HTTP_COOKIE"=>"_session_id=c84ace84796670c052c6ceb2451fb0f2; is_admin=yes", "HTTP_X_FORWARDED_SERVER"=>"glu.ttono.us", "REQUEST_URI"=>"/admin", "DOCUMENT_ROOT"=>"/home/kevinc/sites/typo/public", "SERVER_PORT"=>"8007", "QUERY_STRING"=>"", "REMOTE_PORT"=>"63137", "GATEWAY_INTERFACE"=>"CGI/1.1", "HTTP_X_FORWARDED_FOR"=>"65.88.180.234", "HTTP_ACCEPT"=>"*/*", "SCRIPT_FILENAME"=>"/home/kevinc/sites/typo/public/dispatch.fcgi", "REDIRECT_STATUS"=>"200", "REQUEST_METHOD"=>"GET"}
+    # cookie as returned by some Nokia phone browsers (no space after semicolon separator)
+    @alt_cookie_fmt_request_hash = {"HTTP_COOKIE"=>"_session_id=c84ace84796670c052c6ceb2451fb0f2;is_admin=yes"}
     @fake_cgi = Struct.new(:env_table).new(@request_hash)
     @request = ActionController::CgiRequest.new(@fake_cgi)
   end
@@ -325,6 +327,16 @@ class CGIRequestTest < Test::Unit::TestCase
     @request_hash.delete "HTTP_X_FORWARDED_HOST"
     @request_hash['HTTP_HOST'] = "rubyonrails.org:8080"
     assert_equal "rubyonrails.org:8080", @request.host_with_port
+  end
+  
+  def test_cookie_syntax_resilience
+    cookies = CGI::Cookie::parse(@request_hash["HTTP_COOKIE"]);
+    assert_equal ["c84ace84796670c052c6ceb2451fb0f2"], cookies["_session_id"]
+    assert_equal ["yes"], cookies["is_admin"]
+    
+    alt_cookies = CGI::Cookie::parse(@alt_cookie_fmt_request_hash["HTTP_COOKIE"]);
+    assert_equal ["c84ace84796670c052c6ceb2451fb0f2"], alt_cookies["_session_id"]
+    assert_equal ["yes"], alt_cookies["is_admin"]
   end
   
 end
