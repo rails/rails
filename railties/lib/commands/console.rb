97d5f4acdfb986c5dc26f7c48518ef8a7d26f8bb
Jeremy Kemper <jeremy@bitsweat.net>
2005-11-18 07:33:28 +0000

    script/console uses RAILS_ENV environment variable if present.  Closes #2932.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@3083 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/commands/console.rb
+++ railties/lib/commands/console.rb
@@ -12,7 +12,7 @@ libs =  " -r irb/completion"
 libs << " -r #{RAILS_ROOT}/config/environment"
 libs << " -r console_sandbox" if options[:sandbox]
 
-ENV['RAILS_ENV'] = ARGV.first || 'development'
+ENV['RAILS_ENV'] = ARGV.first || ENV['RAILS_ENV'] || 'development'
 if options[:sandbox]
   puts "Loading #{ENV['RAILS_ENV']} environment in sandbox."
   puts "Any modifications you make will be rolled back on exit."
