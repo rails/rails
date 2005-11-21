5177333e70cbb454679041b939fd074f166a38b8
Jeremy Kemper <jeremy@bitsweat.net>
2005-11-21 08:34:50 +0000

    Use Kernel.binding rather than binding to allow columns of that name.  References #2973.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@3139 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activerecord/lib/active_record/validations.rb
+++ activerecord/lib/active_record/validations.rb
@@ -254,7 +254,7 @@ module ActiveRecord
       def evaluate_condition(condition, record)
         case condition
           when Symbol: record.send(condition)
-          when String: eval(condition, binding)
+          when String: eval(condition, Kernel.binding)
           else
             if condition_block?(condition)
               condition.call(record)
@@ -764,7 +764,7 @@ module ActiveRecord
           if validation.is_a?(Symbol)
             self.send(validation)
           elsif validation.is_a?(String)
-            eval(validation, binding)
+            eval(validation, Kernel.binding)
           elsif validation_block?(validation)
             validation.call(self)
           elsif validation_class?(validation, validation_method)
