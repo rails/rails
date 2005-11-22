3e51cd8b8e4a9d9c9e7520bde091479067b56acf
Jeremy Kemper <jeremy@bitsweat.net>
2005-11-22 21:33:56 +0000

    Model generator: correct relative path to test_helper in unit test.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@3162 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/rails_generator/generators/components/model/templates/unit_test.rb
+++ railties/lib/rails_generator/generators/components/model/templates/unit_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + '<%= '/..' * class_nesting_depth %>/../test_helper'
 
 class <%= class_name %>Test < Test::Unit::TestCase
   fixtures :<%= table_name %>
