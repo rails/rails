3ed6675c68f5c0531024d160a5bbe2c706b383cc
Jeremy Kemper <jeremy@bitsweat.net>
2005-12-05 21:48:23 +0000

    Clear the connection cache entry when a new connection is established on the same class.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@3222 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activerecord/lib/active_record/base.rb
+++ activerecord/lib/active_record/base.rb
@@ -243,26 +243,6 @@ module ActiveRecord #:nodoc:
     # on to any new database connections made and which can be retrieved on both a class and instance level by calling +logger+.
     cattr_accessor :logger
 
-    @@connection_cache = Hash.new { |h, k| h[k] = Hash.new }
-
-    # Returns the connection currently associated with the class. This can
-    # also be used to "borrow" the connection to do database work unrelated
-    # to any of the specific Active Records.
-    def self.connection
-      @@connection_cache[Thread.current.object_id][name] ||= retrieve_connection
-    end
-
-    def self.clear_connection_cache!
-      @@connection_cache.clear
-    end
-
-    # Returns the connection currently associated with the class. This can
-    # also be used to "borrow" the connection to do database work that isn't
-    # easily done without going straight to SQL.
-    def connection
-      self.class.connection
-    end
-
     def self.inherited(child) #:nodoc:
       @@subclasses[self] ||= []
       @@subclasses[self] << child
