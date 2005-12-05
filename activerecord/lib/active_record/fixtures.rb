b13573ed3796508f224f0b86f13462272c212fe3
Jeremy Kemper <jeremy@bitsweat.net>
2005-12-05 19:58:11 +0000

    Clear connection cache after rolling back transaction.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@3221 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activerecord/lib/active_record/fixtures.rb
+++ activerecord/lib/active_record/fixtures.rb
@@ -503,13 +503,12 @@ module Test #:nodoc:
       alias_method :setup, :setup_with_fixtures
 
       def teardown_with_fixtures
-        ActiveRecord::Base.clear_connection_cache!
-      ensure
         # Rollback changes.
         if use_transactional_fixtures?
           ActiveRecord::Base.connection.rollback_db_transaction
           ActiveRecord::Base.unlock_mutex
         end
+        ActiveRecord::Base.clear_connection_cache!
       end
 
       alias_method :teardown, :teardown_with_fixtures
