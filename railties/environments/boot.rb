2c5d34280962c1d1653cfe4eda4a8df50bf72436
David Heinemeier Hansson <david@loudthinking.com>
2005-12-08 03:54:53 +0000

    Warn people not to change boot.rb
    git-svn-id: http://svn-commit.rubyonrails.org/rails/branches/stable@3232 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/environments/boot.rb
+++ railties/environments/boot.rb
@@ -1,3 +1,5 @@
+# Don't change this file. Configuration is done in config/environment.rb and config/environments/*.rb
+
 unless defined?(RAILS_ROOT)
   root_path = File.join(File.dirname(__FILE__), '..')
   unless RUBY_PLATFORM =~ /mswin32/
