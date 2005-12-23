af60120d30157837d92b02953e04131facd1f7de
Tobias Lütke <tobias.luetke@gmail.com>
2005-12-23 19:47:10 +0000

    cosmetic change to assert_valid
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@3341 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_controller/assertions.rb
+++ actionpack/lib/action_controller/assertions.rb
@@ -304,7 +304,7 @@ module Test #:nodoc:
       # ensures that the passed record is valid by active record standards. returns the error messages if not
       def assert_valid(record)                                   
         clean_backtrace do
-          assert record.valid?, record.errors.full_messages        
+          assert record.valid?, record.errors.full_messages.join("\n")
         end
       end             
       
