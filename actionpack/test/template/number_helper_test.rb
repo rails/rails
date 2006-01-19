363b79f9428395249d7509ddc88e1080902d6256
Marcel Molina <marcel@vernix.org>
2006-01-19 05:31:48 +0000

    Refactor human_size to exclude decimal place if it is zero.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@3437 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/test/template/number_helper_test.rb
+++ actionpack/test/template/number_helper_test.rb
@@ -34,19 +34,20 @@ class NumberHelperTest < Test::Unit::TestCase
   end
 
   def test_number_to_human_size
-    assert_equal("0 Bytes", number_to_human_size(0))
-    assert_equal("3 Bytes", number_to_human_size(3.14159265))
-    assert_equal("123 Bytes", number_to_human_size(123.0))
-    assert_equal("123 Bytes", number_to_human_size(123))
-    assert_equal("1.2 KB", number_to_human_size(1234))
-    assert_equal("12.1 KB", number_to_human_size(12345))
-    assert_equal("1.2 MB", number_to_human_size(1234567))
-    assert_equal("1.1 GB", number_to_human_size(1234567890))
-    assert_equal("1.1 TB", number_to_human_size(1234567890123))
-    assert_equal("444.0 KB", number_to_human_size(444.kilobytes))
-    assert_equal("1023.0 MB", number_to_human_size(1023.megabytes))
-    assert_equal("3.0 TB", number_to_human_size(3.terabytes))
-    assert_nil number_to_human_size('x')
+    assert_equal '0 Bytes',   human_size(0)
+    assert_equal '3 Bytes',   human_size(3.14159265)
+    assert_equal '123 Bytes', human_size(123.0)
+    assert_equal '123 Bytes', human_size(123)
+    assert_equal '1.2 KB',    human_size(1234)
+    assert_equal '12.1 KB',   human_size(12345)
+    assert_equal '1.2 MB',    human_size(1234567)
+    assert_equal '1.1 GB',    human_size(1234567890)
+    assert_equal '1.1 TB',    human_size(1234567890123)
+    assert_equal '444 KB',    human_size(444.kilobytes)
+    assert_equal '1023 MB',   human_size(1023.megabytes)
+    assert_equal '3 TB',      human_size(3.terabytes)
+    assert_nil human_size('x')
+    assert_nil human_size(nil)
   end
 
   def test_number_with_precision
