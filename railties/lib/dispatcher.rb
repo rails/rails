2b02596b964302ecbc38cb39bf820af6e32ee95e
Jeremy Kemper <jeremy@bitsweat.net>
2006-03-01 22:08:00 +0000

    Clear stale, cached connections left behind by defunct threads. Eliminate duplicate connection cache.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@3730 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/dispatcher.rb
+++ railties/lib/dispatcher.rb
@@ -67,13 +67,12 @@ class Dispatcher
         ActionController::Routing::Routes.reload if Dependencies.load?
         prepare_breakpoint
         require_dependency('application.rb') unless Object.const_defined?(:ApplicationController)
-        ActiveRecord::Base.verify_connection_cache!
+        ActiveRecord::Base.verify_active_connections!
       end
-    
+
       def reset_after_dispatch
         reset_application! if Dependencies.load?
         Breakpoint.deactivate_drb if defined?(BREAKPOINT_SERVER_PORT)
-        # ActiveRecord::Base.clear_connection_cache!
       end
 
       def prepare_breakpoint
