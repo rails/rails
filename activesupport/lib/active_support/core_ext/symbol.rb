484e1c65d8710faeb9f08d33898dc329683b4dab
David Heinemeier Hansson <david@loudthinking.com>
2006-04-02 16:28:30 +0000

    Added docs for to_proc
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@4132 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/core_ext/symbol.rb
+++ activesupport/lib/active_support/core_ext/symbol.rb
@@ -1,4 +1,11 @@
-class Symbol #:nodoc:
+class Symbol
+  # Turns the symbol into a simpel proc, which is especially useful for enumerations. Examples:
+  #
+  #   # The same as people.collect { |p| p.name }
+  #   people.collect(&:name)
+  #
+  #   # The same as people.select { |p| p.manager? }.collect { |p| p.salary }
+  #   people.select(&:manager?).collect(&:salary)
   def to_proc
     Proc.new { |obj, *args| obj.send(self, *args) }
   end
