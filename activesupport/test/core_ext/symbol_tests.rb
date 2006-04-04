f4382af5434dc37f0792e1f3591d659f28c30051
Nicholas Seckar <nseckar@gmail.com>
2006-04-04 19:41:38 +0000

    fsking svn...
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@4160 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/test/core_ext/symbol_tests.rb
+++
@@ -1,8 +0,0 @@
-require 'test/unit'
-require File.dirname(__FILE__) + '/../../lib/active_support/core_ext/symbol'
-
-class TestSymbol < Test::Case::TestUnit
-  def test_to_proc
-    assert_equal %w(one two three), [:one, :two, :three].map &:to_s
-  end
-end
