4363bd02391cf43bfd48faaebff5486209b7f93b
David Heinemeier Hansson <david@loudthinking.com>
2006-04-09 22:14:08 +0000

    Use Aras RubyForge uploader
    git-svn-id: http://svn-commit.rubyonrails.org/rails/branches/stable@4204 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- release.rb
+++ release.rb
@@ -1,11 +1,7 @@
 #!/usr/local/bin/ruby
 
+VERSION  = ARGV.first
 PACKAGES = %w( activesupport activerecord actionpack actionmailer actionwebservice )
-
-unless ruby_forge_password = ARGV.first
-  print "rubyforge.org's password: "
-  ruby_forge_password = STDIN.gets.chomp
-end
 
 # Checkout source
 `rm -rf release && svn export http://dev.rubyonrails.org/svn/rails/trunk release`
@@ -14,16 +10,16 @@ end
 `cd release/railties && rake template=jamis package`
 
 # Upload documentation
-`cd release/rails/doc/api && scp -r * davidhh@wrath.rubyonrails.com:public_html/rails`
-PACKAGES.each do |p| 
-  `cd release/#{p} && echo "Publishing documentation for #{p}" && rake template=jamis pdoc`
-end
+`cd release/rails/doc/api && scp -r * davidhh@wrath.rubyonrails.com:public_html/api`
 
 # Upload packages
 (PACKAGES + %w(railties)).each do |p| 
-  `cd release/#{p} && echo "Releasing #{p}" && RUBY_FORGE_PASSWORD=#{ruby_forge_password} rake release`
+  `cd release/#{p} && echo "Releasing #{p}" && rake release`
 end
 
 # Upload rails tgz/zip
+`rubyforge add_release rails rails 'REL #{VERSION}' rails-#{VERSION}.tgz`
+`rubyforge add_release rails rails 'REL #{VERSION}' rails-#{VERSION}.zip`
 
 # Create SVN tag
+puts "Remeber to create SVN tag"
