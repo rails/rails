94e3e22eb8a7e22e68397b10f8fdeac99b0f540c
Marcel Molina <marcel@vernix.org>
2006-04-26 19:29:35 +0000

    Mention in docs that config.frameworks doesn't work when getting Rails via Gems. Closes #4857. [Alisdair McDiarmid]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@4278 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/environments/environment.rb
+++ railties/environments/environment.rb
@@ -13,7 +13,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence those specified here
   
-  # Skip frameworks you're not going to use
+  # Skip frameworks you're not going to use (only works if using vendor/rails)
   # config.frameworks -= [ :action_web_service, :action_mailer ]
 
   # Add additional load paths for your own custom dirs
