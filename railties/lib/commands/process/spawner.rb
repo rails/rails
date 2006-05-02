2b0a1f4a219a7553400010c4ff01d24a9e143169
David Heinemeier Hansson <david@loudthinking.com>
2006-05-02 22:32:45 +0000

    Added -n/--process to script/process/spawner name the process pid (default is dispatch) [DHH]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@4323 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/commands/process/spawner.rb
+++ railties/lib/commands/process/spawner.rb
@@ -95,7 +95,7 @@ ARGV.options do |opts|
   opts.on("-i", "--instances=number", Integer, "Number of instances (default: #{OPTIONS[:instances]})")            { |OPTIONS[:instances]| }
   opts.on("-r", "--repeat=seconds",   Integer, "Repeat spawn attempts every n seconds (default: off)")             { |OPTIONS[:repeat]| }
   opts.on("-e", "--environment=name", String,  "test|development|production (default: #{OPTIONS[:environment]})")  { |OPTIONS[:environment]| }
-  opts.on("-n", "--process=name",     String,  "default: #{OPTIONS[:process]})")                                   { |OPTIONS[:process]| }
+  opts.on("-n", "--process=name",     String,  "default: #{OPTIONS[:process]}")                                    { |OPTIONS[:process]| }
   opts.on("-s", "--spawner=path",     String,  "default: #{OPTIONS[:spawner]}")                                    { |OPTIONS[:spawner]| }
   opts.on("-d", "--dispatcher=path",  String,  "default: #{OPTIONS[:dispatcher]}") { |dispatcher| OPTIONS[:dispatcher] = File.expand_path(dispatcher) }
 
