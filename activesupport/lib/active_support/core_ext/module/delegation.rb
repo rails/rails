befd62c2fc04356980c4de493ffdee40e7a36c87
Jeremy Kemper <jeremy@bitsweat.net>
2006-05-08 18:04:07 +0000

    Document Active Support's Module::delegate. Closes #5002.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@4329 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/core_ext/module/delegation.rb
+++ activesupport/lib/active_support/core_ext/module/delegation.rb
@@ -1,8 +1,33 @@
 class Module
+  # Provides a delegate class method to easily expose contained objects' methods
+  # as your own. Pass one or more methods (specified as symbols or strings)
+  # and the name of the target object as the final :to option (also a symbol
+  # or string).  At least one method and the :to option are required.
+  #
+  # Delegation is particularly useful with Active Record associations:
+  #   class Greeter < ActiveRecord::Base
+  #     def hello()   "hello"   end
+  #     def goodbye() "goodbye" end
+  #   end
+  #
+  #   class Foo < ActiveRecord::Base
+  #     belongs_to :greeter
+  #     delegate :hello, :to => :greeter
+  #   end
+  #
+  #   Foo.new.hello   # => "hello"
+  #   Foo.new.goodbye # => NoMethodError: undefined method `goodbye' for #<Foo:0x1af30c>
+  #
+  # With multiple delegates to the same target are allowed:
+  #   class Foo < ActiveRecord::Base
+  #     delegate :hello, :goodbye, :to => :greeter
+  #   end
+  #
+  #   Foo.new.goodbye # => "goodbye"
   def delegate(*methods)
     options = methods.pop
     unless options.is_a?(Hash) && to = options[:to]
-      raise ArgumentError, "Delegation needs a target. Supply an options hash with a :to key"
+      raise ArgumentError, "Delegation needs a target. Supply an options hash with a :to key as the last argument (e.g. delegate :hello, :to => :greeter)."
     end
 
     methods.each do |method|
