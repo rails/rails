c02d1f7d9b73f708d8f656044b096454344ccb78
Marcel Molina <marcel@vernix.org>
2006-06-28 13:51:25 +0000

    Fix CGI extensions when they expect string but get nil in Windows. Closes #5276 [mislav@nippur.irb.hr]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@4499 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_controller/cgi_ext/raw_post_data_fix.rb
+++ actionpack/lib/action_controller/cgi_ext/raw_post_data_fix.rb
@@ -36,7 +36,7 @@ class CGI #:nodoc:
         if boundary = extract_multipart_form_boundary(content_type)
           @multipart = true
           @params = read_multipart(boundary, content_length)
-        elsif content_type.downcase != 'application/x-www-form-urlencoded'
+        elsif content_type.blank? || content_type.downcase != 'application/x-www-form-urlencoded'
           read_params(method, content_length)
           @params = {}
         end
