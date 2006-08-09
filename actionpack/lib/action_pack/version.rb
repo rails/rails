0d21068cf712437f2185469d3da15f93e4214634
David Heinemeier Hansson <david@loudthinking.com>
2006-08-09 05:05:26 +0000

    Preparing for Rails 1.1.5
    git-svn-id: http://svn-commit.rubyonrails.org/rails/branches/stable@4738 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_pack/version.rb
+++ actionpack/lib/action_pack/version.rb
@@ -2,7 +2,7 @@ module ActionPack #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 1
     MINOR = 12
-    TINY  = 3
+    TINY  = 4
     
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
--- activerecord/lib/active_record/version.rb
+++ activerecord/lib/active_record/version.rb
@@ -2,7 +2,7 @@ module ActiveRecord
   module VERSION #:nodoc:
     MAJOR = 1
     MINOR = 14
-    TINY  = 3
+    TINY  = 4
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
