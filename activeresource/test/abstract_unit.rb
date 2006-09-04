c003a4acea24aad279b8cd6eec49e20bc45859f3
Jeremy Kemper <jeremy@bitsweat.net>
2006-09-04 09:50:15 +0000

    consistent load path to prevent double loads
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@4984 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activeresource/test/abstract_unit.rb
+++ activeresource/test/abstract_unit.rb
@@ -1,8 +1,7 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
-$:.unshift(File.dirname(__FILE__) + '/.')
-
 require 'active_resource'
 require 'test/unit'
 require 'active_support/breakpoint'
 
-require "#{File.dirname(__FILE__)}/http_mock"
+$:.unshift(File.dirname(__FILE__) + '/.')
+require 'http_mock'
