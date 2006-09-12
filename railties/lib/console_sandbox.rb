3c0e7b1b51cb775e55a4b10a9872f74cecacd6a7
Jeremy Kemper <jeremy@bitsweat.net>
2006-09-12 01:17:11 +0000

    Fix script/console --sandbox for internal transactions changes. Closes #5738.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@5088 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/console_sandbox.rb
+++ railties/lib/console_sandbox.rb
@@ -1,6 +1,6 @@
-ActiveRecord::Base.lock_mutex
+ActiveRecord::Base.send :increment_open_transactions
 ActiveRecord::Base.connection.begin_db_transaction
 at_exit do
   ActiveRecord::Base.connection.rollback_db_transaction
-  ActiveRecord::Base.unlock_mutex
+  ActiveRecord::Base.send :decrement_open_transactions
 end
