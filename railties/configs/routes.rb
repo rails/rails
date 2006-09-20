5ddc82c3a11a71806ea91f66fdb28fbbe30716f7
David Heinemeier Hansson <david@loudthinking.com>
2006-09-20 09:33:59 +0000

    Added map.connect ':controller/:action/:id.:format' as a default route to config/routes.rb [DHH]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@5148 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/configs/routes.rb
+++ railties/configs/routes.rb
@@ -18,5 +18,6 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/service.wsdl', :action => 'wsdl'
 
   # Install the default route as the lowest priority.
+  map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action/:id'
 end
