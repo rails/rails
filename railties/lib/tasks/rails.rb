b96fbe81b8e998b2880eeaffdd072347d851b943
Jeremy Kemper <jeremy@bitsweat.net>
2006-09-22 00:03:05 +0000

    Look for rake tasks in plugin subdirs. Closes #6259.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@5159 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/tasks/rails.rb
+++ railties/lib/tasks/rails.rb
@@ -5,4 +5,4 @@ Dir["#{File.dirname(__FILE__)}/*.rake"].each { |ext| load ext }
 
 # Load any custom rakefile extensions
 Dir["./lib/tasks/**/*.rake"].sort.each { |ext| load ext }
-Dir["./vendor/plugins/*/tasks/**/*.rake"].sort.each { |ext| load ext }
+Dir["./vendor/plugins/**/tasks/**/*.rake"].sort.each { |ext| load ext }
