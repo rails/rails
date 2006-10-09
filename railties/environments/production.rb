0d8b8056be56b1ec988ca7c7db44f5b3768792e9
David Heinemeier Hansson <david@loudthinking.com>
2006-10-09 02:20:43 +0000

    Docfix (closes #6234)
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@5262 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/environments/production.rb
+++ railties/environments/production.rb
@@ -14,5 +14,5 @@ config.action_controller.perform_caching             = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
-# Disable delivery errors if you bad email addresses should just be ignored
+# Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
