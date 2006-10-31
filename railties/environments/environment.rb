7e8dd0322c99949fb77b4ea2b42a667f796475fa
Michael Koziarski <michael@koziarski.com>
2006-10-31 01:33:33 +0000

    Revert environment.rb changes for autoloading
    git-svn-id: http://svn-commit.rubyonrails.org/rails/branches/1-2-pre-release@5377 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/environments/environment.rb
+++ railties/environments/environment.rb
@@ -19,12 +19,8 @@ Rails::Initializer.run do |config|
   # Only load the plugins named here, by default all plugins in vendor/plugins are loaded
   # config.plugins = %W( exception_notification ssl_requirement )
 
-  # Add additional load paths (these are searched when explicitly require something)
+  # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-  # Add additional autoloading load paths
-  # (these paths will be subject to auto-loading of constants)
-  # config.autoload_paths += Dir["#{RAILS_ROOT}/models/*/"]
 
   # Force all environments to use the same logger level 
   # (by default production uses :info, the others :debug)
