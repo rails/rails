9f8c805f5d45b93269e13249472586dea4bf651b
Sam Stephenson <sam@37signals.com>
2006-11-12 03:47:27 +0000

    Merge [5486] from trunk.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/branches/1-2-pre-release@5498 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/json.rb
+++ activesupport/lib/active_support/json.rb
@@ -4,14 +4,20 @@ module ActiveSupport
   module JSON #:nodoc:
     class CircularReferenceError < StandardError #:nodoc:
     end
-    # returns the literal string as its JSON encoded form.  Useful for passing javascript variables into functions.
-    #
-    # page.call 'Element.show', ActiveSupport::JSON::Variable.new("$$(#items li)")
+    
+    # A string that returns itself as as its JSON-encoded form.
     class Variable < String #:nodoc:
       def to_json
         self
       end
     end
+    
+    # When +true+, Hash#to_json will omit quoting string or symbol keys
+    # if the keys are valid JavaScript identifiers.  Note that this is
+    # technically improper JSON (all object keys must be quoted), so if
+    # you need strict JSON compliance, set this option to +false+.
+    mattr_accessor :unquote_hash_key_identifiers
+    @@unquote_hash_key_identifiers = true
 
     class << self
       REFERENCE_STACK_VARIABLE = :json_reference_stack
@@ -20,6 +26,11 @@ module ActiveSupport
         raise_on_circular_reference(value) do
           Encoders[value.class].call(value)
         end
+      end
+      
+      def can_unquote_identifier?(key)
+        return false unless unquote_hash_key_identifiers
+        key.to_s =~ /^[[:alpha:]_$][[:alnum:]_$]*$/
       end
       
       protected
