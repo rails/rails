afe49dcf5b0219afd989aaeb1ff6fc9ab6b70a43
Jeremy Kemper <jeremy@bitsweat.net>
2006-11-13 00:53:20 +0000

    update_element_function is deprecated
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@5501 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/test/controller/capture_test.rb
+++ actionpack/test/controller/capture_test.rb
@@ -7,15 +7,15 @@ class CaptureController < ActionController::Base
   def content_for
     render :layout => "talk_from_action"
   end
-  
+
   def erb_content_for
     render :layout => "talk_from_action"
   end
-  
+
   def block_content_for
     render :layout => "talk_from_action"
   end
-  
+
   def non_erb_block_content_for
     render :layout => "talk_from_action"
   end
@@ -43,36 +43,38 @@ class CaptureTest < Test::Unit::TestCase
     get :capturing
     assert_equal "Dreamy days", @response.body.strip
   end
-  
+
   def test_content_for
     get :content_for
     assert_equal expected_content_for_output, @response.body
   end
-  
+
   def test_erb_content_for
     get :content_for
     assert_equal expected_content_for_output, @response.body
   end
-  
+
   def test_block_content_for
     get :block_content_for
     assert_equal expected_content_for_output, @response.body
   end
-  
+
   def test_non_erb_block_content_for
     get :non_erb_block_content_for
     assert_equal expected_content_for_output, @response.body
   end
 
   def test_update_element_with_capture
-    get :update_element_with_capture
+    assert_deprecated 'update_element_function' do
+      get :update_element_with_capture
+    end
     assert_equal(
       "<script type=\"text/javascript\">\n//<![CDATA[\n$('products').innerHTML = '\\n  <p>Product 1</p>\\n  <p>Product 2</p>\\n';\n\n//]]>\n</script>" +
-        "\n\n$('status').innerHTML = '\\n  <b>You bought something!</b>\\n';", 
+        "\n\n$('status').innerHTML = '\\n  <b>You bought something!</b>\\n';",
       @response.body.strip
     )
   end
-  
+
   private
   def expected_content_for_output
     "<title>Putting stuff in the title!</title>\n\nGreat stuff!"
