f6e85e4ec68d677a90986b47f71ac762fc19c3f7
Michael Koziarski <michael@koziarski.com>
2006-11-27 23:56:27 +0000

    Merge [5641] to Trunk
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@5642 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_controller/integration.rb
+++ actionpack/lib/action_controller/integration.rb
@@ -113,7 +113,7 @@ module ActionController
       # performed on the location header.
       def follow_redirect!
         raise "not a redirect! #{@status} #{@status_message}" unless redirect?
-        get(interpret_uri(headers['Location'].first))
+        get(interpret_uri(headers['location'].first))
         status
       end
 
