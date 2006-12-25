1fb2f21d1a7776b972e9c828225c686c19e93326
Rick Olson <technoweenie@gmail.com>
2006-12-25 09:37:06 +0000

    apply [5784] to 1.2 pre-release
    git-svn-id: http://svn-commit.rubyonrails.org/rails/branches/1-2-pre-release@5787 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/webrick_server.rb
+++ railties/lib/webrick_server.rb
@@ -66,6 +66,9 @@ class DispatchServlet < WEBrick::HTTPServlet::AbstractServlet
   def initialize(server, options) #:nodoc:
     @server_options = options
     @file_handler = WEBrick::HTTPServlet::FileHandler.new(server, options[:server_root])
+    # Change to the RAILS_ROOT, since Webrick::Daemon.start does a Dir::cwd("/") 
+    # OPTIONS['working_directory'] is an absolute path of the RAILS_ROOT, set in railties/lib/commands/servers/webrick.rb 
+    Dir.chdir(OPTIONS['working_directory']) if defined?(OPTIONS) && File.directory?(OPTIONS['working_directory']) 
     super
   end
 
