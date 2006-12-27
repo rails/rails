d88e83f211e47a8fa8220c880cf7a7309e552503
David Heinemeier Hansson <david@loudthinking.com>
2006-12-27 00:42:31 +0000

    False is a reasonable default
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@5793 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/core_ext/module/attr_accessor_with_default.rb
+++ activesupport/lib/active_support/core_ext/module/attr_accessor_with_default.rb
@@ -19,7 +19,7 @@ class Module
   #   attr_accessor_with_default(:element_name) { name.underscore } 
   #
   def attr_accessor_with_default(sym, default = nil, &block)
-    raise 'Default value or block required' unless default || block
+    raise 'Default value or block required' unless !default.nil? || block
     define_method(sym, block_given? ? block : Proc.new { default })
     module_eval(<<-EVAL, __FILE__, __LINE__)
       def #{sym}=(value)
