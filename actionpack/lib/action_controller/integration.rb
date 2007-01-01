c26cca3f458b07b144af15fb9ec9ad2b3319be5e
Jamis Buck <jamis@37signals.com>
2007-01-01 03:04:23 +0000

    Make sure html_document is reset between integration test requests
    git-svn-id: http://svn-commit.rubyonrails.org/rails/branches/1-2-pre-release@5829 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_controller/integration.rb
+++ actionpack/lib/action_controller/integration.rb
@@ -493,6 +493,8 @@ module ActionController
     %w(get post cookies assigns xml_http_request).each do |method|
       define_method(method) do |*args|
         reset! unless @integration_session
+        # reset the html_document variable, but only for new get/post calls
+        @html_document = nil unless %w(cookies assigns).include?(method)
         returning @integration_session.send(method, *args) do
           copy_session_variables!
         end
