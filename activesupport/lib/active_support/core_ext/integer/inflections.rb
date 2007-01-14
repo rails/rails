68849e3239f1e363da02e6ffd3457f281167de4a
Jeremy Kemper <jeremy@bitsweat.net>
2007-01-14 12:20:52 +0000

    Merge [5924] from trunk. References #7023.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/branches/1-2-pre-release@5925 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/core_ext/integer/inflections.rb
+++ activesupport/lib/active_support/core_ext/integer/inflections.rb
@@ -1,11 +1,17 @@
-require File.dirname(__FILE__) + '/../../inflector' unless defined? Inflector
+require 'active_support/inflector'
+
 module ActiveSupport #:nodoc:
   module CoreExtensions #:nodoc:
     module Integer #:nodoc:
       module Inflections
-        # 1.ordinalize  # => "1st"
-        # 3.ordinalize  # => "3rd"
-        # 10.ordinalize # => "10th"
+        # Ordinalize turns a number into an ordinal string used to denote the
+        # position in an ordered sequence such as 1st, 2nd, 3rd, 4th.
+        #
+        # Examples
+        #   1.ordinalize    # => "1st"
+        #   2.ordinalize    # => "2nd"
+        #   1002.ordinalize # => "1002nd"
+        #   1003.ordinalize # => "1003rd"
         def ordinalize
           Inflector.ordinalize(self)
         end
--- activesupport/lib/active_support/core_ext/string/inflections.rb
+++ activesupport/lib/active_support/core_ext/string/inflections.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../../inflector' unless defined? Inflector
+require 'active_support/inflector'
 
 module ActiveSupport #:nodoc:
   module CoreExtensions #:nodoc:
