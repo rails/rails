a3dafb07ec64d3ae7f4fe1becf3b51105901418e
David Heinemeier Hansson <david@loudthinking.com>
2007-01-17 04:41:02 +0000

    Prepare for release of 1.2
    git-svn-id: http://svn-commit.rubyonrails.org/rails/branches/1-2-pre-release@5968 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_pack/version.rb
+++ actionpack/lib/action_pack/version.rb
@@ -1,8 +1,8 @@
 module ActionPack #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 1
-    MINOR = 12
-    TINY  = 5
+    MINOR = 13
+    TINY  = 0
     
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
