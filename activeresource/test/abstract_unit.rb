1d8cd1d744fd1e8b25aa764ddd59ef1aa100eff4
Jeremy Kemper <jeremy@bitsweat.net>
2007-01-24 18:29:07 +0000

    Squash test warnings.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@6033 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activeresource/test/abstract_unit.rb
+++ activeresource/test/abstract_unit.rb
@@ -5,32 +5,7 @@ require 'active_resource'
 require 'active_resource/http_mock'
 require 'active_support/breakpoint'
 
-ActiveResource::Base.logger = Logger.new("#{File.dirname(__FILE__)}/debug.log")
+$:.unshift "#{File.dirname(__FILE__)}/../test"
+require 'setter_trap'
 
-class SetterTrap < Builder::BlankSlate
-  class << self
-    def rollback_sets(obj)
-      returning yield(setter_trap = new(obj)) do
-        setter_trap.rollback_sets
-      end
-    end
-  end
-  
-  def initialize(obj)
-    @cache = {}
-    @obj = obj
-  end
-  
-  def respond_to?(method)
-    @obj.respond_to?(method)
-  end
-  
-  def method_missing(method, *args, &proc)
-    @cache[method] = @obj.send(method.to_s[0 ... -1]) if method.to_s[-1 .. -1] == "=" unless @cache[method]
-    @obj.send method, *args, &proc
-  end
-  
-  def rollback_sets
-    @cache.each { |k, v| @obj.send k, v }
-  end
-end
+ActiveResource::Base.logger = Logger.new("#{File.dirname(__FILE__)}/debug.log")
---
+++ activeresource/test/setter_trap.rb
@@ -0,0 +1,29 @@
+require 'builder'
+
+class SetterTrap < Builder::BlankSlate
+  class << self
+    def rollback_sets(obj)
+      returning yield(setter_trap = new(obj)) do
+        setter_trap.rollback_sets
+      end
+    end
+  end
+
+  def initialize(obj)
+    @cache = {}
+    @obj = obj
+  end
+
+  def respond_to?(method)
+    @obj.respond_to?(method)
+  end
+
+  def method_missing(method, *args, &proc)
+    @cache[method] ||= @obj.send($`) if method.to_s =~ /=$/
+    @obj.send method, *args, &proc
+  end
+
+  def rollback_sets
+    @cache.each { |k, v| @obj.send k, v }
+  end
+end
