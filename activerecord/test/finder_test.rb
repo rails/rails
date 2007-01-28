7ce675352653dfd1a538bee82d67af99b442047a
Jeremy Kemper <jeremy@bitsweat.net>
2007-01-28 09:11:15 +0000

    Post title and body not null. References #6778.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@6067 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activerecord/test/finder_test.rb
+++ activerecord/test/finder_test.rb
@@ -462,7 +462,7 @@ class FinderTest < Test::Unit::TestCase
 
   # http://dev.rubyonrails.org/ticket/6778
   def test_find_ignores_previously_inserted_record
-    post = Post.create!
+    post = Post.create!(:title => 'test', :body => 'it out')
     assert_equal [], Post.find_all_by_id(nil)
   end
 
