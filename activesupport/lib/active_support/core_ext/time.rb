593b1b837d24e61ea8f5386d66669585f3c14d5a
Jeremy Kemper <jeremy@bitsweat.net>
2007-02-01 18:06:16 +0000

    Ruby 1.8-cvs and 1.9 define a private Time#to_date that overrides Active Support. Make it public to preserve compatibility.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@6099 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/core_ext/time.rb
+++ activesupport/lib/active_support/core_ext/time.rb
@@ -1,3 +1,13 @@
+require 'date'
+require 'time'
+
+# Ruby 1.8-cvs and 1.9 define private Time#to_date
+class Time
+  %w(to_date to_datetime).each do |method|
+    public method if private_instance_methods.include?(method)
+  end
+end
+
 require File.dirname(__FILE__) + '/time/behavior'
 require File.dirname(__FILE__) + '/time/calculations'
 require File.dirname(__FILE__) + '/time/conversions'
