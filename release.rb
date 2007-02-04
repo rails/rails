ed6509c843d3ba76ac993f60a40fccd51ba0b305
David Heinemeier Hansson <david@loudthinking.com>
2007-02-04 22:04:19 +0000

    Update release script on stable branch
    git-svn-id: http://svn-commit.rubyonrails.org/rails/branches/1-2-stable@6123 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- release.rb
+++ release.rb
@@ -4,7 +4,7 @@ VERSION  = ARGV.first
 PACKAGES = %w( activesupport activerecord actionpack actionmailer actionwebservice )
 
 # Checkout source
-`rm -rf release && svn export http://dev.rubyonrails.org/svn/rails/trunk release`
+`rm -rf release && svn export http://dev.rubyonrails.org/svn/rails/branches/1-2-stable release`
 
 # Create Rails packages
 `cd release/railties && rake template=jamis package`
