45dd256889cddd1a1746d122344a966873e03921
Jeremy Kemper <jeremy@bitsweat.net>
2007-02-21 23:13:32 +0000

    Missed commit for [6184]. Generate a random secret for new app's cookie session store.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@6197 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/helpers/application.rb
+++ railties/helpers/application.rb
@@ -2,6 +2,9 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  # Pick a unique cookie name to distinguish our session data from others'
-  session :session_key => '_<%= app_name %>_session_id'
+  # Pick a unique cookie name to distinguish our session from others.
+  # Session data is stored in a cookie by default, so the data is hashed
+  # with a secret to ensure its integrity.
+  session :session_key => '_<%= app_name %>_session',
+          :secret => '<%= CGI::Session.generate_unique_id(app_name) %>'
 end
