1902ef6caa68329aad0cf6ebfebdaa9ce2ea8427
Michael Koziarski <michael@koziarski.com>
2007-02-27 23:25:16 +0000

    Give DateTime correct .to_s implementations, lets it play nice with ActiveRecord quoting. [gbuesing] Closes #7649
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@6263 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/core_ext/date_time.rb
+++ activesupport/lib/active_support/core_ext/date_time.rb
@@ -1,4 +1,6 @@
 require 'date'
 require "#{File.dirname(__FILE__)}/time/behavior"
+require "#{File.dirname(__FILE__)}/date_time/conversions"
 
 DateTime.send(:include, ActiveSupport::CoreExtensions::Time::Behavior)
+DateTime.send(:include, ActiveSupport::CoreExtensions::DateTime::Conversions)
