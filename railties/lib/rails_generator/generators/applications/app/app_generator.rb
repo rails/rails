8a9496cb4e795e8b626117195d9f25f2b7e9c2f8
Nicholas Seckar <nseckar@gmail.com>
2007-03-14 23:45:17 +0000

    Add environment variable RAILS_DEFAULT_DATABASE, which allows the builtin default of 'mysql' to be overridden.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@6429 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/rails_generator/generators/applications/app/app_generator.rb
+++ railties/lib/rails_generator/generators/applications/app/app_generator.rb
@@ -7,7 +7,8 @@ class AppGenerator < Rails::Generator::Base
 
   DATABASES = %w(mysql oracle postgresql sqlite2 sqlite3 frontbase)
 
-  default_options   :db => "mysql", :shebang => DEFAULT_SHEBANG, :freeze => false
+  default_options   :db => (ENV["RAILS_DEFAULT_DATABASE"] || "mysql"),
+    :shebang => DEFAULT_SHEBANG, :freeze => false
   mandatory_options :source => "#{File.dirname(__FILE__)}/../../../../.."
 
   def initialize(runtime_args, runtime_options = {})
