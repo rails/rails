b445a74c5e1cdb10333b02515e42b584463ae10b
David Heinemeier Hansson <david@loudthinking.com>
2007-04-24 18:09:56 +0000

    Removed default WSDL route now that AWS is debundled from Rails
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@6573 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/configs/routes.rb
+++ railties/configs/routes.rb
@@ -19,9 +19,6 @@ ActionController::Routing::Routes.draw do |map|
   # -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
 
-  # Allow downloading Web Service WSDL as a file with an extension instead of a file named 'wsdl'
-  map.connect ':controller/service.wsdl', :action => 'wsdl'
-
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id'
 end
