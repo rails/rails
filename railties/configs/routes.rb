6ffa68e9ebfc9403bb0454ec3a8d8346a812585e
David Heinemeier Hansson <david@loudthinking.com>
2007-04-29 14:05:34 +0000

    Include sub-resources example
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@6631 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/configs/routes.rb
+++ railties/configs/routes.rb
@@ -15,8 +15,10 @@ ActionController::Routing::Routes.draw do |map|
   # Sample resource route with options:
   # map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
 
-  # You can have the root of your site routed with map.root
-  # -- just remember to delete public/index.html.
+  # Sample resource route with sub-resources:
+  # map.resources :products, :has_many => [ :comments, :sales ], :has_ony => :seller
+
+  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
 
   # Install the default routes as the lowest priority.
