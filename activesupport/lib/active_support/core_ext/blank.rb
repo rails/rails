788d4b0cc3a4f875b93cdeecae4b6e04edfef4b2
Jeremy Kemper <jeremy@bitsweat.net>
2007-05-20 23:22:09 +0000

    Merge [6793] to stable from trunk: document Object#blank?. References #6491.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/branches/1-2-stable@6794 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/core_ext/blank.rb
+++ activesupport/lib/active_support/core_ext/blank.rb
@@ -1,5 +1,11 @@
-class Object #:nodoc:
-  # "", "   ", nil, [], and {} are blank
+class Object
+  # An object is blank if it's nil, empty, or a whitespace string.
+  # For example, "", "   ", nil, [], and {} are blank.
+  #
+  # This simplifies
+  #   if !address.nil? && !address.empty?
+  # to
+  #   if !address.blank?
   def blank?
     if respond_to?(:empty?) && respond_to?(:strip)
       empty? or strip.empty?
