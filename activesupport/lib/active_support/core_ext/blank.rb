ae2db50873a760162053ed43a01c8d84872bd7c8
Jeremy Kemper <jeremy@bitsweat.net>
2007-05-20 23:19:10 +0000

    Document Object#blank?. Closes #6491.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@6793 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/core_ext/blank.rb
+++ activesupport/lib/active_support/core_ext/blank.rb
@@ -1,6 +1,12 @@
-class Object 
-  # "", "   ", nil, [], and {} are blank
-  def blank? #:nodoc:
+class Object
+  # An object is blank if it's nil, empty, or a whitespace string.
+  # For example, "", "   ", nil, [], and {} are blank.
+  #
+  # This simplifies
+  #   if !address.nil? && !address.empty?
+  # to
+  #   if !address.blank?
+  def blank?
     if respond_to?(:empty?) && respond_to?(:strip)
       empty? or strip.empty?
     elsif respond_to?(:empty?)
