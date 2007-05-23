a11374ad5a48b76455b352b8336489e9f79b00b2
Jeremy Kemper <jeremy@bitsweat.net>
2007-05-23 07:03:31 +0000

    whiny nil shouldn't depend on Active Record
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@6818 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/whiny_nil.rb
+++ activesupport/lib/active_support/whiny_nil.rb
@@ -1,23 +1,23 @@
-# Extensions to nil which allow for more helpful error messages for 
+# Extensions to nil which allow for more helpful error messages for
 # people who are new to rails.
 #
 # The aim is to ensure that when users pass nil to methods where that isn't
 # appropriate, instead of NoMethodError and the name of some method used
-# by the framework users will see a message explaining what type of object 
+# by the framework users will see a message explaining what type of object
 # was expected.
 
 class NilClass
-  WHINERS = [ ::ActiveRecord::Base, ::Array ]
-  
+  WHINERS = [::Array]
+  WHINERS << ::ActiveRecord::Base if defined? ::ActiveRecord
+
   @@method_class_map = Hash.new
-  
+
   WHINERS.each do |klass|
     methods = klass.public_instance_methods - public_instance_methods
-    methods.each do |method|
-      @@method_class_map[method.to_sym] = klass
-    end
+    class_name = klass.name
+    methods.each { |method| @@method_class_map[method.to_sym] = class_name }
   end
-  
+
   def id
     raise RuntimeError, "Called id for nil, which would mistakenly be 4 -- if you really wanted the id of nil, use object_id", caller
   end
@@ -27,11 +27,11 @@ class NilClass
       raise_nil_warning_for @@method_class_map[method], method, caller
     end
 
-    def raise_nil_warning_for(klass = nil, selector = nil, with_caller = nil)
+    def raise_nil_warning_for(class_name = nil, selector = nil, with_caller = nil)
       message = "You have a nil object when you didn't expect it!"
-      message << "\nYou might have expected an instance of #{klass}." if klass
+      message << "\nYou might have expected an instance of #{class_name}." if class_name
       message << "\nThe error occurred while evaluating nil.#{selector}" if selector
-      
+
       raise NoMethodError, message, with_caller || caller
     end
 end
