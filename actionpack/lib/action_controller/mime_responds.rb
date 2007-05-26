672941decb854ade7979c098c6a42634a58d2365
Jeremy Kemper <jeremy@bitsweat.net>
2007-05-26 03:11:34 +0000

    Introduce a default respond_to block for custom types. Closes #8174.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@6856 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_controller/mime_responds.rb
+++ actionpack/lib/action_controller/mime_responds.rb
@@ -11,51 +11,51 @@ module ActionController #:nodoc:
       #   def index
       #     @people = Person.find(:all)
       #   end
-      # 
+      #
       # Here's the same action, with web-service support baked in:
-      # 
+      #
       #   def index
       #     @people = Person.find(:all)
-      # 
+      #
       #     respond_to do |format|
       #       format.html
       #       format.xml { render :xml => @people.to_xml }
       #     end
       #   end
-      # 
-      # What that says is, "if the client wants HTML in response to this action, just respond as we 
-      # would have before, but if the client wants XML, return them the list of people in XML format." 
+      #
+      # What that says is, "if the client wants HTML in response to this action, just respond as we
+      # would have before, but if the client wants XML, return them the list of people in XML format."
       # (Rails determines the desired response format from the HTTP Accept header submitted by the client.)
-      # 
-      # Supposing you have an action that adds a new person, optionally creating their company 
+      #
+      # Supposing you have an action that adds a new person, optionally creating their company
       # (by name) if it does not already exist, without web-services, it might look like this:
-      # 
+      #
       #   def create
       #     @company = Company.find_or_create_by_name(params[:company][:name])
       #     @person  = @company.people.create(params[:person])
-      # 
+      #
       #     redirect_to(person_list_url)
       #   end
-      # 
+      #
       # Here's the same action, with web-service support baked in:
-      # 
+      #
       #   def create
       #     company  = params[:person].delete(:company)
       #     @company = Company.find_or_create_by_name(company[:name])
       #     @person  = @company.people.create(params[:person])
-      # 
+      #
       #     respond_to do |format|
       #       format.html { redirect_to(person_list_url) }
       #       format.js
       #       format.xml  { render :xml => @person.to_xml(:include => @company) }
       #     end
       #   end
-      # 
-      # If the client wants HTML, we just redirect them back to the person list. If they want Javascript 
-      # (wants.js), then it is an RJS request and we render the RJS template associated with this action. 
-      # Lastly, if the client wants XML, we render the created person as XML, but with a twist: we also 
-      # include the person’s company in the rendered XML, so you get something like this:
-      # 
+      #
+      # If the client wants HTML, we just redirect them back to the person list. If they want Javascript
+      # (wants.js), then it is an RJS request and we render the RJS template associated with this action.
+      # Lastly, if the client wants XML, we render the created person as XML, but with a twist: we also
+      # include the person's company in the rendered XML, so you get something like this:
+      #
       #   <person>
       #     <id>...</id>
       #     ...
@@ -65,67 +65,57 @@ module ActionController #:nodoc:
       #       ...
       #     </company>
       #   </person>
-      # 
+      #
       # Note, however, the extra bit at the top of that action:
-      # 
+      #
       #   company  = params[:person].delete(:company)
       #   @company = Company.find_or_create_by_name(company[:name])
-      # 
-      # This is because the incoming XML document (if a web-service request is in process) can only contain a 
+      #
+      # This is because the incoming XML document (if a web-service request is in process) can only contain a
       # single root-node. So, we have to rearrange things so that the request looks like this (url-encoded):
-      # 
+      #
       #   person[name]=...&person[company][name]=...&...
-      # 
+      #
       # And, like this (xml-encoded):
-      # 
+      #
       #   <person>
       #     <name>...</name>
       #     <company>
       #       <name>...</name>
       #     </company>
       #   </person>
-      # 
-      # In other words, we make the request so that it operates on a single entity—a person. Then, in the action, 
-      # we extract the company data from the request, find or create the company, and then create the new person 
+      #
+      # In other words, we make the request so that it operates on a single entity's person. Then, in the action,
+      # we extract the company data from the request, find or create the company, and then create the new person
       # with the remaining data.
-      # 
-      # Note that you can define your own XML parameter parser which would allow you to describe multiple entities 
-      # in a single request (i.e., by wrapping them all in a single root note), but if you just go with the flow 
+      #
+      # Note that you can define your own XML parameter parser which would allow you to describe multiple entities
+      # in a single request (i.e., by wrapping them all in a single root note), but if you just go with the flow
       # and accept Rails' defaults, life will be much easier.
-      # 
+      #
       # If you need to use a MIME type which isn't supported by default, you can register your own handlers in
       # environment.rb as follows.
-      # 
+      #
       #   Mime::Type.register "image/jpg", :jpg
       def respond_to(*types, &block)
         raise ArgumentError, "respond_to takes either types or a block, never both" unless types.any? ^ block
         block ||= lambda { |responder| types.each { |type| responder.send(type) } }
-        responder = Responder.new(block.binding)
+        responder = Responder.new(self)
         block.call(responder)
         responder.respond
       end
     end
-    
-    class Responder #:nodoc:
-      default_block_format = <<-END
-        Proc.new { 
-          @template.template_format = '%s'
-          render :action => "\#{action_name}", :content_type => Mime::%s
-        }
-      END
 
-      DEFAULT_BLOCKS = [:html, :js, :xml].inject({}) do |memo, ext| 
-        default_block = default_block_format % [ext, ext.to_s.upcase]
-        memo.update(ext => default_block)
-      end
-      
-      def initialize(block_binding)
-        @block_binding = block_binding
-        @mime_type_priority = eval(
-          "(params[:format] && Mime::EXTENSION_LOOKUP[params[:format]]) ? " +
-          "[ Mime::EXTENSION_LOOKUP[params[:format]] ] : request.accepts", 
-          block_binding
-        )
+    class Responder #:nodoc:
+      def initialize(controller)
+        @controller    = controller
+        @request       = controller.request
+        @response      = controller.response
+
+        format = @request.parameters[:format]
+        @mime_type_priority = format && Mime::EXTENSION_LOOKUP[format] ?
+          [ Mime::EXTENSION_LOOKUP[format] ] :
+          @request.accepts
 
         @order     = []
         @responses = {}
@@ -133,22 +123,20 @@ module ActionController #:nodoc:
 
       def custom(mime_type, &block)
         mime_type = mime_type.is_a?(Mime::Type) ? mime_type : Mime::Type.lookup(mime_type.to_s)
-        
+
         @order << mime_type
-        
+
         if block_given?
           @responses[mime_type] = Proc.new do
-            eval <<-END, @block_binding
-              @template.template_format = '#{mime_type.to_sym}'
-              response.content_type     = '#{mime_type.to_s}'
-            END
+            @response.template.template_format = mime_type.to_sym
+            @response.content_type = mime_type.to_s
             block.call
           end
         else
-          if source = DEFAULT_BLOCKS[mime_type.to_sym]
-            @responses[mime_type] = eval(source, @block_binding)
-          else
-            raise ActionController::RenderError, "Expected a block but none was given for custom mime handler #{mime_type}"
+          @responses[mime_type] = Proc.new do
+            @response.template.template_format = mime_type.to_sym
+            @response.content_type = mime_type.to_s
+            @controller.send :render, :action => @controller.action_name
           end
         end
       end
@@ -156,17 +144,17 @@ module ActionController #:nodoc:
       def any(*args, &block)
         args.each { |type| send(type, &block) }
       end
-      
+
       def method_missing(symbol, &block)
         mime_constant = symbol.to_s.upcase
-        
+
         if Mime::SET.include?(Mime.const_get(mime_constant))
           custom(Mime.const_get(mime_constant), &block)
         else
           super
         end
       end
-      
+
       def respond
         for priority in @mime_type_priority
           if priority == Mime::ALL
@@ -179,11 +167,11 @@ module ActionController #:nodoc:
             end
           end
         end
-        
+
         if @order.include?(Mime::ALL)
           @responses[Mime::ALL].call
         else
-          eval 'render(:nothing => true, :status => "406 Not Acceptable")', @block_binding
+          @controller.send :head, :not_acceptable
         end
       end
     end
--- actionpack/test/controller/mime_responds_test.rb
+++ actionpack/test/controller/mime_responds_test.rb
@@ -11,7 +11,7 @@ class RespondToController < ActionController::Base
       type.all  { render :text => "Nothing" }
     end
   end
-  
+
   def js_or_html
     respond_to do |type|
       type.html { render :text => "HTML"    }
@@ -34,13 +34,13 @@ class RespondToController < ActionController::Base
       type.all  { render :text => "Nothing" }
     end
   end
-  
+
   def just_xml
     respond_to do |type|
       type.xml  { render :text => "XML" }
     end
   end
-  
+
   def using_defaults
     respond_to do |type|
       type.html
@@ -48,11 +48,11 @@ class RespondToController < ActionController::Base
       type.xml
     end
   end
-  
+
   def using_defaults_with_type_list
     respond_to(:html, :js, :xml)
   end
-  
+
   def made_for_content_type
     respond_to do |type|
       type.rss  { render :text => "RSS"  }
@@ -68,7 +68,7 @@ class RespondToController < ActionController::Base
       type.all  { render :text => "Nothing" }
     end
   end
-  
+
   def custom_constant_handling
     Mime::Type.register("text/x-mobile", :mobile)
 
@@ -76,10 +76,10 @@ class RespondToController < ActionController::Base
       type.html   { render :text => "HTML"   }
       type.mobile { render :text => "Mobile" }
     end
-    
+
     Mime.send :remove_const, :MOBILE
   end
-  
+
   def custom_constant_handling_without_block
     Mime::Type.register("text/x-mobile", :mobile)
 
@@ -87,10 +87,9 @@ class RespondToController < ActionController::Base
       type.html   { render :text => "HTML"   }
       type.mobile
     end
-    
+
     Mime.send :remove_const, :MOBILE    
   end
-  
 
   def handle_any
     respond_to do |type|
@@ -109,7 +108,7 @@ class RespondToController < ActionController::Base
   def rescue_action(e)
     raise
   end
-  
+
   protected
     def set_layout
       if action_name == "all_types_with_layout"
@@ -128,12 +127,12 @@ class MimeControllerTest < Test::Unit::TestCase
     @controller = RespondToController.new
     @request.host = "www.example.com"
   end
-  
+
   def test_html
     @request.env["HTTP_ACCEPT"] = "text/html"
     get :js_or_html
     assert_equal 'HTML', @response.body
-    
+
     get :html_or_xml
     assert_equal 'HTML', @response.body
 
@@ -207,31 +206,37 @@ class MimeControllerTest < Test::Unit::TestCase
   def test_using_defaults
     @request.env["HTTP_ACCEPT"] = "*/*"
     get :using_defaults
+    assert_equal "text/html", @response.content_type
     assert_equal 'Hello world!', @response.body
 
     @request.env["HTTP_ACCEPT"] = "text/javascript"
     get :using_defaults
+    assert_equal "text/javascript", @response.content_type
     assert_equal '$("body").visualEffect("highlight");', @response.body
 
     @request.env["HTTP_ACCEPT"] = "application/xml"
     get :using_defaults
+    assert_equal "application/xml", @response.content_type
     assert_equal "<p>Hello world!</p>\n", @response.body
   end
-  
+
   def test_using_defaults_with_type_list
     @request.env["HTTP_ACCEPT"] = "*/*"
     get :using_defaults_with_type_list
+    assert_equal "text/html", @response.content_type
     assert_equal 'Hello world!', @response.body
 
     @request.env["HTTP_ACCEPT"] = "text/javascript"
     get :using_defaults_with_type_list
+    assert_equal "text/javascript", @response.content_type
     assert_equal '$("body").visualEffect("highlight");', @response.body
 
     @request.env["HTTP_ACCEPT"] = "application/xml"
     get :using_defaults_with_type_list
+    assert_equal "application/xml", @response.content_type
     assert_equal "<p>Hello world!</p>\n", @response.body
   end
-  
+
   def test_with_atom_content_type
     @request.env["CONTENT_TYPE"] = "application/atom+xml"
     get :made_for_content_type
@@ -243,7 +248,7 @@ class MimeControllerTest < Test::Unit::TestCase
     get :made_for_content_type
     assert_equal "RSS", @response.body
   end
-  
+
   def test_synonyms
     @request.env["HTTP_ACCEPT"] = "application/javascript"
     get :js_or_html
@@ -253,14 +258,16 @@ class MimeControllerTest < Test::Unit::TestCase
     get :html_xml_or_rss
     assert_equal "XML", @response.body
   end
-  
+
   def test_custom_types
     @request.env["HTTP_ACCEPT"] = "application/crazy-xml"
     get :custom_type_handling
+    assert_equal "application/crazy-xml", @response.content_type
     assert_equal 'Crazy XML', @response.body
 
     @request.env["HTTP_ACCEPT"] = "text/html"
     get :custom_type_handling
+    assert_equal "text/html", @response.content_type
     assert_equal 'HTML', @response.body
   end
 
@@ -269,7 +276,7 @@ class MimeControllerTest < Test::Unit::TestCase
     get :html_or_xml
     assert_equal 'HTML', @response.body
   end
-  
+
   def test_firefox_simulation
     @request.env["HTTP_ACCEPT"] = "text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"
     get :html_or_xml
@@ -289,13 +296,13 @@ class MimeControllerTest < Test::Unit::TestCase
     get :handle_any
     assert_equal 'Either JS or XML', @response.body
   end
-  
+
   def test_rjs_type_skips_layout
     @request.env["HTTP_ACCEPT"] = "text/javascript"
     get :all_types_with_layout
     assert_equal 'RJS for all_types_with_layout', @response.body
   end
-  
+
   def test_html_type_with_layout
     @request.env["HTTP_ACCEPT"] = "text/html"
     get :all_types_with_layout
@@ -309,19 +316,19 @@ class MimeControllerTest < Test::Unit::TestCase
     xhr :get, :using_defaults
     assert_equal '$("body").visualEffect("highlight");', @response.body
   end
-  
+
   def test_custom_constant
     get :custom_constant_handling, :format => "mobile"
+    assert_equal "text/x-mobile", @response.content_type
     assert_equal "Mobile", @response.body
   end
-  
-  def custom_constant_handling_without_block
-    
-    assert_raised(ActionController::RenderError) do
-      get :custom_constant_handling, :format => "mobile"
-    end
+
+  def test_custom_constant_handling_without_block
+    get :custom_constant_handling_without_block, :format => "mobile"
+    assert_equal "text/x-mobile", @response.content_type
+    assert_equal "Mobile", @response.body
   end
-  
+
   def test_forced_format
     get :html_xml_or_rss
     assert_equal "HTML", @response.body
@@ -335,7 +342,7 @@ class MimeControllerTest < Test::Unit::TestCase
     get :html_xml_or_rss, :format => "rss"
     assert_equal "RSS", @response.body
   end
-  
+
   def test_extension_synonyms
     get :html_xml_or_rss, :format => "xhtml"
     assert_equal "HTML", @response.body
