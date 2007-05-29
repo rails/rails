34a5a896e1a55aebb0ae4b99de525c85b9eda2ed
Jeremy Kemper <jeremy@bitsweat.net>
2007-05-29 21:49:37 +0000

    Missed svn add from [6897]. References #8393.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@6898 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

---
+++ railties/test/console_app_test.rb
@@ -0,0 +1,25 @@
+require File.dirname(__FILE__) + '/abstract_unit'
+
+require 'action_controller' # console_app uses 'action_controller/integration'
+
+require 'dispatcher'
+require 'console_app'
+
+# console_app sets Test::Unit.run to work around the at_exit hook in test/unit, which kills IRB
+Test::Unit.run = false
+
+class ConsoleAppTest < Test::Unit::TestCase
+  def test_reload_should_fire_preparation_callbacks    
+    a = b = c = nil
+
+    Dispatcher.to_prepare { a = b = c = 1 }
+    Dispatcher.to_prepare { b = c = 2 }
+    Dispatcher.to_prepare { c = 3 } 
+
+    reload!
+
+    assert_equal 1, a
+    assert_equal 2, b
+    assert_equal 3, c
+  end
+end
