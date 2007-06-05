6204a52facf97ac741f61decf773c01494245f02
Jeremy Kemper <jeremy@bitsweat.net>
2007-06-05 02:42:57 +0000

    Sexy migrations for the session_migration generator. Closes #8561.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@6938 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/rails_generator/generators/components/session_migration/templates/migration.rb
+++ railties/lib/rails_generator/generators/components/session_migration/templates/migration.rb
@@ -1,9 +1,9 @@
 class <%= class_name %> < ActiveRecord::Migration
   def self.up
     create_table :<%= session_table_name %> do |t|
-      t.column :session_id, :string
-      t.column :data, :text
-      t.column :updated_at, :datetime
+      t.string :session_id, :null => false
+      t.text :data
+      t.timestamps
     end
 
     add_index :<%= session_table_name %>, :session_id
