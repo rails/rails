e4e275dde3b6ff1bcdc5f12631faa48585cb491f
Jeremy Kemper <jeremy@bitsweat.net>
2007-06-11 18:05:02 +0000

    Fix syntax error in dispatcher than wrecked failsafe responses. Closes #8625.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@7002 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/dispatcher.rb
+++ railties/lib/dispatcher.rb
@@ -40,8 +40,7 @@ class Dispatcher
       end
     rescue Exception => exception  # errors from CGI dispatch
       failsafe_response(cgi, output, '500 Internal Server Error', exception) do
-        controller ||= ApplicationController rescue LoadError nil
-        controller ||= ActionController::Base
+        controller ||= ApplicationController rescue ActionController::Base
         controller.process_with_exception(request, response, exception).out(output)
       end
     ensure
