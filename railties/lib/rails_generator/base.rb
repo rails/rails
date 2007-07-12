f9b7394430f50c110b0f07d7e3c06d5b3d9b0f44
Rick Olson <technoweenie@gmail.com>
2007-07-12 06:23:50 +0000

    Default to plural table name in Rails Generator if ActiveRecord is not present.  Closes #8963 [evan]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@7183 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/rails_generator/base.rb
+++ railties/lib/rails_generator/base.rb
@@ -228,7 +228,7 @@ module Rails
           @name = name
           base_name, @class_path, @file_path, @class_nesting, @class_nesting_depth = extract_modules(@name)
           @class_name_without_nesting, @singular_name, @plural_name = inflect_names(base_name)
-          @table_name = ActiveRecord::Base.pluralize_table_names ? plural_name : singular_name
+          @table_name = (!defined?(ActiveRecord::Base) || ActiveRecord::Base.pluralize_table_names) ? plural_name : singular_name
           if @class_nesting.empty?
             @class_name = @class_name_without_nesting
           else
