1eb79bcc3b5df2ee4e037d06835a88961eb0a429
Michael Koziarski <michael@koziarski.com>
2007-07-19 10:09:40 +0000

    Let alias_attribute work with attributes with initial capital letters (legacy columns etc).  Closes #8596 [mpalmer]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@7195 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/core_ext/module/aliasing.rb
+++ activesupport/lib/active_support/core_ext/module/aliasing.rb
@@ -62,8 +62,8 @@ class Module
   #   e.title    # => "Megastars"
   def alias_attribute(new_name, old_name)
     module_eval <<-STR, __FILE__, __LINE__+1
-      def #{new_name}; #{old_name}; end
-      def #{new_name}?; #{old_name}?; end
+      def #{new_name}; self.#{old_name}; end
+      def #{new_name}?; self.#{old_name}?; end
       def #{new_name}=(v); self.#{old_name} = v; end
     STR
   end
--- activesupport/test/core_ext/module/attribute_aliasing_test.rb
+++ activesupport/test/core_ext/module/attribute_aliasing_test.rb
@@ -2,15 +2,20 @@ require File.dirname(__FILE__) + '/../../abstract_unit'
 
 module AttributeAliasing
   class Content
-    attr_accessor :title
+    attr_accessor :title, :Data
   
     def title?
       !title.nil?
+    end
+    
+    def Data?
+      !self.Data.nil?
     end
   end
 
   class Email < Content
     alias_attribute :subject, :title
+    alias_attribute :body, :Data
   end
 end
 
@@ -27,5 +32,23 @@ class AttributeAliasingTest < Test::Unit::TestCase
     e.subject = "We got a long way to go"
     assert_equal "We got a long way to go", e.title
     assert e.title?
+  end
+
+  def test_aliasing_to_uppercase_attributes
+    # Although it's very un-Ruby, some people's AR-mapped tables have
+    # upper-case attributes, and when people want to alias those names
+    # to more sensible ones, everything goes *foof*.
+    e = AttributeAliasing::Email.new
+    
+    assert !e.body?
+    assert !e.Data?
+    
+    e.body = "No, really, this is not a joke."
+    assert_equal "No, really, this is not a joke.", e.Data
+    assert e.Data?
+    
+    e.Data = "Uppercased methods are teh suck"
+    assert_equal "Uppercased methods are teh suck", e.body
+    assert e.body?
   end
 end
