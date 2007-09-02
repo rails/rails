2fc4be68a6b65074726bee493920040da0e6e04d
Michael Koziarski <michael@koziarski.com>
2007-09-02 23:50:51 +0000

    Remove duplication in test_help.rb Closes #3328 [jarkko]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@7395 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/test_help.rb
+++ railties/lib/test_help.rb
@@ -13,5 +13,5 @@ Test::Unit::TestCase.fixture_path = RAILS_ROOT + "/test/fixtures/"
 ActionController::IntegrationTest.fixture_path = Test::Unit::TestCase.fixture_path
 
 def create_fixtures(*table_names)
-  Fixtures.create_fixtures(RAILS_ROOT + "/test/fixtures", table_names)
+  Fixtures.create_fixtures(Test::Unit::TestCase.fixture_path, table_names)
 end
