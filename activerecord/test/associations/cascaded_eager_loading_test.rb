b4ad9e0cb9f0b21e454937625325c2b957ef7057
Jeremy Kemper <jeremy@bitsweat.net>
2007-09-11 06:47:27 +0000

    Kill lingering acts
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@7460 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activerecord/test/associations/cascaded_eager_loading_test.rb
+++ activerecord/test/associations/cascaded_eager_loading_test.rb
@@ -1,5 +1,4 @@
 require 'abstract_unit'
-require 'active_record/acts/list'
 require 'fixtures/post'
 require 'fixtures/comment'
 require 'fixtures/author'
--- activerecord/test/mixin_test.rb
+++ activerecord/test/mixin_test.rb
@@ -1,6 +1,4 @@
 require 'abstract_unit'
-require 'active_record/acts/tree'
-require 'active_record/acts/nested_set'
 
 class Mixin < ActiveRecord::Base
 end
