0ba96e0568c2cf2f8cb8fe96fa48ea82a0228338
Michael Koziarski <michael@koziarski.com>
2007-09-22 17:19:42 +0000

    Remove the AWS web_service generator
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@7543 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/rails_generator/generators/components/web_service/templates/api_definition.rb
+++
@@ -1,5 +0,0 @@
-class <%= class_name %>Api < ActionWebService::API::Base
-<% for method_name in args -%>
-  api_method :<%= method_name %>
-<% end -%>
-end
--- railties/lib/rails_generator/generators/components/web_service/templates/controller.rb
+++
@@ -1,8 +0,0 @@
-class <%= class_name %>Controller < ApplicationController
-  wsdl_service_name '<%= class_name %>'
-<% for method_name in args -%>
-
-  def <%= method_name %>
-  end
-<% end -%>
-end
