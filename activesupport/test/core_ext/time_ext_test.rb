15467a43f5e0c107d5048950d1718ccccbbaf01e
David Heinemeier Hansson <david@loudthinking.com>
2007-09-22 17:58:31 +0000

    Fix tests across DST (closes #9020) [mpalmer]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@7557 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/test/core_ext/time_ext_test.rb
+++ activesupport/test/core_ext/time_ext_test.rb
@@ -250,11 +250,13 @@ class TimeExtCalculationsTest < Test::Unit::TestCase
   end
 
   def test_next_week
-    assert_equal Time.local(2005,2,28), Time.local(2005,2,22,15,15,10).next_week
-    assert_equal Time.local(2005,2,29), Time.local(2005,2,22,15,15,10).next_week(:tuesday)
-    assert_equal Time.local(2005,3,4), Time.local(2005,2,22,15,15,10).next_week(:friday)
-    assert_equal Time.local(2006,10,30), Time.local(2006,10,23,0,0,0).next_week
-    assert_equal Time.local(2006,11,1), Time.local(2006,10,23,0,0,0).next_week(:wednesday)
+    with_timezone 'US/Eastern' do
+      assert_equal Time.local(2005,2,28), Time.local(2005,2,22,15,15,10).next_week
+      assert_equal Time.local(2005,2,29), Time.local(2005,2,22,15,15,10).next_week(:tuesday)
+      assert_equal Time.local(2005,3,4), Time.local(2005,2,22,15,15,10).next_week(:friday)
+      assert_equal Time.local(2006,10,30), Time.local(2006,10,23,0,0,0).next_week
+      assert_equal Time.local(2006,11,1), Time.local(2006,10,23,0,0,0).next_week(:wednesday)
+    end
   end
 
   def test_next_week_near_daylight_start
