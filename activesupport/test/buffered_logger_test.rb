106b236824bbdf1cc9675b9531bac0c30a1926b0
David Heinemeier Hansson <david@loudthinking.com>
2007-09-25 16:48:50 +0000

    Include the tests
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@7635 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

---
+++ activesupport/test/buffered_logger_test.rb
@@ -0,0 +1,41 @@
+require File.dirname(__FILE__) + '/abstract_unit'
+require 'stringio'
+
+class BufferedLoggerTest < Test::Unit::TestCase
+  def setup
+    @message = "A debug message"
+    @output  = StringIO.new
+    @logger  = ActiveSupport::BufferedLogger.new(@output)
+  end
+  
+  def test_should_log_debugging_message_when_debugging
+    @logger.level = Logger::DEBUG
+    @logger.add(Logger::DEBUG, @message)
+    assert @output.string.include?(@message)
+  end
+
+  def test_should_not_log_debug_messages_when_log_level_is_info
+    @logger.level = Logger::INFO
+    @logger.add(Logger::DEBUG, @message)
+    assert ! @output.string.include?(@message)
+  end
+
+  def test_should_add_message_passed_as_block_when_using_add
+    @logger.level = Logger::INFO
+    @logger.add(Logger::INFO) {@message}
+    assert @output.string.include?(@message)
+  end
+  
+  def test_should_add_message_passed_as_block_when_using_shortcut
+    @logger.level = Logger::INFO
+    @logger.info {@message}
+    assert @output.string.include?(@message)
+  end
+  
+  def test_should_not_evaluate_block_if_message_wont_be_logged
+    @logger.level = Logger::INFO
+    evaluated = false
+    @logger.add(Logger::DEBUG) {evaluated = true}
+    assert evaluated == false  
+  end
+end
