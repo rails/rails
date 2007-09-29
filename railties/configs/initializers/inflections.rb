1b56d328931dd62f53331cd8206cd6459500b8da
David Heinemeier Hansson <david@loudthinking.com>
2007-09-29 21:47:38 +0000

    Remind that you need a server restart for initializer defaults
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@7680 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/configs/initializers/inflections.rb
+++ railties/configs/initializers/inflections.rb
@@ -1,3 +1,5 @@
+# Be sure to restart your server when you modify this file.
+
 # Add new inflection rules using the following format 
 # (all these examples are active by default):
 # Inflector.inflections do |inflect|
--- railties/environments/environment.rb
+++ railties/environments/environment.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your web server when you modify this file.
+# Be sure to restart your server when you modify this file.
 
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
