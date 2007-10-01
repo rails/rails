ec4989618ec08d20024a05500bea088514fbdfd4
David Heinemeier Hansson <david@loudthinking.com>
2007-10-01 02:08:15 +0000

    Removed calls to fixtures in generated tests as fixtures :all is now present by default in test_helper.rb [DHH]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@7705 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/rails_generator/generators/components/observer/templates/unit_test.rb
+++ railties/lib/rails_generator/generators/components/observer/templates/unit_test.rb
@@ -1,8 +1,6 @@
 require File.dirname(__FILE__) + '<%= '/..' * class_nesting_depth %>/../test_helper'
 
 class <%= class_name %>ObserverTest < Test::Unit::TestCase
-  fixtures :<%= table_name %>
-
   # Replace this with your real tests.
   def test_truth
     assert true
--- railties/lib/rails_generator/generators/components/scaffold/templates/functional_test.rb
+++ railties/lib/rails_generator/generators/components/scaffold/templates/functional_test.rb
@@ -5,8 +5,6 @@ require '<%= controller_file_path %>_controller'
 class <%= controller_class_name %>Controller; def rescue_action(e) raise e end; end
 
 class <%= controller_class_name %>ControllerTest < Test::Unit::TestCase
-  fixtures :<%= table_name %>
-
   def setup
     @controller = <%= controller_class_name %>Controller.new
     @request    = ActionController::TestRequest.new
