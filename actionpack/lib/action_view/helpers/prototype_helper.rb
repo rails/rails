4430a00c048c195d6fbfb3eacb2bb195f709ac1a
Jeremy Kemper <jeremy@bitsweat.net>
2007-10-07 06:45:01 +0000

    BlankSlate -> BasicObject
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@7772 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_view/helpers/prototype_helper.rb
+++ actionpack/lib/action_view/helpers/prototype_helper.rb
@@ -785,7 +785,7 @@ module ActionView
     end
 
     # Converts chained method calls on DOM proxy elements into JavaScript chains 
-    class JavaScriptProxy < Builder::BlankSlate #:nodoc:
+    class JavaScriptProxy < BasicObject #:nodoc:
       def initialize(generator, root = nil)
         @generator = generator
         @generator << root if root
