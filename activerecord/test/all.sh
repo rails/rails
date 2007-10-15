619f2897f59850842951982928d14ccacb16ac3b
Jeremy Kemper <jeremy@bitsweat.net>
2007-10-15 03:46:15 +0000

    Update test/all.sh
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@7896 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activerecord/test/all.sh
+++ activerecord/test/all.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-	echo "Usage: $0 connections/<db_library>" 1>&2
+	echo "Usage: $0 <database>" 1>&2
 	exit 1
 fi
 
-ruby -I $1 -e 'Dir.foreach(".") { |file| require file if file =~ /_test.rb$/ }'
+ruby -I connections/native_$1 -e 'Dir["**/*_test.rb"].each { |path| require path }'
