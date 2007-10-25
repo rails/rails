192e463d058f07c878534c23cf7da5a66a27eb8d
Michael Koziarski <michael@koziarski.com>
2007-10-25 07:09:09 +0000

    Add an ignored options parameter to ActiveSupport::JSON::Variable#to_json to make sure it duck-types nicely with the other JSON types. [Pascal Belloncle]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@8013 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/json/variable.rb
+++ activesupport/lib/active_support/json/variable.rb
@@ -2,7 +2,7 @@ module ActiveSupport
   module JSON
     # A string that returns itself as its JSON-encoded form.
     class Variable < String
-      def to_json
+      def to_json(options=nil)
         self
       end
     end
