86883a87ccb7f8b835e7e57a0b6e33e039e058ff
David Heinemeier Hansson <david@loudthinking.com>
2007-11-21 15:55:57 +0000

    Cleanup documentation a bit and be more specific
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@8182 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/environments/environment.rb
+++ railties/environments/environment.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your server when you modify this file.
+# Be sure to restart your server when you modify this file
 
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
@@ -11,13 +11,18 @@
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here
+  # Settings in config/environments/* take precedence over those specified here.
+  # Application configuration should go into files in config/initializers
+  # -- all .rb files in that directory are automatically loaded.
+  # See Rails::Configuration for more options.
 
-  # Skip frameworks you're not going to use (only works if using vendor/rails)
-  # config.frameworks -= [ :active_resource, :action_mailer ]
+  # Skip frameworks you're not going to use (only works if using vendor/rails).
+  # To use Rails without a database, you must remove the Active Record framework
+  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-  # Only load the plugins named here, in the order given. By default, all plugins in vendor/plugins are loaded in alphabetical order.
-  # :all can be used as a placeholder for all plugins not explicitly named.
+  # Only load the plugins named here, in the order given. By default, all plugins 
+  # in vendor/plugins are loaded in alphabetical order.
+  # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
   # Add additional load paths for your own custom dirs
@@ -49,9 +54,4 @@ Rails::Initializer.run do |config|
 
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
-
-  # See Rails::Configuration for more options
-
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded
 end
