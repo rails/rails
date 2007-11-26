1370d1578727bdbfee06ccdf2109e8ca5649a652
Jeremy Kemper <jeremy@bitsweat.net>
2007-11-26 22:45:03 +0000

    Make the record_timestamps class-inheritable so it can be set per model. References #10004.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@8217 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activerecord/lib/active_record/timestamp.rb
+++ activerecord/lib/active_record/timestamp.rb
@@ -12,7 +12,7 @@ module ActiveRecord
       base.alias_method_chain :create, :timestamps
       base.alias_method_chain :update, :timestamps
 
-      base.cattr_accessor :record_timestamps, :instance_writer => false
+      base.class_inheritable_accessor :record_timestamps, :instance_writer => false
       base.record_timestamps = true
     end
 
