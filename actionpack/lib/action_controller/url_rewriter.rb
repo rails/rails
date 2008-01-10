59f222dbf7c05e3d1f9e71831bc79e2a5ed4a5b5
Jeremy Kemper <jeremy@bitsweat.net>
2008-01-10 02:51:09 +0000

    UrlWriter respects relative_url_root. Closes #10748.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@8616 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_controller/url_rewriter.rb
+++ actionpack/lib/action_controller/url_rewriter.rb
@@ -1,17 +1,17 @@
-module ActionController  
+module ActionController
   # Write URLs from arbitrary places in your codebase, such as your mailers.
-  # 
+  #
   # Example:
-  # 
+  #
   #   class MyMailer
   #     include ActionController::UrlWriter
   #     default_url_options[:host] = 'www.basecamphq.com'
-  #     
+  #
   #     def signup_url(token)
   #       url_for(:controller => 'signup', action => 'index', :token => token)
   #     end
   #  end
-  # 
+  #
   # In addition to providing +url_for+, named routes are also accessible after
   # including UrlWriter.
   module UrlWriter
@@ -19,64 +19,65 @@ module ActionController
     # is provided.
     mattr_accessor :default_url_options
     self.default_url_options = {}
-    
+
     def self.included(base) #:nodoc:
-      ActionController::Routing::Routes.install_helpers base 
+      ActionController::Routing::Routes.install_helpers(base)
       base.mattr_accessor :default_url_options
       base.default_url_options ||= default_url_options
     end
-    
-    # Generate a url based on the options provided, default_url_options and the 
+
+    # Generate a url based on the options provided, default_url_options and the
     # routes defined in routes.rb.  The following options are supported:
-    # 
+    #
     # * <tt>:only_path</tt> If true, the relative url is returned. Defaults to false.
     # * <tt>:protocol</tt> The protocol to connect to. Defaults to 'http'.
-    # * <tt>:host</tt> Specifies the host the link should be targetted at. If <tt>:only_path</tt> is false, this option must be 
-    #   provided either explicitly, or via default_url_options. 
+    # * <tt>:host</tt> Specifies the host the link should be targetted at. If <tt>:only_path</tt> is false, this option must be
+    #   provided either explicitly, or via default_url_options.
     # * <tt>:port</tt> Optionally specify the port to connect to.
     # * <tt>:anchor</tt> An anchor name to be appended to the path.
-    # 
+    # * <tt>:skip_relative_url_root</tt> If true, the url is not constructed using the relative_url_root set in <tt>ActionController::AbstractRequest.relative_url_root</tt>.
+    #
     # Any other key(:controller, :action, etc...) given to <tt>url_for</tt> is forwarded to the Routes module.
-    # 
+    #
     # Examples:
-    # 
+    #
     #    url_for :controller => 'tasks', :action => 'testing', :host=>'somehost.org', :port=>'8080'    # => 'http://somehost.org:8080/tasks/testing'
     #    url_for :controller => 'tasks', :action => 'testing', :host=>'somehost.org', :anchor => 'ok', :only_path => true    # => '/tasks/testing#ok'
     #    url_for :controller => 'tasks', :action => 'testing', :host=>'somehost.org', :number => '33'  # => 'http://somehost.org/tasks/testing?number=33'
-    # 
     def url_for(options)
       options = self.class.default_url_options.merge(options)
-      
+
       url = ''
 
-      unless options.delete :only_path
+      unless options.delete(:only_path)
         url << (options.delete(:protocol) || 'http')
-        url << '://' unless url.match("://") #dont add separator if its already been specified in :protocol 
-        
+        url << '://' unless url.match("://")
+
         raise "Missing host to link to! Please provide :host parameter or set default_url_options[:host]" unless options[:host]
 
         url << options.delete(:host)
         url << ":#{options.delete(:port)}" if options.key?(:port)
       else
-        # Delete the unused options to prevent their appearance in the query string
-        [:protocol, :host, :port].each { |k| options.delete k }
+        # Delete the unused options to prevent their appearance in the query string.
+        [:protocol, :host, :port, :skip_relative_url_root].each { |k| options.delete(k) }
       end
 
-      anchor = "##{CGI.escape options.delete(:anchor).to_param.to_s}" if options.key?(:anchor)
+      url << ActionController::AbstractRequest.relative_url_root.to_s unless options[:skip_relative_url_root]
+      anchor = "##{CGI.escape options.delete(:anchor).to_param.to_s}" if options[:anchor]
       url << Routing::Routes.generate(options, {})
       url << anchor if anchor
 
-      return url
-    end    
+      url
+    end
   end
-  
+
   # Rewrites URLs for Base.redirect_to and Base.url_for in the controller.
   class UrlRewriter #:nodoc:
     RESERVED_OPTIONS = [:anchor, :params, :only_path, :host, :protocol, :port, :trailing_slash, :skip_relative_url_root]
     def initialize(request, parameters)
       @request, @parameters = request, parameters
     end
-    
+
     def rewrite(options = {})
       rewrite_url(options)
     end
@@ -123,7 +124,7 @@ module ActionController
         # Generates the query string, too
         Routing::Routes.generate(options, @request.symbolized_path_parameters)
       end
-      
+
       def rewrite_authentication(options)
         if options[:user] && options[:password]
           "#{CGI.escape(options.delete(:user))}:#{CGI.escape(options.delete(:password))}@"
