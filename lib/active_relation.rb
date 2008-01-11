2e8ba954f4a8d14a1b2c147477cba3410aca0414
Nick Kallen <nkallen@nick-kallens-computer-2.local>
2008-01-10 22:01:33 -0800

    reorganized requires

--- lib/active_relation.rb
+++ lib/active_relation.rb
@@ -6,22 +6,10 @@ require 'activerecord'
 
 require 'active_relation/sql_builder'
 
-require 'active_relation/relations/base'
-require 'active_relation/relations/compound'
-require 'active_relation/relations/table'
-require 'active_relation/relations/join'
-require 'active_relation/relations/attribute'
-require 'active_relation/relations/projection'
-require 'active_relation/relations/selection'
-require 'active_relation/relations/order'
-require 'active_relation/relations/range'
-require 'active_relation/relations/rename'
-require 'active_relation/relations/deletion'
-require 'active_relation/relations/insertion'
-
-require 'active_relation/predicates'
-
 require 'active_relation/extensions/object'
 require 'active_relation/extensions/array'
 require 'active_relation/extensions/base'
 require 'active_relation/extensions/hash'
+
+require 'active_relation/relations'
+require 'active_relation/predicates'
