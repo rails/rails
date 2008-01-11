dbdd86c689e962641d98ed0474a60d401582eac9
Nick Kallen <nkallen@nick-kallens-computer-2.local>
2008-01-10 21:54:09 -0800

    renamed files, removed _relation suffix

--- lib/active_relation/relations/compound_relation.rb
+++
@@ -1,9 +0,0 @@
-module ActiveRelation
-  module Relations
-    class Compound < Base
-      attr_reader :relation
-  
-      delegate :attributes, :attribute, :joins, :selects, :orders, :table, :inserts, :limit, :offset, :to => :relation
-    end
-  end
-end
