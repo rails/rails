19a63d1acfc9b7668bc19fa2796d4e11a2180cef
Nick Kallen <nkallen@nick-kallens-computer-2.local>
2008-01-12 20:20:24 -0800

    comment

--- lib/active_relation/extensions/base.rb
+++ lib/active_relation/extensions/base.rb
@@ -20,6 +20,8 @@ class ActiveRecord::Base
   end
 end
 
+# all of the below disables normal AR behavior. It's rather destructive and purely for demonstration purposes (see scratch_spec).
+
 class ActiveRecord::Associations::BelongsToAssociation
   def instantiate(record, joins = [])
     @target = proxy_reflection.klass.instantiate(record, joins)
