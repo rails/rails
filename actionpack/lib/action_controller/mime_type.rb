250a1194b91e6ddb3e2e80d48753a1591992164b
Michael Koziarski <michael@koziarski.com>
2008-01-20 22:55:25 +0000

    Ensure mime types can be compared with symbols.  Closes #10796 [bscofield]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@8677 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_controller/mime_type.rb
+++ actionpack/lib/action_controller/mime_type.rb
@@ -145,7 +145,10 @@ module Mime
     end
     
     def ==(mime_type)
-      (@synonyms + [ self ]).any? { |synonym| synonym.to_s == mime_type.to_s } if mime_type
+      return false unless mime_type
+      (@synonyms + [ self ]).any? do |synonym| 
+        synonym.to_s == mime_type.to_s || synonym.to_sym == mime_type.to_sym 
+      end
     end
     
     private
