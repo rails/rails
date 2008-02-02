c5d89bc9344f8a4cfbfe0d42b1b2d6378820bf85
Jeremy Kemper <jeremy@bitsweat.net>
2008-02-02 21:42:39 +0000

    Slight camelize speedup [Pratik Naik]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@8788 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/inflector.rb
+++ activesupport/lib/active_support/inflector.rb
@@ -146,7 +146,7 @@ module Inflector
   #   "active_record/errors".camelize(:lower) #=> "activeRecord::Errors"
   def camelize(lower_case_and_underscored_word, first_letter_in_uppercase = true)
     if first_letter_in_uppercase
-      lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::" + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
+      lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
     else
       lower_case_and_underscored_word.first + camelize(lower_case_and_underscored_word)[1..-1]
     end
