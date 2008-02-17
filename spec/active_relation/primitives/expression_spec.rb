28b11fdf4d4e5cd4b98d6c79fc88999a41532103
Nick Kallen <nkallen@nick-kallens-computer-2.local>
2008-02-16 16:14:11 -0800

    white box testing

--- spec/active_relation/primitives/expression_spec.rb
+++ spec/active_relation/primitives/expression_spec.rb
@@ -34,23 +34,6 @@ module ActiveRelation
           @expression.to_attribute.should == Attribute.new(@expression.relation, @expression.alias, nil, @expression)
         end
       end
-      
-      describe '#qualify' do
-        it "manufactures an expression aliased with that expression's qualified name" do
-          @expression.qualify.should == Expression.new(@attribute, "COUNT", @expression.qualified_name, @expression)
-        end
-      end
-    end
-    
-    describe '=~' do
-      it "obtains if the expressions are identical" do
-        Expression.new(@attribute, "COUNT").should =~ Expression.new(@attribute, "COUNT")
-      end
-      
-      it "obtains if the expressions have an overlapping history" do
-        Expression.new(@attribute, "COUNT", nil, Expression.new(@attribute, "COUNT")).should =~ Expression.new(@attribute, "COUNT")
-        Expression.new(@attribute, "COUNT").should =~ Expression.new(@attribute, "COUNT", nil, Expression.new(@attribute, "COUNT"))  
-      end
     end
     
     describe '#to_sql' do
