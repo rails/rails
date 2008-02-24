da667908f6c84ecb5dbf7c3e2b9d22ffe4c386a7
Nick Kallen <nkallen@nick-kallens-computer-2.local>
2008-02-23 16:51:54 -0800

    updating functionality

---
+++ spec/active_relation/unit/relations/update_spec.rb
@@ -0,0 +1,27 @@
+require File.join(File.dirname(__FILE__), '..', '..', '..', 'spec_helper')
+
+module ActiveRelation
+  describe Update do
+    before do
+      @relation = Table.new(:users)
+    end
+  
+    describe '#to_sql' do
+      it 'manufactures sql updating attributes' do
+        Update.new(@relation, @relation[:name] => "nick").to_sql.should be_like("""
+          UPDATE `users`
+          SET `users`.`name` = 'nick'
+        """)
+      end
+      
+      it 'manufactures sql updating a selection relation' do
+        Update.new(@relation.select(@relation[:id].equals(1)), @relation[:name] => "nick").to_sql.should be_like("""
+          UPDATE `users`
+          SET `users`.`name` = 'nick'
+          WHERE `users`.`id` = 1
+        """)
+      end
+      
+    end
+  end
+end
