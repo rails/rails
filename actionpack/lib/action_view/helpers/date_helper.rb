a02c6070f052b9f52803b02a41defb10262cfa6c
David Heinemeier Hansson <david@loudthinking.com>
2008-03-15 20:00:41 +0000

    Docfix (closes #11263) [Irfy]
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@9031 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- actionpack/lib/action_view/helpers/date_helper.rb
+++ actionpack/lib/action_view/helpers/date_helper.rb
@@ -17,27 +17,27 @@ module ActionView
 
       # Reports the approximate distance in time between two Time or Date objects or integers as seconds.
       # Set <tt>include_seconds</tt> to true if you want more detailed approximations when distance < 1 min, 29 secs
-      # Distances are reported base on the following table:
+      # Distances are reported based on the following table:
       #
-      # 0 <-> 29 secs                                                             # => less than a minute
-      # 30 secs <-> 1 min, 29 secs                                                # => 1 minute
-      # 1 min, 30 secs <-> 44 mins, 29 secs                                       # => [2..44] minutes
-      # 44 mins, 30 secs <-> 89 mins, 29 secs                                     # => about 1 hour
-      # 89 mins, 29 secs <-> 23 hrs, 59 mins, 29 secs                             # => about [2..24] hours
-      # 23 hrs, 59 mins, 29 secs <-> 47 hrs, 59 mins, 29 secs                     # => 1 day
-      # 47 hrs, 59 mins, 29 secs <-> 29 days, 23 hrs, 59 mins, 29 secs            # => [2..29] days
-      # 29 days, 23 hrs, 59 mins, 30 secs <-> 59 days, 23 hrs, 59 mins, 29 secs   # => about 1 month
-      # 59 days, 23 hrs, 59 mins, 30 secs <-> 1 yr minus 1 sec                    # => [2..12] months
-      # 1 yr <-> 2 yrs minus 1 secs                                               # => about 1 year
-      # 2 yrs <-> max time or date                                                # => over [2..X] years
+      #   0 <-> 29 secs                                                             # => less than a minute
+      #   30 secs <-> 1 min, 29 secs                                                # => 1 minute
+      #   1 min, 30 secs <-> 44 mins, 29 secs                                       # => [2..44] minutes
+      #   44 mins, 30 secs <-> 89 mins, 29 secs                                     # => about 1 hour
+      #   89 mins, 29 secs <-> 23 hrs, 59 mins, 29 secs                             # => about [2..24] hours
+      #   23 hrs, 59 mins, 29 secs <-> 47 hrs, 59 mins, 29 secs                     # => 1 day
+      #   47 hrs, 59 mins, 29 secs <-> 29 days, 23 hrs, 59 mins, 29 secs            # => [2..29] days
+      #   29 days, 23 hrs, 59 mins, 30 secs <-> 59 days, 23 hrs, 59 mins, 29 secs   # => about 1 month
+      #   59 days, 23 hrs, 59 mins, 30 secs <-> 1 yr minus 1 sec                    # => [2..12] months
+      #   1 yr <-> 2 yrs minus 1 secs                                               # => about 1 year
+      #   2 yrs <-> max time or date                                                # => over [2..X] years
       #
-      # With include_seconds = true and the difference < 1 minute 29 seconds
-      # 0-4   secs      # => less than 5 seconds
-      # 5-9   secs      # => less than 10 seconds
-      # 10-19 secs      # => less than 20 seconds
-      # 20-39 secs      # => half a minute
-      # 40-59 secs      # => less than a minute
-      # 60-89 secs      # => 1 minute
+      # With <tt>include_seconds</tt> = true and the difference < 1 minute 29 seconds:
+      #   0-4   secs      # => less than 5 seconds
+      #   5-9   secs      # => less than 10 seconds
+      #   10-19 secs      # => less than 20 seconds
+      #   20-39 secs      # => half a minute
+      #   40-59 secs      # => less than a minute
+      #   60-89 secs      # => 1 minute
       #
       # ==== Examples
       #   from_time = Time.now
