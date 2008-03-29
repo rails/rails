9ffbbd08bb5648bcb57270a7c9e60c60eff3508c
David Heinemeier Hansson <david@loudthinking.com>
2008-03-29 18:01:49 +0000

    Removed outdated scaffold from template
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@9130 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/lib/rails_generator/generators/components/controller/templates/controller.rb
+++ railties/lib/rails_generator/generators/components/controller/templates/controller.rb
@@ -1,7 +1,4 @@
 class <%= class_name %>Controller < ApplicationController
-<% if options[:scaffold] -%>
-  scaffold :<%= singular_name %>
-<% end -%>
 <% for action in actions -%>
 
   def <%= action %>
