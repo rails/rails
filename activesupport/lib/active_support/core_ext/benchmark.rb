bf3c2172389679e937a88a7a8379c7112740628b
Josh Peek <josh@joshpeek.com>
2008-03-30 03:04:41 +0000

    Ruby 1.9 compat: Suppress Benchmark.realtime method redefined warning.
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@9142 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- activesupport/lib/active_support/core_ext/benchmark.rb
+++ activesupport/lib/active_support/core_ext/benchmark.rb
@@ -1,12 +1,12 @@
 require 'benchmark'
 
-module Benchmark
+class << Benchmark
   remove_method :realtime
+
   def realtime
     r0 = Time.now
     yield
     r1 = Time.now
     r1.to_f - r0.to_f
   end
-  module_function :realtime
 end
