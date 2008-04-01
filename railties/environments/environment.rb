4e07037afa7081538a4e6386464348f23d020585
David Heinemeier Hansson <david@loudthinking.com>
2008-04-01 17:15:31 +0000

    gems should be specified before plugins
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@9199 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/environments/environment.rb
+++ railties/environments/environment.rb
@@ -20,16 +20,16 @@ Rails::Initializer.run do |config|
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-  # Only load the plugins named here, in the order given. By default, all plugins 
-  # in vendor/plugins are loaded in alphabetical order.
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
   # Specify gems that this application depends on. 
   # They can then be installed with rake gem:install on new installations.
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
+
+  # Only load the plugins named here, in the order given. By default, all plugins 
+  # in vendor/plugins are loaded in alphabetical order.
+  # :all can be used as a placeholder for all plugins not explicitly named
+  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
