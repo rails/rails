7e94cf7f4d4c865a84cf835349bdb51c5e71a842
Rick Olson <technoweenie@gmail.com>
2008-04-08 03:47:08 +0000

    set ActiveSupport.escape_html_entities_in_json = false for new rails apps
    git-svn-id: http://svn-commit.rubyonrails.org/rails/trunk@9239 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- railties/configs/initializers/new_rails_defaults.rb
+++ railties/configs/initializers/new_rails_defaults.rb
@@ -9,3 +9,7 @@ ActiveRecord::Base.include_root_in_json = true
 
 # Use ISO 8601 format for JSON serialized times and dates
 ActiveSupport.use_standard_json_time_format = true
+
+# Don't escape HTML entities in JSON, leave that for the #json_escape helper
+# if you're including raw json in an HTML page.
+ActiveSupport.escape_html_entities_in_json = false
