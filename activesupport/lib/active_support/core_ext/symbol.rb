db11ef9546b1ad16cf539395b75450c1c8e9714c
Jeremy Kemper <jeremy@bitsweat.net>
2008-04-18 14:50:10 -0700

    Override Ruby 1.8.7's incompatible Symbol#to_proc.

--- activesupport/lib/active_support/core_ext/symbol.rb
+++ activesupport/lib/active_support/core_ext/symbol.rb
@@ -1,4 +1,11 @@
-unless :test.respond_to?(:to_proc)
+# Remove 1.8.7's incompatible method.
+if :to_proc.respond_to?(:to_proc) && [1] != ([[1, 2]].map(&:first) rescue false)
+  class Symbol
+    remove_method :to_proc
+  end
+end
+
+unless :to_proc.respond_to?(:to_proc)
   class Symbol
     # Turns the symbol into a simple proc, which is especially useful for enumerations. Examples:
     #
