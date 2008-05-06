e792d4ab70448f79142fdf492390682ff5ea6398
Paul Barry <mail@paulbarry.com>
2008-05-06 00:52:12 -0400

    Added code to make initialization process call rails/init.rb for unpacked gems

--- railties/lib/rails/plugin/locator.rb
+++ railties/lib/rails/plugin/locator.rb
@@ -78,7 +78,8 @@ module Rails
     # a <tt>rails/init.rb</tt> file.
     class GemLocator < Locator
       def plugins
-        specs = Gem.loaded_specs.values.select do |spec|
+        specs = initializer.configuration.gems.map(&:specification)
+        specs + Gem.loaded_specs.values.select do |spec|
           spec.loaded_from && # prune stubs
             File.exist?(File.join(spec.full_gem_path, "rails", "init.rb"))
         end
