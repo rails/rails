8f2f88f128104355b100487008800aeb369ca425
Cheah Chu Yeow <chuyeow@gmail.com>
2008-05-11 13:50:19 +0800

    Remove unused JSON methods. [#164 state:resolved]
    Signed-off-by: Pratik Naik <pratiknaik@gmail.com>

--- activesupport/lib/active_support/json.rb
+++ activesupport/lib/active_support/json.rb
@@ -1,5 +1,3 @@
-
-
 module ActiveSupport
   # If true, use ISO 8601 format for dates and times.  Otherwise, fall back to the ActiveSupport legacy format.
   mattr_accessor :use_standard_json_time_format
@@ -17,33 +15,6 @@ module ActiveSupport
           /[\010\f\n\r\t"\\]/
         end
       @escape_html_entities_in_json = value
-    end
-  end
-
-  module JSON
-    RESERVED_WORDS = %w(
-      abstract      delete        goto          private       transient
-      boolean       do            if            protected     try
-      break         double        implements    public        typeof
-      byte          else          import        return        var
-      case          enum          in            short         void
-      catch         export        instanceof    static        volatile
-      char          extends       int           super         while
-      class         final         interface     switch        with
-      const         finally       long          synchronized
-      continue      float         native        this
-      debugger      for           new           throw
-      default       function      package       throws
-    ) #:nodoc:
-
-    class << self
-      def valid_identifier?(key) #:nodoc:
-        key.to_s =~ /^[[:alpha:]_$][[:alnum:]_$]*$/ && !reserved_word?(key)
-      end
-
-      def reserved_word?(key) #:nodoc:
-        RESERVED_WORDS.include?(key.to_s)
-      end
     end
   end
 end
