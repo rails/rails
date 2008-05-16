70e4bcf5cb039bea073851faed2df9f465e6f2f8
Chris Kampmeier <chris@kampers.net>
2008-05-16 12:09:46 -0700

    Fix a couple spelling errors in docs

--- railties/configs/initializers/new_rails_defaults.rb
+++ railties/configs/initializers/new_rails_defaults.rb
@@ -1,4 +1,4 @@
-# These settins change the behavior of Rails 2 apps and will be defaults
+# These settings change the behavior of Rails 2 apps and will be defaults
 # for Rails 3. You can remove this initializer when Rails 3 is released.
 
 # Only save the attributes that have changed since the record was loaded.
