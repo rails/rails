1855fd29e00c2eeaf77ef34bb2b8de502eaf3655
Xavier Noria <fxn@hashref.com>
2008-05-19 21:32:26 +0200

    Class#method in regular font

--- activeresource/lib/active_resource/validations.rb
+++ activeresource/lib/active_resource/validations.rb
@@ -217,7 +217,7 @@ module ActiveResource
   end
   
   # Module to allow validation of ActiveResource objects, which creates an Errors instance for every resource.
-  # Methods are implemented by overriding +Base#validate+ or its variants   Each of these methods can inspect 
+  # Methods are implemented by overriding Base#validate or its variants   Each of these methods can inspect 
   # the state of the object, which usually means  ensuring that a number of attributes have a certain value 
   # (such as not empty, within a given range, matching a certain regular expression and so on).
   #
