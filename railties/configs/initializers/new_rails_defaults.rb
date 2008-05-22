888d5059bdb71e6d034d0c09d7e3b3e235fd8507
Michael Koziarski <michael@koziarski.com>
2008-05-22 19:28:52 +1200

    Return Partial Updates to be purely opt in to prevent users from inadvertently corrupting data.

--- railties/configs/initializers/new_rails_defaults.rb
+++ railties/configs/initializers/new_rails_defaults.rb
@@ -1,9 +1,6 @@
 # These settings change the behavior of Rails 2 apps and will be defaults
 # for Rails 3. You can remove this initializer when Rails 3 is released.
 
-# Only save the attributes that have changed since the record was loaded.
-ActiveRecord::Base.partial_updates = true
-
 # Include ActiveRecord class name as root for JSON serialized output.
 ActiveRecord::Base.include_root_in_json = true
 
