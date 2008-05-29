377b2fdd0817d4706df9a5de13e651f1928c5e81
Cheah Chu Yeow <chuyeow@gmail.com>
2008-05-29 12:15:12 -0700

    Add examples for #to_json for Date, Time, DateTime, TimeWithZone for the case when ActiveSupport.use_standard_json_time_format = true (and also when it is false).

--- activesupport/lib/active_support/time_with_zone.rb
+++ activesupport/lib/active_support/time_with_zone.rb
@@ -102,7 +102,19 @@ module ActiveSupport
       "#{time.strftime("%Y-%m-%dT%H:%M:%S")}#{formatted_offset(true, 'Z')}"
     end
     alias_method :iso8601, :xmlschema
-  
+
+    # Returns a JSON string representing the TimeWithZone. If ActiveSupport.use_standard_json_time_format is set to
+    # true, the ISO 8601 format is used.
+    #
+    # ==== Examples:
+    #
+    #   # With ActiveSupport.use_standard_json_time_format = true
+    #   Time.utc(2005,2,1,15,15,10).in_time_zone.to_json
+    #   # => "2005-02-01T15:15:10Z"
+    #
+    #   # With ActiveSupport.use_standard_json_time_format = false
+    #   Time.utc(2005,2,1,15,15,10).in_time_zone.to_json
+    #   # => "2005/02/01 15:15:10 +0000"
     def to_json(options = nil)
       if ActiveSupport.use_standard_json_time_format
         xmlschema.inspect
