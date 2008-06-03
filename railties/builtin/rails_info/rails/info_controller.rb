442d2f1032df6744c11f21b5ec6281c77e39c242
David Heinemeier Hansson <david@loudthinking.com>
2008-06-02 21:55:42 -0500

    Fixed that RailsInfoController wasnt considering all requests local in development mode (Edgard Castro) [#310 state:resolved]

--- railties/builtin/rails_info/rails/info_controller.rb
+++ railties/builtin/rails_info/rails/info_controller.rb
@@ -1,6 +1,6 @@
 class Rails::InfoController < ActionController::Base
   def properties
-    if local_request?
+    if consider_all_requests_local || local_request?
       render :inline => Rails::Info.to_html
     else
       render :text => '<p>For security purposes, this information is only available to local requests.</p>', :status => 500
