06b6f435cb7ed4b171eb600d3a9286829b8b2482
Joshua Peek <josh@joshpeek.com>
2008-06-07 23:46:06 -0500

    Wrap Initializer after_initialize inside mocha block.

--- railties/test/initializer_test.rb
+++ railties/test/initializer_test.rb
@@ -30,66 +30,66 @@ class Initializer_load_environment_Test < Test::Unit::TestCase
 
 end
 
-class Initializer_after_initialize_with_blocks_environment_Test < Test::Unit::TestCase
-  def setup
-    config = ConfigurationMock.new("")
-    config.after_initialize do
-      $test_after_initialize_block1 = "success"
+uses_mocha 'Initializer after_initialize' do
+  class Initializer_after_initialize_with_blocks_environment_Test < Test::Unit::TestCase
+    def setup
+      config = ConfigurationMock.new("")
+      config.after_initialize do
+        $test_after_initialize_block1 = "success"
+      end
+      config.after_initialize do
+        $test_after_initialize_block2 = "congratulations"
+      end
+      assert_nil $test_after_initialize_block1
+      assert_nil $test_after_initialize_block2
+
+      Rails::Initializer.any_instance.expects(:gems_dependencies_loaded).returns(true)
+      Rails::Initializer.run(:after_initialize, config)
     end
-    config.after_initialize do
-      $test_after_initialize_block2 = "congratulations"
+
+    def teardown
+      $test_after_initialize_block1 = nil
+      $test_after_initialize_block2 = nil
     end
-    assert_nil $test_after_initialize_block1
-    assert_nil $test_after_initialize_block2
 
-    Rails::Initializer.any_instance.expects(:gems_dependencies_loaded).returns(true)
-    Rails::Initializer.run(:after_initialize, config)
-  end
-
-  def teardown
-    $test_after_initialize_block1 = nil
-    $test_after_initialize_block2 = nil
-  end
-
-  def test_should_have_called_the_first_after_initialize_block
-    assert_equal "success", $test_after_initialize_block1
-  end
-
-  def test_should_have_called_the_second_after_initialize_block
-    assert_equal "congratulations", $test_after_initialize_block2
-  end
-end
-
-class Initializer_after_initialize_with_no_block_environment_Test < Test::Unit::TestCase
-
-  def setup
-    config = ConfigurationMock.new("")
-    config.after_initialize do
-      $test_after_initialize_block1 = "success"
+    def test_should_have_called_the_first_after_initialize_block
+      assert_equal "success", $test_after_initialize_block1
     end
-    config.after_initialize # don't pass a block, this is what we're testing!
-    config.after_initialize do
-      $test_after_initialize_block2 = "congratulations"
+
+    def test_should_have_called_the_second_after_initialize_block
+      assert_equal "congratulations", $test_after_initialize_block2
     end
-    assert_nil $test_after_initialize_block1
-
-    Rails::Initializer.any_instance.expects(:gems_dependencies_loaded).returns(true)
-    Rails::Initializer.run(:after_initialize, config)
   end
 
-  def teardown
-    $test_after_initialize_block1 = nil
-    $test_after_initialize_block2 = nil
-  end
+  class Initializer_after_initialize_with_no_block_environment_Test < Test::Unit::TestCase
+    def setup
+      config = ConfigurationMock.new("")
+      config.after_initialize do
+        $test_after_initialize_block1 = "success"
+      end
+      config.after_initialize # don't pass a block, this is what we're testing!
+      config.after_initialize do
+        $test_after_initialize_block2 = "congratulations"
+      end
+      assert_nil $test_after_initialize_block1
 
-  def test_should_have_called_the_first_after_initialize_block
-    assert_equal "success", $test_after_initialize_block1, "should still get set"
-  end
+      Rails::Initializer.any_instance.expects(:gems_dependencies_loaded).returns(true)
+      Rails::Initializer.run(:after_initialize, config)
+    end
 
-  def test_should_have_called_the_second_after_initialize_block
-    assert_equal "congratulations", $test_after_initialize_block2
-  end
+    def teardown
+      $test_after_initialize_block1 = nil
+      $test_after_initialize_block2 = nil
+    end
 
+    def test_should_have_called_the_first_after_initialize_block
+      assert_equal "success", $test_after_initialize_block1, "should still get set"
+    end
+
+    def test_should_have_called_the_second_after_initialize_block
+      assert_equal "congratulations", $test_after_initialize_block2
+    end
+  end
 end
 
 uses_mocha 'framework paths' do
