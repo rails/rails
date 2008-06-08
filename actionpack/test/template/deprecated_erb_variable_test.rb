f42dab8221a2cfc13b49f6ac713e0845f25b65c3
Jeremy Kemper <jeremy@bitsweat.net>
2008-06-08 01:36:33 -0700

    Revert "Missed add: deprecated erb_variable test"
    This reverts commit 56f5c5582c97b60a89ce44f5dfccba0b5aed4357.

--- actionpack/test/template/deprecated_erb_variable_test.rb
+++
@@ -1,9 +0,0 @@
-require 'abstract_unit'
-
-class DeprecatedErbVariableTest < ActionView::TestCase
-  def test_setting_erb_variable_warns
-    assert_deprecated 'erb_variable' do
-      ActionView::Base.erb_variable = '_erbout'
-    end
-  end
-end
