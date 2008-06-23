f9b48f36fa21d96210af59870fcace27f2c45fc8
Cheah Chu Yeow <chuyeow@gmail.com>
2008-06-23 12:15:09 +0800

    Use consistent "takes precedence over" verbiage and remove some unnecessary words.

--- activesupport/lib/active_support/core_ext/hash/reverse_merge.rb
+++ activesupport/lib/active_support/core_ext/hash/reverse_merge.rb
@@ -1,14 +1,14 @@
 module ActiveSupport #:nodoc:
   module CoreExtensions #:nodoc:
     module Hash #:nodoc:
-      # Allows for reverse merging where its the keys in the calling hash that wins over those in the <tt>other_hash</tt>.
-      # This is particularly useful for initializing an incoming option hash with default values:
+      # Allows for reverse merging where the keys in the calling hash take precedence over those in the <tt>other_hash</tt>.
+      # This is particularly useful for initializing an option hash with default values:
       #
       #   def setup(options = {})
       #     options.reverse_merge! :size => 25, :velocity => 10
       #   end
       #
-      # The default <tt>:size</tt> and <tt>:velocity</tt> is only set if the +options+ passed in doesn't already have those keys set.
+      # The default <tt>:size</tt> and <tt>:velocity</tt> is only set if the +options+ passed in doesn't already have those keys.
       module ReverseMerge
         # Performs the opposite of merge, with the keys and values from the first hash taking precedence over the second.
         def reverse_merge(other_hash)
