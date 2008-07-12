b603de08825eec05f1b97d0f5d71462f3fa4c222
Joshua Peek <josh@joshpeek.com>
2008-07-12 12:15:31 -0500

    Improve test coverage and create fixtures for RenderPartialWithRecordIdentificationTests

--- actionpack/test/activerecord/render_partial_with_record_identification_test.rb
+++ actionpack/test/activerecord/render_partial_with_record_identification_test.rb
@@ -56,26 +56,31 @@ class RenderPartialWithRecordIdentificationTest < ActiveRecordTestCase
   def test_rendering_partial_with_has_many_and_belongs_to_association
     get :render_with_has_many_and_belongs_to_association
     assert_template 'projects/_project'
+    assert_equal 'Active RecordActive Controller', @response.body
   end
 
   def test_rendering_partial_with_has_many_association
     get :render_with_has_many_association
     assert_template 'replies/_reply'
+    assert_equal 'Birdman is better!', @response.body
   end
 
   def test_rendering_partial_with_named_scope
     get :render_with_named_scope
     assert_template 'replies/_reply'
+    assert_equal 'Birdman is better!Nuh uh!', @response.body
   end
 
   def test_render_with_record
     get :render_with_record
     assert_template 'developers/_developer'
+    assert_equal 'David', @response.body
   end
 
   def test_render_with_record_collection
     get :render_with_record_collection
     assert_template 'developers/_developer'
+    assert_equal 'DavidJamisfixture_3fixture_4fixture_5fixture_6fixture_7fixture_8fixture_9fixture_10Jamis', @response.body
   end
 
   def test_rendering_partial_with_has_one_association
@@ -165,11 +170,13 @@ class RenderPartialWithRecordIdentificationAndNestedControllersTest < ActiveReco
   def test_render_with_record_in_nested_controller
     get :render_with_record_in_nested_controller
     assert_template 'fun/games/_game'
+    assert_equal 'Pong', @response.body
   end
 
   def test_render_with_record_collection_in_nested_controller
     get :render_with_record_collection_in_nested_controller
     assert_template 'fun/games/_game'
+    assert_equal 'PongTank', @response.body
   end
 end
 
@@ -184,10 +191,12 @@ class RenderPartialWithRecordIdentificationAndNestedDeeperControllersTest < Acti
   def test_render_with_record_in_deeper_nested_controller
     get :render_with_record_in_deeper_nested_controller
     assert_template 'fun/serious/games/_game'
+    assert_equal 'Chess', @response.body
   end
 
   def test_render_with_record_collection_in_deeper_nested_controller
     get :render_with_record_collection_in_deeper_nested_controller
     assert_template 'fun/serious/games/_game'
+    assert_equal 'ChessSudokuSolitaire', @response.body
   end
 end
