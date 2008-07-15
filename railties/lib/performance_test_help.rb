b337ab0221085ab1a941d87f06eb4904b5852c82
Pratik Naik <pratiknaik@gmail.com>
2008-07-15 21:07:25 +0100

    Move performance test helper settings to railties

---
+++ railties/lib/performance_test_help.rb
@@ -0,0 +1,6 @@
+require 'action_controller/performance_test'
+
+ActionController::Base.perform_caching = true
+ActionView::Base.cache_template_loading = true
+ActiveSupport::Dependencies.mechanism = :require
+Rails.logger.level = ActiveSupport::BufferedLogger::INFO
