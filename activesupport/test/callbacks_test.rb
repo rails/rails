be078ee162fcae883a5621a30929879cd783a238
Joshua Peek <josh@joshpeek.com>
2008-07-15 21:55:28 -0500

    Run callbacks from object's metaclass [#575 state:resolved]

--- activesupport/test/callbacks_test.rb
+++ activesupport/test/callbacks_test.rb
@@ -84,6 +84,30 @@ class CallbacksTest < Test::Unit::TestCase
   end
 end
 
+class MetaclassCallbacksTest < Test::Unit::TestCase
+  module ModuleWithCallbacks
+    def self.extended(object)
+      object.metaclass.before_save :raise_metaclass_callback_called
+    end
+
+    def module_callback_called?
+      @module_callback_called ||= false
+    end
+
+    def raise_metaclass_callback_called
+      @module_callback_called = true
+    end
+  end
+
+  def test_metaclass_callbacks
+    person = Person.new
+    person.extend(ModuleWithCallbacks)
+    assert !person.module_callback_called?
+    person.save
+    assert person.module_callback_called?
+  end
+end
+
 class ConditionalCallbackTest < Test::Unit::TestCase
   def test_save_conditional_person
     person = ConditionalPerson.new
