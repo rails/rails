7a84681016a176185df95b347b94c37496cac159
Joshua Peek <josh@joshpeek.com>
2008-07-16 18:03:24 -0500

    Revert "Added Object#metaclass"
    This reverts commit 98dd7226fa2fb306008fb5d89e38ac2391453664.

--- activesupport/lib/active_support/core_ext/object.rb
+++ activesupport/lib/active_support/core_ext/object.rb
@@ -1,5 +1,4 @@
 require 'active_support/core_ext/object/conversions'
 require 'active_support/core_ext/object/extending'
 require 'active_support/core_ext/object/instance_variables'
-require 'active_support/core_ext/object/metaclass'
 require 'active_support/core_ext/object/misc'
