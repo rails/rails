c609be45966316bb107e0bad2b0935ac4a0d0c41
Joshua Peek <josh@joshpeek.com>
2008-07-18 23:30:36 -0500

    Ruby 1.9: Ensure Memoizable#freeze is only overriden once to avoid an endless loop

--- activesupport/lib/active_support/memoizable.rb
+++ activesupport/lib/active_support/memoizable.rb
@@ -1,19 +1,5 @@
 module ActiveSupport
   module Memoizable #:nodoc:
-    def self.extended(obj)
-      klass = obj.respond_to?(:class_eval) ? obj : obj.metaclass
-      klass.class_eval do
-        def freeze
-          methods.each do |method|
-            if m = method.to_s.match(/^unmemoized_(.*)/)
-              send(m[1])
-            end
-          end
-          super
-        end
-      end
-    end
-
     def memoize(*symbols)
       symbols.each do |symbol|
         original_method = "unmemoized_#{symbol}"
@@ -23,6 +9,18 @@ module ActiveSupport
         raise "Already memoized #{symbol}" if klass.instance_methods.map(&:to_s).include?(original_method)
 
         klass.class_eval <<-EOS, __FILE__, __LINE__
+          unless instance_methods.map(&:to_s).include?("freeze_without_memoizable")
+            alias_method :freeze_without_memoizable, :freeze
+            def freeze
+              methods.each do |method|
+                if m = method.to_s.match(/^unmemoized_(.*)/)
+                  send(m[1])
+                end
+              end
+              freeze_without_memoizable
+            end
+          end
+
           alias_method :#{original_method}, :#{symbol}
           def #{symbol}(reload = false)
             if !reload && defined? #{memoized_ivar}
