108ed4a5660d4ebdc52563250463c762e3f27988
Joshua Peek <josh@joshpeek.com>
2008-07-19 00:24:02 -0500

    Ruby 1.9: Strip encoding from ERB source since you can not change character encoding during a method

--- actionpack/lib/action_view/template_handlers/erb.rb
+++ actionpack/lib/action_view/template_handlers/erb.rb
@@ -48,8 +48,11 @@ module ActionView
       self.erb_trim_mode = '-'
 
       def compile(template)
-        src = ::ERB.new(template.source, nil, erb_trim_mode, '@output_buffer').src
-        "__in_erb_template=true;#{src}"
+        src = ::ERB.new("<% __in_erb_template=true %>#{template.source}", nil, erb_trim_mode, '@output_buffer').src
+
+        # Ruby 1.9 prepends an encoding to the source. However this is
+        # useless because you can only set an encoding on the first line
+        RUBY_VERSION >= '1.9' ? src.sub(/\A#coding:.*\n/, '') : src
       end
     end
   end
