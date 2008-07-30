e5fadfea8a808b145489122d2c59a92bcbb341be
Pat Nakajima <patnakajima@gmail.com>
2008-07-30 16:37:57 -0400

    Added documentation for ActiveSupport::StringInquirer class

--- activesupport/lib/active_support/string_inquirer.rb
+++ activesupport/lib/active_support/string_inquirer.rb
@@ -1,4 +1,14 @@
 module ActiveSupport
+  # Wrapping a string in this class gives you a prettier way to test
+  # for equality. The value returned by <tt>Rails.env</tt> is wrapped
+  # in a StringInquirer object so instead of calling this:
+  #
+  #   Rails.env == "production"
+  #
+  # you can call this:
+  #
+  #   Rails.env.production?
+  #
   class StringInquirer < String
     def method_missing(method_name, *arguments)
       if method_name.to_s.ends_with?("?")
