06e4eb49eaabde76e38e4f275619fe2df51f1e0a
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2008-08-04 12:31:05 +0200

    Added file name information for errors and exceptions in script/runnner
    Signed-off-by: Michael Koziarski <michael@koziarski.com>

--- railties/lib/commands/runner.rb
+++ railties/lib/commands/runner.rb
@@ -42,7 +42,7 @@ if code_or_file.nil?
   $stderr.puts "Run '#{$0} -h' for help."
   exit 1
 elsif File.exist?(code_or_file)
-  eval(File.read(code_or_file))
+  eval(File.read(code_or_file), nil, code_or_file)
 else
   eval(code_or_file)
 end
