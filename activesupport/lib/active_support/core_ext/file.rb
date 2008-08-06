73056500f88d569fa497d846dfe6b501a9e03739
Joshua Peek <josh@joshpeek.com>
2008-08-06 14:39:13 -0500

    Make File.atomic_write copy the original permissions or use the directories default.

--- activesupport/lib/active_support/core_ext/file.rb
+++ activesupport/lib/active_support/core_ext/file.rb
@@ -1,21 +1,5 @@
-require 'tempfile'
+require 'active_support/core_ext/file/atomic'
 
-# Write to a file atomically.  Useful for situations where you don't
-# want other processes or threads to see half-written files.
-#
-#   File.atomic_write("important.file") do |file|
-#     file.write("hello")
-#   end
-#
-# If your temp directory is not on the same filesystem as the file you're 
-# trying to write, you can provide a different temporary directory.
-# 
-#   File.atomic_write("/data/something.important", "/data/tmp") do |f|
-#     file.write("hello")
-#   end
-def File.atomic_write(file_name, temp_dir = Dir.tmpdir)
-  temp_file = Tempfile.new(File.basename(file_name), temp_dir)
-  yield temp_file
-  temp_file.close
-  File.rename(temp_file.path, file_name)
+class File #:nodoc:
+  extend ActiveSupport::CoreExtensions::File::Atomic
 end
