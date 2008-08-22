381210daa0b8db26be85841b8ccf889d0ef67d75
Amos King <amos.l.king@gmail.com>
2008-08-22 13:31:13 +0100

    camelize(:lower) should always downcase first character. [#696 state:resolved]
    Signed-off-by: Pratik Naik <pratiknaik@gmail.com>

--- activesupport/lib/active_support/inflector.rb
+++ activesupport/lib/active_support/inflector.rb
@@ -173,7 +173,7 @@ module ActiveSupport
       if first_letter_in_uppercase
         lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
       else
-        lower_case_and_underscored_word.first + camelize(lower_case_and_underscored_word)[1..-1]
+        lower_case_and_underscored_word.first.downcase + camelize(lower_case_and_underscored_word)[1..-1]
       end
     end
 
