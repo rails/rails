cf28109158054fbab91de2d6d86efe1b40e68d93
Tarmo Tänav <tarmo@itech.ee>
2008-08-23 18:05:52 +0300

    Always require activesupport, even if its constant already exists
    This is needed because the existance of the ActiveSupport
    constant by itself does not guarantee that the whole library
    has been loaded.
    
    Also load the StringInquirer in the Rails#env method as
    the it might be called inside the initializer block
    before activesupport itself has been loaded.

--- activerecord/lib/active_record.rb
+++ activerecord/lib/active_record.rb
@@ -24,16 +24,14 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-unless defined? ActiveSupport
-  active_support_path = File.dirname(__FILE__) + "/../../activesupport/lib"
-  if File.exist?(active_support_path)
-    $:.unshift active_support_path
-    require 'active_support'
-  else
-    require 'rubygems'
-    gem 'activesupport'
-    require 'active_support'
-  end
+active_support_path = File.dirname(__FILE__) + "/../../activesupport/lib"
+if File.exist?(active_support_path)
+  $:.unshift active_support_path
+  require 'active_support'
+else
+  require 'rubygems'
+  gem 'activesupport'
+  require 'active_support'
 end
 
 require 'active_record/base'
