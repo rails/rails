7f179f8540ab92dbd9d3e650b465de5b694d93d4
Tom Stuart <tom@obsess.com>
2008-08-29 15:11:25 +0100

    Make NamedScope#size behave identically to AssociationCollection#size. [#933 state:resolved]
    Signed-off-by: Pratik Naik <pratiknaik@gmail.com>

--- activerecord/lib/active_record/named_scope.rb
+++ activerecord/lib/active_record/named_scope.rb
@@ -101,7 +101,7 @@ module ActiveRecord
     
     class Scope
       attr_reader :proxy_scope, :proxy_options
-      NON_DELEGATE_METHODS = %w(nil? send object_id class extend find count sum average maximum minimum paginate first last empty? any? respond_to?).to_set
+      NON_DELEGATE_METHODS = %w(nil? send object_id class extend find size count sum average maximum minimum paginate first last empty? any? respond_to?).to_set
       [].methods.each do |m|
         unless m =~ /^__/ || NON_DELEGATE_METHODS.include?(m.to_s)
           delegate m, :to => :proxy_found
@@ -134,6 +134,10 @@ module ActiveRecord
         else
           find(:last, *args)
         end
+      end
+
+      def size
+        @found ? @found.length : count
       end
 
       def empty?
