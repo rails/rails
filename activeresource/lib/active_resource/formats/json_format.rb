caabe228bc6c6e920043334d717e72093559e118
Rasik Pandey <rbpandey@gmail.com>
2008-08-30 04:19:18 +0300

    Format related patches to support serializing data out in the correct format with correct http request headers per http method type [#450 state:resolved]
    Signed-off-by: Tarmo Tänav <tarmo@itech.ee>
    Signed-off-by: Jeremy Kemper <jeremy@bitsweat.net>

--- activeresource/lib/active_resource/formats/json_format.rb
+++ activeresource/lib/active_resource/formats/json_format.rb
@@ -2,19 +2,19 @@ module ActiveResource
   module Formats
     module JsonFormat
       extend self
-      
+
       def extension
         "json"
       end
-      
+
       def mime_type
         "application/json"
       end
-      
-      def encode(hash)
+
+      def encode(hash, options={})
         hash.to_json
       end
-      
+
       def decode(json)
         ActiveSupport::JSON.decode(json)
       end
