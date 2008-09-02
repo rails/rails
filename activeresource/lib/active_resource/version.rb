e5c52a3a6e811f254bf7ea4ee12f99463bb176e9
David Heinemeier Hansson <david@loudthinking.com>
2008-09-02 16:49:32 +0200

    Prepare for 2.0.4 release

--- activeresource/lib/active_resource/version.rb
+++ activeresource/lib/active_resource/version.rb
@@ -2,7 +2,7 @@ module ActiveResource
   module VERSION #:nodoc:
     MAJOR = 2
     MINOR = 0
-    TINY  = 3
+    TINY  = 4
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
--- activesupport/lib/active_support/version.rb
+++ activesupport/lib/active_support/version.rb
@@ -2,7 +2,7 @@ module ActiveSupport
   module VERSION #:nodoc:
     MAJOR = 2
     MINOR = 0
-    TINY  = 3
+    TINY  = 4
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
