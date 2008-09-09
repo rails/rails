8e68ec4dcceb606853a50d41076c7d5e118089b7
Akira Matsuda <ronnie@dio.jp>
2008-09-09 17:24:23 +0900

    Ruby 1.9 compat: use String#lines instead of to_a in Ruby 1.9
    [#1001 state:resolved]
    
    Signed-off-by: Jeremy Kemper <jeremy@bitsweat.net>

--- railties/lib/commands/plugin.rb
+++ railties/lib/commands/plugin.rb
@@ -901,7 +901,7 @@ class RecursiveHTTPFetcher
   def initialize(urls_to_fetch, level = 1, cwd = ".")
     @level = level
     @cwd = cwd
-    @urls_to_fetch = urls_to_fetch.to_a
+    @urls_to_fetch = RUBY_VERSION >= '1.9' ? urls_to_fetch.lines : urls_to_fetch.to_a
     @quiet = false
   end
 
