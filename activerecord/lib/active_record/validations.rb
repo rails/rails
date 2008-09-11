095ad690f3fb6df4e89446dd40e03cf2c204e42a
Michael Koziarski <michael@koziarski.com>
2008-09-11 22:47:45 +0200

    Remove the .to_s fix in validates_uniqueness_of, as Chars get quoted correctly.

--- activerecord/lib/active_record/validations.rb
+++ activerecord/lib/active_record/validations.rb
@@ -665,7 +665,7 @@ module ActiveRecord
             condition_params = [value]
           else
             condition_sql = "LOWER(#{sql_attribute}) #{comparison_operator}"
-            condition_params = [value.chars.downcase.to_s]
+            condition_params = [value.chars.downcase]
           end
 
           if scope = configuration[:scope]
