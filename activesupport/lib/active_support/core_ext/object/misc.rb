91871f25f01f6b5d67e7da4aeb6dec0fae7b18fe
Cody Fauser <cody@jadedpixel.com>
2008-09-21 19:01:48 -0400

    Improve explanation of with_options

--- activesupport/lib/active_support/core_ext/object/misc.rb
+++ activesupport/lib/active_support/core_ext/object/misc.rb
@@ -40,7 +40,11 @@ class Object
     value
   end
 
-  # An elegant way to refactor out common options
+  # An elegant way to factor duplication out of options passed to a series of
+  # method calls. Each method called in the block, with the block variable as
+  # the receiver, will have its options merged with the default +options+ hash
+  # provided. Each method called on the block variable must take an options
+  # hash as its final argument.
   # 
   #   with_options :order => 'created_at', :class_name => 'Comment' do |post|
   #     post.has_many :comments, :conditions => ['approved = ?', true], :dependent => :delete_all
