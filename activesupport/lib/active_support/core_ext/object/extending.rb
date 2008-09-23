ec39711b1b3ca3818986fe32504b80323066f695
Michael Koziarski <michael@koziarski.com>
2008-09-23 14:26:41 +0200

    Revert "Performance: faster Object.subclasses_of" as it breaks jruby.
    This reverts commit 0a11165cdc8de33c73ba5c9ee3196eed9489879c.

--- activesupport/lib/active_support/core_ext/object/extending.rb
+++ activesupport/lib/active_support/core_ext/object/extending.rb
@@ -3,18 +3,17 @@ class Object
     Class.remove_class(*subclasses_of(*superclasses))
   end
 
-  # Exclude this class unless it's a subclass of our supers and is defined.
-  # We check defined? in case we find a removed class that has yet to be
-  # garbage collected. This also fails for anonymous classes -- please
-  # submit a patch if you have a workaround.
   def subclasses_of(*superclasses) #:nodoc:
     subclasses = []
 
-    superclasses.each do |sup|
-      ObjectSpace.each_object(class << sup; self; end) do |k|
-        if k != sup && (k.name.blank? || eval("defined?(::#{k}) && ::#{k}.object_id == k.object_id"))
-          subclasses << k
-        end
+    # Exclude this class unless it's a subclass of our supers and is defined.
+    # We check defined? in case we find a removed class that has yet to be
+    # garbage collected. This also fails for anonymous classes -- please
+    # submit a patch if you have a workaround.
+    ObjectSpace.each_object(Class) do |k|
+      if superclasses.any? { |superclass| k < superclass } &&
+        (k.name.blank? || eval("defined?(::#{k}) && ::#{k}.object_id == k.object_id"))
+        subclasses << k
       end
     end
 
