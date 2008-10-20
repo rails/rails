318beb56eb8ce86a325d60b71a118c282a4601ff
Geoff Garside <geoff@geoffgarside.co.uk>
2008-10-20 14:54:42 +0100

    Bump active_support/vendor.rb tzinfo version number [#1237 state:resolved]

--- activesupport/lib/active_support/vendor.rb
+++ activesupport/lib/active_support/vendor.rb
@@ -22,5 +22,5 @@ end
 begin
   gem 'tzinfo', '~> 0.3.11'
 rescue Gem::LoadError
-  $:.unshift "#{File.dirname(__FILE__)}/vendor/tzinfo-0.3.9"
+  $:.unshift "#{File.dirname(__FILE__)}/vendor/tzinfo-0.3.11"
 end
