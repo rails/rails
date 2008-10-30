2dcdcf1552748f331bf6f6194ffddabd837bcac9
Joshua Peek <josh@joshpeek.com>
2008-10-30 15:52:12 -0500

    config.load_paths should be frozen [#728 state:resolved]

--- railties/lib/initializer.rb
+++ railties/lib/initializer.rb
@@ -243,6 +243,7 @@ module Rails
       end
 
       # Freeze the arrays so future modifications will fail rather than do nothing mysteriously
+      configuration.load_paths.freeze
       configuration.load_once_paths.freeze
     end
 
