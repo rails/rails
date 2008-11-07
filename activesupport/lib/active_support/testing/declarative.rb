ae9581e0f3961a8a9754db6588b24200d4c443a5
Jeremy Kemper <jeremy@bitsweat.net>
2008-11-07 13:25:40 -0500

    Extract test method declaration

---
+++ activesupport/lib/active_support/testing/declarative.rb
@@ -0,0 +1,21 @@
+module ActiveSupport
+  module Testing
+    module Declarative
+      # test "verify something" do
+      #   ...
+      # end
+      def test(name, &block)
+        test_name = "test_#{name.gsub(/\s+/,'_')}".to_sym
+        defined = instance_method(test_name) rescue false
+        raise "#{test_name} is already defined in #{self}" if defined
+        if block_given?
+          define_method(test_name, &block)
+        else
+          define_method(test_name) do
+            flunk "No implementation provided for #{name}"
+          end
+        end
+      end
+    end
+  end
+end
