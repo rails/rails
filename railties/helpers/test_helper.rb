e3fcf9b66889caba7852e273de778e0e52c6585c
Jeremy Kemper <jeremy@bitsweat.net>
2008-11-17 10:39:46 -0800

    Use ActiveSupport::TestCase in generated test/test_helper.rb

--- railties/helpers/test_helper.rb
+++ railties/helpers/test_helper.rb
@@ -2,7 +2,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 
-class Test::Unit::TestCase
+class ActiveSupport::TestCase
   # Transactional fixtures accelerate your tests by wrapping each test method
   # in a transaction that's rolled back on completion.  This ensures that the
   # test database remains unchanged so your fixtures don't have to be reloaded
