9ad165cb9d4ca5df9b5ba6f01752727427fed949
gbuesing <gbuesing@gmail.com>
2008-11-18 09:01:57 -0600

    Update bundled TZInfo to 0.3.12

--- activesupport/lib/active_support/vendor.rb
+++ activesupport/lib/active_support/vendor.rb
@@ -20,9 +20,9 @@ rescue Gem::LoadError
 end
 
 begin
-  gem 'tzinfo', '~> 0.3.11'
+  gem 'tzinfo', '~> 0.3.12'
 rescue Gem::LoadError
-  $:.unshift "#{File.dirname(__FILE__)}/vendor/tzinfo-0.3.11"
+  $:.unshift "#{File.dirname(__FILE__)}/vendor/tzinfo-0.3.12"
 end
 
 # TODO I18n gem has not been released yet
