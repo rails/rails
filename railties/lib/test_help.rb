d22fe41cf87d781ce1af264b37ba3eca762b74c3
Pratik Naik <pratiknaik@gmail.com>
2008-11-18 08:40:38 +0530

    Fix a typo in test helper

--- railties/lib/test_help.rb
+++ railties/lib/test_help.rb
@@ -4,7 +4,7 @@ silence_warnings { RAILS_ENV = "test" }
 
 require 'test/unit'
 require 'active_support/test_case'
-require 'active_controller/test_case'
+require 'action_controller/test_case'
 require 'action_view/test_case'
 require 'action_controller/integration'
 require 'action_mailer/test_case' if defined?(ActionMailer)
