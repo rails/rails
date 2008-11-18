dac9180f8727d73dae816769de1d590e11bc0a75
gbuesing <gbuesing@gmail.com>
2008-11-18 09:24:58 -0600

    TimeZone offset tests: use current_period, to ensure TimeZone#utc_offset is up-to-date

--- activesupport/test/time_zone_test.rb
+++ activesupport/test/time_zone_test.rb
@@ -51,7 +51,7 @@ class TimeZoneTest < Test::Unit::TestCase
 
     define_method("test_utc_offset_for_#{name}") do
       silence_warnings do # silence warnings raised by tzinfo gem
-        period = zone.tzinfo.period_for_utc(Time.utc(2006,1,1,0,0,0))
+        period = zone.tzinfo.current_period
         assert_equal period.utc_offset, zone.utc_offset
       end
     end
