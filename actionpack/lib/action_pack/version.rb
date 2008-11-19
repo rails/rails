51a19ae2bf33e66b23ff5c91bf584b2efa9e669f
David Heinemeier Hansson <david@loudthinking.com>
2008-11-19 14:12:38 +0100

    Assume that the next version is going to be 2.3 for now

--- actionpack/lib/action_pack/version.rb
+++ actionpack/lib/action_pack/version.rb
@@ -1,8 +1,8 @@
 module ActionPack #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 2
-    MINOR = 2
-    TINY  = 1
+    MINOR = 3
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
--- activeresource/lib/active_resource/version.rb
+++ activeresource/lib/active_resource/version.rb
@@ -1,8 +1,8 @@
 module ActiveResource
   module VERSION #:nodoc:
     MAJOR = 2
-    MINOR = 2
-    TINY  = 1
+    MINOR = 3
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
