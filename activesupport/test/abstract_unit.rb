53104c34b1eb607a98d04605b12454ad852d84c6
Jeremy Kemper <jeremy@bitsweat.net>
2008-11-20 12:08:42 -0800

    Require mocha >= 0.9.0 for Active Support tests. Don't manipulate the load path.

--- activesupport/test/abstract_unit.rb
+++ activesupport/test/abstract_unit.rb
@@ -1,44 +1,22 @@
-# encoding: utf-8
-
+require 'rubygems'
 require 'test/unit'
+gem 'mocha', '>= 0.9.0'
+require 'mocha'
 
-$:.unshift "#{File.dirname(__FILE__)}/../lib"
-$:.unshift File.dirname(__FILE__)
 require 'active_support'
 require 'active_support/test_case'
 
-if RUBY_VERSION < '1.9'
-  $KCODE = 'UTF8'
-end
-
-def uses_gem(gem_name, test_name, version = '> 0')
-  require 'rubygems'
-  gem gem_name.to_s, version
-  require gem_name.to_s
+def uses_mocha(test_name, &block)
   yield
-rescue LoadError
-  $stderr.puts "Skipping #{test_name} tests. `gem install #{gem_name}` and try again."
 end
 
-# Wrap tests that use Mocha and skip if unavailable.
-unless defined? uses_mocha
-  def uses_mocha(test_name, &block)
-    uses_gem('mocha', test_name, '>= 0.5.5', &block)
-  end
+def uses_memcached(test_name)
+  require 'memcache'
+  MemCache.new('localhost').stats
+  yield
+rescue MemCache::MemCacheError
+  $stderr.puts "Skipping #{test_name} tests. Start memcached and try again."
 end
-
-unless defined? uses_memcached
-  def uses_memcached(test_name)
-    require 'memcache'
-    MemCache.new('localhost').stats
-    yield
-  rescue MemCache::MemCacheError
-    $stderr.puts "Skipping #{test_name} tests. Start memcached and try again."
-  end
-end
-
-# Show backtraces for deprecated behavior for quicker cleanup.
-ActiveSupport::Deprecation.debug = true
 
 def with_kcode(code)
   if RUBY_VERSION < '1.9'
@@ -51,4 +29,11 @@ def with_kcode(code)
   else
     yield
   end
+end
+
+# Show backtraces for deprecated behavior for quicker cleanup.
+ActiveSupport::Deprecation.debug = true
+
+if RUBY_VERSION < '1.9'
+  $KCODE = 'UTF8'
 end
