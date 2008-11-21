e50530ca3ab5db53ebc74314c54b62b91b932389
David Heinemeier Hansson <david@loudthinking.com>
2008-11-21 09:06:46 +0100

    Reduced the number of literal aliases to the range that has actually seen personal use. With the massive savings in overhead, I was able to fit Array#forty_two

--- activesupport/lib/active_support/core_ext/array/access.rb
+++ activesupport/lib/active_support/core_ext/array/access.rb
@@ -43,29 +43,9 @@ module ActiveSupport #:nodoc:
           self[4]
         end
 
-        # Equal to <tt>self[5]</tt>.
-        def sixth
-          self[5]
-        end
-
-        # Equal to <tt>self[6]</tt>.
-        def seventh
-          self[6]
-        end
-
-        # Equal to <tt>self[7]</tt>.
-        def eighth
-          self[7]
-        end
-
-        # Equal to <tt>self[8]</tt>.
-        def ninth
-          self[8]
-        end
-
-        # Equal to <tt>self[9]</tt>.
-        def tenth
-          self[9]
+        # Equal to <tt>self[41]</tt>. Also known as accessing "the reddit".
+        def forty_two
+          self[41]
         end
       end
     end
