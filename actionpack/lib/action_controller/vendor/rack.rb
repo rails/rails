4d2ccbb364b62275bad142d9a0b66ee7fd0c424a
Jeremy Kemper <jeremy@bitsweat.net>
2008-11-22 22:40:32 -0800

    Use a relative require for bundled rack lib

--- actionpack/lib/action_controller/vendor/rack.rb
+++ actionpack/lib/action_controller/vendor/rack.rb
@@ -4,5 +4,6 @@ begin
   gem 'rack', '~> 0.4.0'
   require 'rack'
 rescue Gem::LoadError
-  require "#{File.dirname(__FILE__)}/rack-0.4.0/rack"
+  $LOAD_PATH << "#{File.dirname(__FILE__)}/rack-0.4.0"
+  require 'rack'
 end
