668872efd85291895d3e68f3a5af312973a1be74
David Heinemeier Hansson <david@loudthinking.com>
2008-11-30 15:54:44 -0600

    Add restart notice where missing

--- railties/configs/initializers/new_rails_defaults.rb
+++ railties/configs/initializers/new_rails_defaults.rb
@@ -1,3 +1,5 @@
+# Be sure to restart your server when you modify this file.
+
 # These settings change the behavior of Rails 2 apps and will be defaults
 # for Rails 3. You can remove this initializer when Rails 3 is released.
 
--- railties/configs/initializers/session_store.rb
+++ railties/configs/initializers/session_store.rb
@@ -1,3 +1,5 @@
+# Be sure to restart your server when you modify this file.
+
 # Your secret key for verifying cookie session data integrity.
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
