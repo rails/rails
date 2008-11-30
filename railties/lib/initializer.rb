471f0240189fcdd2067621d51427dc09565fdc6a
David Heinemeier Hansson <david@loudthinking.com>
2008-11-30 14:58:42 -0600

    Me loves me some whitespace

--- railties/lib/initializer.rb
+++ railties/lib/initializer.rb
@@ -513,10 +513,15 @@ Run `rake gems:install` to install the missing gems.
     def initialize_time_zone
       if configuration.time_zone
         zone_default = Time.__send__(:get_zone, configuration.time_zone)
+
         unless zone_default
-          raise %{Value assigned to config.time_zone not recognized. Run "rake -D time" for a list of tasks for finding appropriate time zone names.}
+          raise \
+            'Value assigned to config.time_zone not recognized.' +
+            'Run "rake -D time" for a list of tasks for finding appropriate time zone names.'
         end
+
         Time.zone_default = zone_default
+
         if configuration.frameworks.include?(:active_record)
           ActiveRecord::Base.time_zone_aware_attributes = true
           ActiveRecord::Base.default_timezone = :utc
