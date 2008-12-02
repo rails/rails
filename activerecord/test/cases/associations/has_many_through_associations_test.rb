757e4364dc3f808f0002a6c8cb03531e69e2f356
Emilio Tagua <miloops@gmail.com>
2008-12-02 14:12:09 -0300

    Fix: counter_cache should decrement on deleting associated records.
    [#1195 state:committed]
    
    Signed-off-by: Jeremy Kemper <jeremy@bitsweat.net>

--- activerecord/test/cases/associations/has_many_through_associations_test.rb
+++ activerecord/test/cases/associations/has_many_through_associations_test.rb
@@ -2,6 +2,10 @@ require "cases/helper"
 require 'models/post'
 require 'models/person'
 require 'models/reader'
+require 'models/comment'
+require 'models/tag'
+require 'models/tagging'
+require 'models/author'
 
 class HasManyThroughAssociationsTest < ActiveRecord::TestCase
   fixtures :posts, :readers, :people
@@ -79,6 +83,17 @@ class HasManyThroughAssociationsTest < ActiveRecord::TestCase
     end
     
     assert posts(:welcome).reload.people(true).empty?
+  end
+
+  def test_deleting_updates_counter_cache
+    taggable = Tagging.first.taggable
+    taggable.taggings.push(Tagging.new)
+    taggable.reload
+    assert_equal 1, taggable.taggings_count
+
+    taggable.taggings.delete(taggable.taggings.first)
+    taggable.reload
+    assert_equal 0, taggable.taggings_count
   end
 
   def test_replace_association
