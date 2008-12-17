f23c2796ee4a217d48bcefbc0056aaab6b1b64fa
Joshua Peek <josh@joshpeek.com>
2008-12-17 10:20:19 -0600

    When checking for the wrong routing method, ensure the environment is passed to recognize optimize [#1406 state:resolved]

--- actionpack/lib/action_controller/routing/recognition_optimisation.rb
+++ actionpack/lib/action_controller/routing/recognition_optimisation.rb
@@ -56,7 +56,7 @@ module ActionController
         result = recognize_optimized(path, environment) and return result
 
         # Route was not recognized. Try to find out why (maybe wrong verb).
-        allows = HTTP_METHODS.select { |verb| routes.find { |r| r.recognize(path, :method => verb) } }
+        allows = HTTP_METHODS.select { |verb| routes.find { |r| r.recognize(path, environment.merge(:method => verb)) } }
 
         if environment[:method] && !HTTP_METHODS.include?(environment[:method])
           raise NotImplemented.new(*allows)
