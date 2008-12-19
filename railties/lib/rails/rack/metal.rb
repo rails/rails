12e416a04b764d0b212e04a53b770f60dc0d7071
Joshua Peek <josh@joshpeek.com>
2008-12-19 11:04:27 -0600

    Diverge Metal implementation from Rack::Cascade since we want the last app to return its headers and body if the status is a 404.

--- railties/lib/rails/rack/metal.rb
+++ railties/lib/rails/rack/metal.rb
@@ -1,26 +1,35 @@
-require 'rails/rack/cascade'
+require 'active_support/ordered_hash'
 
 module Rails
   module Rack
-    module Metal
+    class Metal
       NotFoundResponse = [404, {}, []].freeze
       NotFound = lambda { NotFoundResponse }
 
-      class << self
-        def new(app)
-          Cascade.new(builtins + [app])
-        end
+      def self.metals
+        base = "#{Rails.root}/app/metal"
+        matcher = /\A#{Regexp.escape(base)}\/(.*)\.rb\Z/
 
-        def builtins
-          base = "#{Rails.root}/app/metal"
-          matcher = /\A#{Regexp.escape(base)}\/(.*)\.rb\Z/
-
-          Dir["#{base}/**/*.rb"].sort.map do |file|
-            file.sub!(matcher, '\1')
-            require file
-            file.classify.constantize
-          end
+        Dir["#{base}/**/*.rb"].sort.map do |file|
+          file.sub!(matcher, '\1')
+          require file
+          file.classify.constantize
         end
+      end
+
+      def initialize(app)
+        @app = app
+        @metals = ActiveSupport::OrderedHash.new
+        self.class.metals.each { |app| @metals[app] = true }
+        freeze
+      end
+
+      def call(env)
+        @metals.keys.each do |app|
+          result = app.call(env)
+          return result unless result[0].to_i == 404
+        end
+        @app.call(env)
       end
     end
   end
