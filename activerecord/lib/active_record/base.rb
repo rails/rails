f7bd0beb67c5d9d50e37aa596605b91e61197fbe
Daniel Luz <dev@mernen.com>
2008-12-21 22:38:12 +0000

    Ensure Model#last doesn't affects order for another finders inside the same scope [#1499 state:resolved]
    Signed-off-by: Pratik Naik <pratiknaik@gmail.com>

--- activerecord/lib/active_record/base.rb
+++ activerecord/lib/active_record/base.rb
@@ -1494,11 +1494,16 @@ module ActiveRecord #:nodoc:
           end
 
           if scoped?(:find, :order)
-            scoped_order = reverse_sql_order(scope(:find, :order))
-            scoped_methods.select { |s| s[:find].update(:order => scoped_order) }
+            scope = scope(:find)
+            original_scoped_order = scope[:order]
+            scope[:order] = reverse_sql_order(original_scoped_order)
           end
 
-          find_initial(options.merge({ :order => order }))
+          begin
+            find_initial(options.merge({ :order => order }))
+          ensure
+            scope[:order] = original_scoped_order if original_scoped_order
+          end
         end
 
         def reverse_sql_order(order_query)
