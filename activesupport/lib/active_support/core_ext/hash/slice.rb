c9d4335418823500548ad8fbc86af7c910b7644b
trans <transfire@gmail.com>
2008-12-25 00:24:05 +0000

    MaKe Hash#slice! return removed values, akin to Array [#971 state:resolved]
    Signed-off-by: Frederick Cheung <frederick.cheung@gmail.com>

--- activesupport/lib/active_support/core_ext/hash/slice.rb
+++ activesupport/lib/active_support/core_ext/hash/slice.rb
@@ -24,10 +24,17 @@ module ActiveSupport #:nodoc:
         end
 
         # Replaces the hash with only the given keys.
+        # Returns a hash contained the removed key/value pairs
+        #   {:a => 1, :b => 2, :c => 3, :d => 4}.slice!(:a, :b) # => {:c => 3, :d =>4}
         def slice!(*keys)
-          replace(slice(*keys))
+          keys = keys.map! { |key| convert_key(key) } if respond_to?(:convert_key)
+          omit = slice(*self.keys - keys)
+          hash = slice(*keys)
+          replace(hash)
+          omit
         end
       end
     end
   end
 end
+
