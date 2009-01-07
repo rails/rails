b1530545d259c144770bd8fd7881cb16160c0afc
Pratik Naik <pratiknaik@gmail.com>
2009-01-07 17:51:11 +0000

    Fix JSON decoder date-converter regexp [#1662 state:resolved] [Jonathan del Strother]

--- activesupport/lib/active_support/json/decoding.rb
+++ activesupport/lib/active_support/json/decoding.rb
@@ -16,7 +16,7 @@ module ActiveSupport
       
       protected
         # matches YAML-formatted dates
-        DATE_REGEX = /^\d{4}-\d{2}-\d{2}|\d{4}-\d{1,2}-\d{1,2}[ \t]+\d{1,2}:\d{2}:\d{2}(\.[0-9]*)?(([ \t]*)Z|[-+]\d{2}?(:\d{2})?)?$/
+        DATE_REGEX = /^(?:\d{4}-\d{2}-\d{2}|\d{4}-\d{1,2}-\d{1,2}[ \t]+\d{1,2}:\d{2}:\d{2}(\.[0-9]*)?(([ \t]*)Z|[-+]\d{2}?(:\d{2})?)?)$/
 
         # Ensure that ":" and "," are always followed by a space
         def convert_json_to_yaml(json) #:nodoc:
