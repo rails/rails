29e7a0242853a5e102b6846b87723fc26a1ffb08
Joshua Peek <josh@joshpeek.com>
2009-01-17 11:12:18 -0600

    Ensure any method sent to RewindableIO reads the original IO object [#1767 state:resolved]

--- actionpack/lib/action_controller/rack_ext/multipart.rb
+++ actionpack/lib/action_controller/rack_ext/multipart.rb
@@ -6,8 +6,8 @@ module Rack
           result = parse_multipart_without_rewind(env)
 
           begin
-            env['rack.input'].rewind
-          rescue NoMethodError, Errno::ESPIPE
+            env['rack.input'].rewind if env['rack.input'].respond_to?(:rewind)
+          rescue Errno::ESPIPE
             # Handles exceptions raised by input streams that cannot be rewound
             # such as when using plain CGI under Apache
           end
