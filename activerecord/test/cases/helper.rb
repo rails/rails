97bc170a79e81851e8f2e744c688e3993ccad70c
Michael Koziarski <michael@koziarski.com>
2009-01-18 14:30:51 +1300

    Fix has_and_belongs_to_many_associations tests. #1738
    Conflicts:
    
    	activerecord/test/cases/helper.rb

--- activerecord/test/cases/helper.rb
+++ activerecord/test/cases/helper.rb
@@ -31,7 +31,7 @@ rescue LoadError
 end
 
 ActiveRecord::Base.connection.class.class_eval do
-  IGNORED_SQL = [/^PRAGMA/, /^SELECT currval/, /^SELECT CAST/, /^SELECT @@IDENTITY/, /^SELECT @@ROWCOUNT/]
+  IGNORED_SQL = [/^PRAGMA/, /^SELECT currval/, /^SELECT CAST/, /^SELECT @@IDENTITY/, /^SELECT @@ROWCOUNT/, /SHOW FIELDS/]
 
   def execute_with_query_record(sql, name = nil, &block)
     $queries_executed ||= []
