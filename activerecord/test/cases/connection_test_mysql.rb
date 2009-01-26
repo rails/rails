e5c211c5eb6e8f1651130b7aa0a5291e5d184ab4
Dov Murik <dov.murik@gmail.com>
2009-01-26 16:10:00 +0200

    Mysql#reconnect is set according to the 'reconnect' key in the connection spec.
    The 'reconenct' boolean option is read from the connection specification
    and is used to set the reconnect attribute of Mysql.  The default is
    false in order not to change existing application behaviour.
    
    Also, reconnect is set AFTER real_connect is called, so its value sticks
    (the mysql gem sets reconnect to false inside real_connect).
    
    Signed-off-by: Michael Koziarski <michael@koziarski.com>
    [#1797 state:committed]

--- activerecord/test/cases/connection_test_mysql.rb
+++ activerecord/test/cases/connection_test_mysql.rb
@@ -2,7 +2,22 @@ require "cases/helper"
 
 class MysqlConnectionTest < ActiveRecord::TestCase
   def setup
+    super
     @connection = ActiveRecord::Base.connection
+  end
+
+  def test_mysql_reconnect_attribute_after_connection_with_reconnect_true
+    run_without_connection do |orig_connection|
+      ActiveRecord::Base.establish_connection(orig_connection.merge({:reconnect => true}))
+      assert ActiveRecord::Base.connection.raw_connection.reconnect
+    end
+  end
+
+  def test_mysql_reconnect_attribute_after_connection_with_reconnect_false
+    run_without_connection do |orig_connection|
+      ActiveRecord::Base.establish_connection(orig_connection.merge({:reconnect => false}))
+      assert !ActiveRecord::Base.connection.raw_connection.reconnect
+    end
   end
 
   def test_no_automatic_reconnection_after_timeout
@@ -26,5 +41,16 @@ class MysqlConnectionTest < ActiveRecord::TestCase
     sleep 2
     @connection.verify!
     assert @connection.active?
+  end
+
+  private
+
+  def run_without_connection
+    original_connection = ActiveRecord::Base.remove_connection
+    begin
+      yield original_connection
+    ensure
+      ActiveRecord::Base.establish_connection(original_connection)
+    end
   end
 end
