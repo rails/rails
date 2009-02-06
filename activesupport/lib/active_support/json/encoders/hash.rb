5f56d90085ea484b99e080c231c17ddc6cda71d1
Jeremy Kemper <jeremy@bitsweat.net>
2009-02-06 10:05:16 -0800

    Use Array.wrap, remove unneeded returning block

--- activesupport/lib/active_support/json/encoders/hash.rb
+++ activesupport/lib/active_support/json/encoders/hash.rb
@@ -31,17 +31,16 @@ class Hash
   def to_json(options = {}) #:nodoc:
     hash_keys = self.keys
 
-    if options[:except]
-      hash_keys = hash_keys - Array(options[:except])
-    elsif options[:only]
-      hash_keys = hash_keys & Array(options[:only])
+    if except = options[:except]
+      hash_keys = hash_keys - Array.wrap(except)
+    elsif only = options[:only]
+      hash_keys = hash_keys & Array.wrap(only)
     end
 
-    returning result = '{' do
-      result << hash_keys.map do |key|
-        "#{ActiveSupport::JSON.encode(key.to_s)}: #{ActiveSupport::JSON.encode(self[key], options)}"
-      end * ', '
-      result << '}'
-    end
+    result = '{'
+    result << hash_keys.map do |key|
+      "#{ActiveSupport::JSON.encode(key.to_s)}: #{ActiveSupport::JSON.encode(self[key], options)}"
+    end * ', '
+    result << '}'
   end
 end
