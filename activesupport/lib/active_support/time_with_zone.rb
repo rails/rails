5120429c3138d46490a1c4a611ebd93410f4f885
Geoff Buesing <gbuesing@gmail.com>
2009-02-08 23:40:42 -0600

    TimeWithZone#advance: use #any? instead of #detect

--- activesupport/lib/active_support/time_with_zone.rb
+++ activesupport/lib/active_support/time_with_zone.rb
@@ -230,7 +230,7 @@ module ActiveSupport
     def advance(options)
       # If we're advancing a value of variable length (i.e., years, weeks, months, days), advance from #time,
       # otherwise advance from #utc, for accuracy when moving across DST boundaries
-      if options.detect {|k,v| [:years, :weeks, :months, :days].include? k}
+      if options.any? {|k,v| [:years, :weeks, :months, :days].include? k}
         method_missing(:advance, options)
       else
         utc.advance(options).in_time_zone(time_zone)
