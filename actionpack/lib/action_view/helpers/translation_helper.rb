7527cdf79c640eae5db29a6f3f9b955aa50bc29e
David Heinemeier Hansson <david@loudthinking.com>
2009-02-10 12:57:12 +0100

    Added partial scoping to TranslationHelper#translate, so if you call translate('.foo') from the people/index.html.erb template, you'll actually be calling I18n.translate(people.index.foo) [DHH]

--- actionpack/lib/action_view/helpers/translation_helper.rb
+++ actionpack/lib/action_view/helpers/translation_helper.rb
@@ -3,19 +3,37 @@ require 'action_view/helpers/tag_helper'
 module ActionView
   module Helpers
     module TranslationHelper
+      # Delegates to I18n#translate but also performs two additional functions. First, it'll catch MissingTranslationData exceptions 
+      # and turn them into inline spans that contains the missing key, such that you can see in a view what is missing where.
+      #
+      # Second, it'll scope the key by the current partial if the key starts with a period. So if you call translate(".foo") from the
+      # people/index.html.erb template, you'll actually be calling I18n.translate("people.index.foo"). This makes it less repetitive
+      # to translate many keys within the same partials and gives you a simple framework for scoping them consistently. If you don't
+      # prepend the key with a period, nothing is converted.
       def translate(key, options = {})
         options[:raise] = true
-        I18n.translate(key, options)
+        I18n.translate(scope_key_by_partial(key), options)
       rescue I18n::MissingTranslationData => e
         keys = I18n.send(:normalize_translation_keys, e.locale, e.key, e.options[:scope])
         content_tag('span', keys.join(', '), :class => 'translation_missing')
       end
       alias :t :translate
 
+      # Delegates to I18n.localize with no additional functionality.
       def localize(*args)
         I18n.localize *args
       end
       alias :l :localize
+
+
+      private
+        def scope_key_by_partial(key)
+          if key.to_s.first == "."
+            template.path_without_format_and_extension.gsub(%r{/_?}, ".") + key.to_s
+          else
+            key
+          end
+        end
     end
   end
 end
