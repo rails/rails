b0792a3e7be88e3060af19bab01cd3d26d347e4c
Joshua Sierles <joshua@37signals.com>
2009-02-12 13:24:36 -0600

    Allow memcache-client versions > 1.5.x to override bundled version
    Signed-off-by: Joshua Peek <josh@joshpeek.com>

--- activesupport/lib/active_support/vendor.rb
+++ activesupport/lib/active_support/vendor.rb
@@ -14,7 +14,7 @@ rescue Gem::LoadError
 end
 
 begin
-  gem 'memcache-client', '~> 1.5.1'
+  gem 'memcache-client', '>= 1.5.1'
 rescue Gem::LoadError
   $:.unshift "#{File.dirname(__FILE__)}/vendor/memcache-client-1.5.1"
 end
