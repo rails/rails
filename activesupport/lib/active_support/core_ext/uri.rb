98ddc6478252c2a2e24f2169125f2b3f53dd7b36
Jeremy Kemper <jeremy@bitsweat.net>
2009-02-20 18:25:02 -0800

    URI.unescape fix removes the old unescape method

--- activesupport/lib/active_support/core_ext/uri.rb
+++ activesupport/lib/active_support/core_ext/uri.rb
@@ -1,13 +1,10 @@
-require 'uri'
-
-module ActiveSupport
-  if RUBY_VERSION == "1.9.1" && defined?(RUBY_PATCHLEVEL) && RUBY_PATCHLEVEL == 0
-    ::URI::Parser.class_eval do
-      def unescape(str, escaped = @regexp[:ESCAPED])
-        enc = (str.encoding == Encoding::US_ASCII) ? Encoding::UTF_8 : str.encoding
-        str.gsub(escaped) { [$&[1, 2].hex].pack('C') }.force_encoding(enc)
-      end
+if RUBY_VERSION == "1.9.1" && defined?(RUBY_PATCHLEVEL) && RUBY_PATCHLEVEL == 0
+  require 'uri'
+  URI::Parser.class_eval do
+    remove_method :unescape
+    def unescape(str, escaped = @regexp[:ESCAPED])
+      enc = (str.encoding == Encoding::US_ASCII) ? Encoding::UTF_8 : str.encoding
+      str.gsub(escaped) { [$&[1, 2].hex].pack('C') }.force_encoding(enc)
     end
   end
 end
-
