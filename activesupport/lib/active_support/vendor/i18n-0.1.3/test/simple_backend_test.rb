943d64b1c857944666bff2572857abe866615274
Sven Fuchs <svenfuchs@artweb-design.de>
2009-02-27 17:07:06 +0100

    Vendorize i18n 0.1.3 gem (fixes issues with incompatible character encodings in Ruby 1.9) (Akira Matsuda) [#2038 state:committed]
    Signed-off-by: David Heinemeier Hansson <david@loudthinking.com>

--- activesupport/lib/active_support/vendor/i18n-0.1.3/test/simple_backend_test.rb
+++ activesupport/lib/active_support/vendor/i18n-0.1.3/test/simple_backend_test.rb
@@ -253,6 +253,32 @@ class I18nSimpleBackendInterpolateTest < Test::Unit::TestCase
     assert_equal 'Häi David!', @backend.send(:interpolate, nil, 'Häi {{name}}!', :name => 'David')
   end
 
+  def test_interpolate_given_an_unicode_value_hash_interpolates_to_the_string
+    assert_equal 'Hi ゆきひろ!', @backend.send(:interpolate, nil, 'Hi {{name}}!', :name => 'ゆきひろ')
+  end
+
+  def test_interpolate_given_an_unicode_value_hash_interpolates_into_unicode_string
+    assert_equal 'こんにちは、ゆきひろさん!', @backend.send(:interpolate, nil, 'こんにちは、{{name}}さん!', :name => 'ゆきひろ')
+  end
+
+  if Kernel.const_defined?(:Encoding)
+    def test_interpolate_given_a_non_unicode_multibyte_value_hash_interpolates_into_a_string_with_the_same_encoding
+      assert_equal euc_jp('Hi ゆきひろ!'), @backend.send(:interpolate, nil, 'Hi {{name}}!', :name => euc_jp('ゆきひろ'))
+    end
+
+    def test_interpolate_given_an_unicode_value_hash_into_a_non_unicode_multibyte_string_raises_encoding_compatibility_error
+      assert_raises(Encoding::CompatibilityError) do
+        @backend.send(:interpolate, nil, euc_jp('こんにちは、{{name}}さん!'), :name => 'ゆきひろ')
+      end
+    end
+
+    def test_interpolate_given_a_non_unicode_multibyte_value_hash_into_an_unicode_string_raises_encoding_compatibility_error
+      assert_raises(Encoding::CompatibilityError) do
+        @backend.send(:interpolate, nil, 'こんにちは、{{name}}さん!', :name => euc_jp('ゆきひろ'))
+      end
+    end
+  end
+
   def test_interpolate_given_nil_as_a_string_returns_nil
     assert_nil @backend.send(:interpolate, nil, nil, :name => 'David')
   end
@@ -271,6 +297,12 @@ class I18nSimpleBackendInterpolateTest < Test::Unit::TestCase
 
   def test_interpolate_given_a_string_containing_a_reserved_key_raises_reserved_interpolation_key
     assert_raises(I18n::ReservedInterpolationKey) { @backend.send(:interpolate, nil, '{{default}}', {:default => nil}) }
+  end
+  
+  private
+  
+  def euc_jp(string)
+    string.encode!(Encoding::EUC_JP)
   end
 end
 
@@ -485,9 +517,21 @@ end
 class I18nSimpleBackendLoadPathTest < Test::Unit::TestCase
   include I18nSimpleBackendTestSetup
 
+  def teardown
+    I18n.load_path = []
+  end
+
   def test_nested_load_paths_do_not_break_locale_loading
     @backend = I18n::Backend::Simple.new
     I18n.load_path = [[File.dirname(__FILE__) + '/locale/en.yml']]
+    assert_nil backend_get_translations
+    assert_nothing_raised { @backend.send :init_translations }
+    assert_not_nil backend_get_translations
+  end
+
+  def test_adding_arrays_of_filenames_to_load_path_do_not_break_locale_loading
+    @backend = I18n::Backend::Simple.new
+    I18n.load_path << Dir[File.dirname(__FILE__) + '/locale/*.{rb,yml}']
     assert_nil backend_get_translations
     assert_nothing_raised { @backend.send :init_translations }
     assert_not_nil backend_get_translations
