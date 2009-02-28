3b169cd693f45911ee71e26708fb9267811c8d83
Charles Nutter <charles.nutter@sun.com>
2009-02-28 17:47:00 -0600

    Speed up script/console by launching IRB directly.
    [#2104 state:committed]
    
    Signed-off-by: Jeremy Kemper <jeremy@bitsweat.net>

--- railties/lib/commands/console.rb
+++ railties/lib/commands/console.rb
@@ -1,26 +1,23 @@
-irb = RUBY_PLATFORM =~ /(:?mswin|mingw)/ ? 'irb.bat' : 'irb'
-
 require 'optparse'
 
-options = { :sandbox => false, :irb => irb }
+options = { :sandbox => false }
 OptionParser.new do |opt|
   opt.banner = "Usage: console [environment] [options]"
   opt.on('-s', '--sandbox', 'Rollback database modifications on exit.') { |v| options[:sandbox] = v }
-  opt.on("--irb=[#{irb}]", 'Invoke a different irb.') { |v| options[:irb] = v }
   opt.on("--debugger", 'Enable ruby-debugging for the console.') { |v| options[:debugger] = v }
   opt.parse!(ARGV)
 end
 
-libs =  " -r irb/completion"
-libs << %( -r "#{RAILS_ROOT}/config/environment")
-libs << " -r console_app"
-libs << " -r console_sandbox" if options[:sandbox]
-libs << " -r console_with_helpers"
+require 'irb'
+require "irb/completion"
+require "#{RAILS_ROOT}/config/environment"
+require "console_app"
+require "console_sandbox" if options[:sandbox]
+require "console_with_helpers"
 
 if options[:debugger]
   begin
     require 'ruby-debug'
-    libs << " -r ruby-debug"
     puts "=> Debugger enabled"
   rescue Exception
     puts "You need to install ruby-debug to run the console in debugging mode. With gems, use 'gem install ruby-debug'"
@@ -42,4 +39,6 @@ if options[:sandbox]
 else
   puts "Loading #{ENV['RAILS_ENV']} environment (Rails #{Rails.version})"
 end
-exec "#{options[:irb]} #{libs} --simple-prompt"
+
+ARGV.replace(["--simple-prompt"])
+IRB.start
