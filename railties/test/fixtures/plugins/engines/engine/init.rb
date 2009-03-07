4185fb1e58d8142162c3a39f9302559688dd431b
karmi <karel.minarik@gmail.com>
2009-03-07 16:02:52 +0000

    Raise LoadError instead of a runtime exception [#1498 state:resolved]
    Signed-off-by: Pratik Naik <pratiknaik@gmail.com>

--- railties/test/fixtures/plugins/engines/engine/init.rb
+++ railties/test/fixtures/plugins/engines/engine/init.rb
@@ -1,3 +1,3 @@
 # My app/models dir must be in the load path.
 require 'engine_model'
-raise 'missing model from my app/models dir' unless defined?(EngineModel)
+raise LoadError, 'missing model from my app/models dir' unless defined?(EngineModel)
