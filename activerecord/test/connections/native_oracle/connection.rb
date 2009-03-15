04fea8a07ba94d381b03fae4a657077a7b966229
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2009-03-15 14:40:07 +0200

    modified native_oracle/connection.rb to run it with oracle_enhanced adapter

--- activerecord/test/connections/native_oracle/connection.rb
+++ activerecord/test/connections/native_oracle/connection.rb
@@ -1,25 +1,36 @@
+# gem "rsim-activerecord-oracle_enhanced-adapter"
+# gem "activerecord-oracle_enhanced-adapter"
+# uses local copy of oracle_enhanced adapter
+$:.unshift("../../oracle-enhanced/lib")
+require 'active_record/connection_adapters/oracle_enhanced_adapter'
+
 print "Using Oracle\n"
 require_dependency 'models/course'
 require 'logger'
 
-ActiveRecord::Base.logger = Logger.new STDOUT
-ActiveRecord::Base.logger.level = Logger::WARN
+# ActiveRecord::Base.logger = Logger.new STDOUT
+# ActiveRecord::Base.logger.level = Logger::WARN
+ActiveRecord::Base.logger = Logger.new("debug.log")
 
 # Set these to your database connection strings
-db = ENV['ARUNIT_DB'] || 'activerecord_unittest'
+db = ENV['ARUNIT_DB'] || 'XE'
 
 ActiveRecord::Base.configurations = {
   'arunit' => {
-    :adapter  => 'oracle',
+    :adapter  => 'oracle_enhanced',
+    :database => db,
+    :host => "arunit", # used just by JRuby to construct JDBC connect string
     :username => 'arunit',
     :password => 'arunit',
-    :database => db,
+    :emulate_oracle_adapter => true
   },
   'arunit2' => {
-    :adapter  => 'oracle',
+    :adapter  => 'oracle_enhanced',
+    :database => db,
+    :host => "arunit", # used just by JRuby to construct JDBC connect string
     :username => 'arunit2',
     :password => 'arunit2',
-    :database => db
+    :emulate_oracle_adapter => true
   }
 }
 
