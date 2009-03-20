d01e639dafece0cbae08512f4645bcee7a5a8224
Pratik Naik <pratiknaik@gmail.com>
2009-03-20 15:33:40 +0000

    Remove unused columns from the ActiveModel test schema

--- activemodel/test/schema.rb
+++ activemodel/test/schema.rb
@@ -2,21 +2,12 @@ ActiveRecord::Schema.define do
   create_table :topics, :force => true do |t|
     t.string   :title
     t.string   :author_name
-    t.string   :author_email_address
-    t.datetime :written_on
-    t.time     :bonus_time
-    t.date     :last_read
     t.text     :content
     t.boolean  :approved, :default => true
-    t.integer  :replies_count, :default => 0
-    t.integer  :parent_id
     t.string   :type
   end
 
   create_table :developers, :force => true do |t|
     t.string   :name
-    t.integer  :salary, :default => 70000
-    t.datetime :created_at
-    t.datetime :updated_at
   end
 end
