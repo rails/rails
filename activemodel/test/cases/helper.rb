e945bcfe4a519e6cf7349443c48fecce2e8c9d67
Pratik Naik <pratiknaik@gmail.com>
2009-03-20 11:22:27 +0000

    Add test sqlite3 db to .gitignore

--- activemodel/test/cases/helper.rb
+++ activemodel/test/cases/helper.rb
@@ -9,9 +9,6 @@ require 'active_record'
 require 'logger'
 ActiveRecord::Base.logger = Logger.new("debug.log")
 
-class SqliteError < StandardError
-end
-
 # Setup database connection
 db_file = "#{FIXTURES_ROOT}/fixture_database.sqlite3"
 ActiveRecord::Base.configurations = { ActiveRecord::Base.name => { :adapter => 'sqlite3', :database => db_file, :timeout => 5000 } }
@@ -19,7 +16,7 @@ unless File.exist?(db_file)
   puts "SQLite3 database not found at #{db_file}. Rebuilding it."
   sqlite_command = %Q{sqlite3 "#{db_file}" "create table a (a integer); drop table a;"}
   puts "Executing '#{sqlite_command}'"
-  raise SqliteError.new("Seems that there is no sqlite3 executable available") unless system(sqlite_command)
+  raise StandardError.new("Seems that there is no sqlite3 executable available") unless system(sqlite_command)
 end
 ActiveRecord::Base.establish_connection(ActiveRecord::Base.name)
 
