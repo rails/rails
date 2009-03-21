b4a1718832c70005e45a930d5106857cf882e147
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-21 04:00:51 -0700

    Convert hash extension modules to class reopens

--- activesupport/lib/active_support/core_ext/hash.rb
+++ activesupport/lib/active_support/core_ext/hash.rb
@@ -1,2 +1,10 @@
+require 'active_support/core_ext/hash/deep_merge'
+require 'active_support/core_ext/hash/diff'
+require 'active_support/core_ext/hash/except'
+require 'active_support/core_ext/hash/indifferent_access'
+require 'active_support/core_ext/hash/keys'
+require 'active_support/core_ext/hash/reverse_merge'
+require 'active_support/core_ext/hash/slice'
+
 require 'active_support/core_ext/util'
-ActiveSupport.core_ext Hash, %w(keys indifferent_access deep_merge reverse_merge conversions diff slice except)
+ActiveSupport.core_ext Hash, %w(conversions)
--- activesupport/lib/active_support/core_ext/hash/diff.rb
+++ activesupport/lib/active_support/core_ext/hash/diff.rb
@@ -1,19 +1,13 @@
-module ActiveSupport #:nodoc:
-  module CoreExtensions #:nodoc:
-    module Hash #:nodoc:
-      module Diff
-        # Returns a hash that represents the difference between two hashes.
-        #
-        # Examples:
-        #
-        #   {1 => 2}.diff(1 => 2)         # => {}
-        #   {1 => 2}.diff(1 => 3)         # => {1 => 2}
-        #   {}.diff(1 => 2)               # => {1 => 2}
-        #   {1 => 2, 3 => 4}.diff(1 => 2) # => {3 => 4}
-        def diff(h2)
-          self.dup.delete_if { |k, v| h2[k] == v }.merge(h2.dup.delete_if { |k, v| self.has_key?(k) })
-        end
-      end
-    end
+class Hash
+  # Returns a hash that represents the difference between two hashes.
+  #
+  # Examples:
+  #
+  #   {1 => 2}.diff(1 => 2)         # => {}
+  #   {1 => 2}.diff(1 => 3)         # => {1 => 2}
+  #   {}.diff(1 => 2)               # => {1 => 2}
+  #   {1 => 2, 3 => 4}.diff(1 => 2) # => {3 => 4}
+  def diff(h2)
+    dup.delete_if { |k, v| h2[k] == v }.merge(h2.dup.delete_if { |k, v| has_key?(k) })
   end
 end
