bd28c7b1b8a569d431cc93924ad1ff5fdb59f401
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-21 03:34:49 -0700

    blank? and duplicable? are Object extensions

--- activesupport/lib/active_support/core_ext/object.rb
+++ activesupport/lib/active_support/core_ext/object.rb
@@ -1,3 +1,6 @@
+require 'active_support/core_ext/object/blank'
+require 'active_support/core_ext/object/duplicable'
+
 require 'active_support/core_ext/object/conversions'
 require 'active_support/core_ext/object/extending'
 require 'active_support/core_ext/object/instance_variables'
--- activesupport/lib/active_support/core_ext/object/blank.rb
+++ activesupport/lib/active_support/core_ext/object/blank.rb
@@ -12,7 +12,7 @@ class Object
   def blank?
     respond_to?(:empty?) ? empty? : !self
   end
-    
+
   # An object is present if it's not blank.
   def present?
     !blank?
