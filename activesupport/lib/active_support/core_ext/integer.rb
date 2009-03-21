54cf0fc476b2d77adb8a124c27a79a048fa4ddb5
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-21 04:13:57 -0700

    Convert integer extension modules to class reopens

--- activesupport/lib/active_support/core_ext/integer.rb
+++ activesupport/lib/active_support/core_ext/integer.rb
@@ -1,2 +1,5 @@
+require 'active_support/core_ext/integer/even_odd'
+require 'active_support/core_ext/integer/inflections'
+
 require 'active_support/core_ext/util'
-ActiveSupport.core_ext Integer, %w(even_odd inflections time)
+ActiveSupport.core_ext Integer, %w(time)
--- activesupport/lib/active_support/core_ext/integer/even_odd.rb
+++ activesupport/lib/active_support/core_ext/integer/even_odd.rb
@@ -1,29 +1,16 @@
-module ActiveSupport #:nodoc:
-  module CoreExtensions #:nodoc:
-    module Integer #:nodoc:
-      # For checking if a fixnum is even or odd.
-      #
-      #   2.even?  # => true
-      #   2.odd?   # => false
-      #   1.even?  # => false
-      #   1.odd?   # => true
-      #   0.even?  # => true
-      #   0.odd?   # => false
-      #   -1.even? # => false
-      #   -1.odd?  # => true
-      module EvenOdd
-        def multiple_of?(number)
-          self % number == 0
-        end
-
-        def even?
-          multiple_of? 2
-        end if RUBY_VERSION < '1.9'
-
-        def odd?
-          !even?
-        end if RUBY_VERSION < '1.9'
-      end
-    end
+class Integer
+  # Check whether the integer is evenly divisible by the argument.
+  def multiple_of?(number)
+    self % number == 0
   end
+
+  # Is the integer a multiple of 2?
+  def even?
+    multiple_of? 2
+  end if RUBY_VERSION < '1.9'
+
+  # Is the integer not a multiple of 2?
+  def odd?
+    !even?
+  end if RUBY_VERSION < '1.9'
 end
