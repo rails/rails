b8d59b7f845b6de8b4359900f228efc13e7d1ee5
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-21 04:27:13 -0700

    Convert Numeric extension module to a class reopen

--- activesupport/lib/active_support/core_ext/numeric.rb
+++ activesupport/lib/active_support/core_ext/numeric.rb
@@ -1,2 +1,4 @@
+require 'active_support/core_ext/numeric/conversions'
+
 require 'active_support/core_ext/util'
-ActiveSupport.core_ext Numeric, %w(time bytes conversions)
+ActiveSupport.core_ext Numeric, %w(time bytes)
--- activesupport/lib/active_support/core_ext/numeric/conversions.rb
+++ activesupport/lib/active_support/core_ext/numeric/conversions.rb
@@ -1,19 +1,15 @@
-module ActiveSupport #:nodoc:
-  module CoreExtensions #:nodoc:
-    module Numeric #:nodoc:
-      module Conversions
-        # Assumes self represents an offset from UTC in seconds (as returned from Time#utc_offset)
-        # and turns this into an +HH:MM formatted string. Example:
-        #
-        #   -21_600.to_utc_offset_s   # => "-06:00"
-        def to_utc_offset_s(colon=true)
-          seconds = self
-          sign = (seconds < 0 ? -1 : 1)
-          hours = seconds.abs / 3600
-          minutes = (seconds.abs % 3600) / 60
-          "%+03d%s%02d" % [ hours * sign, colon ? ":" : "", minutes ]
-        end
-      end
-    end
+class Numeric
+  UTC_OFFSET_WITH_COLON = '%+03d:%02d'
+  UTC_OFFSET_WITHOUT_COLON = UTC_OFFSET_WITH_COLON.sub(':', '')
+
+  # Assumes self represents an offset from UTC in seconds (as returned from Time#utc_offset)
+  # and turns this into an +HH:MM formatted string. Example:
+  #
+  #   -21_600.to_utc_offset_s   # => "-06:00"
+  def to_utc_offset_s(colon = true)
+    format = colon ? UTC_OFFSET_WITH_COLON : UTC_OFFSET_WITHOUT_COLON
+    hours = self / 3600
+    minutes = (abs % 3600) / 60
+    format % [hours, minutes]
   end
 end
