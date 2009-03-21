baf73e19a6f05fcb46c06d759b08f3cbe22ed1c9
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-21 04:30:28 -0700

    Convert Range extension module to a class reopen

--- activesupport/lib/active_support/core_ext/range.rb
+++ activesupport/lib/active_support/core_ext/range.rb
@@ -1,2 +1,4 @@
+require 'active_support/core_ext/range/overlaps'
+
 require 'active_support/core_ext/util'
-ActiveSupport.core_ext Range, %w(conversions overlaps include_range blockless_step)
+ActiveSupport.core_ext Range, %w(conversions include_range blockless_step)
