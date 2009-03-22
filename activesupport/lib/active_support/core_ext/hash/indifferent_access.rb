0bd668fd8867b01c15f943613707b3c7eb59afff
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-21 18:44:54 -0700

    Namespace HashWithIndifferentAccess

--- activesupport/lib/active_support/core_ext/hash/indifferent_access.rb
+++ activesupport/lib/active_support/core_ext/hash/indifferent_access.rb
@@ -2,7 +2,7 @@ require 'active_support/hash_with_indifferent_access'
 
 class Hash
   def with_indifferent_access
-    hash = HashWithIndifferentAccess.new(self)
+    hash = ActiveSupport::HashWithIndifferentAccess.new(self)
     hash.default = self.default
     hash
   end
