3f7130aed842bd077acb5bacab8f99a83ea0480b
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-24 17:04:27 -0700

    Document NameError extensions

--- activesupport/lib/active_support/core_ext/name_error.rb
+++ activesupport/lib/active_support/core_ext/name_error.rb
@@ -1,10 +1,9 @@
-# Add a +missing_name+ method to NameError instances.
-class NameError #:nodoc:  
-  # Add a method to obtain the missing name from a NameError.
+class NameError
+  # Extract the name of the missing constant from the exception message.
   def missing_name
     $1 if /((::)?([A-Z]\w*)(::[A-Z]\w*)*)$/ =~ message
   end
-  
+
   # Was this exception raised because the given name was missing?
   def missing_name?(name)
     if name.is_a? Symbol
