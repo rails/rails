1c5a6944d38e6818d254f272057b513b038b2270
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-26 19:07:44 -0700

    Convert Time extension modules to class reopens

--- activesupport/lib/active_support/core_ext/time.rb
+++ activesupport/lib/active_support/core_ext/time.rb
@@ -4,5 +4,8 @@ require 'time'
 require 'active_support/core_ext/time/publicize_conversion_methods'
 require 'active_support/core_ext/time/marshal_with_utc_flag'
 
+require 'active_support/core_ext/time/calculations'
+require 'active_support/core_ext/time/zones'
+
 require 'active_support/core_ext/util'
-ActiveSupport.core_ext Time, %w(behavior calculations conversions zones)
+ActiveSupport.core_ext Time, %w(behavior conversions)
