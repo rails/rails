7985298948534bf13e01e054f8ff4c5248780dca
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-26 19:03:37 -0700

    Convert DateTime extension modules to class reopens

--- activesupport/lib/active_support/core_ext/date_time.rb
+++ activesupport/lib/active_support/core_ext/date_time.rb
@@ -1,11 +1,12 @@
-require 'active_support/core_ext/util'
 require 'date'
-require 'active_support/core_ext/time/behavior'
-require 'active_support/core_ext/time/zones'
 
+require 'active_support/core_ext/date_time/calculations'
+require 'active_support/core_ext/date_time/zones'
+
+require 'active_support/core_ext/time/behavior'
 class DateTime
   include ActiveSupport::CoreExtensions::Time::Behavior
-  include ActiveSupport::CoreExtensions::Time::Zones
 end
 
-ActiveSupport.core_ext DateTime, %w(calculations conversions)
+require 'active_support/core_ext/util'
+ActiveSupport.core_ext DateTime, %w(conversions)
---
+++ activesupport/lib/active_support/core_ext/date_time/zones.rb
@@ -0,0 +1,17 @@
+class DateTime
+  # Returns the simultaneous time in <tt>Time.zone</tt>.
+  #
+  #   Time.zone = 'Hawaii'             # => 'Hawaii'
+  #   DateTime.new(2000).in_time_zone  # => Fri, 31 Dec 1999 14:00:00 HST -10:00
+  #
+  # This method is similar to Time#localtime, except that it uses <tt>Time.zone</tt> as the local zone
+  # instead of the operating system's time zone.
+  #
+  # You can also pass in a TimeZone instance or string that identifies a TimeZone as an argument, 
+  # and the conversion will be based on that zone instead of <tt>Time.zone</tt>.
+  #
+  #   DateTime.new(2000).in_time_zone('Alaska')  # => Fri, 31 Dec 1999 15:00:00 AKST -09:00
+  def in_time_zone(zone = ::Time.zone)
+    ActiveSupport::TimeWithZone.new(utc? ? self : getutc, ::Time.__send__(:get_zone, zone))
+  end
+end
