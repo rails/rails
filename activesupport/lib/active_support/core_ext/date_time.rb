dad8dabd31fb4808769272a4ba3380c1a9a30548
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-28 23:46:23 -0700

    Convert DateTime extensions to class reopens

--- activesupport/lib/active_support/core_ext/date_time.rb
+++ activesupport/lib/active_support/core_ext/date_time.rb
@@ -1,12 +1,6 @@
 require 'date'
 
+require 'active_support/core_ext/date_time/acts_like'
 require 'active_support/core_ext/date_time/calculations'
+require 'active_support/core_ext/date_time/conversions'
 require 'active_support/core_ext/date_time/zones'
-
-require 'active_support/core_ext/time/behavior'
-class DateTime
-  include ActiveSupport::CoreExtensions::Time::Behavior
-end
-
-require 'active_support/core_ext/util'
-ActiveSupport.core_ext DateTime, %w(conversions)
