9130c3147a8d23097d384208e31161928f16156b
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-28 23:46:54 -0700

    Convert Hash conversions extension module to class reopen

--- activesupport/lib/active_support/core_ext/hash.rb
+++ activesupport/lib/active_support/core_ext/hash.rb
@@ -1,3 +1,4 @@
+require 'active_support/core_ext/hash/conversions'
 require 'active_support/core_ext/hash/deep_merge'
 require 'active_support/core_ext/hash/diff'
 require 'active_support/core_ext/hash/except'
@@ -5,6 +6,3 @@ require 'active_support/core_ext/hash/indifferent_access'
 require 'active_support/core_ext/hash/keys'
 require 'active_support/core_ext/hash/reverse_merge'
 require 'active_support/core_ext/hash/slice'
-
-require 'active_support/core_ext/util'
-ActiveSupport.core_ext Hash, %w(conversions)
