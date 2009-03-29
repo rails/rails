726984208098d78390e56eb3cc72257fbbe309e8
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-28 23:53:04 -0700

    Explicit dependency on OrderedHash

--- activesupport/lib/active_support/ordered_options.rb
+++ activesupport/lib/active_support/ordered_options.rb
@@ -1,5 +1,7 @@
+require 'active_support/ordered_hash'
+
 module ActiveSupport #:nodoc:
-  class OrderedOptions < OrderedHash #:nodoc:
+  class OrderedOptions < OrderedHash
     def []=(key, value)
       super(key.to_sym, value)
     end
