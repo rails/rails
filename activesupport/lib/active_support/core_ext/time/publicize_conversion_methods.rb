fe34d10f0c6a22c6a87a6fef5f3ebce6c7af70c9
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-29 00:24:18 -0700

    JRuby doesn't have UnboundMethod#name

--- activesupport/lib/active_support/core_ext/time/publicize_conversion_methods.rb
+++ activesupport/lib/active_support/core_ext/time/publicize_conversion_methods.rb
@@ -3,7 +3,7 @@ require 'date'
 class Time
   # Ruby 1.8-cvs and early 1.9 series define private Time#to_date
   %w(to_date to_datetime).each do |method|
-    if (m = instance_method(method) rescue nil) && private_instance_methods.include?(m.name)
+    if private_instance_methods.include?(method) || private_instance_methods.include?(method.to_sym)
       public method
     end
   end
