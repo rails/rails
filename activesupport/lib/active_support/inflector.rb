4d464552443b8b3e906f7e231602ac0f362758ed
Jeremy Kemper <jeremy@bitsweat.net>
2009-03-28 23:50:50 -0700

    Explicit dependency on multibyte String extension

--- activesupport/lib/active_support/inflector.rb
+++ activesupport/lib/active_support/inflector.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 require 'iconv'
 
+require 'active_support/core_ext/string/multibyte'
+
 module ActiveSupport
   # The Inflector transforms words from singular to plural, class names to table names, modularized class names to ones without,
   # and class names to foreign keys. The default inflections for pluralization, singularization, and uncountable words are kept
