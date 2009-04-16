fa750e08a8d8c9f07afd88e616284549598926e2
David Heinemeier Hansson <david@loudthinking.com>
2009-04-16 17:25:55 -0500

    Added :touch option to belongs_to associations that will touch the parent record when the current record is saved or destroyed [DHH]

--- activerecord/test/cases/timestamp_test.rb
+++ activerecord/test/cases/timestamp_test.rb
@@ -1,8 +1,10 @@
 require 'cases/helper'
 require 'models/developer'
+require 'models/owner'
+require 'models/pet'
 
 class TimestampTest < ActiveRecord::TestCase
-  fixtures :developers
+  fixtures :developers, :owners, :pets
 
   def setup
     @developer = Developer.first
@@ -26,5 +28,48 @@ class TimestampTest < ActiveRecord::TestCase
     @developer.touch
     
     assert @previously_updated_at != @developer.updated_at
+  end
+  
+  def test_touching_a_different_attribute
+    previously_created_at = @developer.created_at
+    @developer.touch(:created_at)
+
+    assert previously_created_at != @developer.created_at
+  end
+  
+  def test_saving_a_record_with_a_belongs_to_that_specifies_touching_the_parent_should_update_the_parent_updated_at
+    pet   = Pet.first
+    owner = pet.owner
+    previously_owner_updated_at = owner.updated_at
+    
+    pet.name = "Fluffy the Third"
+    pet.save
+    
+    assert previously_owner_updated_at != pet.owner.updated_at
+  end
+
+  def test_destroying_a_record_with_a_belongs_to_that_specifies_touching_the_parent_should_update_the_parent_updated_at
+    pet   = Pet.first
+    owner = pet.owner
+    previously_owner_updated_at = owner.updated_at
+    
+    pet.destroy
+    
+    assert previously_owner_updated_at != pet.owner.updated_at
+  end
+  
+  def test_saving_a_record_with_a_belongs_to_that_specifies_touching_a_specific_attribute_the_parent_should_update_that_attribute
+    Pet.belongs_to :owner, :touch => :happy_at
+
+    pet   = Pet.first
+    owner = pet.owner
+    previously_owner_happy_at = owner.happy_at
+    
+    pet.name = "Fluffy the Third"
+    pet.save
+    
+    assert previously_owner_happy_at != pet.owner.happy_at
+  ensure
+    Pet.belongs_to :owner, :touch => true
   end
 end
