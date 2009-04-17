60896ca6f4c89260cb9770487f80dec829674b89
Jeremy Kemper <jeremy@bitsweat.net>
2009-04-17 13:44:59 -0500

    Clearer String#first and #last edge cases. Fix that foo.first(0) ==  instead of foo.

--- activesupport/test/core_ext/string_ext_test.rb
+++ activesupport/test/core_ext/string_ext_test.rb
@@ -132,10 +132,12 @@ class StringInflectionsTest < Test::Unit::TestCase
 
     assert_equal "h", s.first
     assert_equal "he", s.first(2)
+    assert_equal "", s.first(0)
 
     assert_equal "o", s.last
     assert_equal "llo", s.last(3)
     assert_equal "hello", s.last(10)
+    assert_equal "", s.last(0)
 
     assert_equal 'x', 'x'.first
     assert_equal 'x', 'x'.first(4)
