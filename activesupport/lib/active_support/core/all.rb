3202671dc9360c4a89d80355824b239b52b2f611
Jeremy Kemper <jeremy@bitsweat.net>
2009-04-17 21:20:57 -0700

    Fix core/all require of adjacent core features

--- activesupport/lib/active_support/core/all.rb
+++ activesupport/lib/active_support/core/all.rb
@@ -1,4 +1,4 @@
 require 'active_support/core'
-Dir["#{File.dirname(__FILE__)}/core/*.rb"].sort.each do |path|
+Dir["#{File.dirname(__FILE__)}/*.rb"].sort.each do |path|
   require "active_support/core/#{File.basename(path, '.rb')}"
 end
