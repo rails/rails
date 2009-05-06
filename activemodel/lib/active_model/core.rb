2f42433f0a9d777e2a8f5fa05370ba464823933c
Jeremy Kemper <jeremy@bitsweat.net>
2009-05-05 21:40:53 -0700

    Cherry-pick the inflector only

--- activemodel/lib/active_model/core.rb
+++ activemodel/lib/active_model/core.rb
@@ -1,12 +1,3 @@
-begin
-  require 'active_support'
-rescue LoadError
-  activesupport_path = "#{File.dirname(__FILE__)}/../../../activesupport/lib"
-  if File.directory?(activesupport_path)
-    $:.unshift activesupport_path
-    require 'active_support'
-  end
-end
-
-# So far, we only need the string inflections and not the rest of ActiveSupport.
+activesupport_path = "#{File.dirname(__FILE__)}/../../../activesupport/lib"
+$:.unshift(activesupport_path) if File.directory?(activesupport_path)
 require 'active_support/inflector'
