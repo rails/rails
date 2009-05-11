ddbeb15a5e7e0c3c5f316ccf65b557bc5311a6c4
Jeremy Kemper <jeremy@bitsweat.net>
2009-05-11 12:01:08 -0700

    Revert "Fixed bug with polymorphic has_one :as pointing to an STI record"
    [#2594 state:open]
    
    This reverts commit 99c103be1165da9c8299bc0977188ecf167e06a5.

--- activerecord/lib/active_record/associations/has_one_association.rb
+++ activerecord/lib/active_record/associations/has_one_association.rb
@@ -90,7 +90,7 @@ module ActiveRecord
             when @reflection.options[:as]
               @finder_sql = 
                 "#{@reflection.quoted_table_name}.#{@reflection.options[:as]}_id = #{owner_quoted_id} AND " +
-                "#{@reflection.quoted_table_name}.#{@reflection.options[:as]}_type = #{@owner.class.quote_value(@owner.class.name.to_s)}"
+                "#{@reflection.quoted_table_name}.#{@reflection.options[:as]}_type = #{@owner.class.quote_value(@owner.class.base_class.name.to_s)}"
             else
               @finder_sql = "#{@reflection.quoted_table_name}.#{@reflection.primary_key_name} = #{owner_quoted_id}"
           end
