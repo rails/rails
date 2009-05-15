01064e25c9a31ce0f69a8cab0adc5e6743422581
Mike Gunderloy <MikeG1@larkfarm.com>
2009-05-15 10:07:51 -0500

    Fix comment in seeds.rb to make more sense.

--- railties/configs/seeds.rb
+++ railties/configs/seeds.rb
@@ -4,4 +4,4 @@
 # Examples:
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+#   Mayor.create(:name => 'Daley', :city => cities.first)
