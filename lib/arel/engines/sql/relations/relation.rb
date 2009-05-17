982f100d49095614df320d449f66f69aa827a763
Bryan Helmkamp <bryan@brynary.com>
2009-05-17 18:40:24 -0400

    Refactor #select_sql. Extract methods to generate clauses

--- lib/arel/engines/sql/relations/relation.rb
+++ lib/arel/engines/sql/relations/relation.rb
@@ -6,16 +6,17 @@ module Arel
 
     def select_sql
       [
-        "SELECT     #{attributes.collect { |a| a.to_sql(Sql::SelectClause.new(self)) }.join(', ')}",
+        "SELECT     #{select_clauses.join(', ')}",
         "FROM       #{table_sql(Sql::TableReference.new(self))}",
-        (joins(self)                                                                                    unless joins(self).blank? ),
-        ("WHERE     #{wheres   .collect { |w| w.to_sql(Sql::WhereClause.new(self)) }.join("\n\tAND ")}" unless wheres.blank?      ),
-        ("GROUP BY  #{groupings.collect { |g| g.to_sql(Sql::GroupClause.new(self)) }.join(', ')}"       unless groupings.blank?   ),
-        ("ORDER BY  #{orders   .collect { |o| o.to_sql(Sql::OrderClause.new(self)) }.join(', ')}"       unless orders.blank?      ),
-        ("LIMIT     #{taken}"                                                                           unless taken.blank?       ),
-        ("OFFSET    #{skipped}"                                                                         unless skipped.blank?     )
+        (joins(self)                                   unless joins(self).blank? ),
+        ("WHERE     #{where_clauses.join("\n\tAND ")}" unless wheres.blank?      ),
+        ("GROUP BY  #{group_clauses.join(', ')}"       unless groupings.blank?   ),
+        ("ORDER BY  #{order_clauses.join(', ')}"       unless orders.blank?      ),
+        ("LIMIT     #{taken}"                          unless taken.blank?       ),
+        ("OFFSET    #{skipped}"                        unless skipped.blank?     )
       ].compact.join("\n")
     end
+
 
     def inclusion_predicate_sql
       "IN"
@@ -24,5 +25,24 @@ module Arel
     def christener
       @christener ||= Sql::Christener.new
     end
+
+  protected
+
+    def select_clauses
+      attributes.collect { |a| a.to_sql(Sql::SelectClause.new(self)) }
+    end
+
+    def where_clauses
+      wheres.collect { |w| w.to_sql(Sql::WhereClause.new(self)) }
+    end
+
+    def group_clauses
+      groupings.collect { |g| g.to_sql(Sql::GroupClause.new(self)) }
+    end
+
+    def order_clauses
+      orders.collect { |o| o.to_sql(Sql::OrderClause.new(self)) }
+    end
+
   end
 end
