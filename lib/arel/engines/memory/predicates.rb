0352d28a56de36946e691a0df390cfdfa7b1de7c
Bryan Helmkamp <bryan@brynary.com>
2009-05-17 16:31:16 -0400

    Moving a few stray SQL references in the memory engine code

--- lib/arel/engines/memory/predicates.rb
+++ lib/arel/engines/memory/predicates.rb
@@ -1,23 +1,8 @@
 module Arel
-
   class Binary < Predicate
     def eval(row)
       operand1.eval(row).send(operator, operand2.eval(row))
     end
-  end
-
-  class CompoundPredicate < Binary
-    def to_sql(formatter = nil)
-      "(#{operand1.to_sql(formatter)} #{predicate_sql} #{operand2.to_sql(formatter)})"
-    end
-  end
-
-  class Or < CompoundPredicate
-    def predicate_sql; "OR" end
-  end
-
-  class And < CompoundPredicate
-    def predicate_sql; "AND" end
   end
 
   class Equality < Binary
--- lib/arel/engines/sql/predicates.rb
+++ lib/arel/engines/sql/predicates.rb
@@ -5,6 +5,20 @@ module Arel
     end
   end
 
+  class CompoundPredicate < Binary
+    def to_sql(formatter = nil)
+      "(#{operand1.to_sql(formatter)} #{predicate_sql} #{operand2.to_sql(formatter)})"
+    end
+  end
+
+  class Or < CompoundPredicate
+    def predicate_sql; "OR" end
+  end
+
+  class And < CompoundPredicate
+    def predicate_sql; "AND" end
+  end
+
   class Equality < Binary
     def predicate_sql
       operand2.equality_predicate_sql
