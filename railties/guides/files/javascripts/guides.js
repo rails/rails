bf1b1e976b8315f67f82e324e6d3bc3d0b353fd5
Joseph Pecoraro <joepeck02@gmail.com>
2009-05-29 18:35:55 -0400

    Semi-fix for Firefox code Copy and Paste The underlieing problem is that Firefox ignores the css 'white-space:pre' rule when actually copying and pasting. This Javascript turns newlines into <br /> tags inside the code containers.
    This will run in Safari, Opera, Chrome, Webkit, etc. gracefully.
    This will not run in IE due to IE not having the methods needed.
    
    A permanent solution would be at the Markdown level in such a way that
    the highlighter still runs on the output with brake tags.

--- railties/guides/files/javascripts/guides.js
+++ railties/guides/files/javascripts/guides.js
@@ -1,8 +1,17 @@
 function guideMenu(){
-
   if (document.getElementById('guides').style.display == "none") {
     document.getElementById('guides').style.display = "block";
   } else {
     document.getElementById('guides').style.display = "none";
   }
+}
+
+// Fix Copy+Paste of Code blocks in Firefox 3
+if ( window.addEventListener && document.getElementsByClassName ) {
+  window.addEventListener('load', function() {
+    var list = document.getElementsByClassName('code_container');
+    for (var i=0, len=list.length; i<len; i++) {
+      list[i].innerHTML = list[i].innerHTML.replace(/\n/g, '<br />');
+    }
+  }, false);
 }
