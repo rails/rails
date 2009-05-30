dd888c9603972146eb57c90235f856ea91037d0b
Joseph Pecoraro <joepeck02@gmail.com>
2009-05-30 00:48:22 -0400

    Removed the Javascript. It conflicts with the highlighting code.

--- railties/guides/files/javascripts/guides.js
+++ railties/guides/files/javascripts/guides.js
@@ -5,13 +5,3 @@ function guideMenu(){
     document.getElementById('guides').style.display = "none";
   }
 }
-
-// Fix Copy+Paste of Code blocks in Firefox 3
-if ( window.addEventListener && document.getElementsByClassName ) {
-  window.addEventListener('load', function() {
-    var list = document.getElementsByClassName('code_container');
-    for (var i=0, len=list.length; i<len; i++) {
-      list[i].innerHTML = list[i].innerHTML.replace(/\n/g, '<br />');
-    }
-  }, false);
-}
