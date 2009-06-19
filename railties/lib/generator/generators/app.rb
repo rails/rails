6658711775d718f5e1b8b8a86c34c5c0c71fe186
José Valim <jose.valim@gmail.com>
2009-06-19 12:45:08 +0200

    Template wwas not found? Raise an error.

--- railties/lib/generator/generators/app.rb
+++ railties/lib/generator/generators/app.rb
@@ -165,6 +165,8 @@ module Rails::Generators
 
     def apply_rails_template
       apply options[:template] if options[:template]
+    rescue LoadError, Errno::ENOENT => e
+      raise "The template [#{template}] could not be loaded. Error: #{e}"
     end
 
     def freeze?
