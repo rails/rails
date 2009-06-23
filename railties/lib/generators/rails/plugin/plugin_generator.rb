ccd26575d12301816b11bdb22847e10173bd3ea2
José Valim <jose.valim@gmail.com>
2009-06-23 21:07:12 +0200

    Clean up plugin generator.

--- railties/lib/generators/rails/plugin/plugin_generator.rb
+++ railties/lib/generators/rails/plugin/plugin_generator.rb
@@ -1,13 +1,19 @@
 module Rails
   module Generators
     class PluginGenerator < NamedBase
+      class_option :with_tasks, :type => :boolean, :aliases => "-r", :default => false,
+                                :desc => "When supplied creates tasks base files."
+
+      class_option :with_generator, :type => :boolean, :aliases => "-g", :default => false,
+                                    :desc => "When supplied creates generator base files."
+
+      # TODO Check class collision
+
       def create_root
         self.root = File.expand_path("vendor/plugins/#{file_name}", root)
         empty_directory '.'
         FileUtils.cd(root)
       end
-
-      # TODO Check class collision
 
       def create_root_files
         %w(README MIT-LICENSE Rakefile init.rb install.rb uninstall.rb).each do |file|
@@ -20,12 +26,6 @@ module Rails
       end
 
       add_test_framework_option!
-
-      class_option :with_tasks, :type => :boolean, :aliases => "-r", :default => false,
-                                :desc => "When supplied creates tasks base files."
-
-      class_option :with_generator, :type => :boolean, :aliases => "-g", :default => false,
-                                    :desc => "When supplied creates generator base files."
 
       def create_tasks_files
         return unless options[:with_tasks]
