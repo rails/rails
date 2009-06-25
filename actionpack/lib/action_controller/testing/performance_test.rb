97159fad6a69cc0bc6fe504e2063bb98fcf6e42d
Pratik Naik <pratiknaik@gmail.com>
2009-06-25 17:03:04 +0100

    Missed file from the previous commit 92b229e1251d7d3578c3f73e8185972354e76436

---
+++ actionpack/lib/action_controller/testing/performance_test.rb
@@ -0,0 +1,15 @@
+require 'active_support/testing/performance'
+require 'active_support/testing/default'
+
+module ActionController
+  # An integration test that runs a code profiler on your test methods.
+  # Profiling output for combinations of each test method, measurement, and
+  # output format are written to your tmp/performance directory.
+  #
+  # By default, process_time is measured and both flat and graph_html output
+  # formats are written, so you'll have two output files per test method.
+  class PerformanceTest < ActionController::IntegrationTest
+    include ActiveSupport::Testing::Performance
+    include ActiveSupport::Testing::Default
+  end
+end
