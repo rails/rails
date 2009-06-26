d40bd851190dd734a09965979c8ade285e51d52c
José Valim <jose.valim@gmail.com>
2009-06-26 10:49:43 +0200

    Small fixes.

--- railties/lib/generators/rails/integration_test/integration_test_generator.rb
+++ railties/lib/generators/rails/integration_test/integration_test_generator.rb
@@ -1,7 +1,7 @@
 module Rails
   module Generators
     class IntegrationTestGenerator < NamedBase
-      check_class_collisions :suffix => "Test"
+      check_class_collision :suffix => "Test"
 
       def create_test_files
         template 'integration_test.rb', File.join('test/integration', class_path, "#{file_name}_test.rb")
