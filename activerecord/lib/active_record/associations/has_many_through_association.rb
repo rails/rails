45e6f19925f23c3db257c15371d8f512cca843cd
Pratik Naik <pratiknaik@gmail.com>
2009-06-30 23:55:09 +0100

    Revert "Revert "Generate proper :counter_sql from :finder_sql when there is a newline character immediately following 'SELECT' [#2118 state:resolved]""
    This reverts commit 80f1f863cd0f9cba89079511282de5710a2e1832.
    
    The feature doesn't work on Postgres, so don't test it on Postgres.
    Also, Postgres compatibility is irrelevant to the ticket/patch in question.

--- activerecord/lib/active_record/associations/has_many_through_association.rb
+++ activerecord/lib/active_record/associations/has_many_through_association.rb
@@ -90,15 +90,7 @@ module ActiveRecord
               @finder_sql = construct_conditions
           end
 
-          if @reflection.options[:counter_sql]
-            @counter_sql = interpolate_sql(@reflection.options[:counter_sql])
-          elsif @reflection.options[:finder_sql]
-            # replace the SELECT clause with COUNT(*), preserving any hints within /* ... */
-            @reflection.options[:counter_sql] = @reflection.options[:finder_sql].sub(/SELECT (\/\*.*?\*\/ )?(.*)\bFROM\b/im) { "SELECT #{$1}COUNT(*) FROM" }
-            @counter_sql = interpolate_sql(@reflection.options[:counter_sql])
-          else
-            @counter_sql = @finder_sql
-          end
+          construct_counter_sql
         end
 
         def has_cached_counter?
