b9f4ea7198aadaeaaf6761316f0d3c96e73e4753
José Valim <jose.valim@gmail.com>
2009-07-13 18:29:40 +0200

    Add hooks for integration and performance tests.

--- railties/lib/generators/rails/integration_test/integration_test_generator.rb
+++ railties/lib/generators/rails/integration_test/integration_test_generator.rb
@@ -1,11 +1,7 @@
 module Rails
   module Generators
     class IntegrationTestGenerator < NamedBase
-      check_class_collision :suffix => "Test"
-
-      def create_test_files
-        template 'integration_test.rb', File.join('test/integration', class_path, "#{file_name}_test.rb")
-      end
+      hook_for :integration_tool, :as => :integration
     end
   end
 end
--- railties/lib/generators/rails/performance_test/performance_test_generator.rb
+++ railties/lib/generators/rails/performance_test/performance_test_generator.rb
@@ -1,11 +1,7 @@
 module Rails
   module Generators
     class PerformanceTestGenerator < NamedBase
-      check_class_collision :suffix => "Test"
-
-      def create_test_files
-        template 'performance_test.rb', File.join('test/performance', class_path, "#{file_name}_test.rb")
-      end
+      hook_for :performance_tool, :as => :performance
     end
   end
 end
