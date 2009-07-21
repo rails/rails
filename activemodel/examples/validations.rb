108545a9d69215aa0205f7e6b52eb1573443d404
Joshua Peek <josh@joshpeek.com>
2009-07-21 00:56:27 -0500

    Clean up validation example

--- activemodel/examples/validations.rb
+++ activemodel/examples/validations.rb
@@ -1,31 +1,24 @@
-$:.push "activesupport/lib"
-$:.push "activemodel/lib"
-
-require "active_model/validations"
-require "active_model/deprecated_error_methods"
-require "active_model/errors"
-require "active_model/naming"
+require 'activemodel'
 
 class Person
+  include ActiveModel::Conversion
   include ActiveModel::Validations
-  extend ActiveModel::Naming
-  
+
   validates_presence_of :name
-  
+
   attr_accessor :name
+
   def initialize(attributes = {})
     @name = attributes[:name]
   end
-  
+
   def persist
     @persisted = true
   end
-  
+
   def new_record?
     @persisted
   end
-  
-  def to_model() self end
 end
 
 person1 = Person.new
