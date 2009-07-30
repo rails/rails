83537bf09010a56510927939f3ea31d30f612a25
Pratik Naik <pratiknaik@gmail.com>
2009-07-30 17:49:05 +0100

    Generator config should be commented by default

--- railties/lib/generators/rails/app/templates/config/environment.rb
+++ railties/lib/generators/rails/app/templates/config/environment.rb
@@ -45,9 +45,9 @@ Rails::Initializer.run do |config|
 
   # Configure generators values. Many other options are available, be sure to
   # check the documentation.
-  config.generators do |g|
-    g.orm             :active_record
-    g.template_engine :erb
-    g.test_framework  :test_unit, :fixture => true
-  end
+  # config.generators do |g|
+  #   g.orm             :active_record
+  #   g.template_engine :erb
+  #   g.test_framework  :test_unit, :fixture => true
+  # end
 end
