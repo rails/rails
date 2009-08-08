407fbb50907b34f062c7670b570c44abeb21e2f6
Matt Duncan <mrduncan@gmail.com>
2009-08-07 22:55:19 -0400

    Adding :from scoping to ActiveRecord calculations
    Signed-off-by: Michael Koziarski <michael@koziarski.com>

--- activerecord/test/cases/calculations_test.rb
+++ activerecord/test/cases/calculations_test.rb
@@ -5,6 +5,8 @@ require 'models/edge'
 require 'models/owner'
 require 'models/pet'
 require 'models/toy'
+require 'models/club'
+require 'models/organization'
 
 Company.has_many :accounts
 
@@ -224,6 +226,10 @@ class CalculationsTest < ActiveRecord::TestCase
 
   def test_should_sum_scoped_field
     assert_equal 15, companies(:rails_core).companies.sum(:id)
+  end
+
+  def test_should_sum_scoped_field_with_from
+    assert_equal Club.count, Organization.clubs.count
   end
 
   def test_should_sum_scoped_field_with_conditions
--- activerecord/test/models/organization.rb
+++ activerecord/test/models/organization.rb
@@ -1,4 +1,6 @@
 class Organization < ActiveRecord::Base
   has_many :member_details
   has_many :members, :through => :member_details
+
+  named_scope :clubs, { :from => 'clubs' }
 end
