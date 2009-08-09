18b4ac6992bdd51b9c279c064b60692bb80235ba
Arthur Zapparoli <arthurzap@gmail.com>
2009-08-09 14:42:38 -0300

    Model#human_attribute_name now accept symbols [#3025 status:resolved]
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- activerecord/lib/active_record/base.rb
+++ activerecord/lib/active_record/base.rb
@@ -1364,7 +1364,7 @@ module ActiveRecord #:nodoc:
         end
         defaults << options[:default] if options[:default]
         defaults.flatten!
-        defaults << attribute_key_name.humanize
+        defaults << attribute_key_name.to_s.humanize
         options[:count] ||= 1
         I18n.translate(defaults.shift, options.merge(:default => defaults, :scope => [:activerecord, :attributes]))
       end
