97a5c7a5163b4bf7398939a4ccaf1d04c530ec52
Pratik Naik <pratiknaik@gmail.com>
2009-08-09 18:13:03 +0100

    Make enumerable test run stand alone

--- activesupport/test/core_ext/enumerable_test.rb
+++ activesupport/test/core_ext/enumerable_test.rb
@@ -1,5 +1,6 @@
 require 'abstract_unit'
 require 'active_support/core_ext/array'
+require 'active_support/core_ext/symbol'
 require 'active_support/core_ext/enumerable'
 
 Payment = Struct.new(:price)
