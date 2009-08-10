f97dae5ebe2f19273d3f92e5ea9baba788c8e89f
Joshua Peek <josh@joshpeek.com>
2009-08-10 13:51:48 -0500

    Extract common dirty tracking methods in AMo

--- activerecord/lib/active_record/attribute_methods/dirty.rb
+++ activerecord/lib/active_record/attribute_methods/dirty.rb
@@ -1,90 +1,19 @@
+require 'active_support/core_ext/object/tap'
+
 module ActiveRecord
   module AttributeMethods
-    # Track unsaved attribute changes.
-    #
-    # A newly instantiated object is unchanged:
-    #   person = Person.find_by_name('Uncle Bob')
-    #   person.changed?       # => false
-    #
-    # Change the name:
-    #   person.name = 'Bob'
-    #   person.changed?       # => true
-    #   person.name_changed?  # => true
-    #   person.name_was       # => 'Uncle Bob'
-    #   person.name_change    # => ['Uncle Bob', 'Bob']
-    #   person.name = 'Bill'
-    #   person.name_change    # => ['Uncle Bob', 'Bill']
-    #
-    # Save the changes:
-    #   person.save
-    #   person.changed?       # => false
-    #   person.name_changed?  # => false
-    #
-    # Assigning the same value leaves the attribute unchanged:
-    #   person.name = 'Bill'
-    #   person.name_changed?  # => false
-    #   person.name_change    # => nil
-    #
-    # Which attributes have changed?
-    #   person.name = 'Bob'
-    #   person.changed        # => ['name']
-    #   person.changes        # => { 'name' => ['Bill', 'Bob'] }
-    #
-    # Resetting an attribute returns it to its original state:
-    #   person.reset_name!    # => 'Bill'
-    #   person.changed?       # => false
-    #   person.name_changed?  # => false
-    #   person.name           # => 'Bill'
-    #
-    # Before modifying an attribute in-place:
-    #   person.name_will_change!
-    #   person.name << 'y'
-    #   person.name_change    # => ['Bill', 'Billy']
     module Dirty
       extend ActiveSupport::Concern
-
-      DIRTY_AFFIXES = [
-        { :suffix => '_changed?' },
-        { :suffix => '_change' },
-        { :suffix => '_will_change!' },
-        { :suffix => '_was' },
-        { :prefix => 'reset_', :suffix => '!' }
-      ]
+      include ActiveModel::Dirty
 
       included do
-        attribute_method_affix *DIRTY_AFFIXES
-
-        alias_method_chain :save,            :dirty
-        alias_method_chain :save!,           :dirty
-        alias_method_chain :update,          :dirty
-        alias_method_chain :reload,          :dirty
+        alias_method_chain :save,   :dirty
+        alias_method_chain :save!,  :dirty
+        alias_method_chain :update, :dirty
+        alias_method_chain :reload, :dirty
 
         superclass_delegating_accessor :partial_updates
         self.partial_updates = true
-      end
-
-      # Do any attributes have unsaved changes?
-      #   person.changed? # => false
-      #   person.name = 'bob'
-      #   person.changed? # => true
-      def changed?
-        !changed_attributes.empty?
-      end
-
-      # List of attributes with unsaved changes.
-      #   person.changed # => []
-      #   person.name = 'bob'
-      #   person.changed # => ['name']
-      def changed
-        changed_attributes.keys
-      end
-
-      # Map of changed attrs => [original value, new value].
-      #   person.changes # => {}
-      #   person.name = 'bob'
-      #   person.changes # => { 'name' => ['bill', 'bob'] }
-      def changes
-        changed.inject({}) { |h, attr| h[attr] = attribute_change(attr); h }
       end
 
       # Attempts to +save+ the record and clears changed attributes if successful.
@@ -97,49 +26,15 @@ module ActiveRecord
 
       # Attempts to <tt>save!</tt> the record and clears changed attributes if successful.
       def save_with_dirty!(*args) #:nodoc:
-        status = save_without_dirty!(*args)
-        changed_attributes.clear
-        status
+        save_without_dirty!(*args).tap { changed_attributes.clear }
       end
 
       # <tt>reload</tt> the record and clears changed attributes.
       def reload_with_dirty(*args) #:nodoc:
-        record = reload_without_dirty(*args)
-        changed_attributes.clear
-        record
+        reload_without_dirty(*args).tap { changed_attributes.clear }
       end
 
       private
-        # Map of change <tt>attr => original value</tt>.
-        def changed_attributes
-          @changed_attributes ||= {}
-        end
-
-        # Handle <tt>*_changed?</tt> for +method_missing+.
-        def attribute_changed?(attr)
-          changed_attributes.include?(attr)
-        end
-
-        # Handle <tt>*_change</tt> for +method_missing+.
-        def attribute_change(attr)
-          [changed_attributes[attr], __send__(attr)] if attribute_changed?(attr)
-        end
-
-        # Handle <tt>*_was</tt> for +method_missing+.
-        def attribute_was(attr)
-          attribute_changed?(attr) ? changed_attributes[attr] : __send__(attr)
-        end
-
-        # Handle <tt>reset_*!</tt> for +method_missing+.
-        def reset_attribute!(attr)
-          self[attr] = changed_attributes[attr] if attribute_changed?(attr)
-        end
-
-        # Handle <tt>*_will_change!</tt> for +method_missing+.
-        def attribute_will_change!(attr)
-          changed_attributes[attr] = clone_attribute_value(:read_attribute, attr)
-        end
-
         # Wrap write_attribute to remember original attribute value.
         def write_attribute(attr, value)
           attr = attr.to_s
