6356a48faa07908e7eb1759b0a849adc9e0a1d97
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2009-08-10 17:32:38 +0300

    Reduced size of table name prefix and suffix in migrations test as in Oracle identifier name cannot be larger than 30 characters

--- activerecord/test/cases/migration_test.rb
+++ activerecord/test/cases/migration_test.rb
@@ -30,13 +30,14 @@ if ActiveRecord::Base.connection.supports_migrations?
       conn = ActiveRecord::Base.connection
 
       conn.drop_table(ActiveRecord::Migrator.schema_migrations_table_name) if conn.table_exists?(ActiveRecord::Migrator.schema_migrations_table_name)
-      ActiveRecord::Base.table_name_prefix = 'foo_'
-      ActiveRecord::Base.table_name_suffix = '_bar'
+      # Use shorter prefix and suffix as in Oracle database identifier cannot be larger than 30 characters
+      ActiveRecord::Base.table_name_prefix = 'p_'
+      ActiveRecord::Base.table_name_suffix = '_s'
       conn.drop_table(ActiveRecord::Migrator.schema_migrations_table_name) if conn.table_exists?(ActiveRecord::Migrator.schema_migrations_table_name)
 
       conn.initialize_schema_migrations_table
 
-      assert_equal "foo_unique_schema_migrations_bar", conn.indexes(ActiveRecord::Migrator.schema_migrations_table_name)[0][:name]
+      assert_equal "p_unique_schema_migrations_s", conn.indexes(ActiveRecord::Migrator.schema_migrations_table_name)[0][:name]
     ensure
       ActiveRecord::Base.table_name_prefix = ""
       ActiveRecord::Base.table_name_suffix = ""
