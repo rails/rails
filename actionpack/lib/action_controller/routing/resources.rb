df6617bc8ac1677ab2b2cf7ed2859cdcc393ccb5
Jeremy Kemper <jeremy@bitsweat.net>
2009-08-15 15:56:52 -0700

    Normalize route generation order: associations, yield block, then own routes.

--- actionpack/lib/action_controller/routing/resources.rb
+++ actionpack/lib/action_controller/routing/resources.rb
@@ -531,14 +531,14 @@ module ActionController
         with_options :controller => resource.controller do |map|
           map_associations(resource, options)
 
+          if block_given?
+            with_options(options.slice(*INHERITABLE_OPTIONS).merge(:path_prefix => resource.nesting_path_prefix, :name_prefix => resource.nesting_name_prefix), &block)
+          end
+
           map_collection_actions(map, resource)
           map_default_collection_actions(map, resource)
           map_new_actions(map, resource)
           map_member_actions(map, resource)
-
-          if block_given?
-            with_options(options.slice(*INHERITABLE_OPTIONS).merge(:path_prefix => resource.nesting_path_prefix, :name_prefix => resource.nesting_name_prefix), &block)
-          end
         end
       end
 
@@ -546,16 +546,16 @@ module ActionController
         resource = SingletonResource.new(entities, options)
 
         with_options :controller => resource.controller do |map|
-          map_collection_actions(map, resource)
-          map_new_actions(map, resource)
-          map_member_actions(map, resource)
-          map_default_singleton_actions(map, resource)
-
           map_associations(resource, options)
 
           if block_given?
             with_options(options.slice(*INHERITABLE_OPTIONS).merge(:path_prefix => resource.nesting_path_prefix, :name_prefix => resource.nesting_name_prefix), &block)
           end
+
+          map_collection_actions(map, resource)
+          map_new_actions(map, resource)
+          map_member_actions(map, resource)
+          map_default_singleton_actions(map, resource)
         end
       end
 
