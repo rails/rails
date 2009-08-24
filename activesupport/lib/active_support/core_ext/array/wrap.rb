016905f97fd6aa3b8f30298054e6e428cd92dcf0
Xavier Noria <fxn@hashref.com>
2009-08-25 00:44:46 +0200

    explains difference between Array.wrap and Array()

--- activesupport/lib/active_support/core_ext/array/wrap.rb
+++ activesupport/lib/active_support/core_ext/array/wrap.rb
@@ -1,6 +1,15 @@
 class Array
   # Wraps the object in an Array unless it's an Array.  Converts the
   # object to an Array using #to_ary if it implements that.
+  #
+  # It differs with Array() in that it does not call +to_a+ on
+  # the argument:
+  #
+  #   Array(:foo => :bar)      # => [[:foo, :bar]]
+  #   Array.wrap(:foo => :bar) # => [{:foo => :bar}]
+  #
+  #   Array("foo\nbar")        # => ["foo\n", "bar"], in Ruby 1.8
+  #   Array.wrap("foo\nbar")   # => ["foo\nbar"]
   def self.wrap(object)
     case object
     when nil
