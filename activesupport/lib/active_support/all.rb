d6a944f778f015982994ef6156b77ef22f993733
Jeremy Kemper <jeremy@bitsweat.net>
2009-08-23 17:16:31 -0700

    Add active_support/all for forward compatibility.

---
+++ activesupport/lib/active_support/all.rb
@@ -0,0 +1,8 @@
+# For forward compatibility with Rails 3.
+#
+# require 'active_support' loads a very bare minumum in Rails 3.
+# require 'active_support/all' loads the whole suite like Rails 2 did.
+#
+# To prepare for Rails 3, switch to require 'active_support/all' now.
+
+require 'active_support'
