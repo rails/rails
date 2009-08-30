d8e7ff12a82f4cde563d6d8777e644811929bf50
Chad Woolley <thewoolleyman@gmail.com>
2009-08-29 23:43:53 -0700

    CI config updates: do not send CI emails unless explicitly enabled, use 'gem update --system', and send emails from an address which can post to the core list
    [#3116 state:committed]
    
    Signed-off-by: Jeremy Kemper <jeremy@bitsweat.net>

--- ci/cruise_config.rb
+++ ci/cruise_config.rb
@@ -1,9 +1,9 @@
 Project.configure do |project|
   # Send email notifications about broken and fixed builds to core mailing list
-  if Socket.gethostname =~ /ci.rubyonrails.org/
+  if Socket.gethostname =~ /ci.rubyonrails.org/ && ENV['ENABLE_RAILS_CI_EMAILS'] == 'true'
     project.email_notifier.emails = ['rubyonrails-core@googlegroups.com']
   end
 
-  project.build_command = 'sudo update_rubygems && ruby ci/ci_build.rb'
-  project.email_notifier.from = 'thewoolleyman+railsci@gmail.com'
+  project.build_command = 'sudo gem update --system && ruby ci/ci_build.rb'
+  project.email_notifier.from = 'thewoolleyman@gmail.com'
 end
