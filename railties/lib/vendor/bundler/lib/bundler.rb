f416f9f0aef654fea94cae10027aca866d07c659
Carl Lerche <carllerche@mac.com>
2009-09-03 09:31:04 -0700

    Vendor the latest version of the bundler

--- railties/lib/vendor/bundler/lib/bundler.rb
+++ railties/lib/vendor/bundler/lib/bundler.rb
@@ -1,24 +1,34 @@
+require 'pathname'
 require 'logger'
 require 'set'
+require 'erb'
 # Required elements of rubygems
 require "rubygems/remote_fetcher"
 require "rubygems/installer"
 
 require "bundler/gem_bundle"
-require "bundler/installer"
+require "bundler/source"
 require "bundler/finder"
-require "bundler/gem_specification"
+require "bundler/gem_ext"
 require "bundler/resolver"
-require "bundler/manifest"
-require "bundler/dependency"
-require "bundler/runtime"
+require "bundler/environment"
+require "bundler/dsl"
 require "bundler/cli"
+require "bundler/repository"
+require "bundler/dependency"
 
 module Bundler
   VERSION = "0.5.0"
 
-  def self.run(options = {})
-    manifest = ManifestBuilder.load(options[:path], options[:manifest])
-    manifest.install
+  class << self
+    attr_writer :logger
+
+    def logger
+      @logger ||= begin
+        logger = Logger.new(STDOUT, Logger::INFO)
+        logger.formatter = proc {|_,_,_,msg| "#{msg}\n" }
+        logger
+      end
+    end
   end
 end
