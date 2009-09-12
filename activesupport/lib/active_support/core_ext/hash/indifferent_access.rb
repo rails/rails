a692e5b9709b7f9c88b38770e783ae4544abbd74
Xavier Noria <fxn@hashref.com>
2009-09-12 15:06:00 +0200

    documents Hash#with_indifferent_access

--- activesupport/lib/active_support/core_ext/hash/indifferent_access.rb
+++ activesupport/lib/active_support/core_ext/hash/indifferent_access.rb
@@ -1,6 +1,11 @@
 require 'active_support/hash_with_indifferent_access'
 
 class Hash
+
+  # Returns an +ActiveSupport::HashWithIndifferentAccess+ out of its receiver:
+  #
+  #   {:a => 1}.with_indifferent_access["a"] # => 1
+  #
   def with_indifferent_access
     hash = ActiveSupport::HashWithIndifferentAccess.new(self)
     hash.default = self.default
