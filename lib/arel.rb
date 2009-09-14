8852db7087a8f4f98e5fd26fa33bac14a5400979
Emilio Tagua <miloops@gmail.com>
2009-09-14 17:36:41 -0300

    Cherry pick AS extensions.

--- lib/arel.rb
+++ lib/arel.rb
@@ -1,3 +1,7 @@
+require 'active_support/inflector'
+require 'active_support/core_ext/module/delegation'
+require 'active_support/core_ext/class/attribute_accessors'
+
 require 'active_record'
 require 'active_record/connection_adapters/abstract/quoting'
 
