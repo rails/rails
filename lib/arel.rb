a727cef713c75875941f56fc1e3779216c93d539
Jeremy Kemper <jeremy@bitsweat.net>
2009-09-14 03:12:54 -0700

    Remove explicit rubygems require. Use Active Support provided by Active Record. Add self to load path after requiring Active Record.

--- lib/arel.rb
+++ lib/arel.rb
@@ -1,13 +1,7 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-
-require 'rubygems'
-require 'active_support'
-require 'active_support/dependencies'
-require 'active_support/core_ext/class/attribute_accessors'
-require 'active_support/core_ext/module/delegation'
 require 'active_record'
 require 'active_record/connection_adapters/abstract/quoting'
 
+$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'arel/algebra'
 require 'arel/engines'
 require 'arel/session'
