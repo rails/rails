52aeb8d2e72223f9b40b0193c151c252a3f4fb09
Joshua Peek <josh@joshpeek.com>
2009-09-15 16:33:15 -0500

    Beef up AD::Rescue to replace global exception handling lost in ApplicationController

--- actionpack/lib/action_dispatch/middleware/rescue.rb
+++ actionpack/lib/action_dispatch/middleware/rescue.rb
@@ -1,14 +1,26 @@
 module ActionDispatch
   class Rescue
-    def initialize(app, rescuer)
-      @app, @rescuer = app, rescuer
+    def initialize(app, rescuers = {}, &block)
+      @app, @rescuers = app, {}
+      rescuers.each { |exception, rescuer| rescue_from(exception, rescuer) }
+      instance_eval(&block) if block_given?
     end
 
     def call(env)
       @app.call(env)
     rescue Exception => exception
-      env['action_dispatch.rescue.exception'] = exception
-      @rescuer.call(env)
+      if rescuer = @rescuers[exception.class.name]
+        env['action_dispatch.rescue.exception'] = exception
+        rescuer.call(env)
+      else
+        raise exception
+      end
     end
+
+    protected
+      def rescue_from(exception, rescuer)
+        exception = exception.class.name if exception.is_a?(Exception)
+        @rescuers[exception.to_s] = rescuer
+      end
   end
 end
