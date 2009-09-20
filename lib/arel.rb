d343fdd8f836f27f38b69d0d7d26591eb7d325f6
Bryan Helmkamp <bryan@brynary.com>
2009-09-20 12:08:36 -0400

    When Arel is loaded the $LOAD_PATH should already be set properly, so don't modify it

--- lib/arel.rb
+++ lib/arel.rb
@@ -5,7 +5,6 @@ require 'active_support/core_ext/class/attribute_accessors'
 require 'active_record'
 require 'active_record/connection_adapters/abstract/quoting'
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'arel/algebra'
 require 'arel/engines'
 require 'arel/session'
