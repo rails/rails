fab8b25c15064a020e62e2baca11ed1f551f4d6f
Bob Remeika <bob.remeika@gmail.com>
2009-09-23 01:15:31 -0700

    ObserveFieldTest uses url_for from AjaxTestCase

--- actionpack/test/javascript/ajax_test.rb
+++ actionpack/test/javascript/ajax_test.rb
@@ -110,5 +110,55 @@ class ButtonToRemoteTest < AjaxTestCase
       button(callback => "undoRequestCompleted(request)")
     end
   end
+end
 
+class ObserveFieldTest < AjaxTestCase
+  def protect_against_forgery?
+    false
+  end
+
+  def field(options = {})
+    observe_field("title", options)
+  end
+
+  test "basic" do
+    assert_html field,
+      %w(data-observe="true")
+  end
+
+  test "with a :frequency option" do
+    assert_html field(:frequency => 5.minutes),
+      %w(data-observe="true" data-frequency="300")
+  end
+
+  test "using a url string" do
+    assert_html field(:url => "/some/other/url"),
+      %w(data-observe="true" data-url="/some/other/url")
+  end
+
+  test "using a url hash" do
+    assert_html field(:url => {:controller => :blog, :action => :update}),
+      %w(data-observe="true" data-url="/url/hash")
+  end
+
+#  def test_observe_field
+#    assert_dom_equal %(<script type=\"text/javascript\">\n//<![CDATA[\nnew Form.Element.Observer('glass', 300, function(element, value) {new Ajax.Request('http://www.example.com/reorder_if_empty', {asynchronous:true, evalScripts:true, parameters:value})})\n//]]>\n</script>),
+#      observe_field("glass", :frequency => 5.minutes, :url => { :action => "reorder_if_empty" })
+#  end
+#
+#  def test_observe_field_using_with_option
+#    expected = %(<script type=\"text/javascript\">\n//<![CDATA[\nnew Form.Element.Observer('glass', 300, function(element, value) {new Ajax.Request('http://www.example.com/check_value', {asynchronous:true, evalScripts:true, parameters:'id=' + encodeURIComponent(value)})})\n//]]>\n</script>)
+#    assert_dom_equal expected, observe_field("glass", :frequency => 5.minutes, :url => { :action => "check_value" }, :with => 'id')
+#    assert_dom_equal expected, observe_field("glass", :frequency => 5.minutes, :url => { :action => "check_value" }, :with => "'id=' + encodeURIComponent(value)")
+#  end
+#
+#  def test_observe_field_using_json_in_with_option
+#    expected = %(<script type=\"text/javascript\">\n//<![CDATA[\nnew Form.Element.Observer('glass', 300, function(element, value) {new Ajax.Request('http://www.example.com/check_value', {asynchronous:true, evalScripts:true, parameters:{'id':value}})})\n//]]>\n</script>)
+#    assert_dom_equal expected, observe_field("glass", :frequency => 5.minutes, :url => { :action => "check_value" }, :with => "{'id':value}")
+#  end
+#
+#  def test_observe_field_using_function_for_callback
+#    assert_dom_equal %(<script type=\"text/javascript\">\n//<![CDATA[\nnew Form.Element.Observer('glass', 300, function(element, value) {alert('Element changed')})\n//]]>\n</script>),
+#      observe_field("glass", :frequency => 5.minutes, :function => "alert('Element changed')")
+#  end
 end
