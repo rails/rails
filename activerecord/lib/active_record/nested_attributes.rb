329b14aa8fdd291a00d17ba12c2e0ab4c3a157cc
George Ogata <george.ogata@gmail.com>
2009-09-29 11:37:29 -0400

    Fix nested attributes examples again.
    Those hashes don't have indifferent access.

--- activerecord/lib/active_record/nested_attributes.rb
+++ activerecord/lib/active_record/nested_attributes.rb
@@ -46,14 +46,14 @@ module ActiveRecord
     # create the member and avatar in one go:
     #
     #   params = { :member => { :name => 'Jack', :avatar_attributes => { :icon => 'smiling' } } }
-    #   member = Member.create(params['member'])
+    #   member = Member.create(params[:member])
     #   member.avatar.id # => 2
     #   member.avatar.icon # => 'smiling'
     #
     # It also allows you to update the avatar through the member:
     #
-    #   params = { :member' => { :avatar_attributes => { :id => '2', :icon => 'sad' } } }
-    #   member.update_attributes params['member']
+    #   params = { :member => { :avatar_attributes => { :id => '2', :icon => 'sad' } } }
+    #   member.update_attributes params[:member]
     #   member.avatar.icon # => 'sad'
     #
     # By default you will only be able to set and update attributes on the
