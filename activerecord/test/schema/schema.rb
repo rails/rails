2aef092625ebe4b188d993e27f464c01595f803b
Bryan Stearns <bryanstearns@gmail.com>
2009-09-30 23:17:50 -0700

    Add failing test that triggers the stack overflow for #2578.
    Signed-off-by: Eloy Duran <eloy.de.enige@gmail.com>

--- activerecord/test/schema/schema.rb
+++ activerecord/test/schema/schema.rb
@@ -192,6 +192,11 @@ ActiveRecord::Schema.define do
     t.string :info
   end
 
+  create_table :invoices, :force => true do |t|
+    t.integer :balance
+    t.datetime :updated_at
+  end
+
   create_table :items, :force => true do |t|
     t.column :name, :integer
   end
@@ -215,6 +220,11 @@ ActiveRecord::Schema.define do
   create_table :legacy_things, :force => true do |t|
     t.integer :tps_report_number
     t.integer :version, :null => false, :default => 0
+  end
+
+  create_table :line_items, :force => true do |t|
+    t.integer :invoice_id
+    t.integer :amount
   end
 
   create_table :lock_without_defaults, :force => true do |t|
