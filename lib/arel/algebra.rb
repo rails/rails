71d65b7571d5ff01e0de3c08a9396dc1e1386118
Bryan Helmkamp <bryan@brynary.com>
2009-09-30 23:35:23 -0400

    Pull primitives up one level

--- lib/arel/algebra.rb
+++ lib/arel/algebra.rb
@@ -1,4 +1,9 @@
 require 'arel/algebra/core_extensions'
+
+require 'arel/algebra/attribute'
+require 'arel/algebra/expression'
+require 'arel/algebra/ordering'
 require 'arel/algebra/predicates'
 require 'arel/algebra/relations'
-require 'arel/algebra/primitives'
+require 'arel/algebra/value'
+
--- lib/arel/algebra/primitives.rb
+++
@@ -1,5 +0,0 @@
-require 'arel/algebra/primitives/attribute'
-require 'arel/algebra/primitives/ordering'
-require 'arel/algebra/primitives/value'
-require 'arel/algebra/primitives/expression'
-
