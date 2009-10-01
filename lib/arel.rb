836a57732c6ddd3842f1bf50e687049a098f7383
Bryan Helmkamp <bryan@brynary.com>
2009-09-30 23:21:42 -0400

    Autoload Arel::Session

--- lib/arel.rb
+++ lib/arel.rb
@@ -5,6 +5,8 @@ require 'active_support/core_ext/class/attribute_accessors'
 require 'active_record'
 require 'active_record/connection_adapters/abstract/quoting'
 
-require 'arel/algebra'
-require 'arel/engines'
-require 'arel/session'
+module Arel
+  require 'arel/algebra'
+  require 'arel/engines'
+  autoload :Session, 'arel/session'
+end
