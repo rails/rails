95b7e4f7d76928765a2392bf21d8f0644822c6d0
Michael Koziarski <michael@koziarski.com>
2009-10-08 14:02:12 +1300

    field_error_proc needs to return a safe string

--- actionpack/lib/action_view/helpers/active_record_helper.rb
+++ actionpack/lib/action_view/helpers/active_record_helper.rb
@@ -3,7 +3,7 @@ require 'action_view/helpers/form_helper'
 
 module ActionView
   class Base
-    @@field_error_proc = Proc.new{ |html_tag, instance| "<div class=\"fieldWithErrors\">#{html_tag}</div>" }
+    @@field_error_proc = Proc.new{ |html_tag, instance| "<div class=\"fieldWithErrors\">#{html_tag}</div>".html_safe! }
     cattr_accessor :field_error_proc
   end
 
