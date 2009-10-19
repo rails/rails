1ac5cf478825391071d34ec3d7f294fe28c0fceb
Pratik Naik <pratiknaik@gmail.com>
2009-10-19 15:06:54 -0200

    Make sure boot.rb requires rubygems only when needed

--- railties/lib/rails/generators/rails/app/templates/config/boot.rb
+++ railties/lib/rails/generators/rails/app/templates/config/boot.rb
@@ -1,15 +1,18 @@
 # Package management
 # Choose one
 
-# Use Bundler (preferred)
 environment = File.expand_path('../../vendor/gems/environment', __FILE__)
-require environment if File.exist?(environment)
-
-# Use 2.x style vendor/rails directory
 vendor_rails = File.expand_path('../../vendor/rails', __FILE__)
-Dir["#{vendor_rails}/*/lib"].each { |path| $:.unshift(path) } if File.exist?(vendor_rails)
 
-# Load Rails from traditional RubyGems
-require 'rubygems'
+if File.exist?(environment)
+  # Use Bundler (preferred)
+  require environment
+elsif File.exist?(vendor_rails)
+  # Use 2.x style vendor/rails directory
+  Dir["#{vendor_rails}/*/lib"].each { |path| $:.unshift(path) }
+else
+  # Load Rails from traditional RubyGems
+  require 'rubygems'
+end
 
 require 'rails'
