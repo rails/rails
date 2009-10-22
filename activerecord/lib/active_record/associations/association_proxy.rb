2f1ded306728ce8ff614e09a113398328e7fe6ca
Mat Brown <mat@patch.com>
2009-10-22 10:20:44 -0400

    Fix instance_eval calls to association proxies
    In the current stable, ActiveRecord::Associations::AssociationProxy#method_missing calls yield() if a block is given, causing the block to always be evaluated in its calling context. However, in the case of instance_eval, correct behavior requires that the block be passed directly to the @target, rather than being evaluated inside a different block. Incidentally, this also simplifies the code slightly.
    
    [#3412 state:committed]
    
    Signed-off-by: Jeremy Kemper <jeremy@bitsweat.net>

--- activerecord/lib/active_record/associations/association_proxy.rb
+++ activerecord/lib/active_record/associations/association_proxy.rb
@@ -208,14 +208,10 @@ module ActiveRecord
 
       private
         # Forwards any missing method call to the \target.
-        def method_missing(method, *args)
+        def method_missing(method, *args, &block)
           if load_target
             if @target.respond_to?(method)
-              if block_given?
-                @target.send(method, *args)  { |*block_args| yield(*block_args) }
-              else
-                @target.send(method, *args)
-              end
+              @target.send(method, *args, &block)
             else
               super
             end
