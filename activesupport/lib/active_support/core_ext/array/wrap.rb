a0fc92f455e239fd667ed267b76855eaaf190d2c
Yehuda Katz <wycats@gmail.com>
2009-10-28 01:43:33 -0700

    This is all that's needed in 1.8.7+

--- activesupport/lib/active_support/core_ext/array/wrap.rb
+++ activesupport/lib/active_support/core_ext/array/wrap.rb
@@ -1,17 +1,18 @@
 class Array
   # Wraps the object in an Array unless it's an Array.  Converts the
   # object to an Array using #to_ary if it implements that.
-  def self.wrap(object)
-    case object
-    when nil
-      []
-    when self
-      object
-    else
-      if object.respond_to?(:to_ary)
-        object.to_ary
-      else
+  array = Array("foo\nbar")
+
+  if array.size == 1
+    def self.wrap(object)
+      Array(object)
+    end
+  else
+    def self.wrap(object)
+      if object.is_a?(String)
         [object]
+      else
+        Array(object)
       end
     end
   end
