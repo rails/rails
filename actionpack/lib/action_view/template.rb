2ccd4e790e0885a2c210f518e6ea3de542126746
Bryan Helmkamp <bryan@brynary.com>
2009-11-09 15:14:18 -0500

    Silence warning: method redefined; discarding old filename

--- actionpack/lib/action_view/template.rb
+++ actionpack/lib/action_view/template.rb
@@ -64,7 +64,7 @@ module ActionView #:nodoc:
 
       def load!
         return if @loaded
-        
+
         @paths = {}
         templates_in_path do |template|
           template.load!
@@ -108,8 +108,9 @@ module ActionView #:nodoc:
       @@exempt_from_layout.merge(regexps)
     end
 
-    attr_accessor :template_path, :filename, :load_path, :base_path
+    attr_accessor :template_path, :load_path, :base_path
     attr_accessor :locale, :name, :format, :extension
+    attr_writer :filename
     delegate :to_s, :to => :path
 
     def initialize(template_path, load_path = nil)
