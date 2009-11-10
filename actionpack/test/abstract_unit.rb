a31ecff554c9ef18af5ee85d57f8d10f9f31b320
Joshua Peek <josh@joshpeek.com>
2009-11-09 22:51:48 -0600

    Don't warn me if I'm not using bundler to run tests

--- actionpack/test/abstract_unit.rb
+++ actionpack/test/abstract_unit.rb
@@ -2,7 +2,6 @@ root = File.expand_path('../../..', __FILE__)
 begin
   require "#{root}/vendor/gems/environment"
 rescue LoadError
-  $stderr.puts "Missing bundled environment, continuing: #{$!}"
   $:.unshift "#{root}/activesupport/lib"
   $:.unshift "#{root}/activemodel/lib"
 end
