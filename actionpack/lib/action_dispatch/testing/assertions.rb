1004fcb76761aa42ff18d8aa373614a41a259d3c
Joshua Peek <josh@joshpeek.com>
2009-11-09 21:37:37 -0600

    Fixed AD assertion autoloads [#3470 state:resolved]

--- actionpack/lib/action_dispatch/testing/assertions.rb
+++ actionpack/lib/action_dispatch/testing/assertions.rb
@@ -1,8 +1,21 @@
 module ActionDispatch
   module Assertions
-    %w(response selector tag dom routing model).each do |kind|
-      require "action_dispatch/testing/assertions/#{kind}"
-      include const_get("#{kind.camelize}Assertions")
+    autoload :DomAssertions, 'action_dispatch/testing/assertions/dom'
+    autoload :ModelAssertions, 'action_dispatch/testing/assertions/model'
+    autoload :ResponseAssertions, 'action_dispatch/testing/assertions/response'
+    autoload :RoutingAssertions, 'action_dispatch/testing/assertions/routing'
+    autoload :SelectorAssertions, 'action_dispatch/testing/assertions/selector'
+    autoload :TagAssertions, 'action_dispatch/testing/assertions/tag'
+
+    extend ActiveSupport::Concern
+
+    included do
+      include DomAssertions
+      include ModelAssertions
+      include ResponseAssertions
+      include RoutingAssertions
+      include SelectorAssertions
+      include TagAssertions
     end
   end
 end
