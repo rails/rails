f62a8831fe1015c1056f01ad58dc5aba9cb6658b
Jeremy Kemper <jeremy@bitsweat.net>
2009-11-10 21:04:00 -0800

    test-unit 2: filter_backtrace is private

--- railties/test/backtrace_cleaner_test.rb
+++ railties/test/backtrace_cleaner_test.rb
@@ -18,12 +18,12 @@ if defined? Test::Unit::Util::BacktraceFilter
     test "test with backtrace should use the rails backtrace cleaner to clean" do
       Rails.stubs(:backtrace_cleaner).returns(stub(:clean))
       Rails.backtrace_cleaner.expects(:clean).with(@backtrace, nil)
-      @test.filter_backtrace(@backtrace)
+      @test.send(:filter_backtrace, @backtrace)
     end
     
     test "filter backtrace should have the same arity as Test::Unit::Util::BacktraceFilter" do
       assert_nothing_raised do
-        @test.filter_backtrace(@backtrace, '/opt/local/lib')
+        @test.send(:filter_backtrace, @backtrace, '/opt/local/lib')
       end
     end
   end
