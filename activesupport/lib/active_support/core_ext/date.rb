7eb4f2ecc53831d850682d88634ccf41676e40a2
Jeremy Kemper <jeremy@bitsweat.net>
2009-11-14 01:13:54 -0800

    Consolidate date & time landscape: require 'active_support/time'

--- activesupport/lib/active_support/core_ext/date.rb
+++
@@ -1,7 +0,0 @@
-require 'date'
-
-require 'active_support/core_ext/date/acts_like'
-require 'active_support/core_ext/date/freeze'
-
-require 'active_support/core_ext/date/calculations'
-require 'active_support/core_ext/date/conversions'
