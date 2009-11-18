2ddbef421cb877bc219ac2737bbba793c53edbde
Xavier Noria <fxn@hashref.com>
2009-11-19 00:10:19 +0100

    explains the motivation for duplicable?

--- activesupport/lib/active_support/core_ext/object/duplicable.rb
+++ activesupport/lib/active_support/core_ext/object/duplicable.rb
@@ -1,3 +1,19 @@
+# Most objects are cloneable, but not all. For example you can't dup +nil+:
+#
+#   nil.dup # => TypeError: can't dup NilClass
+#
+# Classes may signal their instances are not duplicable removing +dup+/+clone+
+# or raising exceptions from them. So, to dup an arbitrary object you normally
+# use an optimistic approach and are ready to catch an exception, say:
+#
+#   arbitrary_object.dup rescue object
+#
+# Rails dups objects in a few critical spots where they are not that arbitrary.
+# That rescue is very expensive (like 40 times slower than a predicate), and it
+# is often triggered.
+#
+# That's why we hardcode the following cases and check duplicable? instead of
+# using that rescue idiom.
 class Object
   # Can you safely .dup this object?
   # False for nil, false, true, symbols, numbers, class and module objects; true otherwise.
