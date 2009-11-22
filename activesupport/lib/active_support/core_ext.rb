0bc150c884e9840fb6e917a818312eab3da83300
Jeremy Kemper <jeremy@bitsweat.net>
2009-11-22 14:50:12 -0800

    Rails 3: move core_ext/blank to core_ext/object/blank for forward compatibility

--- activesupport/lib/active_support/core_ext.rb
+++ activesupport/lib/active_support/core_ext.rb
@@ -1,4 +1,8 @@
-Dir[File.dirname(__FILE__) + "/core_ext/*.rb"].sort.each do |path|
-  filename = File.basename(path, '.rb')
-  require "active_support/core_ext/#{filename}"
+filenames = Dir["#{File.dirname(__FILE__)}/core_ext/*.rb"].sort.map do |path|
+  File.basename(path, '.rb')
 end
+
+# deprecated
+filenames -= %w(blank)
+
+filenames.each { |filename| require "active_support/core_ext/#{filename}" }
--- activesupport/lib/active_support/core_ext/object.rb
+++ activesupport/lib/active_support/core_ext/object.rb
@@ -1,3 +1,4 @@
+require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/object/conversions'
 require 'active_support/core_ext/object/extending'
 require 'active_support/core_ext/object/instance_variables'
