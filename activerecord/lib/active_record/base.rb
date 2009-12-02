32395899d7c97f69b508b7d7f9b7711f28586679
Gabe da Silveira <gabe@websaviour.com>
2009-12-02 15:28:54 -0800

    Replace reset_counter_cache with reset_counters that has API inline with existing update_counters method
    [#1211 state:committed]
    
    Signed-off-by: Jeremy Kemper <jeremy@bitsweat.net>

--- activerecord/lib/active_record/base.rb
+++ activerecord/lib/active_record/base.rb
@@ -967,20 +967,25 @@ module ActiveRecord #:nodoc:
         connection.select_value(sql, "#{name} Count").to_i
       end
 
-      # Reset a counter cache for all records.
+      # Resets one or more counter caches to their correct value using an SQL
+      # count query.  This is useful when adding new counter caches, or if the
+      # counter has been corrupted or modified directly by SQL.
       #
       # ==== Parameters
       #
-      # * +association_name+ - The name of of the association counter cache to reset
+      # * +id+ - The id of the object you wish to reset a counter on.
+      # * +counters+ - One or more counter names to reset
       #
       # ==== Examples
-      #   # For all Post records reset the comments_count
-      #   Post.reset_counter_cache(:comments)
-      def reset_counter_cache(association)
-        child_class = reflect_on_association(association).klass
-        counter_name = child_class.reflect_on_association(self.name.downcase.to_sym).counter_cache_column
+      #
+      #   # For Post with id #1 records reset the comments_count
+      #   Post.reset_counters(1, :comments)
+      def reset_counters(id, *counters)
+        object = find(id)
+        counters.each do |association|
+          child_class = reflect_on_association(association).klass
+          counter_name = child_class.reflect_on_association(self.name.downcase.to_sym).counter_cache_column
 
-        find_each do |object|
           connection.update("UPDATE #{quoted_table_name} SET #{connection.quote_column_name(counter_name)} = #{object.send(association).count} WHERE #{connection.quote_column_name(primary_key)} = #{quote_value(object.id)}", "#{name} UPDATE")
         end
       end
