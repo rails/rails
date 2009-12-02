1ee9b40b18a0bed5bb10a0785f7e2730bac983f6
Joshua Peek <josh@joshpeek.com>
2009-12-02 15:07:01 -0600

    Failing tests for to_param/to_query not escaping "[]"

--- activesupport/lib/active_support/core_ext/object/to_query.rb
+++ activesupport/lib/active_support/core_ext/object/to_query.rb
@@ -15,7 +15,7 @@ class Array
   # Converts an array into a string suitable for use as a URL query string,
   # using the given +key+ as the param name.
   #
-  #   ['Rails', 'coding'].to_query('hobbies') # => "hobbies%5B%5D=Rails&hobbies%5B%5D=coding"
+  #   ['Rails', 'coding'].to_query('hobbies') # => "hobbies[]=Rails&hobbies[]=coding"
   def to_query(key)
     prefix = "#{key}[]"
     collect { |value| value.to_query(prefix) }.join '&'
