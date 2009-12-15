fda3ac6e6824d494102e8fc5f4114a550caa22b1
David Heinemeier Hansson <david@loudthinking.com>
2009-12-14 18:01:47 -0800

    Add Enumerable#exclude? to bring parity to Enumerable#include? and avoid if !x.include?/else calls [DHH]

--- activesupport/lib/active_support/core_ext/enumerable.rb
+++ activesupport/lib/active_support/core_ext/enumerable.rb
@@ -102,6 +102,15 @@ module Enumerable
     size = block_given? ? select(&block).size : self.size
     size > 1
   end
+<<<<<<< HEAD
+=======
+  
+  # The negative of the Enumerable#include?. Returns true if the collection does not include the object.
+  def exclude?(object)
+    !include?(object)
+  end
+end
+>>>>>>> 7b61541... Add Enumerable#exclude? to bring parity to Enumerable#include? and avoid if !x.include?/else calls [DHH]
 
   # Returns true if none of the elements match the given block.
   #
