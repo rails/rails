a9002056761a481589852d6e8680f752a5b823b7
Dwayne Litzenberger <dlitz@infonium.ca>
2009-12-22 15:17:55 -0500

    Fix ActiveSupport::JSON encoding of control characters [\x00-\x1f]
    According to RFC 4627, only the following Unicode code points are
    allowed unescaped in JSON:
    
      unescaped = %x20-21 / %x23-5B / %x5D-10FFFF
    
    However, ActiveSupport::JSON did not escape the range %x00-1f.  This caused
    parse errors when trying to decode the resulting output.
    
    [#3345 state:committed]
    
    Signed-off-by: Jeremy Kemper <jeremy@bitsweat.net>

--- activesupport/test/json/encoding_test.rb
+++ activesupport/test/json/encoding_test.rb
@@ -22,7 +22,9 @@ class TestJSONEncoding < Test::Unit::TestCase
 
   StringTests   = [[ 'this is the <string>',     %("this is the \\u003Cstring\\u003E")],
                    [ 'a "string" with quotes & an ampersand', %("a \\"string\\" with quotes \\u0026 an ampersand") ],
-                   [ 'http://test.host/posts/1', %("http://test.host/posts/1")]]
+                   [ 'http://test.host/posts/1', %("http://test.host/posts/1")],
+                   [ "Control characters: \x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f",
+                     %("Control characters: \\u0000\\u0001\\u0002\\u0003\\u0004\\u0005\\u0006\\u0007\\b\\t\\n\\u000B\\f\\r\\u000E\\u000F\\u0010\\u0011\\u0012\\u0013\\u0014\\u0015\\u0016\\u0017\\u0018\\u0019\\u001A\\u001B\\u001C\\u001D\\u001E\\u001F") ]]
 
   ArrayTests    = [[ ['a', 'b', 'c'],          %([\"a\",\"b\",\"c\"])          ],
                    [ [1, 'a', :b, nil, false], %([1,\"a\",\"b\",null,false]) ]]
