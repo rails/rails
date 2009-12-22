a43a9c81cf3d85e8dca6afdd92307ce153643ebe
David Heinemeier Hansson <david@loudthinking.com>
2009-12-21 16:41:02 -0800

    Dont need to specify password_confirmation, that happens automatically

--- railties/lib/rails/generators/rails/app/templates/app/controllers/application_controller.rb
+++ railties/lib/rails/generators/rails/app/templates/app/controllers/application_controller.rb
@@ -4,5 +4,5 @@
 class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery
-  filter_parameter_logging :password, :password_confirmation
+  filter_parameter_logging :password
 end
