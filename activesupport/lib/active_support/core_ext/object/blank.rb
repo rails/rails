1668ad3baf36aa1fd495e7229c46ebbdebdffd27
David Heinemeier Hansson <david@loudthinking.com>
2009-12-27 17:54:43 -0800

    Added Object#presence that returns the object if it's #present? otherwise returns nil [DHH/Colin Kelley]

--- activesupport/lib/active_support/core_ext/object/blank.rb
+++ activesupport/lib/active_support/core_ext/object/blank.rb
@@ -2,11 +2,11 @@ class Object
   # An object is blank if it's false, empty, or a whitespace string.
   # For example, "", "   ", +nil+, [], and {} are blank.
   #
-  # This simplifies
+  # This simplifies:
   #
   #   if !address.nil? && !address.empty?
   #
-  # to
+  # ...to:
   #
   #   if !address.blank?
   def blank?
@@ -16,6 +16,24 @@ class Object
   # An object is present if it's not blank.
   def present?
     !blank?
+  end
+  
+  # Returns object if it's #present? otherwise returns nil.
+  # object.presence is equivalent to object.present? ? object : nil.
+  #
+  # This is handy for any representation of objects where blank is the same
+  # as not present at all.  For example, this simplifies a common check for
+  # HTTP POST/query parameters:
+  #
+  #   state   = params[:state]   if params[:state].present?
+  #   country = params[:country] if params[:country].present?
+  #   region  = state || country || 'US'
+  #
+  # ...becomes:
+  #
+  #   region = params[:state].presence || params[:country].presence || 'US'
+  def presence
+    self if present?
   end
 end
 
