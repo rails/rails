8571aa613f1707401b0fe54c0fa1687604c48e0a
Jeremy Kemper <jeremy@bitsweat.net>
2010-01-02 18:39:36 -0800

    Revert "Remove optional join_dependency argument as Relation always supplies it"
    This reverts commit 8e57deed8b4efad6ea1c551f415b74596111f890.

--- activerecord/lib/active_record/associations.rb
+++ activerecord/lib/active_record/associations.rb
@@ -1465,7 +1465,8 @@ module ActiveRecord
           after_destroy(method_name)
         end
 
-        def find_with_associations(options = {}, join_dependency)
+        def find_with_associations(options = {}, join_dependency = nil)
+          join_dependency ||= JoinDependency.new(self, merge_includes(scope(:find, :include), options[:include]), options[:joins])
           rows = select_all_rows(options, join_dependency)
           join_dependency.instantiate(rows)
         rescue ThrowResult
