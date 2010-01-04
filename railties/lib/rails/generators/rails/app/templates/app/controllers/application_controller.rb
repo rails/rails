950e6bb4912bc87e693eab8731920a122bcff2c2
David Heinemeier Hansson <david@loudthinking.com>
2010-01-03 21:32:40 -0500

    Remove self-evident comment about how subclasses work

--- railties/lib/rails/generators/rails/app/templates/app/controllers/application_controller.rb
+++ railties/lib/rails/generators/rails/app/templates/app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
   protect_from_forgery
   filter_parameter_logging :password
