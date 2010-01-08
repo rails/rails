26a0fefc6d87107161a0d3a171d852f6cd2b4888
Pratik Naik <pratiknaik@gmail.com>
2010-01-09 03:43:17 +0530

    Dont require activerecord

--- lib/arel.rb
+++ lib/arel.rb
@@ -2,9 +2,6 @@ require 'active_support/inflector'
 require 'active_support/core_ext/module/delegation'
 require 'active_support/core_ext/class/attribute_accessors'
 
-require 'active_record'
-require 'active_record/connection_adapters/abstract/quoting'
-
 module Arel
   require 'arel/algebra'
   require 'arel/engines'
