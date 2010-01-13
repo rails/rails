116df09c3e4a57a6b0a57a5134c451687113ece1
José Valim <jose.valim@gmail.com>
2010-01-13 22:59:27 +0100

    Add notifications to ActiveResource.

--- activeresource/lib/active_resource/railtie.rb
+++ activeresource/lib/active_resource/railtie.rb
@@ -1,2 +1,11 @@
 require "active_resource"
 require "rails"
+
+module ActiveResource
+  class Railtie < Rails::Railtie
+    plugin_name :active_resource
+
+    require "active_resource/railties/subscriber"
+    subscriber ActiveResource::Railties::Subscriber.new
+  end
+end
