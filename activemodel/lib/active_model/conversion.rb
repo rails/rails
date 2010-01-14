23e434f08fa756e9a8c472a8212c6f72ac92b32b
Mikel Lindsaar <raasdnil@gmail.com>
2010-01-15 08:29:08 +1100

    ActiveModel::Conversion documentation

--- activemodel/lib/active_model/conversion.rb
+++ activemodel/lib/active_model/conversion.rb
@@ -1,5 +1,16 @@
 module ActiveModel
-  # Include ActiveModel::Conversion if your object "acts like an ActiveModel model".
+  # If your object is already designed to implement all of the Active Model featurs
+  # include this module in your Class.
+  # 
+  #   class MyClass
+  #     include ActiveModel::Conversion
+  #   end
+  # 
+  # Returns self to the <tt>:to_model</tt> method.
+  # 
+  # If your model does not act like an Active Model object, then you should define
+  # <tt>:to_model</tt> yourself returning a proxy object that wraps your object
+  # with Active Model compliant methods.
   module Conversion
     def to_model
       self
