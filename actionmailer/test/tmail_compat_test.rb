77986f6bdb18414d730fa1ec686365dff26bb4fe
José Valim and Mikel Lindsaar <raasdnil@gmail.com>
2010-01-22 12:49:13 +1100

    Added use of AS::Notifications for tmail_compat.rb

--- actionmailer/test/tmail_compat_test.rb
+++ actionmailer/test/tmail_compat_test.rb
@@ -4,7 +4,6 @@ class TmailCompatTest < Test::Unit::TestCase
 
   def test_set_content_type_raises_deprecation_warning
     mail = Mail.new
-    STDERR.expects(:puts) # Deprecation warning
     assert_nothing_raised do
       mail.set_content_type "text/plain"
     end
@@ -13,7 +12,6 @@ class TmailCompatTest < Test::Unit::TestCase
 
   def test_transfer_encoding_raises_deprecation_warning
     mail = Mail.new
-    STDERR.expects(:puts) # Deprecation warning
     assert_nothing_raised do
       mail.transfer_encoding "base64"
     end
