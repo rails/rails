f85f5dfc8ffefff174b695c6363211d342f77a57
Xavier Noria <fxn@hashref.com>
2010-01-26 22:48:21 +0100

    removes unused Class#descedents

--- activesupport/lib/active_support/core_ext/object/extending.rb
+++ activesupport/lib/active_support/core_ext/object/extending.rb
@@ -1,40 +1,6 @@
 require 'active_support/core_ext/class/removal'
 require 'active_support/core_ext/object/blank'
 
-class Class
-  # Rubinius
-  if defined?(Class.__subclasses__)
-    def descendents
-      subclasses = []
-      __subclasses__.each {|k| subclasses << k; subclasses.concat k.descendents }
-      subclasses
-    end
-  else
-    # MRI
-    begin
-      ObjectSpace.each_object(Class.new) {}
-
-      def descendents
-        subclasses = []
-        ObjectSpace.each_object(class << self; self; end) do |k|
-          subclasses << k unless k == self
-        end
-        subclasses
-      end
-    # JRuby
-    rescue StandardError
-      def descendents
-        subclasses = []
-        ObjectSpace.each_object(Class) do |k|
-          subclasses << k if k < self
-        end
-        subclasses.uniq!
-        subclasses
-      end
-    end
-  end
-end
-
 class Object
   def extended_by #:nodoc:
     ancestors = class << self; ancestors end
