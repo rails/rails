6404feee500dc5a2268da1e41af08a5ff847338a
José Valim <jose.valim@gmail.com>
2010-01-26 21:05:50 +0100

    AC railtie should configure helpers path.

--- activesupport/lib/active_support/railtie.rb
+++ activesupport/lib/active_support/railtie.rb
@@ -7,13 +7,13 @@ module ActiveSupport
 
     # Loads support for "whiny nil" (noisy warnings when methods are invoked
     # on +nil+ values) if Configuration#whiny_nils is true.
-    initializer :initialize_whiny_nils do |app|
+    initializer "active_support.initialize_whiny_nils" do |app|
       require 'active_support/whiny_nil' if app.config.whiny_nils
     end
 
     # Sets the default value for Time.zone
     # If assigned value cannot be matched to a TimeZone, an exception will be raised.
-    initializer :initialize_time_zone do |app|
+    initializer "active_support.initialize_time_zone" do |app|
       require 'active_support/core_ext/time/zones'
       zone_default = Time.__send__(:get_zone, app.config.time_zone)
 
@@ -36,7 +36,7 @@ module I18n
     config.i18n.engines_load_path = []
     config.i18n.load_path = []
 
-    initializer :initialize_i18n do
+    initializer "i18n.initialize" do
       require 'active_support/i18n'
 
       ActionDispatch::Callbacks.to_prepare do
