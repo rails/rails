31248fe3698c8d6c54465dfdf463ea49749e1c33
José Valim <jose.valim@gmail.com>
2010-02-04 10:39:55 +0100

    Remove instrument! and require thread from AS::Notifications.

--- activesupport/lib/active_support/notifications.rb
+++ activesupport/lib/active_support/notifications.rb
@@ -45,7 +45,7 @@ module ActiveSupport
     class << self
       attr_writer :notifier
       delegate :publish, :subscribe, :to => :notifier
-      delegate :instrument, :instrument!, :to => :instrumenter
+      delegate :instrument, :to => :instrumenter
 
       def notifier
         @notifier ||= Notifier.new
