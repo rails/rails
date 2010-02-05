55c1a86ea4326c7ad4ba3f22d55c7183d8c6a1ef
Santiago Pastorino and José Ignacio Costa <santiago+jose@wyeworks.com>
2010-02-05 18:19:42 -0200

    Added backwards compatibility for html_safe!

--- activesupport/lib/active_support/core_ext/string/output_safety.rb
+++ activesupport/lib/active_support/core_ext/string/output_safety.rb
@@ -111,11 +111,45 @@ module ActiveSupport #:nodoc:
 end
 
 class String
-  def html_safe!
-    raise "Use html_safe with your strings instead of html_safe! See http://yehudakatz.com/2010/02/01/safebuffers-and-rails-3-0/ for the full story."
-  end
+  alias_method :add_without_safety, :+
 
   def html_safe
     ActiveSupport::SafeBuffer.new(self)
   end
+
+  def html_safe?
+    defined?(@_rails_html_safe) && @_rails_html_safe
+  end
+
+  def html_safe!
+    ActiveSupport::Deprecation.warn("Use html_safe with your strings instead of html_safe! See http://yehudakatz.com/2010/02/01/safebuffers-and-rails-3-0/ for the full story.", caller)
+    @_rails_html_safe = true
+    self
+  end
+
+  def add_with_safety(other)
+    result = add_without_safety(other)
+    if html_safe? && also_html_safe?(other)
+      result.html_safe!
+    else
+      result
+    end
+  end
+  alias_method :+, :add_with_safety
+
+  def concat_with_safety(other_or_fixnum)
+    result = concat_without_safety(other_or_fixnum)
+    unless html_safe? && also_html_safe?(other_or_fixnum)
+      @_rails_html_safe = false
+    end
+    result
+  end
+  alias_method_chain :concat, :safety
+  undef_method :<<
+  alias_method :<<, :concat_with_safety
+
+  private
+   def also_html_safe?(other)
+     other.respond_to?(:html_safe?) && other.html_safe?
+   end
 end
