fd567785f4670b31db300324642cf2464025313e
Jeremy Kemper <jeremy@bitsweat.net>
2010-02-05 12:23:22 -0800

    Make UrlWriter includable in a Module

--- actionpack/lib/action_controller/metal/url_for.rb
+++ actionpack/lib/action_controller/metal/url_for.rb
@@ -1,4 +1,5 @@
 require 'active_support/core_ext/class/attribute'
+require 'active_support/core_ext/module/attribute_accessors'
 
 module ActionController
   # In <b>routes.rb</b> one defines URL-to-controller mappings, but the reverse
@@ -87,7 +88,14 @@ module ActionController
 
     included do
       ActionController::Routing::Routes.install_helpers(self)
-      class_attribute :default_url_options
+
+      # Including in a class uses an inheritable hash. Modules get a plain hash.
+      if respond_to?(:class_attribute)
+        class_attribute :default_url_options
+      else
+        mattr_accessor :default_url_options
+      end
+
       self.default_url_options = {}
     end
 
