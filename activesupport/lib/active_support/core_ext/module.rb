b8bb54af7ff6652327d99f6a7cf5c96a3f3f876d
Xavier Noria <fxn@hashref.com>
2010-02-11 23:15:04 +0100

    defines Module#anonymous?

--- activesupport/lib/active_support/core_ext/module.rb
+++ activesupport/lib/active_support/core_ext/module.rb
@@ -1,6 +1,6 @@
 require 'active_support/core_ext/module/aliasing'
 require 'active_support/core_ext/module/introspection'
-
+require 'active_support/core_ext/module/anonymous'
 require 'active_support/core_ext/module/attribute_accessors'
 require 'active_support/core_ext/module/attr_internal'
 require 'active_support/core_ext/module/attr_accessor_with_default'
---
+++ activesupport/lib/active_support/core_ext/module/anonymous.rb
@@ -0,0 +1,20 @@
+class Module  
+  # A module may or may not have a name.
+  #
+  #   module M; end
+  #   M.name # => "M"
+  #
+  #   m = Module.new
+  #   m.name # => ""
+  #
+  # A module gets a name when it is first assigned to a constant. Either
+  # via the +module+ or +class+ keyword or by an explicit assignment:
+  #
+  #   m = Module.new # creates an anonymous module
+  #   M = m          # => m gets a name here as a side-effect
+  #   m.name         # => "M"
+  #
+  def anonymous?
+    name == ''
+  end
+end
