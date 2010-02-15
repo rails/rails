4419497e403387a3cc93c4c1a3b6a07314a85e43
Joshua Peek <josh@joshpeek.com>
2010-02-15 10:38:39 -0600

    Make sure AS test env doesn't have duplicate libs in load path

---
+++ activesupport/test/load_paths_test.rb
@@ -0,0 +1,14 @@
+require 'abstract_unit'
+
+class LoadPathsTest < Test::Unit::TestCase
+  def test_uniq_load_paths
+    load_paths_count = $LOAD_PATH.inject({}) { |paths, path|
+      expanded_path = File.expand_path(path)
+      paths[expanded_path] ||= 0
+      paths[expanded_path] += 1
+      paths
+    }
+
+    assert_equal [], load_paths_count.select { |k, v| v > 1 }, $LOAD_PATH.inspect
+  end
+end
