6f4c45199b791ac2c90aa6742f8ffbd62b02ed7b
Caio Chassot <dev@caiochassot.com>
2010-02-22 04:09:51 -0300

    In app_generator, fix hardcoded `bundle` call to derive the bundle executable name from Thor::Util.ruby_command.
    This ensures that eg. when `rails` is called from `ruby1.9`, `bundle1.9` is called.
    
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- railties/lib/generators/rails/app/app_generator.rb
+++ railties/lib/generators/rails/app/app_generator.rb
@@ -178,7 +178,8 @@ module Rails::Generators
     end
 
     def bundle_if_dev_or_edge
-      run "bundle install" if dev_or_edge?
+      bundle_command = Thor::Util.ruby_command.sub(%r[ruby(?=[^/]*$)], 'bundle')
+      run "#{bundle_command} install" if dev_or_edge?
     end
 
     protected
