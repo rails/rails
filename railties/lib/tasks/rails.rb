f56a1631be74d416625a436f4b96bba64b4d2855
Jeremy Kemper <jeremy@bitsweat.net>
2010-02-26 08:54:42 -0800

    Typo: _tasks -> _paths

--- railties/lib/tasks/rails.rb
+++ railties/lib/tasks/rails.rb
@@ -8,7 +8,7 @@ deprecated_paths = Dir["#{RAILS_ROOT}/vendor/plugins/*/tasks/**/*.rake"].sort
 if deprecated_paths.any?
   plugins = deprecated_paths.map { |p| $1 if p =~ %r((vendor/plugins/[^/]+/tasks)) }.compact
   ActiveSupport::Deprecation.warn "Rake tasks in #{plugins.to_sentence} are deprecated. Use lib/tasks instead."
-  deprecated_tasks.each { |ext| load ext }
+  deprecated_paths.each { |ext| load ext }
 end
 Dir["#{RAILS_ROOT}/vendor/plugins/*/lib/tasks/**/*.rake"].sort.each { |ext| load ext }
 Dir["#{RAILS_ROOT}/lib/tasks/**/*.rake"].sort.each { |ext| load ext }
