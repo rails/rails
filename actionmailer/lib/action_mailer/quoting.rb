cd5ec4744dec9f8a443d1318bfafc9cf02752819
Pavel Golubeff <golubeff@MacBook-Pro.local>
2010-03-03 09:54:52 +0100

    Fix quoting regexp encoding in ActionMailer
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- actionmailer/lib/action_mailer/quoting.rb
+++ actionmailer/lib/action_mailer/quoting.rb
@@ -22,7 +22,7 @@ module ActionMailer
     # A quick-and-dirty regexp for determining whether a string contains any
     # characters that need escaping.
     if !defined?(CHARS_NEEDING_QUOTING)
-      CHARS_NEEDING_QUOTING = /[\000-\011\013\014\016-\037\177-\377]/
+      CHARS_NEEDING_QUOTING = Regexp.new('[\000-\011\013\014\016-\037\177-\377]', nil, 'n')
     end
 
     # Quote the given text if it contains any "illegal" characters
