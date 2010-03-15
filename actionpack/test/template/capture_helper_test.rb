a594a22267bfd3346e00923742c4aa7edad0cef7
Xavier Noria <fxn@hashref.com>
2010-03-15 18:29:21 +0100

    with_output_buffer cannot assume there's an output_buffer
    [#4182 state:committed]
    
    Signed-off-by: Jeremy Kemper <jeremy@bitsweat.net>

--- actionpack/test/template/capture_helper_test.rb
+++ actionpack/test/template/capture_helper_test.rb
@@ -12,4 +12,10 @@ class CaptureHelperTest < ActionView::TestCase
     assert content_for?(:title)
     assert ! content_for?(:something_else)
   end
+
+  def test_with_output_buffer_does_not_assume_there_is_an_output_buffer
+    av = ActionView::Base.new
+    assert_nil av.output_buffer
+    assert_equal "", av.with_output_buffer {}
+  end
 end
