1e2caa5d52b9f367a1d7dc51d063a7ab1444b712
Santiago Pastorino and Sebastian Martinez <santiago+sebastian@wyeworks.com>
2010-03-18 18:59:13 -0300

    Added missing requires abstract_unit and activesupport to the loadpath of ts_isolated [#4215 state:committed]
    Signed-off-by: wycats <wycats@gmail.com>

--- activesupport/test/callbacks_test.rb
+++ activesupport/test/callbacks_test.rb
@@ -1,4 +1,4 @@
-# require 'abstract_unit'
+require 'abstract_unit'
 require 'test/unit'
 require 'active_support'
 
--- activesupport/test/flush_cache_on_private_memoization_test.rb
+++ activesupport/test/flush_cache_on_private_memoization_test.rb
@@ -1,3 +1,4 @@
+require 'abstract_unit'
 require 'active_support'
 require 'test/unit'
 
