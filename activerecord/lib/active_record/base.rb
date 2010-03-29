5f7bc47303933ec67700f6390c98d889f6d9b0d1
wycats <wycats@gmail.com>
2010-03-28 23:45:38 -0700

    Rename modularized_table_name_prefix to full_table_name_prefix [#4032 state:resolved]

--- activerecord/lib/active_record/base.rb
+++ activerecord/lib/active_record/base.rb
@@ -768,7 +768,7 @@ module ActiveRecord #:nodoc:
               contained = contained.singularize if parent.pluralize_table_names
               contained << '_'
             end
-            name = "#{modularized_table_name_prefix}#{contained}#{undecorated_table_name(base.name)}#{table_name_suffix}"
+            name = "#{full_table_name_prefix}#{contained}#{undecorated_table_name(base.name)}#{table_name_suffix}"
           end
 
         @quoted_table_name = nil
@@ -776,7 +776,7 @@ module ActiveRecord #:nodoc:
         name
       end
 
-      def modularized_table_name_prefix #:nodoc:
+      def full_table_name_prefix #:nodoc:
         (parents.detect{ |p| p.respond_to?(:table_name_prefix) } || self).table_name_prefix
       end
 
