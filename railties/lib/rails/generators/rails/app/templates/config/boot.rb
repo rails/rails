c88b9337cf25acf0a765f90c434d134f582864ea
José Valim <jose.valim@gmail.com>
2010-04-14 00:02:00 +0200

    Allow a Rails application to be initialized from any directory and not just from inside it (ht: Andre Arko).

--- railties/lib/rails/generators/rails/app/templates/config/boot.rb
+++ railties/lib/rails/generators/rails/app/templates/config/boot.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
+
 # Set up gems listed in the Gemfile.
-if File.exist?(File.expand_path('../../Gemfile', __FILE__))
+gemfile = File.expand_path('../../Gemfile', __FILE__)
+if File.exist?(gemfile)
+  ENV['BUNDLE_GEMFILE'] = gemfile
   require 'bundler'
   Bundler.setup
 end
