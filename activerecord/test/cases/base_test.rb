d92e3111fed2a5372d4bb8ac66c9d8542f730488
Aaron Patterson <aaron.patterson@gmail.com>
2010-04-16 21:35:10 -0700

    making yaml tests less specific to emitter. [#4425 state:resolved]
    Signed-off-by: wycats <wycats@gmail.com>

--- activerecord/test/cases/base_test.rb
+++ activerecord/test/cases/base_test.rb
@@ -2095,7 +2095,8 @@ class BasicsTest < ActiveRecord::TestCase
     assert_equal written_on_in_current_timezone, xml.elements["//written-on"].text
     assert_equal "datetime" , xml.elements["//written-on"].attributes['type']
 
-    assert_equal "--- Have a nice day\n" , xml.elements["//content"].text
+    assert_match(/^--- Have a nice day\n/ , xml.elements["//content"].text)
+    assert_equal 'Have a nice day' , YAML.load(xml.elements["//content"].text)
     assert_equal "yaml" , xml.elements["//content"].attributes['type']
 
     assert_equal "david@loudthinking.com", xml.elements["//author-email-address"].text
