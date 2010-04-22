920df0a4758d003dc284c13e439e0b9f60a6e6f0
Santiago Pastorino <santiago@wyeworks.com>
2010-04-22 12:33:54 -0300

    Make ActionDispatch url_for use HWIA symbolize_keys
    Signed-off-by: Jeremy Kemper <jeremy@bitsweat.net>

--- actionpack/test/controller/url_for_test.rb
+++ actionpack/test/controller/url_for_test.rb
@@ -258,13 +258,17 @@ module AbstractController
       end
 
       def test_with_stringified_keys
-        assert_equal("/c/a", W.new.url_for('controller' => 'c', 'action' => 'a', 'only_path' => true))
         assert_equal("/c", W.new.url_for('controller' => 'c', 'only_path' => true))
+        assert_equal("/c/a", W.new.url_for('controller' => 'c', 'action' => 'a', 'only_path' => true))
       end
 
       def test_with_hash_with_indifferent_access
-        assert_equal("/c/a", W.new.url_for(HashWithIndifferentAccess.new('controller' => 'c', 'action' => 'a', 'only_path' => true)))
+        W.default_url_options[:controller] = 'd'
+        W.default_url_options[:only_path]  = false
         assert_equal("/c", W.new.url_for(HashWithIndifferentAccess.new('controller' => 'c', 'only_path' => true)))
+
+        W.default_url_options[:action] = 'b'
+        assert_equal("/c/a", W.new.url_for(HashWithIndifferentAccess.new('controller' => 'c', 'action' => 'a', 'only_path' => true)))
       end
 
       private
