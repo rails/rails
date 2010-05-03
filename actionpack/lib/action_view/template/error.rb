8ae9b05fa0ca579ecabcd563e6ba75ea0f44f074
José Valim <jose.valim@gmail.com>
2010-05-03 12:54:52 +0200

    Make backtrace_cleaner work as expected. Prior to this patch, the Full Trace rarely showed the full trace. Also, increase performance considerably.

--- actionpack/lib/action_view/template/error.rb
+++ actionpack/lib/action_view/template/error.rb
@@ -21,17 +21,18 @@ module ActionView
       super("Missing #{template_type} #{path} with #{details.inspect} in view paths #{display_paths}")
     end
   end
+
   class Template
     # The Template::Error exception is raised when the compilation of the template fails. This exception then gathers a
     # bunch of intimate details and uses it to report a very precise exception message.
     class Error < ActionViewError #:nodoc:
       SOURCE_CODE_RADIUS = 3
 
-      attr_reader :original_exception
+      attr_reader :original_exception, :backtrace
 
       def initialize(template, assigns, original_exception)
         @template, @assigns, @original_exception = template, assigns.dup, original_exception
-        @backtrace = compute_backtrace
+        @backtrace = original_exception.backtrace
       end
 
       def file_name
@@ -40,14 +41,6 @@ module ActionView
 
       def message
         ActiveSupport::Deprecation.silence { original_exception.message }
-      end
-
-      def clean_backtrace
-        if defined?(Rails) && Rails.respond_to?(:backtrace_cleaner)
-          Rails.backtrace_cleaner.clean(original_exception.backtrace)
-        else
-          original_exception.backtrace
-        end
       end
 
       def sub_template_message
@@ -87,29 +80,16 @@ module ActionView
         @line_number ||=
           if file_name
             regexp = /#{Regexp.escape File.basename(file_name)}:(\d+)/
-
-            $1 if message =~ regexp or clean_backtrace.find { |line| line =~ regexp }
+            $1 if message =~ regexp || backtrace.find { |line| line =~ regexp }
           end
       end
 
       def to_s
-        "\n#{self.class} (#{message}) #{source_location}:\n" +
-        "#{source_extract}\n    #{clean_backtrace.join("\n    ")}\n\n"
-      end
-
-      # don't do anything nontrivial here. Any raised exception from here becomes fatal
-      # (and can't be rescued).
-      def backtrace
-        @backtrace
+        "\n#{self.class} (#{message}) #{source_location}:\n\n" +
+        "#{source_extract(4)}\n    #{backtrace.join("\n    ")}\n\n"
       end
 
       private
-        def compute_backtrace
-          [
-            "#{source_location.capitalize}\n\n#{source_extract(4)}\n    " +
-            clean_backtrace.join("\n    ")
-          ]
-        end
 
         def source_location
           if line_number
