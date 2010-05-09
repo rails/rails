8d2f6c16e381f5fff6d3f24f5c73a443577a1488
Pratik Naik <pratiknaik@gmail.com>
2010-05-09 12:42:48 +0100

    Revert "Revert "Add index length support for MySQL [#1852 state:open]""
    This reverts commit 6626833db13a69786f9f6cd56b9f53c4017c3e39.

--- activerecord/lib/active_record/schema_dumper.rb
+++ activerecord/lib/active_record/schema_dumper.rb
@@ -177,6 +177,7 @@ HEADER
             statment_parts << index.columns.inspect
             statment_parts << (':name => ' + index.name.inspect)
             statment_parts << ':unique => true' if index.unique
+            statment_parts << (':length => ' + Hash[*index.columns.zip(index.lengths).flatten].inspect) if index.lengths.compact.present?
 
             '  ' + statment_parts.join(', ')
           end
