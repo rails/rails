8c5e1652c7d1343a4b4acbc10bbcb59e202bf37d
Rizwan Reza <rizwanreza@gmail.com>
2010-05-17 01:55:56 +0430

    Renames Array#rand -> Array#random_element
    Signed-off-by: Xavier Noria <fxn@hashref.com>

--- activesupport/lib/active_support/core_ext/array/random_access.rb
+++ activesupport/lib/active_support/core_ext/array/random_access.rb
@@ -1,14 +1,4 @@
 class Array
-  # This method is deprecated because it masks Kernel#rand within the Array class itself, 
-  # which may be used by a 3rd party library extending Array in turn. See
-  #
-  #   https://rails.lighthouseapp.com/projects/8994-ruby-on-rails/tickets/4555
-  #
-  def rand # :nodoc:
-    ActiveSupport::Deprecation.warn "Array#rand is deprecated, use random_element instead", caller
-    random_element
-  end
-
   # Returns a random element from the array.
   def random_element
     self[Kernel.rand(length)]
