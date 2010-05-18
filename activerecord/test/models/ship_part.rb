a5696e36c6b49381e84184fcc2f164285a26a166
Ian White <ian.w.white@gmail.com>
2010-05-18 10:30:11 +0100

    Nested records (re: autosave) are now updated even when the intermediate parent record is unchanged [#4242]
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- activerecord/test/models/ship_part.rb
+++ activerecord/test/models/ship_part.rb
@@ -1,5 +1,7 @@
 class ShipPart < ActiveRecord::Base
   belongs_to :ship
-
+  has_many :trinkets, :class_name => "Treasure", :as => :looter
+  accepts_nested_attributes_for :trinkets, :allow_destroy => true
+  
   validates_presence_of :name
 end
