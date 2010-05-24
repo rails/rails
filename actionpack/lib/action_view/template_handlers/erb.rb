50b7c0c104a87a9b650f4d257d986823cc34cf13
wycats <wycats@gmail.com>
2010-05-24 11:18:20 -0700

    Give the ERB String the encoding of the original template

--- actionpack/lib/action_view/template_handlers/erb.rb
+++ actionpack/lib/action_view/template_handlers/erb.rb
@@ -12,7 +12,7 @@ module ActionView
 
       def compile(template)
         magic = $1 if template.source =~ /\A(<%#.*coding[:=]\s*(\S+)\s*-?%>)/
-        erb = "#{magic}<% __in_erb_template=true %>#{template.source}"
+        erb = "#{magic}<% __in_erb_template=true %>#{template.source}".force_encoding(template.source.encoding)
         ::ERB.new(erb, nil, erb_trim_mode, '@output_buffer').src
       end
     end
