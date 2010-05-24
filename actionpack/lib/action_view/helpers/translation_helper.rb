2b8eb5404e5545fb80eb6edf09c61bd5030034e8
Santiago Pastorino <santiago@wyeworks.com>
2010-05-24 17:21:18 -0300

    Revert "translation method for arrays on TranslationHelper module returns an array where values for keys of the form (.|_)html keys are html_safe"
    This reverts commit 05c95b5c5815c0b3ae55fda7a897922b7f3ec2c7.
    
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- actionpack/lib/action_view/helpers/translation_helper.rb
+++ actionpack/lib/action_view/helpers/translation_helper.rb
@@ -3,7 +3,7 @@ require 'action_view/helpers/tag_helper'
 module ActionView
   module Helpers
     module TranslationHelper
-      # Delegates to I18n#translate but also performs three additional functions. First, it'll catch MissingTranslationData exceptions
+      # Delegates to I18n#translate but also performs three additional functions. First, it'll catch MissingTranslationData exceptions 
       # and turn them into inline spans that contains the missing key, such that you can see in a view what is missing where.
       #
       # Second, it'll scope the key by the current partial if the key starts with a period. So if you call translate(".foo") from the
@@ -11,19 +11,20 @@ module ActionView
       # to translate many keys within the same partials and gives you a simple framework for scoping them consistently. If you don't
       # prepend the key with a period, nothing is converted.
       #
-      # Third, it’ll mark the translation as safe HTML if the key has the suffix "_html" or the last element of the key is the word
-      # "html". For example, calling translate("footer_html") or translate("footer.html") will return a safe HTML string that won’t
-      # be escaped by other HTML helper methods. This naming convention helps to identify translations that include HTML tags so that
+      # Third, it’ll mark the translation as safe HTML if the key has the suffix "_html" or the last element of the key is the word 
+      # "html". For example, calling translate("footer_html") or translate("footer.html") will return a safe HTML string that won’t 
+      # be escaped by other HTML helper methods. This naming convention helps to identify translations that include HTML tags so that 
       # you know what kind of output to expect when you call translate in a template.
 
-      def translate(keys, options = {})
-        options[:raise]  = true
-        are_keys_a_string  = keys.is_a?(String)
-        keys = scope_keys_by_partial(keys)
-
-        translations = I18n.translate(keys, options)
-        translations = html_safe_translation_keys(keys, Array.wrap(translations))
-        are_keys_a_string ? translations.first : translations
+      def translate(key, options = {})
+        options[:raise] = true
+        translation = I18n.translate(scope_key_by_partial(key), options)
+        translation = (translation.respond_to?(:join) ? translation.join : translation)
+        if html_safe_translation_key?(key)
+          translation.html_safe
+        else
+          translation
+        end
       rescue I18n::MissingTranslationData => e
         keys = I18n.normalize_keys(e.locale, e.key, e.options[:scope])
         content_tag('span', keys.join(', '), :class => 'translation_missing')
@@ -37,24 +38,23 @@ module ActionView
       alias :l :localize
 
       private
-
-        def scope_keys_by_partial(keys)
-          Array.wrap(keys).map do |key|
-            if key.to_s.first == "."
-              unless @_virtual_path
-                raise "Cannot use t(#{key.inspect}) shortcut because path is not available"
-              end
-            @_virtual_path.gsub(%r{/_?}, ".") + key
+        
+        def scope_key_by_partial(key)
+          strkey = key.respond_to?(:join) ? key.join : key.to_s
+          if strkey.first == "."
+            if @_virtual_path
+              @_virtual_path.gsub(%r{/_?}, ".") + strkey
             else
-              key
+              raise "Cannot use t(#{key.inspect}) shortcut because path is not available"
             end
+          else
+            key
           end
         end
 
-        def html_safe_translation_keys(keys, translations)
-          keys.zip(translations).map do |key, translation|
-            key =~ /(\b|_|\.)html$/ ? translation.html_safe : translation
-          end
+        def html_safe_translation_key?(key)
+          key = key.is_a?(Array) ? key.last : key.to_s
+          key =~ /(\b|_|\.)html$/
         end
     end
   end
