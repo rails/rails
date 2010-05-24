b5f9c8822dce55b33a4a6ab929f5b86b055a0284
Santiago Pastorino <santiago@wyeworks.com>
2010-05-24 17:49:21 -0300

    translate method doesn't support arrays anymore and is optimized to be used with strings or symbols
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- actionpack/test/template/translation_helper_test.rb
+++ actionpack/test/template/translation_helper_test.rb
@@ -18,18 +18,6 @@ class TranslationHelperTest < ActiveSupport::TestCase
     assert_equal expected, translate(:foo)
   end
 
-  def test_translation_of_an_array
-    I18n.expects(:translate).with(["foo", "bar"], :raise => true).returns(["foo", "bar"])
-    assert_equal "foobar", translate(["foo", "bar"])
-  end
-
-  def test_translation_of_an_array_with_html
-    expected = '<a href="#">foo</a><a href="#">bar</a>'
-    I18n.expects(:translate).with(["foo", "bar", "html"], :raise => true).returns(['<a href="#">foo</a>', '<a href="#">bar</a>'])
-    @view = ActionView::Base.new(ActionController::Base.view_paths, {})
-    assert_equal expected, @view.render(:file => "test/array_translation")
-  end
-
   def test_delegates_localize_to_i18n
     @time = Time.utc(2008, 7, 8, 12, 18, 38)
     I18n.expects(:localize).with(@time)
@@ -43,9 +31,9 @@ class TranslationHelperTest < ActiveSupport::TestCase
   end
 
   def test_scoping_by_partial_of_an_array
-    I18n.expects(:translate).with("test.scoped_array_translation.foo.bar", :raise => true).returns(["foo", "bar"])
+    I18n.expects(:translate).with("test.scoped_translation.foo.bar", :raise => true).returns(["foo", "bar"])
     @view = ActionView::Base.new(ActionController::Base.view_paths, {})
-    assert_equal "foobar", @view.render(:file => "test/scoped_array_translation")
+    assert_equal "foobar", @view.render(:file => "test/scoped_translation")
   end
   
   def test_translate_does_not_mark_plain_text_as_safe_html
