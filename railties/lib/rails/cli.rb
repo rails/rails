dcc758acb0bf210ec8a1c25afeafb7da0334f0b9
Chad Fowler <chad@chadfowler.com>
2010-06-10 14:00:58 -0400

    detect being inside a rails application even from a subdirectory
    Signed-off-by: David Heinemeier Hansson <david@loudthinking.com>

--- railties/lib/rails/cli.rb
+++ railties/lib/rails/cli.rb
@@ -1,23 +1,5 @@
 require 'rbconfig'
-
-module Rails
-  module ScriptRailsLoader
-    RUBY = File.join(*RbConfig::CONFIG.values_at("bindir", "ruby_install_name")) + RbConfig::CONFIG["EXEEXT"]
-    SCRIPT_RAILS = File.join('script', 'rails')
-
-    def self.exec_script_rails!
-      cwd = Dir.pwd
-      exec RUBY, SCRIPT_RAILS, *ARGV if File.exists?(SCRIPT_RAILS)
-      Dir.chdir("..") do
-        # Recurse in a chdir block: if the search fails we want to be sure
-        # the application is generated in the original working directory.
-        exec_script_rails! unless cwd == Dir.pwd
-      end
-    rescue SystemCallError
-      # could not chdir, no problem just return
-    end
-  end
-end
+require 'rails/script_rails_loader'
 
 Rails::ScriptRailsLoader.exec_script_rails!
 
