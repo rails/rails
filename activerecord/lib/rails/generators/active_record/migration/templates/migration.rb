3344c011da17cb59f4452101c8e65067d7d30cce
Santiago Pastorino <santiago@wyeworks.com>
2010-06-25 13:07:17 -0300

    Avoid a blank line before the add/remove columns

--- activerecord/lib/rails/generators/active_record/migration/templates/migration.rb
+++ activerecord/lib/rails/generators/active_record/migration/templates/migration.rb
@@ -1,6 +1,6 @@
 class <%= migration_class_name %> < ActiveRecord::Migration
   def self.up
-<% attributes.each do |attribute| %>
+<% attributes.each do |attribute| -%>
   <%- if migration_action -%>
     <%= migration_action %>_column :<%= table_name %>, :<%= attribute.name %><% if migration_action == 'add' %>, :<%= attribute.type %><% end %>
   <%- end -%>
@@ -8,7 +8,7 @@ class <%= migration_class_name %> < ActiveRecord::Migration
   end
 
   def self.down
-<% attributes.reverse.each do |attribute| %>
+<% attributes.reverse.each do |attribute| -%>
   <%- if migration_action -%>
     <%= migration_action == 'add' ? 'remove' : 'add' %>_column :<%= table_name %>, :<%= attribute.name %><% if migration_action == 'remove' %>, :<%= attribute.type %><% end %>
   <%- end -%>
