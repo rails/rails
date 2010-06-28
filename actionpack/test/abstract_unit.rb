6d04fa6dc4865af1112c4d35a456a81008815ee7
José Valim <jose.valim@gmail.com>
2010-06-28 12:23:41 +0200

    Deprecate the old router DSL. Since it is still used intensively across ActionPack test suite, patches that translates Rails internal tests to the new router DSL are welcome (note though that a few tests shouldn't be translated since they are testing exactly the old mapper API, like the ones in actionpack/test/controller/resource_test.rb and actionpack/test/controller/routing_test.rb)

--- actionpack/test/abstract_unit.rb
+++ actionpack/test/abstract_unit.rb
@@ -41,6 +41,14 @@ require 'pp' # require 'pp' early to prevent hidden_methods from not picking up 
 module Rails
 end
 
+# Monkey patch the old router initialization to be silenced.
+class ActionDispatch::Routing::DeprecatedMapper
+  def initialize_with_silencer(*args)
+    ActiveSupport::Deprecation.silence { initialize_without_silencer(*args) }
+  end
+  alias_method_chain :initialize, :silencer
+end
+
 ActiveSupport::Dependencies.hook!
 
 # Show backtraces for deprecated behavior for quicker cleanup.
