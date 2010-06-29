d4c7d3fd94e5a885a6366eaeb3b908bb58ffd4db
wycats <wycats@gmail.com>
2010-06-29 12:18:17 -0700

    Create a deprecation behavior that triggers a notification for deprecation notices, and make the behaviors independent of the environment names.
    * In Rails 2.3 apps being upgraded, you will need to add the deprecation
      configuration to each of your environments. Failing to do so will
      result in the same behavior as Rails 2.3, but with an outputted warning
      to provide information on how to set up the setting.
    * New Rails 3 applications generate the setting
    * The notification style will send deprecation notices using
      ActiveSupport::Notifications. Third-party tools can listen in to
      these notifications to provide a streamlined view of the
      deprecation notices occurring in your app.
    * The payload in the notification is the deprecation warning itself
      as well as the callstack from the point that triggered the
      notification.

--- railties/test/isolation/abstract_unit.rb
+++ railties/test/isolation/abstract_unit.rb
@@ -100,7 +100,7 @@ module TestHelpers
         end
       end
 
-      add_to_config 'config.secret_token = "3b7cd727ee24e8444053437c36cc66c4"; config.session_store :cookie_store, :key => "_myapp_session"'
+      add_to_config 'config.secret_token = "3b7cd727ee24e8444053437c36cc66c4"; config.session_store :cookie_store, :key => "_myapp_session"; config.active_support.deprecation = :log'
     end
 
     def make_basic_app
@@ -110,6 +110,7 @@ module TestHelpers
       app = Class.new(Rails::Application)
       app.config.secret_token = "3b7cd727ee24e8444053437c36cc66c4"
       app.config.session_store :cookie_store, :key => "_myapp_session"
+      app.config.active_support.deprecation = :log
 
       yield app if block_given?
       app.initialize!
