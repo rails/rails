ac42e6951f0feb1a9890a3bbf5ebad87ebec08cc
David Trasbo <me@dtrasbo.com>
2010-06-29 16:45:20 +0200

    Deprecate ActiveRecord::Base#class_name [#379 state:committed]
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- activerecord/lib/active_record/base.rb
+++ activerecord/lib/active_record/base.rb
@@ -1285,6 +1285,8 @@ module ActiveRecord #:nodoc:
 
       # Turns the +table_name+ back into a class name following the reverse rules of +table_name+.
       def class_name(table_name = table_name) # :nodoc:
+        ActiveSupport::Deprecation.warn("ActiveRecord::Base#class_name is deprecated and will be removed in Rails 2.3.9.", caller)
+
         # remove any prefix and/or suffix from the table name
         class_name = table_name[table_name_prefix.length..-(table_name_suffix.length + 1)].camelize
         class_name = class_name.singularize if pluralize_table_names
