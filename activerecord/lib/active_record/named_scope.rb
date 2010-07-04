bfbdeeae3080c89a0b27e42d684cbeb6206c6f60
José Valim <jose.valim@gmail.com>
2010-07-04 15:57:44 +0200

    Use bind instead of instance_exec cause it may be causing memory leaks. Also, provide a simpler and sane implementation for scoped. [#5044 state:resolved]

--- activerecord/lib/active_record/named_scope.rb
+++ activerecord/lib/active_record/named_scope.rb
@@ -8,15 +8,16 @@ module ActiveRecord
     #
     # You can define a scope that applies to all finders using ActiveRecord::Base.default_scope.
     def self.included(base)
-      base.class_eval do
-        extend ClassMethods
-        named_scope :scoped, lambda { |scope| scope }
-      end
+      base.extend ClassMethods
     end
 
     module ClassMethods
       def scopes
         read_inheritable_attribute(:scopes) || write_inheritable_attribute(:scopes, {})
+      end
+
+      def scoped(scope, &block)
+        Scope.new(self, scope, &block)
       end
 
       # Adds a class method for retrieving and querying objects. A scope represents a narrowing of a database query,
@@ -84,22 +85,22 @@ module ActiveRecord
       #   assert_equal expected_options, Shirt.colored('red').proxy_options
       def named_scope(name, options = {}, &block)
         name = name.to_sym
+
         scopes[name] = lambda do |parent_scope, *args|
           Scope.new(parent_scope, case options
             when Hash
               options
             when Proc
               if self.model_name != parent_scope.model_name
-                parent_scope.instance_exec(*args, &options)
+                options.bind(parent_scope).call(*args)
               else
                 options.call(*args)
               end
           end, &block)
         end
-        (class << self; self end).instance_eval do
-          define_method name do |*args|
-            scopes[name].call(self, *args)
-          end
+
+        singleton_class.send :define_method, name do |*args|
+          scopes[name].call(self, *args)
         end
       end
     end
