2244bb00131f2f5ec4d1912f7dd6378ba0f3b58f
Aaron Patterson <aaron.patterson@gmail.com>
2010-07-16 13:50:58 -0700

    fixing whitespace errors

--- activerecord/test/cases/connection_pool_test.rb
+++ activerecord/test/cases/connection_pool_test.rb
@@ -5,17 +5,17 @@ class ConnectionManagementTest < ActiveRecord::TestCase
     @env = {}
     @app = stub('App')
     @management = ActiveRecord::ConnectionAdapters::ConnectionManagement.new(@app)
-    
+
     @connections_cleared = false
     ActiveRecord::Base.stubs(:clear_active_connections!).with { @connections_cleared = true }
   end
-  
+
   test "clears active connections after each call" do
     @app.expects(:call).with(@env)
     @management.call(@env)
     assert @connections_cleared
   end
-  
+
   test "doesn't clear active connections when running in a test case" do
     @env['rack.test'] = true
     @app.expects(:call).with(@env)
