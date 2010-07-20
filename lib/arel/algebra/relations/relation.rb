35d66b6eee3cf51c37fa38eb7010c4dec8cd13a4
Aaron Patterson <aaron.patterson@gmail.com>
2010-07-20 15:18:22 -0700

    classify is not needed, so removing

--- lib/arel/algebra/relations/relation.rb
+++ lib/arel/algebra/relations/relation.rb
@@ -43,10 +43,13 @@ module Arel
         join(other_relation, OuterJoin)
       end
 
-      [:where, :project, :order, :take, :skip, :group, :from, :having].each do |operation_name|
+      %w{
+        where project order take skip group from having
+      }.each do |operation_name|
         class_eval <<-OPERATION, __FILE__, __LINE__
           def #{operation_name}(*arguments, &block)
-            arguments.all?(&:blank?) && !block_given?? self : #{operation_name.to_s.classify}.new(self, *arguments, &block)
+            arguments.all?(&:blank?) && !block_given? ?
+              self : #{operation_name.capitalize}.new(self, *arguments, &block)
           end
         OPERATION
       end
