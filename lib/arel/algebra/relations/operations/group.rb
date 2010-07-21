6da9e6d6818b3f33b73e929ea57c40ea8d2f1829
Aaron Patterson <aaron.patterson@gmail.com>
2010-07-20 17:37:03 -0700

    unfactoring more meta programming

--- lib/arel/algebra/relations/operations/group.rb
+++ lib/arel/algebra/relations/operations/group.rb
@@ -1,12 +1,18 @@
 module Arel
   class Group < Compound
-    attributes :relation, :groupings
-    deriving :==
+    attr_reader :groupings
 
     def initialize(relation, *groupings, &block)
-      @relation = relation
+      super(relation)
       @groupings = (groupings + arguments_from_block(relation, &block)) \
         .collect { |g| g.bind(relation) }
+    end
+
+    def == other
+      super ||
+        self.class === other &&
+        @relation == other.relation &&
+        @groupings == other.groupings
     end
   end
 end
