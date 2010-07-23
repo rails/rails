affeb51569d291ef7304189fd77b32c6e269af57
Jeroen van Dijk and Josh Kalderimis <jeroen@jeevidee.nl>
2010-07-23 14:42:21 +0200

    Move config_accessor :asset_host from ActionController::Base to AbstractController which fixes issues with asset_host in ActionMailer
    Including:
      - Moved mailer objects in separate directory
      - Added two tests for asset_host configuration option

--- actionpack/lib/action_controller/base.rb
+++ actionpack/lib/action_controller/base.rb
@@ -63,7 +63,7 @@ module ActionController
       klass.helper :all
     end
 
-    config_accessor :asset_host, :asset_path
+    config_accessor :asset_path
     ActiveSupport.run_load_hooks(:action_controller, self)
   end
 end
