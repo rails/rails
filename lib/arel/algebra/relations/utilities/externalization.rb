d2092e7e0bb5260bd02d9eeea4decf7efa34e29c
Aaron Patterson <aaron.patterson@gmail.com>
2010-07-26 14:23:13 -0700

    reorganizing classes

--- lib/arel/algebra/relations/utilities/externalization.rb
+++ lib/arel/algebra/relations/utilities/externalization.rb
@@ -12,14 +12,4 @@ module Arel
       @attributes ||= Header.new(relation.attributes.map { |a| a.to_attribute(self) })
     end
   end
-
-  module Relation
-    def externalize
-      @externalized ||= externalizable?? Externalization.new(self) : self
-    end
-
-    def externalizable?
-      false
-    end
-  end
 end
