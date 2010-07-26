f70c0b348d2f703d763a8136df7837567f7cb691
Thiago Pradi <tchandy@gmail.com>
2010-07-25 21:38:15 -0300

    Removing unnecessary code [#5192 state:resolved]
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- activerecord/test/cases/autosave_association_test.rb
+++ activerecord/test/cases/autosave_association_test.rb
@@ -171,7 +171,7 @@ class TestDefaultAutosaveAssociationOnAHasOneAssociation < ActiveRecord::TestCas
 end
 
 class TestDefaultAutosaveAssociationOnABelongsToAssociation < ActiveRecord::TestCase
-  fixtures :companies
+  fixtures :companies, :posts, :tags, :taggings
 
   def test_should_save_parent_but_not_invalid_child
     client = Client.new(:name => 'Joe (the Plumber)')
@@ -311,6 +311,12 @@ class TestDefaultAutosaveAssociationOnABelongsToAssociation < ActiveRecord::Test
 
     assert_equal num_orders +1, Order.count
     assert_equal num_customers +2, Customer.count
+  end
+
+  def test_store_association_with_a_polymorphic_relationship
+    num_tagging = Tagging.count
+    tags(:misc).create_tagging(:taggable => posts(:thinking))
+    assert_equal num_tagging +1, Tagging.count
   end
 end
 
