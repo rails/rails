faf14aeed6cc79bd356f2ac9739362b2ba5732bc
Aaron Patterson <aaron.patterson@gmail.com>
2010-07-25 17:42:18 -0700

    more unfactoring before we refactor

--- lib/arel/algebra/relations/operations/skip.rb
+++ lib/arel/algebra/relations/operations/skip.rb
@@ -1,7 +1,28 @@
 module Arel
   class Skip < Compound
-    attributes :relation, :skipped
-    deriving   :initialize, :==
-    requires   :skipping
+    attr_reader :relation, :skipped
+
+    def initialize relation, skipped
+      super(relation)
+      @skipped = skipped
+    end
+
+    def == other
+      super ||
+        Skip === other &&
+        relation == other.relation &&
+        skipped == other.skipped
+    end
+
+    def engine
+      engine   = relation.engine
+
+      # Temporary check of whether or not the engine supports where.
+      if engine.respond_to?(:supports) && !engine.supports(:skipping)
+        Memory::Engine.new
+      else
+        engine
+      end
+    end
   end
 end
