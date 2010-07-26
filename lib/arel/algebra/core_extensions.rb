edb786204fb45ba2a877ec7a3b26e84119b13377
Aaron Patterson <aaron.patterson@gmail.com>
2010-07-25 18:17:54 -0700

    removing unused file

--- lib/arel/algebra/core_extensions.rb
+++ lib/arel/algebra/core_extensions.rb
@@ -1,4 +1,3 @@
 require 'arel/algebra/core_extensions/object'
-require 'arel/algebra/core_extensions/class'
 require 'arel/algebra/core_extensions/symbol'
 require 'arel/algebra/core_extensions/hash'
