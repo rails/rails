496134516e19acd93772cfff8eafa4cb2dd8bc8d
Aaron Patterson <aaron.patterson@gmail.com>
2010-07-25 17:56:34 -0700

    more unfactoring

--- lib/arel/algebra/relations/operations/where.rb
+++ lib/arel/algebra/relations/operations/where.rb
@@ -1,18 +1,34 @@
 module Arel
   class Where < Compound
-    attributes :relation, :predicates
-    deriving   :==
-    requires   :restricting
+    attr_reader :predicates
 
     def initialize(relation, *predicates)
+      super(relation)
       predicates = [yield(relation)] + predicates if block_given?
       @predicates = predicates.map { |p| p.bind(relation) }
-      @relation   = relation
       @wheres = nil
     end
 
     def wheres
       @wheres ||= relation.wheres + predicates
+    end
+
+    def == other
+      super ||
+        Where === other &&
+        relation == other.relation &&
+        predicates == other.predicates
+    end
+
+    def engine
+      engine   = relation.engine
+
+      # Temporary check of whether or not the engine supports where.
+      if engine.respond_to?(:supports) && !engine.supports(:restricting)
+        Memory::Engine.new
+      else
+        engine
+      end
     end
   end
 end
