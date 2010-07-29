401cd97923fb52c8f8c458b8cb276b338e0b20f3
Piotr Sarnacki <drogus@gmail.com>
2010-07-29 14:12:25 +0200

    Modified ActionDispatch::Static to allow passing multiple roots

--- actionpack/lib/action_dispatch/middleware/static.rb
+++ actionpack/lib/action_dispatch/middleware/static.rb
@@ -2,11 +2,43 @@ require 'rack/utils'
 
 module ActionDispatch
   class Static
+    class FileHandler
+      def initialize(at, root)
+        @at = at.chomp("/")
+        @file_server = ::Rack::File.new(root)
+      end
+
+      def file_exist?(path)
+        (path = full_readable_path(path)) && File.file?(path)
+      end
+
+      def directory_exist?(path)
+        (path = full_readable_path(path)) && File.directory?(path)
+      end
+
+      def call(env)
+        env["PATH_INFO"].gsub!(/^#{@at}/, "")
+        @file_server.call(env)
+      end
+
+      private
+        def includes_path?(path)
+          @at == "" || path =~ /^#{@at}/
+        end
+
+        def full_readable_path(path)
+          return unless includes_path?(path)
+          path = path.gsub(/^#{@at}/, "")
+          File.join(@file_server.root, ::Rack::Utils.unescape(path))
+        end
+    end
+
     FILE_METHODS = %w(GET HEAD).freeze
 
-    def initialize(app, root)
+    def initialize(app, roots)
       @app = app
-      @file_server = ::Rack::File.new(root)
+      roots = normalize_roots(roots)
+      @file_handlers = file_handlers(roots)
     end
 
     def call(env)
@@ -14,15 +46,15 @@ module ActionDispatch
       method = env['REQUEST_METHOD']
 
       if FILE_METHODS.include?(method)
-        if file_exist?(path)
-          return @file_server.call(env)
+        if file_handler = file_exist?(path)
+          return file_handler.call(env)
         else
           cached_path = directory_exist?(path) ? "#{path}/index" : path
           cached_path += ::ActionController::Base.page_cache_extension
 
-          if file_exist?(cached_path)
+          if file_handler = file_exist?(cached_path)
             env['PATH_INFO'] = cached_path
-            return @file_server.call(env)
+            return file_handler.call(env)
           end
         end
       end
@@ -32,13 +64,21 @@ module ActionDispatch
 
     private
       def file_exist?(path)
-        full_path = File.join(@file_server.root, ::Rack::Utils.unescape(path))
-        File.file?(full_path) && File.readable?(full_path)
+        @file_handlers.detect { |f| f.file_exist?(path) }
       end
 
       def directory_exist?(path)
-        full_path = File.join(@file_server.root, ::Rack::Utils.unescape(path))
-        File.directory?(full_path) && File.readable?(full_path)
+        @file_handlers.detect { |f| f.directory_exist?(path) }
+      end
+
+      def normalize_roots(roots)
+        roots.is_a?(Hash) ? roots : { "/" => roots.chomp("/") }
+      end
+
+      def file_handlers(roots)
+        roots.map do |at, root|
+          FileHandler.new(at, root)
+        end
       end
   end
 end
--- actionpack/test/dispatch/static_test.rb
+++ actionpack/test/dispatch/static_test.rb
@@ -1,28 +1,23 @@
 require 'abstract_unit'
 
-class StaticTest < ActiveSupport::TestCase
-  DummyApp = lambda { |env|
-    [200, {"Content-Type" => "text/plain"}, ["Hello, World!"]]
-  }
-  App = ActionDispatch::Static.new(DummyApp, "#{FIXTURE_LOAD_PATH}/public")
-
-  test "serves dynamic content" do
+module StaticTests
+  def test_serves_dynamic_content
     assert_equal "Hello, World!", get("/nofile")
   end
 
-  test "serves static index at root" do
+  def test_serves_static_index_at_root
     assert_equal "/index.html", get("/index.html")
     assert_equal "/index.html", get("/index")
     assert_equal "/index.html", get("/")
   end
 
-  test "serves static file in directory" do
+  def test_serves_static_file_in_directory
     assert_equal "/foo/bar.html", get("/foo/bar.html")
     assert_equal "/foo/bar.html", get("/foo/bar/")
     assert_equal "/foo/bar.html", get("/foo/bar")
   end
 
-  test "serves static index file in directory" do
+  def test_serves_static_index_file_in_directory
     assert_equal "/foo/index.html", get("/foo/index.html")
     assert_equal "/foo/index.html", get("/foo/")
     assert_equal "/foo/index.html", get("/foo")
@@ -30,6 +25,50 @@ class StaticTest < ActiveSupport::TestCase
 
   private
     def get(path)
-      Rack::MockRequest.new(App).request("GET", path).body
+      Rack::MockRequest.new(@app).request("GET", path).body
     end
+end
+
+class StaticTest < ActiveSupport::TestCase
+  DummyApp = lambda { |env|
+    [200, {"Content-Type" => "text/plain"}, ["Hello, World!"]]
+  }
+  App = ActionDispatch::Static.new(DummyApp, "#{FIXTURE_LOAD_PATH}/public")
+
+  def setup
+    @app = App
+  end
+
+  include StaticTests
+end
+
+class MultipleDirectorisStaticTest < ActiveSupport::TestCase
+  DummyApp = lambda { |env|
+    [200, {"Content-Type" => "text/plain"}, ["Hello, World!"]]
+  }
+  App = ActionDispatch::Static.new(DummyApp,
+            { "/"     => "#{FIXTURE_LOAD_PATH}/public",
+              "/blog" => "#{FIXTURE_LOAD_PATH}/blog_public",
+              "/foo"  => "#{FIXTURE_LOAD_PATH}/non_existing_dir"
+            })
+
+  def setup
+    @app = App
+  end
+
+  include StaticTests
+
+  test "serves files from other mounted directories" do
+    assert_equal "/blog/index.html", get("/blog/index.html")
+    assert_equal "/blog/index.html", get("/blog/index")
+    assert_equal "/blog/index.html", get("/blog/")
+
+    assert_equal "/blog/blog.html", get("/blog/blog/")
+    assert_equal "/blog/blog.html", get("/blog/blog.html")
+    assert_equal "/blog/blog.html", get("/blog/blog")
+
+    assert_equal "/blog/subdir/index.html", get("/blog/subdir/index.html")
+    assert_equal "/blog/subdir/index.html", get("/blog/subdir/")
+    assert_equal "/blog/subdir/index.html", get("/blog/subdir")
+  end
 end
