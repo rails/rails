6ed1ba472e0f649726d9a0d04056934d1afe4573
Neeraj Singh <neerajdotname@gmail.com>
2010-08-03 17:26:59 -0400

    Ensure we can nest include calls [#5285 state:resolved]
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- activerecord/test/models/car.rb
+++ activerecord/test/models/car.rb
@@ -1,4 +1,9 @@
 class Car < ActiveRecord::Base
+  has_many :tyres
   has_many :engines
   has_many :wheels, :as => :wheelable
+
+  scope :incl_tyres, includes(:tyres)
+  scope :incl_engines, includes(:engines)
+
 end
