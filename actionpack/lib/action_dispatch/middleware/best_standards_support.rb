6767946374353f90ce05e68d38bcb93dcb8bae56
wycats <wycats@gmail.com>
2010-08-09 11:48:31 -0700

    Improve best_standards_support to use only IE=Edge in development mode

--- actionpack/lib/action_dispatch/middleware/best_standards_support.rb
+++ actionpack/lib/action_dispatch/middleware/best_standards_support.rb
@@ -1,12 +1,21 @@
 module ActionDispatch
   class BestStandardsSupport
-    def initialize(app)
+    def initialize(app, type = true)
       @app = app
+
+      @header = case type
+      when true
+        "IE=Edge,chrome=1"
+      when :builtin
+        "IE=Edge"
+      when false
+        nil
+      end
     end
 
     def call(env)
       status, headers, body = @app.call(env)
-      headers["X-UA-Compatible"] = "IE=Edge,chrome=1"
+      headers["X-UA-Compatible"] = @header
       [status, headers, body]
     end
   end
