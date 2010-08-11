a56ee4c9a23ca4f41ea001810a7c2358cc043f1d
Aaron Patterson <aaron.patterson@gmail.com>
2010-08-11 11:43:27 -0700

    avoiding tap saves us time

--- activerecord/lib/active_record/relation/query_methods.rb
+++ activerecord/lib/active_record/relation/query_methods.rb
@@ -49,7 +49,9 @@ module ActiveRecord
 
     def where(opts, *rest)
       value = build_where(opts, rest)
-      value ? clone.tap {|r| r.where_values += Array.wrap(value) } : clone
+      copy = clone
+      copy.where_values += Array.wrap(value) if value
+      copy
     end
 
     def having(*args)
@@ -58,7 +60,9 @@ module ActiveRecord
     end
 
     def limit(value = true)
-      clone.tap {|r| r.limit_value = value }
+      copy = clone
+      copy.limit_value = value
+      copy
     end
 
     def offset(value = true)
