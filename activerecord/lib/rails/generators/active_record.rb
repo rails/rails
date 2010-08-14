c25c81e7c9656dee2b5ac9d490784df44e8e23f9
Jack Dempsey <jack.dempsey@gmail.com>
2010-08-14 00:40:21 -0400

    split out active_record migration logic so others can easily reuse [#5389 state:resolved]
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- activerecord/lib/rails/generators/active_record.rb
+++ activerecord/lib/rails/generators/active_record.rb
@@ -1,26 +1,18 @@
 require 'rails/generators/named_base'
 require 'rails/generators/migration'
 require 'rails/generators/active_model'
+require 'rails/generators/active_record/migration'
 require 'active_record'
 
 module ActiveRecord
   module Generators
     class Base < Rails::Generators::NamedBase #:nodoc:
       include Rails::Generators::Migration
+      extend ActiveRecord::Generators::Migration
 
       # Set the current directory as base for the inherited generators.
       def self.base_root
         File.dirname(__FILE__)
-      end
-
-      # Implement the required interface for Rails::Generators::Migration.
-      def self.next_migration_number(dirname) #:nodoc:
-        next_migration_number = current_migration_number(dirname) + 1
-        if ActiveRecord::Base.timestamped_migrations
-          [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
-        else
-          "%.3d" % next_migration_number
-        end
       end
     end
   end
