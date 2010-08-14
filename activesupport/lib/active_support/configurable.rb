0d5a060710de0642d5e1538cd6a2de6a24bc77e0
Xavier Noria <fxn@hashref.com>
2010-08-14 18:35:01 +0200

    Revert "Add example label to activesupport/configurable"
    This reverts commit f480b2cea69247239f6fc2ad171b231595cc08c6.
    
    Reason: API guidelines discourage this, see http://edgeguides.rubyonrails.org/api_documentation_guidelines.html#example-code

--- activesupport/lib/active_support/configurable.rb
+++ activesupport/lib/active_support/configurable.rb
@@ -33,8 +33,6 @@ module ActiveSupport
 
     # Reads and writes attributes from a configuration <tt>OrderedHash</tt>.
     # 
-    # Example:
-    # 
     #   require 'active_support/configurable'      
     #  
     #   class User
