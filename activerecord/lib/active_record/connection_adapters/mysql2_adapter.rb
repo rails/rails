612c233a285a72e537143a61290fe271b0470723
Aaron Patterson <aaron.patterson@gmail.com>
2010-08-20 10:15:52 -0700

    adding FOUND_ROWS to the connect flags for mysql2

--- activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb
@@ -6,6 +6,11 @@ module ActiveRecord
   class Base
     def self.mysql2_connection(config)
       config[:username] = 'root' if config[:username].nil?
+
+      if Mysql2::Client.const_defined? :FOUND_ROWS
+        config[:flags] = Mysql2::Client::FOUND_ROWS
+      end
+
       client = Mysql2::Client.new(config.symbolize_keys)
       options = [config[:host], config[:username], config[:password], config[:database], config[:port], config[:socket], 0]
       ConnectionAdapters::Mysql2Adapter.new(client, logger, options, config)
