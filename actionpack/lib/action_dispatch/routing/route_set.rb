b1c66f060bba22d16abf6d24d3df762c240e367c
Piotr Sarnacki <drogus@gmail.com>
2010-08-25 13:41:56 +0200

    Move RoutesProxy to separate file

--- actionpack/lib/action_dispatch/routing/route_set.rb
+++ actionpack/lib/action_dispatch/routing/route_set.rb
@@ -261,38 +261,6 @@ module ActionDispatch
         named_routes.install(destinations, regenerate_code)
       end
 
-      class RoutesProxy
-        include ActionDispatch::Routing::UrlFor
-
-        attr_accessor :scope, :routes
-        alias :_routes :routes
-
-        def initialize(routes, scope)
-          @routes, @scope = routes, scope
-        end
-
-        def url_options
-          scope.send(:_with_routes, routes) do
-            scope.url_options
-          end
-        end
-
-        def method_missing(method, *args)
-          if routes.url_helpers.respond_to?(method)
-            self.class.class_eval <<-RUBY, __FILE__, __LINE__ + 1
-              def #{method}(*args)
-                options = args.extract_options!
-                args << url_options.merge((options || {}).symbolize_keys)
-                routes.url_helpers.#{method}(*args)
-              end
-            RUBY
-            send(method, *args)
-          else
-            super
-          end
-        end
-      end
-
       module MountedHelpers
       end
 
