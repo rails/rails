60de0e56b7f57f0052d7e4f43bd2ef9e363c6a1a
Joost Baaij <joost@spacebabies.nl>
2010-08-27 22:20:53 +0200

    call-seq directive makes rdoc cry and replaces method names with a curly brace; besides it doesn't add anything when you only list one call example since this is obviously documented already

--- actionpack/lib/action_controller/vendor/html-scanner/html/selector.rb
+++ actionpack/lib/action_controller/vendor/html-scanner/html/selector.rb
@@ -207,18 +207,12 @@ module HTML
 
     class << self
 
-      # :call-seq:
-      #   Selector.for_class(cls) => selector
-      #
       # Creates a new selector for the given class name.
       def for_class(cls)
         self.new([".?", cls])
       end
 
 
-      # :call-seq:
-      #   Selector.for_id(id) => selector
-      #
       # Creates a new selector for the given id.
       def for_id(id)
         self.new(["#?", id])
@@ -227,9 +221,6 @@ module HTML
     end
 
 
-    # :call-seq:
-    #   Selector.new(string, [values ...]) => selector
-    #
     # Creates a new selector from a CSS 2 selector expression.
     #
     # The first argument is the selector expression. All other arguments
@@ -342,9 +333,6 @@ module HTML
     end
 
 
-    # :call-seq:
-    #   match(element, first?) => array or nil
-    #
     # Matches an element against the selector.
     #
     # For a simple selector this method returns an array with the
@@ -434,9 +422,6 @@ module HTML
     end
 
 
-    # :call-seq:
-    #   select(root) => array
-    #
     # Selects and returns an array with all matching elements, beginning
     # with one node and traversing through all children depth-first.
     # Returns an empty array if no match is found.
