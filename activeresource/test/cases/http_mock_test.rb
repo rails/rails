676b5bf970b9ffc5a56fdc7b0b7489f199b3741d
Mikel Lindsaar <raasdnil@gmail.com>
2010-08-30 15:46:11 +1000

    Updating documentation on ActiveResource HTTP Mock and also adding test coverage

--- activeresource/test/cases/http_mock_test.rb
+++ activeresource/test/cases/http_mock_test.rb
@@ -72,6 +72,34 @@ class HttpMockTest < ActiveSupport::TestCase
     
   end
 
+  test "allows you to send in pairs directly to the respond_to method" do
+    matz  = { :id => 1, :name => "Matz" }.to_xml(:root => "person")
+    
+    create_matz = ActiveResource::Request.new(:post, '/people.xml', matz, {})
+    created_response = ActiveResource::Response.new("", 201, {"Location" => "/people/1.xml"})
+    get_matz = ActiveResource::Request.new(:get, '/people/1.xml', nil)
+    ok_response = ActiveResource::Response.new(matz, 200, {})
+    
+    pairs = {create_matz => created_response, get_matz => ok_response}
+    
+    ActiveResource::HttpMock.respond_to(pairs)
+    assert_equal 2, ActiveResource::HttpMock.responses.length
+    assert_equal "", ActiveResource::HttpMock.responses.assoc(create_matz)[1].body
+    assert_equal matz, ActiveResource::HttpMock.responses.assoc(get_matz)[1].body
+  end
+
+  test "resets all mocked responses on each call to respond_to by default" do
+    ActiveResource::HttpMock.respond_to do |mock|
+      mock.send(:get, "/people/1", {}, "XML1")
+    end
+    assert_equal 1, ActiveResource::HttpMock.responses.length
+
+    ActiveResource::HttpMock.respond_to do |mock|
+      mock.send(:get, "/people/2", {}, "XML2")
+    end
+    assert_equal 1, ActiveResource::HttpMock.responses.length
+  end
+
   def request(method, path, headers = {}, body = nil)
     if [:put, :post].include? method
       @http.send(method, path, body, headers)
