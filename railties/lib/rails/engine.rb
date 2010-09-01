a5e509d6b1a26f12fad65a69d01f450c69e136c1
Piotr Sarnacki <drogus@gmail.com>
2010-09-01 11:07:08 +0200

    We should avoid creating additional initializers when we can, adding them makes boot process slower

--- railties/lib/rails/engine.rb
+++ railties/lib/rails/engine.rb
@@ -441,11 +441,9 @@ module Rails
       require environment if environment
     end
 
-    initializer :default_asset_path do
-      config.asset_path = "/#{engine_name}%s" unless config.asset_path
-    end
-
     initializer :append_asset_paths do
+      config.asset_path = "/#{engine_name}%s" unless config.asset_path
+
       public_path = config.paths.public.to_a.first
       if config.compiled_asset_path && File.exist?(public_path)
         config.static_asset_paths[config.compiled_asset_path] = public_path
