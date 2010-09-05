88dfe16d9536b8de4cc69bbe649131840bca9a2c
Santiago Pastorino <santiago@wyeworks.com>
2010-09-05 10:06:09 -0300

    Fix code style a bit

--- actionpack/lib/action_view/helpers/number_helper.rb
+++ actionpack/lib/action_view/helpers/number_helper.rb
@@ -459,8 +459,7 @@ module ActionView
           raise ArgumentError, ":units must be a Hash or String translation scope."
         end.keys.map{|e_name| DECIMAL_UNITS.invert[e_name] }.sort_by{|e| -e}
 
-        number_exponent = 0
-        number_exponent = Math.log10(number).floor if number != 0
+        number_exponent = number != 0 ? Math.log10(number).floor : 0
         display_exponent = unit_exponents.find{|e| number_exponent >= e }
         number  /= 10 ** display_exponent
 
