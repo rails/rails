87b685680669d13bf9ce879fa0037784150e8b62
Aaron Patterson <aaron.patterson@gmail.com>
2010-09-06 17:39:09 -0700

    adding backwards compat methods

--- lib/arel/nodes/equality.rb
+++ lib/arel/nodes/equality.rb
@@ -1,6 +1,9 @@
 module Arel
   module Nodes
     class Equality < Arel::Nodes::Binary
+      def operator; :== end
+      alias :operand1 :left
+      alias :operand2 :right
     end
   end
 end
--- spec/arel/nodes/equality_spec.rb
+++ spec/arel/nodes/equality_spec.rb
@@ -1,6 +1,33 @@
 module Arel
   module Nodes
     describe 'equality' do
+      # FIXME: backwards compat
+      describe 'backwards compat' do
+        describe 'operator' do
+          it 'returns :==' do
+            attr = Table.new(:users)[:id]
+            left  = attr.eq(10)
+            check left.operator.should == :==
+          end
+        end
+
+        describe 'operand1' do
+          it "should equal left" do
+            attr = Table.new(:users)[:id]
+            left  = attr.eq(10)
+            check left.left.should == left.operand1
+          end
+        end
+
+        describe 'operand2' do
+          it "should equal right" do
+            attr = Table.new(:users)[:id]
+            left  = attr.eq(10)
+            check left.right.should == left.operand2
+          end
+        end
+      end
+
       describe 'or' do
         it 'makes an OR node' do
           attr = Table.new(:users)[:id]
