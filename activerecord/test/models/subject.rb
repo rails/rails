9859feb6a8f283fee9b601e19aaed75819e3949c
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2010-09-10 00:13:14 +0300

    fixed Subject model after_initialize callback for test_oracle_synonym test

--- activerecord/test/models/subject.rb
+++ activerecord/test/models/subject.rb
@@ -1,12 +1,16 @@
 # used for OracleSynonymTest, see test/synonym_test_oracle.rb
 #
 class Subject < ActiveRecord::Base
+
+  # added initialization of author_email_address in the same way as in Topic class
+  # as otherwise synonym test was failing
+  after_initialize :set_email_address
+
   protected
-    # added initialization of author_email_address in the same way as in Topic class
-    # as otherwise synonym test was failing
-    def after_initialize
+    def set_email_address
       if self.new_record?
         self.author_email_address = 'test@test.com'
       end
     end
+
 end
