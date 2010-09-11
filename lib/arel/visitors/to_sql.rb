8ce904933db3ec9884194f07881581ec0cc8281e
Emilio Tagua <miloops@gmail.com>
2010-09-11 11:13:04 +0800

    Visit ActiveSupport Multibyte Chars if ActiveSupport is defined.

--- lib/arel/visitors/to_sql.rb
+++ lib/arel/visitors/to_sql.rb
@@ -194,6 +194,7 @@ module Arel
 
       def visit_TrueClass o; quote(o) end
       def visit_String o; quote(o) end
+      def visit_ActiveSupport_Multibyte_Chars o; quote(o) end if defined?(ActiveSupport)
       def visit_Symbol o; quote(o) end
       def visit_Time o; quote(o) end
       def visit_Date o; quote(o) end
