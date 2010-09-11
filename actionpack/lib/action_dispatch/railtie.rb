8b66c91c51bfc163fa8c2af019a1a6f64c3e6a53
Mikel Lindsaar <raasdnil@gmail.com>
2010-09-11 20:45:40 +1000

    Partial revert of commit #7fae0aa4ac116 removing action_dispatch.prepare_dispatcher per commit #48bf667a8b1
    See line note:
    http://github.com/rails/rails/commit/7fae0aa4ac116bcbf522a9a4f19972c653d366fa#commitcomment-145148

--- actionpack/lib/action_dispatch/railtie.rb
+++ actionpack/lib/action_dispatch/railtie.rb
@@ -10,11 +10,6 @@ module ActionDispatch
     config.action_dispatch.best_standards_support = true
     config.action_dispatch.tld_length = 1
 
-    # Prepare dispatcher callbacks and run 'prepare' callbacks
-    initializer "action_dispatch.prepare_dispatcher" do |app|
-      ActionDispatch::Callbacks.to_prepare { app.routes_reloader.execute_if_updated }
-    end
-
     initializer "action_dispatch.configure" do |app|
       ActionDispatch::Http::URL.tld_length = app.config.action_dispatch.tld_length
     end
