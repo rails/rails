b9402889ff4cc6fc18ad0d035fecc8e36f320f90
Emilio Tagua <miloops@gmail.com>
2010-09-11 11:28:00 +0800

    Add maximum to SqlLiteral.

--- lib/arel/nodes/sql_literal.rb
+++ lib/arel/nodes/sql_literal.rb
@@ -4,6 +4,10 @@ module Arel
       def count distinct = false
         Count.new [self], distinct
       end
+
+      def maximum
+        Nodes::Max.new [self], Nodes::SqlLiteral.new('max_id')
+      end
     end
   end
 end
