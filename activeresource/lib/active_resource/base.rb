ec13ce9c4895916420cfc156c10519f5ae914fb6
Gaston Ramos <ramos.gaston@gmail.com>
2010-09-13 17:14:37 -0300

    - refactoring, move prefix_options check to a custom method

--- activeresource/lib/active_resource/base.rb
+++ activeresource/lib/active_resource/base.rb
@@ -621,11 +621,7 @@ module ActiveResource
       #   # => /posts/5/comments/1.xml?active=1
       #
       def element_path(id, prefix_options = {}, query_options = nil)
-
-        p_options = HashWithIndifferentAccess.new(prefix_options)
-        prefix_parameters.each do |p|
-          raise MissingPrefixParam if p_options[p].blank?
-        end
+        check_prefix_options(prefix_options)
 
         prefix_options, query_options = split_options(prefix_options) if query_options.nil?
         "#{prefix(prefix_options)}#{collection_name}/#{URI.escape id.to_s}.#{format.extension}#{query_string(query_options)}"
@@ -848,6 +844,14 @@ module ActiveResource
       end
 
       private
+
+        def check_prefix_options(prefix_options)
+          p_options = HashWithIndifferentAccess.new(prefix_options)
+          prefix_parameters.each do |p|
+            raise(MissingPrefixParam, "#{p} prefix_option is missing") if p_options[p].blank?
+          end
+        end
+
         # Find every resource
         def find_every(options)
           begin
