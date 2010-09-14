5d11fa9f6a850ac4374a01e402740ca8a7b50f32
Aaron Patterson <aaron.patterson@gmail.com>
2010-09-14 15:17:09 -0700

    adding sum to sql literal

--- lib/arel/nodes/sql_literal.rb
+++ lib/arel/nodes/sql_literal.rb
@@ -5,6 +5,10 @@ module Arel
         Count.new [self], distinct
       end
 
+      def sum
+        Nodes::Sum.new [self], Nodes::SqlLiteral.new('sum_id')
+      end
+
       def maximum
         Nodes::Max.new [self], Nodes::SqlLiteral.new('max_id')
       end
