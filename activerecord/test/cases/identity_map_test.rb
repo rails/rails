6b0b95f1bd92e2ef35573cb59e8a14bd3ffb3777
Emilio Tagua <miloops@gmail.com>
2010-09-15 11:30:15 -0300

    Add test for update_attributes and identity map.

--- activerecord/test/cases/identity_map_test.rb
+++ activerecord/test/cases/identity_map_test.rb
@@ -274,4 +274,17 @@ class IdentityMapTest < ActiveRecord::TestCase
     assert_not_equal developer.salary, same_developer.salary
   end
 
+  def test_reload_object_if_update_attributes_fails
+    developer = Developer.first
+    developer.salary = 0
+
+    assert !developer.update_attributes(:salary => 0)
+
+    same_developer = Developer.first
+
+    assert_not_same  developer, same_developer
+    assert_not_equal 0, same_developer.salary
+    assert_not_equal developer.salary, same_developer.salary
+  end
+
 end
