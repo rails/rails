a3210d90be844b306bbbf3324c7ad1ad389a14b9
Emilio Tagua <miloops@gmail.com>
2010-09-17 16:46:56 -0300

    implicit_readonly is not set until records are loaded, just check readonly_value and then set readonly status.

--- activerecord/lib/active_record/relation.rb
+++ activerecord/lib/active_record/relation.rb
@@ -61,9 +61,7 @@ module ActiveRecord
     def to_a
       return @records if loaded?
 
-      readonly = @readonly_value.nil? ? @implicit_readonly : @readonly_value
-
-      @records = if readonly
+      @records = if @readonly_value
         IdentityMap.without do
           eager_loading? ? find_with_associations : @klass.find_by_sql(arel.to_sql, @bind_values)
         end
@@ -77,6 +75,7 @@ module ActiveRecord
 
       # @readonly_value is true only if set explicitly. @implicit_readonly is true if there
       # are JOINS and no explicit SELECT.
+      readonly = @readonly_value.nil? ? @implicit_readonly : @readonly_value
       @records.each { |record| record.readonly! } if readonly
 
       @loaded = true
