9e5a4423cdbbe161dade56306d7c4d70f5371be2
Aaron Patterson <aaron.patterson@gmail.com>
2010-09-20 15:59:52 -0700

    removing some debugging code.  oops!

--- lib/arel/nodes/in.rb
+++ lib/arel/nodes/in.rb
@@ -1,10 +1,6 @@
 module Arel
   module Nodes
     class In < Equality
-      def initialize left, right
-        raise if Arel::SelectManager === right
-        super
-      end
     end
   end
 end
