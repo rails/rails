10a63e2a0cbdc1e8e4d7aef87afc868bd599ca94
Emilio Tagua <miloops@gmail.com>
2010-09-22 11:36:09 -0300

    Refactor methods in html node to avoid injects.
    Signed-off-by: Santiago Pastorino <santiago@wyeworks.com>

--- actionpack/lib/action_controller/vendor/html-scanner/html/node.rb
+++ actionpack/lib/action_controller/vendor/html-scanner/html/node.rb
@@ -38,18 +38,14 @@ module HTML #:nodoc:
     private
 
       def keys_to_strings(hash)
-        hash.keys.inject({}) do |h,k|
-          h[k.to_s] = hash[k]
-          h
-        end
+        Hash[hash.keys.map {|k| [k.to_s, hash[k]]}]
       end
 
       def keys_to_symbols(hash)
-        hash.keys.inject({}) do |h,k|
+        Hash[hash.keys.map do |k|
           raise "illegal key #{k.inspect}" unless k.respond_to?(:to_sym)
-          h[k.to_sym] = hash[k]
-          h
-        end
+          [k.to_sym, hash[k]]
+        end]
       end
   end
 
