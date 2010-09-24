c21fcdcf837f43ae61f2be7592d8aab001c51b25
Aaron Patterson <aaron.patterson@gmail.com>
2010-09-24 16:26:24 -0700

    deleting lots of unused files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,27 +5,14 @@ require 'arel'
 
 require 'support/matchers/be_like'
 require 'support/check'
-Dir[File.join(File.dirname(__FILE__),'support/shared/*')].each { |f| require f }
-
-if adapter = ENV['ADAPTER']
-  require "support/connections/#{adapter}_connection.rb"
-end
+require 'support/fake_record'
+require 'support/shared/tree_manager_shared'
 
 Spec::Runner.configure do |config|
   config.include Matchers
   config.include Check
 
-  if defined?(ActiveRecord::Base)
-    tmp = File.expand_path('../../tmp', __FILE__)
-
-    FileUtils.mkdir_p(tmp)
-    ActiveRecord::Base.logger = Logger.new("#{tmp}/debug.log")
-    ActiveRecord::Base.establish_connection("unit")
-
-    require "support/schemas/#{ENV['ADAPTER']}_schema.rb"
-
-    config.before do
-      Arel::Table.engine = Arel::Sql::Engine.new(ActiveRecord::Base)
-    end
+  config.before do
+    Arel::Table.engine = Arel::Sql::Engine.new(FakeRecord::Base)
   end
 end
