a0db7be4aff6fc6ebf67bbbd441c47d678bd5195
Aaron Patterson <aaron.patterson@gmail.com>
2010-09-24 14:48:25 -0700

    attr_accessor_with_default should raise an ArgumentError not a RuntimeError

--- activesupport/lib/active_support/core_ext/module/attr_accessor_with_default.rb
+++ activesupport/lib/active_support/core_ext/module/attr_accessor_with_default.rb
@@ -18,9 +18,8 @@ class Module
   #
   #   attr_accessor_with_default(:element_name) { name.underscore }
   #
-  def attr_accessor_with_default(sym, default = nil, &block)
-    raise 'Default value or block required' unless !default.nil? || block
-    define_method(sym, block_given? ? block : Proc.new { default })
+  def attr_accessor_with_default(sym, default = Proc.new)
+    define_method(sym, block_given? ? default : Proc.new { default })
     module_eval(<<-EVAL, __FILE__, __LINE__ + 1)
       def #{sym}=(value)                        # def age=(value)
         class << self; attr_reader :#{sym} end  #   class << self; attr_reader :age end
