1b7d30bfc925562cffff10db2a0388770e9bc06c
Krekoten' Marjan <krekoten@gmail.com>
2010-09-25 10:55:22 +0300

    Fix 'warning: method redefined' [#5551 state:resolved]
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- activesupport/lib/active_support/core_ext/module/attr_accessor_with_default.rb
+++ activesupport/lib/active_support/core_ext/module/attr_accessor_with_default.rb
@@ -21,10 +21,10 @@ class Module
   def attr_accessor_with_default(sym, default = Proc.new)
     define_method(sym, block_given? ? default : Proc.new { default })
     module_eval(<<-EVAL, __FILE__, __LINE__ + 1)
-      def #{sym}=(value)                        # def age=(value)
-        class << self; attr_reader :#{sym} end  #   class << self; attr_reader :age end
-        @#{sym} = value                         #   @age = value
-      end                                       # end
+      def #{sym}=(value)                          # def age=(value)
+        class << self; attr_accessor :#{sym} end  #   class << self; attr_accessor :age end
+        @#{sym} = value                           #   @age = value
+      end                                         # end
     EVAL
   end
 end
