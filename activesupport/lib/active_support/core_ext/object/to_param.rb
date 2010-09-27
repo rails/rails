rails/rails

commit 5c858220085dc4ddc1bec496747059dfbe32f1da

Hash#to_param is doesn't use sort anymore, some tests added for Hash#to_param

activesupport/lib/active_support/core_ext/object/to_param.rb
@@ -44,6 +44,6 @@ class Hash
   def to_param(namespace = nil)
     collect do |key, value|
       value.to_query(namespace ? "#{namespace}[#{key}]" : key)
-    end.sort * '&'
+    end * '&'
   end
 end

activesupport/test/core_ext/hash_ext_test.rb
@@ -2,6 +2,8 @@ require 'abstract_unit'
 require 'active_support/core_ext/hash'
 require 'bigdecimal'
 require 'active_support/core_ext/string/access'
+require 'active_support/ordered_hash'
+require 'active_support/core_ext/object/conversions'
 
 class HashExtTest < Test::Unit::TestCase
   def setup
@@ -446,6 +448,29 @@ class IWriteMyOwnXML
     xml.level_one do
       xml.tag!(:second_level, 'content')
     end
+  end
+end
+
+class HashExtToParamTests < Test::Unit::TestCase
+  class ToParam < String
+    def to_param
+      "#{self}-1"
+    end
+  end
+
+  def test_string_hash
+    assert_equal '', {}.to_param
+    assert_equal 'hello=world', { :hello => "world" }.to_param
+    assert_equal 'hello=10', { "hello" => 10 }.to_param
+    assert_equal 'hello=world&say_bye=true', ActiveSupport::OrderedHash[:hello, "world", "say_bye", true].to_param
+  end
+
+  def test_number_hash
+    assert_equal '10=20&30=40&50=60', ActiveSupport::OrderedHash[10, 20, 30, 40, 50, 60].to_param
+  end
+
+  def test_to_param_hash
+    assert_equal 'custom=param-1&custom2=param2-1', ActiveSupport::OrderedHash[ToParam.new('custom'), ToParam.new('param'), ToParam.new('custom2'), ToParam.new('param2')].to_param
   end
 end
 

commit 10dec0e65e1f4d87f411b4361045eba86b121be9

let Hash#to_param and Hash#to_query sort again
This was a regression introduced in 5c858220085dc4ddc1bec496747059dfbe32f1da. We bring
sorting back because people rely on it, eg for constructing consistent cache keys.

activesupport/lib/active_support/core_ext/object/to_param.rb
@@ -35,7 +35,8 @@ end
 
 class Hash
   # Converts a hash into a string suitable for use as a URL query string. An optional <tt>namespace</tt> can be
-  # passed to enclose the param names (see example below).
+  # passed to enclose the param names (see example below). The keys in the query string are sorted lexicographically
+  # in ascending order.
   #
   # ==== Examples
   #   { :name => 'David', :nationality => 'Danish' }.to_param # => "name=David&nationality=Danish"
@@ -44,6 +45,6 @@ class Hash
   def to_param(namespace = nil)
     collect do |key, value|
       value.to_query(namespace ? "#{namespace}[#{key}]" : key)
-    end * '&'
+    end.sort * '&'
   end
 end

activesupport/test/core_ext/hash_ext_test.rb
@@ -462,19 +462,23 @@ class HashExtToParamTests < Test::Unit::TestCase
     assert_equal '', {}.to_param
     assert_equal 'hello=world', { :hello => "world" }.to_param
     assert_equal 'hello=10', { "hello" => 10 }.to_param
-    assert_equal 'hello=world&say_bye=true', ActiveSupport::OrderedHash[:hello, "world", "say_bye", true].to_param
+    assert_equal 'hello=world&say_bye=true', {:hello => "world", "say_bye" => true}.to_param
   end
 
   def test_number_hash
-    assert_equal '10=20&30=40&50=60', ActiveSupport::OrderedHash[10, 20, 30, 40, 50, 60].to_param
+    assert_equal '10=20&30=40&50=60', {10 => 20, 30 => 40, 50 => 60}.to_param
   end
 
   def test_to_param_hash
-    assert_equal 'custom=param-1&custom2=param2-1', ActiveSupport::OrderedHash[ToParam.new('custom'), ToParam.new('param'), ToParam.new('custom2'), ToParam.new('param2')].to_param
+    assert_equal 'custom2=param2-1&custom=param-1', {ToParam.new('custom') => ToParam.new('param'), ToParam.new('custom2') => ToParam.new('param2')}.to_param
   end
 
   def test_to_param_hash_escapes_its_keys_and_values
     assert_equal 'param+1=A+string+with+%2F+characters+%26+that+should+be+%3F+escaped', { 'param 1' => 'A string with / characters & that should be ? escaped' }.to_param
+  end
+  
+  def test_to_param_orders_by_key_in_ascending_order
+    assert_equal 'a=2&b=1&c=0', ActiveSupport::OrderedHash[*%w(b 1 c 0 a 2)].to_param
   end
 end
 