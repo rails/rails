95d53054dee164c1c988ffc0549143e6c16412bf
Gaston Ramos <ramos.gaston@gmail.com>
2010-09-27 20:47:58 -0300

    - fix ActiveResource has_one association   resource_id column is on the associated resource (belogns_to) - refactoring association test cases

--- activeresource/lib/active_resource/associations.rb
+++ activeresource/lib/active_resource/associations.rb
@@ -4,10 +4,12 @@ module ActiveResource
     def hash_options(association, resource)
       h = { :klass => klass_for(association, resource) }
       h[:host_klass]      = self
-      if association == :has_many
+      case association
+        when :has_many
         h[:association_col] = "#{h[:host_klass].to_s.downcase}_id".to_sym
-      else
-        h[:association_col] = "#{h[:klass].to_s.underscore}_id".to_sym
+
+        when :has_one
+        h[:association_col] = "#{h[:host_klass].to_s.underscore}_id".to_sym
       end
       h
     end
@@ -27,11 +29,9 @@ module ActiveResource
       #
       #----------------------------------------------------------------------#
       define_method(klass_name) do
-        resource_id = send(h[:association_col])
-
         if instance_variable_get("@#{resource}").nil?
           instance_variable_set("@#{resource}",
-                                h[:klass].find(resource_id) )
+                                h[:klass].find(:first, :params => { h[:association_col] => id }) )
         end
         return instance_variable_get("@#{resource}")
       end
