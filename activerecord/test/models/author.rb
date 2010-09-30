14c4881f9c7bf4eae61e548542ee309c013e1fca
Jon Leighton <j@jonathanleighton.com>
2010-09-30 22:01:03 +0100

    Prevent test_has_many_through_a_has_many_through_association_on_through_reflection failing for me due to ordering of the results

--- activerecord/test/models/author.rb
+++ activerecord/test/models/author.rb
@@ -84,14 +84,14 @@ class Author < ActiveRecord::Base
   has_many :favorite_authors, :through => :author_favorites, :order => 'name'
 
   has_many :tagging,         :through => :posts # through polymorphic has_one
-  has_many :taggings,        :through => :posts, :source => :taggings # through polymorphic has_many
+  has_many :taggings,        :through => :posts, :source => :taggings # through polymorphic has_many TODO: Why is the :source needed?
   has_many :tags,            :through => :posts # through has_many :through (on source reflection + polymorphic)
   has_many :distinct_tags,   :through => :posts, :source => :tags, :select => "DISTINCT tags.*", :order => "tags.name"
   has_many :post_categories, :through => :posts, :source => :categories
 
   has_many :books
   has_many :subscriptions,        :through => :books
-  has_many :subscribers,          :through => :subscriptions # through has_many :through (on through reflection)
+  has_many :subscribers,          :through => :subscriptions, :order => "subscribers.nick" # through has_many :through (on through reflection)
   has_many :distinct_subscribers, :through => :subscriptions, :source => :subscriber, :select => "DISTINCT subscribers.*", :order => "subscribers.nick"
 
   has_one :essay, :primary_key => :name, :as => :writer
