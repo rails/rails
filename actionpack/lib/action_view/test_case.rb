f656796d05715174568536cfe119a3959a020f23
David Chelimsky <dchelimsky@gmail.com>
2010-10-02 12:35:17 -0500

    Rename _assigns to view_assigns in AV::TC
    - also add tests
    - also deprecate _assigns
    
    [#5751 state:resolved]
    
    Signed-off-by: Santiago Pastorino <santiago@wyeworks.com>

--- actionpack/lib/action_view/test_case.rb
+++ actionpack/lib/action_view/test_case.rb
@@ -103,7 +103,7 @@ module ActionView
       end
 
       def render(options = {}, local_assigns = {}, &block)
-        view.assign(_assigns)
+        view.assign(view_assigns)
         @rendered << output = view.render(options, local_assigns, &block)
         output
       end
@@ -169,15 +169,19 @@ module ActionView
 
       alias_method :_view, :view
 
-      EXCLUDE_IVARS = %w{
+      INTERNAL_IVARS = %w{
+        @__name__
         @_assertion_wrapped
+        @_assertions
         @_result
+        @_routes
         @controller
         @layouts
         @locals
         @method_name
         @output_buffer
         @partials
+        @passed
         @rendered
         @request
         @routes
@@ -187,16 +191,24 @@ module ActionView
         @view_context_class
       }
 
-      def _instance_variables
-        instance_variables.map(&:to_s) - EXCLUDE_IVARS
+      def _user_defined_ivars
+        instance_variables.map(&:to_s) - INTERNAL_IVARS
+      end
+
+      # Returns a Hash of instance variables and their values, as defined by
+      # the user in the test case, which are then assigned to the view being
+      # rendered. This is generally intended for internal use and extension
+      # frameworks.
+      def view_assigns
+        _user_defined_ivars.inject({}) do |hash, var|
+          hash.merge(var.sub('@','').to_sym => instance_variable_get(var))
+        end
       end
 
       def _assigns
-        _instance_variables.inject({}) do |hash, var|
-          name = var[1..-1].to_sym
-          hash[name] = instance_variable_get(var)
-          hash
-        end
+        ActiveSupport::Deprecation.warn "ActionView::TestCase#_assigns is deprecated and will be removed in future versions. " <<
+          "Please use view_assigns instead."
+        view_assigns
       end
 
       def _routes
