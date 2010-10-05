8a9747021085c569f0118db1093bc12cfa2ba915
Aaron Patterson <aaron.patterson@gmail.com>
2010-10-04 17:08:25 -0700

    raising an argument error if tempfile is not provided

--- actionpack/test/dispatch/uploaded_file_test.rb
+++ actionpack/test/dispatch/uploaded_file_test.rb
@@ -2,18 +2,24 @@ require 'abstract_unit'
 
 module ActionDispatch
   class UploadedFileTest < ActiveSupport::TestCase
+    def test_constructor_with_argument_error
+      assert_raises(ArgumentError) do
+        Http::UploadedFile.new({})
+      end
+    end
+
     def test_original_filename
-      uf = Http::UploadedFile.new(:filename => 'foo')
+      uf = Http::UploadedFile.new(:filename => 'foo', :tempfile => Object.new)
       assert_equal 'foo', uf.original_filename
     end
 
     def test_content_type
-      uf = Http::UploadedFile.new(:type => 'foo')
+      uf = Http::UploadedFile.new(:type => 'foo', :tempfile => Object.new)
       assert_equal 'foo', uf.content_type
     end
 
     def test_headers
-      uf = Http::UploadedFile.new(:head => 'foo')
+      uf = Http::UploadedFile.new(:head => 'foo', :tempfile => Object.new)
       assert_equal 'foo', uf.headers
     end
 
