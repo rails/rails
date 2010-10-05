3cc35633872a0072764d8edb20f1fc4e14adf729
Jon Leighton <j@jonathanleighton.com>
2010-10-05 20:20:27 +0100

    A failing test for a nested has many through association loaded via Foo.joins(:bar)

--- activerecord/test/cases/associations/nested_has_many_through_associations_test.rb
+++ activerecord/test/cases/associations/nested_has_many_through_associations_test.rb
@@ -45,4 +45,10 @@ class NestedHasManyThroughAssociationsTest < ActiveRecord::TestCase
     author = authors(:bob)
     assert_equal [posts(:misc_by_bob), posts(:misc_by_mary)], author.similar_posts.sort_by(&:id)
   end
+  
+  def test_nested_has_many_through_as_a_join
+    # All authors with subscribers where one of the subscribers' nick is 'alterself'
+    authors = Author.joins(:subscribers).where('subscribers.nick' => 'alterself')
+    assert_equal [authors(:david)], authors
+  end
 end
