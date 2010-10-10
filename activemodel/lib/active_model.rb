83d5180bec0f586bf0e88c61b5eaf006c8658547
wycats <wycats@gmail.com>
2010-10-10 16:11:04 -0700

    Always pull in version for frameworks (standardize autoload / require / none)

--- activemodel/lib/active_model.rb
+++ activemodel/lib/active_model.rb
@@ -24,7 +24,7 @@
 activesupport_path = File.expand_path('../../../activesupport/lib', __FILE__)
 $:.unshift(activesupport_path) if File.directory?(activesupport_path) && !$:.include?(activesupport_path)
 require 'active_support'
-
+require 'active_model/version'
 
 module ActiveModel
   extend ActiveSupport::Autoload
@@ -46,7 +46,6 @@ module ActiveModel
   autoload :Serialization
   autoload :TestCase
   autoload :Translation
-  autoload :VERSION
   autoload :Validations
   autoload :Validator
 
