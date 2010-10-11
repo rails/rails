ee7863eaace9e1680108282cede4736fcc303187
Aaron Patterson <aaron.patterson@gmail.com>
2010-10-11 11:30:22 -0700

    removing some load path tomfoolery

--- actionpack/lib/action_view.rb
+++ actionpack/lib/action_view.rb
@@ -21,9 +21,6 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #++
 
-activesupport_path = File.expand_path('../../../activesupport/lib', __FILE__)
-$:.unshift(activesupport_path) if File.directory?(activesupport_path) && !$:.include?(activesupport_path)
-
 require 'active_support/ruby/shim'
 require 'active_support/core_ext/class/attribute_accessors'
 
