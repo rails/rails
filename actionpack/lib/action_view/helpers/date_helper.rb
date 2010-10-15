1b66a646db9aad9e9bb18df967eb29cd1eb6453b
Aditya Sanghi <asanghi@me.com>
2010-10-15 22:43:01 +0530

    patching to ensure separator is printed with order [#5816 state:resolved]
    Signed-off-by: Santiago Pastorino <santiago@wyeworks.com>

--- actionpack/lib/action_view/helpers/date_helper.rb
+++ actionpack/lib/action_view/helpers/date_helper.rb
@@ -893,6 +893,8 @@ module ActionView
         # Returns the separator for a given datetime component
         def separator(type)
           case type
+            when :year
+              @options[:discard_year] ? "" : @options[:date_separator]
             when :month
               @options[:discard_month] ? "" : @options[:date_separator]
             when :day
