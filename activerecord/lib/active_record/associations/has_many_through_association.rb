fcabfa428e57af115aca56f5c9aba99afae2cf7c
Jon Leighton <j@jonathanleighton.com>
2010-10-19 18:43:09 +0100

    Remove obsolete require to active_record/associations/nested_has_many_through

--- activerecord/lib/active_record/associations/has_many_through_association.rb
+++ activerecord/lib/active_record/associations/has_many_through_association.rb
@@ -1,5 +1,4 @@
 require "active_record/associations/through_association_scope"
-require "active_record/associations/nested_has_many_through"
 require 'active_support/core_ext/object/blank'
 
 module ActiveRecord
