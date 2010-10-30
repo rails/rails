7d5762d2c2147f8240614997682e5e535a3e4f33
Aaron Patterson <aaron.patterson@gmail.com>
2010-10-30 12:28:49 -0700

    no need to merge where values if no new where values have been added

--- activerecord/lib/active_record/relation/spawn_methods.rb
+++ activerecord/lib/active_record/relation/spawn_methods.rb
@@ -28,17 +28,19 @@ module ActiveRecord
 
       merged_wheres = @where_values + r.where_values
 
-      # Remove duplicates, last one wins.
-      seen = {}
-      merged_wheres = merged_wheres.reverse.reject { |w|
-        nuke = false
-        if w.respond_to?(:operator) && w.operator == :==
-          name       = w.left.name
-          nuke       = seen[name]
-          seen[name] = true
-        end
-        nuke
-      }.reverse
+      unless @where_values.empty?
+        # Remove duplicates, last one wins.
+        seen = {}
+        merged_wheres = merged_wheres.reverse.reject { |w|
+          nuke = false
+          if w.respond_to?(:operator) && w.operator == :==
+            name       = w.left.name
+            nuke       = seen[name]
+            seen[name] = true
+          end
+          nuke
+        }.reverse
+      end
 
       merged_relation.where_values = merged_wheres
 
