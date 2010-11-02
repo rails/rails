c5c2e4fefde539e1c1753d4ca184c3bf81d496be
Pratik Naik <pratiknaik@gmail.com>
2010-11-02 01:23:58 +0000

    Add timestamps to Topic

--- activerecord/test/schema/schema.rb
+++ activerecord/test/schema/schema.rb
@@ -552,6 +552,7 @@ ActiveRecord::Schema.define do
     t.string   :parent_title
     t.string   :type
     t.string   :group
+    t.timestamps
   end
 
   create_table :toys, :primary_key => :toy_id ,:force => true do |t|
