6b3f521b806614a073e4418307b56241fe287940
Carl Lerche <me@carllerche.com>
2010-11-16 15:45:31 -0800

    Remove some useless comments from the release task

--- tasks/release.rb
+++ tasks/release.rb
@@ -69,17 +69,3 @@ namespace :all do
   task :install => FRAMEWORKS.map { |f| "#{f}:install" } + ['rails:install']
   task :push    => FRAMEWORKS.map { |f| "#{f}:push"    } + ['rails:push']
 end
-
-__END__
-version = ARGV.pop
-
-%w( activesupport activemodel activerecord activeresource actionpack actionmailer railties ).each do |framework|
-  puts "Building and pushing #{framework}..."
-  `cd #{framework} && gem build #{framework}.gemspec && gem push #{framework}-#{version}.gem && rm #{framework}-#{version}.gem`
-end
-
-puts "Building and pushing Rails..."
-`gem build rails.gemspec`
-`gem push rails-#{version}.gem`
-`rm rails-#{version}.gem`
-
