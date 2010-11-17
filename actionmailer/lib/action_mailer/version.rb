6eab52d507324378d4608a9b723a1a40bc6463ff
David Heinemeier Hansson <david@loudthinking.com>
2010-11-16 21:39:45 -0600

    Preparing for 3.0.4.rc release

--- actionmailer/lib/action_mailer/version.rb
+++ actionmailer/lib/action_mailer/version.rb
@@ -2,8 +2,8 @@ module ActionMailer
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 0
-    TINY  = 3
-    PRE   = nil
+    TINY  = 4
+    PRE   = "rc"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
