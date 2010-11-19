861cdc4c59dabdc8cca42a9f29b7cccb3cd23505
Emilio Tagua <miloops@gmail.com>
2010-11-20 07:48:49 +0800

    Remove unneeded local var.

--- activerecord/lib/active_record/nested_attributes.rb
+++ activerecord/lib/active_record/nested_attributes.rb
@@ -446,8 +446,7 @@ module ActiveRecord
     end
 
     def raise_nested_attributes_record_not_found(association_name, record_id)
-      reflection = self.class.reflect_on_association(association_name)
-      raise RecordNotFound, "Couldn't find #{reflection.klass.name} with ID=#{record_id} for #{self.class.name} with ID=#{id}"
+      raise RecordNotFound, "Couldn't find #{self.class.reflect_on_association(association_name).klass.name} with ID=#{record_id} for #{self.class.name} with ID=#{id}"
     end
   end
 end
