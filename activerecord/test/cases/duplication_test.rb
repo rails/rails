28f4df0908180783f8cf863a8bbb30c1c139bd8d
Aaron Patterson <aaron.patterson@gmail.com>
2010-11-23 11:10:56 -0800

    testing duped attributes are independent

--- activerecord/test/cases/duplication_test.rb
+++ activerecord/test/cases/duplication_test.rb
@@ -41,6 +41,13 @@ module ActiveRecord
       assert cloned.frozen?, 'topic should be frozen'
     end
 
+    def test_clone_is_shallow
+      topic = Topic.first
+      cloned = topic.clone
+      topic.author_name = 'Aaron'
+      assert_equal 'Aaron', cloned.author_name
+    end
+
     def test_dup_with_modified_attributes
       topic = Topic.first
       topic.author_name = 'Aaron'
@@ -63,6 +70,17 @@ module ActiveRecord
       duped.author_name = 'meow'
 
       assert_not_equal topic.changes, duped.changes
+    end
+
+    def test_dup_attributes_are_independent
+      topic = Topic.first
+      duped = topic.dup
+
+      duped.author_name = 'meow'
+      topic.author_name = 'Aaron'
+
+      assert_equal 'Aaron', topic.author_name
+      assert_equal 'meow', duped.author_name
     end
   end
 end
