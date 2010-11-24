57c7ed082053a9eeeb521aba5bbd558ea9c0b5c1
John Firebaugh <john_firebaugh@us.ibm.com>
2010-11-23 18:04:05 -0800

    Introduce ActionDispatch::Reloader
    Based on the implementation on the 2-3-stable branch, patches by Hongli
    Lai <hongli@phusion.nl>, and helpful suggestions from José Valim.
    
    Hongli Lai's patches included locking around the request cycle; this is
    now handled by Rack::Lock (https://github.com/rack/rack/issues/issue/87/).
    
    [#2873]

--- railties/lib/rails/application.rb
+++ railties/lib/rails/application.rb
@@ -156,6 +156,7 @@ module Rails
         middleware.use ::ActionDispatch::ShowExceptions, config.consider_all_requests_local if config.action_dispatch.show_exceptions
         middleware.use ::ActionDispatch::RemoteIp, config.action_dispatch.ip_spoofing_check, config.action_dispatch.trusted_proxies
         middleware.use ::Rack::Sendfile, config.action_dispatch.x_sendfile_header
+        middleware.use ::ActionDispatch::Reloader unless config.cache_classes
         middleware.use ::ActionDispatch::Callbacks, !config.cache_classes
         middleware.use ::ActionDispatch::Cookies
 
