90b0f6149160e6fedb12ea54f713ec3399bebec8
José Valim <jose.valim@gmail.com>
2010-11-25 10:44:42 +0100

    #:nodoc: internal methods.

--- actionpack/lib/action_dispatch/routing/mapper.rb
+++ actionpack/lib/action_dispatch/routing/mapper.rb
@@ -707,61 +707,61 @@ module ActionDispatch
         end
 
         private
-          def scope_options
+          def scope_options #:nodoc:
             @scope_options ||= private_methods.grep(/^merge_(.+)_scope$/) { $1.to_sym }
           end
 
-          def merge_path_scope(parent, child)
+          def merge_path_scope(parent, child) #:nodoc:
             Mapper.normalize_path("#{parent}/#{child}")
           end
 
-          def merge_shallow_path_scope(parent, child)
+          def merge_shallow_path_scope(parent, child) #:nodoc:
             Mapper.normalize_path("#{parent}/#{child}")
           end
 
-          def merge_as_scope(parent, child)
+          def merge_as_scope(parent, child) #:nodoc:
             parent ? "#{parent}_#{child}" : child
           end
 
-          def merge_shallow_prefix_scope(parent, child)
+          def merge_shallow_prefix_scope(parent, child) #:nodoc:
             parent ? "#{parent}_#{child}" : child
           end
 
-          def merge_module_scope(parent, child)
+          def merge_module_scope(parent, child) #:nodoc:
             parent ? "#{parent}/#{child}" : child
           end
 
-          def merge_controller_scope(parent, child)
+          def merge_controller_scope(parent, child) #:nodoc:
             child
           end
 
-          def merge_path_names_scope(parent, child)
+          def merge_path_names_scope(parent, child) #:nodoc:
             merge_options_scope(parent, child)
           end
 
-          def merge_constraints_scope(parent, child)
+          def merge_constraints_scope(parent, child) #:nodoc:
             merge_options_scope(parent, child)
           end
 
-          def merge_defaults_scope(parent, child)
+          def merge_defaults_scope(parent, child) #:nodoc:
             merge_options_scope(parent, child)
           end
 
-          def merge_blocks_scope(parent, child)
+          def merge_blocks_scope(parent, child) #:nodoc:
             merged = parent ? parent.dup : []
             merged << child if child
             merged
           end
 
-          def merge_options_scope(parent, child)
+          def merge_options_scope(parent, child) #:nodoc:
             (parent || {}).except(*override_keys(child)).merge(child)
           end
 
-          def merge_shallow_scope(parent, child)
+          def merge_shallow_scope(parent, child) #:nodoc:
             child ? true : false
           end
 
-          def override_keys(child)
+          def override_keys(child) #:nodoc:
             child.key?(:only) || child.key?(:except) ? [:only, :except] : []
           end
       end
@@ -1167,7 +1167,7 @@ module ActionDispatch
             @scope[:scope_level_resource]
           end
 
-          def apply_common_behavior_for(method, resources, options, &block)
+          def apply_common_behavior_for(method, resources, options, &block) #:nodoc:
             if resources.length > 1
               resources.each { |r| send(method, r, options, &block) }
               return true
@@ -1197,23 +1197,23 @@ module ActionDispatch
             false
           end
 
-          def action_options?(options)
+          def action_options?(options) #:nodoc:
             options[:only] || options[:except]
           end
 
-          def scope_action_options?
+          def scope_action_options? #:nodoc:
             @scope[:options].is_a?(Hash) && (@scope[:options][:only] || @scope[:options][:except])
           end
 
-          def scope_action_options
+          def scope_action_options #:nodoc:
             @scope[:options].slice(:only, :except)
           end
 
-          def resource_scope?
+          def resource_scope? #:nodoc:
             [:resource, :resources].include?(@scope[:scope_level])
           end
 
-          def resource_method_scope?
+          def resource_method_scope? #:nodoc:
             [:collection, :member, :new].include?(@scope[:scope_level])
           end
 
@@ -1239,7 +1239,7 @@ module ActionDispatch
             @scope[:scope_level_resource] = old_resource
           end
 
-          def resource_scope(resource)
+          def resource_scope(resource) #:nodoc:
             with_scope_level(resource.is_a?(SingletonResource) ? :resource : :resources, resource) do
               scope(parent_resource.resource_scope) do
                 yield
@@ -1247,30 +1247,30 @@ module ActionDispatch
             end
           end
 
-          def nested_options
+          def nested_options #:nodoc:
             {}.tap do |options|
               options[:as] = parent_resource.member_name
               options[:constraints] = { "#{parent_resource.singular}_id".to_sym => id_constraint } if id_constraint?
             end
           end
 
-          def id_constraint?
+          def id_constraint? #:nodoc:
             @scope[:constraints] && @scope[:constraints][:id].is_a?(Regexp)
           end
 
-          def id_constraint
+          def id_constraint #:nodoc:
             @scope[:constraints][:id]
           end
 
-          def canonical_action?(action, flag)
+          def canonical_action?(action, flag) #:nodoc:
             flag && resource_method_scope? && CANONICAL_ACTIONS.include?(action.to_s)
           end
 
-          def shallow_scoping?
+          def shallow_scoping? #:nodoc:
             shallow? && @scope[:scope_level] == :member
           end
 
-          def path_for_action(action, path)
+          def path_for_action(action, path) #:nodoc:
             prefix = shallow_scoping? ?
               "#{@scope[:shallow_path]}/#{parent_resource.path}/:id" : @scope[:path]
 
@@ -1281,11 +1281,11 @@ module ActionDispatch
             end
           end
 
-          def action_path(name, path = nil)
+          def action_path(name, path = nil) #:nodoc:
             path || @scope[:path_names][name.to_sym] || name.to_s
           end
 
-          def prefix_name_for_action(as, action)
+          def prefix_name_for_action(as, action) #:nodoc:
             if as
               as.to_s
             elsif !canonical_action?(action, @scope[:scope_level])
@@ -1293,7 +1293,7 @@ module ActionDispatch
             end
           end
 
-          def name_for_action(as, action)
+          def name_for_action(as, action) #:nodoc:
             prefix = prefix_name_for_action(as, action)
             prefix = Mapper.normalize_name(prefix) if prefix
             name_prefix = @scope[:as]
@@ -1323,7 +1323,7 @@ module ActionDispatch
           end
       end
 
-      module Shorthand
+      module Shorthand #:nodoc:
         def match(*args)
           if args.size == 1 && args.last.is_a?(Hash)
             options  = args.pop
