1b80352f84422dd1c4853cf0207a356fcd58ce7a
Ryan Bigg <radarlistener@gmail.com>
2010-11-26 14:25:46 +1100

    Update ActionMailer documentation to not use deprecated template_root method as documentation, but rather raise_delivery_errors method

--- actionmailer/lib/action_mailer/base.rb
+++ actionmailer/lib/action_mailer/base.rb
@@ -273,7 +273,7 @@ module ActionMailer #:nodoc:
   # = Configuration options
   #
   # These options are specified on the class level, like
-  # <tt>ActionMailer::Base.template_root = "/my/templates"</tt>
+  # <tt>ActionMailer::Base.raise_delivery_errors = true</tt>
   #
   # * <tt>default</tt> - You can pass this in at a class level as well as within the class itself as
   #   per the above section.
