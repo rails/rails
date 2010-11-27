1d57cce6bb910f0a49768d9786b8220caf3019e5
Franck Verrot <franck@verrot.fr>
2010-11-27 11:12:27 +0100

    Add timestamps to the Person test model

--- activerecord/test/schema/schema.rb
+++ activerecord/test/schema/schema.rb
@@ -412,6 +412,7 @@ ActiveRecord::Schema.define do
     t.string     :gender, :limit => 1
     t.references :number1_fan
     t.integer    :lock_version, :null => false, :default => 0
+    t.timestamps
   end
 
   create_table :pets, :primary_key => :pet_id ,:force => true do |t|
