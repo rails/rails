10b1887a719588c1252906169f9d3d12ee1387c6
Aaron Patterson <aaron.patterson@gmail.com>
2010-11-30 17:48:10 -0800

    JoinBase initialize can be deleted

--- activerecord/lib/active_record/associations/class_methods/join_dependency/join_base.rb
+++ activerecord/lib/active_record/associations/class_methods/join_dependency/join_base.rb
@@ -3,14 +3,6 @@ module ActiveRecord
     module ClassMethods
       class JoinDependency # :nodoc:
         class JoinBase < JoinPart # :nodoc:
-          # Extra joins provided when the JoinDependency was created
-          attr_reader :table_joins
-
-          def initialize(active_record, joins = nil)
-            super(active_record)
-            @table_joins = joins
-          end
-
           def ==(other)
             other.class == self.class &&
               other.active_record == active_record
