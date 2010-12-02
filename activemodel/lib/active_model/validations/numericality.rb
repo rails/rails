acbabd96268237826e1ab094273a1d7d53b4e579
Santiago Pastorino <santiago@wyeworks.com>
2010-12-02 15:43:12 -0200

    Don't compute this string again

--- activemodel/lib/active_model/validations/numericality.rb
+++ activemodel/lib/active_model/validations/numericality.rb
@@ -24,7 +24,7 @@ module ActiveModel
       def validate_each(record, attr_name, value)
         before_type_cast = "#{attr_name}_before_type_cast"
 
-        raw_value = record.send("#{attr_name}_before_type_cast") if record.respond_to?(before_type_cast.to_sym)
+        raw_value = record.send(before_type_cast) if record.respond_to?(before_type_cast.to_sym)
         raw_value ||= value
 
         return if options[:allow_nil] && raw_value.nil?
