0afebd5b31bdc6edc1399c77153e65f5ea156fe1
Aaron Patterson <aaron.patterson@gmail.com>
2010-12-01 18:00:24 -0800

    no need for self

--- activerecord/lib/active_record/associations/class_methods/join_dependency/join_association.rb
+++ activerecord/lib/active_record/associations/class_methods/join_dependency/join_association.rb
@@ -52,7 +52,7 @@ module ActiveRecord
 
           def find_parent_in(other_join_dependency)
             other_join_dependency.join_parts.detect do |join_part|
-              self.parent == join_part
+              parent == join_part
             end
           end
 
