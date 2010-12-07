4018b5d66a810a34273d4b198688896f6f7459f3
Aaron Patterson <aaron.patterson@gmail.com>
2010-12-06 20:26:08 -0800

    quoting limit nodes

--- lib/arel/visitors/mysql.rb
+++ lib/arel/visitors/mysql.rb
@@ -25,7 +25,7 @@ module Arel
           ("SET #{o.values.map { |value| visit value }.join ', '}" unless o.values.empty?),
           ("WHERE #{o.wheres.map { |x| visit x }.join ' AND '}" unless o.wheres.empty?),
           ("ORDER BY #{o.orders.map { |x| visit x }.join(', ')}" unless o.orders.empty?),
-          ("LIMIT #{o.limit}" if o.limit),
+          ("LIMIT #{visit o.limit}" if o.limit),
         ].compact.join ' '
       end
 
