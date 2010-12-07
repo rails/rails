13d22766902af1d6f709b85a73e089b7afbb9dbf
Aaron Patterson <aaron.patterson@gmail.com>
2010-12-06 20:16:16 -0800

    consolidating dot visitor methods

--- lib/arel/visitors/to_sql.rb
+++ lib/arel/visitors/to_sql.rb
@@ -105,7 +105,7 @@ module Arel
         [
           o.cores.map { |x| visit_Arel_Nodes_SelectCore x }.join,
           ("ORDER BY #{o.orders.map { |x| visit x }.join(', ')}" unless o.orders.empty?),
-          ("LIMIT #{o.limit}" if o.limit),
+          ("LIMIT #{visit o.limit}" if o.limit),
           (visit(o.offset) if o.offset),
           (visit(o.lock) if o.lock),
         ].compact.join ' '
