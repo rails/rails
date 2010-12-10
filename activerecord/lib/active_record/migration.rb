3d01ef6dd1ed83b44702d117b4c1c658dfa8ba75
Piotr Sarnacki <drogus@gmail.com>
2010-12-10 02:14:58 +0100

    require core_ext/array/wrap in active_record/migration
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- activerecord/lib/active_record/migration.rb
+++ activerecord/lib/active_record/migration.rb
@@ -1,3 +1,5 @@
+require "active_support/core_ext/array/wrap"
+
 module ActiveRecord
   # Exception that can be raised to stop migrations from going backwards.
   class IrreversibleMigration < ActiveRecordError
