491ce5b6ce1a3af42d02efff9a2e42b1a8980553
Jon Leighton <j@jonathanleighton.com>
2010-12-12 17:03:41 +0000

    Verify that creating a has_many through record where there is a default_scope on the join model works correctly (creates the join record with the default scope applied)

--- activerecord/test/models/categorization.rb
+++ activerecord/test/models/categorization.rb
@@ -3,3 +3,12 @@ class Categorization < ActiveRecord::Base
   belongs_to :category
   belongs_to :author
 end
+
+class SpecialCategorization < ActiveRecord::Base
+  self.table_name = 'categorizations'
+
+  default_scope where(:special => true)
+
+  belongs_to :author
+  belongs_to :category
+end
