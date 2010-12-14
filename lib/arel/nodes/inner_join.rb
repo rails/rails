3a9b9a2d4948c943ca0e6fd35393da5846d95b82
Aaron Patterson <aaron.patterson@gmail.com>
2010-12-14 10:26:18 -0800

    removing test code.  :-(

--- lib/arel/nodes/inner_join.rb
+++ lib/arel/nodes/inner_join.rb
@@ -1,10 +1,6 @@
 module Arel
   module Nodes
     class InnerJoin < Arel::Nodes::Join
-      def initialize left, right
-        raise if right == Arel::Nodes::StringJoin
-        super
-      end
     end
   end
 end
