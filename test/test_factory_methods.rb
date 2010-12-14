f694d9e7c332cf4d9bce6742b440bf97308cefb5
Aaron Patterson <aaron.patterson@gmail.com>
2010-12-13 22:16:29 -0800

    adding tests for factory methods

---
+++ test/test_factory_methods.rb
@@ -0,0 +1,27 @@
+require 'helper'
+
+module Arel
+  module FactoryMethods
+    class TestFactoryMethods < MiniTest::Unit::TestCase
+      class Factory
+        include Arel::FactoryMethods
+      end
+
+      def setup
+        @factory = Factory.new
+      end
+
+      def test_create_join
+        join = @factory.create_join :one, :two, :three
+        assert_kind_of Nodes::Join, join
+        assert_equal :three, join.constraint
+      end
+
+      def test_create_on
+        on = @factory.create_on :one
+        assert_instance_of Nodes::On, on
+        assert_equal :one, on.expr
+      end
+    end
+  end
+end
