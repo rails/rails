6bfa846a2cc950bbb3d8b30f076589a60f38b821
Aaron Patterson <aaron.patterson@gmail.com>
2010-12-15 14:01:33 -0800

    dry up compute type in attribute

--- activerecord/lib/active_record/serializers/xml_serializer.rb
+++ activerecord/lib/active_record/serializers/xml_serializer.rb
@@ -226,23 +226,17 @@ module ActiveRecord #:nodoc:
 
     class Attribute < ActiveModel::Serializers::Xml::Serializer::Attribute #:nodoc:
       def compute_type
-        case
-          when @serializable.class.serialized_attributes.has_key?(name)
-            type = super
-          when @serializable.class.columns_hash.has_key?(name)
-            type = @serializable.class.columns_hash[name].type
-          else
-            type = NilClass
-        end
+        klass = @serializable.class
+        type = if klass.serialized_attributes.key?(name)
+                 super
+               elsif klass.columns_hash.key?(name)
+                 klass.columns_hash[name].type
+               else
+                 NilClass
+               end
 
-        case type
-        when :text
-          :string
-        when :time
-          :datetime
-        else
-          type
-        end
+        { :text => :string,
+          :time => :datetime }[type] || type
       end
       protected :compute_type
     end
