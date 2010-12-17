f7e33620caffecb7f065c5e4f5a19597f8c35cdd
Piotr Sarnacki <drogus@gmail.com>
2010-12-17 16:06:38 +0100

    Allow to use BUNDLE_GEMFILE env variable in boot.rb [#6184 state:resolved]

--- railties/lib/rails/generators/rails/app/templates/config/boot.rb
+++ railties/lib/rails/generators/rails/app/templates/config/boot.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 # Set up gems listed in the Gemfile.
 gemfile = File.expand_path('../../Gemfile', __FILE__)
 begin
-  ENV['BUNDLE_GEMFILE'] = gemfile
+  ENV['BUNDLE_GEMFILE'] ||= gemfile
   require 'bundler'
   Bundler.setup
 rescue Bundler::GemNotFound => e
