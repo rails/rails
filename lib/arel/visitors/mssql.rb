rails/arel

Patched Arel v2.0.6 to support MSSQL SQL queries. Based on work of James Abley (https://github.com/jabley/arel).

lib/arel/visitors/mssql.rb
@@ -1,0 +1,16 @@
+module Arel
+  module Visitors
+    class MSSQL < Arel::Visitors::ToSql
+      private
+
+      def visit_Arel_Nodes_Limit o
+        ""
+      end
+
+      def visit_Arel_Nodes_Top o
+        "TOP #{visit o.value}"
+      end
+
+    end
+  end
+end

lib/arel/visitors/to_sql.rb
@@ -76,7 +76,7 @@ module Arel
         [
           o.cores.map { |x| visit_Arel_Nodes_SelectCore x }.join,
           ("ORDER BY #{o.orders.map { |x| visit x }.join(', ')}" unless o.orders.empty?),
-          ("LIMIT #{o.limit}" if o.limit),
+          (visit(o.limit) if o.limit),
           (visit(o.offset) if o.offset),
           (visit(o.lock) if o.lock),
         ].compact.join ' '
@@ -84,7 +84,9 @@ module Arel
 
       def visit_Arel_Nodes_SelectCore o
         [
-          "SELECT #{o.projections.map { |x| visit x }.join ', '}",
+          "SELECT",
+          (visit(o.top) if o.top),
+          "#{o.projections.map { |x| visit x }.join ', '}",
           ("FROM #{visit o.froms}" if o.froms),
           ("WHERE #{o.wheres.map { |x| visit x }.join ' AND ' }" unless o.wheres.empty?),
           ("GROUP BY #{o.groups.map { |x| visit x }.join ', ' }" unless o.groups.empty?),
@@ -98,6 +100,15 @@ module Arel
 
       def visit_Arel_Nodes_Offset o
         "OFFSET #{visit o.expr}"
+      end
+
+      def visit_Arel_Nodes_Limit o
+        "LIMIT #{visit o.value}"
+      end
+
+      # FIXME: this does nothing on most databases, but does on MSSQL
+      def visit_Arel_Nodes_Top o
+        ""
       end
 
       # FIXME: this does nothing on SQLLite3, but should do things on other

rails/rails

lib/arel/visitors/mssql.rb
@@ -1,0 +1,16 @@
+module Arel
+  module Visitors
+    class MSSQL < Arel::Visitors::ToSql
+      private
+
+      def visit_Arel_Nodes_Limit o
+        ""
+      end
+
+      def visit_Arel_Nodes_Top o
+        "TOP #{visit o.expr}"
+      end
+
+    end
+  end
+end

lib/arel/visitors/to_sql.rb
@@ -76,7 +76,7 @@ module Arel
         [
           o.cores.map { |x| visit_Arel_Nodes_SelectCore x }.join,
           ("ORDER BY #{o.orders.map { |x| visit x }.join(', ')}" unless o.orders.empty?),
-          ("LIMIT #{visit o.limit}" if o.limit),
+          (visit(o.limit) if o.limit),
           (visit(o.offset) if o.offset),
           (visit(o.lock) if o.lock),
         ].compact.join ' '
@@ -84,7 +84,9 @@ module Arel
 
       def visit_Arel_Nodes_SelectCore o
         [
-          "SELECT #{o.projections.map { |x| visit x }.join ', '}",
+          "SELECT",
+          (visit(o.top) if o.top),
+          "#{o.projections.map { |x| visit x }.join ', '}",
           ("FROM #{visit o.froms}" if o.froms),
           ("WHERE #{o.wheres.map { |x| visit x }.join ' AND ' }" unless o.wheres.empty?),
           ("GROUP BY #{o.groups.map { |x| visit x }.join ', ' }" unless o.groups.empty?),
@@ -98,6 +100,15 @@ module Arel
 
       def visit_Arel_Nodes_Offset o
         "OFFSET #{visit o.expr}"
+      end
+
+      def visit_Arel_Nodes_Limit o
+        "LIMIT #{visit o.expr}"
+      end
+
+      # FIXME: this does nothing on most databases, but does on MSSQL
+      def visit_Arel_Nodes_Top o
+        ""
       end
 
       # FIXME: this does nothing on SQLLite3, but should do things on other