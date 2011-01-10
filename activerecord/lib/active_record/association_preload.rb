09f37367f85b2b71934971c5a7db96df90233f63
Aaron Patterson <aaron.patterson@gmail.com>
2011-01-10 11:31:04 -0800

    calculate to_s on the primary key once

--- activerecord/lib/active_record/association_preload.rb
+++ activerecord/lib/active_record/association_preload.rb
@@ -340,8 +340,8 @@ module ActiveRecord
           klass = klass_name.constantize
 
           table_name = klass.quoted_table_name
-          primary_key = reflection.options[:primary_key] || klass.primary_key
-          column_type = klass.columns.detect{|c| c.name == primary_key.to_s}.class
+          primary_key = (reflection.options[:primary_key] || klass.primary_key).to_s
+          column_type = klass.columns.detect{|c| c.name == primary_key}.type
           ids = id_map.keys.map do |id|
             if column_type == :integer
               id.to_i
