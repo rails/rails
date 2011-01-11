a61e3acef25d8fb86275d41a9d0d1ba163d7e0bb
Santiago Pastorino <santiago@wyeworks.com>
2011-01-11 18:45:34 -0200

    html_safe.to_str makes no sense

--- actionpack/lib/action_controller/caching/fragments.rb
+++ actionpack/lib/action_controller/caching/fragments.rb
@@ -59,7 +59,7 @@ module ActionController #:nodoc:
 
         key = fragment_cache_key(key)
         instrument_fragment_cache :write_fragment, key do
-          content = content.html_safe.to_str if content.respond_to?(:html_safe)
+          content = content.to_str
           cache_store.write(key, content, options)
         end
         content
