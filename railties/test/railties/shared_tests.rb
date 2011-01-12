58f90b206f6df4661f472ac5e7b4a142a1af6ad6
Aaron Patterson <aaron.patterson@gmail.com>
2011-01-12 10:44:35 -0800

    remove locales external to the system before assertion

--- railties/test/railties/shared_tests.rb
+++ railties/test/railties/shared_tests.rb
@@ -200,7 +200,7 @@ YAML
 
       boot_rails
 
-      assert_equal %W(
+      expected_locales = %W(
         #{RAILS_FRAMEWORK_ROOT}/activesupport/lib/active_support/locale/en.yml
         #{RAILS_FRAMEWORK_ROOT}/activemodel/lib/active_model/locale/en.yml
         #{RAILS_FRAMEWORK_ROOT}/activerecord/lib/active_record/locale/en.yml
@@ -208,7 +208,13 @@ YAML
         #{@plugin.path}/config/locales/en.yml
         #{app_path}/config/locales/en.yml
         #{app_path}/app/locales/en.yml
-      ).map { |path| File.expand_path(path) }, I18n.load_path.map { |path| File.expand_path(path) }
+      ).map { |path| File.expand_path(path) }
+
+      actual_locales = I18n.load_path.map { |path|
+        File.expand_path(path)
+      } & expected_locales # remove locales external to Rails
+
+      assert_equal expected_locales, actual_locales
 
       assert_equal "2", I18n.t(:foo)
       assert_equal "1", I18n.t(:bar)
