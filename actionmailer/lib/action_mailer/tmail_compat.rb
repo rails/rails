b141fa6280868059dfc7fb6934d2a6b39b7d13b8
Mikel Lindsaar <mikel@rubyx.com>
2011-01-18 11:54:30 +1100

    Cleaning up style, concat inside method calls is ugly

--- actionmailer/lib/action_mailer/tmail_compat.rb
+++ actionmailer/lib/action_mailer/tmail_compat.rb
@@ -2,16 +2,18 @@ module Mail
   class Message
 
     def set_content_type(*args)
-      ActiveSupport::Deprecation.warn('Message#set_content_type is deprecated, please just call ' <<
-                                      'Message#content_type with the same arguments', caller[0,2])
+      message = 'Message#set_content_type is deprecated, please just call ' <<
+                'Message#content_type with the same arguments'
+      ActiveSupport::Deprecation.warn(message, caller[0,2])
       content_type(*args)
     end
 
     alias :old_transfer_encoding :transfer_encoding
     def transfer_encoding(value = nil)
       if value
-        ActiveSupport::Deprecation.warn('Message#transfer_encoding is deprecated, please call ' <<
-                                        'Message#content_transfer_encoding with the same arguments', caller[0,2])
+        message = 'Message#transfer_encoding is deprecated, ' <<
+                  'please call Message#content_transfer_encoding with the same arguments'
+        ActiveSupport::Deprecation.warn(message, caller[0,2])
         content_transfer_encoding(value)
       else
         old_transfer_encoding
@@ -19,14 +21,15 @@ module Mail
     end
 
     def transfer_encoding=(value)
-      ActiveSupport::Deprecation.warn('Message#transfer_encoding= is deprecated, please call ' <<
-                                      'Message#content_transfer_encoding= with the same arguments', caller[0,2])
+      message = 'Message#transfer_encoding= is deprecated, ' <<
+                'please call Message#content_transfer_encoding= with the same arguments'
+      ActiveSupport::Deprecation.warn(message, caller[0,2])
       self.content_transfer_encoding = value
     end
 
     def original_filename
-      ActiveSupport::Deprecation.warn('Message#original_filename is deprecated, ' <<
-                                      'please call Message#filename', caller[0,2])
+      message = 'Message#original_filename is deprecated, please call Message#filename'
+      ActiveSupport::Deprecation.warn(message, caller[0,2])
       filename
     end
 
