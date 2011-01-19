af5c81339a19c731924c58b820ec549b0062562b
Daniel Azuma <dazuma@gmail.com>
2011-01-19 12:40:15 -0800

    Chain predications off of named functions

--- lib/arel/nodes/named_function.rb
+++ lib/arel/nodes/named_function.rb
@@ -3,6 +3,8 @@ module Arel
     class NamedFunction < Arel::Nodes::Function
       attr_accessor :name
 
+      include Arel::Predications
+
       def initialize name, expr, aliaz = nil
         super(expr, aliaz)
         @name = name
