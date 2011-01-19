2570c85cb7de03a2c9cc183c73707a267f2efb91
Aaron Patterson <aaron.patterson@gmail.com>
2011-01-19 14:31:22 -0800

    fixing psych support in big decimal, fixing tests to support YAML 1.1

--- activesupport/test/core_ext/bigdecimal_test.rb
+++ activesupport/test/core_ext/bigdecimal_test.rb
@@ -4,10 +4,10 @@ require 'active_support/core_ext/big_decimal'
 
 class BigDecimalTest < Test::Unit::TestCase
   def test_to_yaml
-    assert_equal("--- 100000.30020320320000000000000000000000000000001\n", BigDecimal.new('100000.30020320320000000000000000000000000000001').to_yaml)
-    assert_equal("--- .Inf\n",  BigDecimal.new('Infinity').to_yaml)
-    assert_equal("--- .NaN\n",  BigDecimal.new('NaN').to_yaml)
-    assert_equal("--- -.Inf\n", BigDecimal.new('-Infinity').to_yaml)
+    assert_match("--- 100000.30020320320000000000000000000000000000001\n", BigDecimal.new('100000.30020320320000000000000000000000000000001').to_yaml)
+    assert_match("--- .Inf\n",  BigDecimal.new('Infinity').to_yaml)
+    assert_match("--- .NaN\n",  BigDecimal.new('NaN').to_yaml)
+    assert_match("--- -.Inf\n", BigDecimal.new('-Infinity').to_yaml)
   end
 
   def test_to_d
