bc9b1075aec2a63f789b105cd11886a2c8c45ccf
Jamis Buck <jamis@37signals.com>
2011-01-19 15:55:32 -0700

    Revert "make TestCaseTest work for pre-1.9 rubies, too"
    This reverts commit 7938039548c3269cefe1bc1368f726557793f771.

--- activesupport/test/test_case_test.rb
+++ activesupport/test/test_case_test.rb
@@ -2,8 +2,6 @@ require 'abstract_unit'
 
 module ActiveSupport
   class TestCaseTest < ActiveSupport::TestCase
-    IS_MINITEST = defined?(MiniTest::Assertions) && TestCase < MiniTest::Assertions 
-
     class FakeRunner
       attr_reader :puked
 
@@ -14,65 +12,46 @@ module ActiveSupport
       def puke(klass, name, e)
         @puked << [klass, name, e]
       end
-
-      unless IS_MINITEST
-        def add_error(e)
-          puke(nil, nil, e)
-        end
-
-        def add_run
-        end
-
-        def add_assertion
-        end
-
-        def add_failure(msg, locations=nil)
-        end
-      end
     end
 
-    def test_callback_with_exception
-      tc = Class.new(TestCase) do
-        setup :bad_callback
-        def bad_callback; raise 'oh noes' end
-        def test_true; assert true end
-      end
+    if defined?(MiniTest::Assertions) && TestCase < MiniTest::Assertions
+      def test_callback_with_exception
+        tc = Class.new(TestCase) do
+          setup :bad_callback
+          def bad_callback; raise 'oh noes' end
+          def test_true; assert true end
+        end
 
-      test_name = 'test_true'
-      fr = FakeRunner.new
+        test_name = 'test_true'
+        fr = FakeRunner.new
 
-      test = tc.new test_name
-      test.run(fr) {}
-      klass, name, exception = *fr.puked.first
+        test = tc.new test_name
+        test.run fr
+        klass, name, exception = *fr.puked.first
 
-      if IS_MINITEST
         assert_equal tc, klass
         assert_equal test_name, name
+        assert_equal 'oh noes', exception.message
       end
 
-      assert_match %r{oh noes}, exception.message
-    end
+      def test_teardown_callback_with_exception
+        tc = Class.new(TestCase) do
+          teardown :bad_callback
+          def bad_callback; raise 'oh noes' end
+          def test_true; assert true end
+        end
 
-    def test_teardown_callback_with_exception
-      tc = Class.new(TestCase) do
-        teardown :bad_callback
-        def bad_callback; raise 'oh noes' end
-        def test_true; assert true end
-      end
+        test_name = 'test_true'
+        fr = FakeRunner.new
 
-      test_name = 'test_true'
-      fr = FakeRunner.new
+        test = tc.new test_name
+        test.run fr
+        klass, name, exception = *fr.puked.first
 
-      test = tc.new test_name
-      test.run(fr) {}
-      klass, name, exception = *fr.puked.first
-
-      if IS_MINITEST
         assert_equal tc, klass
         assert_equal test_name, name
+        assert_equal 'oh noes', exception.message
       end
-
-      assert_match %r{oh noes}, exception.message
     end
   end
 end
