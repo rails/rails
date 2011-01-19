039b807afddd8f61884cbccd4de26116ba5cfe3a
Jamis Buck <jamis@37signals.com>
2011-01-19 15:56:14 -0700

    Revert "rein in GC during tests by making them run (at most) once per second"
    This reverts commit 35984f50ec740c311fc6ae0d30f47c0fbf1a7eee.

--- activesupport/lib/active_support/testing/garbage_collection.rb
+++ activesupport/lib/active_support/testing/garbage_collection.rb
@@ -3,9 +3,6 @@ module ActiveSupport
     module GarbageCollection
       def self.included(base)
         base.teardown :scrub_leftover_instance_variables
-
-        base.setup :begin_gc_deferment
-        base.teardown :reconsider_gc_deferment
       end
 
       private
@@ -15,27 +12,6 @@ module ActiveSupport
       def scrub_leftover_instance_variables
         (instance_variables.map(&:to_sym) - RESERVED_INSTANCE_VARIABLES).each do |var|
           remove_instance_variable(var)
-        end
-      end
-
-      # Minimum interval, in seconds, at which to run GC. Might be less
-      # frequently than this, if a single test takes longer than this to
-      # run.
-      DEFERRED_GC_THRESHOLD = (ENV['DEFERRED_GC_THRESHOLD'] || 1.0).to_f
-
-      @@last_gc_run = Time.now
-
-      def begin_gc_deferment
-        GC.disable if DEFERRED_GC_THRESHOLD > 0
-      end
-
-      def reconsider_gc_deferment
-        if DEFERRED_GC_THRESHOLD > 0 && Time.now - @@last_gc_run >= DEFERRED_GC_THRESHOLD
-          GC.enable
-          GC.start
-          GC.disable
-
-          @@last_gc_run = Time.now
         end
       end
     end
