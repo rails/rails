89a5f1463d7e9546ed7a0cf482afea99ba2040e7
José Valim <jose.valim@gmail.com>
2011-02-10 16:50:35 +0100

    Revert "Removed Array#safe_join in AS core_ext and moved it to a view helper with the same same."
    Applied the wrong version.
    
    This reverts commit 98c0c5db50a7679b3d58769ac22cb0a27a62c930.

---
+++ actionpack/lib/action_view/helpers/raw_output_helper.rb
@@ -0,0 +1,18 @@
+module ActionView #:nodoc:
+  # = Action View Raw Output Helper
+  module Helpers #:nodoc:
+    module RawOutputHelper
+      # This method outputs without escaping a string. Since escaping tags is
+      # now default, this can be used when you don't want Rails to automatically
+      # escape tags. This is not recommended if the data is coming from the user's
+      # input.
+      #
+      # For example:
+      #
+      # <%=raw @user.name %>
+      def raw(stringish)
+        stringish.to_s.html_safe
+      end
+    end
+  end
+end
