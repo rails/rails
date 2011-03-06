8e0affbd861369d84273a4f89917fda24157ead4
Paco Guzman <fjguzman@aspgems.com>
2011-03-06 22:35:26 +0100

    Configuration options are symbols

--- activemodel/lib/active_model/validations/with.rb
+++ activemodel/lib/active_model/validations/with.rb
@@ -50,9 +50,9 @@ module ActiveModel
       #   end
       #
       # Configuration options:
-      # * <tt>on</tt> - Specifies when this validation is active
+      # * <tt>:on</tt> - Specifies when this validation is active
       #   (<tt>:create</tt> or <tt>:update</tt>
-      # * <tt>if</tt> - Specifies a method, proc or string to call to determine
+      # * <tt>:if</tt> - Specifies a method, proc or string to call to determine
       #   if the validation should occur (e.g. <tt>:if => :allow_validation</tt>,
       #   or <tt>:if => Proc.new { |user| user.signup_step > 2 }</tt>).
       #   The method, proc or string should return or evaluate to a true or false value.
