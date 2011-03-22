cef10d8ec58a661c231c5a9e0dc3e01165831807
Joshua Peek <josh@joshpeek.com>
2011-03-22 11:15:24 -0500

    Assign sprockets static root

--- railties/lib/rails/application.rb
+++ railties/lib/rails/application.rb
@@ -160,6 +160,7 @@ module Rails
 
       env = Sprockets::Environment.new(root.to_s)
       env.logger = Rails.logger
+      env.static_root = Rails.root.join("public/assets")
       env.ensure_fresh_assets = !config.action_controller.perform_caching
 
       self.class.default_sprockets_paths.each do |pattern|
