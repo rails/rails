ec0cacc2938cb7159ae6085fc653b2be906a748f
Aaron Patterson <aaron.patterson@gmail.com>
2011-03-28 16:22:37 -0700

    testing app delegation from the ConnectionManagement middleware

--- activerecord/test/cases/connection_management_test.rb
+++ activerecord/test/cases/connection_management_test.rb
@@ -3,24 +3,41 @@ require "cases/helper"
 module ActiveRecord
   module ConnectionAdapters
     class ConnectionManagementTest < ActiveRecord::TestCase
+      class App
+        attr_reader :calls
+        def initialize
+          @calls = []
+        end
+
+        def call(env)
+          @calls << env
+          [200, {}, [['hi mom']]]
+        end
+      end
+
       def setup
         @env = {}
-        @app = stub('App')
+        @app = App.new
         @management = ConnectionManagement.new(@app)
 
         @connections_cleared = false
         ActiveRecord::Base.stubs(:clear_active_connections!).with { @connections_cleared = true }
       end
 
+      def test_app_delegation
+        manager = ConnectionManagement.new(@app)
+
+        manager.call @env
+        assert_equal [@env], @app.calls
+      end
+
       test "clears active connections after each call" do
-        @app.expects(:call).with(@env)
         @management.call(@env)
         assert @connections_cleared
       end
 
       test "doesn't clear active connections when running in a test case" do
         @env['rack.test'] = true
-        @app.expects(:call).with(@env)
         @management.call(@env)
         assert !@connections_cleared
       end
