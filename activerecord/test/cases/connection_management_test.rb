7b4866e0aef1ed8ec08fd9eac104df42bce3c1c9
Aaron Patterson <aaron.patterson@gmail.com>
2011-03-28 16:08:42 -0700

    namespacing connection management tests. :heart:

--- activerecord/test/cases/connection_management_test.rb
+++ activerecord/test/cases/connection_management_test.rb
@@ -1,25 +1,29 @@
 require "cases/helper"
 
-class ConnectionManagementTest < ActiveRecord::TestCase
-  def setup
-    @env = {}
-    @app = stub('App')
-    @management = ActiveRecord::ConnectionAdapters::ConnectionManagement.new(@app)
+module ActiveRecord
+  module ConnectionAdapters
+    class ConnectionManagementTest < ActiveRecord::TestCase
+      def setup
+        @env = {}
+        @app = stub('App')
+        @management = ConnectionManagement.new(@app)
 
-    @connections_cleared = false
-    ActiveRecord::Base.stubs(:clear_active_connections!).with { @connections_cleared = true }
-  end
+        @connections_cleared = false
+        ActiveRecord::Base.stubs(:clear_active_connections!).with { @connections_cleared = true }
+      end
 
-  test "clears active connections after each call" do
-    @app.expects(:call).with(@env)
-    @management.call(@env)
-    assert @connections_cleared
-  end
+      test "clears active connections after each call" do
+        @app.expects(:call).with(@env)
+        @management.call(@env)
+        assert @connections_cleared
+      end
 
-  test "doesn't clear active connections when running in a test case" do
-    @env['rack.test'] = true
-    @app.expects(:call).with(@env)
-    @management.call(@env)
-    assert !@connections_cleared
+      test "doesn't clear active connections when running in a test case" do
+        @env['rack.test'] = true
+        @app.expects(:call).with(@env)
+        @management.call(@env)
+        assert !@connections_cleared
+      end
+    end
   end
 end
