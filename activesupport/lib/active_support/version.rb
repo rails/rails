f13fe8ff56bd2a0f428af362ca3fa18ae46da553
Aaron Patterson <aaron.patterson@gmail.com>
2011-03-28 13:32:09 -0700

    bumping version to 3.0.6.rc1

--- activesupport/lib/active_support/version.rb
+++ activesupport/lib/active_support/version.rb
@@ -2,8 +2,8 @@ module ActiveSupport
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 0
-    TINY  = 5
-    PRE   = nil
+    TINY  = 6
+    PRE   = "rc1"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
