bed7a1acc46c9b211b7feaa21cc10395283392ab
Joshua Peek <josh@joshpeek.com>
2011-03-29 16:17:49 -0500

    Copy use_sprockets config

--- actionpack/lib/abstract_controller/asset_paths.rb
+++ actionpack/lib/abstract_controller/asset_paths.rb
@@ -3,7 +3,7 @@ module AbstractController
     extend ActiveSupport::Concern
 
     included do
-      config_accessor :asset_host, :asset_path, :assets_dir, :javascripts_dir, :stylesheets_dir
+      config_accessor :asset_host, :asset_path, :assets_dir, :javascripts_dir, :stylesheets_dir, :use_sprockets
     end
   end
 end
