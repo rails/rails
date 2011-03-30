05f3df328c69f49e26703ed29637ae0acf896225
Santiago Pastorino <santiago@wyeworks.com>
2011-03-30 16:37:01 -0300

    Bring back i18n_key to avoid regression

--- activemodel/lib/active_model/translation.rb
+++ activemodel/lib/active_model/translation.rb
@@ -44,7 +44,7 @@ module ActiveModel
     # Specify +options+ with additional translating options.
     def human_attribute_name(attribute, options = {})
       defaults = lookup_ancestors.map do |klass|
-        :"#{self.i18n_scope}.attributes.#{klass.model_name.underscore}.#{attribute}"
+        :"#{self.i18n_scope}.attributes.#{klass.model_name.i18n_key}.#{attribute}"
       end
 
       defaults << :"attributes.#{attribute}"
