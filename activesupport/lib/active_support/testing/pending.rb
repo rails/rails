286709336577c767498785bc7be486eefe3faa4b
David Heinemeier Hansson <david@loudthinking.com>
2011-03-30 21:16:29 -0700

    Delegate pending to skip if Minitest is available

--- activesupport/lib/active_support/testing/pending.rb
+++ activesupport/lib/active_support/testing/pending.rb
@@ -11,32 +11,36 @@ module ActiveSupport
         @@at_exit = false
 
         def pending(description = "", &block)
-          if description.is_a?(Symbol)
-            is_pending = $tags[description]
-            return block.call unless is_pending
-          end
-
-          if block_given?
-            failed = false
-
-            begin
-              block.call
-            rescue Exception
-              failed = true
+          if defined?(::MiniTest)
+            skip(description.blank? ? nil : description)
+          else
+            if description.is_a?(Symbol)
+              is_pending = $tags[description]
+              return block.call unless is_pending
             end
 
-            flunk("<#{description}> did not fail.") unless failed
-          end
+            if block_given?
+              failed = false
 
-          caller[0] =~ (/(.*):(.*):in `(.*)'/)
-          @@pending_cases << "#{$3} at #{$1}, line #{$2}"
-          print "P"
+              begin
+                block.call
+              rescue Exception
+                failed = true
+              end
 
-          @@at_exit ||= begin
-            at_exit do
-              puts "\nPending Cases:"
-              @@pending_cases.each do |test_case|
-                puts test_case
+              flunk("<#{description}> did not fail.") unless failed
+            end
+
+            caller[0] =~ (/(.*):(.*):in `(.*)'/)
+            @@pending_cases << "#{$3} at #{$1}, line #{$2}"
+            print "P"
+
+            @@at_exit ||= begin
+              at_exit do
+                puts "\nPending Cases:"
+                @@pending_cases.each do |test_case|
+                  puts test_case
+                end
               end
             end
           end
