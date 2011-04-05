30264bb57b0f54c5bdd7cd23d0a29b36879954a3
Durran Jordan <durran@gmail.com>
2011-04-05 11:01:04 +0200

    Destroying records via nested attributes works independent of reject_if:
    - When a :_destroy truthiness is provided in the attributes hash, the
      record should get destroyed regardless of the result of the proc or
        method supplied to :reject_if. (If :allow_destroy is true)
    
    [#6006 state:committed]
    
    Signed-off-by: Santiago Pastorino <santiago@wyeworks.com>

--- activerecord/lib/active_record/nested_attributes.rb
+++ activerecord/lib/active_record/nested_attributes.rb
@@ -402,6 +402,7 @@ module ActiveRecord
     end
 
     def call_reject_if(association_name, attributes)
+      return false if has_destroy_flag?(attributes)
       case callback = nested_attributes_options[association_name][:reject_if]
       when Symbol
         method(callback).arity == 0 ? send(callback) : send(callback, attributes)
