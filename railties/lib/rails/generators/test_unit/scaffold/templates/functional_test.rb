d2e95083f884cc180bcd3a9a117a0e3589a1bf93
Prem Sichanugrist <s@sikachu.com>
2011-04-10 11:27:26 +0700

    Rails will now generate Ruby 1.9 style hash when running scaffold_controller generator on Ruby 1.9.x
    The new hash syntax of Ruby 1.9 looks more superior, so we decide to switch to it in the places that appropriate.
    
    This patch has been requested by DHH.

--- railties/lib/rails/generators/test_unit/scaffold/templates/functional_test.rb
+++ railties/lib/rails/generators/test_unit/scaffold/templates/functional_test.rb
@@ -19,30 +19,30 @@ class <%= controller_class_name %>ControllerTest < ActionController::TestCase
 
   test "should create <%= singular_table_name %>" do
     assert_difference('<%= class_name %>.count') do
-      post :create, :<%= singular_table_name %> => @<%= singular_table_name %>.attributes
+      post :create, <%= key_value singular_table_name, "@#{singular_table_name}.attributes" %>
     end
 
     assert_redirected_to <%= singular_table_name %>_path(assigns(:<%= singular_table_name %>))
   end
 
   test "should show <%= singular_table_name %>" do
-    get :show, :id => @<%= singular_table_name %>.to_param
+    get :show, <%= key_value :id, "@#{singular_table_name}.to_param" %>
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :id => @<%= singular_table_name %>.to_param
+    get :edit, <%= key_value :id, "@#{singular_table_name}.to_param" %>
     assert_response :success
   end
 
   test "should update <%= singular_table_name %>" do
-    put :update, :id => @<%= singular_table_name %>.to_param, :<%= singular_table_name %> => @<%= singular_table_name %>.attributes
+    put :update, <%= key_value :id, "@#{singular_table_name}.to_param" %>, <%= key_value singular_table_name, "@#{singular_table_name}.attributes" %>
     assert_redirected_to <%= singular_table_name %>_path(assigns(:<%= singular_table_name %>))
   end
 
   test "should destroy <%= singular_table_name %>" do
     assert_difference('<%= class_name %>.count', -1) do
-      delete :destroy, :id => @<%= singular_table_name %>.to_param
+      delete :destroy, <%= key_value :id, "@#{singular_table_name}.to_param" %>
     end
 
     assert_redirected_to <%= index_helper %>_path
