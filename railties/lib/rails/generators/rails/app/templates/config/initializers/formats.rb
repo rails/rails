3c56226339d99fb090886816c085a5ccce3064fe
Wilker Lucio <wilkerlucio@gmail.com>
2011-04-10 22:37:49 -0300

    initializer sample file for validates_format_of aliases

---
+++ railties/lib/rails/generators/rails/app/templates/config/initializers/formats.rb
@@ -0,0 +1,4 @@
+# Be sure to restart your server when you modify this file.
+
+# Add new format alias for use in validates_format_of:
+# ActiveModel::Validations::FormatValidator.register_format_alias(:email, /\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/)
