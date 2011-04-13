e7280cb1bd9309840c44f479bc9e60b0faf302f3
David Heinemeier Hansson <david@loudthinking.com>
2011-04-13 18:08:40 +0200

    Use driver_ujs now that we no longer have to depend on a shared name and insert all prototype dependencies if thats specified

--- railties/lib/rails/generators/rails/app/templates/app/assets/javascripts/application.js
+++
@@ -1,6 +0,0 @@
-# FIXME: Tell people that this is a manifest file, real code should go into discrete files
-# FIXME: Tell people how Sprockets and CoffeeScript works
-#
-#= require jquery
-#= require jquery_ujs
-#= require_tree .
