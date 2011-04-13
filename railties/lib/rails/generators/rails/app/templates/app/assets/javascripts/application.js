f3f28ee493c72d69efd7c849c8a11fa699a459a5
David Heinemeier Hansson <david@loudthinking.com>
2011-04-13 17:37:48 +0200

    Dont need to make application.js into coffee since it will mostly be a manifest file anyway

--- railties/lib/rails/generators/rails/app/templates/app/assets/javascripts/application.js
+++ railties/lib/rails/generators/rails/app/templates/app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-# Place your application-specific JavaScript functions and classes here
+# FIXME: Tell people that this is a manifest file, real code should go into discrete files
 # FIXME: Tell people how Sprockets and CoffeeScript works
 #
 #= require jquery
