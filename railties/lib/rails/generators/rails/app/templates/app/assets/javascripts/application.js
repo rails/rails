9333ca74b37c6c24262783daf118984621ace69e
Joshua Peek <josh@joshpeek.com>
2011-04-13 09:47:29 -0500

    Add default require directives for jquery

--- railties/lib/rails/generators/rails/app/templates/app/assets/javascripts/application.js
+++ railties/lib/rails/generators/rails/app/templates/app/assets/javascripts/application.js
@@ -1,2 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // FIXME: Tell people how Sprockets and Coffe works
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
