90033025edfee9823db10ea5662942b13fb10a14
Prem Sichanugrist <s@sikachu.com>
2011-04-14 20:32:18 +0700

    Update scaffold controller to generate scss file if Sass is available

--- railties/lib/rails/generators/rails/stylesheets/stylesheets_generator.rb
+++ railties/lib/rails/generators/rails/stylesheets/stylesheets_generator.rb
@@ -2,7 +2,21 @@ module Rails
   module Generators
     class StylesheetsGenerator < Base
       def copy_stylesheets_file
-        template "scaffold.css", "app/assets/stylesheets/scaffold.css" if behavior == :invoke
+        if behavior == :invoke
+          template "scaffold.#{stylesheet_extension}", "app/assets/stylesheets/scaffold.#{stylesheet_extension}"
+        end
+      end
+
+    private
+      def stylesheet_extension
+        using_sass? ? "css.scss" : "css"
+      end
+
+      def using_sass?
+        require 'sass'
+        defined?(Sass)
+      rescue LoadError
+        false
       end
     end
   end
