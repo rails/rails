1787447ada8cf224bf5552c6ae8a3db053fa5839
Santiago Pastorino <santiago@wyeworks.com>
2011-04-14 18:16:01 -0300

    Prepare for the 3.0.7.rc1 release

--- activemodel/lib/active_model/version.rb
+++ activemodel/lib/active_model/version.rb
@@ -2,8 +2,8 @@ module ActiveModel
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 0
-    TINY  = 6
-    PRE   = nil
+    TINY  = 7
+    PRE   = "rc1"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
