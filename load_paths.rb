ae13cb1523b97e860e7a8eeec905528a273d843a
José Valim <jose.valim@gmail.com>
2011-04-15 18:13:17 +0200

    Remove rescue as it was clobbering the real error.

--- load_paths.rb
+++ load_paths.rb
@@ -1,32 +1,9 @@
-begin
-  require File.expand_path('../.bundle/environment', __FILE__)
-rescue LoadError
-  begin
-    # bust gem prelude
-    if defined? Gem
-      Gem.source_index
-      gem 'bundler'
-    else
-      require 'rubygems'
-    end
-    require 'bundler'
-    Bundler.setup
-  rescue LoadError
-    module Bundler
-      def self.require(*args, &block); end
-      def self.method_missing(*args, &block); end
-    end
-
-    %w(
-      actionmailer
-      actionpack
-      activemodel
-      activerecord
-      activeresource
-      activesupport
-      railties
-    ).each do |framework|
-      $:.unshift File.expand_path("../#{framework}/lib", __FILE__)
-    end
-  end
+# bust gem prelude
+if defined? Gem
+  Gem.source_index
+  gem 'bundler'
+else
+  require 'rubygems'
 end
+require 'bundler'
+Bundler.setup
