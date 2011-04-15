rails/rails

commit bbe0a507f287c20ab4ae8a244fbfc810665deda5

Remove unnecessary code from define_read_method and add assertion to make sure the underscored version is actually generated

activerecord/lib/active_record/attribute_methods/read.rb
@@ -70,13 +70,10 @@ module ActiveRecord
             if cache_attribute?(attr_name)
               access_code = "@attributes_cache['#{attr_name}'] ||= (#{access_code})"
             end
-            if symbol =~ /^[a-zA-Z_]\w*[!?=]?$/
-              generated_attribute_methods.module_eval("def _#{symbol}; #{access_code}; end; alias #{symbol} _#{symbol}", __FILE__, __LINE__)
-            else
-              generated_attribute_methods.module_eval do
-                define_method("_#{symbol}") { eval(access_code) }
-                alias_method(symbol, "_#{symbol}")
-              end
+
+            generated_attribute_methods.module_eval do
+              define_method("_#{attr_name}") { eval(access_code) }
+              alias_method(attr_name, "_#{attr_name}")
             end
           end
       end

activerecord/test/cases/attribute_methods_test.rb
@@ -77,6 +77,7 @@ class AttributeMethodsTest < ActiveRecord::TestCase
   def test_respond_to?
     topic = Topic.find(1)
     assert_respond_to topic, "title"
+    assert_respond_to topic, "_title"
     assert_respond_to topic, "title?"
     assert_respond_to topic, "title="
     assert_respond_to topic, :title

commit e01dfb27fc5573db9070dce788f2e5ee62094722

Undo performances regressions I introduced in bbe0a507f287c20ab4ae8a244fbfc810665deda5 and add test for an edge case. Add comments to explain the intent of the code.

activerecord/lib/active_record/attribute_methods/read.rb
@@ -43,7 +43,7 @@ module ActiveRecord
             end
 
             if attr_name == primary_key && attr_name != "id"
-              define_read_method(:id, attr_name, columns_hash[attr_name])
+              define_read_method('id', attr_name, columns_hash[attr_name])
             end
           end
 
@@ -59,7 +59,9 @@ module ActiveRecord
           end
 
           # Define an attribute reader method.  Cope with nil column.
-          def define_read_method(symbol, attr_name, column)
+          # method_name is the same as attr_name except when a non-standard primary key is used,
+          # we still define #id as an accessor for the key
+          def define_read_method(method_name, attr_name, column)
             cast_code = column.type_cast_code('v')
             access_code = "(v=@attributes['#{attr_name}']) && #{cast_code}"
 
@@ -71,9 +73,25 @@ module ActiveRecord
               access_code = "@attributes_cache['#{attr_name}'] ||= (#{access_code})"
             end
 
-            generated_attribute_methods.module_eval do
-              define_method("_#{attr_name}") { eval(access_code) }
-              alias_method(attr_name, "_#{attr_name}")
+            # Where possible, generate the method by evalling a string, as this will result in
+            # faster accesses because it avoids the block eval and then string eval incurred
+            # by the second branch.
+            #
+            # The second, slower, branch is necessary to support instances where the database
+            # returns columns with extra stuff in (like 'my_column(omg)').
+            if method_name =~ /^[a-zA-Z_]\w*[!?=]?$/
+              generated_attribute_methods.module_eval <<-STR, __FILE__, __LINE__
+                def _#{method_name}
+                  #{access_code}
+                end
+
+                alias #{method_name} _#{method_name}
+              STR
+            else
+              generated_attribute_methods.module_eval do
+                define_method("_#{method_name}") { eval(access_code) }
+                alias_method(method_name, "_#{method_name}")
+              end
             end
           end
       end

activerecord/test/cases/attribute_methods_test.rb
@@ -10,6 +10,7 @@ require 'models/company'
 require 'models/category'
 require 'models/reply'
 require 'models/contact'
+require 'models/keyboard'
 
 class AttributeMethodsTest < ActiveRecord::TestCase
   fixtures :topics, :developers, :companies, :computers
@@ -87,6 +88,16 @@ class AttributeMethodsTest < ActiveRecord::TestCase
     assert_respond_to topic, "attribute_names"
     assert !topic.respond_to?("nothingness")
     assert !topic.respond_to?(:nothingness)
+  end
+
+  def test_respond_to_with_custom_primary_key
+    keyboard = Keyboard.create
+    assert_not_nil keyboard.key_number
+    assert_equal keyboard.key_number, keyboard.id
+    assert keyboard.respond_to?('key_number')
+    assert keyboard.respond_to?('_key_number')
+    assert keyboard.respond_to?('id')
+    assert keyboard.respond_to?('_id')
   end
 
   # Syck calls respond_to? before actually calling initialize