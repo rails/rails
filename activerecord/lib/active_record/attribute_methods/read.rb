rails/rails

commit a3639be4ed5e89b59c4ad0154d44f58e3e48d484

Fixes performance issue introduced in 3.0.6 (issue #6695)

activerecord/lib/active_record/attribute_methods/read.rb
@@ -77,9 +77,11 @@ module ActiveRecord
       # Returns the value of the attribute identified by <tt>attr_name</tt> after it has been typecast (for example,
       # "2004-12-12" in a data column is cast to a date object, like Date.new(2004, 12, 12)).
       def read_attribute(attr_name)
-        send "_#{attr_name}"
-      rescue NoMethodError
-        _read_attribute attr_name
+        if respond_to? "_#{attr_name}"
+          send "_#{attr_name}"
+        else
+          _read_attribute attr_name
+        end
       end
 
       def _read_attribute(attr_name)

commit 912e5ff7903f6e44f3dcc7029b715a1670fc3040

Return nil from read_attribute if _foo is defined, but @attributes has no 'foo' key. Fixes breakage in test_find_only_some_columns on Ruby 1.9 which was introduced in a3639be4ed5e89b59c4ad0154d44f58e3e48d484.

activerecord/lib/active_record/attribute_methods/read.rb
@@ -78,7 +78,7 @@ module ActiveRecord
       # "2004-12-12" in a data column is cast to a date object, like Date.new(2004, 12, 12)).
       def read_attribute(attr_name)
         if respond_to? "_#{attr_name}"
-          send "_#{attr_name}"
+          send "_#{attr_name}" if @attributes.has_key?(attr_name.to_s)
         else
           _read_attribute attr_name
         end