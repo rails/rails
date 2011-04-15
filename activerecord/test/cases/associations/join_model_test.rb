e68a83c9ecdc5b9395e0dde687cafdf8330f9de0
Jon Leighton <j@jonathanleighton.com>
2011-04-15 02:19:11 +0100

    Refactor test to avoid hackery

--- activerecord/test/cases/associations/join_model_test.rb
+++ activerecord/test/cases/associations/join_model_test.rb
@@ -516,10 +516,10 @@ class AssociationsJoinModelTest < ActiveRecord::TestCase
   end
 
   def test_has_many_through_collection_size_uses_counter_cache_if_it_exists
-    author = authors(:david)
-    author.stubs(:_read_attribute).with('comments_count').returns(100)
-    assert_equal 100, author.comments.size
-    assert !author.comments.loaded?
+    c = categories(:general)
+    c.categorizations_count = 100
+    assert_equal 100, c.categorizations.size
+    assert !c.categorizations.loaded?
   end
 
   def test_adding_junk_to_has_many_through_should_raise_type_mismatch
