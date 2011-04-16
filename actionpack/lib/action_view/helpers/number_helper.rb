7f98b544e3b29c7ffdb8cb4a2ad1da5d1a8c611c
Matt Duncan <mrduncan@gmail.com>
2011-04-15 20:18:30 -0400

    Negative format example should use a negative number

--- actionpack/lib/action_view/helpers/number_helper.rb
+++ actionpack/lib/action_view/helpers/number_helper.rb
@@ -100,7 +100,7 @@ module ActionView
       #  number_to_currency(1234567890.506, :precision => 3)  # => $1,234,567,890.506
       #  number_to_currency(1234567890.506, :locale => :fr)   # => 1 234 567 890,506 €
       #
-      #  number_to_currency(1234567890.50, :negative_format => "(%u%n)")
+      #  number_to_currency(-1234567890.50, :negative_format => "(%u%n)")
       #  # => ($1,234,567,890.51)
       #  number_to_currency(1234567890.50, :unit => "&pound;", :separator => ",", :delimiter => "")
       #  # => &pound;1234567890,50
