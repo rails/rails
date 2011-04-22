0ce94f40203d4dcff577d8e3aadbc963adca569c
Sebastian Martinez <sebastian@wyeworks.com>
2011-04-22 20:39:26 -0300

    dot missing here

--- activerecord/lib/active_record/connection_adapters/abstract/schema_statements.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/schema_statements.rb
@@ -28,7 +28,7 @@ module ActiveRecord
       # Returns an array of indexes for the given table.
       # def indexes(table_name, name = nil) end
 
-      # Checks to see if an index exists on a table for a given index definition
+      # Checks to see if an index exists on a table for a given index definition.
       #
       # === Examples
       #  # Check an index exists
