a8870d140ececc6327533d08b112cece03c41499
Schneems <richard.schneeman@gmail.com>
2011-04-23 02:23:12 +0800

    show http method in routing error message

--- actionpack/lib/action_dispatch/middleware/show_exceptions.rb
+++ actionpack/lib/action_dispatch/middleware/show_exceptions.rb
@@ -50,7 +50,7 @@ module ActionDispatch
         # Only this middleware cares about RoutingError. So, let's just raise
         # it here.
         if headers['X-Cascade'] == 'pass'
-           raise ActionController::RoutingError, "No route matches #{env['PATH_INFO'].inspect}"
+           raise ActionController::RoutingError, "No route matches [#{env['REQUEST_METHOD']}] #{env['PATH_INFO'].inspect}"
         end
       rescue Exception => exception
         raise exception if env['action_dispatch.show_exceptions'] == false
