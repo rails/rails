8d7efe6a4e25b8707d46006cf20f6ce499e83780
David Heinemeier Hansson <david@loudthinking.com>
2011-04-23 13:22:15 +0200

    Stop adding actual dummy tests (they clutter up the real ones in a new app), just show how its done

--- railties/lib/rails/generators/test_unit/integration/templates/integration_test.rb
+++ railties/lib/rails/generators/test_unit/integration/templates/integration_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 class <%= class_name %>Test < ActionDispatch::IntegrationTest
   fixtures :all
 
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
