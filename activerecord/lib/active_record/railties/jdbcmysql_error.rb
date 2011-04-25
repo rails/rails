6464f7b9be3ccf3f05a36bc6be0de2cb8ebda97d
Rashmi Yadav <rays.rashmi@gmail.com>
2011-04-25 23:05:17 +0530

    Fixed error when running db:create with jdbcmysql

---
+++ activerecord/lib/active_record/railties/jdbcmysql_error.rb
@@ -0,0 +1,16 @@
+#FIXME Remove if ArJdbcMysql will give.
+module ArJdbcMySQL
+  class Error < StandardError
+    attr_accessor :error_number, :sql_state
+
+    def initialize msg
+      super
+      @error_number = nil
+      @sql_state    = nil
+    end
+
+    # Mysql gem compatibility
+    alias_method :errno, :error_number
+    alias_method :error, :message
+  end
+end
