146474256cf2f09f9706980032688b2c7d484e24
Aaron Patterson <aaron.patterson@gmail.com>
2011-04-29 09:41:37 -0700

    lean on rubygems to provide error messages about missing gems

--- activerecord/lib/active_record/connection_adapters/mysql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/mysql_adapter.rb
@@ -3,15 +3,8 @@ require 'active_support/core_ext/kernel/requires'
 require 'active_support/core_ext/object/blank'
 require 'set'
 
-begin
-  require 'mysql'
-rescue LoadError
-  raise "!!! Missing the mysql gem. Add it to your Gemfile: gem 'mysql'"
-end
-
-unless defined?(Mysql::Result) && Mysql::Result.method_defined?(:each_hash)
-  raise "!!! Outdated mysql gem. Upgrade to 2.8.1 or later. In your Gemfile: gem 'mysql', '2.8.1'. Or use gem 'mysql2'"
-end
+gem 'mysql', '~> 2.8.1'
+require 'mysql'
 
 class Mysql
   class Time
