06671c37de93702323ee281aedc4023bb8d7cdb6
Burke Libbey <burke@burkelibbey.org>
2011-05-07 18:30:50 -0500

    Added activesupport requires for Array.wrap in previously modified files

--- actionpack/lib/action_view/path_set.rb
+++ actionpack/lib/action_view/path_set.rb
@@ -1,3 +1,5 @@
+require "active_support/core_ext/array/wrap"
+
 module ActionView #:nodoc:
   # = Action View PathSet
   class PathSet < Array #:nodoc:
--- actionpack/lib/action_view/template/error.rb
+++ actionpack/lib/action_view/template/error.rb
@@ -1,3 +1,4 @@
+require "active_support/core_ext/array/wrap"
 require "active_support/core_ext/enumerable"
 
 module ActionView
