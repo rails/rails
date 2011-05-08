b8db7503ecdf6fed4aa7861a7df48e99ae2195f1
Arun Agrawal <arun@fromjaipur.com>
2011-05-08 22:31:21 +0530

    ActiveRecord create should work now with jdbcmysql #jruby

--- activerecord/lib/active_record/connection_adapters/jdbc_jdbcmysql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/jdbc_jdbcmysql_adapter.rb
@@ -3,3 +3,25 @@
 gem 'activerecord-jdbcmysql-adapter'
 require 'arjdbc/mysql'
 
+module ActiveRecord::ConnectionAdapters
+
+  class MysqlAdapter < JdbcAdapter
+
+    def insert(sql, name = nil, pk = nil, id_value = nil, sequence_name = nil, binds = [])
+      sql, binds = sql_for_insert(sql, pk, id_value, sequence_name, binds)
+      id      = exec_insert(sql, name, binds)
+      id_value || id
+    end
+
+    protected
+
+    def exec_insert(sql, name, binds)
+      binds = binds.dup
+
+      # Pretend to support bind parameters
+      execute sql.gsub('?') { quote(*binds.shift.reverse) }, name
+    end
+
+
+  end
+end
