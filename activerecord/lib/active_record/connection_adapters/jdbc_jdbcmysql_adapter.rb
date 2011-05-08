4d0aed3d9de2902926e03d6cedc591b6e0f6bac3
Arun Agrawal <arun@fromjaipur.com>
2011-05-08 22:35:53 +0530

    Drying code. Using insert from database_statements.rb #jruby

--- activerecord/lib/active_record/connection_adapters/jdbc_jdbcmysql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/jdbc_jdbcmysql_adapter.rb
@@ -7,12 +7,6 @@ module ActiveRecord::ConnectionAdapters
 
   class MysqlAdapter < JdbcAdapter
 
-    def insert(sql, name = nil, pk = nil, id_value = nil, sequence_name = nil, binds = [])
-      sql, binds = sql_for_insert(sql, pk, id_value, sequence_name, binds)
-      id      = exec_insert(sql, name, binds)
-      id_value || id
-    end
-
     protected
 
     def exec_insert(sql, name, binds)
@@ -22,6 +16,9 @@ module ActiveRecord::ConnectionAdapters
       execute sql.gsub('?') { quote(*binds.shift.reverse) }, name
     end
 
+    def last_inserted_id(result)
+      result
+    end
 
   end
 end
