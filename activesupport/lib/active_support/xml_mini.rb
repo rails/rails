7e07712eeae51ef0855f6f2dd8bb0e87b0d08d24
Mike Howard <mike@clove.com>
2011-05-09 08:26:22 -0600

    added comment on regex to xml_mini.rb#_dasherize

--- activesupport/lib/active_support/xml_mini.rb
+++ activesupport/lib/active_support/xml_mini.rb
@@ -139,6 +139,7 @@ module ActiveSupport
     protected
 
     def _dasherize(key)
+      # $2 must be a non-greedy regex for this to work
       left, middle, right = /\A(_*)(.*?)(_*)\Z/.match(key.strip)[1,3]
       "#{left}#{middle.tr('_', '-')}#{right}"
     end
