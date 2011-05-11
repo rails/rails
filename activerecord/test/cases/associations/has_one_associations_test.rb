aa1fc1c88cb58b6387cb7a8560aac78ef56fa024
Jon Leighton <j@jonathanleighton.com>
2011-05-11 08:11:17 +0100

    Bring back obj.association_loaded? as a deprecated method. Fixes #472.

--- activerecord/test/cases/associations/has_one_associations_test.rb
+++ activerecord/test/cases/associations/has_one_associations_test.rb
@@ -359,4 +359,13 @@ class HasOneAssociationsTest < ActiveRecord::TestCase
     assert_equal pirate.id, ships(:black_pearl).reload.pirate_id
     assert_nil new_ship.pirate_id
   end
+
+  def test_deprecated_association_loaded
+    firm   = companies(:first_firm)
+    firm.association(:account).stubs(:loaded?).returns(stub)
+
+    assert_deprecated do
+      assert_equal firm.association(:account).loaded?, firm.account_loaded?
+    end
+  end
 end
