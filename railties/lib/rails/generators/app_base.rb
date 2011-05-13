c55d7ab9cbe33ecc0999da9e32e7e229da55c55f
Xavier Noria <fxn@hashref.com>
2011-05-13 22:22:26 +0200

    move bundler loading down to #bundle_command

--- railties/lib/rails/generators/app_base.rb
+++ railties/lib/rails/generators/app_base.rb
@@ -5,8 +5,6 @@ require 'rails/version' unless defined?(Rails::VERSION)
 require 'rbconfig'
 require 'open-uri'
 require 'uri'
-require 'bundler'
-require 'bundler/cli'
 
 module Rails
   module Generators
@@ -187,6 +185,9 @@ module Rails
       end
 
       def bundle_command(command)
+        require 'bundler'
+        require 'bundler/cli'
+
         say_status :run, "bundle #{command}"
         Bundler::CLI.new.send(command)
       end
