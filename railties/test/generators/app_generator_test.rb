d52fc25ac7a5e38612ad3c04f69735750d5f1a8c
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-05-18 00:31:01 -0500

    Fixing typo in test for turn gem

--- railties/test/generators/app_generator_test.rb
+++ railties/test/generators/app_generator_test.rb
@@ -213,7 +213,7 @@ class AppGeneratorTest < Rails::Generators::TestCase
   def test_turn_gem_is_not_included_in_gemfile_if_skipping_test_unit
     run_generator [destination_root, "--skip-test-unit"]
     assert_file "Gemfile" do |contents|
-      assert_no_match /gem 'tuarn'/, contents unless RUBY_VERSION < '1.9.2'
+      assert_no_match /gem 'turn'/, contents unless RUBY_VERSION < '1.9.2'
     end
   end
 
