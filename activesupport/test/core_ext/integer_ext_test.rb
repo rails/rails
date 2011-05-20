df705045b82818143065eac494c840fc6ac3d7e5
Alberto Leal <albertonb@gmail.com>
2011-05-20 06:36:44 -0700

    Added some tests to check whether an integer is multiple_of many values at the same time.

--- activesupport/test/core_ext/integer_ext_test.rb
+++ activesupport/test/core_ext/integer_ext_test.rb
@@ -5,10 +5,14 @@ class IntegerExtTest < Test::Unit::TestCase
   def test_multiple_of
     [ -7, 0, 7, 14 ].each { |i| assert i.multiple_of?(7) }
     [ -7, 7, 14 ].each { |i| assert ! i.multiple_of?(6) }
+    [ -8, 0, 4, 12 ].each { |i| assert i.multiple_of?(2,4) }
+    [ -8, 11, 15, 21 ].each { |i| assert ! i.multiple_of?(2,3) }
 
     # test the 0 edge case
     assert 0.multiple_of?(0)
     assert !5.multiple_of?(0)
+    assert 0.multiple_of?(0, 2)
+    assert !5.multiple_of?(0, 5)
 
     # test with a prime
     assert !22953686867719691230002707821868552601124472329079.multiple_of?(2)
