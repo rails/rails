1ffc3063b793d8a1355b7c646ab9f73ea0b8c5f7
Arun Agrawal <arun@fromjaipur.com>
2011-05-22 02:02:56 +0530

    No more need rubygems here.

--- activeresource/test/abstract_unit.rb
+++ activeresource/test/abstract_unit.rb
@@ -3,7 +3,6 @@ require File.expand_path('../../../load_paths', __FILE__)
 lib = File.expand_path("#{File.dirname(__FILE__)}/../lib")
 $:.unshift(lib) unless $:.include?('lib') || $:.include?(lib)
 
-require 'rubygems'
 require 'test/unit'
 require 'active_resource'
 require 'active_support'
