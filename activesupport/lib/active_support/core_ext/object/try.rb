29a5aeaae976bf8432d57ec996c7c81932a39de6
dmathieu <42@dmathieu.com>
2011-05-21 23:43:12 +0200

    don't raise NoMethodError the tried method doesn't exists

--- activesupport/lib/active_support/core_ext/object/try.rb
+++ activesupport/lib/active_support/core_ext/object/try.rb
@@ -28,6 +28,8 @@ class Object
   def try(*a, &b)
     if a.empty? && block_given?
       yield self
+    elsif !a.empty? && !respond_to?(a.first)
+      nil
     else
       __send__(*a, &b)
     end
