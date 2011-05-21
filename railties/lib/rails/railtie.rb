e2908356672d4459ada0064f773efd820efda822
Patrick Quinn <patrick@patrickquinn.net>
2011-05-20 20:02:20 -0400

    Make Rake::DSL methods available within Railtie rake_tasks block
    Fixes incompatibility introduced by Rake 0.9.0

--- railties/lib/rails/railtie.rb
+++ railties/lib/rails/railtie.rb
@@ -178,6 +178,7 @@ module Rails
     end
 
     def load_tasks
+      extend Rake::DSL if defined? Rake::DSL
       self.class.rake_tasks.each(&:call)
 
       # load also tasks from all superclasses
