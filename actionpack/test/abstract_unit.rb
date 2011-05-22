90f028a4dc48152baded4cba9ec66474c2e98f48
Prem Sichanugrist <s@sikachu.com>
2011-05-22 19:02:54 -0400

    Make sure that we're able to run Action Pack test standalone.
    Sprocket was trying to look for these two things:
    
    * `Rails::VERSION::STRING`
    * `.initializer(str)` in `Rails::Engine`
    
    This commit added require to version.rb into Abstract Unit, and mocking the `Rails::Engine.initializer` in `sprockets_helper_test.rb`

--- actionpack/test/abstract_unit.rb
+++ actionpack/test/abstract_unit.rb
@@ -1,4 +1,5 @@
 require File.expand_path('../../../load_paths', __FILE__)
+require File.expand_path('../../../version', __FILE__)
 
 lib = File.expand_path("#{File.dirname(__FILE__)}/../lib")
 $:.unshift(lib) unless $:.include?('lib') || $:.include?(lib)
