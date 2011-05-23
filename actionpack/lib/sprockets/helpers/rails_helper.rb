42732e6333a6f959e4af90001bfbb9e0d8ce80e0
wycats <wycats@gmail.com>
2011-05-23 16:29:20 -0700

    Move javascript/css helpers into the Sprockets helper and just have them override the old behavior

--- actionpack/lib/sprockets/helpers/rails_helper.rb
+++ actionpack/lib/sprockets/helpers/rails_helper.rb
@@ -9,6 +9,50 @@ module Sprockets
         end
       end
 
+      def asset_path(source, default_ext = nil, body = false)
+        source = source.logical_path if source.respond_to?(:logical_path)
+        path = asset_paths.compute_public_path(source, 'assets', default_ext, true)
+        body ? "#{path}?body=1" : path
+      end
+
+      def javascript_include_tag(source, options = {})
+        debug = options.key?(:debug) ? options.delete(:debug) : debug_assets?
+        body  = options.key?(:body)  ? options.delete(:body)  : false
+
+        if debug && asset = asset_paths.asset_for(source, 'js')
+          asset.to_a.map { |dep|
+            javascript_include_tag(dep, :debug => false, :body => true)
+          }.join("\n").html_safe
+        else
+          options = {
+            'type' => "text/javascript",
+            'src'  => asset_path(source, 'js', body)
+          }.merge(options.stringify_keys)
+
+          content_tag 'script', "", options
+        end
+      end
+
+      def stylesheet_link_tag(source, options = {})
+        debug = options.key?(:debug) ? options.delete(:debug) : debug_assets?
+        body  = options.key?(:body)  ? options.delete(:body)  : false
+
+        if debug && asset = asset_paths.asset_for(source, 'css')
+          asset.to_a.map { |dep|
+            stylesheet_link_tag(dep, :debug => false, :body => true)
+          }.join("\n").html_safe
+        else
+          options = {
+            'rel'   => "stylesheet",
+            'type'  => "text/css",
+            'media' => "screen",
+            'href'  => asset_path(source, 'css', body)
+          }.merge(options.stringify_keys)
+
+          tag 'link', options
+        end
+      end
+
       class AssetPaths < ActionView::Helpers::AssetPaths #:nodoc:
         def compute_public_path(source, dir, ext=nil, include_host=true)
           super(source, 'assets', ext, include_host)
