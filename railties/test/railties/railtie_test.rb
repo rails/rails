rails/rails

commit b921679d0dd3d5ca16f0362c2b34e7703cfe15e5

fixes Rake::RDocTask deprecation warnings from rake 0.9.0

railties/lib/rails/generators/rails/plugin_new/templates/Rakefile
@@ -5,9 +5,9 @@ rescue LoadError
   puts 'You must `gem install bundler` and `bundle install` to run rake tasks'
 end
 
-require 'rake/rdoctask'
+require 'rdoc/task'
 
-Rake::RDocTask.new(:rdoc) do |rdoc|
+RDoc::Task.new(:rdoc) do |rdoc|
   rdoc.rdoc_dir = 'rdoc'
   rdoc.title    = '<%= camelized %>'
   rdoc.options << '--line-numbers' << '--inline-source'

railties/lib/rails/tasks/documentation.rake
@@ -1,7 +1,7 @@
-require 'rake/rdoctask'
+require 'rdoc/task'
 
 # Monkey-patch to remove redoc'ing and clobber descriptions to cut down on rake -T noise
-class RDocTaskWithoutDescriptions < Rake::RDocTask
+class RDocTaskWithoutDescriptions < RDoc::Task
   def define
     task rdoc_task_name
 

railties/test/railties/railtie_test.rb
@@ -97,7 +97,7 @@ module RailtiesTest
       assert !$ran_block
       require 'rake'
       require 'rake/testtask'
-      require 'rake/rdoctask'
+      require 'rdoc/task'
 
       AppTemplate::Application.load_tasks
       assert $ran_block
@@ -121,7 +121,7 @@ module RailtiesTest
       assert_equal [], $ran_block
       require 'rake'
       require 'rake/testtask'
-      require 'rake/rdoctask'
+      require 'rdoc/task'
 
       AppTemplate::Application.load_tasks
       assert $ran_block.include?("my_tie")

railties/test/railties/shared_tests.rb
@@ -237,7 +237,7 @@ module RailtiesTest
 
       boot_rails
       require 'rake'
-      require 'rake/rdoctask'
+      require 'rdoc/task'
       require 'rake/testtask'
       Rails.application.load_tasks
       Rake::Task[:foo].invoke

railties/lib/rails/generators/rails/plugin/templates/Rakefile.tt
@@ -1,6 +1,6 @@
 #!/usr/bin/env rake
 require 'rake/testtask'
-require 'rake/rdoctask'
+require 'rdoc/task'
 
 desc 'Default: run unit tests.'
 task :default => :test
@@ -14,7 +14,7 @@ Rake::TestTask.new(:test) do |t|
 end
 
 desc 'Generate documentation for the <%= file_name %> plugin.'
-Rake::RDocTask.new(:rdoc) do |rdoc|
+RDoc::Task.new(:rdoc) do |rdoc|
   rdoc.rdoc_dir = 'rdoc'
   rdoc.title    = '<%= class_name %>'
   rdoc.options << '--line-numbers' << '--inline-source'

commit 24b28a2a0c6a58e177b07ce3ccf56dc975541780

require 'rake/rdoctask' if failed to require 'rdoc/task'
fixes a regression introduced by b921679d0dd3d5ca16f0362c2b34e7703cfe15e5
require 'rdoc/task' fails if the system RDoc version is below 2.4.2 (e.g. CRuby 1.8.7)

Rakefile
@@ -1,6 +1,11 @@
 #!/usr/bin/env rake
 
-require 'rdoc/task'
+
+begin
+  require 'rdoc/task'
+rescue LoadError
+  require 'rake/rdoctask'
+end
 require 'net/http'
 
 $:.unshift File.expand_path('..', __FILE__)

railties/lib/rails/generators/rails/plugin_new/templates/Rakefile
@@ -4,8 +4,11 @@ begin
 rescue LoadError
   puts 'You must `gem install bundler` and `bundle install` to run rake tasks'
 end
-
-require 'rdoc/task'
+begin
+  require 'rdoc/task'
+rescue LoadError
+  require 'rake/rdoctask'
+end
 
 RDoc::Task.new(:rdoc) do |rdoc|
   rdoc.rdoc_dir = 'rdoc'

railties/lib/rails/tasks/documentation.rake
@@ -1,4 +1,8 @@
-require 'rdoc/task'
+begin
+  require 'rdoc/task'
+rescue LoadError
+  require 'rake/rdoctask'
+end
 
 # Monkey-patch to remove redoc'ing and clobber descriptions to cut down on rake -T noise
 class RDocTaskWithoutDescriptions < RDoc::Task

railties/test/railties/railtie_test.rb
@@ -97,7 +97,12 @@ module RailtiesTest
       assert !$ran_block
       require 'rake'
       require 'rake/testtask'
-      require 'rdoc/task'
+      begin
+        require 'rdoc/task'
+      rescue LoadError
+        require 'rake/rdoctask'
+      end
+
 
       AppTemplate::Application.load_tasks
       assert $ran_block

railties/test/railties/shared_tests.rb
@@ -237,7 +237,11 @@ module RailtiesTest
 
       boot_rails
       require 'rake'
-      require 'rdoc/task'
+      begin
+        require 'rdoc/task'
+      rescue LoadError
+        require 'rake/rdoctask'
+      end
       require 'rake/testtask'
       Rails.application.load_tasks
       Rake::Task[:foo].invoke