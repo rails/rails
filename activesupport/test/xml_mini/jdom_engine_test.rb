0094351a240598025141409e3aa309ff8b2f5c6c
Arun Agrawal <arun@fromjaipur.com>
2011-05-27 00:48:59 +0530

    Adding tests for file_from_xml for XmlMini_JDOM #jruby

--- activesupport/test/xml_mini/jdom_engine_test.rb
+++ activesupport/test/xml_mini/jdom_engine_test.rb
@@ -15,20 +15,20 @@ if RUBY_PLATFORM =~ /java/
       XmlMini.backend = @default_backend
     end
 
-    # def test_file_from_xml
-    #   hash = Hash.from_xml(<<-eoxml)
-    #     <blog>
-    #       <logo type="file" name="logo.png" content_type="image/png">
-    #       </logo>
-    #     </blog>
-    #   eoxml
-    #   assert hash.has_key?('blog')
-    #   assert hash['blog'].has_key?('logo')
-    #
-    #   file = hash['blog']['logo']
-    #   assert_equal 'logo.png', file.original_filename
-    #   assert_equal 'image/png', file.content_type
-    # end
+    def test_file_from_xml
+       hash = Hash.from_xml(<<-eoxml)
+         <blog>
+           <logo type="file" name="logo.png" content_type="image/png">
+           </logo>
+         </blog>
+       eoxml
+       assert hash.has_key?('blog')
+       assert hash['blog'].has_key?('logo')
+
+       file = hash['blog']['logo']
+       assert_equal 'logo.png', file.original_filename
+       assert_equal 'image/png', file.content_type
+    end
 
     def test_exception_thrown_on_expansion_attack
       assert_raise NativeException do
