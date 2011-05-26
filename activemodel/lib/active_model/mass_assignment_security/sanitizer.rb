b35d77b22f41d6478b92f3ed17768089900093a2
Bogdan Gusiev <agresso@gmail.com>
2011-05-26 10:32:08 +0300

    Remove unused class attribute

--- activemodel/lib/active_model/mass_assignment_security/sanitizer.rb
+++ activemodel/lib/active_model/mass_assignment_security/sanitizer.rb
@@ -2,11 +2,6 @@ module ActiveModel
   module MassAssignmentSecurity
     module Sanitizer
 
-      extend ActiveSupport::Concern
-      included do
-        class_attribute :strict_mass_assignment
-      end
-
       # Returns all attributes not denied by the authorizer.
       def sanitize(attributes, strict = false)
         sanitized_attributes = attributes.reject { |key, value| deny?(key) }
@@ -18,10 +13,10 @@ module ActiveModel
 
       def debug_protected_attribute_removal(attributes, sanitized_attributes, strict)
         removed_keys = attributes.keys - sanitized_attributes.keys
-        process_error(removed_keys, strict) if removed_keys.any?
+        process_removed_keys(removed_keys, strict) if removed_keys.any?
       end
 
-      def process_error(attrs, strict)
+      def process_removed_keys(attrs, strict)
         message = "Can't mass-assign protected attributes: #{attrs.join(', ')}"
         if strict
           raise ActiveModel::MassAssignmentSecurity::Error.new(message, attrs)
