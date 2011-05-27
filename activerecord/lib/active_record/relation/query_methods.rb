8a4013ddd07c42110f5155503f2b2e4097b761ed
Brian Mathiyakom <brian@rarevisions.net>
2011-05-27 13:59:18 -0700

    Fix issue #1272
    Set reverse_order_value when asked to reverse_order().
    Do the actual reversal in build_arel.

--- activerecord/lib/active_record/relation/query_methods.rb
+++ activerecord/lib/active_record/relation/query_methods.rb
@@ -9,7 +9,7 @@ module ActiveRecord
                   :select_values, :group_values, :order_values, :joins_values,
                   :where_values, :having_values, :bind_values,
                   :limit_value, :offset_value, :lock_value, :readonly_value, :create_with_value,
-                  :from_value, :reorder_value
+                  :from_value, :reorder_value, :reverse_order_value
 
     def includes(*args)
       args.reject! {|a| a.blank? }
@@ -158,13 +158,9 @@ module ActiveRecord
     end
 
     def reverse_order
-      order_clause = arel.order_clauses
-
-      order = order_clause.empty? ?
-        "#{table_name}.#{primary_key} DESC" :
-        reverse_sql_order(order_clause).join(', ')
-
-      except(:order).order(Arel.sql(order))
+      relation = clone
+      relation.reverse_order_value = !relation.reverse_order_value
+      relation
     end
 
     def arel
@@ -186,6 +182,7 @@ module ActiveRecord
       arel.group(*@group_values.uniq.reject{|g| g.blank?}) unless @group_values.empty?
 
       order = @reorder_value ? @reorder_value : @order_values
+      order = reverse_sql_order(order) if @reverse_order_value
       arel.order(*order.uniq.reject{|o| o.blank?}) unless order.empty?
 
       build_select(arel, @select_values.uniq)
@@ -306,6 +303,8 @@ module ActiveRecord
     end
 
     def reverse_sql_order(order_query)
+      order_query = ["#{quoted_table_name}.#{quoted_primary_key} ASC"] if order_query.empty?
+
       order_query.join(', ').split(',').collect do |s|
         s.gsub!(/\sasc\Z/i, ' DESC') || s.gsub!(/\sdesc\Z/i, ' ASC') || s.concat(' DESC')
       end
