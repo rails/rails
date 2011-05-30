d72c25e5c09b21b637fa117a6d9c06fb488d7b4c
dmathieu <42@dmathieu.com>
2011-05-30 16:49:56 +0200

    remove the warning when testing whiny_nil

--- activesupport/test/whiny_nil_test.rb
+++ activesupport/test/whiny_nil_test.rb
@@ -3,10 +3,9 @@ require 'active_support/whiny_nil'
 
 class WhinyNilTest < Test::Unit::TestCase
   def test_id
-    nil.stubs(:object_id).returns(999)
     nil.id
   rescue RuntimeError => nme
     assert_no_match(/nil:NilClass/, nme.message)
-    assert_match(/999/, nme.message)
+    assert_match(Regexp.new(nil.object_id.to_s), nme.message)
   end
 end
