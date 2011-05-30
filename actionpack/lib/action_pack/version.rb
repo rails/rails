f4073d530f76ebd5371017032874e5865ebd8ecb
Aaron Patterson <aaron.patterson@gmail.com>
2011-05-30 12:54:16 -0700

    bumping to rc3

--- actionpack/lib/action_pack/version.rb
+++ actionpack/lib/action_pack/version.rb
@@ -3,7 +3,7 @@ module ActionPack
     MAJOR = 3
     MINOR = 0
     TINY  = 8
-    PRE   = "rc2"
+    PRE   = "rc3"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
--- activeresource/lib/active_resource/version.rb
+++ activeresource/lib/active_resource/version.rb
@@ -3,7 +3,7 @@ module ActiveResource
     MAJOR = 3
     MINOR = 0
     TINY  = 8
-    PRE   = "rc2"
+    PRE   = "rc3"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
