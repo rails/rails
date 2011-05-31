64d7348b98efe7f7ca36551571880210b2451626
Jon Leighton <j@jonathanleighton.com>
2011-05-31 20:32:09 +0100

    Only save the record once when calling create! on a collection association. Fixes #1360.

--- activerecord/lib/active_record/associations/has_many_association.rb
+++ activerecord/lib/active_record/associations/has_many_association.rb
@@ -7,9 +7,14 @@ module ActiveRecord
     # is provided by its child HasManyThroughAssociation.
     class HasManyAssociation < CollectionAssociation #:nodoc:
 
-      def insert_record(record, validate = true)
+      def insert_record(record, validate = true, raise = false)
         set_owner_attributes(record)
-        record.save(:validate => validate)
+
+        if raise
+          record.save!(:validate => validate)
+        else
+          record.save(:validate => validate)
+        end
       end
 
       private
