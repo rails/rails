62b207fe4972342ae2fb0ef2482835cdfe1dbe89
Ryan Bigg <radarlistener@gmail.com>
2011-06-04 19:27:02 +1000

    Set Rails.application to the engine so that generators work in engines

--- railties/lib/rails/commands.rb
+++ railties/lib/rails/commands.rb
@@ -24,6 +24,7 @@ when 'generate', 'destroy', 'plugin'
     Rails.application.require_environment!
 
     if defined?(ENGINE_PATH) && engine = Rails::Engine.find(ENGINE_PATH)
+      Rails.application = engine
       Rails.application.load_generators(engine)
     else
       Rails.application.load_generators
