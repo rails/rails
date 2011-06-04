550064cbde409180eba8aad8c352cb6783902b5f
Jon Leighton <j@jonathanleighton.com>
2011-06-04 23:49:54 +0100

    Bring back oracle specific crap

--- activerecord/test/connections/oracle.rb
+++ activerecord/test/connections/oracle.rb
@@ -1,1 +1,7 @@
 ENV['ARCONN'] = 'oracle'
+
+# uses oracle_enhanced adapter in ENV['ORACLE_ENHANCED_PATH'] or from github.com/rsim/oracle-enhanced.git
+require 'active_record/connection_adapters/oracle_enhanced_adapter'
+
+# otherwise failed with silence_warnings method missing exception
+require 'active_support/core_ext/kernel/reporting'
