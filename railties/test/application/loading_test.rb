077ffe0287e47f299f2627577a7d555220b81d09
Arun Agrawal <arun@fromjaipur.com>
2011-06-06 00:55:24 +0530

    loading_test.rb with RAILS_ENV=development

--- railties/test/application/loading_test.rb
+++ railties/test/application/loading_test.rb
@@ -4,8 +4,14 @@ class LoadingTest < Test::Unit::TestCase
   include ActiveSupport::Testing::Isolation
 
   def setup
+    @prev_rails_env = ENV['RAILS_ENV']
+    ENV['RAILS_ENV'] = 'development'
     build_app
     boot_rails
+  end
+
+  def teardown
+    ENV['RAILS_ENV'] = @prev_rails_env
   end
 
   def app
