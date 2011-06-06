10eb47913d0dce1702f9e20040e49e146fdc1207
Aaron Patterson <aaron.patterson@gmail.com>
2011-06-06 15:16:52 -0700

    updating configs for oracle

--- activerecord/test/connections/oracle.rb
+++ activerecord/test/connections/oracle.rb
@@ -1,7 +1,1 @@
-ENV['ARCONN'] = 'oracle'
-
-# uses oracle_enhanced adapter in ENV['ORACLE_ENHANCED_PATH'] or from github.com/rsim/oracle-enhanced.git
-require 'active_record/connection_adapters/oracle_enhanced_adapter'
-
-# otherwise failed with silence_warnings method missing exception
-require 'active_support/core_ext/kernel/reporting'
+ENV['ARCONN'] = 'oracle_enhanced'
