f7dc5af0c3720b555108e0514911abf9cbb165f3
Paul McMahon <paul@mobalean.com>
2011-06-08 18:52:37 -0700

    Magic encoding comment required in case schema includes utf-8 characters (e.g. for default column value).

--- activerecord/lib/active_record/schema_dumper.rb
+++ activerecord/lib/active_record/schema_dumper.rb
@@ -41,6 +41,7 @@ module ActiveRecord
         define_params = @version ? ":version => #{@version}" : ""
 
         stream.puts <<HEADER
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
