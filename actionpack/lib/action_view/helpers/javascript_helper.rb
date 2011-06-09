64c90c20897f693b88ffce6dd9afa77fc8667245
GB <gberenfield@berenfield.com>
2011-06-08 21:43:22 -0400

    * fixed Issue #1576 which persists in 3.0.9rc1

--- actionpack/lib/action_view/helpers/javascript_helper.rb
+++ actionpack/lib/action_view/helpers/javascript_helper.rb
@@ -49,7 +49,7 @@ module ActionView
       # Escape carrier returns and single and double quotes for JavaScript segments.
       def escape_javascript(javascript)
         if javascript
-          javascript.gsub(/(\\|<\/|\r\n|[\n\r"'])/) {|match| JS_ESCAPE_MAP[match] }
+          (javascript.gsub(/(\\|<\/|\r\n|[\n\r"'])/) {|match| JS_ESCAPE_MAP[match] }).to_s.html_safe
         else
           ''
         end
