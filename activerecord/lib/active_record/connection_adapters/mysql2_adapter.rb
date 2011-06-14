e56567c0b22f9455098e4dd444f44480ae16e329
Brian Lopez <seniorlopez@gmail.com>
2011-06-14 15:17:01 -0700

    Turn off eager casting for mysql2, so ActiveRecord can lazily cast values later. This should help performance for tables with complex types like DATETIME, TIMESTAMP

--- activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb
@@ -629,7 +629,7 @@ module ActiveRecord
         end
 
         def configure_connection
-          @connection.query_options.merge!(:as => :array)
+          @connection.query_options.merge!(:as => :array, :cast => false)
 
           # By default, MySQL 'where id is null' selects the last inserted id.
           # Turn this off. http://dev.rubyonrails.org/ticket/6778
