22d0e9aa4616570500a451c8ddc5955d2695417d
Vishnu Atrai <me@vishnuatrai.com>
2011-06-29 23:34:16 +0530

    load exceptaions in metal/datastreaming.rb explictly

--- actionpack/lib/action_controller/metal/data_streaming.rb
+++ actionpack/lib/action_controller/metal/data_streaming.rb
@@ -1,5 +1,4 @@
 require 'active_support/core_ext/file/path'
-require "action_controller/metal/exceptions"
 
 module ActionController #:nodoc:
   # Methods for sending arbitrary data and for streaming files to the browser,
