5aed7ccfc6627c62889f84bdeeb7073d8bc86b28
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2011-06-30 20:30:07 -0300

    Revert "Use "SET CONSTRAINTS ALL DEFERRED" and "SET CONSTRAINTS ALL IMMEDIATE" to disable foreign keys on postgresql adapter"
    This reverts commit 8c98dd129afb649fcdc9e4d6a4590a1457a096f5.

--- activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
@@ -446,13 +446,13 @@ module ActiveRecord
       end
 
       def disable_referential_integrity #:nodoc:
-        if supports_disable_referential_integrity?
-          execute "SET CONSTRAINTS ALL DEFERRED"
+        if supports_disable_referential_integrity? then
+          execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER ALL" }.join(";"))
         end
         yield
       ensure
-        if supports_disable_referential_integrity?
-          execute "SET CONSTRAINTS ALL IMMEDIATE"
+        if supports_disable_referential_integrity? then
+          execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER ALL" }.join(";"))
         end
       end
 
