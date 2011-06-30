c314203d8755d211ec12fbb0d8d1b513a4bdeb39
Bogdan Gusiev <agresso@gmail.com>
2011-06-30 16:27:01 +0300

    Fixed ActionView::FormOptionsHelper#select with :multiple => false (cherry picked from commit 0fdac01876c37c692e9107b6120e03aec692663d)
    Signed-off-by: Andrew White <andyw@pixeltrix.co.uk>

--- actionpack/lib/action_view/helpers/form_helper.rb
+++ actionpack/lib/action_view/helpers/form_helper.rb
@@ -1066,7 +1066,7 @@ module ActionView
             options["name"] ||= tag_name_with_index(@auto_index)
             options["id"] = options.fetch("id"){ tag_id_with_index(@auto_index) }
           else
-            options["name"] ||= tag_name + (options.has_key?('multiple') ? '[]' : '')
+            options["name"] ||= tag_name + (options['multiple'] ? '[]' : '')
             options["id"] = options.fetch("id"){ tag_id }
           end
         end
