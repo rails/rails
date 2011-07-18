01040b5dc990e79052627451603feea0f149fe41
JudeArasu <judearasu@gmail.com>
2011-07-19 01:33:45 +0530

    notextile tag has been placed with ascii quotes

--- railties/guides/rails_guides/textile_extensions.rb
+++ railties/guides/rails_guides/textile_extensions.rb
@@ -25,7 +25,7 @@ module RailsGuides
 
     def plusplus(body)
       body.gsub!(/\+(.*?)\+/) do |m|
-        "<notextile><tt>#{$1}</tt></notextile>"
+        "&lt;notextile&gt;<tt>#{$1}</tt>&lt;/notextile&gt;"
       end
 
       # The real plus sign
@@ -36,8 +36,9 @@ module RailsGuides
       body.gsub!(%r{<(yaml|shell|ruby|erb|html|sql|plain)>(.*?)</\1>}m) do |m|
         es = ERB::Util.h($2)
         css_class = $1.in?(['erb', 'shell']) ? 'html' : $1
-        %{<notextile><div class="code_container"><code class="#{css_class}">#{es}</code></div></notextile>}
+        %{&lt;notextile&gt;<div class="code_container"><code class="#{css_class}">#{es}</code></div>&lt;/notextile&gt;}
       end
     end
   end
 end
+
