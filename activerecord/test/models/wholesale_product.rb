06c6252b10be3d5b9cfae5e9c26e53f53d218dd3
Gabriel Horner <gabriel.horner@gmail.com>
2011-07-22 18:43:27 -0400

    simplify and be more explicit about create and after_initialize tests

--- activerecord/test/models/wholesale_product.rb
+++ activerecord/test/models/wholesale_product.rb
@@ -3,11 +3,7 @@ class WholesaleProduct < ActiveRecord::Base
   after_initialize :set_prices
 
   def set_prices
-    if msrp.nil? && !wholesale.nil?
-      self.msrp = 2 * wholesale
-    elsif !msrp.nil? && wholesale.nil?
-      self.wholesale = msrp / 2
-    end
+    self.msrp = 2 * wholesale if wholesale
   end
 
 end
