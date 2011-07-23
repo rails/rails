b1b850dbdaa1bc287c184e869c0e1b08953d634d
Aaron Kalin <akalin@martinisoftware.com>
2011-07-23 09:56:42 -0700

    Remove leading spaces in optional requires

--- railties/lib/rails/generators/rails/app/templates/config/application.rb
+++ railties/lib/rails/generators/rails/app/templates/config/application.rb
@@ -4,12 +4,12 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 <% else -%>
 # Pick the frameworks you want:
-<%= comment_if :skip_active_record %> require "active_record/railtie"
+<%= comment_if :skip_active_record %>require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
-<%= comment_if :skip_sprockets %> require "sprockets/railtie"
-<%= comment_if :skip_test_unit %> require "rails/test_unit/railtie"
+<%= comment_if :skip_sprockets %>require "sprockets/railtie"
+<%= comment_if :skip_test_unit %>require "rails/test_unit/railtie"
 <% end -%>
 
 # If you have a Gemfile, require the default gems, the ones in the
