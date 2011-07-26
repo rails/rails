8f67029a097220247041579f473fed7490df8cc2
Santiago Pastorino <santiago@wyeworks.com>
2011-07-26 18:10:15 -0300

    Remove cruise files

--- ci/cruise_config.rb
+++
@@ -1,9 +0,0 @@
-Project.configure do |project|
-  project.build_command = 'sudo gem update --system && ruby ci/ci_build.rb'
-  project.email_notifier.from = 'rails-ci@wyeworks.com'
-
-  # project.campfire_notifier.account  = 'rails'
-  # project.campfire_notifier.token    = ''
-  # project.campfire_notifier.room     = 'Rails 3'
-  # project.campfire_notifier.ssl      = true
-end
