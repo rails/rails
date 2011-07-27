035d31bd93d68724912d656e03cba7c6b503d25a
Christos Zisopoulos <christos@me.com>
2011-07-27 16:12:22 +0200

    Improve performance and memory usage for options_for_select with Ruby 1.8.7

--- actionpack/lib/action_view/helpers/form_options_helper.rb
+++ actionpack/lib/action_view/helpers/form_options_helper.rb
@@ -323,12 +323,12 @@ module ActionView
         return container if String === container
 
         selected, disabled = extract_selected_and_disabled(selected).map do | r |
-           Array.wrap(r).map(&:to_s)
+           Array.wrap(r).map { |item| item.to_s }
         end
 
         container.map do |element|
           html_attributes = option_html_attributes(element)
-          text, value = option_text_and_value(element).map(&:to_s)
+          text, value = option_text_and_value(element).map { |item| item.to_s }
           selected_attribute = ' selected="selected"' if option_value_selected?(value, selected)
           disabled_attribute = ' disabled="disabled"' if disabled && option_value_selected?(value, disabled)
           %(<option value="#{ERB::Util.html_escape(value)}"#{selected_attribute}#{disabled_attribute}#{html_attributes}>#{ERB::Util.html_escape(text)}</option>)
