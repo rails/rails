a8aa666f162fb07c0c08906583d459216c142f77
Akira Matsuda <ronnie@dio.jp>
2011-07-28 19:31:54 +0900

    callback methods are Class methods

--- activerecord/lib/active_record/callbacks.rb
+++ activerecord/lib/active_record/callbacks.rb
@@ -262,7 +262,7 @@ module ActiveRecord
 
     def deprecated_callback_method(symbol) #:nodoc:
       if respond_to?(symbol, true)
-        ActiveSupport::Deprecation.warn("Overwriting #{symbol} in your models has been deprecated, please use Base##{symbol} :method_name instead")
+        ActiveSupport::Deprecation.warn("Overwriting #{symbol} in your models has been deprecated, please use Base.#{symbol} :method_name instead")
         send(symbol)
       end
     end
