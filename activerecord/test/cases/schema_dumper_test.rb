44a0893a1a8d64235114c2c2dbf61609f9416764
Arun Agrawal <arun@fromjaipur.com>
2011-07-31 17:28:03 +0530

    Returning and skip for test for 1.8

--- activerecord/test/cases/schema_dumper_test.rb
+++ activerecord/test/cases/schema_dumper_test.rb
@@ -15,7 +15,7 @@ class SchemaDumperTest < ActiveRecord::TestCase
   end
 
   def test_magic_comment
-    skip "only test magic comments on 1.9" if RUBY_VERSION < '1.9'
+    return skip "only test magic comments on 1.9" if RUBY_VERSION < '1.9'
     assert_match "# encoding: #{@stream.external_encoding.name}", standard_dump
   end
 
