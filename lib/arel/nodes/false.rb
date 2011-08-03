3712071148485a8a6f2e28944f1d4932ce72b4bd
Daniel Cadenas <dcadenas@gmail.com>
2011-08-03 20:58:29 -0300

    Remove short circuit evaluation and leave AST as it is

--- lib/arel/nodes/false.rb
+++ lib/arel/nodes/false.rb
@@ -1,17 +1,6 @@
 module Arel
   module Nodes
     class False < Arel::Nodes::Node
-      def not
-        True.new
-      end
-
-      def or right
-        right
-      end
-
-      def and right
-        self
-      end
     end
   end
 end
--- lib/arel/nodes/true.rb
+++ lib/arel/nodes/true.rb
@@ -1,17 +1,6 @@
 module Arel
   module Nodes
     class True < Arel::Nodes::Node
-      def not
-        False.new
-      end
-
-      def or right
-        self
-      end
-
-      def and right
-        right
-      end
     end
   end
 end
