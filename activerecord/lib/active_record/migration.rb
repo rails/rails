eae7baf93f4d88614654e9439f0c2d1f89778b1f
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-08-02 22:35:16 -0400

    Don't override delegate if it's already been set.  Remove superclass.delegate check since delegate will always be present.

--- activerecord/lib/active_record/migration.rb
+++ activerecord/lib/active_record/migration.rb
@@ -329,8 +329,8 @@ module ActiveRecord
     end
 
     def self.method_missing(name, *args, &block) # :nodoc:
-      self.delegate = self.new
-      (delegate || superclass.delegate).send(name, *args, &block)
+      self.delegate ||= self.new
+      delegate.send(name, *args, &block)
     end
 
     cattr_accessor :verbose
