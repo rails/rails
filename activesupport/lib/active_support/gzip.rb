65a648be10148a8a616b28464763bd0e71635429
Santiago Pastorino <santiago@wyeworks.com>
2011-08-06 13:18:51 -0700

    Merge pull request #2450 from guilleiguaran/activesupport-gzip-1.8
    Fix ActiveSupport::Gzip under Ruby 1.8.7. Closes #2416

--- activesupport/lib/active_support/gzip.rb
+++ activesupport/lib/active_support/gzip.rb
@@ -1,5 +1,6 @@
 require 'zlib'
 require 'stringio'
+require 'active_support/core_ext/string/encoding'
 
 module ActiveSupport
   # A convenient wrapper for the zlib standard library that allows compression/decompression of strings with gzip.
