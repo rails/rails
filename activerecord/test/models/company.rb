1054f6620bf208453f245de14dc1aee359348d27
Yasuo Honda <yasuo.honda@gmail.com>
2011-08-06 21:55:13 -0400

    Removed :order => "id" from :clients_of_firm to fix ORA-00907 errors.

--- activerecord/test/models/company.rb
+++ activerecord/test/models/company.rb
@@ -43,7 +43,7 @@ class Firm < Company
       :after_remove  => :log_after_remove
   has_many :unsorted_clients, :class_name => "Client"
   has_many :clients_sorted_desc, :class_name => "Client", :order => "id DESC"
-  has_many :clients_of_firm, :foreign_key => "client_of", :class_name => "Client", :order => "id"
+  has_many :clients_of_firm, :foreign_key => "client_of", :class_name => "Client"
   has_many :unvalidated_clients_of_firm, :foreign_key => "client_of", :class_name => "Client", :validate => false
   has_many :dependent_clients_of_firm, :foreign_key => "client_of", :class_name => "Client", :order => "id", :dependent => :destroy
   has_many :exclusively_dependent_clients_of_firm, :foreign_key => "client_of", :class_name => "Client", :order => "id", :dependent => :delete_all
