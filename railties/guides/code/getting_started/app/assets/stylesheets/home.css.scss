d7154d483269fc25d771d5b8b7b8a2c889e4b3f5
James Gifford <james@jamesrgifford.com>
2011-09-12 10:30:45 -0400

    Added getting_started code, updated guide with link to rails github repo and path to code

---
+++ railties/guides/code/getting_started/app/assets/stylesheets/home.css.scss
@@ -0,0 +1,3 @@
+// Place all the styles related to the home controller here.
+// They will automatically be included in application.css.
+// You can use Sass (SCSS) here: http://sass-lang.com/
---
+++ railties/guides/code/getting_started/test/unit/helpers/comments_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class CommentsHelperTest < ActionView::TestCase
+end
