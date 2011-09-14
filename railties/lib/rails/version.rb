3f9665be11f9938baa9f617f3391cc0d7e3ff10b
Santiago Pastorino <santiago@wyeworks.com>
2011-09-14 16:01:48 -0700

    Bumping up to 3.1.1.rc1

--- railties/lib/rails/version.rb
+++ railties/lib/rails/version.rb
@@ -2,8 +2,8 @@ module Rails
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 1
-    TINY  = 0
-    PRE   = nil
+    TINY  = 1
+    PRE   = "rc1"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
