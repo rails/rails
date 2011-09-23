66fa70fc388fffa1a083a7d608c59d997a116aac
Ryan Oblak <rroblak@gmail.com>
2011-09-22 18:35:15 -0700

    Added tests to ensure correct NameError handling in constantize and safe_constantize

--- activesupport/test/constantize_test_cases.rb
+++ activesupport/test/constantize_test_cases.rb
@@ -15,6 +15,14 @@ module ConstantizeTestCases
     assert_raise(NameError) { yield("An invalid string") }
     assert_raise(NameError) { yield("InvalidClass\n") }
     assert_raise(NameError) { yield("Ace::Base::ConstantizeTestCases") }
+    
+    # any NameError it raises should have name set to the full specified String (not just part of it)
+    begin
+      yield("Ace::Base::Blargle")
+      assert false
+    rescue NameError => e
+      assert_equal "Ace::Base::Blargle", e.name
+    end
   end
   
   def run_safe_constantize_tests_on
@@ -27,5 +35,14 @@ module ConstantizeTestCases
     assert_nothing_raised { assert_equal nil, yield("InvalidClass\n") }
     assert_nothing_raised { assert_equal nil, yield("blargle") }
     assert_nothing_raised { assert_equal nil, yield("Ace::Base::ConstantizeTestCases") }
+    
+    # should re-raise any NameError it encounters that doesn't correspond to the specified String
+    NameError.any_instance.stubs(:name).returns("not-blargle")
+    begin
+      yield("blargle")
+      assert false
+    rescue NameError => e
+      assert_equal "not-blargle", e.name
+    end
   end
 end
