cbc9d25cac2b66a587e6abf5caf2fb7faa3e82cc
kennyj <kennyj@gmail.com>
2011-10-05 03:05:21 +0900

    fixed #2980. I think that this problem is 2 pieces. Testcase problem and config missing problem.

--- actionpack/lib/sprockets/helpers/rails_helper.rb
+++ actionpack/lib/sprockets/helpers/rails_helper.rb
@@ -8,8 +8,7 @@ module Sprockets
 
       def asset_paths
         @asset_paths ||= begin
-          config     = self.config if respond_to?(:config)
-          config   ||= Rails.application.config
+          config     = Rails.application.config
           controller = self.controller if respond_to?(:controller)
           paths = RailsHelper::AssetPaths.new(config, controller)
           paths.asset_environment = asset_environment
