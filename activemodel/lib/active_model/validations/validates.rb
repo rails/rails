78bab03804c05cf36554bce1835545eac14f2eb3
Evgeniy Dolzhenko <dolzenko@gmail.com>
2011-10-11 14:13:08 +0100

    Use .add instead of << to add errors

--- activemodel/lib/active_model/validations/validates.rb
+++ activemodel/lib/active_model/validations/validates.rb
@@ -27,7 +27,7 @@ module ActiveModel
       #
       #   class EmailValidator < ActiveModel::EachValidator
       #     def validate_each(record, attribute, value)
-      #       record.errors[attribute] << (options[:message] || "is not an email") unless
+      #       record.errors.add attribute, (options[:message] || "is not an email") unless
       #         value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
       #     end
       #   end
@@ -48,7 +48,7 @@ module ActiveModel
       #
       #     class TitleValidator < ActiveModel::EachValidator
       #       def validate_each(record, attribute, value)
-      #         record.errors[attribute] << "must start with 'the'" unless value =~ /\Athe/i
+      #         record.errors.add attribute, "must start with 'the'" unless value =~ /\Athe/i
       #       end
       #     end
       #
