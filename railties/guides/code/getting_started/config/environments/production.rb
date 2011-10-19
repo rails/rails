1359152345e84b951becf687ab6f26e30a9af3ce
David Heinemeier Hansson <david@loudthinking.com>
2011-10-19 16:16:44 -0500

    Encourage use of tagged logging even when using a different logger

--- railties/guides/code/getting_started/config/environments/production.rb
+++ railties/guides/code/getting_started/config/environments/production.rb
@@ -37,7 +37,7 @@ Blog::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups
-  # config.logger = SyslogLogger.new
+  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
