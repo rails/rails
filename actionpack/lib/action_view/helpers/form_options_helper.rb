902764379838d0d2b98e262c4039b7069cada3f8
Aaron Patterson <aaron.patterson@gmail.com>
2011-10-26 10:30:14 -0700

    scumbag 1.8: strings are enumerable :'(

--- actionpack/lib/action_view/helpers/form_options_helper.rb
+++ actionpack/lib/action_view/helpers/form_options_helper.rb
@@ -584,7 +584,7 @@ module ActionView
         #   [nil, []]
         #   { nil => [] }
         #
-        if !choices.empty? && choices.first.last.respond_to?(:each)
+        if !choices.empty? && Array === choices.first.last
           option_tags = grouped_options_for_select(choices, :selected => selected_value, :disabled => options[:disabled])
         else
           option_tags = options_for_select(choices, :selected => selected_value, :disabled => options[:disabled])
