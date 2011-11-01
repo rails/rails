ff9c2799c1a065fb132df64da6d19683c647b5b4
Laust Rud Jacobsen <laust@object.io>
2011-11-01 07:29:48 +0100

    dump_schema_information: explicitly order inserts into schema_migrations table
    This change reduces churn in the db/development_structure.sql file when using :sql as
    active_record.schema_format, and makes comparing diffs much easier.
    
    Test ensures the output SQL-statements are lexically ordered by version.

--- activerecord/lib/active_record/connection_adapters/abstract/schema_statements.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/schema_statements.rb
@@ -405,7 +405,7 @@ module ActiveRecord
 
       def dump_schema_information #:nodoc:
         sm_table = ActiveRecord::Migrator.schema_migrations_table_name
-        migrated = select_values("SELECT version FROM #{sm_table}")
+        migrated = select_values("SELECT version FROM #{sm_table} ORDER BY version")
         migrated.map { |v| "INSERT INTO #{sm_table} (version) VALUES ('#{v}');" }.join("\n\n")
       end
 
