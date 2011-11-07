f50ee78e9cfdaa66196ffcfd8115a31dfec418a0
Akira Matsuda <ronnie@dio.jp>
2011-11-07 12:30:22 +0900

    documentations for 3.2 style migration

--- railties/guides/code/getting_started/db/migrate/20110901012815_create_comments.rb
+++ railties/guides/code/getting_started/db/migrate/20110901012815_create_comments.rb
@@ -1,11 +1,11 @@
 class CreateComments < ActiveRecord::Migration
   def change
-    create_table :comments do |t|
-      t.string :commenter
-      t.text :body
-      t.references :post
+    create_table :comments do
+      string :commenter
+      text :body
+      references :post
 
-      t.timestamps
+      timestamps
     end
     add_index :comments, :post_id
   end
