42cf5eba260f97e6da0ef66b954d95b9e7c2b102
Akira Matsuda <ronnie@dio.jp>
2011-11-07 14:31:12 +0900

    Remove redundant blank line at the bottom

--- railties/lib/rails/generators/rails/controller/templates/controller.rb
+++ railties/lib/rails/generators/rails/controller/templates/controller.rb
@@ -3,7 +3,7 @@ class <%= class_name %>Controller < ApplicationController
 <% actions.each do |action| -%>
   def <%= action %>
   end
-
+<%= "\n" unless action == actions.last -%>
 <% end -%>
 end
 <% end -%>
