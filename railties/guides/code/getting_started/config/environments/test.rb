0af93089deaf6dc9428c0f2f7a376cdb6f81daee
Alex Tambellini <atambellini@gmail.com>
2011-11-17 09:44:17 -0500

    Move schema_format :sql config setting from test.rb to application.rb
    I've moved the schema_format :sql config setting to application.rb because you would
    never enable this only for the test environment. If you use database constraints
    or database specific data types you would want all of your environments to use them.

--- railties/guides/code/getting_started/config/environments/test.rb
+++ railties/guides/code/getting_started/config/environments/test.rb
@@ -29,11 +29,6 @@ Blog::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Use SQL instead of Active Record's schema dumper when creating the test database.
-  # This is necessary if your schema can't be completely dumped by the schema dumper,
-  # like if you have constraints or database-specific column types
-  # config.active_record.schema_format = :sql
-
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
