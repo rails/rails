rails/rails

commit 38d26b0cb56d82093889efa95992a35ba3bb9f29

Move conditionals to separate tasks so they can be reused.

activerecord/lib/active_record/railties/databases.rake
@@ -291,15 +291,11 @@ db_namespace = namespace :db do
   end
 
   desc 'Create the database, load the schema, and initialize with the seed data (use db:reset to also drop the db first)'
-  task :setup => :environment do
-    db_namespace["create"].invoke
-    db_namespace["schema:load"].invoke if ActiveRecord::Base.schema_format == :ruby
-    db_namespace["structure:load"].invoke if ActiveRecord::Base.schema_format == :sql
-    db_namespace["seed"].invoke
-  end
+  task :setup => :seed
 
   desc 'Load the seed data from db/seeds.rb'
-  task :seed => 'db:abort_if_pending_migrations' do
+  task :seed => ['db:schema:load_if_ruby', 'db:structure:load_if_sql'] do
+    db_namespace['abort_if_pending_migrations'].invoke
     Rails.application.load_seed
   end
 
@@ -361,6 +357,10 @@ db_namespace = namespace :db do
       else
         abort %{#{file} doesn't exist yet. Run `rake db:migrate` to create it then try again. If you do not intend to use a database, you should instead alter #{Rails.root}/config/application.rb to limit the frameworks that will be loaded}
       end
+    end
+
+    task :load_if_ruby => 'db:create' do
+      db_namespace["schema:load"].invoke if ActiveRecord::Base.schema_format == :ruby
     end
   end
 
@@ -436,6 +436,10 @@ db_namespace = namespace :db do
       else
         raise "Task not supported by '#{abcs[env]['adapter']}'"
       end
+    end
+
+    task :load_if_sql => 'db:create' do
+      db_namespace["structure:load"].invoke if ActiveRecord::Base.schema_format == :sql
     end
   end
 

commit 42954c426b2338bf90f286b8e425b2e192d483ae

Stop db:seed from performing a reload of the db Fix regression introduced by 38d26b0cb56d82093889efa95992a35ba3bb9f29

activerecord/lib/active_record/railties/databases.rake
@@ -295,10 +295,10 @@ db_namespace = namespace :db do
   end
 
   desc 'Create the database, load the schema, and initialize with the seed data (use db:reset to also drop the db first)'
-  task :setup => :seed
+  task :setup => ['db:schema:load_if_ruby', 'db:structure:load_if_sql', :seed]
 
   desc 'Load the seed data from db/seeds.rb'
-  task :seed => ['db:schema:load_if_ruby', 'db:structure:load_if_sql'] do
+  task :seed do
     db_namespace['abort_if_pending_migrations'].invoke
     Rails.application.load_seed
   end