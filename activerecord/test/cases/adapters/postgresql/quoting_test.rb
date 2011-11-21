06c23c4c7ff842f7c6237f3ac43fc9d19509a947
Sven Bohm <bohms@msu.edu>
2011-11-21 13:14:16 -0500

    postgresql adapter handles quoting of not a number (NaN) and Infinity

--- activerecord/test/cases/adapters/postgresql/quoting_test.rb
+++ activerecord/test/cases/adapters/postgresql/quoting_test.rb
@@ -19,6 +19,18 @@ module ActiveRecord
           assert_equal 'f', @conn.type_cast(false, nil)
           assert_equal 'f', @conn.type_cast(false, c)
         end
+
+        def test_quote_float_nan
+          nan = 0.0/0
+          c = Column.new(nil, 1, 'float')
+          assert_equal "'NaN'", @conn.quote(nan, c)
+        end
+
+        def test_quote_float_infinity
+          infinity = 1.0/0
+          c = Column.new(nil, 1, 'float')
+          assert_equal "'Infinity'", @conn.quote(infinity, c)
+        end
       end
     end
   end
