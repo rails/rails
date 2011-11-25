696d01f7f4a8ed787924a41cce6df836cd73c46f
José Valim <jose.valim@gmail.com>
2011-11-25 09:49:54 +0000

    Add docs to serializers. Update CHANGELOGs.

--- actionpack/lib/action_controller/metal/serialization.rb
+++ actionpack/lib/action_controller/metal/serialization.rb
@@ -1,4 +1,27 @@
 module ActionController
+  # Action Controller Serialization
+  #
+  # Overrides render :json to check if the given object implements +active_model_serializer+
+  # as a method. If so, use the returned serializer instead of calling +to_json+ in the object.
+  #
+  # This module also provides a serialization_scope method that allows you to configure the
+  # +serialization_scope+ of the serializer. Most apps will likely set the +serialization_scope+
+  # to the current user:
+  #
+  #    class ApplicationController < ActionController::Base
+  #      serialization_scope :current_user
+  #    end
+  #
+  # If you need more complex scope rules, you can simply override the serialization_scope:
+  #
+  #    class ApplicationController < ActionController::Base
+  #      private
+  #      
+  #      def serialization_scope
+  #        current_user
+  #      end
+  #    end
+  #
   module Serialization
     extend ActiveSupport::Concern
 
