ebb8ea22f30dc029f4a868a550828443b160f639
José Valim <jose.valim@gmail.com>
2011-11-25 09:13:19 +0000

    Add generators for serializers.

---
+++ railties/lib/rails/generators/test_unit/serializer/templates/unit_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+<% module_namespacing do -%>
+class <%= class_name %>SerializerTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
+<% end -%>
