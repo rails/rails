2815d8102b3f473ca57b845fee7ebc59ffd74e63
Aaron Patterson <aaron.patterson@gmail.com>
2011-11-29 13:15:20 -0800

    Leased connections return false on second lease

--- activerecord/test/cases/connection_adapters/abstract_adapter_test.rb
+++ activerecord/test/cases/connection_adapters/abstract_adapter_test.rb
@@ -3,13 +3,22 @@ require "cases/helper"
 module ActiveRecord
   module ConnectionAdapters
     class AbstractAdapterTest < ActiveRecord::TestCase
-      def test_in_use?
-        adapter = AbstractAdapter.new nil, nil
+      attr_reader :adapter
 
+      def setup
+        @adapter = AbstractAdapter.new nil, nil
+      end
+
+      def test_in_use?
         # FIXME: change to refute in Rails 4.0 / mt
         assert !adapter.in_use?, 'adapter is not in use'
         assert adapter.lease, 'lease adapter'
         assert adapter.in_use?, 'adapter is in use'
+      end
+
+      def test_lease_twice
+        assert adapter.lease, 'should lease adapter'
+        assert !adapter.lease, 'should not lease adapter'
       end
     end
   end
