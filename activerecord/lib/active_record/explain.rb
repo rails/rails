f675d1a2cbbaa4a8284be1e895189ae4659ffad4
José Valim <jose.valim@gmail.com>
2011-12-03 12:45:09 +0100

    Use the log subscriber instead of subscribing for a block which expires notifications caches.

--- activerecord/lib/active_record/explain.rb
+++ activerecord/lib/active_record/explain.rb
@@ -2,35 +2,41 @@ module ActiveRecord
   module Explain
     extend ActiveSupport::Concern
 
-    # available_queries_for_explain collects the queries available for
-    # explain. If the value is nil, it means queries are not being
-    # currently collected. A false value indicates collecting is turned
-    # off. Otherwise it is an array of queries.
-    def self.available_queries_for_explain
-      Thread.current[:available_queries_for_explain]
-    end
-
     module ClassMethods
       # If auto explain is enabled, this method triggers EXPLAIN logging for the
       # queries triggered by the block if it takes more than the threshold as a
       # whole. That is, the threshold is not checked against each individual
       # query, but against the duration of the entire block. This approach is
       # convenient for relations.
+      #
+      # available_queries_for_explain collects the queries available for
+      # explain. If the value is nil, it means queries are not being
+      # currently collected. A false value indicates collecting is turned
+      # off. Otherwise it is an array of queries.
       def logging_query_plan(&block) # :nodoc:
         threshold = auto_explain_threshold_in_seconds
         current   = Thread.current
         if threshold && current[:available_queries_for_explain].nil?
           begin
-            current[:available_queries_for_explain] = []
+            value = current[:available_queries_for_explain] = []
             start = Time.now
-            result, sqls, binds = collecting_sqls_for_explain(&block)
-            logger.warn(exec_explain(sqls, binds)) if Time.now - start > threshold
+            result = yield
+            logger.warn(exec_explain(value)) if Time.now - start > threshold
             result
           ensure
             current[:available_queries_for_explain] = nil
           end
         else
           yield
+        end
+      end
+
+      # Receives a payload and check if the SQL query should be collected.
+      def collect_sql_for_explain(payload)
+        if value = Thread.current[:available_queries_for_explain]
+          unless ignore_explain_notification?(payload)
+            value << [payload[:sql], payload[:binds]]
+          end
         end
       end
 
@@ -41,32 +47,10 @@ module ActiveRecord
         payload[:exception] || SKIP_EXPLAIN_FOR.include?(payload[:name])
       end
 
-      # Collects all queries executed while the passed block runs. Returns an
-      # array with three elements, the result of the block, the strings with the
-      # queries, and their respective bindings.
-      def collecting_sqls_for_explain # :nodoc:
-        sqls  = []
-        binds = []
-        callback = lambda do |*args|
-          payload = args.last
-          unless ignore_explain_notification?(payload)
-            sqls  << payload[:sql]
-            binds << payload[:binds]
-          end
-        end
-
-        result = nil
-        ActiveSupport::Notifications.subscribed(callback, "sql.active_record") do
-          result = yield
-        end
-
-        [result, sqls, binds]
-      end
-
-      # Makes the adapter execute EXPLAIN for the given queries and bindings.
+      # Makes the adapter execute EXPLAIN for the tuples of queries and bindings.
       # Returns a formatted string ready to be logged.
-      def exec_explain(sqls, binds) # :nodoc:
-        sqls.zip(binds).map do |sql, bind|
+      def exec_explain(value) # :nodoc:
+        value && value.map do |sql, bind|
           [].tap do |msg|
             msg << "EXPLAIN for: #{sql}"
             unless bind.empty?
