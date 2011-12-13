1f3ab1aea4c4b894b5cc8d5293ad5e7ef11c93fd
José Valim <jose.valim@gmail.com>
2011-12-13 15:39:58 +0100

    Fix failing AP tests.

--- actionpack/test/dispatch/routing_test.rb
+++ actionpack/test/dispatch/routing_test.rb
@@ -2296,7 +2296,7 @@ class TestRoutingMapper < ActionDispatch::IntegrationTest
 
   def test_named_routes_collision_is_avoided_unless_explicitly_given_as
     assert_equal "/c/1", routes_collision_path(1)
-    assert_equal "/forced_collision", routes_forced_collision_path
+    assert_equal "/fc", routes_forced_collision_path
   end
 
   def test_redirect_argument_error
