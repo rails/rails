b83ced3d67968fcded81bbb9b7f47a3f6ff2a012
José Valim <jose.valim@gmail.com>
2011-12-20 18:33:28 +0100

    Remove dead code from AMo.

--- activemodel/lib/active_model/test_case.rb
+++ activemodel/lib/active_model/test_case.rb
@@ -1,16 +1,4 @@
 module ActiveModel #:nodoc:
   class TestCase < ActiveSupport::TestCase #:nodoc:
-    def with_kcode(kcode)
-      if RUBY_VERSION < '1.9'
-        orig_kcode, $KCODE = $KCODE, kcode
-        begin
-          yield
-        ensure
-          $KCODE = orig_kcode
-        end
-      else
-        yield
-      end
-    end
   end
 end
