16dbe1db1eb0ebf6111d9938b88d1f0652b78734
Jon Leighton <j@jonathanleighton.com>
2011-12-22 19:22:59 +0000

    Make ActiveRecord::Relation#pluck work with serialized attributes

--- activerecord/lib/active_record/relation/calculations.rb
+++ activerecord/lib/active_record/relation/calculations.rb
@@ -168,7 +168,7 @@ module ActiveRecord
 
     # This method is designed to perform select by a single column as direct SQL query
     # Returns <tt>Array</tt> with values of the specified column name
-    # The values has same data type as column. 
+    # The values has same data type as column.
     #
     # Examples:
     #
@@ -177,9 +177,9 @@ module ActiveRecord
     #   Person.where(:confirmed => true).limit(5).pluck(:id)
     #
     def pluck(column_name)
-      scope = self.select(column_name)
-      self.connection.select_values(scope.to_sql).map! do |value|
-        type_cast_using_column(value, column_for(column_name))
+      column_name = column_name.to_s
+      klass.connection.select_all(select(column_name).arel).map! do |attributes|
+        klass.type_cast_attribute(attributes.keys.first, klass.initialize_attributes(attributes))
       end
     end
 
