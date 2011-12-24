98fb94fa44de0a0115f8629f8a5f4b739afedcae
Vishnu Atrai <me@vishnuatrai.com>
2011-12-25 00:09:48 +0530

    remove deprecated whiny nil related code

--- railties/guides/code/getting_started/config/environments/test.rb
+++ railties/guides/code/getting_started/config/environments/test.rb
@@ -11,9 +11,6 @@ Blog::Application.configure do
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
 
-  # Log error messages when you accidentally call methods on nil
-  config.whiny_nils = true
-
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
