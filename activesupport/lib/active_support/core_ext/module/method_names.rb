33fb719667d0e2799efc206b8fdb9359896ddb09
Sergey Nartimov <just.lest@gmail.com>
2011-12-25 20:52:05 +0300

    ruby 1.9 returns method names as symbols

--- activesupport/lib/active_support/core_ext/module/method_names.rb
+++ activesupport/lib/active_support/core_ext/module/method_names.rb
@@ -1,14 +1,9 @@
 class Module
-  if instance_methods[0].is_a?(Symbol)
-    def instance_method_names(*args)
-      instance_methods(*args).map(&:to_s)
-    end
+  def instance_method_names(*args)
+    instance_methods(*args).map { |name| name.to_s }
+  end
 
-    def method_names(*args)
-      methods(*args).map(&:to_s)
-    end
-  else
-    alias_method :instance_method_names, :instance_methods
-    alias_method :method_names, :methods
+  def method_names(*args)
+    methods(*args).map { |name| name.to_s }
   end
 end
