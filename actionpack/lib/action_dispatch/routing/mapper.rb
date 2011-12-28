623a48ea966f8518803da6552cf664c246de6ac5
buddhamagnet <buddhamagnet@gmail.com>
2011-12-28 22:58:29 +0000

    amended constraints block to do...end format

--- actionpack/lib/action_dispatch/routing/mapper.rb
+++ actionpack/lib/action_dispatch/routing/mapper.rb
@@ -25,13 +25,13 @@ module ActionDispatch
         def matches?(env)
           req = @request.new(env)
 
-          @constraints.each { |constraint|
+          @constraints.each do |constraint|
             if constraint.respond_to?(:matches?) && !constraint.matches?(req)
               return false
             elsif constraint.respond_to?(:call) && !constraint.call(*constraint_args(constraint, req))
               return false
             end
-          }
+          end
 
           return true
         end
