59f2696d0012c451190a9e3fe1b92c3619dee1a2
Aaron Patterson <aaron.patterson@gmail.com>
2011-12-30 16:10:53 -0800

    rename start to run and use Thread.pass rather than sleeping to schedule the watchdog

--- activerecord/lib/active_record/connection_adapters/abstract/connection_pool.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/connection_pool.rb
@@ -75,7 +75,7 @@ module ActiveRecord
           @frequency = frequency
         end
 
-        def start
+        def run
           return unless frequency
           Thread.new(frequency, pool) { |t, p|
             while true
@@ -107,7 +107,7 @@ module ActiveRecord
 
         @timeout = spec.config[:wait_timeout] || 5
         @reaper  = Reaper.new self, spec.config[:reaping_frequency]
-        @reaper.start
+        @reaper.run
 
         # default max pool size to 5
         @size = (spec.config[:pool] && spec.config[:pool].to_i) || 5
