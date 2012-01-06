d28a15ede59f6434f1b7a8d01be060fa73b4746c
Aaron Patterson <aaron.patterson@gmail.com>
2012-01-06 11:24:01 -0800

    refactor assert_response

--- actionpack/lib/action_dispatch/testing/assertions/response.rb
+++ actionpack/lib/action_dispatch/testing/assertions/response.rb
@@ -26,14 +26,17 @@ module ActionDispatch
       #   assert_response 401
       #
       def assert_response(type, message = nil)
-        if type.in?([:success, :missing, :redirect, :error]) && @response.send("#{type}?")
-          assert true # to count the assertion
-        elsif type.is_a?(Fixnum) && @response.response_code == type
-          assert true # to count the assertion
-        elsif type.is_a?(Symbol) && @response.response_code == Rack::Utils::SYMBOL_TO_STATUS_CODE[type]
-          assert true # to count the assertion
+        message ||= "Expected response to be a <#{type}>, but was <#{@response.response_code}>"
+
+        if Symbol === type
+          if [:success, :missing, :redirect, :error].include?(type)
+            assert @response.send("#{type}?"), message
+          else
+            code = Rack::Utils::SYMBOL_TO_STATUS_CODE[type]
+            assert_equal @response.response_code, code, message
+          end
         else
-          flunk "Expected response to be a <#{type}>, but was <#{@response.response_code}>"
+          assert_equal type, @response.response_code, message
         end
       end
 
