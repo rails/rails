cc88ffed7a2e17ff53eb8796bc1fa2536ebca00d
Aaron Patterson <aaron.patterson@gmail.com>
2012-01-11 12:01:04 -0800

    remove dependency on the filesystem

--- activerecord/test/cases/migration/logger_test.rb
+++ activerecord/test/cases/migration/logger_test.rb
@@ -3,12 +3,17 @@ require "cases/helper"
 module ActiveRecord
   class Migration
     class LoggerTest < ActiveRecord::TestCase
+      Migration = Struct.new(:version) do
+        def migrate direction
+          # do nothing
+        end
+      end
+
       def test_migration_should_be_run_without_logger
         previous_logger = ActiveRecord::Base.logger
         ActiveRecord::Base.logger = nil
-        assert_nothing_raised do
-          ActiveRecord::Migrator.migrate(MIGRATIONS_ROOT + "/valid")
-        end
+        migrations = [Migration.new(1), Migration.new(2), Migration.new(3)]
+        ActiveRecord::Migrator.new(:up, migrations).migrate
       ensure
         ActiveRecord::Base.logger = previous_logger
       end
