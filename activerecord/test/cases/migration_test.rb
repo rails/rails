66fe5072fd63b8781cd9adff9fb4d6203dde4a7f
Aaron Patterson <aaron.patterson@gmail.com>
2012-01-11 17:19:23 -0800

    use ruby rather than mocha

--- activerecord/test/cases/migration_test.rb
+++ activerecord/test/cases/migration_test.rb
@@ -70,7 +70,9 @@ class MigrationTest < ActiveRecord::TestCase
     # using a copy as we need the drop_table method to
     # continue to work for the ensure block of the test
     temp_conn = Person.connection.dup
-    temp_conn.expects(:drop_table).never
+    temp_conn.extend(Module.new {
+      def drop_table; raise "no"; end
+    })
     temp_conn.create_table :testings2, :force => true do |t|
       t.column :foo, :string
     end
