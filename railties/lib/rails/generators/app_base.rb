70631e723ca9503b387f25076b9fb7f68726f81e
José Valim <jose.valim@gmail.com>
2012-01-13 06:25:43 -0800

    Merge pull request #4451 from guilleiguaran/add-therubyracer-commented

--- railties/lib/rails/generators/app_base.rb
+++ railties/lib/rails/generators/app_base.rb
@@ -202,7 +202,9 @@ module Rails
             group :assets do
               gem 'sass-rails',   :git => 'git://github.com/rails/sass-rails.git',   :branch => '3-2-stable'
               gem 'coffee-rails', :git => 'git://github.com/rails/coffee-rails.git', :branch => '3-2-stable'
-              #{"gem 'therubyrhino'\n" if defined?(JRUBY_VERSION)}
+
+              # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+              #{javascript_runtime_gemfile_entry}
               gem 'uglifier', '>= 1.0.3'
             end
           GEMFILE
@@ -213,7 +215,9 @@ module Rails
             group :assets do
               gem 'sass-rails',   '~> 3.2.3'
               gem 'coffee-rails', '~> 3.2.1'
-              #{"gem 'therubyrhino'\n" if defined?(JRUBY_VERSION)}
+
+              # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+              #{javascript_runtime_gemfile_entry}
               gem 'uglifier', '>= 1.0.3'
             end
           GEMFILE
@@ -224,6 +228,14 @@ module Rails
 
       def javascript_gemfile_entry
         "gem '#{options[:javascript]}-rails'" unless options[:skip_javascript]
+      end
+
+      def javascript_runtime_gemfile_entry
+        if defined?(JRUBY_VERSION)
+          "gem 'therubyrhino'\n"
+        else
+          "# gem 'therubyracer'\n"
+        end
       end
 
       def bundle_command(command)
