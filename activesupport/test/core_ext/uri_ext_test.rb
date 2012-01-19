b7e88aaf98bedf9486316b8bf3abfac1eb67e1cb
kennyj <kennyj@gmail.com>
2012-01-19 19:38:23 +0900

    Convert URI.parser.parse to URI.parse, and remove ruby 1.8.x code.

--- activesupport/test/core_ext/uri_ext_test.rb
+++ activesupport/test/core_ext/uri_ext_test.rb
@@ -7,11 +7,7 @@ class URIExtTest < ActiveSupport::TestCase
   def test_uri_decode_handle_multibyte
     str = "\xE6\x97\xA5\xE6\x9C\xAC\xE8\xAA\x9E" # Ni-ho-nn-go in UTF-8, means Japanese.
 
-    if URI.const_defined?(:Parser)
-      parser = URI::Parser.new
-      assert_equal str, parser.unescape(parser.escape(str))
-    else
-      assert_equal str, URI.unescape(URI.escape(str))
-    end
+    parser = URI::Parser.new
+    assert_equal str, parser.unescape(parser.escape(str))
   end
 end
