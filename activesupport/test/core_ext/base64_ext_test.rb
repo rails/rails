912f14419076c26c1d281848b74048cfcfd1d85f
Vijay Dev <vijaydev.cse@gmail.com>
2012-01-23 16:17:56 +0530

    test base64 encode and decode

--- activesupport/test/core_ext/base64_ext_test.rb
+++ activesupport/test/core_ext/base64_ext_test.rb
@@ -7,4 +7,13 @@ class Base64Test < Test::Unit::TestCase
       assert_no_match(/\n/, ActiveSupport::Base64.encode64s("oneverylongstringthatwouldnormallybesplitupbynewlinesbytheregularbase64"))
     end
   end
+
+  def test_encode_and_decode
+    ActiveSupport::Deprecation.silence do
+      string = "foobar"
+      encoded_string = ActiveSupport::Base64.encode64(string)
+      assert_equal "Zm9vYmFy\n", encoded_string
+      assert_equal string, ActiveSupport::Base64.decode64(encoded_string)
+    end
+  end
 end
