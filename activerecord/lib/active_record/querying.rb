8f1d0efb46cc8c796e0c33385904decb56572ba1
Brian Underwood <bunderwood@rbmtechnologies.com>
2012-01-30 10:24:19 -0500

    Add all_exist? class method method for models and relations which determines if all IDs/objects specified exist in the scope of the query

--- activerecord/lib/active_record/querying.rb
+++ activerecord/lib/active_record/querying.rb
@@ -2,7 +2,7 @@ require 'active_support/core_ext/module/delegation'
 
 module ActiveRecord
   module Querying
-    delegate :find, :first, :first!, :last, :last!, :all, :exists?, :any?, :many?, :to => :scoped
+    delegate :find, :first, :first!, :last, :last!, :all, :exists?, :all_exist?, :any?, :many?, :to => :scoped
     delegate :first_or_create, :first_or_create!, :first_or_initialize, :to => :scoped
     delegate :destroy, :destroy_all, :delete, :delete_all, :update, :update_all, :to => :scoped
     delegate :find_each, :find_in_batches, :to => :scoped
