ca9dff943e1a1e73892bded100be672ddae782e2
Bogdan Gusiev <agresso@gmail.com>
2012-02-05 12:31:02 +0200

    AS::Callbacks: remove unneeded line of code

--- activesupport/lib/active_support/callbacks.rb
+++ activesupport/lib/active_support/callbacks.rb
@@ -145,7 +145,6 @@ module ActiveSupport
         deprecate_per_key_option(_options)
         _update_filter(self.options, _options)
 
-        @filter           = _compile_filter(@raw_filter)
         @compiled_options = _compile_options(@options)
       end
 
