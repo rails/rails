33514a173c5ab59325db8427183f6d46e18cedb1
Jeremy Kemper <jeremy@bitsweat.net>
2012-02-05 23:09:09 -0700

    Get ActiveRecord::TestCase back on its feet, despite deprecation. It requires SQLCounter which was moved to AR internal tests only.

--- activerecord/lib/active_record/test_case.rb
+++ activerecord/lib/active_record/test_case.rb
@@ -14,7 +14,7 @@ module ActiveRecord
     end
 
     def teardown
-      ActiveRecord::SQLCounter.log.clear
+      SQLCounter.log.clear
     end
 
     def cleanup_identity_map
@@ -30,5 +30,65 @@ module ActiveRecord
         assert_equal expected.to_s, actual.to_s, message
       end
     end
+
+    def assert_sql(*patterns_to_match)
+      SQLCounter.log = []
+      yield
+      SQLCounter.log
+    ensure
+      failed_patterns = []
+      patterns_to_match.each do |pattern|
+        failed_patterns << pattern unless SQLCounter.log.any?{ |sql| pattern === sql }
+      end
+      assert failed_patterns.empty?, "Query pattern(s) #{failed_patterns.map{ |p| p.inspect }.join(', ')} not found.#{SQLCounter.log.size == 0 ? '' : "\nQueries:\n#{SQLCounter.log.join("\n")}"}"
+    end
+
+    def assert_queries(num = 1)
+      SQLCounter.log = []
+      yield
+    ensure
+      assert_equal num, SQLCounter.log.size, "#{SQLCounter.log.size} instead of #{num} queries were executed.#{SQLCounter.log.size == 0 ? '' : "\nQueries:\n#{SQLCounter.log.join("\n")}"}"
+    end
+
+    def assert_no_queries(&block)
+      prev_ignored_sql = SQLCounter.ignored_sql
+      SQLCounter.ignored_sql = []
+      assert_queries(0, &block)
+    ensure
+      SQLCounter.ignored_sql = prev_ignored_sql
+    end
+
   end
+
+  class SQLCounter
+    class << self
+      attr_accessor :ignored_sql, :log
+    end
+
+    self.log = []
+
+    self.ignored_sql = [/^PRAGMA (?!(table_info))/, /^SELECT currval/, /^SELECT CAST/, /^SELECT @@IDENTITY/, /^SELECT @@ROWCOUNT/, /^SAVEPOINT/, /^ROLLBACK TO SAVEPOINT/, /^RELEASE SAVEPOINT/, /^SHOW max_identifier_length/, /^BEGIN/, /^COMMIT/]
+
+    # FIXME: this needs to be refactored so specific database can add their own
+    # ignored SQL.  This ignored SQL is for Oracle.
+    ignored_sql.concat [/^select .*nextval/i, /^SAVEPOINT/, /^ROLLBACK TO/, /^\s*select .* from all_triggers/im]
+
+
+    attr_reader :ignore
+
+    def initialize(ignore = Regexp.union(self.class.ignored_sql))
+      @ignore = ignore
+    end
+
+    def call(name, start, finish, message_id, values)
+      sql = values[:sql]
+
+      # FIXME: this seems bad. we should probably have a better way to indicate
+      # the query was cached
+      return if 'CACHE' == values[:name] || ignore =~ sql
+      self.class.log << sql
+    end
+  end
+
+  ActiveSupport::Notifications.subscribe('sql.active_record', SQLCounter.new)
 end
--- activerecord/test/cases/helper.rb
+++ activerecord/test/cases/helper.rb
@@ -61,37 +61,6 @@ ensure
   ActiveRecord::Base.default_timezone = old_zone
 end
 
-module ActiveRecord
-  class SQLCounter
-    cattr_accessor :ignored_sql
-    self.ignored_sql = [/^PRAGMA (?!(table_info))/, /^SELECT currval/, /^SELECT CAST/, /^SELECT @@IDENTITY/, /^SELECT @@ROWCOUNT/, /^SAVEPOINT/, /^ROLLBACK TO SAVEPOINT/, /^RELEASE SAVEPOINT/, /^SHOW max_identifier_length/, /^BEGIN/, /^COMMIT/]
-
-    # FIXME: this needs to be refactored so specific database can add their own
-    # ignored SQL.  This ignored SQL is for Oracle.
-    ignored_sql.concat [/^select .*nextval/i, /^SAVEPOINT/, /^ROLLBACK TO/, /^\s*select .* from all_triggers/im]
-
-    cattr_accessor :log
-    self.log = []
-
-    attr_reader :ignore
-
-    def initialize(ignore = Regexp.union(self.class.ignored_sql))
-      @ignore = ignore
-    end
-
-    def call(name, start, finish, message_id, values)
-      sql = values[:sql]
-
-      # FIXME: this seems bad. we should probably have a better way to indicate
-      # the query was cached
-      return if 'CACHE' == values[:name] || ignore =~ sql
-      self.class.log << sql
-    end
-  end
-
-  ActiveSupport::Notifications.subscribe('sql.active_record', SQLCounter.new)
-end
-
 unless ENV['FIXTURE_DEBUG']
   module ActiveRecord::TestFixtures::ClassMethods
     def try_to_load_dependency_with_silence(*args)
