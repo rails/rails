5bfee55947909506d133c9b336da989fd4903bd7
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-02-07 00:15:20 -0500

    --edge option should generate app with rails 3-2-stable

--- railties/lib/rails/generators/app_base.rb
+++ railties/lib/rails/generators/app_base.rb
@@ -143,7 +143,7 @@ module Rails
           GEMFILE
         elsif options.edge?
           <<-GEMFILE.strip_heredoc
-            gem 'rails',     :git => 'git://github.com/rails/rails.git'
+            gem 'rails',     :git => 'git://github.com/rails/rails.git', :branch => '3-2-stable'
             gem 'journey',   :git => 'git://github.com/rails/journey.git'
             gem 'arel',      :git => 'git://github.com/rails/arel.git'
           GEMFILE
