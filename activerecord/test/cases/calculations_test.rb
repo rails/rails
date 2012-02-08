78a6b6a5fd372254c7b49009fb1f573b677b634a
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-02-08 16:34:29 -0200

    PostgreSQL does not work in the same way of the other adapters

--- activerecord/test/cases/calculations_test.rb
+++ activerecord/test/cases/calculations_test.rb
@@ -485,8 +485,12 @@ class CalculationsTest < ActiveRecord::TestCase
   end
 
   def test_pluck_not_auto_table_name_prefix_if_column_joined
-    c = Company.create!(:name => "test", :contracts => [Contract.new(:developer_id => 7)])
-    # No chance for typecast here
-    assert_equal ["7"], Company.joins(:contracts).pluck(:developer_id)
+    Company.create!(:name => "test", :contracts => [Contract.new(:developer_id => 7)])
+    # FIXME: PostgreSQL should works in the same way of the other adapters
+    if current_adapter?(:PostgreSQLAdapter)
+      assert_equal ["7"], Company.joins(:contracts).pluck(:developer_id)
+    else
+      assert_equal [7], Company.joins(:contracts).pluck(:developer_id)
+    end
   end
 end
