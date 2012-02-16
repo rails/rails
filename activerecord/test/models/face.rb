389d1c5b65568e316ac55aa6534e782648855b92
Andy Pliszka <andy@isp44.com>
2012-02-16 00:46:23 -0500

    Test for circular reference while saving has_one relationship

--- activerecord/test/models/face.rb
+++ activerecord/test/models/face.rb
@@ -4,4 +4,6 @@ class Face < ActiveRecord::Base
   # These is a "broken" inverse_of for the purposes of testing
   belongs_to :horrible_man, :class_name => 'Man', :inverse_of => :horrible_face
   belongs_to :horrible_polymorphic_man, :polymorphic => true, :inverse_of => :horrible_polymorphic_face
+
+  accepts_nested_attributes_for :man
 end
