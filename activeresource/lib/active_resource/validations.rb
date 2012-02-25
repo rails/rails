d671b397398c1e439d68fa1cb3254b6da265b907
Salva Revert <salva@peertransfer.com>
2012-02-25 15:48:55 +0100

    Fix Errors#from_array when an attribute name starts like

--- activeresource/lib/active_resource/validations.rb
+++ activeresource/lib/active_resource/validations.rb
@@ -15,12 +15,11 @@ module ActiveResource
       clear unless save_cache
       humanized_attributes = Hash[@base.attributes.keys.map { |attr_name| [attr_name.humanize, attr_name] }]
       messages.each do |message|
-        attr_message = humanized_attributes.keys.detect do |attr_name|
+        attr_message = humanized_attributes.keys.sort_by {|a| a.length }.reverse.detect do |attr_name|
           if message[0, attr_name.size + 1] == "#{attr_name} "
             add humanized_attributes[attr_name], message[(attr_name.size + 1)..-1]
           end
         end
-
         self[:base] << message if attr_message.nil?
       end
     end
