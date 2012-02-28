7d8cd840c5201322090c94eca96d305159ea2450
Alexander Simonov <alex@simonov.me>
2012-02-28 11:33:54 +0200

    Use quoted_table_name and quoted_primary_key instead quoting strings

--- activerecord/lib/active_record/relation.rb
+++ activerecord/lib/active_record/relation.rb
@@ -409,7 +409,7 @@ module ActiveRecord
         where(conditions).delete_all
       else
         statement = if joins_values.present?
-          pk = "#{@klass.connection.quote_table_name table_name}.#{@klass.connection.quote_column_name primary_key}"
+          pk = "#{@klass.connection.quoted_table_name}.#{@klass.connection.quoted_primary_key}"
           ids = select(pk)
           unscoped { @klass.where("#{pk} IN (SELECT * FROM (#{ids.to_sql}) AS derived_#{table_name}_#{primary_key}_ids)").build_arel.compile_delete }
         else
