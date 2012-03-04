0d3d72502761cdb9f16c390b7eb51c8633139eb6
J T Calhoun <jtcalhoun@tacostadium.com>
2012-03-03 18:35:24 -0600

    use read_attribute to allow scoping by an aggregation's attribute value instead of its converted value object

--- activerecord/lib/active_record/validations/uniqueness.rb
+++ activerecord/lib/active_record/validations/uniqueness.rb
@@ -26,7 +26,7 @@ module ActiveRecord
         relation = relation.and(table[finder_class.primary_key.to_sym].not_eq(record.send(:id))) if record.persisted?
 
         Array(options[:scope]).each do |scope_item|
-          scope_value = record.send(scope_item)
+          scope_value = record.read_attribute(scope_item)
           reflection = record.class.reflect_on_association(scope_item)
           if reflection
             scope_value = record.send(reflection.foreign_key)
