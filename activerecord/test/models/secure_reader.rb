f18c0547b0a7ff97fec78cb1f0c95c2531290a94
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-03-05 17:00:48 -0300

    Not need to pass join attributes to association build

--- activerecord/test/models/secure_reader.rb
+++
@@ -1,9 +0,0 @@
-class SecureReader < ActiveRecord::Base
-  self.table_name = "readers"
-
-  belongs_to :secure_post, :class_name => "Post", :foreign_key => "post_id"
-  belongs_to :secure_person, :inverse_of => :secure_readers, :class_name => "Person", :foreign_key => "person_id"
-
-
-  attr_accessible nil
-end
