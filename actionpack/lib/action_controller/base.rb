945ee359331a0058d34e1d203b9ebe1cc0af19a1
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2012-03-06 09:04:06 -0300

    Add some docs for ActionController::Base.without_modules

--- actionpack/lib/action_controller/base.rb
+++ actionpack/lib/action_controller/base.rb
@@ -171,6 +171,16 @@ module ActionController
   class Base < Metal
     abstract!
 
+    # Shortcut helper to map all ActionController default modules except the ones given:
+    #
+    #   class MetalController
+    #     ActionController::Base.without_modules(:ParamsWrapper, :Streaming).each do |module|
+    #       include module
+    #     end
+    #   end
+    #
+    # This gives better control over what you want to exclude and makes it easier
+    # to create a bare controller class, instead of listing each required module manually.
     def self.without_modules(*modules)
       modules = modules.map do |m|
         m.is_a?(Symbol) ? ActionController.const_get(m) : m
