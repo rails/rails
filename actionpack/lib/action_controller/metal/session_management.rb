74fe7e1018585ed9ea086952f578079bb7577652
Santiago Pastorino <santiago@wyeworks.com>
2012-03-06 18:13:30 -0200

    Deprecate ActionController::SessionManagement

--- actionpack/lib/action_controller/metal/session_management.rb
+++ actionpack/lib/action_controller/metal/session_management.rb
@@ -2,6 +2,11 @@ module ActionController #:nodoc:
   module SessionManagement #:nodoc:
     extend ActiveSupport::Concern
 
+    included do
+      ActiveSupport::Deprecation.warn "ActionController::SessionManagement " \
+       "is deprecated because it has no contents since Rails 3.1", caller
+    end
+
     module ClassMethods
 
     end
