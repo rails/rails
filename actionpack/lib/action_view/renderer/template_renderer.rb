bcea8cd6d62f73cebc88256f7f54430c16f1b89d
José Valim <jose.valim@gmail.com>
2012-03-07 14:55:06 +0100

    Just change the formats on first render, closes #5307, closes #5308.

--- actionpack/lib/action_view/renderer/template_renderer.rb
+++ actionpack/lib/action_view/renderer/template_renderer.rb
@@ -8,8 +8,13 @@ module ActionView
       @details = extract_details(options)
       extract_format(options[:file] || options[:template], @details)
       template = determine_template(options)
-      @lookup_context.rendered_format ||= template.formats.first
-      @lookup_context.formats = template.formats
+      context  = @lookup_context
+
+      unless context.rendered_format
+        context.rendered_format = template.formats.first
+        context.formats = template.formats
+      end
+
       render_template(template, options[:layout], options[:locals])
     end
 
