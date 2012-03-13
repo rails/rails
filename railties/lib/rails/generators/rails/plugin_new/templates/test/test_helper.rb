919db1bbbb9c2565c2dc1816812b7031f74b4e8e
Piotr Sarnacki <drogus@gmail.com>
2012-03-13 21:28:34 +0100

    Check for existence of exactly the called `fixture_path=` method

--- railties/lib/rails/generators/rails/plugin_new/templates/test/test_helper.rb
+++ railties/lib/rails/generators/rails/plugin_new/templates/test/test_helper.rb
@@ -10,6 +10,6 @@ Rails.backtrace_cleaner.remove_silencers!
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 # Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path)
+if ActiveSupport::TestCase.method_defined?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 end
