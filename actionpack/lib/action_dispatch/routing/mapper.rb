f9872207564c9c9dd5b4c7ecf02f2387cbceed9a
Santiago Pastorino <santiago@wyeworks.com>
2012-03-17 12:42:29 -0300

    Add missing require

--- actionpack/lib/action_dispatch/routing/mapper.rb
+++ actionpack/lib/action_dispatch/routing/mapper.rb
@@ -1,5 +1,6 @@
 require 'active_support/core_ext/hash/except'
 require 'active_support/core_ext/object/blank'
+require 'active_support/core_ext/enumerable'
 require 'active_support/inflector'
 require 'action_dispatch/routing/redirection'
 
