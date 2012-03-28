2d5b60e982cf210c36f849185382546cf16b52b9
Santiago Pastorino <santiago@wyeworks.com>
2012-03-27 20:09:37 -0700

    Merge pull request #5616 from cbartlett/migration-indent
    Fix indenting in migration generator

--- activerecord/lib/rails/generators/active_record/migration/templates/migration.rb
+++ activerecord/lib/rails/generators/active_record/migration/templates/migration.rb
@@ -13,9 +13,9 @@ class <%= migration_class_name %> < ActiveRecord::Migration
 <% attributes.each do |attribute| -%>
   <%- if migration_action -%>
     <%= migration_action %>_column :<%= table_name %>, :<%= attribute.name %><% if migration_action == 'add' %>, :<%= attribute.type %><%= attribute.inject_options %><% end %>
-    <% if attribute.has_index? && migration_action == 'add' %>
+    <%- if attribute.has_index? && migration_action == 'add' -%>
     add_index :<%= table_name %>, :<%= attribute.index_name %><%= attribute.inject_index_options %>
-    <% end -%>
+    <%- end -%>
   <%- end -%>
 <%- end -%>
   end
