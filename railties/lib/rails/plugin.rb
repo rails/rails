921c40426839b65478b475e5c6d73cd1e1493fac
Vijay Dev <vijaydev.cse@gmail.com>
2012-03-29 17:40:42 +0530

    fix incorrect url in the deprecation message for vendor/plugins [ci skip]

--- railties/lib/rails/plugin.rb
+++ railties/lib/rails/plugin.rb
@@ -53,7 +53,7 @@ module Rails
     end
 
     def initialize(root)
-      ActiveSupport::Deprecation.warn "You have Rails 2.3-style plugins in vendor/plugins! Support for these plugins will be removed in Rails 4.0. Move them out and bundle them in your Gemfile, or fold them in to your app as lib/myplugin/* and config/initializers/myplugin.rb. See the release notes for more on this: http://weblog.rubyonrails.org/2012/01/04/rails-3-2-0-rc2-has-been-released"
+      ActiveSupport::Deprecation.warn "You have Rails 2.3-style plugins in vendor/plugins! Support for these plugins will be removed in Rails 4.0. Move them out and bundle them in your Gemfile, or fold them in to your app as lib/myplugin/* and config/initializers/myplugin.rb. See the release notes for more on this: http://weblog.rubyonrails.org/2012/1/4/rails-3-2-0-rc2-has-been-released"
       @name = File.basename(root).to_sym
       config.root = root
     end
