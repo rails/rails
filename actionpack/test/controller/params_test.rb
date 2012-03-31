7735a410531e2191eff13de403a4e847d6487400
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-03-31 00:11:10 -0500

    Add basic tests for ActionController params

---
+++ actionpack/test/controller/params_test.rb
@@ -0,0 +1,45 @@
+require "abstract_unit"
+
+class ParamsTest < ActionDispatch::IntegrationTest
+  class TestController < ActionController::Base
+    def params_test ; end
+  end
+
+  def setup
+    @controller = TestController.new
+  end
+
+  def test_post_params
+    with_test_route_set do
+      post "/", {"username" => "david"}
+      assert_equal "david", @controller.params[:username]
+      assert_nil @controller.params[:baz]
+    end
+  end
+
+  def test_route_params
+    with_test_route_set do
+      get "/bar"
+      assert_equal "bar", @controller.params[:foo]
+    end
+  end
+
+  def test_nested_params
+    with_test_route_set do
+      post "/", {"user" => {"username" => "guille"}}
+      assert_equal "guille", @controller.params[:user][:username]
+      assert_nil @controller.params[:user][:baz]
+    end
+  end
+
+  private
+    def with_test_route_set
+      with_routing do |set|
+        set.draw do
+          match '/', :to => 'params_test/test#params_test'
+          match '/:foo', :to => 'params_test/test#params_test'
+        end
+        yield
+      end
+    end
+end
