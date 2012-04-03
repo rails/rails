ce06081441b27b22dca97085614be8d1ac354be5
Roman V. Babenko <romanvbabenko@gmail.com>
2012-04-03 03:25:15 +0300

    Unnecessary Rubygems dependency check has been removed at BacktraceCleaner#add_gem_filters

--- railties/lib/rails/backtrace_cleaner.rb
+++ railties/lib/rails/backtrace_cleaner.rb
@@ -17,10 +17,7 @@ module Rails
 
     private
       def add_gem_filters
-        return unless defined?(Gem)
-
         gems_paths = (Gem.path | [Gem.default_dir]).map { |p| Regexp.escape(p) }
-        return if gems_paths.empty?
 
         gems_regexp = %r{(#{gems_paths.join('|')})/gems/([^/]+)-([\w.]+)/(.*)}
         add_filter { |line| line.sub(gems_regexp, '\2 (\3) \4') }
