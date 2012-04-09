2e431599596913e24d7a5b053b632d0eae83517b
Santiago Pastorino <santiago@wyeworks.com>
2012-04-08 22:10:03 -0300

    Remove Fixnum#to_sym support in 1.8.7

--- activesupport/lib/active_support/core_ext/hash/keys.rb
+++ activesupport/lib/active_support/core_ext/hash/keys.rb
@@ -22,7 +22,7 @@ class Hash
   # to +to_sym+.
   def symbolize_keys!
     keys.each do |key|
-      self[(key.to_sym rescue key) || key] = delete(key)
+      self[(key.to_sym rescue key)] = delete(key)
     end
     self
   end
