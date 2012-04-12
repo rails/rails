18699b0dc81b994e79437c87dad61ec5135265af
Jon Leighton <j@jonathanleighton.com>
2012-04-12 15:02:00 +0100

    move apply_finder_options to active_record_deprecated_finders

--- activerecord/lib/active_record/relation/spawn_methods.rb
+++ activerecord/lib/active_record/relation/spawn_methods.rb
@@ -128,27 +128,5 @@ module ActiveRecord
       result
     end
 
-    VALID_FIND_OPTIONS = [ :conditions, :include, :joins, :limit, :offset, :extend, :references,
-                           :order, :select, :readonly, :group, :having, :from, :lock ]
-
-    def apply_finder_options(options)
-      relation = clone
-      return relation unless options
-
-      options.assert_valid_keys(VALID_FIND_OPTIONS)
-      finders = options.dup
-      finders.delete_if { |key, value| value.nil? && key != :limit }
-
-      ((VALID_FIND_OPTIONS - [:conditions, :include, :extend]) & finders.keys).each do |finder|
-        relation = relation.send(finder, finders[finder])
-      end
-
-      relation = relation.where(finders[:conditions]) if options.has_key?(:conditions)
-      relation = relation.includes(finders[:include]) if options.has_key?(:include)
-      relation = relation.extending(finders[:extend]) if options.has_key?(:extend)
-
-      relation
-    end
-
   end
 end
