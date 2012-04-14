9f35d5e174ec3748c118bfa0fc40650daf07d733
Oscar Del Ben <info@oscardelben.com>
2012-04-14 09:49:46 +0200

    No need for if_params at the end of the initializer as an initializer always returns the object instance

--- activemodel/lib/active_model/model.rb
+++ activemodel/lib/active_model/model.rb
@@ -66,7 +66,7 @@ module ActiveModel
     def initialize(params={})
       params.each do |attr, value|
         self.public_send("#{attr}=", value)
-      end if params
+      end
     end
 
     def persisted?
