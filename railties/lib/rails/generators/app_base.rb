78d8d8a05d313978d0282e578cc889c1b3b7e4b1
Vasiliy Ermolovich <younash@gmail.com>
2012-04-14 17:51:33 +0300

    remove extra new line from generated Gemfile

--- railties/lib/rails/generators/app_base.rb
+++ railties/lib/rails/generators/app_base.rb
@@ -120,7 +120,7 @@ module Rails
       end
 
       def database_gemfile_entry
-        options[:skip_active_record] ? "" : "gem '#{gem_for_database}'\n"
+        options[:skip_active_record] ? "" : "gem '#{gem_for_database}'"
       end
 
       def include_all_railties?
