387d4e7ee071f7968ef0f7b503289b509858b468
Vishnu Atrai <me@vishnuatrai.com>
2012-04-16 22:22:36 +0530

    remove autoload for OrderedHash, usages removed already

--- activesupport/lib/active_support.rb
+++ activesupport/lib/active_support.rb
@@ -65,7 +65,6 @@ module ActiveSupport
     autoload :MessageVerifier
     autoload :Multibyte
     autoload :OptionMerger
-    autoload :OrderedHash
     autoload :OrderedOptions
     autoload :Rescuable
     autoload :StringInquirer
