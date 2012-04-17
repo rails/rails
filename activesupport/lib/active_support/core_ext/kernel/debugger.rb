6f80ea2aec83523640b505ddd5b6e87f0100a85b
Aditya Sanghi <asanghi@me.com>
2012-04-18 00:48:59 +0530

    review changes for #5875

--- activesupport/lib/active_support/core_ext/kernel/debugger.rb
+++ activesupport/lib/active_support/core_ext/kernel/debugger.rb
@@ -1,6 +1,6 @@
 module Kernel
   unless respond_to?(:debugger)
-    # Starts a debugging session if debugger has been loaded (call rails server --debugger to do load it).
+    # Starts a debugging session if +debugger+ gem has been loaded (call rails server --debugger to do load it).
     def debugger
       message = "\n***** Debugger requested, but was not available (ensure debugger is listed in Gemfile/installed as gem): Start server with --debugger to enable *****\n"
       defined?(Rails) ? Rails.logger.info(message) : $stderr.puts(message)
