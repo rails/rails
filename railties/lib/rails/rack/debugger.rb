67ede880550836ce9f66164c56c8afeb397a54da
Aditya Sanghi <asanghi@me.com>
2012-04-18 02:24:44 +0530

    another attempt at the language

--- railties/lib/rails/rack/debugger.rb
+++ railties/lib/rails/rack/debugger.rb
@@ -12,7 +12,7 @@ module Rails
         ::Debugger.settings[:autoeval] = true if ::Debugger.respond_to?(:settings)
         puts "=> Debugger enabled"
       rescue LoadError
-        puts "You need to install debugger to run the server in debugging mode. With gems, use 'gem install debugger'"
+        puts "You need to install a debugger to run the server in debugging mode. With gems, use 'gem install debugger'"
         exit
       end
 
