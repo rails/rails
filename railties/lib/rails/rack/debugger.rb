f139c06271bef4bb53ea904d0d02b6aab93201a1
Aditya Sanghi <asanghi@me.com>
2012-04-17 15:31:05 +0530

    stamp out ruby-debug19 with extreme prejudice :)

--- railties/lib/rails/rack/debugger.rb
+++ railties/lib/rails/rack/debugger.rb
@@ -6,13 +6,13 @@ module Rails
 
         ARGV.clear # clear ARGV so that rails server options aren't passed to IRB
 
-        require 'ruby-debug'
+        require 'debugger'
 
         ::Debugger.start
         ::Debugger.settings[:autoeval] = true if ::Debugger.respond_to?(:settings)
         puts "=> Debugger enabled"
       rescue LoadError
-        puts "You need to install ruby-debug19 to run the server in debugging mode. With gems, use 'gem install ruby-debug19'"
+        puts "You need to install debugger to run the server in debugging mode. With gems, use 'gem install debugger'"
         exit
       end
 
