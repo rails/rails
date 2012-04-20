dbb4c4ddb601e06fef37f63d644a5297f3292c2a
Oscar Del Ben <info@oscardelben.com>
2012-04-20 10:47:38 +0200

    New Getting started guide wont have tags

--- guides/code/getting_started/app/models/post.rb
+++ guides/code/getting_started/app/models/post.rb
@@ -3,7 +3,6 @@ class Post < ActiveRecord::Base
                     :length => { :minimum => 5 }
 
   has_many :comments, :dependent => :destroy
-  has_many :tags
 
   accepts_nested_attributes_for :tags, :allow_destroy => :true,
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
