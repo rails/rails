44cc0bdd620890f8ae5c99d450ac74972187657a
Milovan Zogovic <milovan.zogovic@gmail.com>
2012-04-20 10:25:03 +0200

    using dash instead of dot for cache_key

--- activerecord/lib/active_record/integration.rb
+++ activerecord/lib/active_record/integration.rb
@@ -39,7 +39,8 @@ module ActiveRecord
       when new_record?
         "#{self.class.model_name.cache_key}/new"
       when timestamp = self[:updated_at]
-        "#{self.class.model_name.cache_key}/#{id}-#{timestamp.utc.to_f}"
+        timestamp = timestamp.to_f.to_s.gsub('.','-')
+        "#{self.class.model_name.cache_key}/#{id}-#{timestamp}"
       else
         "#{self.class.model_name.cache_key}/#{id}"
       end
