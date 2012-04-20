55c9de697cf7d77703b28f7721710bdc4efecf8d
Oscar Del Ben <info@oscardelben.com>
2012-04-20 12:53:18 +0200

    Add index and links section to Getting started guide

--- guides/code/getting_started/app/controllers/posts_controller.rb
+++ guides/code/getting_started/app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
 
   def show
     @post = Post.find(params[:id])
--- guides/code/getting_started/config/routes.rb
+++ guides/code/getting_started/config/routes.rb
@@ -3,6 +3,7 @@ Blog::Application.routes.draw do
   #   resources :comments
   # end
 
+  get "posts" => "posts#index"
   get "posts/new"
   post "posts/create"
   get "posts/:id" => "posts#show"
@@ -56,7 +57,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
