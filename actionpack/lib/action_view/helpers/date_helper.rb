3bc6ec2e0d2bdfec4673aa91989083b9129f8739
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2012-04-23 19:16:02 -0300

    fixed non matching documentation behaviour with method semantics on distance_of_time_in_words

--- actionpack/lib/action_view/helpers/date_helper.rb
+++ actionpack/lib/action_view/helpers/date_helper.rb
@@ -67,7 +67,8 @@ module ActionView
       def distance_of_time_in_words(from_time, to_time = 0, include_seconds = false, options = {})
         from_time = from_time.to_time if from_time.respond_to?(:to_time)
         to_time = to_time.to_time if to_time.respond_to?(:to_time)
-        distance_in_minutes = (((to_time - from_time).abs)/60).round
+        from_time, to_time = to_time, from_time if from_time > to_time
+        distance_in_minutes = (((to_time - from_time).abs)/60.0).round
         distance_in_seconds = ((to_time - from_time).abs).round
 
         I18n.with_options :locale => options[:locale], :scope => :'datetime.distance_in_words' do |locale|
