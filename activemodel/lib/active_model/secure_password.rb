b0547890fdc5b94d1a3a202f2086c27470ef2c76
Oscar Del Ben <info@oscardelben.com>
2012-04-24 19:08:57 +0200

    Correct documentation of SecurePassword

--- activemodel/lib/active_model/secure_password.rb
+++ activemodel/lib/active_model/secure_password.rb
@@ -58,7 +58,8 @@ module ActiveModel
         BCrypt::Password.new(password_digest) == unencrypted_password && self
       end
 
-      # Encrypts the password into the password_digest attribute.
+      # Encrypts the password into the password_digest attribute, only if the
+      # new password is not blank.
       def password=(unencrypted_password)
         @password = unencrypted_password
         unless unencrypted_password.blank?
