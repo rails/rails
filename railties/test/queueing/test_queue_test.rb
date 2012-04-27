39045de878f6f5b4a026758ed1504dd757ed74d2
Aaron Patterson <aaron.patterson@gmail.com>
2012-04-27 14:26:26 -0700

    Extend stdlib queue for the test queue

--- railties/test/queueing/test_queue_test.rb
+++ railties/test/queueing/test_queue_test.rb
@@ -3,9 +3,7 @@ require 'rails/queueing'
 
 class TestQueueTest < ActiveSupport::TestCase
   class Job
-    attr_reader :id
-    def initialize(id, &block)
-      @id = id
+    def initialize(&block)
       @block = block
     end
 
@@ -15,21 +13,23 @@ class TestQueueTest < ActiveSupport::TestCase
   end
 
   def setup
+    #@queue = Rails::Queueing::TestQueue.new
     @queue = Rails::Queueing::TestQueue.new
   end
 
   def test_contents
-    assert_equal [], @queue.contents
-    job = Job.new(1)
+    assert @queue.empty?
+    job = Job.new
     @queue.push job
-    assert_equal [job], @queue.contents
+    refute @queue.empty?
+    assert_equal job, @queue.pop
   end
 
   def test_order
     processed = []
 
-    job1 = Job.new(1) { processed << 1 }
-    job2 = Job.new(2) { processed << 2 }
+    job1 = Job.new { processed << 1 }
+    job2 = Job.new { processed << 2 }
 
     @queue.push job1
     @queue.push job2
@@ -42,7 +42,7 @@ class TestQueueTest < ActiveSupport::TestCase
     t = nil
     ran = false
 
-    job = Job.new(1) do
+    job = Job.new do
       ran = true
       t = Thread.current
     end
@@ -50,7 +50,7 @@ class TestQueueTest < ActiveSupport::TestCase
     @queue.push job
     @queue.drain
 
-    assert_equal [], @queue.contents
+    assert @queue.empty?
     assert ran, "The job runs synchronously when the queue is drained"
     assert_not_equal t, Thread.current
   end
