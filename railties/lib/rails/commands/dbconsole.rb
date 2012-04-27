5d685f9db77e05afe6fbf50ee77d1fb6f63aeaef
Alexey Vakhov <vakhov@gmail.com>
2012-04-27 11:28:53 +0400

    Add Rails::DBConsole tests

--- railties/lib/rails/commands/dbconsole.rb
+++ railties/lib/rails/commands/dbconsole.rb
@@ -5,12 +5,15 @@ require 'rbconfig'
 
 module Rails
   class DBConsole
+    attr_reader :arguments
+
     def self.start(app)
       new(app).start
     end
 
-    def initialize(app)
+    def initialize(app, arguments = ARGV)
       @app = app
+      @arguments = arguments
     end
 
     def start
@@ -31,28 +34,14 @@ module Rails
           options['header'] = h
         end
 
-        opt.parse!(ARGV)
-        abort opt.to_s unless (0..1).include?(ARGV.size)
+        opt.parse!(arguments)
+        abort opt.to_s unless (0..1).include?(arguments.size)
       end
 
       unless config = @app.config.database_configuration[Rails.env]
         abort "No database is configured for the environment '#{Rails.env}'"
       end
 
-
-      def find_cmd(*commands)
-        dirs_on_path = ENV['PATH'].to_s.split(File::PATH_SEPARATOR)
-        commands += commands.map{|cmd| "#{cmd}.exe"} if RbConfig::CONFIG['host_os'] =~ /mswin|mingw/
-
-        full_path_command = nil
-        found = commands.detect do |cmd|
-          dir = dirs_on_path.detect do |path|
-            full_path_command = File.join(path, cmd)
-            File.executable? full_path_command
-          end
-        end
-        found ? full_path_command : abort("Couldn't find database client: #{commands.join(', ')}. Check your $PATH and try again.")
-      end
 
       case config["adapter"]
       when /^mysql/
@@ -72,17 +61,17 @@ module Rails
 
         args << config['database']
 
-        exec(find_cmd('mysql', 'mysql5'), *args)
+        find_cmd_and_exec(['mysql', 'mysql5'], *args)
 
       when "postgresql", "postgres"
         ENV['PGUSER']     = config["username"] if config["username"]
         ENV['PGHOST']     = config["host"] if config["host"]
         ENV['PGPORT']     = config["port"].to_s if config["port"]
         ENV['PGPASSWORD'] = config["password"].to_s if config["password"] && include_password
-        exec(find_cmd('psql'), config["database"])
+        find_cmd_and_exec('psql', config["database"])
 
       when "sqlite"
-        exec(find_cmd('sqlite'), config["database"])
+        find_cmd_and_exec('sqlite', config["database"])
 
       when "sqlite3"
         args = []
@@ -91,7 +80,7 @@ module Rails
         args << "-header" if options['header']
         args << config['database']
 
-        exec(find_cmd('sqlite3'), *args)
+        find_cmd_and_exec('sqlite3', *args)
 
       when "oracle", "oracle_enhanced"
         logon = ""
@@ -102,10 +91,33 @@ module Rails
           logon << "@#{config['database']}" if config['database']
         end
 
-        exec(find_cmd('sqlplus'), logon)
+        find_cmd_and_exec('sqlplus', logon)
 
       else
         abort "Unknown command-line client for #{config['database']}. Submit a Rails patch to add support!"
+      end
+    end
+
+    protected
+
+    def find_cmd_and_exec(commands, *args)
+      commands = Array(commands)
+
+      dirs_on_path = ENV['PATH'].to_s.split(File::PATH_SEPARATOR)
+      commands += commands.map{|cmd| "#{cmd}.exe"} if RbConfig::CONFIG['host_os'] =~ /mswin|mingw/
+
+      full_path_command = nil
+      found = commands.detect do |cmd|
+        dir = dirs_on_path.detect do |path|
+          full_path_command = File.join(path, cmd)
+          File.executable? full_path_command
+        end
+      end
+
+      if found
+        exec full_path_command, *args
+      else
+        abort("Couldn't find database client: #{commands.join(', ')}. Check your $PATH and try again.")
       end
     end
   end
