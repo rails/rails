f576a7b812fc61e1b5732860f7091653dc0bc978
簡煒航 <tonytonyjan@gmail.com>
2012-04-30 14:12:43 +0800

    If a non ASCII character is detected, using MD5.

--- guides/rails_guides/indexer.rb
+++ guides/rails_guides/indexer.rb
@@ -57,11 +57,11 @@ module RailsGuides
       level_hash
     end
 
-    def title_to_idx(title)
-      idx = title.strip.parameterize.sub(/^\d+/, '')
+   def title_to_idx(title)
+      idx = title=~/[^\u0000-\u007F]/ ? ("id-"+Digest::MD5.hexdigest(title.strip)) : title.strip
+      idx = idx.parameterize.sub(/^\d+/, '')
       if idx.blank?
         puts "BLANK ID: please put an explicit ID for section #{title}, as in h5(#my-id)" if warnings
-        idx = ("id-"+Digest::MD5.hexdigest(title.strip)).parameterize.sub(/^\d+/, '')
       end
       idx
     end
