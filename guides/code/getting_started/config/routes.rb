06a1519a4dc714822bec2312a41950aedba82541
Oscar Del Ben <info@oscardelben.com>
2012-04-30 09:08:32 +0200

    Rewrite adding a second model section of Getting Started Guide

--- guides/code/getting_started/config/routes.rb
+++ guides/code/getting_started/config/routes.rb
@@ -1,6 +1,8 @@
 Blog::Application.routes.draw do
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
