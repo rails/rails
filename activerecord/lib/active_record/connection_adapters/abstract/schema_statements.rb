fa34acec20574699c2de19d97f9ae54f540fe69d
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2012-05-09 08:52:44 -0300

    Stop showing deprecations for Ruby 1.8.7 with remove_column
    String is Enumerable in 1.8.7, which means that passing a String to
    remove_column was generating deprecation warnings during tests.

--- activerecord/lib/active_record/connection_adapters/abstract/schema_statements.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/schema_statements.rb
@@ -269,13 +269,15 @@ module ActiveRecord
       #  remove_column(:suppliers, :qualification)
       #  remove_columns(:suppliers, :qualification, :experience)
       def remove_column(table_name, *column_names)
-        if column_names.first.kind_of?(Enumerable)
+        if column_names.flatten!
           message = 'Passing array to remove_columns is deprecated, please use ' +
                     'multiple arguments, like: `remove_columns(:posts, :foo, :bar)`'
           ActiveSupport::Deprecation.warn message, caller
         end
 
-        columns_for_remove(table_name, *column_names).each {|column_name| execute "ALTER TABLE #{quote_table_name(table_name)} DROP #{column_name}" }
+        columns_for_remove(table_name, *column_names).each do |column_name|
+          execute "ALTER TABLE #{quote_table_name(table_name)} DROP #{column_name}"
+        end
       end
       alias :remove_columns :remove_column
 
