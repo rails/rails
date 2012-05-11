53cc85fad3440d5817ecf7e0e8cd0c7ff3d9a062
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-05-11 17:08:31 -0500

    added docs to String#to_date

--- activesupport/lib/active_support/core_ext/string/conversions.rb
+++ activesupport/lib/active_support/core_ext/string/conversions.rb
@@ -15,6 +15,12 @@ class String
     end
   end
 
+  # Converts a string to a Date value.
+  #
+  #   "1-1-2012".to_date   #=> Sun, 01 Jan 2012  
+  #   "01/01/2012".to_date #=> Sun, 01 Jan 2012  
+  #   "2012-12-13".to_date #=> Thu, 13 Dec 2012  
+  #   "12/13/2012".to_date #=> ArgumentError: invalid date
   def to_date
     unless blank?
       date_values = ::Date._parse(self, false).values_at(:year, :mon, :mday)
