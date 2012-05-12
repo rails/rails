99e2a223e8a5fd0943f2e2168fb2866eb831030c
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2012-05-12 15:49:26 -0300

    Remove tailing spaces :scissors: [ci skip]

--- railties/lib/rails/generators/rails/app/templates/config/application.rb
+++ railties/lib/rails/generators/rails/app/templates/config/application.rb
@@ -43,7 +43,7 @@ module <%= app_const_base %>
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-    
+
     # Enable escaping HTML in JSON. The default is false.
     # config.active_support.escape_html_entities_in_json = true
 
