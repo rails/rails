f2af26dc817281b39a11d1f7131824dfbd4f7a76
Vijay Dev <vijaydev.cse@gmail.com>
2012-05-12 18:07:26 +0530

    fix incorrect example [ci skip]

--- activesupport/lib/active_support/core_ext/integer/multiple.rb
+++ activesupport/lib/active_support/core_ext/integer/multiple.rb
@@ -2,7 +2,7 @@ class Integer
   # Check whether the integer is evenly divisible by the argument.
   #
   #   0.multiple_of?(0)  #=> true
-  #   5.multiple_of?(5)  #=> false
+  #   6.multiple_of?(5)  #=> false
   #   10.multiple_of?(2) #=> true
   def multiple_of?(number)
     number != 0 ? self % number == 0 : zero?
