817f9e64ee4e5b02efd9b4047c23340bf4b75586
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-05-13 12:45:25 -0500

    unnecessary 'examples' noise in Hash#assert_valid_keys docs

--- activesupport/lib/active_support/core_ext/hash/keys.rb
+++ activesupport/lib/active_support/core_ext/hash/keys.rb
@@ -45,7 +45,6 @@ class Hash
   # Note that keys are NOT treated indifferently, meaning if you use strings for keys but assert symbols
   # as keys, this will fail.
   #
-  # ==== Examples
   #   { :name => 'Rob', :years => '28' }.assert_valid_keys(:name, :age) # => raises "ArgumentError: Unknown key: years"
   #   { :name => 'Rob', :age => '28' }.assert_valid_keys('name', 'age') # => raises "ArgumentError: Unknown key: name"
   #   { :name => 'Rob', :age => '28' }.assert_valid_keys(:name, :age) # => passes, raises nothing
