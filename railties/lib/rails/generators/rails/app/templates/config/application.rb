ec68258f19678d47acc6e5684c9dbc18a7382589
José Valim <jose.valim@gmail.com>
2012-05-14 08:03:40 +0200

    Default escape_html_entities_in_json to true in new apps

--- railties/lib/rails/generators/rails/app/templates/config/application.rb
+++ railties/lib/rails/generators/rails/app/templates/config/application.rb
@@ -49,8 +49,8 @@ module <%= app_const_base %>
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
-    # Enable escaping HTML in JSON. The default is false.
-    # config.active_support.escape_html_entities_in_json = true
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
