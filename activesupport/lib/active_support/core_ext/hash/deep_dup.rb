b2f73c40b8a995ed685fc76f2df724b15ead16b6
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-05-15 10:00:09 -0500

    Backporting some docs on core_ext/hash

--- activesupport/lib/active_support/core_ext/hash/deep_dup.rb
+++ activesupport/lib/active_support/core_ext/hash/deep_dup.rb
@@ -1,5 +1,12 @@
 class Hash
   # Returns a deep copy of hash.
+  #
+  #   hash = { :a => { :b => 'b' } }
+  #   dup  = hash.deep_dup
+  #   dup[:a][:c] = 'c'
+  #
+  #   hash[:a][:c] #=> nil
+  #   dup[:a][:c]  #=> "c"
   def deep_dup
     duplicate = self.dup
     duplicate.each_pair do |k,v|
--- activesupport/lib/active_support/core_ext/hash/deep_merge.rb
+++ activesupport/lib/active_support/core_ext/hash/deep_merge.rb
@@ -1,11 +1,16 @@
 class Hash
   # Returns a new hash with +self+ and +other_hash+ merged recursively.
+  #
+  #   h1 = {:x => {:y => [4,5,6]}, :z => [7,8,9]}
+  #   h2 = {:x => {:y => [7,8,9]}, :z => "xyz"}
+  #
+  #   h1.deep_merge(h2) #=> { :x => {:y => [7, 8, 9]}, :z => "xyz" }
+  #   h2.deep_merge(h1) #=> { :x => {:y => [4, 5, 6]}, :z => [7, 8, 9] }
   def deep_merge(other_hash)
     dup.deep_merge!(other_hash)
   end
 
-  # Returns a new hash with +self+ and +other_hash+ merged recursively.
-  # Modifies the receiver in place.
+  # Same as +deep_merge+, but modifies +self+.
   def deep_merge!(other_hash)
     other_hash.each_pair do |k,v|
       tv = self[k]
