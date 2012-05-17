251b22f7610f6b62028297058b6321b788f4f610
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-05-16 21:33:49 -0300

    Don't need to force size to nil

--- actionpack/lib/action_view/helpers/tags/date_field.rb
+++ actionpack/lib/action_view/helpers/tags/date_field.rb
@@ -5,7 +5,6 @@ module ActionView
         def render
           options = @options.stringify_keys
           options["value"] = @options.fetch("value") { value(object).try(:to_date) }
-          options["size"] = nil
           @options = options
           super
         end
