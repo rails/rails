099bb5d7b0e139d097b0c9bab4cdc8a36cf05ffa
Piotr Sarnacki <drogus@gmail.com>
2012-05-18 12:47:55 -0700

    Ensure that CollectionAssociation#replace returns proper target
    The fix commited in e2a070c was returning the `new_target`, as
    a try to return whatever user replaced association with. The
    problem is, the resulting association target may be ordered
    differently. In such case we want to return the target that
    will be later used for that association.

--- activerecord/lib/active_record/associations/collection_association.rb
+++ activerecord/lib/active_record/associations/collection_association.rb
@@ -476,7 +476,7 @@ module ActiveRecord
                                   "new records could not be saved."
           end
 
-          new_target
+          target
         end
 
         def concat_records(records)
