7c95be54b4c3f8ad2273eea39afa233f8f8b31c1
Piotr Sarnacki <drogus@gmail.com>
2012-05-20 16:19:11 -0700

    Fix generators to help with ambiguous `ApplicationController` issue
    In development mode, dependencies are loaded dynamically at runtime,
    using `const_missing`. Because of that, when one of the constants is
    already loaded and `const_missing` is not triggered, user can end up
    with unexpected results.
    
    Given such file in an Engine:
    
    ```ruby
    module Blog
      class PostsController < ApplicationController
      end
    end
    ```
    
    If you load it first, before loading any application files, it will
    correctly load `Blog::ApplicationController`, because second line will
    hit `const_missing`. However if you load `ApplicationController` first,
    the constant will be loaded already, `const_missing` hook will not be
    fired and in result `PostsController` will inherit from
    `ApplicationController` instead of `Blog::ApplicationController`.
    
    Since it can't be fixed in `AS::Dependencies`, the easiest fix is to
    just explicitly load application controller.
    
    closes #6413

--- railties/lib/rails/generators/rails/controller/templates/controller.rb
+++ railties/lib/rails/generators/rails/controller/templates/controller.rb
@@ -1,3 +1,7 @@
+<% if namespaced? -%>
+require "<%= namespaced_file_path %>/application_controller"
+<% end -%>
+
 <% module_namespacing do -%>
 class <%= class_name %>Controller < ApplicationController
 <% actions.each do |action| -%>
