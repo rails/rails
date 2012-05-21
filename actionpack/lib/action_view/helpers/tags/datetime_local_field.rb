a9a9e8a3cb64faddc347271c00f975e04a92e394
Carlos Galdino <carloshsgaldino@gmail.com>
2012-05-21 18:52:56 -0300

    Refactor date related helpers

--- actionpack/lib/action_view/helpers/tags/datetime_local_field.rb
+++ actionpack/lib/action_view/helpers/tags/datetime_local_field.rb
@@ -1,20 +1,16 @@
 module ActionView
   module Helpers
     module Tags
-      class DatetimeLocalField < TextField #:nodoc:
-        def render
-          options = @options.stringify_keys
-          options["type"] = "datetime-local"
-          options["value"] = @options.fetch("value") { format_local_date_time_string(value(object)) }
-          options["min"] = format_local_date_time_string(options["min"])
-          options["max"] = format_local_date_time_string(options["max"])
-          @options = options
-          super
+      class DatetimeLocalField < DatetimeField #:nodoc:
+        class << self
+          def field_type
+            @field_type ||= "datetime-local"
+          end
         end
 
         private
 
-          def format_local_date_time_string(value)
+          def format_date(value)
             value.try(:strftime, "%Y-%m-%dT%T")
           end
       end
