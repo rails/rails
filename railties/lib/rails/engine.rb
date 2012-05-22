1ca1b1ab9176c780989be7e5a11d27fb97fe663a
Aaron Patterson <aaron.patterson@gmail.com>
2012-05-22 16:43:12 -0700

    use RUBY_PLATFORM in case of cross compiled ruby

--- railties/lib/rails/engine.rb
+++ railties/lib/rails/engine.rb
@@ -652,7 +652,7 @@ module Rails
       root = File.exist?("#{root_path}/#{flag}") ? root_path : default
       raise "Could not find root path for #{self}" unless root
 
-      RbConfig::CONFIG['host_os'] =~ /mswin|mingw/ ?
+      RUBY_PLATFORM =~ /mswin|mingw/ ?
         Pathname.new(root).expand_path : Pathname.new(root).realpath
     end
 
