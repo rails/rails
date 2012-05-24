rails/rails

commit 4001835db00ce44cb75bca33ec02cd76b8ccc790

removing more pathnameisms

railties/lib/rails/engine.rb
@@ -416,9 +416,9 @@ module Rails
 
       # Finds engine with given path
       def find(path)
-        expanded_path = File.expand_path path.to_s
+        expanded_path = File.expand_path path
         Rails::Engine::Railties.engines.find { |engine|
-          File.expand_path(engine.root.to_s) == expanded_path
+          File.expand_path(engine.root) == expanded_path
         }
       end
     end
@@ -652,7 +652,7 @@ module Rails
       root = File.exist?("#{root_path}/#{flag}") ? root_path : default
       raise "Could not find root path for #{self}" unless root
 
-      Pathname.new File.realpath root
+      File.realpath root
     end
 
     def default_middleware_stack

railties/lib/rails/paths.rb
@@ -1,5 +1,3 @@
-require "pathname"
-
 module Rails
   module Paths
     # This object is an extended hash that behaves as root of the <tt>Rails::Paths</tt> system.
@@ -186,7 +184,7 @@ module Rails
         raise "You need to set a path root" unless @root.path
 
         map do |p|
-          Pathname.new(@root.path).join(p)
+          File.join @root.path, p
         end
       end
 

commit 66db0b9440e4db594c951d38f641e1910fd44db3

Fixed backward incompatibility for engines.
- Many engines rely on being able to join directories to the Rails root:

    Rails.root.join('somedir')

- This was now impossible because Rails.root returned a String:

    NoMethodError: undefined method `join' for "/code/myrailsapp":String

- This was broken in 4001835db00ce44cb75bca33ec02cd76b8ccc790

railties/lib/rails/engine.rb
@@ -652,7 +652,7 @@ module Rails
       root = File.exist?("#{root_path}/#{flag}") ? root_path : default
       raise "Could not find root path for #{self}" unless root
 
-      File.realpath root
+      Pathname.new File.realpath root
     end
 
     def default_middleware_stack