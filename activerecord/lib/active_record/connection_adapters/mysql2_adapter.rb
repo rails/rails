c90f3de6d3f0e1d12ad1884d89958cd12fb26f79
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-05-25 12:00:00 -0300

    Merge pull request #5244 from fotos/myqsl2_wait_timeout
    Maximum wait_timeout on Windows is 2147483

--- activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb
@@ -264,7 +264,7 @@ module ActiveRecord
 
         # increase timeout so mysql server doesn't disconnect us
         wait_timeout = @config[:wait_timeout]
-        wait_timeout = 2592000 unless wait_timeout.is_a?(Fixnum)
+        wait_timeout = 2147483 unless wait_timeout.is_a?(Fixnum)
         variable_assignments << "@@wait_timeout = #{wait_timeout}"
 
         execute("SET #{variable_assignments.join(', ')}", :skip_logging)
