ac50b633d4b9fd12bf0212a5cf8ebc5ab3ee8f37
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-05-26 22:24:06 -0500

    add examples to Hash#deep_stringify_keys and Hash#deep_symbolize_keys [ci skip]

--- activesupport/lib/active_support/core_ext/hash/keys.rb
+++ activesupport/lib/active_support/core_ext/hash/keys.rb
@@ -93,6 +93,11 @@ class Hash
   # Return a new hash with all keys converted to strings.
   # This includes the keys from the root hash and from all
   # nested hashes.
+  #
+  #   hash = { person: { name: 'Rob', age: '28' } }
+  #
+  #   hash.deep_stringify_keys
+  #   # => { "person" => { "name" => "Rob", "age" => "28" } }
   def deep_stringify_keys
     deep_transform_keys{ |key| key.to_s }
   end
@@ -107,6 +112,11 @@ class Hash
   # Return a new hash with all keys converted to symbols, as long as
   # they respond to +to_sym+. This includes the keys from the root hash
   # and from all nested hashes.
+  #
+  #   hash = { 'person' => { 'name' => 'Rob', 'age' => '28' } }
+  #
+  #   hash.deep_symbolize_keys
+  #   # => { person: { name: "Rob", age: "28" } }
   def deep_symbolize_keys
     deep_transform_keys{ |key| key.to_sym rescue key }
   end
