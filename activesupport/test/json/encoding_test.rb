624f801d27e946cae2244924e7f6a04565666036
Erich Menge <erich.menge@me.com>
2012-05-30 10:24:23 -0500

    True, False, and Nil should be represented in as_json as themselves.
    Conflicts:
    
    	activesupport/lib/active_support/json/encoding.rb
    	activesupport/test/json/encoding_test.rb

--- activesupport/test/json/encoding_test.rb
+++ activesupport/test/json/encoding_test.rb
@@ -270,6 +270,12 @@ class TestJSONEncoding < Test::Unit::TestCase
                  JSON.parse(json_string_and_date))
   end
 
+  def test_nil_true_and_false_represented_as_themselves
+    assert_equal nil,   nil.as_json
+    assert_equal true,  true.as_json
+    assert_equal false, false.as_json
+  end
+
   protected
 
     def object_keys(json_object)
