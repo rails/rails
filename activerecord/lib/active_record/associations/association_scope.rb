9340f89849606dba02f44038171f3837f883fd4e
Aaron Patterson <aaron.patterson@gmail.com>
2012-05-30 15:09:13 -0700

    predicate builder should not recurse for determining where columns. Thanks to Ben Murphy for reporting this
    CVE-2012-2661

--- activerecord/lib/active_record/associations/association_scope.rb
+++ activerecord/lib/active_record/associations/association_scope.rb
@@ -96,7 +96,7 @@ module ActiveRecord
 
             conditions.each do |condition|
               if options[:through] && condition.is_a?(Hash)
-                condition = { table.name => condition }
+                condition = disambiguate_condition(table, condition)
               end
 
               scope = scope.where(interpolate(condition))
@@ -113,7 +113,7 @@ module ActiveRecord
 
             conditions.each do |condition|
               condition = interpolate(condition)
-              condition = { (table.table_alias || table.name) => condition } unless i == 0
+              condition = disambiguate_condition(table, condition) unless i == 0
 
               scope = scope.where(condition)
             end
@@ -138,6 +138,21 @@ module ActiveRecord
         end
       end
 
+      def disambiguate_condition(table, condition)
+        if condition.is_a?(Hash)
+          Hash[
+            condition.map do |k, v|
+              if v.is_a?(Hash)
+                [k, v]
+              else
+                [table.table_alias || table.name, { k => v }]
+              end
+            end
+          ]
+        else
+          condition
+        end
+      end
     end
   end
 end
