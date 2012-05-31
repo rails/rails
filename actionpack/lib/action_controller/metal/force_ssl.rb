490050c0cf631673b39918c37d8c7071268c45cb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-05-31 10:36:18 -0400

    Extracted redirect logic from ActionController::Force::ClassMethods.force_ssl
    Prior to this patch the existing .force_ssl method handles both
    defining the filter and handling the logic for performing the redirect.
    
    With this patch the logic for redirecting to the HTTPS protocol is
    separated from the filter logic that determines if a redirect should
    occur. By separating the two levels of behavior, an instance method for
    ActionController (i.e. #force_ssl_redirect) is exposed and available
    for more granular SSL enforcement.

--- actionpack/lib/action_controller/metal/force_ssl.rb
+++ actionpack/lib/action_controller/metal/force_ssl.rb
@@ -50,13 +50,13 @@ module ActionController
     # ==== Parameters
     # * <tt>host</tt> - Redirect to a different host name
     def force_ssl_redirect(host = nil)
-        unless request.ssl?
-          redirect_options = {:protocol => 'https://', :status => :moved_permanently}
-          redirect_options.merge!(:host => host) if host
-          redirect_options.merge!(:params => request.query_parameters)
-          flash.keep if respond_to?(:flash)
-          redirect_to redirect_options
-        end
+      unless request.ssl?
+        redirect_options = {:protocol => 'https://', :status => :moved_permanently}
+        redirect_options.merge!(:host => host) if host
+        redirect_options.merge!(:params => request.query_parameters)
+        flash.keep if respond_to?(:flash)
+        redirect_to redirect_options
       end
+    end
   end
 end
