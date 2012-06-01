51d1747a9f51c31947660c9b70dcec82454a7921
Seb Bacon <seb.bacon@gmail.com>
2012-06-01 07:46:45 +0100

    Strip [nil] from parameters hash
    CVE-2012-2660

--- actionpack/test/controller/request/query_string_parsing_test.rb
+++ actionpack/test/controller/request/query_string_parsing_test.rb
@@ -82,6 +82,12 @@ class QueryStringParsingTest < ActionController::IntegrationTest
 
   test "query string without equal" do
     assert_parses({ "action" => nil }, "action")
+    assert_parses({"action" => nil}, "action")
+    assert_parses({"action" => {"foo" => nil}}, "action[foo]")
+    assert_parses({"action" => {"foo" => { "bar" => nil }}}, "action[foo][bar]")
+    assert_parses({"action" => {"foo" => { "bar" => nil }}}, "action[foo][bar][]")
+    assert_parses({"action" => {"foo" => nil}}, "action[foo][]")
+    assert_parses({"action"=>{"foo"=>[{"bar"=>nil}]}}, "action[foo][][bar]")
   end
 
   test "query string with empty key" do
