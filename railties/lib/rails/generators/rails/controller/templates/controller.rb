ec106e9d439a9ec48dc9268d8eca0f3595aa7cb9
Akira Matsuda <ronnie@dio.jp>
2012-06-01 22:51:15 +0900

    remove unneeded blank line from !namespeced? controller generators

--- railties/lib/rails/generators/rails/controller/templates/controller.rb
+++ railties/lib/rails/generators/rails/controller/templates/controller.rb
@@ -1,7 +1,7 @@
 <% if namespaced? -%>
 require_dependency "<%= namespaced_file_path %>/application_controller"
-<% end -%>
 
+<% end -%>
 <% module_namespacing do -%>
 class <%= class_name %>Controller < ApplicationController
 <% actions.each do |action| -%>
