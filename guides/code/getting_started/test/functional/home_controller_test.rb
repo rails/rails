f82066886780a9d5290cd3cfc3335c5798bfea57
spyhole <thankleft@gmail.com>
2012-06-02 13:33:39 +0800

    Update guides/code/getting_started/test/functional/home_controller_test.rb

--- guides/code/getting_started/test/functional/home_controller_test.rb
+++ guides/code/getting_started/test/functional/home_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class WelcomeControllerTest < ActionController::TestCase
+class HomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
