b4f1c169089f7e9048dcbe7b38923f3162af6cce
spyhole <thankleft@gmail.com>
2012-06-02 12:17:13 +0800

    Update guides/code/getting_started/config/routes.rb

--- guides/code/getting_started/config/routes.rb
+++ guides/code/getting_started/config/routes.rb
@@ -53,7 +53,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
