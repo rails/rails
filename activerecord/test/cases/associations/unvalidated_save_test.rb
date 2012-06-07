d01e3a039964134de7d1fb5f3d4993b083b60598
Joseph Lord <joseph@human-friendly.com>
2012-06-07 13:47:53 +0100

    Good set of tests illustrating the behaviour.

--- activerecord/test/cases/associations/unvalidated_save_test.rb
+++ activerecord/test/cases/associations/unvalidated_save_test.rb
@@ -6,15 +6,12 @@ class UnvalidatedSaveTest < ActiveRecord::TestCase
 
   def test_save_validate_false
     door  = Door.new
-#    puts "House IDs: #{House.order(:id).pluck(:id)}"
     last_house_id = House.order(:id).last.id
     bad_house_id = last_house_id + 10000
     house_count = House.count
-#    assert_equal house, door.house
-#    assert !door.persisted?
     door.house_id = bad_house_id
-    door.save(validate: false)
-    door.reload
+    assert_raise(ActiveRecord::InvalidForeignKey) { door.save(validate: false) }
+    assert_raise(ActiveRecord::RecordNotFound) { door.reload }
     assert_equal bad_house_id, door.house_id
     assert_raises(ActiveRecord::RecordNotFound) { House.find bad_house_id }
     assert_equal house_count, House.count
@@ -22,20 +19,46 @@ class UnvalidatedSaveTest < ActiveRecord::TestCase
   
   def test_save_validate_false_after_valid_test
     door  = Door.new
-#    puts "House IDs: #{House.order(:id).pluck(:id)}"
     last_house_id = House.order(:id).last.id
     bad_house_id = last_house_id + 10000
     house_count = House.count
-#    assert_equal house, door.house
-#    assert !door.persisted?
     door.house_id = bad_house_id
-#    assert !door.valid?
-#    puts door.inspect
     door.valid?
-    door.save(validate: false)
-    door.reload
+    assert_raise(ActiveRecord::InvalidForeignKey) { door.save(validate: false) }
+    assert_raise(ActiveRecord::RecordNotFound) { door.reload }
     assert_equal bad_house_id, door.house_id
     assert_raises(ActiveRecord::RecordNotFound) { House.find bad_house_id }
     assert_equal house_count, House.count
   end
+  def test_save_validate_false_after_association_created
+    houseA = House.new
+    door  = Door.new
+    door.house = houseA
+    last_house_id = House.order(:id).last.id
+    bad_house_id = last_house_id + 10000
+    house_count = House.count
+    door.house_id = bad_house_id
+    assert_raise(ActiveRecord::InvalidForeignKey) { door.save(validate: false) }
+    assert_raise(ActiveRecord::RecordNotFound) { door.reload }
+    assert_equal bad_house_id, door.house_id
+    assert_raises(ActiveRecord::RecordNotFound) { House.find bad_house_id }
+    assert_equal house_count, House.count
+  end
+  
+  def test_save_validate_false_after_valid_test_after_association_created
+    houseA = House.new
+    door  = Door.new
+    door.house = houseA
+    last_house_id = House.order(:id).last.id
+    bad_house_id = last_house_id + 10000
+    house_count = House.count
+    door.house_id = bad_house_id
+    door.valid?
+    assert_raise(ActiveRecord::InvalidForeignKey) { door.save(validate: false) }
+    assert_raise(ActiveRecord::RecordNotFound) { door.reload }
+    assert_equal bad_house_id, door.house_id
+    assert_raises(ActiveRecord::RecordNotFound) { House.find bad_house_id }
+    assert_equal house_count, House.count
+  end
+  
 end
