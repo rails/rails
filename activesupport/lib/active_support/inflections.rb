8496786cdad1f5d9f74c663c94585211d48507ac
Eido NABESHIMA <closer009@gmail.com>
2012-06-11 16:24:17 +0900

    Update b5d82e20a8be3bf6b588f780640505d7ec12d126

--- activesupport/lib/active_support/inflections.rb
+++ activesupport/lib/active_support/inflections.rb
@@ -50,6 +50,7 @@ module ActiveSupport
     inflect.singular(/(quiz)zes$/i, '\1')
     inflect.singular(/(database)s$/i, '\1')
 
+    inflect.irregular('campus', 'campuses')
     inflect.irregular('person', 'people')
     inflect.irregular('man', 'men')
     inflect.irregular('child', 'children')
