f5e7cb84cd377feb1b60c5356ce02123e9c94380
Aaron Patterson <aaron.patterson@gmail.com>
2012-06-14 09:42:24 -0700

    AV::TestCase does not need to be loaded when eagerly loading the framework

--- actionpack/lib/action_view.rb
+++ actionpack/lib/action_view.rb
@@ -36,7 +36,6 @@ module ActionView
     autoload :LookupContext
     autoload :PathSet
     autoload :Template
-    autoload :TestCase
 
     autoload_under "renderer" do
       autoload :Renderer
@@ -72,6 +71,8 @@ module ActionView
       autoload :WrongEncodingError
     end
   end
+
+  autoload :TestCase
 
   ENCODING_FLAG = '#.*coding[:=]\s*(\S+)[ \t]*'
 end
