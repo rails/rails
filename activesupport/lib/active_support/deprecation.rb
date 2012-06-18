a556c7e25b135e08fd0707f0e2cb01100db65501
Francesco Rodríguez <lrodriguezsanc@gmail.com>
2012-06-17 23:19:18 -0500

    bump AS deprecation_horizon to 4.0

--- activesupport/lib/active_support/deprecation.rb
+++ activesupport/lib/active_support/deprecation.rb
@@ -9,7 +9,7 @@ module ActiveSupport
       # The version the deprecated behavior will be removed, by default.
       attr_accessor :deprecation_horizon
     end
-    self.deprecation_horizon = '3.2'
+    self.deprecation_horizon = '4.0'
 
     # By default, warnings are not silenced and debugging is off.
     self.silenced = false
