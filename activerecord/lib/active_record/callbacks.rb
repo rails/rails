3b4d286cd9d2a6ebb69cb08933f13171aa14304c
Andrés Mejía <andmej@gmail.com>
2012-06-19 13:41:36 -0500

    Typo in documentation.

--- activerecord/lib/active_record/callbacks.rb
+++ activerecord/lib/active_record/callbacks.rb
@@ -34,7 +34,7 @@ module ActiveRecord
   # Examples:
   #   class CreditCard < ActiveRecord::Base
   #     # Strip everything but digits, so the user can specify "555 234 34" or
-  #     # "5552-3434" or both will mean "55523434"
+  #     # "5552-3434" and both will mean "55523434"
   #     before_validation(:on => :create) do
   #       self.number = number.gsub(/[^0-9]/, "") if attribute_present?("number")
   #     end
