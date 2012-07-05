edcfb1e6463ae13443c53d04d9aa403458893e99
xdite <xdite@rocodev.com>
2012-07-06 00:24:13 +0800

    add common flash types, such as error, warning

--- actionpack/lib/action_controller/metal/flash.rb
+++ actionpack/lib/action_controller/metal/flash.rb
@@ -4,18 +4,27 @@ module ActionController #:nodoc:
 
     included do
       delegate :flash, :to => :request
-      delegate :alert, :notice, :to => "request.flash"
-      helper_method :alert, :notice
+      delegate :alert, :notice, :error, :warning, :to => "request.flash"
+      helper_method :alert, :notice, :error, :warning
     end
 
     protected
       def redirect_to(options = {}, response_status_and_flash = {}) #:doc:
+
         if alert = response_status_and_flash.delete(:alert)
           flash[:alert] = alert
         end
 
         if notice = response_status_and_flash.delete(:notice)
           flash[:notice] = notice
+        end
+
+        if error = response_status_and_flash.delete(:error)
+          flash[:error] = error
+        end
+
+        if warning = response_status_and_flash.delete(:warning)
+          flash[:warning] = warning
         end
 
         if other_flashes = response_status_and_flash.delete(:flash)
