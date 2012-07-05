14671087f8f0a52bef2bf67cbf91359a64250ab3
Piotr Sarnacki <drogus@gmail.com>
2012-07-05 22:02:11 +0200

    Implement ActionView::Template::Types
    AV::Template::Types is a small abstraction to allow to specify template types
    that can be used in ActionView. When Action Pack is loaded it's replaced with
    Mime::Type.

--- actionpack/lib/action_dispatch.rb
+++ actionpack/lib/action_dispatch.rb
@@ -104,4 +104,5 @@ autoload :Mime, 'action_dispatch/http/mime_type'
 
 ActiveSupport.on_load(:action_view) do
   ActionView::Base.default_formats ||= Mime::SET.symbols
+  ActionView::Template::Types.delegate_to Mime
 end
