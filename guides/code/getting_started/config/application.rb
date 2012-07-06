a5fe8ae69da1b7a088985de063c3c221860d3cd8
Jey Balachandran <jey.balachandran@gmail.com>
2012-07-06 08:06:06 -0700

    Filter password_confirmation param from being logged.
    Most common Rails authentication libraries use password_confirmation
    field on registration which should be excluded by default alongside
    the password field.

--- guides/code/getting_started/config/application.rb
+++ guides/code/getting_started/config/application.rb
@@ -33,7 +33,7 @@ module Blog
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
