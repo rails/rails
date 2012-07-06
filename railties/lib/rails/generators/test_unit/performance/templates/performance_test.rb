ae3fdac89626eadd03cc72e603c884634dc4d868
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-07-06 15:53:17 -0500

    update performance test template to use test method

--- railties/lib/rails/generators/test_unit/performance/templates/performance_test.rb
+++ railties/lib/rails/generators/test_unit/performance/templates/performance_test.rb
@@ -6,7 +6,7 @@ class <%= class_name %>Test < ActionDispatch::PerformanceTest
   # self.profile_options = { :runs => 5, :metrics => [:wall_time, :memory],
   #                          :output => 'tmp/performance', :formats => [:flat] }
 
-  def test_homepage
+  test "homepage" do
     get '/'
   end
 end
