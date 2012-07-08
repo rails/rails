0cae7c60ac6e6209c3d9a7cc64ad2298b7e27fe4
Aleksey Magusev <lexmag@gmail.com>
2012-07-08 17:01:49 +0400

    Add references statements to migration generator
    AddXXXToYYY/RemoveXXXFromYYY migrations are produced with references
    statements, for instance
    
        rails g migration AddReferencesToProducts user:references
    supplier:references{polymorphic}
    
    will generate the migration with:
    
        add_reference :products, :user, index: true
        add_reference :products, :supplier, polymorphic: true, index: true

--- activerecord/lib/rails/generators/active_record/migration/templates/migration.rb
+++ activerecord/lib/rails/generators/active_record/migration/templates/migration.rb
@@ -2,29 +2,41 @@ class <%= migration_class_name %> < ActiveRecord::Migration
 <%- if migration_action == 'add' -%>
   def change
 <% attributes.each do |attribute| -%>
+  <%- if attribute.reference? -%>
+    add_reference :<%= table_name %>, :<%= attribute.name %><%= attribute.inject_options %>
+  <%- else -%>
     add_column :<%= table_name %>, :<%= attribute.name %>, :<%= attribute.type %><%= attribute.inject_options %>
     <%- if attribute.has_index? -%>
     add_index :<%= table_name %>, :<%= attribute.index_name %><%= attribute.inject_index_options %>
     <%- end -%>
+  <%- end -%>
 <%- end -%>
   end
 <%- else -%>
   def up
 <% attributes.each do |attribute| -%>
-  <%- if migration_action -%>
-    <%= migration_action %>_column :<%= table_name %>, :<%= attribute.name %>
+<%- if migration_action -%>
+  <%- if attribute.reference? -%>
+    remove_reference :<%= table_name %>, :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %>
+  <%- else -%>
+    remove_column :<%= table_name %>, :<%= attribute.name %>
   <%- end -%>
+<%- end -%>
 <%- end -%>
   end
 
   def down
 <% attributes.reverse.each do |attribute| -%>
-  <%- if migration_action -%>
+<%- if migration_action -%>
+  <%- if attribute.reference? -%>
+    add_reference :<%= table_name %>, :<%= attribute.name %><%= attribute.inject_options %>
+  <%- else -%>
     add_column :<%= table_name %>, :<%= attribute.name %>, :<%= attribute.type %><%= attribute.inject_options %>
     <%- if attribute.has_index? -%>
     add_index :<%= table_name %>, :<%= attribute.index_name %><%= attribute.inject_index_options %>
     <%- end -%>
   <%- end -%>
+<%- end -%>
 <%- end -%>
   end
 <%- end -%>
