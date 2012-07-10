211d88b71b3df2ae161b23579a79f8e937132388
Aleksey Magusev <lexmag@gmail.com>
2012-07-11 01:38:21 +0400

    Add join table migration generator
    For instance, running
    
        rails g migration CreateMediaJoinTable artists musics:uniq
    
    will create a migration with
    
        create_join_table :artists, :musics do |t|
          # t.index [:artist_id, :music_id]
          t.index [:music_id, :artist_id], unique: true
        end

--- activerecord/lib/rails/generators/active_record/migration/migration_generator.rb
+++ activerecord/lib/rails/generators/active_record/migration/migration_generator.rb
@@ -11,15 +11,28 @@ module ActiveRecord
       end
 
       protected
-        attr_reader :migration_action
+      attr_reader :migration_action, :join_tables
 
-        def set_local_assigns!
-          if file_name =~ /^(add|remove)_.*_(?:to|from)_(.*)/
-            @migration_action = $1
-            @table_name       = $2.pluralize
+      def set_local_assigns!
+        case file_name
+        when /^(add|remove)_.*_(?:to|from)_(.*)/
+          @migration_action = $1
+          @table_name       = $2.pluralize
+        when /join_table/
+          if attributes.length == 2
+            @migration_action = 'join'
+            @join_tables      = attributes.map(&:name)
+
+            set_index_names
           end
         end
+      end
 
+      def set_index_names
+        attributes.each_with_index do |attr, i|
+          attr.index_name = [attr, attributes[i - 1]].map{ |a| :"#{a.name.singularize}_id"}
+        end
+      end
     end
   end
 end
