2c22376fe04b89e8f34620139720b85a85ce3428
Dmitry Vorotilin <d.vorotilin@gmail.com>
2012-07-11 12:18:52 +0400

    Common behavior with adding formats to lookup_context for TemplateRenderer and PartialRenderer

--- actionpack/lib/action_view/renderer/partial_renderer.rb
+++ actionpack/lib/action_view/renderer/partial_renderer.rb
@@ -281,6 +281,8 @@ module ActionView
       @block   = block
       @details = extract_details(options)
 
+      prepend_formats(options[:formats])
+
       if String === partial
         @object     = options[:object]
         @path       = partial
