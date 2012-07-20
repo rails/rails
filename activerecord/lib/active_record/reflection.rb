45285f56f4993e3910df59e7e68b465a287ef089
Jon Leighton <j@jonathanleighton.com>
2012-07-20 19:18:12 +0100

    remove unused method

--- activerecord/lib/active_record/reflection.rb
+++ activerecord/lib/active_record/reflection.rb
@@ -116,10 +116,6 @@ module ActiveRecord
           active_record == other_aggregation.active_record
       end
 
-      def sanitized_conditions #:nodoc:
-        @sanitized_conditions ||= klass.send(:sanitize_sql, options[:conditions]) if options[:conditions]
-      end
-
       private
         def derive_class_name
           name.to_s.camelize
