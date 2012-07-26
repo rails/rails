3166606dcbbbea5d6cc07bda233273e1dfa8821d
Aaron Patterson <aaron.patterson@gmail.com>
2012-07-26 14:59:39 -0700

    bumping to 3.0.16

--- activesupport/lib/active_support/version.rb
+++ activesupport/lib/active_support/version.rb
@@ -2,7 +2,7 @@ module ActiveSupport
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 0
-    TINY  = 15
+    TINY  = 16
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
