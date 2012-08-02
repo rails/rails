4028bed8128f3d4eac2a55e85a504f1143944eae
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-08-02 14:01:44 -0300

    Do not consider the numeric attribute as changed if the old value is zero and the new value is not a string.
    Before this commit this was the behavior
    
    r = Review.find_by_issue(0)
    r.issue
    => 0
    r.changes
    => {}
    r.issue = 0
    => 0
    r.changed?
    => true
    r.changes
    => {"issue"=>[0,0]}
    
    Fixes #7237
    
    Conflicts:
    	activerecord/CHANGELOG.md

--- activerecord/test/cases/dirty_test.rb
+++ activerecord/test/cases/dirty_test.rb
@@ -201,7 +201,7 @@ class DirtyTest < ActiveRecord::TestCase
     end
   end
 
-  def test_nullable_integer_zero_to_string_zero_not_marked_as_changed
+  def test_integer_zero_to_string_zero_not_marked_as_changed
     pirate = Pirate.new
     pirate.parrot_id = 0
     pirate.catchphrase = 'arrr'
@@ -212,6 +212,19 @@ class DirtyTest < ActiveRecord::TestCase
     pirate.parrot_id = '0'
     assert !pirate.changed?
   end
+
+  def test_integer_zero_to_integer_zero_not_marked_as_changed
+    pirate = Pirate.new
+    pirate.parrot_id = 0
+    pirate.catchphrase = 'arrr'
+    assert pirate.save!
+
+    assert !pirate.changed?
+
+    pirate.parrot_id = 0
+    assert !pirate.changed?
+  end
+
 
   def test_zero_to_blank_marked_as_changed
     pirate = Pirate.new
