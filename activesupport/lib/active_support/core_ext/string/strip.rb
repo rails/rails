b4a8f4c0a5c826cd30f3d2bf310f32afeb67929e
Ace Suares <ace@suares.an>
2012-08-04 12:28:12 -0400

    added with_indent parameter to strip_heredoc

--- activesupport/lib/active_support/core_ext/string/strip.rb
+++ activesupport/lib/active_support/core_ext/string/strip.rb
@@ -19,8 +19,12 @@ class String
   #
   # Technically, it looks for the least indented line in the whole string, and removes
   # that amount of leading whitespace.
-  def strip_heredoc
+  #
+  # Use strip_heredoc(2) if you want the result to be preceded by 2 spaces. For instance, if you want to
+  # insert something in a place that already has two spaces indent, like config/environments/production.rb
+  #
+  def strip_heredoc(with_indent = 0)
     indent = scan(/^[ \t]*(?=\S)/).min.try(:size) || 0
-    gsub(/^[ \t]{#{indent}}/, '')
+    gsub(/^[ \t]{#{indent}}/, ' ' * with_indent)
   end
 end
