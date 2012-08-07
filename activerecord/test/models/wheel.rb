d2aad7ad70bc4d6719f047b9316e159b74fbf860
yalab <rudeboyjet@gmail.com>
2012-08-08 00:13:33 +0900

    Remove garbage line.

--- activerecord/test/models/wheel.rb
+++ activerecord/test/models/wheel.rb
@@ -1,6 +1,5 @@
 class Wheel < ActiveRecord::Base
   belongs_to :wheelable, :polymorphic => true, :counter_cache => true
-  attr_accessor :brand_new
   before_destroy :detachable?
 
   def detachable?
