82fe1160510c48a52bf06da235ddd65e1ad3b525
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-08-07 19:39:38 -0300

    Fix the deprecation horizon. [ci skip]

--- activesupport/lib/active_support/json/variable.rb
+++ activesupport/lib/active_support/json/variable.rb
@@ -5,7 +5,7 @@ module ActiveSupport
     # Deprecated: A string that returns itself as its JSON-encoded form.
     class Variable < String
       def initialize(*args)
-        ActiveSupport::Deprecation.warn 'ActiveSupport::JSON::Variable is deprecated and will be removed in Rails 4.0. ' \
+        ActiveSupport::Deprecation.warn 'ActiveSupport::JSON::Variable is deprecated and will be removed in Rails 4.1. ' \
                                         'For your own custom JSON literals, define #as_json and #encode_json yourself.'
         super
       end
