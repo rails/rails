1151959f97eeece3c2ae43e26e10bd4c77cbd50c
Santiago Pastorino <santiago@wyeworks.com>
2012-08-08 14:33:39 -0700

    Do not mark strip_tags result as html_safe
    Thanks to Marek Labos & Nethemba
    
    CVE-2012-3465

--- actionpack/lib/action_view/helpers/sanitize_helper.rb
+++ actionpack/lib/action_view/helpers/sanitize_helper.rb
@@ -81,7 +81,7 @@ module ActionView
       #   strip_tags("<div id='top-bar'>Welcome to my website!</div>")
       #   # => Welcome to my website!
       def strip_tags(html)
-        self.class.full_sanitizer.sanitize(html).try(:html_safe)
+        self.class.full_sanitizer.sanitize(html)
       end
 
       # Strips all link tags from +text+ leaving just the link text.
