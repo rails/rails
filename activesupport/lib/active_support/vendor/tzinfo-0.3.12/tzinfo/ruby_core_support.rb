a1d2a220477b06c9aba3402b0493e91f590767e2
Aman Gupta <aman@tmm1.net>
2012-08-16 11:52:43 -0700

    fix TZInfo on ruby 1.9
    http://developer.uservoice.com/blog/2012/03/04/how-to-upgrade-a-rails-2-3-app-to-ruby-1-9-3/

--- activesupport/lib/active_support/vendor/tzinfo-0.3.12/tzinfo/ruby_core_support.rb
+++ activesupport/lib/active_support/vendor/tzinfo-0.3.12/tzinfo/ruby_core_support.rb
@@ -47,9 +47,30 @@ module TZInfo
       def self.datetime_new!(ajd = 0, of = 0, sg = Date::ITALY)
         DateTime.new!(ajd, of, sg)
       end
-    else
+    elsif DateTime.respond_to? :new0
       def self.datetime_new!(ajd = 0, of = 0, sg = Date::ITALY)
         DateTime.new0(ajd, of, sg)
+      end
+    else
+      HALF_DAYS_IN_DAY = rational_new!(1, 2)
+
+      def self.datetime_new!(ajd = 0, of = 0, sg = Date::ITALY)
+        # Convert from an Astronomical Julian Day number to a civil Julian Day number.
+        jd = ajd + of + HALF_DAYS_IN_DAY
+
+        # Ruby trunk revision 31862 changed the behaviour of DateTime.jd so that it will no
+        # longer accept a fractional civil Julian Day number if further arguments are specified.
+        # Calculate the hours, minutes and seconds to pass to jd.
+
+        jd_i = jd.to_i
+        jd_i -= 1 if jd < 0
+        hours = (jd - jd_i) * 24
+        hours_i = hours.to_i
+        minutes = (hours - hours_i) * 60
+        minutes_i = minutes.to_i
+        seconds = (minutes - minutes_i) * 60
+
+        DateTime.jd(jd_i, hours_i, minutes_i, seconds, of, sg)
       end
     end
   end
