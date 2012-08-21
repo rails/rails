b85894e854d09b2e4c00c06cc4f1077fd6c3c31d
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2012-08-21 17:35:38 -0300

    Set eager load option in "make_basic_app" for railties tests
    Avoid output with config option not set when running some tests.

--- railties/test/isolation/abstract_unit.rb
+++ railties/test/isolation/abstract_unit.rb
@@ -136,6 +136,7 @@ module TestHelpers
       require "action_controller/railtie"
 
       app = Class.new(Rails::Application)
+      app.config.eager_load = false
       app.config.secret_token = "3b7cd727ee24e8444053437c36cc66c4"
       app.config.session_store :cookie_store, :key => "_myapp_session"
       app.config.active_support.deprecation = :log
