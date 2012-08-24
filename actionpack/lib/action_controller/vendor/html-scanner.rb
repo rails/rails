3fb10fd4790841ab82c4f0697b5527b7d6a40c6c
Piotr Sarnacki <drogus@gmail.com>
2012-08-25 01:11:22 +0200

    Add deprecation horizon to html-scanner

--- actionpack/lib/action_controller/vendor/html-scanner.rb
+++ actionpack/lib/action_controller/vendor/html-scanner.rb
@@ -1,4 +1,5 @@
 require 'action_view/vendor/html-scanner'
 require 'active_support/deprecation'
 
-ActiveSupport::Deprecation.warn 'vendored html-scanner was moved to action_view, please require "action_view/vendor/html-scanner" instead'
+ActiveSupport::Deprecation.warn 'Vendored html-scanner was moved to action_view, please require "action_view/vendor/html-scanner" instead. ' +
+                                'This file will be removed in Rails 4.1'
