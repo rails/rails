21be1808a0887a12cf8124ad01a54bc075361f03
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-08-26 02:54:46 -0300

    Move the serialized attribute tests to their own file

--- activerecord/test/cases/attribute_methods_test.rb
+++ activerecord/test/cases/attribute_methods_test.rb
@@ -729,17 +729,6 @@ class AttributeMethodsTest < ActiveRecord::TestCase
     Object.send(:undef_method, :title) # remove test method from object
   end
 
-  def test_list_of_serialized_attributes
-    assert_equal %w(content), Topic.serialized_attributes.keys
-    assert_equal %w(preferences), Contact.serialized_attributes.keys
-  end
-
-  def test_serialized_attributes_are_class_level_settings
-    topic = Topic.new
-    assert_raise(NoMethodError) { topic.serialized_attributes = [] }
-    assert_deprecated { topic.serialized_attributes }
-  end
-
   def test_instance_method_should_be_defined_on_the_base_class
     subklass = Class.new(Topic)
 
