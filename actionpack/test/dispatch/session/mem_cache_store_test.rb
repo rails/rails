c53ec0c22d28cc09a79605cf5c2d9359dfb5b4de
kennyj <kennyj@gmail.com>
2012-08-31 00:08:30 +0900

    Remove needless require

--- actionpack/test/dispatch/session/mem_cache_store_test.rb
+++ actionpack/test/dispatch/session/mem_cache_store_test.rb
@@ -1,5 +1,4 @@
 require 'abstract_unit'
-require 'fixtures/session_autoload_test/session_autoload_test/foo'
 
 # You need to start a memcached server inorder to run these tests
 class MemCacheStoreTest < ActionDispatch::IntegrationTest
