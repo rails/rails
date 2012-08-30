e7c23a4a7a0a576231b9d77f4ec47f7f10eb7d42
Christos Zisopoulos <christos@me.com>
2012-08-30 14:27:07 +0200

    Further improve RENDER_DEPENDENCY regexp comments

--- actionpack/lib/action_view/digestor.rb
+++ actionpack/lib/action_view/digestor.rb
@@ -16,7 +16,7 @@ module ActionView
     #   render(message.topics) => render("topics/topic")
     RENDER_DEPENDENCY = /
       render\s*                     # render, followed by optional whitespace
-      \(?                           # start a optional parenthesis for the render call
+      \(?                           # start an optional parenthesis for the render call
       (partial:|:partial\s+=>)?\s*  # naming the partial, used with collection -- 1st capture
       ([@a-z"'][@a-z_\/\."']+)      # the template name itself -- 2nd capture
     /x
