1fa5c3b39f5d0bb23c8c3d60b023613386629946
Prem Sichanugrist <s@sikac.hu>
2012-09-17 00:46:26 -0400

    Fix footnote format
    Markdown has footnote, but Redcarpet doesn't support it.

--- guides/rails_guides/markdown/renderer.rb
+++ guides/rails_guides/markdown/renderer.rb
@@ -25,12 +25,23 @@ HTML
       def paragraph(text)
         if text =~ /^(TIP|IMPORTANT|CAUTION|WARNING|NOTE|INFO|TODO)[.:](.*?)/
           convert_notes(text)
+        elsif text =~ /^\[<sup>(\d+)\]:<\/sup> (.+)$/
+          linkback = %(<a href="#footnote-#{$1}-ref"><sup>#{$1}</sup></a>)
+          %(<p class="footnote" id="footnote-#{$1}">#{linkback} #{$2}</p>)
         else
+          text = convert_footnotes(text)
           "<p>#{text}</p>"
         end
       end
 
       private
+
+        def convert_footnotes(text)
+          text.gsub(/\[<sup>(\d+)\]<\/sup>/i) do
+            %(<sup class="footnote" id="footnote-#{$1}-ref">) +
+              %(<a href="#footnote-#{$1}">#{$1}</a></sup>)
+          end
+        end
 
         def brush_for(code_type)
           case code_type
