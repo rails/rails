1e56f1f14c7b033a6d180514c83e61193812fede
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-09-20 11:47:32 -0500

    update AMo::ForbiddenAttributesError documentation [ci skip]

--- activemodel/lib/active_model/forbidden_attributes_protection.rb
+++ activemodel/lib/active_model/forbidden_attributes_protection.rb
@@ -1,4 +1,16 @@
 module ActiveModel
+  # Raised when forbidden attributes are used for mass assignment.
+  #
+  #   class Person < ActiveRecord::Base
+  #   end
+  #
+  #   params = ActionController::Parameters.new(name: 'Bob')
+  #   Person.new(params)
+  #   # => ActiveModel::ForbiddenAttributesError
+  #
+  #   params.permit!
+  #   Person.new(params)
+  #   # => #<Person id: nil, name: "Bob">
   class ForbiddenAttributesError < StandardError
   end
 
