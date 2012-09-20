6783c3f449f9c80268905fe79e444f40d1b2f883
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-09-20 12:14:29 -0500

    change AMo::ForbiddenAttributesProtection#sanitize_for_mass_assignment to protected

--- activemodel/lib/active_model/forbidden_attributes_protection.rb
+++ activemodel/lib/active_model/forbidden_attributes_protection.rb
@@ -14,13 +14,14 @@ module ActiveModel
   class ForbiddenAttributesError < StandardError
   end
 
-  module ForbiddenAttributesProtection
-    def sanitize_for_mass_assignment(attributes, options = {})
-      if attributes.respond_to?(:permitted?) && !attributes.permitted?
-        raise ActiveModel::ForbiddenAttributesError
-      else
-        attributes
+  module ForbiddenAttributesProtection # :nodoc:
+    protected
+      def sanitize_for_mass_assignment(attributes, options = {})
+        if attributes.respond_to?(:permitted?) && !attributes.permitted?
+          raise ActiveModel::ForbiddenAttributesError
+        else
+          attributes
+        end
       end
-    end
   end
 end
