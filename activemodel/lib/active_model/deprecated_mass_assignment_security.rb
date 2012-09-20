356eed9c6034064c097cf607588185b1c526d93b
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-09-20 12:14:53 -0500

    add :nodoc: to AMo::DeprecatedMassAssignmentSecurity::ClassMethods [ci skip]

--- activemodel/lib/active_model/deprecated_mass_assignment_security.rb
+++ activemodel/lib/active_model/deprecated_mass_assignment_security.rb
@@ -2,14 +2,14 @@ module ActiveModel
   module DeprecatedMassAssignmentSecurity # :nodoc:
     extend ActiveSupport::Concern
 
-     module ClassMethods
-       def attr_protected(*args) # :nodoc:
+     module ClassMethods # :nodoc:
+       def attr_protected(*args)
          raise "`attr_protected` is extracted out of Rails into a gem. " \
            "Please use new recommended protection model for params " \
            "or add `protected_attributes` to your Gemfile to use old one."
        end
 
-       def attr_accessible(*args) # :nodoc:
+       def attr_accessible(*args)
          raise "`attr_accessible` is extracted out of Rails into a gem. " \
            "Please use new recommended protection model for params " \
            "or add `protected_attributes` to your Gemfile to use old one."
