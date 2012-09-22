b21012bc3911d53eef6fc3bc3222c8e762664671
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-09-21 23:38:18 -0500

    nodoc AR::Railtie [ci skip]

--- activerecord/lib/active_record/railtie.rb
+++ activerecord/lib/active_record/railtie.rb
@@ -10,7 +10,7 @@ require "action_controller/railtie"
 
 module ActiveRecord
   # = Active Record Railtie
-  class Railtie < Rails::Railtie
+  class Railtie < Rails::Railtie # :nodoc:
     config.active_record = ActiveSupport::OrderedOptions.new
 
     config.app_generators.orm :active_record, :migration => true,
@@ -76,7 +76,7 @@ module ActiveRecord
         config.after_initialize do |app|
           ActiveSupport.on_load(:active_record) do
             filename = File.join(app.config.paths["db"].first, "schema_cache.dump")
-    
+
             if File.file?(filename)
               cache = Marshal.load File.binread filename
               if cache.version == ActiveRecord::Migrator.current_version
