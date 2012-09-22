dc3e767fd66b488ed2f539d67400d9226c7c97d9
Robin Dupret <robin.dupret@gmail.com>
2012-09-22 17:11:31 +0300

    Little fix in application.rb template
    The specified rake command was "rake -D time" but we should use "rake time:zones:all"

--- railties/lib/rails/generators/rails/app/templates/config/application.rb
+++ railties/lib/rails/generators/rails/app/templates/config/application.rb
@@ -28,7 +28,7 @@ module <%= app_const_base %>
     # config.autoload_paths += %W(#{config.root}/extras)
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # Run "rake time:zones:all" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
