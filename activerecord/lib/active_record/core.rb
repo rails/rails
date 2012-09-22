a0457317183a7ab0add760cf6678b72c4e3c0e8f
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-09-22 18:29:37 -0500

    remove added indentation from the last commit [ci skip]

--- activerecord/lib/active_record/core.rb
+++ activerecord/lib/active_record/core.rb
@@ -1,4 +1,4 @@
-  require 'active_support/core_ext/hash/indifferent_access'
+require 'active_support/core_ext/hash/indifferent_access'
 require 'active_support/core_ext/object/duplicable'
 require 'thread'
 
