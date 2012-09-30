cc5076b47c78caccd7a62d299d961bf1b7ba0c3c
Jarl Friis <jarl@softace.dk>
2012-09-30 20:22:53 +0200

    Reducing ActiveSupport::Cache::FILENAME_MAX_SIZE to 116 to support ecryptfs, where the maximum filename length is approximately 143 (see http://unix.stackexchange.com/a/32834).

--- activesupport/lib/active_support/cache/file_store.rb
+++ activesupport/lib/active_support/cache/file_store.rb
@@ -12,7 +12,7 @@ module ActiveSupport
       attr_reader :cache_path
 
       DIR_FORMATTER = "%03X"
-      FILENAME_MAX_SIZE = 228 # max filename size on file system is 255, minus room for timestamp and random characters appended by Tempfile (used by atomic write)
+      FILENAME_MAX_SIZE = 116 # max filename size on file system is normally 255 (but 143 on ecryptfs), minus room for timestamp and random characters appended by Tempfile (used by atomic write)
       EXCLUDED_DIRS = ['.', '..'].freeze
 
       def initialize(cache_path, options = nil)
