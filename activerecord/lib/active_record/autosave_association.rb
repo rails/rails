ac158e7403ebc1328b1d2a97a638c1f72ce2bde8
Matt Jones <al2o3cr@gmail.com>
2012-10-02 17:46:45 -0400

    WIP on preventing cycles in autosave

--- activerecord/lib/active_record/autosave_association.rb
+++ activerecord/lib/active_record/autosave_association.rb
@@ -236,8 +236,21 @@ module ActiveRecord
     # Returns whether or not this record has been changed in any way (including whether
     # any of its nested autosave associations are likewise changed)
     def changed_for_autosave?
-      new_record? || changed? || marked_for_destruction? || nested_records_changed_for_autosave?
+      @_changed_for_autosave_called ||= false
+      if @_changed_for_autosave_called
+        # traversing a cyclic graph of objects; stop it
+        result = false
+      else
+        begin
+          @_changed_for_autosave_called = true
+          result = new_record? || changed? || marked_for_destruction? || nested_records_changed_for_autosave?
+        ensure
+          @_changed_for_autosave_called = false
+        end
+      end
+      result
     end
+
 
     private
 
