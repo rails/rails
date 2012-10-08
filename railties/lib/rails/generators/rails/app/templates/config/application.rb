d81e76ddd6038a55c647b119ad393404af978b94
David Heinemeier Hansson <david@loudthinking.com>
2012-10-08 17:44:51 +0200

    Not precompiling your assets for production is a bad idea -- do not encourage it

--- railties/lib/rails/generators/rails/app/templates/config/application.rb
+++ railties/lib/rails/generators/rails/app/templates/config/application.rb
@@ -11,12 +11,8 @@ require "action_mailer/railtie"
 <%= comment_if :skip_test_unit %>require "rails/test_unit/railtie"
 <% end -%>
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line.
-  Bundler.require(*Rails.groups(assets: %w(development test)))
-  # If you want your assets lazily compiled in production, use this line.
-  # Bundler.require(:default, :assets, Rails.env)
-end
+# Assume that assets are precompiled for production
+Bundler.require(*Rails.groups(assets: %w(development test))) if defined?(Bundler)
 
 module <%= app_const_base %>
   class Application < Rails::Application
