483abdefb10384867192b086b5e5577689f3afdb
Joseph Lord <joseph@human-friendly.com>
2012-10-08 12:46:07 +0100

    Add additional test for just touching the association and commented lines indicating key differences between the tests.

--- activerecord/test/cases/associations/unvalidated_save_test.rb
+++ activerecord/test/cases/associations/unvalidated_save_test.rb
@@ -20,17 +20,17 @@ class UnvalidatedSaveTest < ActiveRecord::TestCase
   def test_save_validate_false_after_valid_test
     # This passess
     door  = Door.new
+    # door.house = House.new # This would make the test fail below!!!
     bad_house_id = House.order(:id).last.id + 10000
     house_count = House.count
     door.house_id = bad_house_id
-    door.valid?
     assert_raise(ActiveRecord::InvalidForeignKey) { door.save(validate: false) }
     assert_raise(ActiveRecord::RecordNotFound) { door.reload }
     assert_equal bad_house_id, door.house_id
     assert_raises(ActiveRecord::RecordNotFound) { House.find bad_house_id }
     assert_equal house_count, House.count
   end
-  
+
   def test_save_validate_false_after_association_created
     # This fails due to the association having been set before the id is set to something invalid
     door  = Door.new
@@ -38,6 +38,23 @@ class UnvalidatedSaveTest < ActiveRecord::TestCase
     bad_house_id = House.order(:id).last.id + 10000
     house_count = House.count
     door.house_id = bad_house_id
+    # door.house # This would make it pass even with the above
+    # door.valid? # This would also make it pass even with the above
+    assert_raise(ActiveRecord::InvalidForeignKey) { door.save(validate: false) }  # Fails here
+    assert_raise(ActiveRecord::RecordNotFound) { door.reload }
+    assert_equal bad_house_id, door.house_id
+    assert_raises(ActiveRecord::RecordNotFound) { House.find bad_house_id }
+    assert_equal house_count, House.count
+  end
+
+  def test_save_validate_false_after_association_created_touched
+    # This passes because the touch on door.house cleans the stale reference
+    door  = Door.new
+    door.house = House.new
+    bad_house_id = House.order(:id).last.id + 10000
+    house_count = House.count
+    door.house_id = bad_house_id
+    door.house # This makes it pass
     assert_raise(ActiveRecord::InvalidForeignKey) { door.save(validate: false) }
     assert_raise(ActiveRecord::RecordNotFound) { door.reload }
     assert_equal bad_house_id, door.house_id
