daf5b66a116ba71f9f917ec98c9ddc83a35ed3ae
David Heinemeier Hansson <david@loudthinking.com>
2012-10-08 17:52:07 +0200

    Encoding already defaults to utf-8 per lib/rails/application/configuration.rb:23

--- railties/lib/rails/generators/rails/app/templates/config/application.rb
+++ railties/lib/rails/generators/rails/app/templates/config/application.rb
@@ -31,9 +31,6 @@ module <%= app_const_base %>
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
-
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
