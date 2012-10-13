40475cf3649af4a6d868299b1796b36358f90917
Arturo Pie <arturotd08@yahoo.ca>
2012-10-13 18:30:43 -0400

    #7914 Using a better way to get the defaults from db.
    According to postgreSQL documentation:
    (http://www.postgresql.org/docs/8.2/static/catalog-pg-attrdef.html)
    we should not be using 'adsrc' field because this field is unaware of
    outside changes that could affect the way that default values are
    represented. Thus, I changed the queries to use
    "pg_get_expr(adbin, adrelid)" instead of the historical "adsrc" field.

--- activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
@@ -763,7 +763,8 @@ module ActiveRecord
         #  - ::regclass is a function that gives the id for a table name
         def column_definitions(table_name) #:nodoc:
           exec_query(<<-end_sql, 'SCHEMA').rows
-              SELECT a.attname, format_type(a.atttypid, a.atttypmod), d.adsrc, a.attnotnull, a.atttypid, a.atttypmod
+              SELECT a.attname, format_type(a.atttypid, a.atttypmod),
+                     pg_get_expr(d.adbin, d.adrelid), a.attnotnull, a.atttypid, a.atttypmod
                 FROM pg_attribute a LEFT JOIN pg_attrdef d
                   ON a.attrelid = d.adrelid AND a.attnum = d.adnum
                WHERE a.attrelid = '#{quote_table_name(table_name)}'::regclass
