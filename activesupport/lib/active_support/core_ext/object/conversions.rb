74bf8afb9fb92b4c45ffdd37b91e0580b86c1590
Xavier Noria <fxn@hashref.com>
2012-10-16 12:05:00 +0200

    Revert "remove unnecessary object/conversions file"
    This file is used at least by Active Merchant, its existence
    is maybe not necessary but no big deal either.
    
    This reverts commit ae9b3d7cecd77b9ace38671b183e1a360bf632b6.

---
+++ activesupport/lib/active_support/core_ext/object/conversions.rb
@@ -0,0 +1,4 @@
+require 'active_support/core_ext/object/to_param'
+require 'active_support/core_ext/object/to_query'
+require 'active_support/core_ext/array/conversions'
+require 'active_support/core_ext/hash/conversions'
