1f73a53071dc50b3264decde7cc35e1b154695ec
Yves Senn <yves.senn@gmail.com>
2012-10-16 21:34:58 +0200

    active_model/validations requires necessary files to run

--- activemodel/lib/active_model/validations.rb
+++ activemodel/lib/active_model/validations.rb
@@ -5,6 +5,7 @@ require 'active_support/core_ext/hash/keys'
 require 'active_support/core_ext/hash/except'
 require 'active_model/errors'
 require 'active_model/validations/callbacks'
+require 'active_model/validator'
 
 module ActiveModel
 
