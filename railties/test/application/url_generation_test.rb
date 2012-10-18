ff79df004c3cd1b3eb9d63e20f991331d35e51b5
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-10-18 14:33:40 -0300

    Remove warning setting eager_load

--- railties/test/application/url_generation_test.rb
+++ railties/test/application/url_generation_test.rb
@@ -17,6 +17,7 @@ module ApplicationTests
         config.secret_token = "3b7cd727ee24e8444053437c36cc66c4"
         config.session_store :cookie_store, key: "_myapp_session"
         config.active_support.deprecation = :log
+        config.eager_load = false
       end
 
       MyApp.initialize!
