71c67d1fd863bfa8fa820e4dc6e1d158586d6d4b
AvnerCohen <israbirding@gmail.com>
2012-10-20 16:54:43 +0200

    Hash Syntax changes to 1.9 format

--- activesupport/lib/active_support/core_ext/hash/slice.rb
+++ activesupport/lib/active_support/core_ext/hash/slice.rb
@@ -21,7 +21,7 @@ class Hash
   # Returns a hash containing the removed key/value pairs.
   #
   #   { a: 1, b: 2, c: 3, d: 4 }.slice!(:a, :b)
-  #   # => {:c => 3, :d => 4}
+  #   # => {c: 3, d: 4}
   def slice!(*keys)
     keys.map! { |key| convert_key(key) } if respond_to?(:convert_key, true)
     omit = slice(*self.keys - keys)
@@ -33,7 +33,7 @@ class Hash
   # Removes and returns the key/value pairs matching the given keys.
   #
   #   { a: 1, b: 2, c: 3, d: 4 }.extract!(:a, :b)
-  #   # => {:a => 1, :b => 2}
+  #   # => {a: 1, b: 2}
   def extract!(*keys)
     keys.each_with_object({}) { |key, result| result[key] = delete(key) }
   end
