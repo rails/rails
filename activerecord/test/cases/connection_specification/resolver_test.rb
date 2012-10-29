721cf54ee7b88fe6a4920b4ecabdd5beaae904a5
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-10-28 23:47:20 -0200

    Fix the skip code.
    Checking for the constant doesn't work

--- activerecord/test/cases/connection_specification/resolver_test.rb
+++ activerecord/test/cases/connection_specification/resolver_test.rb
@@ -9,7 +9,7 @@ module ActiveRecord
         end
 
         def test_url_host_no_db
-          skip "only if mysql is available" unless defined?(MysqlAdapter)
+          skip "only if mysql is available" unless current_adapter?(:MysqlAdapter) or current_adapter?(:Mysql2Adapter)
           spec = resolve 'mysql://foo?encoding=utf8'
           assert_equal({
             :adapter  => "mysql",
@@ -19,7 +19,7 @@ module ActiveRecord
         end
 
         def test_url_host_db
-          skip "only if mysql is available" unless defined?(MysqlAdapter)
+          skip "only if mysql is available" unless current_adapter?(:MysqlAdapter) or current_adapter?(:Mysql2Adapter)
           spec = resolve 'mysql://foo/bar?encoding=utf8'
           assert_equal({
             :adapter  => "mysql",
@@ -29,7 +29,7 @@ module ActiveRecord
         end
 
         def test_url_port
-          skip "only if mysql is available" unless defined?(MysqlAdapter)
+          skip "only if mysql is available" unless current_adapter?(:MysqlAdapter) or current_adapter?(:Mysql2Adapter)
           spec = resolve 'mysql://foo:123?encoding=utf8'
           assert_equal({
             :adapter  => "mysql",
@@ -40,7 +40,7 @@ module ActiveRecord
         end
 
         def test_encoded_password
-          skip "only if mysql is available" unless defined?(MysqlAdapter)
+          skip "only if mysql is available" unless current_adapter?(:MysqlAdapter) or current_adapter?(:Mysql2Adapter)
           password = 'am@z1ng_p@ssw0rd#!'
           encoded_password = URI.encode_www_form_component(password)
           spec = resolve "mysql://foo:#{encoded_password}@localhost/bar"
