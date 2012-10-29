fdfb3990cde29c48554551d1309b06c8ef584f32
Santiago Pastorino <santiago@wyeworks.com>
2012-10-29 14:38:23 -0200

    Bump to 3.2.9.rc1

--- activeresource/lib/active_resource/version.rb
+++ activeresource/lib/active_resource/version.rb
@@ -2,8 +2,8 @@ module ActiveResource
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 2
-    TINY  = 8
-    PRE   = nil
+    TINY  = 9
+    PRE   = "rc1"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
