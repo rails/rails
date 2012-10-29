29b1dc273e1ad6b9e13bf48fe3f12047850f9554
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-10-29 11:14:20 -0200

    Add test to deprecation of Path#children

--- railties/test/application/paths_test.rb
+++ railties/test/application/paths_test.rb
@@ -82,5 +82,11 @@ module ApplicationTests
       assert_not_in_load_path "tmp"
       assert_not_in_load_path "tmp", "cache"
     end
+
+    test "deprecated children method" do
+      assert_deprecated "children is deprecated and will be removed in Rails 4.1." do
+        @paths["app/assets"].children
+      end
+    end
   end
 end
