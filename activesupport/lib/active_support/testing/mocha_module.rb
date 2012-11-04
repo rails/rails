4cc09904f6e1d2d87588e4f8661e4a2530701077
James Mead <james@floehopper.org>
2012-11-04 13:48:06 +0000

    Raise MiniTest::Assertion instead of Mocha::ExpectationError.
    The latter is not recognized by MiniTest as an assertion failure and so
    it is records a test error, not a test failure as I ought to. This leads
    to confusing output in the test results.

--- activesupport/lib/active_support/testing/mocha_module.rb
+++ activesupport/lib/active_support/testing/mocha_module.rb
@@ -3,7 +3,14 @@ module ActiveSupport
     module MochaModule
       begin
         require 'mocha/api'
+        require 'mocha/expectation_error_factory'
+        require 'minitest/unit'
+
         include Mocha::API
+
+        def self.included(mod)
+          Mocha::ExpectationErrorFactory.exception_class = ::MiniTest::Assertion
+        end
 
         def before_setup
           mocha_setup
