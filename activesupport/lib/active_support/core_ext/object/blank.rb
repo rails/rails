9ec3637bc5e03b5138c430ae015dbcbed999f2e8
Aman Gupta <aman@tmm1.net>
2012-11-13 20:50:29 -0800

    backport String#encoding_aware? and String#blank? encoding fixes from rails3

--- activesupport/lib/active_support/core_ext/object/blank.rb
+++ activesupport/lib/active_support/core_ext/object/blank.rb
@@ -1,3 +1,6 @@
+# encoding: utf-8
+require 'active_support/core_ext/string/encoding'
+
 class Object
   # An object is blank if it's false, empty, or a whitespace string.
   # For example, "", "   ", +nil+, [], and {} are blank.
@@ -63,9 +66,24 @@ class Hash #:nodoc:
   alias_method :blank?, :empty?
 end
 
-class String #:nodoc:
+class String
+  # 0x3000: fullwidth whitespace
+  NON_WHITESPACE_REGEXP = %r![^\s#{[0x3000].pack("U")}]!
+
+  # A string is blank if it's empty or contains whitespaces only:
+  #
+  #   "".blank?                 # => true
+  #   "   ".blank?              # => true
+  #   "　".blank?               # => true
+  #   " something here ".blank? # => false
+  #
   def blank?
-    self !~ /\S/
+    # 1.8 does not takes [:space:] properly
+    if encoding_aware?
+      self !~ /[^[:space:]]/
+    else
+      self !~ NON_WHITESPACE_REGEXP
+    end
   end
 end
 
