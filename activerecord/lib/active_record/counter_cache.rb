0a178000f5394c08e32346f42b05b17451ce4e42
Matthew Robertson <matthewrobertson03@gmail.com>
2012-12-02 19:35:35 -0800

    grammar improvements for increment_counter and decrement_counter docs

--- activerecord/lib/active_record/counter_cache.rb
+++ activerecord/lib/active_record/counter_cache.rb
@@ -79,11 +79,12 @@ module ActiveRecord
         where(primary_key => id).update_all updates.join(', ')
       end
 
-      # Increment a number field by one, usually representing a count.
+      # Increment a numeric field by one, via a direct SQL update.
       #
-      # This is used for caching aggregate values, so that they don't need to be computed every time.
-      # For example, a DiscussionBoard may cache post_count and comment_count otherwise every time the board is
-      # shown it would have to run an SQL query to find how many posts and comments there are.
+      # This method is used primarily for maintaining counter_cache columns used to
+      # store aggregate values. For example, a DiscussionBoard may cache posts_count
+      # and comments_count to avoid running an SQL query to calculate the number of
+      # posts and comments there are each time it is displayed.
       #
       # ==== Parameters
       #
@@ -98,9 +99,10 @@ module ActiveRecord
         update_counters(id, counter_name => 1)
       end
 
-      # Decrement a number field by one, usually representing a count.
+      # Decrement a numeric field by one, via a direct SQL update.
       #
-      # This works the same as increment_counter but reduces the column value by 1 instead of increasing it.
+      # This works the same as increment_counter but reduces the column value by
+      # 1 instead of increasing it.
       #
       # ==== Parameters
       #
