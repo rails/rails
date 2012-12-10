c720ff75d8c3512feed4f80635ebc3e0ed7abcc6
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-12-10 15:29:38 -0300

    Remove warning of unused variable

--- activerecord/test/cases/calculations_test.rb
+++ activerecord/test/cases/calculations_test.rb
@@ -500,7 +500,7 @@ class CalculationsTest < ActiveRecord::TestCase
   end
 
   def test_pluck_not_auto_table_name_prefix_if_column_joined
-    c = Company.create!(:name => "test", :contracts => [Contract.new(:developer_id => 7)])
+    Company.create!(:name => "test", :contracts => [Contract.new(:developer_id => 7)])
     assert_equal [7], Company.joins(:contracts).pluck(:developer_id).map(&:to_i)
   end
 end
