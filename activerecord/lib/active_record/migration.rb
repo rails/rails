d68e299167c8da07dc63a55197313b5c3396c3a4
Yves Senn <yves.senn@gmail.com>
2012-12-12 21:51:38 +0100

    recognize migrations, in folders containing numbers and 'rb'.
    Backport of #8500
    Closes #8492
    
    Conflicts:
    
    	activerecord/test/cases/migrator_test.rb

--- activerecord/lib/active_record/migration.rb
+++ activerecord/lib/active_record/migration.rb
@@ -627,7 +627,7 @@ module ActiveRecord
         seen = Hash.new false
 
         migrations = files.map do |file|
-          version, name, scope = file.scan(/([0-9]+)_([_a-z0-9]*)\.?([_a-z0-9]*)?.rb/).first
+          version, name, scope = file.scan(/([0-9]+)_([_a-z0-9]*)\.?([_a-z0-9]*)?\.rb\z/).first
 
           raise IllegalMigrationNameError.new(file) unless version
           version = version.to_i
