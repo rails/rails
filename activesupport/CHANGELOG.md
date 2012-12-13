815a9431ab61376a7e8e1bdff21f87bc557992f8
Brett Carter <brett@janrain.com>
2012-12-13 15:52:19 -0800

    Remove unicode character encoding from ActiveSupport::JSON.encode
    The encoding scheme (e.g. ☠ -> "\u2620") was broken for characters
    not in the Basic Multilingual Plane.  It is possible to escape them
    for json using the weird encoding scheme of a twelve-character
    sequence representing the UTF-16 surrogate pair (e.g. '𠜎' ->
    "\u270e\u263a") but this wasn't properly handled in the escaping code.
    Since raw UTF-8 is allowed in json, it was decided to simply pass
    through the raw bytes rather than attempt to escape them.
    
    Backport of https://github.com/zbskii/rails/commit/9ace3a8820a5270f9b3f37b593f8bbea3e940f73
    
    Conflicts:
    
    	activesupport/CHANGELOG.md
    	activesupport/lib/active_support/json/encoding.rb
    	activesupport/test/json/encoding_test.rb

--- activesupport/CHANGELOG.md
+++ activesupport/CHANGELOG.md
@@ -1,5 +1,12 @@
 ## Rails 3.2.10 (unreleased)
 
+*   Remove surrogate unicode character encoding from ActiveSupport::JSON.encode
+    The encoding scheme was broken for unicode characters outside the basic
+    multilingual plane; since json is assumed to be UTF-8, and we already force the
+    encoding to UTF-8 simply pass through the un-encoded characters.
+
+    *Brett Carter*
+
 *   Fix mocha v0.13.0 compatibility. *James Mead*
 
 *   `#as_json` isolates options when encoding a hash. [Backport #8185]
--- activesupport/lib/active_support/json/encoding.rb
+++ activesupport/lib/active_support/json/encoding.rb
@@ -122,13 +122,7 @@ module ActiveSupport
           if string.respond_to?(:force_encoding)
             string = string.encode(::Encoding::UTF_8, :undef => :replace).force_encoding(::Encoding::BINARY)
           end
-          json = string.
-            gsub(escape_regex) { |s| ESCAPED_CHARS[s] }.
-            gsub(/([\xC0-\xDF][\x80-\xBF]|
-                   [\xE0-\xEF][\x80-\xBF]{2}|
-                   [\xF0-\xF7][\x80-\xBF]{3})+/nx) { |s|
-            s.unpack("U*").pack("n*").unpack("H*")[0].gsub(/.{4}/n, '\\\\u\&')
-          }
+          json = string.gsub(escape_regex) { |s| ESCAPED_CHARS[s] }
           json = %("#{json}")
           json.force_encoding(::Encoding::UTF_8) if json.respond_to?(:force_encoding)
           json
