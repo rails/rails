05e9075541f435b579a96a2519ab71ec27ace895
Rafael Mendonça França <rafaelmfranca@gmail.com>
2012-12-14 15:17:23 -0300

    Improve the documentation of cache_if and cache_unless

--- actionpack/lib/action_view/helpers/cache_helper.rb
+++ actionpack/lib/action_view/helpers/cache_helper.rb
@@ -120,9 +120,8 @@ module ActionView
         nil
       end
 
-      #Chache fragments of a view if +condition+ is true
+      # Cache fragments of a view if +condition+ is true
       #
-      #=== Example
       #   <%= cache_if admin?, project do %>
       #     <b>All the topics on this project</b>
       #     <%= render project.topics %>
@@ -137,8 +136,12 @@ module ActionView
         nil
       end
 
-      #Chache fragments of a view unless +condition+ is true
-      #(see +cache_if+ for a example)
+      # Cache fragments of a view unless +condition+ is true
+      #
+      #   <%= cache_unless admin?, project do %>
+      #     <b>All the topics on this project</b>
+      #     <%= render project.topics %>
+      #   <% end %>
       def cache_unless(condition, name = {}, options = nil, &block)
         cache_if !condition, name, options, &block
       end
