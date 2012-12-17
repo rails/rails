1068322063705e9f7ebb57af471e76dd3a94d3cc
Gary S. Weaver <garysweaver@gmail.com>
2012-12-17 08:54:48 -0500

    postgres adapter disable_referential_integrity now only falls back to trying to disable/enable USER triggers when can't disable/reenable ALL triggers. doesn't execute as many statements

--- activerecord/lib/active_record/connection_adapters/postgresql/referential_integrity.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/referential_integrity.rb
@@ -6,27 +6,21 @@ module ActiveRecord
           true
         end
 
-        def disable_referential_integrity #:nodoc:
-          all_disabled = []
-          user_disabled = []
-          if supports_disable_referential_integrity? then
-            tables.each do |name|
-              begin
-                execute "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER ALL;"
-                all_disabled << name
-              rescue
-                execute "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER USER;"
-                user_disabled << name
-              end
-            end
+      def disable_referential_integrity #:nodoc:
+        if supports_disable_referential_integrity? then
+          begin
+            execute tables.collect{|name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER ALL"}.join(";")
+          rescue
+            execute tables.collect{|name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER USER"}.join(";")
           end
-          yield
-        ensure
-          if supports_disable_referential_integrity? then
-            execute((
-                all_disabled.collect{|name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER ALL"} + 
-                user_disabled.collect{|name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER USER"}
-              ).join(";"))
+        end
+        yield
+      ensure
+        if supports_disable_referential_integrity? then
+          begin
+            execute tables.collect{|name| "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER ALL"}.join(";")
+          rescue
+            execute tables.collect{|name| "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER USER"}.join(";")
           end
         end
       end
