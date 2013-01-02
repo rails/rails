aca0c6d315258fe83bea81ce4d5e606ac18856ec
Gosha Arinich <me@goshakkk.name>
2013-01-02 18:33:52 +0300

    remove unnecessary #dup

--- actionpack/lib/action_dispatch/journey/formatter.rb
+++ actionpack/lib/action_dispatch/journey/formatter.rb
@@ -37,8 +37,7 @@ module ActionDispatch
       private
 
         def extract_parameterized_parts(route, options, recall, parameterize = nil)
-          constraints = recall.merge(options)
-          data = constraints.dup
+          data = recall.merge(options)
 
           keys_to_keep = route.parts.reverse.drop_while { |part|
             !options.key?(part) || (options[part] || recall[part]).nil?
