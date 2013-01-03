66258c0e48ed5cf26641a3096272a272611a783c
Tim Raymond <xtjraymondx@gmail.com>
2013-01-03 15:08:50 -0500

    Adding a usage message to install.rb script
    Previously, invoking the install.rb script with zero arguments
    caused it to attempt to install all gems without a version
    suffix, which would fail spectacularly. Failing gracefully with
    a usage message is more helpful to a first-time user.

--- install.rb
+++ install.rb
@@ -1,5 +1,10 @@
 version = ARGV.pop
 
+if version.nil?
+  puts "Usage: ruby install.rb version"
+  exit(64)
+end
+
 %w( activesupport activemodel activerecord actionpack actionmailer railties ).each do |framework|
   puts "Installing #{framework}..."
   `cd #{framework} && gem build #{framework}.gemspec && gem install #{framework}-#{version}.gem --local --no-ri --no-rdoc && rm #{framework}-#{version}.gem`
