e05a504acb220fb8445afa598c0390606f38a7bf
David Heinemeier Hansson <david@loudthinking.com>
2013-01-06 12:42:51 +0100

    This is not that common of a change to warrant top-line billing and you should read more about the consequences than just toggling a boolean to make best use of it

--- railties/lib/rails/generators/rails/app/templates/config/application.rb
+++ railties/lib/rails/generators/rails/app/templates/config/application.rb
@@ -22,13 +22,8 @@ module <%= app_const_base %>
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-
-    # Use SQL instead of Active Record's schema dumper when creating the database.
-    # This is necessary if your schema can't be completely dumped by the schema dumper,
-    # like if you have constraints or database-specific column types.
-    # config.active_record.schema_format = :sql
-
 <% if options.skip_sprockets? -%>
+
     # Disable the asset pipeline.
     config.assets.enabled = false
 <% end -%>
