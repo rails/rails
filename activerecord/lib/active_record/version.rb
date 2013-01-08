709af0523337e4c87760cbe118914ed0ab28d326
Aaron Patterson <aaron.patterson@gmail.com>
2013-01-07 16:58:10 -0800

    bumping to 2.3.15 :cry::gun:

--- activerecord/lib/active_record/version.rb
+++ activerecord/lib/active_record/version.rb
@@ -2,7 +2,7 @@ module ActiveRecord
   module VERSION #:nodoc:
     MAJOR = 2
     MINOR = 3
-    TINY  = 14
+    TINY  = 15
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
--- railties/lib/rails/version.rb
+++ railties/lib/rails/version.rb
@@ -2,7 +2,7 @@ module Rails
   module VERSION #:nodoc:
     MAJOR = 2
     MINOR = 3
-    TINY  = 14
+    TINY  = 15
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
