e80546cdec56a9c3fcaf6217cba08a02d789d2bc
Aaron Patterson <aaron.patterson@gmail.com>
2013-01-08 13:50:22 -0800

    remove yaml as a param parser :burn:
    If you revert this commit, I will hunt you down, I will make you regret
    ever terrible thing you've ever done, I will make you suffer.

--- actionpack/lib/action_dispatch/middleware/params_parser.rb
+++ actionpack/lib/action_dispatch/middleware/params_parser.rb
@@ -49,8 +49,6 @@ module ActionDispatch
         when :xml_simple, :xml_node
           data = request.deep_munge(Hash.from_xml(request.body.read) || {})
           data.with_indifferent_access
-        when :yaml
-          YAML.load(request.raw_post)
         when :json
           data = request.deep_munge ActiveSupport::JSON.decode(request.body)
           data = {:_json => data} unless data.is_a?(Hash)
