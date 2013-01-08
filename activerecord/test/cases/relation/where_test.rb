507d23c421b189ac8386e0605c57ab3db831001e
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2013-01-08 20:00:26 -0200

    Fix syntax error and remove duplicated test

--- activerecord/test/cases/relation/where_test.rb
+++ activerecord/test/cases/relation/where_test.rb
@@ -82,6 +82,10 @@ module ActiveRecord
       assert_equal 0, Post.where(:posts => {}).count
     end
 
+    def test_where_with_table_name_and_empty_array
+      assert_equal 0, Post.where(:id => []).count
+    end
+
     def test_where_with_empty_hash_and_no_foreign_key
       assert_equal 0, Edge.where(:sink => {}).count
     end
@@ -90,12 +94,6 @@ module ActiveRecord
       [[], {}, nil, ""].each do |blank|
         assert_equal 4, Edge.where(blank).order("sink_id").to_a.size
       end
-    def test_where_with_table_name_and_empty_array
-      assert_equal 0, Post.where(:id => []).count
-    end
-
-    def test_where_with_empty_hash_and_no_foreign_key
-      assert_equal 0, Edge.where(:sink => {}).count
     end
   end
 end
