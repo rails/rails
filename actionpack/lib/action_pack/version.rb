7909e7f10c82be05d989340ad49e9d9c08fb0ead
Aaron Patterson <aaron.patterson@gmail.com>
2013-01-07 16:22:08 -0800

    bumping version

--- actionpack/lib/action_pack/version.rb
+++ actionpack/lib/action_pack/version.rb
@@ -2,7 +2,7 @@ module ActionPack
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 0
-    TINY  = 18
+    TINY  = 19
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
--- activerecord/lib/active_record/version.rb
+++ activerecord/lib/active_record/version.rb
@@ -2,7 +2,7 @@ module ActiveRecord
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 0
-    TINY  = 18
+    TINY  = 19
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
