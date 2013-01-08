7763f39a7f7e705bbd6b0a46f3dede48f3dae185
Ernie Miller <ernie@erniemiller.org>
2013-01-08 18:41:59 -0500

    Fix for CVE-2013-0155

--- activerecord/lib/active_record/base.rb
+++ activerecord/lib/active_record/base.rb
@@ -2340,6 +2340,8 @@ module ActiveRecord #:nodoc:
         def sanitize_sql_hash_for_conditions(attrs, default_table_name = quoted_table_name, top_level = true)
           attrs = expand_hash_conditions_for_aggregates(attrs)
 
+          return '1 = 2' if !top_level && attrs.is_a?(Hash) && attrs.empty?
+
           conditions = attrs.map do |attr, value|
             table_name = default_table_name
 
