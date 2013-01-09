2c613f9d370d5de9eef22d57ba0e6a796579df3f
Akira Matsuda <ronnie@dio.jp>
2013-01-09 15:05:10 +0900

    Reset table_name_{prefix,suffix}, and table_name after each test
    because some tests were not resetting them, and thus the tests were order dependent

--- activerecord/test/cases/migration_test.rb
+++ activerecord/test/cases/migration_test.rb
@@ -33,6 +33,9 @@ class MigrationTest < ActiveRecord::TestCase
   end
 
   def teardown
+    ActiveRecord::Base.table_name_prefix = ""
+    ActiveRecord::Base.table_name_suffix = ""
+
     ActiveRecord::Base.connection.initialize_schema_migrations_table
     ActiveRecord::Base.connection.execute "DELETE FROM #{ActiveRecord::Migrator.schema_migrations_table_name}"
 
@@ -44,6 +47,7 @@ class MigrationTest < ActiveRecord::TestCase
     %w(reminders people_reminders prefix_reminders_suffix).each do |table|
       Reminder.connection.drop_table(table) rescue nil
     end
+    Reminder.reset_table_name
     Reminder.reset_column_information
 
     %w(last_name key bio age height wealth birthday favorite_day
@@ -257,9 +261,6 @@ class MigrationTest < ActiveRecord::TestCase
     ActiveRecord::Base.table_name_suffix = ""
     Reminder.reset_table_name
     assert_equal "schema_migrations", ActiveRecord::Migrator.schema_migrations_table_name
-  ensure
-    ActiveRecord::Base.table_name_prefix = ""
-    ActiveRecord::Base.table_name_suffix = ""
   end
 
   def test_proper_table_name
@@ -286,9 +287,6 @@ class MigrationTest < ActiveRecord::TestCase
     Reminder.reset_table_name
     assert_equal "prefix_table_suffix", ActiveRecord::Migrator.proper_table_name('table')
     assert_equal "prefix_table_suffix", ActiveRecord::Migrator.proper_table_name(:table)
-    ActiveRecord::Base.table_name_prefix = ""
-    ActiveRecord::Base.table_name_suffix = ""
-    Reminder.reset_table_name
   end
 
   def test_rename_table_with_prefix_and_suffix
@@ -307,8 +305,6 @@ class MigrationTest < ActiveRecord::TestCase
 
     assert_equal "hello world", Thing.first.content
   ensure
-    ActiveRecord::Base.table_name_prefix = ''
-    ActiveRecord::Base.table_name_suffix = ''
     Thing.reset_table_name
     Thing.reset_sequence_name
   end
@@ -326,9 +322,6 @@ class MigrationTest < ActiveRecord::TestCase
     WeNeedReminders.down
     assert_raise(ActiveRecord::StatementInvalid) { Reminder.first }
   ensure
-    ActiveRecord::Base.table_name_prefix = ''
-    ActiveRecord::Base.table_name_suffix = ''
-    Reminder.reset_table_name
     Reminder.reset_sequence_name
   end
 
