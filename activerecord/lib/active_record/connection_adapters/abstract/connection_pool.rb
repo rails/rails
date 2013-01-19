bc43763247e25058ca1ab50637a649f0b5b186eb
David <DevilDavidWang@gmail.com>
2013-01-20 07:17:50 +0800

    fix anonymous class issue

--- activerecord/lib/active_record/connection_adapters/abstract/connection_pool.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/connection_pool.rb
@@ -517,6 +517,7 @@ module ActiveRecord
 
       def establish_connection(owner, spec)
         @class_to_pool.clear
+        raise RuntimeError, "Anonymous class is not allowed." unless owner.name
         owner_to_pool[owner.name] = ConnectionAdapters::ConnectionPool.new(spec)
       end
 
