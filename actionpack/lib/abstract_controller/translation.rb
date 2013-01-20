37d15d4e1b22b4566b0c409c00680b4d961a194c
Jens Bissinger <whiterabbit.init@gmail.com>
2013-01-20 19:15:15 +0100

    Add documentation for abstract controller #translate and #localize method.

--- actionpack/lib/abstract_controller/translation.rb
+++ actionpack/lib/abstract_controller/translation.rb
@@ -1,5 +1,14 @@
 module AbstractController
   module Translation
+    # Delegates to <tt>I18n#translate</tt> but also performs one additional function.
+    #
+    # It'll scope the key by the current action if the key starts
+    # with a period. So if you call <tt>translate(".foo")</tt> from the
+    # <tt>PeopleController#index</tt> action, you'll actually be calling
+    # <tt>I18n.translate("people.index.foo")</tt>. This makes it less repetitive
+    # to translate many keys within the same controller / action and gives you a simple framework
+    # for scoping them consistently. If you don't prepend the key with a period,
+    # nothing is converted.
     def translate(*args)
       key = args.first
       if key.is_a?(String) && (key[0] == '.')
@@ -11,6 +20,7 @@ module AbstractController
     end
     alias :t :translate
 
+    # Delegates to <tt>I18n.localize</tt> with no additional functionality.
     def localize(*args)
       I18n.localize(*args)
     end
