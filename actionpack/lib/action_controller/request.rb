61eed87ce32caf534bf1f52dd8134097b4ad9e1b
Aaron Patterson <aaron.patterson@gmail.com>
2013-01-24 14:42:40 -0800

    backporting deep_munge

--- actionpack/lib/action_controller/request.rb
+++ actionpack/lib/action_controller/request.rb
@@ -491,5 +491,26 @@ EOM
           value
         end
       end
+    protected
+
+    # Remove nils from the params hash
+    def deep_munge(hash)
+      hash.each_value do |v|
+        case v
+        when Array
+          v.grep(Hash) { |x| deep_munge(x) }
+        when Hash
+          deep_munge(v)
+        end
+      end
+
+      keys = hash.keys.find_all { |k| hash[k] == [nil] }
+      keys.each { |k| hash[k] = nil }
+      hash
+    end
+
+    def parse_query(qs)
+      deep_munge(super)
+    end
   end
 end
