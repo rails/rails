0400139038585888d227eac6a3391e87c5e8cf0d
Akira Matsuda <ronnie@dio.jp>
2013-01-24 21:44:02 +0900

    Revert "Unused methods, module, etc."
    This reverts commit 4e05bfb8e254c3360a3ca4a6cb332995314338fe.
    
    Reason: BlankTopic#blank? should not be removed to check that dynamic finder with a bang can find a model that responds to `blank?`

--- activerecord/test/models/company_in_module.rb
+++ activerecord/test/models/company_in_module.rb
@@ -71,6 +71,14 @@ module MyApplication
         i.belongs_to :nested_qualified_billing_firm, :class_name => 'MyApplication::Billing::Nested::Firm'
         i.belongs_to :nested_unqualified_billing_firm, :class_name => 'Nested::Firm'
       end
+
+      validate :check_empty_credit_limit
+
+      protected
+
+      def check_empty_credit_limit
+        errors.add_on_empty "credit_limit"
+      end
     end
   end
 end
