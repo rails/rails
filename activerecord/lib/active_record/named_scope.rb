e846094740d4e80a7a9b0e0af92c67c95c6cfd5e
Peter Kovacs <peter@kovax.org>
2013-01-25 08:46:55 -0500

    Don't delegate `try` to :proxy_found
    Allow Model.scoped(...).try( :find, :first ) to work as expected.

--- activerecord/lib/active_record/named_scope.rb
+++ activerecord/lib/active_record/named_scope.rb
@@ -107,7 +107,7 @@ module ActiveRecord
 
     class Scope
       attr_reader :proxy_scope, :proxy_options, :current_scoped_methods_when_defined
-      NON_DELEGATE_METHODS = %w(nil? send object_id class extend find size count sum average maximum minimum paginate first last empty? any? respond_to?).to_set
+      NON_DELEGATE_METHODS = %w(nil? send object_id class extend find size count sum average maximum minimum paginate first last empty? any? respond_to? try).to_set
       [].methods.each do |m|
         unless m =~ /^__/ || NON_DELEGATE_METHODS.include?(m.to_s)
           delegate m, :to => :proxy_found
