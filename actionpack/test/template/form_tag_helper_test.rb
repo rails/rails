709fbd3f323607f6974bfc4aeaeca791479e5fbe
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2013-01-26 22:07:47 -0200

    Fix failing test related to escaping include_blank in select_tag
    Rails 3.0.x doesn't have the :prompt option in select_tag, it was
    introduced in c5d54be746473ce5610e5e6de51f6b9d2495c935 that is only
    available from 3.1.x on.
    
    The test and related fix were introduced in
    c9795871baacbe85ed4f19c2b830006fc6faeffd for Rails 3.0.17, as a fix for
    a security vulnerability. The code is completely fine but the test was
    using the invalid :prompt option for this version, probably because it
    was cherry-picked from other branch which has the option.

--- actionpack/test/template/form_tag_helper_test.rb
+++ actionpack/test/template/form_tag_helper_test.rb
@@ -195,8 +195,8 @@ class FormTagHelperTest < ActionView::TestCase
     assert_dom_equal expected, actual
   end
 
-  def test_select_tag_escapes_prompt
-    actual = select_tag "places", "<option>Home</option><option>Work</option><option>Pub</option>".html_safe, :prompt => "<script>alert(1337)</script>"
+  def test_select_tag_escapes_include_blank
+    actual = select_tag "places", "<option>Home</option><option>Work</option><option>Pub</option>".html_safe, :include_blank => "<script>alert(1337)</script>"
     expected = %(<select id="places" name="places"><option value="">&lt;script&gt;alert(1337)&lt;/script&gt;</option><option>Home</option><option>Work</option><option>Pub</option></select>)
     assert_dom_equal expected, actual
   end
