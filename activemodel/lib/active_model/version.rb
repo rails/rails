b875be03b50029e94244945f072a6cbbff2e1c24
Aaron Patterson <aaron.patterson@gmail.com>
2013-01-28 12:47:48 -0800

    bumping to 3.0.20

--- activemodel/lib/active_model/version.rb
+++ activemodel/lib/active_model/version.rb
@@ -2,7 +2,7 @@ module ActiveModel
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 0
-    TINY  = 19
+    TINY  = 20
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
--- activerecord/lib/active_record/version.rb
+++ activerecord/lib/active_record/version.rb
@@ -2,7 +2,7 @@ module ActiveRecord
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 0
-    TINY  = 19
+    TINY  = 20
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
