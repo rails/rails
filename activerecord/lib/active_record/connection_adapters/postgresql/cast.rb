8c0bce61444c845f760e3eeb34ba63b45ac253e3
jlxw <jason@jeyel.com>
2013-02-05 16:49:33 +0800

    Fix regex to strip quotations from hstore values
    Previously regex did not strip quotation marks where hstore values were multi-line strings.

--- activerecord/lib/active_record/connection_adapters/postgresql/cast.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/cast.rb
@@ -30,8 +30,8 @@ module ActiveRecord
             nil
           elsif String === string
             Hash[string.scan(HstorePair).map { |k,v|
-              v = v.upcase == 'NULL' ? nil : v.gsub(/^"(.*)"$/,'\1').gsub(/\\(.)/, '\1')
-              k = k.gsub(/^"(.*)"$/,'\1').gsub(/\\(.)/, '\1')
+              v = v.upcase == 'NULL' ? nil : v.gsub(/\A"(.*)"\Z/m,'\1').gsub(/\\(.)/, '\1')
+              k = k.gsub(/\A"(.*)"\Z/m,'\1').gsub(/\\(.)/, '\1')
               [k,v]
             }]
           else
