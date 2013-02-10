d29235d2a4df94b6db7d3e45ab15a6799363384e
Aaron Patterson <aaron.patterson@gmail.com>
2013-02-09 16:54:48 -0800

    fixing call to columns hash. run the damn tests when you backport!

--- activerecord/lib/active_record/relation/predicate_builder.rb
+++ activerecord/lib/active_record/relation/predicate_builder.rb
@@ -53,7 +53,7 @@ module ActiveRecord
             attribute.eq(value.name)
           when Integer, ActiveSupport::Duration
             # Arel treats integers as literals, but they should be quoted when compared with strings
-            column = engine.connection.schema_cache.columns_hash(table.name)[attribute.name.to_s]
+            column = engine.connection.schema_cache.columns_hash[table.name][attribute.name.to_s]
             attribute.eq(Arel::Nodes::SqlLiteral.new(engine.connection.quote(value, column)))
           else
             attribute.eq(value)
