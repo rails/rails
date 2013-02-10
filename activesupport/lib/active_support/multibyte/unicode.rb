65bf0945ee84c9e16865f1d49fa678e930fc33f6
Norman Clarke <norman@njclarke.com>
2013-02-10 12:05:28 -0300

    Update to latest Unicode data.
    Release notes at: http://www.unicode.org/versions/Unicode6.2.0/

--- activesupport/lib/active_support/multibyte/unicode.rb
+++ activesupport/lib/active_support/multibyte/unicode.rb
@@ -11,7 +11,7 @@ module ActiveSupport
       NORMALIZATION_FORMS = [:c, :kc, :d, :kd]
 
       # The Unicode version that is supported by the implementation
-      UNICODE_VERSION = '6.1.0'
+      UNICODE_VERSION = '6.2.0'
 
       # The default normalization used for operations that require
       # normalization. It can be set to any of the normalizations
