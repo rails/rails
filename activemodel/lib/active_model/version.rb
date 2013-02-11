1dccd44a5c74f20b0406ecc8d39373226f73af35
Aaron Patterson <aaron.patterson@gmail.com>
2013-02-10 19:05:41 -0800

    bumping version

--- activemodel/lib/active_model/version.rb
+++ activemodel/lib/active_model/version.rb
@@ -2,7 +2,7 @@ module ActiveModel
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 2
-    TINY  = 11
+    TINY  = 12
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
