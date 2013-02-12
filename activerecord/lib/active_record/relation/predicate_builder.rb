db14d6875bb95aa4efb021f031e954bebe618279
Dylan Smith <Dylan.Smith@shopify.com>
2013-02-12 06:32:09 -0500

    active_record: Fix regression in predicate builder for join aliases.
    The predicate builder was causing an exception to be raised when trying to
    get the column for quoting integers when compared to an attribute on an
    aliased table.
    
    This commit takes the same approach as Arel, checking if the table exists,
    and otherwise passing a nil column to quote.

--- activerecord/lib/active_record/relation/predicate_builder.rb
+++ activerecord/lib/active_record/relation/predicate_builder.rb
@@ -101,7 +101,8 @@ module ActiveRecord
         when Integer, ActiveSupport::Duration
           # Arel treats integers as literals, but they should be quoted when compared with strings
           table = attribute.relation
-          column = table.engine.connection.schema_cache.columns_hash(table.name)[attribute.name.to_s]
+          schema_cache = table.engine.connection.schema_cache
+          column = schema_cache.table_exists?(table.name) ? schema_cache.columns_hash(table.name)[attribute.name.to_s] : nil
           attribute.eq(Arel::Nodes::SqlLiteral.new(table.engine.connection.quote(value, column)))
         else
           attribute.eq(value)
