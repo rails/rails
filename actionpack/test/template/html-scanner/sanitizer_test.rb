8be6913990c30f63618173da722148892348dcc9
Charlie Somerville <charlie@charliesomerville.com>
2013-02-13 09:09:53 +1100

    fix incorrect ^$ usage leading to XSS in sanitize_css [CVE-2013-1855]

--- actionpack/test/template/html-scanner/sanitizer_test.rb
+++ actionpack/test/template/html-scanner/sanitizer_test.rb
@@ -279,6 +279,11 @@ class SanitizerTest < ActionController::TestCase
     assert_equal '', sanitize_css(raw)
   end
 
+  def test_should_sanitize_across_newlines
+    raw = %(\nwidth:\nexpression(alert('XSS'));\n)
+    assert_equal '', sanitize_css(raw)
+  end
+
   def test_should_sanitize_img_vbscript
     assert_sanitized %(<img src='vbscript:msgbox("XSS")' />), '<img />'
   end
