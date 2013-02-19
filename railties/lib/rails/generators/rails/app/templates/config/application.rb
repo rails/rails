212ef5278b673aee304519d886d5db86a87b1437
Andrew White <andyw@pixeltrix.co.uk>
2013-02-19 07:48:06 +0000

    Remove config.autoload_paths from config/application.rb
    Most application developers shouldn't be adding paths to
    `config.autoload_paths` as it won't be eagerly loaded so
    remove it from the generated config/application.rb file.
    
    If a developer does want to autoload from paths outside of the app
    directory then they should add the path to `config.eager_load_paths`
    as these paths are also autoloaded as well.

--- railties/lib/rails/generators/rails/app/templates/config/application.rb
+++ railties/lib/rails/generators/rails/app/templates/config/application.rb
@@ -20,9 +20,6 @@ module <%= app_const_base %>
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
