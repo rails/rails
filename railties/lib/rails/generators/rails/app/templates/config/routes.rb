33d92bf69ec5e6f385612b69a6295f5a80332ef8
Kohei Hasegawa <ameutau@gmail.com>
2013-02-26 03:25:06 +0900

    Change default root route helper

--- railties/lib/rails/generators/rails/app/templates/config/routes.rb
+++ railties/lib/rails/generators/rails/app/templates/config/routes.rb
@@ -3,7 +3,7 @@
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
