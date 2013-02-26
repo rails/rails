3f9baeb2ec08138a0da09870ae60fd6b8165c07f
David Czarnecki <dczarnecki@agoragames.com>
2013-02-26 10:49:47 -0500

    Use less hyperbole in the language re: allowed parameters

--- railties/lib/rails/generators/rails/scaffold_controller/templates/controller.rb
+++ railties/lib/rails/generators/rails/scaffold_controller/templates/controller.rb
@@ -56,7 +56,7 @@ class <%= controller_class_name %>Controller < ApplicationController
       @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Only allow a trusted parameter "white list" through.
     def <%= "#{singular_table_name}_params" %>
       <%- if attributes_names.empty? -%>
       params[<%= ":#{singular_table_name}" %>]
