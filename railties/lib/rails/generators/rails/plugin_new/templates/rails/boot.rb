670a2673946e2f933ff1c16699637958ef84c831
José Valim <jose.valim@plataformatec.com.br>
2013-02-25 20:42:36 -0700

    Apparently we can't get away from adding lib to the load path

--- railties/lib/rails/generators/rails/plugin_new/templates/rails/boot.rb
+++ railties/lib/rails/generators/rails/plugin_new/templates/rails/boot.rb
@@ -2,3 +2,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+$LOAD_PATH.unshift File.expand_path('../../../../lib', __FILE__)
