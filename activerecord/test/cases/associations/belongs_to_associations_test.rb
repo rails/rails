d18dec57640dafec2c7f5c3b9574c74be4b32316
Anupam Choudhury <contact2anupam@gmail.com>
2013-03-07 16:14:18 +0530

    Removed unnecessary and irrelevant test cases

--- activerecord/test/cases/associations/belongs_to_associations_test.rb
+++ activerecord/test/cases/associations/belongs_to_associations_test.rb
@@ -21,9 +21,8 @@ class BelongsToAssociationsTest < ActiveRecord::TestCase
            :posts, :tags, :taggings, :comments, :sponsors, :members
 
   def test_belongs_to
-    Client.find(3).firm.name
+    assert_not_nil Client.find(3).firm
     assert_equal companies(:first_firm).name, Client.find(3).firm.name
-    assert_not_nil Client.find(3).firm, "Microsoft should have a firm"
   end
 
   def test_belongs_to_with_primary_key
