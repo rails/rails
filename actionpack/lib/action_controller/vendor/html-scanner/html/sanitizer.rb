77403a9b04073713e2b758c22174a81bb3cd62b9
Aaron Patterson <aaron.patterson@gmail.com>
2013-03-15 15:04:00 -0700

    fix protocol checking in sanitization [CVE-2013-1857]
    Conflicts:
    	actionpack/lib/action_controller/vendor/html-scanner/html/sanitizer.rb

--- actionpack/lib/action_controller/vendor/html-scanner/html/sanitizer.rb
+++ actionpack/lib/action_controller/vendor/html-scanner/html/sanitizer.rb
@@ -65,7 +65,7 @@ module HTML
 
     # A regular expression of the valid characters used to separate protocols like
     # the ':' in 'http://foo.com'
-    self.protocol_separator     = /:|(&#0*58)|(&#x70)|(%|&#37;)3A/
+    self.protocol_separator     = /:|(&#0*58)|(&#x70)|(&#x0*3a)|(%|&#37;)3A/i
 
     # Specifies a Set of HTML attributes that can have URIs.
     self.uri_attributes         = Set.new(%w(href src cite action longdesc xlink:href lowsrc))
@@ -170,7 +170,7 @@ module HTML
 
     def contains_bad_protocols?(attr_name, value)
       uri_attributes.include?(attr_name) &&
-      (value =~ /(^[^\/:]*):|(&#0*58)|(&#x70)|(%|&#37;)3A/ && !allowed_protocols.include?(value.split(protocol_separator).first))
+      (value =~ /(^[^\/:]*):|(&#0*58)|(&#x70)|(&#x0*3a)|(%|&#37;)3A/i && !allowed_protocols.include?(value.split(protocol_separator).first.downcase.strip))
     end
   end
 end
