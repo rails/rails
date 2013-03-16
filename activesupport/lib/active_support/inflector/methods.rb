feaa6e2048fe86bcf07e967d6e47b865e42e055b
Steve Klabnik <steve@steveklabnik.com>
2013-03-16 15:24:36 -0700

    Revert "Merge pull request #8156 from fredwu/acronym_fix-master"
    This reverts commit 867dc1700f32aae6f98c4651bd501597e6b52bc0, reversing
    changes made to 9a421aaa8285cf2a7ecb1af370748b0337818930.
    
    This breaks anyone who's using ForceSSL: https://travis-ci.org/rails-api/rails-api/jobs/5556065
    
    Please see comments on #8156 for some discussion.

--- activesupport/lib/active_support/inflector/methods.rb
+++ activesupport/lib/active_support/inflector/methods.rb
@@ -91,7 +91,7 @@ module ActiveSupport
       word = camel_cased_word.to_s.dup
       word.gsub!('::', '/')
       word.gsub!(/(?:([A-Za-z\d])|^)(#{inflections.acronym_regex})(?=\b|[^a-z])/) { "#{$1}#{$1 && '_'}#{$2.downcase}" }
-      word.gsub!(/(?!#{inflections.acronym_regex})\b([A-Z\d]+)([A-Z][a-z])/,'\1_\2')
+      word.gsub!(/([A-Z\d]+)([A-Z][a-z])/,'\1_\2')
       word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
       word.tr!("-", "_")
       word.downcase!
