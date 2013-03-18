3773c2fc1f63daf3a6f92ef49720e801988ac990
Aaron Patterson <aaron.patterson@gmail.com>
2013-03-18 10:01:40 -0700

    bumping to 2.3.18

--- actionpack/lib/action_pack/version.rb
+++ actionpack/lib/action_pack/version.rb
@@ -2,7 +2,7 @@ module ActionPack #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 2
     MINOR = 3
-    TINY  = 17
+    TINY  = 18
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
--- activesupport/lib/active_support/version.rb
+++ activesupport/lib/active_support/version.rb
@@ -2,7 +2,7 @@ module ActiveSupport
   module VERSION #:nodoc:
     MAJOR = 2
     MINOR = 3
-    TINY  = 17
+    TINY  = 18
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
