d0f957df427a7692f84b9da64157b73672d6b7a7
Quinn Shanahan <q.shanahan@gmail.com>
2013-03-22 17:25:40 -0500

    respond to PUTs with resource rather than no content
    When using default behavior with respond_with(resource), if the
    resource has been updated, return the resource and 200 ok rather
    than no content header.

--- actionpack/test/controller/mime_responds_test.rb
+++ actionpack/test/controller/mime_responds_test.rb
@@ -867,21 +867,21 @@ class RespondWithControllerTest < ActionController::TestCase
     end
   end
 
-  def test_using_resource_for_put_with_xml_yields_no_content_on_success
+  def test_using_resource_for_put_with_xml_yields_ok_on_success
     @request.accept = "application/xml"
     put :using_resource
     assert_equal "application/xml", @response.content_type
-    assert_equal 204, @response.status
-    assert_equal "", @response.body
+    assert_equal 200, @response.status
+    assert_equal "<name>david</name>", @response.body
   end
 
-  def test_using_resource_for_put_with_json_yields_no_content_on_success
+  def test_using_resource_for_put_with_json_yields_ok_on_success
     Customer.any_instance.stubs(:to_json).returns('{"name": "David"}')
     @request.accept = "application/json"
     put :using_resource
     assert_equal "application/json", @response.content_type
-    assert_equal 204, @response.status
-    assert_equal "", @response.body
+    assert_equal 200, @response.status
+    assert_equal '{"name": "David"}', @response.body
   end
 
   def test_using_resource_for_put_with_xml_yields_unprocessable_entity_on_failure
