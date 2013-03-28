18a926b11ba9cc248a7eaa015fe731737b7ac483
Jeremy McAnally <jmcanally@github.com>
2013-03-28 16:00:49 -0400

    Don't ever undefine object_id

--- activesupport/lib/active_support/vendor/builder-2.1.2/blankslate.rb
+++ activesupport/lib/active_support/vendor/builder-2.1.2/blankslate.rb
@@ -21,7 +21,7 @@ class BlankSlate
     # hide +instance_eval+ or any method beginning with "__".
     def hide(name)
       if (instance_methods.include?(name) || instance_methods.include?(name.to_sym)) and
-        name !~ /^(__|instance_eval)/
+        name !~ /^(__|instance_eval|object_id)/
         @hidden_methods ||= {}
         @hidden_methods[name] = instance_method(name)
         undef_method name
