6716fe95d643b33cd55c53e0013a08c31188e2f8
Noemj <olli.rissanen@helsinki.fi>
2013-04-11 19:40:48 +0300

    Duped the attributes in find_or methods

--- activerecord/lib/active_record/relation.rb
+++ activerecord/lib/active_record/relation.rb
@@ -165,17 +165,20 @@ module ActiveRecord
     #   end
     #   # => <User id: 2, first_name: 'Scarlett', last_name: 'Johansson'>
     def find_or_create_by(attributes, &block)
-      find_by(attributes) || create(attributes, &block)
+      temp_attrs = attributes.dup
+      find_by(attributes) || create(temp_attrs , &block)
     end
 
     # Like <tt>find_or_create_by</tt>, but calls <tt>create!</tt> so an exception is raised if the created record is invalid.
     def find_or_create_by!(attributes, &block)
-      find_by(attributes) || create!(attributes, &block)
+      temp_attrs = attributes.dup
+      find_by(attributes) || create!(temp_attrs , &block)
     end
 
     # Like <tt>find_or_create_by</tt>, but calls <tt>new</tt> instead of <tt>create</tt>.
     def find_or_initialize_by(attributes, &block)
-      find_by(attributes) || new(attributes, &block)
+      temp_attrs = attributes.dup
+      find_by(attributes) || new(temp_attrs , &block)
     end
 
     # Runs EXPLAIN on the query or queries triggered by this relation and
