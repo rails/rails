3d625c365e9f99226e9907c8af79a7eb39c7a89c
Anupam Choudhury <contact2anupam@gmail.com>
2013-04-13 16:21:30 +0530

    Removed unused setup and teardown

--- actionmailer/test/asset_host_test.rb
+++ actionmailer/test/asset_host_test.rb
@@ -12,15 +12,9 @@ end
 class AssetHostTest < ActiveSupport::TestCase
   def setup
     set_delivery_method :test
-    ActionMailer::Base.perform_deliveries = true
-    ActionMailer::Base.deliveries.clear
     AssetHostMailer.configure do |c|
       c.asset_host = "http://www.example.com"
     end
-  end
-
-  def teardown
-    restore_delivery_method
   end
 
   def test_asset_host_as_string
