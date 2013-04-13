a548c4591b014e12b54b9b336a33164df93b0625
Xavier Noria <fxn@hashref.com>
2013-04-13 08:26:43 +0200

    simplifies the RDoc of AR::RuntimeRegistry
    The previous version was kind of duplicating the documentation of
    AS::PerThreadRegistry. Just say how to use it, the thread locals
    registry is know part of our vocabulary (though a pointer to
    Active Support is added for reference).

--- activerecord/lib/active_record/runtime_registry.rb
+++ activerecord/lib/active_record/runtime_registry.rb
@@ -1,29 +1,14 @@
 require 'active_support/per_thread_registry'
 
 module ActiveRecord
-  # This is a registry class for storing local variables in Active Record. The
-  # class allows you to access variables that are local to the current thread.
-  # These thread local variables are stored as attributes in the
-  # +RuntimeRegistry+ class.
-  #
-  # You can access the thread local variables by calling a variable's name on
-  # the +RuntimeRegistry+ class. For example, if you wanted to obtain the
-  # connection handler for the current thread, you would invoke:
-  #
-  #   ActiveRecord::RuntimeRegistry.instance.connection_handler
-  #
-  # The +PerThreadRegistry+ module will make a new +RuntimeRegistry+ instance
-  # and store it in +Thread.current+. Whenever you make a call for an attribute
-  # on the +RuntimeRegistry+ class, the call will be sent to the instance that
-  # is stored in +Thread.current+.
-  #
-  # Note that you can also make the following call which would provide an
-  # equivalent result as the previous code:
+  # This is a thread locals registry for Active Record. For example
   #
   #   ActiveRecord::RuntimeRegistry.connection_handler
   #
-  # However, this is less performant because it makes a call to +method_missing+
-  # before it sends the method call to the +instance+.
+  # returns the connection handler local to the current thread.
+  #
+  # See the documentation of <tt>ActiveSupport::PerThreadRegistry</tt>
+  # for further details.
   class RuntimeRegistry
     extend ActiveSupport::PerThreadRegistry
 
