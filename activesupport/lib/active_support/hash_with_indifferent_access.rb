c52c2682bf03192755ccf8c15ab641090815cacf
Vipul A M <vipulnsward@gmail.com>
2013-04-19 16:42:24 +0530

    symbolize_keys => symbolize_keys! on new hash;
    "There're" => There are for better readability

--- activesupport/lib/active_support/hash_with_indifferent_access.rb
+++ activesupport/lib/active_support/hash_with_indifferent_access.rb
@@ -223,7 +223,7 @@ module ActiveSupport
     def deep_stringify_keys; dup end
     undef :symbolize_keys!
     undef :deep_symbolize_keys!
-    def symbolize_keys; to_hash.symbolize_keys end
+    def symbolize_keys; to_hash.symbolize_keys! end
     def deep_symbolize_keys; to_hash.deep_symbolize_keys end
     def to_options!; self end
 
