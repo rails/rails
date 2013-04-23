a145c1b4afb880f077a1811c3aec29d40bbcad81
Alexey Vakhov <vakhov@gmail.com>
2013-04-23 10:05:09 +0400

    Cosmetic fixes in AM::Base docs. Missed spaces in OpenSSL constants enumerations.
    Injected at a1639ad.

--- actionmailer/lib/action_mailer/base.rb
+++ actionmailer/lib/action_mailer/base.rb
@@ -334,8 +334,8 @@ module ActionMailer
   #     and starts to use it.
   #   * <tt>:openssl_verify_mode</tt> - When using TLS, you can set how OpenSSL checks the certificate. This is
   #     really useful if you need to validate a self-signed and/or a wildcard certificate. You can use the name
-  #     of an OpenSSL verify constant ('none', 'peer', 'client_once','fail_if_no_peer_cert') or directly the
-  #     constant  (OpenSSL::SSL::VERIFY_NONE, OpenSSL::SSL::VERIFY_PEER,...).
+  #     of an OpenSSL verify constant ('none', 'peer', 'client_once', 'fail_if_no_peer_cert') or directly the
+  #     constant  (OpenSSL::SSL::VERIFY_NONE, OpenSSL::SSL::VERIFY_PEER, ...).
   #
   # * <tt>sendmail_settings</tt> - Allows you to override options for the <tt>:sendmail</tt> delivery method.
   #   * <tt>:location</tt> - The location of the sendmail executable. Defaults to <tt>/usr/sbin/sendmail</tt>.
