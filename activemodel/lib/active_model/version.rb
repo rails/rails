1ec64297f9347986a11ce38af2cc3434d99c66c2
David Heinemeier Hansson <david@loudthinking.com>
2013-04-29 08:29:18 -0700

    Bump version to rc1

--- activemodel/lib/active_model/version.rb
+++ activemodel/lib/active_model/version.rb
@@ -1,7 +1,7 @@
 module ActiveModel
   # Returns the version of the currently loaded ActiveModel as a Gem::Version
   def self.version
-    Gem::Version.new "4.0.0.beta1"
+    Gem::Version.new "4.0.0.rc1"
   end
 
   module VERSION #:nodoc:
--- version.rb
+++ version.rb
@@ -3,7 +3,7 @@ module Rails
     MAJOR = 4
     MINOR = 0
     TINY  = 0
-    PRE   = "beta1"
+    PRE   = "rc1"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
