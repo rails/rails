b2a02c89febb12368bd5fbff498bd94199a407b6
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-05-01 16:22:34 -0700

    Merge pull request #10404 from chadmoone/fix-pg-uuid-default
    Allow override of PostgreSQL UUID primary key default

--- activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
@@ -330,9 +330,38 @@ module ActiveRecord
       class TableDefinition < ActiveRecord::ConnectionAdapters::TableDefinition
         include ColumnMethods
 
+        # Defines the primary key field.
+        # Use of the native PostgreSQL UUID type is supported, and can be used
+        # by defining your tables as such:
+        #
+        #   create_table :stuffs, id: :uuid do |t|
+        #     t.string :content
+        #     t.timestamps
+        #   end
+        #
+        # By default, this will use the +uuid_generate_v4()+ function from the
+        # +uuid-ossp+ extension, which MUST be enabled on your databse.  To enable
+        # the +uuid-ossp+ extension, you can use the +enable_extension+ method in your
+        # migrations To use a UUID primary key without +uuid-ossp+ enabled, you can
+        # set the +:default+ option to nil:
+        #
+        #   create_table :stuffs, id: false do |t|
+        #     t.primary_key :id, :uuid, default: nil
+        #     t.uuid :foo_id
+        #     t.timestamps
+        #   end
+        #
+        # You may also pass a different UUID generation function from +uuid-ossp+
+        # or another library.
+        #
+        # Note that setting the UUID primary key default value to +nil+
+        # will require you to assure that you always provide a UUID value
+        # before saving a record (as primary keys cannot be nil).  This might be
+        # done via the SecureRandom.uuid method and a +before_save+ callback,
+        # for instance.
         def primary_key(name, type = :primary_key, options = {})
           return super unless type == :uuid
-          options[:default] ||= 'uuid_generate_v4()'
+          options[:default] = options.fetch(:default, 'uuid_generate_v4()')
           options[:primary_key] = true
           column name, type, options
         end
