950ef055852fce3c7dca7fa1c5cc3eb3f9c93265
Lars Kanis <lars@greiz-reinsdorf.de>
2013-05-02 22:28:33 +0200

    Add parameter :sslcompression to PostgreSQL adapter.
    It is new in PostgreSQL-9.2 .

--- activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
@@ -20,8 +20,8 @@ module ActiveRecord
     VALID_CONN_PARAMS = [:host, :hostaddr, :port, :dbname, :user, :password, :connect_timeout,
                          :client_encoding, :options, :application_name, :fallback_application_name,
                          :keepalives, :keepalives_idle, :keepalives_interval, :keepalives_count,
-                         :tty, :sslmode, :requiressl, :sslcert, :sslkey, :sslrootcert, :sslcrl,
-                         :requirepeer, :krbsrvname, :gsslib, :service]
+                         :tty, :sslmode, :requiressl, :sslcompression, :sslcert, :sslkey,
+                         :sslrootcert, :sslcrl, :requirepeer, :krbsrvname, :gsslib, :service]
 
     # Establishes a connection to the database that's used by all Active Record objects
     def postgresql_connection(config)
