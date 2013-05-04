e208ad925a5e3321bc0052cbd7ef0b29de136164
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-05-04 10:59:56 -0300

    Use Ruby 1.9 syntax

--- railties/lib/rails/generators/rails/scaffold/scaffold_generator.rb
+++ railties/lib/rails/generators/rails/scaffold/scaffold_generator.rb
@@ -8,8 +8,8 @@ module Rails
 
       class_option :stylesheets, type: :boolean, desc: "Generate Stylesheets"
       class_option :stylesheet_engine, desc: "Engine for Stylesheets"
-      class_option :assets, :type => :boolean
-      class_option :resource_route, :type => :boolean
+      class_option :assets, type: :boolean
+      class_option :resource_route, type: :boolean
 
       def handle_skip
         @options = @options.merge(stylesheets: false) unless options[:assets]
