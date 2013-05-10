0be27b709bd62a2dc5c8998adbfd993ec06790ac
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-05-10 14:10:10 -0300

    So not make Fixnum#/ private on Ruby verions less than 1.9.3
    In those version to_date call Fixnum#/, what will cause a failure

--- actionpack/test/template/date_helper_test.rb
+++ actionpack/test/template/date_helper_test.rb
@@ -19,7 +19,7 @@ class DateHelperTest < ActionView::TestCase
   end
 
   def assert_distance_of_time_in_words(from, to=nil)
-    Fixnum.send :private, :/  # test we avoid Integer#/ (redefined by mathn)
+    Fixnum.send(:private, :/) if RUBY_VERSION >= '1.9.3' # test we avoid Integer#/ (redefined by mathn)
 
     to ||= from
 
@@ -98,9 +98,8 @@ class DateHelperTest < ActionView::TestCase
     # test to < from
     assert_equal "about 4 hours", distance_of_time_in_words(from + 4.hours, to)
     assert_equal "less than 20 seconds", distance_of_time_in_words(from + 19.seconds, to, true)
-
-   ensure
-     Fixnum.send :public, :/
+  ensure
+    Fixnum.send(:public, :/) if RUBY_VERSION >= '1.9.3'
   end
 
   def test_distance_in_words
