dfb69f2b8a85586ad70071baba416747cbb07364
Zachary Scott <zachary@zacharyscott.net>
2013-05-15 00:48:31 -0400

    Document Rails::Generators::MailerGenerator

--- actionmailer/lib/rails/generators/mailer/mailer_generator.rb
+++ actionmailer/lib/rails/generators/mailer/mailer_generator.rb
@@ -1,11 +1,13 @@
-module Rails
-  module Generators
+module Rails # :nodoc:
+  module Generators # :nodoc:
+    # Implements the Rails::Generators for creating new mailers.
     class MailerGenerator < NamedBase
       source_root File.expand_path("../templates", __FILE__)
 
       argument :actions, type: :array, default: [], banner: "method method"
       check_class_collision
 
+      # Generates the mailer file for the application.
       def create_mailer_file
         template "mailer.rb", File.join('app/mailers', class_path, "#{file_name}.rb")
       end
