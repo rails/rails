2dbb346ffe1be19e3480564391edb34cce7c8dde
Takehiro Adachi <takehiro0740@gmail.com>
2013-05-18 18:22:21 +0900

    Fix tests which started to fail due to commit 0123c39f41e2062311b2197e6e230ef8ad67e20e
    Due to commit 0123c39f41e2062311b2197e6e230ef8ad67e20e, column
    topic.unique_replies_count has been added, and these test started to
    fail since the tests depends on the topic tables column info.

--- activerecord/test/cases/reflection_test.rb
+++ activerecord/test/cases/reflection_test.rb
@@ -35,18 +35,18 @@ class ReflectionTest < ActiveRecord::TestCase
 
   def test_read_attribute_names
     assert_equal(
-      %w( id title author_name author_email_address bonus_time written_on last_read content important group approved replies_count parent_id parent_title type created_at updated_at ).sort,
+      %w( id title author_name author_email_address bonus_time written_on last_read content important group approved replies_count unique_replies_count parent_id parent_title type created_at updated_at ).sort,
       @first.attribute_names.sort
     )
   end
 
   def test_columns
-    assert_equal 17, Topic.columns.length
+    assert_equal 18, Topic.columns.length
   end
 
   def test_columns_are_returned_in_the_order_they_were_declared
     column_names = Topic.columns.map { |column| column.name }
-    assert_equal %w(id title author_name author_email_address written_on bonus_time last_read content important approved replies_count parent_id parent_title type group created_at updated_at), column_names
+    assert_equal %w(id title author_name author_email_address written_on bonus_time last_read content important approved replies_count unique_replies_count parent_id parent_title type group created_at updated_at), column_names
   end
 
   def test_content_columns
