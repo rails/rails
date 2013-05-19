84d3f7bd097ce88bc7fb2924e6ad2afb4297d01b
Vijay Dev <vijaydev.cse@gmail.com>
2013-05-19 21:05:35 +0530

    Revert "Document Rails::Generators::MailerGenerator"
    This reverts commit dfb69f2b8a85586ad70071baba416747cbb07364.
    
    Reason: Let's not nodoc without reason.
    
    [ci skip]

--- actionmailer/lib/rails/generators/mailer/mailer_generator.rb
+++ actionmailer/lib/rails/generators/mailer/mailer_generator.rb
@@ -1,13 +1,11 @@
-module Rails # :nodoc:
-  module Generators # :nodoc:
-    # Implements the Rails::Generators for creating new mailers.
+module Rails
+  module Generators
     class MailerGenerator < NamedBase
       source_root File.expand_path("../templates", __FILE__)
 
       argument :actions, type: :array, default: [], banner: "method method"
       check_class_collision
 
-      # Generates the mailer file for the application.
       def create_mailer_file
         template "mailer.rb", File.join('app/mailers', class_path, "#{file_name}.rb")
       end
