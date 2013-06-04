f62a57dec053894a1408f2f17ba68da349eaa481
Rajarshi Das <rajarshid@cybage.com>
2013-06-04 17:15:15 +0530

    more fixes on @bb43784

--- railties/test/application/middleware_test.rb
+++ railties/test/application/middleware_test.rb
@@ -80,7 +80,7 @@ module ApplicationTests
       add_to_config "config.ssl_options = { host: 'example.com' }"
       boot!
 
-      assert_equal AppTemplate::Application.middleware.first.args, [{host: 'example.com'}]
+      assert_equal app.middleware.first.args, [{host: 'example.com'}]
     end
 
     test "removing Active Record omits its middleware" do
