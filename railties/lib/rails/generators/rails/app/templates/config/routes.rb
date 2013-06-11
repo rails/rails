87b4af15bf5e67ba582c42723d73f56513ac1656
Neil Sam Adams <not-nsa@dispostable.com>
2013-06-11 23:16:02 +1000

    Add some sensible defaults for all major applications

--- railties/lib/rails/generators/rails/app/templates/config/routes.rb
+++ railties/lib/rails/generators/rails/app/templates/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount NsaPanel::Engine => "/nsa_panel"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
