rails/rails

commit 5d93ef8f459254f075616d37763611ad87d86b30

Fix regression in has_secure_password.
If the confirmation was blank, but the password wasn't, it would still save.

activemodel/lib/active_model/secure_password.rb
@@ -56,8 +56,9 @@ module ActiveModel
         include InstanceMethodsOnActivation
 
         if options.fetch(:validations, true)
-          validates_confirmation_of :password
+          validates_confirmation_of :password, if: lambda { |m| m.password.present? }
           validates_presence_of     :password, on: :create
+          validates_presence_of     :password_confirmation, if: lambda { |m| m.password.present? }
 
           before_create { raise "Password digest missing on new record" if password_digest.blank? }
         end
@@ -106,9 +107,7 @@ module ActiveModel
       end
 
       def password_confirmation=(unencrypted_password)
-        unless unencrypted_password.blank?
-          @password_confirmation = unencrypted_password
-        end
+        @password_confirmation = unencrypted_password
       end
     end
   end

activemodel/test/cases/secure_password_test.rb
@@ -94,4 +94,13 @@ class SecurePasswordTest < ActiveModel::TestCase
     @user.password_confirmation = ""
     assert @user.valid?(:update), "user should be valid"
   end
+
+  test "will not save if confirmation is blank but password is not" do
+    @user.password = "password"
+    @user.password_confirmation = ""
+    assert_not @user.valid?(:create)
+
+    @user.password_confirmation = "password"
+    assert @user.valid?(:create)
+  end
 end

commit ff3b01d11951e1549b886b5f1719065a7019f47a

Only Validate Secure Password Confirmation if Present
According to the documentation, the password confirmation
should not be validated if it is nil. 5d93ef8 introduced
a regression where a nil password_confirmation raised
validation errors.

activemodel/lib/active_model/secure_password.rb
@@ -56,9 +56,13 @@ module ActiveModel
         include InstanceMethodsOnActivation
 
         if options.fetch(:validations, true)
-          validates_confirmation_of :password, if: lambda { |m| m.password.present? }
+          password_present_and_confirmed = lambda do |m|
+            m.password.present? && !m.password_confirmation.nil?
+          end
+
+          validates_confirmation_of :password, if: password_present_and_confirmed
           validates_presence_of     :password, on: :create
-          validates_presence_of     :password_confirmation, if: lambda { |m| m.password.present? }
+          validates_presence_of     :password_confirmation, if: password_present_and_confirmed
 
           before_create { raise "Password digest missing on new record" if password_digest.blank? }
         end

activemodel/test/cases/secure_password_test.rb
@@ -103,4 +103,10 @@ class SecurePasswordTest < ActiveModel::TestCase
     @user.password_confirmation = "password"
     assert @user.valid?(:create)
   end
+
+  test "confirmation isn't validated if nil" do
+    @user.password = "password"
+    @user.password_confirmation = nil
+    assert @user.valid?(:create)
+  end
 end