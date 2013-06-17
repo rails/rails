6fc2bcff36c428ace2f130dec43e98ba2c31c605
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-06-17 10:01:03 -0700

    Merge pull request #10971 from dtaniwaki/escape_link_to_unless
    Always escape the result of link_to_unless method

--- actionpack/lib/action_view/helpers/url_helper.rb
+++ actionpack/lib/action_view/helpers/url_helper.rb
@@ -380,7 +380,7 @@ module ActionView
           if block_given?
             block.arity <= 1 ? capture(name, &block) : capture(name, options, html_options, &block)
           else
-            name
+            ERB::Util.html_escape(name)
           end
         else
           link_to(name, options, html_options)
