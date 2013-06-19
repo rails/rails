0b502cb87906505e519b404ef2cd1d607c89a44a
Fred Wu <ifredwu@gmail.com>
2013-06-19 23:32:05 +1000

    Fixes AciveModel::Model with no ancestors, fixes #11004
    Signed-off-by: José Valim <jose.valim@plataformatec.com.br>

--- activemodel/test/cases/model_test.rb
+++ activemodel/test/cases/model_test.rb
@@ -4,6 +4,10 @@ class ModelTest < ActiveModel::TestCase
   include ActiveModel::Lint::Tests
 
   module DefaultValue
+    def self.included(klass)
+      klass.class_eval { attr_accessor :hello }
+    end
+
     def initialize(*args)
       @attr ||= 'default value'
     end
@@ -15,13 +19,17 @@ class ModelTest < ActiveModel::TestCase
     attr_accessor :attr
   end
 
+  class SimpleModel
+    include ActiveModel::Model
+  end
+
   def setup
     @model = BasicModel.new
   end
 
   def test_initialize_with_params
     object = BasicModel.new(attr: "value")
-    assert_equal object.attr, "value"
+    assert_equal "value", object.attr
   end
 
   def test_initialize_with_nil_or_empty_hash_params_does_not_explode
@@ -39,6 +47,19 @@ class ModelTest < ActiveModel::TestCase
 
   def test_mixin_inclusion_chain
     object = BasicModel.new
-    assert_equal object.attr, 'default value'
+    assert_equal 'default value', object.attr
+  end
+
+  def test_mixin_initializer_when_args_exist
+    object = BasicModel.new(hello: 'world')
+    assert_equal 'world', object.hello
+  end
+
+  def test_mixin_initializer_when_args_dont_exist
+    assert_raises(NoMethodError) { SimpleModel.new(hello: 'world') }
+  end
+
+  def test_mixin_when_no_ancestors
+    assert SimpleModel.new
   end
 end
