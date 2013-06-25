4da9228cbc9338ad06f9c8c59b42a639f7f1c66f
Guillermo Iguaran <guilleiguaran@gmail.com>
2013-06-25 09:02:12 -0500

    Use sass-rails 4.0.0 in new apps

--- railties/lib/rails/generators/app_base.rb
+++ railties/lib/rails/generators/app_base.rb
@@ -193,7 +193,7 @@ module Rails
         else
           <<-GEMFILE.strip_heredoc
             # Use SCSS for stylesheets
-            gem 'sass-rails', '~> 4.0.0.rc2'
+            gem 'sass-rails', '~> 4.0.0'
 
             # Use Uglifier as compressor for JavaScript assets
             gem 'uglifier', '>= 1.3.0'
