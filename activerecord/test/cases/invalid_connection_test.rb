cb8c95033392f70e1b6566a3c6dc6e033045f0c6
Jon Leighton <j@jonathanleighton.com>
2013-06-28 11:37:42 +0100

    Fix rake test_sqlite3_mem
    Clearly nobody uses this except me. It's fast people!

--- activerecord/test/cases/invalid_connection_test.rb
+++ activerecord/test/cases/invalid_connection_test.rb
@@ -1,20 +1,15 @@
 require "cases/helper"
-require "models/bird"
 
 class TestAdapterWithInvalidConnection < ActiveRecord::TestCase
   self.use_transactional_fixtures = false
 
-  def setup
-    @spec = ActiveRecord::Base.connection_config
-    non_existing_spec = {adapter: @spec[:adapter], database: "i_do_not_exist"}
-    ActiveRecord::Base.establish_connection(non_existing_spec)
-  end
-
-  def teardown
-    ActiveRecord::Base.establish_connection(@spec)
+  class Bird < ActiveRecord::Base
+    # Can't just use current adapter; sqlite3 will create a database
+    # file on the fly.
+    establish_connection adapter: 'mysql', database: 'i_do_not_exist'
   end
 
   test "inspect on Model class does not raise" do
-    assert_equal "Bird(no database connection)", Bird.inspect
+    assert_equal "#{Bird.name}(no database connection)", Bird.inspect
   end
 end
