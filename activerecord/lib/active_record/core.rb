9f76531c3a7a6ff32515d2238cf26dbd772467d7
Aaron Patterson <aaron.patterson@gmail.com>
2013-07-02 17:03:32 -0700

    push attribute constant cache in to the attribute methods module

--- activerecord/lib/active_record/core.rb
+++ activerecord/lib/active_record/core.rb
@@ -90,12 +90,6 @@ module ActiveRecord
     module ClassMethods
       def initialize_generated_modules
         super
-        # force attribute methods to be higher in inheritance hierarchy than other generated methods
-        generated_attribute_methods.const_set(:AttrNames, Module.new {
-          def self.const_missing(name)
-            const_set(name, [name.to_s.sub(/ATTR_/, '')].pack('h*').freeze)
-          end
-        })
 
         generated_feature_methods
       end
