965cf33650819963a41800bda716369e1f331eba
Paul Nikitochkin <paul.nikitochkin@gmail.com>
2013-07-03 22:31:04 +0300

    Removed deprecated outside_transaction?

--- activerecord/test/cases/transactions_test.rb
+++ activerecord/test/cases/transactions_test.rb
@@ -410,16 +410,6 @@ class TransactionTest < ActiveRecord::TestCase
     assert !@second.destroyed?, 'not destroyed'
   end
 
-  if current_adapter?(:PostgreSQLAdapter) && defined?(PGconn::PQTRANS_IDLE)
-    def test_outside_transaction_works
-      assert assert_deprecated { Topic.connection.outside_transaction? }
-      Topic.connection.begin_db_transaction
-      assert assert_deprecated { !Topic.connection.outside_transaction? }
-      Topic.connection.rollback_db_transaction
-      assert assert_deprecated { Topic.connection.outside_transaction? }
-    end
-  end
-
   def test_sqlite_add_column_in_transaction
     return true unless current_adapter?(:SQLite3Adapter)
 
