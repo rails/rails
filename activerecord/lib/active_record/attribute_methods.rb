7e6ea012d2a2fce872d01d2ce045a6f136a42ea4
Aaron Patterson <aaron.patterson@gmail.com>
2013-07-02 17:44:30 -0700

    freeze the constant value so it isn't duped when used as a hash key

--- activerecord/lib/active_record/attribute_methods.rb
+++ activerecord/lib/active_record/attribute_methods.rb
@@ -23,7 +23,7 @@ module ActiveRecord
       def self.set_name_cache(name, value)
         const_name = "ATTR_#{name}"
         unless const_defined? const_name
-          const_set const_name, value
+          const_set const_name, value.dup.freeze
         end
       end
     }
