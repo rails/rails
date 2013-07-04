18caf55669ffc2154632ffb96a6c667451adfdbc
Ankit Gupta <ankit.gupta8898@gmail.com>
2013-07-04 22:05:16 +0100

    Changing all strings for table names, column names as symbols & Replaced older rocket sign to new :

--- activerecord/test/migrations/to_copy_with_timestamps/20090101010101_people_have_hobbies.rb
+++ activerecord/test/migrations/to_copy_with_timestamps/20090101010101_people_have_hobbies.rb
@@ -1,9 +1,9 @@
 class PeopleHaveLastNames < ActiveRecord::Migration
   def self.up
-    add_column "people", "hobbies", :text
+    add_column :people, :hobbies, :text
   end
 
   def self.down
-    remove_column "people", "hobbies"
+    remove_column :people, :hobbies
   end
 end
