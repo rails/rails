16e4d013d8a264aed99deeba18d96a1ac541a715
Aaron Patterson <aaron.patterson@gmail.com>
2013-07-08 16:36:46 -0700

    close our connection when we are done

--- activerecord/test/cases/scoping/default_scoping_test.rb
+++ activerecord/test/cases/scoping/default_scoping_test.rb
@@ -56,7 +56,10 @@ class DefaultScopingTest < ActiveRecord::TestCase
 
   def test_default_scoping_with_threads
     2.times do
-      Thread.new { assert DeveloperOrderedBySalary.all.to_sql.include?('salary DESC') }.join
+      Thread.new {
+        assert DeveloperOrderedBySalary.all.to_sql.include?('salary DESC')
+        DeveloperOrderedBySalary.connection.close
+      }.join
     end
   end
 
@@ -360,9 +363,11 @@ class DefaultScopingTest < ActiveRecord::TestCase
     threads << Thread.new do
       Thread.current[:long_default_scope] = true
       assert_equal 1, ThreadsafeDeveloper.all.to_a.count
+      ThreadsafeDeveloper.connection.close
     end
     threads << Thread.new do
       assert_equal 1, ThreadsafeDeveloper.all.to_a.count
+      ThreadsafeDeveloper.connection.close
     end
     threads.each(&:join)
   end
