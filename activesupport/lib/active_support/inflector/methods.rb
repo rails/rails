261da32a84dafbe41aee82343db84d8868162d57
Akira Matsuda <ronnie@dio.jp>
2013-07-10 18:45:26 +0900

    Speed up AS::Inflector.underscore
    Benchmark:
              user     system      total        real
    old   6.090000   0.120000   6.210000 (  6.202039)
    new   5.930000   0.110000   6.040000 (  6.042022)

--- activesupport/lib/active_support/inflector/methods.rb
+++ activesupport/lib/active_support/inflector/methods.rb
@@ -89,8 +89,7 @@ module ActiveSupport
     #
     #   'SSLError'.underscore.camelize # => "SslError"
     def underscore(camel_cased_word)
-      word = camel_cased_word.to_s.dup
-      word.gsub!('::', '/')
+      word = camel_cased_word.to_s.gsub('::', '/')
       word.gsub!(/(?:([A-Za-z\d])|^)(#{inflections.acronym_regex})(?=\b|[^a-z])/) { "#{$1}#{$1 && '_'}#{$2.downcase}" }
       word.gsub!(/([A-Z\d]+)([A-Z][a-z])/,'\1_\2')
       word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
