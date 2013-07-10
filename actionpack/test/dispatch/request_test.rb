5153f19934df2156e8e297d43f4192273425ee68
Yvan BARTHÉLEMY <yvan@amizz.com>
2013-07-10 17:13:28 +0200

    Make CORS requests considered as Ajax requests
    jQuery does not send the usual HTTP_X_REQUESTED_BY header that is
    used by Rails to detect Ajax Request when using CORS. To overcome
    this, a new cors? method was added to request, and this is used in
    xhr? to determine wether the request is Ajax.
    
    This fixes a problem where HTML page was returned instead of JSON
    response because the CORS request was not considered as Ajax.

--- actionpack/test/dispatch/request_test.rb
+++ actionpack/test/dispatch/request_test.rb
@@ -382,6 +382,22 @@ class RequestTest < ActiveSupport::TestCase
     assert request.xhr?
   end
 
+  test "cross-origin resourse sharing request" do
+    request = stub_request
+    assert !request.cors?
+
+    request = stub_request 'HTTP_HOST' => 'test.dev', 'HTTP_PORT' => '80', 'rack.url_scheme' => 'http', 'HTTP_ORIGIN' => 'http://test.dev'
+    assert !request.cors?
+
+    request = stub_request 'HTTP_HOST' => 'test.dev', 'HTTP_PORT' => '80', 'rack.url_scheme' => 'http', 'HTTP_ORIGIN' => 'https://test.dev'
+    assert request.cors?
+    assert request.xhr?
+
+    request = stub_request 'HTTP_HOST' => 'test.dev', 'HTTP_PORT' => '80', 'rack.url_scheme' => 'http', 'HTTP_ORIGIN' => 'http://otherdomain.dev'
+    assert request.cors?
+    assert request.xhr?
+  end
+
   test "reports ssl" do
     request = stub_request
     assert !request.ssl?
