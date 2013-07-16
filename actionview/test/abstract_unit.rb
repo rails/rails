81e5be0692205bfee2bb87fc5f3377602c72fb70
Łukasz Strzałkowski <lukasz.strzalkowski@gmail.com>
2013-07-16 15:58:50 +0200

    Remove unused in AV fixture-controllers

--- actionview/test/abstract_unit.rb
+++ actionview/test/abstract_unit.rb
@@ -353,32 +353,3 @@ module RoutingTestHelpers
   end
 end
 
-class ResourcesController < ActionController::Base
-  def index() render :nothing => true end
-  alias_method :show, :index
-end
-
-class ThreadsController  < ResourcesController; end
-class MessagesController < ResourcesController; end
-class CommentsController < ResourcesController; end
-class ReviewsController < ResourcesController; end
-class AuthorsController < ResourcesController; end
-class LogosController < ResourcesController; end
-
-class AccountsController <  ResourcesController; end
-class AdminController   <  ResourcesController; end
-class ProductsController < ResourcesController; end
-class ImagesController < ResourcesController; end
-class PreferencesController < ResourcesController; end
-
-module Backoffice
-  class ProductsController < ResourcesController; end
-  class TagsController < ResourcesController; end
-  class ManufacturersController < ResourcesController; end
-  class ImagesController < ResourcesController; end
-
-  module Admin
-    class ProductsController < ResourcesController; end
-    class ImagesController < ResourcesController; end
-  end
-end
