d85e7e76d158b9232e71d981fee431ded6e04bed
Ankit Gupta <ankit.gupta8898@gmail.com>
2013-07-18 14:55:02 +0100

    Added some test cases in guides code

--- guides/code/getting_started/test/controllers/posts_controller_test.rb
+++ guides/code/getting_started/test/controllers/posts_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should create a post" do 
+  	@request.env['HTTP_AUTHORIZATION'] = authenticate_with
+  	post(:create, post: { title: "Love Hina", text: "This is a dummy text" })
+  	assert_not_nil Post.find_by(title: "Love Hina")
+  end
+
+  test "should render to new if error in post create" do 
+  	@request.env['HTTP_AUTHORIZATION'] = authenticate_with
+  	post(:create, post: { title: "L", text: "This is a dummy text" })
+  	assert_template 'new'
+  end
+
+  private
+  	def authenticate_with
+  	  ActionController::HttpAuthentication::Basic.encode_credentials('dhh', 'secret')
+  	end
 end
