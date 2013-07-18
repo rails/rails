bae07dcce2f6821e202e1e0b544421d6851e8ecf
Damien Mathieu <42@dmathieu.com>
2013-07-18 17:15:09 +0200

    use the appropriate RDoc code markup
    Thanks @fxn

--- railties/lib/rails/console/helpers.rb
+++ railties/lib/rails/console/helpers.rb
@@ -2,14 +2,14 @@ module Rails
   module ConsoleMethods
     # Gets the helper methods available to the controller.
     #
-    # This method assumes an `ApplicationController` exists, and it extends `ActionController::Base`
+    # This method assumes an +ApplicationController+ exists, and it extends +ActionController::Base+
     def helper
       @helper ||= ApplicationController.helpers
     end
 
     # Gets a new instance of a controller object.
     #
-    # This method assumes an `ApplicationController` exists, and it extends `ActionController::Base`
+    # This method assumes an +ApplicationController+ exists, and it extends +ActionController::Base+
     def controller
       @controller ||= ApplicationController.new
     end
