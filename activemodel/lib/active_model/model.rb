605610414a0ea74b9e238df1daf38b21344ac197
Gilad Zohari <gzohari@gmail.com>
2013-07-22 00:14:50 +0300

    Improved conditional for iterator
    {} isn't falsy

--- activemodel/lib/active_model/model.rb
+++ activemodel/lib/active_model/model.rb
@@ -78,7 +78,7 @@ module ActiveModel
     def initialize(params={})
       params.each do |attr, value|
         self.public_send("#{attr}=", value)
-      end if params
+      end if params.present?
 
       super()
     end
