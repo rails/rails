4ce11c0150bc7c56931cd24ce5a9792e6605795b
Vipul A M <vipulnsward@gmail.com>
2013-07-31 01:21:16 +0530

    Change from `map` => `map!` and `collect!` to save creation of extra array.

--- actionview/lib/action_view/helpers/text_helper.rb
+++ actionview/lib/action_view/helpers/text_helper.rb
@@ -215,7 +215,7 @@ module ActionView
       def word_wrap(text, options = {})
         line_width = options.fetch(:line_width, 80)
 
-        text.split("\n").collect do |line|
+        text.split("\n").collect! do |line|
           line.length > line_width ? line.gsub(/(.{1,#{line_width}})(\s+|$)/, "\\1\n").strip : line
         end * "\n"
       end
@@ -264,7 +264,7 @@ module ActionView
         if paragraphs.empty?
           content_tag(wrapper_tag, nil, html_options)
         else
-          paragraphs.map { |paragraph|
+          paragraphs.map! { |paragraph|
             content_tag(wrapper_tag, paragraph, html_options, options[:sanitize])
           }.join("\n\n").html_safe
         end
