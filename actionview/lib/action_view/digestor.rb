rails/rails

commit 09f6fe1cd4099837c5801fe654804ae058c211b4

Fix "Stack Level Too Deep" error when rendering recursive partials
When rendering recursive partial Action View is trying to generate the
view digest infinitly causing a stack level error.

Fixes #11340

actionview/lib/action_view/digestor.rb
@@ -7,10 +7,14 @@ module ActionView
     @@cache = ThreadSafe::Cache.new
 
     def self.digest(name, format, finder, options = {})
-      cache_key = [name, format] + Array.wrap(options[:dependencies])
-      @@cache[cache_key.join('.')] ||= begin
+      cache_key = ([name, format] + Array.wrap(options[:dependencies])).join('.')
+      @@cache.fetch(cache_key) do
+        @@cache[cache_key] ||= nil if options[:partial] # Prevent re-entry
+
         klass = options[:partial] || name.include?("/_") ? PartialDigestor : Digestor
-        klass.new(name, format, finder, options).digest
+        digest = klass.new(name, format, finder, options).digest
+
+        @@cache[cache_key] = digest # Store the value
       end
     end
 

actionview/test/fixtures/digestor/level/_recursion.html.erb
@@ -1,0 +1,1 @@
+<%= render 'recursion' %>

actionview/test/fixtures/digestor/level/recursion.html.erb
@@ -1,0 +1,1 @@
+<%= render 'recursion' %>

actionview/test/template/digestor_test.rb
@@ -95,6 +95,31 @@ class TemplateDigestorTest < ActionView::TestCase
     end
   end
 
+  def test_recursion_in_renders
+    assert digest("level/recursion") # assert recursion is possible
+    assert_not_nil digest("level/recursion") # assert digest is stored
+  end
+
+  def test_chaning_the_top_templete_on_recursion
+    assert digest("level/recursion") # assert recursion is possible
+
+    assert_digest_difference("level/recursion") do
+      change_template("level/recursion")
+    end
+
+    assert_not_nil digest("level/recursion") # assert digest is stored
+  end
+
+  def test_chaning_the_partial_templete_on_recursion
+    assert digest("level/recursion") # assert recursion is possible
+
+    assert_digest_difference("level/recursion") do
+      change_template("level/_recursion")
+    end
+
+    assert_not_nil digest("level/recursion") # assert digest is stored
+  end
+
   def test_dont_generate_a_digest_for_missing_templates
     assert_equal '', digest("nothing/there")
   end

commit 650810fc6c693029188399caccdbee5589908945

AV::Digestor thread safety fixes.
This fixes potential thread safety issues introduced in 09f6fe1. The problem
with the original code was that the "recursion-stopping" `nil` values could
be seen by other threads.

actionview/lib/action_view/digestor.rb
@@ -1,20 +1,40 @@
 require 'thread_safe'
 require 'action_view/dependency_tracker'
+require 'monitor'
 
 module ActionView
   class Digestor
     cattr_reader(:cache)
-    @@cache = ThreadSafe::Cache.new
+    @@cache          = ThreadSafe::Cache.new
+    @@digest_monitor = Monitor.new
 
-    def self.digest(name, format, finder, options = {})
-      cache_key = ([name, format] + Array.wrap(options[:dependencies])).join('.')
-      @@cache.fetch(cache_key) do
-        @@cache[cache_key] ||= nil if options[:partial] # Prevent re-entry
+    class << self
+      def digest(name, format, finder, options = {})
+        cache_key = ([name, format] + Array.wrap(options[:dependencies])).join('.')
+        # this is a correctly done double-checked locking idiom
+        # (ThreadSafe::Cache's lookups have volatile semantics)
+        @@cache[cache_key] || @@digest_monitor.synchronize do
+          @@cache.fetch(cache_key) do # re-check under lock
+            compute_and_store_digest(cache_key, name, format, finder, options)
+          end
+        end
+      end
 
-        klass = options[:partial] || name.include?("/_") ? PartialDigestor : Digestor
-        digest = klass.new(name, format, finder, options).digest
+      private
+      def compute_and_store_digest(cache_key, name, format, finder, options) # called under @@digest_monitor lock
+        klass = if options[:partial] || name.include?("/_")
+          # Prevent re-entry or else recursive templates will blow the stack.
+          # There is no need to worry about other threads seeing the +false+ value,
+          # as they will then have to wait for this thread to let go of the @@digest_monitor lock.
+          pre_stored = @@cache.put_if_absent(cache_key, false).nil? # put_if_absent returns nil on insertion
+          PartialDigestor
+        else
+          Digestor
+        end
 
-        @@cache[cache_key] = digest # Store the value
+        @@cache[cache_key] = digest = klass.new(name, format, finder, options).digest # Store the actual digest
+      ensure
+        @@cache.delete_pair(cache_key, false) if pre_stored && !digest # something went wrong, make sure not to corrupt the @@cache
       end
     end
 