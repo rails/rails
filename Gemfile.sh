2b02d0daf793abfc406d67ab095844233a527769
Charlie Somerville <charlie@charliesomerville.com>
2013-08-06 23:50:53 -0700

    install sqlite3

--- Gemfile.sh
+++ Gemfile.sh
@@ -1,3 +1,4 @@
-gem install mocha -v=0.13.1
-gem install rake  -v=10.1.0
-gem install rdoc  -v=4.0.1
+gem install mocha   -v=0.13.1
+gem install rake    -v=10.1.0
+gem install rdoc    -v=4.0.1
+gem install sqlite3 -v=1.3.7
