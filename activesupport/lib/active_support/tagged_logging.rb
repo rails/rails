adf6e30e3ad448c1edf2878255922a3faaa77c99
Eugene Kalenkovich <rubify@softover.com>
2013-08-18 11:49:02 -0700

    Fix 1.8.7 incompatible respond_to_missing

--- activesupport/lib/active_support/tagged_logging.rb
+++ activesupport/lib/active_support/tagged_logging.rb
@@ -72,8 +72,14 @@ module ActiveSupport
       @logger.send(method, *args)
     end
 
-    def respond_to_missing?(*args)
-      @logger.respond_to? *args
+    if RUBY_VERSION < '1.9'
+      def respond_to?(*args)
+        super || @logger.respond_to?(*args)
+      end
+    else
+      def respond_to_missing?(*args)
+        @logger.respond_to? *args
+      end
     end
 
     private
