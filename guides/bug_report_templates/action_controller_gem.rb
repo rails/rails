1c96c0b620d4895e98f1a211707436b203d4802e
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-08-22 06:06:19 -0700

    Merge pull request #11967 from jetthoughts/add_action_controller_bug_report_template
    Added bug report template for ActionController

---
+++ guides/bug_report_templates/action_controller_gem.rb
@@ -0,0 +1,42 @@
+# Activate the gem you are reporting the issue against.
+gem 'rails', '4.0.0'
+
+require 'rails'
+require 'action_controller/railtie'
+
+class TestApp < Rails::Application
+  config.root = File.dirname(__FILE__)
+  config.session_store :cookie_store, key: 'cookie_store_key'
+  config.secret_token    = 'secret_token'
+  config.secret_key_base = 'secret_key_base'
+
+  config.logger = Logger.new($stdout)
+  Rails.logger  = config.logger
+
+  routes.draw do
+    get '/' => 'test#index'
+  end
+end
+
+class TestController < ActionController::Base
+  def index
+    render text: 'Home'
+  end
+end
+
+require 'minitest/autorun'
+require 'rack/test'
+
+class BugTest < MiniTest::Unit::TestCase
+  include Rack::Test::Methods
+
+  def test_returns_success
+    get '/'
+    assert last_response.ok?
+  end
+
+  private
+    def app
+      Rails.application
+    end
+end
