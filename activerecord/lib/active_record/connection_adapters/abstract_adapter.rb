e029e1058193387886fb7e8bb5e7e49e88dde5e2
kares <self@kares.org>
2013-08-28 09:13:53 +0200

    type_cast_config_to_boolean should return true for 'true'
    currently `'false'` is returned as `false`
    while `'true'` is not type-casted to `true`
    which is still truth-y but a bit confusing

--- activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
@@ -81,6 +81,8 @@ module ActiveRecord
       def self.type_cast_config_to_boolean(config)
         if config == "false"
           false
+        elsif config == "true"
+          true
         else
           config
         end
