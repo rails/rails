98c085357e87ee49aec27d270f18502f2a062bef
Aaron Patterson <aaron.patterson@gmail.com>
2013-08-28 17:53:03 -0700

    avoid extra empty array allocation

--- activerecord/lib/active_record/associations/preloader/association.rb
+++ activerecord/lib/active_record/associations/preloader/association.rb
@@ -75,24 +75,24 @@ module ActiveRecord
           owners_map = owners_by_key
           owner_keys = owners_map.keys.compact
 
-          if klass.nil? || owner_keys.empty?
-            records = []
-          else
+          # Each record may have multiple owners, and vice-versa
+          records_by_owner = Hash[owners.map { |owner| [owner, []] }]
+
+          if klass && owner_keys.any?
             # Some databases impose a limit on the number of ids in a list (in Oracle it's 1000)
             # Make several smaller queries if necessary or make one query if the adapter supports it
             sliced  = owner_keys.each_slice(klass.connection.in_clause_length || owner_keys.size)
             records = sliced.flat_map { |slice| records_for(slice) }
-          end
 
-          # Each record may have multiple owners, and vice-versa
-          records_by_owner = Hash[owners.map { |owner| [owner, []] }]
-          records.each do |record|
-            owner_key = owner_id_for records, record
+            records.each do |record|
+              owner_key = owner_id_for records, record
 
-            owners_map[owner_key].each do |owner|
-              records_by_owner[owner] << record
+              owners_map[owner_key].each do |owner|
+                records_by_owner[owner] << record
+              end
             end
           end
+
           records_by_owner
         end
 
