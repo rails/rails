1c6556f750a665c367fd9844b690912bd7d4351f
Santiago Pastorino <santiago@wyeworks.com>
2013-08-30 10:44:25 -0700

    Merge pull request #12085 from valk/master
    FIX reload! within rails console --sandbox causes undefined method rollb...

--- activerecord/lib/active_record/railties/console_sandbox.rb
+++ activerecord/lib/active_record/railties/console_sandbox.rb
@@ -1,5 +1,7 @@
 ActiveRecord::Base.connection.begin_transaction(joinable: false)
 
 at_exit do
-  ActiveRecord::Base.connection.rollback_transaction
+  if ActiveRecord::Base.connection.transaction_open?
+    ActiveRecord::Base.connection.rollback_transaction
+  end
 end
