1a01551050e93c5ae9d090992b601009d1d60e1d
Tom Caspy <tcaspy@gmail.com>
2013-08-30 16:08:15 +0300

    when destroying nested objects, making sure validates length gets properly called Fixes #3961

--- activemodel/lib/active_model/validations/length.rb
+++ activemodel/lib/active_model/validations/length.rb
@@ -39,22 +39,30 @@ module ActiveModel
 
       def validate_each(record, attribute, value)
         value = tokenize(value)
-        value_length = value.respond_to?(:length) ? value.length : value.to_s.length
+
+        value_length = value.respond_to?(:length) ? remove_marked_for_destruction(value).length : value.to_s.length
+
         errors_options = options.except(*RESERVED_OPTIONS)
 
         CHECKS.each do |key, validity_check|
           next unless check_value = options[key]
-
           if !value.nil? || skip_nil_check?(key)
             next if value_length.send(validity_check, check_value)
           end
-
           errors_options[:count] = check_value
 
           default_message = options[MESSAGES[key]]
           errors_options[:message] ||= default_message if default_message
 
           record.errors.add(attribute, MESSAGES[key], errors_options)
+        end
+      end
+
+      def remove_marked_for_destruction(value)
+        if value.all?{|i| i.respond_to?(:marked_for_destruction?)}
+          value.reject(&:marked_for_destruction?)
+        else
+          value
         end
       end
 
