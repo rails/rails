86df5aac3852a967ff9f4a80ea64fd750739e19f
Timm <kaspth@gmail.com>
2013-09-03 09:30:37 +0200

    Added deprecation warning to ActionDispatch::Assertions::TagAssertions.

---
+++ actionpack/lib/action_dispatch/testing/assertions/tag.rb
@@ -0,0 +1,3 @@
+require 'active_support/deprecation'
+
+ActiveSupport::Deprecation.warn("ActionDispatch::Assertions::TagAssertions has been eprecated. Use the assert_select methods from SelectorAssertions in action_view/testing/assertions/selector.rb.")
