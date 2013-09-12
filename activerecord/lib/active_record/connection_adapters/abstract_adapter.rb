08376d3796482fefcd03ad1d7e0bcce0df4c9913
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-09-11 21:04:11 -0300

    Fix inverted conditional
    Oops, I should had run the tests

--- activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
@@ -446,7 +446,7 @@ module ActiveRecord
       end
 
       def without_prepared_statement?(binds)
-        @prepared_statements || binds.empty?
+        !@prepared_statements || binds.empty?
       end
     end
   end
