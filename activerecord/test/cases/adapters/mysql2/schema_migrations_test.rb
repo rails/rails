866d505632c9b61ad50f6a35a4a3d39a477dd2dc
itzki <itzki.h@gmail.com>
2013-09-17 07:58:22 +0900

    Fix AR::Migrator to create schema_migrations for MySQL "utf8mb4"
    The max length is limited when the encoding is "utf8mb4" in MySQL.
    So the migrator should call the wrapped method to create schema_migrations
    same as loading schema task.

--- activerecord/test/cases/adapters/mysql2/schema_migrations_test.rb
+++ activerecord/test/cases/adapters/mysql2/schema_migrations_test.rb
@@ -4,21 +4,35 @@ module ActiveRecord
   module ConnectionAdapters
     class Mysql2Adapter
       class SchemaMigrationsTest < ActiveRecord::TestCase
+        def setup
+          super
+          @conn = ActiveRecord::Base.connection
+          @smtn = ActiveRecord::Migrator.schema_migrations_table_name
+          @config = @conn.instance_variable_get(:@config)
+        end
+
         def test_initializes_schema_migrations_for_encoding_utf8mb4
-          conn = ActiveRecord::Base.connection
+          @conn.drop_table(@smtn) if @conn.table_exists?(@smtn)
+          original_encoding = @config[:encoding]
+          @config[:encoding] = 'utf8mb4'
 
-          smtn = ActiveRecord::Migrator.schema_migrations_table_name
-          conn.drop_table(smtn) if conn.table_exists?(smtn)
+          @conn.initialize_schema_migrations_table
 
-          config = conn.instance_variable_get(:@config)
-          original_encoding = config[:encoding]
-
-          config[:encoding] = 'utf8mb4'
-          conn.initialize_schema_migrations_table
-
-          assert conn.column_exists?(smtn, :version, :string, limit: Mysql2Adapter::MAX_INDEX_LENGTH_FOR_UTF8MB4)
+          assert @conn.column_exists?(@smtn, :version, :string, limit: Mysql2Adapter::MAX_INDEX_LENGTH_FOR_UTF8MB4)
         ensure
-          config[:encoding] = original_encoding
+          @config[:encoding] = original_encoding
+        end
+
+        def test_initializes_schema_migrations_through_migrator_for_encoding_utf8mb4
+          @conn.drop_table(@smtn) if @conn.table_exists?(@smtn)
+          original_encoding = @config[:encoding]
+          @config[:encoding] = 'utf8mb4'
+
+          ActiveRecord::Migrator.new(:up, [])
+
+          assert @conn.column_exists?(@smtn, :version, :string, limit: Mysql2Adapter::MAX_INDEX_LENGTH_FOR_UTF8MB4)
+        ensure
+          @config[:encoding] = original_encoding
         end
       end
     end
