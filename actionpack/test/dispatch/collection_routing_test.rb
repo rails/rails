9b3e7df04de789cb9d1550763ddd8928b2f32890
Ujjwal Thaakar <ujjwalthaakar@gmail.com>
2013-09-22 16:26:35 +0530

    Added another resource to the collection option test

--- actionpack/test/dispatch/collection_routing_test.rb
+++ actionpack/test/dispatch/collection_routing_test.rb
@@ -3,7 +3,7 @@ require 'abstract_unit'
 class TestCollectionRouting < ActionDispatch::IntegrationTest
   test 'collection option' do
     draw do
-      resources :posts, collection: true
+      resources :posts, :blogs, collection: true
     end
 
     get '/posts'
@@ -29,6 +29,31 @@ class TestCollectionRouting < ActionDispatch::IntegrationTest
     get '/posts/1..10,15/edit'
     assert_equal 'posts#edit_many', @response.body
     assert_equal '/posts/1..10,15/edit', edit_posts_path(ids: "1..10,15")
+
+
+    get '/blogs'
+    assert_equal 'blogs#index', @response.body
+    assert_equal '/blogs', blogs_path
+
+    get '/blogs/1..10,15'
+    assert_equal 'blogs#index', @response.body
+    assert_equal '/blogs/1..10,15', blogs_path(ids: "1..10,15")
+
+    post '/blogs'
+    assert_equal 'blogs#create', @response.body
+
+    patch '/blogs/1..10'
+    assert_equal 'blogs#update_many', @response.body
+
+    put '/blogs/1..10'
+    assert_equal 'blogs#replace', @response.body
+
+    delete '/blogs/1..10'
+    assert_equal 'blogs#destroy_many', @response.body
+
+    get '/blogs/1..10,15/edit'
+    assert_equal 'blogs#edit_many', @response.body
+    assert_equal '/blogs/1..10,15/edit', edit_blogs_path(ids: "1..10,15")
   end
 
   private
