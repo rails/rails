3c0b77beadaff537df92798f08fc9c369caa9fce
T.J. Schuck <tj@getharvest.com>
2013-09-24 12:16:06 -0400

    HTML escape backtick for IE

--- actionview/test/template/erb_util_test.rb
+++ actionview/test/template/erb_util_test.rb
@@ -39,13 +39,13 @@ class ErbUtilTest < ActiveSupport::TestCase
 
   def test_rest_in_ascii
     (0..127).to_a.map {|int| int.chr }.each do |chr|
-      next if %('"&<>).include?(chr)
+      next if %('"&<>`).include?(chr)
       assert_equal chr, html_escape(chr)
     end
   end
 
   def test_html_escape_once
-    assert_equal '1 &lt;&gt;&amp;&quot;&#39; 2 &amp; 3', html_escape_once('1 <>&"\' 2 &amp; 3')
+    assert_equal '1 &lt;&gt;&amp;&quot;&#39;&#96; 2 &amp; 3', html_escape_once('1 <>&"\'` 2 &amp; 3')
   end
 
   def test_html_escape_once_returns_unsafe_strings_when_passed_unsafe_strings
