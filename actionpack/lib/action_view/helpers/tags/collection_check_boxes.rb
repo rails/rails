bea5e32955f630b9280ca49d44a3f10c4c664f8b
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-09-24 17:02:36 -0700

    Merge pull request #12344 from angelic/collection_check_boxes_with_name
    Use the given name in html_options for the hidden field in collection_ch...

--- actionpack/lib/action_view/helpers/tags/collection_check_boxes.rb
+++ actionpack/lib/action_view/helpers/tags/collection_check_boxes.rb
@@ -27,7 +27,8 @@ module ActionView
 
           # Append a hidden field to make sure something will be sent back to the
           # server if all check boxes are unchecked.
-          hidden = @template_object.hidden_field_tag("#{tag_name}[]", "", :id => nil)
+          hidden_name = @html_options[:name] || "#{tag_name}[]"
+          hidden = @template_object.hidden_field_tag(hidden_name, "", :id => nil)
 
           rendered_collection + hidden
         end
