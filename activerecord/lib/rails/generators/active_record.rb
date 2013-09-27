12720c77f5503478b308f278c067d5cb5cf840dd
Yves Senn <yves.senn@gmail.com>
2013-09-27 16:16:58 +0200

    Make `.next_migration_number` reusable for third party AR generators.

--- activerecord/lib/rails/generators/active_record.rb
+++ activerecord/lib/rails/generators/active_record.rb
@@ -1,22 +1,16 @@
 require 'rails/generators/named_base'
-require 'rails/generators/migration'
 require 'rails/generators/active_model'
+require 'rails/generators/active_record/migration'
 require 'active_record'
 
 module ActiveRecord
   module Generators # :nodoc:
     class Base < Rails::Generators::NamedBase # :nodoc:
-      include Rails::Generators::Migration
+      include ActiveRecord::Generators::Migration
 
       # Set the current directory as base for the inherited generators.
       def self.base_root
         File.dirname(__FILE__)
-      end
-
-      # Implement the required interface for Rails::Generators::Migration.
-      def self.next_migration_number(dirname)
-        next_migration_number = current_migration_number(dirname) + 1
-        ActiveRecord::Migration.next_migration_number(next_migration_number)
       end
     end
   end
