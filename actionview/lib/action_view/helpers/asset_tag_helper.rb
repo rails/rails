bf495069cbad76bebbe775fc4c0f8f88ddf29099
Joseph Zidell <jzidell@websiteswithclass.com>
2013-10-03 18:30:42 -0400

    Fixed return strings in documentation

--- actionview/lib/action_view/helpers/asset_tag_helper.rb
+++ actionview/lib/action_view/helpers/asset_tag_helper.rb
@@ -225,13 +225,13 @@ module ActionView
       # ==== Examples
       #
       #   image_alt('rails.png')
-      #   # => <img alt="Rails" src="/assets/rails.png" />
+      #   # => Rails
       #
       #   image_alt('hyphenated-file-name.png')
-      #   # => <img alt="Hyphenated file name" src="/assets/hyphenated-file-name.png" />
+      #   # => Hyphenated file name
       #
       #   image_alt('underscored_file_name.png')
-      #   # => <img alt="Underscored file name" src="/assets/underscored_file_name.png" />
+      #   # => Underscored file name
       def image_alt(src)
         File.basename(src, '.*').sub(/-[[:xdigit:]]{32}\z/, '').tr('-_', ' ').capitalize
       end
