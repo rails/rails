6061472b8c310158a2a2e8e9a6b81a1aef6b60fe
Timm <kaspth@gmail.com>
2013-10-11 12:57:36 +0200

    Changed deprecation message in dom and selector assertions in Action Dispatch.

--- actionpack/lib/action_dispatch/testing/assertions/selector.rb
+++ actionpack/lib/action_dispatch/testing/assertions/selector.rb
@@ -1,3 +1,3 @@
 require 'active_support/deprecation'
 
-ActiveSupport::Deprecation.warn("ActionDispatch::Assertions::SelectorAssertions has been moved to Action View in action_view/testing/assertions/selector.rb.")
+ActiveSupport::Deprecation.warn("ActionDispatch::Assertions::SelectorAssertions has been has been extracted to the rails-dom-testing gem.")
