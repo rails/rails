16c0023a9a14688bbc96d81e809bd26ccf651438
Marc Schütz <schuetzm@gmx.net>
2013-10-12 20:27:13 +0200

    Make logging to stdout work again with implicit `development` env

--- railties/test/env_helpers.rb
+++ railties/test/env_helpers.rb
@@ -1,7 +1,10 @@
+require 'rails'
+
 module EnvHelpers
   private
 
   def with_rails_env(env)
+    Rails.instance_variable_set :@_env, nil
     switch_env 'RAILS_ENV', env do
       switch_env 'RACK_ENV', nil do
         yield
@@ -10,6 +13,7 @@ module EnvHelpers
   end
 
   def with_rack_env(env)
+    Rails.instance_variable_set :@_env, nil
     switch_env 'RACK_ENV', env do
       switch_env 'RAILS_ENV', nil do
         yield
