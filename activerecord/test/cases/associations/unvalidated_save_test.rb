cb57ce8971227e07fbc8578f0f27562bbc5014f3
Joseph Lord <joseph@human-friendly.com>
2013-10-14 13:37:01 +0100

    Remove test that describes incorrect current behaviour

--- activerecord/test/cases/associations/unvalidated_save_test.rb
+++ activerecord/test/cases/associations/unvalidated_save_test.rb
@@ -77,21 +77,4 @@ class UnvalidatedSaveTest < ActiveRecord::TestCase
     assert_equal house_count, House.count
   end
   
-  def test_save_validate_false_after_association_created_bad_behaviour
-    # This passes but I think it shouldn't.  
-    # The intention is to illustrate the current behaviour rather than desired behaviour
-    door  = Door.new
-    houseA = door.house = House.new # This makes the test fail below!!!
-    bad_house_id = House.order(:id).last.id + 10000
-    house_count = House.count
-    door.house_id = bad_house_id
-    door.save(validate: false)
-    door.reload
-    houseA.reload
-    assert_not_equal bad_house_id, door.house_id
-    assert_equal houseA.id, door.house_id
-    assert_raises(ActiveRecord::RecordNotFound) { House.find bad_house_id }
-    assert_equal house_count + 1, House.count
-  end
-  
 end
