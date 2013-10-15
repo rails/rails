2a0c4403fd905c3d2561c8ed69ed77dbd27c6bfa
Aaron Patterson <aaron.patterson@gmail.com>
2013-10-15 11:48:53 -0700

    bumping to 3.2.15

--- actionpack/lib/action_pack/version.rb
+++ actionpack/lib/action_pack/version.rb
@@ -3,7 +3,7 @@ module ActionPack
     MAJOR = 3
     MINOR = 2
     TINY  = 15
-    PRE   = "rc3"
+    PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
--- activerecord/lib/active_record/version.rb
+++ activerecord/lib/active_record/version.rb
@@ -3,7 +3,7 @@ module ActiveRecord
     MAJOR = 3
     MINOR = 2
     TINY  = 15
-    PRE   = "rc3"
+    PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
