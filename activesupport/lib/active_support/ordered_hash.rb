35813faf54f1d3f02a40229f14df93bf024d13cf
Charlie Somerville <charlie@charliesomerville.com>
2013-10-15 15:42:33 -0400

    in fact we don't even need to bother creating a new class

--- activesupport/lib/active_support/ordered_hash.rb
+++ activesupport/lib/active_support/ordered_hash.rb
@@ -4,8 +4,4 @@ YAML.add_builtin_type("omap") do |type, val|
   ActiveSupport::OrderedHash[val.map(&:to_a).map(&:first)]
 end
 
-# OrderedHash is namespaced to prevent conflicts with other implementations
-module ActiveSupport
-  class OrderedHash < ::Hash #:nodoc:
-  end
-end
+ActiveSupport::OrderedHash = Hash
