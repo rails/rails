422b3d0dcbef3813c4d6d27d8a63961f74beb6aa
Charlie Somerville <charlie@charliesomerville.com>
2013-10-15 15:17:27 -0400

    delete custom Enumerable#group_by

--- activesupport/lib/active_support/core_ext/enumerable.rb
+++ activesupport/lib/active_support/core_ext/enumerable.rb
@@ -1,40 +1,6 @@
 require 'active_support/ordered_hash'
 
 module Enumerable
-  # Ruby 1.8.7 introduces group_by, but the result isn't ordered. Override it.
-  remove_method(:group_by) if [].respond_to?(:group_by) && RUBY_VERSION < '1.9'
-
-  # Collect an enumerable into sets, grouped by the result of a block. Useful,
-  # for example, for grouping records by date.
-  #
-  # Example:
-  #
-  #   latest_transcripts.group_by(&:day).each do |day, transcripts| 
-  #     p "#{day} -> #{transcripts.map(&:class).join(', ')}"
-  #   end
-  #   "2006-03-01 -> Transcript"
-  #   "2006-02-28 -> Transcript"
-  #   "2006-02-27 -> Transcript, Transcript"
-  #   "2006-02-26 -> Transcript, Transcript"
-  #   "2006-02-25 -> Transcript"
-  #   "2006-02-24 -> Transcript, Transcript"
-  #   "2006-02-23 -> Transcript"
-  def group_by
-    assoc = ActiveSupport::OrderedHash.new
-
-    each do |element|
-      key = yield(element)
-
-      if assoc.has_key?(key)
-        assoc[key] << element
-      else
-        assoc[key] = [element]
-      end
-    end
-
-    assoc
-  end unless [].respond_to?(:group_by)
-
   # Calculates a sum from the elements. Examples:
   #
   #  payments.sum { |p| p.price * p.tax_rate }
