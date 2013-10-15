af1dc7f08a4e63cde7bd33d1c2afc782de65383d
Brad Murray <wyaeld@gmail.com>
2013-10-15 21:08:47 +1300

    update digestor code based on review

--- actionview/lib/action_view/digestor.rb
+++ actionview/lib/action_view/digestor.rb
@@ -35,13 +35,13 @@ module ActionView
           Digestor
         end
 
-        # Store the actual digest if config.cache_template_loading is true
         digest = klass.new(name, format, finder, options).digest
+        # Store the actual digest if config.cache_template_loading is true
         @@cache[cache_key] = digest if ActionView::Resolver.caching?
         digest
       ensure
         # something went wrong or ActionView::Resolver.caching? is false, make sure not to corrupt the @@cache
-        @@cache.delete_pair(cache_key, false) if pre_stored || !digest 
+        @@cache.delete_pair(cache_key, false) if pre_stored && !digest 
       end
     end
 
