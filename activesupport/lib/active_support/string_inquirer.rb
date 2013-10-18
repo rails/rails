9bca8f5c666f5cbee3d726f6efc6c5b9041b86e2
Chris Keele <dev@chriskeele.com>
2013-10-18 11:09:03 -0500

    Add chainable indempotent 'not' method to StringInquirer.

--- activesupport/lib/active_support/string_inquirer.rb
+++ activesupport/lib/active_support/string_inquirer.rb
@@ -4,11 +4,20 @@ module ActiveSupport
   # in a StringInquirer object so instead of calling this:
   #
   #   Rails.env == 'production'
+  #   Rails.env != 'production'
   #
   # you can call this:
   #
   #   Rails.env.production?
+  #   Rails.env.not.production?
   class StringInquirer < String
+
+      attr_accessor :negated
+
+      def not
+        self.dup.tap{ |s| s.negated = !s.negated }
+      end
+
     private
 
       def respond_to_missing?(method_name, include_private = false)
@@ -17,7 +26,7 @@ module ActiveSupport
 
       def method_missing(method_name, *arguments)
         if method_name[-1] == '?'
-          self == method_name[0..-2]
+          (self == method_name[0..-2]) ^ self.negated
         else
           super
         end
