b91f81c45bce0ecde0901d1a595051b8552a93fa
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-10-23 19:38:54 -0200

    Preparing for 4.0.1.rc3 release

--- actionmailer/lib/action_mailer/version.rb
+++ actionmailer/lib/action_mailer/version.rb
@@ -1,7 +1,7 @@
 module ActionMailer
   # Returns the version of the currently loaded ActionMailer as a Gem::Version
   def self.version
-    Gem::Version.new "4.0.1.rc2"
+    Gem::Version.new "4.0.1.rc3"
   end
 
   module VERSION #:nodoc:
--- version.rb
+++ version.rb
@@ -3,7 +3,7 @@ module Rails
     MAJOR = 4
     MINOR = 0
     TINY  = 1
-    PRE   = "rc2"
+    PRE   = "rc3"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
