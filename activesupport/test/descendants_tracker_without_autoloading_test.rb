3dcb238bfc30bd0ffb33a08cc224dc2462aa644c
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-10-24 20:14:30 -0200

    Improve the test code for #8422
    * Remove unneeded comments.
    * Use key? instead of keys.include?.

--- activesupport/test/descendants_tracker_without_autoloading_test.rb
+++ activesupport/test/descendants_tracker_without_autoloading_test.rb
@@ -5,13 +5,13 @@ require 'descendants_tracker_test_cases'
 class DescendantsTrackerWithoutAutoloadingTest < ActiveSupport::TestCase
   include DescendantsTrackerTestCases
 
+  # Regression test for #8422. https://github.com/rails/rails/issues/8442
   def test_clear_without_autoloaded_singleton_parent
     mark_as_autoloaded do
       parent_instance = Parent.new
-      parent_instance.singleton_class.descendants   #adds singleton class in @@direct_descendants
-      ActiveSupport::DescendantsTracker.clear   #clear is supposed to remove singleton class keys so GC can remove them.
-      assert !ActiveSupport::DescendantsTracker.class_variable_get(:@@direct_descendants).keys.include?(parent_instance.singleton_class)
+      parent_instance.singleton_class.descendants
+      ActiveSupport::DescendantsTracker.clear
+      assert !ActiveSupport::DescendantsTracker.class_variable_get(:@@direct_descendants).key?(parent_instance.singleton_class)
     end
   end
-
 end
