669b08892bf961d87811e7ac5f731f839cebe0d7
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-10-24 20:11:50 -0200

    Merge pull request #8491 from igagnidz/master
    Issue #8442

--- activesupport/test/descendants_tracker_without_autoloading_test.rb
+++ activesupport/test/descendants_tracker_without_autoloading_test.rb
@@ -4,4 +4,14 @@ require 'descendants_tracker_test_cases'
 
 class DescendantsTrackerWithoutAutoloadingTest < ActiveSupport::TestCase
   include DescendantsTrackerTestCases
+
+  def test_clear_without_autoloaded_singleton_parent
+    mark_as_autoloaded do
+      parent_instance = Parent.new
+      parent_instance.singleton_class.descendants   #adds singleton class in @@direct_descendants
+      ActiveSupport::DescendantsTracker.clear   #clear is supposed to remove singleton class keys so GC can remove them.
+      assert !ActiveSupport::DescendantsTracker.class_variable_get(:@@direct_descendants).keys.include?(parent_instance.singleton_class)
+    end
+  end
+
 end
