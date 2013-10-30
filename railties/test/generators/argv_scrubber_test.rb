2f11668eb844f6d7d64eaf4b4856d8a8b41e3a0c
Aaron Patterson <aaron.patterson@gmail.com>
2013-10-30 15:33:46 -0700

    add a test for the rc commandline option

--- railties/test/generators/argv_scrubber_test.rb
+++ railties/test/generators/argv_scrubber_test.rb
@@ -64,6 +64,24 @@ module Rails
         file.unlink
       end
 
+      def test_new_rc_option
+        file = Tempfile.new 'myrcfile'
+        file.puts '--hello-world'
+        file.flush
+
+        message = nil
+        scrubber = Class.new(ARGVScrubber) {
+          define_method(:puts) { |msg| message = msg }
+        }.new ['new', "--rc=#{file.path}"]
+        args = scrubber.prepare!
+        assert_equal [nil, '--hello-world'], args
+        assert_match 'hello-world', message
+        assert_match file.path, message
+      ensure
+        file.close
+        file.unlink
+      end
+
       def test_no_rc
         scrubber = ARGVScrubber.new ['new', '--no-rc']
         args = scrubber.prepare!
