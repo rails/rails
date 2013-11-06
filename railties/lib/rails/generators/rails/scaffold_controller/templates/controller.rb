aa56edb7972a51e6e2f3d32beeb627727685811a
Ujjwal Thaakar <ujjwalthaakar@gmail.com>
2013-11-07 04:59:42 +0530

    Fixed whitespace issues. Now both collection and single resource controllers are properly formatted.

--- railties/lib/rails/generators/rails/scaffold_controller/templates/controller.rb
+++ railties/lib/rails/generators/rails/scaffold_controller/templates/controller.rb
@@ -5,24 +5,23 @@ require_dependency "<%= namespaced_file_path %>/application_controller"
 <% module_namespacing do -%>
 class <%= controller_class_name %>Controller < ApplicationController
   before_action :set_<%= singular_table_name %>, only: [:show, :edit, :update, :destroy]
-  <% if collection_routing? %>
+  
+  <%- if collection_routing? -%>
   # GET <%= route_url %>/1..10,17
   def index
     @<%= plural_table_name %> = <%= singular_table_name %>_ids ? <%= orm_class.find(class_name, "#{singular_table_name}_ids") %> : <%= orm_class.all(class_name) %>
   end
-  <% else %>
+
+  # <%= singular_table_name.capitalize %> ids
+  def <%= singular_table_name %>_ids
+    @<%= plural_table_name %>_ids ||= params.permit(ids: 1..2**32-1)
+  end
+  <%- else -%>
   # GET <%= route_url %>
   def index
     @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
   end
-  <% end %>
-
-  # <%= singular_table_name.capitalize %> ids
-  <% if collection_routing? %>
-  def <%= singular_table_name %>_ids
-    @<%= plural_table_name %>_ids ||= params.permit(ids: 1..2**32-1)
-  end
-  <% end %>
+  <%- end -%>
 
   # GET <%= route_url %>/1
   def show
@@ -38,7 +37,7 @@ class <%= controller_class_name %>Controller < ApplicationController
   end
 
   # POST <%= route_url %>
-  <% if collection_routing? %>
+  <%- if collection_routing? -%>
   def create
     <%= plural_table_name %>_params ? create_many : create_one
   end
@@ -52,7 +51,7 @@ class <%= controller_class_name %>Controller < ApplicationController
     @<%= singular_table_name %> = <%= class_name %>.create(<%= singular_table_name %>_params)
     redirect_to <%= singular_table_name %>_url(@<%= singular_table_name %>), notice: <%= "'#{human_name} was successfully updated.'" %>
   end
-  <% else -%>
+  <%- else -%>
   def create
     @<%= singular_table_name %> = <%= orm_class.build(class_name, "#{singular_table_name}_params") %>
 
@@ -62,7 +61,7 @@ class <%= controller_class_name %>Controller < ApplicationController
       render action: 'new'
     end
   end
-  <% end -%>
+  <%- end -%>
 
   # PATCH/PUT <%= route_url %>/1
   def update
@@ -78,7 +77,6 @@ class <%= controller_class_name %>Controller < ApplicationController
     @<%= orm_instance.destroy %>
     redirect_to <%= index_helper %>_url, notice: <%= "'#{human_name} was successfully destroyed.'" %>
   end
-
   <% if collection_routing? %>
   # Collection routes
   # PATCH <%= route_url %>/1..10
@@ -98,8 +96,7 @@ class <%= controller_class_name %>Controller < ApplicationController
   def destroy_many
     @<%= plural_table_name %> = <%= orm_class.destroy(class_name, "#{plural_table_name}_ids") %>
   end
-  <% end -%>
-
+  <% end %>
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_<%= singular_table_name %>
@@ -114,8 +111,8 @@ class <%= controller_class_name %>Controller < ApplicationController
       params.require(:<%= singular_table_name %>).permit(<%= attributes_names.map { |name| ":#{name}" }.join(', ') %>)
       <%- end -%>
     end
-
-    <% if collection_routing? -%>
+    <%- if collection_routing? -%>
+    
     # Only allow a trusted collection parameter "white list" through.
     def <%= "#{plural_table_name}_params" %>
       <%- if attributes_names.empty? -%>
@@ -124,6 +121,6 @@ class <%= controller_class_name %>Controller < ApplicationController
       params.require(<%= ":#{plural_table_name}" %>).permit(<%= attributes_names.map { |name| ":#{name}" }.join(', ') %>)
       <%- end -%>
     end
-    <% end -%>
+    <%- end -%>
 end
 <% end -%>
