6da5ff45c6d69912fb96870f31aaf0c0590fa212
Godfrey Chan <godfreykfc@gmail.com>
2013-11-05 20:36:09 -0800

    Moved AS::JSON::DATE_REGEX as it's only used for decoding

--- activesupport/lib/active_support/json/encoding.rb
+++ activesupport/lib/active_support/json/encoding.rb
@@ -21,9 +21,6 @@ module ActiveSupport
   end
 
   module JSON
-    # matches YAML-formatted dates
-    DATE_REGEX = /^(?:\d{4}-\d{2}-\d{2}|\d{4}-\d{1,2}-\d{1,2}[T \t]+\d{1,2}:\d{2}:\d{2}(\.[0-9]*)?(([ \t]*)Z|[-+]\d{2}?(:\d{2})?))$/
-
     # Dumps objects in JSON (JavaScript Object Notation).
     # See www.json.org for more info.
     #
