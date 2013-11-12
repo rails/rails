49933594c1e2fe2564561b54d510faad269dc3d9
Charlie Somerville <charlie@charliesomerville.com>
2013-11-11 19:17:20 -0800

    delete activeresource/

--- activeresource/lib/active_resource/version.rb
+++
@@ -1,9 +0,0 @@
-module ActiveResource
-  module VERSION #:nodoc:
-    MAJOR = 2
-    MINOR = 3
-    TINY  = 14
-
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
-end
--- activeresource/lib/activeresource.rb
+++
@@ -1,2 +0,0 @@
-require 'active_resource'
-ActiveSupport::Deprecation.warn 'require "activeresource" is deprecated and will be removed in Rails 3. Use require "active_resource" instead.'
