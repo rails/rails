bea9c9b4c0f9bb7356ea2058118fe40495432010
Michael Koziarski <michael@koziarski.com>
2013-11-19 09:00:08 +1300

    Ensure simple_format escapes its html attributes
    The previous behavior equated the sanitize option for simple_format with the
    escape option of content_tag, however these are two distinct concepts.
    
    This fixes CVE-2013-6416
    
    Conflicts:
    	actionview/lib/action_view/helpers/text_helper.rb

--- actionview/lib/action_view/helpers/text_helper.rb
+++ actionview/lib/action_view/helpers/text_helper.rb
@@ -268,7 +268,7 @@ module ActionView
           content_tag(wrapper_tag, nil, html_options)
         else
           paragraphs.map! { |paragraph|
-            content_tag(wrapper_tag, paragraph, html_options, false)
+            content_tag(wrapper_tag, raw(paragraph), html_options, false)
           }.join("\n\n").html_safe
         end
       end
