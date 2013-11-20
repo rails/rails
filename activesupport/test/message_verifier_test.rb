d4c649a1e68bb9c95b411f52822cce62094689c6
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-11-19 22:20:35 -0200

    :scissors:
    [ci skip]

--- activesupport/test/message_verifier_test.rb
+++ activesupport/test/message_verifier_test.rb
@@ -11,7 +11,7 @@ require 'active_support/time'
 require 'active_support/json'
 
 class MessageVerifierTest < ActiveSupport::TestCase
-  
+
   class JSONSerializer
     def dump(value)
       ActiveSupport::JSON.encode(value)
@@ -21,7 +21,7 @@ class MessageVerifierTest < ActiveSupport::TestCase
       ActiveSupport::JSON.decode(value)
     end
   end
-  
+
   def setup
     @verifier = ActiveSupport::MessageVerifier.new("Hey, I'm a secret!")
     @data = { :some => "data", :now => Time.local(2010) }
@@ -43,7 +43,7 @@ class MessageVerifierTest < ActiveSupport::TestCase
     assert_not_verified("#{data}--#{hash.reverse}")
     assert_not_verified("purejunk")
   end
-  
+
   def test_alternative_serialization_method
     prev = ActiveSupport.use_standard_json_time_format
     ActiveSupport.use_standard_json_time_format = true
@@ -54,7 +54,7 @@ class MessageVerifierTest < ActiveSupport::TestCase
   ensure
     ActiveSupport.use_standard_json_time_format = prev
   end
-  
+
   def assert_not_verified(message)
     assert_raise(ActiveSupport::MessageVerifier::InvalidSignature) do
       @verifier.verify(message)
