1c105787d2f85307f68133b3836cea63b8fd7964
Ryuta Kamizono <kamipo@gmail.com>
2013-11-20 11:42:06 +0900

    Use `SchemaStatements#initialize_schema_migrations_table` instead of `ActiveRecord::SchemaMigration.create_table`.

--- activerecord/lib/active_record/migration.rb
+++ activerecord/lib/active_record/migration.rb
@@ -896,7 +896,7 @@ module ActiveRecord
 
       validate(@migrations)
 
-      ActiveRecord::SchemaMigration.create_table
+      Base.connection.initialize_schema_migrations_table
     end
 
     def current_version
