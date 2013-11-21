7c30f99242a8415a149624464208fecd47c62f27
Vipul A M <vipulnsward@gmail.com>
2013-11-22 00:44:10 +0530

    Remove unused param `title`to  `TransitionTable#visualizer`

--- actionpack/lib/action_dispatch/journey/gtg/transition_table.rb
+++ actionpack/lib/action_dispatch/journey/gtg/transition_table.rb
@@ -69,7 +69,7 @@ module ActionDispatch
           svg.join.sub(/width="[^"]*"/, '').sub(/height="[^"]*"/, '')
         end
 
-        def visualizer(paths, title = 'FSM')
+        def visualizer(paths)
           viz_dir   = File.join File.dirname(__FILE__), '..', 'visualizer'
           fsm_js    = File.read File.join(viz_dir, 'fsm.js')
           fsm_css   = File.read File.join(viz_dir, 'fsm.css')
