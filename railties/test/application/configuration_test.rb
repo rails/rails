9d86f6ce6efbe9b8b03c0992945bc2d056e6678e
Rafael Mendonça França <rafaelmfranca@gmail.com>
2013-11-22 00:03:28 -0200

    Don't need to use a controller to test the verifier

--- railties/test/application/configuration_test.rb
+++ railties/test/application/configuration_test.rb
@@ -274,19 +274,13 @@ module ApplicationTests
         app.config.session_store :disabled
       end
 
-      class ::OmgController < ActionController::Base
-        def index
-          render text: Rails.application.message_verifier.generate("some_value")
-        end
-      end
+      message = app.message_verifier.generate("some_value")
 
-      get "/"
-
-      assert_equal 'some_value', Rails.application.message_verifier.verify(last_response.body)
+      assert_equal 'some_value', Rails.application.message_verifier.verify(message)
 
       secret = app.key_generator.generate_key('default')
       verifier = ActiveSupport::MessageVerifier.new(secret)
-      assert_equal 'some_value', verifier.verify(last_response.body)
+      assert_equal 'some_value', verifier.verify(message)
     end
 
     test "application verifier can build different verifiers" do
