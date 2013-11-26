6eba8d27e6feecca88303df6500ef6b376344e29
Yves Senn <yves.senn@gmail.com>
2013-11-26 09:58:24 +0100

    `rename_index`: add the new index before removing the old one.
    This prevents the following error when a MySQL index on a foreign key
    column is renamed:
    
    ```
    ActiveRecord::StatementInvalid: Mysql2::Error: Cannot drop index 'index_engines_on_car_id': needed in a foreign key constraint: DROP INDEX `index_engines_on_car_id` ON `engines`
    ```
    
    refs: #13038.

--- activerecord/test/cases/adapters/mysql2/schema_migrations_test.rb
+++ activerecord/test/cases/adapters/mysql2/schema_migrations_test.rb
@@ -4,21 +4,34 @@ module ActiveRecord
   module ConnectionAdapters
     class Mysql2Adapter
       class SchemaMigrationsTest < ActiveRecord::TestCase
+        def test_renaming_index_on_foreign_key
+          connection.add_index "engines", "car_id"
+          connection.execute "ALTER TABLE engines ADD CONSTRAINT fk_engines_cars FOREIGN KEY (car_id) REFERENCES cars(id)"
+
+          connection.rename_index("engines", "index_engines_on_car_id", "idx_renamed")
+          assert_equal ["idx_renamed"], connection.indexes("engines").map(&:name)
+        ensure
+          connection.execute "ALTER TABLE engines DROP FOREIGN KEY fk_engines_cars"
+        end
+
         def test_initializes_schema_migrations_for_encoding_utf8mb4
-          conn = ActiveRecord::Base.connection
-
           smtn = ActiveRecord::Migrator.schema_migrations_table_name
-          conn.drop_table(smtn) if conn.table_exists?(smtn)
+          connection.drop_table(smtn) if connection.table_exists?(smtn)
 
-          config = conn.instance_variable_get(:@config)
+          config = connection.instance_variable_get(:@config)
           original_encoding = config[:encoding]
 
           config[:encoding] = 'utf8mb4'
-          conn.initialize_schema_migrations_table
+          connection.initialize_schema_migrations_table
 
-          assert conn.column_exists?(smtn, :version, :string, limit: Mysql2Adapter::MAX_INDEX_LENGTH_FOR_UTF8MB4)
+          assert connection.column_exists?(smtn, :version, :string, limit: Mysql2Adapter::MAX_INDEX_LENGTH_FOR_UTF8MB4)
         ensure
           config[:encoding] = original_encoding
+        end
+
+        private
+        def connection
+          @connection ||= ActiveRecord::Base.connection
         end
       end
     end
