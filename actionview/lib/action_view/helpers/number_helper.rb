8c158c8385e4d01577984df899b29693ba07949d
Michael Koziarski <michael@koziarski.com>
2013-12-02 10:12:47 +1300

    Escape the unit value provided to number_to_currency
    Previously the unit values were trusted leading to potential XSS vulnerabilities.
    
    Fixes: CVE-2013-6415

--- actionview/lib/action_view/helpers/number_helper.rb
+++ actionview/lib/action_view/helpers/number_helper.rb
@@ -394,6 +394,7 @@ module ActionView
       def escape_unsafe_delimiters_and_separators(options)
         options[:separator] = ERB::Util.html_escape(options[:separator]) if options[:separator] && !options[:separator].html_safe?
         options[:delimiter] = ERB::Util.html_escape(options[:delimiter]) if options[:delimiter] && !options[:delimiter].html_safe?
+        options[:unit]      = ERB::Util.html_escape(options[:unit]) if options[:unit] && !options[:unit].html_safe?
         options
       end
 
