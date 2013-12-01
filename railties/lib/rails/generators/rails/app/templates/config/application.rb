805768a67cac0be9ef84921309c5321fd358631c
Guillermo Iguaran <guilleiguaran@gmail.com>
2013-12-01 02:15:50 -0500

    config.assets.enabled isn't used anymore to disable Rails asset pipeline

--- railties/lib/rails/generators/rails/app/templates/config/application.rb
+++ railties/lib/rails/generators/rails/app/templates/config/application.rb
@@ -28,10 +28,5 @@ module <%= app_const_base %>
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-<% if options.skip_sprockets? -%>
-
-    # Disable the asset pipeline.
-    config.assets.enabled = false
-<% end -%>
   end
 end
