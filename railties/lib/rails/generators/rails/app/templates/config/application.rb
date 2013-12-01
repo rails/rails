57fd6ba4dba848c860ee142ea1fba0592519b3bf
Guillermo Iguaran <guilleiguaran@gmail.com>
2013-12-01 02:12:25 -0500

    Honor to RAILS_GROUPS env variable while loading gems with Bundler

--- railties/lib/rails/generators/rails/app/templates/config/application.rb
+++ railties/lib/rails/generators/rails/app/templates/config/application.rb
@@ -13,7 +13,7 @@ require "action_mailer/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module <%= app_const_base %>
   class Application < Rails::Application
