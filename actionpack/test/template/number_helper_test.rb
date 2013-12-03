72cebbcb59c308b2bfe6431a824a6c75fdeddc37
Nathan Witmer <nathan@zerowidth.com>
2013-12-03 14:32:26 -0700

    Escape the unit value provided to number_to_currency
    Fixes CVE-2013-6415.
    
    Previously the values were trusted blindly allowing for potential XSS attacks.
    
    This is different from the original upstream patch for 3.x in that return values
    from other number helper methods are not marked as html_safe, so the html
    escaping always applies. This requires applications to explicitly set .html_safe
    on unit strings and number separators when calling number_to_currency.

--- actionpack/test/template/number_helper_test.rb
+++ actionpack/test/template/number_helper_test.rb
@@ -24,9 +24,10 @@ class NumberHelperTest < ActionView::TestCase
     assert_equal("$1,234,567,890.51", number_to_currency(1234567890.506))
     assert_equal("$1,234,567,892", number_to_currency(1234567891.50, {:precision => 0}))
     assert_equal("$1,234,567,890.5", number_to_currency(1234567890.50, {:precision => 1}))
-    assert_equal("&pound;1234567890,50", number_to_currency(1234567890.50, {:unit => "&pound;", :separator => ",", :delimiter => ""}))
+    assert_equal("&pound;1234567890,50", number_to_currency(1234567890.50, {:unit => raw("&pound;"), :separator => ",", :delimiter => ""}))
+    assert_equal("&amp;pound;1234567890,50", number_to_currency(1234567890.50, {:unit => "&pound;", :separator => ",", :delimiter => ""}))
     assert_equal("$1,234,567,890.50", number_to_currency("1234567890.50"))
-    assert_equal("1,234,567,890.50 K&#269;", number_to_currency("1234567890.50", {:unit => "K&#269;", :format => "%n %u"}))
+    assert_equal("1,234,567,890.50 K&#269;", number_to_currency("1234567890.50", {:unit => raw("K&#269;"), :format => "%n %u"}))
     #assert_equal("$x.", number_to_currency("x")) # fails due to API consolidation
     assert_equal("$x", number_to_currency("x"))
     assert_nil number_to_currency(nil)
