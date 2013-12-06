f1a05e741d19ec7b5366c2136ffda9e03dcdcc12
Arun Agrawal <arunagw@gmail.com>
2013-12-06 16:48:28 +0100

    Skipping debugger from plugin Gemfile for JRuby

--- railties/test/generators/plugin_generator_test.rb
+++ railties/test/generators/plugin_generator_test.rb
@@ -58,6 +58,17 @@ class PluginGeneratorTest < Rails::Generators::TestCase
     assert_file "test/integration/navigation_test.rb", /ActionDispatch::IntegrationTest/
   end
 
+  def test_inclusion_of_debugger
+    run_generator [destination_root, '--full']
+    if defined?(JRUBY_VERSION)
+      assert_file "Gemfile" do |content|
+        assert_no_match(/debugger/, content)
+      end
+    else
+      assert_file "Gemfile", /# gem 'debugger'/
+    end
+  end
+
   def test_generating_test_files_in_full_mode_without_unit_test_files
     run_generator [destination_root, "-T", "--full"]
 
