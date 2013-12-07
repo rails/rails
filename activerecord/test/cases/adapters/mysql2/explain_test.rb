75f453ff4cff8b9652cc8f54ee68f775341148e9
Yasuo Honda <yasuo.honda@gmail.com>
2013-12-08 06:51:08 +0900

    Support MySQL 5.7 explain

--- activerecord/test/cases/adapters/mysql2/explain_test.rb
+++ activerecord/test/cases/adapters/mysql2/explain_test.rb
@@ -10,15 +10,15 @@ module ActiveRecord
         def test_explain_for_one_query
           explain = Developer.where(:id => 1).explain
           assert_match %(EXPLAIN for: SELECT `developers`.* FROM `developers`  WHERE `developers`.`id` = 1), explain
-          assert_match %(developers | const), explain
+          assert_match /developers |.* const/, explain
         end
 
         def test_explain_with_eager_loading
           explain = Developer.where(:id => 1).includes(:audit_logs).explain
           assert_match %(EXPLAIN for: SELECT `developers`.* FROM `developers`  WHERE `developers`.`id` = 1), explain
-          assert_match %(developers | const), explain
+          assert_match /developers |.* const/, explain
           assert_match %(EXPLAIN for: SELECT `audit_logs`.* FROM `audit_logs`  WHERE `audit_logs`.`developer_id` IN (1)), explain
-          assert_match %(audit_logs | ALL), explain
+          assert_match /audit_logs |.* ALL/, explain
         end
       end
     end
