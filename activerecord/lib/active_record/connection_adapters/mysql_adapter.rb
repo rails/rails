9161cfee236bb23f0c471572b9037dae2eab3791
TroySK <troysk704@gmail.com>
2013-12-07 14:18:34 +0530

    Check MySQL connection using ping

--- activerecord/lib/active_record/connection_adapters/mysql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/mysql_adapter.rb
@@ -167,8 +167,8 @@ module ActiveRecord
       # CONNECTION MANAGEMENT ====================================
 
       def active?
-        if @connection.respond_to?(:stat)
-          @connection.stat
+        if @connection.respond_to?(:ping)
+          @connection.ping
         else
           @connection.query 'select 1'
         end
