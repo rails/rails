b8569b9337309c2d6c72566a3426994b6da443d7
Kassio Borges <kassioborgesm@gmail.com>
2013-12-13 11:37:19 -0200

    Fix mysql to support duplicated column names
    This will fix the [broken
    test](https://github.com/rails/rails/commit/4a2650836680f51490e999c3c8441a2f9adff96e)
     `test_with_limiting_with_custom_select`.
    
    The query's result was built in a hash with column name as key, if the
    result have a duplicated column name the last value was
    overriding the first one.

--- activerecord/test/cases/finder_test.rb
+++ activerecord/test/cases/finder_test.rb
@@ -855,7 +855,7 @@ class FinderTest < ActiveRecord::TestCase
 
   def test_with_limiting_with_custom_select
     posts = Post.references(:authors).merge(
-      :includes => :author, :select => ' posts.*, authors.id as "author_id"',
+      :includes => :author, :select => 'posts.*, authors.id as "author_id"',
       :limit => 3, :order => 'posts.id'
     ).to_a
     assert_equal 3, posts.size
