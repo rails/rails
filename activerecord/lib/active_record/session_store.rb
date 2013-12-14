1805682efa8a10a06ad3674e35181f63496e02e0
Chris Maddox <tyre77@gmail.com>
2013-12-13 17:02:16 -0800

    Update Session Store Documentation
    session_id doesn't need to be a text column, just string (VARCHAR)

--- activerecord/lib/active_record/session_store.rb
+++ activerecord/lib/active_record/session_store.rb
@@ -9,7 +9,7 @@ module ActiveRecord
   #
   # The default assumes a +sessions+ tables with columns:
   #   +id+ (numeric primary key),
-  #   +session_id+ (text, or longtext if your session data exceeds 65K), and
+  #   +session_id+ (string, :limit => 255), and
   #   +data+ (text or longtext; careful if your session data exceeds 65KB).
   #
   # The +session_id+ column should always be indexed for speedy lookups.
