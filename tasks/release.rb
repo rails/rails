dc90709eaf1242f565de3a9282d7939f6bc853cf
David Heinemeier Hansson <david@loudthinking.com>
2013-12-17 16:10:52 -0800

    Fix dependency order

--- tasks/release.rb
+++ tasks/release.rb
@@ -1,4 +1,4 @@
-FRAMEWORKS = %w( activesupport activemodel activerecord actionpack actionview actionmailer railties )
+FRAMEWORKS = %w( activesupport activemodel activerecord actionview actionpack actionmailer railties )
 
 root    = File.expand_path('../../', __FILE__)
 version = File.read("#{root}/RAILS_VERSION").strip
