rails/rails

commit 1678e959e973de32287b65c52ebc6cce87148951

remove nil check
calling replace_keys could possibly do a nil check twice, this commit
reduces it to once.

activerecord/lib/active_record/associations/belongs_to_polymorphic_association.rb
@@ -11,7 +11,7 @@ module ActiveRecord
 
         def replace_keys(record)
           super
-          owner[reflection.foreign_type] = record && record.class.base_class.name
+          owner[reflection.foreign_type] = record.class.base_class.name
         end
 
         def different_target?(record)

activerecord/lib/active_record/associations/belongs_to_association.rb
@@ -11,7 +11,11 @@ module ActiveRecord
         raise_on_type_mismatch!(record) if record
 
         update_counters(record)
-        replace_keys(record)
+        if record
+          replace_keys(record)
+        else
+          remove_keys
+        end
         set_inverse_instance(record) if record
 
         @updated = true if record
@@ -58,11 +62,11 @@ module ActiveRecord
         end
 
         def replace_keys(record)
-          if record
-            owner[reflection.foreign_key] = record[reflection.association_primary_key(record.class)]
-          else
-            owner[reflection.foreign_key] = nil
-          end
+          owner[reflection.foreign_key] = record[reflection.association_primary_key(record.class)]
+        end
+
+        def remove_keys
+          owner[reflection.foreign_key] = nil
         end
 
         def foreign_key_present?

commit 01162b3d77b50057791526d29b97c16a891b6048

Fix nil assignment to polymorphic belongs_to
Assigning nil to a polymorphic belongs_to would nullify its _id field by
not its _type field.

Fixes failing test from c141dfc838a5dca9f197814410fa5d44c143129c.
Regression from 1678e959e973de32287b65c52ebc6cce87148951.

activerecord/lib/active_record/associations/belongs_to_polymorphic_association.rb
@@ -14,6 +14,11 @@ module ActiveRecord
           owner[reflection.foreign_type] = record.class.base_class.name
         end
 
+        def remove_keys
+          super
+          owner[reflection.foreign_type] = nil
+        end
+
         def different_target?(record)
           super || record.class != klass
         end