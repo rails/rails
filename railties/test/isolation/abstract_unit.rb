959cfcef7255bba720ce3f15323056533ea7b50a
José Valim <jose.valim@plataformatec.com.br>
2013-12-24 16:35:43 +0100

    Ensure secret_key_base is set for all environments

--- railties/test/isolation/abstract_unit.rb
+++ railties/test/isolation/abstract_unit.rb
@@ -136,6 +136,7 @@ module TestHelpers
       end
 
       add_to_config <<-RUBY
+        secrets.secret_key_base = "3b7cd727ee24e8444053437c36cc66c4"
         config.eager_load = false
         config.session_store :cookie_store, key: "_myapp_session"
         config.active_support.deprecation = :log
@@ -155,8 +156,8 @@ module TestHelpers
       require "action_view/railtie"
 
       app = Class.new(Rails::Application)
-      app.config.eager_load = false
       app.secrets.secret_key_base = "3b7cd727ee24e8444053437c36cc66c4"
+      app.config.eager_load = false
       app.config.session_store :cookie_store, key: "_myapp_session"
       app.config.active_support.deprecation = :log
 
