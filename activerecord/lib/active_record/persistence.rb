97dedf26090d3bea1ffe91570e85f987a993fcc9
Aayush khandelwal <aayush.khandelwal@vinsol.com>
2013-12-25 18:16:27 +0530

    Typo rectified commom => common

--- activerecord/lib/active_record/persistence.rb
+++ activerecord/lib/active_record/persistence.rb
@@ -361,7 +361,7 @@ module ActiveRecord
     #   assert_equal 25, account.credit        # check it is updated in memory
     #   assert_equal 25, account.reload.credit # check it is also persisted
     #
-    # Another commom use case is optimistic locking handling:
+    # Another common use case is optimistic locking handling:
     #
     #   def with_optimistic_retry
     #     begin
