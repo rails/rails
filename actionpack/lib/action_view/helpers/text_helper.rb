addf13ba74f1679fa072402a95436369b794e656
kud86 <mrwlasow@gmail.com>
2014-01-02 14:14:30 +0500

    Fixed `truncate` documentation
    Fixed examples

--- actionpack/lib/action_view/helpers/text_helper.rb
+++ actionpack/lib/action_view/helpers/text_helper.rb
@@ -82,7 +82,10 @@ module ActionView
       #   # => "And they f... (continued)"
       #
       #   truncate("<p>Once upon a time in a world far far away</p>")
-      #   # => "<p>Once upon a time in a wo..."
+      #   # => "&lt;p&gt;Once upon a time in a wo..."
+      #
+      #   truncate("<p>Once upon a time in a world far far away</p>", escape: false)
+      #   # => "<p>Once upon a time in a wo...
       #
       #   truncate("Once upon a time in a world far far away") { link_to "Continue", "#" }
       #   # => "Once upon a time in a wo...<a href="#">Continue</a>"
