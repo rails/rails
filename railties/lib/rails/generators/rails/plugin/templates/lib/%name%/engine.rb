ee9e4c340e68cd6f274b1443b68f1d3e91db7b6f
Robin Böning <rb@magiclabs.de>
2014-01-05 20:41:16 +0100

    Allow hyphenated names for `rails plugin new` generator.
    It was not possible to create a new gem with a hyphenated name via the `rails plugin new` generator.
    
    The naming guide of rubygems clearly says dashes should be used for gems that extend other gems. http://guides.rubygems.org/name-your-gem/

--- railties/lib/rails/generators/rails/plugin/templates/lib/%name%/engine.rb
+++
@@ -1,7 +0,0 @@
-module <%= camelized %>
-  class Engine < ::Rails::Engine
-<% if mountable? -%>
-    isolate_namespace <%= camelized %>
-<% end -%>
-  end
-end
--- railties/lib/rails/generators/rails/plugin/templates/lib/tasks/%namespaced_name%_tasks.rake
+++ railties/lib/rails/generators/rails/plugin/templates/lib/tasks/%namespaced_name%_tasks.rake
@@ -1,4 +1,4 @@
 # desc "Explaining what the task does"
-# task :<%= name %> do
+# task :<%= underscored_name %> do
 #   # Task goes here
 # end
