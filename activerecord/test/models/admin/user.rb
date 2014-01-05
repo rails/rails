901a0c8b4a3b1de8ff1ec8feb4b67f727c07dc8b
Thales Oliveira <oliveiraethales@gmail.com>
2014-01-05 20:55:02 -0200

    Fix: ActiveRecord::Store TypeError conversion when using YAML coder
    Renaming the test accordingly to its behaviour
    
    Adding 'Fixes' statement to changelog
    
    Improving tests legibility & changelog
    
    Undoing mistakenly removed empty line & further improving changelog

--- activerecord/test/models/admin/user.rb
+++ activerecord/test/models/admin/user.rb
@@ -14,6 +14,7 @@ class Admin::User < ActiveRecord::Base
   end
 
   belongs_to :account
+  store :params, accessors: [ :color ], coder: YAML
   store :settings, :accessors => [ :color, :homepage ]
   store_accessor :settings, :favorite_food
   store :preferences, :accessors => [ :remember_login ]
