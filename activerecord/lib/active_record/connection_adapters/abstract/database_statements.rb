da65fe9e11b7f445847228753e6df46d376e29ab
Aaron Patterson <aaron.patterson@gmail.com>
2014-01-09 14:17:04 -0800

    Revert "ask the fixture set for the sql statements"
    This reverts commit 026d0555685087845b74dd87a0417b5a164b1c13.
    
    Conflicts:
    	activerecord/lib/active_record/fixtures.rb
    
    Fixes #13383

--- activerecord/lib/active_record/connection_adapters/abstract/database_statements.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/database_statements.rb
@@ -286,10 +286,6 @@ module ActiveRecord
       # Inserts the given fixture into the table. Overridden in adapters that require
       # something beyond a simple insert (eg. Oracle).
       def insert_fixture(fixture, table_name)
-        execute fixture_sql(fixture, table_name), 'Fixture Insert'
-      end
-
-      def fixture_sql(fixture, table_name)
         columns = schema_cache.columns_hash(table_name)
 
         key_list   = []
@@ -298,7 +294,7 @@ module ActiveRecord
           quote(value, columns[name])
         end
 
-        "INSERT INTO #{quote_table_name(table_name)} (#{key_list.join(', ')}) VALUES (#{value_list.join(', ')})"
+        execute "INSERT INTO #{quote_table_name(table_name)} (#{key_list.join(', ')}) VALUES (#{value_list.join(', ')})", 'Fixture Insert'
       end
 
       def empty_insert_statement_value
