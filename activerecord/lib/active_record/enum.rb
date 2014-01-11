4e58662694d249ce69e26db2c1032fb24c380925
Greg Molnar <molnargerg@gmail.com>
2014-01-11 10:16:18 +0100

    fix enum value typecast issue

--- activerecord/lib/active_record/enum.rb
+++ activerecord/lib/active_record/enum.rb
@@ -74,7 +74,7 @@ module ActiveRecord
           define_method("#{name}=") { |value|
             if enum_values.has_key?(value) || value.blank?
               self[name] = enum_values[value]
-            elsif enum_values.has_value?(value)
+            elsif enum_values.has_value?(value) || (value.respond_to?(:to_i) and enum_values.has_value?(value.to_i))
               # Assigning a value directly is not a end-user feature, hence it's not documented.
               # This is used internally to make building objects from the generated scopes work
               # as expected, i.e. +Conversation.archived.build.archived?+ should be true.
