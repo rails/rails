aec4aa1702753f29ae79787c743f6f047ad9a80d
Aaron Patterson <aaron.patterson@gmail.com>
2014-01-13 11:50:42 -0800

    check bind parameter uniqueness

--- activerecord/test/cases/associations/association_scope_test.rb
+++ activerecord/test/cases/associations/association_scope_test.rb
@@ -7,8 +7,13 @@ module ActiveRecord
     class AssociationScopeTest < ActiveRecord::TestCase
       test 'does not duplicate conditions' do
         association_scope = AssociationScope.new(Author.new.association(:welcome_posts))
-        wheres = association_scope.scope.where_values.map(&:right)
+        scope = association_scope.scope
+        binds = scope.bind_values.map(&:last)
+        wheres = scope.where_values.map(&:right).reject { |node|
+          Arel::Nodes::BindParam === node
+        }
         assert_equal wheres.uniq, wheres
+        assert_equal binds.uniq, binds
       end
     end
   end
