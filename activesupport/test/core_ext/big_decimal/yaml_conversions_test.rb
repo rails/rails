4ac114471cc14de44b77ced3dd4a0676c13b818b
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-02-01 18:29:53 -0200

    Assert the file is deprecated

--- activesupport/test/core_ext/big_decimal/yaml_conversions_test.rb
+++ activesupport/test/core_ext/big_decimal/yaml_conversions_test.rb
@@ -1,8 +1,8 @@
 require 'abstract_unit'
-require 'active_support/core_ext/big_decimal/yaml_conversions'
 
 class BigDecimalYamlConversionsTest < ActiveSupport::TestCase
   def test_to_yaml
+    assert_deprecated { require 'active_support/core_ext/big_decimal/yaml_conversions' }
     assert_match("--- 100000.30020320320000000000000000000000000000001\n", BigDecimal.new('100000.30020320320000000000000000000000000000001').to_yaml)
     assert_match("--- .Inf\n",  BigDecimal.new('Infinity').to_yaml)
     assert_match("--- .NaN\n",  BigDecimal.new('NaN').to_yaml)
