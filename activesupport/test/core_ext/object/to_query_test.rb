d6e3fd775b8a0277b07b06c22c29f66dbafe6559
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-02-05 23:47:16 -0200

    Test with a blank value

--- activesupport/test/core_ext/object/to_query_test.rb
+++ activesupport/test/core_ext/object/to_query_test.rb
@@ -49,8 +49,8 @@ class ToQueryTest < ActiveSupport::TestCase
   def test_nested_empty_hash
     assert_query_equal 'a=1&b%5Bc%5D=3&b%5Bd%5D=',
       { a: 1, b: { c: 3, d: {} } }
-    assert_query_equal 'b%5Bc%5D=3&b%5Be%5D=&b%5Bf%5D=&p=12',
-      { p: 12, b: { c: 3, e: nil, f: '' } }
+    assert_query_equal 'b%5Bc%5D=false&b%5Be%5D=&b%5Bf%5D=&p=12',
+      { p: 12, b: { c: false, e: nil, f: '' } }
     assert_query_equal 'b%5Bc%5D=3&b%5Bf%5D=&b%5Bk%5D=',
       { b: { c: 3, k: {}, f: '' } }
   end
