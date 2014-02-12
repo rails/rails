rails/rails

commit 20e041579f5fe9df51bdc40048cc1ef78915e116

Support creating a table migration generator
Sometimes you want to create a table without an associated model and
test, which is also not a join table. With this commit, you can now
do that.

Example:

    rails g migration create_posts title:string
or
    rails g migration CreatePosts title:string

This commit also moves the template the model generator uses for the
migration to the migration templates folder, as it seems a more
sensible place for it now that it is shared code.

activerecord/lib/rails/generators/active_record/model/model_generator.rb
@@ -15,7 +15,7 @@ module ActiveRecord
       def create_migration_file
         return unless options[:migration] && options[:parent].nil?
         attributes.each { |a| a.attr_options.delete(:index) if a.reference? && !a.has_index? } if options[:indexes] == false
-        migration_template "migration.rb", "db/migrate/create_#{table_name}.rb"
+        migration_template "../../migration/templates/create_table_migration.rb", "db/migrate/create_#{table_name}.rb"
       end
 
       def create_model_file

activerecord/lib/rails/generators/active_record/migration/migration_generator.rb
@@ -8,13 +8,14 @@ module ActiveRecord
       def create_migration_file
         set_local_assigns!
         validate_file_name!
-        migration_template "migration.rb", "db/migrate/#{file_name}.rb"
+        migration_template @migration_template, "db/migrate/#{file_name}.rb"
       end
 
       protected
       attr_reader :migration_action, :join_tables
 
       def set_local_assigns!
+        @migration_template = "migration.rb"
         case file_name
         when /^(add|remove)_.*_(?:to|from)_(.*)/
           @migration_action = $1
@@ -26,6 +27,9 @@ module ActiveRecord
 
             set_index_names
           end
+        when /^create_(.+)/
+          @table_name = $1.pluralize
+          @migration_template = "create_table_migration.rb"
         end
       end
 
@@ -44,7 +48,10 @@ module ActiveRecord
       end
 
       private
-
+        def attributes_with_index
+          attributes.select { |a| !a.reference? && a.has_index? }
+        end
+        
         def validate_file_name!
           unless file_name =~ /^[_a-z0-9]+$/
             raise IllegalMigrationNameError.new(file_name)

railties/test/generators/migration_generator_test.rb
@@ -172,8 +172,19 @@ class MigrationGeneratorTest < Rails::Generators::TestCase
     end
   end
 
-  def test_should_create_empty_migrations_if_name_not_start_with_add_or_remove
-    migration = "create_books"
+  def test_create_table_migration
+    run_generator ["create_books", "title:string", "content:text"]
+    assert_migration "db/migrate/create_books.rb" do |content|
+      assert_method :change, content do |change|
+        assert_match(/create_table :books/, change)
+        assert_match(/  t\.string :title/, change)
+        assert_match(/  t\.text :content/, change)
+      end
+    end
+  end
+
+  def test_should_create_empty_migrations_if_name_not_start_with_add_or_remove_or_create
+    migration = "delete_books"
     run_generator [migration, "title:string", "content:text"]
 
     assert_migration "db/migrate/#{migration}.rb" do |content|
@@ -182,7 +193,7 @@ class MigrationGeneratorTest < Rails::Generators::TestCase
       end
     end
   end
-
+  
   def test_properly_identifies_usage_file
     assert generator_class.send(:usage_path)
   end

commit d0b574cf24fe0497e9d57bf4f5f210369108b38b

Fix search for create_table_migration template override
A developer should be able to customize the template used by the model or migration generator, by adding a file to their source tree in a predictable location.

This functionality was likely broken by:
https://github.com/rails/rails/commit/20e041579f5fe9df51bdc40048cc1ef78915e116

After that change, you could now generate a migration:

`rails g migration CreateFoo`

and it would look for a custom template at `myproject/lib/templates/active_record/migration/create_table_migration.rb`

However, if you generated the migration by generating a model:

`rails g model Bar`

it would look for a template at the unpredictable and awkward path `myproject/lib/templates/migration/templates/create_table_migration.rb`


This Pull Request adds the migration generator's template paths to the model generator's search paths. When you generate a model, it will now look for templates in order:

```
<myproject>/lib/templates/active_record/model/create_table_migration.rb
<activerecord gem>/lib/rails/generators/active_record/model/templates/create_table_migration.rb
<myproject>/lib/templates/active_record/migration/create_table_migration.rb
<activerecord gem>/lib/rails/generators/active_record/migration/templates/create_table_migration.rb
```

This has the side-effect of allowing you to have different templates for creating a table, depending on whether you generated a model or the migration directly.

activerecord/lib/rails/generators/active_record/model/model_generator.rb
@@ -18,7 +18,18 @@ module ActiveRecord
       def create_migration_file
         return unless options[:migration] && options[:parent].nil?
         attributes.each { |a| a.attr_options.delete(:index) if a.reference? && !a.has_index? } if options[:indexes] == false
-        migration_template "../../migration/templates/create_table_migration.rb", "db/migrate/create_#{table_name}.rb"
+        ensure_migration_source_paths
+        migration_template "create_table_migration.rb", "db/migrate/create_#{table_name}.rb"
+      end
+
+      def ensure_migration_source_paths
+        return if @migration_source_paths_added
+        Rails::Generators.templates_path.each do |path|
+          source_paths << File.join(path, self.class.base_name, "migration")
+        end
+        path = File.expand_path(File.join(self.class.base_name, "migration", "templates"), self.class.base_root)
+        source_paths << path
+        @migration_source_paths_added = true
       end
 
       def create_model_file