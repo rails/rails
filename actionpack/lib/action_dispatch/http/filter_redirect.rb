ead6e85eda59d4289c391730aca514a946e5b414
Trevor Turk <trevorturk@gmail.com>
2014-02-13 22:59:34 -0600

    Fall back to filter_parameters when filter_redirect is not set for redirect log lines, re: 86e3aaab

--- actionpack/lib/action_dispatch/http/filter_redirect.rb
+++ actionpack/lib/action_dispatch/http/filter_redirect.rb
@@ -3,13 +3,14 @@ module ActionDispatch
     module FilterRedirect
 
       FILTERED = '[FILTERED]'.freeze # :nodoc:
+      NULL_PARAM_FILTER = ParameterFilter.new # :nodoc:
 
       def filtered_location
         filters = location_filter
         if !filters.empty? && location_filter_match?(filters)
           FILTERED
         else
-          location
+          parameter_filtered_location
         end
       end
 
@@ -30,6 +31,21 @@ module ActionDispatch
           elsif Regexp === filter
             location.match(filter)
           end
+        end
+      end
+
+
+      def parameter_filter
+        ParameterFilter.new request.env.fetch("action_dispatch.parameter_filter") {
+          return NULL_PARAM_FILTER
+        }
+      end
+
+      KV_RE   = '[^&;=]+'
+      PAIR_RE = %r{(#{KV_RE})=(#{KV_RE})}
+      def parameter_filtered_location
+        location.gsub(PAIR_RE) do |_|
+          parameter_filter.filter([[$1, $2]]).first.join("=")
         end
       end
 
