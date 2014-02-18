rails/rails

commit e220a34e396f026bbee8c7492aaa0ca515995a05

Update Docs in favor to use render plain instead of text option ref #14062

actionmailer/lib/action_mailer/base.rb
@@ -724,11 +724,11 @@ module ActionMailer
     #     format.html
     #   end
     #
-    # You can even render text directly without using a template:
+    # You can even render plain directly without using a template:
     #
     #   mail(to: 'mikel@test.lindsaar.net') do |format|
-    #     format.text { render text: "Hello Mikel!" }
-    #     format.html { render text: "<h1>Hello Mikel!</h1>" }
+    #     format.text { render plain: "Hello Mikel!" }
+    #     format.html { render plain: "<h1>Hello Mikel!</h1>" }
     #   end
     #
     # Which will render a +multipart/alternative+ email with +text/plain+ and

actionpack/lib/action_controller/base.rb
@@ -45,7 +45,7 @@ module ActionController
   #
   #   def server_ip
   #     location = request.env["SERVER_ADDR"]
-  #     render text: "This server hosted at #{location}"
+  #     render plain: "This server hosted at #{location}"
   #   end
   #
   # == Parameters

actionpack/lib/action_controller/metal/data_streaming.rb
@@ -96,7 +96,7 @@ module ActionController #:nodoc:
       end
 
       # Sends the given binary data to the browser. This method is similar to
-      # <tt>render text: data</tt>, but also allows you to specify whether
+      # <tt>render plain: data</tt>, but also allows you to specify whether
       # the browser should display the response as a file attachment (i.e. in a
       # download dialog) or as inline data. You may also set the content type,
       # the apparent file name, and other things.

actionpack/lib/action_controller/metal/http_authentication.rb
@@ -11,11 +11,11 @@ module ActionController
     #     http_basic_authenticate_with name: "dhh", password: "secret", except: :index
     #
     #     def index
-    #       render text: "Everyone can see me!"
+    #       render plain: "Everyone can see me!"
     #     end
     #
     #     def edit
-    #       render text: "I'm only accessible if you know the password"
+    #       render plain: "I'm only accessible if you know the password"
     #     end
     #  end
     #
@@ -127,11 +127,11 @@ module ActionController
     #     before_action :authenticate, except: [:index]
     #
     #     def index
-    #       render text: "Everyone can see me!"
+    #       render plain: "Everyone can see me!"
     #     end
     #
     #     def edit
-    #       render text: "I'm only accessible if you know the password"
+    #       render plain: "I'm only accessible if you know the password"
     #     end
     #
     #     private
@@ -321,11 +321,11 @@ module ActionController
     #     before_action :authenticate, except: [ :index ]
     #
     #     def index
-    #       render text: "Everyone can see me!"
+    #       render plain: "Everyone can see me!"
     #     end
     #
     #     def edit
-    #       render text: "I'm only accessible if you know the password"
+    #       render plain: "I'm only accessible if you know the password"
     #     end
     #
     #     private

commit 1c248dfbabd2f2d99191e3ab2a013061e7a1c896

Fix render plain docs example in AM::Base
The errors were introduced in e220a34e39

actionmailer/lib/action_mailer/base.rb
@@ -724,11 +724,11 @@ module ActionMailer
     #     format.html
     #   end
     #
-    # You can even render plain directly without using a template:
+    # You can even render plain text directly without using a template:
     #
     #   mail(to: 'mikel@test.lindsaar.net') do |format|
     #     format.text { render plain: "Hello Mikel!" }
-    #     format.html { render plain: "<h1>Hello Mikel!</h1>" }
+    #     format.html { render html: "<h1>Hello Mikel!</h1>".html_safe }
     #   end
     #
     # Which will render a +multipart/alternative+ email with +text/plain+ and