rails/rails

commit 3af4ae82e587b12664626e7b22bc6cc21ebbca2e

Make sure inverse_of is visible on the has_many callbacks

activerecord/test/cases/associations/has_many_associations_test.rb
@@ -518,6 +518,13 @@ class HasManyAssociationsTest < ActiveRecord::TestCase
     end
   end
 
+  def test_inverse_on_before_validate
+    firm = companies(:first_firm)
+    assert_queries(1) do
+      firm.clients_of_firm << Client.new("name" => "Natural Company")
+    end
+  end
+
   def test_new_aliased_to_build
     company = companies(:first_firm)
     new_client = assert_no_queries { company.clients_of_firm.new("name" => "Another Client") }

activerecord/lib/active_record/associations/has_many_association.rb
@@ -32,6 +32,7 @@ module ActiveRecord
 
       def insert_record(record, validate = true, raise = false)
         set_owner_attributes(record)
+        set_inverse_instance(record)
 
         if raise
           record.save!(:validate => validate)

activerecord/test/models/company.rb
@@ -39,7 +39,7 @@ class Firm < Company
   has_many :unsorted_clients, :class_name => "Client"
   has_many :unsorted_clients_with_symbol, :class_name => :Client
   has_many :clients_sorted_desc, -> { order "id DESC" }, :class_name => "Client"
-  has_many :clients_of_firm, -> { order "id" }, :foreign_key => "client_of", :class_name => "Client"
+  has_many :clients_of_firm, -> { order "id" }, :foreign_key => "client_of", :class_name => "Client", :inverse_of => :firm
   has_many :clients_ordered_by_name, -> { order "name" }, :class_name => "Client"
   has_many :unvalidated_clients_of_firm, :foreign_key => "client_of", :class_name => "Client", :validate => false
   has_many :dependent_clients_of_firm, -> { order "id" }, :foreign_key => "client_of", :class_name => "Client", :dependent => :destroy
@@ -116,6 +116,10 @@ class Client < Company
   belongs_to :bob_firm, -> { where :name => "Bob" }, :class_name => "Firm", :foreign_key => "client_of"
   has_many :accounts, :through => :firm, :source => :accounts
   belongs_to :account
+
+  validate do
+    firm
+  end
 
   class RaisedOnSave < RuntimeError; end
   attr_accessor :raise_on_save

commit 8ec6e72de3e2d597835d1085138a7419c40b2531

3af4ae82e587b12664626e7b22bc6cc21ebbca2e introduced a regression on has many association, where calling a child from parent in child's callback resulted in same child records getting added repeatedly to target.
Fixes #13387.

activerecord/lib/active_record/associations/association.rb
@@ -102,6 +102,15 @@ module ActiveRecord
         @association_scope = nil
       end
 
+      # Returns if a reversible instance has been set
+      def inverse_instance_set?(record)
+        if invertible_for?(record)
+          record.association(inverse_reflection_for(record).name).inversed
+        else
+          false
+        end
+      end
+
       # Set the inverse association, if possible
       def set_inverse_instance(record)
         if invertible_for?(record)

activerecord/lib/active_record/associations/collection_association.rb
@@ -388,7 +388,7 @@ module ActiveRecord
         callback(:before_add, record) unless skip_callbacks
         yield(record) if block_given?
 
-        if association_scope.distinct_value && index = @target.index(record)
+        if (inverse_instance_set?(record) || association_scope.distinct_value) && index = @target.index(record)
           @target[index] = record
         else
           @target << record

activerecord/test/cases/associations/has_many_associations_test.rb
@@ -22,6 +22,8 @@ require 'models/engine'
 require 'models/categorization'
 require 'models/minivan'
 require 'models/speedometer'
+require 'models/zine'
+require 'models/interest'
 
 class HasManyAssociationsTestForReorderWithJoinDependency < ActiveRecord::TestCase
   fixtures :authors, :posts, :comments
@@ -1830,4 +1832,13 @@ class HasManyAssociationsTest < ActiveRecord::TestCase
       end
     end
   end
+
+  test "calling child from parent in child callback doesn't add extra repeated records to target" do
+    man = Zine.create!
+    man.interests << Interest.new
+
+    assert_equal 1, man.interests.count
+    assert_equal 1, man.interests.to_a.count
+  end
+
 end

activerecord/test/cases/associations_test.rb
@@ -20,6 +20,7 @@ require 'models/molecule'
 require 'models/electron'
 require 'models/man'
 require 'models/interest'
+require 'models/zine'
 
 class AssociationsTest < ActiveRecord::TestCase
   fixtures :accounts, :companies, :developers, :projects, :developers_projects,

activerecord/test/cases/nested_attributes_test.rb
@@ -7,6 +7,7 @@ require "models/parrot"
 require "models/treasure"
 require "models/man"
 require "models/interest"
+require 'models/zine'
 require "models/owner"
 require "models/pet"
 require 'active_support/hash_with_indifferent_access'

activerecord/test/cases/validations/association_validation_test.rb
@@ -5,6 +5,7 @@ require 'models/reply'
 require 'models/owner'
 require 'models/pet'
 require 'models/man'
+require 'models/zine'
 require 'models/interest'
 
 class AssociationValidationTest < ActiveRecord::TestCase

activerecord/test/cases/validations/presence_validation_test.rb
@@ -2,6 +2,7 @@
 require "cases/helper"
 require 'models/man'
 require 'models/face'
+require 'models/zine'
 require 'models/interest'
 require 'models/speedometer'
 require 'models/dashboard'

activerecord/test/models/interest.rb
@@ -2,4 +2,10 @@ class Interest < ActiveRecord::Base
   belongs_to :man, :inverse_of => :interests
   belongs_to :polymorphic_man, :polymorphic => true, :inverse_of => :polymorphic_interests
   belongs_to :zine, :inverse_of => :interests
+
+  after_save :check_if_fun
+
+  def check_if_fun
+    zine && zine.validate_i_am_fun
+  end
 end

activerecord/test/models/zine.rb
@@ -1,3 +1,7 @@
 class Zine < ActiveRecord::Base
   has_many :interests, :inverse_of => :zine
+
+  def validate_i_am_fun
+    !interests.to_a.any?{|i| i.topic =~ /boringz/}
+  end
 end