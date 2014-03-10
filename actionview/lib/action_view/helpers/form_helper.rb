b87962f3a853548b0ba08f31167e1fe86fa91dfd
Cakey | Buddy Magsipoc <keikun17@gmail.com>
2014-03-10 20:10:38 +0800

    Fix 'fields_for' doc typo.

--- actionview/lib/action_view/helpers/form_helper.rb
+++ actionview/lib/action_view/helpers/form_helper.rb
@@ -457,7 +457,7 @@ module ActionView
       # doesn't create the form tags themselves. This makes fields_for suitable
       # for specifying additional model objects in the same form.
       #
-      # Although the usage and purpose of +field_for+ is similar to +form_for+'s,
+      # Although the usage and purpose of +fields_for+ is similar to +form_for+'s,
       # its method signature is slightly different. Like +form_for+, it yields
       # a FormBuilder object associated with a particular model object to a block,
       # and within the block allows methods to be called on the builder to
@@ -1268,7 +1268,7 @@ module ActionView
       # doesn't create the form tags themselves. This makes fields_for suitable
       # for specifying additional model objects in the same form.
       #
-      # Although the usage and purpose of +field_for+ is similar to +form_for+'s,
+      # Although the usage and purpose of +fields_for+ is similar to +form_for+'s,
       # its method signature is slightly different. Like +form_for+, it yields
       # a FormBuilder object associated with a particular model object to a block,
       # and within the block allows methods to be called on the builder to
