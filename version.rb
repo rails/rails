26bfdf9f300be2ada831a34b8eb71f55a9a5122f
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-03-11 14:28:49 -0300

    Preparing for 4.0.4.rc1 release

--- version.rb
+++ version.rb
@@ -2,8 +2,8 @@ module Rails
   module VERSION
     MAJOR = 4
     MINOR = 0
-    TINY  = 3
-    PRE   = nil
+    TINY  = 4
+    PRE   = "rc1"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
