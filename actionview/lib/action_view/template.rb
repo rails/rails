c63b18de1865182e027a97ea4186717a71792b81
Łukasz Strzałkowski <lukasz.strzalkowski@gmail.com>
2014-03-12 15:42:21 +0100

    Add variants to Template class

--- actionview/lib/action_view/template.rb
+++ actionview/lib/action_view/template.rb
@@ -97,7 +97,7 @@ module ActionView
 
     extend Template::Handlers
 
-    attr_accessor :locals, :formats, :virtual_path
+    attr_accessor :locals, :formats, :variants, :virtual_path
 
     attr_reader :source, :identifier, :handler, :original_encoding, :updated_at
 
@@ -123,6 +123,7 @@ module ActionView
       @virtual_path      = details[:virtual_path]
       @updated_at        = details[:updated_at] || Time.now
       @formats           = Array(format).map { |f| f.respond_to?(:ref) ? f.ref : f  }
+      @variants          = [details[:variant]]
       @compile_mutex     = Mutex.new
     end
 
