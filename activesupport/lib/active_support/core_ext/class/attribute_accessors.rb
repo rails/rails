178e261a66412e792ea672307f0463dc79e874cc
Jeremy Kemper <jeremykemper@gmail.com>
2014-03-15 06:26:06 -0700

    Remove deprecation on active_support/core_ext/class/attribute_accessors requires. Appropriate to keep this, users don't care that the implementation got unified.

--- activesupport/lib/active_support/core_ext/class/attribute_accessors.rb
+++ activesupport/lib/active_support/core_ext/class/attribute_accessors.rb
@@ -1,6 +1,4 @@
-require 'active_support/deprecation'
+# cattr_* became mattr_* aliases in 7dfbd91b0780fbd6a1dd9bfbc176e10894871d2d,
+# but we keep this around for libraries that directly require it knowing they
+# want cattr_*. No need to deprecate.
 require 'active_support/core_ext/module/attribute_accessors'
-
-ActiveSupport::Deprecation.warn(
-  "The cattr_* method definitions have been moved into active_support/core_ext/module/attribute_accessors. Please require that instead."
-)
