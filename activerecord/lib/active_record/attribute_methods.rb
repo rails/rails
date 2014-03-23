91b7c477c19a18dfd2d5844d1d103220d83a0392
Kris Kemper <kriskemper@gmail.com>
2014-03-23 15:33:39 -0400

    Allow threads entering method_missing in race condition one more chance

--- activerecord/lib/active_record/attribute_methods.rb
+++ activerecord/lib/active_record/attribute_methods.rb
@@ -58,6 +58,7 @@ module ActiveRecord
       def initialize_generated_modules # :nodoc:
         @generated_attribute_methods = Module.new { extend Mutex_m }
         @attribute_methods_generated = false
+        @thread_registry = {}
         include @generated_attribute_methods
       end
 
@@ -79,6 +80,16 @@ module ActiveRecord
         generated_attribute_methods.synchronize do
           super if @attribute_methods_generated
           @attribute_methods_generated = false
+          @thread_registry.clear
+        end
+      end
+
+      def current_thread_retried_method_invocation?
+        if @thread_registry[Thread.current.object_id]
+          return true
+        else
+          @thread_registry[Thread.current.object_id] = true
+          return false
         end
       end
 
@@ -190,7 +201,8 @@ module ActiveRecord
     # If we haven't generated any methods yet, generate them, then
     # see if we've created the method we're looking for.
     def method_missing(method, *args, &block) # :nodoc:
-      if self.class.define_attribute_methods && respond_to_without_attributes?(method)
+      self.class.define_attribute_methods
+      if !self.class.current_thread_retried_method_invocation? && respond_to_without_attributes?(method)
         # make sure to invoke the correct attribute method, as we might have gotten here via a `super`
         # call in a overwritten attribute method
         if attribute_method = self.class.find_generated_attribute_method(method)
