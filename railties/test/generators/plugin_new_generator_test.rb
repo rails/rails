ed4cf0ac5d5b0c5bb239f2ad61ac4ee32ef166ad
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-03-26 22:19:29 -0300

    Fix build with bundler 1.6

--- railties/test/generators/plugin_new_generator_test.rb
+++ railties/test/generators/plugin_new_generator_test.rb
@@ -139,7 +139,7 @@ class PluginNewGeneratorTest < Rails::Generators::TestCase
       assert_match(/bukkits/, contents)
     end
     assert_match(/run  bundle install/, result)
-    assert_match(/Using bukkits \(0\.0\.1\)/, result)
+    assert_match(/Using bukkits \(?0\.0\.1\)?/, result)
     assert_match(/Your bundle is complete/, result)
     assert_equal 1, result.scan("Your bundle is complete").size
   end
