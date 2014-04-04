b058897da8d4421cc10c278f7bfab0e53463a078
Matthew Draper <matthew@trebex.net>
2014-04-04 22:35:29 +1030

    Ensure we supply Kernel#y for 1.9 too
    In 1.9, it doesn't live in its own file, so we'll have to define it
    ourselves.
    
    Check RUBY_VERSION, instead of rescuing the require, because we want
    this to break if `psych/y` moves in a future Ruby release.

--- railties/lib/rails/application.rb
+++ railties/lib/rails/application.rb
@@ -332,6 +332,25 @@ module Rails
       config.helpers_paths
     end
 
+    console do
+      require "pp"
+    end
+
+    console do
+      unless ::Kernel.private_method_defined?(:y)
+        if RUBY_VERSION >= '2.0'
+          require "psych/y"
+        else
+          module ::Kernel
+            def y(*objects)
+              puts ::Psych.dump_stream(*objects)
+            end
+            private :y
+          end
+        end
+      end
+    end
+
   protected
 
     alias :build_middleware_stack :app
