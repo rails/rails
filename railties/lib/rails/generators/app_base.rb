61a8fd5e1b23fc0a0630cd1831abdda0bedd1c73
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-04-04 18:03:00 -0300

    Use sass-rails 4.0.3

--- railties/lib/rails/generators/app_base.rb
+++ railties/lib/rails/generators/app_base.rb
@@ -249,7 +249,7 @@ module Rails
                                     'Use SCSS for stylesheets')
         else
           gems << GemfileEntry.version('sass-rails',
-                                     '~> 4.0.2',
+                                     '~> 4.0.3',
                                      'Use SCSS for stylesheets')
         end
 
