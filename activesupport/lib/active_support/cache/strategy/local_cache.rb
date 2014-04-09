40fec3dab61715943de6b4cbdc7b2b8717fddc07
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-04-09 14:12:21 -0300

    Merge pull request #14667 from lucasmazza/require-per-thread-registry
    Add missing require so requiring `active_support/cache` works again.

--- activesupport/lib/active_support/cache/strategy/local_cache.rb
+++ activesupport/lib/active_support/cache/strategy/local_cache.rb
@@ -1,5 +1,6 @@
 require 'active_support/core_ext/object/duplicable'
 require 'active_support/core_ext/string/inflections'
+require 'active_support/per_thread_registry'
 
 module ActiveSupport
   module Cache
