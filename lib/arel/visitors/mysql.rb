5dceb0e9dc1e1e0abe595194ba1ef9f6112afd05
Aaron Patterson <aaron.patterson@gmail.com>
2014-04-08 19:43:42 -0700

    fixing mysql visitor

--- lib/arel/visitors/mysql.rb
+++ lib/arel/visitors/mysql.rb
@@ -2,25 +2,31 @@ module Arel
   module Visitors
     class MySQL < Arel::Visitors::ToSql
       private
-      def visit_Arel_Nodes_Union o, suppress_parens = false
-        left_result = case o.left
+      def visit_Arel_Nodes_Union o, collector, suppress_parens = false
+        unless suppress_parens
+          collector << "( "
+        end
+
+        collector =   case o.left
                       when Arel::Nodes::Union
-                        visit_Arel_Nodes_Union o.left, true
+                        visit_Arel_Nodes_Union o.left, collector, true
                       else
-                        visit o.left
+                        visit o.left, collector
                       end
 
-        right_result = case o.right
+        collector << " UNION "
+
+        collector =    case o.right
                        when Arel::Nodes::Union
-                         visit_Arel_Nodes_Union o.right, true
+                         visit_Arel_Nodes_Union o.right, collector, true
                        else
-                         visit o.right
+                         visit o.right, collector
                        end
 
         if suppress_parens
-          "#{left_result} UNION #{right_result}"
+          collector
         else
-          "( #{left_result} UNION #{right_result} )"
+          collector << " )"
         end
       end
 
@@ -31,26 +37,43 @@ module Arel
       ###
       # :'(
       # http://dev.mysql.com/doc/refman/5.0/en/select.html#id3482214
-      def visit_Arel_Nodes_SelectStatement o
+      def visit_Arel_Nodes_SelectStatement o, collector
         if o.offset && !o.limit
           o.limit = Arel::Nodes::Limit.new(Nodes.build_quoted(18446744073709551615))
         end
         super
       end
 
-      def visit_Arel_Nodes_SelectCore o
+      def visit_Arel_Nodes_SelectCore o, collector
         o.froms ||= Arel.sql('DUAL')
         super
       end
 
-      def visit_Arel_Nodes_UpdateStatement o
-        [
-          "UPDATE #{visit o.relation}",
-          ("SET #{o.values.map { |value| visit value }.join ', '}" unless o.values.empty?),
-          ("WHERE #{o.wheres.map { |x| visit x }.join ' AND '}" unless o.wheres.empty?),
-          ("ORDER BY #{o.orders.map { |x| visit x }.join(', ')}" unless o.orders.empty?),
-          (visit(o.limit) if o.limit),
-        ].compact.join ' '
+      def visit_Arel_Nodes_UpdateStatement o, collector
+        collector << "UPDATE "
+        collector = visit o.relation, collector
+
+        unless o.values.empty?
+          collector << "SET "
+          collector = inject_join o.values, collector, ', '
+        end
+
+        unless o.wheres.empty?
+          collector << "SET "
+          collector = inject_join o.wheres, collector, ' AND '
+        end
+
+        unless o.orders.empty?
+          collector << "ORDER BY "
+          collector = inject_join o.wheres, collector, ', '
+        end
+
+        if o.limit
+          collector << " "
+          visit(o.limit, collector)
+        else
+          collector
+        end
       end
 
     end
--- test/visitors/test_mysql.rb
+++ test/visitors/test_mysql.rb
@@ -7,14 +7,18 @@ module Arel
         @visitor = MySQL.new Table.engine.connection
       end
 
+      def compile node
+        @visitor.accept(node, Collectors::SQLString.new).value
+      end
+
       it 'squashes parenthesis on multiple unions' do
         subnode = Nodes::Union.new Arel.sql('left'), Arel.sql('right')
         node    = Nodes::Union.new subnode, Arel.sql('topright')
-        assert_equal 1, @visitor.accept(node).scan('(').length
+        assert_equal 1, compile(node).scan('(').length
 
         subnode = Nodes::Union.new Arel.sql('left'), Arel.sql('right')
         node    = Nodes::Union.new Arel.sql('topleft'), subnode
-        assert_equal 1, @visitor.accept(node).scan('(').length
+        assert_equal 1, compile(node).scan('(').length
       end
 
       ###
@@ -23,7 +27,7 @@ module Arel
       it 'defaults limit to 18446744073709551615' do
         stmt = Nodes::SelectStatement.new
         stmt.offset = Nodes::Offset.new(1)
-        sql = @visitor.accept(stmt)
+        sql = compile(stmt)
         sql.must_be_like "SELECT FROM DUAL LIMIT 18446744073709551615 OFFSET 1"
       end
 
@@ -31,24 +35,24 @@ module Arel
         sc = Arel::Nodes::UpdateStatement.new
         sc.relation = Table.new(:users)
         sc.limit = Nodes::Limit.new(Nodes.build_quoted("omg"))
-        assert_equal("UPDATE \"users\" LIMIT 'omg'", @visitor.accept(sc))
+        assert_equal("UPDATE \"users\" LIMIT 'omg'", compile(sc))
       end
 
       it 'uses DUAL for empty from' do
         stmt = Nodes::SelectStatement.new
-        sql = @visitor.accept(stmt)
+        sql = compile(stmt)
         sql.must_be_like "SELECT FROM DUAL"
       end
 
       describe 'locking' do
         it 'defaults to FOR UPDATE when locking' do
           node = Nodes::Lock.new(Arel.sql('FOR UPDATE'))
-          @visitor.accept(node).must_be_like "FOR UPDATE"
+          compile(node).must_be_like "FOR UPDATE"
         end
 
         it 'allows a custom string to be used as a lock' do
           node = Nodes::Lock.new(Arel.sql('LOCK IN SHARE MODE'))
-          @visitor.accept(node).must_be_like "LOCK IN SHARE MODE"
+          compile(node).must_be_like "LOCK IN SHARE MODE"
         end
       end
     end
