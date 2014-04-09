93036b168b2877f2fe314b7fa0e8701878e32602
Aaron Patterson <aaron.patterson@gmail.com>
2014-04-08 17:04:08 -0700

    fix sqlite visitor

--- lib/arel/visitors/sqlite.rb
+++ lib/arel/visitors/sqlite.rb
@@ -4,10 +4,11 @@ module Arel
       private
 
       # Locks are not supported in SQLite
-      def visit_Arel_Nodes_Lock o
+      def visit_Arel_Nodes_Lock o, collector
+        collector
       end
 
-      def visit_Arel_Nodes_SelectStatement o
+      def visit_Arel_Nodes_SelectStatement o, collector
         o.limit = Arel::Nodes::Limit.new(-1) if o.offset && !o.limit
         super
       end
--- test/visitors/test_sqlite.rb
+++ test/visitors/test_sqlite.rb
@@ -10,13 +10,13 @@ module Arel
       it 'defaults limit to -1' do
         stmt = Nodes::SelectStatement.new
         stmt.offset = Nodes::Offset.new(1)
-        sql = @visitor.accept(stmt)
+        sql = @visitor.accept(stmt, Collectors::SQLString.new).value
         sql.must_be_like "SELECT LIMIT -1 OFFSET 1"
       end
 
       it 'does not support locking' do
         node = Nodes::Lock.new(Arel.sql('FOR UPDATE'))
-        @visitor.accept(node).must_be_nil
+        assert_equal '', @visitor.accept(node, Collectors::SQLString.new).value
       end
     end
   end
