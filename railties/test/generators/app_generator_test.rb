f612c2b0526e326eb6c25a7bbf375ac3ac793a3c
Matthew Draper <matthew@trebex.net>
2014-04-11 02:28:35 +0930

    Move assets precompile (and version) to an initializer
    sprockets-rails 2.1 needs the precompile list to be available in all
    environments.

--- railties/test/generators/app_generator_test.rb
+++ railties/test/generators/app_generator_test.rb
@@ -237,6 +237,7 @@ class AppGeneratorTest < Rails::Generators::TestCase
 
   def test_generator_if_skip_sprockets_is_given
     run_generator [destination_root, "--skip-sprockets"]
+    assert_no_file "config/initializers/assets.rb"
     assert_file "config/application.rb" do |content|
       assert_match(/#\s+require\s+["']sprockets\/railtie["']/, content)
     end
@@ -252,7 +253,6 @@ class AppGeneratorTest < Rails::Generators::TestCase
       assert_no_match(/config\.assets\.digest = true/, content)
       assert_no_match(/config\.assets\.js_compressor = :uglifier/, content)
       assert_no_match(/config\.assets\.css_compressor = :sass/, content)
-      assert_no_match(/config\.assets\.version = '1\.0'/, content)
     end
   end
 
