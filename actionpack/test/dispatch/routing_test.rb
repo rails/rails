rails/rails

commit a6b9ea2e21ae51f6dfea8cea757ee0ecbebcd87e

Use nested_scope? not shallow? to determine whether to copy options
The method `shallow?` returns false if the parent resource is a singleton so
we need to check if we're not inside a nested scope before copying the :path
and :as options to their shallow equivalents.

Fixes #14388.

(cherry picked from commit 924345abcff0101e3221f87d1d56d3d6517a79a4)

Conflicts:
	actionpack/CHANGELOG.md

actionpack/lib/action_dispatch/routing/mapper.rb
@@ -697,7 +697,7 @@ module ActionDispatch
           options[:path] = args.flatten.join('/') if args.any?
           options[:constraints] ||= {}
 
-          unless shallow?
+          unless nested_scope?
             options[:shallow_path] ||= options[:path] if options.key?(:path)
             options[:shallow_prefix] ||= options[:as] if options.key?(:as)
           end
@@ -1534,6 +1534,10 @@ module ActionDispatch
 
           def resource_method_scope? #:nodoc:
             RESOURCE_METHOD_SCOPES.include? @scope[:scope_level]
+          end
+
+          def nested_scope? #:nodoc:
+            @scope[:scope_level] == :nested
           end
 
           def with_exclusive_scope

actionpack/test/dispatch/routing_test.rb
@@ -1945,6 +1945,42 @@ class TestRoutingMapper < ActionDispatch::IntegrationTest
     assert_equal '/comments/3/preview', preview_comment_path(:id => '3')
   end
 
+  def test_shallow_nested_resources_inside_resource
+    draw do
+      resource :membership, shallow: true do
+        resources :cards
+      end
+    end
+
+    get '/membership/cards'
+    assert_equal 'cards#index', @response.body
+    assert_equal '/membership/cards', membership_cards_path
+
+    get '/membership/cards/new'
+    assert_equal 'cards#new', @response.body
+    assert_equal '/membership/cards/new', new_membership_card_path
+
+    post '/membership/cards'
+    assert_equal 'cards#create', @response.body
+
+    get '/cards/1'
+    assert_equal 'cards#show', @response.body
+    assert_equal '/cards/1', card_path('1')
+
+    get '/cards/1/edit'
+    assert_equal 'cards#edit', @response.body
+    assert_equal '/cards/1/edit', edit_card_path('1')
+
+    put '/cards/1'
+    assert_equal 'cards#update', @response.body
+
+    patch '/cards/1'
+    assert_equal 'cards#update', @response.body
+
+    delete '/cards/1'
+    assert_equal 'cards#destroy', @response.body
+  end
+
   def test_shallow_nested_resources_within_scope
     draw do
       scope '/hello' do

commit 176ce1ddb7b4ed51a6127d4c520d32a2f4d61741

Merge pull request #14684 from Futurelearn/jc-deep-nested-shallow-url-helper-bug
Add a failing test for a URL helper that was broken by a6b9ea2.

(cherry picked from commit 142448259d7f450df49fdac06bf912edfecaa633)

Conflicts:
	actionpack/CHANGELOG.md

actionpack/lib/action_dispatch/routing/mapper.rb
@@ -991,6 +991,7 @@ module ActionDispatch
             @as         = options[:as]
             @param      = (options[:param] || :id).to_sym
             @options    = options
+            @shallow    = false
           end
 
           def default_actions
@@ -1051,6 +1052,13 @@ module ActionDispatch
             "#{path}/:#{nested_param}"
           end
 
+          def shallow=(value)
+            @shallow = value
+          end
+
+          def shallow?
+            @shallow
+          end
         end
 
         class SingletonResource < Resource #:nodoc:
@@ -1357,7 +1365,7 @@ module ActionDispatch
           end
 
           with_scope_level(:nested) do
-            if shallow? && nesting_depth > 1
+            if shallow? && shallow_nesting_depth > 1
               shallow_scope(parent_resource.nested_scope, nested_options) { yield }
             else
               scope(parent_resource.nested_scope, nested_options) { yield }
@@ -1572,6 +1580,7 @@ module ActionDispatch
           end
 
           def resource_scope(kind, resource) #:nodoc:
+            resource.shallow = @scope[:shallow]
             old_resource, @scope[:scope_level_resource] = @scope[:scope_level_resource], resource
             @nesting.push(resource)
 
@@ -1594,6 +1603,10 @@ module ActionDispatch
 
           def nesting_depth #:nodoc:
             @nesting.size
+          end
+
+          def shallow_nesting_depth #:nodoc:
+            @nesting.select(&:shallow?).size
           end
 
           def param_constraint? #:nodoc:

actionpack/test/dispatch/routing_test.rb
@@ -1994,6 +1994,24 @@ class TestRoutingMapper < ActionDispatch::IntegrationTest
     assert_equal 'cards#destroy', @response.body
   end
 
+  def test_shallow_deeply_nested_resources
+    draw do
+      resources :blogs do
+        resources :posts do
+          resources :comments, shallow: true
+        end
+      end
+    end
+
+    get '/comments/1'
+    assert_equal 'comments#show', @response.body
+
+    assert_equal '/comments/1', comment_path('1')
+    assert_equal '/blogs/new', new_blog_path
+    assert_equal '/blogs/1/posts/new', new_blog_post_path(:blog_id => 1)
+    assert_equal '/blogs/1/posts/2/comments/new', new_blog_post_comment_path(:blog_id => 1, :post_id => 2)
+  end
+
   def test_shallow_nested_resources_within_scope
     draw do
       scope '/hello' do