129205157b231e90ad8612630daeca9214d424d8
ZENATI YASSINE <zenati@informatique.univ-paris-diderot.fr>
2014-04-16 21:49:39 +0200

    [ci skip] Added examples for telephone_field_tag method

--- actionview/lib/action_view/helpers/form_tag_helper.rb
+++ actionview/lib/action_view/helpers/form_tag_helper.rb
@@ -592,6 +592,19 @@ module ActionView
       #
       # ==== Options
       # * Accepts the same options as text_field_tag.
+      #
+      # ==== Examples
+      #   telephone_field_tag 'name'
+      #   # => <input id="name" name="name" type="tel" />
+      #
+      #   telephone_field_tag 'tel', '0123456789'
+      #   # => <input id="tel" name="tel" type="tel" value="0123456789" />
+      #
+      #   telephone_field_tag 'tel', nil, class: 'special_input'
+      #   # => <input class="special_input" id="tel" name="tel" type="tel" />
+      #
+      #   telephone_field_tag 'tel', '0123456789', class: 'special_input', disabled: true
+      #   # => <input disabled="disabled" class="special_input" id="tel" name="tel" type="tel" value="0123456789" />
       def telephone_field_tag(name, value = nil, options = {})
         text_field_tag(name, value, options.stringify_keys.update("type" => "tel"))
       end
