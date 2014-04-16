ae243b1d524807770f24272320c7ceb088b3ad21
Breno Moura <basmoura@gmail.com>
2014-04-16 02:08:30 -0300

    Changes the test methods' declaration

--- activemodel/test/cases/translation_test.rb
+++ activemodel/test/cases/translation_test.rb
@@ -11,49 +11,49 @@ class ActiveModelI18nTests < ActiveModel::TestCase
     I18n.backend.reload!
   end
 
-  def test_translated_model_attributes
+  test "translated model attributes" do
     I18n.backend.store_translations 'en', activemodel: { attributes: { person: { name: 'person name attribute' } } }
     assert_equal 'person name attribute', Person.human_attribute_name('name')
   end
 
-  def test_translated_model_attributes_with_default
+  test "translated model attributes with default" do
     I18n.backend.store_translations 'en', attributes: { name: 'name default attribute' }
     assert_equal 'name default attribute', Person.human_attribute_name('name')
   end
 
-  def test_translated_model_attributes_using_default_option
+  test "translated model attributes using default option" do
     assert_equal 'name default attribute', Person.human_attribute_name('name', default: "name default attribute")
   end
 
-  def test_translated_model_attributes_using_default_option_as_symbol
+  test "translated model attributes using default option as symbol" do
     I18n.backend.store_translations 'en', default_name: 'name default attribute'
     assert_equal 'name default attribute', Person.human_attribute_name('name', default: :default_name)
   end
 
-  def test_translated_model_attributes_falling_back_to_default
+  test "translated model attributes falling back to default" do
     assert_equal 'Name', Person.human_attribute_name('name')
   end
 
-  def test_translated_model_attributes_using_default_option_as_symbol_and_falling_back_to_default
+  test "translated model attributes using default option as symbol and falling back to default" do
     assert_equal 'Name', Person.human_attribute_name('name', default: :default_name)
   end
 
-  def test_translated_model_attributes_with_symbols
+  test "translated model attributes with symbols" do
     I18n.backend.store_translations 'en', activemodel: { attributes: { person: { name: 'person name attribute'} } }
     assert_equal 'person name attribute', Person.human_attribute_name(:name)
   end
 
-  def test_translated_model_attributes_with_ancestor
+  test "translated model attributes with ancestor" do
     I18n.backend.store_translations 'en', activemodel: { attributes: { child: { name: 'child name attribute'} } }
     assert_equal 'child name attribute', Child.human_attribute_name('name')
   end
 
-  def test_translated_model_attributes_with_ancestors_fallback
+  test "translated model attributes with ancestors fallback" do
     I18n.backend.store_translations 'en', activemodel: { attributes: { person: { name: 'person name attribute'} } }
     assert_equal 'person name attribute', Child.human_attribute_name('name')
   end
 
-  def test_translated_model_attributes_with_attribute_matching_namespaced_model_name
+  test "translated model attributes with attribute matching namespaced model name" do
     I18n.backend.store_translations 'en', activemodel: { attributes: {
       person: { gender: 'person gender'},
       :"person/gender" => { attribute: 'person gender attribute' }
@@ -63,43 +63,43 @@ class ActiveModelI18nTests < ActiveModel::TestCase
     assert_equal 'person gender attribute', Person::Gender.human_attribute_name('attribute')
   end
 
-  def test_translated_deeply_nested_model_attributes
+  test "translated deeply nested model attributes" do
     I18n.backend.store_translations 'en', activemodel: { attributes: { :"person/contacts/addresses" => { street: 'Deeply Nested Address Street' } } }
     assert_equal 'Deeply Nested Address Street', Person.human_attribute_name('contacts.addresses.street')
   end
 
-  def test_translated_nested_model_attributes
+  test "translated nested model attributes" do
     I18n.backend.store_translations 'en', activemodel: { attributes: { :"person/addresses" => { street: 'Person Address Street' } } }
     assert_equal 'Person Address Street', Person.human_attribute_name('addresses.street')
   end
 
-  def test_translated_nested_model_attributes_with_namespace_fallback
+  test "translated nested model attributes with namespace fallback" do
     I18n.backend.store_translations 'en', activemodel: { attributes: { addresses: { street: 'Cool Address Street' } } }
     assert_equal 'Cool Address Street', Person.human_attribute_name('addresses.street')
   end
 
-  def test_translated_model_names
+  test "translated model names" do
     I18n.backend.store_translations 'en', activemodel: { models: { person: 'person model' } }
     assert_equal 'person model', Person.model_name.human
   end
 
-  def test_translated_model_names_with_sti
+  test "translated model names with sti" do
     I18n.backend.store_translations 'en', activemodel: { models: { child: 'child model' } }
     assert_equal 'child model', Child.model_name.human
   end
 
-  def test_translated_model_names_with_ancestors_fallback
+  test "translated model names with ancestors fallback" do
     I18n.backend.store_translations 'en', activemodel: { models: { person: 'person model' } }
     assert_equal 'person model', Child.model_name.human
   end
 
-  def test_human_does_not_modify_options
+  test "human does not modify options" do
     options = { default: 'person model' }
     Person.model_name.human(options)
     assert_equal({ default: 'person model' }, options)
   end
 
-  def test_human_attribute_name_does_not_modify_options
+  test "human attribute name does not modify options" do
     options = { default: 'Cool gender' }
     Person.human_attribute_name('gender', options)
     assert_equal({ default: 'Cool gender' }, options)
