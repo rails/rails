c35e438620f2d56562251571377995359546393d
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-04-22 17:20:52 -0500

    Merge pull request #14573 from habermann24/has_many_through_fix
    Properly handle scoping with has_many :through. Fixes #14537.
    
    Conflicts:
    	activerecord/CHANGELOG.md
    	activerecord/test/cases/associations/has_many_through_associations_test.rb
    
    Conflicts:
    	activerecord/CHANGELOG.md

--- activerecord/lib/active_record/associations/through_association.rb
+++ activerecord/lib/active_record/associations/through_association.rb
@@ -14,9 +14,11 @@ module ActiveRecord
         def target_scope
           scope = super
           chain.drop(1).each do |reflection|
+            relation = reflection.klass.all
+            relation.merge!(reflection.scope) if reflection.scope
+
             scope.merge!(
-              reflection.klass.all.
-                except(:select, :create_with, :includes, :preload, :joins, :eager_load)
+              relation.except(:select, :create_with, :includes, :preload, :joins, :eager_load)
             )
           end
           scope
