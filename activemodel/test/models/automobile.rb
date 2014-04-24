b904fb6cf16f54b14d9ab6e9d17b5233ec00cd47
Felix Brix <felix.brix@onlinehome.de>
2014-04-24 14:17:33 +0200

    Update automobile.rb

--- activemodel/test/models/automobile.rb
+++ activemodel/test/models/automobile.rb
@@ -6,8 +6,8 @@ class Automobile
   attr_accessor :make, :model, :approved
 
   def validations
-    validates_presence_of :make
-    validates_length_of   :model, within: 2..10
+    validates_presence_of   :make
+    validates_length_of     :model, within: 2..10
     validates_acceptance_of :approved, allow_nil: false
   end
 end
