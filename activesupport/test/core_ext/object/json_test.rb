f76e27555df9cb86cd20158a1198a132a45f3916
Kuldeep Aggarwal <kd.engineer@yahoo.co.in>
2014-05-04 22:37:55 +0530

    Remove deprecated file(`active_support/core_ext/object/to_json.rb`) and its test case.

--- activesupport/test/core_ext/object/json_test.rb
+++
@@ -1,9 +0,0 @@
-require 'abstract_unit'
-
-class JsonTest < ActiveSupport::TestCase
-  # See activesupport/test/json/encoding_test.rb for JSON encoding tests
-
-  def test_deprecated_require_to_json_rb
-    assert_deprecated { require 'active_support/core_ext/object/to_json' }
-  end
-end
