f49054b88355b7b16cabbbe5c48de42871d399c6
Luciano Costa <luciano@monttez.com>
2014-05-08 20:43:47 -0300

    adding compatibility with utf8mb4 on mysql2 for schema_migrations table

--- activerecord/lib/active_record/connection_adapters/abstract/schema_statements.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/schema_statements.rb
@@ -419,15 +419,24 @@ module ActiveRecord
 
       # Should not be called normally, but this operation is non-destructive.
       # The migrations module handles this automatically.
+      MAX_INDEX_LENGTH_FOR_UTF8MB4 = 191
       def initialize_schema_migrations_table
         sm_table = ActiveRecord::Migrator.schema_migrations_table_name
+        connection_config = ActiveRecord::Base.connection_config
 
         unless table_exists?(sm_table)
           create_table(sm_table, :id => false) do |schema_migrations_table|
             schema_migrations_table.column :version, :string, :null => false
           end
-          add_index sm_table, :version, :unique => true,
+
+          index_options = {
+            :unique => true, 
             :name => "#{Base.table_name_prefix}unique_schema_migrations#{Base.table_name_suffix}"
+          }
+
+          index_options[:length] = MAX_INDEX_LENGTH_FOR_UTF8MB4 if 'mysql2' == connection_config[:adapter] && 'utf8mb4' == connection_config[:encoding]
+
+          add_index sm_table, :version, index_options
 
           # Backwards-compatibility: if we find schema_info, assume we've
           # migrated up to that point:
