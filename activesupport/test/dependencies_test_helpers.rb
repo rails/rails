5b824d1f6ff75eb145dbfd250738dc8aa88248bd
Timm <kaspth@gmail.com>
2014-05-13 21:59:54 +0200

    Refactor with_loading helper method to use new interface.

--- activesupport/test/dependencies_test_helpers.rb
+++ activesupport/test/dependencies_test_helpers.rb
@@ -1,18 +1,17 @@
 module DependenciesTestHelpers
   def with_loading(*from)
-    old_mechanism, ActiveSupport::Dependencies.mechanism = ActiveSupport::Dependencies.mechanism, :load
+    old_mechanism = ENV.delete('NO_RELOAD')
     this_dir = File.dirname(__FILE__)
     parent_dir = File.dirname(this_dir)
     path_copy = $LOAD_PATH.dup
     $LOAD_PATH.unshift(parent_dir) unless $LOAD_PATH.include?(parent_dir)
     prior_autoload_paths = ActiveSupport::Dependencies.autoload_paths
-    ActiveSupport::Dependencies.autoload_paths = from.collect { |f| "#{this_dir}/#{f}" }
+    ActiveSupport::Dependencies.autoload_paths = from.map { |f| File.join(this_dir, f) }
     yield
   ensure
-    $LOAD_PATH.replace(path_copy)
+    $LOAD_PATH.replace(path_copy) if path_copy
     ActiveSupport::Dependencies.autoload_paths = prior_autoload_paths
-    ActiveSupport::Dependencies.mechanism = old_mechanism
-    ActiveSupport::Dependencies.explicitly_unloadable_constants = []
+    ENV['NO_RELOAD'] = old_mechanism
   end
 
   def with_autoloading_fixtures(&block)
