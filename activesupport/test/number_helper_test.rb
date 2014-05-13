04dbecadc2971aafad09f981d441ee7ce590d34b
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-05-13 16:11:40 -0300

    Merge pull request #15072 from mjtko/fix/issue-15064
    [Fixes #15064] Calling number_to_delimited on a ActiveSupport::SafeBuffer results in mangled output
    Conflicts:
    	activesupport/CHANGELOG.md

--- activesupport/test/number_helper_test.rb
+++ activesupport/test/number_helper_test.rb
@@ -1,5 +1,6 @@
 require 'abstract_unit'
 require 'active_support/number_helper'
+require 'active_support/core_ext/string/output_safety'
 
 module ActiveSupport
   module NumberHelper
@@ -97,6 +98,7 @@ module ActiveSupport
           assert_equal("123,456,789.78901", number_helper.number_to_delimited(123456789.78901))
           assert_equal("0.78901", number_helper.number_to_delimited(0.78901))
           assert_equal("123,456.78", number_helper.number_to_delimited("123456.78"))
+          assert_equal("123,456.78", number_helper.number_to_delimited("123456.78".html_safe))
         end
       end
 
