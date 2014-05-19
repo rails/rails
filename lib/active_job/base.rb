fd1e61adfc40275fe23fdcbc7796f7a72bf12fa3
David Heinemeier Hansson <david@loudthinking.com>
2014-05-19 09:47:55 +0200

    Dont need the explicit error handling -- if the require fails, it will raise exactly the error we want to communicate anyway. Also use the load path, so we can allow plugins, rather than requre_relative

--- lib/active_job/base.rb
+++ lib/active_job/base.rb
@@ -1,9 +1,7 @@
-require 'active_job/errors'
 require 'active_job/queue_adapters/inline_adapter'
 require 'active_support/core_ext/string/inflections'
 
 module ActiveJob
-
   class Base
     cattr_accessor(:queue_adapter)   { ActiveJob::QueueAdapters::InlineAdapter }
     cattr_accessor(:queue_base_name) { "active_jobs" }
@@ -19,19 +17,9 @@ module ActiveJob
       end
 
       def adapter=(adapter_name)
-        adapter_name = adapter_name.to_s
-        unless %w(inline resque sidekiq sucker_punch).include?(adapter_name)
-          fail ActiveJob::NotImplementedError
-        end
-
-        begin
-          require_relative "queue_adapters/#{adapter_name}_adapter"
-          ActiveJob::Base.queue_adapter = "ActiveJob::QueueAdapters::#{adapter_name.camelize}Adapter".constantize
-        rescue
-          fail ActiveJob::Error.new("#{adapter_name} is missing")
-        end
+        require "active_job/queue_adapters/#{adapter_name}_adapter"
+        ActiveJob::Base.queue_adapter = "ActiveJob::QueueAdapters::#{adapter_name.to_s.camelize}Adapter".constantize
       end
     end
-
   end
 end
