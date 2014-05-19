6198978fa23e7acc22bc0196db9315a60bea52d0
Larry Lv <larrylv1990@gmail.com>
2014-05-20 01:36:41 +0800

    Make tests for `Person` pass.

--- test/models/person.rb
+++ test/models/person.rb
@@ -2,18 +2,18 @@ require 'active_model/global_identification'
 
 class Person
   include ActiveModel::GlobalIdentification
-  
+
   attr_reader :id
-  
+
   def self.find(id)
     new(id)
   end
-  
+
   def initialize(id)
     @id = id
   end
-  
+
   def ==(other_person)
-    other_person.is_a?(Person) && id == other_person.id
+    other_person.is_a?(Person) && id.to_s == other_person.id.to_s
   end
 end
