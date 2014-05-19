fc80f4e53256235a2b1a40965f4679e5abccc907
Mike Perham <mperham@gmail.com>
2014-05-19 13:13:40 -0700

    RDoc enqueue

--- lib/active_job/enqueuing.rb
+++ lib/active_job/enqueuing.rb
@@ -2,6 +2,14 @@ require 'active_job/parameters'
 
 module ActiveJob
   module Enqueuing
+    ##
+    # Push a job onto the queue.  The arguments must be legal JSON types
+    # (string, int, float, nil, true, false, hash or array) or
+    # ActiveModel::GlobalIdentication instances.  Arbitrary Ruby objects
+    # are not supported.
+    #
+    # The return value is adapter-specific and may change in a future
+    # ActiveJob release.
     def enqueue(*args)
       queue_adapter.queue self, *Parameters.serialize(args)
     end
