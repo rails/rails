fd372e66f0934617bef3b840cd0ea75429a24544
David Heinemeier Hansson <david@loudthinking.com>
2014-05-20 19:09:45 +0200

    Collapse LogSubscriber into Logging

--- lib/active_job/logging.rb
+++ lib/active_job/logging.rb
@@ -1,7 +1,32 @@
-require 'active_job/log_subscriber'
+require 'active_support/core_ext/string/filters'
 
 module ActiveJob
   module Logging
     mattr_accessor(:logger) { ActiveSupport::Logger.new(STDOUT) }
+
+    class LogSubscriber < ActiveSupport::LogSubscriber
+      def enqueue(event)
+        info "Enqueued #{event.payload[:job].name} to #{queue_name(event)}" + args_info(event)
+      end
+
+      def enqueue_at(event)
+        info "Enqueued #{event.payload[:job].name} to #{queue_name(event)} at #{event.payload[:timestamp]}" + args_info(event)
+      end
+
+      private
+        def queue_name(event)
+          event.payload[:adapter].name.demodulize.remove('Adapter')
+        end
+
+        def args_info(event)
+          event.payload[:args].any? ? ": #{event.payload[:args].inspect}" : ""
+        end
+
+        def logger
+          ActiveJob::Base.logger
+        end
+    end
   end
 end
+
+ActiveJob::Logging::LogSubscriber.attach_to :active_job
