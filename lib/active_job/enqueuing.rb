ccd1c21fe52a8183dc4470f08dd9c85f4b53afec
David Heinemeier Hansson <david@loudthinking.com>
2014-05-20 18:14:01 +0200

    Remove stray ##

--- lib/active_job/enqueuing.rb
+++ lib/active_job/enqueuing.rb
@@ -2,7 +2,6 @@ require 'active_job/parameters'
 
 module ActiveJob
   module Enqueuing
-    ##
     # Push a job onto the queue.  The arguments must be legal JSON types
     # (string, int, float, nil, true, false, hash or array) or
     # ActiveModel::GlobalIdentication instances.  Arbitrary Ruby objects
