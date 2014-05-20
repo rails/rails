bb39fffc5556093243e5b4df5c95bb2df6d6b72a
Yves Senn <yves.senn@gmail.com>
2014-05-20 10:33:50 +0200

    docs, `instantiate` expects `String` keys. [Rafal Piekarski & Yves Senn]
    Closes #15122
    Closes #15107
    
    Conflicts:
    	activerecord/test/cases/persistence_test.rb

--- activerecord/test/cases/persistence_test.rb
+++ activerecord/test/cases/persistence_test.rb
@@ -802,4 +802,14 @@ class PersistencesTest < ActiveRecord::TestCase
     end
   end
 
+  def test_instantiate_creates_a_new_instance
+    post = Post.instantiate("title" => "appropriate documentation", "type" => "SpecialPost")
+    assert_equal "appropriate documentation", post.title
+    assert_instance_of SpecialPost, post
+
+    # body was not initialized
+    assert_raises ActiveModel::MissingAttributeError do
+      post.body
+    end
+  end
 end
