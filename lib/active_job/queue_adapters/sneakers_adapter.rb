cbfc8b367ed04fdbb748d0712c33b0b62d3de331
Douwe Maan <douwe@selenight.nl>
2014-05-20 11:32:29 +0200

    Have Sneakers adapter take queue_name into account.

--- lib/active_job/queue_adapters/sneakers_adapter.rb
+++ lib/active_job/queue_adapters/sneakers_adapter.rb
@@ -1,18 +1,22 @@
 require 'sneakers'
+require 'thread'
 
 module ActiveJob
   module QueueAdapters
     class SneakersAdapter
+      @mutex = Mutex.new
+        
       class << self
         def queue(job, *args)
-          JobWrapper.enqueue([job, *args])
+          @mutex.synchronize do
+            JobWrapper.from_queue job.queue_name
+            JobWrapper.enqueue [ job, *args ]
+          end
         end
       end
 
       class JobWrapper
         include Sneakers::Worker
-
-        self.from_queue("queue", {})
 
         def work(job, *args)
           job.new.perform *Parameters.deserialize(args)
