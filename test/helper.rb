1513705b9d8d1d77d84ef56cf7b14ba33b8ecfa1
David Heinemeier Hansson <david@loudthinking.com>
2014-05-21 15:50:29 +0200

    Show which adapter the tests are currently run for

--- test/helper.rb
+++ test/helper.rb
@@ -6,6 +6,8 @@ $LOAD_PATH << File.dirname(__FILE__) + "/../lib"
 require 'active_job'
 require "adapters/#{ENV['AJADAPTER'] || 'inline'}"
 
+puts "Testing using #{ENV['AJADAPTER'] || 'inline'}"
 require 'active_support/testing/autorun'
+
 
 ActiveJob::Base.logger.level = Logger::ERROR
