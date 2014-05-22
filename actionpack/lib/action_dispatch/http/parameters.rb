8d8ebe3db529c30486bb4d2310c9ea158b49a459
Aaron Patterson <aaron.patterson@gmail.com>
2014-05-22 15:37:08 -0700

    just return path parameters

--- actionpack/lib/action_dispatch/http/parameters.rb
+++ actionpack/lib/action_dispatch/http/parameters.rb
@@ -24,14 +24,13 @@ module ActionDispatch
       alias :params :parameters
 
       def path_parameters=(parameters) #:nodoc:
-        @symbolized_path_params = nil
         @env.delete('action_dispatch.request.parameters')
         @env[Routing::RouteSet::PARAMETERS_KEY] = parameters
       end
 
       # The same as <tt>path_parameters</tt> with explicitly symbolized keys.
       def symbolized_path_parameters
-        @symbolized_path_params ||= path_parameters.symbolize_keys
+        path_parameters
       end
 
       # Returns a hash with the \parameters used to form the \path of the request.
