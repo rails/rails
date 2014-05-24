36e3ca6cc526d867613b8dd76026bf2b2c5dd4a4
Xavier Noria <fxn@hashref.com>
2014-05-25 00:43:31 +0200

    integration tests do not need to load fixtures by hand
    I don't know when this changed, checked back up to 3.2 and integration
    tests instantiate fixtures just fine.

--- guides/code/getting_started/test/test_helper.rb
+++ guides/code/getting_started/test/test_helper.rb
@@ -6,9 +6,6 @@ class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
