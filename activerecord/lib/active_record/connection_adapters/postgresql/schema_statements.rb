b9c43e0ee9beda5d1587e168695afa80186f8c22
Kuldeep Aggarwal <kd.engineer@yahoo.co.in>
2014-05-26 22:57:32 +0530

    remove unwanted `to_sym` call.

--- activerecord/lib/active_record/connection_adapters/postgresql/schema_statements.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/schema_statements.rb
@@ -5,7 +5,7 @@ module ActiveRecord
         private
 
         def visit_AddColumn(o)
-          sql_type = type_to_sql(o.type.to_sym, o.limit, o.precision, o.scale)
+          sql_type = type_to_sql(o.type, o.limit, o.precision, o.scale)
           sql = "ADD COLUMN #{quote_column_name(o.name)} #{sql_type}"
           add_column_options!(sql, column_options(o))
         end
