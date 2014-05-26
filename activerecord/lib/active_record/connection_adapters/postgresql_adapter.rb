b952ca369212898058f6e789ed27fe97d3a5bbf4
Yves Senn <yves.senn@gmail.com>
2014-05-26 10:27:53 +0200

    pg, remove unused code. Use `extract_schema_and_table` instead.

--- activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
@@ -760,16 +760,6 @@ module ActiveRecord
           end_sql
         end
 
-        def extract_pg_identifier_from_name(name) # :nodoc:
-          match_data = name.start_with?('"') ? name.match(/\"([^\"]+)\"/) : name.match(/([^\.]+)/)
-
-          if match_data
-            rest = name[match_data[0].length, name.length]
-            rest = rest[1, rest.length] if rest.start_with? "."
-            [match_data[1], (rest.length > 0 ? rest : nil)]
-          end
-        end
-
         def extract_table_ref_from_insert_sql(sql) # :nodoc:
           sql[/into\s+([^\(]*).*values\s*\(/im]
           $1.strip if $1
