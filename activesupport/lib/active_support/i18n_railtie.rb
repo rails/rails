0bef6ff9af6be8d0def825a09188697526f7dae2
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-05-27 14:39:24 -0300

    Require active_support at the railties
    Railties need to be used without having to require any dependecy first.

--- activesupport/lib/active_support/i18n_railtie.rb
+++ activesupport/lib/active_support/i18n_railtie.rb
@@ -1,3 +1,4 @@
+require "active_support"
 require "active_support/file_update_checker"
 require "active_support/core_ext/array/wrap"
 
--- activesupport/lib/active_support/railtie.rb
+++ activesupport/lib/active_support/railtie.rb
@@ -1,3 +1,4 @@
+require "active_support"
 require "active_support/i18n_railtie"
 
 module ActiveSupport
