8147fa61c5a98addaf6481ece10cdf38493625c1
Timm <kaspth@gmail.com>
2014-05-28 11:20:15 +0200

    Use 1.9 syntax.

--- actionmailer/test/assert_select_email_test.rb
+++ actionmailer/test/assert_select_email_test.rb
@@ -8,16 +8,16 @@ class AssertSelectEmailTest < ActionMailer::TestCase
 
   class AssertSelectMailer < ActionMailer::Base
     def test(html)
-      mail :body => html, :content_type => "text/html",
-        :subject => "Test e-mail", :from => "test@test.host", :to => "test <test@test.host>"
+      mail body: html, content_type: "text/html",
+        subject: "Test e-mail", from: "test@test.host", to: "test <test@test.host>"
     end
   end
 
   class AssertMultipartSelectMailer < ActionMailer::Base
     def test(options)
-      mail :subject => "Test e-mail", :from => "test@test.host", :to => "test <test@test.host>" do |format|
-        format.text { render :text => options[:text] }
-        format.html { render :text => options[:html] }
+      mail subject: "Test e-mail", from: "test@test.host", to: "test <test@test.host>" do |format|
+        format.text { render text: options[:text] }
+        format.html { render text: options[:html] }
       end
     end
   end
@@ -42,7 +42,7 @@ class AssertSelectEmailTest < ActionMailer::TestCase
   end
 
   def test_assert_select_email_multipart
-    AssertMultipartSelectMailer.test(:html => "<div><p>foo</p><p>bar</p></div>", :text => 'foo bar').deliver
+    AssertMultipartSelectMailer.test(html: "<div><p>foo</p><p>bar</p></div>", text: 'foo bar').deliver
     assert_select_email do
       assert_select "div:root" do
         assert_select "p:first-child", "foo"
