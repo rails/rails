8d3be503a95104247f9e9acc19a40611d571cdce
Leonardo D. Schlossmacher <e@leods92.com>
2014-06-04 13:46:23 -0300

    Fixed restriction error attribute translation.

--- activerecord/lib/active_record/associations/has_many_association.rb
+++ activerecord/lib/active_record/associations/has_many_association.rb
@@ -14,7 +14,7 @@ module ActiveRecord
 
         when :restrict_with_error
           unless empty?
-            record = klass.human_attribute_name(reflection.name).downcase
+            record = reflection.active_record.human_attribute_name(reflection.name).downcase
             owner.errors.add(:base, :"restrict_dependent_destroy.many", record: record)
             false
           end
--- activerecord/lib/active_record/associations/has_one_association.rb
+++ activerecord/lib/active_record/associations/has_one_association.rb
@@ -11,7 +11,7 @@ module ActiveRecord
 
         when :restrict_with_error
           if load_target
-            record = klass.human_attribute_name(reflection.name).downcase
+            record = reflection.active_record.human_attribute_name(reflection.name).downcase
             owner.errors.add(:base, :"restrict_dependent_destroy.one", record: record)
             false
           end
