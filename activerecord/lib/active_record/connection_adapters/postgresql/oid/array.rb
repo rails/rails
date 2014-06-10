b5aa210bf75eacb86d423f677991aef1468cb3c6
Sean Griffin <sean@thoughtbot.com>
2014-06-10 08:01:39 -0600

    Inline PG array type casting helper

--- activerecord/lib/active_record/connection_adapters/postgresql/oid/array.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/oid/array.rb
@@ -10,11 +10,33 @@ module ActiveRecord
             @subtype = subtype
           end
 
-          def type_cast(value)
-            if ::String === value
-              ConnectionAdapters::PostgreSQLColumn.string_to_array value, @subtype
+          def type_cast_from_database(value)
+            if value.is_a?(::String)
+              type_cast_array(parse_pg_array(value))
             else
-              value
+              super
+            end
+          end
+
+          # Loads pg_array_parser if available. String parsing can be
+          # performed quicker by a native extension, which will not create
+          # a large amount of Ruby objects that will need to be garbage
+          # collected. pg_array_parser has a C and Java extension
+          begin
+            require 'pg_array_parser'
+            include PgArrayParser
+          rescue LoadError
+            require 'active_record/connection_adapters/postgresql/array_parser'
+            include PostgreSQL::ArrayParser
+          end
+
+          private
+
+          def type_cast_array(value)
+            if value.is_a?(::Array)
+              value.map { |item| type_cast_array(item) }
+            else
+              @subtype.type_cast_from_database(value)
             end
           end
         end
