30b76e5575c57056361741ebbede972f8ef477de
Nat Budin <nbudin@patientslikeme.com>
2014-06-17 10:11:22 -0700

    Don't include inheritance column in the through_scope_attributes

--- activerecord/test/models/club.rb
+++ activerecord/test/models/club.rb
@@ -5,7 +5,7 @@ class Club < ActiveRecord::Base
   has_one :sponsor
   has_one :sponsored_member, :through => :sponsor, :source => :sponsorable, :source_type => "Member"
   belongs_to :category
-
+  
   has_many :favourites, -> { where(memberships: { favourite: true }) }, through: :memberships, source: :member
 
   private
@@ -13,4 +13,11 @@ class Club < ActiveRecord::Base
   def private_method
     "I'm sorry sir, this is a *private* club, not a *pirate* club"
   end
+end
+
+class SuperClub < ActiveRecord::Base
+  self.table_name = "clubs"
+  
+  has_many :memberships, :class_name => "SuperMembership", :foreign_key => "club_id"
+  has_many :members, :through => :memberships
 end
