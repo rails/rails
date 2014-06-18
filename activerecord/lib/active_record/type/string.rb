c7802dccba69517083000261477f7e1d410e1cca
Sean Griffin <sean@thoughtbot.com>
2014-06-17 18:51:38 -0600

    Detect in-place modifications on Strings

--- activerecord/lib/active_record/type/string.rb
+++ activerecord/lib/active_record/type/string.rb
@@ -9,13 +9,28 @@ module ActiveRecord
         true
       end
 
+      def changed_in_place?(raw_old_value, new_value)
+        if new_value.is_a?(::String)
+          raw_old_value != new_value
+        end
+      end
+
+      def type_cast_for_database(value)
+        if value.is_a?(::String)
+          ::String.new(value)
+        else
+          super
+        end
+      end
+
       private
 
       def cast_value(value)
         case value
         when true then "1"
         when false then "0"
-        else value.to_s
+        # String.new is slightly faster than dup
+        else ::String.new(value.to_s)
         end
       end
     end
