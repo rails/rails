40e33ff9fc81f919b143576180b9202794a37e91
Sean Griffin <sean@thoughtbot.com>
2014-06-24 06:43:03 -0600

    Silence warning in test
    We still had one file using `column_for_attribute` when it could return
    nil, causing deprecation warnings in the tests.

--- activerecord/lib/active_record/attribute_methods/time_zone_conversion.rb
+++ activerecord/lib/active_record/attribute_methods/time_zone_conversion.rb
@@ -42,10 +42,10 @@ module ActiveRecord
 
       module ClassMethods
         private
-        def create_time_zone_conversion_attribute?(name, column)
+        def create_time_zone_conversion_attribute?(name, cast_type)
           time_zone_aware_attributes &&
             !self.skip_time_zone_conversion_for_attributes.include?(name.to_sym) &&
-            (:datetime == column.type)
+            (:datetime == cast_type.type)
         end
       end
     end
