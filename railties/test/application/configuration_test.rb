297bff7f8f01fbda2a6bacaed4afb3d060292b9f
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-06-26 18:37:48 -0300

    Make sure Active Support configurations are applied correctly
    Before this patch configuration set using config.active_support
    would not be set.
    
    Closes #15364

--- railties/test/application/configuration_test.rb
+++ railties/test/application/configuration_test.rb
@@ -454,6 +454,18 @@ module ApplicationTests
       assert ActionView::Resolver.caching?
     end
 
+    test "configure Active Support using config.active_support" do
+      add_to_config <<-RUBY
+        config.active_support.escape_html_entities_in_json = true
+      RUBY
+
+      require 'active_support/json'
+      require "#{app_path}/config/environment"
+
+      assert ActiveSupport.escape_html_entities_in_json
+      assert ActiveSupport::JSON::Encoding.escape_html_entities_in_json
+    end
+
     test "config.action_dispatch.show_exceptions is sent in env" do
       make_basic_app do |app|
         app.config.action_dispatch.show_exceptions = true
