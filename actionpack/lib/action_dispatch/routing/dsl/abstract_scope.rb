cc0f98a6f422b6948d805d18753afa78ef3040a2
Ujjwal Thaakar <ujjwalthaakar@gmail.com>
2014-07-01 07:22:52 +0530

    Implemented initialization of Abstract Scope We set the options and then set all instance variables
    Added a nil refinement so that the system do not break with a nil parent

--- actionpack/lib/action_dispatch/routing/dsl/abstract_scope.rb
+++ actionpack/lib/action_dispatch/routing/dsl/abstract_scope.rb
@@ -1,3 +1,7 @@
+require 'active_support/core_ext/array/extract_options'
+require 'action_dispatch/routing/dsl/abstract_scope/nil_refinement'
+require 'action_dispatch/routing/dsl/abstract_scope/normalization'
+
 module DSL
   class AbstractScope
     # Constants
@@ -10,7 +14,40 @@ module DSL
     # Accessors
     # =========
     attr_accessor :parent
-    attr_reader :controller, :action
+    attr_reader :controller, :action, :set, :concerns
+
+    def initialize(parent, *args)
+      @parent, @set, @concerns = parent, parent.set, parent.concerns
+
+      # Extract options out of the variable arguments
+      options = args.extract_options!.dup
+
+      options[:path] = args.flatten.join('/') if args.any?
+      options[:constraints] ||= {}
+
+      if options[:constraints].is_a?(Hash)
+        defaults = options[:constraints].select do
+          |k, v| URL_OPTIONS.include?(k) && (v.is_a?(String) || v.is_a?(Fixnum))
+        end
+
+        (options[:defaults] ||= {}).reverse_merge!(defaults)
+      else
+        block, options[:constraints] = options[:constraints], {}
+      end
+
+      SCOPE_OPTIONS.each do |option|
+        if option == :blocks
+          value = block
+        elsif option == :options
+          value = options
+        else
+          value = options.delete(option)
+        end
+
+        # Set instance variables
+        instance_variable_set(:"@_#{option}", value) if value
+      end
+    end
 
     def path
       merge_with_slash(parent.path, @_path)
@@ -60,7 +97,7 @@ module DSL
 
     protected
       def merge_with_slash(parent, child)
-        Mapper.normalize_path("#{parent}/#{child}")
+        normalize_path("#{parent}/#{child}")
       end
 
       def merge_with_underscore(parent, child)
---
+++ actionpack/lib/action_dispatch/routing/dsl/abstract_scope/normalization.rb
@@ -0,0 +1,17 @@
+module DSL
+  class AbstractScope
+    protected
+    # Invokes Journey::Router::Utils.normalize_path and ensure that
+    # (:locale) becomes (/:locale) instead of /(:locale). Except
+    # for root cases, where the latter is the correct one.
+    def normalize_path(path)
+      path = Journey::Router::Utils.normalize_path(path)
+      path.gsub!(%r{/(\(+)/?}, '\1/') unless path =~ %r{^/\(+[^)]+\)$}
+      path
+    end
+
+    def normalize_name(name)
+      normalize_path(name)[1..-1].tr("/", "_")
+    end
+  end
+end
