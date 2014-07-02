664feb2d002501bd4a6db42f75b2221d4b9410cc
Sean Griffin <sean@thoughtbot.com>
2014-07-02 11:55:52 -0600

    Quote range strings when quoting PG ranges
    The test case for CVE-2014-3483 doesn't actually send the generated SQL
    to the database. The generated SQL is actually invalid for real inputs.

--- activerecord/test/cases/adapters/postgresql/range_test.rb
+++ activerecord/test/cases/adapters/postgresql/range_test.rb
@@ -262,6 +262,23 @@ _SQL
       assert_raises(ArgumentError) { PostgresqlRange.create!(float_range: "(0.5, 0.7]") }
     end
 
+    def test_update_all_with_ranges
+      PostgresqlRange.create!
+
+      PostgresqlRange.update_all(int8_range: 1..100)
+
+      assert_equal 1...101, PostgresqlRange.first.int8_range
+    end
+
+    def test_ranges_correctly_escape_input
+      e = assert_raises(ActiveRecord::StatementInvalid) do
+        range = "1,2]'; SELECT * FROM users; --".."a"
+        PostgresqlRange.update_all(int8_range: range)
+      end
+
+      assert e.message.starts_with?("PG::InvalidTextRepresentation")
+    end
+
     private
       def assert_equal_round_trip(range, attribute, value)
         round_trip(range, attribute, value)
