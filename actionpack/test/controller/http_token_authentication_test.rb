9bc0afffeaa277a29e70f5de8543f65d405bfe08
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-07-04 14:02:21 -0300

    Merge pull request #16011 from xjlu/token_and_options
    Improve token_and_options regex and test

--- actionpack/test/controller/http_token_authentication_test.rb
+++ actionpack/test/controller/http_token_authentication_test.rb
@@ -139,16 +139,36 @@ class HttpTokenAuthenticationTest < ActionController::TestCase
     assert_equal(expected, actual)
   end
 
+  test "token_and_options returns correct token with nounce option" do
+    token = "rcHu+HzSFw89Ypyhn/896A="
+    nonce_hash = {nonce: "123abc"}
+    actual = ActionController::HttpAuthentication::Token.token_and_options(sample_request(token, nonce_hash))
+    expected_token = token
+    expected_nonce = {"nonce" => nonce_hash[:nonce]}
+    assert_equal(expected_token, actual.first)
+    assert_equal(expected_nonce, actual.last)
+  end
+
   test "token_and_options returns nil with no value after the equal sign" do
     actual = ActionController::HttpAuthentication::Token.token_and_options(malformed_request).first
     expected = nil
     assert_equal(expected, actual)
   end
 
+  test "raw_params returns a tuple of two key value pair strings" do
+    auth = sample_request("rcHu+HzSFw89Ypyhn/896A=").authorization.to_s
+    actual = ActionController::HttpAuthentication::Token.raw_params(auth)
+    expected = ["token=\"rcHu+HzSFw89Ypyhn/896A=\"", "nonce=\"def\""]
+    assert_equal(expected, actual)
+  end
+
   private
 
-    def sample_request(token)
-      @sample_request ||= OpenStruct.new authorization: %{Token token="#{token}", nonce="def"}
+    def sample_request(token, options = {nonce: "def"})
+      authorization = options.inject([%{Token token="#{token}"}]) do |arr, (k, v)|
+        arr << "#{k}=\"#{v}\""
+      end.join(", ")
+      @sample_request ||= OpenStruct.new authorization: authorization
     end
 
     def malformed_request
