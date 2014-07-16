7dc0f3fc8d3160db3408ef5a20c43a7268c0cd8e
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-07-16 14:38:07 -0300

    Document the PartialIteration object

--- actionview/lib/action_view/renderer/partial_renderer.rb
+++ actionview/lib/action_view/renderer/partial_renderer.rb
@@ -1,23 +1,29 @@
 require 'thread_safe'
 
 module ActionView
-  class PartialIteration # :nodoc:
-    attr_reader :size, :index
+  class PartialIteration
+    # The number of iterations that will be done by the partial.
+    attr_reader :size
+
+    # The current iteration of the partial.
+    attr_reader :index
 
     def initialize(size)
       @size  = size
       @index = 0
     end
 
+    # Check if this is the first iteration of the partial.
     def first?
       index == 0
     end
 
+    # Check if this is the last iteration of the partial.
     def last?
       index == size - 1
     end
 
-    def iterate!
+    def iterate! # :nodoc:
       @index += 1
     end
   end
