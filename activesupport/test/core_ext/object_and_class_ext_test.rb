4b2eb17bb2948b6d5784a6d7feb120bb10e87db6
Akshay Vishnoi <akshay.vishnoi@vinsol.com>
2014-07-21 15:46:53 +0530

    Remove not-required file class/subclasses

--- activesupport/test/core_ext/object_and_class_ext_test.rb
+++ activesupport/test/core_ext/object_and_class_ext_test.rb
@@ -1,7 +1,6 @@
 require 'abstract_unit'
 require 'active_support/time'
 require 'active_support/core_ext/object'
-require 'active_support/core_ext/class/subclasses'
 
 class ObjectTests < ActiveSupport::TestCase
   class DuckTime
