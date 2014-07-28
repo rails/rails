a09f0257580c240fc91347d60725cc5ac0d908c7
Yves Senn <yves.senn@gmail.com>
2014-07-28 17:30:43 +0200

    docs, clarify attribute query methods on numeric columns. Closes #16246.
    [ci skip]

--- activerecord/lib/active_record/base.rb
+++ activerecord/lib/active_record/base.rb
@@ -141,6 +141,7 @@ module ActiveRecord #:nodoc:
   #
   # In addition to the basic accessors, query methods are also automatically available on the Active Record object.
   # Query methods allow you to test whether an attribute value is present.
+  # For numeric values, +persent+ is defined as non-zero.
   #
   # For example, an Active Record User with the <tt>name</tt> attribute has a <tt>name?</tt> method that you can call
   # to determine whether the user has a name:
