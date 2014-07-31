bfc2b23128fb6157d9bdaa2d2e957162fc4090db
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2014-07-30 23:09:17 -0300

    Simplify path setup

--- railties/test/generators/generators_test_helper.rb
+++ railties/test/generators/generators_test_helper.rb
@@ -7,7 +7,7 @@ module Rails
   class << self
     remove_possible_method :root
     def root
-      @root ||= File.expand_path(File.join(File.dirname(__FILE__), '..', 'fixtures'))
+      @root ||= File.expand_path('../../fixtures', __FILE__)
     end
   end
 end
