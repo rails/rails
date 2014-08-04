7562c76587eac7149c037d3648c2fa5616f80ff3
Jon Atack <jon@atack.com>
2014-08-05 00:05:23 +0200

    Update itself.rb [ci skip]

--- activesupport/lib/active_support/core_ext/object/itself.rb
+++ activesupport/lib/active_support/core_ext/object/itself.rb
@@ -1,5 +1,5 @@
 class Object
-  unless respond_to?(:itself) # TODO: Remove this file when we drop support to Ruby < 2.2
+  unless respond_to?(:itself) # TODO: Remove this file when we drop support for Ruby < 2.2
     # Returns the object itself. Useful when dealing with a chaining scenario, like Active Record scopes:
     #
     #   Event.public_send(state.presence_in?([ :trashed, :drafted ]) || :itself).order(:created_at)
