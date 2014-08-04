23f902c05fa51a29a26e7afce1db2de475e1c8d6
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-08-04 16:23:22 -0300

    Only define Objetc#itself when it is not defined
    Ruby 2.2 will include Kernel#itself so we don't need to define again.
    
    See
    https://github.com/ruby/ruby/commit/0a0160d6b659f6131a525fe1579e7c463d4c197e

--- activesupport/lib/active_support/core_ext/object/itself.rb
+++ activesupport/lib/active_support/core_ext/object/itself.rb
@@ -1,10 +1,12 @@
 class Object
-  # Returns the object itself. Useful when dealing with a chaining scenario, like Active Record scopes:
-  #
-  #   Event.public_send(state.presence_in?([ :trashed, :drafted ]) || :itself).order(:created_at)
-  #   
-  # @return Object
-  def itself
-    self
+  unless respond_to?(:itself) # TODO: Remove this file when we drop support to Ruby < 2.2
+    # Returns the object itself. Useful when dealing with a chaining scenario, like Active Record scopes:
+    #
+    #   Event.public_send(state.presence_in?([ :trashed, :drafted ]) || :itself).order(:created_at)
+    #
+    # @return Object
+    def itself
+      self
+    end
   end
 end
