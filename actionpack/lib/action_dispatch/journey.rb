8a200f3612386e3d4d3241900befc40b723181c8
Jack Danger Canty <github@jackcanty.com>
2014-08-04 22:02:21 -0700

    Adding initial documentation to Journey

--- actionpack/lib/action_dispatch/journey.rb
+++ actionpack/lib/action_dispatch/journey.rb
@@ -3,3 +3,12 @@ require 'action_dispatch/journey/gtg/builder'
 require 'action_dispatch/journey/gtg/simulator'
 require 'action_dispatch/journey/nfa/builder'
 require 'action_dispatch/journey/nfa/simulator'
+# Journey is a router that uses a state machine to match HTTP requests to
+# defined routes much like a computer language parses source code. Journey defines
+# routes in a Generalized Transition Graph which is a nondeterministic finite
+# state machine that allows moving from state to state by matching regular
+# expressions.
+# This is a fancy way of saying that instead of matching an incoming route to a
+# long list of complete regular expressions representing each possible route it
+# breaks the incoming request attributes into pieces and quickly narrows down
+# the possible matching routes by following a map of possibilities (the GTG)
--- actionpack/lib/action_dispatch/journey/nodes/node.rb
+++ actionpack/lib/action_dispatch/journey/nodes/node.rb
@@ -1,3 +1,123 @@
+# The Journey::Node objects comprise the abstract syntax tree (AST) used by
+# Journey for route recognition.
+#
+# Given the following set of two routes
+#
+#   get ':action', to: SomeController
+#   get 'users/:id', to: 'users#show'
+#
+# To transition between these states we use an abstract syntax tree made up of
+# instances of subclasses of Journey::Nodes::Node. Nodes have "left" and
+# "right" parts, with the left representing some content of the node itself and
+# the right representing a further expanstion of the syntax tree. The above
+# sample route definition produces an AST that has a `Nodes::Or` at the root
+# with two `Nodes::Cat` children under it, each reflecting one of the route
+# definitions.
+#
+#   Or
+#     Cat
+#       left:
+#         Slash
+#           left: "/"
+#       right:
+#         Cat
+#           left:
+#             Symbol:
+#               left:   ":action"
+#               regexp: /[^\.\/\?]+/
+#           right:
+#             Group
+#               left:
+#                 Cat
+#                   left:
+#                     Dot
+#                       left: "."
+#               right:
+#                 Symbol
+#                   left    ":format"
+#                   regexp: /[^\.\/\?]+/
+#     Cat
+#       left:
+#         Slash
+#           left: "/"
+#       right:
+#         Cat
+#           left:
+#             Literal:
+#               left: "users"
+#           right:
+#             Cat
+#               left:
+#                 Slash
+#                   left: "/"
+#               Cat
+#                 left:
+#                   Symbol
+#                     left    ":id"
+#                     regexp: /[^\.\/\?]+/
+#                 right:
+#                   Group
+#                     left:
+#                       Cat
+#                         left:
+#                           Dot
+#                             left: "."
+#                     right:
+#                       Symbol
+#                         left    ":format"
+#                         regexp: /[^\.\/\?]+/
+#
+# From this AST Journey will generate a General Transition Graph in an instance
+# of Journey::GTG::TransitionTable. A General Transition Graph is a fancy way
+# of saying a state machine where moving from state to state involves regular
+# expressions.
+#
+# The sample routeset about results in this transition table:
+#
+#   @accepting = {  # an index of which states (by number) are valid final
+#     2 => true,    # states. If, after processing the whole AST through these
+#     6 => true,    # states the final state is not one of these numbers then
+#     7 => true,    # the route recognition fails.
+#     9 => true
+#   },
+#   @memos = {
+#     2 => [<#Route>, <#Route>], # lists of routes that match specific state numbers
+#     6 => [<#Route>, <#Route>],
+#     7 => [<#Route>, <#Route>],
+#     9 => [<#Route>, <#Route>],
+#   }
+#   @regexp_states = { # These regular expressions constrain moving from state to state
+#     1 => {
+#       /[^\.\/\?]+/ => 2
+#     },
+#     4 => {
+#       /[^\.\/\?]+/ => 6
+#     },
+#     5 => {
+#       /[^\.\/\?]+/ => 7
+#     },
+#     8 => {
+#       /[^\.\/\?]+/ => 9
+#     }
+#   },
+#   @string_states = { # similar to @regexp_states these strings allow calculation to
+#     0 => {           # proceed from state to state.
+#       "/" => 1
+#     },
+#     1 => {
+#       "users" => 3   # Note that if the path matches "users" while in state number
+#     },               # 1 we to state number 3 but 3 is not an 'accepting' state. This
+#     2 => {           # matches our intuition that a "users/:id" pattern can't be
+#       "." => 4       # satisfied by "users", we need to keep processing the string
+#     },               # and extract an ":id" symbol to be accepted as a valid route.
+#     3 => {
+#       "/" => 5       # This will move us from state 3 ("users") to state 5 ("users/").
+#     },               # Then @regexp_states[5] matches any valid :id and will move us to
+#     7 => {           # state 7 which is a valid acceptable state.
+#       "." => 8
+#     }
+#   }
+#
 require 'action_dispatch/journey/visitors'
 
 module ActionDispatch
