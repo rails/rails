9456990b14ea77f3d489ad6b6a65af96f3cda9ef
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-08-06 21:17:19 -0300

    Check attributes passed to create_with and where
    If the request parameters are passed to create_with and where they can
    be used to do mass assignment when used in combination with
    Relation#create.
    
    Fixes CVE-2014-3514

--- activerecord/test/cases/forbidden_attributes_protection_test.rb
+++ activerecord/test/cases/forbidden_attributes_protection_test.rb
@@ -66,4 +66,34 @@ class ForbiddenAttributesProtectionTest < ActiveRecord::TestCase
     person = Person.new
     assert_nil person.assign_attributes(ProtectedParams.new({}))
   end
+
+  def test_create_with_checks_permitted
+    params = ProtectedParams.new(first_name: 'Guille', gender: 'm')
+
+    assert_raises(ActiveModel::ForbiddenAttributesError) do
+      Person.create_with(params).create!
+    end
+  end
+
+  def test_create_with_works_with_params_values
+    params = ProtectedParams.new(first_name: 'Guille')
+
+    person = Person.create_with(first_name: params[:first_name]).create!
+    assert_equal 'Guille', person.first_name
+  end
+
+  def test_where_checks_permitted
+    params = ProtectedParams.new(first_name: 'Guille', gender: 'm')
+
+    assert_raises(ActiveModel::ForbiddenAttributesError) do
+      Person.where(params).create!
+    end
+  end
+
+  def test_where_works_with_params_values
+    params = ProtectedParams.new(first_name: 'Guille')
+
+    person = Person.where(first_name: params[:first_name]).create!
+    assert_equal 'Guille', person.first_name
+  end
 end
