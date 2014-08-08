rails/rails

commit fbe38c9e9d4fe9f82518e8ffc1d757459b0c5f1c

Pull spring gem entry into the Gemfile template instead of gemfile_entries so it can be grouped with the other development tools

railties/lib/rails/generators/rails/app/templates/Gemfile
@@ -21,14 +21,23 @@ source 'https://rubygems.org'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+group :development, :test do
 <% unless defined?(JRUBY_VERSION) -%>
-# To use a debugger
+  # Call 'debugger' anywhere in the code to stop execution and get a debugger console
   <%- if RUBY_VERSION < '2.0.0' -%>
-# gem 'debugger', group: [:development, :test]
+  gem 'debugger'
   <%- else -%>
-# gem 'byebug', group: [:development, :test]
+  gem 'byebug'
   <%- end -%>
+  
+  # Access an IRB console on exceptions page and /console in development
+  gem 'web-console'
+<%- if spring_install? %>
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'spring'
 <% end -%>
+<% end -%>
+end
 
 <% if RUBY_PLATFORM.match(/bccwin|cygwin|emx|mingw|mswin|wince/) -%>
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem

railties/lib/rails/generators/app_base.rb
@@ -113,7 +113,6 @@ module Rails
          javascript_gemfile_entry,
          jbuilder_gemfile_entry,
          sdoc_gemfile_entry,
-         spring_gemfile_entry,
          psych_gemfile_entry,
          @extra_entries].flatten.find_all(&@gem_filter)
       end
@@ -304,12 +303,6 @@ module Rails
         else
           GemfileEntry.new 'therubyracer', nil, comment, { platforms: :ruby }, true
         end
-      end
-
-      def spring_gemfile_entry
-        return [] unless spring_install?
-        comment = 'Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring'
-        GemfileEntry.new('spring', nil, comment, group: :development)
       end
 
       def psych_gemfile_entry

commit 301b5fd52265298e1fe16a7914bc656c808d931e

Fixes test for Gemfile entry changes
Broken by fbe38c9e9d4fe9f82518e8ffc1d757459b0c5f1c

railties/lib/rails/generators/rails/app/templates/Gemfile
@@ -29,7 +29,7 @@ group :development, :test do
   <%- else -%>
   gem 'byebug'
   <%- end -%>
-  
+
   # Access an IRB console on exceptions page and /console in development
   gem 'web-console'
 <%- if spring_install? %>

railties/lib/rails/generators/rails/plugin/templates/Gemfile
@@ -31,7 +31,7 @@ end
 <% end -%>
 <%= gem.commented_out ? '# ' : '' %>gem '<%= gem.name %>'<%= %(, '#{gem.version}') if gem.version -%>
 <% if gem.options.any? -%>
-,<%= gem.padding(max_width) %><%= gem.options.map { |k,v|
+, <%= gem.options.map { |k,v|
   "#{k}: #{v.inspect}" }.join(', ') %>
 <% end -%>
 <% end -%>

railties/test/generators/app_generator_test.rb
@@ -299,7 +299,7 @@ class AppGeneratorTest < Rails::Generators::TestCase
     if defined?(JRUBY_VERSION)
       assert_gem "therubyrhino"
     else
-      assert_file "Gemfile", /# gem\s+["']therubyracer["']+, \s+platforms: :ruby$/
+      assert_file "Gemfile", /# gem 'therubyracer', platforms: :ruby/
     end
   end
 
@@ -340,7 +340,7 @@ class AppGeneratorTest < Rails::Generators::TestCase
 
   def test_inclusion_of_jbuilder
     run_generator
-    assert_file "Gemfile", /gem 'jbuilder'/
+    assert_gem 'jbuilder'
   end
 
   def test_inclusion_of_a_debugger
@@ -351,9 +351,9 @@ class AppGeneratorTest < Rails::Generators::TestCase
         assert_no_match(/debugger/, content)
       end
     elsif RUBY_VERSION < '2.0.0'
-      assert_file "Gemfile", /# gem 'debugger'/
+      assert_gem 'debugger'
     else
-      assert_file "Gemfile", /# gem 'byebug'/
+      assert_gem 'byebug'
     end
   end
 
@@ -419,9 +419,14 @@ class AppGeneratorTest < Rails::Generators::TestCase
     assert_file "foo bar/config/initializers/session_store.rb", /key: '_foo_bar/
   end
 
+  def test_web_console
+    run_generator
+    assert_gem 'web-console'
+  end
+
   def test_spring
     run_generator
-    assert_file "Gemfile", /gem 'spring', \s+group: :development/
+    assert_gem 'spring'
   end
 
   def test_spring_binstubs
@@ -523,6 +528,6 @@ class AppGeneratorTest < Rails::Generators::TestCase
   end
 
   def assert_gem(gem)
-    assert_file "Gemfile", /^gem\s+["']#{gem}["']$/
+    assert_file "Gemfile", /^\s*gem\s+["']#{gem}["']$*/
   end
 end