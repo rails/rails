06a1a28f947b2eeb7c26ae5dc0efc20b140f5d86
Arthur Neves <arthurnn@gmail.com>
2014-08-11 11:32:12 -0400

    Fix SQL syntax in Enum docs. [skip ci]

--- activerecord/lib/active_record/enum.rb
+++ activerecord/lib/active_record/enum.rb
@@ -65,7 +65,7 @@ module ActiveRecord
   #
   # Use that class method when you need to know the ordinal value of an enum:
   #
-  #   Conversation.where("status <> ?", Conversation.statuses[:archived])
+  #   Conversation.where(status: Conversation.statuses[:archived])
   #
   # Where conditions on an enum attribute must use the ordinal value of an enum.
   module Enum
