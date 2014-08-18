68c643f8e70084d89f7bddff2b863b6b83411f4a
Abdelkader Boudih <terminale@gmail.com>
2014-08-18 08:17:36 +0000

    [ActiveJob] Add queue_name_prefix so all queue are prefixed . We can split queues on a per environement basis as well as seggregate per application so we can split queues on a per env basis
    ActiveJob::Base.queue_name_prefix = 'foo'

--- activejob/lib/active_job/queue_name.rb
+++ activejob/lib/active_job/queue_name.rb
@@ -3,10 +3,13 @@ module ActiveJob
     extend ActiveSupport::Concern
 
     module ClassMethods
+      mattr_accessor(:queue_name_prefix)
       mattr_accessor(:default_queue_name) { "default" }
 
       def queue_as(part_name)
-        self.queue_name = part_name.to_s.presence || default_queue_name
+        queue_name = part_name.to_s.presence || default_queue_name
+        name_parts = [queue_name_prefix.presence, queue_name]
+        self.queue_name = name_parts.compact.join('_')
       end
     end
 
