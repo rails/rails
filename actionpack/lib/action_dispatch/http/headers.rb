22b2cf3fb1cc9f4f853c8fe8d73dd6657217c226
daisuko <striker.daisuko@gmail.com>
2014-08-19 22:09:38 +0900

    not be able to match HTTP header with underscore. #16519

--- actionpack/lib/action_dispatch/http/headers.rb
+++ actionpack/lib/action_dispatch/http/headers.rb
@@ -27,7 +27,7 @@ module ActionDispatch
         SERVER_SOFTWARE
       ]).freeze
 
-      HTTP_HEADER = /\A[A-Za-z0-9-]+\z/
+      HTTP_HEADER = /\AHTTP_[A-Z0-9!\#$%&'*+\-^_`|~]+\z/
 
       include Enumerable
       attr_reader :env
@@ -88,7 +88,7 @@ module ActionDispatch
       # not contained within the headers hash.
       def env_name(key)
         key = key.to_s
-        if key =~ HTTP_HEADER
+        unless key =~ HTTP_HEADER || key.include?('.')
           key = key.upcase.tr('-', '_')
           key = "HTTP_" + key unless CGI_VARIABLES.include?(key)
         end
