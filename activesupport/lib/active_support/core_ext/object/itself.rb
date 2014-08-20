24a9bc4fc8d0885c64e27a62fb2703416c6ab95d
Zachary Scott <e@zzak.io>
2014-08-19 23:45:43 -0700

    Add documentation intro to example for `Object#itself`.
    Also moved comment for removal above `unless()` to not confuse RDoc with
    the documentation for this method.

--- activesupport/lib/active_support/core_ext/object/itself.rb
+++ activesupport/lib/active_support/core_ext/object/itself.rb
@@ -1,6 +1,9 @@
 class Object
-  unless respond_to?(:itself) # TODO: Remove this file when we drop support for Ruby < 2.2
-    # Returns the object itself. Useful when dealing with a chaining scenario, like Active Record scopes:
+  # TODO: Remove this file when we drop support for Ruby < 2.2
+  unless respond_to?(:itself)
+    # Returns the object itself.
+    #
+    # Useful for chaining methods, such as Active Record scopes:
     #
     #   Event.public_send(state.presence_in([ :trashed, :drafted ]) || :itself).order(:created_at)
     #
