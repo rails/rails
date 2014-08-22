e63a02ccfbbdc0aeefd57617c835a2adc8588dd3
Abdelkader Boudih <terminale@gmail.com>
2014-08-22 19:30:36 +0000

    [ActiveJob] Add hook for test_framework and test templates

--- activejob/lib/rails/generators/job/job_generator.rb
+++ activejob/lib/rails/generators/job/job_generator.rb
@@ -7,11 +7,13 @@ module Rails
 
       class_option :queue, type: :string, default: 'default', desc: 'The queue name for the generated job'
 
+      check_class_collision suffix: 'Job'
+
+      hook_for :test_framework
+
       def self.default_generator_root
         File.dirname(__FILE__)
       end
-
-      check_class_collision suffix: 'Job'
 
       def create_job_file
         template 'job.rb', File.join('app/jobs', class_path, "#{file_name}_job.rb")
