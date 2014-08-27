1be831d42417c49b4d31361346d67147b39701e1
Robin Dupret <robin.dupret@gmail.com>
2014-08-27 19:47:39 +0200

    Avoid skipping Sidekiq tests on Rubinius
    Now that Travis provides support for Rubinius 2.2.10, we can run the
    Sidekiq tests against this version.

--- activejob/test/helper.rb
+++ activejob/test/helper.rb
@@ -10,18 +10,12 @@ def sidekiq?
   @adapter == 'sidekiq'
 end
 
-def rubinius?
-  RUBY_ENGINE == 'rbx'
-end
-
 def ruby_193?
   RUBY_VERSION == '1.9.3' && RUBY_ENGINE != 'java'
 end
 
-#Sidekiq don't work with MRI 1.9.3
-#Travis uses rbx 2.6 which don't support unicode characters in methods.
-#Remove the check when Travis change to rbx 2.7+
-exit  if sidekiq?  && (ruby_193? || rubinius?)
+# Sidekiq doesn't work with MRI 1.9.3
+exit if sidekiq? && ruby_193?
 
 require "adapters/#{@adapter}"
 
