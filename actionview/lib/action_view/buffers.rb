0da95fd702b568e222d99d5ddccf0e1ce5a55d41
Aaron Patterson <aaron.patterson@gmail.com>
2014-09-14 13:26:39 -0700

    Stop nil checking on safe_append=
    ERB compiler guarantees safe_append= will be called with a string, so
    nil checks don't make sense.  Anything else calling this method should
    check for nil themselves before calling

--- actionview/lib/action_view/buffers.rb
+++ actionview/lib/action_view/buffers.rb
@@ -13,10 +13,6 @@ module ActionView
     end
     alias :append= :<<
 
-    def safe_concat(value)
-      return self if value.nil?
-      super(value.to_s)
-    end
     alias :safe_append= :safe_concat
   end
 
