97e24337a03ec441fec5eca193f945be0d3aabd0
Trung Pham <trung@phamcom.com>
2014-09-18 12:37:25 -0700

    allow overriding sub job queue_adapter without affecting the base job or sibling job.

--- activejob/test/cases/adapter_test.rb
+++ activejob/test/cases/adapter_test.rb
@@ -5,4 +5,25 @@ class AdapterTest < ActiveSupport::TestCase
     ActiveJob::Base.queue_adapter = ENV['AJADAPTER'].to_sym
     assert_equal ActiveJob::Base.queue_adapter, "active_job/queue_adapters/#{ENV['AJADAPTER']}_adapter".classify.constantize
   end
+
+  test 'should allow overriding the queue_adapter at the child class level without affecting the parent or its sibling' do
+
+    base_queue_adapter = ActiveJob::Base.queue_adapter
+
+    class ChildJobOne < ActiveJob::Base
+      self.queue_adapter = 'test'
+    end
+    assert_not_equal ActiveJob::Base.queue_adapter, ChildJobOne.queue_adapter
+    assert_equal ActiveJob::QueueAdapters::TestAdapter, ChildJobOne.queue_adapter
+
+    class ChildJobTwo < ActiveJob::Base
+      self.queue_adapter = 'inline'
+    end
+    assert_equal ActiveJob::QueueAdapters::InlineAdapter, ChildJobTwo.queue_adapter
+
+    assert_equal ActiveJob::QueueAdapters::TestAdapter, ChildJobOne.queue_adapter, "ChildJobOne's queue adapter should remain unchanged"
+    assert_equal base_queue_adapter, ActiveJob::Base.queue_adapter, "ActiveJob::Base's queue adapter should remain unchanged"
+
+  end
 end
+
