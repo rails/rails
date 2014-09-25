8cbf301b4a6565b62dd3db4a8dc7a19dbef7087c
Godfrey Chan <godfreykfc@gmail.com>
2014-09-26 00:26:29 +0900

    Fix actionpack test cases broken by #16888

--- actionpack/test/controller/new_base/render_file_test.rb
+++ actionpack/test/controller/new_base/render_file_test.rb
@@ -13,15 +13,6 @@ module RenderFile
       render :file => File.join(File.dirname(__FILE__), '../../fixtures/test/render_file_with_ivar')
     end
 
-    def without_file_key
-      render File.join(File.dirname(__FILE__), *%w[.. .. fixtures test hello_world])
-    end
-
-    def without_file_key_with_instance_variable
-      @secret = 'in the sauce'
-      render File.join(File.dirname(__FILE__), '../../fixtures/test/render_file_with_ivar')
-    end
-
     def relative_path
       @secret = 'in the sauce'
       render :file => '../../fixtures/test/render_file_with_ivar'
@@ -41,11 +32,6 @@ module RenderFile
       path = File.join(File.dirname(__FILE__), '../../fixtures/test/render_file_with_locals')
       render :file => path, :locals => {:secret => 'in the sauce'}
     end
-
-    def without_file_key_with_locals
-      path = FIXTURES.join('test/render_file_with_locals').to_s
-      render path, :locals => {:secret => 'in the sauce'}
-    end
   end
 
   class TestBasic < Rack::TestCase
@@ -58,16 +44,6 @@ module RenderFile
 
     test "rendering template with ivar" do
       get :with_instance_variables
-      assert_response "The secret is in the sauce\n"
-    end
-
-    test "rendering path without specifying the :file key" do
-      get :without_file_key
-      assert_response "Hello world!"
-    end
-
-    test "rendering path without specifying the :file key with ivar" do
-      get :without_file_key_with_instance_variable
       assert_response "The secret is in the sauce\n"
     end
 
@@ -88,11 +64,6 @@ module RenderFile
 
     test "rendering file with locals" do
       get :with_locals
-      assert_response "The secret is in the sauce\n"
-    end
-
-    test "rendering path without specifying the :file key with locals" do
-      get :without_file_key_with_locals
       assert_response "The secret is in the sauce\n"
     end
   end
