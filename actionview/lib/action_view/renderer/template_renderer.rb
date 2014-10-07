9461583d3fe8b0269ebdf0ebefc1304f658f4bcd
Akira Matsuda <ronnie@dio.jp>
2014-10-07 19:50:27 +0900

    Avoid creating unneeded Hash instance and calling slow Hash#fetch

--- actionview/lib/action_view/renderer/template_renderer.rb
+++ actionview/lib/action_view/renderer/template_renderer.rb
@@ -18,7 +18,7 @@ module ActionView
 
     # Determine the template to be rendered using the given options.
     def determine_template(options)
-      keys = options.fetch(:locals, {}).keys
+      keys = options.has_key?(:locals) ? options[:locals].keys : []
 
       if options.key?(:body)
         Template::Text.new(options[:body])
