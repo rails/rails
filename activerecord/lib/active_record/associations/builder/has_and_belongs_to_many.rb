f43f56e16e99dae083cf1400a48c236d1af265d4
Sammy Larbi <sam@codeodor.com>
2014-10-08 15:58:11 -0500

    Ensure HABTM relationships produce valid class names (Fixes #17119)

--- activerecord/lib/active_record/associations/builder/has_and_belongs_to_many.rb
+++ activerecord/lib/active_record/associations/builder/has_and_belongs_to_many.rb
@@ -98,7 +98,7 @@ module ActiveRecord::Associations::Builder
 
     def middle_options(join_model)
       middle_options = {}
-      middle_options[:class] = join_model
+      middle_options[:class_name] = "#{lhs_model.name}::#{join_model.name}"
       middle_options[:source] = join_model.left_reflection.name
       if options.key? :foreign_key
         middle_options[:foreign_key] = options[:foreign_key]
