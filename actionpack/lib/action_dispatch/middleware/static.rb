3437f260a5903b651bf8b8f751345fb288fa9052
Aaron Patterson <aaron.patterson@gmail.com>
2014-10-10 16:00:03 -0700

    FileHandler should not be called for files outside the root
    FileHandler#matches? should return false for files that are outside the
    "root" path.
    
    Conflicts:
    	actionpack/lib/action_dispatch/middleware/static.rb
    
    Conflicts:
    	actionpack/lib/action_dispatch/middleware/static.rb
    	actionpack/test/dispatch/static_test.rb

--- actionpack/lib/action_dispatch/middleware/static.rb
+++ actionpack/lib/action_dispatch/middleware/static.rb
@@ -12,7 +12,7 @@ module ActionDispatch
     def match?(path)
       path = path.dup
 
-      full_path = path.empty? ? @root : File.join(@root, escape_glob_chars(unescape_path(path)))
+      full_path = path.empty? ? @root : File.join(@root, escape_glob_chars(clean_path_info(unescape_path(path))))
       paths = "#{full_path}#{ext}"
 
       matches = Dir[paths]
@@ -41,6 +41,25 @@ module ActionDispatch
     def escape_glob_chars(path)
       path.force_encoding('binary') if path.respond_to? :force_encoding
       path.gsub(/[*?{}\[\]]/, "\\\\\\&")
+    end
+
+    private
+
+    PATH_SEPS = Regexp.union(*[::File::SEPARATOR, ::File::ALT_SEPARATOR].compact)
+
+    def clean_path_info(path_info)
+      parts = path_info.split PATH_SEPS
+
+      clean = []
+
+      parts.each do |part|
+        next if part.empty? || part == '.'
+        part == '..' ? clean.pop : clean << part
+      end
+
+      clean.unshift '/' if parts.empty? || parts.first.empty?
+
+      ::File.join(*clean)
     end
   end
 
