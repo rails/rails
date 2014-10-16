f551a046dfcf590cb4c10d899407118ecfaf06c3
Aditya Kapoor <aditya.kapoor@vinsol.com>
2014-10-16 12:05:01 +0530

    remove unneeded file

--- railties/lib/rails/commands/update.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'generators'))
-
-if ARGV.size == 0
-  Rails::Generators.help
-  exit
-end
-
-name = ARGV.shift
-Rails::Generators.invoke name, ARGV, behavior: :skip
