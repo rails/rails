815a59180aa6b2c9fd4c9ef00b3b162b21ed3859
Pablo Herrero <pablodherrero@gmail.com>
2014-10-17 21:36:50 -0300

    Use each_with_object instead of inject

--- activesupport/test/load_paths_test.rb
+++ activesupport/test/load_paths_test.rb
@@ -2,12 +2,11 @@ require 'abstract_unit'
 
 class LoadPathsTest < ActiveSupport::TestCase
   def test_uniq_load_paths
-    load_paths_count = $LOAD_PATH.inject({}) { |paths, path|
+    load_paths_count = $LOAD_PATH.each_with_object({}) do |path, paths|
       expanded_path = File.expand_path(path)
       paths[expanded_path] ||= 0
       paths[expanded_path] += 1
-      paths
-    }
+    end
     load_paths_count[File.expand_path('../../lib', __FILE__)] -= 1
 
     load_paths_count.select! { |k, v| v > 1 }
