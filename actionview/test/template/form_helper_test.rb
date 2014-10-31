0d6a56d63525c708e398eb99bb56eea18444d751
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-10-31 16:41:51 -0200

    Improve test of private properties of objects in form tags

--- actionview/test/template/form_helper_test.rb
+++ actionview/test/template/form_helper_test.rb
@@ -154,26 +154,8 @@ class FormHelperTest < ActionView::TestCase
     def initialize; end
   end
 
-  class FooObject
-
-    def method_missing(*args)
-      nil
-    end
-
-    private
-      def private_property
-        raise "This method should not be called."
-      end
-  end
-
   def test_tags_base_child_without_render_method
     assert_raise(NotImplementedError) { FooTag.new.render }
-  end
-
-  def test_tags_base_value_honors_public_private
-    test_object = Class.new { private def my_method ; end }.new
-    tag = ActionView::Helpers::Tags::Base.new 'test_object', :my_method, nil
-    assert_raise(NoMethodError) { tag.send :value, test_object }
   end
 
   def test_label
@@ -1804,18 +1786,17 @@ class FormHelperTest < ActionView::TestCase
   end
 
   def test_form_tags_do_not_call_private_properties_on_form_object
-    obj = FooObject.new
+    obj = Class.new do
+      private
+
+      def private_property
+        raise "This method should not be called."
+      end
+    end.new
+
     form_for(obj, as: "other_name", url: '/', html: { id: "edit-other-name" }) do |f|
-      concat f.hidden_field(:private_property)
-      concat f.submit('Create Foo')
+      assert_raise(NoMethodError) { f.hidden_field(:private_property) }
     end
-
-    expected =  whole_form("/", "edit-other-name", "new_other_name", method: "post") do
-      "<input id='other_name_private_property' name='other_name[private_property]' type='hidden' />" +
-      "<input name='commit' value='Create Foo' type='submit' />"
-    end
-
-    assert_dom_equal expected, output_buffer
   end
 
   def test_form_for_with_method_as_part_of_html_options
