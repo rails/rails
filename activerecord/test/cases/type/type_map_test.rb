rails/rails

commit da99a2a2982d35f670ad9647463e09bfe9032b70

PERF: optimise type lookup to avoid invoking procs

activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
@@ -431,16 +431,22 @@ module ActiveRecord
       private
 
         def get_oid_type(oid, fmod, column_name, sql_type = '') # :nodoc:
-          if !type_map.key?(oid)
+
+          result = type_map.fetch(oid, fmod, sql_type) {
+            nil
+          }
+
+          unless result
             load_additional_types(type_map, [oid])
+            result = type_map.fetch(oid, fmod, sql_type) {
+              warn "unknown OID #{oid}: failed to recognize type of '#{column_name}'. It will be treated as String."
+              Type::Value.new.tap do |cast_type|
+                type_map.register_type(oid, cast_type)
+              end
+            }
           end
 
-          type_map.fetch(oid, fmod, sql_type) {
-            warn "unknown OID #{oid}: failed to recognize type of '#{column_name}'. It will be treated as String."
-            Type::Value.new.tap do |cast_type|
-              type_map.register_type(oid, cast_type)
-            end
-          }
+          result
         end
 
         def initialize_type_map(m) # :nodoc:

activerecord/lib/active_record/type/hash_lookup_type_map.rb
@@ -1,6 +1,12 @@
 module ActiveRecord
   module Type
     class HashLookupTypeMap < TypeMap # :nodoc:
+
+      def initialize
+        @cache = {}
+        super
+      end
+
       delegate :key?, to: :@mapping
 
       def lookup(type, *args)
@@ -8,11 +14,23 @@ module ActiveRecord
       end
 
       def fetch(type, *args, &block)
-        @mapping.fetch(type, block).call(type, *args)
+        cache = (@cache[type] ||= {})
+        resolved = cache[args]
+
+        unless resolved
+          resolved = cache[args] = @mapping.fetch(type, block).call(type, *args)
+        end
+
+        resolved
       end
 
       def alias_type(type, alias_type)
         register_type(type) { |_, *args| lookup(alias_type, *args) }
+      end
+
+      def register_type(key, value=nil, &block)
+        @cache = {}
+        super(key, value, &block)
       end
     end
   end

commit 74c35b46e362a128fe06f909744da8bb1aeed0e0

Introduce test to demonstrate regression caused by da99a2a2

activerecord/test/cases/type/type_map_test.rb
@@ -124,6 +124,16 @@ module ActiveRecord
         assert_equal mapping.lookup(3), 'string'
         assert_kind_of Type::Value, mapping.lookup(4)
       end
+
+      def test_aliases_are_not_improperly_cached
+        mapping = HashLookupTypeMap.new
+
+        mapping.register_type("foo") { |*args| args.join("-") }
+        mapping.alias_type("bar", "foo")
+
+        assert_equal "bar-1-2-3", mapping.lookup("bar", 1, 2, 3)
+        assert_equal "foo-1-2-3", mapping.lookup("foo", 1, 2, 3)
+      end
     end
   end
 end