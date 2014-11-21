e0e9d5bd8600c01de6b73487bf66046d179c0ed7
Guo Xiang Tan <tgx_world@hotmail.com>
2014-11-21 17:44:24 +0800

    Fix select_tag generating tag when set to false.
    Fixes https://github.com/rails/rails/issues/17701.

--- actionview/test/template/form_tag_helper_test.rb
+++ actionview/test/template/form_tag_helper_test.rb
@@ -232,6 +232,12 @@ class FormTagHelperTest < ActionView::TestCase
     assert_dom_equal expected, actual
   end
 
+  def test_select_tag_with_include_blank_false
+    actual = select_tag "places", "<option>Home</option><option>Work</option><option>Pub</option>".html_safe, include_blank: false
+    expected = %(<select id="places" name="places"><option>Home</option><option>Work</option><option>Pub</option></select>)
+    assert_dom_equal expected, actual
+  end
+
   def test_select_tag_with_include_blank_string
     actual = select_tag "places", "<option>Home</option><option>Work</option><option>Pub</option>".html_safe, include_blank: 'Choose'
     expected = %(<select id="places" name="places"><option value="">Choose</option><option>Home</option><option>Work</option><option>Pub</option></select>)
