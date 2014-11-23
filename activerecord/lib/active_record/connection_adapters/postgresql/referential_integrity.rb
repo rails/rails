7ddbb0057ab5a71a2a2c05548d8dd927aa5c0bca
Andrey Nering <andrey.nering@gmail.com>
2014-11-23 11:15:56 -0200

    The Postgres connection adapter assumed that was ever possible to disable referential integrity, causing problems when it was not possible.
    Disabling referential integrity is possible when the Postgres user is a superuser. It can be verified with (SQL):
    
        SHOW is_superuser;
    
    It was added a warning when the user is not a postgres superuser and it is not possible to disable referential integrity, to help the developer know about possible problems.
    
    And now the adapter just commit the "disable trigger" SQL when it know it can be done.

--- activerecord/lib/active_record/connection_adapters/postgresql/referential_integrity.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/referential_integrity.rb
@@ -2,26 +2,28 @@ module ActiveRecord
   module ConnectionAdapters
     module PostgreSQL
       module ReferentialIntegrity # :nodoc:
-        def supports_disable_referential_integrity? # :nodoc:
-          true
+        def is_superuser? # :nodoc:
+          if execute('SHOW is_superuser')[0]['is_superuser'] === 'on'
+            true
+          else
+            false
+          end
         end
 
         def disable_referential_integrity # :nodoc:
-          if supports_disable_referential_integrity?
-            begin
-              execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER ALL" }.join(";"))
-            rescue
-              execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER USER" }.join(";"))
-            end
+          if is_superuser?
+            execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER ALL" }.join(";"))
+          else
+            puts 'WARNING: Rails can\'t disable referencial integrity if the Postgres user is not a superuser. ' + \
+              'It is recommended to run tests with a Postgres superuser.'
+            execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER USER" }.join(";"))
           end
           yield
         ensure
-          if supports_disable_referential_integrity?
-            begin
-              execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER ALL" }.join(";"))
-            rescue
-              execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER USER" }.join(";"))
-            end
+          if is_superuser?
+            execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER ALL" }.join(";"))
+          else
+            execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER USER" }.join(";"))
           end
         end
       end
