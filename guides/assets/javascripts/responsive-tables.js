4cab0064001021d18c140f4e89f35600b5226699
Rishi Jain <rishi@joshsoftware.com>
2014-11-25 15:53:24 +0530

    removed unncessary white spaces [ci skip]

--- guides/assets/javascripts/responsive-tables.js
+++ guides/assets/javascripts/responsive-tables.js
@@ -16,24 +16,24 @@ $(document).ready(function() {
       });
     }
   };
-   
+
   $(window).load(updateTables);
   $(window).bind("resize", updateTables);
-   
-	
+
+
 	function splitTable(original)
 	{
 		original.wrap("<div class='table-wrapper' />");
-		
+
 		var copy = original.clone();
 		copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
 		copy.removeClass("responsive");
-		
+
 		original.closest(".table-wrapper").append(copy);
 		copy.wrap("<div class='pinned' />");
 		original.wrap("<div class='scrollable' />");
 	}
-	
+
 	function unsplitTable(original) {
     original.closest(".table-wrapper").find(".pinned").remove();
     original.unwrap();
