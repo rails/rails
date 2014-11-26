76d8a2a63821eb2afcad7e6adfd126c5448bb6c9
Siva Gollapalli <siva@joshsoftware.com>
2014-11-26 20:31:30 +0530

    Issue#17703 Test case for tempfile attribute
    + To avoid regression I am adding this test case on action pack
    
    Changed to assert from assert_equal
    
    + Added msg to assertion
    
    Modified msg

--- actionpack/test/controller/test_case_test.rb
+++ actionpack/test/controller/test_case_test.rb
@@ -908,6 +908,11 @@ XML
     assert_equal File.open(path, READ_PLAIN).read, plain_file_upload.read
   end
 
+  def test_fixture_file_upload_should_be_able_access_to_tempfile
+    file = fixture_file_upload(FILES_DIR + "/mona_lisa.jpg", "image/jpg")
+    assert file.respond_to?(:tempfile), "expected tempfile should respond on fixture file object, got nothing"
+  end
+
   def test_fixture_file_upload
     post :test_file_upload,
       params: {
