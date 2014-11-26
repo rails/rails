07541863a0f60c9b27ba911de9236ecc0c5e01a5
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-11-26 05:42:35 -0200

    Merge pull request #17779 from dilpreet92/server_location
    Getting the location of the server

--- actionpack/lib/action_controller/base.rb
+++ actionpack/lib/action_controller/base.rb
@@ -44,7 +44,7 @@ module ActionController
   # The full request object is available via the request accessor and is primarily used to query for HTTP headers:
   #
   #   def server_ip
-  #     location = request.env["SERVER_ADDR"]
+  #     location = request.env["REMOTE_ADDR"]
   #     render plain: "This server hosted at #{location}"
   #   end
   #
