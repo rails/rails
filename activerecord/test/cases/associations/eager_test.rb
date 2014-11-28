62de9cf8c19e4dc9496e75a17f173952539a00b6
Santiago Pastorino <santiago@wyeworks.com>
2014-11-28 11:26:28 -0200

    Merge pull request #17811 from mrnugget/fixes_nested_association_preloading
    Adds preloaded_records method to NullPreloader - fixes #16070

--- activerecord/test/cases/associations/eager_test.rb
+++ activerecord/test/cases/associations/eager_test.rb
@@ -269,6 +269,14 @@ class EagerAssociationTest < ActiveRecord::TestCase
     end
   end
 
+  def test_three_level_nested_preloading_does_not_raise_exception_when_association_does_not_exist
+    post_id = Comment.where(author_id: nil).where.not(post_id: nil).first.post_id
+
+    assert_nothing_raised do
+      Post.preload(:comments => [{:author => :essays}]).find(post_id)
+    end
+  end
+
   def test_nested_loading_through_has_one_association
     aa = AuthorAddress.all.merge!(:includes => {:author => :posts}).find(author_addresses(:david_address).id)
     assert_equal aa.author.posts.count, aa.author.posts.length
