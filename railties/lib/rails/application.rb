185d7cdb429764c50fb62035fa918b7ba0a71fae
claudiob <claudiob@gmail.com>
2014-11-28 18:38:08 -0800

    Remove Psych hack for Ruby 1.9
    A special `if` statement to support `Psych` for Ruby < 2.0 can be
    dropped now that Rails requires Ruby >= 2.0.

--- railties/lib/rails/application.rb
+++ railties/lib/rails/application.rb
@@ -420,16 +420,7 @@ module Rails
 
     console do
       unless ::Kernel.private_method_defined?(:y)
-        if RUBY_VERSION >= '2.0'
-          require "psych/y"
-        else
-          module ::Kernel
-            def y(*objects)
-              puts ::Psych.dump_stream(*objects)
-            end
-            private :y
-          end
-        end
+        require "psych/y"
       end
     end
 
