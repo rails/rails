e47b523fae8680d71178297f7e913099430d5f2a
Aaron Patterson <aaron.patterson@gmail.com>
2014-12-01 11:46:45 -0800

    ugh, forgot to add this test to cdd90f39d796986dabf1678b3277b230dbe18961

--- activerecord/test/cases/adapters/postgresql/change_schema_test.rb
+++ activerecord/test/cases/adapters/postgresql/change_schema_test.rb
@@ -21,6 +21,11 @@ module ActiveRecord
         connection.change_column :strings, :somedate, :timestamp, using: 'CAST("somedate" AS timestamp)'
         assert_equal :datetime, connection.columns(:strings).find { |c| c.name == 'somedate' }.type
       end
+
+      def test_change_type_with_symbol
+        connection.change_column :strings, :somedate, :timestamp, cast_as: :timestamp
+        assert_equal :datetime, connection.columns(:strings).find { |c| c.name == 'somedate' }.type
+      end
     end
   end
 end
