63462ec94e99fa03159fd0acca5a69c0bf252836
Godfrey Chan <godfreykfc@gmail.com>
2014-12-12 11:19:50 -0800

    --edge on 4-2-stable should track the 4-2-stable branch
    See 24e1fff

--- railties/test/generators/shared_generator_tests.rb
+++ railties/test/generators/shared_generator_tests.rb
@@ -112,7 +112,7 @@ module SharedGeneratorTests
 
   def test_edge_option
     assert_generates_with_bundler edge: true
-    assert_file 'Gemfile', %r{^gem\s+["']rails["'],\s+github:\s+["']#{Regexp.escape("rails/rails")}["']$}
+    assert_file 'Gemfile', %r{^gem\s+["']rails["'],\s+github:\s+["']#{Regexp.escape("rails/rails")}["'],\s+branch:\s+["']#{Regexp.escape("4-2-stable")}["']$$}
   end
 
   def test_skip_gemfile
