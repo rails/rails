4c0ef7708b002a647859e28032ac9c5940c0e91c
claudiob <claudiob@gmail.com>
2014-12-17 16:55:08 -0800

    Add docs for Numeric time-related methods
    Add docs for `minutes`, `hours`, `days`, `weeks` and `fortnights`.
    Fix docs for `in_milliseconds`.
    
    [ci skip]

--- activesupport/lib/active_support/core_ext/numeric/time.rb
+++ activesupport/lib/active_support/core_ext/numeric/time.rb
@@ -36,33 +36,51 @@ class Numeric
   end
   alias :second :seconds
 
+  # Returns a Duration instance matching the number of minutes provided.
+  #
+  #   2.minutes # => 120 seconds
   def minutes
     ActiveSupport::Duration.new(self * 60, [[:seconds, self * 60]])
   end
   alias :minute :minutes
 
+  # Returns a Duration instance matching the number of hours provided.
+  #
+  #   2.hours # => 7_200 seconds
   def hours
     ActiveSupport::Duration.new(self * 3600, [[:seconds, self * 3600]])
   end
   alias :hour :hours
 
+  # Returns a Duration instance matching the number of days provided.
+  #
+  #   2.days # => 2 days
   def days
     ActiveSupport::Duration.new(self * 24.hours, [[:days, self]])
   end
   alias :day :days
 
+  # Returns a Duration instance matching the number of weeks provided.
+  #
+  #   2.weeks # => 14 days
   def weeks
     ActiveSupport::Duration.new(self * 7.days, [[:days, self * 7]])
   end
   alias :week :weeks
 
+  # Returns a Duration instance matching the number of fortnights provided.
+  #
+  #   2.fortnights # => 28 days
   def fortnights
     ActiveSupport::Duration.new(self * 2.weeks, [[:days, self * 14]])
   end
   alias :fortnight :fortnights
 
+  # Returns the number of milliseconds equivalent to the seconds provided.
   # Used with the standard time durations, like 1.hour.in_milliseconds --
   # so we can feed them to JavaScript functions like getTime().
+  #
+  #   2.in_milliseconds # => 2_000
   def in_milliseconds
     self * 1000
   end
