be49e98091dc8af147686c653df9ed438956979d
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-12-19 15:12:31 -0200

    Remove rack and i18n master from bug report tempaltes [ci skip]

--- guides/bug_report_templates/action_controller_master.rb
+++ guides/bug_report_templates/action_controller_master.rb
@@ -3,8 +3,6 @@ unless File.exist?('Gemfile')
     source 'https://rubygems.org'
     gem 'rails', github: 'rails/rails'
     gem 'arel', github: 'rails/arel'
-    gem 'rack', github: 'rack/rack'
-    gem 'i18n', github: 'svenfuchs/i18n'
   GEMFILE
 
   system 'bundle'
