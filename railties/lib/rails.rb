a770d7e404f994d717843035eba9c1094a6e90d8
claudiob <claudiob@gmail.com>
2014-12-20 10:09:45 -0800

    Add docs for `Rails.env` and `Rails.env=`
    [ci skip]

--- railties/lib/rails.rb
+++ railties/lib/rails.rb
@@ -56,10 +56,18 @@ module Rails
       application && application.config.root
     end
 
+    # Returns the current Rails environment.
+    #
+    #   Rails.env # => "development"
+    #   Rails.env.development? # => true
+    #   Rails.env.production? # => false
     def env
       @_env ||= ActiveSupport::StringInquirer.new(ENV["RAILS_ENV"] || ENV["RACK_ENV"] || "development")
     end
 
+    # Sets the Rails environment.
+    #
+    #   Rails.env = "staging" # => "staging"
     def env=(environment)
       @_env = ActiveSupport::StringInquirer.new(environment)
     end
