2a110fcfc8ef3536a8622c66b625463fe610c7e2
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2014-12-23 15:58:51 +0900

    fix typo in nodoc

--- actionmailer/lib/action_mailer/delivery_job.rb
+++ actionmailer/lib/action_mailer/delivery_job.rb
@@ -6,7 +6,7 @@ module ActionMailer
   class DeliveryJob < ActiveJob::Base #:nodoc:
     queue_as :mailers
 
-    def perform(mailer, mail_method, delivery_method, *args) #:nodoc#
+    def perform(mailer, mail_method, delivery_method, *args) #:nodoc:
       mailer.constantize.public_send(mail_method, *args).send(delivery_method)
     end
   end
