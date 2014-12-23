96f0aad6ec8c3ae94485679ac8092c4089310481
Jose Añasco <joseanasco1@gmail.com>
2014-12-23 09:38:44 -0500

    Remove block from super

--- actionview/lib/action_view/helpers/tags/search_field.rb
+++ actionview/lib/action_view/helpers/tags/search_field.rb
@@ -3,18 +3,21 @@ module ActionView
     module Tags # :nodoc:
       class SearchField < TextField # :nodoc:
         def render
-          super do |options|
-            if options["autosave"]
-              if options["autosave"] == true
-                options["autosave"] = request.host.split(".").reverse.join(".")
-              end
-              options["results"] ||= 10
-            end
+          options = @options.stringify_keys
 
-            if options["onsearch"]
-              options["incremental"] = true unless options.has_key?("incremental")
+          if options["autosave"]
+            if options["autosave"] == true
+              options["autosave"] = request.host.split(".").reverse.join(".")
             end
+            options["results"] ||= 10
           end
+
+          if options["onsearch"]
+            options["incremental"] = true unless options.has_key?("incremental")
+          end
+
+          @options = options
+          super
         end
       end
     end
