8a73f4b5b9d342a45b3c8e29a13142adbca9ab21
Cristian Bica <cristian.bica@gmail.com>
2014-12-30 17:53:42 +0200

    ActiveJob testing improvements
    1. The :test adapter wasn't going through a full cycle of
    serialize/deserialize when performing jobs. Now it does
    2. Regular AJ tests were not run for the :test adapter. Now they are
    3. ActiveJob::TestHelper uses assert_valid_keys but doesn’t requires
    the file that implements that method. Now it does

--- activejob/test/cases/adapter_test.rb
+++ activejob/test/cases/adapter_test.rb
@@ -2,7 +2,6 @@ require 'helper'
 
 class AdapterTest < ActiveSupport::TestCase
   test "should load #{ENV['AJADAPTER']} adapter" do
-    ActiveJob::Base.queue_adapter = ENV['AJADAPTER'].to_sym
-    assert_equal "active_job/queue_adapters/#{ENV['AJADAPTER']}_adapter".classify.constantize, ActiveJob::Base.queue_adapter
+    assert_equal "active_job/queue_adapters/#{ENV['AJADAPTER']}_adapter".classify, ActiveJob::Base.queue_adapter.name
   end
 end
