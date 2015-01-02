5b043f95c35b0796553b28cefbdadaced2db6cf1
Rafael Mendonça França <rafaelmfranca@gmail.com>
2015-01-02 14:16:15 -0300

    Merge pull request #17978 from kommen/fixed-pr-14903
    Ensure append_info_to_payload is called even if an exception is raised.
    
    Conflicts:
    	actionpack/CHANGELOG.md
    
    Conflicts:
    	actionpack/CHANGELOG.md

--- actionpack/lib/action_controller/metal/instrumentation.rb
+++ actionpack/lib/action_controller/metal/instrumentation.rb
@@ -28,10 +28,13 @@ module ActionController
       ActiveSupport::Notifications.instrument("start_processing.action_controller", raw_payload.dup)
 
       ActiveSupport::Notifications.instrument("process_action.action_controller", raw_payload) do |payload|
-        result = super
-        payload[:status] = response.status
-        append_info_to_payload(payload)
-        result
+        begin
+          result = super
+          payload[:status] = response.status
+          result
+        ensure
+          append_info_to_payload(payload)
+        end
       end
     end
 
