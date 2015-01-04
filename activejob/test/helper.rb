4ab0320fb4ad61b65b1350b531e6993f79943803
Rafael Mendonça França <rafaelmfranca@gmail.com>
2015-01-04 15:14:54 -0300

    No need to disable sidikiq tests with 1.9.3 anymore

--- activejob/test/helper.rb
+++ activejob/test/helper.rb
@@ -7,17 +7,6 @@ GlobalID.app = 'aj'
 
 @adapter  = ENV['AJADAPTER'] || 'inline'
 
-def sidekiq?
-  @adapter == 'sidekiq'
-end
-
-def ruby_193?
-  RUBY_VERSION == '1.9.3' && RUBY_ENGINE != 'java'
-end
-
-# Sidekiq doesn't work with MRI 1.9.3
-exit if sidekiq? && ruby_193?
-
 if ENV['AJ_INTEGRATION_TESTS']
   require 'support/integration/helper'
 else
