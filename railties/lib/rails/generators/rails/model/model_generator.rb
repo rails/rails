884d123e131c931b754213f4e4442a01378aa6f2
shunsukeaida <s.aida@scsk.jp>
2015-01-07 13:04:57 +0900

    acronyms like ORM should be in uppercase, so overwrite the desc generated by hook_for

--- railties/lib/rails/generators/rails/model/model_generator.rb
+++ railties/lib/rails/generators/rails/model/model_generator.rb
@@ -6,7 +6,7 @@ module Rails
       include Rails::Generators::ModelHelpers
 
       argument :attributes, type: :array, default: [], banner: "field[:type][:index] field[:type][:index]"
-      hook_for :orm, required: true
+      hook_for :orm, required: true, desc: "ORM to be invoked"
     end
   end
 end
