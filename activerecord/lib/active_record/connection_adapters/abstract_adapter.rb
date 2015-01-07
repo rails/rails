4542f89946cd3e8096d1649b42566dae7d13160e
Sean Griffin <sean@seantheprogrammer.com>
2015-01-07 15:59:15 -0700

    Merge pull request #18377 from brainopia/custom_connection_with_count
    Fix count on a separate connection

--- activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
@@ -382,7 +382,7 @@ module ActiveRecord
       end
 
       def column_name_for_operation(operation, node) # :nodoc:
-        node.to_sql
+        visitor.accept(node, collector).value
       end
 
       protected
