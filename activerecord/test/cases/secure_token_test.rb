47316feee0f061f80e26c51fb0d41f537407ab9c
robertomiranda <rjmaltamar@gmail.com>
2015-01-09 18:10:38 -0500

    Switch Secure Token generation to Base58
    Update Secure Token Doc [ci skip]
    
    remove require securerandom, core_ext/securerandom already do that ref 7e006057

--- activerecord/test/cases/secure_token_test.rb
+++ activerecord/test/cases/secure_token_test.rb
@@ -22,18 +22,4 @@ class SecureTokenTest < ActiveRecord::TestCase
     assert_not_equal @user.token, old_token
     assert_not_equal @user.auth_token, old_auth_token
   end
-
-  def test_raise_after_ten_unsuccessful_attempts_to_generate_a_unique_token
-    User.stubs(:exists?).returns(*Array.new(10, true))
-    assert_raises(RuntimeError) do
-      @user.save
-    end
-  end
-
-  def test_return_unique_token_after_nine_unsuccessful_attempts
-    User.stubs(:exists?).returns(*Array.new(10) { |i| i == 9 ? false : true })
-    @user.save
-    assert_not_nil @user.token
-    assert_not_nil @user.auth_token
-  end
 end
