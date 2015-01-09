7e006057e082c4eab7424618b24f330e47be8744
Guillermo Iguaran <guilleiguaran@gmail.com>
2015-01-09 18:24:54 -0500

    Add missing require

--- activesupport/lib/active_support/core_ext/securerandom.rb
+++ activesupport/lib/active_support/core_ext/securerandom.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 module SecureRandom
   BASE58_ALPHABET = ('0'..'9').to_a  + ('A'..'Z').to_a + ('a'..'z').to_a - ['0', 'O', 'I', 'l']
   # SecureRandom.base58 generates a random base58 string.
