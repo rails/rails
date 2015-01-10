cdfe511b746c3e5a6294f26ac378cc1f04add072
Eugene Gilburg <eugene.gilburg@gmail.com>
2015-01-09 17:24:33 -0800

    refactor performance of base58 by leveraging base64

--- activesupport/test/core_ext/securerandom.rb
+++ activesupport/test/core_ext/securerandom.rb
@@ -17,4 +17,20 @@ class SecureRandomTest < ActiveSupport::TestCase
     assert_not_equal s1, s2
     assert_equal 24, s1.length
   end
+
+  def test_base58_minimum_length
+    assert_equal '', SecureRandom.base58(0)
+    assert_raise(ArgumentError) { SecureRandom.base58(-1) }
+  end
+
+  def test_base58_charset
+    base58_alphabet = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a - ['0', 'O', 'I', 'l']
+    assert_equal 58, base58_alphabet.uniq.size
+
+    s = SecureRandom.base58(1000)
+
+    s.each_char do |c|
+      assert_includes base58_alphabet, c
+    end
+  end
 end
