d36a769234911c8374e09069eb054d4c60eb1b99
Sean Griffin <sean@thoughtbot.com>
2015-01-25 14:47:47 -0700

    Implement equality for `BindParam`
    It is impossible to test equality of things constructing trees with bind
    params otherwise.

--- lib/arel/nodes/bind_param.rb
+++ lib/arel/nodes/bind_param.rb
@@ -1,6 +1,9 @@
 module Arel
   module Nodes
     class BindParam < Node
+      def ==(other)
+        other.is_a?(BindParam)
+      end
     end
   end
 end
---
+++ test/nodes/test_bind_param.rb
@@ -0,0 +1,15 @@
+require 'helper'
+
+module Arel
+  module Nodes
+    describe 'BindParam' do
+      it 'is equal to other bind params' do
+        BindParam.new.must_equal(BindParam.new)
+      end
+
+      it 'is not equal to other nodes' do
+        BindParam.new.wont_equal(Node.new)
+      end
+    end
+  end
+end
