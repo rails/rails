d88f4bc1c1b5cae920cb76d571c3411ce3c41af2
Yves Senn <yves.senn@gmail.com>
2015-01-29 09:33:55 +0100

    reassign `file_fixture_path` in plugins `test_helper.rb`. refs #18658.

--- railties/lib/rails/generators/rails/plugin/templates/test/test_helper.rb
+++ railties/lib/rails/generators/rails/plugin/templates/test/test_helper.rb
@@ -20,4 +20,5 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "files"
 end
