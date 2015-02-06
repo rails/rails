c40e52ea68e21ba5b098343c7e1f285800f97008
Pratik Naik <pratiknaik@gmail.com>
2015-02-07 00:47:45 +0530

    Use just one redis connection

--- lib/action_cable/channel/redis.rb
+++ lib/action_cable/channel/redis.rb
@@ -10,21 +10,22 @@ module ActionCable
 
       def subscribe_to(redis_channel, callback = nil)
         @_redis_channels ||= []
-        @_redis_channels << redis_channel
 
         callback ||= -> (message) { broadcast ActiveSupport::JSON.decode(message) }
-        redis.pubsub.subscribe(redis_channel, &callback)
+        @_redis_channels << [ redis_channel, callback ]
+
+        pubsub.subscribe(redis_channel, &callback)
       end
 
       protected
         def unsubscribe_from_redis_channels
           if @_redis_channels
-            @_redis_channels.each { |channel| @connection.pubsub.unsubscribe(channel) }
+            @_redis_channels.each { |channel, callback| pubsub.unsubscribe_proc(channel, callback) }
           end
         end
 
-        def redis
-          @connection.redis
+        def pubsub
+          @connection.pubsub
         end
     end
 
