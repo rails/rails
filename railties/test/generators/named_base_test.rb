8ba75488773c2d7e4cdf267fb8d4553e8363910c
Yves Senn <yves.senn@gmail.com>
2015-02-09 10:41:37 +0100

    tests, reset global to previous value.
    The default of the global might change. It's better to reset it to
    what it was than a hardcoded value.

--- railties/test/generators/named_base_test.rb
+++ railties/test/generators/named_base_test.rb
@@ -49,11 +49,13 @@ class NamedBaseTest < Rails::Generators::TestCase
   end
 
   def test_named_generator_attributes_without_pluralized
+    original_pluralize_table_names = ActiveRecord::Base.pluralize_table_names
     ActiveRecord::Base.pluralize_table_names = false
+
     g = generator ['admin/foo']
     assert_name g, 'admin_foo', :table_name
   ensure
-    ActiveRecord::Base.pluralize_table_names = true
+    ActiveRecord::Base.pluralize_table_names = original_pluralize_table_names
   end
 
   def test_scaffold_plural_names
