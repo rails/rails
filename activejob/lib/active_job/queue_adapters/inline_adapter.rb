f2a928906a42a0c399c1445f673062b6bcc92ef1
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2015-02-09 18:56:38 +0100

    Support enqueue_at in AJ inline adapter.
    Previously, it was impossible to use inline adapter with delaying
    job, like: `MyWorker.set(wait: 10.seconds).perform_later`
    Now, this job runs immediately.

--- activejob/lib/active_job/queue_adapters/inline_adapter.rb
+++ activejob/lib/active_job/queue_adapters/inline_adapter.rb
@@ -14,8 +14,8 @@ module ActiveJob
           Base.execute(job.serialize)
         end
 
-        def enqueue_at(*) #:nodoc:
-          raise NotImplementedError.new("Use a queueing backend to enqueue jobs in the future. Read more at http://guides.rubyonrails.org/active_job_basics.html")
+        def enqueue_at(job, _) #:nodoc:
+          Base.execute(job.serialize)
         end
       end
     end
