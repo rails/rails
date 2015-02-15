8c837e5fcc2a3ac639b3b93b0024bd2c20d3b6ed
Sean Griffin <sean@thoughtbot.com>
2015-02-15 13:49:21 -0700

    Add a global type registry, used to lookup and register types
    As per previous discussions, we want to give users the ability to
    reference their own types with symbols, instead of having to pass the
    object manually. This adds the class that will be used to do so.
    
        ActiveRecord::Type.register(:money, MyMoneyType)

--- activerecord/lib/active_record/type.rb
+++ activerecord/lib/active_record/type.rb
@@ -16,5 +16,30 @@ require 'active_record/type/text'
 require 'active_record/type/time'
 require 'active_record/type/unsigned_integer'
 
+require 'active_record/type/adapter_specific_registry'
 require 'active_record/type/type_map'
 require 'active_record/type/hash_lookup_type_map'
+
+module ActiveRecord
+  module Type
+    @registry = AdapterSpecificRegistry.new
+
+    class << self
+      attr_accessor :registry # :nodoc:
+
+      def register(*args)
+        registry.register(*args)
+      end
+
+      def lookup(*args, adapter: current_adapter_name, **kwargs)
+        registry.lookup(*args, adapter: adapter, **kwargs)
+      end
+
+      private
+
+      def current_adapter_name
+        ActiveRecord::Base.connection.adapter_name.downcase.to_sym
+      end
+    end
+  end
+end
