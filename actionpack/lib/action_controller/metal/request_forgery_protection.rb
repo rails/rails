d0303d03a94994b7653c629f3cf1578c82a3eccf
Rafael Mendonça França <rafaelmfranca@gmail.com>
2015-02-18 19:37:56 -0200

    Try only to decode strings
    This approach will avoid us to check for NoMethodError when trying to
    decode

--- actionpack/lib/action_controller/metal/request_forgery_protection.rb
+++ actionpack/lib/action_controller/metal/request_forgery_protection.rb
@@ -275,11 +275,13 @@ module ActionController #:nodoc:
       # session token. Essentially the inverse of
       # +masked_authenticity_token+.
       def valid_authenticity_token?(session, encoded_masked_token)
-        return false if encoded_masked_token.nil? || encoded_masked_token.empty?
+        if encoded_masked_token.nil? || encoded_masked_token.empty? || !encoded_masked_token.is_a?(String)
+          return false
+        end
 
         begin
           masked_token = Base64.strict_decode64(encoded_masked_token)
-        rescue ArgumentError, NoMethodError # encoded_masked_token is invalid Base64
+        rescue ArgumentError # encoded_masked_token is invalid Base64
           return false
         end
 
