052aac1771fb452fd14290ced68a8e802f0c51a6
Sameer Rahmani <lxsameer@gnu.org>
2015-02-21 17:28:23 +0330

    ':each' option added to JsonValidator
    This option enforce each object inside the root element of json
    ( If it was an array ) to be valid according to given options which
    are the same as JsonValidator options.
    
    Warning: Use with caution it may be slow.

--- activemodel/lib/active_model/validations/json.rb
+++ activemodel/lib/active_model/validations/json.rb
@@ -16,10 +16,10 @@ module ActiveModel
       end
 
       # Check whether that record include each of given fields
-      def include_validator(fields)
+      def include_validator(fields, attr_value = attribute_value)
         if fields.respond_to? :each
           fields.each do |key|
-            if attribute_value.kind_of?(Hash) && !attribute_value.include?(key.to_s)
+            if attr_value.kind_of?(Hash) && !attr_value.include?(key.to_s)
               add_error "'#{attribute}' does not contains '#{key}'"
             end
           end
@@ -27,12 +27,12 @@ module ActiveModel
       end
 
       # Check for formats of a specific keys
-      def format_validator(opts)
-        if opts.kind_of? Hash
+      def format_validator(opts, attr_value = attribute_value)
+        with_hash :format, opts do
           opts.each do |field, format_opts|
 
             regexp = format_opts[:with]
-            value  = attribute_value[field].to_s
+            value  = attr_value[field].to_s
 
             unless regexp
               raise ArgumentError, "'with' argument is missing for 'format'."
@@ -43,7 +43,39 @@ module ActiveModel
         end
       end
 
+      # In case of an Array as json root object, <tt>:each</tt>
+      # Allow to provide validate options for each element in
+      # the given Array.
+      def each_validator(opts)
+        if attribute_value.respond_to? :each
+          attribute_value.each do |json_obj|
+
+            # Skip if current element is not an object
+            next unless json_obj.kind_of? Hash
+
+            # Loop over <tt>:each</tt> options and match them
+            # against current value.
+            with_hash :each, opts do |opt_key, opt_value|
+              if respond_to? "#{opt_key}_validator"
+                send("#{opt_key}_validator", opt_value, json_obj)
+              end
+            end
+
+          end
+        end
+      end
+
       private
+
+      def with_hash(opt_name, opts, &block)
+        if opts.kind_of? Hash
+          opts.each do |key, value|
+            yield key, value
+          end
+        else
+          raise ArgumentError, "'#{opt_name}' needs a Hash as value."
+        end
+      end
 
       def add_error(msg)
         record.errors[attribute] << (options[:message] || msg || 'is invalid')
@@ -65,6 +97,8 @@ module ActiveModel
       # * <tt>:message</tt> - A custom error message. (default is: "is invalid").
       # * <tt>:include</tt> - An array of keys with given <tt>json</tt> data
       # should include.
+      # * <tt>:format</tt> - A hash with json field names as key and a hash
+      # with <tt>FormatValidator</tt> options as value.
       #
       # There is also a list of default options supported by every validator:
       # +:if+, +:unless+, +:on+, +:allow_nil+, +:allow_blank+, and +:strict+.
