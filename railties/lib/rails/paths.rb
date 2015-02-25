0e9e9d62aaed7739bcf0fc41daf34fc4a66f051c
Robert Krzyzanowski <technoguyrob@gmail.com>
2015-02-25 00:25:58 -0600

    fix typo [ci-skip]

--- railties/lib/rails/paths.rb
+++ railties/lib/rails/paths.rb
@@ -7,7 +7,7 @@ module Rails
     #   root = Root.new "/rails"
     #   root.add "app/controllers", eager_load: true
     #
-    # The command above creates a new root object and add "app/controllers" as a path.
+    # The command above creates a new root object and adds "app/controllers" as a path.
     # This means we can get a <tt>Rails::Paths::Path</tt> object back like below:
     #
     #   path = root["app/controllers"]
