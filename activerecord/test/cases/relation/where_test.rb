rails/rails

commit 5cfe01e61b7eb5f280d246af714272d857c18c46

Don't error when passing an empty array to a polymorphic association
Fixes #19087

activerecord/lib/active_record/relation/predicate_builder.rb
@@ -61,8 +61,11 @@ module ActiveRecord
         end
 
         column = reflection.foreign_key
-        primary_key = reflection.association_primary_key(base_class)
-        value = convert_value_to_association_ids(value, primary_key)
+
+        if base_class
+          primary_key = reflection.association_primary_key(base_class)
+          value = convert_value_to_association_ids(value, primary_key)
+        end
       end
 
       queries << build(table[column], value)

activerecord/test/cases/relation/where_test.rb
@@ -106,6 +106,18 @@ module ActiveRecord
       assert_equal expected.to_sql, actual.to_sql
     end
 
+    def test_polymorphic_empty_array_where
+      treasure = Treasure.new
+      treasure.id = 1
+      hidden = HiddenTreasure.new
+      hidden.id = 2
+
+      expected = PriceEstimate.where("1=0")
+      actual   = PriceEstimate.where(estimate_of: [])
+
+      assert_equal expected.to_a, actual.to_a
+    end
+
     def test_polymorphic_nested_relation_where
       expected = PriceEstimate.where(estimate_of_type: 'Treasure', estimate_of_id: Treasure.where(id: [1,2]))
       actual   = PriceEstimate.where(estimate_of: Treasure.where(id: [1,2]))

commit 1248907fa23e3e94a865d1a2a84e4179f0b7e7f0

Fix regression introduced by 5cfe01e61b7eb5f280d246af714272d857c18c46
We were only testing this behavior against polymorphic associations.

activerecord/lib/active_record/relation/predicate_builder.rb
@@ -56,7 +56,9 @@ module ActiveRecord
       # For polymorphic relationships, find the foreign key and type:
       # PriceEstimate.where(estimate_of: treasure)
       if klass && reflection = klass._reflect_on_association(column)
-        if reflection.polymorphic? && base_class = polymorphic_base_class_from_value(value)
+        base_class = polymorphic_base_class_from_value(value)
+
+        if reflection.polymorphic? && base_class
           queries << build(table[reflection.foreign_type], base_class)
         end
 

activerecord/test/cases/relation/where_test.rb
@@ -2,6 +2,7 @@ require "cases/helper"
 require "models/author"
 require "models/binary"
 require "models/cake_designer"
+require "models/category"
 require "models/chef"
 require "models/comment"
 require "models/edge"
@@ -283,6 +284,12 @@ module ActiveRecord
 
     def test_where_on_association_with_custom_primary_key_with_array_of_ids
       essay = Essay.where(writer: ["David"]).first
+
+      assert_equal essays(:david_modest_proposal), essay
+    end
+
+    def test_where_on_non_polymorphic_association_with_custom_primary_key
+      essay = Essay.where(category: [Category.new(name: "General")]).first
 
       assert_equal essays(:david_modest_proposal), essay
     end