92dce324b95678d142f8e1967a9ec16bf1efc250
Anton Davydov <antondavydov.o@gmail.com>
2015-02-28 20:46:21 +0300

    [ci skip] Add code examples for Module#anonymous? documentation

--- activesupport/lib/active_support/core_ext/module/anonymous.rb
+++ activesupport/lib/active_support/core_ext/module/anonymous.rb
@@ -7,6 +7,13 @@ class Module
   #   m = Module.new
   #   m.name # => nil
   #
+  # +anonymous?+ method returns true if module does not have a name:
+  #
+  #   Module.new.anonymous? # => true
+  #
+  #   module M; end
+  #   M.anonymous?          # => false
+  #
   # A module gets a name when it is first assigned to a constant. Either
   # via the +module+ or +class+ keyword or by an explicit assignment:
   #
