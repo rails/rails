8a5475cb5ffdba9b95d7ae1d2d323540522fc9d6
Sushruth Sivaramakrishnan <sivsushruth@gmail.com>
2015-03-05 11:32:51 +0530

    Doc fix [ci skip]

--- activesupport/lib/active_support/tagged_logging.rb
+++ activesupport/lib/active_support/tagged_logging.rb
@@ -43,7 +43,7 @@ module ActiveSupport
       end
 
       def current_tags
-        # We use our object ID here to void conflicting with other instances
+        # We use our object ID here to avoid conflicting with other instances
         thread_key = @thread_key ||= "activesupport_tagged_logging_tags:#{object_id}".freeze
         Thread.current[thread_key] ||= []
       end
