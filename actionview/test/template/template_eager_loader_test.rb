5ca681cace452b93ee6f2f27be00b55f28ea9fae
Juho Leinonen <messionyk@gmail.com>
2015-03-06 15:09:18 +0200

    Make TemplateEagerLoader tests more reliable

--- actionview/test/template/template_eager_loader_test.rb
+++ actionview/test/template/template_eager_loader_test.rb
@@ -26,13 +26,13 @@ class TemplateEagerLoaderTest < ActiveSupport::TestCase
     end
 
     def test_cache_templates_calls_find_all
-      resolver.expects(:find_all)
+      resolver.expects(:find_all).returns([])
       subject.eager_load
     end
 
     def test_compile_called
       template = mock
-      subject.stubs(:templates).returns([template])
+      resolver.stubs(:find_all).returns([template])
       template.expects(:compile!)
       subject.eager_load
     end
@@ -40,7 +40,7 @@ class TemplateEagerLoaderTest < ActiveSupport::TestCase
     def test_multiple_templates
       template1 = mock
       template2 = mock
-      subject.stubs(:templates).returns([template1, template2])
+      resolver.stubs(:find_all).returns([template1, template2])
       template1.expects(:compile!)
       template2.expects(:compile!)
       subject.eager_load
@@ -77,48 +77,48 @@ class TemplateEagerLoaderTest < ActiveSupport::TestCase
       end
 
       def test_cache_templates_parses_partial_names
-        resolver.expects(:find_all).with('partial', anything, anything, anything, anything, anything).at_least_once
+        resolver.expects(:find_all).with('partial', anything, anything, anything, anything, anything).returns([]).at_least_once
         subject.eager_load
       end
 
       def test_cache_templates_passes_right_locals_with_partial
-        resolver.expects(:find_all).with('partial', anything, anything, anything, anything, %w(partial partial_counter partial_iteration))
-        resolver.expects(:find_all).with('partial', anything, anything, anything, anything, [])
+        resolver.expects(:find_all).with('partial', anything, anything, anything, anything, %w(partial partial_counter partial_iteration)).returns([])
+        resolver.expects(:find_all).with('partial', anything, anything, anything, anything, []).returns([])
         subject.eager_load
       end
     end
 
     def test_partials_and_locals_correct_when_not_a_partial
-      resolver.expects(:find_all).with(anything, anything, false, anything, anything, [])
+      resolver.expects(:find_all).with(anything, anything, false, anything, anything, []).returns([])
       subject.eager_load
     end
 
     def test_not_being_partial_does_not_lead_into_multiple_caching_attemps
-      resolver.expects(:find_all).at_most_once
+      resolver.expects(:find_all).returns([]).at_most_once
       subject.eager_load
     end
 
     def test_details_have_correct_formats
-      resolver.expects(:find_all).with(anything, anything, anything, has_entry(formats: [:html]), anything, anything)
+      resolver.expects(:find_all).with(anything, anything, anything, has_entry(formats: [:html]), anything, anything).returns([])
       subject.eager_load
     end
 
     def test_formats_work_without_the_extension_in_filename
       subject.stubs(:path_names).with(anything).returns(['index.haml'])
-      resolver.expects(:find_all).with(anything, anything, anything, has_entry(formats: [:html]), anything, anything)
+      resolver.expects(:find_all).with(anything, anything, anything, has_entry(formats: [:html]), anything, anything).returns([])
       subject.eager_load
     end
 
     def test_details_locale_defaults_to_english
-      resolver.expects(:find_all).with(anything, anything, anything, has_entry(locale: [:en]), anything, anything)
+      resolver.expects(:find_all).with(anything, anything, anything, has_entry(locale: [:en]), anything, anything).returns([])
       subject.eager_load
     end
 
-    def test_details_locale_works_with_other_default_than_english
+    def test_details_with_other_default_than_english
       I18n.stubs(:available_locales).returns([:other, :en])
       I18n.stubs(:default_locale).returns([:other])
-      resolver.stubs(:find_all)
-      resolver.expects(:find_all).with(anything, anything, anything, has_entry(locale: [:other, :en]), anything, anything)
+      resolver.stubs(:find_all).returns([])
+      resolver.expects(:find_all).with(anything, anything, anything, has_entry(locale: [:other, :en]), anything, anything).returns([])
       subject.eager_load
     end
 
@@ -126,30 +126,30 @@ class TemplateEagerLoaderTest < ActiveSupport::TestCase
       I18n.stubs(:available_locales).returns([:fi, :en, :cn])
       I18n.stubs(:default_locale).returns([:cn])
       I18n.stubs(:locale).returns([:fi])
-      resolver.stubs(:find_all)
-      resolver.expects(:find_all).with(anything, anything, anything, has_entry(locale: [:fi, :en, :cn]), anything, anything)
+      resolver.stubs(:find_all).returns([])
+      resolver.expects(:find_all).with(anything, anything, anything, has_entry(locale: [:fi, :en, :cn]), anything, anything).returns([])
       subject.eager_load
     end
 
 
     def test_details_variants_is_not_set_when_view_is_not_a_variant
-      resolver.expects(:find_all).with(anything, anything, anything, has_entry(variants: []), anything, anything)
+      resolver.expects(:find_all).with(anything, anything, anything, has_entry(variants: []), anything, anything).returns([])
       subject.eager_load
     end
 
     def test_details_variants_get_set_correctly
       subject.stubs(:path_names).with(anything).returns(['index.html+variant.erb'])
-      resolver.expects(:find_all).with(anything, anything, anything, has_entry(variants: ['variant']), anything, anything)
+      resolver.expects(:find_all).with(anything, anything, anything, has_entry(variants: ['variant']), anything, anything).returns([])
       subject.eager_load
     end
 
     def test_cache_templates_action
-      resolver.expects(:find_all).with('index', anything, anything, anything, anything, anything)
+      resolver.expects(:find_all).with('index', anything, anything, anything, anything, anything).returns([])
       subject.eager_load
     end
 
     def test_cache_templates_prefix
-      resolver.expects(:find_all).with(anything, 'people', anything, anything, anything, anything)
+      resolver.expects(:find_all).with(anything, 'people', anything, anything, anything, anything).returns([])
       subject.eager_load
     end
   end
@@ -159,7 +159,7 @@ class TemplateEagerLoaderTest < ActiveSupport::TestCase
       super
       subject.stubs(:path_names).with('people/assets').returns(['index.html.erb'])
       subject.stubs(:view_paths).returns(['app/views/people/assets/index.html.erb'])
-      resolver.stubs(:find_all)
+      resolver.stubs(:find_all).returns([])
     end
 
     def test_two_layer_nested_prefixes_calls_view_paths
@@ -168,7 +168,7 @@ class TemplateEagerLoaderTest < ActiveSupport::TestCase
     end
 
     def test_two_layer_nested_prefixes_returns_desired_results
-      resolver.expects(:find_all).with(anything,'people/assets', anything, anything, anything, anything)
+      resolver.expects(:find_all).with(anything,'people/assets', anything, anything, anything, anything).returns([])
       subject.eager_load
     end
   end
@@ -176,7 +176,7 @@ class TemplateEagerLoaderTest < ActiveSupport::TestCase
   class Prefixes < TemplateEagerLoaderTest
     def setup
       super
-      resolver.stubs(:find_all)
+      resolver.stubs(:find_all).returns([])
     end
 
     def test_prefixes_calls_view_paths
@@ -185,7 +185,7 @@ class TemplateEagerLoaderTest < ActiveSupport::TestCase
     end
 
     def test_prefixes_returns_desired_results
-      resolver.expects(:find_all).with(anything,'people', anything, anything, anything, anything)
+      resolver.expects(:find_all).with(anything,'people', anything, anything, anything, anything).returns([])
       subject.eager_load
     end
 
@@ -193,8 +193,8 @@ class TemplateEagerLoaderTest < ActiveSupport::TestCase
       subject.stubs(:prefixes).returns(['prefix1', 'prefix2'])
       subject.stubs(:path_names).with('prefix1').returns(['index.html.erb'])
       subject.stubs(:path_names).with('prefix2').returns(['index.html.erb'])
-      resolver.expects(:find_all).with(anything, 'prefix1', anything, anything, anything, anything)
-      resolver.expects(:find_all).with(anything, 'prefix2', anything, anything, anything, anything)
+      resolver.expects(:find_all).with(anything, 'prefix1', anything, anything, anything, anything).returns([])
+      resolver.expects(:find_all).with(anything, 'prefix2', anything, anything, anything, anything).returns([])
       subject.eager_load
     end
   end
