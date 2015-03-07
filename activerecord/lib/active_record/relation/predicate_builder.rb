1a8101eb2e37abd808854e0e4661449c5d3c77b7
Ben Woosley <ben.woosley@gmail.com>
2015-03-06 18:49:54 -0800

    Expose PredicateBuilder#convert_dot_notation_to_hash
    This enables us to simplify PredicateBuilder a bit by collapsing
    
    We also optimize WhereClauseFactory a bit by preventing it from
    calling #convert_dot_notation_to_hash twice.

--- activerecord/lib/active_record/relation/predicate_builder.rb
+++ activerecord/lib/active_record/relation/predicate_builder.rb
@@ -23,14 +23,54 @@ module ActiveRecord
       register_handler(AssociationQueryValue, AssociationQueryHandler.new(self))
     end
 
+    def convert_dot_notation_to_hash(attributes)
+      attributes = attributes.stringify_keys
+      dot_notation = attributes.keys.select { |s| s.include?(".") }
+
+      dot_notation.each do |key|
+        table_name, column_name = key.split(".")
+        value = attributes.delete(key)
+        attributes[table_name] ||= {}
+
+        attributes[table_name] = attributes[table_name].merge(column_name => value)
+      end
+
+      attributes
+    end
+
     def build_from_hash(attributes)
-      attributes = convert_dot_notation_to_hash(attributes)
-      expand_from_hash(attributes)
+      return ["1=0"] if attributes.empty?
+
+      attributes.flat_map do |key, value|
+        if value.is_a?(Hash)
+          associated_predicate_builder(key).build_from_hash(value)
+        else
+          expand(key, value)
+        end
+      end
     end
 
     def create_binds(attributes)
-      attributes = convert_dot_notation_to_hash(attributes)
-      create_binds_for_hash(attributes)
+      result = attributes.dup
+      binds = []
+
+      attributes.each do |column_name, value|
+        case value
+        when Hash
+          attrs, bvs = associated_predicate_builder(column_name).create_binds(value)
+          result[column_name] = attrs
+          binds += bvs
+        when Relation
+          binds += value.bound_attributes
+        else
+          if can_be_bound?(column_name, value)
+            result[column_name] = Arel::Nodes::BindParam.new
+            binds << Relation::QueryAttribute.new(column_name.to_s, value, table.type(column_name))
+          end
+        end
+      end
+
+      [result, binds]
     end
 
     def expand(column, value)
@@ -80,61 +120,10 @@ module ActiveRecord
 
     attr_reader :table
 
-    def expand_from_hash(attributes)
-      return ["1=0"] if attributes.empty?
-
-      attributes.flat_map do |key, value|
-        if value.is_a?(Hash)
-          associated_predicate_builder(key).expand_from_hash(value)
-        else
-          expand(key, value)
-        end
-      end
-    end
-
-
-    def create_binds_for_hash(attributes)
-      result = attributes.dup
-      binds = []
-
-      attributes.each do |column_name, value|
-        case value
-        when Hash
-          attrs, bvs = associated_predicate_builder(column_name).create_binds_for_hash(value)
-          result[column_name] = attrs
-          binds += bvs
-        when Relation
-          binds += value.bound_attributes
-        else
-          if can_be_bound?(column_name, value)
-            result[column_name] = Arel::Nodes::BindParam.new
-            binds << Relation::QueryAttribute.new(column_name.to_s, value, table.type(column_name))
-          end
-        end
-      end
-
-      [result, binds]
-    end
-
     private
 
     def associated_predicate_builder(association_name)
       self.class.new(table.associated_table(association_name))
-    end
-
-    def convert_dot_notation_to_hash(attributes)
-      attributes = attributes.stringify_keys
-      dot_notation = attributes.keys.select { |s| s.include?(".") }
-
-      dot_notation.each do |key|
-        table_name, column_name = key.split(".")
-        value = attributes.delete(key)
-        attributes[table_name] ||= {}
-
-        attributes[table_name] = attributes[table_name].merge(column_name => value)
-      end
-
-      attributes
     end
 
     def handler_for(object)
