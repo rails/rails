f9369c0f51e8c404dd12024037a055594bf0ace2
sshaw <skye.shaw@gmail.com>
2015-03-06 22:15:36 -0500

    More docs on tagged logging [ci skip]
    Mention that tags can also come from a Proc that accepts the request
    object or something's to_s method

--- railties/lib/rails/rack/logger.rb
+++ railties/lib/rails/rack/logger.rb
@@ -7,6 +7,9 @@ require 'rack/body_proxy'
 module Rails
   module Rack
     # Sets log tags, logs the request, calls the app, and flushes the logs.
+    #
+    # Log tags can be an Array containing: methods that the <tt>request</tt> object responds to, a Proc
+    # that accepts an instance of the <tt>request</tt> object, or something that responds to <tt>to_s</tt>.
     class Logger < ActiveSupport::LogSubscriber
       def initialize(app, taggers = nil)
         @app          = app
