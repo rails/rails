5f6fc9e389e90d44cf07318f0c3c51a12dfcb6a5
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-03-15 08:57:06 +0900

    add mailer suffix to comments for email previews

--- railties/lib/rails/generators/test_unit/mailer/templates/preview.rb
+++ railties/lib/rails/generators/test_unit/mailer/templates/preview.rb
@@ -1,9 +1,9 @@
 <% module_namespacing do -%>
-# Preview all emails at http://localhost:3000/rails/mailers/<%= file_path %>
+# Preview all emails at http://localhost:3000/rails/mailers/<%= file_path %>_mailer
 class <%= class_name %>MailerPreview < ActionMailer::Preview
 <% actions.each do |action| -%>
 
-  # Preview this email at http://localhost:3000/rails/mailers/<%= file_path %>/<%= action %>
+  # Preview this email at http://localhost:3000/rails/mailers/<%= file_path %>_mailer/<%= action %>
   def <%= action %>
     <%= class_name %>Mailer.<%= action %>
   end
