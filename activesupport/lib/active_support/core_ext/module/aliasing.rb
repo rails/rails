a982a42d766169c2170d7f100c2a5ceb5430efb1
Kir Shatrov <shatrov@me.com>
2015-03-20 23:23:41 +0200

    Deprecate alias_method_chain in favour of Module#prepend …as discussed #19413

--- activesupport/lib/active_support/core_ext/module/aliasing.rb
+++ activesupport/lib/active_support/core_ext/module/aliasing.rb
@@ -21,6 +21,8 @@ class Module
   #
   # so you can safely chain foo, foo?, foo! and/or foo= with the same feature.
   def alias_method_chain(target, feature)
+    ActiveSupport::Deprecation.warn("alias_method_chain is deprecated. Please, use Module#prepend instead. From module, you can access the original method using super.")
+
     # Strip out punctuation on predicates, bang or writer methods since
     # e.g. target?_without_feature is not a valid method name.
     aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
