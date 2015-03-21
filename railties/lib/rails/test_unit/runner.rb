a8bf335793881c1710a5a8e3a604a75a5bb3b199
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-03-21 16:27:43 +0900

    use `Dir.exist?` instead of deprecated `Dir.exists?`

--- railties/lib/rails/test_unit/runner.rb
+++ railties/lib/rails/test_unit/runner.rb
@@ -63,7 +63,7 @@ module Rails
             options[:line] &&= options[:line].to_i
           else
             arg = arg.gsub(':', '')
-            if Dir.exists?("#{arg}")
+            if Dir.exist?("#{arg}")
               options[:patterns] << File.expand_path("#{arg}/**/*_test.rb")
             elsif File.file?(arg)
               options[:patterns] << File.expand_path(arg)
