3f0de070a8a539c83c7605d371516a73c8938303
Gaurav Sharma <gaurav2728@gmail.com>
2015-03-23 11:32:56 +0530

    update mysql link that has been replaced

--- activerecord/lib/active_record/connection_adapters/abstract_mysql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/abstract_mysql_adapter.rb
@@ -915,7 +915,7 @@ module ActiveRecord
         variables['wait_timeout'] = self.class.type_cast_config_to_integer(wait_timeout)
 
         # Make MySQL reject illegal values rather than truncating or blanking them, see
-        # http://dev.mysql.com/doc/refman/5.6/en/server-sql-mode.html#sqlmode_strict_all_tables
+        # http://dev.mysql.com/doc/refman/5.6/en/sql-mode.html#sqlmode_strict_all_tables
         # If the user has provided another value for sql_mode, don't replace it.
         unless variables.has_key?('sql_mode')
           variables['sql_mode'] = strict_mode? ? 'STRICT_ALL_TABLES' : ''
