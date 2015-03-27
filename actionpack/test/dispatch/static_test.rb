0a6dd5ddc30e7e50f378ccdfee3b02c38e25a3a9
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2015-03-28 02:01:54 +0900

    add tests to change Encoding.default_(internal/external) for actionpack/test/dispatch/static_test.rb

--- actionpack/test/dispatch/static_test.rb
+++ actionpack/test/dispatch/static_test.rb
@@ -4,6 +4,16 @@ require 'rbconfig'
 require 'zlib'
 
 module StaticTests
+  def setup
+    @default_internal_encoding = Encoding.default_internal
+    @default_external_encoding = Encoding.default_external
+  end
+
+  def teardown
+    Encoding.default_internal = @default_internal_encoding
+    Encoding.default_external = @default_external_encoding
+  end
+
   def test_serves_dynamic_content
     assert_equal "Hello, World!", get("/nofile").body
   end
@@ -13,6 +23,12 @@ module StaticTests
   end
 
   def test_handles_urls_with_ascii_8bit
+    assert_equal "Hello, World!", get("/doorkeeper%E3E4".force_encoding('ASCII-8BIT')).body
+  end
+
+  def test_handles_urls_with_ascii_8bit_on_win_31j
+    Encoding.default_internal = "Windows-31J"
+    Encoding.default_external = "Windows-31J"
     assert_equal "Hello, World!", get("/doorkeeper%E3E4".force_encoding('ASCII-8BIT')).body
   end
 
@@ -214,6 +230,7 @@ class StaticTest < ActiveSupport::TestCase
   }
 
   def setup
+    super
     @root = "#{FIXTURE_LOAD_PATH}/public"
     @app = ActionDispatch::Static.new(DummyApp, @root, "public, max-age=60")
   end
@@ -243,6 +260,7 @@ end
 
 class StaticEncodingTest < StaticTest
   def setup
+    super
     @root = "#{FIXTURE_LOAD_PATH}/公共"
     @app = ActionDispatch::Static.new(DummyApp, @root, "public, max-age=60")
   end
