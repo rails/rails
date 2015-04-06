48a735aff7032afaf5534613b10c635acead042a
Matthew Draper <matthew@trebex.net>
2015-04-07 05:51:43 +0930

    Fix the Interlock middleware
    We can't actually lean on Rack::Lock's implementation, so we'll just
    copy it instead. It's simple enough that that's not too troubling.

--- actionpack/lib/action_dispatch/middleware/load_interlock.rb
+++ actionpack/lib/action_dispatch/middleware/load_interlock.rb
@@ -1,12 +1,21 @@
 require 'active_support/dependencies'
-require 'rack/lock'
+require 'rack/body_proxy'
 
 module ActionDispatch
-  class LoadInterlock < ::Rack::Lock
-    FLAG = 'activesupport.dependency_race'.freeze
+  class LoadInterlock
+    def initialize(app)
+      @app = app
+    end
 
-    def initialize(app, mutex = ::ActiveSupport::Dependencies.interlock)
-      super
+    def call(env)
+      interlock = ActiveSupport::Dependencies.interlock
+      interlock.start_running
+      response = @app.call(env)
+      body = Rack::BodyProxy.new(response[2]) { interlock.done_running }
+      response[2] = body
+      response
+    ensure
+      interlock.done_running unless body
     end
   end
 end
