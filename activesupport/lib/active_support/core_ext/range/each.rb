d3684c4154df66059ba77d022eca285563b8f506
Yuki Nishijima <mail@yukinishijima.net>
2015-04-13 20:15:08 -0800

    Use #prepend rather than using 2 aliases

--- activesupport/lib/active_support/core_ext/range/each.rb
+++ activesupport/lib/active_support/core_ext/range/each.rb
@@ -1,27 +1,21 @@
-class Range #:nodoc:
-
-  def each_with_time_with_zone(&block)
-    ensure_iteration_allowed
-    each_without_time_with_zone(&block)
-  end
-  # TODO: change to Module#prepend as soon as the fix is backported to MRI 2.2:
-  # https://bugs.ruby-lang.org/issues/10847
-  alias_method :each_without_time_with_zone, :each
-  alias_method :each, :each_with_time_with_zone
-
-  def step_with_time_with_zone(n = 1, &block)
-    ensure_iteration_allowed
-    step_without_time_with_zone(n, &block)
-  end
-  # TODO: change to Module#prepend as soon as the fix is backported to MRI 2.2:
-  # https://bugs.ruby-lang.org/issues/10847
-  alias_method :step_without_time_with_zone, :step
-  alias_method :step, :step_with_time_with_zone
-
-  private
-  def ensure_iteration_allowed
-    if first.is_a?(Time)
-      raise TypeError, "can't iterate from #{first.class}"
+module ActiveSupport
+  module EachTimeWithZone #:nodoc:
+    def each(&block)
+      ensure_iteration_allowed
+      super
     end
+
+    def step(n = 1, &block)
+      ensure_iteration_allowed
+      super
+    end
+
+    private
+
+      def ensure_iteration_allowed
+        raise TypeError, "can't iterate from #{first.class}" if first.is_a?(Time)
+      end
   end
 end
+
+Range.prepend(ActiveSupport::EachTimeWithZone)
