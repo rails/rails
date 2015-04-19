8b09b45458cf9c7117f84e99449f32084e123db7
Matthew Draper <matthew@trebex.net>
2015-04-20 09:10:08 +0930

    Only *configure* the queue in setup; DB creation must come later
    setup gets called from the initializer, so it happens more than once in
    a test run. Trying to drop the database again after the first process is
    connected is.. ineffective. And entirely pointless.
    
    Instead, defer creating the database to start_workers -- which only
    happens once, right before we start doing anything real.

--- activejob/test/support/integration/adapters/queue_classic.rb
+++ activejob/test/support/integration/adapters/queue_classic.rb
@@ -3,17 +3,7 @@ module QueueClassicJobsManager
     ENV['QC_DATABASE_URL'] ||= 'postgres:///active_jobs_qc_int_test'
     ENV['QC_RAILS_DATABASE'] = 'false'
     ENV['QC_LISTEN_TIME']    = "0.5"
-    uri = URI.parse(ENV['QC_DATABASE_URL'])
-    user = uri.user||ENV['USER']
-    pass = uri.password
-    db   = uri.path[1..-1]
-    %x{#{"PGPASSWORD=\"#{pass}\"" if pass} psql -c 'drop database if exists "#{db}"' -U #{user} -t template1}
-    %x{#{"PGPASSWORD=\"#{pass}\"" if pass} psql -c 'create database "#{db}"' -U #{user} -t template1}
     ActiveJob::Base.queue_adapter = :queue_classic
-    QC::Setup.create
-  rescue PG::ConnectionBad
-    puts "Cannot run integration tests for queue_classic. To be able to run integration tests for queue_classic you need to install and start postgresql.\n"
-    exit
   end
 
   def clear_jobs
@@ -21,12 +11,24 @@ module QueueClassicJobsManager
   end
 
   def start_workers
+    uri = URI.parse(ENV['QC_DATABASE_URL'])
+    user = uri.user||ENV['USER']
+    pass = uri.password
+    db   = uri.path[1..-1]
+    %x{#{"PGPASSWORD=\"#{pass}\"" if pass} psql -c 'drop database if exists "#{db}"' -U #{user} -t template1}
+    %x{#{"PGPASSWORD=\"#{pass}\"" if pass} psql -c 'create database "#{db}"' -U #{user} -t template1}
+    QC::Setup.create
+
     QC.default_conn_adapter.disconnect
     QC.default_conn_adapter = nil
     @pid = fork do
       worker = QC::Worker.new(q_name: 'integration_tests')
       worker.start
     end
+
+  rescue PG::ConnectionBad
+    puts "Cannot run integration tests for queue_classic. To be able to run integration tests for queue_classic you need to install and start postgresql.\n"
+    exit
   end
 
   def stop_workers
