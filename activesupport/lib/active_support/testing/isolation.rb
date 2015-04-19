61103ac7b0801201533e2c853957da4d80d81b51
Alexey Gaziev <alex.gaziev@gmail.com>
2015-04-19 12:18:16 +0800

    MRI compatible code for ActiveSupport::Testing::Isolation::Subprocess

--- activesupport/lib/active_support/testing/isolation.rb
+++ activesupport/lib/active_support/testing/isolation.rb
@@ -69,17 +69,17 @@ module ActiveSupport
           else
             Tempfile.open("isolation") do |tmpfile|
               env = {
-                ISOLATION_TEST: self.class.name,
-                ISOLATION_OUTPUT: tmpfile.path
+                'ISOLATION_TEST' => self.class.name,
+                'ISOLATION_OUTPUT' => tmpfile.path
               }
 
               load_paths = $-I.map {|p| "-I\"#{File.expand_path(p)}\"" }.join(" ")
               orig_args = ORIG_ARGV.join(" ")
               test_opts = "-n#{self.class.name}##{self.name}"
-              command = "#{Gem.ruby} #{load_paths} #{$0} #{orig_args} #{test_opts}"
+              command = "#{Gem.ruby} #{load_paths} #{$0} '#{orig_args}' #{test_opts}"
 
               # IO.popen lets us pass env in a cross-platform way
-              child = IO.popen([env, command])
+              child = IO.popen(env, command)
 
               begin
                 Process.wait(child.pid)
