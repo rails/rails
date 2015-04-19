22fb5a77b623dd2e735e122bbcbc70b886e1d6f9
Godfrey Chan <godfreykfc@gmail.com>
2015-04-19 15:12:19 -0700

    Add a "generic" bug report template
    This template gives contributors a starting point to use when reporting bugs
    that does not involve Active Record or Action Pack.

---
+++ guides/bug_report_templates/generic_gem.rb
@@ -0,0 +1,15 @@
+# Activate the gems you are reporting the issue against.
+gem 'activesupport', '4.0.0'
+require 'active_support'
+require 'active_support/core_ext/object/blank'
+require 'minitest/autorun'
+
+# Ensure backward compatibility with Minitest 4
+Minitest::Test = MiniTest::Unit::TestCase unless defined?(Minitest::Test)
+
+class BugTest < Minitest::Test
+  def test_stuff
+    assert "zomg".present?
+    refute "".present?
+  end
+end
