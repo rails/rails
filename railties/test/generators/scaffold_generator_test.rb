451b1e35746fc1f7089200ac20811080089ef192
Santiago Pastorino <santiago@wyeworks.com>
2015-04-19 20:16:57 -0400

    Add api scaffold test for route, controller and its tests

--- railties/test/generators/scaffold_generator_test.rb
+++ railties/test/generators/scaffold_generator_test.rb
@@ -87,6 +87,61 @@ class ScaffoldGeneratorTest < Rails::Generators::TestCase
     assert_file "app/assets/stylesheets/product_lines.css"
   end
 
+  def test_api_scaffold_on_invoke
+    run_generator %w(product_line title:string product:belongs_to user:references --api)
+
+    # Model
+    assert_file "app/models/product_line.rb", /class ProductLine < ActiveRecord::Base/
+    assert_file "test/models/product_line_test.rb", /class ProductLineTest < ActiveSupport::TestCase/
+    assert_file "test/fixtures/product_lines.yml"
+    assert_migration "db/migrate/create_product_lines.rb", /belongs_to :product, index: true/
+    assert_migration "db/migrate/create_product_lines.rb", /references :user, index: true/
+
+    # Route
+    assert_file "config/routes.rb" do |route|
+      assert_match(/resources :product_lines, except: \[:new, :edit\]$/, route)
+    end
+
+    # Controller
+    assert_file "app/controllers/product_lines_controller.rb" do |content|
+      assert_match(/class ProductLinesController < ApplicationController/, content)
+      assert_no_match(/respond_to/, content)
+
+      assert_match(/before_action :set_product_line, only: \[:show, :update, :destroy\]/, content)
+
+      assert_instance_method :index, content do |m|
+        assert_match(/@product_lines = ProductLine\.all/, m)
+        assert_match(/render json: @product_lines/, m)
+      end
+
+      assert_instance_method :show, content do |m|
+        assert_match(/render json: @product_line/, m)
+      end
+
+      assert_instance_method :create, content do |m|
+        assert_match(/@product_line = ProductLine\.new\(product_line_params\)/, m)
+        assert_match(/@product_line\.save/, m)
+        assert_match(/@product_line\.errors/, m)
+      end
+
+      assert_instance_method :update, content do |m|
+        assert_match(/@product_line\.update\(product_line_params\)/, m)
+        assert_match(/@product_line\.errors/, m)
+      end
+
+      assert_instance_method :destroy, content do |m|
+        assert_match(/@product_line\.destroy/, m)
+      end
+    end
+
+    assert_file "test/controllers/product_lines_controller_test.rb" do |test|
+      assert_match(/class ProductLinesControllerTest < ActionController::TestCase/, test)
+      assert_match(/post :create, params: \{ product_line: \{ product_id: @product_line\.product_id, title: @product_line\.title, user_id: @product_line\.user_id \} \}/, test)
+      assert_match(/patch :update, params: \{ id: @product_line, product_line: \{ product_id: @product_line\.product_id, title: @product_line\.title, user_id: @product_line\.user_id \} \}/, test)
+      assert_no_match(/assert_redirected_to/, test)
+    end
+  end
+
   def test_functional_tests_without_attributes
     run_generator ["product_line"]
 
