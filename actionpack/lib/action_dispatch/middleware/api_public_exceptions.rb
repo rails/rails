4204778f8d26582c77fe6c1ff41d39fc523e50c0
Santiago Pastorino <santiago@wyeworks.com>
2015-04-21 16:16:02 -0400

    Adhere to Rails convention for private indentation

--- actionpack/lib/action_dispatch/middleware/api_public_exceptions.rb
+++ actionpack/lib/action_dispatch/middleware/api_public_exceptions.rb
@@ -17,31 +17,30 @@ module ActionDispatch
     end
 
     private
-
-    def render(status, content_type, body)
-      format = content_type && "to_#{content_type.to_sym}"
-      if format && body.respond_to?(format)
-        render_format(status, content_type, body.public_send(format))
-      else
-        render_html(status)
+      def render(status, content_type, body)
+        format = content_type && "to_#{content_type.to_sym}"
+        if format && body.respond_to?(format)
+          render_format(status, content_type, body.public_send(format))
+        else
+          render_html(status)
+        end
       end
-    end
 
-    def render_format(status, content_type, body)
-      [status, {'Content-Type' => "#{content_type}; charset=#{ActionDispatch::Response.default_charset}",
-                'Content-Length' => body.bytesize.to_s}, [body]]
-    end
-
-    def render_html(status)
-      found = false
-      path = "#{public_path}/#{status}.#{I18n.locale}.html" if I18n.locale
-      path = "#{public_path}/#{status}.html" unless path && (found = File.exist?(path))
-
-      if found || File.exist?(path)
-        render_format(status, 'text/html', File.read(path))
-      else
-        [404, { "X-Cascade" => "pass" }, []]
+      def render_format(status, content_type, body)
+        [status, {'Content-Type' => "#{content_type}; charset=#{ActionDispatch::Response.default_charset}",
+                  'Content-Length' => body.bytesize.to_s}, [body]]
       end
-    end
+
+      def render_html(status)
+        found = false
+        path = "#{public_path}/#{status}.#{I18n.locale}.html" if I18n.locale
+        path = "#{public_path}/#{status}.html" unless path && (found = File.exist?(path))
+
+        if found || File.exist?(path)
+          render_format(status, 'text/html', File.read(path))
+        else
+          [404, { "X-Cascade" => "pass" }, []]
+        end
+      end
   end
 end
