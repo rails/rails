c9e354ba4e99a3474569e158ce46efd58451ce65
Yves Senn <yves.senn@gmail.com>
2015-04-27 12:10:43 +0200

    test, for `create_table` and `foreign_key: true` no-op. Closes #19794.
    Add a test-case to make sure that `create_table` with
    a `foreign_key: true` and an adapter without foreign key support
    does not blow up.
    
    Motivated by #19794.
    
    Originating from: https://github.com/rails/rails/commit/99a6f9e60ea55924b44f894a16f8de0162cf2702#commitcomment-10855210

--- activerecord/test/cases/migration/references_foreign_key_test.rb
+++ activerecord/test/cases/migration/references_foreign_key_test.rb
@@ -130,4 +130,24 @@ module ActiveRecord
     end
   end
 end
+else
+class ReferencesWithoutForeignKeySupportTest < ActiveRecord::TestCase
+  setup do
+    @connection = ActiveRecord::Base.connection
+    @connection.create_table(:testing_parents, force: true)
+  end
+
+  teardown do
+    @connection.drop_table("testings", if_exists: true)
+    @connection.drop_table("testing_parents", if_exists: true)
+  end
+
+  test "ignores foreign keys defined with the table" do
+    @connection.create_table :testings do |t|
+      t.references :testing_parent, foreign_key: true
+    end
+
+    assert_includes @connection.tables, "testings"
+  end
+end
 end
