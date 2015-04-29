ef98a3b0addcba889aeac7fcb6ad71d2edf7942b
Thiago Augusto <thiago.android88@gmail.com>
2015-04-29 00:17:27 -0300

    Update routing_test.rb 'and' operator
    Modified 'and' to '&&' operator

--- actionpack/test/controller/routing_test.rb
+++ actionpack/test/controller/routing_test.rb
@@ -211,7 +211,7 @@ class LegacyRouteSetTests < ActiveSupport::TestCase
   def test_class_and_lambda_constraints
     subdomain = Class.new {
       def matches? request
-        request.subdomain.present? and request.subdomain != 'clients'
+        request.subdomain.present? && request.subdomain != 'clients'
       end
     }
 
