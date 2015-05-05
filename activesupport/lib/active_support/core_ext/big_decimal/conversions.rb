f61dd4f9d4c48b1d447b499d0541d2f6421aedd7
Bernard Potocki <bernard.potocki@imanel.org>
2015-05-05 23:57:56 +0200

    Unify behavior of all Numeric extensions and use Module.prepend instead of alias_method

--- activesupport/lib/active_support/core_ext/big_decimal/conversions.rb
+++ activesupport/lib/active_support/core_ext/big_decimal/conversions.rb
@@ -1,15 +1,14 @@
 require 'bigdecimal'
 require 'bigdecimal/util'
 
-class BigDecimal
-  DEFAULT_STRING_FORMAT = 'F'
-  alias_method :to_default_s, :to_s
+module ActiveSupport
+  module BigDecimalWithDefaultFormat #:nodoc:
+    DEFAULT_STRING_FORMAT = 'F'
 
-  def to_s(format = nil, options = nil)
-    if format.is_a?(Symbol)
-      to_formatted_s(format, options || {})
-    else
-      to_default_s(format || DEFAULT_STRING_FORMAT)
+    def to_s(format = nil)
+      super(format || DEFAULT_STRING_FORMAT)
     end
   end
 end
+
+BigDecimal.prepend(ActiveSupport::BigDecimalWithDefaultFormat)
