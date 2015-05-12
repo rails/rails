0ce547b60964885c12e7055205869d1e4ba4e46b
Vladislav Petrov <electronicchest@gmail.com>
2015-05-13 01:59:22 +0300

    Move setting locale to mailers controller

--- railties/lib/rails/application_controller.rb
+++ railties/lib/rails/application_controller.rb
@@ -2,8 +2,6 @@ class Rails::ApplicationController < ActionController::Base # :nodoc:
   self.view_paths = File.expand_path('../templates', __FILE__)
   layout 'application'
 
-  before_action :set_locale
-
   protected
 
   def require_local!
@@ -14,9 +12,5 @@ class Rails::ApplicationController < ActionController::Base # :nodoc:
 
   def local_request?
     Rails.application.config.consider_all_requests_local || request.local?
-  end
-
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
