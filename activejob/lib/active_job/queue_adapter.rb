17c10038c5c34b3050628b7ea20d59e16dc00dbc
Gourav Tiwari <gouravtiwari21@gmail.com>
2015-05-19 09:30:37 -0700

    doc changes for readability [ci skip]

--- activejob/lib/active_job/queue_adapter.rb
+++ activejob/lib/active_job/queue_adapter.rb
@@ -4,7 +4,7 @@ require 'active_support/core_ext/string/inflections'
 
 module ActiveJob
   # The <tt>ActiveJob::QueueAdapter</tt> module is used to load the
-  # correct adapter. The default queue adapter is the :inline queue.
+  # correct adapter. The default queue adapter is the +:inline+ queue.
   module QueueAdapter #:nodoc:
     extend ActiveSupport::Concern
 
@@ -20,7 +20,7 @@ module ActiveJob
       end
 
       # Specify the backend queue provider. The default queue adapter
-      # is the :inline queue. See QueueAdapters for more
+      # is the +:inline+ queue. See QueueAdapters for more
       # information.
       def queue_adapter=(name_or_adapter_or_class)
         self._queue_adapter = interpret_adapter(name_or_adapter_or_class)
