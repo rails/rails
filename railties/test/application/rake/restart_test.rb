6fc83f8efeb1637af5d17b0d7b0c4b2e3db3c001
Yoong Kang Lim <yoongkang.lim@gmail.com>
2015-05-26 20:47:36 +1000

    `rake restart` should work without a `tmp` folder
    In restart.rake, the creation of tmp/restart.txt
    would fail if the tmp folder does not exist in the
    app. This is a problem because apps cloned using
    git would not have the tmp folder, as the folder is
    in .gitignore. This commit creates the tmp folder
    if it does not exist.
    
    Fixes #20299
    
    [Yoong Kang Lim, Sunny Juneja]

--- railties/test/application/rake/restart_test.rb
+++ railties/test/application/rake/restart_test.rb
@@ -13,17 +13,25 @@ module ApplicationTests
       def teardown
         teardown_app
       end
-      
+
       test 'rake restart touches tmp/restart.txt' do
         Dir.chdir(app_path) do
           `rake restart`
           assert File.exist?("tmp/restart.txt")
-          
+
           prev_mtime = File.mtime("tmp/restart.txt")
           sleep(1)
           `rake restart`
           curr_mtime = File.mtime("tmp/restart.txt")
           assert_not_equal prev_mtime, curr_mtime
+        end
+      end
+
+      test 'rake restart should work even if tmp folder does not exist' do
+        Dir.chdir(app_path) do
+          FileUtils.remove_dir('tmp')
+          `rake restart`
+          assert File.exist?('tmp/restart.txt')
         end
       end
     end
