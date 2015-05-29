af1a61745a8fdab129b83536c324f667a50fafbf
Roque Pinel <repinel@gmail.com>
2015-05-29 10:57:15 -0500

    Revert "Replace use of alias chains with prepend at core_ext/date and core_ext/time"

--- activesupport/lib/active_support/core_ext/date/calculations.rb
+++ activesupport/lib/active_support/core_ext/date/calculations.rb
@@ -1,13 +1,12 @@
 require 'date'
+require 'active_support/duration'
 require 'active_support/core_ext/object/acts_like'
 require 'active_support/core_ext/date/zones'
-require 'active_support/core_ext/date/operators'
 require 'active_support/core_ext/time/zones'
 require 'active_support/core_ext/date_and_time/calculations'
 
 class Date
   include DateAndTime::Calculations
-  prepend ActiveSupport::DateOperators
 
   class << self
     attr_accessor :beginning_of_week_default
@@ -86,6 +85,26 @@ class Date
   end
   alias :at_end_of_day :end_of_day
 
+  def plus_with_duration(other) #:nodoc:
+    if ActiveSupport::Duration === other
+      other.since(self)
+    else
+      plus_without_duration(other)
+    end
+  end
+  alias_method :plus_without_duration, :+
+  alias_method :+, :plus_with_duration
+
+  def minus_with_duration(other) #:nodoc:
+    if ActiveSupport::Duration === other
+      plus_with_duration(-other)
+    else
+      minus_without_duration(other)
+    end
+  end
+  alias_method :minus_without_duration, :-
+  alias_method :-, :minus_with_duration
+
   # Provides precise Date calculations for years, months, and days. The +options+ parameter takes a hash with
   # any of these keys: <tt>:years</tt>, <tt>:months</tt>, <tt>:weeks</tt>, <tt>:days</tt>.
   def advance(options)
@@ -110,4 +129,15 @@ class Date
       options.fetch(:day, day)
     )
   end
+  
+  # Allow Date to be compared with Time by converting to DateTime and relying on the <=> from there.
+  def compare_with_coercion(other)
+    if other.is_a?(Time)
+      self.to_datetime <=> other
+    else
+      compare_without_coercion(other)
+    end
+  end
+  alias_method :compare_without_coercion, :<=>
+  alias_method :<=>, :compare_with_coercion
 end
