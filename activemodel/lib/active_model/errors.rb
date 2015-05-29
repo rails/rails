40543046039d8985a899cda796b686ee16d6e9bd
Manish Puri <manishspuri@gmail.com>
2015-05-29 15:37:33 -0400

    formatting changes

--- activemodel/lib/active_model/errors.rb
+++ activemodel/lib/active_model/errors.rb
@@ -43,11 +43,11 @@ module ActiveModel
   #     end
   #   end
   #
-  # The last three methods are required in your object for Errors to be
+  # The last three methods are required in your object for +Errors+ to be
   # able to generate error messages correctly and also handle multiple
   # languages. Of course, if you extend your object with ActiveModel::Translation
   # you will not need to implement the last two. Likewise, using
-  # ActiveModel::Validations will handle the validation related methods
+  # <tt>ActiveModel::Validations</tt> will handle the validation related methods
   # for you.
   #
   # The above allows you to do:
@@ -225,7 +225,7 @@ module ActiveModel
     end
     alias :blank? :empty?
 
-    # Returns an xml formatted representation of the Errors hash.
+    # Returns an xml formatted representation of the +Errors+ hash.
     #
     #   person.errors.add(:name, :blank, message: "can't be blank")
     #   person.errors.add(:name, :not_specified, message: "must be specified")
@@ -342,7 +342,7 @@ module ActiveModel
     end
 
     # Will add an error message to each of the attributes in +attributes+ that
-    # is blank (using Object#blank?).
+    # is blank (using <tt>Object#blank?</tt>).
     #
     #   person.errors.add_on_blank(:name)
     #   person.errors.messages
