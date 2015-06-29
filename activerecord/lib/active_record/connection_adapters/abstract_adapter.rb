e975d7cd1a6cb177f914024ffec8dd9a6cdc4ba1
Jori Hardman <jorihardman@gmail.com>
2015-06-29 11:37:05 -0500

    Ensure that microsecond precision is only used for version of mysql that support it. Fixes #19711

--- activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
@@ -107,6 +107,18 @@ module ActiveRecord
         @prepared_statements = false
       end
 
+      class Version
+        include Comparable
+
+        def initialize(version_string)
+          @version = version_string.split('.').map(&:to_i)
+        end
+
+        def <=>(version_string)
+          @version <=> version_string.split('.').map(&:to_i)
+        end
+      end
+
       class BindCollector < Arel::Collectors::Bind
         def compile(bvs, conn)
           casted_binds = conn.prepare_binds_for_database(bvs)
