338e28de15d49b8f49b5694e0e1a5e31d571428c
David Heinemeier Hansson <david@loudthinking.com>
2015-07-07 22:36:18 +0200

    Make the entire internal channel private

--- lib/action_cable/connection/internal_channel.rb
+++ lib/action_cable/connection/internal_channel.rb
@@ -3,28 +3,28 @@ module ActionCable
     module InternalChannel
       extend ActiveSupport::Concern
 
-      def internal_redis_channel
-        "action_cable/#{connection_identifier}"
-      end
-
-      def subscribe_to_internal_channel
-        if connection_identifier.present?
-          callback = -> (message) { process_internal_message(message) }
-          @_internal_redis_subscriptions ||= []
-          @_internal_redis_subscriptions << [ internal_redis_channel, callback ]
-
-          pubsub.subscribe(internal_redis_channel, &callback)
-          logger.info "Registered connection (#{connection_identifier})"
-        end
-      end
-
-      def unsubscribe_from_internal_channel
-        if @_internal_redis_subscriptions.present?
-          @_internal_redis_subscriptions.each { |channel, callback| pubsub.unsubscribe_proc(channel, callback) }
-        end
-      end
-
       private
+        def internal_redis_channel
+          "action_cable/#{connection_identifier}"
+        end
+
+        def subscribe_to_internal_channel
+          if connection_identifier.present?
+            callback = -> (message) { process_internal_message(message) }
+            @_internal_redis_subscriptions ||= []
+            @_internal_redis_subscriptions << [ internal_redis_channel, callback ]
+
+            pubsub.subscribe(internal_redis_channel, &callback)
+            logger.info "Registered connection (#{connection_identifier})"
+          end
+        end
+
+        def unsubscribe_from_internal_channel
+          if @_internal_redis_subscriptions.present?
+            @_internal_redis_subscriptions.each { |channel, callback| pubsub.unsubscribe_proc(channel, callback) }
+          end
+        end
+
         def process_internal_message(message)
           message = ActiveSupport::JSON.decode(message)
 
