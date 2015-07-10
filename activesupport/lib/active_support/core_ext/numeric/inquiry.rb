0e65731590fb103dd6a08232f2f7fd9e9081d2f2
Julio Lopez <ljuliom@gmail.com>
2015-07-09 22:09:07 -0500

    added missing indentation [ci skip]

--- activesupport/lib/active_support/core_ext/numeric/inquiry.rb
+++ activesupport/lib/active_support/core_ext/numeric/inquiry.rb
@@ -1,26 +1,26 @@
 unless 1.respond_to?(:positive?) # TODO: Remove this file when we drop support to ruby < 2.3
-class Numeric
-  # Returns true if the number is positive.
-  #
-  #   1.positive?  # => true
-  #   0.positive?  # => false
-  #   -1.positive? # => false
-  def positive?
-    self > 0
+  class Numeric
+    # Returns true if the number is positive.
+    #
+    #   1.positive?  # => true
+    #   0.positive?  # => false
+    #   -1.positive? # => false
+    def positive?
+      self > 0
+    end
+
+    # Returns true if the number is negative.
+    #
+    #   -1.negative? # => true
+    #   0.negative?  # => false
+    #   1.negative?  # => false
+    def negative?
+      self < 0
+    end
   end
 
-  # Returns true if the number is negative.
-  #
-  #   -1.negative? # => true
-  #   0.negative?  # => false
-  #   1.negative?  # => false
-  def negative?
-    self < 0
+  class Complex
+    undef :positive?
+    undef :negative?
   end
-end
-
-class Complex
-  undef :positive?
-  undef :negative?
-end
 end
