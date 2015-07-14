5bbde1f1751f57bf1d5eb7e160584c348bb5a8d5
William Griffiths <william@ycymro.com>
2015-07-14 18:26:23 +0100

    Change image_alt to remove 64 character checksum

--- actionview/lib/action_view/helpers/asset_tag_helper.rb
+++ actionview/lib/action_view/helpers/asset_tag_helper.rb
@@ -237,7 +237,7 @@ module ActionView
       #   image_alt('underscored_file_name.png')
       #   # => Underscored file name
       def image_alt(src)
-        File.basename(src, '.*').sub(/-[[:xdigit:]]{32}\z/, '').tr('-_', ' ').capitalize
+        File.basename(src, '.*').sub(/-[[:xdigit:]]{64}\z/, '').tr('-_', ' ').capitalize
       end
 
       # Returns an HTML video tag for the +sources+. If +sources+ is a string,
