6ffec3c16c0ee0b0ab4643907af1f2ed80a71a9a
Sean Griffin <sean@thoughtbot.com>
2015-07-19 17:28:52 -0600

    Fix tests broken by previous commit

--- activesupport/lib/active_support/inflector/methods.rb
+++ activesupport/lib/active_support/inflector/methods.rb
@@ -92,7 +92,7 @@ module ActiveSupport
       return camel_cased_word unless camel_cased_word =~ /[A-Z-]|::/
       word = camel_cased_word.to_s.gsub('::'.freeze, '/'.freeze)
       word.gsub!(/(?:(?<=([A-Za-z\d]))|\b)(#{inflections.acronym_regex})(?=\b|[^a-z])/) { "#{$1 && '_'.freeze }#{$2.downcase}" }
-      word.gsub!(/([A-Z\d]+)([A-Z][a-z])/, ' \1_\2'.freeze)
+      word.gsub!(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2'.freeze)
       word.gsub!(/([a-z\d])([A-Z])/, '\1_\2'.freeze)
       word.tr!("-".freeze, "_".freeze)
       word.downcase!
