12b0b26df7560ab5199ba830586864085441508f
Roque Pinel <repinel@gmail.com>
2015-07-19 22:00:36 -0400

    Fix state being carried over from previous transaction
    This clears the transaction record state when the transaction finishes
    with a `:committed` status.
    
    Considering the following example where `name` is a required attribute.
    Before we had `new_record?` returning `true` for a persisted record:
    
    ```ruby
      author = Author.create! name: 'foo'
      author.name = nil
      author.save        # => false
      author.new_record? # => true
    ```

--- activerecord/lib/active_record/transactions.rb
+++ activerecord/lib/active_record/transactions.rb
@@ -379,6 +379,10 @@ module ActiveRecord
         raise ActiveRecord::Rollback unless status
       end
       status
+    ensure
+      if @transaction_state && @transaction_state.committed?
+        clear_transaction_record_state
+      end
     end
 
     protected
--- activerecord/test/cases/transactions_test.rb
+++ activerecord/test/cases/transactions_test.rb
@@ -175,6 +175,13 @@ class TransactionTest < ActiveRecord::TestCase
     assert topic.new_record?, "#{topic.inspect} should be new record"
   end
 
+  def test_transaction_state_is_cleared_when_record_is_persisted
+    author = Author.create! name: 'foo'
+    author.name = nil
+    assert_not author.save
+    assert_not author.new_record?
+  end
+
   def test_update_should_rollback_on_failure
     author = Author.find(1)
     posts_count = author.posts.size
