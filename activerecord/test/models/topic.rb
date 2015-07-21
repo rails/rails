d2e35e11872942e46994fea8df108832fbd50890
Mehmet Emin İNAÇ <mehmetemininac@gmail.com>
2015-07-21 23:29:51 +0300

    fix rails tests

--- activerecord/test/models/topic.rb
+++ activerecord/test/models/topic.rb
@@ -32,7 +32,7 @@ class Topic < ActiveRecord::Base
     end
   end
 
-  has_many :replies, :dependent => :destroy, :foreign_key => "parent_id"
+  has_many :replies, dependent: :destroy, foreign_key: "parent_id", autosave: true
   has_many :approved_replies, -> { approved }, class_name: 'Reply', foreign_key: "parent_id", counter_cache: 'replies_count'
 
   has_many :unique_replies, :dependent => :destroy, :foreign_key => "parent_id"
