738ccea3b06b28ad269daf451cc634ff4c12cf4d
wurde <andy.bettisworth@accreu.com>
2015-07-22 00:12:42 -0500

    make use of to: convention over hash rockets

--- railties/lib/rails/generators/rails/app/templates/config/routes.rb
+++ railties/lib/rails/generators/rails/app/templates/config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #   get 'products/:id', to: 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  #   get 'products/:id/purchase', to: 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
