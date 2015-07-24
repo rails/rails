0789e066f1194519c4f7c36533c8f02bd14968b2
Rafael Mendonça França <rafaelmfranca@gmail.com>
2015-07-24 15:44:23 -0300

    Merge pull request #21007 from clayton-shopify/fix-mailto-encoding
    Encode the email address as prescribed in RFC 6068 section 2.

--- actionview/lib/action_view/helpers/url_helper.rb
+++ actionview/lib/action_view/helpers/url_helper.rb
@@ -463,7 +463,8 @@ module ActionView
         }.compact
         extras = extras.empty? ? '' : '?' + extras.join('&')
 
-        html_options["href"] = "mailto:#{email_address}#{extras}"
+        encoded_email_address = ERB::Util.url_encode(email_address).gsub("%40", "@")
+        html_options["href"] = "mailto:#{encoded_email_address}#{extras}"
 
         content_tag(:a, name || email_address, html_options, &block)
       end
