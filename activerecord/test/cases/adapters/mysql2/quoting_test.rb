330f74956763719f86acca8a0337ba320f06fd73
Yasuo Honda <yasuo.honda@gmail.com>
2015-07-26 01:09:05 +0000

    Remove @connection instance variable only when defined

--- activerecord/test/cases/adapters/mysql2/quoting_test.rb
+++ activerecord/test/cases/adapters/mysql2/quoting_test.rb
@@ -7,14 +7,14 @@ class Mysql2QuotingTest < ActiveRecord::Mysql2TestCase
 
   test 'quoted date precision for gte 5.6.4' do
     @connection.stubs(:full_version).returns('5.6.4')
-    @connection.remove_instance_variable(:@version)
+    @connection.remove_instance_variable(:@version) if @connection.instance_variable_defined?(:@version)
     t = Time.now.change(usec: 1)
     assert_match(/\.000001\z/, @connection.quoted_date(t))
   end
 
   test 'quoted date precision for lt 5.6.4' do
     @connection.stubs(:full_version).returns('5.6.3')
-    @connection.remove_instance_variable(:@version)
+    @connection.remove_instance_variable(:@version) if @connection.instance_variable_defined?(:@version)
     t = Time.now.change(usec: 1)
     refute_match(/\.000001\z/, @connection.quoted_date(t))
   end
