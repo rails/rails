e49c2cd743eaf3fbaa1215e4922b2a59f8e3b5f8
Ronak Jangir <ronakjangir47@gmail.com>
2015-08-15 14:14:55 +0530

    Replacing lambda with proc getting argument error because of it.

--- activesupport/lib/active_support/testing/method_call_assertions.rb
+++ activesupport/lib/active_support/testing/method_call_assertions.rb
@@ -5,7 +5,7 @@ module ActiveSupport
         def assert_called(object, method_name, message = nil, times: 1)
           times_called = 0
 
-          object.stub(method_name, -> { times_called += 1 }) { yield }
+          object.stub(method_name, proc { times_called += 1 }) { yield }
 
           error = "Expected #{method_name} to be called #{times} times, " \
             "but was called #{times_called} times"
