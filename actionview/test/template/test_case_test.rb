2ae68eef39bba322fe72066c35f43b8d3477f59f
eileencodes <eileencodes@gmail.com>
2015-08-15 16:33:39 -0400

    Fix test assign_parameter method signature
    Oops, I broke the build :(
    
    Fixes the method signature of `assign_parameters` which now takes 6
    arguments instead of 4. We likely will end up chaning the method
    signature further so good to know this test is here.

--- actionview/test/template/test_case_test.rb
+++ actionview/test/template/test_case_test.rb
@@ -209,7 +209,7 @@ module ActionView
     end
 
     test "is able to use routes" do
-      controller.request.assign_parameters(@routes, 'foo', 'index')
+      controller.request.assign_parameters(@routes, 'foo', 'index', {}, '/foo', [])
       assert_equal '/foo', url_for
       assert_equal '/bar', url_for(:controller => 'bar')
     end
