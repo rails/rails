43dbdfb00cbdc3471d3e0467706a5e001fdc50b5
maclover7 <me@jonathanmoss.me>
2015-08-16 21:35:34 -0400

    Add tests for new versioning system

---
+++ activemodel/test/version_test.rb
@@ -0,0 +1,11 @@
+require 'cases/helper'
+
+class VersionTest < ActiveSupport::TestCase
+  def test_framework_version_returns_a_string
+    assert ActiveModel::VERSION.is_a?(String)
+  end
+
+  def test_framework_version_is_equal_to_rails_version_file
+    assert_equal ActiveModel::VERSION, File.read(File.expand_path('../../../RAILS_VERSION', __FILE__)).strip
+  end
+end
