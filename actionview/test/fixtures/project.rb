a9c6a583b6ed4b6482652f98c2aeda1a60268fc9
Lachlan Sylvester <lachlan.sylvester@publicisfrontfoot.com.au>
2015-08-28 15:31:54 +1000

    Fix calling cache helper with a relation

--- actionview/test/fixtures/project.rb
+++ actionview/test/fixtures/project.rb
@@ -1,3 +1,7 @@
 class Project < ActiveRecord::Base
   has_and_belongs_to_many :developers, -> { uniq }
+
+  def self.collection_cache_key(collection = all, timestamp_column = :updated_at)
+    "projects-#{collection.count}"
+  end
 end
