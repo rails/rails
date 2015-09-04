afacd96b072e52093394112cc2e0aacc5070b482
Christos Zisopoulos <christos@me.com>
2015-09-05 00:23:53 +0200

    Unscopes :order from query to fix postgres ORDER failures

--- activerecord/lib/active_record/collection_cache_key.rb
+++ activerecord/lib/active_record/collection_cache_key.rb
@@ -3,7 +3,12 @@ module ActiveRecord
 
     def collection_cache_key(collection = all, timestamp_column = :updated_at) # :nodoc:
       model_signature = collection.model_name.cache_key
-      unique_signature = collection.pluck(primary_key, timestamp_column).flatten.join("-")
+
+      if collection.loaded?
+        unique_signature = collection.pluck(primary_key, timestamp_column).flatten.join("-")
+      else
+        unique_signature = collection.unscope(:order).pluck(primary_key, timestamp_column).flatten.join("-")
+      end
 
       "#{model_signature}/collection-digest-#{Digest::SHA256.hexdigest(unique_signature)}"
     end
