4ff626cac901b41f86646dab1939d2a95b2d26bd
Pavel Pravosud <pavel@pravosud.com>
2015-09-05 17:48:15 -0700

    Make ActiveRecordException descendants args optional
    This change allows to instantiate all ActiveRecordError descendant
    execption classes without arguments, which might be useful in testing
    and is far less surprising than mandatory arguments.

--- activerecord/lib/active_record/validations.rb
+++ activerecord/lib/active_record/validations.rb
@@ -12,10 +12,16 @@ module ActiveRecord
   class RecordInvalid < ActiveRecordError
     attr_reader :record
 
-    def initialize(record)
-      @record = record
-      errors = @record.errors.full_messages.join(", ")
-      super(I18n.t(:"#{@record.class.i18n_scope}.errors.messages.record_invalid", :errors => errors, :default => :"errors.messages.record_invalid"))
+    def initialize(record = nil)
+      if record
+        @record = record
+        errors = @record.errors.full_messages.join(", ")
+        message = I18n.t(:"#{@record.class.i18n_scope}.errors.messages.record_invalid", errors: errors, default: :"errors.messages.record_invalid")
+      else
+        message = "Record invalid"
+      end
+
+      super(message)
     end
   end
 
