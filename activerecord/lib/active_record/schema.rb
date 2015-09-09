5b1ea3ea00579e45ed8b46a23a007c328f766786
Rafael Mendonça França <rafaelmfranca@gmail.com>
2015-09-09 06:45:54 -0300

    Invert the conditional

--- activerecord/lib/active_record/schema.rb
+++ activerecord/lib/active_record/schema.rb
@@ -40,7 +40,7 @@ module ActiveRecord
     def define(info, &block) # :nodoc:
       instance_eval(&block)
 
-      unless info[:version].blank?
+      if info[:version].present?
         initialize_schema_migrations_table
         connection.assume_migrated_upto_version(info[:version], migrations_paths)
       end
