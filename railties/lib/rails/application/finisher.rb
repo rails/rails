6b32b989c136c27cd367f98a0f85687b9eb961e8
Aditya Kapoor <adityakapoor.mait@gmail.com>
2015-09-13 03:29:21 +0530

    remove unneeded info route
    since it being redirected to the actions route so having this route doesnt make any sense

--- railties/lib/rails/application/finisher.rb
+++ railties/lib/rails/application/finisher.rb
@@ -24,7 +24,6 @@ module Rails
           app.routes.append do
             get '/rails/info/properties' => "rails/info#properties"
             get '/rails/info/routes'     => "rails/info#routes"
-            get '/rails/info'            => "rails/info#index"
             get '/'                      => "rails/welcome#index"
           end
         end
