d30f934af03d2bab0ddfa8b850ad910891829db3
Akira Matsuda <ronnie@dio.jp>
2015-09-22 23:34:23 +0900

    AMo typos

--- activemodel/lib/active_model/validations/confirmation.rb
+++ activemodel/lib/active_model/validations/confirmation.rb
@@ -9,7 +9,7 @@ module ActiveModel
 
       def validate_each(record, attribute, value)
         if (confirmed = record.send("#{attribute}_confirmation"))
-          unless confimation_value_equal?(record, attribute, value, confirmed)
+          unless confirmation_value_equal?(record, attribute, value, confirmed)
             human_attribute_name = record.class.human_attribute_name(attribute)
             record.errors.add(:"#{attribute}_confirmation", :confirmation, options.except(:case_sensitive).merge!(attribute: human_attribute_name))
           end
@@ -27,7 +27,7 @@ module ActiveModel
         end.compact)
       end
 
-      def confimation_value_equal?(record, attribute, value, confirmed)
+      def confirmation_value_equal?(record, attribute, value, confirmed)
         if !options[:case_sensitive] && value.is_a?(String)
           value.casecmp(confirmed) == 0
         else
