df3a3b37debcca383406f06b6ec6bc5c7c486592
Ronak Jangir <ronakjangir47@gmail.com>
2015-09-24 00:16:42 +0530

    skipped assertion on datetime seconds precision as it is only valid for newer mysql versions

--- activerecord/test/cases/type/date_time_test.rb
+++ activerecord/test/cases/type/date_time_test.rb
@@ -5,6 +5,7 @@ module ActiveRecord
   module Type
     class IntegerTest < ActiveRecord::TestCase
       def test_datetime_seconds_precision_applied_to_timestamp
+        skip "This test is invalid if subsecond precision isn't supported" unless subsecond_precision_supported?
         p = Task.create!(starting: ::Time.now)
         assert_equal p.starting.usec, p.reload.starting.usec
       end
