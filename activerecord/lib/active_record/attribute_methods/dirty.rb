8a811c83aa386e4ad26553c913d1a450d8a98d5f
Sean Griffin <sean@seantheprogrammer.com>
2015-09-24 13:43:33 -0600

    Encapsulate a lot of the logic from `Dirty` in an object
    In order to improve the performance of dirty checking, we're going to
    need to duplicate all of the `previous_` methods in Active Model.
    However, these methods are basically the same as their non-previous
    counterparts, but comparing `@original_attributes` to
    `@previous_original_attributes` instead of `@attributes` and
    `@original_attributes`. This will help reduce that duplication.

--- activerecord/lib/active_record/attribute_methods/dirty.rb
+++ activerecord/lib/active_record/attribute_methods/dirty.rb
@@ -1,4 +1,5 @@
 require 'active_support/core_ext/module/attribute_accessors'
+require 'active_record/attribute_mutation_tracker'
 
 module ActiveRecord
   module AttributeMethods
@@ -40,12 +41,13 @@ module ActiveRecord
 
       def init_internals
         super
-        @original_attributes = @attributes.dup
+        @mutation_tracker = AttributeMutationTracker.new(@attributes, @attributes.dup)
       end
 
       def initialize_dup(other) # :nodoc:
         super
-        @original_attributes = self.class._default_attributes.dup
+        @mutation_tracker = AttributeMutationTracker.new(@attributes,
+                                                         self.class._default_attributes.dup)
       end
 
       def changes_applied
@@ -77,7 +79,7 @@ module ActiveRecord
         if defined?(@cached_changed_attributes)
           @cached_changed_attributes
         else
-          calculate_changed_attributes.freeze
+          super.reverse_merge(@mutation_tracker.changed_values).freeze
         end
       end
 
@@ -88,20 +90,17 @@ module ActiveRecord
       end
 
       def attribute_changed_in_place?(attr_name)
-        original_database_value = @original_attributes[attr_name].value_before_type_cast
-        @attributes[attr_name].changed_in_place_from?(original_database_value)
+        @mutation_tracker.changed_in_place?(attr_name)
       end
 
       private
 
       def changes_include?(attr_name)
-        attr_name = attr_name.to_s
-        super || attribute_modified?(attr_name) || attribute_changed_in_place?(attr_name)
+        super || @mutation_tracker.changed?(attr_name)
       end
 
       def clear_attribute_change(attr_name)
-        attr_name = attr_name.to_s
-        @original_attributes[attr_name] = @attributes[attr_name].dup
+        @mutation_tracker.forget_change(attr_name)
       end
 
       def _update_record(*)
@@ -116,22 +115,8 @@ module ActiveRecord
         changed & self.class.column_names
       end
 
-      def attribute_modified?(attr_name)
-        @attributes[attr_name].changed_from?(@original_attributes.fetch_value(attr_name))
-      end
-
       def store_original_attributes
-        @original_attributes = @attributes.map do |attr|
-          attr.with_value_from_database(attr.value_for_database)
-        end
-      end
-
-      def calculate_changed_attributes
-        attribute_names.each_with_object({}.with_indifferent_access) do |attr_name, result|
-          if changes_include?(attr_name)
-            result[attr_name] = @original_attributes.fetch_value(attr_name)
-          end
-        end
+        @mutation_tracker = @mutation_tracker.now_tracking(@attributes)
       end
 
       def cache_changed_attributes
