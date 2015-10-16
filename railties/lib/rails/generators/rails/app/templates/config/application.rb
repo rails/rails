25888d892a32c75227308d8eb8ed9638d1a4673e
Marat Galiev <kazanlug@gmail.com>
2015-10-16 23:04:55 +0300

    Use "rake time:zones:all" in description instead of "rake -D time", which causes "invalid option: -D" warning

--- railties/lib/rails/generators/rails/app/templates/config/application.rb
+++ railties/lib/rails/generators/rails/app/templates/config/application.rb
@@ -26,7 +26,7 @@ module <%= app_const_base %>
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # Run "rake time:zones:all" for a time zone names list. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
