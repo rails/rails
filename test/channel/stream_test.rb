df5a32dfbc94723d847aa8d8034041a2bd8751e2
Pratik Naik <pratiknaik@gmail.com>
2015-10-16 18:19:35 -0500

    Fix stream tests

--- test/channel/stream_test.rb
+++ test/channel/stream_test.rb
@@ -18,7 +18,7 @@ class ActionCable::Channel::StreamTest < ActionCable::TestCase
 
   test "streaming start and stop" do
     run_in_eventmachine do
-      @connection.expects(:pubsub).returns mock().tap { |m| m.expects(:subscribe).with("test_room_1") }
+      @connection.expects(:pubsub).returns mock().tap { |m| m.expects(:subscribe).with("test_room_1").returns stub_everything(:pubsub) }
       channel = ChatChannel.new @connection, "{id: 1}", { id: 1 }
 
       @connection.expects(:pubsub).returns mock().tap { |m| m.expects(:unsubscribe_proc) }
@@ -29,7 +29,7 @@ class ActionCable::Channel::StreamTest < ActionCable::TestCase
   test "stream_for" do
     run_in_eventmachine do
       EM.next_tick do
-        @connection.expects(:pubsub).returns mock().tap { |m| m.expects(:subscribe).with("action_cable:channel:stream_test:chat:Room#1-Campfire") }
+        @connection.expects(:pubsub).returns mock().tap { |m| m.expects(:subscribe).with("action_cable:channel:stream_test:chat:Room#1-Campfire").returns stub_everything(:pubsub) }
       end
 
       channel = ChatChannel.new @connection, ""
