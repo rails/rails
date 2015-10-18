06d24d2dd81dba4385b82f3893c58abb38508da6
Jake Worth <jakeworth82@gmail.com>
2015-10-18 17:46:46 -0500

    Document format_date method [ci skip]

--- actionview/lib/action_view/helpers/tags/week_field.rb
+++ actionview/lib/action_view/helpers/tags/week_field.rb
@@ -4,6 +4,8 @@ module ActionView
       class WeekField < DatetimeField # :nodoc:
         private
 
+          # Returns _value_ in "2015-W41" format if _value_ responds to
+          # strftime; otherwise returns nil.
           def format_date(value)
             value.try(:strftime, "%Y-W%W")
           end
