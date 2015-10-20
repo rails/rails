0ceaa733aa327e2117c5e059f4cd00126449e8e8
Sean Griffin <sean@seantheprogrammer.com>
2015-10-20 14:22:07 -0600

    Don't add classes to the top level namespace
    I've been writing too much Rust. My mind is still in the mode of things
    being auto-namespaced based on the file...

--- activerecord/lib/active_record/connection_adapters/determine_if_preparable_visitor.rb
+++ activerecord/lib/active_record/connection_adapters/determine_if_preparable_visitor.rb
@@ -1,18 +1,22 @@
-module DetermineIfPreparableVisitor
-  attr_reader :preparable
+module ActiveRecord
+  module ConnectionAdapters
+    module DetermineIfPreparableVisitor
+      attr_reader :preparable
 
-  def accept(*)
-    @preparable = true
-    super
-  end
+      def accept(*)
+        @preparable = true
+        super
+      end
 
-  def visit_Arel_Nodes_In(*)
-    @preparable = false
-    super
-  end
+      def visit_Arel_Nodes_In(*)
+        @preparable = false
+        super
+      end
 
-  def visit_Arel_Nodes_SqlLiteral(*)
-    @preparable = false
-    super
+      def visit_Arel_Nodes_SqlLiteral(*)
+        @preparable = false
+        super
+      end
+    end
   end
 end
