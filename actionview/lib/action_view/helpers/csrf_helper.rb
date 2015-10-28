3083c8c453e51c7234af049942fc7c68ad229215
Kopylov German <roverrr@gmail.com>
2015-10-29 01:14:36 +0300

    Remove unnecessary line break in csrf helper

--- actionview/lib/action_view/helpers/csrf_helper.rb
+++ actionview/lib/action_view/helpers/csrf_helper.rb
@@ -22,7 +22,7 @@ module ActionView
           [
             tag('meta', :name => 'csrf-param', :content => request_forgery_protection_token),
             tag('meta', :name => 'csrf-token', :content => form_authenticity_token)
-          ].join("\n").html_safe
+          ].join.html_safe
         end
       end
 
