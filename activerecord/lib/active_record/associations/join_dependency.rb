ec77ed710153d8b604f6b1508fe6f8993f36aa87
Rafael Mendonça França <rafaelmfranca@gmail.com>
2015-10-27 22:07:41 -0200

    Merge pull request #18383 from scambra/habtm-with-where-includes-16032-for-master
    Includes HABTM returns correct size now

--- activerecord/lib/active_record/associations/join_dependency.rb
+++ activerecord/lib/active_record/associations/join_dependency.rb
@@ -151,7 +151,8 @@ module ActiveRecord
 
         message_bus.instrument('instantiation.active_record', payload) do
           result_set.each { |row_hash|
-            parent = parents[row_hash[primary_key]] ||= join_root.instantiate(row_hash, column_aliases)
+            parent_key = primary_key ? row_hash[primary_key] : row_hash
+            parent = parents[parent_key] ||= join_root.instantiate(row_hash, column_aliases)
             construct(parent, join_root, row_hash, result_set, seen, model_cache, aliases)
           }
         end
