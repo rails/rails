17e6f1507b7f2c2a883c180f4f9548445d6dfbda
Aaron Patterson <aaron.patterson@gmail.com>
2015-10-29 10:42:44 -0700

    use secure string comparisons for basic auth username / password
    this will avoid timing attacks against applications that use basic auth.
    
    CVE-2015-7576

--- activesupport/lib/active_support/security_utils.rb
+++ activesupport/lib/active_support/security_utils.rb
@@ -1,3 +1,5 @@
+require 'digest'
+
 module ActiveSupport
   module SecurityUtils
     # Constant time string comparison.
@@ -16,5 +18,10 @@ module ActiveSupport
       res == 0
     end
     module_function :secure_compare
+
+    def variable_size_secure_compare(a, b) # :nodoc:
+      secure_compare(::Digest::SHA256.hexdigest(a), ::Digest::SHA256.hexdigest(b))
+    end
+    module_function :variable_size_secure_compare
   end
 end
