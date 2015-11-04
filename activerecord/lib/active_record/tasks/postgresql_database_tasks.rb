667373eb80374ed3f03e133858fdae4f26e0ba19
Andrew White <andrew.white@unboxedconsulting.com>
2015-11-04 11:55:42 +0000

    Remove deprecated pg_dump -i flag
    Backport of f383552

--- activerecord/lib/active_record/tasks/postgresql_database_tasks.rb
+++ activerecord/lib/active_record/tasks/postgresql_database_tasks.rb
@@ -51,7 +51,7 @@ module ActiveRecord
           search_path = search_path.split(",").map{|search_path_part| "--schema=#{Shellwords.escape(search_path_part.strip)}" }.join(" ")
         end
 
-        command = "pg_dump -i -s -x -O -f #{Shellwords.escape(filename)} #{search_path} #{Shellwords.escape(configuration['database'])}"
+        command = "pg_dump -s -x -O -f #{Shellwords.escape(filename)} #{search_path} #{Shellwords.escape(configuration['database'])}"
         raise 'Error dumping database' unless Kernel.system(command)
 
         File.open(filename, "a") { |f| f << "SET search_path TO #{ActiveRecord::Base.connection.schema_search_path};\n\n" }
