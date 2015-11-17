662c768a24b14f25c3cd6796ac59044cee6e5321
Rafael Laurindo <rafaelplaurindo@gmail.com>
2015-11-16 22:44:51 -0200

    Support the creation of two or more databases

--- activerecord/lib/active_record/tasks/database_tasks.rb
+++ activerecord/lib/active_record/tasks/database_tasks.rb
@@ -90,12 +90,16 @@ module ActiveRecord
 
       def create(*arguments)
         configuration = arguments.first
-        class_for_adapter(configuration['adapter']).new(*arguments).create
-      rescue DatabaseAlreadyExists
-        $stderr.puts "#{configuration['database']} already exists"
-      rescue Exception => error
-        $stderr.puts error, *(error.backtrace)
-        $stderr.puts "Couldn't create database for #{configuration.inspect}"
+        begin
+          class_for_adapter(configuration['adapter']).new(*arguments).create
+        rescue DatabaseAlreadyExists
+          $stderr.puts "#{configuration['database']} already exists"
+        rescue Exception => error
+          $stderr.puts error, *(error.backtrace)
+          $stderr.puts "Couldn't create database for #{configuration.inspect}"
+        else
+          $stderr.puts "Database #{configuration['database']} has been created"
+        end
       end
 
       def create_all
@@ -269,9 +273,23 @@ module ActiveRecord
         environments = [environment]
         # add test environment only if no RAILS_ENV was specified.
         environments << 'test' if environment == 'development' && ENV['RAILS_ENV'].nil?
+        db_configs = ActiveRecord::Base.configurations
+        configurations = []
 
-        configurations = ActiveRecord::Base.configurations.values_at(*environments)
-        configurations.compact.each do |configuration|
+        if ENV['NAMESPACE']
+          namespaces = [ENV['NAMESPACE']]
+        else
+          namespaces = ENV['NAMESPACES'] ? ENV['NAMESPACES'].split(",") : []
+        end
+        namespaces_configs = db_configs.values_at(*namespaces)
+        if namespaces_configs.any?
+          namespaces_configs.each do |namespace|
+            configurations << namespace.values_at(*environments)
+          end
+        end
+
+        configurations << db_configs.values_at(*environments)
+        configurations.flatten.compact.each do |configuration|
           yield configuration unless configuration['database'].blank?
         end
       end
