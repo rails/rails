9982d60b34b976a826358644d1157d2589b92e94
Godfrey Chan <godfreykfc@gmail.com>
2015-11-26 09:38:37 -0800

    🆗🆖🆗

--- activesupport/test/file_evented_update_checker_test.rb
+++ activesupport/test/file_evented_update_checker_test.rb
@@ -18,24 +18,24 @@ if ENV['LISTEN'] == '1'
 end
 
 begin
-  GC.disable if ENV['LISTEN'] == '1' 
+  GC.disable if ENV['LISTEN'] == '1' && ENV['LISTEN_GC_FIX'] == '1'
 
   Dir.chdir("..") do
     i = 1
-  
+
     Dir.glob("**/*") do |entry|
       break if i > 5000
-    
+
       if File.file?(entry) && !File.zero?(entry)
         print "#{i}) Opening `#{entry}`: "
         File.open(entry, 'r') { |f| puts f.read(10).inspect }
       end
-      
+
       i += 1
     end
   end
 ensure
-  GC.enable if ENV['LISTEN'] == '1'
+  GC.enable if ENV['LISTEN'] == '1' && ENV['LISTEN_GC_FIX'] == '1'
 end
 
 # class FileEventedUpdateCheckerTest < ActiveSupport::TestCase
