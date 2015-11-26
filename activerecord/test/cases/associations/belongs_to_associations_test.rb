9c6ae33380bd08a893be4a4a7feb58ec03e7c11f
Tim Breitkreutz <tim@generalthings.com>
2015-11-26 10:46:03 -0800

    Backport of https://github.com/rails/rails/pull/22251 to 4-2-stable
    Fix a where clause on a relation to itself, such as:
    
        Planet(star: Planet.all.select(:star_id))
    
    Previously the inner select was erroneously concatenated with
    another select item.
    
    Fixes #20602

--- activerecord/test/cases/associations/belongs_to_associations_test.rb
+++ activerecord/test/cases/associations/belongs_to_associations_test.rb
@@ -24,6 +24,7 @@ require 'models/admin/user'
 require 'models/ship'
 require 'models/treasure'
 require 'models/parrot'
+require 'models/like'
 
 class BelongsToAssociationsTest < ActiveRecord::TestCase
   fixtures :accounts, :companies, :developers, :projects, :topics,
@@ -1010,6 +1011,23 @@ class BelongsToAssociationsTest < ActiveRecord::TestCase
     record = Record.create!
     Column.create! record: record
     assert_equal 1, Column.count
+  end
+
+  def test_self_query_thru_belongs_to_self_without_primary_key
+    post = posts(:welcome)
+    like = post.likes.create!
+    like2 = post.likes.create!
+
+    likes = Like.where(post: Like.all.select(:post_id))
+    assert_equal [like.id, like2.id], likes.map(&:id)
+    assert_equal [like.post_id, like2.post_id], likes.map(&:post_id)
+  end
+
+  def test_self_query_thru_belongs_to_self_with_primary_key
+    post = posts(:welcome)
+
+    comments = Comment.where(post: post.comments.select(:post_id))
+    assert_equal 2, comments.count
   end
 end
 
