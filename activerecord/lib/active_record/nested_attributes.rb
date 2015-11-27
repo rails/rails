0fde6f554b75b13b0435dd70f1c3ec02bc209e0d
Andrew White <andyw@pixeltrix.co.uk>
2015-11-27 13:46:46 +0000

    Don't short-circuit reject_if proc
    When updating an associated record via nested attribute hashes the
    reject_if proc could be bypassed if the _destroy flag was set in the
    attribute hash and allow_destroy was set to false.
    
    The fix is to only short-circuit if the _destroy flag is set and the
    option allow_destroy is set to true. It also fixes an issue where
    a new record wasn't created if _destroy was set and the option
    allow_destroy was set to false.
    
    CVE-2015-7577

--- activerecord/lib/active_record/nested_attributes.rb
+++ activerecord/lib/active_record/nested_attributes.rb
@@ -542,7 +542,7 @@ module ActiveRecord
     # has_destroy_flag? or if a <tt>:reject_if</tt> proc exists for this
     # association and evaluates to +true+.
     def reject_new_record?(association_name, attributes)
-      has_destroy_flag?(attributes) || call_reject_if(association_name, attributes)
+      will_be_destroyed?(association_name, attributes) || call_reject_if(association_name, attributes)
     end
 
     # Determines if a record with the particular +attributes+ should be
@@ -551,13 +551,23 @@ module ActiveRecord
     #
     # Returns false if there is a +destroy_flag+ on the attributes.
     def call_reject_if(association_name, attributes)
-      return false if has_destroy_flag?(attributes)
+      return false if will_be_destroyed?(association_name, attributes)
+
       case callback = self.nested_attributes_options[association_name][:reject_if]
       when Symbol
         method(callback).arity == 0 ? send(callback) : send(callback, attributes)
       when Proc
         callback.call(attributes)
       end
+    end
+
+    # Only take into account the destroy flag if <tt>:allow_destroy</tt> is true
+    def will_be_destroyed?(association_name, attributes)
+      allow_destroy?(association_name) && has_destroy_flag?(attributes)
+    end
+
+    def allow_destroy?(association_name)
+      self.nested_attributes_options[association_name][:allow_destroy]
     end
 
     def raise_nested_attributes_record_not_found!(association_name, record_id)
