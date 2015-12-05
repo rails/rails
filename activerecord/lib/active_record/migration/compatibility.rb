badaf353ba4591ce78fa0c305b656f7cae56992b
Matthew Draper <matthew@trebex.net>
2015-12-06 06:09:00 +1030

    In 4.2 migrations, `timestamps` defaulted to `null: true`
    .. it also showed a deprecation warning, but we obviously needn't retain
    that.

--- activerecord/lib/active_record/migration/compatibility.rb
+++ activerecord/lib/active_record/migration/compatibility.rb
@@ -4,6 +4,30 @@ module ActiveRecord
       V5_0 = Current
 
       module FourTwoShared
+        module TableDefinition
+          def timestamps(*, **options)
+            options[:null] = true if options[:null].nil?
+            super
+          end
+        end
+
+        def create_table(table_name, options = {})
+          if block_given?
+            super(table_name, options) do |t|
+              class << t
+                prepend TableDefinition
+              end
+              yield t
+            end
+          else
+            super
+          end
+        end
+
+        def add_timestamps(*, **options)
+          options[:null] = true if options[:null].nil?
+          super
+        end
       end
 
       class V4_2 < V5_0
