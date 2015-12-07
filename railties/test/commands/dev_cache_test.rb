2af9c080799c25a0bb4f6f6b8d2685b6c0e47299
Kasper Timm Hansen <kaspth@gmail.com>
2015-12-07 20:36:22 +0100

    Clarify the need to run command twice.
    We had 2 pull requests erronously trying to remove the first command.
    Add some comments for clarity.

--- railties/test/commands/dev_cache_test.rb
+++ railties/test/commands/dev_cache_test.rb
@@ -22,8 +22,8 @@ module CommandsTests
 
     test 'dev:cache deletes file and outputs message' do
       Dir.chdir(app_path) do
-        output = `rails dev:cache`        
-        output = `rails dev:cache`
+        `rails dev:cache` # Create caching file.
+        output = `rails dev:cache` # Delete caching file.
         assert_not File.exist?('tmp/caching-dev.txt')
         assert_match(%r{Development mode is no longer being cached}, output)
       end
