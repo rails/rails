64c5e5b4bd2642bec3049e24e896a561a6ae1355
Dharam Gollapudi <dharam.gollapudi@gmail.com>
2015-12-10 15:37:32 -0800

    Mark the methods as private

--- railties/lib/rails/commands/rake_proxy.rb
+++ railties/lib/rails/commands/rake_proxy.rb
@@ -8,33 +8,35 @@ In addition to those, you can now run the rake tasks as rails commands:
 
     EOT
 
-    def write_rake_tasks_help_message
-      puts RAKE_TASKS_HELP_MESSAGE
-    end
+    private
 
-    def write_rake_tasks
-      width = rake_tasks.map { |t| t.name_with_args.length }.max || 10
-      rake_tasks.each do |t|
-        printf("#{Rake.application.name} %-#{width}s  # %s\n", t.name_with_args, t.comment)
+      def write_rake_tasks_help_message
+        puts RAKE_TASKS_HELP_MESSAGE
       end
-    end
 
-    def rake_tasks
-      return @rake_tasks if defined?(@rake_tasks)
-
-      require_application_and_environment!
-      Rake::TaskManager.record_task_metadata = true
-      Rake.application.instance_variable_set(:@name, 'rails')
-      Rails.application.load_tasks
-      @rake_tasks = Rake.application.tasks.select(&:comment)
-    end
-
-    def invoke_rake
-      Rake.application.standard_exception_handling do
-        Rake.application.init('rails')
-        Rake.application.load_rakefile
-        Rake.application.top_level
+      def write_rake_tasks
+        width = rake_tasks.map { |t| t.name_with_args.length }.max || 10
+        rake_tasks.each do |t|
+          printf("#{Rake.application.name} %-#{width}s  # %s\n", t.name_with_args, t.comment)
+        end
       end
-    end
+
+      def rake_tasks
+        return @rake_tasks if defined?(@rake_tasks)
+
+        require_application_and_environment!
+        Rake::TaskManager.record_task_metadata = true
+        Rake.application.instance_variable_set(:@name, 'rails')
+        Rails.application.load_tasks
+        @rake_tasks = Rake.application.tasks.select(&:comment)
+      end
+
+      def invoke_rake
+        Rake.application.standard_exception_handling do
+          Rake.application.init('rails')
+          Rake.application.load_rakefile
+          Rake.application.top_level
+        end
+      end
   end
 end
