d1d1e303a8be3f0cc025e5369cefa9d993575b24
David Heinemeier Hansson <david@loudthinking.com>
2015-12-16 20:26:29 +0100

    Only mount the ActionCable server if ApplicationCable has been defined

--- railties/lib/rails/generators/rails/app/templates/config/routes.rb
+++ railties/lib/rails/generators/rails/app/templates/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
-  mount ActionCable.server => '/cable'
+  mount ActionCable.server => '/cable' if defined?(ApplicationCable)
 end
