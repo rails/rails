f229d0451306508a8a16af337deef01b5d66b6d7
David Heinemeier Hansson <david@loudthinking.com>
2015-12-16 20:19:49 +0100

    By default, mount Action Cable on the router

--- railties/lib/rails/generators/rails/app/templates/config/routes.rb
+++ railties/lib/rails/generators/rails/app/templates/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
 end
