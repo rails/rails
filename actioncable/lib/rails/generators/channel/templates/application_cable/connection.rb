0625d77784a0e62b491ad730e4ff8566686acf81
David Heinemeier Hansson <david@loudthinking.com>
2015-12-16 18:23:11 +0100

    Note that changes to the cable setup requires restarting the server

--- actioncable/lib/rails/generators/channel/templates/application_cable/connection.rb
+++ actioncable/lib/rails/generators/channel/templates/application_cable/connection.rb
@@ -1,3 +1,4 @@
+# Be sure to restart your server when you modify this file. Action Cable runs in an EventMachine loop that does not support auto reloading.
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
