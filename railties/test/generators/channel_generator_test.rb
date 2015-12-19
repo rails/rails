61366f5a3db91cae4313cc9f19396e96cddf643a
Ryo Hashimoto <ryohashimoto@gmail.com>
2015-12-19 13:43:11 +0900

    Action Cable channel generator should not create JS assets in --api mode

---
+++ railties/test/generators/channel_generator_test.rb
@@ -0,0 +1,30 @@
+require 'generators/generators_test_helper'
+require 'rails/generators/channel/channel_generator'
+
+class ChannelGeneratorTest < Rails::Generators::TestCase
+  include GeneratorsTestHelper
+  tests Rails::Generators::ChannelGenerator
+
+  def test_channel_is_created
+    run_generator ['chat']
+    
+    assert_file "app/channels/chat_channel.rb" do |channel|
+      assert_match(/class ChatChannel < ApplicationCable::Channel/, channel)
+    end
+
+    assert_file "app/assets/javascripts/channels/chat.coffee" do |channel|
+      assert_match(/App.cable.subscriptions.create "ChatChannel"/, channel)
+    end
+  end
+
+  def test_channel_asset_is_not_created
+    Rails::Generators.options[:rails][:assets] = false
+    run_generator ['chat']
+
+    assert_file "app/channels/chat_channel.rb" do |channel|
+      assert_match(/class ChatChannel < ApplicationCable::Channel/, channel)
+    end
+
+    assert_no_file "app/assets/javascripts/channels/chat.coffee"
+  end
+end
