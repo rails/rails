2b562d750c553b7aa3e00e592e8a446cb5682b1c
Stan Lo <a22301613@yahoo.com.tw>
2015-12-23 16:43:44 +0800

    Basic test setup done

--- actionmailer/test/caching_test.rb
+++ actionmailer/test/caching_test.rb
@@ -8,7 +8,26 @@ require 'active_support/time'
 require 'mailers/base_mailer'
 require 'mailers/proc_mailer'
 require 'mailers/asset_mailer'
+require "byebug"
 
 class CachingTest < ActiveSupport::TestCase
   include Rails::Dom::Testing::Assertions::DomAssertions
+
+  def random_name
+    (0...8).map { (65 + rand(26)).chr }.join
+  end
+
+  test "mail template cache nothing by default" do
+    email1 = BaseMailer.welcome_with_random_string("uncached")
+    email2 = BaseMailer.welcome_with_random_string("uncached")
+
+    assert_not_equal(email1.body.encoded.strip, email2.body.encoded.strip)
+  end
+
+  test "mail template caches value just like normal view" do
+    email1 = BaseMailer.welcome_with_random_string("cached")
+    email2 = BaseMailer.welcome_with_random_string("cached")
+
+    assert_equal(email1.body.encoded.strip, email2.body.encoded.strip)
+  end
 end
