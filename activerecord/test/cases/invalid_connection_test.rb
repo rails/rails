4b1f67a11a6fd5680a7ce37b695a4d86e6a2c302
Yves Senn <yves.senn@gmail.com>
2015-12-28 09:00:38 +0100

    tests, don't connect to mysql when running sqlite3 tests.
    This resolves the following error:
    
    ```
    Error:
    TestAdapterWithInvalidConnection#test_inspect_on_Model_class_does_not_raise:
    TypeError: superclass mismatch for class Mysql2Adapter
        /Users/senny/Projects/rails/activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb:35:in `<module:ConnectionAdapters>'
        /Users/senny/Projects/rails/activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb:34:in `<module:ActiveRecord>'
        /Users/senny/Projects/rails/activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb:6:in `<top (required)>'
        /Users/senny/Projects/rails/activesupport/lib/active_support/dependencies.rb:302:in `require'
        /Users/senny/Projects/rails/activesupport/lib/active_support/dependencies.rb:302:in `block in require'
        /Users/senny/Projects/rails/activesupport/lib/active_support/dependencies.rb:268:in `load_dependency'
        /Users/senny/Projects/rails/activesupport/lib/active_support/dependencies.rb:302:in `require'
        /Users/senny/Projects/rails/activerecord/lib/active_record/connection_adapters/connection_specification.rb:174:in `spec'
        /Users/senny/Projects/rails/activerecord/lib/active_record/connection_handling.rb:50:in `establish_connection'
        /Users/senny/Projects/rails/activerecord/test/cases/invalid_connection_test.rb:12:in `setup'
    ```

--- activerecord/test/cases/invalid_connection_test.rb
+++ activerecord/test/cases/invalid_connection_test.rb
@@ -1,5 +1,6 @@
 require "cases/helper"
 
+if current_adapter?(:Mysql2Adapter)
 class TestAdapterWithInvalidConnection < ActiveRecord::TestCase
   self.use_transactional_tests = false
 
@@ -19,4 +20,5 @@ class TestAdapterWithInvalidConnection < ActiveRecord::TestCase
   test "inspect on Model class does not raise" do
     assert_equal "#{Bird.name} (call '#{Bird.name}.connection' to establish a connection)", Bird.inspect
   end
+end
 end
