333aab3d51124a00c7f2ba498d4910eb7cb570e6
Akira Matsuda <ronnie@dio.jp>
2016-01-05 12:57:51 +0900

    :scissors: Unnecessary spacing in the generated code

--- railties/lib/rails/generators/rails/plugin/templates/test/test_helper.rb
+++ railties/lib/rails/generators/rails/plugin/templates/test/test_helper.rb
@@ -1,7 +1,7 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require File.expand_path("../../<%= options[:dummy_path] -%>/config/environment.rb",  __FILE__)
+require File.expand_path("../../<%= options[:dummy_path] -%>/config/environment.rb", __FILE__)
 <% unless options[:skip_active_record] -%>
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../<%= options[:dummy_path] -%>/db/migrate", __FILE__)]
 <% if options[:mountable] -%>
