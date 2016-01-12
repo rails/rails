a6107a08e78869c5e08a06ab6ff4b5652151567d
Fabio Napoleoni <f.napoleoni@gmail.com>
2016-01-12 18:08:36 +0100

    EventProxy to provide broadcasting of client side events with a nice DSL

--- actioncable/lib/action_cable/channel/broadcasting.rb
+++ actioncable/lib/action_cable/channel/broadcasting.rb
@@ -9,7 +9,8 @@ module ActionCable
 
       class_methods do
         # Broadcast a hash to a unique broadcasting for this <tt>model</tt> in this channel.
-        def broadcast_to(model, message)
+        def broadcast_to(model, message = nil)
+          return EventProxy.new(self, model) if message.nil? # If invoked with single argument
           ActionCable.server.broadcast(broadcasting_for([ channel_name, model ]), message)
         end
 
@@ -22,6 +23,19 @@ module ActionCable
           else
             model.to_param
           end
+        end
+      end
+
+      # This class is used internally to provide a method proxy for evented client side channels
+      class EventProxy
+        def initialize(channel, model)
+          @channel, @model = channel, model
+        end
+
+        def method_missing(meth, *args)
+          message = {event_name: meth}
+          message[:args] = args if args.any?
+          @channel.broadcast_to(@model, message)
         end
       end
     end
--- actioncable/test/channel/broadcasting_test.rb
+++ actioncable/test/channel/broadcasting_test.rb
@@ -15,6 +15,22 @@ class ActionCable::Channel::BroadcastingTest < ActiveSupport::TestCase
     ChatChannel.broadcast_to(Room.new(1), "Hello World")
   end
 
+  test "broadcast_to with single argument" do
+    proxy = ChatChannel.broadcast_to(Room.new(1))
+    assert_instance_of ActionCable::Channel::Broadcasting::EventProxy, proxy
+  end
+
+  test "proxied methods are converted in an event hash" do
+    ActionCable.stubs(:server).returns (server = mock)
+    arguments = {event_name: :some_event, args: [:foo, :bar]}
+    server.expects(:broadcast).with('action_cable:channel:broadcasting_test:chat:Room#1-Campfire', arguments)
+    # Proxied methods are converted into json messages and broadcasted
+    ChatChannel.broadcast_to(Room.new(1)).some_event(:foo, :bar)
+    # Without arguments it only broadcast event name
+    server.expects(:broadcast).with('action_cable:channel:broadcasting_test:chat:Room#1-Campfire', event_name: :some_other_event)
+    ChatChannel.broadcast_to(Room.new(1)).some_other_event
+  end
+
   test "broadcasting_for with an object" do
     assert_equal "Room#1-Campfire", ChatChannel.broadcasting_for(Room.new(1))
   end
