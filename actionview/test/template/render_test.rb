be543e8e18246d5c2faa336eb3c0a03b797f3517
Aaron Patterson <aaron.patterson@gmail.com>
2016-01-20 10:39:19 -0800

    allow :file to be outside rails root, but anything else must be inside the rails view directory
    Conflicts:
    	actionpack/test/controller/render_test.rb
    	actionview/lib/action_view/template/resolver.rb
    
    CVE-2016-0752

--- actionview/test/template/render_test.rb
+++ actionview/test/template/render_test.rb
@@ -142,6 +142,13 @@ module RenderTestCases
     assert_equal "only partial", @view.render("test/partial_only")
   end
 
+  def test_render_outside_path
+    assert File.exist?(File.join(File.dirname(__FILE__), '../../test/abstract_unit.rb'))
+    assert_raises ActionView::MissingTemplate do
+      @view.render(:template => "../\\../test/abstract_unit.rb")
+    end
+  end
+
   def test_render_partial
     assert_equal "only partial", @view.render(:partial => "test/partial_only")
   end
