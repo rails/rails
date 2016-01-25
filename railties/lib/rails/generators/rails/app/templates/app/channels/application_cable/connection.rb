6accef4e11b0c793e1c085536b5ed27f32b6a0c3
claudiob <claudiob@gmail.com>
2016-01-24 20:48:25 -0800

    [ci skip] Don’t explicitly mention EventMachine
    Since #23152 eliminated the EventMachine dependency, we don’t need to
    explicitly mention EventMachine.
    
    Nevertheless, I'm not 100% sure about saying "the websocket-driver loop"
    driver… any suggestions, @matthewd or @pixeltrix ? :sweat_smile:
    
    [ci skip]

--- railties/lib/rails/generators/rails/app/templates/app/channels/application_cable/connection.rb
+++ railties/lib/rails/generators/rails/app/templates/app/channels/application_cable/connection.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your server when you modify this file. Action Cable runs in an EventMachine loop that does not support auto reloading.
+# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
   end
