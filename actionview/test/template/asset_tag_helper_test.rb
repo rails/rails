e5e42a3687801a1dc1c3e36ea784a7a1479a9230
Rafael Mendonça França <rafaelmfranca@gmail.com>
2016-01-27 14:12:32 -0500

    Add tests to #23288

--- actionview/test/template/asset_tag_helper_test.rb
+++ actionview/test/template/asset_tag_helper_test.rb
@@ -459,6 +459,7 @@ class AssetTagHelperTest < ActionView::TestCase
     [nil, '/', '/foo/bar/', 'foo/bar/'].each do |prefix|
       assert_equal 'Rails', image_alt("#{prefix}rails.png")
       assert_equal 'Rails', image_alt("#{prefix}rails-9c0a079bdd7701d7e729bd956823d153.png")
+      assert_equal 'Rails', image_alt("#{prefix}rails-f56ef62bc41b040664e801a38f068082a75d506d9048307e8096737463503d0b.png")
       assert_equal 'Long file name with hyphens', image_alt("#{prefix}long-file-name-with-hyphens.png")
       assert_equal 'Long file name with underscores', image_alt("#{prefix}long_file_name_with_underscores.png")
     end
