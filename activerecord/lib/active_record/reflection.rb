eba55447f092dfa99dc813dc7ea21cee9c26b85c
yui-knk <spiketeika@gmail.com>
2016-01-29 14:10:33 +0900

    [ci skip] Update internal documents about ActiveRecord's Reflection
    ActiveRecord's Reflection was refactored by
    f8d2899d12d59360f29c5eb6a1b1a8fe4ec82ca0 .
    Top of ancestors chain was changed to `AbstractReflection` from
    `MacroReflection`, and new Reflections were added.

--- activerecord/lib/active_record/reflection.rb
+++ activerecord/lib/active_record/reflection.rb
@@ -124,9 +124,20 @@ module ActiveRecord
       end
     end
 
-    # Holds all the methods that are shared between MacroReflection, AssociationReflection
-    # and ThroughReflection
-    class AbstractReflection # :nodoc:
+    # Holds all the methods that are shared between MacroReflection and ThroughReflection.
+    #
+    #   AbstractReflection
+    #     MacroReflection
+    #       AggregateReflection
+    #       AssociationReflection
+    #         HasManyReflection
+    #         HasOneReflection
+    #         BelongsToReflection
+    #         HasAndBelongsToManyReflection
+    #     ThroughReflection
+    #       PolymorphicReflection
+    #         RuntimeReflection
+     class AbstractReflection # :nodoc:
       def table_name
         klass.table_name
       end
@@ -232,14 +243,6 @@ module ActiveRecord
 
     # Base class for AggregateReflection and AssociationReflection. Objects of
     # AggregateReflection and AssociationReflection are returned by the Reflection::ClassMethods.
-    #
-    #   MacroReflection
-    #     AggregateReflection
-    #     AssociationReflection
-    #       HasManyReflection
-    #       HasOneReflection
-    #       BelongsToReflection
-    #         ThroughReflection
     class MacroReflection < AbstractReflection
       # Returns the name of the macro.
       #
