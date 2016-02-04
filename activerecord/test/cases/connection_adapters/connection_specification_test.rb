d871696b4a8b021b67a3ce6c3cb5d5dcbbb578fe
Jeremy Baker <jhubert@gmail.com>
2016-02-04 10:59:52 -0800

    Add a resolver test for the missing scheme

--- activerecord/test/cases/connection_adapters/connection_specification_test.rb
+++ activerecord/test/cases/connection_adapters/connection_specification_test.rb
@@ -7,6 +7,11 @@ module ActiveRecord
         spec = ConnectionSpecification.new({ :a => :b }, "bar")
         assert_not_equal(spec.config.object_id, spec.dup.config.object_id)
       end
+
+      def test_handle_missing_scheme
+        spec = ConnectionSpecification.new({ :url => 'testing' }, "bar")
+        assert_not_equal(spec.config.object_id, spec.dup.config.object_id)
+      end
     end
   end
 end
