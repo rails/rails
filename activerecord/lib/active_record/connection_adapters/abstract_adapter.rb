36d1dbf63a4928f8d321d09a0a1e7a3d721e8cc1
Ryuta Kamizono <kamipo@gmail.com>
2016-02-06 22:42:52 +0900

    Remove unused require
    `require 'active_support/core_ext/benchmark'` was added by 4ecdf24.
    But currently unused anymore.

--- activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
@@ -1,5 +1,4 @@
 require 'active_record/type'
-require 'active_support/core_ext/benchmark'
 require 'active_record/connection_adapters/determine_if_preparable_visitor'
 require 'active_record/connection_adapters/schema_cache'
 require 'active_record/connection_adapters/sql_type_metadata'
