b2d1de2697f70239e2e80d93c0d728cd8509eae9
Lobsiinvok <qazbnm456@gmail.com>
2016-02-20 17:36:49 +0800

    fix typo

--- activerecord/test/cases/associations/has_and_belongs_to_many_associations_test.rb
+++ activerecord/test/cases/associations/has_and_belongs_to_many_associations_test.rb
@@ -146,7 +146,7 @@ class HasAndBelongsToManyAssociationsTest < ActiveRecord::TestCase
     assert_equal 1, country.treaties.count
   end
 
-  def test_join_table_composit_primary_key_should_not_warn
+  def test_join_table_composite_primary_key_should_not_warn
     country = Country.new(:name => 'India')
     country.country_id = 'c1'
     country.save!
