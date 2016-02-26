57e3d863be979019d99f232d858c5b272d19c724
Rafael Laurindo <rafaelplaurindo@gmail.com>
2016-02-26 00:33:58 -0300

    update line code of activerecord/lib/active_record/migration.rb as rails origin repository

--- activerecord/lib/active_record/migration.rb
+++ activerecord/lib/active_record/migration.rb
@@ -1076,9 +1076,7 @@ module ActiveRecord
         files = Dir[*paths.map { |p| "#{p}/[0-9]*_*.rb" }]
 
         migrations = files.map do |file|
-          version, name, scope = file
-            .scan(/([0-9]+)_([_a-z0-9]*)\.?([_a-z0-9]*)?\.rb\z/).first
-
+          version, name, scope = parse_migration_filename(file)
           raise IllegalMigrationNameError.new(file) unless version
           version = version.to_i
           name = name.camelize
