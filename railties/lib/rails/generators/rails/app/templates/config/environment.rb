5a06beda7e44185ef2a1bbbee58d682ad77a1bd7
Xavier Noria <fxn@hashref.com>
2016-03-06 09:47:42 +0100

    use __dir__ to load the application in environment.rb
    Stuff I see in passing while I am doing a walkthrough.
    
    See rationale in 5b8738c.

--- railties/lib/rails/generators/rails/app/templates/config/environment.rb
+++ railties/lib/rails/generators/rails/app/templates/config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
-require File.expand_path('../application', __FILE__)
+require File.expand_path('application', __dir__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
