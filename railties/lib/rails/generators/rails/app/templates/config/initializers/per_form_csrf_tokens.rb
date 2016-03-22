317f9b3b4e0dfedc0753541052317db021b25913
Luke Horvat <lukehorvat@gmail.com>
2016-03-22 12:27:33 +1100

    Use Rails.configuration instead of Rails.application.config in initializers

--- railties/lib/rails/generators/rails/app/templates/config/initializers/per_form_csrf_tokens.rb
+++ railties/lib/rails/generators/rails/app/templates/config/initializers/per_form_csrf_tokens.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Enable per-form CSRF tokens.
-Rails.application.config.action_controller.per_form_csrf_tokens = true
+Rails.configuration.action_controller.per_form_csrf_tokens = true
--- railties/lib/rails/generators/rails/app/templates/config/initializers/ssl_options.rb
+++ railties/lib/rails/generators/rails/app/templates/config/initializers/ssl_options.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure SSL options to enable HSTS with subdomains.
-Rails.application.config.ssl_options = { hsts: { subdomains: true } }
+Rails.configuration.ssl_options = { hsts: { subdomains: true } }
