4e621822c65717c1d8c6c91afb5d8a422aecb077
bogdanvlviv <bogdanvlviv@gmail.com>
2016-03-23 16:24:05 +0200

    improved template of model

--- activerecord/lib/rails/generators/active_record/model/templates/model.rb
+++ activerecord/lib/rails/generators/active_record/model/templates/model.rb
@@ -1,5 +1,5 @@
 <% module_namespacing do -%>
-class <%= class_name %> < <%= parent_class_name.classify %>
+class <%= class_name %> < ApplicationRecord
 <% attributes.select(&:reference?).each do |attribute| -%>
   belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', required: true' if attribute.required? %>
 <% end -%>
