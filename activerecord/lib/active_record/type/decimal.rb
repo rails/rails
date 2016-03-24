8f4f3af580520760d90d8df8c2da9075590c8af4
Sean Griffin <sean@seantheprogrammer.com>
2016-03-24 16:06:40 -0600

    Apply scale before precision when coercing floats to decimal
    Since precision is always larger than scale, it can actually change
    rounding behavior. Given a precision of 5 and a scale of 3, when you
    apply the precision of 5 to `1.25047`, the result is `1.2505`, which
    when the scale is applied would be `1.251` instead of the expected
    `1.250`.
    
    This issue appears to only occur with floats, as scale doesn't apply to
    other numeric types, and the bigdecimal constructor actually ignores
    precision entirely when working with strings. There's no way we could
    handle this for the "unknown object which responds to `to_d`" case, as
    we can't assume an interface for applying the scale.
    
    Fixes #24235

--- activerecord/lib/active_record/type/decimal.rb
+++ activerecord/lib/active_record/type/decimal.rb
@@ -27,12 +27,12 @@ module ActiveRecord
           end
         end
 
-        scale ? casted_value.round(scale) : casted_value
+        apply_scale(casted_value)
       end
 
       def convert_float_to_big_decimal(value)
         if precision
-          BigDecimal(value, float_precision)
+          BigDecimal(apply_scale(value), float_precision)
         else
           value.to_d
         end
@@ -43,6 +43,14 @@ module ActiveRecord
           ::Float::DIG + 1
         else
           precision.to_i
+        end
+      end
+
+      def apply_scale(value)
+        if scale
+          value.round(scale)
+        else
+          value
         end
       end
     end
