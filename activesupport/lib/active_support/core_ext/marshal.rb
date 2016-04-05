b7f1860074293781e365dfed1a7bd58f360d2ad5
Rafael França <rafaelmfranca@gmail.com>
2016-04-05 00:42:10 -0300

    Merge pull request #24345 from mtsmfm/fix-marshal-with-autoloading-for-nested-class
    Fix marshal with autoloading for nested class/module

--- activesupport/lib/active_support/core_ext/marshal.rb
+++ activesupport/lib/active_support/core_ext/marshal.rb
@@ -5,7 +5,7 @@ module Marshal
     def load_with_autoloading(source)
       load_without_autoloading(source)
     rescue ArgumentError, NameError => exc
-      if exc.message.match(%r|undefined class/module (.+)|)
+      if exc.message.match(%r|undefined class/module (.+?)(::)?\z|)
         # try loading the class/module
         loaded = $1.constantize
 
