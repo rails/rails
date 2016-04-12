rails/rails

commit 04ac5655be91f49cd4dfe2838df96213502fb274

Ensure associations still work when the table name contains a dot
This issue occured because associations now call `where` directly, and a
dot in the key name for `where` means nested tables. For this fix, we
now pass the table name as a symbol, and do not attempt to expand
symbols containing a dot.

This is a temporary fix. I do not think we should support table names
containing a dot, as it has a special meaning in most backends, as well
as most APIs that involve table names. This commit does not include a
test, as I am going to deprecate table names containing dots in the
following commit.

Fixes #24367

activerecord/lib/active_record/associations/association_scope.rb
@@ -64,11 +64,11 @@ module ActiveRecord
         foreign_key = join_keys.foreign_key
 
         value = transform_value(owner[foreign_key])
-        scope = scope.where(table.name => { key => value })
+        scope = scope.where(table.name.to_sym => { key => value })
 
         if reflection.type
           polymorphic_type = transform_value(owner.class.base_class.name)
-          scope = scope.where(table.name => { reflection.type => polymorphic_type })
+          scope = scope.where(table.name.to_sym => { reflection.type => polymorphic_type })
         end
 
         scope

activerecord/lib/active_record/relation/predicate_builder.rb
@@ -84,6 +84,7 @@ module ActiveRecord
       return ["1=0"] if attributes.empty?
 
       attributes.flat_map do |key, value|
+        key = key.to_s
         if value.is_a?(Hash)
           associated_predicate_builder(key).expand_from_hash(value)
         else
@@ -136,7 +137,9 @@ module ActiveRecord
     end
 
     def convert_dot_notation_to_hash(attributes)
-      dot_notation = attributes.keys.select { |s| s.include?(".".freeze) }
+      dot_notation = attributes.keys.select do |s|
+        s.respond_to?(:include?) && s.include?(".".freeze)
+      end
 
       dot_notation.each do |key|
         table_name, column_name = key.split(".".freeze)

activerecord/lib/active_record/relation/where_clause_factory.rb
@@ -15,7 +15,6 @@ module ActiveRecord
         when Hash
           attributes = predicate_builder.resolve_column_aliases(opts)
           attributes = klass.send(:expand_hash_conditions_for_aggregates, attributes)
-          attributes.stringify_keys!
 
           attributes, binds = predicate_builder.create_binds(attributes)
 

commit fb6f27d4186215a9c52ca0cb220903d68384c13b

Test coverage for dot notation with keyword-style args
e.g.
```ruby
foo.joins(:topics).where('topics.approved': false)
```

Demonstrates regression in 04ac565 due to missing test coverage.

activerecord/test/cases/finder_test.rb
@@ -652,9 +652,14 @@ class FinderTest < ActiveRecord::TestCase
     assert_raise(ActiveRecord::RecordNotFound) { Topic.where(approved: true).find(1) }
   end
 
-  def test_find_on_hash_conditions_with_explicit_table_name
+  def test_find_on_hash_conditions_with_qualified_attribute_dot_notation_string
     assert Topic.where('topics.approved' => false).find(1)
     assert_raise(ActiveRecord::RecordNotFound) { Topic.where('topics.approved' => true).find(1) }
+  end
+
+  def test_find_on_hash_conditions_with_qualified_attribute_dot_notation_symbol
+    assert Topic.where('topics.approved': false).find(1)
+    assert_raise(ActiveRecord::RecordNotFound) { Topic.where('topics.approved': true).find(1) }
   end
 
   def test_find_on_hash_conditions_with_hashed_table_name
@@ -664,6 +669,7 @@ class FinderTest < ActiveRecord::TestCase
 
   def test_find_on_combined_explicit_and_hashed_table_names
     assert Topic.where('topics.approved' => false, topics: { author_name: "David" }).find(1)
+    assert Topic.where('topics.approved': false, topics: { author_name: "David" }).find(1)
     assert_raise(ActiveRecord::RecordNotFound) { Topic.where('topics.approved' => true, topics: { author_name: "David" }).find(1) }
     assert_raise(ActiveRecord::RecordNotFound) { Topic.where('topics.approved' => false, topics: { author_name: "Melanie" }).find(1) }
   end