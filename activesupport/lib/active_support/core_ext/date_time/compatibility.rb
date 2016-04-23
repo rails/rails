941eee5c8ebb82f97434f93fb233502cbb4f8270
yui-knk <spiketeika@gmail.com>
2016-04-23 23:51:49 +0900

    Move `DateTime#getlocal` to `/core_ext/date_time/calculations.rb`
    `DateTime#getlocal` is newly added public API.
    It's responsible is same as `DateTime#utc`, so `calculations.rb` is
    a best plase to define this method.
    For keeping consistency with `DateTime#utc`, defines `#localtime` and
    defines `getlocal` as an alias method.

--- activesupport/lib/active_support/core_ext/date_time/compatibility.rb
+++ activesupport/lib/active_support/core_ext/date_time/compatibility.rb
@@ -2,15 +2,4 @@ require 'active_support/core_ext/date_and_time/compatibility'
 
 class DateTime
   prepend DateAndTime::Compatibility
-
-  # Returns a <tt>Time.local()</tt> instance of the simultaneous time in your
-  # system's <tt>ENV['TZ']</tt> zone.
-  def getlocal(utc_offset = nil)
-    utc = getutc
-
-    Time.utc(
-      utc.year, utc.month, utc.day,
-      utc.hour, utc.min, utc.sec + utc.sec_fraction
-    ).getlocal(utc_offset)
-  end
 end
