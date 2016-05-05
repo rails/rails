743b5bbd311a4c696cf95d2cf21994c8d2806764
Jake Howerton <jake@zipmark.com>
2016-05-05 17:32:02 -0400

    check for database connection prior to enabling query cache in middleware

--- activerecord/lib/active_record/query_cache.rb
+++ activerecord/lib/active_record/query_cache.rb
@@ -24,27 +24,31 @@ module ActiveRecord
     end
 
     def self.run
-      connection    = ActiveRecord::Base.connection
-      enabled       = connection.query_cache_enabled
-      connection_id = ActiveRecord::Base.connection_id
-      connection.enable_query_cache!
+      if ActiveRecord::Base.connected?
+        connection    = ActiveRecord::Base.connection
+        enabled       = connection.query_cache_enabled
+        connection_id = ActiveRecord::Base.connection_id
+        connection.enable_query_cache!
 
-      [enabled, connection_id]
+        [enabled, connection_id]
+      end
     end
 
     def self.complete(state)
-      enabled, connection_id = state
+      if ActiveRecord::Base.connected?
+        enabled, connection_id = state
 
-      ActiveRecord::Base.connection_id = connection_id
-      ActiveRecord::Base.connection.clear_query_cache
-      ActiveRecord::Base.connection.disable_query_cache! unless enabled
+        ActiveRecord::Base.connection_id = connection_id
+        ActiveRecord::Base.connection.clear_query_cache
+        ActiveRecord::Base.connection.disable_query_cache! unless enabled
+      end
     end
 
     def self.install_executor_hooks(executor = ActiveSupport::Executor)
       executor.register_hook(self)
 
       executor.to_complete do
-        unless ActiveRecord::Base.connection.transaction_open?
+        unless ActiveRecord::Base.connected? && ActiveRecord::Base.connection.transaction_open?
           ActiveRecord::Base.clear_active_connections!
         end
       end
