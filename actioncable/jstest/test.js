d5d191d36da8463b7393b4d4529147732225cbcf
Jon Moss <me@jonathanmoss.me>
2016-05-07 10:54:58 -0400

    Install testing harness and run in Travis CI

---
+++ actioncable/jstest/test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
