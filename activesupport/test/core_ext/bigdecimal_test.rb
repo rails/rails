330e011c6c7a56a09e7f26db8750a19ca7865261
Yosuke Kabuto <bluewhale1982@gmail.com>
2016-05-15 14:49:53 +0900

    Add some assertions for BigDecimal#to_s

--- activesupport/test/core_ext/bigdecimal_test.rb
+++ activesupport/test/core_ext/bigdecimal_test.rb
@@ -5,5 +5,7 @@ class BigDecimalTest < ActiveSupport::TestCase
   def test_to_s
     bd = BigDecimal.new '0.01'
     assert_equal '0.01', bd.to_s
+    assert_equal '+0.01', bd.to_s('+F')
+    assert_equal '+0.0 1', bd.to_s('+1F')
   end
 end
