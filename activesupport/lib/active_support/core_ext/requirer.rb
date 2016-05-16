58aa39a1a6bd59b57bf10c0b3a75fefdd9ddf995
Yosuke Kabuto <bluewhale1982@gmail.com>
2016-05-16 13:33:59 +0900

    Refactor Requirer#require_all

--- activesupport/lib/active_support/core_ext/requirer.rb
+++ activesupport/lib/active_support/core_ext/requirer.rb
@@ -8,7 +8,7 @@
 #                                                   └── wrap.rb
 #
 # If you want to require all files in the array directory,
-# you can use Kernel.require(file) like below.
+# you can use Kernel.require(file) like below as always.
 #
 # rails/activesupport/lib/active_support/core_ext/array.rb
 #   require 'active_support/core_ext/array/access'
@@ -19,22 +19,26 @@
 #   require 'active_support/core_ext/array/prepend_and_append'
 #   require 'active_support/core_ext/array/wrap'
 #
-# Or, you can use Requirer#require.
-#   Requirer.new(__FILE__).require
+# Or, you can use Requirer#require_all.
+#   Requirer.new(__FILE__).require_all
+#
+# If the first parameter of Requirer constructor is "/path/to/example.rb",
+# all files will be required recursively which are in "/path/to/example".
 #
 # Of course, you can exclude file(s) like this.
-#   Requirer.new(__FILE__, exclude: ['access', 'wrap']).require
+#   Requirer.new(__FILE__, exclude: ['access', 'wrap']).require_all
 #     Or
-#   Requirer.new(__FILE__).require(exclude: ['access', 'wrap'])
+#   Requirer.new(__FILE__).require_all(exclude: ['access', 'wrap'])
 #
 class Requirer
   DEFAULT_EXTENSION = '.rb'.freeze
+
   def initialize(file, exclude: [])
     @cwd      = dir(file)
     @excluded = normalize_files(exclude)
   end
 
-  def require(exclude: [])
+  def require_all(exclude: [])
     @excluded += normalize_files(exclude)
 
     files =
@@ -47,10 +51,14 @@ class Requirer
   private
 
   def dir(file)
-    Pathname(
-      File
-      .absolute_path(file)
-      .sub(/#{Regexp.new(DEFAULT_EXTENSION)}$/, ''))
+    directory =
+      Pathname(
+        File
+        .absolute_path(file)
+        .sub(/#{Regexp.new(DEFAULT_EXTENSION)}$/, ''))
+
+    return directory if directory.directory?
+    raise LoadError, "directory \"#{directory}\" does not exist."
   end
 
   def normalize_files(files)
@@ -60,13 +68,25 @@ class Requirer
   end
 end
 
+# Define Array#append_extension(extension = '.rb')
+# which does append the extension to each string element.
+#
+# [1, [], 'hogerb', 'fuga.rb'].append_extension
+# => [1, [], 'hogerb.rb', 'fuga.rb']
+#
 class Array
-  def append_extension(ext = '.rb')
-    map do |elm|
-      if elm.is_a?(String) && elm !~ /#{Regexp.new(ext)}$/
-        elm + ext
-      else
-        elm
+  method_name = :append_extension
+
+  if instance_methods(false).include? method_name
+    raise NameError, "##{method_name} has already been defined."
+  else
+    define_method method_name do |ext = '.rb'|
+      map do |elm|
+        if elm.is_a?(String) && elm !~ /#{Regexp.escape(ext)}$/
+          elm + ext
+        else
+          elm
+        end
       end
     end
   end
--- activesupport/test/core_ext/requirer_test.rb
+++ activesupport/test/core_ext/requirer_test.rb
@@ -3,64 +3,84 @@ require 'active_support/core_ext/requirer'
 
 class ArrayTest < ActiveSupport::TestCase
   def test_append_extension
-    list = [1, [], 'hoge', 'fuga.rb']
-    assert_equal [1, [], 'hoge.rb', 'fuga.rb'], list.append_extension
+    list = [1, [], 'hogerb', 'fuga.rb']
+    assert_equal [1, [], 'hogerb.rb', 'fuga.rb'], list.append_extension
   end
 end
 
 class RequirerTest < ActiveSupport::TestCase
-  MOCK_PATH = '/Your/App/Path/requirer.rb'.freeze
+  MOCK_PATH      = '/tmp'.freeze
+  MOCK_BASE_FILE = '/tmp.rb'.freeze
+  MOCK_FILES     = %w(a.rb b.rb c.rb).map { |f| [MOCK_PATH, f].join('/') }
+
+  def setup
+    FileUtils.touch MOCK_FILES
+  end
+
+  def cleanup
+    FileUtils.rm MOCK_FILES
+  end
 
   def test_initialize_without_parameter_exclude
+    setup
     rq = Requirer.new(MOCK_PATH)
 
-    assert_equal rq.send(:dir, MOCK_PATH), rq.instance_variable_get(:@cwd)
+    assert_equal rq.send(:dir, MOCK_BASE_FILE), rq.instance_variable_get(:@cwd)
     assert_equal [], rq.instance_variable_get(:@excluded)
+  ensure
+    cleanup
   end
 
   def test_initialize_with_parameter_exclude
-    files_to_be_excluded = %w(a b c.rb)
-    rq = Requirer.new(MOCK_PATH, exclude: files_to_be_excluded)
+    setup
+    files_to_be_excluded = %w(b c.rb)
+    rq = Requirer.new(MOCK_BASE_FILE, exclude: files_to_be_excluded)
 
-    assert_equal(rq.send(:dir, MOCK_PATH), rq.instance_variable_get(:@cwd))
-    assert_equal(
-      %w(a b c).map { |f| ['/Your/App/Path/requirer', f].join('/') },
-      rq.instance_variable_get(:@excluded))
+    assert_equal(rq.send(:dir, MOCK_BASE_FILE), rq.instance_variable_get(:@cwd))
+    assert_equal %w(/tmp/b /tmp/c), rq.instance_variable_get(:@excluded)
+  ensure
+    cleanup
   end
 
   def test_dir
-    rq           = Requirer.new(MOCK_PATH)
-    return_value = rq.send(:dir, MOCK_PATH)
+    setup
+    rq           = Requirer.new(MOCK_BASE_FILE)
+    return_value = rq.send(:dir, MOCK_BASE_FILE)
 
-    assert_equal Pathname, return_value.class
-    assert_equal '/Your/App/Path/requirer', return_value.to_s
+    assert_kind_of Pathname, return_value
+    assert_equal MOCK_PATH, return_value.to_s
+  ensure
+    cleanup
+  end
+
+  def test_dir_which_does_not_exist
+    assert_raise LoadError do
+      Requirer.new('/tmp/this_does_not_exist.rb')
+    end
   end
 
   def test_normalize_files
-    rq           = Requirer.new(MOCK_PATH)
+    setup
+    rq           = Requirer.new(MOCK_BASE_FILE)
     files        = %w(x yarb z.rb)
     return_value = rq.send(:normalize_files, files)
 
-    assert_equal(
-      %w(x yarb z).map { |f| ['/Your/App/Path/requirer', f].join('/') },
-      return_value)
+    assert_equal %w(/tmp/x /tmp/yarb /tmp/z), return_value
+  ensure
+    cleanup
   end
 
-  fork do
-    def test_require
-      files_to_be_excluded = %w(wrap.rb access)
-      path  = "#{Rails.root}/lib/active_support/core_ext/array"
-      files =
-        %w(conversions extract_options grouping inquiry
-           prepend_and_append)
+  def test_require
+    setup
+    files_to_be_excluded = %w(b c.rb)
+    Requirer.new(MOCK_BASE_FILE, exclude: files_to_be_excluded).require_all
 
-      Requirer.new(path, exclude: files_to_be_excluded).require
+    %w(a).each { |f| assert_equal false, require("#{MOCK_PATH}/#{f}") }
 
-      files.each { |f| assert_equal false, require("#{path}/#{f}") }
-
-      files_to_be_excluded.each do |f|
-        assert_equal true, require("#{path}/#{File.basename(f)}")
-      end
+    files_to_be_excluded.each do |f|
+      assert_equal true, require("#{MOCK_PATH}/#{File.basename(f)}")
     end
+  ensure
+    cleanup
   end
 end
