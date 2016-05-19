73333d5817e87ef22288eddd75f4544f312e7fd7
Omkar Patil <omkar.patil@gmail.com>
2016-05-19 18:20:58 +0530

    Options can be specified to has_secure_token method

--- activerecord/lib/active_record/secure_token.rb
+++ activerecord/lib/active_record/secure_token.rb
@@ -7,8 +7,9 @@ module ActiveRecord
       #
       #   # Schema: User(token:string, auth_token:string)
       #   class User < ActiveRecord::Base
-      #     has_secure_token
+      #     has_secure_token  # OR has_secure_token uniq: true
       #     has_secure_token :auth_token
+      #     has_secure_token :api_key, uniq: true, length: 32
       #   end
       #
       #   user = User.new
@@ -23,16 +24,44 @@ module ActiveRecord
       # Note that it's still possible to generate a race condition in the database in the same way that
       # {validates_uniqueness_of}[rdoc-ref:Validations::ClassMethods#validates_uniqueness_of] can.
       # You're encouraged to add a unique index in the database to deal with this even more unlikely scenario.
-      def has_secure_token(attribute = :token)
+      def has_secure_token(attribute = :token, options = {})
+        attribute, options = :token, attribute if options.empty? && attribute.is_a?(Hash)
+        options = options.with_indifferent_access
         # Load securerandom only when has_secure_token is used.
         require 'active_support/core_ext/securerandom'
-        define_method("regenerate_#{attribute}") { update! attribute => self.class.generate_unique_secure_token }
-        before_create { self.send("#{attribute}=", self.class.generate_unique_secure_token) unless self.send("#{attribute}?")}
+
+        define_method("regenerate_#{attribute}") do
+          token = self.class.generate_unique_secure_token(attribute, options)
+          update_attributes attribute => token
+        end
+
+        before_create do
+          unless self.send("#{attribute}?")
+            self.send("#{attribute}=", self.class.generate_unique_secure_token(attribute, options))
+          end
+        end
       end
 
-      def generate_unique_secure_token
-        SecureRandom.base58(24)
+      def generate_unique_secure_token(attribute, options)
+        if options[:uniq]
+          random_token_for_class(self, attribute, options)
+        else
+          secure_random_token(options)
+        end
       end
+
+      def random_token_for_class(klass, attribute, options)
+        loop do
+          random_token = secure_random_token(options)
+          break random_token unless klass.exists?(attribute => random_token)
+        end
+      end
+
+      def secure_random_token(options)
+        length = options[:length] || 24
+        SecureRandom.base58(length)
+      end
+
     end
   end
 end
