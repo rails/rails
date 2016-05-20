4f8f3e55716045ab0e7a0e492bfdaabac8135009
Charles Skariah <charles2101@gmail.com>
2016-05-21 02:29:28 +0530

    correcting intentation
    correcting indentation of the application

--- actioncable/lib/rails/generators/channel/templates/channel.rb
+++ actioncable/lib/rails/generators/channel/templates/channel.rb
@@ -1,17 +1,18 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 <% module_namespacing do -%>
-class <%= class_name %>Channel < ApplicationCable::Channel
-  def subscribed
-    # stream_from "some_channel"
-  end
+  class <%= class_name %>Channel < ApplicationCable::Channel
+    def subscribed
+      # stream_from "some_channel"
+    end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    def unsubscribed
+      # Any cleanup needed when channel is unsubscribed
+    end
+    
+    <% actions.each do |action| -%>
+      def <%= action %>
+      end
+    <% end -%>
+    
   end
-<% actions.each do |action| -%>
-
-  def <%= action %>
-  end
-<% end -%>
-end
 <% end -%>
