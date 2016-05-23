661ed303f49128b92a5a88d25deebcebbc891b55
Rafael Mendonça França <rafaelmfranca@gmail.com>
2016-05-23 20:48:43 -0300

    Revert "Add default exceptions affected by suppress (#25099)"
    This reverts commit 28492204ee59a5aca2f3bc7b161d45724552686d.
    
    Reason: `suppress` without an argument doesn't actually tell what is
    supressing. Also, it can be confused with ActiveRecord::Base#suppress.

--- activesupport/test/core_ext/kernel_test.rb
+++ activesupport/test/core_ext/kernel_test.rb
@@ -42,15 +42,6 @@ class KernelSuppressTest < ActiveSupport::TestCase
     end
   end
 
-  def test_suppress_with_defaults
-    suppress { raise RuntimeError }
-    suppress { raise ArgumentError }
-
-    assert_raise(LoadError) do
-      suppress { raise LoadError }
-    end
-  end
-
   def test_suppression
     suppress(ArgumentError) { raise ArgumentError }
     suppress(LoadError) { raise LoadError }
