5017d149dffc8c2ddac351bff3fb57d2351ce112
Rafael Mendonça França <rafaelmfranca@gmail.com>
2016-05-27 21:36:45 -0300

    remove_instance_variable is a private method in 1.9.3

--- activerecord/test/cases/adapters/mysql2/quoting_test.rb
+++ activerecord/test/cases/adapters/mysql2/quoting_test.rb
@@ -14,7 +14,7 @@ class Mysql2QuotingTest < ActiveRecord::TestCase
 
   test 'quoted date precision for lt 5.6.4' do
     @connection.stubs(:full_version).returns('5.6.3')
-    @connection.remove_instance_variable(:@version) if @connection.instance_variable_defined?(:@version)
+    @connection.send(:remove_instance_variable, :@version) if @connection.instance_variable_defined?(:@version)
     t = Time.now.change(usec: 1)
     refute_match(/\.000001\z/, @connection.quoted_date(t))
   end
