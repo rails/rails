fc3fccc351f34c006010a389b6bb9b7f4c6985e9
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-05-30 11:04:46 -0400

    Collapse all new default initializers into a single file
    - Adjusted tests also for this new behavior.
    - Based on the discussion in
      https://github.com/rails/rails/pull/25184#issuecomment-222454583.

--- railties/lib/rails/generators/rails/app/templates/config/initializers/new_framework_defaults/ssl_options.rb
+++
@@ -1,6 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Configure SSL options to enable HSTS with subdomains. This is a new
-# Rails 5.0 default, so it is introduced as a configuration option to ensure
-# that apps made on earlier versions of Rails are not affected when upgrading.
-Rails.application.config.ssl_options = { hsts: { subdomains: true } }
