6a4685c28051454ffd222c59770f13514a515f3c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-05-31 14:45:39 +1200

    Add true/false quoting test to MySQL2 adapter.

---
+++ activerecord/test/cases/adapters/mysql2/quoting_test.rb
@@ -0,0 +1,16 @@
+
+require "cases/helper"
+
+class Mysql2QuotingTest < ActiveRecord::Mysql2TestCase
+  def setup
+    @conn = ActiveRecord::Base.connection
+  end
+  
+  def test_type_cast_true
+    assert_equal 1, @conn.type_cast(true)
+  end
+
+  def test_type_cast_false
+    assert_equal 0, @conn.type_cast(false)
+  end
+end
