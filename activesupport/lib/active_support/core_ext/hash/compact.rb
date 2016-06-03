0dd23446a32b4e4974572a39582ea4c11475b80a
Igor Pstyga <igor.pstyga@gmail.com>
2016-06-03 04:54:34 -0700

    Improve Hash#compact! documentation and tests
    Make it clear what should be returned when no changes were made to the
    hash.
    
      { c: true }.compact! # => nil

--- activesupport/lib/active_support/core_ext/hash/compact.rb
+++ activesupport/lib/active_support/core_ext/hash/compact.rb
@@ -2,18 +2,21 @@ class Hash
   # Returns a hash with non +nil+ values.
   #
   #   hash = { a: true, b: false, c: nil }
-  #   hash.compact       # => { a: true, b: false }
-  #   hash               # => { a: true, b: false, c: nil }
-  #   { c: nil }.compact # => {}
+  #   hash.compact        # => { a: true, b: false }
+  #   hash                # => { a: true, b: false, c: nil }
+  #   { c: nil }.compact  # => {}
+  #   { c: true }.compact # => { c: true }
   def compact
     self.select { |_, value| !value.nil? }
   end
 
   # Replaces current hash with non +nil+ values.
+  # Returns nil if no changes were made, otherwise returns the hash.
   #
   #   hash = { a: true, b: false, c: nil }
-  #   hash.compact! # => { a: true, b: false }
-  #   hash          # => { a: true, b: false }
+  #   hash.compact!        # => { a: true, b: false }
+  #   hash                 # => { a: true, b: false }
+  #   { c: true }.compact! # => nil
   def compact!
     self.reject! { |_, value| value.nil? }
   end
