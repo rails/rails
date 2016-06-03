66ebbc4952f6cfb37d719f63036441ef98149418
Arthur Neves <arthurnn@gmail.com>
2016-06-03 17:17:38 -0400

    Dont re-define class SQLite3Adapter on test
    We were declaring  in a few tests, which depending of
    the order load will cause an error, as the super class could change.
    
    see https://github.com/rails/rails/commit/ac1c4e141b20c1067af2c2703db6e1b463b985da#commitcomment-17731383

--- activerecord/test/cases/adapters/sqlite3/quoting_test.rb
+++ activerecord/test/cases/adapters/sqlite3/quoting_test.rb
@@ -3,98 +3,92 @@ require 'bigdecimal'
 require 'yaml'
 require 'securerandom'
 
-module ActiveRecord
-  module ConnectionAdapters
-    class SQLite3Adapter
-      class QuotingTest < ActiveRecord::SQLite3TestCase
-        def setup
-          @conn = ActiveRecord::Base.connection
-        end
+class SQLite3QuotingTest < ActiveRecord::SQLite3TestCase
+  def setup
+    @conn = ActiveRecord::Base.connection
+  end
 
-        def test_type_cast_binary_encoding_without_logger
-          @conn.extend(Module.new { def logger; end })
-          binary = SecureRandom.hex
-          expected = binary.dup.encode!(Encoding::UTF_8)
-          assert_equal expected, @conn.type_cast(binary)
-        end
+  def test_type_cast_binary_encoding_without_logger
+    @conn.extend(Module.new { def logger; end })
+    binary = SecureRandom.hex
+    expected = binary.dup.encode!(Encoding::UTF_8)
+    assert_equal expected, @conn.type_cast(binary)
+  end
 
-        def test_type_cast_symbol
-          assert_equal 'foo', @conn.type_cast(:foo)
-        end
+  def test_type_cast_symbol
+    assert_equal 'foo', @conn.type_cast(:foo)
+  end
 
-        def test_type_cast_date
-          date = Date.today
-          expected = @conn.quoted_date(date)
-          assert_equal expected, @conn.type_cast(date)
-        end
+  def test_type_cast_date
+    date = Date.today
+    expected = @conn.quoted_date(date)
+    assert_equal expected, @conn.type_cast(date)
+  end
 
-        def test_type_cast_time
-          time = Time.now
-          expected = @conn.quoted_date(time)
-          assert_equal expected, @conn.type_cast(time)
-        end
+  def test_type_cast_time
+    time = Time.now
+    expected = @conn.quoted_date(time)
+    assert_equal expected, @conn.type_cast(time)
+  end
 
-        def test_type_cast_numeric
-          assert_equal 10, @conn.type_cast(10)
-          assert_equal 2.2, @conn.type_cast(2.2)
-        end
+  def test_type_cast_numeric
+    assert_equal 10, @conn.type_cast(10)
+    assert_equal 2.2, @conn.type_cast(2.2)
+  end
 
-        def test_type_cast_nil
-          assert_equal nil, @conn.type_cast(nil)
-        end
+  def test_type_cast_nil
+    assert_equal nil, @conn.type_cast(nil)
+  end
 
-        def test_type_cast_true
-          assert_equal 't', @conn.type_cast(true)
-        end
+  def test_type_cast_true
+    assert_equal 't', @conn.type_cast(true)
+  end
 
-        def test_type_cast_false
-          assert_equal 'f', @conn.type_cast(false)
-        end
+  def test_type_cast_false
+    assert_equal 'f', @conn.type_cast(false)
+  end
 
-        def test_type_cast_bigdecimal
-          bd = BigDecimal.new '10.0'
-          assert_equal bd.to_f, @conn.type_cast(bd)
-        end
+  def test_type_cast_bigdecimal
+    bd = BigDecimal.new '10.0'
+    assert_equal bd.to_f, @conn.type_cast(bd)
+  end
 
-        def test_type_cast_unknown_should_raise_error
-          obj = Class.new.new
-          assert_raise(TypeError) { @conn.type_cast(obj) }
-        end
+  def test_type_cast_unknown_should_raise_error
+    obj = Class.new.new
+    assert_raise(TypeError) { @conn.type_cast(obj) }
+  end
 
-        def test_type_cast_object_which_responds_to_quoted_id
-          quoted_id_obj = Class.new {
-            def quoted_id
-              "'zomg'"
-            end
-
-            def id
-              10
-            end
-          }.new
-          assert_equal 10, @conn.type_cast(quoted_id_obj)
-
-          quoted_id_obj = Class.new {
-            def quoted_id
-              "'zomg'"
-            end
-          }.new
-          assert_raise(TypeError) { @conn.type_cast(quoted_id_obj) }
-        end
-
-        def test_quoting_binary_strings
-          value = "hello".encode('ascii-8bit')
-          type = Type::String.new
-
-          assert_equal "'hello'", @conn.quote(type.serialize(value))
-        end
-
-        def test_quoted_time_returns_date_qualified_time
-          value = ::Time.utc(2000, 1, 1, 12, 30, 0, 999999)
-          type = Type::Time.new
-
-          assert_equal "'2000-01-01 12:30:00.999999'", @conn.quote(type.serialize(value))
-        end
+  def test_type_cast_object_which_responds_to_quoted_id
+    quoted_id_obj = Class.new {
+      def quoted_id
+        "'zomg'"
       end
-    end
+
+      def id
+        10
+      end
+    }.new
+    assert_equal 10, @conn.type_cast(quoted_id_obj)
+
+    quoted_id_obj = Class.new {
+      def quoted_id
+        "'zomg'"
+      end
+    }.new
+    assert_raise(TypeError) { @conn.type_cast(quoted_id_obj) }
+  end
+
+  def test_quoting_binary_strings
+    value = "hello".encode('ascii-8bit')
+    type = Type::String.new
+
+    assert_equal "'hello'", @conn.quote(type.serialize(value))
+  end
+
+  def test_quoted_time_returns_date_qualified_time
+    value = ::Time.utc(2000, 1, 1, 12, 30, 0, 999999)
+    type = Type::Time.new
+
+    assert_equal "'2000-01-01 12:30:00.999999'", @conn.quote(type.serialize(value))
   end
 end
--- activerecord/test/cases/adapters/sqlite3/statement_pool_test.rb
+++ activerecord/test/cases/adapters/sqlite3/statement_pool_test.rb
@@ -1,24 +1,20 @@
 require 'cases/helper'
 
-module ActiveRecord::ConnectionAdapters
-  class SQLite3Adapter
-    class StatementPoolTest < ActiveRecord::SQLite3TestCase
-      if Process.respond_to?(:fork)
-        def test_cache_is_per_pid
+class SQLite3StatementPoolTest < ActiveRecord::SQLite3TestCase
+  if Process.respond_to?(:fork)
+    def test_cache_is_per_pid
 
-          cache = StatementPool.new(10)
-          cache['foo'] = 'bar'
-          assert_equal 'bar', cache['foo']
+      cache = StatementPool.new(10)
+      cache['foo'] = 'bar'
+      assert_equal 'bar', cache['foo']
 
-          pid = fork {
-            lookup = cache['foo'];
-            exit!(!lookup)
-          }
+      pid = fork {
+        lookup = cache['foo'];
+        exit!(!lookup)
+      }
 
-          Process.waitpid pid
-          assert $?.success?, 'process should exit successfully'
-        end
-      end
+      Process.waitpid pid
+      assert $?.success?, 'process should exit successfully'
     end
   end
 end
