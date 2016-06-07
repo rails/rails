171c46777b995e1a50b8e31dd81e8ea608f9945a
David Amrani <david@amrani.io>
2016-06-06 23:01:24 -0700

    convert accented characters to ascii before calculating the path size

--- activerecord/lib/active_record/fixtures.rb
+++ activerecord/lib/active_record/fixtures.rb
@@ -868,7 +868,7 @@ module ActiveRecord
       def fixtures(*fixture_set_names)
         if fixture_set_names.first == :all
           fixture_set_names = Dir["#{fixture_path}/{**,*}/*.{yml}"]
-          fixture_set_names.map! { |f| f[(fixture_path.to_s.size + 1)..-5] }
+          fixture_set_names.map! { |f| f[(ActiveSupport::Inflector.transliterate(fixture_path).size + 1)..-5] }
         else
           fixture_set_names = fixture_set_names.flatten.map { |n| n.to_s }
         end
