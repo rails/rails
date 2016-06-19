1e272edc86b5a820b4d59e9d675a1a5d8245d133
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-06-18 20:17:28 -0700

    Fix failing railties tests
    - Railties tests related to fetching asset URL started failing after the
      release of sprockets 3.6.1 on Travis.
    - This was due to the change in
      https://github.com/rails/sprockets/pull/311/files
      which changed the logic in `concat_javascript_sources` to add `;` at
      the end of file if the source did not end with semicolon.
    - Bumped up sprockets minor version and fixed the failing tests.

--- railties/test/railties/engine_test.rb
+++ railties/test/railties/engine_test.rb
@@ -1155,10 +1155,10 @@ YAML
       assert_equal "App's bar partial", last_response.body.strip
 
       get("/assets/foo.js")
-      assert_equal "// Bukkit's foo js", last_response.body.strip
+      assert_match "// Bukkit's foo js", last_response.body.strip
 
       get("/assets/bar.js")
-      assert_equal "// App's bar js", last_response.body.strip
+      assert_match "// App's bar js", last_response.body.strip
 
       # ensure that railties are not added twice
       railties = Rails.application.send(:ordered_railties).map(&:class)
