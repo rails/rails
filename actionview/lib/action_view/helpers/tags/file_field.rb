9d441579247b28979539d117b730548afa12fefb
eileencodes <eileencodes@gmail.com>
2016-06-21 17:23:08 -0400

    Revert "Merge pull request #17973 from maurogeorge/file_field_hidden_field"
    The reason we are reverting this commit is because it created breaking
    changes for file upload gems. For more information see discussion here:
    https://github.com/rails/rails/issues/17947#issuecomment-225154294
    
    This reverts commit c455817804e4df64c46c17a0cdec0e5a1ca5ba2e, reversing
    changes made to 8b3cd74b8a09ef85a43d7631bb062a9ec7f57227.
    
    Conflicts:
    	actionview/CHANGELOG.md
    	actionview/lib/action_view/helpers/form_helper.rb

--- actionview/lib/action_view/helpers/tags/file_field.rb
+++ actionview/lib/action_view/helpers/tags/file_field.rb
@@ -2,21 +2,6 @@ module ActionView
   module Helpers
     module Tags # :nodoc:
       class FileField < TextField # :nodoc:
-
-        def render
-          options = @options.stringify_keys
-
-          if options.fetch("include_hidden", true)
-            add_default_name_and_id(options)
-            options[:type] = "file"
-            tag("input", name: options["name"], type: "hidden", value: "") + tag("input", options)
-          else
-            options.delete("include_hidden")
-            @options = options
-
-            super
-          end
-        end
       end
     end
   end
