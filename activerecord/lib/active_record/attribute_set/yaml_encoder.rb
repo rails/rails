7b86ea6715ee987e61a7f3bd8e72b1bbfcfbbbe7
Jon Moss <me@jonathanmoss.me>
2016-06-24 12:45:03 -0400

    Revert "[ci skip] better docs for ActiveRecord::AttributeSet::YAMLEncoder#decode"
    This reverts commit 7ea502ae141fc26b736c7a73bdf7a676b1f9fc87, per
    internal discussion with @sgrif -- this is documenting the
    implementation of a class that isn't intended to be public API.

--- activerecord/lib/active_record/attribute_set/yaml_encoder.rb
+++ activerecord/lib/active_record/attribute_set/yaml_encoder.rb
@@ -7,11 +7,6 @@ module ActiveRecord
         @default_types = default_types
       end
 
-      # Populate +coder+ with 'concise_attributes' using +attribute_set+.
-      #
-      # If any attribute's name matches a +default_types+ key, then the
-      # attribute will be cast by passing +nil+ to
-      # ActiveRecord::Attribute#with_type.
       def encode(attribute_set, coder)
         coder['concise_attributes'] = attribute_set.each_value.map do |attr|
           if attr.type.equal?(default_types[attr.name])
@@ -22,18 +17,6 @@ module ActiveRecord
         end
       end
 
-      # Decode a previously encoded +coder+.
-      #
-      # +coder+ should be the result of previously encoding an Active Record
-      # model, using #encode.
-      #
-      # If +coder+ contains an 'attributes' key, then #decode returns the value
-      # under the 'attributes' key.
-      #
-      # Otherwise, #decode will build an attributes hash from the provided coder,
-      # using its value under 'concise_attributes', while casting any nil types
-      # using default_types. The resulting hash is used to return a new
-      # ActiveRecord::AttributeSet.
       def decode(coder)
         if coder['attributes']
           coder['attributes']
