035d88205765c8880265115b51493a389c810849
Cédric Félizard <cedric@felizard.fr>
2016-06-26 10:26:16 +0000

    Fix typo in exception class name

--- actionview/lib/action_view/helpers/tags/datetime_field.rb
+++ actionview/lib/action_view/helpers/tags/datetime_field.rb
@@ -14,7 +14,7 @@ module ActionView
         private
 
           def format_date(value)
-            raise NoImplementedError
+            raise NotImplementedError
           end
 
           def datetime_value(value)
