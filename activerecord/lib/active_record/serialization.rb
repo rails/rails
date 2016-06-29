a5faf6fb7279ebd1fab92d51bcc705469b7b98d9
Brian Abreu <brian@nuts.com>
2016-06-28 18:32:49 -0700

    Correctly handle frozen options for ActiveRecord::Serialization#serializable_hash.

--- activerecord/lib/active_record/serialization.rb
+++ activerecord/lib/active_record/serialization.rb
@@ -9,7 +9,7 @@ module ActiveRecord #:nodoc:
     end
 
     def serializable_hash(options = nil)
-      options = options.try(:clone) || {}
+      options = options.try(:dup) || {}
 
       options[:except] = Array(options[:except]).map(&:to_s)
       options[:except] |= Array(self.class.inheritance_column)
