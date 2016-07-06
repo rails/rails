c8a18aaf44a84f1ef0df007aa3f8446752dc327d
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-07-06 21:26:48 +0900

    remove `-t` option from default sendmail arguments [ci skip]
    Follow up to #24436

--- actionmailer/lib/action_mailer/delivery_methods.rb
+++ actionmailer/lib/action_mailer/delivery_methods.rb
@@ -51,7 +51,7 @@ module ActionMailer
       #
       #   add_delivery_method :sendmail, Mail::Sendmail,
       #     location:  '/usr/sbin/sendmail',
-      #     arguments: '-i -t'
+      #     arguments: '-i'
       def add_delivery_method(symbol, klass, default_options={})
         class_attribute(:"#{symbol}_settings") unless respond_to?(:"#{symbol}_settings")
         send(:"#{symbol}_settings=", default_options)
