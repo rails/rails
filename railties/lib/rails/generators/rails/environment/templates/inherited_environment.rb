501f045666788bb044767febd6688c6e28fdeec5
Stephen Blackstone <stephen.b@eleganttech.co>
2016-07-07 17:10:02 -0400

    Add generator for environments

---
+++ railties/lib/rails/generators/rails/environment/templates/inherited_environment.rb
@@ -0,0 +1,5 @@
+require_relative 'config/environments/<%= options[:from] %>.rb'
+
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+end
