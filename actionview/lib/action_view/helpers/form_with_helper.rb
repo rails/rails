a51423c6e4b36da399041b59091a01d0121bc5c9
Marek <marek.kirejczyk@gmail.com>
2016-07-09 20:58:31 +0200

    form_with uses Tag::Base family of classes instead of FormHelper methods

--- actionview/lib/action_view/helpers/form_with_helper.rb
+++ actionview/lib/action_view/helpers/form_with_helper.rb
@@ -4,30 +4,32 @@ module ActionView
 
       class FormWithBuilder < FormBuilder
 
-        FIELD_HELPERS = [:fields_for, :label, :text_field, :password_field,
-                        :hidden_field, :file_field, :text_area, :check_box,
-                        :radio_button, :color_field, :search_field,
+        FIELD_HELPERS = [:fields_for, :label, :check_box, :radio_button,
+                        :hidden_field, :file_field, :text_area, :text_field,
+                        :password_field, :color_field, :search_field,
                         :telephone_field, :phone_field, :date_field,
                         :time_field, :datetime_field, :datetime_local_field,
                         :month_field, :week_field, :url_field, :email_field,
                         :number_field, :range_field]
 
-        GENERATED_FIELD_HELPERS = field_helpers - [:label, :check_box, :radio_button, :fields_for, :hidden_field, :file_field]
+        GENERATED_FIELD_HELPERS = FIELD_HELPERS - [:label, :check_box, :radio_button, :fields_for, :hidden_field, :file_field]
 
         GENERATED_FIELD_HELPERS.each do |selector|
+          tag_class_name = selector.to_s.camelize
           class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
             def #{selector}(method, *args, **options)  # def text_field(method, *args, **options)
-              @template.send(                          #   @template.send(
-                #{selector.inspect},                   #     "text_field",
-                @object_name,                          #     @object_name,
-                method,                                #     method,
+              Tags::#{tag_class_name}.new(             #   @template.send(
+                @object_name,                          #     "text_field",
+                method,                                #     @object_name,
+                @template,                             #     method,
                 prepare_options(options, args[0]))     #     prepare_options(args[0], options))
+              .render                                  #   @template.send(
             end                                        # end
           RUBY_EVAL
         end
 
         def check_box(method, *args, on: "1", off: "0", **options)
-          @template.check_box(@object_name, method, prepare_options(options), on, off)
+          Tags::CheckBox.new(@object_name, method, @template, on, off, prepare_options(options)).render
         end
 
         private
