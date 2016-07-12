24196a8613fc2df39ff1f8927a3f019d46d46347
Stephen Blackstone <stephen.b@eleganttech.co>
2016-07-12 10:51:31 -0400

    Improve message to users about how the inherited environment works

--- railties/lib/rails/generators/rails/environment/templates/inherited_environment.rb
+++ railties/lib/rails/generators/rails/environment/templates/inherited_environment.rb
@@ -1,5 +1,6 @@
 require_relative 'config/environments/<%= options[:from] %>.rb'
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those in config/environments/<%= options[:from] %>.rb
+
 end
