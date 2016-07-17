51bb44ba8fcfc1683da1d74d21cd9010f3125e1e
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-07-17 16:23:20 +0530

    We need to restore ar_internal_metadata table also after restoring RACK or RAILS ENV
    * Fixes one more random test failure `test_migration_sets_internal_metadata_even_when_fully_migrated`.
    
    * Reproduction -
    
    MTB_VERBOSE=2 minitest_bisect --seed 55673 -Itest "test/cases/migration_test.rb" "test/cases/tasks/database_tasks_test.rb"

--- activerecord/test/cases/migration_test.rb
+++ activerecord/test/cases/migration_test.rb
@@ -425,6 +425,7 @@ class MigrationTest < ActiveRecord::TestCase
     ActiveRecord::Migrator.migrations_paths = old_path
     ENV["RAILS_ENV"] = original_rails_env
     ENV["RACK_ENV"]  = original_rack_env
+    ActiveRecord::Migrator.up(migrations_path)
   end
 
   def test_internal_metadata_stores_environment_when_other_data_exists
