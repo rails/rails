11b463d8cbacfe2cc1662f9e314440de71806ca9
Xavier Noria <fxn@hashref.com>
2016-07-24 21:33:27 +0200

    use \A and \z when you mean \A and \z
    In Ruby ^ and $ mean start and end of *line*.
    
    A regexp that validates an email should not check if
    some line of the string looks like an email, and maybe
    be surrounded by the entire Joyce's Ulysses. What the
    regexp has to check is if the string itself looks like
    an email.
    
    This validator is used only in tests, the ^/$ anchors
    implied no risk.

--- activemodel/test/validators/email_validator.rb
+++ activemodel/test/validators/email_validator.rb
@@ -3,6 +3,6 @@ require 'active_support/core_ext/regexp'
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     record.errors[attribute] << (options[:message] || "is not an email") unless
-      /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i.match?(value)
+      /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.match?(value)
   end
 end
