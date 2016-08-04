ecc9372dd1968d3dbdfe76f4d56afb9a1d77a253
Teng Siong Ong <siong1987@gmail.com>
2016-08-03 23:28:01 -0700

    to integer if it is from environment variable.

--- railties/lib/rails/generators/rails/app/templates/config/puma.rb
+++ railties/lib/rails/generators/rails/app/templates/config/puma.rb
@@ -21,7 +21,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }.to_i
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
