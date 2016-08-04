5ea4729cd78f44d6a59ad484becee58a8215731b
Teng Siong Ong <siong1987@gmail.com>
2016-08-04 14:51:29 -0700

    .to_i is unnecessary.

--- railties/lib/rails/generators/rails/app/templates/config/puma.rb
+++ railties/lib/rails/generators/rails/app/templates/config/puma.rb
@@ -4,7 +4,7 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum, this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
