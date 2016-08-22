735556d6ccbce382724b7c5dc63109c9b324d1ca
Amadeus Folego <amadeusfolego@gmail.com>
2016-08-22 20:36:45 -0300

    Improve TagHelper#tag_option performance
    Freeze string literals and use String instead of
    Regex inside gsub call. This should improve performance from 20% up to
    50% on most cases.

--- actionview/lib/action_view/helpers/tag_helper.rb
+++ actionview/lib/action_view/helpers/tag_helper.rb
@@ -86,11 +86,11 @@ module ActionView
 
         def tag_option(key, value, escape)
           if value.is_a?(Array)
-            value = escape ? safe_join(value, " ") : value.join(" ")
+            value = escape ? safe_join(value, " ".freeze) : value.join(" ".freeze)
           else
             value = escape ? ERB::Util.unwrapped_html_escape(value) : value.to_s
           end
-          %(#{key}="#{value.gsub(/"/, '&quot;'.freeze)}")
+          %(#{key}="#{value.gsub('"'.freeze, '&quot;'.freeze)}")
         end
 
         private
