1f38ef0c03cb218b5ab0c756477e1450d0e672c4
Rafael França <rafaelmfranca@gmail.com>
2016-08-23 00:48:04 -0300

    Merge pull request #26254 from badosu/improve-tag-option
    Improve #tag_option performance

--- actionview/lib/action_view/helpers/tag_helper.rb
+++ actionview/lib/action_view/helpers/tag_helper.rb
@@ -189,7 +189,7 @@ module ActionView
           else
             value = escape ? ERB::Util.unwrapped_html_escape(value) : value.to_s
           end
-          %(#{key}="#{value.gsub(/"/, '&quot;'.freeze)}")
+          %(#{key}="#{value.gsub('"'.freeze, '&quot;'.freeze)}")
         end
     end
   end
