cdd2b278dc16841094f87d5634437348c05d32ac
yunlei <yunlei.liu@appfolio.com>
2016-08-23 13:08:30 -0700

    yl - remove extra …

--- actionpack/lib/action_controller/metal/implicit_render.rb
+++ actionpack/lib/action_controller/metal/implicit_render.rb
@@ -48,7 +48,7 @@ module ActionController
           "NOTE! For XHR/Ajax or API requests, this action would normally " \
           "respond with 204 No Content: an empty white screen. Since you're " \
           "loading it in a web browser, we assume that you expected to " \
-          "actually render a template, not… nothing, so we're showing an " \
+          "actually render a template, not nothing, so we're showing an " \
           "error to be extra-clear. If you expect 204 No Content, carry on. " \
           "That's what you'll get from an XHR or API request. Give it a shot."
 
