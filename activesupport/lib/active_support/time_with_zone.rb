0054e02f73c3ebcb3cbb577983c5bcb8e707ddc0
Andrew White <pixeltrix@users.noreply.github.com>
2016-08-30 08:42:45 +0100

    Merge pull request #25880 from ryandv/fix_performance_regression_in_timewithzone_to_time
    Fix performance regression in `TimeWithZone#to_time`
    (cherry picked from commit 3132fa6b7d9585e04eb44b25b55d298391b040b5)

--- activesupport/lib/active_support/time_with_zone.rb
+++ activesupport/lib/active_support/time_with_zone.rb
@@ -81,7 +81,7 @@ module ActiveSupport
 
     # Returns a <tt>Time</tt> instance of the simultaneous time in the system timezone.
     def localtime(utc_offset = nil)
-      utc.getlocal(utc_offset)
+      @localtime ||= utc.getlocal(utc_offset)
     end
     alias_method :getlocal, :localtime
 
