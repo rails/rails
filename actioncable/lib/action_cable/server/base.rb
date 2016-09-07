f838c213beffd20a3f100fcbe18b8ec4f0fa7120
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-09-07 12:05:36 +0530

    [ci skip] Fix formatting of documentation of worker_pool method from AC::Server::Base

--- actioncable/lib/action_cable/server/base.rb
+++ actioncable/lib/action_cable/server/base.rb
@@ -54,7 +54,7 @@ module ActionCable
 
       # The worker pool is where we run connection callbacks and channel actions. We do as little as possible on the server's main thread.
       # The worker pool is an executor service that's backed by a pool of threads working from a task queue. The thread pool size maxes out
-      # at 4 worker threads by default. Tune the size yourself with `config.action_cable.worker_pool_size`.
+      # at 4 worker threads by default. Tune the size yourself with <tt>config.action_cable.worker_pool_size</tt>.
       #
       # Using Active Record, Redis, etc within your channel actions means you'll get a separate connection from each thread in the worker pool.
       # Plan your deployment accordingly: 5 servers each running 5 Puma workers each running an 8-thread worker pool means at least 200 database
