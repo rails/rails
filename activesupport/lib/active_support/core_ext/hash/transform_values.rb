79c8963a32869709655f903eae31295d205ccfec
Eito Katagiri <eitoball@gmail.com>
2016-09-17 14:53:55 +0900

    define Hash#transform_values and #transform_values! on only Ruby 2.2 and 2.3

--- activesupport/lib/active_support/core_ext/hash/transform_values.rb
+++ activesupport/lib/active_support/core_ext/hash/transform_values.rb
@@ -1,3 +1,4 @@
+unless {}.respond_to?(:transform_values) # TODO: Remove this file when we drop support to ruby < 2.4
 class Hash
   # Returns a new hash with the results of running +block+ once for every value.
   # The keys are unchanged.
@@ -26,4 +27,5 @@ class Hash
       self[key] = yield(value)
     end
   end
+end
 end
