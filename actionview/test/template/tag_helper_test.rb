9e0fa298e0e8f3f489174fa5a89ce385b8216dbb
Kang-Kyu Lee <kangkyu1111@gmail.com>
2016-09-17 18:59:34 -0700

    Fix edge case with tag helper
    The edge case is that when you would call the `tag` helper with no
    content and an option hash with string key, the hash would be assigned
    as the content. Now, we are using Array#extract_options! to extract
    any hash in arguments, and merging it in with the rest of the options
    passed in, which are stored in the `options` hash. We then set the
    content variable equal to `nil`, which is what it should have been.

--- actionview/test/template/tag_helper_test.rb
+++ actionview/test/template/tag_helper_test.rb
@@ -29,10 +29,11 @@ class TagHelperTest < ActionView::TestCase
     assert_equal tag, tag
   end
 
-  def test_tag_options
-    str = tag("p", "class" => "show", :class => "elsewhere")
-    assert_match(/class="show"/, str)
-    assert_match(/class="elsewhere"/, str)
+  def test_tag_builder_options
+    str = tag.p("class" => "show", :class => "elsewhere")
+    assert_match /class="show"/, str
+    assert_match /class="elsewhere"/, str
+    assert_equal "<p class=\"show\" class=\"elsewhere\"></p>", str
   end
 
   def test_tag_options_rejects_nil_option
