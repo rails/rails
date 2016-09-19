05fa6f3db6e2dfe213d05cc2e1fd82a429660fc6
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-09-19 10:17:30 +0900

    use rails command in restart task test

--- railties/test/application/rake/restart_test.rb
+++ railties/test/application/rake/restart_test.rb
@@ -13,32 +13,32 @@ module ApplicationTests
         teardown_app
       end
 
-      test "rake restart touches tmp/restart.txt" do
+      test "rails restart touches tmp/restart.txt" do
         Dir.chdir(app_path) do
-          `rake restart`
+          `bin/rails restart`
           assert File.exist?("tmp/restart.txt")
 
           prev_mtime = File.mtime("tmp/restart.txt")
           sleep(1)
-          `rake restart`
+          `bin/rails restart`
           curr_mtime = File.mtime("tmp/restart.txt")
           assert_not_equal prev_mtime, curr_mtime
         end
       end
 
-      test "rake restart should work even if tmp folder does not exist" do
+      test "rails restart should work even if tmp folder does not exist" do
         Dir.chdir(app_path) do
           FileUtils.remove_dir("tmp")
-          `rake restart`
+          `bin/rails restart`
           assert File.exist?("tmp/restart.txt")
         end
       end
 
-      test "rake restart removes server.pid also" do
+      test "rails restart removes server.pid also" do
         Dir.chdir(app_path) do
           FileUtils.mkdir_p("tmp/pids")
           FileUtils.touch("tmp/pids/server.pid")
-          `rake restart`
+          `bin/rails restart`
           assert_not File.exist?("tmp/pids/server.pid")
         end
       end
