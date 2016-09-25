0894cb98d1019fd0c64de83129e946d188c40f07
Kasper Timm Hansen <kaspth@gmail.com>
2016-09-25 19:51:46 +0200

    Require `active_support/testing/autorun`.
    Revise the require in the test command to use `active_support/testing/autorun`
    and spare us the minitest spec syntax as detailed in 5da4d51.
    
    Also move the require to the top of the file for consistency with the rest of
    Rails' requires across the project.

--- railties/lib/rails/commands/test.rb
+++ railties/lib/rails/commands/test.rb
@@ -1,9 +1,8 @@
 require "rails/test_unit/minitest_plugin"
+require "active_support/testing/autorun"
 
 if defined?(ENGINE_ROOT)
   $LOAD_PATH << File.expand_path("test", ENGINE_ROOT)
 else
   $LOAD_PATH << File.expand_path("../../test", APP_PATH)
 end
-
-require "minitest/autorun"
