66df0df58e31bf9f28d0d16cbd0d70df40583511
Andrew White <andrew.white@unboxed.co>
2016-10-02 17:25:20 +0100

    Revert "Merge pull request #25880 from ryandv/fix_performance_regression_in_timewithzone_to_time"
    Turns out trying to cache on localtime with arguments is too hard
    so we'll do it on DateAndTime::Compatibility#to_time instead.
    
    This reverts commit 0054e02f73c3ebcb3cbb577983c5bcb8e707ddc0.

--- activesupport/lib/active_support/time_with_zone.rb
+++ activesupport/lib/active_support/time_with_zone.rb
@@ -81,7 +81,7 @@ module ActiveSupport
 
     # Returns a <tt>Time</tt> instance of the simultaneous time in the system timezone.
     def localtime(utc_offset = nil)
-      @localtime ||= utc.getlocal(utc_offset)
+      utc.getlocal(utc_offset)
     end
     alias_method :getlocal, :localtime
 
