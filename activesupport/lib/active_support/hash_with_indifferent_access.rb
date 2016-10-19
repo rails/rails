a01cf703e23a96bad044766efad58b4d56d8bf11
claudiob <claudiob@inventati.org>
2016-10-19 11:48:43 -0700

    Add missing require

--- activesupport/lib/active_support/hash_with_indifferent_access.rb
+++ activesupport/lib/active_support/hash_with_indifferent_access.rb
@@ -1,5 +1,6 @@
 require "active_support/core_ext/hash/keys"
 require "active_support/core_ext/hash/reverse_merge"
+require "active_support/core_ext/hash/indifferent_access"
 
 module ActiveSupport
   # Implements a hash where keys <tt>:foo</tt> and <tt>"foo"</tt> are considered
