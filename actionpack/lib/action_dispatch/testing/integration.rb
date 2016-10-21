e16a5fb508bf7833d7f7a9429ccd3b20990b7d4b
Rafael Mendonça França <rafaelmfranca@gmail.com>
2016-10-21 16:14:39 -0300

    Revert "Undefine assings in IntegrationTest"
    This reverts commit 5dde413e1d14c42eb87071db20d075a7b962cb01.
    
    Reason: The gem defines it so we don't need to remove

--- actionpack/lib/action_dispatch/testing/integration.rb
+++ actionpack/lib/action_dispatch/testing/integration.rb
@@ -599,8 +599,6 @@ module ActionDispatch
   class IntegrationTest < ActiveSupport::TestCase
     include TestProcess
 
-    undef :assigns
-
     module UrlOptions
       extend ActiveSupport::Concern
       def url_options
