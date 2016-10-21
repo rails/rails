b6f935bbf9b8470b370ea613dc61218849aabf89
Aaron Patterson <aaron.patterson@gmail.com>
2016-10-21 14:44:17 -0700

    Use `on_load` to trigger commandline processing code
    We need to use on_load so that plugins will get the same functionality

--- railties/lib/rails/commands/test/test_command.rb
+++ railties/lib/rails/commands/test/test_command.rb
@@ -1,6 +1,5 @@
 require "rails/command"
 require "rails/test_unit/minitest_plugin"
-require "rails/test_unit/line_filtering"
 
 module Rails
   module Command
@@ -11,10 +10,6 @@ module Rails
 
       def perform(*)
         $LOAD_PATH << Rails::Command.root.join("test")
-
-        # Add test line filtering support for running test by line number
-        # via the command line.
-        ActiveSupport::TestCase.extend Rails::LineFiltering
 
         Minitest.run_via[:rails] = true
 
--- railties/lib/rails/test_unit/railtie.rb
+++ railties/lib/rails/test_unit/railtie.rb
@@ -1,3 +1,5 @@
+require "rails/test_unit/line_filtering"
+
 if defined?(Rake.application) && Rake.application.top_level_tasks.grep(/^(default$|test(:|$))/).any?
   ENV["RAILS_ENV"] ||= "test"
 end
@@ -9,6 +11,12 @@ module Rails
                                    fixture_replacement: nil
 
       c.integration_tool :test_unit
+    end
+
+    initializer "test_unit.line_filtering" do
+      ActiveSupport.on_load(:active_support_test_case) {
+        ActiveSupport::TestCase.extend Rails::LineFiltering
+      }
     end
 
     rake_tasks do
