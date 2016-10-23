69a3fa1efc979f9d65110560d9779c2f8a74d8f6
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-10-23 20:03:40 +0530

    Use Hash#compact and Hash#compact! from Ruby 2.4
    - Ruby 2.4 has added Hash#compact and Hash#compact! so we can use it
      now.
    - Reference: https://bugs.ruby-lang.org/issues/11818 and https://bugs.ruby-lang.org/issues/12863.

--- activesupport/lib/active_support/core_ext/hash/compact.rb
+++ activesupport/lib/active_support/core_ext/hash/compact.rb
@@ -1,23 +1,27 @@
 class Hash
-  # Returns a hash with non +nil+ values.
-  #
-  #   hash = { a: true, b: false, c: nil }
-  #   hash.compact        # => { a: true, b: false }
-  #   hash                # => { a: true, b: false, c: nil }
-  #   { c: nil }.compact  # => {}
-  #   { c: true }.compact # => { c: true }
-  def compact
-    select { |_, value| !value.nil? }
+  unless Hash.instance_methods(false).include?(:compact)
+    # Returns a hash with non +nil+ values.
+    #
+    #   hash = { a: true, b: false, c: nil }
+    #   hash.compact        # => { a: true, b: false }
+    #   hash                # => { a: true, b: false, c: nil }
+    #   { c: nil }.compact  # => {}
+    #   { c: true }.compact # => { c: true }
+    def compact
+      select { |_, value| !value.nil? }
+    end
   end
 
-  # Replaces current hash with non +nil+ values.
-  # Returns nil if no changes were made, otherwise returns the hash.
-  #
-  #   hash = { a: true, b: false, c: nil }
-  #   hash.compact!        # => { a: true, b: false }
-  #   hash                 # => { a: true, b: false }
-  #   { c: true }.compact! # => nil
-  def compact!
-    reject! { |_, value| value.nil? }
+  unless Hash.instance_methods(false).include?(:compact!)
+    # Replaces current hash with non +nil+ values.
+    # Returns nil if no changes were made, otherwise returns the hash.
+    #
+    #   hash = { a: true, b: false, c: nil }
+    #   hash.compact!        # => { a: true, b: false }
+    #   hash                 # => { a: true, b: false }
+    #   { c: true }.compact! # => nil
+    def compact!
+      reject! { |_, value| value.nil? }
+    end
   end
 end
