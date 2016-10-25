d34a2747cb43cc7f23d134e2853879ef4888e372
Rafael Mendonça França <rafaelmfranca@gmail.com>
2016-10-25 00:08:39 -0200

    Fix plain option in the rails_guides generator

--- guides/rails_guides/markdown.rb
+++ guides/rails_guides/markdown.rb
@@ -158,7 +158,7 @@ module RailsGuides
         @view.content_for(:header_section) { @header }
         @view.content_for(:page_title) { @title }
         @view.content_for(:index_section) { @index }
-        @view.render(layout: @layout, text: @body)
+        @view.render(layout: @layout, plain: @body)
       end
   end
 end
