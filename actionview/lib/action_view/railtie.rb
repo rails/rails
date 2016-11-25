0cafbd4e9e0a226c5ef32eebb826f3acb902b744
Guillermo Iguaran <guilleiguaran@gmail.com>
2016-11-25 11:10:52 -0500

    Convert ActionView::Railtie into Engine

--- actionview/lib/action_view/railtie.rb
+++ actionview/lib/action_view/railtie.rb
@@ -3,7 +3,7 @@ require "rails"
 
 module ActionView
   # = Action View Railtie
-  class Railtie < Rails::Railtie # :nodoc:
+  class Railtie < Rails::Engine # :nodoc:
     config.action_view = ActiveSupport::OrderedOptions.new
     config.action_view.embed_authenticity_token_in_remote_forms = false
     config.action_view.debug_missing_translation = true
