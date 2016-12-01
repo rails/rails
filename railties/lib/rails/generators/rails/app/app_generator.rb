8012c110a2b6b6115071596bb7f1358222825341
David Heinemeier Hansson <david@loudthinking.com>
2016-12-01 18:22:47 +0100

    Don't use manually maintained vendor subdirectories when using yarn
    We should drop the vendor CSS directory anyway, I think.

--- railties/lib/rails/generators/rails/app/app_generator.rb
+++ railties/lib/rails/generators/rails/app/app_generator.rb
@@ -155,8 +155,12 @@ module Rails
     end
 
     def vendor
-      vendor_javascripts
-      vendor_stylesheets
+      if options[:yarn]
+        empty_directory_with_keep_file "vendor"
+      else
+        vendor_javascripts
+        vendor_stylesheets
+      end
     end
 
     def vendor_javascripts
