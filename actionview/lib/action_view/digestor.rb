ed46bedc3bbdfb88c2983172572ffc3d661e054c
Max Melentiev <melentievm@gmail.com>
2016-12-07 10:09:35 +0300

    Convert non-instantiatable classes to modules

--- actionview/lib/action_view/digestor.rb
+++ actionview/lib/action_view/digestor.rb
@@ -3,69 +3,63 @@ require "action_view/dependency_tracker"
 require "monitor"
 
 module ActionView
-  class Digestor
+  module Digestor
     @@digest_mutex = Mutex.new
 
-    module PerExecutionDigestCacheExpiry
-      def self.before(target)
-        ActionView::LookupContext::DetailsKey.clear
+    module_function
+
+    # Supported options:
+    #
+    # * <tt>name</tt>   - Template name
+    # * <tt>finder</tt>  - An instance of <tt>ActionView::LookupContext</tt>
+    # * <tt>dependencies</tt>  - An array of dependent views
+    def digest(name:, finder:, dependencies: [])
+      dependencies ||= []
+      cache_key = [ name, finder.rendered_format, dependencies ].flatten.compact.join(".")
+
+      # this is a correctly done double-checked locking idiom
+      # (Concurrent::Map's lookups have volatile semantics)
+      finder.digest_cache[cache_key] || @@digest_mutex.synchronize do
+        finder.digest_cache.fetch(cache_key) do # re-check under lock
+          partial = name.include?("/_")
+          root = tree(name, finder, partial)
+          dependencies.each do |injected_dep|
+            root.children << Injected.new(injected_dep, nil, nil)
+          end
+          finder.digest_cache[cache_key] = root.digest(finder)
+        end
       end
     end
 
-    class << self
-      # Supported options:
-      #
-      # * <tt>name</tt>   - Template name
-      # * <tt>finder</tt>  - An instance of <tt>ActionView::LookupContext</tt>
-      # * <tt>dependencies</tt>  - An array of dependent views
-      def digest(name:, finder:, dependencies: [])
-        dependencies ||= []
-        cache_key = [ name, finder.rendered_format, dependencies ].flatten.compact.join(".")
+    def logger
+      ActionView::Base.logger || NullLogger
+    end
 
-        # this is a correctly done double-checked locking idiom
-        # (Concurrent::Map's lookups have volatile semantics)
-        finder.digest_cache[cache_key] || @@digest_mutex.synchronize do
-          finder.digest_cache.fetch(cache_key) do # re-check under lock
-            partial = name.include?("/_")
-            root = tree(name, finder, partial)
-            dependencies.each do |injected_dep|
-              root.children << Injected.new(injected_dep, nil, nil)
-            end
-            finder.digest_cache[cache_key] = root.digest(finder)
-          end
-        end
-      end
+    # Create a dependency tree for template named +name+.
+    def tree(name, finder, partial = false, seen = {})
+      logical_name = name.gsub(%r|/_|, "/")
 
-      def logger
-        ActionView::Base.logger || NullLogger
-      end
+      options = {}
+      options[:formats] = [finder.rendered_format] if finder.rendered_format
 
-      # Create a dependency tree for template named +name+.
-      def tree(name, finder, partial = false, seen = {})
-        logical_name = name.gsub(%r|/_|, "/")
+      if template = finder.disable_cache { finder.find_all(logical_name, [], partial, [], options).first }
+        finder.rendered_format ||= template.formats.first
 
-        options = {}
-        options[:formats] = [finder.rendered_format] if finder.rendered_format
-
-        if template = finder.disable_cache { finder.find_all(logical_name, [], partial, [], options).first }
-          finder.rendered_format ||= template.formats.first
-
-          if node = seen[template.identifier] # handle cycles in the tree
-            node
-          else
-            node = seen[template.identifier] = Node.create(name, logical_name, template, partial)
-
-            deps = DependencyTracker.find_dependencies(name, template, finder.view_paths)
-            deps.uniq { |n| n.gsub(%r|/_|, "/") }.each do |dep_file|
-              node.children << tree(dep_file, finder, true, seen)
-            end
-            node
-          end
+        if node = seen[template.identifier] # handle cycles in the tree
+          node
         else
-          logger.error "  '#{name}' file doesn't exist, so no dependencies"
-          logger.error "  Couldn't find template for digesting: #{name}"
-          seen[name] ||= Missing.new(name, logical_name, nil)
+          node = seen[template.identifier] = Node.create(name, logical_name, template, partial)
+
+          deps = DependencyTracker.find_dependencies(name, template, finder.view_paths)
+          deps.uniq { |n| n.gsub(%r|/_|, "/") }.each do |dep_file|
+            node.children << tree(dep_file, finder, true, seen)
+          end
+          node
         end
+      else
+        logger.error "  '#{name}' file doesn't exist, so no dependencies"
+        logger.error "  Couldn't find template for digesting: #{name}"
+        seen[name] ||= Missing.new(name, logical_name, nil)
       end
     end
 
