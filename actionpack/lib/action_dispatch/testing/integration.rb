e11a62a9fea6f94d607cfa82fa41cdb690fa0294
Matthew Draper <matthew@trebex.net>
2016-12-08 07:30:12 +1030

    Merge pull request #27289 from matthewd/fixture-file-module
    Only move fixture_file_upload to IntegrationTest

--- actionpack/lib/action_dispatch/testing/integration.rb
+++ actionpack/lib/action_dispatch/testing/integration.rb
@@ -140,7 +140,7 @@ module ActionDispatch
       DEFAULT_HOST = "www.example.com"
 
       include Minitest::Assertions
-      include RequestHelpers, Assertions
+      include TestProcess, RequestHelpers, Assertions
 
       %w( status status_message headers body redirect? ).each do |method|
         delegate method, :to => :response, :allow_nil => true
@@ -684,7 +684,7 @@ module ActionDispatch
   # Consult the Rails Testing Guide for more.
 
   class IntegrationTest < ActiveSupport::TestCase
-    include TestProcess
+    include TestProcess::FixtureFile
 
     module UrlOptions
       extend ActiveSupport::Concern
