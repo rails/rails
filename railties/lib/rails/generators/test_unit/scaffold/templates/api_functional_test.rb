65690d1a8588aa6c1c9f99cda2dbf1056a983ace
Jon Moss <me@jonathanmoss.me>
2016-12-11 17:08:30 -0500

    Revert changes made in d68562e

--- railties/lib/rails/generators/test_unit/scaffold/templates/api_functional_test.rb
+++ railties/lib/rails/generators/test_unit/scaffold/templates/api_functional_test.rb
@@ -11,31 +11,31 @@ class <%= controller_class_name %>ControllerTest < ActionDispatch::IntegrationTe
   end
 
   test "should get index" do
-    get <%= index_helper %>_url, as: :json
+    get <%= index_helper %>_url
     assert_response :success
   end
 
   test "should create <%= singular_table_name %>" do
     assert_difference('<%= class_name %>.count') do
-      post <%= index_helper %>_url, params: { <%= "#{singular_table_name}: { #{attributes_hash} }" %> }, as: :json
+      post <%= index_helper %>_url, params: { <%= "#{singular_table_name}: { #{attributes_hash} }" %> }
     end
 
     assert_response 201
   end
 
   test "should show <%= singular_table_name %>" do
-    get <%= show_helper %>, as: :json
+    get <%= show_helper %>
     assert_response :success
   end
 
   test "should update <%= singular_table_name %>" do
-    patch <%= show_helper %>, params: { <%= "#{singular_table_name}: { #{attributes_hash} }" %> }, as: :json
+    patch <%= show_helper %>, params: { <%= "#{singular_table_name}: { #{attributes_hash} }" %> }
     assert_response 200
   end
 
   test "should destroy <%= singular_table_name %>" do
     assert_difference('<%= class_name %>.count', -1) do
-      delete <%= show_helper %>, as: :json
+      delete <%= show_helper %>
     end
 
     assert_response 204
