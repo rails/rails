d89e9110264170995a72e22ae6f51784c9eecbe2
Kasper Timm Hansen <kaspth@gmail.com>
2016-12-13 08:39:49 +0100

    Merge pull request #27293 from y-yagi/fix_duplicable_with_2_4
    change return value of `duplicable?` with Ruby 2.4+

--- activesupport/test/core_ext/object/duplicable_test.rb
+++ activesupport/test/core_ext/object/duplicable_test.rb
@@ -4,17 +4,22 @@ require 'active_support/core_ext/object/duplicable'
 require 'active_support/core_ext/numeric/time'
 
 class DuplicableTest < ActiveSupport::TestCase
-  RAISE_DUP  = [nil, false, true, :symbol, 1, 2.3, method(:puts)]
-  ALLOW_DUP = ['1', Object.new, /foo/, [], {}, Time.now, Class.new, Module.new]
+  if RUBY_VERSION >= "2.4.0"
+    RAISE_DUP = [method(:puts)]
+    ALLOW_DUP = ["1", Object.new, /foo/, [], {}, Time.now, Class.new, Module.new, BigDecimal.new("4.56"), nil, false, true, :symbol, 1, 2.3]
+  else
+    RAISE_DUP = [nil, false, true, :symbol, 1, 2.3, method(:puts)]
+    ALLOW_DUP = ["1", Object.new, /foo/, [], {}, Time.now, Class.new, Module.new]
 
-  # Needed to support Ruby 1.9.x, as it doesn't allow dup on BigDecimal, instead
-  # raises TypeError exception. Checking here on the runtime whether BigDecimal
-  # will allow dup or not.
-  begin
-    bd = BigDecimal.new('4.56')
-    ALLOW_DUP << bd.dup
-  rescue TypeError
-    RAISE_DUP << bd
+    # Needed to support Ruby 1.9.x, as it doesn't allow dup on BigDecimal, instead
+    # raises TypeError exception. Checking here on the runtime whether BigDecimal
+    # will allow dup or not.
+    begin
+      bd = BigDecimal.new('4.56')
+      ALLOW_DUP << bd.dup
+    rescue TypeError
+      RAISE_DUP << bd
+    end
   end
 
   def test_duplicable
