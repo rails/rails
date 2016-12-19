48f3e702b9687fb33ddf7257fc9064bcb948165e
Kasper Timm Hansen <kaspth@gmail.com>
2016-12-19 19:09:14 +0100

    Merge pull request #27391 from y-yagi/make_work_test_tool_with_line_filter
    make work bin/test scripts with line filter

--- tools/test.rb
+++ tools/test.rb
@@ -4,6 +4,8 @@ require 'bundler'
 Bundler.setup
 
 require "rails/test_unit/minitest_plugin"
+require "rails/test_unit/line_filtering"
+require "active_support/test_case"
 
 module Rails
   # Necessary to get rerun-snippts working.
@@ -12,4 +14,5 @@ module Rails
   end
 end
 
+ActiveSupport::TestCase.extend Rails::LineFiltering
 Rails::TestUnitReporter.executable = "bin/test"
