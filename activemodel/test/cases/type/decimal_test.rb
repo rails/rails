f16589348fd543f525957579517c9b4dc6bc59c2
John Hawthorn <john@stembolt.com>
2016-12-21 15:33:37 -0800

    Add failing BigDecimal cast test for ruby 2.4
    This adds tests to the typecasting of Integer, Float, and Decimal
    showing that empty strings should cast to nil, and invalid strings
    should result in zero.
    
    All of these tests pass in ruby 2.3.3, and all earlier versions. The
    tests against BigDecimal fail on ruby 2.4.0-rc1.
    
    This happens because ruby changed the behaviour of BigDecimal(str) to
    better match methods Integer() and Float(), which return errors.
    
    See also:
    https://bugs.ruby-lang.org/issues/10286
    https://github.com/ruby/bigdecimal/commit/3081a627cebdc1fc119425c7a9f009dbb6bec8e8

--- activemodel/test/cases/type/decimal_test.rb
+++ activemodel/test/cases/type/decimal_test.rb
@@ -11,6 +11,14 @@ module ActiveModel
         assert_equal BigDecimal.new("1"), type.cast(:"1")
       end
 
+      def test_type_cast_decimal_from_invalid_string
+        type = Decimal.new
+        assert_nil type.cast("")
+        assert_equal BigDecimal.new("1"), type.cast("1ignore")
+        assert_equal BigDecimal.new("0"), type.cast("bad1")
+        assert_equal BigDecimal.new("0"), type.cast("bad")
+      end
+
       def test_type_cast_decimal_from_float_with_large_precision
         type = Decimal.new(precision: ::Float::DIG + 2)
         assert_equal BigDecimal.new("123.0"), type.cast(123.0)
--- activemodel/test/cases/type/float_test.rb
+++ activemodel/test/cases/type/float_test.rb
@@ -9,6 +9,14 @@ module ActiveModel
         assert_equal 1.0, type.cast("1")
       end
 
+      def test_type_cast_float_from_invalid_string
+        type = Type::Float.new
+        assert_nil type.cast("")
+        assert_equal 1.0, type.cast("1ignore")
+        assert_equal 0.0, type.cast("bad1")
+        assert_equal 0.0, type.cast("bad")
+      end
+
       def test_changing_float
         type = Type::Float.new
 
