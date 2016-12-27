800ace40eaba2399f9f267e3962ce6a3c1b92d0a
Shardul Parab <shardulSVparab@gmail.com>
2016-12-27 22:29:35 +0530

    Update request.rb
    Explanation for ActionDispatch::Request#key?

--- actionpack/lib/action_dispatch/http/request.rb
+++ actionpack/lib/action_dispatch/http/request.rb
@@ -84,6 +84,8 @@ module ActionDispatch
       end
     end
 
+    #returns boolean value
+    #returns true if 'key'(the parameter) has been set accordingly
     def key?(key)
       has_header? key
     end
