81a0b4c7b2555c36cf819c846d155a08a0da2e60
Shardul Parab <shardulSVparab@gmail.com>
2016-12-27 23:11:01 +0530

    Update request.rb

--- actionpack/lib/action_dispatch/http/request.rb
+++ actionpack/lib/action_dispatch/http/request.rb
@@ -84,7 +84,6 @@ module ActionDispatch
         PASS_NOT_FOUND
       end
     end
-    
     #key? method returns boolean value
     #returns true if 'key'(the parameter) has been set accordingly
     def key?(key)
