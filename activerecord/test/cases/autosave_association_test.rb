rails/rails

commit d849f42b4ecf687ed5350f5a2402fb795aa33aac

Autosave existing records on HMT associations when the parent is new
To me it seems like this should only be the case if `autosave: true` is
set on the association. However, when implemented that way, it caused
issues with has many associations, where we have explicit tests stating
that child records are updated when the parent is new, even if autosave
is not set (presumably to update the parent id, but other changed
attributes would be persisted as well).

It's quirky, but at least we should be consistently quirky. This
constitutes a minor but subtle change in behavior, and therefore should
not be backported to 4.2 and earlier.

Fixes #19782

activerecord/lib/active_record/associations/has_many_through_association.rb
@@ -38,12 +38,10 @@ module ActiveRecord
       def insert_record(record, validate = true, raise = false)
         ensure_not_nested
 
-        if record.new_record?
-          if raise
-            record.save!(:validate => validate)
-          else
-            return unless record.save(:validate => validate)
-          end
+        if raise
+          record.save!(:validate => validate)
+        else
+          return unless record.save(:validate => validate)
         end
 
         save_through_record(record)

activerecord/test/cases/autosave_association_test.rb
@@ -1278,6 +1278,16 @@ module AutosaveAssociationOnACollectionAssociationTests
     assert_equal new_names, @pirate.reload.send(@association_name).map(&:name)
   end
 
+  def test_should_update_children_when_autosave_is_true_and_parent_is_new_but_child_is_not
+    parrot = Parrot.create!(name: "Polly")
+    parrot.name = "Squawky"
+    pirate = Pirate.new(parrots: [parrot], catchphrase: "Arrrr")
+
+    pirate.save!
+
+    assert_equal "Squawky", parrot.reload.name
+  end
+
   def test_should_automatically_validate_the_associated_models
     @pirate.send(@association_name).each { |child| child.name = '' }
 

commit 9abf606a79729833fd90f624ed580ad85f3ebadc

Should not update children when the parent creation with no reason
This issue was introduced with d849f42 to solve #19782. However, we can
solve #19782 without causing the issue. It is enough to save only when
necessary.

Fixes #27338.

activerecord/lib/active_record/associations/has_many_through_association.rb
@@ -38,10 +38,12 @@ module ActiveRecord
       def insert_record(record, validate = true, raise = false)
         ensure_not_nested
 
-        if raise
-          record.save!(validate: validate)
-        else
-          return unless record.save(validate: validate)
+        if record.new_record? || record.has_changes_to_save?
+          if raise
+            record.save!(validate: validate)
+          else
+            return unless record.save(validate: validate)
+          end
         end
 
         save_through_record(record)

activerecord/test/cases/autosave_association_test.rb
@@ -1391,6 +1391,14 @@ module AutosaveAssociationOnACollectionAssociationTests
     assert_equal "Squawky", parrot.reload.name
   end
 
+  def test_should_not_update_children_when_parent_creation_with_no_reason
+    parrot = Parrot.create!(name: "Polly")
+    assert_equal 0, parrot.updated_count
+
+    Pirate.create!(parrot_ids: [parrot.id], catchphrase: "Arrrr")
+    assert_equal 0, parrot.reload.updated_count
+  end
+
   def test_should_automatically_validate_the_associated_models
     @pirate.send(@association_name).each { |child| child.name = "" }
 

activerecord/test/models/parrot.rb
@@ -13,6 +13,11 @@ class Parrot < ActiveRecord::Base
   def cancel_save_callback_method
     throw(:abort)
   end
+
+  before_update :increment_updated_count
+  def increment_updated_count
+    self.updated_count += 1
+  end
 end
 
 class LiveParrot < Parrot

activerecord/test/schema/schema.rb
@@ -574,6 +574,7 @@ ActiveRecord::Schema.define do
     t.column :color, :string
     t.column :parrot_sti_class, :string
     t.column :killer_id, :integer
+    t.column :updated_count, :integer, default: 0
     if subsecond_precision_supported?
       t.column :created_at, :datetime, precision: 0
       t.column :created_on, :datetime, precision: 0