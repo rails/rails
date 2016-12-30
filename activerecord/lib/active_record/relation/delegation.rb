64e5b897ac944a05a33275e3828a3d4047a6b457
Matthew Draper <matthew@trebex.net>
2016-12-31 10:05:46 +1030

    Mark internal cache constants as private
    Closes #14640

--- activerecord/lib/active_record/relation/delegation.rb
+++ activerecord/lib/active_record/relation/delegation.rb
@@ -15,7 +15,10 @@ module ActiveRecord
           delegate = Class.new(klass) {
             include ClassSpecificRelation
           }
-          const_set klass.name.gsub("::".freeze, "_".freeze), delegate
+          mangled_name = klass.name.gsub("::".freeze, "_".freeze)
+          const_set mangled_name, delegate
+          private_constant mangled_name
+
           cache[klass] = delegate
         end
       end
