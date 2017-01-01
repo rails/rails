c6c3b130e1123a1a6220b893bd392a954c7f569f
Kasper Timm Hansen <kaspth@gmail.com>
2017-01-01 13:41:24 +0100

    Fix Symbol#duplicable? for Ruby 2.4.0.
    Ruby 2.4.0 has trouble duplicating certain symbols created from
    strings via `to_sym`.
    
    It didn't happen with `'symbol'.to_sym.dup` for some reason, but
    works fine with the longer string sample.
    
    Once a newer Ruby version with a fix is released we'll get have
    a failing test case we can fix.
    
    Ref: #27532

--- activesupport/test/core_ext/object/duplicable_test.rb
+++ activesupport/test/core_ext/object/duplicable_test.rb
@@ -5,8 +5,8 @@ require "active_support/core_ext/numeric/time"
 
 class DuplicableTest < ActiveSupport::TestCase
   if RUBY_VERSION >= "2.4.0"
-    RAISE_DUP = [method(:puts), Complex(1), Rational(1)]
-    ALLOW_DUP = ["1", Object.new, /foo/, [], {}, Time.now, Class.new, Module.new, BigDecimal.new("4.56"), nil, false, true, :symbol, 1, 2.3]
+    RAISE_DUP = [method(:puts), Complex(1), Rational(1), 'symbol_from_string'.to_sym]
+    ALLOW_DUP = ["1", Object.new, /foo/, [], {}, Time.now, Class.new, Module.new, BigDecimal.new("4.56"), nil, false, true, 1, 2.3]
   else
     RAISE_DUP = [nil, false, true, :symbol, 1, 2.3, method(:puts), Complex(1), Rational(1)]
     ALLOW_DUP = ["1", Object.new, /foo/, [], {}, Time.now, Class.new, Module.new, BigDecimal.new("4.56")]
