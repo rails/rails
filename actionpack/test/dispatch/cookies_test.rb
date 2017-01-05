rails/rails

commit ae29142142324545a328948e059e8b8118fd7a33

Send cookies with request

actionpack/lib/action_controller/test_case.rb
@@ -317,7 +317,9 @@ module ActionController
       # Note that the request method is not verified. The different methods are
       # available to make the tests more expressive.
       def get(action, *args)
-        process_with_kwargs("GET", action, *args)
+        res = process_with_kwargs("GET", action, *args)
+        cookies.update res.cookies
+        res
       end
 
       # Simulate a POST request with the given parameters and set/volley the response.
@@ -482,6 +484,7 @@ module ActionController
         if cookies = @request.env['action_dispatch.cookies']
           unless @response.committed?
             cookies.write(@response)
+            self.cookies.update(cookies.instance_variable_get(:@cookies))
           end
         end
         @response.prepare!

actionpack/lib/action_dispatch/middleware/cookies.rb
@@ -279,6 +279,10 @@ module ActionDispatch
         self
       end
 
+      def to_header
+        @cookies.map { |k,v| "#{k}=#{v}" }.join ';'
+      end
+
       def handle_options(options) #:nodoc:
         options[:path] ||= "/"
 

actionpack/lib/action_dispatch/testing/test_process.rb
@@ -19,7 +19,7 @@ module ActionDispatch
     end
 
     def cookies
-      @request.cookie_jar
+      @cookie_jar ||= Cookies::CookieJar.build(@request.env, @request.host, @request.ssl?, @request.cookies)
     end
 
     def redirect_to_url

commit c5da6414715e3e61f6e51a40245525aa9504a6f3

Update `cookies` helper on all HTTP requests
Regression introduced by ae29142142324545a328948e059e8b8118fd7a33 / 8363b879fe759f0645179f4521cc64795efbee6e.

Previously, cookies were only updated on `GET` requests. Now we will
update the helper for all requests, as part of `process`. Added
regression tests for all available HTTP method helpers in
`ActionController::TestCase`.

actionpack/lib/action_controller/test_case.rb
@@ -389,9 +389,7 @@ module ActionController
       # Note that the request method is not verified. The different methods are
       # available to make the tests more expressive.
       def get(action, **args)
-        res = process(action, method: "GET", **args)
-        cookies.update res.cookies
-        res
+        process(action, method: "GET", **args)
       end
 
       # Simulate a POST request with the given parameters and set/volley the response.
@@ -519,6 +517,7 @@ module ActionController
             unless @request.cookie_jar.committed?
               @request.cookie_jar.write(@response)
               cookies.update(@request.cookie_jar.instance_variable_get(:@cookies))
+              cookies.update(@response.cookies)
             end
           end
           @response.prepare!

actionpack/test/dispatch/cookies_test.rb
@@ -395,6 +395,15 @@ class CookiesTest < ActionController::TestCase
     assert_equal false, cookies.deleted?("another")
   end
 
+  # Ensure all HTTP methods have their cookies updated
+  [:get, :post, :patch, :put, :delete, :head].each do |method|
+    define_method("test_deleting_cookie_#{method}") do
+      request.cookies[:user_name] = "Joe"
+      public_send method, :logout
+      assert_nil cookies[:user_name]
+    end
+  end
+
   def test_deleted_cookie_predicate_with_mismatching_options
     cookies[:user_name] = "Joe"
     cookies.delete("user_name", path: "/path")