460c3c8c06772653bf64f013080e624e70fa455b
Nick LaMuro <nicklamuro@gmail.com>
2017-01-06 17:49:19 -0600

    Add failing test for where with joins
    This will cause a failure with the changes from 8e2e5f9:
    
    https://github.com/rails/rails/commit/8e2e5f9e3d1f434e265dc104ea9b00ff75702fc3
    
    With the `singularize` call that is being done in that method when there
    is multiple nestings of associations (JOIN calling a JOIN) and the
    `stringify_keys!` is only called once here:
    
    https://github.com/rails/rails/blob/21e5fd4/activerecord/lib/active_record/relation/where_clause_factory.rb#L16
    
    And not in the subsequent recursion in `.predicate_builder`

--- activerecord/test/cases/associations/has_one_associations_test.rb
+++ activerecord/test/cases/associations/has_one_associations_test.rb
@@ -648,11 +648,17 @@ class HasOneAssociationsTest < ActiveRecord::TestCase
   class SpecialBook < ActiveRecord::Base
     self.table_name = "books"
     belongs_to :author, class_name: "SpecialAuthor"
+    has_one :subscription, class_name: "SpecialSupscription", foreign_key: "subscriber_id"
   end
 
   class SpecialAuthor < ActiveRecord::Base
     self.table_name = "authors"
     has_one :book, class_name: "SpecialBook", foreign_key: "author_id"
+  end
+
+  class SpecialSupscription < ActiveRecord::Base
+    self.table_name = "subscriptions"
+    belongs_to :book, class_name: "SpecialBook"
   end
 
   def test_assocation_enum_works_properly
@@ -661,5 +667,16 @@ class HasOneAssociationsTest < ActiveRecord::TestCase
     author.book = book
 
     refute_equal 0, SpecialAuthor.joins(:book).where(books: { status: "published" }).count
+  end
+
+  def test_assocation_enum_works_properly_with_nested_join
+    author = SpecialAuthor.create!(name: "Test")
+    book = SpecialBook.create!(status: "published")
+    author.book = book
+
+    where_clause = { books: { subscriptions: { subscriber_id: nil } } }
+    assert_nothing_raised do
+      SpecialAuthor.joins(book: :subscription).where.not(where_clause)
+    end
   end
 end
