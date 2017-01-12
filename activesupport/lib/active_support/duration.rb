672c5637ad9cef0bf2f83fdd5cdc3f1e80a5874b
Andrey Novikov <envek@envek.name>
2017-01-12 21:17:49 +0300

    Replace calculations by precalculated values, adding comments where necessary

--- activesupport/lib/active_support/duration.rb
+++ activesupport/lib/active_support/duration.rb
@@ -8,13 +8,13 @@ module ActiveSupport
   #   1.month.ago       # equivalent to Time.now.advance(months: -1)
   class Duration
     PARTS_IN_SECONDS = {
-      seconds:                       1, # Used in parse method for ease of handling part hashes with seconds
-      minutes:                      60,
-      hours:                   60 * 60,
-      days:               24 * 60 * 60,
-      weeks:       7    * 24 * 60 * 60,
-      months:     30    * 24 * 60 * 60,
-      years:    (365.25 * 24 * 60 * 60).ceil,
+      seconds:          1, # Used in parse method for ease of handling part hashes with seconds
+      minutes:         60,
+      hours:        3_600,
+      days:        86_400,
+      weeks:      604_800,
+      months:   2_592_000, #  30 days
+      years:   31_557_600, # 365.25 days
     }.freeze
 
     attr_accessor :value, :parts
