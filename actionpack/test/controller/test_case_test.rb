rails/rails

commit cada080e82a7e357d767e4bd85451684bd69c4d6

Only use :as as named parameter if any of the named params are present

actionpack/lib/action_controller/test_case.rb
@@ -650,10 +650,11 @@ module ActionController
         end
       end
 
-      REQUEST_KWARGS = %i(params session flash method body xhr as)
+      REQUEST_KWARGS = %i(params session flash method body xhr)
+      FORMAT_KWARGS = %i(format as)
       def kwarg_request?(args)
         args[0].respond_to?(:keys) && (
-          (args[0].key?(:format) && args[0].keys.size == 1) ||
+          args[0].keys.all? { |k| FORMAT_KWARGS.include?(k) } ||
           args[0].keys.any? { |k| REQUEST_KWARGS.include?(k) }
         )
       end

actionpack/test/controller/test_case_test.rb
@@ -658,6 +658,28 @@ XML
     assert_equal "application/json", @request.headers["CONTENT_TYPE"]
   end
 
+  def test_using_as_json_with_format_does_not_show_deprecation
+    assert_not_deprecated do
+      get :test_params, as: :json, format: :json
+    end
+    assert_equal "application/json", @request.headers["CONTENT_TYPE"]
+    assert_equal(
+      { "format" => "json", "controller" => "test_case_test/test", "action" => "test_params" },
+      ::JSON.parse(@response.body)
+    )
+  end
+
+  def test_using_as_json_with_deprecated_parameters_does_not_set_the_content_type
+    assert_deprecated do
+      get :test_params, bool_value: true, as: :json
+    end
+    assert_equal nil, @request.headers["CONTENT_TYPE"]
+    assert_equal(
+      { "as" => "json", "bool_value" => "true", "controller" => "test_case_test/test", "action" => "test_params" },
+      ::JSON.parse(@response.body)
+    )
+  end
+
   def test_mutating_content_type_headers_for_plain_text_files_sets_the_header
     @request.headers['Content-Type'] = 'text/plain'
     post :render_body, params: { name: 'foo.txt' }

commit 939f28fd32ac26fccf0e82dca33900217cd02644

Only assume kwargs if one hash is passed to process/get etc.
Calling `get` etc. with either an empty params hash or a hash with only
`:format` plus a session hash was assumed to be keyword arguments,
causing the session hash to be ignored.

cada080 (new on 5-0-stable since 5.0.1) introduced the regression for an
empty params hash as `{}.all?` returns true.

actionpack/lib/action_controller/test_case.rb
@@ -653,7 +653,7 @@ module ActionController
       REQUEST_KWARGS = %i(params session flash method body xhr)
       FORMAT_KWARGS = %i(format as)
       def kwarg_request?(args)
-        args[0].respond_to?(:keys) && (
+        args.size == 1 && args[0].respond_to?(:keys) && (
           args[0].keys.all? { |k| FORMAT_KWARGS.include?(k) } ||
           args[0].keys.any? { |k| REQUEST_KWARGS.include?(k) }
         )

actionpack/test/controller/test_case_test.rb
@@ -327,6 +327,11 @@ XML
     assert_equal 'value2', session[:symbol]
   end
 
+  def test_deprecated_process_with_session_and_empty_params
+    assert_deprecated { get :no_op, {}, { 'string' => 'value1' } }
+    assert_equal 'value1', session['string']
+  end
+
   def test_deprecated_process_merges_session_arg
     session[:foo] = 'bar'
     assert_deprecated {
@@ -877,6 +882,12 @@ XML
   def test_request_format_kwarg_overrides_params
     get :test_format, format: 'json', params: { format: 'html' }
     assert_equal 'application/json', @response.body
+  end
+
+  def test_deprecated_request_format_params_with_session
+    assert_deprecated { get :test_format, { format: 'json' }, { 'string': 'value1' } }
+    assert_equal 'application/json', @response.body
+    assert_equal 'value1', session['string']
   end
 
   def test_should_have_knowledge_of_client_side_cookie_state_even_if_they_are_not_set