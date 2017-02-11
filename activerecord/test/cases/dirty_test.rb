4b6eb0325cfc2f5b77b9aafc8b57093d13b3709d
Alex Serban <al.se@me.com>
2017-02-11 17:00:29 -0500

    simplify virtual attribute change test

--- activerecord/test/cases/dirty_test.rb
+++ activerecord/test/cases/dirty_test.rb
@@ -301,12 +301,10 @@ class DirtyTest < ActiveRecord::TestCase
     assert_equal ["arr", "arr matey!"], pirate.catchphrase_change
   end
 
-  def test_nested_attribute_will_change!
-    pirate = Pirate.create!(catchphrase: "arr")
-    parrot = pirate.parrots.create!(name: "Rruby")
-    pirate.update!(parrots_attributes: { "0" => { id: parrot.id, new_name_input: "Rrails" } })
-
-    assert_equal "Rrails", parrot.name
+  def test_virtual_attribute_will_change
+    parrot = Parrot.create!(name: "Ruby")
+    parrot.send(:attribute_will_change!, :cancel_save_from_callback)
+    assert parrot.has_changes_to_save?
   end
 
   def test_association_assignment_changes_foreign_key
