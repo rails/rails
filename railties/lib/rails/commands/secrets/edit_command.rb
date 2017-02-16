28991c92e100ba63b1594cb9ce381abc58fdd3c6
Kasper Timm Hansen <kaspth@gmail.com>
2017-02-16 19:28:54 +0100

    Revise secrets flow.
    * Add support for immediately exiting editors such as atom
      (though done crudely with Time.now), while keeping vim etc. happy.
    * Prefill content if the file has none (in time, I'd like to inline sekrets
      with proper attribution and making it easier to do this prefill before the editing).
    * Add puts statement to guide clue the user in to what's happening.

--- railties/lib/rails/commands/secrets/edit_command.rb
+++ railties/lib/rails/commands/secrets/edit_command.rb
@@ -1,7 +1,9 @@
 require "active_support"
 require "active_support/core_ext/string/strip"
-
 require "rails/generators/rails/app/app_generator"
+
+require "rails/engine"
+require "sekrets"
 
 module Rails
   module Command
@@ -32,17 +34,24 @@ module Rails
             Rails::Generators::AppGenerator.new([ Rails.root ]).setup_encrypted_secrets
           end
 
+          def open_decrypted_file_and_await_write
             Sekrets.tmpdir do
-              IO.binwrite(decrypted_path, contents)
-              mtime = File.mtime(decrypted_path)
+              IO.binwrite(decrypted_path, Sekrets.read(encrypted_path) || prefill_contents)
+              mtime, start_time = File.mtime(decrypted_path), Time.now
 
+              puts "Waiting for secrets file to be saved. Abort with CTRL-C."
               system("\$EDITOR #{decrypted_path}")
 
-              until File.mtime(decrypted_path) != mtime
-                sleep 0.250
-                Sekrets.write(encrypted_path.to_s, IO.binread(decrypted_path))
+              editor_exits_after_open = $?.success? && (Time.now - start_time) < 1
+              if editor_exits_after_open
+                sleep 0.250 until File.mtime(decrypted_path) != mtime
               end
+
+              Sekrets.write(encrypted_path.to_s, IO.binread(decrypted_path))
+              puts "New secrets encrypted and saved."
             end
+          rescue Interrupt
+            puts "Aborted changing encrypted secrets: nothing saved."
           end
 
           def decrypted_path
@@ -51,6 +60,10 @@ module Rails
 
           def encrypted_path
             Rails.root.join("config", "secrets.yml.enc")
+          end
+
+          def prefill_contents
+            "production:\n  secret_key_base: #{SecureRandom.hex(64)}\n\n"
           end
       end
     end
