dbb60ff588f770d01b4b4ebc3905ff305c92ddd1
eileencodes <eileencodes@gmail.com>
2017-02-19 11:54:17 -0500

    Add default configuration to generated system test helper
    This serves as self documentation so users know how to change the
    driver.

--- railties/lib/rails/generators/test_unit/system/templates/system_test_helper.rb
+++ railties/lib/rails/generators/test_unit/system/templates/system_test_helper.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
   teardown do
     take_failed_screenshot
     Capybara.reset_sessions!
