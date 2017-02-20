590e89e29f0be4c21b7a8d97baa3c63eac80d887
chenxu <zztczcx@gmail.com>
2017-02-20 12:57:45 +1100

    add order by field to pluck to fix the distinct error of select

--- activerecord/lib/active_record/collection_cache_key.rb
+++ activerecord/lib/active_record/collection_cache_key.rb
@@ -1,39 +1,15 @@
 module ActiveRecord
   module CollectionCacheKey
     def collection_cache_key(collection = all, timestamp_column = :updated_at) # :nodoc:
-      query_signature = Digest::MD5.hexdigest(collection.to_sql)
-      key = "#{collection.model_name.cache_key}/query-#{query_signature}"
+      model_signature = collection.model_name.cache_key
 
-      if collection.loaded?
-        size = collection.size
-        if size > 0
-          timestamp = collection.max_by(&timestamp_column).public_send(timestamp_column)
-        end
-      else
-        column_type = type_for_attribute(timestamp_column.to_s)
-        column = "#{connection.quote_table_name(collection.table_name)}.#{connection.quote_column_name(timestamp_column)}"
+      original_sql = collection.to_sql
+      unscope_order_sql = collection.unscope(:order).to_sql
 
-        query = collection
-          .unscope(:select)
-          .select("COUNT(*) AS #{connection.quote_column_name("size")}", "MAX(#{column}) AS timestamp")
-          .unscope(:order)
-        result = connection.select_one(query)
+      order_fields = (original_sql.split(" ") - unscope_order_sql.split(" ")).delete_if { |i| ["ORDER", "BY", "desc", "asc", "desc,", "asc,"].include?(i) }
+      unique_signature = collection.pluck(*([primary_key, timestamp_column] + order_fields)).map { |c| c.slice(0, 2) }.flatten.join("-".freeze)
 
-        if result.blank?
-          size = 0
-          timestamp = nil
-        else
-          size = result["size"]
-          timestamp = column_type.deserialize(result["timestamp"])
-        end
-
-      end
-
-      if timestamp
-        "#{key}-#{size}-#{timestamp.utc.to_s(cache_timestamp_format)}"
-      else
-        "#{key}-#{size}"
-      end
+      "#{model_signature}/collection-digest-#{Digest::SHA256.hexdigest(unique_signature)}"
     end
   end
 end
