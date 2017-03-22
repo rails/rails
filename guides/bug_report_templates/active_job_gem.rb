0a1d83f8f6c0cd38c0b3b6c735f3c13831831904
Jon Moss <me@jonathanmoss.me>
2017-03-21 21:21:18 -0400

    Update bug report templates for 5-1-stable

--- guides/bug_report_templates/active_job_gem.rb
+++ guides/bug_report_templates/active_job_gem.rb
@@ -8,7 +8,7 @@ end
 gemfile(true) do
   source "https://rubygems.org"
   # Activate the gem you are reporting the issue against.
-  gem "activejob", "5.0.1"
+  gem "activejob", "5.1.0.rc1"
 end
 
 require "minitest/autorun"
