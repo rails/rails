bb048147912d7ea8c301acd6468e9f869dd0acad
David Heinemeier Hansson <david@loudthinking.com>
2017-03-22 18:09:16 +0100

    Stop complaining about not being able to digest dynamic template partial names and simplify error logging to a single line when not

--- actionview/lib/action_view/digestor.rb
+++ actionview/lib/action_view/digestor.rb
@@ -62,8 +62,10 @@ module ActionView
             node
           end
         else
-          logger.error "  '#{name}' file doesn't exist, so no dependencies"
-          logger.error "  Couldn't find template for digesting: #{name}"
+          unless name.include?('#') # Dynamic template partial names can never be tracked
+            logger.error "  Couldn't find template for digesting: #{name}"
+          end
+
           seen[name] ||= Missing.new(name, logical_name, nil)
         end
       end
