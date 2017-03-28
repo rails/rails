d55f7fee8125720bf416fd4c2fc2fdfdc69debf0
Rafael Mendonça França <rafaelmfranca@gmail.com>
2017-03-28 16:01:35 -0400

    starts_with? is removed in Ruby 2.4

--- activesupport/lib/active_support/evented_file_update_checker.rb
+++ activesupport/lib/active_support/evented_file_update_checker.rb
@@ -127,7 +127,7 @@ module ActiveSupport
 
         normalized_gem_paths = Gem.path.map { |path| File.join path, "" }
         dtw = dtw.reject do |path|
-          normalized_gem_paths.any? { |gem_path| path.to_s.starts_with?(gem_path) }
+          normalized_gem_paths.any? { |gem_path| path.to_s.start_with?(gem_path) }
         end
 
         @ph.filter_out_descendants(dtw)
