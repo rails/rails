ba1d67e12ea19a85602f6cbd50a44d05921825b5
Ross Kaffenberger <rosskaff@gmail.com>
2017-03-29 21:28:07 -0400

    Update comment in sanitizer helper test [skip ci]
    The previously referenced file no longer appears to exist in the project.

--- actionview/test/template/sanitize_helper_test.rb
+++ actionview/test/template/sanitize_helper_test.rb
@@ -1,6 +1,6 @@
 require "abstract_unit"
 
-# The exhaustive tests are in test/controller/html/sanitizer_test.rb.
+# The exhaustive tests are in the rails-html-sanitizer gem.
 # This tests that the helpers hook up correctly to the sanitizer classes.
 class SanitizeHelperTest < ActionView::TestCase
   tests ActionView::Helpers::SanitizeHelper
