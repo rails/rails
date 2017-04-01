767aef30dce654229186fcf87b9923e36f3a90ad
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-04-01 08:29:06 -0300

    Commenting the code by default and adding a warning to restart
    Updating to let the BEGIN block commented by default.
    Adding a warning, as found in another initializers, to restart the server when the file is changed.

--- railties/lib/rails/generators/rails/app/templates/config/initializers/begin.rb
+++ railties/lib/rails/generators/rails/app/templates/config/initializers/begin.rb
@@ -1,5 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
 # Put your code inside of the BEGIN block and it will run
 #  in the very beginning of the application start.
-BEGIN {
-  # Code goes here.
-}
+#
+# BEGIN {
+#   # Code goes here.
+# }
