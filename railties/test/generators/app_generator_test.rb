a6d907e5880e15c641fc457f7e96187fe1e8509d
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-04-09 10:48:56 +0900

    Don't generate system test files if `skip_system_test` option is specified

--- railties/test/generators/app_generator_test.rb
+++ railties/test/generators/app_generator_test.rb
@@ -452,6 +452,17 @@ class AppGeneratorTest < Rails::Generators::TestCase
     end
   end
 
+  def test_does_not_generate_system_test_files_if_skip_system_test_is_given
+    run_generator [destination_root, "--skip_system_test"]
+
+    Dir.chdir(destination_root) do
+      quietly { `./bin/rails g scaffold User` }
+
+      assert_no_file("test/application_system_test_case.rb")
+      assert_no_file("test/system/users_test.rb")
+    end
+  end
+
   def test_generator_if_api_is_given
     run_generator [destination_root, "--api"]
     assert_file "Gemfile" do |content|
