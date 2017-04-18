881def43e326c36a7870bcc3ba56dc4a006b9a38
Ken Mayer <ken@bitwrangler.com>
2017-04-17 17:22:02 -0700

    Add (more) documentation to to_time
    There's a difference in the results between `Date#to_time(:local)` and
    `Date#in_time_zone` but it is subtle and can confuse users (like me :-).

--- activesupport/lib/active_support/core_ext/date/conversions.rb
+++ activesupport/lib/active_support/core_ext/date/conversions.rb
@@ -79,6 +79,9 @@ class Date
   #   date.to_time(:local)           # => 2007-11-10 00:00:00 0800
   #
   #   date.to_time(:utc)             # => 2007-11-10 00:00:00 UTC
+  #
+  # NOTE: The :local timezone is Ruby's *process* timezone, i.e. ENV['TZ']
+  #       If the *application's* timezone is needed, then use +in_time_zone+ instead
   def to_time(form = :local)
     raise ArgumentError, "Expected :local or :utc, got #{form.inspect}." unless [:local, :utc].include?(form)
     ::Time.send(form, year, month, day)
