7dba3f1fb2fd2b3a36c04147a91784b3353639d4
Rafael Mendonça França <rafaelmfranca@gmail.com>
2017-04-25 17:43:37 -0700

    Revert "Revert "Merge pull request #28246 from pwim/backport_delegate_to_scope_rather_than_merge""
    This reverts commit bdab143ab3165c06f4f852c2b39ffb59135b2281.

--- activerecord/test/cases/associations/has_many_associations_test.rb
+++ activerecord/test/cases/associations/has_many_associations_test.rb
@@ -587,13 +587,10 @@ class HasManyAssociationsTest < ActiveRecord::TestCase
   end
 
   def test_update_all_on_association_accessed_before_save_with_explicit_foreign_key
-    # We can use the same cached proxy object because the id is available for the scope
-    firm = Firm.new(name: 'Firm', id: 100)
-    clients_proxy_id = firm.clients.object_id
+    firm = Firm.new(name: "Firm", id: 100)
     firm.clients << Client.first
     firm.save!
-    assert_equal firm.clients.count, firm.clients.update_all(description: 'Great!')
-    assert_equal clients_proxy_id, firm.clients.object_id
+    assert_equal firm.clients.count, firm.clients.update_all(description: "Great!")
   end
 
   def test_belongs_to_sanity
