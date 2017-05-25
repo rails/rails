d7164e06158589fe31ca87d23e07788e520cc335
Kasper Timm Hansen <kaspth@gmail.com>
2017-05-25 20:51:53 +0200

    Try to ensure we run after a request as well.

--- railties/test/application/current_attributes_integration_test.rb
+++ railties/test/application/current_attributes_integration_test.rb
@@ -52,6 +52,24 @@ class CurrentAttributesIntegrationTest < ActiveSupport::TestCase
       <%= Current.customer.try(:name) || 'noone' %>,<%= Time.zone.name %>
     RUBY
 
+    app_file "config/initializers/executor_intercept.rb", <<-RUBY
+      class ExecutorIntercept
+        def initialize(app)
+          @app = app
+        end
+
+        def call(env)
+          @app.call(env).tap do |response|
+            puts "intercept"
+            # `complete!` is called after `response.body.close` is called.
+            response.body << [ "after", Current.customer.try(:name) || "noone", Time.zone.name ].join(",")
+          end
+        end
+      end
+
+      Rails.application.config.middleware.use ExecutorIntercept
+    RUBY
+
     require "#{app_path}/config/environment"
   end
 
@@ -61,9 +79,11 @@ class CurrentAttributesIntegrationTest < ActiveSupport::TestCase
     get "/customers/set_current_customer"
     assert_equal 200, last_response.status
     assert_match(/david,Copenhagen/, last_response.body)
+    assert_match(/after,noone,UTC/,  last_response.body)
 
     get "/customers/set_no_customer"
     assert_equal 200, last_response.status
     assert_match(/noone,UTC/, last_response.body)
+    assert_match(/after,noone,UTC/,  last_response.body)
   end
 end
