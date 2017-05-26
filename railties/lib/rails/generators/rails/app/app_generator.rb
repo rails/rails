7a2c539911fb56234c05158b6679f3be6eabf1ec
ash <ashishg@qburst.com>
2017-05-26 20:20:45 +0530

    Do not show --webpack option in the plugin help message

--- railties/lib/rails/generators/rails/app/app_generator.rb
+++ railties/lib/rails/generators/rails/app/app_generator.rb
@@ -205,6 +205,8 @@ module Rails
     RESERVED_NAMES = %w[application destroy plugin runner test]
 
     class AppGenerator < AppBase # :nodoc:
+      WEBPACKS = %w( react vue angular )
+
       add_shared_options_for "application"
 
       # Add bin/rails options
@@ -216,6 +218,9 @@ module Rails
 
       class_option :skip_bundle, type: :boolean, aliases: "-B", default: false,
                                  desc: "Don't run bundle install"
+
+      class_option :webpack, type: :string, default: nil,
+                             desc: "Preconfigure for app-like JavaScript with Webpack (options: #{WEBPACKS.join('/')})"
 
       def initialize(*args)
         super
