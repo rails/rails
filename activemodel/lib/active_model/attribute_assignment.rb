285cba022ce37977f977376a20ca866197ef33bd
shotat <shotat@users.noreply.github.com>
2017-06-14 09:13:26 +0900

    enhance active model assignment

--- activemodel/lib/active_model/attribute_assignment.rb
+++ activemodel/lib/active_model/attribute_assignment.rb
@@ -42,8 +42,9 @@ module ActiveModel
       end
 
       def _assign_attribute(k, v)
-        if respond_to?("#{k}=")
-          public_send("#{k}=", v)
+        setter = "#{k}="
+        if respond_to?(setter)
+          public_send(setter, v)
         else
           raise UnknownAttributeError.new(self, k)
         end
