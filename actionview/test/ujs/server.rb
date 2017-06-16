9a5cede38a91669ba8b104c3e064b3b103e2dba6
Patrik Bóna <patrik.bona@mrhead.sk>
2017-06-16 06:14:55 +0200

    Do not enable disabled elements during redirects
    Fixes #29473.

--- actionview/test/ujs/server.rb
+++ actionview/test/ujs/server.rb
@@ -64,7 +64,11 @@ class TestsController < ActionController::Base
     if params[:content_type] && params[:content]
       render inline: params[:content], content_type: params[:content_type]
     elsif request.xhr?
-      render json: JSON.generate(data)
+      if params[:with_turbolinks_redirect]
+        render inline: %{Turbolinks.clearCache()\nTurbolinks.visit("http://example.com/", {"action":"replace"})}
+      else
+        render json: JSON.generate(data)
+      end
     elsif params[:iframe]
       payload = JSON.generate(data).gsub("<", "&lt;").gsub(">", "&gt;")
       html = <<-HTML
