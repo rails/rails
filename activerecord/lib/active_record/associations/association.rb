4aa4a449e0de9d01067f9f74ba1ad47829c2715d
Ryuta Kamizono <kamipo@gmail.com>
2017-06-29 05:35:34 +0900

    Remove unused `aliased_table_name` in `Association`
    `aliased_table_name` in `Association` was added at a3502c4.
    `aliased_table_name` in `JoinDependency` (added at 55854c4) is used, but
    it looks like that added one in `Association` is never used from the
    beginning.

--- activerecord/lib/active_record/associations/association.rb
+++ activerecord/lib/active_record/associations/association.rb
@@ -30,14 +30,6 @@ module ActiveRecord
         reset_scope
       end
 
-      # Returns the name of the table of the associated class:
-      #
-      #   post.comments.aliased_table_name # => "comments"
-      #
-      def aliased_table_name
-        klass.table_name
-      end
-
       # Resets the \loaded flag to +false+ and sets the \target to +nil+.
       def reset
         @loaded = false
