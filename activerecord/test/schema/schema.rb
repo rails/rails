480cec64c213fb4a36022c001e2aacde6996be12
Rafael França <rafaelmfranca@gmail.com>
2017-06-28 12:36:42 -0400

    Merge pull request #29604 from kamipo/fix_ids_reader_to_respect_case_sensitive_pk
    Fix `ids_reader` to respect case sensitive primary key

--- activerecord/test/schema/schema.rb
+++ activerecord/test/schema/schema.rb
@@ -107,7 +107,7 @@ ActiveRecord::Schema.define do
     t.boolean :has_fun, null: false, default: false
   end
 
-  create_table :bulbs, force: true do |t|
+  create_table :bulbs, primary_key: "ID", force: true do |t|
     t.integer :car_id
     t.string  :name
     t.boolean :frickinawesome, default: false
