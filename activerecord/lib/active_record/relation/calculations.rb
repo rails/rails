1d264f0bcd5081dcb4e5049a8124706f7623e63b
Ryuta Kamizono <kamipo@gmail.com>
2017-06-29 23:33:11 +0900

    Fix the next version of Rails from 5.3 to 6.0

--- activerecord/lib/active_record/relation/calculations.rb
+++ activerecord/lib/active_record/relation/calculations.rb
@@ -41,7 +41,7 @@ module ActiveRecord
         unless column_name.nil?
           ActiveSupport::Deprecation.warn \
             "When `count' is called with a block, it ignores other arguments. " \
-            "This behavior is now deprecated and will result in an ArgumentError in Rails 5.3."
+            "This behavior is now deprecated and will result in an ArgumentError in Rails 6.0."
         end
 
         return super()
@@ -86,7 +86,7 @@ module ActiveRecord
         unless column_name.nil?
           ActiveSupport::Deprecation.warn \
             "When `sum' is called with a block, it ignores other arguments. " \
-            "This behavior is now deprecated and will result in an ArgumentError in Rails 5.3."
+            "This behavior is now deprecated and will result in an ArgumentError in Rails 6.0."
         end
 
         return super()
