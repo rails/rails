b68177666e640a5fdbabdcaa1d474b87c2640d9c
Tsukuru Tanimichi <info+github@ttanimichi.com>
2017-06-29 18:25:12 +0900

    Revert "Maintain original encoding from path"

--- actionpack/lib/action_dispatch/journey/router/utils.rb
+++ actionpack/lib/action_dispatch/journey/router/utils.rb
@@ -13,13 +13,11 @@ module ActionDispatch
         #   normalize_path("")      # => "/"
         #   normalize_path("/%ab")  # => "/%AB"
         def self.normalize_path(path)
-          encoding = path.encoding
           path = "/#{path}"
           path.squeeze!("/".freeze)
           path.sub!(%r{/+\Z}, "".freeze)
           path.gsub!(/(%[a-f0-9]{2})/) { $1.upcase }
           path = "/" if path == "".freeze
-          path.force_encoding(encoding)
           path
         end
 
