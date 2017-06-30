f3fa8f4b0603fe5ddbf7a6d6ea9692bdb9ffe380
David Heinemeier Hansson <david@loudthinking.com>
2017-07-01 00:02:50 +0200

    No need for rails prefix
    active_file is specific enough.

--- lib/active_file/migration.rb
+++ lib/active_file/migration.rb
@@ -1,6 +1,6 @@
 class ActiveFile::CreateBlobs < ActiveRecord::Migration[5.2]
   def change
-    create_table :rails_active_file_blobs do |t|
+    create_table :active_file_blobs do |t|
       t.string :token
       t.string :filename
       t.string :content_type
