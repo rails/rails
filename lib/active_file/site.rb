4038bda96bba66f64f8f1322e031eefc65616c58
David Heinemeier Hansson <david@loudthinking.com>
2017-07-01 12:10:11 +0200

    Underscore its an interface

--- lib/active_file/site.rb
+++ lib/active_file/site.rb
@@ -1,30 +1,39 @@
+# Abstract class serving as an interface for concrete sites.
 class ActiveFile::Site
   def initialize
   end
 
   def upload(key, data)
+    raise NotImplementedError
   end
 
   def download(key)
+    raise NotImplementedError
   end
 
   def delete(key)
+    raise NotImplementedError
   end
 
   def exists?(key)
+    raise NotImplementedError
   end
 
   def url(key)
+    raise NotImplementedError
   end
 
   def checksum(key)
+    raise NotImplementedError
   end
 
 
   def copy(from_key:, to_key:)
+    raise NotImplementedError
   end
 
   def move(from_key:, to_key:)
+    raise NotImplementedError
   end
 end
 
