b00ff22ca00f33c291b3beb2fe3818a2c30bab28
David Heinemeier Hansson <david@loudthinking.com>
2017-07-03 21:12:11 +0200

    Fix and test expiration

--- lib/active_file/verified_key_with_expiration.rb
+++ lib/active_file/verified_key_with_expiration.rb
@@ -2,7 +2,7 @@ class ActiveFile::VerifiedKeyWithExpiration
   class_attribute :verifier, default: defined?(Rails) ? Rails.application.message_verifier('ActiveFile') : nil
 
   def self.encode(key, expires_in: nil)
-    verifier.generate([ key, expires_in ? Time.now.utc.advance(sec: expires_in) : nil ])
+    verifier.generate([ key, expires_in ? Time.now.utc.advance(seconds: expires_in) : nil ])
   end
 
   def self.decode(encoded_key)
--- test/verified_key_with_expiration_test.rb
+++ test/verified_key_with_expiration_test.rb
@@ -8,4 +8,12 @@ class ActiveFile::VerifiedKeyWithExpirationTest < ActiveSupport::TestCase
     encoded_key = ActiveFile::VerifiedKeyWithExpiration.encode(FIXTURE_KEY)
     assert_equal FIXTURE_KEY, ActiveFile::VerifiedKeyWithExpiration.decode(encoded_key)
   end
+
+  test "with expiration" do
+    encoded_key = ActiveFile::VerifiedKeyWithExpiration.encode(FIXTURE_KEY, expires_in: 1.minute)
+    assert_equal FIXTURE_KEY, ActiveFile::VerifiedKeyWithExpiration.decode(encoded_key)
+
+    travel 2.minutes
+    assert_nil ActiveFile::VerifiedKeyWithExpiration.decode(encoded_key)
+  end
 end
