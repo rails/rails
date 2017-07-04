2a2f8ca521e9ffaebd4227940b802ce88fdbb02d
David Heinemeier Hansson <david@loudthinking.com>
2017-07-04 15:01:55 +0200

    Extract explaining methods

--- lib/active_file/verified_key_with_expiration.rb
+++ lib/active_file/verified_key_with_expiration.rb
@@ -1,15 +1,24 @@
 class ActiveFile::VerifiedKeyWithExpiration
   class_attribute :verifier, default: defined?(Rails) ? Rails.application.message_verifier('ActiveFile') : nil
 
-  def self.encode(key, expires_in: nil)
-    verifier.generate([ key, expires_in ? Time.now.utc.advance(seconds: expires_in) : nil ])
-  end
-
-  def self.decode(encoded_key)
-    key, expires_at = verifier.verified(encoded_key)
-
-    if key
-      key if expires_at.nil? || Time.now.utc < expires_at
+  class << self
+    def encode(key, expires_in: nil)
+      verifier.generate([ key, expires_at(expires_in) ])
     end
+
+    def decode(encoded_key)
+      key, expires_at = verifier.verified(encoded_key)
+
+      key if key && fresh?(expires_at)
+    end
+
+    private
+      def expires_at(expires_in)
+        expires_in ? Time.now.utc.advance(seconds: expires_in) : nil
+      end
+
+      def fresh?(expires_at)
+        expires_at.nil? || Time.now.utc < expires_at
+      end
   end
 end
