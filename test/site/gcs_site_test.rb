04dad4ee83e4594a9fac1112adcfc8a8f0284cad
George Claghorn <george@basecamp.com>
2017-07-05 11:23:56 -0400

    Fix test

--- test/site/gcs_site_test.rb
+++ test/site/gcs_site_test.rb
@@ -8,7 +8,7 @@ if SITE_CONFIGURATIONS[:gcs]
 
     test "signed URL generation" do
       travel_to Time.now do
-        url = SITE.bucket.signed_url(path: FIXTURE_KEY, expires: 120) +
+        url = SITE.bucket.signed_url(FIXTURE_KEY, expires: 120) +
           "&response-content-disposition=inline%3B+filename%3D%22test.txt%22"
 
         assert_equal url, @site.url(FIXTURE_KEY, expires_in: 2.minutes, disposition: :inline, filename: "test.txt")
