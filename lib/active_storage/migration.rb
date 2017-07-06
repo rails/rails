fb88ff78b312edacc83db783ffa48cec15fd0764
Robin Dupret <robin.dupret@gmail.com>
2017-07-06 21:53:51 +0200

    Fix the migration class name
    Due to Active Support auto loading feature, the migration class
    shouldn't be name-spaced under the `ActiveStorage` constant, otherwise,
    running the migrations would throw an error.

--- lib/active_storage/migration.rb
+++ lib/active_storage/migration.rb
@@ -1,4 +1,4 @@
-class ActiveStorage::CreateTables < ActiveRecord::Migration[5.1]
+class ActiveStorageCreateTables < ActiveRecord::Migration[5.1]
   def change
     create_table :active_storage_blobs do |t|
       t.string  :key
--- test/database/setup.rb
+++ test/database/setup.rb
@@ -2,5 +2,5 @@ require "active_storage/migration"
 require_relative "create_users_migration"
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
-ActiveStorage::CreateTables.migrate(:up)
-ActiveStorage::CreateUsers.migrate(:up)
+ActiveStorageCreateTables.migrate(:up)
+ActiveStorageCreateUsers.migrate(:up)
