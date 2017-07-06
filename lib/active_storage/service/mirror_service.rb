04e6728dd50cbe80a8f0039e9718dc96bc46e701
George Claghorn <george@basecamp.com>
2017-07-06 17:00:21 -0400

    Remove unnecessary method delegations

--- lib/active_storage/service/mirror_service.rb
+++ lib/active_storage/service/mirror_service.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/module/delegation"
 class ActiveStorage::Service::MirrorService < ActiveStorage::Service
   attr_reader :services
 
-  delegate :download, :exist?, :url, :byte_size, :checksum, to: :primary_service
+  delegate :download, :exist?, :url, to: :primary_service
 
   def initialize(services:)
     @services = services
