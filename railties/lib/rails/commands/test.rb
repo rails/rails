5d7002bf8d5e6262838c07c3c7dc00078a127789
Kasper Timm Hansen <kaspth@gmail.com>
2017-07-10 21:58:01 +0200

    Revert "Backport test runner loading to 5-0-stable."
    This reverts commit 7e53003dbc8b7b3299df95c51aa399caf05fc4d6.

--- railties/lib/rails/commands/test.rb
+++ railties/lib/rails/commands/test.rb
@@ -1,4 +1,4 @@
-require "rails/test_unit/runner"
+require "rails/test_unit/minitest_plugin"
 
 if defined?(ENGINE_ROOT)
   $LOAD_PATH << File.expand_path("test", ENGINE_ROOT)
@@ -6,5 +6,6 @@ else
   $LOAD_PATH << File.expand_path("../../test", APP_PATH)
 end
 
-Rails::TestUnit::Runner.parse_options(ARGV)
-Rails::TestUnit::Runner.run(ARGV)
+Minitest.run_via = :rails
+
+require "active_support/testing/autorun"
