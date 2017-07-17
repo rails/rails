ea09bf5419ec752dd020d26b03533afa457d09d6
Ryuta Kamizono <kamipo@gmail.com>
2017-07-18 04:15:45 +0900

    Fix `JoinDependency` with using a custom table
    Without this fix, `JoinDependency` doesn't use a custom table alias:
    
    ```
    % ARCONN=sqlite3 be ruby -w -Itest test/cases/relations_test.rb -n test_using_a_custom_table_with_joins_affects_the_wheres
    Using sqlite3
    Run options: -n test_using_a_custom_table_with_joins_affects_the_wheres --seed 14531
    
    E
    
    Error:RelationTest#test_using_a_custom_table_with_joins_affects_the_wheres:
    ActiveRecord::StatementInvalid: SQLite3::SQLException: no such column: posts.author_id: SELECT  "omg_posts".* FROM "posts" "omg_posts" INNER JOIN "authors" ON "authors"."id" = "posts"."author_id" WHERE "omg_posts"."title" = ? LIMIT ?
    ```

--- activerecord/lib/active_record/associations/join_dependency/join_base.rb
+++ activerecord/lib/active_record/associations/join_dependency/join_base.rb
@@ -4,13 +4,16 @@ module ActiveRecord
   module Associations
     class JoinDependency # :nodoc:
       class JoinBase < JoinPart # :nodoc:
+        attr_reader :table
+
+        def initialize(base_klass, table, children)
+          super(base_klass, children)
+          @table = table
+        end
+
         def match?(other)
           return true if self == other
           super && base_klass == other.base_klass
-        end
-
-        def table
-          base_klass.arel_table
         end
       end
     end
