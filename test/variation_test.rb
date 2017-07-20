dda013050fe559e2e9432ae836c1239eac48fbce
David Heinemeier Hansson <david@loudthinking.com>
2017-07-20 14:06:08 -0500

    Use the direct accessor

--- test/variation_test.rb
+++ test/variation_test.rb
@@ -7,9 +7,7 @@ class ActiveStorage::VariationTest < ActiveSupport::TestCase
     blob = ActiveStorage::Blob.create_after_upload! \
       io: File.open(File.expand_path("../fixtures/files/racecar.jpg", __FILE__)), filename: "racecar.jpg", content_type: "image/jpeg"
 
-    variation_key = ActiveStorage::Variant.encode_key(resize: "500x500")
-
-    variant = ActiveStorage::Variant.lookup(blob_key: blob.key, variation_key: variation_key)
+    variant = blob.variant(resize: "100x100")
 
     assert_match /racecar.jpg/, variant.url
   end
