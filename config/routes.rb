39f9ef122dc011d08e0b8d76cad0112c2fa3665f
David Heinemeier Hansson <david@loudthinking.com>
2017-07-21 16:25:11 -0500

    Actually we just want them mounted straight

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-ActiveStorage::Engine.routes.draw do
+Rails.application.routes.draw do
   get  "/rails/active_storage/disk/:encoded_key/*filename" => "active_storage/disk#show", as: :rails_disk_blob
   post "/rails/active_storage/direct_uploads" => "active_storage/direct_uploads#create", as: :rails_direct_uploads
 
