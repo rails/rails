9ac31a3c8a7bf996ef2614a2dc83c1d345c78b35
David Heinemeier Hansson <david@loudthinking.com>
2017-07-21 15:50:19 -0500

    Mount routes on the engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+ActiveStorage::Engine.routes.draw do
   get  "/rails/active_storage/disk/:encoded_key/*filename" => "active_storage/disk#show", as: :rails_disk_blob
   get  "/rails/active_storage/variants/:encoded_blob_key/:encoded_variant_key/*filename" => "active_storage/variants#show", as: :rails_blob_variant
   post "/rails/active_storage/direct_uploads" => "active_storage/direct_uploads#create", as: :rails_direct_uploads
