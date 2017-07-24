c0e2cfdc3fd476e229ede5cda59dea6374a730e7
Kir Shatrov <shatrov@me.com>
2017-07-24 23:38:04 +0300

    Make actionpack frozen string friendly

--- actionpack/lib/action_controller/metal/http_authentication.rb
+++ actionpack/lib/action_controller/metal/http_authentication.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "base64"
 require "active_support/security_utils"
 
@@ -475,7 +477,7 @@ module ActionController
 
       # This removes the <tt>"</tt> characters wrapping the value.
       def rewrite_param_values(array_params)
-        array_params.each { |param| (param[1] || "").gsub! %r/^"|"$/, "" }
+        array_params.each { |param| (param[1] || "".dup).gsub! %r/^"|"$/, "" }
       end
 
       # This method takes an authorization body and splits up the key-value
--- actionpack/test/controller/filters_test.rb
+++ actionpack/test/controller/filters_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "abstract_unit"
 
 class ActionController::Base
@@ -346,7 +348,7 @@ class FilterTest < ActionController::TestCase
   class AroundFilter
     def before(controller)
       @execution_log = "before"
-      controller.class.execution_log << " before aroundfilter " if controller.respond_to? :execution_log
+      controller.class.execution_log += " before aroundfilter " if controller.respond_to? :execution_log
       controller.instance_variable_set(:"@before_ran", true)
     end
 
