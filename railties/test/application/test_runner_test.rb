rails/rails

commit 796a1cf0e2dc7d2b7d6bf30b94ace0e57028849e

* Don't eagerly require Rails' minitest plugin.
By making the Rails minitest behave like a standard minitest plugin
we're much more likely to not break when people use other minitest
plugins. Like minitest-focus and pride.

To do this, we need to behave like minitest: require files up front
and then perform the plugin behavior via the at_exit hook.
This also saves us a fair bit of wrangling with test file loading.

Finally, since the environment and warnings options have to be applied
as early as possible, and since minitest loads plugins at_exit, they
have to be moved to the test command.

* Don't expect the root method.

It's likely this worked because we eagerly loaded the Rails minitest plugin
and that somehow defined a root method on `Rails`.

* Assign a backtrace to failed exceptions.

Otherwise Minitest pukes when attempting to filter the backtrace (which
Rails' backtrace cleaner then removes).

Means the exception message test has to be revised too.

This is likely caused by the rails minitest plugin now being loaded for
these tests and assigning a default backtrace cleaner.

railties/lib/rails/test_unit/runner.rb
@@ -1,0 +1,140 @@
+require "shellwords"
+require "method_source"
+require "rake/file_list"
+require "active_support/core_ext/module/attribute_accessors"
+
+module Rails
+  module TestUnit
+    class Runner
+      mattr_reader :filters, default: []
+
+      class << self
+        def options(opts)
+          opts.on("--warnings", "-w", "Run with Ruby warnings enabled") {}
+          opts.on("--environment", "-e", "Run tests in the ENV environment") {}
+        end
+
+        def parse_options(argv)
+          # Perform manual parsing and cleanup since option parser raises on unknown options.
+          env_index = argv.index("--environment") || argv.index("-e")
+          if env_index
+            argv.delete_at(env_index)
+            environment = argv.delete_at(env_index).strip
+          end
+          ENV["RAILS_ENV"] = environment || "test"
+
+          w_index = argv.index("--warnings") || argv.index("-w")
+          $VERBOSE = argv.delete_at(w_index) if w_index
+        end
+
+        def rake_run(argv = [])
+          ARGV.replace Shellwords.split(ENV["TESTOPTS"] || "")
+
+          run(argv)
+        end
+
+        def run(argv = [])
+          load_tests(argv)
+
+          require "active_support/testing/autorun"
+        end
+
+        def load_tests(argv)
+          patterns = extract_filters(argv)
+
+          tests = Rake::FileList[patterns.any? ? patterns : "test/**/*_test.rb"]
+          tests.exclude("test/system/**/*") if patterns.empty?
+
+          tests.to_a.each { |path| require File.expand_path(path) }
+        end
+
+        def compose_filter(runnable, filter)
+          if filters.any? { |_, lines| lines.any? }
+            CompositeFilter.new(runnable, filter, filters)
+          else
+            filter
+          end
+        end
+
+        private
+          def extract_filters(argv)
+            argv.select { |arg| arg =~ /^\w+\// }.map do |path|
+              case
+              when path =~ /(:\d+)+$/
+                file, *lines = path.split(":")
+                filters << [ file, lines ]
+                file
+              when Dir.exist?(path)
+                "#{path}/**/*_test.rb"
+              else
+                filters << [ path, [] ]
+                path
+              end
+            end
+          end
+      end
+    end
+
+    class CompositeFilter # :nodoc:
+      attr_reader :named_filter
+
+      def initialize(runnable, filter, patterns)
+        @runnable = runnable
+        @named_filter = derive_named_filter(filter)
+        @filters = [ @named_filter, *derive_line_filters(patterns) ].compact
+      end
+
+      # Minitest uses === to find matching filters.
+      def ===(method)
+        @filters.any? { |filter| filter === method }
+      end
+
+      private
+        def derive_named_filter(filter)
+          if filter.respond_to?(:named_filter)
+            filter.named_filter
+          elsif filter =~ %r%/(.*)/% # Regexp filtering copied from Minitest.
+            Regexp.new $1
+          elsif filter.is_a?(String)
+            filter
+          end
+        end
+
+        def derive_line_filters(patterns)
+          patterns.flat_map do |file, lines|
+            if lines.empty?
+              Filter.new(@runnable, file, nil) if file
+            else
+              lines.map { |line| Filter.new(@runnable, file, line) }
+            end
+          end
+        end
+    end
+
+    class Filter # :nodoc:
+      def initialize(runnable, file, line)
+        @runnable, @file = runnable, File.expand_path(file)
+        @line = line.to_i if line
+      end
+
+      def ===(method)
+        return unless @runnable.method_defined?(method)
+
+        if @line
+          test_file, test_range = definition_for(@runnable.instance_method(method))
+          test_file == @file && test_range.include?(@line)
+        else
+          @runnable.instance_method(method).source_location.first == @file
+        end
+      end
+
+      private
+        def definition_for(method)
+          file, start_line = method.source_location
+          end_line = method.source.count("\n") + start_line - 1
+
+          return file, start_line..end_line
+        end
+    end
+  end
+end

railties/test/application/test_runner_test.rb
@@ -16,13 +16,13 @@ module ApplicationTests
     end
 
     def test_run_via_backwardscompatibility
-      require "rails/test_unit/minitest_plugin"
+      require "minitest/rails_plugin"
 
       assert_nothing_raised do
         Minitest.run_via[:ruby] = true
       end
 
-      assert_predicate Minitest.run_via, :ruby?
+      assert Minitest.run_via[:ruby]
     end
 
     def test_run_single_file

activerecord/lib/active_record/tasks/database_tasks.rb
@@ -260,7 +260,7 @@ module ActiveRecord
       def check_schema_file(filename)
         unless File.exist?(filename)
           message = %{#{filename} doesn't exist yet. Run `rails db:migrate` to create it, then try again.}
-          message << %{ If you do not intend to use a database, you should instead alter #{Rails.root}/config/application.rb to limit the frameworks that will be loaded.} if defined?(::Rails)
+          message << %{ If you do not intend to use a database, you should instead alter #{Rails.root}/config/application.rb to limit the frameworks that will be loaded.} if defined?(::Rails.root)
           Kernel.abort message
         end
       end

activesupport/lib/active_support/testing/autorun.rb
@@ -2,8 +2,4 @@ gem 'minitest'
 
 require 'minitest'
 
-if Minitest.respond_to?(:run_via) && !Minitest.run_via.set?
-  Minitest.run_via = :ruby
-end
-
 Minitest.autorun

railties/lib/minitest/rails_plugin.rb
@@ -1,0 +1,49 @@
+require "active_support/core_ext/module/attribute_accessors"
+require "rails/test_unit/reporter"
+
+module Minitest
+  class SuppressedSummaryReporter < SummaryReporter
+    # Disable extra failure output after a run if output is inline.
+    def aggregated_results(*)
+      super unless options[:output_inline]
+    end
+  end
+
+  def self.plugin_rails_options(opts, options)
+    opts.on("-b", "--backtrace", "Show the complete backtrace") do
+      options[:full_backtrace] = true
+    end
+
+    opts.on("-d", "--defer-output", "Output test failures and errors after the test run") do
+      options[:output_inline] = false
+    end
+
+    opts.on("-f", "--fail-fast", "Abort test run on first failure or error") do
+      options[:fail_fast] = true
+    end
+
+    opts.on("-c", "--[no-]color", "Enable color in the output") do |value|
+      options[:color] = value
+    end
+
+    options[:color] = true
+    options[:output_inline] = true
+  end
+
+  # Owes great inspiration to test runner trailblazers like RSpec,
+  # minitest-reporters, maxitest and others.
+  def self.plugin_rails_init(options)
+    unless options[:full_backtrace] || ENV["BACKTRACE"]
+      # Plugin can run without Rails loaded, check before filtering.
+      Minitest.backtrace_filter = ::Rails.backtrace_cleaner if ::Rails.respond_to?(:backtrace_cleaner)
+    end
+
+    # Replace progress reporter for colors.
+    reporter.reporters.delete_if { |reporter| reporter.kind_of?(SummaryReporter) || reporter.kind_of?(ProgressReporter) }
+    reporter << SuppressedSummaryReporter.new(options[:io], options)
+    reporter << ::Rails::TestUnitReporter.new(options[:io], options)
+  end
+
+  # Backwardscompatibility with Rails 5.0 generated plugin test scripts
+  mattr_reader :run_via, default: {}
+end

railties/lib/rails/test_help.rb
@@ -2,7 +2,6 @@
 # so fixtures aren't loaded into that environment
 abort("Abort testing: Your Rails environment is running in production mode!") if Rails.env.production?
 
-require "rails/test_unit/minitest_plugin"
 require 'active_support/test_case'
 require 'action_controller'
 require 'action_controller/test_case'

railties/lib/rails/test_unit/line_filtering.rb
@@ -1,78 +1,12 @@
 require 'method_source'
+require "rails/test_unit/runner"
 
 module Rails
   module LineFiltering # :nodoc:
     def run(reporter, options = {})
-      if options[:patterns] && options[:patterns].any? { |p| p =~ /:\d+/ }
-        options[:filter] = \
-          CompositeFilter.new(self, options[:filter], options[:patterns])
-      end
+      options[:filter] = Rails::TestUnit::Runner.compose_filter(self, options[:filter])
 
       super
     end
-  end
-
-  class CompositeFilter # :nodoc:
-    attr_reader :named_filter
-
-    def initialize(runnable, filter, patterns)
-      @runnable = runnable
-      @named_filter = derive_named_filter(filter)
-      @filters = [ @named_filter, *derive_line_filters(patterns) ].compact
-    end
-
-    # Minitest uses === to find matching filters.
-    def ===(method)
-      @filters.any? { |filter| filter === method }
-    end
-
-    private
-      def derive_named_filter(filter)
-        if filter.respond_to?(:named_filter)
-          filter.named_filter
-        elsif filter =~ %r%/(.*)/% # Regexp filtering copied from Minitest.
-          Regexp.new $1
-        elsif filter.is_a?(String)
-          filter
-        end
-      end
-
-      def derive_line_filters(patterns)
-        patterns.flat_map do |file_and_line|
-          file, *lines = file_and_line.split(':')
-
-          if lines.empty?
-            Filter.new(@runnable, file, nil) if file
-          else
-            lines.map { |line| Filter.new(@runnable, file, line) }
-          end
-        end
-      end
-  end
-
-  class Filter # :nodoc:
-    def initialize(runnable, file, line)
-      @runnable, @file = runnable, File.expand_path(file)
-      @line = line.to_i if line
-    end
-
-    def ===(method)
-      return unless @runnable.method_defined?(method)
-
-      if @line
-        test_file, test_range = definition_for(@runnable.instance_method(method))
-        test_file == @file && test_range.include?(@line)
-      else
-        @runnable.instance_method(method).source_location.first == @file
-      end
-    end
-
-    private
-      def definition_for(method)
-        file, start_line = method.source_location
-        end_line = method.source.count("\n") + start_line - 1
-
-        return file, start_line..end_line
-      end
   end
 end

railties/lib/rails/test_unit/minitest_plugin.rb
@@ -1,134 +1,0 @@
-require "active_support/core_ext/module/attribute_accessors"
-require "rails/test_unit/reporter"
-require "rails/test_unit/test_requirer"
-require 'shellwords'
-
-module Minitest
-  class SuppressedSummaryReporter < SummaryReporter
-    # Disable extra failure output after a run if output is inline.
-    def aggregated_results(*)
-      super unless options[:output_inline]
-    end
-  end
-
-  def self.plugin_rails_options(opts, options)
-    executable = ::Rails::TestUnitReporter.executable
-    opts.separator ""
-    opts.separator "Usage: #{executable} [options] [files or directories]"
-    opts.separator "You can run a single test by appending a line number to a filename:"
-    opts.separator ""
-    opts.separator "    #{executable} test/models/user_test.rb:27"
-    opts.separator ""
-    opts.separator "You can run multiple files and directories at the same time:"
-    opts.separator ""
-    opts.separator "    #{executable} test/controllers test/integration/login_test.rb"
-    opts.separator ""
-    opts.separator "By default test failures and errors are reported inline during a run."
-    opts.separator ""
-
-    opts.separator "Rails options:"
-    opts.on("-e", "--environment ENV",
-            "Run tests in the ENV environment") do |env|
-      options[:environment] = env.strip
-    end
-
-    opts.on("-b", "--backtrace",
-            "Show the complete backtrace") do
-      options[:full_backtrace] = true
-    end
-
-    opts.on("-d", "--defer-output",
-            "Output test failures and errors after the test run") do
-      options[:output_inline] = false
-    end
-
-    opts.on("-f", "--fail-fast",
-            "Abort test run on first failure or error") do
-      options[:fail_fast] = true
-    end
-
-    opts.on("-c", "--[no-]color",
-            "Enable color in the output") do |value|
-      options[:color] = value
-    end
-
-    options[:color] = true
-    options[:output_inline] = true
-    options[:patterns] = opts.order! unless run_via.rake?
-  end
-
-  def self.rake_run(patterns) # :nodoc:
-    self.run_via = :rake unless run_via.set?
-    ::Rails::TestRequirer.require_files(patterns)
-    autorun
-  end
-
-  module RunRespectingRakeTestopts
-    def run(args = [])
-      if run_via.rake?
-        args = Shellwords.split(ENV["TESTOPTS"] || "")
-      end
-
-      super
-    end
-  end
-
-  singleton_class.prepend RunRespectingRakeTestopts
-
-  # Owes great inspiration to test runner trailblazers like RSpec,
-  # minitest-reporters, maxitest and others.
-  def self.plugin_rails_init(options)
-    ENV["RAILS_ENV"] = options[:environment] || "test"
-
-    # If run via `ruby` we've been passed the files to run directly, or if run
-    # via `rake` then they have already been eagerly required.
-    unless run_via.ruby? || run_via.rake?
-      ::Rails::TestRequirer.require_files(options[:patterns])
-    end
-
-    unless options[:full_backtrace] || ENV["BACKTRACE"]
-      # Plugin can run without Rails loaded, check before filtering.
-      Minitest.backtrace_filter = ::Rails.backtrace_cleaner if ::Rails.respond_to?(:backtrace_cleaner)
-    end
-
-    # Replace progress reporter for colors.
-    reporter.reporters.delete_if { |reporter| reporter.kind_of?(SummaryReporter) || reporter.kind_of?(ProgressReporter) }
-    reporter << SuppressedSummaryReporter.new(options[:io], options)
-    reporter << ::Rails::TestUnitReporter.new(options[:io], options)
-  end
-
-  def self.run_via=(runner)
-    if run_via.set?
-      raise ArgumentError, "run_via already assigned"
-    else
-      run_via.runner = runner
-    end
-  end
-
-  class RunVia
-    attr_accessor :runner
-    alias set? runner
-
-    # Backwardscompatibility with Rails 5.0 generated plugin test scripts.
-    def []=(runner, *)
-      @runner = runner
-    end
-
-    def ruby?
-      runner == :ruby
-    end
-
-    def rake?
-      runner == :rake
-    end
-  end
-
-  mattr_reader(:run_via) { RunVia.new }
-end
-
-# Put Rails as the first plugin minitest initializes so other plugins
-# can override or replace our default reporter setup.
-# Since minitest only loads plugins if its extensions are empty we have
-# to call `load_plugins` first.
-Minitest.load_plugins
-Minitest.extensions.unshift 'rails'

commit 156de9f02ba944d90e1c7d5a5f784191c27c5694

Allow bin/rails test task to take absolute paths as arguments
Solves #29923

This regression was caused due to a wrong regex to filter out
paths, introduced in commit 796a1cf0e

The regex was /^\w+\// which did not accept paths with a leading
slash and hence all absolute paths were filtered out.

This change introduces a change in regex which allows for a leading
slash and acts on the matched term accordingly.

While cascading through the case block, the paths are checked for
line number specification, existence of a directory at that path
and if none of those match, then it is considered to be a path to the
file. The regex matchers specified are filtered out via the call
to `Array#compact` since they do not match any of these conditions.

railties/lib/rails/test_unit/runner.rb
@@ -58,7 +58,7 @@ module Rails
 
         private
           def extract_filters(argv)
-            argv.select { |arg| arg =~ /^\w+\// }.map do |path|
+            argv.select { |arg| arg =~ %r{^/?\w+/} }.map do |path|
               case
               when path =~ /(:\d+)+$/
                 file, *lines = path.split(":")
@@ -66,11 +66,11 @@ module Rails
                 file
               when Dir.exist?(path)
                 "#{path}/**/*_test.rb"
-              else
+              when path !~ /\/$/
                 filters << [ path, [] ]
                 path
               end
-            end
+            end.compact
           end
       end
     end

railties/test/application/test_runner_test.rb
@@ -31,10 +31,22 @@ module ApplicationTests
       assert_match "1 runs, 1 assertions, 0 failures", run_test_command("test/models/foo_test.rb")
     end
 
+    def test_run_single_file_with_absolute_path
+      create_test_file :models, "foo"
+      create_test_file :models, "bar"
+      assert_match "1 runs, 1 assertions, 0 failures", run_test_command("#{app_path}/test/models/foo_test.rb")
+    end
+
     def test_run_multiple_files
       create_test_file :models,  "foo"
       create_test_file :models,  "bar"
       assert_match "2 runs, 2 assertions, 0 failures", run_test_command("test/models/foo_test.rb test/models/bar_test.rb")
+    end
+
+    def test_run_multiple_files_with_absolute_paths
+      create_test_file :models,  "foo"
+      create_test_file :models,  "bar"
+      assert_match "2 runs, 2 assertions, 0 failures", run_test_command("#{app_path}/test/models/foo_test.rb #{app_path}/test/models/bar_test.rb")
     end
 
     def test_run_file_with_syntax_error
@@ -258,6 +270,17 @@ module ApplicationTests
       create_test_file :controllers, "accounts_controller"
 
       run_test_command("test/models test/controllers").tap do |output|
+        assert_match "AccountTest", output
+        assert_match "AccountsControllerTest", output
+        assert_match "2 runs, 2 assertions, 0 failures, 0 errors, 0 skips", output
+      end
+    end
+
+    def test_run_multiple_folders_with_absolute_paths
+      create_test_file :models, "account"
+      create_test_file :controllers, "accounts_controller"
+
+      run_test_command("#{app_path}/test/models #{app_path}/test/controllers").tap do |output|
         assert_match "AccountTest", output
         assert_match "AccountsControllerTest", output
         assert_match "2 runs, 2 assertions, 0 failures, 0 errors, 0 skips", output