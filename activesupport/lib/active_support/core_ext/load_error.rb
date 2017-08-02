23106de54f912d949fae023a4ec57223478ec036
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-08-03 08:06:43 +0900

    Remove unused `LoadError::REGEXPS` constant
    since 4ad1a52, `LoadError::REGEXPS` is no longer needed.

--- activesupport/lib/active_support/core_ext/load_error.rb
+++ activesupport/lib/active_support/core_ext/load_error.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 class LoadError
-  REGEXPS = [
-    /^no such file to load -- (.+)$/i,
-    /^Missing \w+ (?:file\s*)?([^\s]+.rb)$/i,
-    /^Missing API definition file in (.+)$/i,
-    /^cannot load such file -- (.+)$/i,
-  ]
-
   # Returns true if the given path name (except perhaps for the ".rb"
   # extension) is the missing file which caused the exception to be raised.
   def is_missing?(location)
