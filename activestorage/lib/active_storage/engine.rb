624b6c210fee56fb8ef98a7a96f588d1bea8d124
David Heinemeier Hansson <david@loudthinking.com>
2017-08-03 10:01:35 -0500

    Can't initialize a message verifier without a key base

--- activestorage/lib/active_storage/engine.rb
+++ activestorage/lib/active_storage/engine.rb
@@ -27,7 +27,9 @@ module ActiveStorage
 
     initializer "active_storage.verifier" do
       config.after_initialize do |app|
-        ActiveStorage.verifier = app.message_verifier("ActiveStorage")
+        if app.config.secret_key_base.present?
+          ActiveStorage.verifier = app.message_verifier("ActiveStorage")
+        end
       end
     end
 
