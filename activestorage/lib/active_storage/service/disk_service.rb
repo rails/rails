b76f4e47e842d4f6e4519046461e1a3da99ccf82
Rafael Mendonça França <rafaelmfranca@gmail.com>
2017-08-03 17:32:59 -0400

    Fix all rubocop violations

--- activestorage/lib/active_storage/service/disk_service.rb
+++ activestorage/lib/active_storage/service/disk_service.rb
@@ -81,8 +81,10 @@ class ActiveStorage::Service::DiskService < ActiveStorage::Service
           content_length: content_length,
           checksum: checksum
         },
-        expires_in: expires_in,
-        purpose: :blob_token
+        {
+          expires_in: expires_in,
+          purpose: :blob_token
+        }
       )
 
       generated_url =
