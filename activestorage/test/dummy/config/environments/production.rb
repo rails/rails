5fc2d89cc71f45bd45301b73e113165e93684c90
David Heinemeier Hansson <david@loudthinking.com>
2017-08-06 19:25:50 -0500

    Document what we should have instead
    Still need to make it happen, tho.

--- activestorage/test/dummy/config/environments/production.rb
+++ activestorage/test/dummy/config/environments/production.rb
@@ -14,10 +14,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
-  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
-  # `config/secrets.yml.key`.
-  config.read_encrypted_secrets = true
+  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
+  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
+  # config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
