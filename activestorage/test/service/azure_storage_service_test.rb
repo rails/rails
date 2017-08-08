6ef37940135c8fe7a6fca2033dc1fe132d2c468f
Mike Gunderloy <MikeG1@larkfarm.com>
2017-08-08 09:35:42 -0500

    Support content_type in AzureStorageService#url
    Add in the content_type option, which is defined as part of the generic
    ActiveStorage::Service class.
    
    Without this option, attempts to generate a URL for an attached file
    fail with "ArgumentError (unknown keyword: content_type)"

--- activestorage/test/service/azure_storage_service_test.rb
+++ activestorage/test/service/azure_storage_service_test.rb
@@ -6,6 +6,15 @@ if SERVICE_CONFIGURATIONS[:azure]
     SERVICE = ActiveStorage::Service.configure(:azure, SERVICE_CONFIGURATIONS)
 
     include ActiveStorage::Service::SharedServiceTests
+
+    test "signed URL generation" do
+      url = @service.url(FIXTURE_KEY, expires_in: 5.minutes,
+        disposition: :inline, filename: "avatar.png", content_type: "image/png")
+
+      assert_match(/(\S+)&rsct=image%2Fpng&rscd=inline%3B\+filename%3D%22avatar.png/, url)
+      assert_match SERVICE_CONFIGURATIONS[:azure][:container], url
+    end
+
   end
 
 else
