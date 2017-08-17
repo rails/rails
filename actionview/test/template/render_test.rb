rails/rails

commit 228f9910389cad7fe0dd7f2bd010fe654f794b37

Move layout logic with collection to be handled only with explicit template is given
Layout is never an available option when rendering with the shortcut
`render @collection`.

actionpack/lib/action_view/renderer/partial_renderer.rb
@@ -282,21 +282,7 @@ module ActionView
         spacer = find_template(@options[:spacer_template]).render(@view, @locals)
       end
 
-      if layout = @options[:layout]
-        layout = find_template(layout, @locals.keys + [@variable, @variable_counter])
-      end
-
       result = @template ? collection_with_template : collection_without_template
-
-      if layout
-        locals = @locals
-        result.map! do |content|
-          locals[@variable] = @collection[result.index(content)]
-          locals[@variable_counter] = result.index(content)
-          layout.render(@view, @locals) { content }
-        end
-      end
-
       result.join(spacer).html_safe
     end
 
@@ -391,12 +377,19 @@ module ActionView
       segments, locals, template = [], @locals, @template
       as, counter = @variable, @variable_counter
 
+      if layout = @options[:layout]
+        layout = find_template(layout, @locals.keys + [@variable, @variable_counter])
+      end
+
       locals[counter] = -1
 
       @collection.each do |object|
         locals[counter] += 1
         locals[as] = object
-        segments << template.render(@view, locals)
+
+        content = template.render(@view, locals)
+        content = layout.render(@view, locals) { content } if layout
+        segments << content
       end
 
       segments

commit d0fb3d49116c94523d00fb951ea34dc02b47333a

Respect layout option when rendering collection
When doing a partial render for a collection there is a layout option
available. It is used to wrap each rendered partial. This works as
expected in all cases (that I've found) except when the collection is
heterogeneous - or at least when the objects in the collection result in
a set of non-unique `to_partial_path` values.

A change back in 228f9910389cad7fe0dd7f2bd010fe654f794b37 caused this
inconsistency. The comment there says the layout option is ignored for
the shortcut `render @collection`, but makes no mention of the longer
form. There has been much churn in this area in the years since that
change, and it seems we ought to respect the option as it's consistent
with the existing documentation, and other ways of calling this partial
render.

actionview/lib/action_view/renderer/partial_renderer.rb
@@ -456,12 +456,17 @@ module ActionView
           index = partial_iteration.index
           path, as, counter, iteration = collection_data[index]
 
+          if layout = @options[:layout]
+            layout = find_template(layout, keys + [as, counter, iteration])
+          end
+
           locals[as]        = object
           locals[counter]   = index
           locals[iteration] = partial_iteration
 
           template = (cache[path] ||= find_template(path, keys + [as, counter, iteration]))
           content = template.render(view, locals)
+          content = layout.render(view, locals) { content } if layout
           partial_iteration.iterate!
           content
         end

actionview/test/template/render_test.rb
@@ -367,6 +367,16 @@ module RenderTestCases
       @view.render(partial: "test/customer", layout: "test/b_layout_for_partial_with_object", object: Customer.new("Amazon"))
   end
 
+  def test_render_partial_with_layout_and_homogeneous_collection
+    assert_equal "<b>hi: Amazon</b><b>hi: Yahoo</b>",
+      @controller_view.render(partial: [ Customer.new("Amazon"), Customer.new("Yahoo") ], layout: "test/b_layout_for_partial", locals: { greeting: "hi" })
+  end
+
+  def test_render_partial_with_layout_and_heterogeneous_collection
+    assert_equal "<b>hi: Amazon</b><b>hi good customer: Yahoo1</b>",
+      @controller_view.render(partial: [ Customer.new("Amazon"), GoodCustomer.new("Yahoo") ], layout: "test/b_layout_for_partial", locals: { greeting: "hi" })
+  end
+
   def test_render_partial_with_empty_array_should_return_nil
     assert_nil @view.render(partial: [])
   end