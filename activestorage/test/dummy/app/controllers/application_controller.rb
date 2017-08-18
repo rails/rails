85a28aaedc14883e650711ce057c586b1a974403
willnet <netwillnet@gmail.com>
2017-08-18 14:11:12 +0900

    Enable protect_from_forgery in all controllers

--- activestorage/test/dummy/app/controllers/application_controller.rb
+++ activestorage/test/dummy/app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
 end
--- activestorage/test/dummy/config/application.rb
+++ activestorage/test/dummy/config/application.rb
@@ -19,7 +19,7 @@ Bundler.require(*Rails.groups)
 
 module Dummy
   class Application < Rails::Application
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     config.active_storage.service = :local
   end
