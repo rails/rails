92431ea5fb74b0732dbe052ddad38f8227903230
bogdanvlviv <bogdanvlviv@gmail.com>
2017-08-20 17:41:49 +0300

    Use 5.2 version of ActiveRecord::Migration for Active Storage

--- activestorage/db/migrate/20170806125915_create_active_storage_tables.rb
+++ activestorage/db/migrate/20170806125915_create_active_storage_tables.rb
@@ -1,4 +1,4 @@
-class CreateActiveStorageTables < ActiveRecord::Migration[5.1]
+class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
   def change
     create_table :active_storage_blobs do |t|
       t.string   :key,        null: false
