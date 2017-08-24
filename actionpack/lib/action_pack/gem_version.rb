b098c6b3069791603eed5477960d8721df7e43d1
eileencodes <eileencodes@gmail.com>
2017-08-24 15:29:07 -0400

    Preparing for 5.1.4.rc1 release

--- actionpack/lib/action_pack/gem_version.rb
+++ actionpack/lib/action_pack/gem_version.rb
@@ -7,8 +7,8 @@ module ActionPack
   module VERSION
     MAJOR = 5
     MINOR = 1
-    TINY  = 3
-    PRE   = nil
+    TINY  = 4
+    PRE   = "rc1"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
