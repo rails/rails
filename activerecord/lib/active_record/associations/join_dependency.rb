rails/rails

commit caa178c178468f7adcc5f4c597280e6362d9e175

Ensure that inverse associations are set before running callbacks
If a parent association was accessed in an `after_find` or
`after_initialize` callback, it would always end up loading the
association, and then immediately overwriting the association we just
loaded. If this occurred in a way that the parent's `current_scope` was
set to eager load the child, this would result in an infinite loop and
eventually overflow the stack.

For records that are created with `.new`, we have a mechanism to
perform an action before the callbacks are run. I've introduced the same
code path for records created with `instantiate`, and updated all code
which sets inverse instances on newly loaded associations to use this
block instead.

Fixes #26320.

activerecord/lib/active_record/associations/join_dependency.rb
@@ -286,9 +286,12 @@ module ActiveRecord
         end
 
         def construct_model(record, node, row, model_cache, id, aliases)
-          model = model_cache[node][id] ||= node.instantiate(row,
-                                                             aliases.column_aliases(node))
           other = record.association(node.reflection.name)
+
+          model = model_cache[node][id] ||=
+            node.instantiate(row, aliases.column_aliases(node)) do |m|
+              other.set_inverse_instance(m)
+            end
 
           if node.reflection.collection?
             other.target.push(model)
@@ -296,7 +299,6 @@ module ActiveRecord
             other.target = model
           end
 
-          other.set_inverse_instance(model)
           model
         end
     end

activerecord/lib/active_record/relation.rb
@@ -562,8 +562,8 @@ module ActiveRecord
     # return value is the relation itself, not the records.
     #
     #   Post.where(published: true).load # => #<ActiveRecord::Relation>
-    def load
-      exec_queries unless loaded?
+    def load(&block)
+      exec_queries(&block) unless loaded?
 
       self
     end
@@ -678,8 +678,8 @@ module ActiveRecord
 
     private
 
-      def exec_queries
-        @records = eager_loading? ? find_with_associations.freeze : @klass.find_by_sql(arel, bound_attributes).freeze
+      def exec_queries(&block)
+        @records = eager_loading? ? find_with_associations.freeze : @klass.find_by_sql(arel, bound_attributes, &block).freeze
 
         preload = preload_values
         preload +=  includes_values unless eager_loading?

activerecord/lib/active_record/association_relation.rb
@@ -29,7 +29,10 @@ module ActiveRecord
     private
 
       def exec_queries
-        super.each { |r| @association.set_inverse_instance r }
+        super do |r|
+          @association.set_inverse_instance r
+          yield r if block_given?
+        end
       end
   end
 end

activerecord/lib/active_record/associations/collection_association.rb
@@ -390,9 +390,9 @@ module ActiveRecord
           end
 
           binds = AssociationScope.get_bind_values(owner, reflection.chain)
-          records = sc.execute(binds, klass, conn)
-          records.each { |record| set_inverse_instance(record) }
-          records
+          sc.execute(binds, klass, conn) do |record|
+            set_inverse_instance(record)
+          end
         end
 
         # We have some records loaded from the database (persisted) and some that are

activerecord/lib/active_record/associations/join_dependency/join_part.rb
@@ -62,8 +62,8 @@ module ActiveRecord
           hash
         end
 
-        def instantiate(row, aliases)
-          base_klass.instantiate(extract_record(row, aliases))
+        def instantiate(row, aliases, &block)
+          base_klass.instantiate(extract_record(row, aliases), &block)
         end
       end
     end

activerecord/lib/active_record/associations/preloader/association.rb
@@ -62,7 +62,12 @@ module ActiveRecord
         private
 
           def associated_records_by_owner(preloader)
-            records = load_records
+            records = load_records do |record|
+              owner = owners_by_key[convert_key(record[association_key_name])]
+              association = owner.association(reflection.name)
+              association.set_inverse_instance(record)
+            end
+
             owners.each_with_object({}) do |owner, result|
               result[owner] = records[convert_key(owner[owner_key_name])] || []
             end
@@ -77,6 +82,15 @@ module ActiveRecord
               @owner_keys.compact!
             end
             @owner_keys
+          end
+
+          def owners_by_key
+            unless defined?(@owners_by_key)
+              @owners_by_key = owners.each_with_object({}) do |owner, h|
+                h[convert_key(owner[owner_key_name])] = owner
+              end
+            end
+            @owners_by_key
           end
 
           def key_conversion_required?
@@ -99,13 +113,13 @@ module ActiveRecord
             @model.type_for_attribute(owner_key_name.to_s).type
           end
 
-          def load_records
+          def load_records(&block)
             return {} if owner_keys.empty?
             # Some databases impose a limit on the number of ids in a list (in Oracle it's 1000)
             # Make several smaller queries if necessary or make one query if the adapter supports it
             slices  = owner_keys.each_slice(klass.connection.in_clause_length || owner_keys.size)
             @preloaded_records = slices.flat_map do |slice|
-              records_for(slice)
+              records_for(slice).load(&block)
             end
             @preloaded_records.group_by do |record|
               convert_key(record[association_key_name])

activerecord/lib/active_record/associations/preloader/collection_association.rb
@@ -9,7 +9,6 @@ module ActiveRecord
               association = owner.association(reflection.name)
               association.loaded!
               association.target.concat(records)
-              records.each { |record| association.set_inverse_instance(record) }
             end
           end
       end

activerecord/lib/active_record/associations/preloader/singular_association.rb
@@ -10,7 +10,6 @@ module ActiveRecord
 
               association = owner.association(reflection.name)
               association.target = record
-              association.set_inverse_instance(record) if record
             end
           end
       end

commit 1a723c65bbe91ad969b67416233d20eff6d2a46a

Should work inverse association when eager loading
This regression was caused by caa178c1. The block for
`set_inverse_instance` should also be passed to join dependency.

Fixes #30402.

activerecord/lib/active_record/associations/join_dependency.rb
@@ -119,7 +119,7 @@ module ActiveRecord
       end
 
       def aliases
-        Aliases.new join_root.each_with_index.map { |join_part, i|
+        @aliases ||= Aliases.new join_root.each_with_index.map { |join_part, i|
           columns = join_part.column_names.each_with_index.map { |column_name, j|
             Aliases::Column.new column_name, "t#{i}_r#{j}"
           }
@@ -127,7 +127,7 @@ module ActiveRecord
         }
       end
 
-      def instantiate(result_set, aliases)
+      def instantiate(result_set, &block)
         primary_key = aliases.column_alias(join_root, join_root.primary_key)
 
         seen = Hash.new { |i, object_id|
@@ -150,7 +150,7 @@ module ActiveRecord
         message_bus.instrument("instantiation.active_record", payload) do
           result_set.each { |row_hash|
             parent_key = primary_key ? row_hash[primary_key] : row_hash
-            parent = parents[parent_key] ||= join_root.instantiate(row_hash, column_aliases)
+            parent = parents[parent_key] ||= join_root.instantiate(row_hash, column_aliases, &block)
             construct(parent, join_root, row_hash, result_set, seen, model_cache, aliases)
           }
         end

activerecord/lib/active_record/relation.rb
@@ -566,7 +566,7 @@ module ActiveRecord
                     relation = self
 
                     if eager_loading?
-                      find_with_associations { |rel| relation = rel }
+                      find_with_associations { |rel, _| relation = rel }
                     end
 
                     conn = klass.connection
@@ -660,7 +660,19 @@ module ActiveRecord
 
       def exec_queries(&block)
         skip_query_cache_if_necessary do
-          @records = eager_loading? ? find_with_associations.freeze : @klass.find_by_sql(arel, &block).freeze
+          @records =
+            if eager_loading?
+              find_with_associations do |relation, join_dependency|
+                if ActiveRecord::NullRelation === relation
+                  []
+                else
+                  rows = connection.select_all(relation.arel, "SQL")
+                  join_dependency.instantiate(rows, &block)
+                end.freeze
+              end
+            else
+              klass.find_by_sql(arel, &block).freeze
+            end
 
           preload = preload_values
           preload += includes_values unless eager_loading?

activerecord/lib/active_record/relation/finder_methods.rb
@@ -372,16 +372,7 @@ module ActiveRecord
         relation = select aliases.columns
         relation = apply_join_dependency(relation, join_dependency)
 
-        if block_given?
-          yield relation
-        else
-          if ActiveRecord::NullRelation === relation
-            []
-          else
-            rows = skip_query_cache_if_necessary { connection.select_all(relation.arel, "SQL") }
-            join_dependency.instantiate(rows, aliases)
-          end
-        end
+        yield relation, join_dependency
       end
 
       def construct_relation_for_exists(relation, conditions)

activerecord/test/cases/associations/eager_test.rb
@@ -40,6 +40,12 @@ class EagerAssociationTest < ActiveRecord::TestCase
     assert_nil member.favourite_club
   end
 
+  def test_should_work_inverse_of_with_eager_load
+    author = authors(:david)
+    assert_same author, author.posts.first.author
+    assert_same author, author.posts.eager_load(:comments).first.author
+  end
+
   def test_loading_with_one_association
     posts = Post.all.merge!(includes: :comments).to_a
     post = posts.find { |p| p.id == 1 }