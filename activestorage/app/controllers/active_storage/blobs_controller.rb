562a8228792703ca1391cb6a395b8c4486cc8112
Dwight Watson <dwightwatson@me.com>
2017-08-30 17:24:42 +1000

    Allow BlobsController to return file response instead of redirect

--- activestorage/app/controllers/active_storage/blobs_controller.rb
+++ activestorage/app/controllers/active_storage/blobs_controller.rb
@@ -7,8 +7,7 @@
 class ActiveStorage::BlobsController < ActionController::Base
   def show
     if blob = find_signed_blob
-      expires_in 5.minutes # service_url defaults to 5 minutes
-      redirect_to blob.service_url(disposition: disposition_param)
+      params[:no_redirect] ? data_response(blob) : redirect_response(blob)
     else
       head :not_found
     end
@@ -21,5 +20,23 @@ class ActiveStorage::BlobsController < ActionController::Base
 
     def disposition_param
       params[:disposition].presence_in(%w( inline attachment )) || "inline"
+    end
+
+    def filename_param
+      params[:filename].presence || File.basename(@service_url)
+    end
+
+    def type_param
+      params[:type].presence || Mime::Type.lookup_by_extension(File.extname(@service_url).split(".").last).to_s
+    end
+
+    def data_response(blob)
+      @service_url = blob.service_url(disposition: "inline")
+      send_data open(@service_url).read, disposition: disposition_param, filename: filename_param, type: type_param
+    end
+
+    def redirect_response(blob)
+      expires_in 5.minutes # service_url defaults to 5 minutes
+      redirect_to blob.service_url(disposition: disposition_param)
     end
 end
