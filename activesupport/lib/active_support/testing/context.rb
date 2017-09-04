b10b7a69d9005e88f76f44a3d204407b243500ef
Stanislav K <lunaticman@gmail.com>
2017-09-04 19:51:05 +0700

    code style changes

--- activesupport/lib/active_support/testing/context.rb
+++ activesupport/lib/active_support/testing/context.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 module ActiveSupport
   module Testing
     module Context
       def context(message, &block)
-        class_name = message.gsub(/\W+/, ' ').strip.split(/\s/).map(&:capitalize).join('')+'Test'
+        class_name = message.gsub(/\W+/, " ").strip.split(/\s/).map(&:capitalize).join("") + "Test"
 
         clean_class = Class.new(self)
         clean_class.instance_methods.each do |test_method|
-          clean_class.send(:undef_method, test_method) if test_method.to_s.start_with?('test_')
+          clean_class.send(:undef_method, test_method) if test_method.to_s.start_with?("test_")
         end
 
         const_set(class_name, Class.new(clean_class, &block))
