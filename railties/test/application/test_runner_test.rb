rails/rails

commit 07d84b7c8db5b85f7521cfc5d2028ed973d9a14b

Allow bin/rails test task to take absolute paths as arguments
Solves #29923

This regression was caused due to a wrong regex to filter out
paths, introduced in commit 796a1cf0e

The regex was /^\w+\// which did not accept paths with a leading
slash and hence all absolute paths were filtered out.

This change introduces a change in regex which allows for a leading
slash and acts on the matched term accordingly.

While cascading through the case block, the paths are checked for
line number specification, existence of a directory at that path
and if none of those match, then it is considered to be a path to the
file. The regex matchers specified are filtered out via the call
to `Array#compact` since they do not match any of these conditions.

railties/lib/rails/test_unit/runner.rb
@@ -58,7 +58,7 @@ module Rails
 
         private
           def extract_filters(argv)
-            argv.select { |arg| arg =~ /^\w+\// }.map do |path|
+            argv.select { |arg| arg =~ %r{^/?\w+/} }.map do |path|
               case
               when path =~ /(:\d+)+$/
                 file, *lines = path.split(":")
@@ -66,11 +66,11 @@ module Rails
                 file
               when Dir.exist?(path)
                 "#{path}/**/*_test.rb"
-              else
+              when path !~ /\/$/
                 filters << [ path, [] ]
                 path
               end
-            end
+            end.compact
           end
       end
     end

railties/test/application/test_runner_test.rb
@@ -31,10 +31,24 @@ module ApplicationTests
       assert_match "1 runs, 1 assertions, 0 failures", run_test_command("test/models/foo_test.rb")
     end
 
+    def test_run_single_file_with_absolute_path
+      create_test_file :models, "foo"
+      create_test_file :models, "bar"
+      assert_match "1 runs, 1 assertions, 0 failures", run_test_command("#{app_path}/test/models/foo_test.rb")
+    end
+
     def test_run_multiple_files
       create_test_file :models,  "foo"
       create_test_file :models,  "bar"
       assert_match "2 runs, 2 assertions, 0 failures", run_test_command("test/models/foo_test.rb test/models/bar_test.rb")
+    end
+
+    def test_run_multiple_files_with_absolute_paths
+      create_test_file :models,  "foo"
+      create_test_file :controllers,  "foobar_controller"
+      create_test_file :models, "bar"
+
+      assert_match "2 runs, 2 assertions, 0 failures", run_test_command("#{app_path}/test/models/foo_test.rb #{app_path}/test/controllers/foobar_controller_test.rb")
     end
 
     def test_run_file_with_syntax_error
@@ -258,6 +272,18 @@ module ApplicationTests
       create_test_file :controllers, "accounts_controller"
 
       run_test_command("test/models test/controllers").tap do |output|
+        assert_match "AccountTest", output
+        assert_match "AccountsControllerTest", output
+        assert_match "2 runs, 2 assertions, 0 failures, 0 errors, 0 skips", output
+      end
+    end
+
+    def test_run_multiple_folders_with_absolute_paths
+      create_test_file :models, "account"
+      create_test_file :controllers, "accounts_controller"
+      create_test_file :helpers, "foo_helper"
+
+      run_test_command("#{app_path}/test/models #{app_path}/test/controllers").tap do |output|
         assert_match "AccountTest", output
         assert_match "AccountsControllerTest", output
         assert_match "2 runs, 2 assertions, 0 failures, 0 errors, 0 skips", output

commit 335c422797a7d7a5ef55e3e98362342e0847e02c

Accept trailing slashes in bin/rails test paths.
07d84b7c8db5b85f7521cfc5d2028ed973d9a14b accidentally made it so that
paths with trailing slashes are ignored by bin/rails test. This is a bit
annoying, as Bash auto-complete adds a trailing slash for directory
names.

This commit fixes the bug and adds relevant tests.

railties/lib/rails/test_unit/runner.rb
@@ -61,7 +61,7 @@ module Rails
         private
           def extract_filters(argv)
             # Extract absolute and relative paths but skip -n /.*/ regexp filters.
-            argv.select { |arg| arg =~ %r%^/?\w+/% && !arg.end_with?("/") }.map do |path|
+            argv.select { |arg| arg =~ %r%^/?\w+/% && (!arg.end_with?("/") || !arg.start_with?("/")) }.map do |path|
               case
               when path =~ /(:\d+)+$/
                 file, *lines = path.split(":")

railties/test/application/test_runner_test.rb
@@ -164,6 +164,17 @@ module ApplicationTests
       end
     end
 
+    def test_run_folder_with_trailing_slash
+      create_test_file :models, "foo"
+      create_test_file :models, "bar"
+      create_test_file :controllers, "foobar_controller"
+      run_test_command("test/models/").tap do |output|
+        assert_match "FooTest", output
+        assert_match "BarTest", output
+        assert_match "2 runs, 2 assertions, 0 failures", output
+      end
+    end
+
     def test_run_named_test
       app_file "test/unit/chu_2_koi_test.rb", <<-RUBY
         require 'test_helper'
@@ -268,8 +279,21 @@ module ApplicationTests
     def test_run_multiple_folders
       create_test_file :models, "account"
       create_test_file :controllers, "accounts_controller"
+      create_test_file :helpers, "foo_helper"
 
       run_test_command("test/models test/controllers").tap do |output|
+        assert_match "AccountTest", output
+        assert_match "AccountsControllerTest", output
+        assert_match "2 runs, 2 assertions, 0 failures, 0 errors, 0 skips", output
+      end
+    end
+
+    def test_run_multiple_folders_with_trailing_slashes
+      create_test_file :models, "account"
+      create_test_file :controllers, "accounts_controller"
+      create_test_file :helpers, "foo_helper"
+
+      run_test_command("test/models/ test/controllers/").tap do |output|
         assert_match "AccountTest", output
         assert_match "AccountsControllerTest", output
         assert_match "2 runs, 2 assertions, 0 failures, 0 errors, 0 skips", output