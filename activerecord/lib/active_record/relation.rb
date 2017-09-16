fda9df7b4e328ed2bfc3dad05899ef6269495688
Jeremy Green <jeremy@octolabs.com>
2017-09-16 10:56:04 -0600

    Update payload names for `sql.active_record` to be more descriptive.
    Fixes #30586.

--- activerecord/lib/active_record/relation.rb
+++ activerecord/lib/active_record/relation.rb
@@ -63,7 +63,7 @@ module ActiveRecord
 
       @klass.connection.insert(
         im,
-        "SQL",
+        "#{@klass} Create",
         primary_key || false,
         primary_key_value,
         nil,
@@ -86,7 +86,7 @@ module ActiveRecord
 
       @klass.connection.update(
         um,
-        "SQL",
+        "#{@klass} Update",
       )
     end
 
@@ -373,7 +373,7 @@ module ActiveRecord
         stmt.wheres = arel.constraints
       end
 
-      @klass.connection.update stmt, "SQL"
+      @klass.connection.update stmt, "#{@klass} Update All"
     end
 
     # Updates an object (or multiple objects) and saves it to the database, if validations pass.
@@ -503,7 +503,7 @@ module ActiveRecord
         stmt.wheres = arel.constraints
       end
 
-      affected = @klass.connection.delete(stmt, "SQL")
+      affected = @klass.connection.delete(stmt, "#{@klass} Destroy")
 
       reset
       affected
