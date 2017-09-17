7d14bda3a7c74ec0262fa021e641a4235f5405d5
Yoshiyuki Hirano <yhirano@me.com>
2017-09-17 10:06:47 +0900

    Fix file missing in activestorage's example code [ci skip]
    * File.open("~/face.jpg") raise error:
      `Errno::ENOENT: No such file or directory @ rb_sysopen - ~/face.jpg`

--- activestorage/lib/active_storage/attached/one.rb
+++ activestorage/lib/active_storage/attached/one.rb
@@ -18,7 +18,7 @@ module ActiveStorage
     #
     #   person.avatar.attach(params[:avatar]) # ActionDispatch::Http::UploadedFile object
     #   person.avatar.attach(params[:signed_blob_id]) # Signed reference to blob from direct upload
-    #   person.avatar.attach(io: File.open("~/face.jpg"), filename: "face.jpg", content_type: "image/jpg")
+    #   person.avatar.attach(io: File.open("/path/to/face.jpg"), filename: "face.jpg", content_type: "image/jpg")
     #   person.avatar.attach(avatar_blob) # ActiveStorage::Blob object
     def attach(attachable)
       if attached? && dependent == :purge_later
