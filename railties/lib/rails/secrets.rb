50345c55db472f6ded652bde6b57d77fd6653c99
claudiob <claudiob@users.noreply.github.com>
2017-09-18 14:04:10 -0700

    Don't reference deleted file secrets.yml
    Since #30067 Rails apps do not include a `config/secrets.yml` file
    anymore so that file should not be referenced anymore.

--- railties/lib/rails/secrets.rb
+++ railties/lib/rails/secrets.rb
@@ -42,7 +42,6 @@ module Rails
 
       def template
         <<-end_of_template.strip_heredoc
-          # See `secrets.yml` for tips on generating suitable keys.
           # production:
           #   external_api_key: 1466aac22e6a869134be3d09b9e89232fc2c2289
 
