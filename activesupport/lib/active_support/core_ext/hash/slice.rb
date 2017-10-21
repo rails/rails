a524c955bfcce118da0ecb21affe28e2a102a54b
Akira Matsuda <ronnie@dio.jp>
2017-10-21 15:22:36 +0900

    Hash#slice is in Ruby 2.5+
    since r60229

--- activesupport/lib/active_support/core_ext/hash/slice.rb
+++ activesupport/lib/active_support/core_ext/hash/slice.rb
@@ -23,7 +23,7 @@ class Hash
   def slice(*keys)
     keys.map! { |key| convert_key(key) } if respond_to?(:convert_key, true)
     keys.each_with_object(self.class.new) { |k, hash| hash[k] = self[k] if has_key?(k) }
-  end
+  end unless method_defined?(:slice)
 
   # Replaces the hash with only the given keys.
   # Returns a hash containing the removed key/value pairs.
