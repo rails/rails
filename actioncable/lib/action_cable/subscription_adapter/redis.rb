0f5623d2660bb8221858e66b7d1853f5723b888c
xxDENxx <msiuden@gmail.com>
2017-10-31 17:47:53 +0300

    ActionCable connect to Redis through unix socket
    Add option to Redis initializer

--- actioncable/lib/action_cable/subscription_adapter/redis.rb
+++ actioncable/lib/action_cable/subscription_adapter/redis.rb
@@ -13,7 +13,7 @@ module ActionCable
       # Overwrite this factory method for Redis connections if you want to use a different Redis library than the redis gem.
       # This is needed, for example, when using Makara proxies for distributed Redis.
       cattr_accessor :redis_connector, default: ->(config) do
-        ::Redis.new(config.slice(:url, :host, :port, :db, :password))
+        ::Redis.new(config.slice(:url, :host, :port, :db, :password, :path))
       end
 
       def initialize(*)
