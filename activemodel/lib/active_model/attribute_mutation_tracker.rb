fc7a6c738122ba9dd6eff4ede08d67e86a3f0ea7
Ryuta Kamizono <kamipo@gmail.com>
2017-11-10 23:48:53 +0900

    Add missing require "active_support/core_ext/hash/indifferent_access"
    https://travis-ci.org/rails/rails/jobs/300163454#L2236

--- activemodel/lib/active_model/attribute_mutation_tracker.rb
+++ activemodel/lib/active_model/attribute_mutation_tracker.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "active_support/core_ext/hash/indifferent_access"
+
 module ActiveModel
   class AttributeMutationTracker # :nodoc:
     OPTION_NOT_GIVEN = Object.new
