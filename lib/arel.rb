8f1db57f2d0f7d2cbb407f2c141dfd61ba8a599b
Rafael Mendonça França <rafaelmfranca@gmail.com>
2017-11-14 14:05:17 -0500

    Prepare for 9.0.0

--- lib/arel.rb
+++ lib/arel.rb
@@ -24,7 +24,7 @@ require 'arel/delete_manager'
 require 'arel/nodes'
 
 module Arel
-  VERSION = '9.0.0.alpha'
+  VERSION = '9.0.0'
 
   def self.sql raw_sql
     Arel::Nodes::SqlLiteral.new raw_sql
