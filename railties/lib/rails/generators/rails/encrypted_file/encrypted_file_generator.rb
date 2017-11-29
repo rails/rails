c8dc4f2e41afa07c2b2ec701e2ba4682e5ccdd37
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2017-11-29 10:38:51 +0100

    Removed "private" generators from command list.
    Appropriate way to handle encrypted command is by `bin/rails credentials` and
    `bin/rails encrypted`
    
    It was displayed on `bin/rails generate` command:
    ```
    Please choose a generator below.
    
    Rails:
      application_record
      assets
      channel
      controller
      encrypted_file
      encryption_key_file
      generator
      ...
    ```

--- railties/lib/rails/generators/rails/encrypted_file/encrypted_file_generator.rb
+++ railties/lib/rails/generators/rails/encrypted_file/encrypted_file_generator.rb
@@ -5,23 +5,7 @@ require "active_support/encrypted_file"
 
 module Rails
   module Generators
-    class EncryptedFileGenerator < Base
-      def add_encrypted_file(file_path, key_path)
-        unless File.exist?(file_path)
-          say "Adding #{file_path} to store encrypted content."
-          say ""
-          say "The following content has been encrypted with the encryption key:"
-          say ""
-          say template, :on_green
-          say ""
-
-          add_encrypted_file_silently(file_path, key_path)
-
-          say "You can edit encrypted file with `bin/rails encrypted:edit #{file_path}`."
-          say ""
-        end
-      end
-
+    class EncryptedFileGenerator < Base # :nodoc:
       def add_encrypted_file_silently(file_path, key_path, template = encrypted_file_template)
         unless File.exist?(file_path)
           setup = { content_path: file_path, key_path: key_path, env_key: "RAILS_MASTER_KEY", raise_if_missing_key: true }
