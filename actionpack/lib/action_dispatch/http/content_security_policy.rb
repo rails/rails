49ddd6a223d34207c946d43b30b5c52526b95879
Simon Dawson <spdawson@gmail.com>
2017-12-04 20:27:23 +0000

    Use Object#deep_dup

--- actionpack/lib/action_dispatch/http/content_security_policy.rb
+++ actionpack/lib/action_dispatch/http/content_security_policy.rb
@@ -175,13 +175,7 @@ module ActionDispatch #:nodoc:
 
     private
       def copy_directives(directives)
-        directives.transform_values do |sources|
-          if sources.is_a?(Array)
-            sources.map(&:dup)
-          else
-            sources
-          end
-        end
+        directives.transform_values { |sources| sources.deep_dup }
       end
 
       def apply_mappings(sources)
