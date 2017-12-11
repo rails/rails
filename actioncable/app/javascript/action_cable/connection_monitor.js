403c001c56e3980e624da2cb1e1e98d667499d40
Richard Macklin <richard.github@nrm.com>
2017-12-11 14:44:52 +0800

    Run decaffeinate on action_cable/*.js
    Using [decaffeinate], we have converted these files from coffeescript
    syntax to ES2015 syntax. Decaffeinate is very conservative in the
    conversion process to ensure exact coffeescript semantics are preserved.
    Most of the time, it's safe to clean up the code, and decaffeinate has
    left suggestions regarding potential cleanups we can take. I'll tackle
    those cleanups separately.
    
    After running decaffeinate, I ran:
    ```
    eslint --fix app/javascript
    ```
    using the eslint configuration from ActiveStorage to automatically
    correct lint violations in the decaffeinated output. This removed 189
    extra semicolons and changed one instance of single quotes to double
    quotes.
    
    Note: decaffeinate and eslint can't parse ERB syntax. So I worked around
    that by temporarily quoting the ERB:
    ```diff
     @ActionCable =
    -  INTERNAL: <%= ActionCable::INTERNAL.to_json %>
    +  INTERNAL: "<%= ActionCable::INTERNAL.to_json %>"
       WebSocket: window.WebSocket
       logger: window.console
    ```
    and then removing those quotes after running decaffeinate and eslint.
    
    [decaffeinate]: https://github.com/decaffeinate/decaffeinate

--- actioncable/app/javascript/action_cable/connection_monitor.js
+++ actioncable/app/javascript/action_cable/connection_monitor.js
@@ -1,95 +1,136 @@
-# Responsible for ensuring the cable connection is in good health by validating the heartbeat pings sent from the server, and attempting
-# revival reconnections if things go astray. Internal class, not intended for direct user manipulation.
-class ActionCable.ConnectionMonitor
-  @pollInterval:
-    min: 3
-    max: 30
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS206: Consider reworking classes to avoid initClass
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+// Responsible for ensuring the cable connection is in good health by validating the heartbeat pings sent from the server, and attempting
+// revival reconnections if things go astray. Internal class, not intended for direct user manipulation.
+(function() {
+  let now = undefined
+  let secondsSince = undefined
+  let clamp = undefined
+  const Cls = (ActionCable.ConnectionMonitor = class ConnectionMonitor {
+    static initClass() {
+      this.pollInterval = {
+        min: 3,
+        max: 30
+      }
+  
+      this.staleThreshold = 6
+  
+      now = () => new Date().getTime()
+  
+      secondsSince = time => (now() - time) / 1000
+  
+      clamp = (number, min, max) => Math.max(min, Math.min(max, number))
+       // Server::Connections::BEAT_INTERVAL * 2 (missed two pings)
+    }
 
-  @staleThreshold: 6 # Server::Connections::BEAT_INTERVAL * 2 (missed two pings)
+    constructor(connection) {
+      this.visibilityDidChange = this.visibilityDidChange.bind(this)
+      this.connection = connection
+      this.reconnectAttempts = 0
+    }
 
-  constructor: (@connection) ->
-    @reconnectAttempts = 0
+    start() {
+      if (!this.isRunning()) {
+        this.startedAt = now()
+        delete this.stoppedAt
+        this.startPolling()
+        document.addEventListener("visibilitychange", this.visibilityDidChange)
+        return ActionCable.log(`ConnectionMonitor started. pollInterval = ${this.getPollInterval()} ms`)
+      }
+    }
 
-  start: ->
-    unless @isRunning()
-      @startedAt = now()
-      delete @stoppedAt
-      @startPolling()
-      document.addEventListener("visibilitychange", @visibilityDidChange)
-      ActionCable.log("ConnectionMonitor started. pollInterval = #{@getPollInterval()} ms")
+    stop() {
+      if (this.isRunning()) {
+        this.stoppedAt = now()
+        this.stopPolling()
+        document.removeEventListener("visibilitychange", this.visibilityDidChange)
+        return ActionCable.log("ConnectionMonitor stopped")
+      }
+    }
 
-  stop: ->
-    if @isRunning()
-      @stoppedAt = now()
-      @stopPolling()
-      document.removeEventListener("visibilitychange", @visibilityDidChange)
-      ActionCable.log("ConnectionMonitor stopped")
+    isRunning() {
+      return (this.startedAt != null) && (this.stoppedAt == null)
+    }
 
-  isRunning: ->
-    @startedAt? and not @stoppedAt?
+    recordPing() {
+      return this.pingedAt = now()
+    }
 
-  recordPing: ->
-    @pingedAt = now()
+    recordConnect() {
+      this.reconnectAttempts = 0
+      this.recordPing()
+      delete this.disconnectedAt
+      return ActionCable.log("ConnectionMonitor recorded connect")
+    }
 
-  recordConnect: ->
-    @reconnectAttempts = 0
-    @recordPing()
-    delete @disconnectedAt
-    ActionCable.log("ConnectionMonitor recorded connect")
+    recordDisconnect() {
+      this.disconnectedAt = now()
+      return ActionCable.log("ConnectionMonitor recorded disconnect")
+    }
 
-  recordDisconnect: ->
-    @disconnectedAt = now()
-    ActionCable.log("ConnectionMonitor recorded disconnect")
+    // Private
 
-  # Private
+    startPolling() {
+      this.stopPolling()
+      return this.poll()
+    }
 
-  startPolling: ->
-    @stopPolling()
-    @poll()
+    stopPolling() {
+      return clearTimeout(this.pollTimeout)
+    }
 
-  stopPolling: ->
-    clearTimeout(@pollTimeout)
+    poll() {
+      return this.pollTimeout = setTimeout(() => {
+        this.reconnectIfStale()
+        return this.poll()
+      }
+      , this.getPollInterval())
+    }
 
-  poll: ->
-    @pollTimeout = setTimeout =>
-      @reconnectIfStale()
-      @poll()
-    , @getPollInterval()
+    getPollInterval() {
+      const {min, max} = this.constructor.pollInterval
+      const interval = 5 * Math.log(this.reconnectAttempts + 1)
+      return Math.round(clamp(interval, min, max) * 1000)
+    }
 
-  getPollInterval: ->
-    {min, max} = @constructor.pollInterval
-    interval = 5 * Math.log(@reconnectAttempts + 1)
-    Math.round(clamp(interval, min, max) * 1000)
+    reconnectIfStale() {
+      if (this.connectionIsStale()) {
+        ActionCable.log(`ConnectionMonitor detected stale connection. reconnectAttempts = ${this.reconnectAttempts}, pollInterval = ${this.getPollInterval()} ms, time disconnected = ${secondsSince(this.disconnectedAt)} s, stale threshold = ${this.constructor.staleThreshold} s`)
+        this.reconnectAttempts++
+        if (this.disconnectedRecently()) {
+          return ActionCable.log("ConnectionMonitor skipping reopening recent disconnect")
+        } else {
+          ActionCable.log("ConnectionMonitor reopening")
+          return this.connection.reopen()
+        }
+      }
+    }
 
-  reconnectIfStale: ->
-    if @connectionIsStale()
-      ActionCable.log("ConnectionMonitor detected stale connection. reconnectAttempts = #{@reconnectAttempts}, pollInterval = #{@getPollInterval()} ms, time disconnected = #{secondsSince(@disconnectedAt)} s, stale threshold = #{@constructor.staleThreshold} s")
-      @reconnectAttempts++
-      if @disconnectedRecently()
-        ActionCable.log("ConnectionMonitor skipping reopening recent disconnect")
-      else
-        ActionCable.log("ConnectionMonitor reopening")
-        @connection.reopen()
+    connectionIsStale() {
+      return secondsSince(this.pingedAt != null ? this.pingedAt : this.startedAt) > this.constructor.staleThreshold
+    }
 
-  connectionIsStale: ->
-    secondsSince(@pingedAt ? @startedAt) > @constructor.staleThreshold
+    disconnectedRecently() {
+      return this.disconnectedAt && (secondsSince(this.disconnectedAt) < this.constructor.staleThreshold)
+    }
 
-  disconnectedRecently: ->
-    @disconnectedAt and secondsSince(@disconnectedAt) < @constructor.staleThreshold
-
-  visibilityDidChange: =>
-    if document.visibilityState is "visible"
-      setTimeout =>
-        if @connectionIsStale() or not @connection.isOpen()
-          ActionCable.log("ConnectionMonitor reopening stale connection on visibilitychange. visbilityState = #{document.visibilityState}")
-          @connection.reopen()
-      , 200
-
-  now = ->
-    new Date().getTime()
-
-  secondsSince = (time) ->
-    (now() - time) / 1000
-
-  clamp = (number, min, max) ->
-    Math.max(min, Math.min(max, number))
+    visibilityDidChange() {
+      if (document.visibilityState === "visible") {
+        return setTimeout(() => {
+          if (this.connectionIsStale() || !this.connection.isOpen()) {
+            ActionCable.log(`ConnectionMonitor reopening stale connection on visibilitychange. visbilityState = ${document.visibilityState}`)
+            return this.connection.reopen()
+          }
+        }
+        , 200)
+      }
+    }
+  })
+  Cls.initClass()
+  return Cls
+})()
