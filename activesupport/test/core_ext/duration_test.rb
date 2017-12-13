041b2052e6d5d715474e34f3625f88f586941bfd
Chen Kinnrot <kinnrot@gmail.com>
2017-12-13 10:16:46 +0200

    Empty duration inspect fix

--- activesupport/test/core_ext/duration_test.rb
+++ activesupport/test/core_ext/duration_test.rb
@@ -71,6 +71,8 @@ class DurationTest < ActiveSupport::TestCase
     assert_equal "7 days",                          7.days.inspect
     assert_equal "1 week",                          1.week.inspect
     assert_equal "2 weeks",                         1.fortnight.inspect
+    assert_equal "0 seconds",                       (10 % 5.seconds).inspect
+    assert_equal "10 minutes",                      (10.minutes + 0.seconds).inspect
   end
 
   def test_inspect_locale
