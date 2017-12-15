e4a6a23aa77185127ce9609777820fab14a689bb
Yasuo Honda <yasuo.honda@gmail.com>
2017-12-15 00:13:07 +0000

    Suppress `warning: BigDecimal.new is deprecated`
    `BigDecimal.new` has been deprecated in BigDecimal 1.3.3
     which will be a default for Ruby 2.5.
    
    Refer
    https://github.com/ruby/bigdecimal/commit/533737338db915b00dc7168c3602e4b462b23503
    
    * This commit has been made as follows:
    
    ```
    cd rails
    git grep -l BigDecimal.new | grep -v guides/source/5_0_release_notes.md | grep -v activesupport/test/xml_mini_test.rb | xargs sed -i -e "s/BigDecimal.new/BigDecimal/g"
    ```
    - `activesupport/test/xml_mini_test.rb`
    Editmanually to remove `.new` and `::`
    
    - guides/source/5_0_release_notes.md
    This is a Rails 5.0 release notes.

--- activesupport/lib/active_support/core_ext/object/duplicable.rb
+++ activesupport/lib/active_support/core_ext/object/duplicable.rb
@@ -108,8 +108,8 @@ require "bigdecimal"
 class BigDecimal
   # BigDecimals are duplicable:
   #
-  #   BigDecimal.new("1.2").duplicable? # => true
-  #   BigDecimal.new("1.2").dup         # => #<BigDecimal:...,'0.12E1',18(18)>
+  #   BigDecimal("1.2").duplicable? # => true
+  #   BigDecimal("1.2").dup         # => #<BigDecimal:...,'0.12E1',18(18)>
   def duplicable?
     true
   end
