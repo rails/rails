429b2a2d3af74cdbe058baa29efebe5ef58c4bef
Ivan Zinovyev <vanyazin@gmail.com>
2017-12-20 12:29:30 +0300

    Fix Illegal parameter data type bigint for operation 'get_lock' error

--- activerecord/lib/active_record/connection_adapters/abstract_mysql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/abstract_mysql_adapter.rb
@@ -117,11 +117,11 @@ module ActiveRecord
       end
 
       def get_advisory_lock(lock_name, timeout = 0) # :nodoc:
-        query_value("SELECT GET_LOCK(#{quote(lock_name)}, #{timeout})") == 1
+        query_value("SELECT GET_LOCK(#{quote(lock_name.to_s)}, #{timeout})") == 1
       end
 
       def release_advisory_lock(lock_name) # :nodoc:
-        query_value("SELECT RELEASE_LOCK(#{quote(lock_name)})") == 1
+        query_value("SELECT RELEASE_LOCK(#{quote(lock_name.to_s)})") == 1
       end
 
       def native_database_types
