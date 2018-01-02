60da5d9cd1d9d5f1443afe6e75bbdf8c3707ed8b
eileencodes <eileencodes@gmail.com>
2018-01-02 16:57:27 -0500

    Add a spec name so we can have multiple databases passed in
    This allows us to have more than one cluster in our application and
    still test them. FYI for now this won't work with fixtures because we
    have to change how the fixtures rake task works. That's a second effort
    that I'm also working on, but isn't technically part of the parallel
    testing PR.

--- activerecord/lib/active_record/test_databases.rb
+++ activerecord/lib/active_record/test_databases.rb
@@ -3,32 +3,32 @@ require "active_support/testing/parallelization"
 module ActiveRecord
   module TestDatabases
     ActiveSupport::Testing::Parallelization.after_fork_hook do |i|
-      create_and_migrate(i)
+      create_and_migrate(i, spec_name: Rails.env)
     end
 
     ActiveSupport::Testing::Parallelization.run_cleanup_hook do |i|
-      puts "stillcalling original?"
-      drop(i)
+      drop(i, spec_name: Rails.env)
     end
 
-    def self.create_and_migrate(i)
+    def self.create_and_migrate(i, spec_name:)
       old, ENV["VERBOSE"] = ENV["VERBOSE"], "false"
 
-      connection_spec = ActiveRecord::Base.configurations[Rails.env]
+      connection_spec = ActiveRecord::Base.configurations[spec_name]
 
       connection_spec["database"] += "-#{i}"
       ActiveRecord::Tasks::DatabaseTasks.create(connection_spec)
       ActiveRecord::Base.establish_connection(connection_spec)
-      if ActiveRecord::Migrator.needs_migration?
+      if ActiveRecord::Base.connection.migration_context.needs_migration?
         ActiveRecord::Tasks::DatabaseTasks.migrate
       end
     ensure
+      ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations[Rails.env])
       ENV["VERBOSE"] = old
     end
 
-    def self.drop(i)
+    def self.drop(i, spec_name:)
       old, ENV["VERBOSE"] = ENV["VERBOSE"], "false"
-      connection_spec = ActiveRecord::Base.configurations[Rails.env]
+      connection_spec = ActiveRecord::Base.configurations[spec_name]
 
       ActiveRecord::Tasks::DatabaseTasks.drop(connection_spec)
     ensure
