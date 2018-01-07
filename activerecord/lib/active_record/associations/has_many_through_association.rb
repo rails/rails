455456437ce2b96fdc632febc742fc4c25ae126d
Ryuta Kamizono <kamipo@gmail.com>
2018-01-07 23:18:54 +0900

    Simply use `scope.delete_all` instead of constructing delete manager

--- activerecord/lib/active_record/associations/has_many_through_association.rb
+++ activerecord/lib/active_record/associations/has_many_through_association.rb
@@ -147,14 +147,7 @@ module ActiveRecord
               count = scope.destroy_all.length
             else
               scope.each(&:_run_destroy_callbacks)
-
-              arel = scope.arel
-
-              stmt = Arel::DeleteManager.new
-              stmt.from scope.klass.arel_table
-              stmt.wheres = arel.constraints
-
-              count = scope.klass.connection.delete(stmt, "SQL")
+              count = scope.delete_all
             end
           when :nullify
             count = scope.update_all(source_reflection.foreign_key => nil)
