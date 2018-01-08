21417e02e906f6b64f013aafefdc0cb8ea8b5e74
Javan Makhmali <javan@javan.us>
2018-01-08 17:07:32 -0500

    Work around Firefox's refusal to dispatch events on disabled elements. Fixes #31393
    Brought to you by a 12 year old Firefox bug: https://bugzilla.mozilla.org/show_bug.cgi?id=329509

--- activestorage/app/javascript/activestorage/helpers.js
+++ activestorage/app/javascript/activestorage/helpers.js
@@ -23,11 +23,20 @@ export function findElement(root, selector) {
 }
 
 export function dispatchEvent(element, type, eventInit = {}) {
+  const { disabled } = element
   const { bubbles, cancelable, detail } = eventInit
   const event = document.createEvent("Event")
+
   event.initEvent(type, bubbles || true, cancelable || true)
   event.detail = detail || {}
-  element.dispatchEvent(event)
+
+  try {
+    element.disabled = false
+    element.dispatchEvent(event)
+  } finally {
+    element.disabled = disabled
+  }
+
   return event
 }
 
