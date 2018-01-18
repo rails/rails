5ac6ec54a673e493cddf6bf2eff5b98e52b3c268
Koichi ITO <koic.ito@gmail.com>
2018-01-18 12:09:16 +0900

    Enable autocorrect for `Lint/EndAlignment` cop
    ### Summary
    
    This PR changes .rubocop.yml.
    
    Regarding the code using `if ... else ... end`, I think the coding style
    that Rails expects is as follows.
    
    ```ruby
    var = if cond
      a
    else
      b
    end
    ```
    
    However, the current .rubocop.yml setting does not offense for the
    following code.
    
    ```ruby
    var = if cond
            a
          else
            b
          end
    ```
    
    I think that the above code expects offense to be warned.
    Moreover, the layout by autocorrect is unnatural.
    
    ```ruby
    var = if cond
      a
          else
            b
          end
    ```
    
    This PR adds a setting to .rubocop.yml to make an offense warning and
    autocorrect as expected by the coding style.
    And this change also fixes `case ... when ... end` together.
    
    Also this PR itself is an example that arranges the layout using
    `rubocop -a`.
    
    ### Other Information
    
    Autocorrect of `Lint/EndAlignment` cop is `false` by default.
    https://github.com/bbatsov/rubocop/blob/v0.51.0/config/default.yml#L1443
    
    This PR changes this value to `true`.
    
    Also this PR has changed it together as it is necessary to enable
    `Layout/ElseAlignment` cop to make this behavior.

--- activesupport/lib/active_support/values/time_zone.rb
+++ activesupport/lib/active_support/values/time_zone.rb
@@ -238,7 +238,7 @@ module ActiveSupport
         when Numeric, ActiveSupport::Duration
           arg *= 3600 if arg.abs <= 13
           all.find { |z| z.utc_offset == arg.to_i }
-          else
+        else
           raise ArgumentError, "invalid argument to TimeZone[]: #{arg.inspect}"
         end
       end
