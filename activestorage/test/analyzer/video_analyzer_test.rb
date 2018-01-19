9b0c74e8780f9769320ee912e43066627602ce68
George Claghorn <george@basecamp.com>
2018-01-19 18:46:59 -0500

    Use helper method

--- activestorage/test/analyzer/video_analyzer_test.rb
+++ activestorage/test/analyzer/video_analyzer_test.rb
@@ -39,7 +39,8 @@ class ActiveStorage::Analyzer::VideoAnalyzerTest < ActiveSupport::TestCase
 
   test "analyzing a video without a video stream" do
     blob = create_file_blob(filename: "video_without_video_stream.mp4", content_type: "video/mp4")
-    assert_equal({ "analyzed" => true, "identified" => true }, blob.tap(&:analyze).metadata)
+    metadata = extract_metadata_from(blob)
+    assert_equal({ "analyzed" => true, "identified" => true }, metadata)
   end
 
   private
