05b7b80bcaeeb0357cdb6143fbeca1b3c73b5fb9
George Claghorn <george@basecamp.com>
2018-01-22 17:09:13 -0500

    Add missing require

--- activestorage/lib/active_storage/downloading.rb
+++ activestorage/lib/active_storage/downloading.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+require "tmpdir"
+
 module ActiveStorage
   module Downloading
     private
       # Opens a new tempfile in #tempdir and copies blob data into it. Yields the tempfile.
-      def download_blob_to_tempfile # :doc:
+      def download_blob_to_tempfile #:doc:
         Tempfile.open([ "ActiveStorage", blob.filename.extension_with_delimiter ], tempdir) do |file|
           download_blob_to file
           yield file
@@ -12,14 +14,14 @@ module ActiveStorage
       end
 
       # Efficiently downloads blob data into the given file.
-      def download_blob_to(file) # :doc:
+      def download_blob_to(file) #:doc:
         file.binmode
         blob.download { |chunk| file.write(chunk) }
         file.rewind
       end
 
       # Returns the directory in which tempfiles should be opened. Defaults to +Dir.tmpdir+.
-      def tempdir # :doc:
+      def tempdir #:doc:
         Dir.tmpdir
       end
   end
