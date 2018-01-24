1280ad6d19fa56e9bf6d6a261c1231326cb3d8c3
Matthew Draper <matthew@trebex.net>
2018-01-25 00:39:07 +1030

    Make discard safe when it follows a manual disconnect
    It doesn't have to do anything, but it shouldn't fail.
    
    Fixes #31766.

--- activerecord/test/cases/connection_adapters/connection_handler_test.rb
+++ activerecord/test/cases/connection_adapters/connection_handler_test.rb
@@ -171,6 +171,46 @@ module ActiveRecord
           assert_equal 3, ActiveRecord::Base.connection.select_value("SELECT COUNT(*) FROM people")
         end
 
+        def test_forked_child_recovers_from_disconnected_parent
+          object_id = ActiveRecord::Base.connection.object_id
+          assert ActiveRecord::Base.connection.active?
+
+          rd, wr = IO.pipe
+          rd.binmode
+          wr.binmode
+
+          outer_pid = fork {
+            ActiveRecord::Base.connection.disconnect!
+
+            pid = fork {
+              rd.close
+              if ActiveRecord::Base.connection.active?
+                pair = [ActiveRecord::Base.connection.object_id,
+                        ActiveRecord::Base.connection.select_value("SELECT COUNT(*) FROM people")]
+                wr.write Marshal.dump pair
+              end
+              wr.close
+
+              exit # allow finalizers to run
+            }
+
+            Process.waitpid pid
+          }
+
+          wr.close
+
+          Process.waitpid outer_pid
+          child_id, child_count = Marshal.load(rd.read)
+
+          assert_not_equal object_id, child_id
+          rd.close
+
+          assert_equal 3, child_count
+
+          # Outer connection is unaffected
+          assert_equal 6, ActiveRecord::Base.connection.select_value("SELECT 2 * COUNT(*) FROM people")
+        end
+
         def test_retrieve_connection_pool_copies_schema_cache_from_ancestor_pool
           @pool.schema_cache = @pool.connection.schema_cache
           @pool.schema_cache.add("posts")
