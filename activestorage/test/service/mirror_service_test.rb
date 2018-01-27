6bc0ef8286a42dc43d743dbe80e57be87dcb9c62
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-01-27 11:56:48 +0900

    fix to use to in upload method

--- activestorage/test/service/mirror_service_test.rb
+++ activestorage/test/service/mirror_service_test.rb
@@ -20,7 +20,7 @@ class ActiveStorage::Service::MirrorServiceTest < ActiveSupport::TestCase
   test "uploading to all services" do
     begin
       data = "Something else entirely!"
-      key  = upload(data)
+      key  = upload(data, to: @service)
 
       assert_equal data, SERVICE.primary.download(key)
       SERVICE.mirrors.each do |mirror|
@@ -33,7 +33,7 @@ class ActiveStorage::Service::MirrorServiceTest < ActiveSupport::TestCase
 
   test "downloading from primary service" do
     data = "Something else entirely!"
-    key  = upload(data)
+    key  = upload(data, to: @service)
 
     assert_equal data, @service.download(key)
   end
@@ -56,10 +56,10 @@ class ActiveStorage::Service::MirrorServiceTest < ActiveSupport::TestCase
   end
 
   private
-    def upload(data)
+    def upload(data, to:)
       SecureRandom.base58(24).tap do |key|
         io = StringIO.new(data).tap(&:read)
-        @service.upload key, io, checksum: Digest::MD5.base64digest(data)
+        to.upload key, io, checksum: Digest::MD5.base64digest(data)
         assert io.eof?
       end
     end
