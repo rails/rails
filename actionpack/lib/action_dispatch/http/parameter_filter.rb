b8519adbab26bfe200e9eeabfb5f241f831da55d
Xavier Noria <fxn@hashref.com>
2018-02-05 21:57:07 +0100

    tentative way to support arbitrary parameter filters

--- actionpack/lib/action_dispatch/http/parameter_filter.rb
+++ actionpack/lib/action_dispatch/http/parameter_filter.rb
@@ -25,7 +25,7 @@ module ActionDispatch
         def self.compile(filters)
           return lambda { |params| params.dup } if filters.empty?
 
-          strings, regexps, blocks = [], [], []
+          strings, regexps, custom_filters, blocks = [], [], [], []
 
           filters.each do |item|
             case item
@@ -34,7 +34,11 @@ module ActionDispatch
             when Regexp
               regexps << item
             else
-              strings << Regexp.escape(item.to_s)
+              if item.respond_to?(:filter?)
+                custom_filters << item
+              else
+                strings << Regexp.escape(item.to_s)
+              end
             end
           end
 
@@ -44,22 +48,24 @@ module ActionDispatch
           regexps << Regexp.new(strings.join("|".freeze), true) unless strings.empty?
           deep_regexps << Regexp.new(deep_strings.join("|".freeze), true) unless deep_strings.empty?
 
-          new regexps, deep_regexps, blocks
+          new regexps, deep_regexps, custom_filters, blocks
         end
 
-        attr_reader :regexps, :deep_regexps, :blocks
+        attr_reader :regexps, :deep_regexps, :custom_filters, :blocks
 
-        def initialize(regexps, deep_regexps, blocks)
+        def initialize(regexps, deep_regexps, custom_filters, blocks)
           @regexps = regexps
           @deep_regexps = deep_regexps.any? ? deep_regexps : nil
+          @custom_filters = custom_filters.any? ? custom_filters : nil
           @blocks = blocks
+          @maintain_parents_stack = deep_regexps || custom_filters
         end
 
         def call(original_params, parents = [])
           filtered_params = original_params.class.new
 
           original_params.each do |key, value|
-            parents.push(key) if deep_regexps
+            parents.push(key) if @maintain_parents_stack
             if regexps.any? { |r| key =~ r }
               value = FILTERED
             elsif deep_regexps && (joined = parents.join(".")) && deep_regexps.any? { |r| joined =~ r }
@@ -68,12 +74,14 @@ module ActionDispatch
               value = call(value, parents)
             elsif value.is_a?(Array)
               value = value.map { |v| v.is_a?(Hash) ? call(v, parents) : v }
+            elsif custom_filters && custom_filters.any? { |cf| cf.filter?(parents) }
+              value = FILTERED
             elsif blocks.any?
               key = key.dup if key.duplicable?
               value = value.dup if value.duplicable?
               blocks.each { |b| b.call(key, value) }
             end
-            parents.pop if deep_regexps
+            parents.pop if @maintain_parents_stack
 
             filtered_params[key] = value
           end
