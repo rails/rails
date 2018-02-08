5d75ef72e61f00bbf987aa5a73e9ca2a1e23d718
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2018-02-08 19:49:50 +0900

    Do not add master key when `RAILS_MASTER_KEY` env specified (#31922)
    Fixes #31917

--- railties/lib/rails/commands/credentials/credentials_command.rb
+++ railties/lib/rails/commands/credentials/credentials_command.rb
@@ -20,7 +20,7 @@ module Rails
         require_application_and_environment!
 
         ensure_editor_available(command: "bin/rails credentials:edit") || (return)
-        ensure_master_key_has_been_added
+        ensure_master_key_has_been_added if Rails.application.credentials.key.nil?
         ensure_credentials_have_been_added
 
         catch_editing_exceptions do
