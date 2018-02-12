2f13b3c9fe21debec4bd9afe4601125f4fe29ce2
Javan Makhmali <javan@javan.us>
2018-02-12 10:34:29 -0500

    Pull activetext.js into dummy app with webpacker

--- test/dummy/config/environments/production.rb
+++ test/dummy/config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  # Verifies that versions and hashed value of the package contents in the project's package.json
+    config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
