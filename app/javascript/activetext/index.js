986d6a6da4a42a9e92e8c0cc96b7edca35e778de
Javan Makhmali <javan@javan.us>
2018-02-12 15:50:42 -0500

    Add initial ActiveStorage integration

--- app/javascript/activetext/index.js
+++ app/javascript/activetext/index.js
@@ -1,1 +1,33 @@
 import * as Trix from "trix"
+import { DirectUpload } from "activestorage"
+
+// FIXME: Hard coded routes
+const directUploadsURL = "/rails/active_storage/direct_uploads"
+const blobsURL = "/rails/active_storage/blobs"
+
+addEventListener("trix-attachment-add", event => {
+  const { attachment } = event
+  if (!attachment.file) return
+
+  const delegate = {
+    directUploadWillStoreFileWithXHR: (xhr) => {
+      xhr.upload.addEventListener("progress", event => {
+        const progress = event.loaded / event.total * 100
+        attachment.setUploadProgress(progress)
+      })
+    }
+  }
+
+  const directUpload = new DirectUpload(attachment.file, directUploadsURL, delegate)
+
+  directUpload.create((error, attributes) => {
+    if (error) {
+      console.warn("Failed to store file for attachment", attachment, error)
+    } else {
+      console.log("Created blob for attachment", attributes, attachment)
+      const { signed_id } = attributes
+      const url = `${blobsURL}/${signed_id}/${encodeURIComponent(attachment.file.name)}`
+      attachment.setAttributes({ url, signed_id })
+    }
+  })
+})
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_02_08_205311) do
+ActiveRecord::Schema.define(version: 2018_02_12_164506) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string "subject"
