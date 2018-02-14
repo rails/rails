fec73898d26d943f8f8f6da897fcf0fecbcffcdb
Javan Makhmali <javan@javan.us>
2018-02-14 09:45:17 -0500

    Dummy: Set ActiveText renderer for current request

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_active_text_renderer
+
+  private
+    def set_active_text_renderer
+      ActiveText.renderer = self.class.renderer.new(request.env)
+    end
 end
