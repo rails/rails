55e2df15cdf86671801a22caaa4ea26d4e8497d5
Javan Makhmali <javan@javan.us>
2018-02-14 14:35:34 -0500

    Extract AttachmentUpload

--- app/javascript/activetext/index.js
+++ app/javascript/activetext/index.js
@@ -1,34 +1,11 @@
 import * as Trix from "trix"
-import { DirectUpload } from "activestorage"
+import { AttachmentUpload } from "./attachment_upload"
 
 addEventListener("trix-attachment-add", event => {
-  const { attachment } = event
-  if (!attachment.file) return
+  const { attachment, target } = event
 
-  const { directUploadUrl, blobUrlTemplate } = event.target.dataset
-
-  const delegate = {
-    directUploadWillStoreFileWithXHR: (xhr) => {
-      xhr.upload.addEventListener("progress", event => {
-        const progress = event.loaded / event.total * 100
-        attachment.setUploadProgress(progress)
-      })
-    }
+  if (attachment.file) {
+    const upload = new AttachmentUpload(attachment, target)
+    upload.start()
   }
-
-  const directUpload = new DirectUpload(attachment.file, directUploadUrl, delegate)
-
-  directUpload.create((error, attributes) => {
-    if (error) {
-      console.warn("Failed to store file for attachment", attachment, error)
-    } else {
-      const sgid = attributes.attachable_sgid
-
-      const url = blobUrlTemplate
-        .replace(":signed_id", attributes.signed_id)
-        .replace(":filename", encodeURIComponent(attributes.filename))
-
-      attachment.setAttributes({ sgid, url })
-    }
-  })
 })
