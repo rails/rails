9ded0f3c420350144a9d41ccee3dc4b7ad9d8813
Javan Makhmali <javan@javan.us>
2018-02-14 10:33:30 -0500

    Create ASt::Attachment records for ASt::Blobs in content

--- lib/active_text/attribute.rb
+++ lib/active_text/attribute.rb
@@ -5,6 +5,13 @@ module ActiveText
     class_methods do
       def active_text_attribute(attribute_name)
         serialize(attribute_name, ActiveText::Content)
+
+        has_many_attached "#{attribute_name}_attachments"
+
+        after_save do
+          blobs = public_send(attribute_name).attachments.map(&:attachable)
+          public_send("#{attribute_name}_attachments_blobs=", blobs)
+        end
       end
     end
   end
