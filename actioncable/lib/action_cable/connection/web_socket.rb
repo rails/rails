43f1f14e55289a5c15044e68a51a8e31fa9f52ce
P. J. Gouvalaris <pjg@asfalia.com>
2018-02-15 11:11:53 +0200

    Add code and reason parameters to websocket close method

--- actioncable/lib/action_cable/connection/web_socket.rb
+++ actioncable/lib/action_cable/connection/web_socket.rb
@@ -22,8 +22,8 @@ module ActionCable
         websocket.transmit data
       end
 
-      def close
-        websocket.close
+      def close(code = nil, reason = nil)
+        websocket.close(code, reason)
       end
 
       def protocol
