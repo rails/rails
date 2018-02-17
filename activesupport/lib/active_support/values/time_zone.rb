33886e28f1c06b7f252c1db0e433794b00c54a2f
Sean Collins <sean@cllns.com>
2018-02-17 16:00:57 -0700

    Remove require Object#blank? monkey patch
    Object#blank? used to be used in this file, but it's not anymore.
    
    This avoids a monkey-patch, for those who want to use just this isolated
    feature of ActiveSupport.

--- activesupport/lib/active_support/values/time_zone.rb
+++ activesupport/lib/active_support/values/time_zone.rb
@@ -2,7 +2,6 @@
 
 require "tzinfo"
 require "concurrent/map"
-require "active_support/core_ext/object/blank"
 
 module ActiveSupport
   # The TimeZone class serves as a wrapper around TZInfo::Timezone instances.
