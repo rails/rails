cfbde022eedde2ae45e2dde9e3d8792933f070f4
Ryuta Kamizono <kamipo@gmail.com>
2018-02-20 23:38:06 +0900

    PostgreSQL: Allow BC dates like datetime consistently
    BC dates are supported by both date and datetime types.
    
    https://www.postgresql.org/docs/current/static/datatype-datetime.html
    
    Since #1097, new datetime allows year zero as 1 BC, but new date does
    not. It should be allowed even in new date consistently.

--- activerecord/test/cases/adapters/postgresql/date_test.rb
+++ activerecord/test/cases/adapters/postgresql/date_test.rb
@@ -21,4 +21,22 @@ class PostgresqlDateTest < ActiveRecord::PostgreSQLTestCase
     topic = Topic.create!(last_read: -1.0 / 0.0)
     assert_equal(-1.0 / 0.0, topic.last_read)
   end
+
+  def test_bc_date
+    date = Date.new(0) - 1.week
+    topic = Topic.create!(last_read: date)
+    assert_equal date, Topic.find(topic.id).last_read
+  end
+
+  def test_bc_date_leap_year
+    date = Time.utc(-4, 2, 29).to_date
+    topic = Topic.create!(last_read: date)
+    assert_equal date, Topic.find(topic.id).last_read
+  end
+
+  def test_bc_date_year_zero
+    date = Time.utc(0, 4, 7).to_date
+    topic = Topic.create!(last_read: date)
+    assert_equal date, Topic.find(topic.id).last_read
+  end
 end
