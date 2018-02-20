dec290bb0ddf1f12f2d276f12d9db15eb00f13cb
Ryuta Kamizono <kamipo@gmail.com>
2018-02-20 23:38:06 +0900

    PostgreSQL: Allow BC dates like datetime consistently
    BC dates are supported by both date and datetime types.
    
    https://www.postgresql.org/docs/current/static/datatype-datetime.html
    
    Since #1097, new datetime allows year zero as 1 BC, but new date does
    not. It should be allowed even in new date consistently.

--- activerecord/lib/active_record/connection_adapters/postgresql/oid/date.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/oid/date.rb
@@ -9,6 +9,9 @@ module ActiveRecord
             case value
             when "infinity" then ::Float::INFINITY
             when "-infinity" then -::Float::INFINITY
+            when / BC$/
+              astronomical_year = format("%04d", -value[/^\d+/].to_i + 1)
+              super(value.sub(/ BC$/, "").sub(/^\d+/, astronomical_year))
             else
               super
             end
