3ec24987216d125a18fb93a3a95635c72952ba2c
Ryuta Kamizono <kamipo@gmail.com>
2018-02-27 22:18:22 +0900

    Merge pull request #32005 from maschwenk/ar-distinct-order-count-regression
    Active Record distinct & order #count regression

--- activerecord/lib/active_record/relation/calculations.rb
+++ activerecord/lib/active_record/relation/calculations.rb
@@ -112,7 +112,7 @@ module ActiveRecord
       if has_include?(column_name)
         relation = construct_relation_for_association_calculations
 
-        if operation.to_s.downcase == "count" && !distinct_value
+        if operation.to_s.downcase == "count"
           relation.distinct!
           # PostgreSQL: ORDER BY expressions must appear in SELECT list when using DISTINCT
           if (column_name == :all || column_name.nil?) && select_values.empty?
--- activerecord/test/cases/relations_test.rb
+++ activerecord/test/cases/relations_test.rb
@@ -1133,6 +1133,12 @@ class RelationTest < ActiveRecord::TestCase
     assert_queries(1) { assert_equal 11, posts.load.size }
   end
 
+  def test_size_with_eager_loading_and_custom_order_and_distinct
+    posts = Post.includes(:comments).order("comments.id").distinct
+    assert_queries(1) { assert_equal 11, posts.size }
+    assert_queries(1) { assert_equal 11, posts.load.size }
+  end
+
   def test_update_all_with_scope
     tag = Tag.first
     Post.tagged_with(tag.id).update_all title: "rofl"
