10fa49d52aa6b8747bbbdfb54e6cd16ffb0f60f2
Ryuta Kamizono <kamipo@gmail.com>
2018-03-05 21:18:00 +0900

    Merge pull request #32170 from koic/deprecate_safe_level_of_erb_new_in_ruby_2_6
    Deprecate safe_level of `ERB.new` in Ruby 2.6

--- tasks/release.rb
+++ tasks/release.rb
@@ -247,6 +247,12 @@ task :announce do
 
     require "erb"
     template = File.read("../tasks/release_announcement_draft.erb")
-    puts ERB.new(template, nil, "<>").result(binding)
+
+    match = ERB.version.match(/\Aerb\.rb \[(?<version>[^ ]+) /)
+    if match && match[:version] >= "2.2.0" # Ruby 2.6+
+      puts ERB.new(template, trim_mode: "<>").result(binding)
+    else
+      puts ERB.new(template, nil, "<>").result(binding)
+    end
   end
 end
