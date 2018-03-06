5fcbdcfb574c731841be12764c50d9587b58345f
Sean Griffin <sean@seantheprogrammer.com>
2018-03-06 15:07:24 -0700

    Revert "PERF: Recover `changes_applied` performance (#31698)"
    This reverts commit a19e91f0fab13cca61acdb1f33e27be2323b9786.

--- activerecord/lib/active_record/persistence.rb
+++ activerecord/lib/active_record/persistence.rb
@@ -374,6 +374,7 @@ module ActiveRecord
       became.send(:initialize)
       became.instance_variable_set("@attributes", @attributes)
       became.instance_variable_set("@mutations_from_database", @mutations_from_database) if defined?(@mutations_from_database)
+      became.instance_variable_set("@changed_attributes", attributes_changed_by_setter)
       became.instance_variable_set("@new_record", new_record?)
       became.instance_variable_set("@destroyed", destroyed?)
       became.errors.copy!(errors)
