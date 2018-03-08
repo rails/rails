bd259ada8a51d0f26de18e0089b0e18b8f3305d4
Andrew White <andrew.white@unboxed.co>
2018-03-08 11:53:40 +0000

    Quote string return values in doc examples [ci skip]

--- actionpack/lib/action_dispatch/routing/url_for.rb
+++ actionpack/lib/action_dispatch/routing/url_for.rb
@@ -206,8 +206,8 @@ module ActionDispatch
       # This maintains the context of the original caller on
       # whether to return a path or full url, e.g:
       #
-      #   threadable_path(threadable)  # => /buckets/1
-      #   threadable_url(threadable)   # => http://example.com/buckets/1
+      #   threadable_path(threadable)  # => "/buckets/1"
+      #   threadable_url(threadable)   # => "http://example.com/buckets/1"
       #
       def route_for(name, *args)
         public_send(:"#{name}_url", *args)
