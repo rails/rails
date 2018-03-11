3f95054f1c5ffe9e9b3bccdabc1ab1a7a4beb24a
Andrew White <andrew.white@unboxed.co>
2018-03-11 18:23:50 +0000

    Normalize date component when writing to time columns
    For legacy reasons Rails stores time columns on sqlite as full
    timestamp strings. However because the date component wasn't being
    normalized this meant that when they were read back they were being
    prefixed with 2001-01-01 by ActiveModel::Type::Time. This had a
    twofold result - first it meant that the fast code path wasn't being
    used because the string was invalid and second it was corrupting the
    second fractional component being read by the Date._parse code path.
    
    Fix this by a combination of normalizing the timestamps on writing
    and also changing Active Model to be more lenient when detecting
    whether a string starts with a date component before creating the
    dummy time value for parsing.

--- activemodel/lib/active_model/type/time.rb
+++ activemodel/lib/active_model/type/time.rb
@@ -31,11 +31,7 @@ module ActiveModel
           return apply_seconds_precision(value) unless value.is_a?(::String)
           return if value.empty?
 
-          if value.start_with?("2000-01-01")
-            dummy_time_value = value
-          else
-            dummy_time_value = "2000-01-01 #{value}"
-          end
+          dummy_time_value = value.sub(/\A(\d\d\d\d-\d\d-\d\d |)/, "2000-01-01 ")
 
           fast_string_to_time(dummy_time_value) || begin
             time_hash = ::Date._parse(dummy_time_value)
