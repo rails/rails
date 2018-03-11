cdccdd0f2e7914ee80f6020dc9b408793a1c113a
Jeremy Green <jeremy@octolabs.com>
2018-03-11 09:03:14 -0500

    Trigger faktory inline the right way

--- activejob/test/adapters/faktory.rb
+++ activejob/test/adapters/faktory.rb
@@ -2,5 +2,5 @@
 
 require 'faktory_worker_ruby'
 require 'faktory/testing'
-Faktory::Testing.inline!
+Faktory::Testing.blockless_inline_is_a_bad_idea_but_I_wanna_do_it_anyway!
 ActiveJob::Base.queue_adapter = :faktory
