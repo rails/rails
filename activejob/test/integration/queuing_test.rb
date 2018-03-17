bba522f72201fdeeedd8c21db9b8b1d6addcd38b
Jeremy Green <jeremy@octolabs.com>
2018-03-17 16:43:06 -0500

    Add a faktory specific priority test (Faktory priorities can only be 1-9)

--- activejob/test/integration/queuing_test.rb
+++ activejob/test/integration/queuing_test.rb
@@ -8,7 +8,6 @@ require "active_support/core_ext/numeric/time"
 
 class QueuingTest < ActiveSupport::TestCase
   test "should run jobs enqueued on a listening queue" do
-    puts "about to call perform_laster with @id = #{}"
     TestJob.perform_later @id
     wait_for_jobs_to_finish_for(5.seconds)
     assert job_executed
@@ -146,11 +145,23 @@ class QueuingTest < ActiveSupport::TestCase
   end
 
   test "should run job with higher priority first" do
-    skip unless adapter_is?(:delayed_job, :que, :faktory)
+    skip unless adapter_is?(:delayed_job, :que)
 
     wait_until = Time.now + 3.seconds
     TestJob.set(wait_until: wait_until, priority: 20).perform_later "#{@id}.1"
     TestJob.set(wait_until: wait_until, priority: 10).perform_later "#{@id}.2"
+    wait_for_jobs_to_finish_for(10.seconds)
+    assert job_executed "#{@id}.1"
+    assert job_executed "#{@id}.2"
+    assert job_executed_at("#{@id}.2") < job_executed_at("#{@id}.1")
+  end
+
+  test "should run job with higher priority first (faktory specific)" do
+    skip unless adapter_is?(:faktory)
+
+    wait_until = Time.now + 3.seconds
+    TestJob.set(wait_until: wait_until, priority: 1).perform_later "#{@id}.1"
+    TestJob.set(wait_until: wait_until, priority: 9).perform_later "#{@id}.2"
     wait_for_jobs_to_finish_for(10.seconds)
     assert job_executed "#{@id}.1"
     assert job_executed "#{@id}.2"
