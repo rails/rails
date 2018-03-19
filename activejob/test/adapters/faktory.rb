b1a7cbb2bade9e1ebb33d5bd26a2afbcd4c7a478
Jeremy Green <jeremy@octolabs.com>
2018-03-18 22:56:44 -0500

    A little clean up

--- activejob/test/adapters/faktory.rb
+++ activejob/test/adapters/faktory.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'faktory_worker_ruby'
-require 'faktory/testing'
+require "faktory_worker_ruby"
+require "faktory/testing"
 Faktory::Testing.blockless_inline_is_a_bad_idea_but_I_wanna_do_it_anyway!
 ActiveJob::Base.queue_adapter = :faktory
