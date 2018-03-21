6a11d833ec64ef6662fa17286e521b1c1ba749fc
Ryuta Kamizono <kamipo@gmail.com>
2018-03-22 02:58:57 +0900

    Merge pull request #32310 from sodabrew/mysql2-0.4-0.5
    Support mysql2 0.4.x and 0.5.x

--- activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb
@@ -1,7 +1,7 @@
 require "active_record/connection_adapters/abstract_mysql_adapter"
 require "active_record/connection_adapters/mysql/database_statements"
 
-gem "mysql2", ">= 0.3.18", "< 0.5"
+gem "mysql2", ">= 0.3.18", "< 0.6.0"
 require "mysql2"
 raise "mysql2 0.4.3 is not supported. Please upgrade to 0.4.4+" if Mysql2::VERSION == "0.4.3"
 
