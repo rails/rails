5368f2508651c92fbae40cd679afbafdd7e98e77
Andrew White <andrew.white@unboxed.co>
2018-03-21 10:19:10 +0000

    Cache url helpers module
    The urls helpers module returned by Rails.application.routes.url_helpers
    isn't cached so to prevent the cost of building the module cache it locally.

--- actionpack/lib/action_dispatch/routing/mapper.rb
+++ actionpack/lib/action_dispatch/routing/mapper.rb
@@ -664,6 +664,7 @@ module ActionDispatch
           def define_generate_prefix(app, name)
             _route = @set.named_routes.get name
             _routes = @set
+            _url_helpers = @set.url_helpers
 
             script_namer = ->(options) do
               prefix_options = options.slice(*_route.segment_keys)
@@ -675,7 +676,7 @@ module ActionDispatch
 
               # We must actually delete prefix segment keys to avoid passing them to next url_for.
               _route.segment_keys.each { |k| options.delete(k) }
-              _routes.url_helpers.send("#{name}_path", prefix_options)
+              _url_helpers.send("#{name}_path", prefix_options)
             end
 
             app.routes.define_mounted_helper(name, script_namer)
