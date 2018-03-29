b39f89cca44a654d8378f79a2eae15eb02679541
Shia <rise.shia@gmail.com>
2018-03-29 16:26:19 +0900

    Compare ruby version with correct way

--- railties/lib/rails/ruby_version_check.rb
+++ railties/lib/rails/ruby_version_check.rb
@@ -1,4 +1,4 @@
-if RUBY_VERSION < "2.2.2" && RUBY_ENGINE == "ruby"
+if Gem::Version.new(RUBY_VERSION) < Gem::Version.new("2.2.2") && RUBY_ENGINE == "ruby"
   desc = defined?(RUBY_DESCRIPTION) ? RUBY_DESCRIPTION : "ruby #{RUBY_VERSION} (#{RUBY_RELEASE_DATE})"
   abort <<-end_message
 
