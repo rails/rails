rails/rails

commit ccea4cf331e757a222770c4fd0a137b3cc9558b7

Use current_config in structure_dump
This looks like a typo from 0f0aa6a275876502e002c054896734d6536ba5cd

activerecord/lib/active_record/railties/databases.rake
@@ -318,7 +318,7 @@ db_namespace = namespace :db do
         ActiveRecord::Base.establish_connection(config)
         filename = ActiveRecord::Tasks::DatabaseTasks.dump_filename(spec_name, :sql)
         current_config = ActiveRecord::Tasks::DatabaseTasks.current_config
-        ActiveRecord::Tasks::DatabaseTasks.structure_dump(config, filename)
+        ActiveRecord::Tasks::DatabaseTasks.structure_dump(current_config, filename)
 
         if ActiveRecord::SchemaMigration.table_exists?
           File.open(filename, "a") do |f|

commit 0a353a97869b2af256d4253533beeb38303cf753

Fix structure:dump for multiple databases
ccea4cf broke multiple database structure:dump, the current_config line
should have been deleted instead. I'm struggling to write a test for
this since the confings are passed from rake to the structure_dump
method rather than the other way around. Hoping to come up with a test
while I work on structure:load commands for multiple databases.

activerecord/lib/active_record/railties/databases.rake
@@ -317,8 +317,7 @@ db_namespace = namespace :db do
       ActiveRecord::DatabaseConfigurations.configs_for(Rails.env) do |spec_name, config|
         ActiveRecord::Base.establish_connection(config)
         filename = ActiveRecord::Tasks::DatabaseTasks.dump_filename(spec_name, :sql)
-        current_config = ActiveRecord::Tasks::DatabaseTasks.current_config
-        ActiveRecord::Tasks::DatabaseTasks.structure_dump(current_config, filename)
+        ActiveRecord::Tasks::DatabaseTasks.structure_dump(config, filename)
 
         if ActiveRecord::SchemaMigration.table_exists?
           File.open(filename, "a") do |f|