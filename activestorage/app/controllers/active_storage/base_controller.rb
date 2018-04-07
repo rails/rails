b2bcfe8b00abda2fb17593f7590065e51c3bc0ce
Andrew White <pixeltrix@users.noreply.github.com>
2018-04-07 08:20:16 +0100

    Merge pull request #32350 from rails/use-current-model-for-as-url-host
    Use an ActiveSupport::CurrentAttributes model to provide the host for service urls

--- activestorage/app/controllers/active_storage/base_controller.rb
+++ activestorage/app/controllers/active_storage/base_controller.rb
@@ -3,4 +3,8 @@
 # The base controller for all ActiveStorage controllers.
 class ActiveStorage::BaseController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action do
+    ActiveStorage::Current.host = request.base_url
+  end
 end
