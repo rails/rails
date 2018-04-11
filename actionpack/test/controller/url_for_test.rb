rails/rails

commit 8ca8a2d773b942c4ea76baabe2df502a339d05b1

Refactor handling of :action default in routing
The longstanding convention in Rails is that if the :action parameter
is missing or nil then it defaults to 'index'. Up until Rails 5.0.0.beta1
this was handled slightly differently than other routing defaults by
deleting it from the route options and adding it to the recall parameters.

With the recent focus of removing unnecessary duplications this has
exposed a problem in this strategy - we are now mutating the request's
path parameters and causing problems for later url generation. This will
typically affect url_for rather a named url helper since the latter
explicitly pass :controller, :action, etc.

The fix is to add a default for :action in the route class if the path
contains an :action segment and no default is passed. This change also
revealed an issue with the parameterized part expiry in that it doesn't
follow a right to left order - as soon as a dynamic segment is required
then all other segments become required.

Fixes #23019.

actionpack/lib/action_dispatch/journey/formatter.rb
@@ -32,8 +32,13 @@ module ActionDispatch
 
           defaults       = route.defaults
           required_parts = route.required_parts
-          parameterized_parts.keep_if do |key, value|
-            (defaults[key].nil? && value.present?) || value.to_s != defaults[key].to_s || required_parts.include?(key)
+
+          route.parts.reverse_each do |key|
+            break if defaults[key].nil? && parameterized_parts[key].present?
+            break if parameterized_parts[key].to_s != defaults[key].to_s
+            break if required_parts.include?(key)
+
+            parameterized_parts.delete(key)
           end
 
           return [route.format(parameterized_parts), params]

actionpack/lib/action_dispatch/routing/inspector.rb
@@ -33,11 +33,11 @@ module ActionDispatch
       end
 
       def controller
-        requirements[:controller] || ':controller'
+        parts.include?(:controller) ? ':controller' : requirements[:controller]
       end
 
       def action
-        requirements[:action] || ':action'
+        parts.include?(:action) ? ':action' : requirements[:action]
       end
 
       def internal?

actionpack/lib/action_dispatch/routing/mapper.rb
@@ -137,6 +137,10 @@ module ActionDispatch
           @conditions = Hash[conditions]
           @defaults = formats[:defaults].merge(@defaults).merge(normalize_defaults(options))
 
+          if path_params.include?(:action) && !@requirements.key?(:action)
+            @defaults[:action] ||= 'index'
+          end
+
           @required_defaults = (split_options[:required_defaults] || []).map(&:first)
         end
 

actionpack/lib/action_dispatch/routing/route_set.rb
@@ -533,12 +533,10 @@ module ActionDispatch
           @recall      = recall
           @set         = set
 
-          normalize_recall!
           normalize_options!
           normalize_controller_action_id!
           use_relative_controller!
           normalize_controller!
-          normalize_action!
         end
 
         def controller
@@ -555,11 +553,6 @@ module ActionDispatch
               @options[key] = @recall[key]
             end
           end
-        end
-
-        # Set 'index' as default action for recall
-        def normalize_recall!
-          @recall[:action] ||= 'index'
         end
 
         def normalize_options!
@@ -612,13 +605,6 @@ module ActionDispatch
             else
               @options[:controller] = controller
             end
-          end
-        end
-
-        # Move 'index' action from options to recall
-        def normalize_action!
-          if @options[:action] == 'index'.freeze
-            @recall[:action] = @options.delete(:action)
           end
         end
 

actionpack/test/controller/routing_test.rb
@@ -1964,11 +1964,11 @@ class RackMountIntegrationTests < ActiveSupport::TestCase
   def test_extras
     params = {:controller => 'people'}
     assert_equal [], @routes.extra_keys(params)
-    assert_equal({:controller => 'people'}, params)
+    assert_equal({:controller => 'people', :action => 'index'}, params)
 
     params = {:controller => 'people', :foo => 'bar'}
     assert_equal [:foo], @routes.extra_keys(params)
-    assert_equal({:controller => 'people', :foo => 'bar'}, params)
+    assert_equal({:controller => 'people', :action => 'index', :foo => 'bar'}, params)
 
     params = {:controller => 'people', :action => 'create', :person => { :name => 'Josh'}}
     assert_equal [:person], @routes.extra_keys(params)

actionpack/test/dispatch/routing/inspector_test.rb
@@ -339,7 +339,7 @@ module ActionDispatch
         end
 
         assert_equal ["Prefix Verb URI Pattern            Controller#Action",
-                      "       GET  /:controller(/:action) (?-mix:api\\/[^\\/]+)#:action"], output
+                      "       GET  /:controller(/:action) :controller#:action"], output
       end
 
       def test_inspect_routes_shows_resources_route_when_assets_disabled

actionpack/test/dispatch/routing_test.rb
@@ -3964,16 +3964,6 @@ class TestUnicodePaths < ActionDispatch::IntegrationTest
 end
 
 class TestMultipleNestedController < ActionDispatch::IntegrationTest
-  module ::Foo
-    module Bar
-      class BazController < ActionController::Base
-        def index
-          render :inline => "<%= url_for :controller => '/pooh', :action => 'index' %>"
-        end
-      end
-    end
-  end
-
   Routes = ActionDispatch::Routing::RouteSet.new.tap do |app|
     app.draw do
       namespace :foo do
@@ -3985,7 +3975,18 @@ class TestMultipleNestedController < ActionDispatch::IntegrationTest
     end
   end
 
-  include Routes.url_helpers
+  module ::Foo
+    module Bar
+      class BazController < ActionController::Base
+        include Routes.url_helpers
+
+        def index
+          render :inline => "<%= url_for :controller => '/pooh', :action => 'index' %>"
+        end
+      end
+    end
+  end
+
   APP = build_app Routes
   def app; APP end
 
@@ -4715,5 +4716,44 @@ class TestPartialDynamicPathSegments < ActionDispatch::IntegrationTest
 
   def assert_params(params)
     assert_equal(params, request.path_parameters)
+  end
+end
+
+class TestPathParameters < ActionDispatch::IntegrationTest
+  Routes = ActionDispatch::Routing::RouteSet.new.tap do |app|
+    app.draw do
+      scope module: 'test_path_parameters' do
+        scope ':locale', locale: /en|ar/ do
+          root to: 'home#index'
+          get '/about', to: 'pages#about'
+        end
+      end
+
+      get ':controller(/:action/(:id))'
+    end
+  end
+
+  class HomeController < ActionController::Base
+    include Routes.url_helpers
+
+    def index
+      render inline: "<%= root_path %>"
+    end
+  end
+
+  class PagesController < ActionController::Base
+    include Routes.url_helpers
+
+    def about
+      render inline: "<%= root_path(locale: :ar) %> | <%= url_for(locale: :ar) %>"
+    end
+  end
+
+  APP = build_app Routes
+  def app; APP end
+
+  def test_path_parameters_are_not_mutated
+    get '/en/about'
+    assert_equal "/ar | /ar/about", @response.body
   end
 end

commit e4900612f65f3e148624cc7e74d6555924282726

Fix: Route generation regression with optionals with default values.
Rails4 and before supported leading segments of urls that were optional
and had default values. This could be used for example for having
locale in the url, and having a global default that was unspecified.
Rails5 with commit 8ca8a2d773b942c4ea broke this behaviour, and commit
0713265fd15f5ce0d6e fixes the case where all optionals have default
values but not the cases when some have it or when some are required.

Fixes #31413

actionpack/lib/action_dispatch/journey/formatter.rb
@@ -35,11 +35,22 @@ module ActionDispatch
 
           defaults       = route.defaults
           required_parts = route.required_parts
+          filter_action_part = true
 
           route.parts.reverse_each do |key|
-            break if defaults[key].nil? && parameterized_parts[key].present?
+            next if (key == :action) && !filter_action_part
+
+            if defaults[key].nil? && parameterized_parts[key].present?
+              filter_action_part = false
+              next
+            end
+
             next if parameterized_parts[key].to_s != defaults[key].to_s
-            break if required_parts.include?(key)
+
+            if required_parts.include?(key)
+              filter_action_part = false
+              next
+            end
 
             parameterized_parts.delete(key)
           end

actionpack/test/controller/url_for_test.rb
@@ -501,12 +501,54 @@ module AbstractController
                 param1: /\d*/,
                 param2: /\d+/
               }
+            get "(:param1)/test2(/:param2)" => "index#index2",
+              defaults: {
+                param1: 1
+              },
+              constraints: {
+                param1: /\d*/,
+                param2: /\d+/
+              }
+            get "(:param1)/test3/:param2" => "index#index3",
+              defaults: {
+                param1: 1
+              },
+              constraints: {
+                param1: /\d*/,
+                param2: /\d+/
+              }
+            get "test4/(:param1)/(:param2)" => "index#index4",
+              defaults: {
+                param1: 1
+              },
+              constraints: {
+                param1: /\d*/,
+                param2: /\d+/
+              }
           end
 
           kls = Class.new { include set.url_helpers }
           kls.default_url_options[:host] = "www.basecamphq.com"
 
+          assert_equal "http://www.basecamphq.com/test", kls.new.url_for(controller: "index")
           assert_equal "http://www.basecamphq.com/test", kls.new.url_for(controller: "index", param1: "1")
+          assert_equal "http://www.basecamphq.com/2/test", kls.new.url_for(controller: "index", param1: "2")
+          assert_equal "http://www.basecamphq.com/test/3", kls.new.url_for(controller: "index", param2: "3")
+
+          assert_equal "http://www.basecamphq.com/test2", kls.new.url_for(controller: "index", param1: "1", action: "index2")
+          assert_equal "http://www.basecamphq.com/2/test2", kls.new.url_for(controller: "index", param1: "2", action: "index2")
+          assert_equal "http://www.basecamphq.com/test2/3", kls.new.url_for(controller: "index", action: "index2", param2: "3")
+          assert_equal "http://www.basecamphq.com/2/test2/3", kls.new.url_for(controller: "index", param1: "2", action: "index2", param2: "3")
+
+          assert_equal "http://www.basecamphq.com/test3/3", kls.new.url_for(controller: "index", action: "index3", param2: "3")
+          assert_equal "http://www.basecamphq.com/test3/3", kls.new.url_for(controller: "index", param1: "1", action: "index3", param2: "3")
+          assert_equal "http://www.basecamphq.com/2/test3/3", kls.new.url_for(controller: "index", param1: "2", action: "index3", param2: "3")
+
+          assert_equal "http://www.basecamphq.com/test4", kls.new.url_for(controller: "index", action: "index4")
+          assert_equal "http://www.basecamphq.com/test4/2", kls.new.url_for(controller: "index", param1: "2", action: "index4")
+          assert_equal "http://www.basecamphq.com/test4/2/3", kls.new.url_for(controller: "index", param1: "2", action: "index4", param2: "3")
+          # Doesn't make sense due to ambiguity, but is consistent with both rails4 and rails5 routing behavior:
+          assert_equal "http://www.basecamphq.com/test4/3", kls.new.url_for(controller: "index", action: "index4", param2: "3")
         end
       end
 