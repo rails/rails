84718df86097442f85999d6f2e6f6b8b59724c3f
Derek Prior <derekprior@gmail.com>
2018-04-19 15:41:01 -0400

    Update schema.rb documentation [CI SKIP]
    The documentation previously claimed that `db/schema.rb` was "the
    authoritative source for your database schema" while simultaneously
    also acknowledging that the file is generated. These two statements are
    incongruous and the guides accurately call out that many database
    constructs are unsupported by `schema.rb`. This change updates the
    comment at the top of `schema.rb` to remove the assertion that the file
    is authoritative.
    
    The documentation also previously referred vaguely to "issues" when
    re-running old migrations. This has been updated slightly to hint at the
    types of problems that one can encounter with old migrations.
    
    In sum, this change attempts to more accurately capture the pros, cons,
    and shortcomings of the two schema formats in the guides and in the
    comment at the top of `schema.rb`.
    
    [Derek Prior & Sean Griffin]
    Co-authored-by: Sean Griffin <sean@seantheprogrammer.com>

--- activerecord/lib/active_record/schema_dumper.rb
+++ activerecord/lib/active_record/schema_dumper.rb
@@ -71,11 +71,11 @@ module ActiveRecord
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
