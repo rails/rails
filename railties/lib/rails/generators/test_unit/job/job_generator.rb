8e2c91df4a8d4f336fe27ec98bd1dfd4934ac9fe
Yoshiyuki Hirano <yhirano@me.com>
2018-04-22 12:09:04 +0900

    Fix duplicated suffix for JobGenerator

--- railties/lib/rails/generators/test_unit/job/job_generator.rb
+++ railties/lib/rails/generators/test_unit/job/job_generator.rb
@@ -10,6 +10,11 @@ module TestUnit # :nodoc:
       def create_test_file
         template "unit_test.rb", File.join("test/jobs", class_path, "#{file_name}_job_test.rb")
       end
+
+      private
+        def file_name
+          @_file_name ||= super.gsub(/_job/i, "")
+        end
     end
   end
 end
