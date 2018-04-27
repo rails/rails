588150b53c15e96c597d1ab3aac9b78050e7af01
Rafael França <rafaelmfranca@gmail.com>
2018-04-27 16:20:16 -0400

    Merge pull request #32733 from Edouard-chin/ec-setupand-teardown
     `SetupAndTeardown` has few caveats that breaks libraries

--- activesupport/test/testing/after_teardown_test.rb
+++ activesupport/test/testing/after_teardown_test.rb
@@ -4,13 +4,14 @@ require "abstract_unit"
 
 module OtherAfterTeardown
   def after_teardown
+    super
+
     @witness = true
   end
 end
 
-class AfterTeardownTest < Minitest::Test
+class AfterTeardownTest < ActiveSupport::TestCase
   include OtherAfterTeardown
-  include ActiveSupport::Testing::SetupAndTeardown
 
   attr_writer :witness
 
@@ -21,11 +22,12 @@ class AfterTeardownTest < Minitest::Test
   end
 
   def after_teardown
-    assert_raises MyError do
+    assert_changes -> { failures.count }, from: 0, to: 1 do
       super
     end
 
     assert_equal true, @witness
+    failures.clear
   end
 
   def test_teardown_raise_but_all_after_teardown_method_are_called
