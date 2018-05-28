751d5edc70f67a6b29571a2005b700781ffec075
David Heinemeier Hansson <david@loudthinking.com>
2018-05-28 17:44:19 +0200

    Ease the rendering of a rich text attribute

--- app/models/action_text/rich_text.rb
+++ app/models/action_text/rich_text.rb
@@ -2,6 +2,7 @@ class ActionText::RichText < ActiveRecord::Base
   self.table_name = "action_text_rich_texts"
 
   serialize :body, ActionText::Content
+  delegate :to_s, to: :body
   
   belongs_to :record, polymorphic: true, touch: true
   has_many_attached :embeds
