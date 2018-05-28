3a515bb3083f5e594b38ee7c9b8e2e755e2c19ac
David Heinemeier Hansson <david@loudthinking.com>
2018-05-28 18:08:16 +0200

    Implicit transformation to html-safe string ready for display

--- app/models/action_text/rich_text.rb
+++ app/models/action_text/rich_text.rb
@@ -2,12 +2,16 @@ class ActionText::RichText < ActiveRecord::Base
   self.table_name = "action_text_rich_texts"
 
   serialize :body, ActionText::Content
-  delegate :to_s, to: :body
   
   belongs_to :record, polymorphic: true, touch: true
   has_many_attached :embeds
 
   after_save do
     self.embeds_blobs = body.attachments.map(&:attachable)
+  end
+
+
+  def to_s
+    body.to_s.html_safe
   end
 end
