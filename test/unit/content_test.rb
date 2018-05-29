ed42e6193050cac070f7d68033ab454b4622bedd
David Heinemeier Hansson <david@loudthinking.com>
2018-05-29 16:13:39 +0200

    Just test text pipeline without dependencies/saving

--- test/unit/content_test.rb
+++ test/unit/content_test.rb
@@ -3,8 +3,8 @@ require_relative '../test_helper'
 module ActionText
   class ContentTest < ActiveSupport::TestCase
     test "plain text conversion" do
-      message = Message.create!(subject: "Greetings", content: "<h1>Hello world</h1>")
-      assert_equal "Hello world", message.content.to_plain_text
+      message = Message.new(subject: "Greetings", content: "<h1>Hello world</h1>")
+      assert_equal "Hello world", message.content.body.to_plain_text
     end
   end
 end
