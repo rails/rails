4b6509c6f28edf40dbb4a284dd1d7f3fd583971f
David Heinemeier Hansson <david@loudthinking.com>
2018-05-29 16:13:19 +0200

    Adding the rich text tables

---
+++ test/dummy/db/migrate/2018052816_create_action_text_tables.rb
@@ -0,0 +1,14 @@
+class CreateActionTextTables < ActiveRecord::Migration[5.2]
+  def change
+    create_table :action_text_rich_texts do |t|
+      t.string     :name, null: false
+      t.text       :body, null: false
+      t.references :record, null: false, polymorphic: true, index: false
+
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
+
+      t.index [ :record_type, :record_id, :name ], name: "index_action_text_rich_texts_uniqueness", unique: true
+    end
+  end
+end
