d2a94561af5ad00f9a620e37aa60ecfd44a571ff
David Heinemeier Hansson <david@loudthinking.com>
2018-06-07 16:19:18 +0200

    Stop having asset pipeline compile default application.js

---
+++ railties/lib/rails/generators/rails/app/templates/app/javascript/packs/application.js
@@ -0,0 +1,4 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
