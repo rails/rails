221fce1bfec1c1d457a7a56b86ae29220599117c
David Heinemeier Hansson <david@loudthinking.com>
2018-06-07 18:04:15 +0200

    Add rails-ujs by default to the Webpack setup

--- railties/lib/rails/generators/rails/app/templates/app/javascript/packs/application.js
+++ railties/lib/rails/generators/rails/app/templates/app/javascript/packs/application.js
@@ -2,3 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+
+import Rails from 'rails-ujs'
+Rails.start()
