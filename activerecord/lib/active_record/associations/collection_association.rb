4caf1389681a9ab74872b75377ceeb82263fe3b6
Rafael Mendonça França <rafaelmfranca@gmail.com>
2018-06-11 13:06:58 -0400

    Use `-=` to change the update the collection on the association
    This also mark the association as loaded given we changed it in memory
    and avoid the next access to the reader to make a query to the databse.

--- activerecord/lib/active_record/associations/collection_association.rb
+++ activerecord/lib/active_record/associations/collection_association.rb
@@ -400,8 +400,7 @@ module ActiveRecord
           records.each { |record| callback(:before_remove, record) }
 
           delete_records(existing_records, method) if existing_records.any?
-          hashed_records = records.group_by { |record| record }
-          target.select! { |record| !hashed_records[record] }
+          self.target -= records
 
           records.each { |record| callback(:after_remove, record) }
         end
