49a1803cdfc4d6809b0414a46968ad8cde76dfd9
David Heinemeier Hansson <david@loudthinking.com>
2018-06-25 14:45:24 +0200

    Channel generator now targets Webpack style

--- railties/lib/rails/generators/rails/app/templates/app/javascript/channels/consumer.js
+++ railties/lib/rails/generators/rails/app/templates/app/javascript/channels/consumer.js
@@ -1,3 +1,6 @@
+// Action Cable provides the framework to deal with WebSockets in Rails.
+// You can generate new channels where WebSocket features live using the `rails generate channel` command.
+
 import ActionCable from "actioncable"
 
 export default ActionCable.createConsumer()
