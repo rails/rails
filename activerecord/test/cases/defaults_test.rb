b67f57d166da77c48d67ba0107c2369e6ef12433
Ryuta Kamizono <kamipo@gmail.com>
2018-07-09 04:06:06 +0900

    Merge pull request #33280 from nkondratyev/fix/mysql-time-default-values
    Fix default value for mysql time types with specified precision

--- activerecord/test/cases/defaults_test.rb
+++ activerecord/test/cases/defaults_test.rb
@@ -106,21 +106,31 @@ if current_adapter?(:Mysql2Adapter)
   class MysqlDefaultExpressionTest < ActiveRecord::TestCase
     include SchemaDumpingHelper
 
-    if ActiveRecord::Base.connection.version >= "5.6.0"
+    if subsecond_precision_supported?
       test "schema dump datetime includes default expression" do
         output = dump_table_schema("datetime_defaults")
-        assert_match %r/t\.datetime\s+"modified_datetime",\s+default: -> { "CURRENT_TIMESTAMP" }/, output
+        assert_match %r/t\.datetime\s+"modified_datetime",\s+default: -> { "CURRENT_TIMESTAMP(?:\(\))?" }/i, output
       end
-    end
 
-    test "schema dump timestamp includes default expression" do
-      output = dump_table_schema("timestamp_defaults")
-      assert_match %r/t\.timestamp\s+"modified_timestamp",\s+default: -> { "CURRENT_TIMESTAMP" }/, output
-    end
+      test "schema dump datetime includes precise default expression" do
+        output = dump_table_schema("datetime_defaults")
+        assert_match %r/t\.datetime\s+"precise_datetime",.+default: -> { "CURRENT_TIMESTAMP\(6\)" }/i, output
+      end
 
-    test "schema dump timestamp without default expression" do
-      output = dump_table_schema("timestamp_defaults")
-      assert_match %r/t\.timestamp\s+"nullable_timestamp"$/, output
+      test "schema dump timestamp includes default expression" do
+        output = dump_table_schema("timestamp_defaults")
+        assert_match %r/t\.timestamp\s+"modified_timestamp",\s+default: -> { "CURRENT_TIMESTAMP(?:\(\))?" }/i, output
+      end
+
+      test "schema dump timestamp includes precise default expression" do
+        output = dump_table_schema("timestamp_defaults")
+        assert_match %r/t\.timestamp\s+"precise_timestamp",.+default: -> { "CURRENT_TIMESTAMP\(6\)" }/i, output
+      end
+
+      test "schema dump timestamp without default expression" do
+        output = dump_table_schema("timestamp_defaults")
+        assert_match %r/t\.timestamp\s+"nullable_timestamp"$/, output
+      end
     end
   end
 
