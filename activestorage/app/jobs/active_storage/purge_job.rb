a1735a0dde6f6efa4cda6347da71a056740da1d3
George Claghorn <george@basecamp.com>
2018-07-11 22:04:42 -0400

    Remove overly-broad retry in ActiveStorage::PurgeJob
    We've never intentionally tripped this retry in production. Add retries for specific errors as needed.

--- activestorage/app/jobs/active_storage/purge_job.rb
+++ activestorage/app/jobs/active_storage/purge_job.rb
@@ -2,9 +2,6 @@
 
 # Provides asynchronous purging of ActiveStorage::Blob records via ActiveStorage::Blob#purge_later.
 class ActiveStorage::PurgeJob < ActiveStorage::BaseJob
-  # FIXME: Limit this to a custom ActiveStorage error
-  retry_on StandardError
-
   def perform(blob)
     blob.purge
   end
