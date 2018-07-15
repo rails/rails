3fb482132bdf075a5cb67f1de87f64520faf30f1
pavel <pavel.rosicky@easy.cz>
2018-07-15 12:17:43 +0200

    test_should_impose_childless_html_tags_in_html failure with JRuby

--- actionpack/test/controller/test_case_test.rb
+++ actionpack/test/controller/test_case_test.rb
@@ -401,7 +401,13 @@ XML
     process :test_xml_output, params: { response_as: "text/html" }
 
     # <area> auto-closes, so the <p> becomes a sibling
-    assert_select "root > area + p"
+    if defined?(JRUBY_VERSION)
+      # https://github.com/sparklemotion/nokogiri/issues/1653
+      # HTML parser "fixes" "broken" markup in slightly different ways
+      assert_select "root > map > area + p"
+    else
+      assert_select "root > area + p"
+    end
   end
 
   def test_should_not_impose_childless_html_tags_in_xml
