42fec4b8de8c40d7778f936e200081c0dded1ed0
Eileen Uchitelle <eileencodes@gmail.com>
2018-07-26 12:55:59 -0400

    Add cpu_time, idle_time, and allocations to Event
    * Use process clock instead of Time.now
    
    This fixes any issues with the system clock changing and also eliminates
    2 object allocations per event.
    
    * Add start! and finish! methods to the event object so we can record
    more information
    
    * Adds cpu time, idle time, and allocation count for a particular event.
    
    Co-authored-by: Aaron Patterson <aaron.patterson@gmail.com>

--- activesupport/lib/active_support/notifications/instrumenter.rb
+++ activesupport/lib/active_support/notifications/instrumenter.rb
@@ -63,6 +63,34 @@ module ActiveSupport
         @end            = ending
         @children       = []
         @duration       = nil
+        @cpu_time_start = nil
+        @cpu_time_finish = nil
+        @allocation_count_start = 0
+        @allocation_count_finish = 0
+      end
+
+      def start!
+        @time = now
+        @cpu_time_start = now_cpu
+        @allocation_count_start = now_allocations
+      end
+
+      def finish!
+        @end = now
+        @cpu_time_finish = now_cpu
+        @allocation_count_finish = now_allocations
+      end
+
+      def cpu_time
+        @cpu_time_finish - @cpu_time_start
+      end
+
+      def idle_time
+        duration - cpu_time
+      end
+
+      def allocations
+        @allocation_count_finish - @allocation_count_start
       end
 
       # Returns the difference in milliseconds between when the execution of the
@@ -88,6 +116,25 @@ module ActiveSupport
       def parent_of?(event)
         @children.include? event
       end
+
+      private
+        def now
+          Process.clock_gettime(Process::CLOCK_MONOTONIC)
+        end
+
+        def now_cpu
+          Process.clock_gettime(Process::CLOCK_PROCESS_CPUTIME_ID)
+        end
+
+        if defined?(JRUBY_VERSION)
+          def now_allocations
+            0
+          end
+        else
+          def now_allocations
+            GC.stat :total_allocated_objects
+          end
+        end
     end
   end
 end
