9666e5c3c486a7172379f95a8772c584a16b6ff2
Piers Chambers <piers@varyonic.com>
2018-07-27 10:50:10 -0400

    npm install karma qunitjs mock-socket --save-dev karma.conf for Rollup/RequireJS plus boilerplate test page to load and run unit tests.

--- actioncable/test/javascript/src/test.js
+++ actioncable/test/javascript/src/test.js
@@ -1,3 +1,27 @@
-//= require action_cable
-//= require ./test_helpers
-//= require_tree ./unit
+var allTestFiles = []
+var TEST_REGEXP = /(spec|test)\.js$/i
+
+// Get a list of all the test files to include
+Object.keys(window.__karma__.files).forEach(function (file) {
+  if (TEST_REGEXP.test(file)) {
+    // Normalize paths to RequireJS module names.
+    // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
+    // then do not normalize the paths
+    var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '')
+    allTestFiles.push(normalizedTestModule)
+  }
+})
+
+requirejs.config({
+  // Karma serves files under /base, which is the basePath from your config file
+  baseUrl: '/base',
+
+  // paths: { 'nodemodule': 'node_modules' } ??? not recommended
+
+  // dynamically load all test files
+  deps: allTestFiles,
+
+  // we have to kickoff jasmine, as it is asynchronous
+  callback: window.__karma__.start
+})
+
