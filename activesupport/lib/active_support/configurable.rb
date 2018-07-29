14931e537f3dcd522b68a5b640aa47272602a137
Ryuta Kamizono <kamipo@gmail.com>
2018-07-29 14:51:00 +0900

    Remove unused `require "active_support/core_ext/regexp"`
    Ruby 2.4 has native `Regexp#match?`.
    
    https://ruby-doc.org/core-2.4.0/Regexp.html#method-i-match-3F
    
    Related #32034.

--- activesupport/lib/active_support/configurable.rb
+++ activesupport/lib/active_support/configurable.rb
@@ -3,7 +3,6 @@
 require "active_support/concern"
 require "active_support/ordered_options"
 require "active_support/core_ext/array/extract_options"
-require "active_support/core_ext/regexp"
 
 module ActiveSupport
   # Configurable provides a <tt>config</tt> method to store and retrieve
