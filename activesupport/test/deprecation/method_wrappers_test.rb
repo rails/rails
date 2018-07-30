rails/rails

commit a982a42d766169c2170d7f100c2a5ceb5430efb1

Deprecate alias_method_chain in favour of Module#prepend …as discussed #19413

activesupport/lib/active_support/deprecation/method_wrappers.rb
@@ -31,12 +31,14 @@ module ActiveSupport
         method_names += options.keys
 
         method_names.each do |method_name|
-          target_module.alias_method_chain(method_name, :deprecation) do |target, punctuation|
-            target_module.send(:define_method, "#{target}_with_deprecation#{punctuation}") do |*args, &block|
+          mod = Module.new do
+            define_method(method_name) do |*args, &block|
               deprecator.deprecation_warning(method_name, options[method_name])
-              send(:"#{target}_without_deprecation#{punctuation}", *args, &block)
+              super(*args, &block)
             end
           end
+
+          target_module.prepend(mod)
         end
       end
     end

activesupport/lib/active_support/core_ext/module/aliasing.rb
@@ -21,6 +21,8 @@ class Module
   #
   # so you can safely chain foo, foo?, foo! and/or foo= with the same feature.
   def alias_method_chain(target, feature)
+    ActiveSupport::Deprecation.warn("alias_method_chain is deprecated. Please, use Module#prepend instead. From module, you can access the original method using super.")
+
     # Strip out punctuation on predicates, bang or writer methods since
     # e.g. target?_without_feature is not a valid method name.
     aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1

activesupport/lib/active_support/core_ext/range/each.rb
@@ -1,18 +1,22 @@
-require 'active_support/core_ext/module/aliasing'
-
 class Range #:nodoc:
 
   def each_with_time_with_zone(&block)
     ensure_iteration_allowed
     each_without_time_with_zone(&block)
   end
-  alias_method_chain :each, :time_with_zone
+  # TODO: change to Module#prepend as soon as the fix is backported to MRI 2.2:
+  # https://bugs.ruby-lang.org/issues/10847
+  alias_method :each_without_time_with_zone, :each
+  alias_method :each, :each_with_time_with_zone
 
   def step_with_time_with_zone(n = 1, &block)
     ensure_iteration_allowed
     step_without_time_with_zone(n, &block)
   end
-  alias_method_chain :step, :time_with_zone
+  # TODO: change to Module#prepend as soon as the fix is backported to MRI 2.2:
+  # https://bugs.ruby-lang.org/issues/10847
+  alias_method :step_without_time_with_zone, :step
+  alias_method :step, :step_with_time_with_zone
 
   private
   def ensure_iteration_allowed

activesupport/lib/active_support/core_ext/range/include_range.rb
@@ -1,5 +1,3 @@
-require 'active_support/core_ext/module/aliasing'
-
 class Range
   # Extends the default Range#include? to support range comparisons.
   #  (1..5).include?(1..5) # => true
@@ -18,6 +16,8 @@ class Range
       include_without_range?(value)
     end
   end
-
-  alias_method_chain :include?, :range
+  # TODO: change to Module#prepend as soon as the fix is backported to MRI 2.2:
+  # https://bugs.ruby-lang.org/issues/10847
+  alias_method :include_without_range?, :include?
+  alias_method :include?, :include_with_range?
 end

activesupport/test/core_ext/module_test.rb
@@ -358,148 +358,178 @@ class MethodAliasingTest < ActiveSupport::TestCase
     Object.instance_eval { remove_const :FooClassWithBarMethod }
   end
 
+  def test_alias_method_chain_deprecated
+    assert_deprecated(/alias_method_chain/) do
+      Module.new do
+        def base
+        end
+
+        def base_with_deprecated
+        end
+
+        alias_method_chain :base, :deprecated
+      end
+    end
+  end
+
   def test_alias_method_chain
-    assert @instance.respond_to?(:bar)
-    feature_aliases = [:bar_with_baz, :bar_without_baz]
+    assert_deprecated(/alias_method_chain/) do
+      assert @instance.respond_to?(:bar)
+      feature_aliases = [:bar_with_baz, :bar_without_baz]
 
-    feature_aliases.each do |method|
-      assert !@instance.respond_to?(method)
+      feature_aliases.each do |method|
+        assert !@instance.respond_to?(method)
+      end
+
+      assert_equal 'bar', @instance.bar
+
+      FooClassWithBarMethod.class_eval { include BarMethodAliaser }
+
+      feature_aliases.each do |method|
+        assert_respond_to @instance, method
+      end
+
+      assert_equal 'bar_with_baz', @instance.bar
+      assert_equal 'bar', @instance.bar_without_baz
     end
-
-    assert_equal 'bar', @instance.bar
-
-    FooClassWithBarMethod.class_eval { include BarMethodAliaser }
-
-    feature_aliases.each do |method|
-      assert_respond_to @instance, method
-    end
-
-    assert_equal 'bar_with_baz', @instance.bar
-    assert_equal 'bar', @instance.bar_without_baz
   end
 
   def test_alias_method_chain_with_punctuation_method
-    FooClassWithBarMethod.class_eval do
-      def quux!; 'quux' end
-    end
+    assert_deprecated(/alias_method_chain/) do
+      FooClassWithBarMethod.class_eval do
+        def quux!; 'quux' end
+      end
 
-    assert !@instance.respond_to?(:quux_with_baz!)
-    FooClassWithBarMethod.class_eval do
-      include BarMethodAliaser
-      alias_method_chain :quux!, :baz
-    end
-    assert_respond_to @instance, :quux_with_baz!
+      assert !@instance.respond_to?(:quux_with_baz!)
+      FooClassWithBarMethod.class_eval do
+        include BarMethodAliaser
+        alias_method_chain :quux!, :baz
+      end
+      assert_respond_to @instance, :quux_with_baz!
 
-    assert_equal 'quux_with_baz', @instance.quux!
-    assert_equal 'quux', @instance.quux_without_baz!
+      assert_equal 'quux_with_baz', @instance.quux!
+      assert_equal 'quux', @instance.quux_without_baz!
+    end
   end
 
   def test_alias_method_chain_with_same_names_between_predicates_and_bang_methods
-    FooClassWithBarMethod.class_eval do
-      def quux!; 'quux!' end
-      def quux?; true end
-      def quux=(v); 'quux=' end
+    assert_deprecated(/alias_method_chain/) do
+      FooClassWithBarMethod.class_eval do
+        def quux!; 'quux!' end
+        def quux?; true end
+        def quux=(v); 'quux=' end
+      end
+
+      assert !@instance.respond_to?(:quux_with_baz!)
+      assert !@instance.respond_to?(:quux_with_baz?)
+      assert !@instance.respond_to?(:quux_with_baz=)
+
+      FooClassWithBarMethod.class_eval { include BarMethodAliaser }
+      assert_respond_to @instance, :quux_with_baz!
+      assert_respond_to @instance, :quux_with_baz?
+      assert_respond_to @instance, :quux_with_baz=
+
+
+      FooClassWithBarMethod.alias_method_chain :quux!, :baz
+      assert_equal 'quux!_with_baz', @instance.quux!
+      assert_equal 'quux!', @instance.quux_without_baz!
+
+      FooClassWithBarMethod.alias_method_chain :quux?, :baz
+      assert_equal false, @instance.quux?
+      assert_equal true,  @instance.quux_without_baz?
+
+      FooClassWithBarMethod.alias_method_chain :quux=, :baz
+      assert_equal 'quux=_with_baz', @instance.send(:quux=, 1234)
+      assert_equal 'quux=', @instance.send(:quux_without_baz=, 1234)
     end
-
-    assert !@instance.respond_to?(:quux_with_baz!)
-    assert !@instance.respond_to?(:quux_with_baz?)
-    assert !@instance.respond_to?(:quux_with_baz=)
-
-    FooClassWithBarMethod.class_eval { include BarMethodAliaser }
-    assert_respond_to @instance, :quux_with_baz!
-    assert_respond_to @instance, :quux_with_baz?
-    assert_respond_to @instance, :quux_with_baz=
-
-
-    FooClassWithBarMethod.alias_method_chain :quux!, :baz
-    assert_equal 'quux!_with_baz', @instance.quux!
-    assert_equal 'quux!', @instance.quux_without_baz!
-
-    FooClassWithBarMethod.alias_method_chain :quux?, :baz
-    assert_equal false, @instance.quux?
-    assert_equal true,  @instance.quux_without_baz?
-
-    FooClassWithBarMethod.alias_method_chain :quux=, :baz
-    assert_equal 'quux=_with_baz', @instance.send(:quux=, 1234)
-    assert_equal 'quux=', @instance.send(:quux_without_baz=, 1234)
   end
 
   def test_alias_method_chain_with_feature_punctuation
-    FooClassWithBarMethod.class_eval do
-      def quux; 'quux' end
-      def quux?; 'quux?' end
-      include BarMethodAliaser
-      alias_method_chain :quux, :baz!
-    end
+    assert_deprecated(/alias_method_chain/) do
+      FooClassWithBarMethod.class_eval do
+        def quux; 'quux' end
+        def quux?; 'quux?' end
+        include BarMethodAliaser
+        alias_method_chain :quux, :baz!
+      end
 
-    assert_nothing_raised do
-      assert_equal 'quux_with_baz', @instance.quux_with_baz!
-    end
+      assert_nothing_raised do
+        assert_equal 'quux_with_baz', @instance.quux_with_baz!
+      end
 
-    assert_raise(NameError) do
-      FooClassWithBarMethod.alias_method_chain :quux?, :baz!
+      assert_raise(NameError) do
+        FooClassWithBarMethod.alias_method_chain :quux?, :baz!
+      end
     end
   end
 
   def test_alias_method_chain_yields_target_and_punctuation
-    args = nil
+    assert_deprecated(/alias_method_chain/) do
+      args = nil
 
-    FooClassWithBarMethod.class_eval do
-      def quux?; end
-      include BarMethods
+      FooClassWithBarMethod.class_eval do
+        def quux?; end
+        include BarMethods
 
-      FooClassWithBarMethod.alias_method_chain :quux?, :baz do |target, punctuation|
-        args = [target, punctuation]
+        FooClassWithBarMethod.alias_method_chain :quux?, :baz do |target, punctuation|
+          args = [target, punctuation]
+        end
       end
-    end
 
-    assert_not_nil args
-    assert_equal 'quux', args[0]
-    assert_equal '?', args[1]
+      assert_not_nil args
+      assert_equal 'quux', args[0]
+      assert_equal '?', args[1]
+    end
   end
 
   def test_alias_method_chain_preserves_private_method_status
-    FooClassWithBarMethod.class_eval do
-      def duck; 'duck' end
-      include BarMethodAliaser
-      private :duck
-      alias_method_chain :duck, :orange
-    end
+    assert_deprecated(/alias_method_chain/) do
+      FooClassWithBarMethod.class_eval do
+        def duck; 'duck' end
+        include BarMethodAliaser
+        private :duck
+        alias_method_chain :duck, :orange
+      end
 
-    assert_raise NoMethodError do
-      @instance.duck
-    end
+      assert_raise NoMethodError do
+        @instance.duck
+      end
 
-    assert_equal 'duck_with_orange', @instance.instance_eval { duck }
-    assert FooClassWithBarMethod.private_method_defined?(:duck)
+      assert_equal 'duck_with_orange', @instance.instance_eval { duck }
+      assert FooClassWithBarMethod.private_method_defined?(:duck)
+    end
   end
 
   def test_alias_method_chain_preserves_protected_method_status
-    FooClassWithBarMethod.class_eval do
-      def duck; 'duck' end
-      include BarMethodAliaser
-      protected :duck
-      alias_method_chain :duck, :orange
-    end
+    assert_deprecated(/alias_method_chain/) do
+      FooClassWithBarMethod.class_eval do
+        def duck; 'duck' end
+        include BarMethodAliaser
+        protected :duck
+        alias_method_chain :duck, :orange
+      end
 
-    assert_raise NoMethodError do
-      @instance.duck
-    end
+      assert_raise NoMethodError do
+        @instance.duck
+      end
 
-    assert_equal 'duck_with_orange', @instance.instance_eval { duck }
-    assert FooClassWithBarMethod.protected_method_defined?(:duck)
+      assert_equal 'duck_with_orange', @instance.instance_eval { duck }
+      assert FooClassWithBarMethod.protected_method_defined?(:duck)
+    end
   end
 
   def test_alias_method_chain_preserves_public_method_status
-    FooClassWithBarMethod.class_eval do
-      def duck; 'duck' end
-      include BarMethodAliaser
-      public :duck
-      alias_method_chain :duck, :orange
-    end
+    assert_deprecated(/alias_method_chain/) do
+      FooClassWithBarMethod.class_eval do
+        def duck; 'duck' end
+        include BarMethodAliaser
+        public :duck
+        alias_method_chain :duck, :orange
+      end
 
-    assert_equal 'duck_with_orange', @instance.duck
-    assert FooClassWithBarMethod.public_method_defined?(:duck)
+      assert_equal 'duck_with_orange', @instance.duck
+      assert FooClassWithBarMethod.public_method_defined?(:duck)
+    end
   end
 
   def test_delegate_with_case

commit 015477ae9109d4105d5a5057d82338f198febb74

A regression in deprecate_methods was introduced in a982a42:
- Refactoring alias_chain to Module#prepend broke the possibility to deprecate class methods since the module
  generated was prepended to the target's instance.

  A suggestion to fix this was  to use `AS#redefine_method` which would solve the
  problem but with the cost of redefining directly the method.
  Decided to go with the same alias_chain implementation as before instead.

- Fixes #33253

activesupport/lib/active_support/deprecation/method_wrappers.rb
@@ -54,23 +54,26 @@ module ActiveSupport
         deprecator = options.delete(:deprecator) || self
         method_names += options.keys
 
-        mod = Module.new do
-          method_names.each do |method_name|
-            define_method(method_name) do |*args, &block|
-              deprecator.deprecation_warning(method_name, options[method_name])
-              super(*args, &block)
-            end
+        method_names.each do |method_name|
+          aliased_method, punctuation = method_name.to_s.sub(/([?!=])$/, ""), $1
+          with_method = "#{aliased_method}_with_deprecation#{punctuation}"
+          without_method = "#{aliased_method}_without_deprecation#{punctuation}"
 
-            case
-            when target_module.protected_method_defined?(method_name)
-              protected method_name
-            when target_module.private_method_defined?(method_name)
-              private method_name
-            end
+          target_module.send(:define_method, with_method) do |*args, &block|
+            deprecator.deprecation_warning(method_name, options[method_name])
+            send(without_method, *args, &block)
+          end
+
+          target_module.send(:alias_method, without_method, method_name)
+          target_module.send(:alias_method, method_name, with_method)
+
+          case
+          when target_module.protected_method_defined?(without_method)
+            target_module.send(:protected, method_name)
+          when target_module.private_method_defined?(without_method)
+            target_module.send(:private, method_name)
           end
         end
-
-        target_module.prepend(mod)
       end
     end
   end

activesupport/test/deprecation/method_wrappers_test.rb
@@ -55,4 +55,39 @@ class MethodWrappersTest < ActiveSupport::TestCase
 
     assert(@klass.private_method_defined?(:old_private_method))
   end
+
+  def test_deprecate_class_method
+    mod = Module.new do
+      extend self
+
+      def old_method
+        "abc"
+      end
+    end
+    ActiveSupport::Deprecation.deprecate_methods(mod, old_method: :new_method)
+
+    warning = /old_method is deprecated and will be removed from Rails \d.\d \(use new_method instead\)/
+    assert_deprecated(warning) { assert_equal "abc", mod.old_method }
+  end
+
+  def test_deprecate_method_when_class_extends_module
+    mod = Module.new do
+      def old_method
+        "abc"
+      end
+    end
+    @klass.extend mod
+    ActiveSupport::Deprecation.deprecate_methods(mod, old_method: :new_method)
+
+    warning = /old_method is deprecated and will be removed from Rails \d.\d \(use new_method instead\)/
+    assert_deprecated(warning) { assert_equal "abc", @klass.old_method }
+  end
+
+  def test_method_with_without_deprecation_is_exposed
+    ActiveSupport::Deprecation.deprecate_methods(@klass, old_method: :new_method)
+
+    warning = /old_method is deprecated and will be removed from Rails \d.\d \(use new_method instead\)/
+    assert_deprecated(warning) { assert_equal "abc", @klass.new.old_method_with_deprecation }
+    assert_equal "abc", @klass.new.old_method_without_deprecation
+  end
 end