fee7fba8934ee6005b7e8eefc4baf7db48f03934
George Claghorn <george@basecamp.com>
2018-07-31 19:29:23 -0400

    Retry ActiveStorage::PurgeJobs on DB deadlock

--- activestorage/app/jobs/active_storage/purge_job.rb
+++ activestorage/app/jobs/active_storage/purge_job.rb
@@ -3,6 +3,7 @@
 # Provides asynchronous purging of ActiveStorage::Blob records via ActiveStorage::Blob#purge_later.
 class ActiveStorage::PurgeJob < ActiveStorage::BaseJob
   discard_on ActiveRecord::RecordNotFound
+  retry_on ActiveRecord::Deadlocked, attempts: 10, wait: :exponentially_longer
 
   def perform(blob)
     blob.purge
