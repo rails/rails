3082786be616173f28488e89b7d8b9bfc5cd0f97
Joel Taylor <joel@glassbreakers.co>
2018-08-06 18:17:49 -0700

    Improve ActiveStorage service adapter error handling

--- activestorage/test/service/configurator_test.rb
+++ activestorage/test/service/configurator_test.rb
@@ -9,6 +9,12 @@ class ActiveStorage::Service::ConfiguratorTest < ActiveSupport::TestCase
     assert_equal "path", service.root
   end
 
+  test "builds correct service instance based on lowercase service name" do
+    service = ActiveStorage::Service::Configurator.build(:foo, foo: { service: "disk", root: "path" })
+    assert_instance_of ActiveStorage::Service::DiskService, service
+    assert_equal "path", service.root
+  end
+
   test "raises error when passing non-existent service name" do
     assert_raise RuntimeError do
       ActiveStorage::Service::Configurator.build(:bigfoot, {})
