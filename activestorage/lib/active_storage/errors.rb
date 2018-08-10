2d3a28b949f2e8a0b755afdb52cbcd55e5ac0d98
George Claghorn <george.claghorn@gmail.com>
2018-08-10 18:37:46 -0400

    Document all Active Storage error classes [ci skip]

--- activestorage/lib/active_storage/errors.rb
+++ activestorage/lib/active_storage/errors.rb
@@ -4,8 +4,16 @@ module ActiveStorage
   # Generic base class for all Active Storage exceptions.
   class Error < StandardError; end
 
+  # Raised when ActiveStorage::Blob#variant is called on a blob that isn't variable.
+  # Use ActiveStorage::Blob#variable? to determine whether a blob is variable.
   class InvariableError < Error; end
+
+  # Raised when ActiveStorage::Blob#preview is called on a blob that isn't previewable.
+  # Use ActiveStorage::Blob#previewable? to determine whether a blob is previewable.
   class UnpreviewableError < Error; end
+
+  # Raised when ActiveStorage::Blob#representation is called on a blob that isn't representable.
+  # Use ActiveStorage::Blob#representable? to determine whether a blob is representable.
   class UnrepresentableError < Error; end
 
   # Raised when uploaded or downloaded data does not match a precomputed checksum.
