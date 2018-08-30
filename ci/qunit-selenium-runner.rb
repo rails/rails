aed2c1fde6adfe4179f501ae8287b88e3ba3acf2
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-08-30 11:47:46 +0900

    Use Selenium/Chrome headless for UJS tests
    It seems UJS tests does not pass with phantoms.
    https://travis-ci.org/rails/rails/jobs/422256491
    
    This manually cherry-picked 4dcf12a340f82fb01453ef3b7aca6udf756bdaa65.

---
+++ ci/qunit-selenium-runner.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "qunit/selenium/test_runner"
+require "chromedriver/helper"
+
+driver_options = Selenium::WebDriver::Chrome::Options.new
+driver_options.add_argument("--headless")
+driver_options.add_argument("--disable-gpu")
+driver_options.add_argument("--no-sandbox")
+
+driver = ::Selenium::WebDriver.for(:chrome, options: driver_options)
+result = QUnit::Selenium::TestRunner.new(driver).open(ARGV[0], timeout: 60)
+driver.quit
+
+puts "Time: #{result.duration} seconds, Total: #{result.assertions[:total]}, Passed: #{result.assertions[:passed]}, Failed: #{result.assertions[:failed]}"
+exit(result.tests[:failed] > 0 ? 1 : 0)
