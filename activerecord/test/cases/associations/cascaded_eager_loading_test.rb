4429540995b061cf120b6468ce76cbb44fcaba9c
Matt Jones <al2o3cr@gmail.com>
2018-08-30 16:53:16 -0400

    failing test for eager loading
    cherry-picked from https://github.com/rails/rails/pull/33763

--- activerecord/test/cases/associations/cascaded_eager_loading_test.rb
+++ activerecord/test/cases/associations/cascaded_eager_loading_test.rb
@@ -165,6 +165,11 @@ class CascadedEagerLoadingTest < ActiveRecord::TestCase
     assert_no_queries { assert_nil post.author }
   end
 
+  def test_eager_association_loading_with_missing_first_record
+    posts = Post.where(id: 3).preload(author: { comments: :post }).to_a
+    assert_equal posts.size, 1
+  end
+
   def test_eager_association_loading_with_recursive_cascading_four_levels_has_many_through
     source = Vertex.all.merge!(includes: { sinks: { sinks: { sinks: :sinks } } }, order: "vertices.id").first
     assert_equal vertices(:vertex_4), assert_no_queries { source.sinks.first.sinks.first.sinks.first }
