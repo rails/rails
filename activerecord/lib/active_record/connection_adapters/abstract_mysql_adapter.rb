70043da86a3510063c668061bd84d1661188d3e8
Yasuo Honda <yasuo.honda@gmail.com>
2018-09-12 23:16:43 +0000

    Raise an exception if :charset is not specified and large prefixes / utf8mb4 are not supported

--- activerecord/lib/active_record/connection_adapters/abstract_mysql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/abstract_mysql_adapter.rb
@@ -263,7 +263,7 @@ module ActiveRecord
         elsif supports_longer_index_key_prefix?
           execute "CREATE DATABASE #{quote_table_name(name)} DEFAULT CHARACTER SET `utf8mb4`"
         else
-          execute "CREATE DATABASE #{quote_table_name(name)} DEFAULT CHARACTER SET `utf8`"
+          raise "Configure a supported :charset and ensure innodb_large_prefix is enabled to support indexes on varchar(255) string columns."
         end
       end
 
