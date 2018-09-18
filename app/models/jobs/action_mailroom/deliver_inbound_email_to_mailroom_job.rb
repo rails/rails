12deb313e02680bbd1c07ada79a890d6734d1bf3
David Heinemeier Hansson <david@loudthinking.com>
2018-09-17 17:16:51 -0700

    Wait on this for later

--- app/models/jobs/action_mailroom/deliver_inbound_email_to_mailroom_job.rb
+++ app/models/jobs/action_mailroom/deliver_inbound_email_to_mailroom_job.rb
@@ -1,9 +1,6 @@
 class ActionMailroom::DeliverInboundEmailToMailroomJob < ApplicationJob
   queue_as :action_mailroom_inbound_email
 
-  # Occasional `SSL_read: decryption failed or bad record mac` that resolve on retry
-  retry_on OpenSSL::SSL::SSLError
-
   def perform(inbound_email)
     ActionMailroom::Router.receive inbound_email
   end
