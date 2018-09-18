a4c0c022d3e07fea5b573bf1b90851e79ad87ac9
David Heinemeier Hansson <david@loudthinking.com>
2018-09-17 17:16:06 -0700

    TOC ordering

--- app/models/action_mailroom/inbound_email.rb
+++ app/models/action_mailroom/inbound_email.rb
@@ -1,10 +1,12 @@
 class ActionMailroom::InboundEmail < ActiveRecord::Base
   self.table_name = "action_mailroom_inbound_emails"
 
-  after_create_commit :deliver_to_mailroom_later
   has_one_attached :raw_message
 
   enum status: %i[ pending processing delivered failed bounced ]
+
+  after_create_commit :deliver_to_mailroom_later
+
 
   def mail
     @mail ||= Mail.new(Mail::Utilities.binary_unsafe_to_crlf(raw_message.download))
