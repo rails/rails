0e00529e04d7c870ffd88b00a6e0357d2787efed
David Heinemeier Hansson <david@loudthinking.com>
2018-09-18 17:21:18 -0700

    Prefix queue name like we do routes

--- app/models/jobs/action_mailroom/deliver_inbound_email_to_mailroom_job.rb
+++ app/models/jobs/action_mailroom/deliver_inbound_email_to_mailroom_job.rb
@@ -1,5 +1,5 @@
 class ActionMailroom::DeliverInboundEmailToMailroomJob < ApplicationJob
-  queue_as :action_mailroom_inbound_email
+  queue_as :rails_action_mailroom_inbound_email
 
   def perform(inbound_email)
     ActionMailroom::Router.receive inbound_email
