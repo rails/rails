f08c37e6bf2af69d6308d2b8213d98f7b205b2a9
David Heinemeier Hansson <david@loudthinking.com>
2018-09-19 16:43:53 -0700

    Anemic test better covered in mailbox/routing_test.rb

--- test/unit/inbound_email/routing_test.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../../test_helper'
-
-class ActionMailroom::InboundEmail::RoutingTest < ActiveSupport::TestCase
-  test "pending emails are delivered to the mailroom" do
-    assert_enqueued_jobs 1, only: ActionMailroom::RoutingJob do
-      create_inbound_email("welcome.eml", status: :pending)
-    end
-  end
-end
