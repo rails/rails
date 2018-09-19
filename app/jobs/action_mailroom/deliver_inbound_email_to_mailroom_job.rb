af360244d2ceaa67db421c3beaf7ca47ea69e316
David Heinemeier Hansson <david@loudthinking.com>
2018-09-19 16:22:53 -0700

    Temp routing directly to named controller
    Need the singleton to live somewhere proper.

--- app/jobs/action_mailroom/deliver_inbound_email_to_mailroom_job.rb
+++ app/jobs/action_mailroom/deliver_inbound_email_to_mailroom_job.rb
@@ -2,6 +2,6 @@ class ActionMailroom::DeliverInboundEmailToMailroomJob < ActiveJob::Base
   queue_as :action_mailroom_inbound_email
 
   def perform(inbound_email)
-    ActionMailroom::Mailbox.route inbound_email
+    ApplicationMailbox.route inbound_email
   end
 end
