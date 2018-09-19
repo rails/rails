c6cae759ef0cb9063dc26015132804a6218281b7
David Heinemeier Hansson <david@loudthinking.com>
2018-09-19 15:15:19 -0700

    Follow db table convention

--- app/jobs/action_mailroom/deliver_inbound_email_to_mailroom_job.rb
+++ app/jobs/action_mailroom/deliver_inbound_email_to_mailroom_job.rb
@@ -1,6 +1,6 @@
 module ActionMailroom
   class DeliverInboundEmailToMailroomJob < ActiveJob::Base
-    queue_as :rails_action_mailroom_inbound_email
+    queue_as :action_mailroom_inbound_email
 
     def perform(inbound_email)
       # ActionMailroom::Router.receive inbound_email
