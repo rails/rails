d5b57c890e2d0d7e42d726fb5a16a0ab30da67ea
Graham Turner <turnertgraham@gmail.com>
2018-09-27 14:58:29 -0400

    Array with single item correctly uses cache_key

--- activesupport/lib/active_support/cache.rb
+++ activesupport/lib/active_support/cache.rb
@@ -647,7 +647,7 @@ module ActiveSupport
             if key.size > 1
               key = key.collect { |element| expanded_key(element) }
             else
-              key = key.first
+              key = expanded_key(key.first)
             end
           when Hash
             key = key.sort_by { |k, _| k.to_s }.collect { |k, v| "#{k}=#{v}" }
