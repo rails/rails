4f5fe2b5784f21eaccd3f8a3a1c95c50072de31a
David Heinemeier Hansson <david@loudthinking.com>
2018-10-02 16:10:25 -0700

    Add import statement to default pack file if relevant

--- lib/templates/installer.rb
+++ lib/templates/installer.rb
@@ -7,3 +7,12 @@ copy_file "#{__dir__}/fixtures.yml", "test/fixtures/action_text/rich_texts.yml"
 # FIXME: Replace with release version on release
 say "Installing JavaScript dependency"
 run "yarn add https://github.com/basecamp/actiontext"
+
+APPLICATION_PATH_PATH = "app/javascript/packs/application.js"
+
+if File.exists?(APPLICATION_PATH_PATH) && File.read(APPLICATION_PATH_PATH) !~ /import "actiontext"/
+  say "Adding import to default JavaScript pack"
+  append_to_file APPLICATION_PATH_PATH, <<-EOS
+import "actiontext"
+EOS
+end
