9fb3dd7551a2bdb8acedf3ceb056071070b6fd66
Javan Makhmali <javan@javan.us>
2018-10-02 19:36:01 -0400

    Uncomment failing tests

--- test/unit/attachment_test.rb
+++ test/unit/attachment_test.rb
@@ -18,33 +18,33 @@ class ActionText::AttachmentTest < ActiveSupport::TestCase
     assert_equal attachable.to_param, attachment.to_param
   end
 
-  # test "converts to TrixAttachment" do
-  #   attachment = attachment_from_html(%Q(<action-text-attachment sgid="#{attachable.attachable_sgid}" caption="Captioned"></action-text-attachment>))
-  #
-  #   trix_attachment = attachment.to_trix_attachment
-  #   assert_kind_of ActionText::TrixAttachment, trix_attachment
-  #
-  #   assert_equal attachable.attachable_sgid, trix_attachment.attributes["sgid"]
-  #   assert_equal attachable.attachable_content_type, trix_attachment.attributes["contentType"]
-  #   assert_equal attachable.filename.to_s, trix_attachment.attributes["filename"]
-  #   assert_equal attachable.byte_size, trix_attachment.attributes["filesize"]
-  #   assert_equal "Captioned", trix_attachment.attributes["caption"]
-  #
-  #   assert_nil trix_attachment.attributes["content"]
-  # end
+  test "converts to TrixAttachment" do
+    attachment = attachment_from_html(%Q(<action-text-attachment sgid="#{attachable.attachable_sgid}" caption="Captioned"></action-text-attachment>))
 
-  # test "converts to TrixAttachment with content" do
-  #   attachment = attachment_from_html(%Q(<action-text-attachment sgid="#{attachable.attachable_sgid}"></action-text-attachment>))
-  #
-  #   trix_attachment = attachment.to_trix_attachment
-  #   assert_kind_of ActionText::TrixAttachment, trix_attachment
-  #
-  #   assert_equal attachable.attachable_sgid, trix_attachment.attributes["sgid"]
-  #   assert_equal attachable.attachable_content_type, trix_attachment.attributes["contentType"]
-  #
-  #   assert_not_nil attachable.to_trix_content_attachment_partial_path
-  #   assert_not_nil trix_attachment.attributes["content"]
-  # end
+    trix_attachment = attachment.to_trix_attachment
+    assert_kind_of ActionText::TrixAttachment, trix_attachment
+
+    assert_equal attachable.attachable_sgid, trix_attachment.attributes["sgid"]
+    assert_equal attachable.attachable_content_type, trix_attachment.attributes["contentType"]
+    assert_equal attachable.filename.to_s, trix_attachment.attributes["filename"]
+    assert_equal attachable.byte_size, trix_attachment.attributes["filesize"]
+    assert_equal "Captioned", trix_attachment.attributes["caption"]
+
+    assert_nil trix_attachment.attributes["content"]
+  end
+
+  test "converts to TrixAttachment with content" do
+    attachment = attachment_from_html(%Q(<action-text-attachment sgid="#{attachable.attachable_sgid}"></action-text-attachment>))
+
+    trix_attachment = attachment.to_trix_attachment
+    assert_kind_of ActionText::TrixAttachment, trix_attachment
+
+    assert_equal attachable.attachable_sgid, trix_attachment.attributes["sgid"]
+    assert_equal attachable.attachable_content_type, trix_attachment.attributes["contentType"]
+
+    assert_not_nil attachable.to_trix_content_attachment_partial_path
+    assert_not_nil trix_attachment.attributes["content"]
+  end
 
   private
     def attachment_from_html(html)
--- test/unit/content_test.rb
+++ test/unit/content_test.rb
@@ -62,46 +62,46 @@ class ActionText::ContentTest < ActiveSupport::TestCase
     assert_equal ActionText::Attachables::MissingAttachable, content.attachments.first.attachable
   end
 
-  # test "extracts missing attachables" do
-  #   html = %Q(<action-text-attachment sgid="missing"></action-text-attachment>)
-  #   content = ActionText::Content.new(html)
-  #   assert_equal 1, content.attachments.size
-  #   assert_equal ActionText::Attachables::MissingAttachable, content.attachments.first.attachable
-  # end
-  #
-  # test "converts Trix-formatted attachments" do
-  #   html = %Q(<figure data-trix-attachment='{"sgid":"123","contentType":"text/plain","width":100,"height":100}' data-trix-attributes='{"caption":"Captioned"}'></figure>)
-  #   content = ActionText::Content.new(html)
-  #   assert_equal 1, content.attachments.size
-  #   assert_equal %Q(<action-text-attachment sgid="123" content-type="text/plain" width="100" height="100" caption="Captioned"></action-text-attachment>), content.to_html
-  # end
-  #
-  # test "ignores Trix-formatted attachments with malformed JSON" do
-  #   html = %Q(<div data-trix-attachment='{"sgid":"garbage...'></div>)
-  #   content = ActionText::Content.new(html)
-  #   assert_equal 0, content.attachments.size
-  # end
-  #
-  # test "minifies attachment markup" do
-  #   html = %Q(<action-text-attachment sgid="123"><div>HTML</div></action-text-attachment>)
-  #   assert_equal %Q(<action-text-attachment sgid="123"></action-text-attachment>), ActionText::Content.new(html).to_html
-  # end
-  #
-  # test "canonicalizes attachment gallery markup" do
-  #   attachment_html = %Q(<action-text-attachment sgid="1" presentation="gallery"></action-text-attachment><action-text-attachment sgid="2" presentation="gallery"></action-text-attachment>)
-  #   html = %Q(<div class="attachment-gallery attachment-gallery--2">#{attachment_html}</div>)
-  #   assert_equal %Q(<div>#{attachment_html}</div>), ActionText::Content.new(html).to_html
-  # end
-  #
-  # test "canonicalizes attachment gallery markup with whitespace" do
-  #   attachment_html = %Q(\n  <action-text-attachment sgid="1" presentation="gallery"></action-text-attachment>\n  <action-text-attachment sgid="2" presentation="gallery"></action-text-attachment>\n)
-  #   html = %Q(<div class="attachment-gallery attachment-gallery--2">#{attachment_html}</div>)
-  #   assert_equal %Q(<div>#{attachment_html}</div>), ActionText::Content.new(html).to_html
-  # end
-  #
-  # test "canonicalizes nested attachment gallery markup" do
-  #   attachment_html = %Q(<action-text-attachment sgid="1" presentation="gallery"></action-text-attachment><action-text-attachment sgid="2" presentation="gallery"></action-text-attachment>)
-  #   html = %Q(<blockquote><div class="attachment-gallery attachment-gallery--2">#{attachment_html}</div></blockquote>)
-  #   assert_equal %Q(<blockquote><div>#{attachment_html}</div></blockquote>), ActionText::Content.new(html).to_html
-  # end
+  test "extracts missing attachables" do
+    html = %Q(<action-text-attachment sgid="missing"></action-text-attachment>)
+    content = ActionText::Content.new(html)
+    assert_equal 1, content.attachments.size
+    assert_equal ActionText::Attachables::MissingAttachable, content.attachments.first.attachable
+  end
+
+  test "converts Trix-formatted attachments" do
+    html = %Q(<figure data-trix-attachment='{"sgid":"123","contentType":"text/plain","width":100,"height":100}' data-trix-attributes='{"caption":"Captioned"}'></figure>)
+    content = ActionText::Content.new(html)
+    assert_equal 1, content.attachments.size
+    assert_equal %Q(<action-text-attachment sgid="123" content-type="text/plain" width="100" height="100" caption="Captioned"></action-text-attachment>), content.to_html
+  end
+
+  test "ignores Trix-formatted attachments with malformed JSON" do
+    html = %Q(<div data-trix-attachment='{"sgid":"garbage...'></div>)
+    content = ActionText::Content.new(html)
+    assert_equal 0, content.attachments.size
+  end
+
+  test "minifies attachment markup" do
+    html = %Q(<action-text-attachment sgid="123"><div>HTML</div></action-text-attachment>)
+    assert_equal %Q(<action-text-attachment sgid="123"></action-text-attachment>), ActionText::Content.new(html).to_html
+  end
+
+  test "canonicalizes attachment gallery markup" do
+    attachment_html = %Q(<action-text-attachment sgid="1" presentation="gallery"></action-text-attachment><action-text-attachment sgid="2" presentation="gallery"></action-text-attachment>)
+    html = %Q(<div class="attachment-gallery attachment-gallery--2">#{attachment_html}</div>)
+    assert_equal %Q(<div>#{attachment_html}</div>), ActionText::Content.new(html).to_html
+  end
+
+  test "canonicalizes attachment gallery markup with whitespace" do
+    attachment_html = %Q(\n  <action-text-attachment sgid="1" presentation="gallery"></action-text-attachment>\n  <action-text-attachment sgid="2" presentation="gallery"></action-text-attachment>\n)
+    html = %Q(<div class="attachment-gallery attachment-gallery--2">#{attachment_html}</div>)
+    assert_equal %Q(<div>#{attachment_html}</div>), ActionText::Content.new(html).to_html
+  end
+
+  test "canonicalizes nested attachment gallery markup" do
+    attachment_html = %Q(<action-text-attachment sgid="1" presentation="gallery"></action-text-attachment><action-text-attachment sgid="2" presentation="gallery"></action-text-attachment>)
+    html = %Q(<blockquote><div class="attachment-gallery attachment-gallery--2">#{attachment_html}</div></blockquote>)
+    assert_equal %Q(<blockquote><div>#{attachment_html}</div></blockquote>), ActionText::Content.new(html).to_html
+  end
 end
