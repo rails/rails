5714ecf36661423ea77b5cba518701ab98aeb19f
George Claghorn <george@basecamp.com>
2018-10-06 22:27:50 -0400

    Expand incineration tests to cover all processed statuses

--- test/unit/inbound_email/incineration_test.rb
+++ test/unit/inbound_email/incineration_test.rb
@@ -1,11 +1,27 @@
 require_relative '../../test_helper'
 
 class ActionMailbox::InboundEmail::IncinerationTest < ActiveSupport::TestCase
-  test "incinerate emails 30 days after they have been processed" do
+  test "incinerating 30 days after delivery" do
     freeze_time
 
     assert_enqueued_with job: ActionMailbox::InboundEmail::IncinerationJob, at: 30.days.from_now do
       create_inbound_email_from_fixture("welcome.eml").delivered!
+    end
+  end
+
+  test "incinerating 30 days after bounce" do
+    freeze_time
+
+    assert_enqueued_with job: ActionMailbox::InboundEmail::IncinerationJob, at: 30.days.from_now do
+      create_inbound_email_from_fixture("welcome.eml").bounced!
+    end
+  end
+
+  test "incinerating 30 days after failure" do
+    freeze_time
+
+    assert_enqueued_with job: ActionMailbox::InboundEmail::IncinerationJob, at: 30.days.from_now do
+      create_inbound_email_from_fixture("welcome.eml").failed!
     end
   end
 end
