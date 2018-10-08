bba5ecc923bbc8a635913c1101188163cb9699be
Graham Conzett <gconzett@orangebarrelmedia.com>
2018-10-08 09:50:51 -0400

    Use content_mime_type

--- activestorage/app/controllers/active_storage/disk_controller.rb
+++ activestorage/app/controllers/active_storage/disk_controller.rb
@@ -61,7 +61,6 @@ class ActiveStorage::DiskController < ActiveStorage::BaseController
     end
 
     def acceptable_content?(token)
-      Mime::Type.lookup(request.content_type) == token[:content_type] &&
-        token[:content_length] == request.content_length
+      token[:content_type] == request.content_mime_type && token[:content_length] == request.content_length
     end
 end
