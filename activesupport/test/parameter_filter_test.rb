32b03b46150b0161eba2321ccac7678511e3d58e
Yoshiyuki Kinjo <kinjo@bita.jp>
2018-10-09 16:14:51 +0900

    Implement AR#inspect using ParamterFilter.
    AR instance support `filter_parameters` since #33756.
    Though Regex or Proc is valid as `filter_parameters`,
    they are not supported as AR#inspect.
    
    I also add :mask option and #filter_params to
    `ActiveSupport::ParameterFilter#new` to implement this.

--- activesupport/test/parameter_filter_test.rb
+++ activesupport/test/parameter_filter_test.rb
@@ -36,6 +36,51 @@ class ParameterFilterTest < ActiveSupport::TestCase
     end
   end
 
+  test "filter should return mask option when value is filtered" do
+    mask = Object.new.freeze
+    test_hashes = [
+    [{ "foo" => "bar" }, { "foo" => "bar" }, %w'food'],
+    [{ "foo" => "bar" }, { "foo" => mask }, %w'foo'],
+    [{ "foo" => "bar", "bar" => "foo" }, { "foo" => mask, "bar" => "foo" }, %w'foo baz'],
+    [{ "foo" => "bar", "baz" => "foo" }, { "foo" => mask, "baz" => mask }, %w'foo baz'],
+    [{ "bar" => { "foo" => "bar", "bar" => "foo" } }, { "bar" => { "foo" => mask, "bar" => "foo" } }, %w'fo'],
+    [{ "foo" => { "foo" => "bar", "bar" => "foo" } }, { "foo" => mask }, %w'f banana'],
+    [{ "deep" => { "cc" => { "code" => "bar", "bar" => "foo" }, "ss" => { "code" => "bar" } } }, { "deep" => { "cc" => { "code" => mask, "bar" => "foo" }, "ss" => { "code" => "bar" } } }, %w'deep.cc.code'],
+    [{ "baz" => [{ "foo" => "baz" }, "1"] }, { "baz" => [{ "foo" => mask }, "1"] }, [/foo/]]]
+
+    test_hashes.each do |before_filter, after_filter, filter_words|
+      parameter_filter = ActiveSupport::ParameterFilter.new(filter_words, mask: mask)
+      assert_equal after_filter, parameter_filter.filter(before_filter)
+
+      filter_words << "blah"
+      filter_words << lambda { |key, value|
+        value.reverse! if key =~ /bargain/
+      }
+      filter_words << lambda { |key, value, original_params|
+        value.replace("world!") if original_params["barg"]["blah"] == "bar" && key == "hello"
+      }
+
+      parameter_filter = ActiveSupport::ParameterFilter.new(filter_words, mask: mask)
+      before_filter["barg"] = { :bargain => "gain", "blah" => "bar", "bar" => { "bargain" => { "blah" => "foo", "hello" => "world" } } }
+      after_filter["barg"]  = { :bargain => "niag", "blah" => mask, "bar" => { "bargain" => { "blah" => mask, "hello" => "world!" } } }
+
+      assert_equal after_filter, parameter_filter.filter(before_filter)
+    end
+  end
+
+  test "filter_param" do
+    parameter_filter = ActiveSupport::ParameterFilter.new(["foo", /bar/])
+    assert_equal "[FILTERED]", parameter_filter.filter_param("food", "secret vlaue")
+    assert_equal "[FILTERED]", parameter_filter.filter_param("baz.foo", "secret vlaue")
+    assert_equal "[FILTERED]", parameter_filter.filter_param("barbar", "secret vlaue")
+    assert_equal "non secret value", parameter_filter.filter_param("baz", "non secret value")
+  end
+
+  test "filter_param can work with empty filters" do
+    parameter_filter = ActiveSupport::ParameterFilter.new
+    assert_equal "bar", parameter_filter.filter_param("foo", "bar")
+  end
+
   test "parameter filter should maintain hash with indifferent access" do
     test_hashes = [
       [{ "foo" => "bar" }.with_indifferent_access, ["blah"]],
@@ -47,5 +92,14 @@ class ParameterFilterTest < ActiveSupport::TestCase
       assert_instance_of ActiveSupport::HashWithIndifferentAccess,
                          parameter_filter.filter(before_filter)
     end
+  end
+
+  test "filter_param should return mask option when value is filtered" do
+    mask = Object.new.freeze
+    parameter_filter = ActiveSupport::ParameterFilter.new(["foo", /bar/], mask: mask)
+    assert_equal mask, parameter_filter.filter_param("food", "secret vlaue")
+    assert_equal mask, parameter_filter.filter_param("baz.foo", "secret vlaue")
+    assert_equal mask, parameter_filter.filter_param("barbar", "secret vlaue")
+    assert_equal "non secret value", parameter_filter.filter_param("baz", "non secret value")
   end
 end
