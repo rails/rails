2039da7ba029b2c9889847660aa0ba7c5d25bdc0
Liroy Leshed <37022194+liroyleshed@users.noreply.github.com>
2018-10-10 20:29:32 +0300

    Remove set_action_text_renderer from dummy app (#26)
    I don't think we need that since it's set automatically through ActionText::Engine.

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :set_action_text_renderer
-
-  private
-    def set_action_text_renderer
-      ActionText.renderer = self.class.renderer.new(request.env)
-    end
 end
