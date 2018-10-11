58dbc1c2ed0e372d9cae4c9e3baebb679a726dc3
Mick Staugaard <mick@staugaard.com>
2018-10-11 13:47:16 -0700

    Stop trying to reconnect on unauthorized cable connections

--- actioncable/lib/action_cable.rb
+++ actioncable/lib/action_cable.rb
@@ -33,9 +33,15 @@ module ActionCable
   INTERNAL = {
     message_types: {
       welcome: "welcome",
+      disconnect: "disconnect",
       ping: "ping",
       confirmation: "confirm_subscription",
       rejection: "reject_subscription"
+    },
+    disconnect_reasons: {
+      unauthorized: "unauthorized",
+      invalid_request: "invalid_request",
+      server_restart: "server_restart"
     },
     default_mount_path: "/cable",
     protocols: ["actioncable-v1-json", "actioncable-unsupported"].freeze
