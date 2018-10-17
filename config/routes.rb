f8c0ddcf016661f9c508e71224d7722409ef0cec
George Claghorn <george@basecamp.com>
2018-10-17 08:53:37 -0400

    Extract module scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope "/rails/action_mailbox" do
-    post "/amazon/inbound_emails"   => "action_mailbox/ingresses/amazon/inbound_emails#create",   as: :rails_amazon_inbound_emails
-    post "/mandrill/inbound_emails" => "action_mailbox/ingresses/mandrill/inbound_emails#create", as: :rails_mandrill_inbound_emails
-    post "/postfix/inbound_emails"  => "action_mailbox/ingresses/postfix/inbound_emails#create",  as: :rails_postfix_inbound_emails
-    post "/sendgrid/inbound_emails" => "action_mailbox/ingresses/sendgrid/inbound_emails#create", as: :rails_sendgrid_inbound_emails
+  scope "/rails/action_mailbox", module: "action_mailbox/ingresses" do
+    post "/amazon/inbound_emails"   => "amazon/inbound_emails#create",   as: :rails_amazon_inbound_emails
+    post "/mandrill/inbound_emails" => "mandrill/inbound_emails#create", as: :rails_mandrill_inbound_emails
+    post "/postfix/inbound_emails"  => "postfix/inbound_emails#create",  as: :rails_postfix_inbound_emails
+    post "/sendgrid/inbound_emails" => "sendgrid/inbound_emails#create", as: :rails_sendgrid_inbound_emails
 
-    # Mailgun requires that the webhook's URL end in 'mime' for it to receive the raw contents of emails.
-    post "/mailgun/inbound_emails/mime" => "action_mailbox/ingresses/mailgun/inbound_emails#create", as: :rails_mailgun_inbound_emails
+    # Mailgun requires that a webhook's URL end in 'mime' for it to receive the raw contents of emails.
+    post "/mailgun/inbound_emails/mime" => "mailgun/inbound_emails#create", as: :rails_mailgun_inbound_emails
   end
 
   # TODO: Should these be mounted within the engine only?
