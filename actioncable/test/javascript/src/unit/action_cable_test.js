bbb49bff5ed0f76a405e8d91ebf50d3c1adecbdf
Richard Macklin <richard.github@nrm.com>
2018-10-18 20:41:03 -0700

    Remove tests that only test javascript object property assignment
    These tests really only assert that you can assign a property to
    the ActionCable global object. That's true for pretty much any object
    in javascript (it would only be false if the object has been frozen, or
    has explicitly set some properties to be nonconfigurable).

--- actioncable/test/javascript/src/unit/action_cable_test.js
+++ actioncable/test/javascript/src/unit/action_cable_test.js
@@ -9,21 +9,11 @@ module("ActionCable", () => {
       test("default is window.WebSocket", assert => {
         assert.equal(ActionCable.WebSocket, window.WebSocket)
       })
-
-      test("configurable", assert => {
-        ActionCable.WebSocket = ""
-        assert.equal(ActionCable.WebSocket, "")
-      })
     })
 
     module("logger", () => {
       test("default is window.console", assert => {
         assert.equal(ActionCable.logger, window.console)
-      })
-
-      test("configurable", assert => {
-        ActionCable.logger = ""
-        assert.equal(ActionCable.logger, "")
       })
     })
   })
