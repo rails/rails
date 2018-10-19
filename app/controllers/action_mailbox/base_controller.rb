fbd4219274f7c30de391ec8d7b6b6c5d76fb57c7
George Claghorn <george@basecamp.com>
2018-10-19 15:21:26 -0400

    Don't short-circuit

--- app/controllers/action_mailbox/base_controller.rb
+++ app/controllers/action_mailbox/base_controller.rb
@@ -4,7 +4,7 @@ class ActionMailbox::BaseController < ActionController::Base
   private
     def authenticate
       authenticate_or_request_with_http_basic("Action Mailbox") do |given_username, given_password|
-        ActiveSupport::SecurityUtils.secure_compare(given_username, username) &&
+        ActiveSupport::SecurityUtils.secure_compare(given_username, username) &
           ActiveSupport::SecurityUtils.secure_compare(given_password, password)
       end
     end
