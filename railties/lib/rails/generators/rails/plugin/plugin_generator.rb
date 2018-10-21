7093ee8ec5250545ba904a2cb1fcd67c5138cc3b
bogdanvlviv <bogdanvlviv@gmail.com>
2018-10-22 00:00:32 +0300

    Remove `:javascript` from `Rails::PluginBuilder::PASSTHROUGH_OPTIONS`
    `--javascript` option was removed by 42198064c35ff3b701496309f90df2abc229efbe

--- railties/lib/rails/generators/rails/plugin/plugin_generator.rb
+++ railties/lib/rails/generators/rails/plugin/plugin_generator.rb
@@ -88,7 +88,7 @@ task default: :test
 
     PASSTHROUGH_OPTIONS = [
       :skip_active_record, :skip_active_storage, :skip_action_mailer, :skip_javascript, :skip_action_cable, :skip_sprockets, :database,
-      :javascript, :skip_yarn, :api, :quiet, :pretend, :skip
+      :skip_yarn, :api, :quiet, :pretend, :skip
     ]
 
     def generate_test_dummy(force = false)
