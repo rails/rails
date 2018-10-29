cb041ddc7e94da15e2db72188545f78da6cadb53
George Claghorn <george@basecamp.com>
2018-10-29 14:19:46 -0400

    Stage an Action Controller extraction

--- app/controllers/action_mailbox/base_controller.rb
+++ app/controllers/action_mailbox/base_controller.rb
@@ -4,12 +4,17 @@ class ActionMailbox::BaseController < ActionController::Base
   private
     def authenticate
       if username.present? && password.present?
-        authenticate_or_request_with_http_basic("Action Mailbox") do |given_username, given_password|
-          ActiveSupport::SecurityUtils.secure_compare(given_username, username) &
-            ActiveSupport::SecurityUtils.secure_compare(given_password, password)
-        end
+        http_basic_authenticate_or_request_with username: username, password: password, realm: "Action Mailbox"
       else
         raise ArgumentError, "Missing required ingress credentials"
+      end
+    end
+
+    # TODO: Extract to ActionController::HttpAuthentication
+    def http_basic_authenticate_or_request_with(username:, password:, realm: nil)
+      authenticate_or_request_with_http_basic(realm || "Application") do |given_username, given_password|
+        ActiveSupport::SecurityUtils.secure_compare(given_username, username) &
+          ActiveSupport::SecurityUtils.secure_compare(given_password, password)
       end
     end
 end
