5d851664b8551942fb869c379c0332e5e5bc9f53
David Heinemeier Hansson <david@loudthinking.com>
2018-11-05 16:58:07 -0800

    Add addresses convenience method and a test for it

--- lib/action_mailbox/mail_ext/recipients.rb
+++ lib/action_mailbox/mail_ext/recipients.rb
@@ -2,4 +2,25 @@ class Mail::Message
   def recipients
     Array(to) + Array(cc) + Array(bcc)
   end
+
+  def recipients_addresses
+    convert_to_addresses recipients
+  end
+
+  def to_addresses
+    convert_to_addresses to
+  end
+
+  def cc_addresses
+    convert_to_addresses cc
+  end
+
+  def bcc_addresses
+    convert_to_addresses bcc
+  end
+
+  private
+    def convert_to_addresses(recipients)
+      recipients.collect { |recipient| Mail::Address.new recipient }
+    end
 end
