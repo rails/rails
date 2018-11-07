fb5ce96bc5c293a1178c2ae52de962bcc59ea5c8
David Heinemeier Hansson <david@loudthinking.com>
2018-11-06 17:03:10 -0800

    Split out all the addresses from recipients

---
+++ lib/action_mailbox/mail_ext/addresses.rb
@@ -0,0 +1,26 @@
+class Mail::Message
+  def from_address
+    Mail::Address.new from
+  end
+
+  def recipients_addresses
+    convert_to_addresses recipients
+  end
+
+  def to_addresses
+    convert_to_addresses to
+  end
+
+  def cc_addresses
+    convert_to_addresses cc
+  end
+
+  def bcc_addresses
+    convert_to_addresses bcc
+  end
+
+  private
+    def convert_to_addresses(recipients)
+      recipients.collect { |recipient| Mail::Address.new recipient }
+    end
+end
