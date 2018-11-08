f4bc364fdf808e58bd30ab44d111384a46615e1a
Ryuta Kamizono <kamipo@gmail.com>
2018-11-09 06:14:12 +0900

    Deprecate `t.indexes = [...]` which is not by design
    Use `t.index ...` instead.

--- activerecord/lib/active_record/connection_adapters/abstract/schema_definitions.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/schema_definitions.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "active_support/deprecation"
+
 module ActiveRecord
   module ConnectionAdapters #:nodoc:
     # Abstract representation of an index definition on a table. Instances of
@@ -256,8 +258,9 @@ module ActiveRecord
     class TableDefinition
       include ColumnMethods
 
-      attr_accessor :indexes
-      attr_reader :name, :temporary, :if_not_exists, :options, :as, :foreign_keys, :comment
+      attr_reader :name, :temporary, :if_not_exists, :options, :as, :comment, :indexes, :foreign_keys
+      attr_writer :indexes
+      deprecate :indexes=
 
       def initialize(
         name,
