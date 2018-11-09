a4d429f4a7c417b719270e9164634c83ded3d0ec
George Claghorn <george@basecamp.com>
2018-11-09 13:45:51 -0500

    Discard incineration jobs for missing inbound emails

--- app/jobs/action_mailbox/incineration_job.rb
+++ app/jobs/action_mailbox/incineration_job.rb
@@ -1,6 +1,8 @@
 class ActionMailbox::IncinerationJob < ActiveJob::Base
   queue_as :action_mailbox_incineration
 
+  discard_on ActiveRecord::RecordNotFound
+
   def self.schedule(inbound_email)
     set(wait: ActionMailbox.incinerate_after).perform_later(inbound_email)
   end
