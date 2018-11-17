2080f25a45f211d152c9086f0832c91374d78bd8
Richard Macklin <richard.github@nrm.com>
2018-11-16 19:36:19 -0800

    Remove unused commonjs plugin from ActionCable rollup config
    This added when we copied the rollup config from ActiveStorage, but
    ActionCable does not have any commonjs dependencies (it doesn't have
    any external dependencies at all), so this plugin is unnecessary here.

--- actioncable/rollup.config.js
+++ actioncable/rollup.config.js
@@ -1,5 +1,4 @@
 import resolve from "rollup-plugin-node-resolve"
-import commonjs from "rollup-plugin-commonjs"
 import babel from "rollup-plugin-babel"
 import uglify from "rollup-plugin-uglify"
 
@@ -21,7 +20,6 @@ export default {
   },
   plugins: [
     resolve(),
-    commonjs(),
     babel(),
     uglify(uglifyOptions)
   ]
