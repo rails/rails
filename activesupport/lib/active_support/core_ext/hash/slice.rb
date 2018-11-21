50d1e00625091bf7f4c84afbdabb9006402a94ea
Ryuta Kamizono <kamipo@gmail.com>
2018-11-21 17:08:06 +0900

    Remove odd spaces [ci skip]

--- activesupport/lib/active_support/core_ext/hash/slice.rb
+++ activesupport/lib/active_support/core_ext/hash/slice.rb
@@ -28,8 +28,8 @@ class Hash
   # Returns a hash containing the removed key/value pairs.
   #
   #   hash = { a: 1, b: 2, c: 3, d: 4 }
-  #   hash.slice!(:a, :b)  # => {:c=>3, :d=>4 }
-  #   hash                 # => {:a=>1, :b=>2 }
+  #   hash.slice!(:a, :b)  # => {:c=>3, :d=>4}
+  #   hash                 # => {:a=>1, :b=>2}
   def slice!(*keys)
     omit = slice(*self.keys - keys)
     hash = slice(*keys)
