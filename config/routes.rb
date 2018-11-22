42f9cd62c34891474df7271d148cdb231ab73d90
George Claghorn <george@basecamp.com>
2018-11-22 16:40:30 -0500

    Revert "Nest"
    This reverts commit 88227658217ee82af7516d6a8013a6c04f037073.

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,7 @@ Rails.application.routes.draw do
 
   # TODO: Should these be mounted within the engine only?
   scope "rails/conductor/action_mailbox/", module: "rails/conductor/action_mailbox" do
-    resources :inbound_emails, as: :rails_conductor_inbound_emails do
-      post "reroute" => "reroutes#create"
-    end
+    resources :inbound_emails, as: :rails_conductor_inbound_emails
+    post ":inbound_email_id/reroute" => "reroutes#create", as: :rails_conductor_inbound_email_reroute
   end
 end
