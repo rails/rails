a1c57cf69f2454d5ba3a086237c1c827e6c9de5a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-12-21 18:12:10 +0900

    Add missing require for `String#to_d`

--- activesupport/lib/active_support/xml_mini.rb
+++ activesupport/lib/active_support/xml_mini.rb
@@ -3,6 +3,7 @@
 require "time"
 require "base64"
 require "bigdecimal"
+require "bigdecimal/util"
 require "active_support/core_ext/module/delegation"
 require "active_support/core_ext/string/inflections"
 require "active_support/core_ext/date_time/calculations"
