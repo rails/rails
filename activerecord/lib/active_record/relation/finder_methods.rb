b7e7b8617594d919b6d0d228473805b4d0662568
Bogdan Gusiev <agresso@gmail.com>
2018-12-27 18:10:13 +0200

    Simplify expected size calculation in AR::Relation#find_some

--- activerecord/lib/active_record/relation/finder_methods.rb
+++ activerecord/lib/active_record/relation/finder_methods.rb
@@ -457,18 +457,7 @@ module ActiveRecord
         return find_some_ordered(ids) unless order_values.present?
 
         result = where(primary_key => ids).to_a
-
-        expected_size =
-          if limit_value && ids.size > limit_value
-            limit_value
-          else
-            ids.size
-          end
-
-        # 11 ids with limit 3, offset 9 should give 2 results.
-        if offset_value && (ids.size - offset_value < expected_size)
-          expected_size = ids.size - offset_value
-        end
+        expected_size = [limit_value, ids.size - offset_index].compact.min
 
         if result.size == expected_size
           result
@@ -478,7 +467,7 @@ module ActiveRecord
       end
 
       def find_some_ordered(ids)
-        ids = ids.slice(offset_value || 0, limit_value || ids.size) || []
+        ids = ids.slice(offset_index, limit_value || ids.size) || []
 
         result = except(:limit, :offset).where(primary_key => ids).records
 
