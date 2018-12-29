a4fe5c5b6606f5b320222069f076c73a2eb3d12e
George Claghorn <george@basecamp.com>
2018-12-28 21:29:22 -0500

    Require railties for all Action Mailbox dependencies

--- actionmailbox/lib/action_mailbox/engine.rb
+++ actionmailbox/lib/action_mailbox/engine.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-require "rails/engine"
+require "rails"
+require "action_controller/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+
 require "action_mailbox"
 
 module ActionMailbox
