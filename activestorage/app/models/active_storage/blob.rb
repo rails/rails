ae68b6b55f70d388d0448b3a29daeae723d128ba
Julik Tarkhanov <me@julik.nl>
2018-12-30 13:15:52 +0100

    Update the #key method comment

--- activestorage/app/models/active_storage/blob.rb
+++ activestorage/app/models/active_storage/blob.rb
@@ -98,8 +98,8 @@ class ActiveStorage::Blob < ActiveRecord::Base
     ActiveStorage.verifier.generate(id, purpose: :blob_id)
   end
 
-  # Returns the key pointing to the file on the service that's associated with this blob. The key is in the
-  # standard secure-token format from Rails. So it'll look like: xtapjjcjiudrlk3tmwyjgpuobabd.
+  # Returns the key pointing to the file on the service that's associated with this blob. The key is the
+  # secure-token format from Rails in lower case. So it'll look like: xtapjjcjiudrlk3tmwyjgpuobabd.
   # This key is not intended to be revealed directly to the user.
   # Always refer to blobs using the signed_id or a verified form of the key.
   def key
