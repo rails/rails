1a15086897ccdd210aa027a8e1f58816e16626ff
chopraanmol1 <chopraanmol1@gmail.com>
2019-01-03 11:38:24 +0530

    Fix MultifetchCacheTest test case

--- actionview/test/activerecord/multifetch_cache_test.rb
+++ actionview/test/activerecord/multifetch_cache_test.rb
@@ -6,7 +6,7 @@ require "active_record/railties/collection_cache_association_loading"
 ActionView::PartialRenderer.prepend(ActiveRecord::Railties::CollectionCacheAssociationLoading)
 
 class MultifetchCacheTest < ActiveRecordTestCase
-  fixtures :topics, :replies
+  fixtures :topics, :replies, :developers
 
   def setup
     view_paths = ActionController::Base.view_paths
@@ -25,11 +25,29 @@ class MultifetchCacheTest < ActiveRecordTestCase
   def test_only_preloading_for_records_that_miss_the_cache
     @view.render partial: "test/partial", collection: [topics(:rails)], cached: true
 
-    @topics = Topic.preload(:replies)
+    @topics = Topic.includes_immediately(:replies)
 
     @view.render partial: "test/partial", collection: @topics, cached: true
 
     assert_not @topics.detect { |topic| topic.id == topics(:rails).id }.replies.loaded?
     assert     @topics.detect { |topic| topic.id != topics(:rails).id }.replies.loaded?
+  end
+
+  def test_only_lazy_loading_for_records_that_miss_the_cache
+    @view.render partial: "test/partial", collection: [developers(:poor_jamis)], cached: true
+
+    @developers = Developer.preload(:replies)
+
+    @view.render partial: "test/partial", collection: @developers, cached: true
+
+    assert @developers.size >= 3
+    assert_not @developers.detect { |developer| developer.id == developers(:poor_jamis).id }.replies.loaded?
+    assert_not @developers.detect { |developer| developer.id != developers(:poor_jamis).id }.replies.loaded?
+
+    @developers.detect { |developer| developer.id != developers(:poor_jamis).id }.replies.to_a
+    @developers.each do |developer|
+      next if developer.id == developers(:poor_jamis).id
+      assert developer.replies.loaded?
+    end
   end
 end
