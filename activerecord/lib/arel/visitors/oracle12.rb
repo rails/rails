95bf9d3a723d6d421b9bc9ce3c4eeafd25831c9e
Ryuta Kamizono <kamipo@gmail.com>
2019-01-09 16:02:09 +0900

    Ensure that visit always replaces the collector

--- activerecord/lib/arel/visitors/oracle12.rb
+++ activerecord/lib/arel/visitors/oracle12.rb
@@ -18,8 +18,8 @@ module Arel # :nodoc: all
         end
 
         def visit_Arel_Nodes_SelectOptions(o, collector)
-          maybe_visit o.offset, collector
-          maybe_visit o.limit, collector
+          collector = maybe_visit o.offset, collector
+          collector = maybe_visit o.limit, collector
           maybe_visit o.lock, collector
         end
 
