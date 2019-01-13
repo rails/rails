017e8ae87bfcebd2834a9f8fb44a985c740fc906
Jonathan Fleckenstein <jonfleck88@gmail.com>
2019-01-13 10:55:17 -0500

    Add test for deliver method on blob.

--- activestorage/test/models/blob_test.rb
+++ activestorage/test/models/blob_test.rb
@@ -168,6 +168,14 @@ class ActiveStorage::BlobTest < ActiveSupport::TestCase
     end
   end
 
+  test "change delivery on instance" do
+    blob = create_file_blob filename: "racecar.jpg"
+
+    assert_equal blob.deliver(:redirect), Rails.application.routes.url_helpers.route_for(:rails_service_blob, blob.signed_id, blob.filename, only_path: true)
+    assert_equal blob.deliver(:proxy), Rails.application.routes.url_helpers.route_for(:rails_blob_proxy, blob.signed_id, blob.filename, only_path: true)
+    assert_equal blob.deliver(:direct)[0..417], blob.service_url[0..417]
+  end
+
   test "purge deletes file from external service" do
     blob = create_blob
 
