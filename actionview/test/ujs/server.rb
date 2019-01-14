5c11f92d935a3c54bd0a02a61e00984a38e82a5e
WoH <wolfgang.hobmaier@datapweb.de>
2019-01-14 20:54:25 +0100

    CSP

--- actionview/test/ujs/server.rb
+++ actionview/test/ujs/server.rb
@@ -24,14 +24,14 @@ module UJS
     config.logger = Logger.new(STDOUT)
     config.log_level = :error
 
-    # config.content_security_policy do |policy|
-    #   policy.default_src :self, :https
-    #   policy.font_src    :self, :https, :data
-    #   policy.img_src     :self, :https, :data
-    #   policy.object_src  :none
-    #   policy.script_src  :self, :https
-    #   policy.style_src   :self, :https
-    # end
+    config.content_security_policy do |policy|
+      policy.default_src :self, :https
+      policy.font_src    :self, :https, :data
+      policy.img_src     :self, :https, :data
+      policy.object_src  :none
+      policy.script_src  :self, :https
+      policy.style_src   :self, :https
+    end
 
     config.content_security_policy_nonce_generator = ->(req) { SecureRandom.base64(16) }
   end
