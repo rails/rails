5cd733a334846669d6435517f7d9913c4a9b1eb1
Pratik Naik <pratiknaik@gmail.com>
2019-01-17 11:13:40 -0600

    Ensure Action Mailbox processes an email only once when received multiple times
    This also adds a new column, message_checksum, to the action_mailbox_inbound_emails table
    for storing SHA1 digest of the email source. Additionally, it makes generating the missing
    message id deterministic and adds a unique index on message_checksum and message_id to
    detect duplicate emails.

--- actionmailbox/db/migrate/20180917164000_create_action_mailbox_tables.rb
+++ actionmailbox/db/migrate/20180917164000_create_action_mailbox_tables.rb
@@ -2,10 +2,13 @@ class CreateActionMailboxTables < ActiveRecord::Migration[6.0]
   def change
     create_table :action_mailbox_inbound_emails do |t|
       t.integer :status, default: 0, null: false
-      t.string  :message_id
+      t.string  :message_id, null: false
+      t.string  :message_checksum, null: false
 
       t.datetime :created_at, precision: 6, null: false
       t.datetime :updated_at, precision: 6, null: false
+
+      t.index [ :message_id, :message_checksum ], unique: true
     end
   end
 end
--- actionmailbox/test/unit/mailbox/routing_test.rb
+++ actionmailbox/test/unit/mailbox/routing_test.rb
@@ -15,11 +15,10 @@ end
 class ActionMailbox::Base::RoutingTest < ActiveSupport::TestCase
   setup do
     $processed = false
-    @inbound_email = create_inbound_email_from_fixture("welcome.eml")
   end
 
   test "string routing" do
-    ApplicationMailbox.route @inbound_email
+    ApplicationMailbox.route create_inbound_email_from_fixture("welcome.eml")
     assert_equal "Discussion: Let's debate these attachments", $processed
   end
 
