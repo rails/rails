88453093d5c74aad489feb34dc9062f55f258dd9
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2019-01-19 23:28:41 +0900

    Fix that adding attachments lose a body

--- actionmailbox/test/controllers/rails/action_mailbox/inbound_emails_controller_test.rb
+++ actionmailbox/test/controllers/rails/action_mailbox/inbound_emails_controller_test.rb
@@ -12,6 +12,25 @@ class Rails::Conductor::ActionMailbox::InboundEmailsControllerTest < ActionDispa
             to: "Replies <replies@example.com>",
             bcc: "",
             in_reply_to: "<4e6e35f5a38b4_479f13bb90078178@small-app-01.mail>",
+            subject: "Create",
+            body: "New Mail"
+          }
+        }
+      end
+
+      assert_equal "New Mail", ActionMailbox::InboundEmail.last.mail.decoded
+    end
+  end
+
+  test "create inbound email with attachments" do
+    with_rails_env("development") do
+      assert_difference -> { ActionMailbox::InboundEmail.count }, +1 do
+        post rails_conductor_inbound_emails_path, params: {
+          mail: {
+            from: "Jason Fried <jason@37signals.com>",
+            to: "Replies <replies@example.com>",
+            bcc: "",
+            in_reply_to: "<4e6e35f5a38b4_479f13bb90078178@small-app-01.mail>",
             subject: "Discussion: Let's debate these attachments",
             body: "Let's talk about these images:",
             attachments: [fixture_file_upload("files/avatar1.jpeg"), fixture_file_upload("files/avatar2.jpeg")]
@@ -19,7 +38,9 @@ class Rails::Conductor::ActionMailbox::InboundEmailsControllerTest < ActionDispa
         }
       end
 
-      assert_equal 2, ActionMailbox::InboundEmail.last.mail.attachments.size
+      mail = ActionMailbox::InboundEmail.last.mail
+      assert_equal 2, mail.attachments.size
+      assert_equal "Let's talk about these images:", mail.text_part.decoded
     end
   end
 
