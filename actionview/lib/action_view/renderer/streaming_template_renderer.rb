1853b0d0abf87dfdd4c3a277c3badb17ca19652e
Aaron Patterson <aaron.patterson@gmail.com>
2019-01-23 12:16:32 -0800

    Pass the view around instead of using an ivar
    If we pass the view instance around it's easier to understand the flow
    control.

--- actionview/lib/action_view/renderer/streaming_template_renderer.rb
+++ actionview/lib/action_view/renderer/streaming_template_renderer.rb
@@ -43,14 +43,14 @@ module ActionView
     # For streaming, instead of rendering a given a template, we return a Body
     # object that responds to each. This object is initialized with a block
     # that knows how to render the template.
-    def render_template(template, layout_name = nil, locals = {}) #:nodoc:
+    def render_template(view, template, layout_name = nil, locals = {}) #:nodoc:
       return [super] unless layout_name && template.supports_streaming?
 
       locals ||= {}
       layout   = layout_name && find_layout(layout_name, locals.keys, [formats.first])
 
       Body.new do |buffer|
-        delayed_render(buffer, template, layout, @view, locals)
+        delayed_render(buffer, template, layout, view, locals)
       end
     end
 
