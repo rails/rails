c7721ea6b3a2319820decccce1a26eb9ebf3b290
Ryuta Kamizono <kamipo@gmail.com>
2019-02-09 13:53:24 +0900

    Refactor to extract defining column methods as `define_column_methods`
    It makes to ease to handle all short-hand methods (e.g. validates
    arguments etc).

--- activerecord/lib/active_record/connection_adapters/abstract/schema_definitions.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/schema_definitions.rb
@@ -198,41 +198,43 @@ module ActiveRecord
     end
 
     module ColumnMethods
+      extend ActiveSupport::Concern
+
       # Appends a primary key definition to the table definition.
       # Can be called multiple times, but this is probably not a good idea.
       def primary_key(name, type = :primary_key, **options)
         column(name, type, options.merge(primary_key: true))
       end
 
+      ##
+      # :method: column
+      # :call-seq: column(name, type, options = {})
+      #
       # Appends a column or columns of a specified type.
       #
       #  t.string(:goat)
       #  t.string(:goat, :sheep)
       #
       # See TableDefinition#column
-      [
-        :bigint,
-        :binary,
-        :boolean,
-        :date,
-        :datetime,
-        :decimal,
-        :float,
-        :integer,
-        :json,
-        :string,
-        :text,
-        :time,
-        :timestamp,
-        :virtual,
-      ].each do |column_type|
-        module_eval <<-CODE, __FILE__, __LINE__ + 1
-          def #{column_type}(*args, **options)
-            args.each { |name| column(name, :#{column_type}, options) }
-          end
-        CODE
+
+      included do
+        define_column_methods :bigint, :binary, :boolean, :date, :datetime, :decimal,
+          :float, :integer, :json, :string, :text, :time, :timestamp, :virtual
+
+        alias :numeric :decimal
       end
-      alias_method :numeric, :decimal
+
+      class_methods do
+        private def define_column_methods(*column_types) # :nodoc:
+          column_types.each do |column_type|
+            module_eval <<-RUBY, __FILE__, __LINE__ + 1
+              def #{column_type}(*names, **options)
+                names.each { |name| column(name, :#{column_type}, options) }
+              end
+            RUBY
+          end
+        end
+      end
     end
 
     # Represents the schema of an SQL table in an abstract way. This class
