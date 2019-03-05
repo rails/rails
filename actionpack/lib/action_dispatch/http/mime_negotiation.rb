c79dcbce9bfd20fe7f72ca431c49965ee39bd645
John Hawthorn <john@hawthorn.email>
2019-03-04 18:24:51 -0800

    Only accept formats from registered mime types
    [CVE-2019-5418]
    [CVE-2019-5419]

--- actionpack/lib/action_dispatch/http/mime_negotiation.rb
+++ actionpack/lib/action_dispatch/http/mime_negotiation.rb
@@ -76,6 +76,11 @@ module ActionDispatch
           else
             [Mime[:html]]
           end
+
+          v = v.select do |format|
+            format.symbol || format.ref == "*/*"
+          end
+
           set_header k, v
         end
       end
