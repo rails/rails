7d0c5391d67e8124955417650ff8d6a4475ecc76
David Heinemeier Hansson <david@loudthinking.com>
2019-03-05 20:08:57 -0800

    Basic filtering idea

--- conductor/app/controllers/rails/conductor/notes_controller.rb
+++ conductor/app/controllers/rails/conductor/notes_controller.rb
@@ -9,6 +9,10 @@ class Rails::Conductor::NotesController < Rails::Conductor::CommandController
 
   private
     def extract_notes
-      capture_stdout { Rails::SourceAnnotationExtractor.enumerate %w[ OPTIMIZE FIXME TODO ].join("|") }
+      capture_stdout { Rails::SourceAnnotationExtractor.enumerate tags_param }
+    end
+
+    def tags_param
+      params[:tag].presence || %w[ OPTIMIZE FIXME TODO ].join("|")
     end
 end
