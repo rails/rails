70192cec42c0133791010537fd602089c81986a4
Shailesh Kalamkar <shailesh.kalamkar@gmail.com>
2019-03-09 12:13:28 +0530

    [ci skip] Minor documentation fixes for consistency

--- actionview/test/ujs/public/vendor/jquery-2.2.0.js
+++ actionview/test/ujs/public/vendor/jquery-2.2.0.js
@@ -9639,7 +9639,7 @@ jQuery.fn.extend( {
 	// 2) For the hidden or detached element
 	// 3) For body or html element, i.e. in case of the html node - it will return itself
 	//
-	// but those exceptions were never presented as a real life use-cases
+	// but those exceptions were never presented as a real life use cases
 	// and might be considered as more preferable results.
 	//
 	// This logic, however, is not guaranteed and can change at any point in the future
