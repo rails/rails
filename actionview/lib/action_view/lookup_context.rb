c555e28a4cf3690a95b5ed108d174f39cedd19b1
John Hawthorn <john@hawthorn.email>
2019-03-18 10:12:41 -0700

    Ignore nil in LookupContext#formats=
    This also removes the mutation we were performing on the values being
    passed in.

--- actionview/lib/action_view/lookup_context.rb
+++ actionview/lib/action_view/lookup_context.rb
@@ -280,6 +280,7 @@ module ActionView
     # add :html as fallback to :js.
     def formats=(values)
       if values
+        values = values.compact
         values.concat(default_formats) if values.delete "*/*"
         if values == [:js]
           values << :html
