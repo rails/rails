98d0f93506b0b97804d0d510ffcc435af9b2a2d5
Ryuta Kamizono <kamipo@gmail.com>
2019-03-19 02:22:06 +0900

    Don't expose internal `type_casted_binds` method
    Internal usage for the method as public has removed at #29623.

--- activerecord/lib/active_record/connection_adapters/abstract/quoting.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/quoting.rb
@@ -138,15 +138,15 @@ module ActiveRecord
         "'#{quote_string(value.to_s)}'"
       end
 
-      def type_casted_binds(binds) # :nodoc:
-        if binds.first.is_a?(Array)
-          binds.map { |column, value| type_cast(value, column) }
-        else
-          binds.map { |attr| type_cast(attr.value_for_database) }
-        end
-      end
-
       private
+        def type_casted_binds(binds)
+          if binds.first.is_a?(Array)
+            binds.map { |column, value| type_cast(value, column) }
+          else
+            binds.map { |attr| type_cast(attr.value_for_database) }
+          end
+        end
+
         def lookup_cast_type(sql_type)
           type_map.lookup(sql_type)
         end
