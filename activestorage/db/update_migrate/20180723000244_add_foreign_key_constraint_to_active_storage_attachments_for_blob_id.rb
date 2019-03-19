135d8a0dc100f10ffa57fb4ed877fc1cc29c7608
Eugeniu Tambur <eugeniu.rtj@gmail.com>
2019-03-19 13:16:57 +0100

    Traducir del: inglés 44/5000 Checking for the existence of only one table

--- activestorage/db/update_migrate/20180723000244_add_foreign_key_constraint_to_active_storage_attachments_for_blob_id.rb
+++ activestorage/db/update_migrate/20180723000244_add_foreign_key_constraint_to_active_storage_attachments_for_blob_id.rb
@@ -2,7 +2,7 @@ class AddForeignKeyConstraintToActiveStorageAttachmentsForBlobId < ActiveRecord:
   def up
     return if foreign_key_exists?(:active_storage_attachments, column: :blob_id)
 
-    if table_exists?(:active_storage_attachments) && table_exists?(:active_storage_blobs)
+    if table_exists?(:active_storage_blobs)
       add_foreign_key :active_storage_attachments, :active_storage_blobs, column: :blob_id
     end
   end
