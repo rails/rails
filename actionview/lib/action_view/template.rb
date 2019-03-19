a4bb6fd008550824ba44fe1f52b1f2a0721f2d2d
John Hawthorn <john@hawthorn.email>
2019-03-19 14:58:28 -0700

    Re-add Template#updated_at as deprecated

--- actionview/lib/action_view/template.rb
+++ actionview/lib/action_view/template.rb
@@ -122,10 +122,10 @@ module ActionView
 
     extend Template::Handlers
 
-    attr_reader :source, :identifier, :handler, :original_encoding
+    attr_reader :source, :identifier, :handler, :original_encoding, :updated_at
     attr_reader :variable, :format, :variant, :locals, :virtual_path
 
-    def initialize(source, identifier, handler, format: nil, variant: nil, locals: nil, virtual_path: nil)
+    def initialize(source, identifier, handler, format: nil, variant: nil, locals: nil, virtual_path: nil, updated_at: nil)
       unless locals
         ActiveSupport::Deprecation.warn "ActionView::Template#initialize requires a locals parameter"
         locals = []
@@ -144,12 +144,19 @@ module ActionView
         $1.to_sym
       end
 
+      if updated_at
+        ActiveSupport::Deprecation.warn "ActionView::Template#updated_at is deprecated"
+        @updated_at        = updated_at
+      else
+        @updated_at        = Time.now
+      end
       @format            = format
       @variant           = variant
       @compile_mutex     = Mutex.new
     end
 
     deprecate :original_encoding
+    deprecate :updated_at
     deprecate def virtual_path=(_); end
     deprecate def locals=(_); end
     deprecate def formats=(_); end
@@ -260,11 +267,11 @@ module ActionView
     # to ensure that references to the template object can be marshalled as well. This means forgoing
     # the marshalling of the compiler mutex and instantiating that again on unmarshalling.
     def marshal_dump # :nodoc:
-      [ @source, @identifier, @handler, @compiled, @locals, @virtual_path, @format, @variant ]
+      [ @source, @identifier, @handler, @compiled, @locals, @virtual_path, @updated_at, @format, @variant ]
     end
 
     def marshal_load(array) # :nodoc:
-      @source, @identifier, @handler, @compiled, @locals, @virtual_path, @format, @variant = *array
+      @source, @identifier, @handler, @compiled, @locals, @virtual_path, @updated_at, @format, @variant = *array
       @compile_mutex = Mutex.new
     end
 
