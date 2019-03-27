a805d72e901f81f5b043dfd9904b80ce93ebfc01
Timo Schilling <timo@schilling.io>
2019-03-27 21:54:30 +0100

    Speed improvement for HashWithIndifferentAccess#except

--- activesupport/lib/active_support/hash_with_indifferent_access.rb
+++ activesupport/lib/active_support/hash_with_indifferent_access.rb
@@ -293,6 +293,9 @@ module ActiveSupport
       super(convert_key(key))
     end
 
+    def except(*keys)
+      slice(*self.keys - keys.map { |key| convert_key(key) })
+    end
     alias_method :without, :except
 
     def stringify_keys!; self end
