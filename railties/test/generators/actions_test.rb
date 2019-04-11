23392eff9f8065475739e58232a5db97604f8056
Ryuta Kamizono <kamipo@gmail.com>
2019-04-12 05:50:51 +0900

    Address new cop offence in railties
    It is necessary to run `rubocop` manually when new cop is added since
    codeclimate checks any offences only in files touched in PRs.

--- railties/test/generators/actions_test.rb
+++ railties/test/generators/actions_test.rb
@@ -200,7 +200,7 @@ class ActionsTest < Rails::Generators::TestCase
     run_generator
 
     action :environment do
-      _ = "# This wont be added"# assignment to silence parse-time warning "unused literal ignored"
+      _ = "# This wont be added" # assignment to silence parse-time warning "unused literal ignored"
       "# This will be added"
     end
 
