fd6648d5979df6e6daf9fa7eb1d01cd87922ae32
Ryuta Kamizono <kamipo@gmail.com>
2019-04-24 02:58:38 +0900

    Rename local variable `n` to `name`

--- activerecord/lib/active_record/associations/builder/belongs_to.rb
+++ activerecord/lib/active_record/associations/builder/belongs_to.rb
@@ -74,11 +74,11 @@ module ActiveRecord::Associations::Builder # :nodoc:
 
     def self.add_touch_callbacks(model, reflection)
       foreign_key = reflection.foreign_key
-      n           = reflection.name
+      name        = reflection.name
       touch       = reflection.options[:touch]
 
       callback = lambda { |changes_method| lambda { |record|
-        BelongsTo.touch_record(record, record.send(changes_method), foreign_key, n, touch, belongs_to_touch_method)
+        BelongsTo.touch_record(record, record.send(changes_method), foreign_key, name, touch, belongs_to_touch_method)
       }}
 
       if reflection.counter_cache_column
