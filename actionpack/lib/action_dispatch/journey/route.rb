20104ba13daae81184a339b8949054aecbbd7655
Alberto Almagro <albertoalmagro@gmail.com>
2019-05-22 23:31:22 +0200

    Remove Route#build as it wasn't used
    After @kamipo CR feedback we realized `Route#build` wasn't used. As it
    is also private API being able to create Routes both with `#new` and
    `#build` was redundant.

--- actionpack/lib/action_dispatch/journey/route.rb
+++ actionpack/lib/action_dispatch/journey/route.rb
@@ -49,16 +49,10 @@ module ActionDispatch
         end
       end
 
-      def self.build(name, app, path, constraints, required_defaults, defaults)
-        new name: name, app: app, path: path, constraints: constraints,
-            required_defaults: required_defaults, defaults: defaults,
-            request_method_match: verb_matcher(constraints.delete(:request_method))
-      end
-
       ##
       # +path+ is a path constraint.
       # +constraints+ is a hash of constraints to be applied to this route.
-      def initialize(name:, app:, path:, constraints:, required_defaults:, defaults:, request_method_match:, precedence: 0, scope_options: {}, internal: false)
+      def initialize(name:, app: nil, path:, constraints: {}, required_defaults: [], defaults: {}, request_method_match: nil, precedence: 0, scope_options: {}, internal: false)
         @name        = name
         @app         = app
         @path        = path
