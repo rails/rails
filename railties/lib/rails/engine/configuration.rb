59619aa00edb75f97e5feeb700e8678e77b98880
Andrew Kress <andrew.kress@gmail.com>
2019-05-28 11:19:24 -0500

    read config/webpacker.yml to determine which path to exclude for zeitwerk:check

--- railties/lib/rails/engine/configuration.rb
+++ railties/lib/rails/engine/configuration.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "rails/railtie/configuration"
+require "yaml"
 
 module Rails
   class Engine
@@ -40,7 +41,7 @@ module Rails
 
           paths.add "app",                 eager_load: true,
                                            glob: "{*,*/concerns}",
-                                           exclude: %w(assets javascript)
+                                           exclude: ["assets", webpacker_path]
           paths.add "app/assets",          glob: "*"
           paths.add "app/controllers",     eager_load: true
           paths.add "app/channels",        eager_load: true, glob: "**/*_channel.rb"
@@ -85,6 +86,11 @@ module Rails
       def autoload_paths
         @autoload_paths ||= paths.autoload_paths
       end
+
+      private
+        def webpacker_path
+          YAML.load_file("#{Rails.root}/config/webpacker.yml")[Rails.env]["source_path"].gsub("app/", "") if File.file?("#{Rails.root}/config/webpacker.yml")
+        end
     end
   end
 end
