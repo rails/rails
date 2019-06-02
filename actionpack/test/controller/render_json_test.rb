a04a0330a56a5eae53fd7ab3ebcab692acf2ea06
Abhay Nikam <nikam.abhay1@gmail.com>
2019-06-02 20:23:10 +0530

    Remove unnecessary require pathname from actionpack controller specs

--- actionpack/test/controller/render_json_test.rb
+++ actionpack/test/controller/render_json_test.rb
@@ -3,7 +3,6 @@
 require "abstract_unit"
 require "controller/fake_models"
 require "active_support/logger"
-require "pathname"
 
 class RenderJsonTest < ActionController::TestCase
   class JsonRenderable
