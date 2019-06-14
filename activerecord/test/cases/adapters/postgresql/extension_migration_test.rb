74174d786bab3208c6ed1b412394443c13e6c961
Ryuta Kamizono <kamipo@gmail.com>
2019-06-15 08:03:16 +0900

    Ensure to reset actually used `@connection.schema_migration`'s table name
    https://buildkite.com/rails/rails/builds/61744#f12cc6cf-7458-4131-917a-9735615f6259/999-1010

--- activerecord/test/cases/adapters/postgresql/extension_migration_test.rb
+++ activerecord/test/cases/adapters/postgresql/extension_migration_test.rb
@@ -27,20 +27,20 @@ class PostgresqlExtensionMigrationTest < ActiveRecord::PostgreSQLTestCase
 
     ActiveRecord::Base.table_name_prefix = "p_"
     ActiveRecord::Base.table_name_suffix = "_s"
-    ActiveRecord::SchemaMigration.reset_table_name
+    @connection.schema_migration.reset_table_name
     ActiveRecord::InternalMetadata.reset_table_name
 
-    ActiveRecord::SchemaMigration.delete_all rescue nil
+    @connection.schema_migration.delete_all rescue nil
     ActiveRecord::Migration.verbose = false
   end
 
   def teardown
-    ActiveRecord::SchemaMigration.delete_all rescue nil
+    @connection.schema_migration.delete_all rescue nil
     ActiveRecord::Migration.verbose = true
 
     ActiveRecord::Base.table_name_prefix = @old_table_name_prefix
     ActiveRecord::Base.table_name_suffix = @old_table_name_suffix
-    ActiveRecord::SchemaMigration.reset_table_name
+    @connection.schema_migration.reset_table_name
     ActiveRecord::InternalMetadata.reset_table_name
 
     super
