108ffa44bf427d98bf6fcd99b7c185585d1066a7
Lee Ourand <leethreesix@gmail.com>
2019-06-28 15:51:19 -0400

    Backports Rails 6 ActiveStorage's route_prefix

--- activestorage/lib/active_storage.rb
+++ activestorage/lib/active_storage.rb
@@ -50,4 +50,5 @@ module ActiveStorage
   mattr_accessor :content_types_to_serve_as_binary, default: []
   mattr_accessor :content_types_allowed_inline, default: []
   mattr_accessor :binary_content_type, default: "application/octet-stream"
+  mattr_accessor :routes_prefix, default: "/rails/active_storage"
 end
