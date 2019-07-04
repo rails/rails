5134a34c9c425ac388173d332d6802ef7ef3e360
John Hawthorn <john@hawthorn.email>
2019-07-04 13:20:30 -0700

    Merge pull request #36592 from jhawthorn/system_testing_preload
    Preload browser driver_path to fix system testing under parallelism

--- actionpack/lib/action_dispatch/system_testing/browser.rb
+++ actionpack/lib/action_dispatch/system_testing/browser.rb
@@ -39,6 +39,19 @@ module ActionDispatch
           end
       end
 
+      # driver_path can be configured as a proc. The webdrivers gem uses this
+      # proc to update web drivers. Running this proc early allows us to only
+      # update the webdriver once and avoid race conditions when using
+      # parallel tests.
+      def preload
+        case type
+        when :chrome
+          ::Selenium::WebDriver::Chrome::Service.driver_path.try(:call)
+        when :firefox
+          ::Selenium::WebDriver::Firefox::Service.driver_path.try(:call)
+        end
+      end
+
       private
         def headless_chrome_browser_options
           capabilities.args << "--headless"
