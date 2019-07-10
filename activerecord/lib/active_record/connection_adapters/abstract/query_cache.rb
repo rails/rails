853f5680e8c79a51f5c0bf1624b1a5d3d3df2802
Eileen M. Uchitelle <eileencodes@users.noreply.github.com>
2019-07-10 14:05:22 -0500

    Merge pull request #36618 from engwan/fix-query-cache-with-shared-ar-connection
    Fix query cache when using shared connections

--- activerecord/lib/active_record/connection_adapters/abstract/query_cache.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/query_cache.rb
@@ -33,17 +33,17 @@ module ActiveRecord
         end
 
         def enable_query_cache!
-          @query_cache_enabled[connection_cache_key(Thread.current)] = true
+          @query_cache_enabled[connection_cache_key(current_thread)] = true
           connection.enable_query_cache! if active_connection?
         end
 
         def disable_query_cache!
-          @query_cache_enabled.delete connection_cache_key(Thread.current)
+          @query_cache_enabled.delete connection_cache_key(current_thread)
           connection.disable_query_cache! if active_connection?
         end
 
         def query_cache_enabled
-          @query_cache_enabled[connection_cache_key(Thread.current)]
+          @query_cache_enabled[connection_cache_key(current_thread)]
         end
       end
 
