8ff8a610cc6169195037ac056acece88e0da718c
Simone Carletti <weppos@weppos.net>
2019-07-26 12:46:52 +0200

    Switch to assert_match

--- actionpack/test/dispatch/request_test.rb
+++ actionpack/test/dispatch/request_test.rb
@@ -893,7 +893,7 @@ class RequestFormat < BaseRequestTest
     assert request.format.html?
 
     output.rewind && (err = output.read)
-    assert err =~ /Error occurred while parsing request parameters/
+    assert_match /Error occurred while parsing request parameters/, err
   end
 
   test "formats with xhr request" do
