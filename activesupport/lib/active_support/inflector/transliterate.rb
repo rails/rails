515238bc6c29f3b89234db3ebf2cd5af90f2663e
vzvu3k6k <vzvu3k6k@gmail.com>
2019-07-30 03:10:56 +0900

    [ci skip] Fix unclosed tags in `Inflector` docs

--- activesupport/lib/active_support/inflector/transliterate.rb
+++ activesupport/lib/active_support/inflector/transliterate.rb
@@ -116,7 +116,7 @@ module ActiveSupport
     # If the optional parameter +locale+ is specified,
     # the word will be parameterized as a word of that language.
     # By default, this parameter is set to <tt>nil</tt> and it will use
-    # the configured <tt>I18n.locale<tt>.
+    # the configured <tt>I18n.locale</tt>.
     def parameterize(string, separator: "-", preserve_case: false, locale: nil)
       # Replace accented chars with their ASCII equivalents.
       parameterized_string = transliterate(string, locale: locale)
