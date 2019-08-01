2ec1bad4ad83bdf8cecf055642ae3eb57d5fe919
Akira Matsuda <ronnie@dio.jp>
2019-08-02 04:27:48 +0900

    Missing require AS/core_ext/object/blank

--- activesupport/lib/active_support/inflector/methods.rb
+++ activesupport/lib/active_support/inflector/methods.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "active_support/inflections"
+require "active_support/core_ext/object/blank"
 
 module ActiveSupport
   # The Inflector transforms words from singular to plural, class names to table
