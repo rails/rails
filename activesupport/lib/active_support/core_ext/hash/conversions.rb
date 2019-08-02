12959dee0f19a8e050dd1236b031c2c690729905
Akira Matsuda <ronnie@dio.jp>
2019-08-02 13:15:32 +0900

    hash_conversion itself does not at all depend on time extensions

--- activesupport/lib/active_support/core_ext/hash/conversions.rb
+++ activesupport/lib/active_support/core_ext/hash/conversions.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "active_support/xml_mini"
-require "active_support/time"
 require "active_support/core_ext/object/blank"
 require "active_support/core_ext/object/to_param"
 require "active_support/core_ext/object/to_query"
