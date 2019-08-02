73977f9a8f57cb69c4f87954e72016c07f59310c
Peter Zhu <peter@peterzhu.ca>
2019-08-02 15:02:17 -0400

    Remove default value from `service_name` column

--- activestorage/db/update_migrate/20190731033524_add_service_name_column_to_active_storage_blobs.rb
+++ activestorage/db/update_migrate/20190731033524_add_service_name_column_to_active_storage_blobs.rb
@@ -3,7 +3,7 @@ class AddServiceNameColumnToActiveStorageBlobs < ActiveRecord::Migration[6.0]
     return if column_exists?(:active_storage_blobs, :service_name)
 
     if table_exists?(:active_storage_blobs)
-      add_column :active_storage_blobs, :service_name, :string, default: "", null: false
+      add_column :active_storage_blobs, :service_name, :string
     end
   end
 end
