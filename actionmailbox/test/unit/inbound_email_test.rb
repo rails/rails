67c2620df4c19e537f5241afcc34351cbe40fbf0
Michael Herold <opensource@michaeljherold.com>
2019-08-07 08:21:30 -0500

    Move the multi-part test into a TestHelper test

--- actionmailbox/test/unit/inbound_email_test.rb
+++ actionmailbox/test/unit/inbound_email_test.rb
@@ -12,39 +12,6 @@ module ActionMailbox
       assert_equal file_fixture("welcome.eml").read, create_inbound_email_from_fixture("welcome.eml").source
     end
 
-    test "multi-part mail can be built in tests using a block" do
-      inbound_email = create_inbound_email_from_mail do
-        to "test@example.com"
-        from "hello@example.com"
-
-        text_part do
-          body "Hello, world"
-        end
-
-        html_part do
-          body "<h1>Hello, world</h1>"
-        end
-      end
-
-      mail = inbound_email.mail
-
-      assert_equal mail.parts.length, 2
-      assert_equal mail.text_part.to_s, <<~TEXT.chomp
-        Content-Type: text/plain;\r
-         charset=UTF-8\r
-        Content-Transfer-Encoding: 7bit\r
-        \r
-        Hello, world
-      TEXT
-      assert_equal mail.html_part.to_s, <<~HTML.chomp
-        Content-Type: text/html;\r
-         charset=UTF-8\r
-        Content-Transfer-Encoding: 7bit\r
-        \r
-        <h1>Hello, world</h1>
-      HTML
-    end
-
     test "email with message id is processed only once when received multiple times" do
       mail = Mail.from_source(file_fixture("welcome.eml").read)
       assert mail.message_id
