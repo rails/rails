ad34b6cfd2690167987e4bac2226afefe733c34d
Rafael Mendonça França <rafael@franca.dev>
2019-08-08 15:08:18 -0400

    Remove invalid autoload of ActionDispatch::Http::Upload
    This constant doesn't exists.

--- actionpack/lib/action_dispatch.rb
+++ actionpack/lib/action_dispatch.rb
@@ -80,7 +80,6 @@ module ActionDispatch
     autoload :MimeNegotiation
     autoload :Parameters
     autoload :ParameterFilter
-    autoload :Upload
     autoload :UploadedFile, "action_dispatch/http/upload"
     autoload :URL
   end
