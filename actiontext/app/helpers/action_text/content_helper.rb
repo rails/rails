9988e2a2913a286e0c6679073a9de783865ec925
Darryl Pogue <darryl@dpogue.ca>
2019-08-15 14:40:45 -0700

    Fix rendering ActionText attachments
    `render` returns a ActionView::AbstractRenderer::RenderedTemplate, whose
    rendered content is in the `body` attribute as a string. Without this,
    rendering this view will cause an undefined method `chomp’ for
    ActionView::AbstractRenderer::RenderedTemplate exception.

--- actiontext/app/helpers/action_text/content_helper.rb
+++ actiontext/app/helpers/action_text/content_helper.rb
@@ -21,13 +21,13 @@ module ActionText
       content.render_attachments do |attachment|
         unless attachment.in?(content.gallery_attachments)
           attachment.node.tap do |node|
-            node.inner_html = render(attachment, in_gallery: false).chomp
+            node.inner_html = render(attachment, in_gallery: false).body.chomp
           end
         end
       end.render_attachment_galleries do |attachment_gallery|
         render(layout: attachment_gallery, object: attachment_gallery) do
           attachment_gallery.attachments.map do |attachment|
-            attachment.node.inner_html = render(attachment, in_gallery: true).chomp
+            attachment.node.inner_html = render(attachment, in_gallery: true).body.chomp
             attachment.to_html
           end.join("").html_safe
         end.chomp
