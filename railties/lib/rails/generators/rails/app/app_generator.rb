04cfbc807f0567af5a27e8ba45eab52f7b9e6618
Rafael Mendonça França <rafael@franca.dev>
2019-08-19 16:53:01 -0400

    Generate keep file in tmp/pids
    Since 9c913116c634fe5fe2159a94b8f1a244801a4877, if you run `rackup`
    in a machine that doesn't have that folder, the server will not start.
    
    This doesn't happen on `rails s` because we create that folder when
    starting the server on
    https://github.com/rails/rails/blob/8c8b02784a637570451d7810fd3dfed4eae9a77d/railties/lib/rails/commands/server/server_command.rb#L70-L74.

--- railties/lib/rails/generators/rails/app/app_generator.rb
+++ railties/lib/rails/generators/rails/app/app_generator.rb
@@ -230,6 +230,7 @@ module Rails
 
     def tmp
       empty_directory_with_keep_file "tmp"
+      empty_directory_with_keep_file "tmp/pids"
       empty_directory "tmp/cache"
       empty_directory "tmp/cache/assets"
     end
