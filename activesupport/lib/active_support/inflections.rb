2c616d3812430af6c7607a16e7814b9c7cc636eb
Peter Carnesciali <pcarn9@gmail.com>
2019-08-19 14:51:07 -0500

    Adds more inflection words

--- activesupport/lib/active_support/inflections.rb
+++ activesupport/lib/active_support/inflections.rb
@@ -63,10 +63,12 @@ module ActiveSupport
     inflect.irregular("person", "people")
     inflect.irregular("man", "men")
     inflect.irregular("child", "children")
+    inflect.irregular("tooth", "teeth")
+    inflect.irregular("goose", "geese")
     inflect.irregular("sex", "sexes")
     inflect.irregular("move", "moves")
     inflect.irregular("zombie", "zombies")
 
-    inflect.uncountable(%w(equipment information rice money species series fish sheep jeans police))
+    inflect.uncountable(%w(equipment information rice money species series fish sheep jeans police moose))
   end
 end
