rails/rails

commit 3e2e8eeb9ea552bd4782538cf9348455f3d0e14a

Use a single thread for all ConnectionPool Reapers
Previously we would spawn one thread per connection pool, which ends up
being wasteful for apps with several connection pools.

activerecord/lib/active_record/connection_adapters/abstract/connection_pool.rb
@@ -294,15 +294,33 @@ module ActiveRecord
           @frequency = frequency
         end
 
+        @@mutex = Mutex.new
+        @@pools = {}
+
+        def self.register_pool(pool, frequency) # :nodoc:
+          @@mutex.synchronize do
+            if @@pools.key?(frequency)
+              @@pools[frequency] << pool
+            else
+              @@pools[frequency] = [pool]
+              Thread.new(frequency) do |t|
+                loop do
+                  sleep t
+                  @@mutex.synchronize do
+                    @@pools[frequency].each do |p|
+                      p.reap
+                      p.flush
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+
         def run
           return unless frequency && frequency > 0
-          Thread.new(frequency, pool) { |t, p|
-            loop do
-              sleep t
-              p.reap
-              p.flush
-            end
-          }
+          self.class.register_pool(pool, frequency)
         end
       end
 

commit 9837242f82c0e16313de0a888b4539376653cc53

Fix connections reaper not running after forking.
This broke in 3e2e8eeb9ea552bd4782538cf9348455f3d0e14a where it switched
to one reaper thread per process. However, the implementation will only
spawn the reaping thread if the reaping frequency has not been seen.
Since the reaping frequencies are stored in a class instance variable,
that variable is copied when the process is forked. As such, a forked
process will not spawn a new reaping thread since the class instance
variable would have contain the reaping frequency that was seen in the
parent process.

Also reverts the use of `WeakRef` introduced in
6302e56d6c1fec048f6438d9f1ac6a8cfaed7eb9 since we properly deregisters
connection pools from the reaper upon discard/disconnect.

activerecord/lib/active_record/connection_adapters/abstract/connection_pool.rb
@@ -3,7 +3,6 @@
 require "thread"
 require "concurrent/map"
 require "monitor"
-require "weakref"
 
 module ActiveRecord
   # Raised when a connection could not be obtained within the connection
@@ -315,40 +314,58 @@ module ActiveRecord
         end
 
         @mutex = Mutex.new
-        @pools = {}
+        @reapers = {}
 
         class << self
+          attr_reader :reapers
+
           def register_pool(pool, frequency) # :nodoc:
             @mutex.synchronize do
-              unless @pools.key?(frequency)
-                @pools[frequency] = []
-                spawn_thread(frequency)
+              @reapers[frequency] ||= {}
+
+              if !@reapers.dig(frequency, :thread)&.alive?
+                @reapers[frequency][:pools] ||= []
+                @reapers[frequency][:thread] = spawn_thread(frequency)
               end
-              @pools[frequency] << WeakRef.new(pool)
+
+              @reapers[frequency][:pools] << pool
+            end
+          end
+
+          def deregister_pool(pool, frequency) # :nodoc:
+            @mutex.synchronize do
+              if @reapers[frequency][:pools]
+                @reapers[frequency][:pools].delete(pool)
+              end
             end
           end
 
           private
             def spawn_thread(frequency)
-              Thread.new(frequency) do |t|
+              thread = Thread.new(frequency) do |t|
                 loop do
                   sleep t
                   @mutex.synchronize do
-                    @pools[frequency].select!(&:weakref_alive?)
-                    @pools[frequency].each do |p|
+                    @reapers[frequency][:pools].each do |p|
                       p.reap
                       p.flush
-                    rescue WeakRef::RefError
                     end
                   end
                 end
               end
+
+              thread.abort_on_exception = true
+              thread
             end
         end
 
         def run
           return unless frequency && frequency > 0
           self.class.register_pool(pool, frequency)
+        end
+
+        def stop # :nodoc:
+          self.class.deregister_pool(pool, frequency)
         end
       end
 
@@ -525,6 +542,8 @@ module ActiveRecord
       def discard! # :nodoc:
         synchronize do
           return if @connections.nil? # already discarded
+          @reaper.stop
+
           @connections.each do |conn|
             conn.discard!
           end
@@ -1124,6 +1143,7 @@ module ActiveRecord
         if pool = owner_to_pool.delete(spec_name)
           pool.automatic_reconnect = false
           pool.disconnect!
+          pool.reaper.stop
           pool.spec.config
         end
       end

activerecord/test/cases/connection_adapters/connection_handler_test.rb
@@ -13,7 +13,8 @@ module ActiveRecord
       def setup
         @handler = ConnectionHandler.new
         @spec_name = "primary"
-        @pool = @handler.establish_connection(ActiveRecord::Base.configurations["arunit"])
+        @config = ActiveRecord::Base.configurations["arunit"]
+        @pool = @handler.establish_connection(@config)
       end
 
       def test_default_env_fall_back_to_default_env_when_rails_env_or_rack_env_is_empty_string
@@ -39,6 +40,29 @@ module ActiveRecord
       ensure
         ActiveRecord::Base.configurations = old_config
         @handler.remove_connection("readonly")
+      end
+
+      def test_establish_connection_releases_reference_to_old_connection_pool
+        reaping_frequency = 0.123
+        @handler.establish_connection(@config.merge(reaping_frequency: reaping_frequency))
+
+        assert_equal(
+          1,
+          ObjectSpace.each_object(ActiveRecord::ConnectionAdapters::ConnectionPool).select do |pool|
+            pool.spec.config[:reaping_frequency] == 0.123
+          end.count
+        )
+
+        2.times { @handler.establish_connection(@config.merge(reaping_frequency: reaping_frequency)) }
+
+        GC.start
+
+        assert_equal(
+          1,
+          ObjectSpace.each_object(ActiveRecord::ConnectionAdapters::ConnectionPool).select do |pool|
+            pool.spec.config[:reaping_frequency] == 0.123
+          end.count
+        )
       end
 
       def test_establish_connection_using_3_levels_config
@@ -251,6 +275,46 @@ module ActiveRecord
           rd.close
 
           assert_equal 3, ActiveRecord::Base.connection.select_value("SELECT COUNT(*) FROM people")
+        end
+
+        def test_reaper_starts_in_forked_child
+          @handler.establish_connection(@config.merge(reaping_frequency: 1))
+
+          reaper = ActiveRecord::ConnectionAdapters::ConnectionPool::Reaper.reapers[1.0]
+          assert reaper[:thread].alive?
+          pools = reaper[:pools]
+          assert_equal 1, pools.size
+
+          rd, wr = IO.pipe
+          rd.binmode
+          wr.binmode
+
+          pid = fork do
+            rd.close
+
+            @handler.establish_connection(@config.merge(reaping_frequency: 1))
+            forked_reaper = ActiveRecord::ConnectionAdapters::ConnectionPool::Reaper.reapers[1.0]
+
+            wr.write(
+              Marshal.dump(
+                thread_alive: forked_reaper[:thread].alive?,
+                pools: forked_reaper[:pools].map(&:object_id)
+              )
+            )
+
+            wr.close
+            exit
+          end
+
+          wr.close
+          Process.waitpid(pid)
+
+          payload = Marshal.load(rd.read)
+          rd.close
+
+          assert payload[:thread_alive]
+          assert_equal 1, payload[:pools].size
+          assert_not_equal pools.map(&:object_id), payload[:pools]
         end
 
         unless in_memory_db?