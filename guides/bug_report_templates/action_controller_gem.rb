780010c33594ca30e4e5138faaed7a709063eb2a
Abhay Nikam <nikam.abhay1@gmail.com>
2019-08-21 14:32:58 +0530

    Fixed ActionController Gem bug report by adding allowed hosts config

--- guides/bug_report_templates/action_controller_gem.rb
+++ guides/bug_report_templates/action_controller_gem.rb
@@ -16,6 +16,7 @@ require "action_controller/railtie"
 
 class TestApp < Rails::Application
   config.root = __dir__
+  config.hosts << "example.org"
   config.session_store :cookie_store, key: "cookie_store_key"
   secrets.secret_key_base = "secret_key_base"
 
