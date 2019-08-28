7ef7113babfac94097e5c34c0105af926e86d6d3
John Hawthorn <john@hawthorn.email>
2019-08-28 11:39:04 -0700

    Remove mysql2 specific rescue in abstract adapter

--- activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/mysql2_adapter.rb
@@ -140,6 +140,14 @@ module ActiveRecord
         def get_full_version
           @connection.server_info[:version]
         end
+
+        def translate_exception(exception, message:, sql:, binds:)
+          if !exception.error_number && exception.is_a?(Mysql2::Error::TimeoutError)
+            ActiveRecord::AdapterTimeout.new(message, sql: sql, binds: binds)
+          else
+            super
+          end
+        end
     end
   end
 end
