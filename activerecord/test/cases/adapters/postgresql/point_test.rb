50863a7d29bc249419538db4843a4a2560704969
Julien Sanchez <julien.sanchez@lim.eu>
2019-08-29 14:46:07 +0200

    Create non-regression tests for PostgreSQL Point type casting

---
+++ activerecord/test/cases/adapters/postgresql/point_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "cases/helper"
+
+module ActiveRecord
+  module ConnectionAdapters
+    class PostgreSQLAdapter < AbstractAdapter
+      class PointTest < ActiveRecord::PostgreSQLTestCase
+        def setup
+          @type = OID::Point.new
+        end
+
+        def test_valid_user_input_cast
+          assert_equal(ActiveRecord::Point.new(3.4, 5.6), @type.cast("3.4, 5.6"))
+          assert_equal(ActiveRecord::Point.new(3.4, 5.6), @type.cast("(3.4, 5.6)"))
+        end
+
+        def test_invalid_user_input_cast
+          assert_nil @type.cast("   ")
+        end
+
+        def test_float_array_cast
+          assert_equal(ActiveRecord::Point.new(3.4, 5.6), @type.cast([3.4, 5.6]))
+        end
+
+        def test_unsupported_type_cast
+          assert_equal(4, @type.cast(4))
+          assert_equal(true, @type.cast(true))
+        end
+      end
+    end
+  end
+end
