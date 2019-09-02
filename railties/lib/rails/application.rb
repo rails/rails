e4c8136a928ec973d75b95aa851f70e0e19df757
y-yagi <yuuji.yaginuma@gmail.com>
2019-09-03 07:57:41 +0900

    Merge pull request #37102 from y-yagi/fixes_37011
    Correctly classify the files and directories that pass to watcher

--- railties/lib/rails/application.rb
+++ railties/lib/rails/application.rb
@@ -350,7 +350,7 @@ module Rails
       files, dirs = config.watchable_files.dup, config.watchable_dirs.dup
 
       ActiveSupport::Dependencies.autoload_paths.each do |path|
-        dirs[path.to_s] = [:rb]
+        File.file?(path) ? files << path.to_s : dirs[path.to_s] = [:rb]
       end
 
       [files, dirs]
