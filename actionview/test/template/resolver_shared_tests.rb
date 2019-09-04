44e9c439dde007f50039981eea4982f8ec7632ef
Ryan Laughlin <rofreg@gmail.com>
2019-09-03 20:22:51 -0400

    Add test

--- actionview/test/template/resolver_shared_tests.rb
+++ actionview/test/template/resolver_shared_tests.rb
@@ -79,6 +79,18 @@ module ResolverSharedTests
     assert_kind_of ActionView::Template::Handlers::ERB, templates[0].handler
   end
 
+  def test_can_find_when_directory_includes_regex_characters
+    with_file "test.directory (with regex characters)/hello_world", "Hello default!"
+
+    templates = resolver.find_all("hello_world", "test.directory (with regex characters)", false, locale: [:en], formats: [:html], variants: [:phone], handlers: [:erb])
+    assert_equal 1, templates.size
+    assert_equal "Hello default!", templates[0].source
+    assert_equal "test.directory (with regex characters)/hello_world", templates[0].virtual_path
+    assert_nil templates[0].format
+    assert_nil templates[0].variant
+    assert_kind_of ActionView::Template::Handlers::Raw, templates[0].handler
+  end
+
   def test_doesnt_find_template_with_wrong_details
     with_file "test/hello_world.html.erb", "Hello plain text!"
 
