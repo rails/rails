ec054d0fe2485d7e5bae320036d432e7243d1a2f
Zhang Kang <piecehealth@sina.com>
2019-09-10 16:05:58 +0800

    Bypass CI test.

--- actioncable/test/subscription_adapter/rabbitmq_test.rb
+++ actioncable/test/subscription_adapter/rabbitmq_test.rb
@@ -8,7 +8,25 @@ class RabbitmqAdapterTest < ActionCable::TestCase
   include CommonSubscriptionAdapterTest
   include ChannelPrefixTest
 
+  def setup
+    super
+    unless rabbimt_mq_is_ready?
+      skip "RabbitMQ is not ready." if ENV["CI"]
+    end
+  end
+
   def cable_config
     { adapter: "rabbitmq" }
   end
+
+  private
+    def rabbimt_mq_is_ready?
+      return @is_ready if defined? @is_ready
+      @is_ready = begin
+        @rx_adapter.send :listener
+        true
+      rescue Bunny::TCPConnectionFailedForAllHosts
+        false
+      end
+    end
 end
