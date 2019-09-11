45a398cf90539b042dbce4425eafe2d44111406b
Ryuta Kamizono <kamipo@gmail.com>
2019-09-11 18:18:47 +0900

    Merge pull request #37170 from eugeneius/insert_all_query_cache_test
    Skip insert all tests when features are unavailable

--- activerecord/test/cases/query_cache_test.rb
+++ activerecord/test/cases/query_cache_test.rb
@@ -655,6 +655,8 @@ class QueryCacheExpiryTest < ActiveRecord::TestCase
   end
 
   def test_insert_all
+    skip unless supports_insert_on_duplicate_skip?
+
     assert_called(ActiveRecord::Base.connection, :clear_query_cache, times: 2) do
       Task.cache { Task.insert({ starting: Time.now }) }
     end
@@ -662,7 +664,9 @@ class QueryCacheExpiryTest < ActiveRecord::TestCase
     assert_called(ActiveRecord::Base.connection, :clear_query_cache, times: 2) do
       Task.cache { Task.insert_all([{ starting: Time.now }]) }
     end
+  end
 
+  def test_insert_all_bang
     assert_called(ActiveRecord::Base.connection, :clear_query_cache, times: 2) do
       Task.cache { Task.insert!({ starting: Time.now }) }
     end
@@ -670,6 +674,10 @@ class QueryCacheExpiryTest < ActiveRecord::TestCase
     assert_called(ActiveRecord::Base.connection, :clear_query_cache, times: 2) do
       Task.cache { Task.insert_all!([{ starting: Time.now }]) }
     end
+  end
+
+  def test_upsert_all
+    skip unless supports_insert_on_duplicate_update?
 
     assert_called(ActiveRecord::Base.connection, :clear_query_cache, times: 2) do
       Task.cache { Task.upsert({ starting: Time.now }) }
