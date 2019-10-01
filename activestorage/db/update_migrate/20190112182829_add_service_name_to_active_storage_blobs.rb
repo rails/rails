e6487e84fc0dae30d3b20b396404881e962f1f3a
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2019-10-01 16:54:37 -0400

    Make Active Storage services aware of configuration names

--- activestorage/db/update_migrate/20190112182829_add_service_name_to_active_storage_blobs.rb
+++ activestorage/db/update_migrate/20190112182829_add_service_name_to_active_storage_blobs.rb
@@ -3,7 +3,7 @@ class AddServiceNameToActiveStorageBlobs < ActiveRecord::Migration[6.0]
     unless column_exists?(:active_storage_blobs, :service_name)
       add_column :active_storage_blobs, :service_name, :string
 
-      if configured_service = Rails.configuration.active_storage.service
+      if configured_service = ActiveStorage::Blob.service.name
         ActiveStorage::Blob.unscoped.update_all(service_name: configured_service)
       end
 
