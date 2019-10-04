d095631bdda66f191d4c2cf9ff0aa8cacb5e5976
Greg Myers <neonmd@hotmail.co.uk>
2019-10-04 21:07:47 +0100

    Update actionpack/lib/action_dispatch/testing/integration.rb

--- actionpack/lib/action_dispatch/testing/integration.rb
+++ actionpack/lib/action_dispatch/testing/integration.rb
@@ -50,7 +50,7 @@ module ActionDispatch
       # Performs an OPTIONS request with the given parameters. See ActionDispatch::Integration::Session#process
       # for more details.
       def options(path, **args)
-        process(:head, path, **args)
+        process(:options, path, **args)
       end
 
       # Follow a single redirect response. If the last response was not a
