75174d6997164b9a479cb4106a8340c4b9fdd545
ezii <manu@korfmann.info>
2019-10-07 14:00:46 +0200

    ………

--- actionview/lib/action_view/template.rb
+++ actionview/lib/action_view/template.rb
@@ -377,7 +377,7 @@ module ActionView
 
           
             Stick('3') do
-              if started_appending > 5 && started_appending < 20
+              if started_appending > 5 && started_appending < 6
                 # banal_source_inspect.push("@output_buffer.safe_append = '<div>' + debug_inspect.to_s + '</div>'")
               end
             end
