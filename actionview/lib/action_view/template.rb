5bdde5fe160561cc31a62e2a6ad8639802832c7a
ezii <manu@korfmann.info>
2019-10-10 22:15:03 +0200

    ………

--- actionview/lib/action_view/template.rb
+++ actionview/lib/action_view/template.rb
@@ -369,8 +369,8 @@ module ActionView
     
     
         banal_source_inspect_raw = banal_source_inspect_raw.split(';').reject do  |source_line|
-          return (source_line =~ /\A@output_buffer.safe_append=(['"])[^\1]\Z/) && true
-          return (source_line =~ /\A['"].freeze\Z/) && true
+          next if (source_line =~ /\A@output_buffer.safe_append=(['"])[^\1]\Z/) && true
+          next if (source_line =~ /\A['"].freeze\Z/) && true
         end
       
         rend = banal_source_inspect_raw.length - 2
