27000bd889afbca0aa2e22158bd80d67982d6788
ezii <manu@korfmann.info>
2019-10-10 23:19:29 +0200

    ………

--- actionview/lib/action_view/template.rb
+++ actionview/lib/action_view/template.rb
@@ -373,8 +373,7 @@ module ActionView
         rend = banal_source_inspect_raw.length - 2
         rstart = 2
         banal_source_inspect_raw.each.with_index do |source_line, i|   
-          if i < rend && i > rstart && source_line =~  (/\A[^@]*@output_buffer\.(?:safe)?_?append[^=]*=.*\Z/)
-            byebug
+          if i < rend && i > rstart && source_line =~  (/\A[^@]*@output_buffer\.(?:safe)?_?append[^=]*=.*\Z/) && source_line !~ /if/ && source_line !~ /else/ && source_line !~ /do/ && source_line !~ /end/
             rails_ehtml do |html|
               html.call(%Q{<div class="tweezer-docking">}, banal_source_inspect)
                 html.call(%Q{<div class="tweezer-digestable">}, banal_source_inspect)
