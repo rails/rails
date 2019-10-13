rails/rails

commit 820cc8012bbfe152358d3cf6a136d94e829ea7ab

Move ActiveJob::Exceptions#instrument to ActiveJob::Instrumentation

activejob/lib/active_job/exceptions.rb
@@ -139,12 +139,6 @@ module ActiveJob
         end
       end
 
-      def instrument(name, error: nil, wait: nil, &block)
-        payload = { job: self, adapter: self.class.queue_adapter, error: error, wait: wait }
-
-        ActiveSupport::Notifications.instrument("#{name}.active_job", payload, &block)
-      end
-
       def executions_for(exceptions)
         if exception_executions
           exception_executions[exceptions.to_s] = (exception_executions[exceptions.to_s] || 0) + 1

activejob/lib/active_job/instrumentation.rb
@@ -5,23 +5,20 @@ module ActiveJob
     extend ActiveSupport::Concern
 
     included do
-      around_enqueue do |job, block|
-        if job.scheduled_at
-          ActiveSupport::Notifications.instrument("enqueue_at.active_job",
-            adapter: job.class.queue_adapter, job: job, &block)
-        else
-          ActiveSupport::Notifications.instrument("enqueue.active_job",
-            adapter: job.class.queue_adapter, job: job, &block)
-        end
+      around_enqueue do |_, block|
+        scheduled_at ? instrument(:enqueue_at, &block) : instrument(:enqueue, &block)
       end
 
-      around_perform do |job, block|
-        payload = { adapter: job.class.queue_adapter, job: job }
-        ActiveSupport::Notifications.instrument("perform_start.active_job", payload.dup)
-        ActiveSupport::Notifications.instrument("perform.active_job", payload) do
-          block.call
-        end
+      around_perform do |_, block|
+        instrument :perform_start
+        instrument :perform, &block
       end
     end
+
+    private
+      def instrument(operation, payload = {}, &block)
+        ActiveSupport::Notifications.instrument \
+          "#{operation}.active_job", payload.merge(adapter: self.class.queue_adapter, job: self), &block
+      end
   end
 end

commit c46475ed62598414ebf60c927c32f1f40023cd1f

Fix keyword arguments warnings caused by #37278
https://buildkite.com/rails/rails/builds/63817#de061bf7-9b91-484d-bdeb-c974f6bed01d/987-994

activejob/lib/active_job/exceptions.rb
@@ -115,7 +115,7 @@ module ActiveJob
     #    end
     #  end
     def retry_job(options = {})
-      instrument :enqueue_retry, **options.slice(:error, :wait) do
+      instrument :enqueue_retry, options.slice(:error, :wait) do
         enqueue options
       end
     end