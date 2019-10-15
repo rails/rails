240cd030c10263abfe274d66966a67abddb38ea5
glaszig <glaszig@gmail.com>
2019-10-15 03:58:30 +0200

    Fix Action Mailbox ingress config example [ci skip]

--- actionmailbox/lib/rails/generators/installer.rb
+++ actionmailbox/lib/rails/generators/installer.rb
@@ -5,6 +5,6 @@ copy_file "#{__dir__}/mailbox/templates/application_mailbox.rb", "app/mailboxes/
 
 environment <<~end_of_config, env: "production"
   # Prepare the ingress controller used to receive mail
-  # config.action_mailbox.ingress = :postfix
+  # config.action_mailbox.ingress = :relay
 
 end_of_config
