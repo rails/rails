6bc5bd26a2e2d277cfc9387fa8259030ffcec6c6
Ryuta Kamizono <kamipo@gmail.com>
2019-10-31 05:31:02 +0900

    Improve deprecation message for nested `where.not` condition

--- activerecord/lib/active_record/relation/query_methods.rb
+++ activerecord/lib/active_record/relation/query_methods.rb
@@ -52,7 +52,15 @@ module ActiveRecord
           ActiveSupport::Deprecation.warn(<<~MSG.squish)
             NOT conditions will no longer behave as NOR in Rails 6.1.
             To continue using NOR conditions, NOT each conditions manually
-            (`#{ opts.keys.map { |key| ".where.not(#{key.inspect} => ...)" }.join }`).
+            (`#{
+              opts.flat_map { |key, value|
+                if value.is_a?(Hash) && value.size > 1
+                  value.map { |k, v| ".where.not(#{key.inspect} => { #{k.inspect} => ... })" }
+                else
+                  ".where.not(#{key.inspect} => ...)"
+                end
+              }.join
+            }`).
           MSG
           @scope.where_clause += where_clause.invert(:nor)
         else
