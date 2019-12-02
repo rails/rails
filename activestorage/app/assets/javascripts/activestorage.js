7d2e4c386633fffb67b6d7e67646630fd74be77a
Bradley Rees <github@bradleyrees.com>
2019-12-02 10:17:25 +0800

    Rebuild ActiveStorage js lib

--- activestorage/app/assets/javascripts/activestorage.js
+++ activestorage/app/assets/javascripts/activestorage.js
@@ -936,6 +936,7 @@
   }
   setTimeout(autostart, 1);
   exports.DirectUpload = DirectUpload;
+  exports.dispatchEvent = dispatchEvent;
   exports.start = start;
   Object.defineProperty(exports, "__esModule", {
     value: true
