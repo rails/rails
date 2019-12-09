4fbb1e9f5bf1f22ad3aa913764b8ecb0c6e06c7d
Joel Hawksley <joel@hawksley.org>
2019-12-09 15:24:44 -0700

    Add test case

---
+++ actionpack/test/lib/test_component.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class TestComponent < ActionView::Base
+  include ActiveModel::Validations
+
+  validates :title, presence: true
+  delegate :render, to: :view_context
+
+  def initialize(title:)
+    @title = title
+  end
+
+  # Entrypoint for rendering. Called by ActionView::RenderingHelper#render.
+  #
+  # Returns ActionView::OutputBuffer.
+  def render_in(view_context)
+    self.class.compile
+    @view_context = view_context
+    validate!
+    rendered_template
+  end
+
+  def self.template
+    <<~'erb'
+    <span title="<%= title %>">(<%= render(plain: "Inline render") %>)</span>
+    erb
+  end
+
+  def self.compile
+    @compiled ||= nil
+    return if @compiled
+
+    class_eval(
+      "def rendered_template; @output_buffer = ActionView::OutputBuffer.new; " +
+      ActionView::Template::Handlers::ERB.erb_implementation.new(template, trim: true).src +
+      "; end"
+    )
+
+    @compiled = true
+  end
+
+private
+  attr_reader :title, :view_context
+end
