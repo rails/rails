9cf9493d59a02cc3b7d478f0f6afe5ef41136a82
Petrik <petrik@deheus.net>
2019-12-17 13:05:08 +0100

    Highlight code with Prism on 'turbolinks:load' [ci skip]

--- guides/assets/javascripts/guides.js
+++ guides/assets/javascripts/guides.js
@@ -29,6 +29,8 @@
   if (window.location.protocol === "file:") Turbolinks.supported = false;
 
   document.addEventListener("turbolinks:load", function() {
+    Prism.highlightAll();
+
     var guidesMenu = document.getElementById("guidesMenu");
     var guides     = document.getElementById("guides");
 
