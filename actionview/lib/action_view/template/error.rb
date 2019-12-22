65cc998a49b2b8b9c8965c24daedb9026eacc274
Kasper Timm Hansen <kaspth@gmail.com>
2019-12-22 08:56:29 +0100

    Merge pull request #38059 from eugeneius/source_extract_encode_template
    Encode template source before extracting lines

--- actionview/lib/action_view/template/error.rb
+++ actionview/lib/action_view/template/error.rb
@@ -85,7 +85,7 @@ module ActionView
         return [] unless num = line_number
         num = num.to_i
 
-        source_code = @template.source.split("\n")
+        source_code = @template.encode!.split("\n")
 
         start_on_line = [ num - SOURCE_CODE_RADIUS - 1, 0 ].max
         end_on_line   = [ num + SOURCE_CODE_RADIUS - 1, source_code.length].min
