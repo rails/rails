8c6add25524b7ec1b595d0f6dca60a2a17290d69
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-12-21 17:56:06 -0800

    Switch to a class style as per reviewer

--- activesupport/lib/active_support/configuration_file.rb
+++ activesupport/lib/active_support/configuration_file.rb
@@ -1,17 +1,25 @@
 # frozen_string_literal: true
 
 module ActiveSupport
-  module SettingFile
+  class ConfigurationFile
+    INVISIBILE_WHITESPACE = /\U+A0/
     # Reads a YAML file that might have ERB syntax, evaluates the ERB, and then
     # parses the resulting YAML.
     #
     # If there are characters that will confuse YAML (Like invisible
     # non-breaking spaces) it will warn you.
-    def read(content, context = nil)
-      if content.include?(/\U+A0/)
+    def initialize(content:, context: nil)
+      @content = content
+      @context = context
+    end
+
+    attr_reader :content
+    attr_reader :context
+
+    def read
+      if content.include?(INVISIBLE_WHITESPACE)
         warn "File contains invisible non-breaking spaces, you may want to remove those"
       end
-
       template = ERB.new(content)
 
       yaml = if context
@@ -24,6 +32,5 @@ module ActiveSupport
     rescue ArgumentError, Psych::SyntaxError => error
       raise Fixture::FormatError, "a YAML error occurred parsing #{path}. Please note that YAML must be consistently indented using spaces. Tabs are not allowed. Please have a look at https://www.yaml.org/faq.html\nThe exact error was:\n  #{error.class}: #{error}", error.backtrace
     end
-    module_function :read
   end
 end
