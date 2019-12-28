f5b6a74f9b560309a6b3207d3337c0ab4900c3f1
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-12-28 12:50:17 -0800

    Rename complete

--- activesupport/lib/active_support.rb
+++ activesupport/lib/active_support.rb
@@ -41,7 +41,7 @@ module ActiveSupport
   autoload :ExecutionWrapper
   autoload :Executor
   autoload :FileUpdateChecker
-  autoload :SettingFile
+  autoload :ConfigurationFile
   autoload :EventedFileUpdateChecker
   autoload :ForkTracker
   autoload :LogSubscriber
