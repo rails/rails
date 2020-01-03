1f938f5265b790d9c5449011863bd25f021ca161
John Crepezzi <john.crepezzi@gmail.com>
2020-01-03 14:38:30 -0500

    Pass env_name as a string in test databases
    In 154abca we switched from using `Rails.env` to fetch the `env_name` to
    `ActiveRecord::ConnectionHandling::DEFAULT_ENV.call.to_sym` which
    changed the type from a `String` to a `Symbol`.
    
    This commit brings things back to the original state, so we can find the
    configurations correctly!
    
    It also modifies the configuration in the configurations array, so that
    future connections can find the database with the updated keyword value.

--- activerecord/lib/active_record/test_databases.rb
+++ activerecord/lib/active_record/test_databases.rb
@@ -5,7 +5,7 @@ require "active_support/testing/parallelization"
 module ActiveRecord
   module TestDatabases # :nodoc:
     ActiveSupport::Testing::Parallelization.after_fork_hook do |i|
-      create_and_load_schema(i, env_name: ActiveRecord::ConnectionHandling::DEFAULT_ENV.call.to_sym)
+      create_and_load_schema(i, env_name: ActiveRecord::ConnectionHandling::DEFAULT_ENV.call)
     end
 
     def self.create_and_load_schema(i, env_name:)
@@ -20,7 +20,12 @@ module ActiveRecord
           db_config.configuration_hash.merge(database: database)
         )
 
+        # Reconstruct with the new configuration
         ActiveRecord::Tasks::DatabaseTasks.reconstruct_from_schema(db_config_copy, ActiveRecord::Base.schema_format, nil)
+
+        # Replace the original configuration with our replacement
+        ActiveRecord::Base.configurations.configurations.delete(db_config)
+        ActiveRecord::Base.configurations.configurations.push(db_config_copy)
       end
     ensure
       ActiveRecord::Base.establish_connection(ActiveRecord::ConnectionHandling::DEFAULT_ENV.call.to_sym)
