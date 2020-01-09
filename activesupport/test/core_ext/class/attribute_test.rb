rails/rails

commit 49d1b5a98dc50ca59bbfb817f90eb5c2c2645d30

Optimize class_attribute instance accessors

activesupport/lib/active_support/core_ext/class/attribute.rb
@@ -84,58 +84,49 @@ class Class
   # To set a default value for the attribute, pass <tt>default:</tt>, like so:
   #
   #   class_attribute :settings, default: {}
-  def class_attribute(
-    *attrs,
-    instance_accessor: true,
-    instance_reader: instance_accessor,
-    instance_writer: instance_accessor,
-    instance_predicate: true,
-    default: nil
-  )
+  def class_attribute(*attrs, instance_accessor: true,
+    instance_reader: instance_accessor, instance_writer: instance_accessor, instance_predicate: true, default: nil)
+
+    class_methods, methods = [], []
     attrs.each do |name|
-      singleton_class.silence_redefinition_of_method(name)
-      define_singleton_method(name) { default }
-
-      singleton_class.silence_redefinition_of_method("#{name}?")
-      define_singleton_method("#{name}?") { !!public_send(name) } if instance_predicate
-
-      ivar = "@#{name}".to_sym
-
-      singleton_class.silence_redefinition_of_method("#{name}=")
-      define_singleton_method("#{name}=") do |val|
-        redefine_singleton_method(name) { val }
-
-        if singleton_class?
-          class_eval do
-            redefine_method(name) do
-              if instance_variable_defined? ivar
-                instance_variable_get ivar
-              else
-                singleton_class.send name
-              end
-            end
-          end
-        end
-        val
+      unless name.is_a?(Symbol) || name.is_a?(String)
+        raise TypeError, "#{name.inspect} is not a symbol nor a string"
       end
 
-      if instance_reader
-        redefine_method(name) do
-          if instance_variable_defined?(ivar)
-            instance_variable_get ivar
-          else
-            self.class.public_send name
-          end
+      class_methods << <<~RUBY # In case the method exists and is not public
+        silence_redefinition_of_method def #{name}
         end
+      RUBY
 
-        redefine_method("#{name}?") { !!public_send(name) } if instance_predicate
-      end
-
-      if instance_writer
-        redefine_method("#{name}=") do |val|
-          instance_variable_set ivar, val
+      methods << <<~RUBY if instance_reader
+        silence_redefinition_of_method def #{name}
+          defined?(@#{name}) ? @#{name} : self.class.#{name}
         end
+      RUBY
+
+
+      class_methods << <<~RUBY
+        silence_redefinition_of_method def #{name}=(value)
+          redefine_method(:#{name}) { value } if singleton_class?
+          redefine_singleton_method(:#{name}) { value }
+          value
+        end
+      RUBY
+
+      methods << <<~RUBY if instance_writer
+        silence_redefinition_of_method(:#{name}=)
+        attr_writer :#{name}
+      RUBY
+
+      if instance_predicate
+        class_methods << "silence_redefinition_of_method def #{name}?; !!self.#{name}; end"
+        methods       << "silence_redefinition_of_method def #{name}?; !!self.#{name}; end"
       end
     end
+
+    location = caller_locations(1, 1).first
+    class_eval(["class << self", *class_methods, "end", *methods].join(";").tr("\n", ";"), location.path, location.lineno)
+
+    attrs.each { |name| public_send("#{name}=", default) }
   end
 end

activesupport/test/core_ext/class/attribute_test.rb
@@ -94,6 +94,16 @@ class ClassAttributeTest < ActiveSupport::TestCase
     assert_equal "foo", object.setting
   end
 
+  test "works well with module singleton classes" do
+    @module = Module.new do
+      class << self
+        class_attribute :settings, default: 42
+      end
+    end
+
+    assert_equal 42, @module.settings
+  end
+
   test "setter returns set value" do
     val = @klass.send(:setting=, 1)
     assert_equal 1, val

commit 0309179fb665f5f4628f2e0deb4050a75a9b742e

Do not define instance predicate if the instance reader isn't as well
This regressed in 49d1b5a98dc50ca59bbfb817f90eb5c2c2645d30.

I didn't notice it because the method would be defined but would
raise a NoMethodError.

activesupport/lib/active_support/core_ext/class/attribute.rb
@@ -120,7 +120,9 @@ class Class
 
       if instance_predicate
         class_methods << "silence_redefinition_of_method def #{name}?; !!self.#{name}; end"
-        methods       << "silence_redefinition_of_method def #{name}?; !!self.#{name}; end"
+        if instance_reader
+          methods << "silence_redefinition_of_method def #{name}?; !!self.#{name}; end"
+        end
       end
     end
 

activesupport/test/core_ext/class/attribute_test.rb
@@ -68,24 +68,31 @@ class ClassAttributeTest < ActiveSupport::TestCase
   test "disabling instance writer" do
     object = Class.new { class_attribute :setting, instance_writer: false }.new
     assert_raise(NoMethodError) { object.setting = "boom" }
+    assert_not_respond_to object, :setting=
   end
 
   test "disabling instance reader" do
     object = Class.new { class_attribute :setting, instance_reader: false }.new
     assert_raise(NoMethodError) { object.setting }
+    assert_not_respond_to object, :setting
     assert_raise(NoMethodError) { object.setting? }
+    assert_not_respond_to object, :setting?
   end
 
   test "disabling both instance writer and reader" do
     object = Class.new { class_attribute :setting, instance_accessor: false }.new
     assert_raise(NoMethodError) { object.setting }
+    assert_not_respond_to object, :setting
     assert_raise(NoMethodError) { object.setting? }
+    assert_not_respond_to object, :setting?
     assert_raise(NoMethodError) { object.setting = "boom" }
+    assert_not_respond_to object, :setting=
   end
 
   test "disabling instance predicate" do
     object = Class.new { class_attribute :setting, instance_predicate: false }.new
     assert_raise(NoMethodError) { object.setting? }
+    assert_not_respond_to object, :setting?
   end
 
   test "works well with singleton classes" do