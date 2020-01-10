fd2bc5cee6b8fc0fcb48a95e66c2ccd8c0c27e19
Akihito Tsukamoto <tsuka05@gmail.com>
2020-01-10 15:25:38 +0900

    Fix NoMethodError on ActiveSupport::Cache::RedisCacheStore#clear with Redis::Distributed

--- activesupport/lib/active_support/cache/redis_cache_store.rb
+++ activesupport/lib/active_support/cache/redis_cache_store.rb
@@ -246,10 +246,14 @@ module ActiveSupport
             pattern = namespace_key(matcher, options)
             cursor = "0"
             # Fetch keys in batches using SCAN to avoid blocking the Redis server.
-            begin
-              cursor, keys = c.scan(cursor, match: pattern, count: SCAN_BATCH_SIZE)
-              c.del(*keys) unless keys.empty?
-            end until cursor == "0"
+            nodes = c.respond_to?(:nodes) ? c.nodes : [c]
+
+            nodes.each do |node|
+              begin
+                cursor, keys = node.scan(cursor, match: pattern, count: SCAN_BATCH_SIZE)
+                node.del(*keys) unless keys.empty?
+              end until cursor == "0"
+            end
           end
         end
       end
