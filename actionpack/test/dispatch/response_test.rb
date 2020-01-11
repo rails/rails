85ce984765f7b1b3fe7d571650ceb20a7dc8f976
Ryuta Kamizono <kamipo@gmail.com>
2020-01-12 06:01:39 +0900

    Merge pull request #38216 from kamipo/fix_failure_for_rack-2.1.0
    rack 2.1.0 changed expires date format from rfc2822 to httpdate

--- actionpack/test/dispatch/response_test.rb
+++ actionpack/test/dispatch/response_test.rb
@@ -238,7 +238,7 @@ class ResponseTest < ActiveSupport::TestCase
     @response.set_cookie("user_name", value: "david", path: "/")
     @response.set_cookie("login", value: "foo&bar", path: "/", expires: Time.utc(2005, 10, 10, 5))
     _status, headers, _body = @response.to_a
-    assert_equal "user_name=david; path=/\nlogin=foo%26bar; path=/; expires=Mon, 10 Oct 2005 05:00:00 -0000", headers["Set-Cookie"]
+    assert_equal "user_name=david; path=/\nlogin=foo%26bar; path=/; expires=Mon, 10 Oct 2005 05:00:00 GMT", headers["Set-Cookie"]
     assert_equal({ "login" => "foo&bar", "user_name" => "david" }, @response.cookies)
   end
 
