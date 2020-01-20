289d3db3327fa9cc9707d7686d6b78c90df8637f
Ryuta Kamizono <kamipo@gmail.com>
2020-01-21 00:16:02 +0900

    Defined attribute should not expand positional hash argument

--- activemodel/test/cases/attribute_methods_test.rb
+++ activemodel/test/cases/attribute_methods_test.rb
@@ -33,7 +33,9 @@ private
     attributes[name.to_s]
   end
 
-  alias attribute_test attribute
+  def attribute_test(name, attrs = {})
+    attrs[name] = attribute(name)
+  end
 
   def private_method
     "<3 <3"
@@ -212,9 +214,25 @@ class AttributeMethodsTest < ActiveModel::TestCase
   test "accessing a suffixed attribute" do
     m = ModelWithAttributes2.new
     m.attributes = { "foo" => "bar" }
+    attrs = {}
 
     assert_equal "bar", m.foo
-    assert_equal "bar", m.foo_test
+    assert_equal "bar", m.foo_test(attrs)
+    assert_equal "bar", attrs["foo"]
+  end
+
+  test "defined attribute doesn't expand positional hash argument" do
+    ModelWithAttributes2.define_attribute_methods(:foo)
+
+    m = ModelWithAttributes2.new
+    m.attributes = { "foo" => "bar" }
+    attrs = {}
+
+    assert_equal "bar", m.foo
+    assert_equal "bar", m.foo_test(attrs)
+    assert_equal "bar", attrs["foo"]
+  ensure
+    ModelWithAttributes2.undefine_attribute_methods
   end
 
   test "should not interfere with method_missing if the attr has a private/protected method" do
