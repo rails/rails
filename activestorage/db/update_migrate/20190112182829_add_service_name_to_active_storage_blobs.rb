7fc2db646c1d955936046777847f078eb79a5587
tbuehl <tbuehl@itm-labs.de>
2020-02-03 19:18:30 +0100

    Skip ActiveStorage migrations if ActiveStorage tables don't exist
    rails app:update copies ActiveStorage migrations even if ActiveStorage hasn't been installed. Therefore the the migrations should only run if the active_storage_blobs table exists

--- activestorage/db/update_migrate/20190112182829_add_service_name_to_active_storage_blobs.rb
+++ activestorage/db/update_migrate/20190112182829_add_service_name_to_active_storage_blobs.rb
@@ -1,13 +1,14 @@
 class AddServiceNameToActiveStorageBlobs < ActiveRecord::Migration[6.0]
   def up
-    unless column_exists?(:active_storage_blobs, :service_name)
-      add_column :active_storage_blobs, :service_name, :string
+    return unless table_exists?(:active_storage_blobs)
+    return if column_exists?(:active_storage_blobs, :service_name)
 
-      if configured_service = ActiveStorage::Blob.service.name
-        ActiveStorage::Blob.unscoped.update_all(service_name: configured_service)
-      end
+    add_column :active_storage_blobs, :service_name, :string
 
-      change_column :active_storage_blobs, :service_name, :string, null: false
+    if configured_service = ActiveStorage::Blob.service.name
+      ActiveStorage::Blob.unscoped.update_all(service_name: configured_service)
     end
+
+    change_column :active_storage_blobs, :service_name, :string, null: false
   end
 end
