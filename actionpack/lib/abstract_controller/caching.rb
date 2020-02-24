rails/rails

commit f9633cd06798ffcc661dd375551a5de86258ad3a

Fix Ruby 2.7 keyword argument warnings in Active Support

activesupport/lib/active_support/cache.rb
@@ -52,12 +52,12 @@ module ActiveSupport
       #
       #   ActiveSupport::Cache.lookup_store(MyOwnCacheStore.new)
       #   # => returns MyOwnCacheStore.new
-      def lookup_store(*store_option)
-        store, *parameters = *Array.wrap(store_option).flatten
+      def lookup_store(*store_args, **store_options)
+        store, *parameters = *Array.wrap(store_args).flatten
 
         case store
         when Symbol
-          retrieve_store_class(store).new(*parameters)
+          retrieve_store_class(store).new(*parameters, **store_options)
         when nil
           ActiveSupport::Cache::MemoryStore.new
         else

activesupport/lib/active_support/deprecation/proxy_wrappers.rb
@@ -121,7 +121,7 @@ module ActiveSupport
     #        (Backtrace information…)
     #        ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
     class DeprecatedConstantProxy < Module
-      def self.new(*args, &block)
+      def self.new(*args, **kwargs, &block)
         object = args.first
 
         return object unless object

activesupport/lib/active_support/encrypted_file.rb
@@ -94,7 +94,7 @@ module ActiveSupport
       end
 
       def handle_missing_key
-        raise MissingKeyError, key_path: key_path, env_key: env_key if raise_if_missing_key
+        raise MissingKeyError.new(key_path: key_path, env_key: env_key) if raise_if_missing_key
       end
   end
 end

activesupport/lib/active_support/message_encryptor.rb
@@ -172,7 +172,7 @@ module ActiveSupport
         iv = cipher.random_iv
         cipher.auth_data = "" if aead_mode?
 
-        encrypted_data = cipher.update(Messages::Metadata.wrap(@serializer.dump(value), metadata_options))
+        encrypted_data = cipher.update(Messages::Metadata.wrap(@serializer.dump(value), **metadata_options))
         encrypted_data << cipher.final
 
         blob = "#{::Base64.strict_encode64 encrypted_data}--#{::Base64.strict_encode64 iv}"

activesupport/lib/active_support/message_verifier.rb
@@ -172,8 +172,8 @@ module ActiveSupport
     #
     #   other_verifier = ActiveSupport::MessageVerifier.new 'd1ff3r3nt-s3Krit'
     #   other_verifier.verify(signed_message) # => ActiveSupport::MessageVerifier::InvalidSignature
-    def verify(*args)
-      verified(*args) || raise(InvalidSignature)
+    def verify(*args, **options)
+      verified(*args, **options) || raise(InvalidSignature)
     end
 
     # Generates a signed message for the provided value.

activesupport/lib/active_support/messages/rotator.rb
@@ -20,12 +20,12 @@ module ActiveSupport
         def decrypt_and_verify(*args, on_rotation: nil, **options)
           super
         rescue MessageEncryptor::InvalidMessage, MessageVerifier::InvalidSignature
-          run_rotations(on_rotation) { |encryptor| encryptor.decrypt_and_verify(*args, options) } || raise
+          run_rotations(on_rotation) { |encryptor| encryptor.decrypt_and_verify(*args, **options) } || raise
         end
 
         private
           def build_rotation(secret = @secret, sign_secret = @sign_secret, options)
-            self.class.new(secret, sign_secret, options)
+            self.class.new(secret, sign_secret, **options)
           end
       end
 
@@ -33,12 +33,12 @@ module ActiveSupport
         include Rotator
 
         def verified(*args, on_rotation: nil, **options)
-          super || run_rotations(on_rotation) { |verifier| verifier.verified(*args, options) }
+          super || run_rotations(on_rotation) { |verifier| verifier.verified(*args, **options) }
         end
 
         private
           def build_rotation(secret = @secret, options)
-            self.class.new(secret, options)
+            self.class.new(secret, **options)
           end
       end
 

activesupport/lib/active_support/multibyte/chars.rb
@@ -122,7 +122,7 @@ module ActiveSupport #:nodoc:
       #
       #   'こんにちは'.mb_chars.limit(7).to_s # => "こん"
       def limit(limit)
-        truncate_bytes(limit, omission: nil)
+        chars(@wrapped_string.truncate_bytes(limit, omission: nil))
       end
 
       # Capitalizes the first letter of every word, when possible.

activesupport/lib/active_support/number_helper/number_converter.rb
@@ -162,11 +162,11 @@ module ActiveSupport
         end
 
         def translate_number_value_with_default(key, **i18n_options)
-          I18n.translate(key, **{default: default_value(key), scope: :number}.merge!(i18n_options))
+          I18n.translate(key, **{ default: default_value(key), scope: :number }.merge!(i18n_options))
         end
 
         def translate_in_locale(key, **i18n_options)
-          translate_number_value_with_default(key, **{locale: options[:locale]}.merge(i18n_options))
+          translate_number_value_with_default(key, **{ locale: options[:locale] }.merge(i18n_options))
         end
 
         def default_value(key)

commit 5813aac648c86068c38f1c146ed15c261b41985b

Simplify `lookup_store`
This fixes a regression #38532 caused by f9633cd0.

Closes #38532.

actionpack/lib/abstract_controller/caching.rb
@@ -15,7 +15,7 @@ module AbstractController
       end
 
       def cache_store=(store)
-        config.cache_store = ActiveSupport::Cache.lookup_store(store)
+        config.cache_store = ActiveSupport::Cache.lookup_store(*store)
       end
 
       private

activesupport/lib/active_support/cache.rb
@@ -52,12 +52,11 @@ module ActiveSupport
       #
       #   ActiveSupport::Cache.lookup_store(MyOwnCacheStore.new)
       #   # => returns MyOwnCacheStore.new
-      def lookup_store(*store_args, **store_options)
-        store, *parameters = *Array.wrap(store_args).flatten
-
+      def lookup_store(store = nil, *parameters)
         case store
         when Symbol
-          retrieve_store_class(store).new(*parameters, **store_options)
+          options = parameters.extract_options!
+          retrieve_store_class(store).new(*parameters, **options)
         when nil
           ActiveSupport::Cache::MemoryStore.new
         else

railties/lib/rails/application/bootstrap.rb
@@ -56,7 +56,7 @@ config.eager_load is set to nil. Please update your config/environments/*.rb fil
       # Initialize cache early in the stack so railties can make use of it.
       initializer :initialize_cache, group: :all do
         unless Rails.cache
-          Rails.cache = ActiveSupport::Cache.lookup_store(config.cache_store)
+          Rails.cache = ActiveSupport::Cache.lookup_store(*config.cache_store)
 
           if Rails.cache.respond_to?(:middleware)
             config.middleware.insert_before(::Rack::Runtime, Rails.cache.middleware)

railties/test/application/middleware_test.rb
@@ -238,9 +238,10 @@ module ApplicationTests
     end
 
     test "Rails.cache does not respond to middleware" do
-      add_to_config "config.cache_store = :memory_store"
+      add_to_config "config.cache_store = :memory_store, { timeout: 10 }"
       boot!
       assert_equal "Rack::Runtime", middleware[5]
+      assert_instance_of ActiveSupport::Cache::MemoryStore, Rails.cache
     end
 
     test "Rails.cache does respond to middleware" do