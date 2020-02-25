67b1a7942e8b0bc2f30af78bbcb81c535a87e5dc
Aaron Patterson <aaron.patterson@gmail.com>
2020-02-25 13:52:45 -0800

    remove useless conditional

--- actionview/lib/action_view/renderer/partial_renderer.rb
+++ actionview/lib/action_view/renderer/partial_renderer.rb
@@ -353,7 +353,6 @@ module ActionView
             layout = find_template(layout.to_s, template_keys)
           end
 
-          object = locals[as] if object.nil? # Respect object when object is false
           locals[as] = object if @has_object
 
           content = template.render(view, locals) do |*name|
