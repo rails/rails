5dc56ecb606963294433d60731cdb73424fb8c6c
Joel Hawksley <joel@hawksley.org>
2020-03-05 14:37:39 -0700

    simplify example components

--- actionpack/test/lib/test_component.rb
+++ actionpack/test/lib/test_component.rb
@@ -1,41 +1,11 @@
 # frozen_string_literal: true
 
-class TestComponent < ActionView::Base
-  delegate :render, to: :view_context
-
-  def initialize(title:)
-    @title = title
-  end
-
-  def render_in(view_context)
-    self.class.compile
-    @view_context = view_context
-    rendered_template
+class TestComponent
+  def render_in(_view_context)
+    "Hello, World!"
   end
 
   def format
     :html
   end
-
-  def self.template
-    <<~'erb'
-    <span title="<%= @title %>">(<%= render(plain: "Inline render") %>)</span>
-    erb
-  end
-
-  def self.compile
-    @compiled ||= nil
-    return if @compiled
-
-    class_eval(
-      "def rendered_template; @output_buffer = ActionView::OutputBuffer.new; " +
-      ActionView::Template::Handlers::ERB.erb_implementation.new(template, trim: true).src +
-      "; end"
-    )
-
-    @compiled = true
-  end
-
-private
-  attr_reader :view_context
 end
