rails/rails

commit fa986ae0cac423bf1ebcb5caeccbecf00c990094

Simplify `lookup_store`
I think allowing nested array parameters is almost useless.

activesupport/lib/active_support/cache.rb
@@ -53,8 +53,7 @@ module ActiveSupport
       #
       #   ActiveSupport::Cache.lookup_store(MyOwnCacheStore.new)
       #   # => returns MyOwnCacheStore.new
-      def lookup_store(*store_option)
-        store, *parameters = *Array.wrap(store_option).flatten
+      def lookup_store(store = nil, *parameters)
 
         case store
         when Symbol

railties/lib/rails/application/bootstrap.rb
@@ -56,7 +56,7 @@ module Rails
       # Initialize cache early in the stack so railties can make use of it.
       initializer :initialize_cache, group: :all do
         unless Rails.cache
-          Rails.cache = ActiveSupport::Cache.lookup_store(config.cache_store)
+          Rails.cache = ActiveSupport::Cache.lookup_store(*config.cache_store)
 
           if Rails.cache.respond_to?(:middleware)
             config.middleware.insert_before(::Rack::Runtime, Rails.cache.middleware)

railties/test/application/middleware_test.rb
@@ -239,9 +239,10 @@ module ApplicationTests
     end
 
     test "Rails.cache does not respond to middleware" do
-      add_to_config "config.cache_store = :memory_store"
+      add_to_config "config.cache_store = :memory_store, { timeout: 10 }"
       boot!
       assert_equal "Rack::Runtime", middleware[5]
+      assert_instance_of ActiveSupport::Cache::MemoryStore, Rails.cache
     end
 
     test "Rails.cache does respond to middleware" do

commit c728f8ec979082e17770624dc67f5f858c9efe23

Restore the `lookup_store` compatibility to accept config as a single object
fa986ae broke the `lookup_store` compatibility. It's public API so
deprecation cycle is required if we want to make a breaking change.

activesupport/lib/active_support/cache.rb
@@ -59,6 +59,8 @@ module ActiveSupport
         when Symbol
           options = parameters.extract_options!
           retrieve_store_class(store).new(*parameters, **options)
+        when Array
+          lookup_store(*store)
         when nil
           ActiveSupport::Cache::MemoryStore.new
         else

activesupport/test/cache/cache_store_setting_test.rb
@@ -66,6 +66,14 @@ class CacheStoreSettingTest < ActiveSupport::TestCase
     assert_equal "/path/to/cache/directory", store.cache_path
   end
 
+  def test_redis_cache_store_with_single_array_object
+    cache_store = [:redis_cache_store, namespace: "foo"]
+
+    store = ActiveSupport::Cache.lookup_store(cache_store)
+    assert_kind_of ActiveSupport::Cache::RedisCacheStore, store
+    assert_equal "foo", store.options[:namespace]
+  end
+
   def test_redis_cache_store_with_ordered_options
     options = ActiveSupport::OrderedOptions.new
     options.update namespace: "foo"