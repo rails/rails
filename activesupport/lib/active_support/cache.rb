5665d0867bb34b941db778630d558c7694bb1506
Ryuta Kamizono <kamipo@gmail.com>
2020-03-25 09:06:54 +0900

    Merge pull request #38810 from kamipo/restore_compatibility_for_lookup_store
    Restore the `lookup_store` compatibility to accept config as a single object

--- activesupport/lib/active_support/cache.rb
+++ activesupport/lib/active_support/cache.rb
@@ -57,6 +57,8 @@ module ActiveSupport
         when Symbol
           options = parameters.extract_options!
           retrieve_store_class(store).new(*parameters, **options)
+        when Array
+          lookup_store(*store)
         when nil
           ActiveSupport::Cache::MemoryStore.new
         else
