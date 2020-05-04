dd870dd029ae891cbd112f49c15c575ebf03288f
Jack McCracken <jack.mccracken@shopify.com>
2020-05-04 13:56:32 -0400

    Add a separator to the cache key for ActiveSupport::CachingKeyGenerator

--- activesupport/test/key_generator_test.rb
+++ activesupport/test/key_generator_test.rb
@@ -71,6 +71,13 @@ else
 
       assert_not_equal derived_key, different_length_key
     end
+
+    test "Does not cache key for different salts and lengths that are different but are equal when concatenated" do
+      derived_key = @caching_generator.generate_key("13", 37)
+      different_length_key = @caching_generator.generate_key("1", 337)
+
+      assert_not_equal derived_key, different_length_key
+    end
   end
 
 end
