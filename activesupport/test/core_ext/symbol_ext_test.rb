343638d7d95821a0c8c7c883c93535f23b8c60bc
Eugene Kenny <elkenny@gmail.com>
2020-05-06 03:04:42 +0100

    Preserve multiple arguments in Symbol#start_with? and Symbol#end_with?
    Followup to 62a20c2a642a2ae6ac7c265af1fe8fdf33114bf4, for consistency
    with the upstream implementation.

--- activesupport/test/core_ext/symbol_ext_test.rb
+++ activesupport/test/core_ext/symbol_ext_test.rb
@@ -9,10 +9,14 @@ class SymbolStartsEndsWithTest < ActiveSupport::TestCase
     assert s.start_with?("h")
     assert s.start_with?("hel")
     assert_not s.start_with?("el")
+    assert s.start_with?("he", "lo")
+    assert_not s.start_with?("el", "lo")
 
     assert s.end_with?("o")
     assert s.end_with?("lo")
     assert_not s.end_with?("el")
+    assert s.end_with?("he", "lo")
+    assert_not s.end_with?("he", "ll")
   end
 
   def test_starts_ends_with_alias
@@ -20,9 +24,13 @@ class SymbolStartsEndsWithTest < ActiveSupport::TestCase
     assert s.starts_with?("h")
     assert s.starts_with?("hel")
     assert_not s.starts_with?("el")
+    assert s.starts_with?("he", "lo")
+    assert_not s.starts_with?("el", "lo")
 
     assert s.ends_with?("o")
     assert s.ends_with?("lo")
     assert_not s.ends_with?("el")
+    assert s.ends_with?("he", "lo")
+    assert_not s.ends_with?("he", "ll")
   end
 end
