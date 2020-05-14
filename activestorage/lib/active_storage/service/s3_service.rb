e8df5648515a0e8324d3b3c4bdb7bde6802cd8be
Travis Pew <travisp@gmail.com>
2020-05-14 11:17:46 -0400

    Include Content-Length in signature for ActiveStorage direct upload
    [CVE-2020-8162]

--- activestorage/lib/active_storage/service/s3_service.rb
+++ activestorage/lib/active_storage/service/s3_service.rb
@@ -79,7 +79,8 @@ module ActiveStorage
     def url_for_direct_upload(key, expires_in:, content_type:, content_length:, checksum:)
       instrument :url, key: key do |payload|
         generated_url = object_for(key).presigned_url :put, expires_in: expires_in.to_i,
-          content_type: content_type, content_length: content_length, content_md5: checksum
+          content_type: content_type, content_length: content_length, content_md5: checksum,
+          whitelist_headers: ['content-length']
 
         payload[:url] = generated_url
 
