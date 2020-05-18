08edf165ae52aa22a8ba90de86729e1a4423459c
Ryuta Kamizono <kamipo@gmail.com>
2020-05-19 01:48:00 +0900

    Fix "Exception: ActionView::Template::Error: wrong number of arguments (2 for 1)"

--- actionpack/lib/action_controller/metal/request_forgery_protection.rb
+++ actionpack/lib/action_controller/metal/request_forgery_protection.rb
@@ -321,11 +321,6 @@ module ActionController #:nodoc:
           global_csrf_token(session)
         end
 
-        one_time_pad = SecureRandom.random_bytes(AUTHENTICITY_TOKEN_LENGTH)
-        encrypted_csrf_token = xor_byte_strings(one_time_pad, raw_token)
-        masked_token = one_time_pad + encrypted_csrf_token
-        Base64.urlsafe_encode64(masked_token, padding: false)
-
         mask_token(raw_token)
       end
 
@@ -376,7 +371,7 @@ module ActionController #:nodoc:
         one_time_pad = SecureRandom.random_bytes(AUTHENTICITY_TOKEN_LENGTH)
         encrypted_csrf_token = xor_byte_strings(one_time_pad, raw_token)
         masked_token = one_time_pad + encrypted_csrf_token
-        Base64.strict_encode64(masked_token)
+        Base64.urlsafe_encode64(masked_token).delete("=")
       end
 
       def compare_with_real_token(token, session) # :doc:
