1494fadd0df6d29ead33280977786e694b37bf75
Ryuta Kamizono <kamipo@gmail.com>
2020-05-19 02:56:09 +0900

    Fix `test_two_classes_autoloading` failure
    That test requires Marshal is prepended by `MarshalWithAutoloading`.

--- activesupport/lib/active_support/cache/mem_cache_store.rb
+++ activesupport/lib/active_support/cache/mem_cache_store.rb
@@ -8,6 +8,7 @@ rescue LoadError => e
 end
 
 require "active_support/core_ext/enumerable"
+require "active_support/core_ext/marshal"
 require "active_support/core_ext/array/extract_options"
 
 module ActiveSupport
