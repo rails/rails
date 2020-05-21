rails/rails

commit 8538dfdc084555673d18cfc3479ebef09f325c9c

Reduce allocations in to_json's include option
Since 7b3919774252f99e55e6b6ec370aafc42adca2b2, `serializable_hash`
allocates fewer objects when options is nil rather than empty.

activemodel/lib/active_model/serialization.rb
@@ -179,7 +179,7 @@ module ActiveModel
         return unless includes = options[:include]
 
         unless includes.is_a?(Hash)
-          includes = Hash[Array(includes).flat_map { |n| n.is_a?(Hash) ? n.to_a : [[n, {}]] }]
+          includes = Hash[Array(includes).flat_map { |n| n.is_a?(Hash) ? n.to_a : [[n, nil]] }]
         end
 
         includes.each do |association, opts|

commit 7e14c16cc099c9effeedcb097484c7c3b64e15f9

Optimise serializable_hash when options are empty
This reverts 8538dfdc084555673d18cfc3479ebef09f325c9c, which broke the
activemodel-serializers-xml gem.

We can still get most of the benefit by applying the optimisation from
7b3919774252f99e55e6b6ec370aafc42adca2b2 to empty hashes as well as nil.
This has the additional benefit of retaining the optimisation when the
user passes an empty options hash.

activemodel/lib/active_model/serialization.rb
@@ -123,7 +123,7 @@ module ActiveModel
     def serializable_hash(options = nil)
       attribute_names = attributes.keys
 
-      return serializable_attributes(attribute_names) unless options
+      return serializable_attributes(attribute_names) if options.blank?
 
       if only = options[:only]
         attribute_names &= Array(only).map(&:to_s)
@@ -179,7 +179,7 @@ module ActiveModel
         return unless includes = options[:include]
 
         unless includes.is_a?(Hash)
-          includes = Hash[Array(includes).flat_map { |n| n.is_a?(Hash) ? n.to_a : [[n, nil]] }]
+          includes = Hash[Array(includes).flat_map { |n| n.is_a?(Hash) ? n.to_a : [[n, {}]] }]
         end
 
         includes.each do |association, opts|