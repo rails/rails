d02d259b6128423a0ddf4118a7143c3e7b3cd22f
Jonathan Hefner <jonathan@hefner.pro>
2020-05-25 21:03:32 -0500

    Fix Active Storage behavior on record dup
    Prior to this commit, a `dup`ed record and its originating record would
    share the same `Attached` proxy objects.  Those proxy objects, in turn,
    would point to the same `Attachment` associations, causing changes made
    through the proxy interface to affect both records.

--- activestorage/lib/active_storage/attached/model.rb
+++ activestorage/lib/active_storage/attached/model.rb
@@ -46,7 +46,8 @@ module ActiveStorage
         generated_association_methods.class_eval <<-CODE, __FILE__, __LINE__ + 1
           # frozen_string_literal: true
           def #{name}
-            @active_storage_attached_#{name} ||= ActiveStorage::Attached::One.new("#{name}", self)
+            @active_storage_attached ||= {}
+            @active_storage_attached[:#{name}] ||= ActiveStorage::Attached::One.new("#{name}", self)
           end
 
           def #{name}=(attachable)
@@ -116,7 +117,8 @@ module ActiveStorage
         generated_association_methods.class_eval <<-CODE, __FILE__, __LINE__ + 1
           # frozen_string_literal: true
           def #{name}
-            @active_storage_attached_#{name} ||= ActiveStorage::Attached::Many.new("#{name}", self)
+            @active_storage_attached ||= {}
+            @active_storage_attached[:#{name}] ||= ActiveStorage::Attached::Many.new("#{name}", self)
           end
 
           def #{name}=(attachables)
@@ -181,6 +183,12 @@ module ActiveStorage
 
     def changed_for_autosave? #:nodoc:
       super || attachment_changes.any?
+    end
+
+    def initialize_dup(*) #:nodoc:
+      super
+      @active_storage_attached = nil
+      @attachment_changes = nil
     end
 
     def reload(*) #:nodoc:
