2f29cf97340817b70de1be27b52fe5c7581fd551
Evgeniy Rashchepkin <e.a.rashchepkin@gmail.com>
2020-06-21 21:31:57 +0500

    Fix issue #39286 (about digesting in ActiveStorage)
    Error entries appear in the log when we request ActiveStorage
    controllers (`ActiveStorage::Representations::ProxyController#show`,
    `ActiveStorage::Blobs::ProxyController#show`).
    
    These entries look like: "Couldn't find template for digesting:
    active_storage/representations/proxy/show".
    
    These controllers use the method
    `ActionController::ConditionalGet#http_cache_forever`,
    and therefore `ActionController::ConditionalGet#combine_etags` method,
    and therefore `ActionController::EtagWithTemplateDigest` module via
    `etaggers` array.
    
    `ActionController::EtagWithTemplateDigest` module requires a template
    (view).
    
    We have no views in ActiveStorage, so `EtagWithTemplateDigest` is
    now turned off in ActiveStorage controllers by
    `etag_with_template_digest` class attribute.

--- activestorage/app/controllers/active_storage/base_controller.rb
+++ activestorage/app/controllers/active_storage/base_controller.rb
@@ -6,6 +6,8 @@ class ActiveStorage::BaseController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  self.etag_with_template_digest = false
+
   private
     def stream(blob)
       blob.download do |chunk|
