c88a5e1b4fbd58b1512ac6077a488ab2fcabcd50
Mike Bijon <mbijon@users.noreply.github.com>
2020-06-30 12:00:59 -0700

    Add tests to confirm XSS vector prevented in JS helper
    Confirms dollar signs and backticks are escaped when using the `j` or `javascript_escape` helper. For CVE-2020-5267

--- actionview/test/template/javascript_helper_test.rb
+++ actionview/test/template/javascript_helper_test.rb
@@ -32,6 +32,14 @@ class JavaScriptHelperTest < ActionView::TestCase
 
     assert_equal %(dont <\\/close> tags), j(%(dont </close> tags))
   end
+  
+  def test_escape_backtick
+    assert_equal "\\`", escape_javascript("`")
+  end
+
+  def test_escape_dollar_sign
+    assert_equal "\\$", escape_javascript("$")
+  end
 
   def test_escape_javascript_with_safebuffer
     given = %('quoted' "double-quoted" new-line:\n </closed>)
