2f618052fac5c7fab0c1280372e983938b72e13a
fedeagripa <federicogagripa@gmail.com>
2020-07-02 16:44:44 -0300

    remove test broking others

--- activerecord/test/cases/reflection_test.rb
+++ activerecord/test/cases/reflection_test.rb
@@ -190,14 +190,6 @@ class ReflectionTest < ActiveRecord::TestCase
     assert_equal "accounts", Firm.reflect_on_association(:account).table_name
   end
 
-  def test_belongs_to_inferred_foreign_key_from_assoc_name
-    Company.belongs_to :foo
-    assert_equal "foo_id", Company.reflect_on_association(:foo).foreign_key
-    Company.belongs_to :bar, class_name: "Xyzzy"
-    assert_equal "bar_id", Company.reflect_on_association(:bar).foreign_key
-    Company.belongs_to :baz, class_name: "Xyzzy", foreign_key: "xyzzy_id"
-    assert_equal "xyzzy_id", Company.reflect_on_association(:baz).foreign_key
-  end
 
   def test_association_reflection_in_modules
     ActiveRecord::Base.store_full_sti_class = false
