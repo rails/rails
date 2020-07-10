a7ed92bd1d92127423ad6dce608d68d451a26821
Petrik <petrik@deheus.net>
2020-07-10 10:20:22 +0200

    Use superclass name in inspect of ActionView::Base.with_empty_template_cache
    When rendering views an anonymous subclass is created by calling
    ActionView::Base.with_empty_template_cache.
    This causes inspect to return an unhelpful description when calling
    inspect:
    `#<#<Class:0x012345012345>:<0x012345012345>`.
    
    This can be confusing when exceptions are raised because it's hard to
    figure out where to look. For example calling an undefined method in a
    template would raise the following exception:
    
        undefined method `undefined' for #<#<Class:0x012345012345>:<0x012345012345>
    
    Instead we can return the non-anonymous superclass name.
    
        undefined method `undefined' for #<ActionView::Base:0x01234502345>
    
    The anonymous class is created in ActionView::Base.with_empty_template_cache.
    See f9bea6304dfba902b1937b3bc29b1ebc2f67e55b
    This seems to be done for performance reasons only, without expecting a
    change to calling `inspect`.

--- actionview/lib/action_view/base.rb
+++ actionview/lib/action_view/base.rb
@@ -190,6 +190,14 @@ module ActionView #:nodoc:
           # correctly.
           define_method(:compiled_method_container)           { subclass }
           define_singleton_method(:compiled_method_container) { subclass }
+
+          def self.name
+            superclass.name
+          end
+
+          def inspect
+            "#<#{self.class.name}:#{'%#016x' % (object_id << 1)}>"
+          end
         }
       end
 
