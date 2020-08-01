778dc41d8d16be7917a3827842cf00820da8904b
Chirag <chirag@bigbinary.com>
2020-08-01 17:16:13 +0530

    Fixes to_query when value is nil
    Makes the output from Hash#to_query parseable back into the same values as were given, with respect to nil values.
    
    hash = { a: nil, b: 'abc' }
    query = hash.to_query
    # => "a&b=abc"
    
    Rack::Utils.parse_nested_query(query)
    # => {"a"=>nil, "b"=>"abc"}

--- activesupport/lib/active_support/core_ext/object/to_query.rb
+++ activesupport/lib/active_support/core_ext/object/to_query.rb
@@ -11,7 +11,7 @@ class Object
   # Converts an object into a string suitable for use as a URL query string,
   # using the given <tt>key</tt> as the param name.
   def to_query(key)
-    "#{CGI.escape(key.to_param)}=#{CGI.escape(to_param.to_s)}"
+    self.nil? ? "#{CGI.escape(key.to_param)}" : "#{CGI.escape(key.to_param)}=#{CGI.escape(to_param.to_s)}"
   end
 end
 
