5bc24ed2b232454d74e94be97c75f289a09e8935
Ryuta Kamizono <kamipo@gmail.com>
2020-08-22 12:39:32 +0900

    Fix keyword argument warning for Ruby 2.6
    https://buildkite.com/rails/rails/builds/71112#a57253a9-6088-4f76-ad75-0df8cf3ba356/1006-1014

--- activerecord/lib/active_record/validations/associated.rb
+++ activerecord/lib/active_record/validations/associated.rb
@@ -5,7 +5,7 @@ module ActiveRecord
     class AssociatedValidator < ActiveModel::EachValidator #:nodoc:
       def validate_each(record, attribute, value)
         if Array(value).reject { |r| valid_object?(r) }.any?
-          record.errors.add(attribute, :invalid, **options.merge(value: value))
+          record.errors.add(attribute, :invalid, options.merge(value: value))
         end
       end
 
