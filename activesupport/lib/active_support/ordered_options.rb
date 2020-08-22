a12a2707acf948eccda85fd0ef9a68940031dd09
Akshay Birajdar <akshayb@joshsoftware.com>
2020-08-23 00:38:55 +0530

    Implement #inspect for ActiveSupport::OrderedOptions

--- activesupport/lib/active_support/ordered_options.rb
+++ activesupport/lib/active_support/ordered_options.rb
@@ -62,6 +62,10 @@ module ActiveSupport
     def extractable_options?
       true
     end
+
+    def inspect
+      "#<#{self.class.name} #{self}>"
+    end
   end
 
   # +InheritableOptions+ provides a constructor to build an +OrderedOptions+
