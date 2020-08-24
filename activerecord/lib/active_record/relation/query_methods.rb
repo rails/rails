ffb6fa093380a44f6f18722f849c01d045fd23d8
Jean Boussier <jean.boussier@gmail.com>
2020-08-24 10:54:08 +0200

    Call uniq! on the `reorder` arguments.
    Before 29874cc4e220edadfdf50ba93db57d2df396e395 the order values
    would be deduplicated when the SQL query is generated, but now
    it has to be eager.

--- activerecord/lib/active_record/relation/query_methods.rb
+++ activerecord/lib/active_record/relation/query_methods.rb
@@ -405,6 +405,7 @@ module ActiveRecord
     # Same as #reorder but operates on relation in-place instead of copying.
     def reorder!(*args) # :nodoc:
       preprocess_order_args(args) unless args.all?(&:blank?)
+      args.uniq!
       self.reordering_value = true
       self.order_values = args
       self
