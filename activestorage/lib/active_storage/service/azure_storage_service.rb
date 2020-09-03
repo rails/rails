848348b050fc98d3b0bbe686fb806ce4073006af
Taylor Kearns <tkearns@gmail.com>
2020-09-03 11:31:03 -0400

    Allow extra client config options for ActiveStorage::Service::AzureStorage
    This is a backport of updates made to [master](https://github.com/rails/rails/pull/35096).
    
    In order to set custom options (e.g. a custom domain name) on the azure storage client, the ActiveStorage adapter must allow more options to be passed to the initializer. Currently `ActiveStorage::Service::AzureStorage` only allows for a fixed set of options.
    
    Here we add the ability to pass additional options to the initializer.

--- activestorage/lib/active_storage/service/azure_storage_service.rb
+++ activestorage/lib/active_storage/service/azure_storage_service.rb
@@ -10,8 +10,8 @@ module ActiveStorage
   class Service::AzureStorageService < Service
     attr_reader :client, :blobs, :container, :signer
 
-    def initialize(storage_account_name:, storage_access_key:, container:)
-      @client = Azure::Storage::Client.create(storage_account_name: storage_account_name, storage_access_key: storage_access_key)
+    def initialize(storage_account_name:, storage_access_key:, container:, **options)
+      @client = Azure::Storage::Client.create(storage_account_name: storage_account_name, storage_access_key: storage_access_key, **options)
       @signer = Azure::Storage::Core::Auth::SharedAccessSignature.new(storage_account_name, storage_access_key)
       @blobs = client.blob_client
       @container = container
