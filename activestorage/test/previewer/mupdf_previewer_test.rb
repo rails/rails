5edd9b66a8a087e1e5d03299a92f63aef1a9af23
Vincent Robert <vincent.robert@touch-sell.com>
2020-09-17 13:06:08 +0200

    Add tests for previewing a PDF with CropBox

--- activestorage/test/previewer/mupdf_previewer_test.rb
+++ activestorage/test/previewer/mupdf_previewer_test.rb
@@ -6,18 +6,27 @@ require "database/setup"
 require "active_storage/previewer/mupdf_previewer"
 
 class ActiveStorage::Previewer::MuPDFPreviewerTest < ActiveSupport::TestCase
-  setup do
-    @blob = create_file_blob(filename: "report.pdf", content_type: "application/pdf")
-  end
-
   test "previewing a PDF document" do
-    ActiveStorage::Previewer::MuPDFPreviewer.new(@blob).preview do |attachable|
+    blob = create_file_blob(filename: "report.pdf", content_type: "application/pdf")
+    ActiveStorage::Previewer::MuPDFPreviewer.new(blob).preview do |attachable|
       assert_equal "image/png", attachable[:content_type]
       assert_equal "report.png", attachable[:filename]
 
       image = MiniMagick::Image.read(attachable[:io])
       assert_equal 612, image.width
       assert_equal 792, image.height
+    end
+  end
+
+  test "previewing a cropped PDF document" do
+    blob = create_file_blob(filename: "cropped.pdf", content_type: "application/pdf")
+    ActiveStorage::Previewer::MuPDFPreviewer.new(blob).preview do |attachable|
+      assert_equal "image/png", attachable[:content_type]
+      assert_equal "cropped.png", attachable[:filename]
+
+      image = MiniMagick::Image.read(attachable[:io])
+      assert_equal 430, image.width
+      assert_equal 145, image.height
     end
   end
 end
