86d75a333d89fba2e5e2cceea6f52d86196d89c5
Sean Doyle <sean.p.doyle24@gmail.com>
2020-09-25 14:06:29 -0400

    move .signed_global_id to ActiveRecord::FixtureSet

--- actiontext/lib/action_text/fixture_set.rb
+++ actiontext/lib/action_text/fixture_set.rb
@@ -2,11 +2,9 @@
 
 module ActionText
   class FixtureSet
-    def self.attachment(fixture_set_name, label)
-      identifier = ActiveRecord::FixtureSet.identify(label)
-      model_name = ActiveRecord::FixtureSet.default_fixture_model_name(fixture_set_name)
-      uri = URI::GID.build(app: GlobalID.app, model_name: model_name, model_id: identifier)
-      signed_global_id = SignedGlobalID.new(uri, for: ActionText::Attachable::LOCATOR_NAME)
+    def self.attachment(fixture_set_name, label, column_type: :integer)
+      signed_global_id = ActiveRecord::FixtureSet.signed_global_id fixture_set_name, label,
+        column_type: column_type, for: ActionText::Attachable::LOCATOR_NAME
 
       %(<action-text-attachment sgid="#{signed_global_id}"></action-text-attachment)
     end
