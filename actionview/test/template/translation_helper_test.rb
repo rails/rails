rails/rails

commit dd7a673782f102d2b58194007bf18a8e496818e3

Remove redundant @virtual_path variable
Following the introduction of the @current_template variable in 1581cab,
the @virtual_path variable is now redundant, as the value of the virtual
path may be accessed via @current_template.virtual_path. This commit
removes @virtual_path and replaces any references to @virtual_path with
@current_template.virtual_path.

actionview/lib/action_view/base.rb
@@ -270,12 +270,12 @@ module ActionView #:nodoc:
     end
 
     def _run(method, template, locals, buffer, add_to_stack: true, &block)
-      _old_output_buffer, _old_virtual_path, _old_template = @output_buffer, @virtual_path, @current_template
+      _old_output_buffer, _old_template = @output_buffer, @current_template
       @current_template = template if add_to_stack
       @output_buffer = buffer
       send(method, locals, buffer, &block)
     ensure
-      @output_buffer, @virtual_path, @current_template = _old_output_buffer, _old_virtual_path, _old_template
+      @output_buffer, @current_template = _old_output_buffer, _old_template
     end
 
     def compiled_method_container

actionview/lib/action_view/context.rb
@@ -18,7 +18,6 @@ module ActionView
     def _prepare_context
       @view_flow     = OutputFlow.new
       @output_buffer = nil
-      @virtual_path  = nil
     end
 
     # Encapsulates the interaction with the view flow so it

actionview/lib/action_view/helpers/translation_helper.rb
@@ -124,10 +124,10 @@ module ActionView
         def scope_key_by_partial(key)
           stringified_key = key.to_s
           if stringified_key.start_with?(".")
-            if @virtual_path
+            if @current_template&.virtual_path
               @_scope_key_by_partial_cache ||= {}
-              @_scope_key_by_partial_cache[@virtual_path] ||= @virtual_path.gsub(%r{/_?}, ".")
-              "#{@_scope_key_by_partial_cache[@virtual_path]}#{stringified_key}"
+              @_scope_key_by_partial_cache[@current_template.virtual_path] ||= @current_template.virtual_path.gsub(%r{/_?}, ".")
+              "#{@_scope_key_by_partial_cache[@current_template.virtual_path]}#{stringified_key}"
             else
               raise "Cannot use t(#{key.inspect}) shortcut because path is not available"
             end

actionview/test/abstract_unit.rb
@@ -59,8 +59,6 @@ module RenderERBUtils
   end
 
   def render_erb(string)
-    @virtual_path = nil
-
     template = ActionView::Template.new(
       string.strip,
       "test template",

actionview/test/activerecord/relation_cache_test.rb
@@ -10,7 +10,6 @@ class RelationCacheTest < ActionView::TestCase
     view_paths     = ActionController::Base.view_paths
     lookup_context = ActionView::LookupContext.new(view_paths, {}, ["test"])
     @view_renderer = ActionView::Renderer.new(lookup_context)
-    @virtual_path  = "path"
     @current_template = lookup_context.find "test/hello_world"
 
     controller.cache_store = ActiveSupport::Cache::MemoryStore.new

actionview/test/template/template_test.rb
@@ -22,7 +22,6 @@ class TestERBTemplate < ActiveSupport::TestCase
     def initialize(*)
       super
       @output_buffer = "original"
-      @virtual_path = nil
     end
 
     def hello
@@ -35,7 +34,7 @@ class TestERBTemplate < ActiveSupport::TestCase
 
     def partial
       ActionView::Template.new(
-        "<%= @virtual_path %>",
+        "<%= @current_template.virtual_path %>",
         "partial",
         ERBHandler,
         virtual_path: "partial",
@@ -115,9 +114,9 @@ class TestERBTemplate < ActiveSupport::TestCase
   end
 
   def test_virtual_path
-    @template = new_template("<%= @virtual_path %>" \
+    @template = new_template("<%= @current_template.virtual_path %>" \
                              "<%= partial.render(self, {}) %>" \
-                             "<%= @virtual_path %>")
+                             "<%= @current_template.virtual_path %>")
     assert_equal "hellopartialhello", render
   end
 

commit 2e7c9d3ee6cc3350a89ecdb2d743957c8ffb4720

Add tests for translate() within a partial block
As a follow-up to [PR#39730][] (which proposes this exact diff to the
`6-0-stable` branch) this commit adds test coverage for calling
`translate()` from within the block of a partial.

There are no implementation changes necessary to pass these tests, but
it's possible that the underlying fix for the existing bug was
unintentionally introduced in [dd7a673][] (since neither [PR#39060][]
nor the [CHANGELOG][] mention the fix explicitly).

Adding test coverage at this point would commit to supporting the new
behavior, and would guard against accidental regressions in the future.

[PR#39730]: https://github.com/rails/rails/pull/39730
[dd7a673]: https://github.com/rails/rails/commit/dd7a673782f102d2b58194007bf18a8e496818e3#diff-cda406008ce3935156e8ba173b1e9837
[PR#39060]: https://github.com/rails/rails/pull/39060
[CHANGELOG]: https://github.com/rails/rails/blob/357e15290ab06af8b4f32dd480b48a8c6e31e9e7/actionview/CHANGELOG.md

actionview/test/fixtures/translations/templates/_layout.erb
@@ -1,0 +1,1 @@
+<%= yield %>

actionview/test/fixtures/translations/templates/translate_in_layout.erb
@@ -1,0 +1,3 @@
+<%= render("translations/templates/layout") do %>
+  <%= translate(".foo") %>
+<% end %>

actionview/test/template/translation_helper_test.rb
@@ -23,7 +23,8 @@ class TranslationHelperTest < ActiveSupport::TestCase
           found_yield_single_argument: { foo: "Foo" },
           found_yield_block: { foo: "Foo" },
           array: { foo: { bar: "Foo Bar" } },
-          default: { foo: "Foo" }
+          default: { foo: "Foo" },
+          translate_in_layout: { foo: "Foo" },
         },
         foo: "Foo",
         hello: "<a>Hello World</a>",
@@ -162,6 +163,10 @@ class TranslationHelperTest < ActiveSupport::TestCase
 
   def test_finds_translation_scoped_by_partial_yielding_translation_and_key
     assert_equal "translations.templates.found_yield_block.foo: Foo", view.render(template: "translations/templates/found_yield_block").strip
+  end
+
+  def test_finds_translation_scoped_by_translate_in_layout
+    assert_equal "Foo", view.render(template: "translations/templates/translate_in_layout").strip
   end
 
   def test_finds_array_of_translations_scoped_by_partial