b2ec2fb88b0e660484f8140e6e0ca4a52da0815f
Nick Wolf <git@nickwolf.com.au>
2020-10-10 21:26:35 +1100

    Refactor test_xdg_config_no_custom_rc with jonathanhefner feedback

--- railties/test/generators/argv_scrubber_test.rb
+++ railties/test/generators/argv_scrubber_test.rb
@@ -5,6 +5,7 @@ require "active_support/testing/autorun"
 require "rails/generators/rails/app/app_generator"
 require "tempfile"
 require "fileutils"
+require "env_helpers"
 
 module Rails
   module Generators
@@ -12,6 +13,7 @@ module Rails
       # Future people who read this... These tests are just to surround the
       # current behavior of the ARGVScrubber, they do not mean that the class
       # *must* act this way, I just want to prevent regressions.
+      include EnvHelpers
 
       def test_version
         ["-v", "--version"].each do |str|
@@ -57,19 +59,15 @@ module Rails
         assert_equal [], args
       end
 
-      def test_xdg_config_no_custom_rc
-        xdg = Dir.tmpdir
-        ENV["XDG_CONFIG_HOME"] = xdg
-        rails_xdg_path = File.join(xdg, "rails")
-        Dir.mkdir(rails_xdg_path, 0700) unless Dir.exist?(rails_xdg_path)
-        Dir.new(rails_xdg_path)
-        railrc_path = File.join(rails_xdg_path, "railsrc")
-        railsrc = File.new(railrc_path, "w")
-        railsrc.close
-
-        scrubber = Class.new(ARGVScrubber)
-        assert_equal scrubber.default_rc_file, railrc_path
-        File.delete(railrc_path) if File.exist?(railrc_path)
+      def test_default_rc_file_with_xdg_config_home
+        Dir.mktmpdir do |dir|
+          rc_file = File.join(dir, "rails/railsrc")
+          FileUtils.mkdir_p(File.dirname(rc_file))
+          FileUtils.touch(rc_file)
+          switch_env("XDG_CONFIG_HOME", dir) do
+            assert_equal rc_file, ARGVScrubber.default_rc_file
+          end
+        end
       end
 
       def test_new_homedir_rc
