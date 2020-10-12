5699122abf7613465c62f2e50e3af017e95cb609
Akira Matsuda <ronnie@dio.jp>
2020-10-12 09:08:46 +0900

    Revert "*Store implements delete_session as a public method"
    This reverts commit d41939a678290c773f9a5275a4aed772b4707f06.
    
    Reason: activerecord-session_store implements `delete_session` as a private method
    https://github.com/rails/activerecord-session_store/blob/f188efb/lib/action_dispatch/session/active_record_store.rb#L94
    
    Thank you @al for pointing this out!
    https://github.com/rails/rails/commit/d41939a6#commitcomment-43152220
    
    Co-authored-by: Alan Larkin <alan.larkin@gmail.com>

--- actionpack/lib/action_dispatch/request/session.rb
+++ actionpack/lib/action_dispatch/request/session.rb
@@ -79,7 +79,7 @@ module ActionDispatch
       def destroy
         clear
         options = self.options || {}
-        @by.delete_session(@req, options.id(@req), options)
+        @by.send(:delete_session, @req, options.id(@req), options)
 
         # Load the new sid to be written with the response.
         @loaded = false
