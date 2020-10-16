496eb969d13b8252463f78b4ae08c762ef508637
Eugene Kenny <elkenny@gmail.com>
2020-10-16 01:00:12 +0100

    Work around getaddrinfo deadlock in forked process
    See https://bugs.ruby-lang.org/issues/17220.
    
    This will prevent every CI run from getting stuck and eventually timing
    out after 30 minutes.

--- activejob/test/support/integration/adapters/sidekiq.rb
+++ activejob/test/support/integration/adapters/sidekiq.rb
@@ -24,6 +24,7 @@ module SidekiqJobsManager
     continue_read, continue_write = IO.pipe
     death_read, death_write = IO.pipe
 
+    sleep 1
     @pid = fork do
       continue_read.close
       death_write.close
