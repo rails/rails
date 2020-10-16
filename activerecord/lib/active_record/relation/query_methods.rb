98b90e702f757c702ae5204177e3833f301a8841
Alex Robbin <agrobbin@gmail.com>
2020-10-16 12:35:17 -0400

    add `prepend_order` as a new query method to prepend the specified order onto any existing order
        ```ruby
        class User < ApplicationRecord
          scope :recent, -> { order(name: :desc) }
        end
    
        most_popular_then_recent = User.recent.prepend_order(popularity: :desc)
        ```
    
    This is useful if you are using a scope that has an order that you want use as a fallback.

--- activerecord/lib/active_record/relation/query_methods.rb
+++ activerecord/lib/active_record/relation/query_methods.rb
@@ -647,6 +647,25 @@ module ActiveRecord
       self
     end
 
+    # Prepends to any existing order defined on the relation with the specified order. For example:
+    #
+    #   User.order('email ASC').prepend_order('id DESC')
+    #
+    # generates a query with `ORDER BY id DESC, email ASC`. This is useful if you are using a scope that has an order that you want use as a fallback.
+    def prepend_order(*args)
+      check_if_method_has_arguments!(:prepend_order, args) do
+        sanitize_order_arguments(args)
+      end
+      spawn.prepend_order!(*args)
+    end
+
+    # Same as #prepend_order but operates on relation in-place instead of copying.
+    def prepend_order!(*args)
+      preprocess_order_args(args) unless args.empty?
+      self.order_values = args | order_values
+      self
+    end
+
     VALID_UNSCOPING_VALUES = Set.new([:where, :select, :group, :order, :lock,
                                      :limit, :offset, :joins, :left_outer_joins, :annotate,
                                      :includes, :eager_load, :preload, :from, :readonly,
