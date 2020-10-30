e4e5f19b05e937e0b8364686352400ab1f89abf1
Sean Doyle <sean.p.doyle24@gmail.com>
2020-10-30 13:21:30 -0400

    close trailing tag

--- actiontext/lib/action_text/fixture_set.rb
+++ actiontext/lib/action_text/fixture_set.rb
@@ -6,7 +6,7 @@ module ActionText
       signed_global_id = ActiveRecord::FixtureSet.signed_global_id fixture_set_name, label,
         column_type: column_type, for: ActionText::Attachable::LOCATOR_NAME
 
-      %(<action-text-attachment sgid="#{signed_global_id}"></action-text-attachment)
+      %(<action-text-attachment sgid="#{signed_global_id}"></action-text-attachment>)
     end
   end
 end
