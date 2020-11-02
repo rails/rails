b7eaaf73fbd7f198cb700f395de53db89fc895bb
Eugene Kenny <elkenny@gmail.com>
2020-11-02 19:36:26 +0000

    Update comment about reloading in development.rb [ci skip]
    This comment predates the existence of `reload_classes_only_on_change`.

--- actiontext/test/dummy/config/environments/development.rb
+++ actiontext/test/dummy/config/environments/development.rb
@@ -3,8 +3,8 @@ Rails.application.configure do
   # config.webpacker.check_yarn_integrity = true
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
+  # In the development environment your application's code is reloaded any time
+  # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
--- activestorage/test/dummy/config/environments/development.rb
+++ activestorage/test/dummy/config/environments/development.rb
@@ -3,8 +3,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
+  # In the development environment your application's code is reloaded any time
+  # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
