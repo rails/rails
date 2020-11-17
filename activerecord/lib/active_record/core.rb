32e113ce2bc3f0635b9ed759a39da9fd5578c45b
Eileen M. Uchitelle <eileencodes@users.noreply.github.com>
2020-11-17 09:14:12 -0500

    Merge pull request #40637 from eileencodes/raise-if-connection_handlers-called-from-new-handling
    Raise if connection_handlers is called in legacy_connection_handling

--- activerecord/lib/active_record/core.rb
+++ activerecord/lib/active_record/core.rb
@@ -166,10 +166,18 @@ module ActiveRecord
       end
 
       def self.connection_handlers
+        unless legacy_connection_handling
+          raise NotImplementedError, "The new connection handling does not support accessing multiple connection handlers."
+        end
+
         @@connection_handlers ||= {}
       end
 
       def self.connection_handlers=(handlers)
+        unless legacy_connection_handling
+          raise NotImplementedError, "The new connection handling does not setting support multiple connection handlers."
+        end
+
         @@connection_handlers = handlers
       end
 
