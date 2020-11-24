9b6459aab6eade068db1eb878f93c0a6ba60d898
matt swanson <matt@mdswanson.com>
2020-11-24 12:19:46 -0500

    Add support for eager loading all rich text associations at once (#39397)
    * Add `with_all_rich_text` method to eager load all RichText models at once
    
    * Update actiontext/test/test_helper.rb
    
    Co-authored-by: Kasper Timm Hansen <kaspth@gmail.com>
    
    * Update actiontext/lib/action_text/attribute.rb
    
    Co-authored-by: Kasper Timm Hansen <kaspth@gmail.com>
    
    Co-authored-by: Kasper Timm Hansen <kaspth@gmail.com>

--- actiontext/test/test_helper.rb
+++ actiontext/test/test_helper.rb
@@ -22,6 +22,23 @@ if ActiveSupport::TestCase.respond_to?(:fixture_path=)
 end
 
 class ActiveSupport::TestCase
+  def assert_queries(expected_count)
+    ActiveRecord::Base.connection.materialize_transactions
+
+    queries = []
+    ActiveSupport::Notifications.subscribe("sql.active_record") do |*, sql:, name:, **|
+      queries << sql unless %w[ SCHEMA TRANSACTION ].include?(name)
+    end
+
+    yield.tap do
+      assert_equal expected_count, queries.size, "#{queries.size} instead of #{expected_count} queries were executed. #{queries.inspect}"
+    end
+  end
+
+  def assert_no_queries(&block)
+    assert_queries(0, &block)
+  end
+
   private
     def create_file_blob(filename:, content_type:, metadata: nil)
       ActiveStorage::Blob.create_and_upload! io: file_fixture(filename).open, filename: filename, content_type: content_type, metadata: metadata
