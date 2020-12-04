f3bef4db5cb401897f0447f82288d83a6e10d24c
Tahsin Hasan <tahsin352@yahoo.com>
2020-12-04 23:50:47 +0600

    fix build error

--- activerecord/test/cases/time_precision_test.rb
+++ activerecord/test/cases/time_precision_test.rb
@@ -45,7 +45,7 @@ if supports_datetime_with_precision?
       assert_equal 123456000, foo.finish.nsec
     end
 
-    unless current_adapter?(:Mysql2Adapter)
+    unless current_adapter?(:Mysql2Adapter, :SQLite3Adapter)
       def test_no_time_precision_isnt_truncated_on_assignment
         @connection.create_table(:foos, force: true)
         @connection.add_column :foos, :start,  :time
