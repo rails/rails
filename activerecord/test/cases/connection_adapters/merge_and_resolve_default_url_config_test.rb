b7dfdc21f68b20718e5a150767f5fed06480ca64
Jon Bracy <jonbracy@gmail.com>
2021-01-16 15:21:44 -0600

    Fix quotes for rubocop

--- activerecord/test/cases/connection_adapters/merge_and_resolve_default_url_config_test.rb
+++ activerecord/test/cases/connection_adapters/merge_and_resolve_default_url_config_test.rb
@@ -127,7 +127,7 @@ module ActiveRecord
       end
 
       def test_adapter_with_url_config
-        config   = { "production" => { "adapter" => "sunstone", "url" => 'https://example.com/path' } }
+        config   = { "production" => { "adapter" => "sunstone", "url" => "https://example.com/path" } }
         actual   = resolve_config(config, "production")
         assert_equal config["production"].symbolize_keys, actual
 
