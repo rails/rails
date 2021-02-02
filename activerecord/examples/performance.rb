a17d0c1ea3784337a2253d98bb45db8a225e35b1
Aditya Pandit <adityapandit38@gmail.com>
2021-02-02 22:23:52 +0530

    Reused defined variable and Single quotes unless interpolation

--- activerecord/examples/performance.rb
+++ activerecord/examples/performance.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require "active_record"
-require "benchmark/ips"
+require 'active_record'
+require 'benchmark/ips'
 
-TIME    = (ENV["BENCHMARK_TIME"] || 20).to_i
-RECORDS = (ENV["BENCHMARK_RECORDS"] || TIME * 1000).to_i
+TIME    = (ENV['BENCHMARK_TIME'] || 20).to_i
+RECORDS = (ENV['BENCHMARK_RECORDS'] || TIME * 1000).to_i
 
-conn = { adapter: "sqlite3", database: ":memory:" }
+conn = { adapter: 'sqlite3', database: ':memory:' }
 
 ActiveRecord::Base.establish_connection(conn)
 
@@ -33,37 +33,37 @@ class Exhibit < ActiveRecord::Base
   def feel; look; user.name end
 
   def self.with_name
-    where("name IS NOT NULL")
+    where('name IS NOT NULL')
   end
 
   def self.with_notes
-    where("notes IS NOT NULL")
+    where('notes IS NOT NULL')
   end
 
   def self.look(exhibits) exhibits.each(&:look) end
   def self.feel(exhibits) exhibits.each(&:feel) end
 end
 
-def progress_bar(int); print "." if (int % 100).zero? ; end
+def progress_bar(int); print '.' if (int % 100).zero? ; end
 
-puts "Generating data..."
+puts 'Generating data...'
 
 module ActiveRecord
   class Faker
-    LOREM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non aliquet diam. Curabitur vel urna metus, quis malesuada elit.
-     Integer consequat tincidunt felis. Etiam non erat dolor. Vivamus imperdiet nibh sit amet diam eleifend id posuere diam malesuada. Mauris at accumsan sem.
-     Donec id lorem neque. Fusce erat lorem, ornare eu congue vitae, malesuada quis neque. Maecenas vel urna a velit pretium fermentum. Donec tortor enim,
-     tempor venenatis egestas a, tempor sed ipsum. Ut arcu justo, faucibus non imperdiet ac, interdum at diam. Pellentesque ipsum enim, venenatis ut iaculis vitae,
-     varius vitae sem. Sed rutrum quam ac elit euismod bibendum. Donec ultricies ultricies magna, at lacinia libero mollis aliquam. Sed ac arcu in tortor elementum
-     tincidunt vel interdum sem. Curabitur eget erat arcu. Praesent eget eros leo. Nam magna enim, sollicitudin vehicula scelerisque in, vulputate ut libero.
-     Praesent varius tincidunt commodo".split
+    LOREM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non aliquet diam. Curabitur vel urna metus, quis malesuada elit.
+    Integer consequat tincidunt felis. Etiam non erat dolor. Vivamus imperdiet nibh sit amet diam eleifend id posuere diam malesuada. Mauris at accumsan sem.
+    Donec id lorem neque. Fusce erat lorem, ornare eu congue vitae, malesuada quis neque. Maecenas vel urna a velit pretium fermentum. Donec tortor enim,
+    tempor venenatis egestas a, tempor sed ipsum. Ut arcu justo, faucibus non imperdiet ac, interdum at diam. Pellentesque ipsum enim, venenatis ut iaculis vitae,
+    varius vitae sem. Sed rutrum quam ac elit euismod bibendum. Donec ultricies ultricies magna, at lacinia libero mollis aliquam. Sed ac arcu in tortor elementum
+    tincidunt vel interdum sem. Curabitur eget erat arcu. Praesent eget eros leo. Nam magna enim, sollicitudin vehicula scelerisque in, vulputate ut libero.
+    Praesent varius tincidunt commodo'.split
 
     def self.name
-      LOREM.grep(/^\w*$/).sort_by { rand }.first(2).join " "
+      LOREM.grep(/^\w*$/).sort_by { rand }.first(2).join ' '
     end
 
     def self.email
-      LOREM.grep(/^\w*$/).sort_by { rand }.first(2).join("@") + ".com"
+      LOREM.grep(/^\w*$/).sort_by { rand }.first(2).join('@') + '.com'
     end
   end
 end
@@ -74,7 +74,7 @@ end
 
 # Using the same paragraph for all exhibits because it is very slow
 # to generate unique paragraphs for all exhibits.
-notes = ActiveRecord::Faker::LOREM.join " "
+notes = ActiveRecord::Faker::LOREM.join ' '
 today = Date.today
 
 puts "Inserting #{RECORDS} users and exhibits..."
@@ -97,89 +97,89 @@ puts "Done!\n"
 
 Benchmark.ips(TIME) do |x|
   ar_obj       = Exhibit.find(1)
-  attrs        = { name: "sam" }
-  attrs_first  = { name: "sam" }
-  attrs_second = { name: "tom" }
+  attrs        = { name: 'sam' }
+  attrs_first  = { name: 'sam' }
+  attrs_second = { name: 'tom' }
   exhibit      = {
     name: ActiveRecord::Faker.name,
     notes: notes,
-    created_at: Date.today
+    created_at: today
   }
 
-  x.report("Model#id") do
+  x.report('Model#id') do
     ar_obj.id
   end
 
-  x.report "Model.new (instantiation)" do
+  x.report 'Model.new (instantiation)' do
     Exhibit.new
   end
 
-  x.report "Model.new (setting attributes)" do
+  x.report 'Model.new (setting attributes)' do
     Exhibit.new(attrs)
   end
 
-  x.report "Model.first" do
+  x.report 'Model.first' do
     Exhibit.first.look
   end
 
-  x.report "Model.take" do
+  x.report 'Model.take' do
     Exhibit.take
   end
 
-  x.report("Model.all limit(100)") do
+  x.report('Model.all limit(100)') do
     Exhibit.look Exhibit.limit(100)
   end
 
-  x.report("Model.all take(100)") do
+  x.report('Model.all take(100)') do
     Exhibit.look Exhibit.take(100)
   end
 
-  x.report "Model.all limit(100) with relationship" do
+  x.report 'Model.all limit(100) with relationship' do
     Exhibit.feel Exhibit.limit(100).includes(:user)
   end
 
-  x.report "Model.all limit(10,000)" do
+  x.report 'Model.all limit(10,000)' do
     Exhibit.look Exhibit.limit(10000)
   end
 
-  x.report "Model.named_scope" do
+  x.report 'Model.named_scope' do
     Exhibit.limit(10).with_name.with_notes
   end
 
-  x.report "Model.create" do
+  x.report 'Model.create' do
     Exhibit.create(exhibit)
   end
 
-  x.report "Resource#attributes=" do
+  x.report 'Resource#attributes=' do
     e = Exhibit.new(attrs_first)
     e.attributes = attrs_second
   end
 
-  x.report "Resource#update" do
-    Exhibit.first.update(name: "bob")
+  x.report 'Resource#update' do
+    Exhibit.first.update(name: 'bob')
   end
 
-  x.report "Resource#destroy" do
+  x.report 'Resource#destroy' do
     Exhibit.first.destroy
   end
 
-  x.report "Model.transaction" do
+  x.report 'Model.transaction' do
     Exhibit.transaction { Exhibit.new }
   end
 
-  x.report "Model.find(id)" do
+  x.report 'Model.find(id)' do
     User.find(1)
   end
 
-  x.report "Model.find_by_sql" do
+  x.report 'Model.find_by_sql' do
     Exhibit.find_by_sql("SELECT * FROM exhibits WHERE id = #{(rand * 1000 + 1).to_i}").first
   end
 
-  x.report "Model.log" do
-    Exhibit.connection.send(:log, "hello", "world") { }
+  x.report 'Model.log' do
+    Exhibit.connection.send(:log, 'hello', 'world') { }
   end
 
-  x.report "AR.execute(query)" do
+  x.report 'AR.execute(query)' do
     ActiveRecord::Base.connection.execute("SELECT * FROM exhibits WHERE id = #{(rand * 1000 + 1).to_i}")
   end
 end
--- activerecord/examples/simple.rb
+++ activerecord/examples/simple.rb
@@ -3,13 +3,13 @@
 require "active_record"
 
 class Person < ActiveRecord::Base
-  establish_connection adapter: "sqlite3", database: "foobar.db"
+  establish_connection adapter: 'sqlite3', database: 'foobar.db'
   connection.create_table table_name, force: true do |t|
     t.string :name
   end
 end
 
-bob = Person.create!(name: "bob")
+bob = Person.create!(name: 'bob')
 puts Person.all.inspect
 bob.destroy
 puts Person.all.inspect
