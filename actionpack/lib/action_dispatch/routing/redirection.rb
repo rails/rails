52363d958673ade198aee831860da74d1f24ff9d
André Luis Leal Cardoso Junior <andrehjr@gmail.com>
2021-02-06 18:39:50 -0300

    Removing monkeypatched `URI#unescape` since main branch follows ruby 2.7

--- actionpack/lib/action_dispatch/routing/redirection.rb
+++ actionpack/lib/action_dispatch/routing/redirection.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "action_dispatch/http/request"
-require "active_support/core_ext/uri"
 require "active_support/core_ext/array/extract_options"
 require "rack/utils"
 require "action_controller/metal/exceptions"
