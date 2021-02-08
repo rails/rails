ce01aafb82bf8673ce41ff289e03c7d75e71762d
André Luis Leal Cardoso Junior <andrehjr@gmail.com>
2021-02-07 21:45:48 -0300

    Removes Symbol#start_with? and Symbol#end_with? since they are defined in Ruby 2.7
    This commit just removes the implementation of those methods and leaves the aliases for `starts_with` and `ends_with?`.

--- activesupport/lib/active_support/core_ext/symbol/starts_ends_with.rb
+++ activesupport/lib/active_support/core_ext/symbol/starts_ends_with.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 class Symbol
-  def start_with?(*prefixes)
-    to_s.start_with?(*prefixes)
-  end unless method_defined?(:start_with?)
-
-  def end_with?(*suffixes)
-    to_s.end_with?(*suffixes)
-  end unless method_defined?(:end_with?)
-
   alias :starts_with? :start_with?
   alias :ends_with? :end_with?
 end
