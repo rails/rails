eddda4d8fb6b6508e11196b14494ceac37b57339
Aaron Patterson <aaron@rubyonrails.org>
2021-02-10 09:36:15 -0800

    Fix possible DoS vector in PostgreSQL money type
    Carefully crafted input can cause a DoS via the regular expressions used
    for validating the money format in the PostgreSQL adapter.  This patch
    fixes the regexp.
    
    Thanks to @dee-see from Hackerone for this patch!
    
    [CVE-2021-22880]

--- activerecord/test/cases/adapters/postgresql/money_test.rb
+++ activerecord/test/cases/adapters/postgresql/money_test.rb
@@ -64,6 +64,14 @@ class PostgresqlMoneyTest < ActiveRecord::PostgreSQLTestCase
     assert_equal(-2.25, type.cast(+"(2.25)"))
   end
 
+  def test_money_regex_backtracking
+    type = PostgresqlMoney.type_for_attribute("wealth")
+    Timeout.timeout(0.1) do
+      assert_equal(0.0, type.cast("$" + "," * 100000 + ".11!"))
+      assert_equal(0.0, type.cast("$" + "." * 100000 + ",11!"))
+    end
+  end
+
   def test_sum_with_type_cast
     @connection.execute("INSERT INTO postgresql_moneys (id, wealth) VALUES (1, '123.45'::money)")
 
