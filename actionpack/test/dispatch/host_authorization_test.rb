83a6ac3fee8fd538ce7e0088913ff54f0f9bcb6f
Aaron Patterson <aaron@rubyonrails.org>
2021-02-10 11:38:17 -0800

    Prevent open redirect when allowed host starts with a dot
    [CVE-2021-22881]
    
    Thanks to @tktech (https://hackerone.com/tktech) for reporting this
    issue and the patch!

--- actionpack/test/dispatch/host_authorization_test.rb
+++ actionpack/test/dispatch/host_authorization_test.rb
@@ -221,6 +221,17 @@ class HostAuthorizationTest < ActionDispatch::IntegrationTest
     assert_match "Blocked host: www.example.com", response.body
   end
 
+  test "only compare to valid hostnames" do
+    @app = ActionDispatch::HostAuthorization.new(App, ".example.com")
+
+    get "/", env: {
+      "HOST" => "example.com#sub.example.com",
+    }
+
+    assert_response :forbidden
+    assert_match "Blocked host: example.com#sub.example.com", response.body
+  end
+
   test "config setting action_dispatch.hosts_response_app is deprecated" do
     assert_deprecated do
       ActionDispatch::HostAuthorization.new(App, "example.com", ->(env) { true })
