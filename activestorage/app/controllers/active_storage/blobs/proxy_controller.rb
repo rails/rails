0dc3bd95aab61e2e69d3d49ed63ca4b8976d3a74
David Heinemeier Hansson <david@loudthinking.com>
2021-02-14 12:20:50 +0100

    Make headers needed for streaming part of streaming method

--- activestorage/app/controllers/active_storage/blobs/proxy_controller.rb
+++ activestorage/app/controllers/active_storage/blobs/proxy_controller.rb
@@ -3,11 +3,9 @@
 # Proxy files through application. This avoids having a redirect and makes files easier to cache.
 class ActiveStorage::Blobs::ProxyController < ActiveStorage::BaseController
   include ActiveStorage::SetBlob
-  include ActiveStorage::SetHeaders
 
   def show
     http_cache_forever public: true do
-      set_content_headers_from @blob
       stream_from_storage @blob
     end
   end
--- activestorage/app/controllers/concerns/active_storage/streaming.rb
+++ activestorage/app/controllers/concerns/active_storage/streaming.rb
@@ -3,8 +3,13 @@
 module ActiveStorage::Streaming
   private
 
-  # Stream the blob from storage directly to the response.
-  def stream_from_storage(blob)
+  # Stream the blob from storage directly to the response. The disposition can be controlled by setting +disposition+.
+  # The content type and filename is set directly from the +blob+.
+  def stream_from_storage(blob, disposition: "inline")
+    response.headers["Content-Type"] = blob.content_type_for_serving
+    response.headers["Content-Disposition"] = ActionDispatch::Http::ContentDisposition.format \
+      disposition: blob.forced_disposition_for_serving || disposition, filename: blob.filename.sanitized
+
     blob.download do |chunk|
       response.stream.write chunk
     end
