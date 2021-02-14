09dd50559375530c1022e6ffbfa3c4f8a2db5362
David Heinemeier Hansson <david@loudthinking.com>
2021-02-14 19:37:59 +0100

    Retain disposition being set straight from params

--- activestorage/app/controllers/concerns/active_storage/streaming.rb
+++ activestorage/app/controllers/concerns/active_storage/streaming.rb
@@ -5,10 +5,11 @@ module ActiveStorage::Streaming
 
   # Stream the blob from storage directly to the response. The disposition can be controlled by setting +disposition+.
   # The content type and filename is set directly from the +blob+.
-  def stream_from_storage(blob, disposition: "inline") # :doc:
+  def stream_from_storage(blob, disposition: nil) # :doc:
     response.headers["Content-Type"] = blob.content_type_for_serving
     response.headers["Content-Disposition"] = ActionDispatch::Http::ContentDisposition.format \
-      disposition: blob.forced_disposition_for_serving || disposition, filename: blob.filename.sanitized
+      disposition: blob.forced_disposition_for_serving || params[:disposition] || disposition || "inline", 
+      filename: blob.filename.sanitized
 
     blob.download do |chunk|
       response.stream.write chunk
