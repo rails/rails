3c90308b175de54cd120f65b113ee774aec203b2
Xavier Noria <fxn@hashref.com>
2021-03-08 07:36:44 +0100

    Deletes AS::Dependencies::ClassCache
    This is an internal class which is no longer needed.

--- activesupport/lib/active_support/dependencies.rb
+++ activesupport/lib/active_support/dependencies.rb
@@ -617,42 +617,6 @@ module ActiveSupport #:nodoc:
       explicitly_unloadable_constants.each { |const| remove_constant const }
     end
 
-    class ClassCache
-      def initialize
-        @store = Concurrent::Map.new
-      end
-
-      def empty?
-        @store.empty?
-      end
-
-      def key?(key)
-        @store.key?(key)
-      end
-
-      def get(key)
-        key = key.name if key.respond_to?(:name)
-        @store[key] ||= Inflector.constantize(key)
-      end
-      alias :[] :get
-
-      def safe_get(key)
-        key = key.name if key.respond_to?(:name)
-        @store[key] ||= Inflector.safe_constantize(key)
-      end
-
-      def store(klass)
-        return self unless klass.respond_to?(:name)
-        raise(ArgumentError, "anonymous classes cannot be cached") if klass.name.empty?
-        @store[klass.name] = klass
-        self
-      end
-
-      def clear!
-        @store.clear
-      end
-    end
-
     # Get the reference for class named +name+.
     # Raises an exception if referenced class does not exist.
     def constantize(name)
