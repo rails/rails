40abab5c849768e0640577e2a81309f86ba51246
Jorge Manrubia <jorge.manrubia@gmail.com>
2021-03-10 23:50:56 +0100

    Rename to something shorter

--- activerecord/lib/active_record/railtie.rb
+++ activerecord/lib/active_record/railtie.rb
@@ -294,7 +294,7 @@ To keep using the current cache store, you can turn off cache versioning entirel
         end
 
         # Support extended queries for deterministic attributes
-        if ActiveRecord::Encryption.config.extend_queries_to_support_multiple_deterministic_encryption_schemes
+        if ActiveRecord::Encryption.config.extend_queries
           ActiveRecord::Encryption::ExtendedDeterministicQueries.install_support
         end
 
