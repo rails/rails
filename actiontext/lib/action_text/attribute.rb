f046617853c54f9004d19eeee8fab07ef4320c35
Jorge Manrubia <jorge.manrubia@gmail.com>
2021-03-12 17:28:28 +0100

    Temporary change just to confirm this is the problem with some intermittent failures in buildkite
    [skip ci]

--- actiontext/lib/action_text/attribute.rb
+++ actiontext/lib/action_text/attribute.rb
@@ -24,6 +24,11 @@ module ActionText
       #
       #   Message.all.with_rich_text_content # Avoids N+1 queries when you just want the body, not the attachments.
       #   Message.all.with_rich_text_content_and_embeds # Avoids N+1 queries when you just want the body and attachments.
+      #
+      #  === Options
+      #
+      #  * <tt>:encrypted</tt> - Pass true to encrypt the rich text attribute. The encryption will be non-deterministic. See
+      #  +ActiveRecord::Encryption::EncryptableRecord.encrypts+. Default: false.
       def has_rich_text(name, encrypted: false)
         class_eval <<-CODE, __FILE__, __LINE__ + 1
           def #{name}
