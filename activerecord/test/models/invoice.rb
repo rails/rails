ab820e4e980700fd6e5df7c1ac178db034c2a746
ermolaev <andruhafirst@yandex.ru>
2021-03-13 13:58:18 +0300

    fix map sum

--- activerecord/test/models/invoice.rb
+++ activerecord/test/models/invoice.rb
@@ -2,5 +2,5 @@
 
 class Invoice < ActiveRecord::Base
   has_many :line_items, autosave: true
-  before_save { |record| record.balance = record.line_items.map(&:amount).sum }
+  before_save { |record| record.balance = record.line_items.filter_map(&:amount).sum }
 end
