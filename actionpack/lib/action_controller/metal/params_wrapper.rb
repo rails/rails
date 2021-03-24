6a1649c5ce78bf4d4733f14df9118b8848808d52
Rafael Mendonça França <rafael@rubyonrails.org>
2021-03-24 22:53:27 +0000

    Fix handling of ParseError in controllers
    It regressed in #34894, which moved the rescue to the wrong method.
    `_wrapper_enabled?` can access the parameters and the exception will
    raise there before even entering `_perfomr_parameter_wrapping`.
    
    Closes #38285.

--- actionpack/lib/action_controller/metal/params_wrapper.rb
+++ actionpack/lib/action_controller/metal/params_wrapper.rb
@@ -275,7 +275,10 @@ module ActionController
         return false unless request.has_content_type?
 
         ref = request.content_mime_type.ref
+
         _wrapper_formats.include?(ref) && _wrapper_key && !request.parameters.key?(_wrapper_key)
+      rescue ActionDispatch::Http::Parameters::ParseError
+       false
       end
 
       def _perform_parameter_wrapping
@@ -289,8 +292,6 @@ module ActionController
 
         # This will display the wrapped hash in the log file.
         request.filtered_parameters.merge! wrapped_filtered_hash
-      rescue ActionDispatch::Http::Parameters::ParseError
-        # swallow parse error exception
       end
   end
 end
