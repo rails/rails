b61b94181b2a0cecab49d90d8f259bc8e39b662a
Security Curious <security-curious@pm.me>
2021-03-27 16:06:59 -0400

    Prevent catastrophic backtracking during mime parsing
    The regular expression used to parse the mime type can results in
    catastrophic backtracking[1] allowing for a ReDOS attack[2].
    
    This commit uses atomic grouping[3] to prevent backtracking.
    
    1. https://www.regular-expressions.info/catastrophic.html
    2. https://en.wikipedia.org/wiki/ReDoS
    3. https://www.regular-expressions.info/atomic.html
    
    [CVE-2021-22902]

--- actionpack/lib/action_dispatch/http/mime_type.rb
+++ actionpack/lib/action_dispatch/http/mime_type.rb
@@ -228,7 +228,7 @@ module Mime
     MIME_PARAMETER_KEY = "[a-zA-Z0-9][a-zA-Z0-9#{Regexp.escape('!#$&-^_.+')}]{0,126}"
     MIME_PARAMETER_VALUE = "#{Regexp.escape('"')}?[a-zA-Z0-9][a-zA-Z0-9#{Regexp.escape('!#$&-^_.+')}]{0,126}#{Regexp.escape('"')}?"
     MIME_PARAMETER = "\s*\;\s*#{MIME_PARAMETER_KEY}(?:\=#{MIME_PARAMETER_VALUE})?"
-    MIME_REGEXP = /\A(?:\*\/\*|#{MIME_NAME}\/(?:\*|#{MIME_NAME})(?:\s*#{MIME_PARAMETER}\s*)*)\z/
+    MIME_REGEXP = /\A(?:\*\/\*|#{MIME_NAME}\/(?:\*|#{MIME_NAME})(?>\s*#{MIME_PARAMETER}\s*)*)\z/
 
     class InvalidMimeType < StandardError; end
 
