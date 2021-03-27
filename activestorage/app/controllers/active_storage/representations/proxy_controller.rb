7bcbb1c1a657976f5d983fd0a9c4448687883266
George Claghorn <george@hey.com>
2021-03-27 08:03:27 -0400

    Active Storage representations: respond with 404 given invalid variation key

--- activestorage/app/controllers/active_storage/representations/proxy_controller.rb
+++ activestorage/app/controllers/active_storage/representations/proxy_controller.rb
@@ -1,19 +1,13 @@
 # frozen_string_literal: true
 
 # Proxy files through application. This avoids having a redirect and makes files easier to cache.
-class ActiveStorage::Representations::ProxyController < ActiveStorage::BaseController
-  include ActiveStorage::SetBlob
+class ActiveStorage::Representations::ProxyController < ActiveStorage::Representations::BaseController
   include ActiveStorage::SetHeaders
 
   def show
     http_cache_forever public: true do
-      set_content_headers_from representation.image
-      stream representation
+      set_content_headers_from @representation.image
+      stream @representation
     end
   end
-
-  private
-    def representation
-      @representation ||= @blob.representation(params[:variation_key]).processed
-    end
 end
