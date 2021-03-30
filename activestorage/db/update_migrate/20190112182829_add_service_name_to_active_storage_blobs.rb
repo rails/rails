9fed622b16dcc94bbc75f90d73883c7fed8e591b
Pierre de La Morinerie <kemenaran@gmail.com>
2021-03-30 09:16:17 +0000

    Explain how ActiveStorage migrations can affect production systems
    Running the ActiveStorage 6.1 service_name migration on semi-large
    production systems can surface two issues:
    
    1. The `update_all` statement can take a long time (e.g. 1s for 100_000
    records), during which the database may be entirely locked (at least on
    Postgres).
    2. The `change_column` statement will make instances not running on
    ActiveStorage 6.1 yet unable to create blobs.
    
    Signal these caveats to people running this migration.

--- activestorage/db/update_migrate/20190112182829_add_service_name_to_active_storage_blobs.rb
+++ activestorage/db/update_migrate/20190112182829_add_service_name_to_active_storage_blobs.rb
@@ -4,9 +4,23 @@ class AddServiceNameToActiveStorageBlobs < ActiveRecord::Migration[6.0]
       add_column :active_storage_blobs, :service_name, :string
 
       if configured_service = ActiveStorage::Blob.service.name
+        # Fill the initial value for service_name.
+        #
+        # N.B: the database will be locked during the whole update.
+        # Depending on the number of Blob records in the database, this can be
+        # quite slow.
+        # If this is an issue for you, you should edit this migration
+        # (for instance by updating the records incrementally).
         ActiveStorage::Blob.unscoped.update_all(service_name: configured_service)
       end
 
+      # Mark the column as not nullable.
+      #
+      # N.B: any other app instances not upgraded to ActiveStorage 6.1 yet will
+      # be unable to create new Blobs until they are upgraded.
+      # If this is an issue for you, you should edit this migration
+      # (for instance by moving the non-nullable change to a separate migration,
+      # that will run after all instances are upgraded to ActiveStorage 6.1).
       change_column :active_storage_blobs, :service_name, :string, null: false
     end
   end
