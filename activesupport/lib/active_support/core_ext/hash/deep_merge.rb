bd05149874a13d5f6949f7313c0e0faaa8118dde
Jean Boussier <jean.boussier@gmail.com>
2021-04-13 10:12:37 +0200

    Only dup hashes in deep_merge

--- activesupport/lib/active_support/core_ext/hash/deep_merge.rb
+++ activesupport/lib/active_support/core_ext/hash/deep_merge.rb
@@ -16,12 +16,12 @@ class Hash
   #   h1.deep_merge(h2) { |key, this_val, other_val| this_val + other_val }
   #   # => { a: 100, b: 450, c: { c1: 300 } }
   def deep_merge(other_hash, &block)
-    deep_dup.deep_merge!(other_hash, &block)
+    deep_dup_hashes(self).deep_merge!(other_hash, &block)
   end
 
   # Same as +deep_merge+, but modifies +self+.
   def deep_merge!(other_hash, &block)
-    merge!(other_hash) do |key, this_val, other_val|
+    merge!(deep_dup_hashes(other_hash)) do |key, this_val, other_val|
       if this_val.is_a?(Hash) && other_val.is_a?(Hash)
         this_val.deep_merge(other_val, &block)
       elsif block_given?
@@ -31,4 +31,13 @@ class Hash
       end
     end
   end
+
+  private
+    def deep_dup_hashes(object)
+      return object unless object.is_a?(Hash)
+
+      copy = object.transform_keys { |k| deep_dup_hashes(k) }
+      copy.transform_values! { |v| deep_dup_hashes(v) }
+      copy
+    end
 end
