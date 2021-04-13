0e3b1a1360e317342eeec05f979494083e23545e
Jorge Manrubia <jorge.manrubia@gmail.com>
2021-04-13 14:11:47 +0200

    Fix: assign previous encryption schemes via previous: config option.
    The previous: config option was being skipped because it was checking
    the existence of a reader method instead of the accessor itself.
    
    This also adds a test for the .configure option that was missing.

--- activerecord/test/cases/encryption/configurable_test.rb
+++ activerecord/test/cases/encryption/configurable_test.rb
@@ -9,6 +9,23 @@ class ActiveRecord::Encryption::ConfigurableTest < ActiveRecord::EncryptionTestC
     assert_equal ActiveRecord::Encryption.key_provider, ActiveRecord::Encryption.context.key_provider
   end
 
+  test ".configure configures initial config properties" do
+    previous_key_provider = ActiveRecord::Encryption::DerivedSecretKeyProvider.new("some secret")
+
+    ActiveRecord::Encryption.configure \
+      primary_key: "the primary key",
+      deterministic_key: "the deterministic key",
+      key_derivation_salt: "the salt",
+      previous: [{ key_provider: previous_key_provider }]
+
+    config = ActiveRecord::Encryption.config
+
+    assert_equal "the primary key", config.primary_key
+    assert_equal "the deterministic key", config.deterministic_key
+    assert_equal "the salt", config.key_derivation_salt
+    assert_equal previous_key_provider, config.previous_schemes.first.key_provider
+  end
+
   test "can add listeners that will get invoked when declaring encrypted attributes" do
     @klass, @attribute_name = nil
     ActiveRecord::Encryption.on_encrypted_attribute_declared do |declared_klass, declared_attribute_name|
