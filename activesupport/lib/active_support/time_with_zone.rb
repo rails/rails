1b01e47e0d7d02fc11797f09e27a23bb59b3d41b
Logan Kane <logan.kane1@gmail.com>
2021-04-12 21:08:02 -0700

    Correct misspelling in as-twz-deprecation message

--- activesupport/lib/active_support/time_with_zone.rb
+++ activesupport/lib/active_support/time_with_zone.rb
@@ -46,7 +46,7 @@ module ActiveSupport
         ActiveSupport::TimeWithZone.name has been deprecated and
         from Rails 7.1 will use the default Ruby implementation.
         You can set `config.active_support.remove_deprecated_time_with_zone_name = true`
-        to enable thew new behavior now.
+        to enable the new behavior now.
       EOM
 
       "Time"
