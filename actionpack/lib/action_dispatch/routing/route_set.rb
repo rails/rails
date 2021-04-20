a60a0d024f1e8a5fc5e0464ebbda03544938c3e5
Scott Holden <sholden@truecar.com>
2021-04-20 11:10:14 -0700

    no need to check both the key and the type

--- actionpack/lib/action_dispatch/routing/route_set.rb
+++ actionpack/lib/action_dispatch/routing/route_set.rb
@@ -822,7 +822,7 @@ module ActionDispatch
         path = route_with_params.path(method_name)
         params = route_with_params.params
 
-        if options.key?(:params) && options[:params].is_a?(Hash)
+        if options[:params].is_a?(Hash)
           params.merge! options[:params]
         end
 
