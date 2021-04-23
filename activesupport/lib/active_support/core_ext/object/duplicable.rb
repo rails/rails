87022e9d018a409e8a3ad2ca344f8af2815d6042
Jari Lammi <jari.lammi@helsinkimissio.fi>
2021-04-23 12:50:33 +0300

    Update duplicable.rb
    Fix deprecation warning.

--- activesupport/lib/active_support/core_ext/object/duplicable.rb
+++ activesupport/lib/active_support/core_ext/object/duplicable.rb
@@ -108,7 +108,7 @@ class BigDecimal
   # raises TypeError exception. Checking here on the runtime whether BigDecimal
   # will allow dup or not.
   begin
-    BigDecimal.new('4.56').dup
+    BigDecimal('4.56').dup
 
     def duplicable?
       true
