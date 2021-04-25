68bb2650d59a7aaff57cbaf9bb0dd65782c89489
Sean Doyle <sean.p.doyle24@gmail.com>
2021-04-25 08:11:17 -0400

    Action Text: forward form: option to hidden input
    Trix's `<trix-editor>` doesn't support the [form][] property like
    `<textarea>` or other form fields.
    
    For example, consider the following HTML and event listener:
    
    ```html
    <form action="/articles" method="post">
      <textarea name="content"></textarea>
    
      <button type="submit">Save</button>
    </form>
    
    <script>
      addEventListener("keydown", ({ key, metaKey, target }) => {
        if (target.form && key == "Enter" && (metaKey || ctrlKey)) {
          form.requestSubmit()
        }
      })
    </script>
    ```
    
    The `target` (an instance of `HTMLTextAreaElement` relies on the
    [HTMLTextAreaElement.form][] property for access to its associated
    `<form>`. While it's usually equivalent to `target.closest("form")`,
    that isn't always the case. Declaring a `[form]` attribute with another
    `<form>` element's `[id]` value can associate a field to a `<form>` that
    is _not an ancestor_. That means that the event listener from above
    would continue to work with this HTML:
    
    ```html
    <textarea name="content" form="new_article"></textarea>
    
    <!-- elsewhere -->
    <form id="new_article" action="/articles" method="post">
      <button type="submit">Save</button>
    </form>
    ```
    
    Unfortunately, if the `<textarea>` element were replaced with a
    `<trix-editor>`, the event listener's reliance on accessing the form as
    a property would break, since the `<trix-editor>` custom element doesn't
    declare that property. There is currently a pull request
    ([basecamp/trix#899][]) to add support for accessing the `form` as a
    property of the `<trix-editor>` element.
    
    The [feedback][] provided on that pull request suggests that we
    implement the `form` property by delegating to the `<input
    type="hidden">` element. Currently, `<input type="hidden">` elements
    constructed by Action Text helpers cannot declare the `[form]`
    attribute.
    
    This commit adds support by special-casing the `options[:form]` key
    within `ActionText::TagHelper#rich_text_area_tag`.
    
    [form]: https://developer.mozilla.org/en-US/docs/Web/API/HTMLTextAreaElement#properties
    [basecamp/trix#899]: https://github.com/basecamp/trix/pull/899#discussion_r618543357
    [feedback]: https://github.com/basecamp/trix/pull/899#discussion_r618543357

--- actiontext/app/helpers/action_text/tag_helper.rb
+++ actiontext/app/helpers/action_text/tag_helper.rb
@@ -20,6 +20,7 @@ module ActionText
     #   # <trix-editor id="content" input="trix_input_post_1" class="trix-content" ...></trix-editor>
     def rich_text_area_tag(name, value = nil, options = {})
       options = options.symbolize_keys
+      form = options.delete(:form)
 
       options[:input] ||= "trix_input_#{ActionText::TagHelper.id += 1}"
       options[:class] ||= "trix-content"
@@ -29,7 +30,7 @@ module ActionText
       options[:data][:blob_url_template] = main_app.rails_service_blob_url(":signed_id", ":filename")
 
       editor_tag = content_tag("trix-editor", "", options)
-      input_tag = hidden_field_tag(name, value.try(:to_trix_html) || value, id: options[:input])
+      input_tag = hidden_field_tag(name, value.try(:to_trix_html) || value, id: options[:input], form: form)
 
       input_tag + editor_tag
     end
