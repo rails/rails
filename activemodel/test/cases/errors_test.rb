cb03ccc618224e5ba308fefbd7f27718b31aa804
lulalala <mark@goodlife.tw>
2021-04-26 23:50:03 +0800

    Remove spec for deprecated methods

--- activemodel/test/cases/errors_test.rb
+++ activemodel/test/cases/errors_test.rb
@@ -127,14 +127,6 @@ class ErrorsTest < ActiveModel::TestCase
     assert_equal ["omg"], errors["name"]
   end
 
-  test "[]= overrides values" do
-    errors = ActiveModel::Errors.new(self)
-    assert_deprecated { errors.messages[:foo] = "omg" }
-    assert_deprecated { errors.messages[:foo] = "zomg" }
-
-    assert_equal ["zomg"], errors[:foo]
-  end
-
   test "attribute_names returns the error attributes" do
     errors = ActiveModel::Errors.new(Person.new)
     errors.add(:foo, "omg")
@@ -473,17 +465,6 @@ class ErrorsTest < ActiveModel::TestCase
     person.errors.add(:name, "cannot be blank")
     person.errors.add(:name, "cannot be nil")
     assert_equal ["name cannot be blank", "name cannot be nil"], person.errors.to_a
-  end
-
-  test "to_h is deprecated" do
-    person = Person.new
-    person.errors.add(:name, "cannot be blank")
-    person.errors.add(:name, "too long")
-
-    expected_deprecation = "ActiveModel::Errors#to_h is deprecated"
-    assert_deprecated(expected_deprecation) do
-      assert_equal({ name: "too long" }, person.errors.to_h)
-    end
   end
 
   test "to_hash returns the error messages hash" do
