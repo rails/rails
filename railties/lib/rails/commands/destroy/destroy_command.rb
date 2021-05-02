a02c73cb7d24d0921e8ba2f44bc1813f084488b3
AdamMusa <adammusa2222@gmail.com>
2021-05-03 00:18:39 +0100

    delete the generated file with scaffold in app/assets/stylesheets/scaffolds.scss

--- railties/lib/rails/commands/destroy/destroy_command.rb
+++ railties/lib/rails/commands/destroy/destroy_command.rb
@@ -18,8 +18,7 @@ module Rails
       def delete_css_file_generate_with_scaffold
         path = Rails.root.join('app', 'assets', 'stylesheets', 'scaffolds.scss')
         FileUtils.remove_file(path,force=true)
-        puts " "*6+"\e[31m#{remove}\e[0m"+" "*4  + path.to_s
-        # puts ANSI.color(:red) { "hello there" }
+        puts " "*6+"\e[31mremove\e[0m"+" "*4  + path.to_s.split("/").reverse.slice(0,4).reverse.join("/")
       end
 
       def perform(*)
