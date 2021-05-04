71437f620f6c7f2a86c4fe07a10c41279fbbc886
Ashik Salman <ashiksp@gmail.com>
2021-05-04 16:43:34 +0530

    Updated #quote comments and removed if-else block.

--- railties/lib/rails/generators/actions.rb
+++ railties/lib/rails/generators/actions.rb
@@ -323,8 +323,7 @@ module Rails
           end
         end
 
-        # Surround string with single quotes if there is no quotes.
-        # Otherwise fall back to double quotes
+        # Always returns value in double quotes.
         def quote(value) # :doc:
           if value.respond_to? :each_pair
             return value.map do |k, v|
@@ -333,11 +332,7 @@ module Rails
           end
           return value.inspect unless value.is_a? String
 
-          if value.include?("'")
-            value.tr("'", '"')
-          else
-            "\"#{value}\""
-          end
+          "\"#{value.tr("'", '"')}\""
         end
 
         # Returns optimized string with indentation
