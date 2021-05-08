8bde9ff2e96f2d7470b128e6a60c5050dd06beeb
Benoit Daloze <eregontp@gmail.com>
2021-05-08 20:13:31 +0200

    Remove redundant escapes

--- actionpack/lib/action_dispatch/http/mime_type.rb
+++ actionpack/lib/action_dispatch/http/mime_type.rb
@@ -226,7 +226,7 @@ module Mime
 
     MIME_NAME = "[a-zA-Z0-9][a-zA-Z0-9#{Regexp.escape('!#$&-^_.+')}]{0,126}"
     MIME_PARAMETER_VALUE = "#{Regexp.escape('"')}?#{MIME_NAME}#{Regexp.escape('"')}?"
-    MIME_PARAMETER = "\s*\;\s*#{MIME_NAME}(?:\=#{MIME_PARAMETER_VALUE})?"
+    MIME_PARAMETER = "\s*;\s*#{MIME_NAME}(?:=#{MIME_PARAMETER_VALUE})?"
     MIME_REGEXP = /\A(?:\*\/\*|#{MIME_NAME}\/(?:\*|#{MIME_NAME})(?>#{MIME_PARAMETER})*\s*)\z/
 
     class InvalidMimeType < StandardError; end
