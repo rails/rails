4d6175032201bd2ebee2e45b3f6465df7ae3cfbd
Ryuta Kamizono <kamipo@gmail.com>
2021-06-07 12:27:28 +0900

    Merge pull request #42366 from yahonda/net_smtp_willbe_bundled_gem_in_ruby31
    Add `net/smtp` gem for bug report templates to support Ruby 3.1

--- guides/bug_report_templates/action_mailbox_gem.rb
+++ guides/bug_report_templates/action_mailbox_gem.rb
@@ -10,6 +10,11 @@ gemfile(true) do
   # Activate the gem you are reporting the issue against.
   gem "rails", "6.1.0"
   gem "sqlite3"
+  if RUBY_VERSION >= "3.1"
+    # net-smtp, net-imap and net-pop were removed from default gems in Ruby 3.1, but is used by the `mail` gem.
+    # So we need to add them as dependencies until `mail` is fixed: https://github.com/mikel/mail/pull/1439
+    gem "net-smtp", require: false
+  end
 end
 
 require "active_record/railtie"
