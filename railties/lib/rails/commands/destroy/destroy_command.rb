5fae452af3683dc50821f14b0eb087efb4ebd3f2
AdamMusa <adammusa2222@gmail.com>
2021-06-09 17:56:46 +0100

    remove puts path in destroy file

--- railties/lib/rails/commands/destroy/destroy_command.rb
+++ railties/lib/rails/commands/destroy/destroy_command.rb
@@ -19,7 +19,6 @@ module Rails
         path = Rails.root.join('app', 'assets', 'stylesheets', 'scaffolds.scss')
         FileUtils.remove_file(path,force=true)
         puts " "*6+"\e[31mremove\e[0m"+" "*4  + path.to_s.split("/").reverse.slice(0,4).reverse.join("/")
-        puts path
       end
 
       def perform(*)
