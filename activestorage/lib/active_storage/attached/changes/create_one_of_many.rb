711e616670120b3c74edacf2c227974682f68752
Bruno Vezoli <c-bruno.vezoli@masterclass.com>
2021-06-17 01:08:28 +0200

    Fix race condition when attaching multiple files
    Co-authored-by: Juan Eduardo Roig <jeroig@gmail.com>
    Co-authored-by: Santiago Bartesaghi <santib@hey.com>

--- activestorage/lib/active_storage/attached/changes/create_one_of_many.rb
+++ activestorage/lib/active_storage/attached/changes/create_one_of_many.rb
@@ -2,9 +2,16 @@
 
 module ActiveStorage
   class Attached::Changes::CreateOneOfMany < Attached::Changes::CreateOne #:nodoc:
+    attr_reader :attachments
+
+    def initialize(name, record, attachable, attachments)
+      super(name, record, attachable)
+      @attachments = attachments
+    end
+
     private
       def find_attachment
-        record.public_send("#{name}_attachments").detect { |attachment| attachment.blob_id == blob.id }
+        attachments.detect { |attachment| attachment.blob_id == blob.id }
       end
   end
 end
