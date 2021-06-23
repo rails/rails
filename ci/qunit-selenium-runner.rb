895a78de2c5193e53a79cdf64be86c88bcb93c3c
Alex Ghiculescu <alex@tanda.co>
2021-06-23 13:33:34 -0500

    add full license

--- ci/qunit-selenium-runner.rb
+++ ci/qunit-selenium-runner.rb
@@ -3,8 +3,30 @@
 require "webdrivers"
 
 # This class based on https://github.com/smontanari/qunit-selenium, with a few tweaks to make it easier to read output.
+# The license from https://github.com/smontanari/qunit-selenium is enclosed:
+#
+# The MIT License (MIT)
+#
 # Copyright (c) 2014 Silvio Montanari
-# License: MIT
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 class TestRun
   TestResult = Struct.new(:tests, :assertions, :duration, :raw_output)
 
