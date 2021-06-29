f21da79701b1a669670bcefeb2998fd618d7a33a
John Hawthorn <john@hawthorn.email>
2021-06-29 12:21:46 -0700

    Merge pull request #42641 from jhawthorn/sqlite3_explain_fix
    Fix explain test on sqlite 3.36.0

--- activerecord/test/cases/adapters/sqlite3/explain_test.rb
+++ activerecord/test/cases/adapters/sqlite3/explain_test.rb
@@ -10,14 +10,14 @@ class SQLite3ExplainTest < ActiveRecord::SQLite3TestCase
   def test_explain_for_one_query
     explain = Author.where(id: 1).explain
     assert_match %r(EXPLAIN for: SELECT "authors"\.\* FROM "authors" WHERE "authors"\."id" = (?:\? \[\["id", 1\]\]|1)), explain
-    assert_match(/(SEARCH )?TABLE authors USING (INTEGER )?PRIMARY KEY/, explain)
+    assert_match(/(SEARCH )?(TABLE )?authors USING (INTEGER )?PRIMARY KEY/, explain)
   end
 
   def test_explain_with_eager_loading
     explain = Author.where(id: 1).includes(:posts).explain
     assert_match %r(EXPLAIN for: SELECT "authors"\.\* FROM "authors" WHERE "authors"\."id" = (?:\? \[\["id", 1\]\]|1)), explain
-    assert_match(/(SEARCH )?TABLE authors USING (INTEGER )?PRIMARY KEY/, explain)
+    assert_match(/(SEARCH )?(TABLE )?authors USING (INTEGER )?PRIMARY KEY/, explain)
     assert_match %r(EXPLAIN for: SELECT "posts"\.\* FROM "posts" WHERE "posts"\."author_id" = (?:\? \[\["author_id", 1\]\]|1)), explain
-    assert_match(/(SCAN )?TABLE posts/, explain)
+    assert_match(/(SEARCH |(SCAN )?(TABLE ))posts/, explain)
   end
 end
