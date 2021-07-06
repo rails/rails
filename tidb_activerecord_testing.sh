24812c6eb6918fa44380842b99048dcca812ab0e
Hooopo Wang <hoooopo@gmail.com>
2021-07-06 18:01:47 +0800

    use bash for speed

---
+++ tidb_activerecord_testing.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eo pipefail
+
+bundle config set --local path '.bundle' 
+
+echo "Setup gem mirror"
+bundle config mirror.https://rubygems.org https://gems.ruby-china.com 
+
+echo "Bundle install"
+bundle install
+
+echo "Setup database for testing"
+cd activerecord && bundle exec rake db:mysql:build && bundle exec rake test:mysql2
