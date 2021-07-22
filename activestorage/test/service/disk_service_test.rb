23c8d977de01fbea2e53fbafc3d0843af57eb97e
Jose Galisteo <ceritium@gmail.com>
2021-07-22 23:28:41 +0200

    Remove useless configuration for disk service test

--- activestorage/test/service/disk_service_test.rb
+++ activestorage/test/service/disk_service_test.rb
@@ -29,14 +29,8 @@ class ActiveStorage::Service::DiskServiceTest < ActiveSupport::TestCase
   end
 
   test "URL generation" do
-    original_url_options = Rails.application.routes.default_url_options.dup
-    Rails.application.routes.default_url_options.merge!(protocol: "http", host: "test.example.com", port: 3001)
-    begin
-      assert_match(/^https:\/\/example.com\/rails\/active_storage\/disk\/.*\/avatar\.png$/,
-        @service.url(@key, expires_in: 5.minutes, disposition: :inline, filename: ActiveStorage::Filename.new("avatar.png"), content_type: "image/png"))
-    ensure
-      Rails.application.routes.default_url_options = original_url_options
-    end
+    assert_match(/^https:\/\/example.com\/rails\/active_storage\/disk\/.*\/avatar\.png$/,
+      @service.url(@key, expires_in: 5.minutes, disposition: :inline, filename: ActiveStorage::Filename.new("avatar.png"), content_type: "image/png"))
   end
 
   test "URL generation without ActiveStorage::Current.url_options set" do
