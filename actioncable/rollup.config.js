0418b0cf6eceafe967e2382b0f15968ffe565e49
David Heinemeier Hansson <david@loudthinking.com>
2021-07-28 16:01:16 -0400

    Retain umd version under the old name, generate ESM version + duplicate under new name

--- actioncable/rollup.config.js
+++ actioncable/rollup.config.js
@@ -1,22 +1,38 @@
 import resolve from "@rollup/plugin-node-resolve"
 import { terser } from "rollup-plugin-terser"
-import pkg from "./package.json"
+
+const terserOptions = {
+  mangle: false,
+  compress: false,
+  format: {
+    beautify: true,
+    indent_level: 2
+  }
+}
 
 export default {
-  input: pkg.module,
-  output: {
-    file: pkg.main,
-    format: "es"
-  },
+  input: "app/javascript/action_cable/index.js",
+  output: [
+    {
+      // Deprecated naming
+      file: "app/assets/javascripts/action_cable.js",
+      format: "umd",
+      name: "ActionCable"
+    },
+
+    {
+      file: "app/assets/javascripts/actioncable.js",
+      format: "umd",
+      name: "ActionCable"
+    },
+
+    {
+      file: "app/assets/javascripts/actioncable.esm.js",
+      format: "es"
+    }
+  ],
   plugins: [
     resolve(),
-    terser({
-      mangle: false,
-      compress: false,
-      format: {
-        beautify: true,
-        indent_level: 2
-      }
-    })
+    terser(terserOptions)
   ]
 }
