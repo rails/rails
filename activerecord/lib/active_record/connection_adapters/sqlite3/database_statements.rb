11c2c0ef2f5e3382fa95fc0e9e94835f74a31702
Jonathan Hefner <jonathan@hefner.pro>
2021-07-28 10:30:06 -0500

    Fix incomplete :nodoc: directives [ci-skip]

--- activerecord/lib/active_record/connection_adapters/sqlite3/database_statements.rb
+++ activerecord/lib/active_record/connection_adapters/sqlite3/database_statements.rb
@@ -72,7 +72,7 @@ module ActiveRecord
         end
         alias :exec_update :exec_delete
 
-        def begin_isolated_db_transaction(isolation) #:nodoc
+        def begin_isolated_db_transaction(isolation) #:nodoc:
           raise TransactionIsolationError, "SQLite3 only supports the `read_uncommitted` transaction isolation level" if isolation != :read_uncommitted
           raise StandardError, "You need to enable the shared-cache mode in SQLite mode before attempting to change the transaction isolation level" unless shared_cache?
 
