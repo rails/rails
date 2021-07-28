eb82efd02717b4bb7cc2a9060b1685610897ef78
David Heinemeier Hansson <david@loudthinking.com>
2021-07-28 15:33:45 -0400

    No underscore style
    Just like activestorage.js

--- actiontext/lib/action_text/engine.rb
+++ actiontext/lib/action_text/engine.rb
@@ -28,7 +28,7 @@ module ActionText
 
     initializer "action_text.asset" do
       if Rails.application.config.respond_to?(:assets)
-        Rails.application.config.assets.precompile += %w( action_text trix )
+        Rails.application.config.assets.precompile += %w( actiontext.js trix.js trix.css )
       end
     end
 
