06744ff5780e106a2758ffc431efdf76b4667a5d
David Heinemeier Hansson <david@loudthinking.com>
2021-09-03 13:38:09 +0200

    Make channels setup generic to all node setups

--- actioncable/lib/rails/generators/channel/channel_generator.rb
+++ actioncable/lib/rails/generators/channel/channel_generator.rb
@@ -16,25 +16,33 @@ module Rails
       def create_channel_file
         template "channel.rb", File.join("app/channels", class_path, "#{file_name}_channel.rb")
 
-        if options[:assets]
-          if behavior == :invoke
-            if defined?(Webpacker::Engine)
-              template "javascript/index.js", "#{Webpacker.config.source_path}/channels/index.js"
-              template "javascript/consumer.js", "#{Webpacker.config.source_path}/channels/consumer.js"
-            else
-              template "javascript/consumer.js", "app/javascript/channels/consumer.js"
+        if options[:assets] && Rails.root.join("app/javascript").exist?
+          using_node      = Rails.root.join("package.json").exist?
+          using_importmap = Rails.root.join("config/importmap.rb").exist?
+
+          # Setup for all channels
+          first_setup_required = !Rails.root.join("app/javascript/channels/index.js").exist?
+          if first_setup_required
+            template "javascript/index.js", "app/javascript/channels/index.js"
+            template "javascript/consumer.js", "app/javascript/channels/consumer.js"
+            append_to_file "app/javascript/application.js",
+              using_node ? %(import "./channels"\n) : %(import "channels"\n)
+
+            if using_importmap
+              append_to_file "config/importmap.rb", <<-RUBY
+pin "@rails/actioncable", to: "actioncable.esm.js"
+pin_all_from "app/javascript/channels", under: "channels"
+  RUBY
             end
           end
 
-          if defined?(Webpacker::Engine)
-            js_template "javascript/channel", File.join(Webpacker.config.source_path, "channels", class_path, "#{file_name}_channel")
-          else
-            channel_js_path = File.join("app/javascript/channels", class_path, "#{file_name}_channel")
-            js_template "javascript/channel", channel_js_path
-            gsub_file "#{channel_js_path}.js", /\.\/consumer/, "channels/consumer"
+          # New individual channel
+          channel_js_path = File.join("app/javascript/channels", class_path, "#{file_name}_channel")
+          js_template "javascript/channel", channel_js_path
+          gsub_file "#{channel_js_path}.js", /\.\/consumer/, "channels/consumer" unless using_node
 
-            append_to_file "app/javascript/application.js", %(\nimport "channels/#{file_name}_channel"\n)
-          end
+          append_to_file "app/javascript/channels/index.js", 
+            using_node ? %(import ",/#{file_name}_channel"\n) : %(import "channels/#{file_name}_channel"\n)
         end
 
         generate_application_cable_files
