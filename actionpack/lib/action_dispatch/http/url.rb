f593e972376db2eb67d2c1bddbf0c2204f1f5b6b
Jean Boussier <jean.boussier@gmail.com>
2021-09-22 16:51:39 +0200

    Append trailing slash when the path is generated
    Instead of trying to parse the path later to insert
    the slash or bail out if the path look like it has a
    format indicator.
    
    This patch is based on the assumption that `url_for(path: '/blah')`
    is not an actual public API.

--- actionpack/lib/action_dispatch/http/url.rb
+++ actionpack/lib/action_dispatch/http/url.rb
@@ -71,7 +71,8 @@ module ActionDispatch
           path = options[:script_name].to_s.chomp("/")
           path << options[:path] if options.key?(:path)
 
-          add_trailing_slash(path) if options[:trailing_slash]
+          path = "/" if options[:trailing_slash] && path.blank?
+
           add_params(path, options[:params]) if options.key?(:params)
           add_anchor(path, options[:anchor]) if options.key?(:anchor)
 
@@ -99,14 +100,6 @@ module ActionDispatch
           def extract_subdomains_from(host, tld_length)
             parts = host.split(".")
             parts[0..-(tld_length + 2)]
-          end
-
-          def add_trailing_slash(path)
-            if path.include?("?")
-              path.sub!(/\?/, '/\&')
-            elsif !path.include?(".")
-              path.sub!(/[^\/]\z|\A\z/, '\&/')
-            end
           end
 
           def build_host_url(host, port, protocol, options, path)
