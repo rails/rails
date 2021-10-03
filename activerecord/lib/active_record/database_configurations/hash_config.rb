f1f86603a96b965e28839e418a5712325bf9ca38
Eugene Kenny <elkenny@gmail.com>
2021-10-03 17:51:01 +0100

    Fix new method name in DatabaseConfig#config deprecation message

--- activerecord/lib/active_record/database_configurations/hash_config.rb
+++ activerecord/lib/active_record/database_configurations/hash_config.rb
@@ -33,7 +33,7 @@ module ActiveRecord
       end
 
       def config
-        ActiveSupport::Deprecation.warn("DatabaseConfig#config will be removed in 7.0.0 in favor of DatabaseConfigurations#configuration_hash which returns a hash with symbol keys")
+        ActiveSupport::Deprecation.warn("DatabaseConfig#config will be removed in 7.0.0 in favor of DatabaseConfig#configuration_hash which returns a hash with symbol keys")
         configuration_hash.stringify_keys
       end
 
