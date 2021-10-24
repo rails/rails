8c1a62644b2dc0f90a6b41a19a2734c2be1e8dfc
Aditya Bhutani <adityab@Aditya-ka-MacBook-Pro.local>
2021-10-24 20:54:25 +0530

    Using pluck(ids) instead of map

--- activerecord/test/cases/relation/field_ordered_values_test.rb
+++ activerecord/test/cases/relation/field_ordered_values_test.rb
@@ -10,20 +10,20 @@ class FieldOrderedValuesTest < ActiveRecord::TestCase
     order = [3, 4, 1]
     posts = Post.in_order_of(:id, order).limit(3)
 
-    assert_equal(order, posts.map(&:id))
+    assert_equal(order, posts.ids)
   end
 
   def test_in_order_of_expression
     order = [3, 4, 1]
     posts = Post.in_order_of(Arel.sql("id * 2"), order.map { |id| id * 2 }).limit(3)
 
-    assert_equal(order, posts.map(&:id))
+    assert_equal(order, posts.ids)
   end
 
   def test_in_order_of_after_regular_order
     order = [3, 4, 1]
     posts = Post.where(type: "Post").order(:type).in_order_of(:id, order).limit(3)
 
-    assert_equal(order, posts.map(&:id))
+    assert_equal(order, posts.ids)
   end
 end
