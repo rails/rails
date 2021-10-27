86faeb8386835e9a522fb24ebfc3af78c38443b3
Quentin "Thornolf" Baudet <quentin.baudet@doctolib.com>
2021-10-28 00:25:09 +0200

    Fix (Inflector::Methods#underscore): small regression

--- activesupport/lib/active_support/inflector/methods.rb
+++ activesupport/lib/active_support/inflector/methods.rb
@@ -97,7 +97,7 @@ module ActiveSupport
       return camel_cased_word.to_s unless /[A-Z-]|::/.match?(camel_cased_word)
       word = camel_cased_word.to_s.gsub("::", "/")
       word.gsub!(inflections.acronyms_underscore_regex) { "#{$1 && '_' }#{$2.downcase}" }
-      word.gsub!(/([A-Z\d]+)(?=[A-Z][a-z])|([a-z\d])(?=[A-Z])/) { ($1 || $2) << "_" }
+      word.gsub!(/([A-Z]+)(?=[A-Z][a-z])|([a-z\d])(?=[A-Z])/) { ($1 || $2) << "_" }
       word.tr!("-", "_")
       word.downcase!
       word
