cb82f5f0a438c7be64ca1c0633556f6a535138b6
Jean Boussier <jean.boussier@gmail.com>
2021-11-25 12:55:53 +0100

    Fix DescendantTracker.clear on Ruby 3.1
    Previously I assumed it was useless, however I was wrong.
    
    The method is called by the reloader to give the illusion that
    the GC is precise. Meaning a class that will be unloaded is
    immediately made invisible without waiting for it to be garbage collected.
    
    This is easy to do up to Ruby 3.0 because `DescendantTracker` keeps
    a map of all tracked classes.
    
    However on 3.1 we need to use the inverse strategy, we keep a WeakMap
    of all the classes we cleared, and we filter the return value of `descendants`
    and `subclasses`.
    
    Since `clear` is private API and is only used when reloading is enabled,
    to reduce the performance impact in production mode, we entirely remove
    this behavior when `config.cache_classes` is enabled.

--- activesupport/lib/active_support/ruby_features.rb
+++ activesupport/lib/active_support/ruby_features.rb
@@ -2,7 +2,6 @@
 
 module ActiveSupport
   module RubyFeatures # :nodoc:
-    CLASS_DESCENDANTS = Class.method_defined?(:descendants) # RUBY_VERSION >= "3.1"
     CLASS_SUBCLASSES = Class.method_defined?(:subclasses) # RUBY_VERSION >= "3.1"
   end
 end
