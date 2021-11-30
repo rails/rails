32d769b439cb7d78c23c2d9cd5b227be5d0e1c15
Espartaco Palma <git@esparta.co>
2021-11-29 21:18:14 -0800

    ActiveSupport::Cache, fix race conditions on test/cache - part VI

--- activesupport/test/cache/behaviors/cache_delete_matched_behavior.rb
+++ activesupport/test/cache/behaviors/cache_delete_matched_behavior.rb
@@ -2,14 +2,23 @@
 
 module CacheDeleteMatchedBehavior
   def test_delete_matched
-    @cache.write("foo", "bar")
-    @cache.write("fu", "baz")
-    @cache.write("foo/bar", "baz")
-    @cache.write("fu/baz", "bar")
-    @cache.delete_matched(/oo/)
-    assert_not @cache.exist?("foo")
-    assert @cache.exist?("fu")
-    assert_not @cache.exist?("foo/bar")
-    assert @cache.exist?("fu/baz")
+    prefix = SecureRandom.alphanumeric # foo
+    @cache.write(prefix, SecureRandom.alphanumeric)
+
+    second_prefix = SecureRandom.alphanumeric # fu
+    @cache.write(second_prefix, SecureRandom.alphanumeric)
+
+    key = "#{prefix}/#{SecureRandom.uuid}"  # foo/bar
+    @cache.write(key, SecureRandom.alphanumeric)
+
+    other_key = "#{second_prefix}/#{SecureRandom.uuid}" # fu/baz
+    @cache.write(other_key, SecureRandom.alphanumeric)
+
+    @cache.delete_matched(/#{prefix}/) # foo
+
+    assert_not @cache.exist?(prefix)
+    assert @cache.exist?(second_prefix)
+    assert_not @cache.exist?(key)
+    assert @cache.exist?(other_key)
   end
 end
--- activesupport/test/cache/behaviors/cache_store_coder_behavior.rb
+++ activesupport/test/cache/behaviors/cache_store_coder_behavior.rb
@@ -24,57 +24,74 @@ module CacheStoreCoderBehavior
   def test_coder_receive_the_entry_on_write
     coder = SpyCoder.new
     @store = lookup_store(coder: coder)
-    @store.write("foo", "bar")
+    key = SecureRandom.uuid
+    value = SecureRandom.alphanumeric
+
+    @store.write(key, value)
     assert_equal 1, coder.dumped_entries.size
     entry = coder.dumped_entries.first
     assert_instance_of ActiveSupport::Cache::Entry, entry
-    assert_equal "bar", entry.value
+    assert_equal value, entry.value
   end
 
   def test_coder_receive_the_entry_on_read
     coder = SpyCoder.new
     @store = lookup_store(coder: coder)
-    @store.write("foo", "bar")
-    @store.read("foo")
+    key = SecureRandom.uuid
+    value = SecureRandom.alphanumeric
+
+    @store.write(key, value)
+    @store.read(key)
     assert_equal 1, coder.loaded_entries.size
     entry = coder.loaded_entries.first
     assert_instance_of ActiveSupport::Cache::Entry, entry
-    assert_equal "bar", entry.value
+    assert_equal value, entry.value
   end
 
   def test_coder_receive_the_entry_on_read_multi
     coder = SpyCoder.new
     @store = lookup_store(coder: coder)
-    @store.write_multi({ "foo" => "bar", "egg" => "spam" })
-    @store.read_multi("foo", "egg")
+    key = SecureRandom.uuid
+    value = SecureRandom.alphanumeric
+    other_key = SecureRandom.uuid
+    other_value = SecureRandom.alphanumeric
+
+    @store.write_multi({ key => value, other_key => other_value })
+    @store.read_multi(key, other_key)
     assert_equal 2, coder.loaded_entries.size
     entry = coder.loaded_entries.first
     assert_instance_of ActiveSupport::Cache::Entry, entry
-    assert_equal "bar", entry.value
+    assert_equal value, entry.value
 
     entry = coder.loaded_entries[1]
     assert_instance_of ActiveSupport::Cache::Entry, entry
-    assert_equal "spam", entry.value
+    assert_equal other_value, entry.value
   end
 
   def test_coder_receive_the_entry_on_write_multi
     coder = SpyCoder.new
     @store = lookup_store(coder: coder)
-    @store.write_multi({ "foo" => "bar", "egg" => "spam" })
+    key = SecureRandom.uuid
+    value = SecureRandom.alphanumeric
+    other_key = SecureRandom.uuid
+    other_value = SecureRandom.alphanumeric
+
+    @store.write_multi({ key => value, other_key => other_value })
     assert_equal 2, coder.dumped_entries.size
     entry = coder.dumped_entries.first
     assert_instance_of ActiveSupport::Cache::Entry, entry
-    assert_equal "bar", entry.value
+    assert_equal value, entry.value
 
     entry = coder.dumped_entries[1]
     assert_instance_of ActiveSupport::Cache::Entry, entry
-    assert_equal "spam", entry.value
+    assert_equal other_value, entry.value
   end
 
   def test_coder_does_not_receive_the_entry_on_read_miss
     coder = SpyCoder.new
     @store = lookup_store(coder: coder)
-    @store.read("foo")
+
+    @store.read(SecureRandom.uuid)
     assert_equal 0, coder.loaded_entries.size
   end
 end
