07a5584999240642454ee8fae094ca49895e71b0
Volmer Campos Soares <git@radicaos.com>
2021-12-06 16:49:44 -0500

    Documentation for Active Model Attributes
    This commit adds documentation to the constants and methods that are
    part of Active Model's Attributes API. So far this API has been hidden
    with the :nodoc: flag since its inception in Active Record and
    subsequent move to Active Model (#30920 and #30985); as the API matures
    and gets ready for public usage, visible documentation for its endpoints
    becomes necessary.
    
    The classes and modules being documented and publicized by this commit
    are the main `Attributes` module, the `Type` namespace, and all the
    standard attribute type classes included in the current API, which users
    will be able to extend and replicate to suit their customization needs.
    
    Some private modules are also receiving documetation, although they will
    continue using the :nodoc: flag. Those are `Attribute` and
    `AttributeSet`. Although they remain private I found useful to add some
    comments to describe their responsibilities.

--- activemodel/lib/active_model/type/date.rb
+++ activemodel/lib/active_model/type/date.rb
@@ -2,7 +2,25 @@
 
 module ActiveModel
   module Type
-    class Date < Value # :nodoc:
+    # Attribute type for date representation. It is registered under the
+    # +:date+ key.
+    #
+    #   class Person
+    #     include ActiveModel::Attributes
+    #
+    #     attribute :birthday, :date
+    #   end
+    #
+    #   person = Person.new(birthday: "1989-07-13")
+    #
+    #   person.birthday.class # => Date
+    #   person.birthday.year  # => 1989
+    #   person.birthday.month # => 7
+    #   person.birthday.day   # => 13
+    #
+    # String values are parsed using the ISO 8601 date format. Any other values
+    # are cast using their +to_date+ method, if it exists.
+    class Date < Value
       include Helpers::Timezone
       include Helpers::AcceptsMultiparameterTime.new
 
