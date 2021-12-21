68e9df0257474cbd7aa3781322487a0defb06596
Jean Boussier <jean.boussier@gmail.com>
2021-12-21 09:54:14 +0100

    Ruby 3.1: Handle `Class#subclasses` existing without `Class#descendants`
    Since `#subclasses` was introduced a bit after `#descendants`, the feature testing
    code assumed that if the former was present, the later would be too.
    
    However it was decided to revert `#descendants` for now, but to keep `#subclasses`
    https://bugs.ruby-lang.org/issues/14394#note-33
    
    Since this was totally unexpected, the `#descendants` core_ext and `DescendantsTracker#descendants`
    are broken on Active Support 7.0.0 / Ruby 3.1.0-dev.
    
    We now test the existence of both methods to handle this new situation. Since
    for now it's planned for `#descendants` to make it back in Ruby 3.2, we keep
    checking for it.

--- activesupport/lib/active_support/ruby_features.rb
+++ activesupport/lib/active_support/ruby_features.rb
@@ -3,5 +3,6 @@
 module ActiveSupport
   module RubyFeatures # :nodoc:
     CLASS_SUBCLASSES = Class.method_defined?(:subclasses) # RUBY_VERSION >= "3.1"
+    CLASS_DESCENDANTS = Class.method_defined?(:descendants)
   end
 end
