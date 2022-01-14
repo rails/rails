d7a75ce47a1d15c5839f1f2d6d497cef93dfbb83
Chris Ortman <chris-ortman@uiowa.edu>
2022-01-14 12:00:41 -0600

    Implements a fix for serialization & deserialization of AR::Time::Value

--- activerecord/lib/active_record/type/time.rb
+++ activerecord/lib/active_record/type/time.rb
@@ -6,6 +6,21 @@ module ActiveRecord
       include Internal::Timezone
 
       class Value < DelegateClass(::Time) # :nodoc:
+        def encode_with(coder)
+          case self.__getobj__
+          when ::Time
+            # Time here means we won't be able to
+            # delegate the serialization to TWZ so we need to
+            # do it ourself
+            time_zone = ::Time.find_zone(self.__getobj__.zone)
+            utc = self.__getobj__.utc
+            time = self.__getobj__
+            ActiveSupport::TimeWithZone.new(utc, time_zone, time).encode_with(coder)
+#            coder.map = { "utc" => utc, "zone" => time_zone, "time" => time }
+          else
+            self.__getobj__.encode_with(coder)
+          end
+        end
       end
 
       def serialize(value)
@@ -29,3 +44,5 @@ module ActiveRecord
     end
   end
 end
+::YAML.dump_tags[ActiveRecord::Type::Time::Value] = "!ruby/object:ActiveSupport::TimeWithZone"
+::YAML.load_tags['!ruby/object:ActiveRecord::Type::Time::Value'] = 'ActiveSupport::TimeWithZone'
