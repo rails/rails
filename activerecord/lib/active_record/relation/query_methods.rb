3292182b9ef807d7d803bbd3eae5ead5443f9bc2
Dávid Halász <skateman@skateman.eu>
2022-01-28 19:14:03 +0100

    Fixes order(ArelNode => direction) in ActiveRecord::QueryMethods
    When passing a hash to the `order` method that has an arel node as a
    key, the preprocess_order_args tries to convert it into a string that
    causes the generated query to fail.

--- activerecord/lib/active_record/relation/query_methods.rb
+++ activerecord/lib/active_record/relation/query_methods.rb
@@ -1602,7 +1602,7 @@ module ActiveRecord
           when Hash
             arg.map { |field, dir|
               case field
-              when Arel::Nodes::SqlLiteral
+              when Arel::Nodes::SqlLiteral, Arel::Nodes::Node, Arel::Attribute
                 field.public_send(dir.downcase)
               else
                 order_column(field.to_s).public_send(dir.downcase)
