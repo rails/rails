fef3c82f4437d143b2c6cb9f2835249a87b1db5d
Chris Salzberg <chris@dejimata.com>
2022-02-03 17:17:03 +0900

    Use different namespace for proxy calls

--- activemodel/CHANGELOG.md
+++ activemodel/CHANGELOG.md
@@ -1,3 +1,9 @@
+*   Use different cache namespace for proxy calls
 
+	Models can currently have different attribute bodies for the same method
+	names, leading to conflicts. Adding a new namespace `:active_model_proxy`
+	fixes the issue.
+
+    *Chris Salzberg*
 
 Please check [7-0-stable](https://github.com/rails/rails/blob/7-0-stable/activemodel/CHANGELOG.md) for previous changes.
--- activemodel/test/cases/attributes_test.rb
+++ activemodel/test/cases/attributes_test.rb
@@ -24,6 +24,33 @@ module ActiveModel
       attribute :string_field, default: "default string"
     end
 
+    class ModelWithGeneratedAttributeMethods
+      include ActiveModel::Attributes
+
+      attribute :foo
+    end
+
+    class ModelWithProxiedAttributeMethods
+      include ActiveModel::AttributeMethods
+
+      attribute_method_suffix "="
+
+      define_attribute_method(:foo)
+
+      def attribute=(_, _)
+      end
+    end
+
+    test "models that proxy attributes do not conflict with models with generated methods" do
+      ModelWithGeneratedAttributeMethods.new
+
+      model = ModelWithProxiedAttributeMethods.new
+
+      assert_nothing_raised do
+        model.foo = "foo"
+      end
+    end
+
     test "properties assignment" do
       data = ModelForAttributesTest.new(
         integer_field: "2.3",
