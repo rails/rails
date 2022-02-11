3c0420c431954d05fef2015a27281069e68cce0c
Jean Boussier <jean.boussier@gmail.com>
2022-02-11 13:09:30 +0100

    ActionDispatch::Executor don't fully trust `body#close`
    Under certain circumstances, the middleware isn't informed that the
    response body has been fully closed which result in request state not
    being fully reset before the next request.
    
    [CVE-2022-23633]

--- activesupport/lib/active_support/isolated_execution_state.rb
+++ activesupport/lib/active_support/isolated_execution_state.rb
@@ -37,6 +37,14 @@ module ActiveSupport
         current[key] = value
       end
 
+      def key?(key)
+        state.key?(key)
+      end
+
+      def delete(key)
+        state.delete(key)
+      end
+
       def clear
         current.clear
       end
