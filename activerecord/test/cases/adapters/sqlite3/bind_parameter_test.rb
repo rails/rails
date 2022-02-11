f85c3721695924652c82c2062735a233c2a83ed1
Kevin McPhillips <github@kevinmcphillips.ca>
2022-02-11 18:11:34 -0500

    Quote ActiveSupport::Duration in mysql2 adapter. Add test coverage for bind parameter where queries.

--- activerecord/test/cases/adapters/sqlite3/bind_parameter_test.rb
+++ activerecord/test/cases/adapters/sqlite3/bind_parameter_test.rb
@@ -1,51 +1,12 @@
 # frozen_string_literal: true
 
 require "cases/helper"
-require "models/topic"
 
 module ActiveRecord
   module ConnectionAdapters
-    class Mysql2Adapter
-      class BindParameterTest < ActiveRecord::Mysql2TestCase
-        fixtures :topics, :posts
-
-        def test_update_question_marks
-          str       = "foo?bar"
-          x         = Topic.first
-          x.title   = str
-          x.content = str
-          x.save!
-          x.reload
-          assert_equal str, x.title
-          assert_equal str, x.content
-        end
-
-        def test_create_question_marks
-          str = "foo?bar"
-          x   = Topic.create!(title: str, content: str)
-          x.reload
-          assert_equal str, x.title
-          assert_equal str, x.content
-        end
-
-        def test_update_null_bytes
-          str       = "foo\0bar"
-          x         = Topic.first
-          x.title   = str
-          x.content = str
-          x.save!
-          x.reload
-          assert_equal str, x.title
-          assert_equal str, x.content
-        end
-
-        def test_create_null_bytes
-          str = "foo\0bar"
-          x   = Topic.create!(title: str, content: str)
-          x.reload
-          assert_equal str, x.title
-          assert_equal str, x.content
-        end
+    class SQLite3Adapter
+      class BindParameterTest < ActiveRecord::SQLite3TestCase
+        fixtures :posts
 
         def test_where_with_string_for_string_column_using_bind_parameters
           count = Post.where("title = ?", "Welcome to the weblog").count
