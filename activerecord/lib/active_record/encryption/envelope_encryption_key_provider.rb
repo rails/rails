62c88676e3fa762b5a6a9454a92e65f942fe4ddd
Jonathan Hefner <jonathan@hefner.pro>
2022-02-15 13:51:03 -0600

    Fix config names for EnvelopeEncryptionKeyProvider [ci-skip]

--- activerecord/lib/active_record/encryption/envelope_encryption_key_provider.rb
+++ activerecord/lib/active_record/encryption/envelope_encryption_key_provider.rb
@@ -6,11 +6,11 @@ module ActiveRecord
     #
     # * It generates a random data-encryption key for each encryption operation.
     # * It stores the generated key along with the encrypted payload. It encrypts this key
-    #   with the master key provided in the credential +active_record.encryption.master key+.
+    #   with the master key provided in the +active_record_encryption.primary_key+ credential.
     #
     # This provider can work with multiple master keys. It will use the last one for encrypting.
     #
-    # When +config.store_key_references+ is true, it will also store a reference to
+    # When +config.active_record.encryption.store_key_references+ is true, it will also store a reference to
     # the specific master key that was used to encrypt the data-encryption key. When not set,
     # it will try all the configured master keys looking for the right one, in order to
     # return the right decryption key.
