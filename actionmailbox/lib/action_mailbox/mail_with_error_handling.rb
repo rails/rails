13cdd7a6aef86b23c3410adff1d4f6eeefc72c67
Eugene Kenny <elkenny@gmail.com>
2022-03-02 22:23:32 +0000

    Fix typo in net-smtp error check

--- actionmailbox/lib/action_mailbox/mail_with_error_handling.rb
+++ actionmailbox/lib/action_mailbox/mail_with_error_handling.rb
@@ -3,8 +3,8 @@
 begin
   require "mail"
 rescue LoadError => error
-  if error.message.match?(/net-stmp/)
-    $stderr.puts "You don't have net-stmp installed in your application. Please add it to your Gemfile and run bundle install"
+  if error.message.match?(/net-smtp/)
+    $stderr.puts "You don't have net-smtp installed in your application. Please add it to your Gemfile and run bundle install"
     raise
   end
 end
