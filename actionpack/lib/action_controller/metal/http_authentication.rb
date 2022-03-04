92345e1d0c806396b5c5d6fbb6b2c4b68e855324
Jean Boussier <jean.boussier@gmail.com>
2022-03-04 16:09:59 +0100

    Fix a typo in http_basic_authenticate_with

--- actionpack/lib/action_controller/metal/http_authentication.rb
+++ actionpack/lib/action_controller/metal/http_authentication.rb
@@ -75,7 +75,7 @@ module ActionController
           # See ActionController::HttpAuthentication::Basic for example usage.
           def http_basic_authenticate_with(name:, password:, realm: nil, **options)
             raise ArgumentError, "Expected name: to be a String, got #{name.class}" unless name.is_a?(String)
-            raise ArgumentError, "Expected password: to be a String, got #{password.class}" unless name.is_a?(String)
+            raise ArgumentError, "Expected password: to be a String, got #{password.class}" unless password.is_a?(String)
             before_action(options) { http_basic_authenticate_or_request_with name: name, password: password, realm: realm }
           end
         end
