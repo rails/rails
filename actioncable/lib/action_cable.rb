645239817d15c36ff008ccb5b6c8887e3b2c8f4b
Xavier Noria <fxn@hashref.com>
2022-03-06 19:52:36 +0100

    Refactor Zeitwerk setup in Action Cable
    I am trying to find a sweet spot here.
    
    Personally, not quite satisfied by having lib/action_cable/zeitwerk.rb. At the
    same time, trying to avoid too taking much space in lib/action_cable.rb.
    
    This patch also moves
    
        require_relative "action_cable/version"
    
    within ActionCable. Why? In a project, I like that the entrypoint defines the
    namespace. That is the main file for that namespace.
    
    Then, files below have it already created, and reopen it.
    
    I just find this deliberate order of execution to be natural, hierarchical,
    matching the project structure.

--- actioncable/lib/action_cable.rb
+++ actioncable/lib/action_cable.rb
@@ -25,11 +25,26 @@
 
 require "active_support"
 require "active_support/rails"
+require "zeitwerk"
 
-require_relative "action_cable/version"
-require_relative "action_cable/zeitwerk"
+Zeitwerk::Loader.for_gem.tap do |loader|
+  loader.ignore(
+    "#{__dir__}/rails", # Contains generators, templates, docs, etc.
+    "#{__dir__}/action_cable/gem_version.rb"
+  )
+
+  loader.do_not_eager_load(
+    "#{__dir__}/action_cable/subscription_adapter", # Adapters are required and loaded on demand.
+    "#{__dir__}/action_cable/test_helper.rb",
+    Dir["#{__dir__}/action_cable/**/test_case.rb"]
+  )
+
+  loader.inflector.inflect("postgresql" => "PostgreSQL")
+end.setup
 
 module ActionCable
+  require_relative "action_cable/version"
+
   INTERNAL = {
     message_types: {
       welcome: "welcome",
