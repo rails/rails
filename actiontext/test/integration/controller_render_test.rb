e9e2fd6f9f4033ca67ddb65c32fd658fefcbbd92
Andrew White <andrew.white@unboxed.co>
2022-03-19 07:20:31 +0000

    Fix deprecation warnings in Action Text tests
    Support for the image/jpg content type will be removed in Rails 7.1
    and the use of it within the Action Text tests appears to be a typo.

--- actiontext/test/integration/controller_render_test.rb
+++ actiontext/test/integration/controller_render_test.rb
@@ -4,7 +4,7 @@ require "test_helper"
 
 class ActionText::ControllerRenderTest < ActionDispatch::IntegrationTest
   test "uses current request environment" do
-    blob = create_file_blob(filename: "racecar.jpg", content_type: "image/jpg")
+    blob = create_file_blob(filename: "racecar.jpg", content_type: "image/jpeg")
     message = Message.create!(content: ActionText::Content.new.append_attachables(blob))
 
     host! "loocalhoost"
@@ -15,7 +15,7 @@ class ActionText::ControllerRenderTest < ActionDispatch::IntegrationTest
   end
 
   test "renders as HTML when the request format is not HTML" do
-    blob = create_file_blob(filename: "racecar.jpg", content_type: "image/jpg")
+    blob = create_file_blob(filename: "racecar.jpg", content_type: "image/jpeg")
     message = Message.create!(content: ActionText::Content.new.append_attachables(blob))
 
     host! "loocalhoost"
@@ -34,7 +34,7 @@ class ActionText::ControllerRenderTest < ActionDispatch::IntegrationTest
   end
 
   test "resolves partials when controller is namespaced" do
-    blob = create_file_blob(filename: "racecar.jpg", content_type: "image/jpg")
+    blob = create_file_blob(filename: "racecar.jpg", content_type: "image/jpeg")
     message = Message.create!(content: ActionText::Content.new.append_attachables(blob))
 
     get admin_message_path(message)
