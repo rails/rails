2d55c05c37f1b4c63b8973914debabb6d8eb5c53
John Bampton <jbampton@gmail.com>
2022-03-29 16:04:19 +1000

    Fix case of `YAML`

--- activesupport/test/core_ext/string_ext_test.rb
+++ activesupport/test/core_ext/string_ext_test.rb
@@ -1009,7 +1009,7 @@ class OutputSafetyTest < ActiveSupport::TestCase
     assert_predicate string, :html_safe?
   end
 
-  test "emits normal string yaml" do
+  test "emits normal string YAML" do
     assert_equal "foo".to_yaml, "foo".html_safe.to_yaml(foo: 1)
   end
 
