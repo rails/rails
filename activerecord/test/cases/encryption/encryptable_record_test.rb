9529dc844e001c03931e3579a03b89713d9c236f
Zack Deveau <zack.ref@gmail.com>
2022-04-22 14:26:44 +0000

    Change ActiveRecord::Coders::YAMLColumn default to safe_load
    In Psych >= 4.0.0, load defaults to safe_load. This commit
    makes the ActiveRecord::Coders::YAMLColum class use Psych safe_load
    as the Rails default.
    
    This default is configurable via ActiveRecord.use_yaml_unsafe_load
    
    We conditionally fallback to the correct unsafe load if use_yaml_unsafe_load
    is set to true. unsafe_load was introduced in Psych 4.0.0
    
    The list of safe_load permitted classes is configurable via
    ActiveRecord.yaml_column_permitted_classes
    
    [CVE-2022-32224]

--- activerecord/test/cases/encryption/encryptable_record_test.rb
+++ activerecord/test/cases/encryption/encryptable_record_test.rb
@@ -42,13 +42,13 @@ class ActiveRecord::Encryption::EncryptableRecordTest < ActiveRecord::Encryption
   end
 
   test "encrypts serialized attributes" do
-    states = %i[ green red ]
+    states = ["green", "red"]
     traffic_light = EncryptedTrafficLight.create!(state: states, long_state: states)
     assert_encrypted_attribute(traffic_light, :state, states)
   end
 
   test "encrypts store attributes with accessors" do
-    traffic_light = EncryptedTrafficLightWithStoreState.create!(color: "red", long_state: %i[ green red ])
+    traffic_light = EncryptedTrafficLightWithStoreState.create!(color: "red", long_state: ["green", "red"])
     assert_equal "red", traffic_light.color
     assert_encrypted_attribute(traffic_light, :state, { "color" => "red" })
   end
