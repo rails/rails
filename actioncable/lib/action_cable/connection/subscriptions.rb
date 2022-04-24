d75a3c51ae7bbdc241ccf4b778681bb485a128b2
piecehealth <piecehealth@sina.com>
2022-04-25 00:01:40 +0800

    make the length of error message predictable.

--- actioncable/lib/action_cable/connection/subscriptions.rb
+++ actioncable/lib/action_cable/connection/subscriptions.rb
@@ -22,7 +22,7 @@ module ActionCable
         end
       rescue Exception => e
         @connection.rescue_with_handler(e)
-        logger.error "Could not execute command from (#{data.inspect}) [#{e.class} - #{e.message}]: #{e.backtrace.first(5).join(" | ")}"
+        logger.error "Could not execute command from (#{data.inspect}) [#{e.class} - #{e.message.truncate(300)}]: #{e.backtrace.first(5).join(" | ")}"
       end
 
       def add(data)
