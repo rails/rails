082e92978803a90872f355e4ef698120bbd96f8f
Eileen M. Uchitelle <eileencodes@users.noreply.github.com>
2022-05-05 14:46:19 -0400

    Merge pull request #45027 from rails/fix-tag-helper-regression
    Fix tag helper regression

--- activesupport/lib/active_support/core_ext/string/output_safety.rb
+++ activesupport/lib/active_support/core_ext/string/output_safety.rb
@@ -12,7 +12,7 @@ class ERB
     JSON_ESCAPE_REGEXP = /[\u2028\u2029&><]/u
 
     # Following XML requirements: https://www.w3.org/TR/REC-xml/#NT-Name
-    TAG_NAME_START_REGEXP_SET = ":A-Z_a-z\u{C0}-\u{D6}\u{D8}-\u{F6}\u{F8}-\u{2FF}\u{370}-\u{37D}\u{37F}-\u{1FFF}" \
+    TAG_NAME_START_REGEXP_SET = "@:A-Z_a-z\u{C0}-\u{D6}\u{D8}-\u{F6}\u{F8}-\u{2FF}\u{370}-\u{37D}\u{37F}-\u{1FFF}" \
                                 "\u{200C}-\u{200D}\u{2070}-\u{218F}\u{2C00}-\u{2FEF}\u{3001}-\u{D7FF}\u{F900}-\u{FDCF}" \
                                 "\u{FDF0}-\u{FFFD}\u{10000}-\u{EFFFF}"
     TAG_NAME_START_REGEXP = /[^#{TAG_NAME_START_REGEXP_SET}]/
