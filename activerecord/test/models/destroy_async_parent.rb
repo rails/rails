01072a33eda2c79d3966c37390712c30027fc630
John Hawthorn <john@hawthorn.email>
2022-05-18 12:17:49 -0700

    Merge pull request #45126 from luanzeba/destroy_async_test_fix
    Fix destroy association async test setup
    
    (cherry picked from commit 49289c88ec9e086faac29f49a231f4ca6ac3112c)

--- activerecord/test/models/destroy_async_parent.rb
+++ activerecord/test/models/destroy_async_parent.rb
@@ -8,7 +8,7 @@
    has_many :dl_keyed_has_many, dependent: :destroy_async,
      foreign_key: :many_key, primary_key: :parent_id
    has_many :dl_keyed_join, dependent: :destroy_async,
-     foreign_key: :destroy_async_parent_id, primary_key: :joins_key
+     foreign_key: :destroy_async_parent_id, primary_key: :parent_id
    has_many :dl_keyed_has_many_through,
      through: :dl_keyed_join, dependent: :destroy_async,
      foreign_key: :dl_has_many_through_key_id, primary_key: :through_key
