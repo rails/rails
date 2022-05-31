rails/rails

commit 98bf64bcb9648f88bff4cb59a7ae4db2b6410241

Use StringScanner to parse Hstore payloads

activerecord/lib/active_record/connection_adapters/postgresql/oid/hstore.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
+require "strscan"
+
 module ActiveRecord
   module ConnectionAdapters
     module PostgreSQL
       module OID # :nodoc:
         class Hstore < Type::Value # :nodoc:
+          ERROR = "Invalid Hstore document: %s"
+
           include ActiveModel::Type::Helpers::Mutable
 
           def type
@@ -12,21 +16,56 @@ module ActiveRecord
           end
 
           def deserialize(value)
-            if value.is_a?(::String)
-              hash = {}
-              value.scan(HstorePair) do |key, value|
-                key.gsub!('\"', '"')
-                key.gsub!('\\\\', '\\')
+            return value unless value.is_a?(::String)
 
-                value&.gsub!('\"', '"')
-                value&.gsub!('\\\\', '\\')
+            scanner = StringScanner.new(value)
+            hash = {}
 
-                hash[key] = value
+            until scanner.eos?
+              unless scanner.skip(/"/)
+                raise(ArgumentError, ERROR % scanner.string.inspect)
               end
-              hash
-            else
-              value
+
+              unless key = scanner.scan_until(/(?<!\\)(?=")/)
+                raise(ArgumentError, ERROR % scanner.string.inspect)
+              end
+
+              unless scanner.skip(/"=>?/)
+                raise(ArgumentError, ERROR % scanner.string.inspect)
+              end
+
+              if scanner.scan(/NULL/)
+                value = nil
+              else
+                unless scanner.skip(/"/)
+                  raise(ArgumentError, ERROR % scanner.string.inspect)
+                end
+
+                unless value = scanner.scan_until(/(?<!\\)(?=")/)
+                  raise(ArgumentError, ERROR % scanner.string.inspect)
+                end
+
+                unless scanner.skip(/"/)
+                  raise(ArgumentError, ERROR % scanner.string.inspect)
+                end
+              end
+
+              key.gsub!('\"', '"')
+              key.gsub!('\\\\', '\\')
+
+              if value
+                value.gsub!('\"', '"')
+                value.gsub!('\\\\', '\\')
+              end
+
+              hash[key] = value
+
+              unless scanner.skip(/, /) || scanner.eos?
+                raise(ArgumentError, ERROR % scanner.string.inspect)
+              end
             end
+
+            hash
           end
 
           def serialize(value)

activerecord/test/cases/adapters/postgresql/hstore_test.rb
@@ -291,6 +291,7 @@ class PostgresqlHstoreTest < ActiveRecord::PostgreSQLTestCase
 
   def test_backslash
     assert_cycle('a\\b' => 'b\\ar', '1"foo' => "2")
+    assert_cycle('a\\"' => 'b\\ar', '1"foo' => "2")
   end
 
   def test_comma

commit 57f70612a113d036c741bcbcd9aa7b0393b2f17f

Fix hstore deserialize regression
https://github.com/rails/rails/commit/98bf64bcb9648f88bff4cb59a7ae4db2b6410241
introduced a StringScanner to ensure that value is a valid hstore
document.

However, the negative lookbehind in the regex used to find the final
double-quote of both the key and the value (/(?<!\\)(?=")/) doesn't
differentiate between a backslash-escaped double-quote and a
backslash-escaped backslash followed by an unescaped double-quote.

In other words a valid hstore document such as:

    postgres=# select '"\\"=>"\\"'::hstore;
       hstore
    ------------
     "\\"=>"\\"

will be incorrectly deemed invalid.

This commit aims to rectify that by switching from scan_until to scan
and lazily matching zero or more of either:

- an escaped pair (either \\ or \"); or

- a character that doesn't need escaping

until the positive lookahead matches a double-quote.

The tests have been updated to include both a key and value that end in
a backslash.

activerecord/lib/active_record/connection_adapters/postgresql/oid/hstore.rb
@@ -26,7 +26,7 @@ module ActiveRecord
                 raise(ArgumentError, ERROR % scanner.string.inspect)
               end
 
-              unless key = scanner.scan_until(/(?<!\\)(?=")/)
+              unless key = scanner.scan(/^(\\[\\"]|[^\\"])*?(?=")/)
                 raise(ArgumentError, ERROR % scanner.string.inspect)
               end
 
@@ -41,7 +41,7 @@ module ActiveRecord
                   raise(ArgumentError, ERROR % scanner.string.inspect)
                 end
 
-                unless value = scanner.scan_until(/(?<!\\)(?=")/)
+                unless value = scanner.scan(/^(\\[\\"]|[^\\"])*?(?=")/)
                   raise(ArgumentError, ERROR % scanner.string.inspect)
                 end
 

activerecord/test/cases/adapters/postgresql/hstore_test.rb
@@ -293,6 +293,7 @@ class PostgresqlHstoreTest < ActiveRecord::PostgreSQLTestCase
   def test_backslash
     assert_cycle('a\\b' => 'b\\ar', '1"foo' => "2")
     assert_cycle('a\\"' => 'b\\ar', '1"foo' => "2")
+    assert_cycle("a\\" => "bar\\", '1"foo' => "2")
   end
 
   def test_comma