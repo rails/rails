58cfda516033cab2801a81c6b8d03130a16aaa9e
eileencodes <eileencodes@gmail.com>
2022-06-10 13:20:41 -0400

    Remove invalid tests in config
    These were accidentally added in
    cb1a753d91f6092c26a2a130229369666172563d when
    https://github.com/rails/rails/pull/45293 was backported to 7-0-stable.
    
    This config isn't available in 7.0 so these tests were failing with
    `NoMethodError: undefined method `log_file_size'`.

--- railties/test/application/configuration_test.rb
+++ railties/test/application/configuration_test.rb
@@ -1712,24 +1712,6 @@ module ApplicationTests
       assert_equal Rails.application.config.paths["log"].first, app.config.default_log_file.path
     end
 
-    test "config.log_file_size returns a 100MB size number in development" do
-      app "development"
-
-      assert_equal 100.megabytes, app.config.log_file_size
-    end
-
-    test "config.log_file_size returns a 100MB size number in test" do
-      app "test"
-
-      assert_equal 100.megabytes, app.config.log_file_size
-    end
-
-    test "config.log_file_size returns no limit in production" do
-      app "production"
-
-      assert_nil app.config.log_file_size
-    end
-
     test "rake_tasks block works at instance level" do
       app_file "config/environments/development.rb", <<-RUBY
         Rails.application.configure do
