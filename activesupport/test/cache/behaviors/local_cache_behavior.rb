cd650611ea86f4ceb6649f5437e3432563678d65
mihaic195 <colceriumi@gmail.com>
2022-06-10 11:40:19 +0100

    Fix local cache method signature

--- activesupport/test/cache/behaviors/local_cache_behavior.rb
+++ activesupport/test/cache/behaviors/local_cache_behavior.rb
@@ -40,6 +40,17 @@ module LocalCacheBehavior
     assert_nil @cache.read(key)
   end
 
+  def test_clear_with_nil_options
+    key = SecureRandom.uuid
+    @cache.with_local_cache do
+      @cache.write(key, SecureRandom.alphanumeric)
+      @cache.clear(nil)
+      assert_nil @cache.read(key)
+    end
+
+    assert_nil @cache.read(key)
+  end
+
   def test_cleanup_clears_local_cache_but_not_remote_cache
     begin
       @cache.cleanup
