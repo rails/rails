c7de5bb7e451ffea49a929e6d3e0404fe4df7b44
Emmanuel Hayford <hi@manny.codes>
2022-06-14 00:30:13 +0200

    Remove Marshal.dump [ci-skip]

--- actionview/lib/action_view/helpers/debug_helper.rb
+++ actionview/lib/action_view/helpers/debug_helper.rb
@@ -5,17 +5,18 @@ require "action_view/helpers/tag_helper"
 module ActionView
   # = Action View Debug Helper
   #
-  # Provides a set of methods for making it easier to debug Rails objects.
+  # Provides a method to debug objects.
   module Helpers # :nodoc:
     module DebugHelper
       include TagHelper
 
-      # Returns a YAML representation of +object+ wrapped with <pre> and </pre>.
+      # Returns a YAML representation of +object+ wrapped with <pre> tags.
       # If the object cannot be converted to YAML using +to_yaml+, +inspect+ will be called instead.
       # Useful for inspecting an object at the time of rendering.
       #
       #   @user = User.new({ username: 'testing', password: 'xyz', age: 42})
       #   debug(@user)
+      #
       #   # =>
       #   <pre class='debug_dump'>--- !ruby/object:User
       #   attributes:
@@ -26,11 +27,9 @@ module ActionView
       #     created_at:
       #   </pre>
       def debug(object)
-        Marshal.dump(object)
         object = ERB::Util.html_escape(object.to_yaml)
         content_tag(:pre, object, class: "debug_dump")
-      rescue # errors from Marshal or YAML
-        # Object couldn't be dumped, perhaps because of singleton methods -- this is the fallback
+      rescue
         content_tag(:code, object.inspect, class: "debug_dump")
       end
     end
