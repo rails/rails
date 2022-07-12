41b0776bff6cf7433a62cb4f06b9668e98bac0c0
eileencodes <eileencodes@gmail.com>
2022-07-12 10:59:22 -0400

    Fix did you mean tests for ruby-trunk (3.2)
    Manual backport of #45322
    
    Original commit message:
    In https://github.com/ruby/ruby/commit/f075be3dcb4b82b89496d1820002bf3d80f653ef
    did_you_mean and error_highlight now use `detailed_message` over
    `message` to display errors.
    
    For cases where we are testing `message`, in 3.2 and above we need to
    test against `detailed_message` instead.
    
    As far as I can tell in a Rails console when these errors are raised the
    `detailed_message` is used so we shouldn't need to make other changes to
    Rails. The only case where this isn't true is in the Railties changes -
    we are explicitly formatting the did you mean message so we need to be
    sure to call `detailed_message` here.
    
    This fixes most of the failing tests for ruby-trunk.

--- activerecord/test/cases/associations/join_model_test.rb
+++ activerecord/test/cases/associations/join_model_test.rb
@@ -346,7 +346,11 @@ class AssociationsJoinModelTest < ActiveRecord::TestCase
       error = assert_raise(ActiveRecord::HasManyThroughAssociationNotFoundError) {
         authors(:david).nothings
       }
-      assert_match "Did you mean?", error.message
+      if error.respond_to?(:detailed_message)
+        assert_match "Did you mean?", error.detailed_message
+      else
+        assert_match "Did you mean?", error.message
+      end
     end
   end
 
