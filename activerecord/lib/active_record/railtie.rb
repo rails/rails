9db14db093d2065a50ed1f14e70f78d35810e05a
Jean Boussier <jean.boussier@gmail.com>
2022-07-14 00:33:10 +0200

    Remove active_record.yaml initializers

--- activerecord/lib/active_record/railtie.rb
+++ activerecord/lib/active_record/railtie.rb
@@ -259,23 +259,5 @@ To keep using the current cache store, you can turn off cache versioning entirel
         self.filter_attributes += Rails.application.config.filter_parameters
       end
     end
-
-    initializer "active_record.use_yaml_unsafe_load" do |app|
-      config.after_initialize do
-        unless app.config.active_record.use_yaml_unsafe_load.nil?
-          ActiveRecord::Base.use_yaml_unsafe_load =
-            app.config.active_record.use_yaml_unsafe_load
-        end
-      end
-    end
-
-    initializer "active_record.yaml_column_permitted_classes" do |app|
-      config.after_initialize do
-        unless app.config.active_record.yaml_column_permitted_classes.nil?
-          ActiveRecord::Base.yaml_column_permitted_classes =
-            app.config.active_record.yaml_column_permitted_classes
-        end
-      end
-    end
   end
 end
