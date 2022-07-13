5436676014340247ee4e53ecd2c71cab34059383
Jean Boussier <jean.boussier@gmail.com>
2022-07-14 00:34:20 +0200

    Remove active_record.yaml initializers

--- activerecord/lib/active_record/railtie.rb
+++ activerecord/lib/active_record/railtie.rb
@@ -222,23 +222,5 @@ MSG
         end
       end
     end
-    
-    initializer "active_record.use_yaml_unsafe_load" do |app|
-      config.after_initialize do
-        unless app.config.active_record.use_yaml_unsafe_load.nil?
-          ActiveRecord::Base.use_yaml_unsafe_load =
-            app.config.active_record.use_yaml_unsafe_load
-        end
-      end
-    end
-
-    initializer "active_record.yaml_column_permitted_classes" do |app|
-      config.after_initialize do
-        unless app.config.active_record.yaml_column_permitted_classes.nil?
-          ActiveRecord::Base.yaml_column_permitted_classes =
-            app.config.active_record.yaml_column_permitted_classes
-        end
-      end
-    end
   end
 end
