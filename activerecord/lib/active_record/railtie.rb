94d81c3c39e3ddc441c3af3f874e53b197cf3f54
Étienne Barrié <etienne.barrie@gmail.com>
2022-07-13 12:26:31 +0200

    Remove unnecessary initializers
    Active Record configs are already copied from the application with the
    `active_record.set_configs` initializer.

--- activerecord/lib/active_record/railtie.rb
+++ activerecord/lib/active_record/railtie.rb
@@ -414,23 +414,5 @@ To keep using the current cache store, you can turn off cache versioning entirel
         end
       end
     end
-
-    initializer "active_record.use_yaml_unsafe_load" do |app|
-      config.after_initialize do
-        unless app.config.active_record.use_yaml_unsafe_load.nil?
-          ActiveRecord.use_yaml_unsafe_load =
-            app.config.active_record.use_yaml_unsafe_load
-        end
-      end
-    end
-
-    initializer "active_record.yaml_column_permitted_classes" do |app|
-      config.after_initialize do
-        unless app.config.active_record.yaml_column_permitted_classes.nil?
-          ActiveRecord.yaml_column_permitted_classes =
-            app.config.active_record.yaml_column_permitted_classes
-        end
-      end
-    end
   end
 end
