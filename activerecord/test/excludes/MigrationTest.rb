785f0e148d779c826cf9a70ba16236692923041c
Hooopo Wang <hoooopo@gmail.com>
2022-08-12 10:36:41 +0800

    fix error

--- activerecord/test/excludes/MigrationTest.rb
+++ activerecord/test/excludes/MigrationTest.rb
@@ -1,9 +1,9 @@
-exlude: :test_create_table_with_query_from_relation, <<~MSG
+exlude :test_create_table_with_query_from_relation, <<~MSG
   Error
   ActiveRecord::StatementInvalid: Mysql2::Error: 'CREATE TABLE ... SELECT' is not implemented yet
 MSG
 
-exclude: :test_create_table_with_query, <<~MSG
+exclude :test_create_table_with_query, <<~MSG
   Error
   ActiveRecord::StatementInvalid: Mysql2::Error: 'CREATE TABLE ... SELECT' is not implemented yet
 MSG
