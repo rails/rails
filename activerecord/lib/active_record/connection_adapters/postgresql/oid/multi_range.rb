98cd59aa658021d8afaa56bb1ac4ae62ca3a6638
Gustavo Warmling Teixeira <gustavo@trustedhealth.com>
2022-08-15 22:25:06 -0300

    Add multirange values tests

--- activerecord/lib/active_record/connection_adapters/postgresql/oid/multi_range.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/oid/multi_range.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module ActiveRecord
-  module ConnectionAdaptares
+  module ConnectionAdapters
     module PostgreSQL
       module OID # :nodoc:
-        class MultiRange < ::Type::Value # :nodoc:
+        class MultiRange < Type::Value # :nodoc:
           attr_reader :subtype, :type
 
-          def initialize(subtype, type: :multirange)
+          def initialize(subtype, type = :multirange)
             @subtype = subtype
             @type = type
           end
@@ -32,16 +32,35 @@ module ActiveRecord
               value.scan(/\{*(\[|\()(.*?)?\,(.*?)?(\]|\))/)
             end
 
-            def parse_lower(value)
-              return -::Float::Infinity if value == "" || value == '-infinity'  
+            # When formatting the bound values of range types, PostgreSQL quotes
+            # the bound value using double-quotes in certain conditions. Within
+            # a double-quoted string, literal " and \ characters are themselves
+            # escaped. In input, PostgreSQL accepts multiple escape styles for "
+            # (either \" or "") but in output always uses "".
+            # See:
+            # * https://www.postgresql.org/docs/current/rangetypes.html#RANGETYPES-IO
+            # * https://www.postgresql.org/docs/current/rowtypes.html#ROWTYPES-IO-SYNTAX
+            def unquote(value)
+              if value.start_with?('"') && value.end_with?('"')
+                unquoted_value = value[1..-2]
+                unquoted_value.gsub!('""', '"')
+                unquoted_value.gsub!("\\\\", "\\")
+                unquoted_value
+              else
+                value
+              end
+            end
 
-              value
+            def parse_lower(value)
+              return infinity_value(value, negative: true) if value == "" || value == '-infinity'  
+
+              @subtype.deserialize(unquote(value))
             end
 
             def parse_upper(value)
-              return ::Float::Infinity if value == "" || value == 'infinity'  
+              return infinity_value(value) if value == "" || value == 'infinity'  
 
-              value
+              @subtype.deserialize(unquote(value))
             end
 
             def extract_range_data(value)
@@ -50,8 +69,8 @@ module ActiveRecord
 
               {
                 exclude_start: value[0] == '(',
-                from: @subtype.deserialize(from),
-                to: @subtype.deserialize(to),
+                from: from,
+                to: to,
                 exclude_end: value[3] == ')',
               }
             end
@@ -59,11 +78,21 @@ module ActiveRecord
             def build_range(value)
               extracted = extract_range_data(value) 
 
-              if !infinity?(from) && extracted[:exclude_start] 
+              if !infinity?(extracted[:from]) && extracted[:exclude_start] 
                 raise ArgumentError, "The Ruby Range object does not support excluding the beginning of a Range. (unsupported value: '#{value}')"
               end
 
-            ::Range.new(extracted[:from], extracted[:to], extracted[:exclude_end])
+              ::Range.new(extracted[:from], extracted[:to], extracted[:exclude_end])
+            end
+
+            def infinity_value(value, negative: false)
+              if @subtype.respond_to?(:infinity)
+                @subtype.infinity(negative: negative)
+              elsif negative
+                -::Float::INFINITY
+              else
+                ::Float::INFINITY
+              end
             end
 
             def infinity?(value)
--- activerecord/test/cases/adapters/postgresql/multirange_test.rb
+++ activerecord/test/cases/adapters/postgresql/multirange_test.rb
@@ -29,15 +29,38 @@ class PostgresqlMultiRangeTest < ActiveRecord::PostgreSQLTestCase
 
     insert_multirange(
       id: 201,
-      date_multirange: "{[''2022-01-02'',''2022-01-04''],[''2022-07-10'',''2022-08-10'']}",
-      num_multirange: "{[0.3,0.9],[-2.3,4.4]}",
-      ts_multirange: "{[''2022-02-02 13:30:10'',''2022-02-04 15:30:23''],[''2022-03-03 07:30:00'',''2022-05-04 15:30:00'']}",
-      tstz_multirange: "{[''2022-05-05 14:30:00+05'',''2022-06-06 13:30:00-03''],[''2022-07-01 13:30:00+05'',''2022-07-22 08:30:00-03'']}",
-      int4_multirange: "{[1,10],[2,9]}",
-      int8_multirange: "{[10,100],[101,150]}",
+      date_multirange: "{[''2022-01-02'',''2022-01-04''),[''2022-07-10'',''2022-08-10''],[2022-10-01'',]}",
+      num_multirange: "{[5.1,6.3),[-2.3,4.4),[12.1,]}",
+      ts_multirange: "{[''2022-02-02 13:30:10'',''2022-02-04 15:30:23''),[''2022-03-03 07:30:00'',''2022-05-04 15:30:00''],[''2022-08-10 13:00:00'',]}",
+      tstz_multirange: "{[''2022-05-05 14:30:00+05'',''2022-06-06 13:30:00-03''),[''2022-07-01 13:30:00+05'',''2022-07-22 08:30:00-03''],[''2022-10-01 08:30:00-03'',]}",
+      int4_multirange: "{[1,11),[500,700],[800,]}",
+      int8_multirange: "{[10,100),[101,150],[200,]}",
     )
 
-    @first_range = PostgresqlMultiRange.find(201)
+    insert_multirange(
+      id: 202,
+      date_multirange: "{[''2022-01-02'',''2022-01-04''],[''2022-07-10'',''2022-08-10''],[2022-08-01'',]}",
+      num_multirange: "{[0.3,0.9],[1.3,4.4),[4.3,]}",
+      ts_multirange: "{[''2022-02-02 13:30:10'',''2022-02-04 15:30:23''],[''2022-03-03 07:30:00'',''2022-05-04 15:30:00''],[''2022-05-03 13:00:00'',]}",
+      tstz_multirange: "{[''2022-05-05 14:30:00+05'',''2022-06-06 13:30:00-03''],[''2022-07-01 13:30:00+05'',''2022-07-22 08:30:00-03''],[''2022-07-21 08:30:00-03'',]}",
+      int4_multirange: "{[1,11],[500,700],[699,]}",
+      int8_multirange: "{[10,100],[102,150],[149,]}",
+    )
+
+    insert_multirange(
+      id: 203,
+      date_multirange: "{[''2022-01-02'',''2022-01-04''],[,]}",
+      num_multirange: "{[0.3,0.9],[,]}",
+      ts_multirange: "{[''2022-02-02 13:30:10'',''2022-02-04 15:30:23''],[,]}",
+      tstz_multirange: "{[''2022-05-05 14:30:00+05'',''2022-06-06 13:30:00-03''],[,]}",
+      int4_multirange: "{[1,11],[,]}",
+      int8_multirange: "{[10,100],[,]}"
+    )
+
+    @new_range = PostgresqlMultiRange.new
+    @multi_range = PostgresqlMultiRange.find(201)
+    @overlaping_ranges = PostgresqlMultiRange.find(202)
+    @infinity_ranges = PostgresqlMultiRange.find(203)
   end
 
   teardown do
@@ -46,12 +69,77 @@ class PostgresqlMultiRangeTest < ActiveRecord::PostgreSQLTestCase
   end
 
   def test_data_type_of_range_types
-    assert_equal :datemultirange, @first_range.column_for_attribute(:date_multirange).type
-    assert_equal :nummultirange, @first_range.column_for_attribute(:num_multirange).type
-    assert_equal :tsmultirange, @first_range.column_for_attribute(:ts_multirange).type
-    assert_equal :tstzmultirange, @first_range.column_for_attribute(:tstz_multirange).type
-    assert_equal :int4multirange, @first_range.column_for_attribute(:int4_multirange).type
-    assert_equal :int8multirange, @first_range.column_for_attribute(:int8_multirange).type
+    assert_equal :datemultirange, @multi_range.column_for_attribute(:date_multirange).type
+    assert_equal :nummultirange, @multi_range.column_for_attribute(:num_multirange).type
+    assert_equal :tsmultirange, @multi_range.column_for_attribute(:ts_multirange).type
+    assert_equal :tstzmultirange, @multi_range.column_for_attribute(:tstz_multirange).type
+    assert_equal :int4multirange, @multi_range.column_for_attribute(:int4_multirange).type
+    assert_equal :int8multirange, @multi_range.column_for_attribute(:int8_multirange).type
+  end
+
+  def test_int4multirange_values
+    assert_equal [1...11, 500...701, 800...::Float::INFINITY], @multi_range.int4_multirange
+    assert_equal [1...12, 500...::Float::INFINITY], @overlaping_ranges.int4_multirange
+    assert_equal [-::Float::INFINITY...::Float::INFINITY], @infinity_ranges.int4_multirange
+  end
+
+  def test_int8multirange_values
+    assert_equal [10...100, 101...151, 200...::Float::INFINITY], @multi_range.int8_multirange
+    assert_equal [10...101, 102...::Float::INFINITY], @overlaping_ranges.int8_multirange
+    assert_equal [-::Float::INFINITY...::Float::INFINITY], @infinity_ranges.int8_multirange
+  end
+
+  def test_tsmultirange_values
+    tz = ::ActiveRecord.default_timezone
+    assert_equal [
+      Time.public_send(tz, 2022, 2, 2, 13, 30, 10)...Time.public_send(tz, 2022, 2, 4, 15, 30, 23),
+      Time.public_send(tz, 2022, 3, 3, 7, 30)..Time.public_send(tz, 2022, 5, 4, 15, 30),
+      Time.public_send(tz, 2022, 8, 10, 13)...::Float::INFINITY
+    ], @multi_range.ts_multirange
+    assert_equal [
+      Time.public_send(tz, 2022, 2, 2, 13, 30, 10)..Time.public_send(tz, 2022, 2, 4, 15, 30, 23),
+      Time.public_send(tz, 2022, 3, 3, 7, 30)...::Float::INFINITY
+    ], @overlaping_ranges.ts_multirange
+    assert_equal [-::Float::INFINITY...::Float::INFINITY], @infinity_ranges.tstz_multirange
+  end
+
+  def test_tstzmultirange_values
+    assert_equal [
+      Time.parse("2022-05-05 09:30:00 UTC")...Time.parse("2022-06-06 16:30:00 UTC"),
+      Time.parse("2022-07-01 08:30:00 UTC")..Time.parse("2022-07-22 11:30:00 UTC"),
+      Time.parse("2022-10-01 11:30:00 UTC")...::Float::INFINITY
+    ], @multi_range.tstz_multirange
+    assert_equal [
+      Time.parse("2022-05-05 09:30:00 UTC")..Time.parse("2022-06-06 16:30:00 UTC"),
+      Time.parse("2022-07-01 08:30:00 UTC")...::Float::INFINITY
+    ], @overlaping_ranges.tstz_multirange
+    assert_equal [-::Float::INFINITY...::Float::INFINITY], @infinity_ranges.tstz_multirange
+  end
+
+  def test_nummultirange_values
+    assert_equal [
+      BigDecimal('-2.3')...BigDecimal('4.4'),
+      BigDecimal('5.1')...BigDecimal('6.3'),
+      BigDecimal('12.1')...::Float::INFINITY
+    ], @multi_range.num_multirange
+    assert_equal [
+      BigDecimal('0.3')..BigDecimal('0.9'),
+      BigDecimal('1.3')...::Float::INFINITY
+    ], @overlaping_ranges.num_multirange
+    assert_equal [-::Float::INFINITY...::Float::INFINITY], @infinity_ranges.num_multirange
+  end
+
+  def test_datemultirange_values
+    assert_equal [
+      Date.new(2022, 1, 2)...Date.new(2022, 1, 4),
+      Date.new(2022, 7, 10)...Date.new(2022, 8, 11),
+      Date.new(2022, 10, 1)...::Float::INFINITY
+    ], @multi_range.date_multirange
+    assert_equal [
+      Date.new(2022, 1, 2)...Date.new(2022, 1, 5),
+      Date.new(2022, 7, 10)...::Float::INFINITY
+    ], @overlaping_ranges.date_multirange
+    assert_equal [-::Float::INFINITY...::Float::INFINITY], @infinity_ranges.date_multirange
   end
 
   private
