76170ca2830e89154e2081891127a5433510b012
eileencodes <eileencodes@gmail.com>
2022-08-23 09:46:36 -0400

    Fix tests in sqlite3
    note that the database tasks for the other adapters havent been changed
    yet so those will still fail

--- activerecord/test/cases/tasks/database_tasks_test.rb
+++ activerecord/test/cases/tasks/database_tasks_test.rb
@@ -1108,7 +1108,7 @@ module ActiveRecord
     end
 
     def test_migrate_clears_schema_cache_afterward
-      assert_called(ActiveRecord::Base, :clear_cache!) do
+      assert_called(ActiveRecord::TemporaryConnection.current_connection, :clear_cache!) do
         ActiveRecord::Tasks::DatabaseTasks.migrate
       end
     end
@@ -1121,7 +1121,7 @@ module ActiveRecord
       define_method("test_#{k}_purge") do
         with_stubbed_new do
           assert_called(eval("@#{v}"), :purge) do
-            ActiveRecord::Tasks::DatabaseTasks.purge "adapter" => k
+            ActiveRecord::Tasks::DatabaseTasks.purge({ "adapter" => k })
           end
         end
       end
