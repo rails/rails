653bb52dbc2bb6f56fe5eb3ae9a12f3526786988
eileencodes <eileencodes@gmail.com>
2022-09-30 08:59:48 -0400

    does this work

--- tools/test_common.rb
+++ tools/test_common.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+if defined?(RubyVM::YJIT)
+  if RubyVM::YJIT.enabled?
+    puts "YJIT enabled? #{RubyVM::YJIT.enabled?}"
+  else
+    puts "YJIT is defined but not enabled"
+  end
+else
+  puts "YJIT is not enabled or defined."
+end
+
 if ENV["BUILDKITE"]
   require "minitest-ci"
 
