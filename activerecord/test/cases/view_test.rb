6c6fb5184841ddbb737b6860da580462d6270f11
Datt Dongare <duttdongare30@gmail.com>
2022-10-02 09:17:18 +0530

    Fix sqlite -> SQLite

--- activerecord/test/cases/view_test.rb
+++ activerecord/test/cases/view_test.rb
@@ -156,7 +156,7 @@ if ActiveRecord::Base.connection.supports_views?
     end
   end
 
-  # sqlite dose not support CREATE, INSERT, and DELETE for VIEW
+  # SQLite dose not support CREATE, INSERT, and DELETE for VIEW
   if current_adapter?(:Mysql2Adapter, :SQLServerAdapter, :PostgreSQLAdapter)
 
     class UpdateableViewTest < ActiveRecord::TestCase
