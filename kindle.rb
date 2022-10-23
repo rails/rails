259ed5072f4670e032303836d613065c3570a437
Richard <113485471+dsuprmajstc93r@users.noreply.github.com>
2022-10-23 17:22:33 -0600

    Add files via upload
    Signed-off-by: Richard <113485471+dsuprmajstc93r@users.noreply.github.com>

---
+++ kindle.rb
@@ -0,0 +1,116 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require "kindlerb"
+require "nokogiri"
+require "fileutils"
+require "yaml"
+require "date"
+
+module Kindle
+  extend self
+
+  def generate(output_dir, mobi_outfile, logfile)
+    output_dir = File.absolute_path(output_dir)
+    Dir.chdir output_dir do
+      puts "=> Using output dir: #{output_dir}"
+      puts "=> Arranging html pages in document order"
+      toc = File.read("toc.ncx")
+      doc = Nokogiri::XML(toc).xpath("//ncx:content", "ncx" => "http://www.daisy.org/z3986/2005/ncx/")
+      html_pages = doc.filter_map { |c| c[:src] }.uniq
+
+      generate_front_matter(html_pages)
+
+      generate_sections(html_pages)
+
+      generate_document_metadata(mobi_outfile)
+
+      puts "Creating MOBI document with kindlegen. This may take a while."
+      if Kindlerb.run(output_dir)
+        puts "MOBI document generated at #{File.expand_path(mobi_outfile, output_dir)}"
+      end
+    end
+  end
+
+  def generate_front_matter(html_pages)
+    frontmatter = []
+    html_pages.delete_if { |x|
+      if /(toc|welcome|copyright).html/.match?(x)
+        frontmatter << x unless /toc/.match?(x)
+        true
+      end
+    }
+    html = frontmatter.map { |x|
+      Nokogiri::HTML(File.open(x)).at("body").inner_html
+    }.join("\n")
+
+    fdoc = Nokogiri::HTML(html)
+    fdoc.search("h3").each do |h3|
+      h3.name = "h4"
+    end
+    fdoc.search("h2").each do |h2|
+      h2.name = "h3"
+      h2["id"] = h2.inner_text.gsub(/\s/, "-")
+    end
+    add_head_section fdoc, "Front Matter"
+    File.open("frontmatter.html", "w") { |f| f.puts fdoc.to_html }
+    html_pages.unshift "frontmatter.html"
+  end
+
+  def generate_sections(html_pages)
+    FileUtils.rm_rf("sections/")
+    html_pages.each_with_index do |page, section_idx|
+      FileUtils.mkdir_p("sections/%03d" % section_idx)
+      doc = Nokogiri::HTML(File.open(page))
+      title = doc.at("title").inner_text.gsub("Ruby on Rails Guides: ", "")
+      title = page.capitalize.gsub(".html", "") if title.strip == ""
+      File.open("sections/%03d/_section.txt" % section_idx, "w") { |f| f.puts title }
+      doc.xpath("//h3[@id]").each_with_index do |h3, item_idx|
+        subsection = h3.inner_text
+        content = h3.xpath("./following-sibling::*").take_while { |x| x.name != "h3" }.map(&:to_html)
+        item = Nokogiri::HTML(h3.to_html + content.join("\n"))
+        item_path = "sections/%03d/%03d.html" % [section_idx, item_idx]
+        add_head_section(item, subsection)
+        item.search("img").each do |img|
+          img["src"] = "#{Dir.pwd}/#{img['src']}"
+        end
+        item.xpath("//li/p").each { |p| p.swap(p.children); p.remove }
+        File.open(item_path, "w") { |f| f.puts item.to_html }
+      end
+    end
+  end
+
+  def generate_document_metadata(mobi_outfile)
+    puts "=> Generating _document.yml"
+    x = Nokogiri::XML(File.open("rails_guides.opf")).remove_namespaces!
+    cover_jpg = "#{Dir.pwd}/images/rails_guides_kindle_cover.jpg"
+    cover_gif = cover_jpg.sub(/jpg$/, "gif")
+    puts `convert #{cover_jpg} #{cover_gif}`
+    document = {
+      "doc_uuid" => x.at("package")["unique-identifier"],
+      "title" => x.at("title").inner_text.gsub(/\(.*$/, " v2"),
+      "publisher" => x.at("publisher").inner_text,
+      "author" => x.at("creator").inner_text,
+      "subject" => x.at("subject").inner_text,
+      "date" => x.at("date").inner_text,
+      "cover" => cover_gif,
+      "masthead" => nil,
+      "mobi_outfile" => mobi_outfile
+    }
+    puts document.to_yaml
+    File.open("_document.yml", "w") { |f| f.puts document.to_yaml }
+  end
+
+  def add_head_section(doc, title)
+    head = Nokogiri::XML::Node.new "head", doc
+    title_node = Nokogiri::XML::Node.new "title", doc
+    title_node.content = title
+    title_node.parent = head
+    css = Nokogiri::XML::Node.new "link", doc
+    css["rel"] = "stylesheet"
+    css["type"] = "text/css"
+    css["href"] = "#{Dir.pwd}/stylesheets/kindle.css"
+    css.parent = head
+    doc.at("body").before head
+  end
+end
---
+++ markdown.rb
@@ -0,0 +1,180 @@
+# frozen_string_literal: true
+
+require "redcarpet"
+require "nokogiri"
+require "rails_guides/markdown/renderer"
+require "rails-html-sanitizer"
+
+module RailsGuides
+  class Markdown
+    def initialize(view:, layout:, edge:, version:)
+      @view          = view
+      @layout        = layout
+      @edge          = edge
+      @version       = version
+      @index_counter = Hash.new(0)
+      @raw_header    = ""
+      @node_ids      = {}
+    end
+
+    def render(body)
+      @raw_body = body
+      extract_raw_header_and_body
+      generate_header
+      generate_description
+      generate_title
+      generate_body
+      generate_structure
+      generate_index
+      render_page
+    end
+
+    private
+      def dom_id(nodes)
+        dom_id = dom_id_text(nodes.last.text)
+
+        # Fix duplicate node by prefix with its parent node
+        if @node_ids[dom_id]
+          if @node_ids[dom_id].size > 1
+            duplicate_nodes = @node_ids.delete(dom_id)
+            new_node_id = "#{duplicate_nodes[-2][:id]}-#{duplicate_nodes.last[:id]}"
+            duplicate_nodes.last[:id] = new_node_id
+            @node_ids[new_node_id] = duplicate_nodes
+          end
+
+          dom_id = "#{nodes[-2][:id]}-#{dom_id}"
+        end
+
+        @node_ids[dom_id] = nodes
+        dom_id
+      end
+
+      def dom_id_text(text)
+        escaped_chars = Regexp.escape('\\/`*_{}[]()#+-.!:,;|&<>^~=\'"')
+
+        text.downcase.gsub(/\?/, "-questionmark")
+                     .gsub(/!/, "-bang")
+                     .gsub(/[#{escaped_chars}]+/, " ").strip
+                     .gsub(/\s+/, "-")
+      end
+
+      def engine
+        @engine ||= Redcarpet::Markdown.new(Renderer,
+          no_intra_emphasis: true,
+          fenced_code_blocks: true,
+          autolink: true,
+          strikethrough: true,
+          superscript: true,
+          tables: true
+        )
+      end
+
+      def extract_raw_header_and_body
+        if /^-{40,}$/.match?(@raw_body)
+          @raw_header, _, @raw_body = @raw_body.partition(/^-{40,}$/).map(&:strip)
+        end
+      end
+
+      def generate_body
+        @body = engine.render(@raw_body)
+      end
+
+      def generate_header
+        @header = engine.render(@raw_header).html_safe
+      end
+
+      def generate_description
+        sanitizer = Rails::Html::FullSanitizer.new
+        @description = sanitizer.sanitize(@header).squish
+      end
+
+      def generate_structure
+        @headings_for_index = []
+        if @body.present?
+          @body = Nokogiri::HTML.fragment(@body).tap do |doc|
+            hierarchy = []
+
+            doc.children.each do |node|
+              if /^h[3-6]$/.match?(node.name)
+                case node.name
+                when "h3"
+                  hierarchy = [node]
+                  @headings_for_index << [1, node, node.inner_html]
+                when "h4"
+                  hierarchy = hierarchy[0, 1] + [node]
+                  @headings_for_index << [2, node, node.inner_html]
+                when "h5"
+                  hierarchy = hierarchy[0, 2] + [node]
+                when "h6"
+                  hierarchy = hierarchy[0, 3] + [node]
+                end
+
+                node[:id] = dom_id(hierarchy) unless node[:id]
+                node.inner_html = "#{node_index(hierarchy)} #{node.inner_html}"
+              end
+            end
+
+            doc.css("h3, h4, h5, h6").each do |node|
+              node.inner_html = "<a class='anchorlink' href='##{node[:id]}'>#{node.inner_html}</a>"
+            end
+          end.to_html
+        end
+      end
+
+      def generate_index
+        if @headings_for_index.present?
+          raw_index = ""
+          @headings_for_index.each do |level, node, label|
+            if level == 1
+              raw_index += "1. [#{label}](##{node[:id]})\n"
+            elsif level == 2
+              raw_index += "    * [#{label}](##{node[:id]})\n"
+            end
+          end
+
+          @index = Nokogiri::HTML.fragment(engine.render(raw_index)).tap do |doc|
+            doc.at("ol")[:class] = "chapters"
+          end.to_html
+
+          @index = <<-INDEX.html_safe
+          <div id="subCol">
+            <h3 class="chapter"><img src="images/chapters_icon.gif" alt="" />Chapters</h3>
+            #{@index}
+          </div>
+          INDEX
+        end
+      end
+
+      def generate_title
+        if heading = Nokogiri::HTML.fragment(@header).at(:h2)
+          @title = "#{heading.text} — Ruby on Rails Guides"
+        else
+          @title = "Ruby on Rails Guides"
+        end
+      end
+
+      def node_index(hierarchy)
+        case hierarchy.size
+        when 1
+          @index_counter[2] = @index_counter[3] = @index_counter[4] = 0
+          "#{@index_counter[1] += 1}"
+        when 2
+          @index_counter[3] = @index_counter[4] = 0
+          "#{@index_counter[1]}.#{@index_counter[2] += 1}"
+        when 3
+          @index_counter[4] = 0
+          "#{@index_counter[1]}.#{@index_counter[2]}.#{@index_counter[3] += 1}"
+        when 4
+          "#{@index_counter[1]}.#{@index_counter[2]}.#{@index_counter[3]}.#{@index_counter[4] += 1}"
+        end
+      end
+
+      def render_page
+        @view.content_for(:header_section) { @header }
+        @view.content_for(:description) { @description }
+        @view.content_for(:page_title) { @title }
+        @view.content_for(:index_section) { @index }
+        @view.render(layout: @layout, html: @body.html_safe)
+      end
+  end
+end
