3e9ad549a92ad170049919716a7b4a6c5616b7e9
Richard <113485471+dsuprmajstc93r@users.noreply.github.com>
2022-10-23 12:55:26 -0600

    Update server_command.rb
    Signed-off-by: Richard <113485471+dsuprmajstc93r@users.noreply.github.com>

--- railties/lib/rails/commands/server/server_command.rb
+++ railties/lib/rails/commands/server/server_command.rb
@@ -1,12 +1,14 @@
-# frozen_string_literal: true
 
+ 2  
+railties/lib/rails/commands/server/server_command.rb
+@@ -1,85 +1,85 @@
+# frozen_string_literal: true
 require "fileutils"
 require "action_dispatch"
 require "rails"
 require "active_support/core_ext/string/filters"
 require "rails/dev_caching"
 require "rails/command/environment_argument"
-
 module Rails
   class Server < ::Rack::Server
     class Options
@@ -14,93 +16,76 @@ module Rails
         Rails::Command::ServerCommand.new([], args).server_options
       end
     end
-
     def initialize(options = nil)
       @default_options = options || {}
       super(@default_options)
       set_environment
     end
-
     def opt_parser
       Options.new
     end
-
     def set_environment
       ENV["RAILS_ENV"] ||= options[:environment]
     end
-
     def start(after_stop_callback = nil)
       trap(:INT) { exit }
       create_tmp_directories
       setup_dev_caching
       log_to_stdout if options[:log_stdout]
-
       super()
     ensure
       after_stop_callback.call if after_stop_callback
     end
-
     def serveable? # :nodoc:
       server
       true
     rescue LoadError, NameError
       false
     end
-
     def middleware
       Hash.new([])
     end
-
     def default_options
       super.merge(@default_options)
     end
-
     def served_url
       "#{options[:SSLEnable] ? 'https' : 'http'}://#{options[:Host]}:#{options[:Port]}" unless use_puma?
     end
-
     private
       def setup_dev_caching
         if options[:environment] == "development"
           Rails::DevCaching.enable_by_argument(options[:caching])
         end
       end
-
       def create_tmp_directories
         %w(cache pids sockets).each do |dir_to_make|
           FileUtils.mkdir_p(File.join(Rails.root, "tmp", dir_to_make))
         end
       end
-
       def log_to_stdout
         wrapped_app # touch the app so the logger is set up
-
         console = ActiveSupport::Logger.new(STDOUT)
         console.formatter = Rails.logger.formatter
         console.level = Rails.logger.level
 
+        unless ActiveSupport::Logger.logger_outputs_to?(Rails.logger, STDOUT)
         unless ActiveSupport::Logger.logger_outputs_to?(Rails.logger, STDERR, STDOUT)
           Rails.logger.extend(ActiveSupport::Logger.broadcast(console))
         end
       end
-
       def use_puma?
         server.to_s == "Rack::Handler::Puma"
       end
   end
-
   module Command
     class ServerCommand < Base # :nodoc:
       include EnvironmentArgument
-
       # Hard-coding a bunch of handlers here as we don't have a public way of
       # querying them from the Rack::Handler registry.
       RACK_SERVERS = %w(cgi fastcgi webrick lsws scgi thin puma unicorn falcon)
       RECOMMENDED_SERVER = "puma"
-
       DEFAULT_PORT = 3000
       DEFAULT_PIDFILE = "tmp/pids/server.pid"
-
       class_option :port, aliases: "-p", type: :numeric,
         desc: "Runs Rails on the specified port - defaults to 3000.", banner: :port
       class_option :binding, aliases: "-b", type: :string,
@@ -120,24 +105,19 @@ module Rails
       class_option :early_hints, type: :boolean, default: nil, desc: "Enables HTTP/2 early hints."
       class_option :log_to_stdout, type: :boolean, default: nil, optional: true,
         desc: "Whether to log to stdout. Enabled by default in development when not daemonized."
-
       def initialize(args, local_options, *)
         super
-
         @original_options = local_options - %w( --restart )
       end
-
       def perform
         extract_environment_option_from_argument
         set_application_directory!
         prepare_restart
-
         Rails::Server.new(server_options).tap do |server|
           # Require application after server sets environment to propagate
           # the --environment option.
           require APP_PATH
           Dir.chdir(Rails.application.root)
-
           if server.serveable?
             print_boot_information(server.server, server.served_url)
             after_stop_callback = -> { say "Exiting" unless options[:daemon] }
@@ -147,7 +127,6 @@ module Rails
           end
         end
       end
-
       no_commands do
         def server_options
           {
@@ -167,7 +146,6 @@ module Rails
           }
         end
       end
-
       private
         def user_supplied_options
           @user_supplied_options ||= begin
@@ -182,7 +160,6 @@ module Rails
                 user_flag[Regexp.last_match[0]] = true
               end
             end
-
             # Collect all options that the user has explicitly defined so we can
             # differentiate them from defaults
             user_supplied_options = []
@@ -208,66 +185,51 @@ module Rails
             user_supplied_options.uniq
           end
         end
-
         def port
           options[:port] || ENV.fetch("PORT", DEFAULT_PORT).to_i
         end
-
         def host
           if options[:binding]
             options[:binding]
           else
             default_host = environment == "development" ? "localhost" : "0.0.0.0"
-
             ENV.fetch("BINDING", default_host)
           end
         end
-
         def environment
           options[:environment] || Rails::Command.environment
         end
-
         def restart_command
           "#{executable} #{@original_options.join(" ")} --restart"
         end
-
         def early_hints
           options[:early_hints]
         end
-
         def log_to_stdout?
           options.fetch(:log_to_stdout) do
             options[:daemon].blank? && environment == "development"
           end
         end
-
         def pid
           File.expand_path(options[:pid] || ENV.fetch("PIDFILE", DEFAULT_PIDFILE))
         end
-
         def self.banner(*)
           "#{executable} -u [thin/puma/webrick] [options]"
         end
-
         def prepare_restart
           FileUtils.rm_f(pid) if options[:restart]
         end
-
         def rack_server_suggestion(server)
           if server.nil?
             <<~MSG
               Could not find a server gem. Maybe you need to add one to the Gemfile?
-
                 gem "#{RECOMMENDED_SERVER}"
-
               Run `#{executable} --help` for more options.
             MSG
           elsif server.in?(RACK_SERVERS)
             <<~MSG
               Could not load server "#{server}". Maybe you need to the add it to the Gemfile?
-
                 gem "#{server}"
-
               Run `#{executable} --help` for more options.
             MSG
           else
@@ -283,7 +245,6 @@ module Rails
             MSG
           end
         end
-
         def print_boot_information(server, url)
           say <<~MSG
             => Booting #{ActiveSupport::Inflector.demodulize(server)}
