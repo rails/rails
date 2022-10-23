1ec72f92a654654deb879891ce5e316944dd90bc
Richard <113485471+dsuprmajstc93r@users.noreply.github.com>
2022-10-23 15:53:20 -0600

    Add files via upload
    Signed-off-by: Richard <113485471+dsuprmajstc93r@users.noreply.github.com>

---
+++ base.rb
@@ -0,0 +1,183 @@
+# frozen_string_literal: true
+
+require "thor"
+require "erb"
+
+require "active_support/core_ext/string/filters"
+require "active_support/core_ext/string/inflections"
+
+require "rails/command/actions"
+
+module Rails
+  module Command
+    class Base < Thor
+      class Error < Thor::Error # :nodoc:
+      end
+
+      class CorrectableError < Error # :nodoc:
+        attr_reader :key, :options
+
+        def initialize(message, key, options)
+          @key     = key
+          @options = options
+          super(message)
+        end
+
+        if defined?(DidYouMean::SpellChecker) && defined?(DidYouMean::Correctable)
+          include DidYouMean::Correctable
+
+          def corrections
+            @corrections ||= DidYouMean::SpellChecker.new(dictionary: options).correct(key)
+          end
+        end
+      end
+
+      include Actions
+
+      class << self
+        def exit_on_failure? # :nodoc:
+          false
+        end
+
+        # Returns true when the app is a Rails engine.
+        def engine?
+          defined?(ENGINE_ROOT)
+        end
+
+        # Tries to get the description from a USAGE file one folder above the command
+        # root.
+        def desc(usage = nil, description = nil, options = {})
+          if usage
+            super
+          else
+            @desc ||= ERB.new(File.read(usage_path), trim_mode: "-").result(binding) if usage_path
+          end
+        end
+
+        # Convenience method to get the namespace from the class name. It's the
+        # same as Thor default except that the Command at the end of the class
+        # is removed.
+        def namespace(name = nil)
+          if name
+            super
+          else
+            @namespace ||= super.chomp("_command").sub(/:command:/, ":")
+          end
+        end
+
+        # Convenience method to hide this command from the available ones when
+        # running rails command.
+        def hide_command!
+          Rails::Command.hidden_commands << self
+        end
+
+        def inherited(base) # :nodoc:
+          super
+
+          if base.name && !base.name.end_with?("Base")
+            Rails::Command.subclasses << base
+          end
+        end
+
+        def perform(command, args, config) # :nodoc:
+          if Rails::Command::HELP_MAPPINGS.include?(args.first)
+            command, args = "help", []
+          end
+
+          dispatch(command, args.dup, nil, config)
+        end
+
+        def printing_commands
+          namespaced_commands
+        end
+
+        def executable
+          "rails #{command_name}"
+        end
+
+        # Use Rails' default banner.
+        def banner(*)
+          "#{executable} #{arguments.map(&:usage).join(' ')} [options]".squish
+        end
+
+        # Sets the base_name taking into account the current class namespace.
+        #
+        #   Rails::Command::TestCommand.base_name # => 'rails'
+        def base_name
+          @base_name ||= if base = name.to_s.split("::").first
+            base.underscore
+          end
+        end
+
+        # Return command name without namespaces.
+        #
+        #   Rails::Command::TestCommand.command_name # => 'test'
+        def command_name
+          @command_name ||= if command = name.to_s.split("::").last
+            command.chomp!("Command")
+            command.underscore
+          end
+        end
+
+        # Path to lookup a USAGE description in a file.
+        def usage_path
+          if default_command_root
+            path = File.join(default_command_root, "USAGE")
+            path if File.exist?(path)
+          end
+        end
+
+        # Default file root to place extra files a command might need, placed
+        # one folder above the command file.
+        #
+        # For a Rails::Command::TestCommand placed in <tt>rails/command/test_command.rb</tt>
+        # would return <tt>rails/test</tt>.
+        def default_command_root
+          path = File.expand_path(relative_command_path, __dir__)
+          path if File.exist?(path)
+        end
+
+        private
+          # Allow the command method to be called perform.
+          def create_command(meth)
+            if meth == "perform"
+              alias_method command_name, meth
+            else
+              # Prevent exception about command without usage.
+              # Some commands define their documentation differently.
+              @usage ||= ""
+              @desc  ||= ""
+
+              super
+            end
+          end
+
+          def command_root_namespace
+            (namespace.split(":") - %w(rails)).join(":")
+          end
+
+          def relative_command_path
+            File.join("../commands", *command_root_namespace.split(":"))
+          end
+
+          def namespaced_commands
+            commands.keys.map do |key|
+              if command_root_namespace.match?(/(\A|:)#{key}\z/)
+                command_root_namespace
+              else
+                "#{command_root_namespace}:#{key}"
+              end
+            end
+          end
+      end
+
+      def help
+        if command_name = self.class.command_name
+          self.class.command_help(shell, command_name)
+        else
+          super
+        end
+      end
+    end
+  end
+end
---
+++ behavior.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+require "active_support"
+
+module Rails
+  module Command
+    module Behavior # :nodoc:
+      extend ActiveSupport::Concern
+
+      class_methods do
+        # Remove the color from output.
+        def no_color!
+          Thor::Base.shell = Thor::Shell::Basic
+        end
+
+        # Track all command subclasses.
+        def subclasses
+          @subclasses ||= []
+        end
+
+        private
+          # Prints a list of generators.
+          def print_list(base, namespaces)
+            return if namespaces.empty?
+            puts "#{base.camelize}:"
+
+            namespaces.each do |namespace|
+              puts("  #{namespace}")
+            end
+
+            puts
+          end
+
+          # Receives namespaces in an array and tries to find matching generators
+          # in the load path.
+          def lookup(namespaces)
+            paths = namespaces_to_paths(namespaces)
+
+            paths.each do |raw_path|
+              lookup_paths.each do |base|
+                path = "#{base}/#{raw_path}_#{command_type}"
+
+                begin
+                  require path
+                  return
+                rescue LoadError => e
+                  raise unless /#{Regexp.escape(path)}$/.match?(e.message)
+                rescue Exception => e
+                  warn "[WARNING] Could not load #{command_type} #{path.inspect}. Error: #{e.message}.\n#{e.backtrace.join("\n")}"
+                end
+              end
+            end
+          end
+
+          # This will try to load any command in the load path to show in help.
+          def lookup!
+            $LOAD_PATH.each do |base|
+              Dir[File.join(base, *file_lookup_paths)].each do |path|
+                path = path.delete_prefix("#{base}/")
+                require path
+              rescue Exception
+                # No problem
+              end
+            end
+          end
+
+          # Convert namespaces to paths by replacing ":" for "/" and adding
+          # an extra lookup. For example, "rails:model" should be searched
+          # in both: "rails/model/model_generator" and "rails/model_generator".
+          def namespaces_to_paths(namespaces)
+            paths = []
+            namespaces.each do |namespace|
+              pieces = namespace.split(":")
+              path = pieces.join("/")
+              paths << "#{path}/#{pieces.last}"
+              paths << path
+            end
+            paths.uniq!
+            paths
+          end
+      end
+    end
+  end
+end
