b5248aca16e417ea4099852314bf533527a07025
Jonathan Hefner <jonathan@hefner.pro>
2022-10-30 15:45:17 -0500

    Add ActionView.deprecator
    This commit adds `ActionView.deprecator` and replaces all usages of
    `ActiveSupport::Deprecation.warn` in `actionview/lib` with
    `ActionView.deprecator`.  This commit also replaces a call to Ruby's
    `Module#deprecate_constant` with Rails' `DeprecatedConstantProxy`, so
    that its deprecation behavior can be configured using
    `ActionView.deprecator`.
    
    Additionally, this commit adds `ActionView.deprecator` to
    `Rails.application.deprecators` so that it can be configured via
    settings such as `config.active_support.report_deprecations`.
    
    This commit also removes a few defunct `assert_deprecated` calls that
    were not failing because they were nested in `assert_raises`, and the
    raised error prevented checking the deprecation.  (One was mistakenly
    kept in d52d7739468153bd6cb7c629f60bd5cd7ebea3eb when converting
    `test_render_file_with_errors` to `test_render_template_with_errors`;
    the other two were added in dd9991bac598bb5da312278a749cf85e19b027cc but
    not removed when the deprecation was completed in
    85ecf6e4098601222b604f7c1cbdcb4e49a6d1f0.)

--- actionpack/test/controller/render_test.rb
+++ actionpack/test/controller/render_test.rb
@@ -395,9 +395,7 @@ class ExpiresInRenderTest < ActionController::TestCase
   def test_dynamic_render
     assert File.exist?(File.expand_path("../../test/abstract_unit.rb", __dir__))
     assert_raises ActionView::MissingTemplate do
-      assert_deprecated(ActionDispatch.deprecator) do
-        get :dynamic_render, params: { id: '../\\../test/abstract_unit.rb' }
-      end
+      get :dynamic_render, params: { id: '../\\../test/abstract_unit.rb' }
     end
   end
 
--- railties/test/application/configuration_test.rb
+++ railties/test/application/configuration_test.rb
@@ -3893,6 +3893,7 @@ module ApplicationTests
       assert_equal ActionController.deprecator, Rails.application.deprecators[:action_controller]
       assert_equal ActionDispatch.deprecator, Rails.application.deprecators[:action_dispatch]
       assert_equal ActionMailer.deprecator, Rails.application.deprecators[:action_mailer]
+      assert_equal ActionView.deprecator, Rails.application.deprecators[:action_view]
       assert_equal ActiveRecord.deprecator, Rails.application.deprecators[:active_record]
       assert_equal ActiveSupport.deprecator, Rails.application.deprecators[:active_support]
     end
