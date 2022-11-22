3b0d08c4efa662a38cee84b5804d6d3617aaa602
Rafael Mendonça França <rafael@rubyonrails.org>
2022-11-22 15:24:24 -0500

    Merge pull request #46545 from fatkodima/eager_load-validation_error
    Autoload `ActiveModel::ValidationError`

--- activemodel/lib/active_model.rb
+++ activemodel/lib/active_model.rb
@@ -58,6 +58,7 @@ module ActiveModel
     autoload :RangeError, "active_model/errors"
     autoload :StrictValidationFailed, "active_model/errors"
     autoload :UnknownAttributeError, "active_model/errors"
+    autoload :ValidationError, "active_model/validations"
   end
 
   module Serializers
