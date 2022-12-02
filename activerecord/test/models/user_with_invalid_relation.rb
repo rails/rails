76b440f287ebfaaa01fade03b3e94ddc9a43d106
Jonathan Hefner <jonathan@hefner.pro>
2022-12-02 11:20:19 -0600

    Rework association model class error messages
    Follow-up to #46605.
    
    For a scenario like:
    
      ```ruby
      class Cat < ActiveRecord::Base
        has_many :lives
      end
    
      class Life
      end
      ```
    
    this commit changes the error from:
    
    > Rails couldn't find a valid model for the lives association. Use the
    > :class_name option on the association declaration to tell Rails which
    > model to use.
    
    to:
    
    > The Life model class for the Cat#lives association is not an
    > ActiveRecord::Base subclass.
    
    Additionally, for a scenario like:
    
      ```ruby
      class Cat < ActiveRecord::Base
        has_many :lives
      end
    
      class Live < ActiveRecord::Base
      end
      ```
    
    this commit changes the error from:
    
    > Rails couldn't find a valid model for the lives association. Use the
    > :class_name option on the association declaration to tell Rails which
    > model to use.
    
    to:
    
    > Missing model class Life for the Cat#lives association. You can
    > specify a different model class with the :class_name option.

--- activerecord/test/models/user_with_invalid_relation.rb
+++ activerecord/test/models/user_with_invalid_relation.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class UserWithInvalidRelation < ActiveRecord::Base
-  has_one :account_invalid_no_class_name_option
-
-  has_one :account_class_name, class_name: "AccountInvalid"
+  has_one :not_a_class
 
   has_one :class_name_provided_not_a_class, class_name: "NotAClass"
+
+  has_one :account_invalid
+
+  has_one :account_class_name, class_name: "AccountInvalid"
 
   has_many :user_info_invalid
   has_many :info_invalids, through: :user_info_invalid
