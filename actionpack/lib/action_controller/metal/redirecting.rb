f6cdce50d4a56a59ba5455a631b52cb84e41a8f8
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2022-12-05 12:27:15 +0000

    Fix sec issue with _url_host_allowed?
    Disallow certain strings from `_url_host_allowed?` to avoid a redirect
    to malicious sites.
    
    [CVE-2023-22797]

--- actionpack/lib/action_controller/metal/redirecting.rb
+++ actionpack/lib/action_controller/metal/redirecting.rb
@@ -196,7 +196,11 @@ module ActionController
 
       def _url_host_allowed?(url)
         host = URI(url.to_s).host
-        host == request.host || host.nil? && url.to_s.start_with?("/")
+
+        return true if host == request.host
+        return false unless host.nil?
+        return false unless url.to_s.start_with?("/")
+        return !url.to_s.start_with?("//")
       rescue ArgumentError, URI::Error
         false
       end
