fb6aff821746cedb18fd9d16c0b543d04199f024
Ivan Marynych <49816584+loqimean@users.noreply.github.com>
2022-12-23 23:44:36 +0200

    Update activesupport/lib/active_support/environment_inquirer.rb

--- activesupport/lib/active_support/environment_inquirer.rb
+++ activesupport/lib/active_support/environment_inquirer.rb
@@ -35,7 +35,7 @@ module ActiveSupport
       in? LOCAL_ENVIRONMENTS
     end
 
-    # Returns true if we're in the production or staging environment.
+    # Returns true if we are in the production or staging environment.
     def remote?
       in? REMOTE_ENVIRONMENTS
     end
