bcfdfefc0865c87e9bc8389a9a587e6890667680
Akira Matsuda <ronnie@dio.jp>
2023-01-01 17:52:10 +0900

    Call AR::Connection's method via connection instance

--- activerecord/test/schema/mysql2_specific_schema.rb
+++ activerecord/test/schema/mysql2_specific_schema.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ActiveRecord::Schema.define do
-  if supports_datetime_with_precision?
+  if connection.supports_datetime_with_precision?
     create_table :datetime_defaults, force: true do |t|
       t.datetime :modified_datetime, precision: nil, default: -> { "CURRENT_TIMESTAMP" }
       t.datetime :precise_datetime, default: -> { "CURRENT_TIMESTAMP(6)" }
