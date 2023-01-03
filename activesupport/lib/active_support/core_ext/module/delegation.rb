8b2f57dc6fc4400a1554a11b24dfb6c854310d81
Akira Matsuda <ronnie@dio.jp>
2023-01-03 19:50:32 +0900

    Let `delegate` define method with proper arity when delegating to a Class / Module
    Method delegation with `...` argument is known to be slow because it allocates
    redundant Array and Hash objects each time when being called.
    see: https://bugs.ruby-lang.org/issues/19165
    
    Current implementation of `delegate` defines all delegation methods in this
    slow form regardless of the original method's arity, but in case the delegation
    target is a Class or a Module, we can investigate the arity of the original
    method in the definition timing, then we can define the delegation in proper
    minimal arity.
    
    This makes 3.5 times faster zero arity method delegation as follows:
    Warming up --------------------------------------
                     old   811.534k i/100ms
                     new     1.807M i/100ms
    Calculating -------------------------------------
                     old      9.809M (± 3.4%) i/s -     49.504M in   5.053355s
                     new     34.360M (± 0.8%) i/s -    173.465M in   5.048692s
    
    Comparison:
                     new: 34360408.4 i/s
                     old:  9809157.4 i/s - 3.50x  (± 0.00) slower

--- activesupport/lib/active_support/core_ext/module/delegation.rb
+++ activesupport/lib/active_support/core_ext/module/delegation.rb
@@ -187,8 +187,8 @@ class Module
     location = caller_locations(1, 1).first
     file, line = location.path, location.lineno
 
-    to = to.to_s
-    to = "self.#{to}" if DELEGATION_RESERVED_METHOD_NAMES.include?(to)
+    receiver = to.to_s
+    receiver = "self.#{receiver}" if DELEGATION_RESERVED_METHOD_NAMES.include?(receiver)
 
     method_def = []
     method_names = []
@@ -199,7 +199,35 @@ class Module
 
       # Attribute writer methods only accept one argument. Makes sure []=
       # methods still accept two arguments.
-      definition = /[^\]]=\z/.match?(method) ? "arg" : "..."
+      definition = \
+        if /[^\]]=\z/.match?(method)
+          "arg"
+        else
+          method_object =
+            begin
+              if to.is_a?(Module)
+                to.method(method)
+              elsif receiver == "self.class"
+                method(method)
+              end
+            rescue NameError
+              # Do nothing. Fall back to `"..."`
+            end
+
+          if method_object
+            parameters = method_object.parameters
+
+            if (parameters.map(&:first) & [:opt, :rest, :keyreq, :key, :keyrest]).any?
+              "..."
+            else
+              defn = parameters.filter_map { |type, arg| arg if type == :req }
+              defn << "&block" if parameters.last&.first == :block
+              defn.join(", ")
+            end
+          else
+            "..."
+          end
+        end
 
       # The following generated method calls the target exactly once, storing
       # the returned value in a dummy variable.
@@ -213,7 +241,7 @@ class Module
 
         method_def <<
           "def #{method_name}(#{definition})" <<
-          "  _ = #{to}" <<
+          "  _ = #{receiver}" <<
           "  if !_.nil? || nil.respond_to?(:#{method})" <<
           "    _.#{method}(#{definition})" <<
           "  end" <<
@@ -224,11 +252,11 @@ class Module
 
         method_def <<
           "def #{method_name}(#{definition})" <<
-          "  _ = #{to}" <<
+          "  _ = #{receiver}" <<
           "  _.#{method}(#{definition})" <<
           "rescue NoMethodError => e" <<
           "  if _.nil? && e.name == :#{method}" <<
-          %(   raise DelegationError, "#{self}##{method_name} delegated to #{to}.#{method}, but #{to} is nil: \#{self.inspect}") <<
+          %(   raise DelegationError, "#{self}##{method_name} delegated to #{receiver}.#{method}, but #{receiver} is nil: \#{self.inspect}") <<
           "  else" <<
           "    raise" <<
           "  end" <<
