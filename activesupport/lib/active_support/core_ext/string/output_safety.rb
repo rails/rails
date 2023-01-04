3468503513c8f87a6ea1bd709a5c114cc2eb332a
Akira Matsuda <ronnie@dio.jp>
2023-01-05 05:25:37 +0900

    Implement SafeBuffer#bytesplice

--- activesupport/lib/active_support/core_ext/string/output_safety.rb
+++ activesupport/lib/active_support/core_ext/string/output_safety.rb
@@ -219,6 +219,10 @@ module ActiveSupport # :nodoc:
     end
     alias << concat
 
+    def bytesplice(*args, value)
+      super(*args, implicit_html_escape_interpolated_argument(value))
+    end
+
     def insert(index, value)
       super(index, implicit_html_escape_interpolated_argument(value))
     end
