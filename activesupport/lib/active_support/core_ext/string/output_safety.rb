217411564e3930e2206a3af598a4762a94e716c6
Akira Matsuda <ronnie@dio.jp>
2023-01-05 05:25:37 +0900

    Implement SafeBuffer#bytesplice
    [CVE-2023-28120]

--- activesupport/lib/active_support/core_ext/string/output_safety.rb
+++ activesupport/lib/active_support/core_ext/string/output_safety.rb
@@ -89,6 +89,10 @@ module ActiveSupport # :nodoc:
     end
     alias << concat
 
+    def bytesplice(*args, value)
+      super(*args, implicit_html_escape_interpolated_argument(value))
+    end
+
     def insert(index, value)
       super(index, implicit_html_escape_interpolated_argument(value))
     end
--- activesupport/test/core_ext/string_ext_test.rb
+++ activesupport/test/core_ext/string_ext_test.rb
@@ -1032,6 +1032,36 @@ class OutputSafetyTest < ActiveSupport::TestCase
     assert_predicate string, :html_safe?
   end
 
+  if "".respond_to?(:bytesplice)
+    test "Bytesplicing safe into safe yields safe" do
+      string = "hello".html_safe
+      string.bytesplice(0, 0, "<b>".html_safe)
+
+      assert_equal "<b>hello", string
+      assert_predicate string, :html_safe?
+
+      string = "hello".html_safe
+      string.bytesplice(0..1, "<b>".html_safe)
+
+      assert_equal "<b>llo", string
+      assert_predicate string, :html_safe?
+    end
+
+    test "Bytesplicing unsafe into safe yields escaped safe" do
+      string = "hello".html_safe
+      string.bytesplice(1, 0, "<b>")
+
+      assert_equal "h&lt;b&gt;ello", string
+      assert_predicate string, :html_safe?
+
+      string = "hello".html_safe
+      string.bytesplice(1..2, "<b>")
+
+      assert_equal "h&lt;b&gt;lo", string
+      assert_predicate string, :html_safe?
+    end
+  end
+
   test "emits normal string YAML" do
     assert_equal "foo".to_yaml, "foo".html_safe.to_yaml(foo: 1)
   end
