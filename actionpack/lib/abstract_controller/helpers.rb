556cc41b627e5f62a9b8f1a2a4a4d8582bfc81d0
Jonathan Hefner <jonathan@hefner.pro>
2023-01-05 15:23:57 -0600

    Merge pull request #46896 from sato11/helper-methods-need-not-be-public
    Tweak doc to show helper methods need not be public [ci-skip]
    
    (cherry picked from commit 1f5c31efd24c1e7d36d9090d71fdb75489c9ca7a)

--- actionpack/lib/abstract_controller/helpers.rb
+++ actionpack/lib/abstract_controller/helpers.rb
@@ -61,6 +61,8 @@ module AbstractController
       #   class ApplicationController < ActionController::Base
       #     helper_method :current_user, :logged_in?
       #
+      #     private
+      #
       #     def current_user
       #       @current_user ||= User.find_by(id: session[:user])
       #     end
