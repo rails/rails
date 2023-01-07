rails/rails

commit 0019dea961dbd758812dbb403460c47cd3472be1

Only use HostAuthorization if configured
Previously, HostAuthorization would be added to the middleware stack
whether or not config.hosts had any entries. While HostAuthorization
does check config.hosts before doing computationally expensive things,
its inclusion ends up being easily avoidable overhead on every request.

railties/lib/rails/application/default_middleware_stack.rb
@@ -13,7 +13,9 @@ module Rails
 
       def build_stack
         ActionDispatch::MiddlewareStack.new do |middleware|
-          middleware.use ::ActionDispatch::HostAuthorization, config.hosts, **config.host_authorization
+          unless config.hosts.empty?
+            middleware.use ::ActionDispatch::HostAuthorization, config.hosts, **config.host_authorization
+          end
 
           if config.force_ssl
             middleware.use ::ActionDispatch::SSL, **config.ssl_options,

railties/test/application/middleware_test.rb
@@ -147,6 +147,13 @@ module ApplicationTests
       assert_equal sorted, middleware
     end
 
+    test "ActionDispatch::HostAuthorization is not included if no hosts are configured" do
+      add_to_config "config.hosts = []"
+      boot!
+
+      assert_not_includes middleware, "ActionDispatch::HostAuthorization"
+    end
+
     test "Rack::Cache is not included by default" do
       boot!
 

commit 27ddfa104552f9ed3b3a98ecef9a4dc903bc064c

Allow setting config.hosts to single IPAddr
This was broken in 0019dea and partially fixed for setting config.hosts
to a nil value in 56ecea0.

However, since HostAuthorization coerces the hosts param to an array to
allow a single String or IPAddr as a valid value, the check should do
the same.

railties/lib/rails/application/default_middleware_stack.rb
@@ -13,7 +13,7 @@ module Rails
 
       def build_stack
         ActionDispatch::MiddlewareStack.new do |middleware|
-          unless config.hosts.nil? || config.hosts.empty?
+          unless Array(config.hosts).empty?
             middleware.use ::ActionDispatch::HostAuthorization, config.hosts, **config.host_authorization
           end
 

railties/test/application/middleware_test.rb
@@ -161,6 +161,13 @@ module ApplicationTests
       assert_not_includes middleware, "ActionDispatch::HostAuthorization"
     end
 
+    test "ActionDispatch::HostAuthorization is included if hosts is set to IPAddr" do
+      add_to_config "config.hosts = IPAddr.new('0.0.0.0/0')"
+      boot!
+
+      assert_includes middleware, "ActionDispatch::HostAuthorization"
+    end
+
     test "Rack::Cache is not included by default" do
       boot!
 