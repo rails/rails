fda7cf37a5cdde131e11478ca7d6cd7e883dec14
Nikita Vasilevsky <nikita.vasilevsky@shopify.com>
2023-01-11 01:00:42 +0000

    Support composite foreign key association assignments
    Given a model with a composite primary key, it is possible to assign
    the association associated with the model by the composite foreign key.
    
    For example, given two `Sharded::BlogPost` and `Sharded::Comment` models
    that are associated using a composite foreign key so that:
    `Sharded::BlogPost.has_many :comments, foreign_key: [:blog_id, :blog_post_id]`,
    it is possible to assign an instance of `Comment` to an instance of
    `BlogPost` like `blog_post.comments << comment`.
    The same applies to the opposing `:belongs_to` association.

--- activerecord/lib/active_record/associations/belongs_to_association.rb
+++ activerecord/lib/active_record/associations/belongs_to_association.rb
@@ -119,10 +119,13 @@ module ActiveRecord
         end
 
         def replace_keys(record, force: false)
-          target_key = record ? record._read_attribute(primary_key(record.class)) : nil
+          target_key_values = record ? Array(primary_key(record.class)).map { |key| record._read_attribute(key) } : []
+          reflection_fk = Array(reflection.foreign_key)
 
-          if force || owner._read_attribute(reflection.foreign_key) != target_key
-            owner[reflection.foreign_key] = target_key
+          if force || reflection_fk.map { |fk| owner._read_attribute(fk) } != target_key_values
+            reflection_fk.zip(target_key_values).each do |key, value|
+              owner[key] = value
+            end
           end
         end
 
