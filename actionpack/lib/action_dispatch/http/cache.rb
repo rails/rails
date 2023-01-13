484fc9185db6c6a6a49ab458b11f9366da02bab2
John Hawthorn <john@hawthorn.email>
2023-01-13 15:54:40 -0800

    Avoid regex backtracking on If-None-Match header
    [CVE-2023-22795]

--- actionpack/lib/action_dispatch/http/cache.rb
+++ actionpack/lib/action_dispatch/http/cache.rb
@@ -18,7 +18,7 @@ module ActionDispatch
         end
 
         def if_none_match_etags
-          if_none_match ? if_none_match.split(/\s*,\s*/) : []
+          if_none_match ? if_none_match.split(",").each(&:strip!) : []
         end
 
         def not_modified?(modified_at)
