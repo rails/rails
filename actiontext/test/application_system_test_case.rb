6c473afe1bac556f0e6561a08f04679d828265ce
Haroon Ahmed <haroon.ahmed25@gmail.com>
2023-01-24 21:37:17 +0000

    Add --no-sandbox flag to run action text integration tests.
    This disables Chromes sandboxing, which is required when running as root
    user.
    
    Fix #46804

--- actiontext/test/application_system_test_case.rb
+++ actiontext/test/application_system_test_case.rb
@@ -3,7 +3,9 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome
+  driven_by :selenium, using: :headless_chrome do |driver_option|
+    driver_option.add_argument("--no-sandbox")
+  end
 end
 
 Capybara.server = :puma, { Silent: true }
