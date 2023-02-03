0e3a5331a125fd7dd2c9f5c685e86c17757c504f
Hartley McGuire <skipkayhil@gmail.com>
2023-02-03 00:45:20 -0500

    Regenerate Action Mailbox dummy app
    Generated using the following script and manually reviewed after:
    skipkayhil/rails-bin@689861173098900d5909bdad257d18b1eba5414d
    
    The :messages route was removed because there isn't actually a
    MessagesController.
    
    config/boot.rb also has its Gemfile path fixed to point at the root
    Gemfile because Action Mailbox does not have a Gemfile.

--- actionmailbox/test/dummy/config/application.rb
+++ actionmailbox/test/dummy/config/application.rb
@@ -2,16 +2,23 @@ require_relative "boot"
 
 require "rails/all"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults Rails::VERSION::STRING.to_f
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # For compatibility with applications that use this config
+    config.action_controller.include_all_helpers = false
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
