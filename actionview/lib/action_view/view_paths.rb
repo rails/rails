785e02a2ed960dbb4d07817a98d7fee55153c3b5
John Hawthorn <john@hawthorn.email>
2023-02-08 20:22:47 -0800

    Cache resolvers from {append,prepend}_view_path
    When prepending or appending view paths, Rails will create an ActionView::Resolver
    for each String or Pathname argument. Previously, these were not cached. If view
    paths were added in a request context (which is a common thing in a multi-tenant
    application), new resolvers were created on each request. This effectively
    disabled template caching for the added view paths.
    
    Further, under certain circumstances, it also created a memory leak.
    
    This commit fixes both issues by turning dynamic view path strings into cached
    resolvers.
    
    Co-authored-by: Dominik Schöler <dominik.schoeler@makandra.de>
    Co-authored-by: John Hawthorn <john@hawthorn.email>

--- actionview/lib/action_view/view_paths.rb
+++ actionview/lib/action_view/view_paths.rb
@@ -71,18 +71,45 @@ module ActionView
     end
 
     module Registry # :nodoc:
-      @all_view_paths = {}
+      @view_paths_by_class = {}
+      @file_system_resolvers = Concurrent::Map.new
+
+      class << self
+        include ActiveSupport::Callbacks
+        define_callbacks :build_file_system_resolver
+      end
 
       def self.get_view_paths(klass)
-        @all_view_paths[klass] || get_view_paths(klass.superclass)
+        @view_paths_by_class[klass] || get_view_paths(klass.superclass)
       end
 
       def self.set_view_paths(klass, paths)
-        @all_view_paths[klass] = paths
+        @view_paths_by_class[klass] = paths
       end
 
       def self.all_resolvers
         @all_view_paths.values.map(&:to_a).flatten.uniq
+      end
+
+      def self.file_system_resolver(path)
+        path = File.expand_path(path)
+        resolver = @file_system_resolvers[path]
+        unless resolver
+          resolver = @file_system_resolvers.fetch_or_store(path) do
+            FileSystemResolver.new(path)
+          end
+        end
+        resolver
+      end
+
+      def self.all_resolvers
+        resolvers = [all_file_system_resolvers]
+        resolvers.concat @view_paths_by_class.values.map(&:to_a)
+        resolvers.flatten.uniq
+      end
+
+      def self.all_file_system_resolvers
+        @file_system_resolvers.values
       end
     end
 
