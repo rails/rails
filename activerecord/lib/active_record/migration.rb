a3a2189c0e24ddb472b99bbe964827406f4abff7
fatkodima <fatkodima123@gmail.com>
2023-03-12 22:50:37 +0200

    Do not log internal options when running migrations

--- activerecord/lib/active_record/migration.rb
+++ activerecord/lib/active_record/migration.rb
@@ -969,9 +969,7 @@ module ActiveRecord
     end
 
     def method_missing(method, *arguments, &block)
-      arg_list = arguments.map(&:inspect) * ", "
-
-      say_with_time "#{method}(#{arg_list})" do
+      say_with_time "#{method}(#{format_arguments(arguments)})" do
         unless connection.respond_to? :revert
           unless arguments.empty? || [:execute, :enable_extension, :disable_extension].include?(method)
             arguments[0] = proper_table_name(arguments.first, table_name_options)
@@ -1078,6 +1076,22 @@ module ActiveRecord
         else
           yield
         end
+      end
+
+      def format_arguments(arguments)
+        arg_list = arguments[0...-1].map(&:inspect)
+        last_arg = arguments.last
+        if last_arg.is_a?(Hash)
+          last_arg = last_arg.reject { |k, _v| internal_option?(k) }
+          arg_list << last_arg.inspect unless last_arg.empty?
+        else
+          arg_list << last_arg.inspect
+        end
+        arg_list.join(", ")
+      end
+
+      def internal_option?(option_name)
+        option_name.start_with?("_")
       end
 
       def command_recorder
