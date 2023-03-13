9e1169b96164eb5ba6bf8ca7744aa3a512cf9439
Hartley McGuire <skipkayhil@gmail.com>
2023-03-13 19:05:18 -0400

    Fix NoMethodError in SafeBuffer#bytesplice
    `html_escape_interpolated_argument` was [renamed][1] to
    `implicit_html_escape_interpolated_argument` during Rails 7.0
    development, so the security fix [backport][2] ended up with the wrong
    method name.
    
    [1]: 147f207a57a03fc7a52040aa1f6878cf70ee0db7
    [2]: 3cf23c3f891e2e81c977ea4ab83b62bc2a444b70

--- activesupport/lib/active_support/core_ext/string/output_safety.rb
+++ activesupport/lib/active_support/core_ext/string/output_safety.rb
@@ -217,7 +217,7 @@ module ActiveSupport #:nodoc:
     alias << concat
 
     def bytesplice(*args, value)
-      super(*args, implicit_html_escape_interpolated_argument(value))
+      super(*args, html_escape_interpolated_argument(value))
     end
 
     def insert(index, value)
