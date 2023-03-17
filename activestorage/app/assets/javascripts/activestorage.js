cfd2c243b73e1b3e83f34306015e602220fdd003
Vipul A M <vipulnsward@gmail.com>
2023-03-18 02:16:07 +0530

    Usage of Event.initevent has been deprecated, ref- https://developer.mozilla.org/en-US/docs/web/api/event/initevent
    Replace and make use of Event constructor instead.
    
    https://developer.mozilla.org/en-US/docs/Web/API/Event/Event

--- activestorage/app/assets/javascripts/activestorage.js
+++ activestorage/app/assets/javascripts/activestorage.js
@@ -482,8 +482,10 @@
   function dispatchEvent(element, type, eventInit = {}) {
     const {disabled: disabled} = element;
     const {bubbles: bubbles, cancelable: cancelable, detail: detail} = eventInit;
-    const event = document.createEvent("Event");
-    event.initEvent(type, bubbles || true, cancelable || true);
+    const event = new Event(type, {
+      bubbles: bubbles || true,
+      cancelable: cancelable || true
+    });
     event.detail = detail || {};
     try {
       element.disabled = false;
