rails/rails

commit 3d00c8b97f3b18d89cdeaebe3d07661300ab14e4

Optimize `camelize` for single word
"hello"
Warming up --------------------------------------
                 old   101.269k i/100ms
                 new   193.263k i/100ms
Calculating -------------------------------------
                 old      1.073M (± 1.0%) i/s -      5.367M in   5.000337s
                 new      1.970M (± 1.0%) i/s -      9.856M in   5.002860s

Comparison:
                 new:  1970358.7 i/s
                 old:  1073491.6 i/s - 1.84x  (± 0.00) slower

"active_record"
Warming up --------------------------------------
                 old    45.864k i/100ms
                 new    44.306k i/100ms
Calculating -------------------------------------
                 old    468.932k (± 1.6%) i/s -      2.385M in   5.087160s
                 new    453.549k (± 1.4%) i/s -      2.304M in   5.080705s

Comparison:
                 old:   468931.7 i/s
                 new:   453549.3 i/s - 1.03x  (± 0.00) slower

"foo_bar_baz/aa_bb_cc"
Warming up --------------------------------------
                 old    18.536k i/100ms
                 new    18.334k i/100ms
Calculating -------------------------------------
                 old    186.371k (± 1.1%) i/s -    945.336k in   5.072959s
                 new    183.743k (± 0.9%) i/s -    935.034k in   5.089229s

Comparison:
                 old:   186371.2 i/s
                 new:   183742.5 i/s - same-ish: difference falls within error

activesupport/lib/active_support/inflector/methods.rb
@@ -70,6 +70,8 @@ module ActiveSupport
       # String#camelize takes a symbol (:upper or :lower), so here we also support :lower to keep the methods consistent.
       if !uppercase_first_letter || uppercase_first_letter == :lower
         string = string.sub(inflections.acronyms_camelize_regex) { |match| match.downcase! || match }
+      elsif string.match?(/\A[a-z\d]*\z/)
+        return inflections.acronyms[string] || string.capitalize
       else
         string = string.sub(/^[a-z\d]*/) { |match| inflections.acronyms[match] || match.capitalize! || match }
       end

commit 130c6750696c889e1792b42f81062b344aa25eec

Ensure `Inflector` methods return un-frozen strings
https://github.com/rails/rails/commit/3d00c8b97f3b18d89cdeaebe3d07661300ab14e4 introduced a regression:

```ruby
ActiveSupport::Inflector.inflections(:en) do |inflect|
  inflect.acronym("API")
end
```

If you have a controller called `APIController`, you may get a crash backtrace that looks like this:

```
FrozenError: can't modify frozen String: "API"
    rails (b8a399cd6ffe) actionpack/lib/action_dispatch/http/request.rb:89:in `controller_class_for'
    rails (b8a399cd6ffe) actionpack/lib/action_dispatch/request/utils.rb:100:in `action_encoding_template'
    rails (b8a399cd6ffe) actionpack/lib/action_dispatch/request/utils.rb:85:in `encode'
    rails (b8a399cd6ffe) actionpack/lib/action_dispatch/request/utils.rb:45:in `set_binary_encoding'
    rails (b8a399cd6ffe) actionpack/lib/action_dispatch/http/parameters.rb:68:in `path_parameters='
```

And you can see the issue in action like this:

```ruby
Tanda @ Rails 7.1.0.alpha (test) :004 > "API".freeze.underscore.frozen?
false # this is expected
Tanda @ Rails 7.1.0.alpha (test) :004 > "API".freeze.underscore.camelize.frozen?
false # this is *not* expected, and is what causes the above code to crash
```

I think the correct behaviour is for the inflector to always return non-frozen strings, even if it's using a cached frozen string as is the case in https://github.com/rails/rails/commit/3d00c8b97f3b18d89cdeaebe3d07661300ab14e4

Other semi-related PRs: https://github.com/rails/rails/pull/41174, https://github.com/rails/rails/pull/41881
cc @amatsuda @byroot

activesupport/lib/active_support/inflector/methods.rb
@@ -71,7 +71,7 @@ module ActiveSupport
       if !uppercase_first_letter || uppercase_first_letter == :lower
         string = string.sub(inflections.acronyms_camelize_regex) { |match| match.downcase! || match }
       elsif string.match?(/\A[a-z\d]*\z/)
-        return inflections.acronyms[string] || string.capitalize
+        return inflections.acronyms[string]&.dup || string.capitalize
       else
         string = string.sub(/^[a-z\d]*/) { |match| inflections.acronyms[match] || match.capitalize! || match }
       end
@@ -95,7 +95,7 @@ module ActiveSupport
     #
     #   camelize(underscore('SSLError'))  # => "SslError"
     def underscore(camel_cased_word)
-      return camel_cased_word.to_s unless /[A-Z-]|::/.match?(camel_cased_word)
+      return camel_cased_word.to_s.dup unless /[A-Z-]|::/.match?(camel_cased_word)
       word = camel_cased_word.to_s.gsub("::", "/")
       word.gsub!(inflections.acronyms_underscore_regex) { "#{$1 && '_' }#{$2.downcase}" }
       word.gsub!(/([A-Z])(?=[A-Z][a-z])|([a-z\d])(?=[A-Z])/) { ($1 || $2) << "_" }

activesupport/test/inflector_test.rb
@@ -220,11 +220,21 @@ class InflectorTest < ActiveSupport::TestCase
     ].each do |camel, under, human, title|
       assert_equal(camel, ActiveSupport::Inflector.camelize(under))
       assert_equal(camel, ActiveSupport::Inflector.camelize(camel))
+      assert_not_predicate(ActiveSupport::Inflector.camelize(under), :frozen?)
+      assert_not_predicate(ActiveSupport::Inflector.camelize(camel), :frozen?)
+
       assert_equal(under, ActiveSupport::Inflector.underscore(under))
       assert_equal(under, ActiveSupport::Inflector.underscore(camel))
+      assert_not_predicate(ActiveSupport::Inflector.underscore(under), :frozen?)
+      assert_not_predicate(ActiveSupport::Inflector.underscore(camel), :frozen?)
+
       assert_equal(title, ActiveSupport::Inflector.titleize(under))
       assert_equal(title, ActiveSupport::Inflector.titleize(camel))
+      assert_not_predicate(ActiveSupport::Inflector.titleize(under), :frozen?)
+      assert_not_predicate(ActiveSupport::Inflector.titleize(camel), :frozen?)
+
       assert_equal(human, ActiveSupport::Inflector.humanize(under))
+      assert_not_predicate(ActiveSupport::Inflector.humanize(camel), :frozen?)
     end
   end
 
@@ -638,5 +648,11 @@ class InflectorTest < ActiveSupport::TestCase
       inflect.clear(:acronyms)
       assert_equal({}, inflect.acronyms)
     end
+  end
+
+  def test_output_is_not_frozen_even_if_input_is_frozen
+    input = "plurals"
+    assert_predicate input, :frozen?
+    assert_not_predicate ActiveSupport::Inflector.pluralize(input), :frozen?
   end
 end