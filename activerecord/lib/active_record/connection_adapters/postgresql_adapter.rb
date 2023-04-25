71503fb9383366eef60fcb099e519f87f162c469
Yasuo Honda <yasuo.honda@gmail.com>
2023-04-25 16:13:30 +0900

    Merge pull request #48048 from tiramizoo/pg-1-5-warning
    Fix pg 1.5.0 deprecation warning

--- activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
@@ -977,7 +977,7 @@ module ActiveRecord
               PG::TextDecoder::TimestampUtc :
               PG::TextDecoder::TimestampWithoutTimeZone
 
-            @timestamp_decoder = decoder_class.new(@timestamp_decoder.to_h)
+            @timestamp_decoder = decoder_class.new(**@timestamp_decoder.to_h)
             @connection.type_map_for_results.add_coder(@timestamp_decoder)
 
             @default_timezone = ActiveRecord.default_timezone
