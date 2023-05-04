1df5ece43bd74cd3c234be4c7e173c3ff532825f
Nishiki Liu <hello@nshki.com>
2023-05-04 08:19:36 -0700

    feat: use config pk type in Action Mailbox migration

--- actionmailbox/test/dummy/db/migrate/20180208205311_create_action_mailbox_tables.rb
+++ actionmailbox/test/dummy/db/migrate/20180208205311_create_action_mailbox_tables.rb
@@ -1,6 +1,6 @@
 class CreateActionMailboxTables < ActiveRecord::Migration[6.0]
   def change
-    create_table :action_mailbox_inbound_emails do |t|
+    create_table :action_mailbox_inbound_emails, id: primary_key_type do |t|
       t.integer :status, default: 0, null: false
       t.string  :message_id, null: false
       t.string  :message_checksum, null: false
@@ -10,4 +10,10 @@ class CreateActionMailboxTables < ActiveRecord::Migration[6.0]
       t.index [ :message_id, :message_checksum ], name: "index_action_mailbox_inbound_emails_uniqueness", unique: true
     end
   end
+
+  private
+    def primary_key_type
+      config = Rails.configuration.generators
+      config.options[config.orm][:primary_key_type] || :primary_key
+    end
 end
