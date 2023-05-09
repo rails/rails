65b9431edb7e1c70e95ed42570a10b32b84ad110
Ajee <93165947+ajee10x@users.noreply.github.com>
2023-05-09 17:06:07 -0400

    Update credentials_test.rb
    Raises an error if the credentials cannot be decrypted.

--- railties/test/application/credentials_test.rb
+++ railties/test/application/credentials_test.rb
@@ -11,9 +11,7 @@ class Rails::CredentialsTest < ActiveSupport::TestCase
 
   test "reads credentials from environment specific path" do
     write_credentials_override(:production)
-
     app("production")
-
     assert_equal "revealed", Rails.application.credentials.mystery
   end
 
@@ -21,36 +19,47 @@ class Rails::CredentialsTest < ActiveSupport::TestCase
     write_credentials_override(:staging)
     add_to_env_config("production", "config.credentials.content_path = config.root.join('config/credentials/staging.yml.enc')")
     add_to_env_config("production", "config.credentials.key_path = config.root.join('config/credentials/staging.key')")
-
     app("production")
-
     assert_equal "revealed", Rails.application.credentials.mystery
   end
 
   test "reads credentials using environment variable key" do
     write_credentials_override(:production, with_key: false)
-
     switch_env("RAILS_MASTER_KEY", credentials_key) do
       app("production")
-
       assert_equal "revealed", Rails.application.credentials.mystery
     end
   end
 
-  private
-    def write_credentials_override(name, with_key: true)
-      Dir.chdir(app_path) do
-        Dir.mkdir  "config/credentials"
-        File.write "config/credentials/#{name}.key", credentials_key if with_key
+  test "application has convenience method for accessing credentials" do
+    app("production")
+    assert_equal(Rails.application.credentials, Rails.credentials)
+  end
 
-        # secret_key_base: secret
-        # mystery: revealed
-        File.write "config/credentials/#{name}.yml.enc",
-          "vgvKu4MBepIgZ5VHQMMPwnQNsLlWD9LKmJHu3UA/8yj6x+3fNhz3DwL9brX7UA==--qLdxHP6e34xeTAiI--nrcAsleXuo9NqiEuhntAhw=="
+  test "raises error if credentials cannot be decrypted" do
+    write_credentials_override(:production, with_key: false)
+    switch_env("RAILS_MASTER_KEY", "wrong_key") do
+      app("production")
+      assert_raises ActiveSupport::MessageEncryptor::InvalidMessage do
+        Rails.application.credentials.mystery
       end
     end
+  end
 
-    def credentials_key
-      "2117e775dc2024d4f49ddf3aeb585919"
+  private
+
+  def write_credentials_override(name, with_key: true)
+    Dir.chdir(app_path) do
+      Dir.mkdir "config/credentials"
+      File.write "config/credentials/#{name}.key", credentials_key if with_key
+      # secret_key_base: secret
+      # mystery: revealed
+      File.write "config/credentials/#{name}.yml.enc",
+                 "vgvKu4MBepIgZ5VHQMMPwnQNsLlWD9LKmJHu3UA/8yj6x+3fNhz3DwL9brX7UA==--qLdxHP6e34xeTAiI--nrcAsleXuo9NqiEuhntAhw=="
     end
+  end
+
+  def credentials_key
+    "2117e775dc2024d4f49ddf3aeb585919"
+  end
 end
