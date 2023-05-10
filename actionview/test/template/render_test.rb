096c785c39e83367a6e2a4158363119ce8c2bcd4
Jean Boussier <jean.boussier@gmail.com>
2023-05-10 10:42:11 +0900

    Properly fallback when ERB column can't be computed
    Fix: https://github.com/rails/rails/issues/48173
    
    If for some reason the column identification fails, we should
    fallback to the old error rendering with only line information.

--- actionview/test/template/render_test.rb
+++ actionview/test/template/render_test.rb
@@ -221,6 +221,19 @@ module RenderTestCases
 
       assert_equal 6, translated_spot[:first_column]
     end
+
+    def test_render_location_map_failure
+      ex = assert_raises(ActionView::Template::Error) {
+        @view.render(template: "test/unparseable_runtime_error")
+      }
+      erb_btl = ex.backtrace_locations.first
+
+      # Get the spot information from ErrorHighlight
+      translating_frame = ActionDispatch::ExceptionWrapper::SourceMapLocation.new(erb_btl, ex.template)
+      translated_spot = translating_frame.spot(ex.cause)
+
+      assert_not_nil translated_spot[:first_column]
+    end
   end
 
   def test_render_partial
