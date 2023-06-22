8355658a56dfa944cc5e87b2d7bece880caf8e00
Hartley McGuire <skipkayhil@gmail.com>
2023-06-21 23:21:29 -0400

    Fix EncryptedConfiguration not behaving like Hash
    Previously, `EncryptedConfiguration` was [updated][1] to use
    `InheritableOptions` so that keys could be called like methods. It was
    later [updated again][2] to ensure that it behaved both like a `Hash` and
    `OrderedOptions`. In this second change, the `InheritableOptions`
    instance was accidentally nested with another `InheritableOptions`
    instance.
    
    This continued to mostly work as expected because `InheritableOptions`
    will fall back to the inner `InheritableOptions` when the outer one
    doesn't have a key. However, any methods that try to treat the outer
    `InheritableOptions` like it should know about all of its keys will fail
    (for example, `#keys`, `#to_h`, `#to_json`, etc.)
    
    This commit fixes the issue by removing the extraneous outer
    `InheritableOptions` instance.
    
    [1]: a6a9fed1719a3cfe47eb1566ae4d6034374fd809
    [2]: 80585daf2def5bf94854be69f81e24a16ce14c55

--- activesupport/test/encrypted_configuration_test.rb
+++ activesupport/test/encrypted_configuration_test.rb
@@ -45,6 +45,7 @@ class EncryptedConfigurationTest < ActiveSupport::TestCase
     assert_not @credentials.something.bad
     assert_equal "bar", @credentials.dig(:something, :nested, :foo)
     assert_equal "bar", @credentials.something.nested.foo
+    assert_equal [:something], @credentials.keys
     assert_equal [:good, :bad, :nested], @credentials.something.keys
     assert_equal ({ good: true, bad: false, nested: { foo: "bar" } }), @credentials.something
   end
