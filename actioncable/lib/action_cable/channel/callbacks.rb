3534c40c33912f9e53330614931d296c9efa1dd9
Jonathan Hefner <jonathan@hefner.pro>
2023-06-26 16:37:21 -0500

    Move example to API docs [ci-skip]

--- actioncable/lib/action_cable/channel/callbacks.rb
+++ actioncable/lib/action_cable/channel/callbacks.rb
@@ -15,6 +15,23 @@ module ActionCable
     # * {before_unsubscribe}[rdoc-ref:ClassMethods#before_unsubscribe]
     # * {after_unsubscribe}[rdoc-ref:ClassMethods#after_unsubscribe] (aliased as
     #   {on_unsubscribe}[rdoc-ref:ClassMethods#on_unsubscribe])
+    #
+    # ==== Example
+    #
+    #   class ChatChannel < ApplicationCable::Channel
+    #     after_subscribe :send_welcome_message, unless: :subscription_rejected?
+    #     after_subscribe :track_subscription
+    #
+    #     private
+    #       def send_welcome_message
+    #         broadcast_to(...)
+    #       end
+    #
+    #       def track_subscription
+    #         # ...
+    #       end
+    #   end
+    #
     module Callbacks
       extend  ActiveSupport::Concern
       include ActiveSupport::Callbacks
