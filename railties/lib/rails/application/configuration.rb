5fbaa524b9921714e73c978336e5167f9d521613
Jean Boussier <jean.boussier@gmail.com>
2023-06-28 15:56:36 +0200

    Active Record commit transaction on `return`, `break` and `throw`
    Fix: https://github.com/rails/rails/issues/45017
    Ref: https://github.com/rails/rails/pull/29333
    Ref: https://github.com/ruby/timeout/pull/30
    
    Historically only raised errors would trigger a rollback, but in Ruby `2.3`, the `timeout` library
    started using `throw` to interupt execution which had the adverse effect of committing open transactions.
    
    To solve this, in Active Record 6.1 the behavior was changed to instead rollback the transaction as it was safer
    than to potentially commit an incomplete transaction.
    
    Using `return`, `break` or `throw` inside a `transaction` block was essentially deprecated from Rails 6.1 onwards.
    
    However with the release of `timeout 0.4.0`, `Timeout.timeout` now raises an error again, and Active Record is able
    to return to its original, less surprising, behavior.

--- railties/lib/rails/application/configuration.rb
+++ railties/lib/rails/application/configuration.rb
@@ -276,6 +276,7 @@ module Rails
 
           if respond_to?(:active_record)
             active_record.run_commit_callbacks_on_first_saved_instances_in_transaction = false
+            active_record.commit_transaction_on_non_local_return = true
             active_record.allow_deprecated_singular_associations_name = false
             active_record.sqlite3_adapter_strict_strings_by_default = true
             active_record.query_log_tags_format = :sqlcommenter
