250c3b01df233f85ec71bda633a166628cae8d7e
Jonathan Hefner <jonathan@hefner.pro>
2023-07-05 16:22:19 -0500

    Add assert_compression helper

--- activesupport/test/cache/behaviors/cache_store_compression_behavior.rb
+++ activesupport/test/cache/behaviors/cache_store_compression_behavior.rb
@@ -8,85 +8,48 @@ module CacheStoreCompressionBehavior
   included do
     test "compression by default" do
       @cache = lookup_store
-
-      assert_uncompressed SMALL_STRING
-      assert_uncompressed SMALL_OBJECT
-      if compression_always_disabled_by_default?
-        assert_uncompressed LARGE_STRING
-        assert_uncompressed LARGE_OBJECT
-      else
-        assert_compressed LARGE_STRING
-        assert_compressed LARGE_OBJECT
-      end
+      assert_compression !compression_always_disabled_by_default?
     end
 
     test "compression can be disabled" do
       @cache = lookup_store(compress: false)
-
-      assert_uncompressed SMALL_STRING
-      assert_uncompressed SMALL_OBJECT
-      assert_uncompressed LARGE_STRING
-      assert_uncompressed LARGE_OBJECT
+      assert_compression false
     end
 
     test ":compress method option overrides initializer option" do
       @cache = lookup_store(compress: true)
-
-      assert_uncompressed SMALL_STRING, compress: false
-      assert_uncompressed SMALL_OBJECT, compress: false
-      assert_uncompressed LARGE_STRING, compress: false
-      assert_uncompressed LARGE_OBJECT, compress: false
+      assert_compression false, with: { compress: false }
 
       @cache = lookup_store(compress: false)
-
-      assert_uncompressed SMALL_STRING, compress: true
-      assert_uncompressed SMALL_OBJECT, compress: true
-      assert_compressed LARGE_STRING, compress: true
-      assert_compressed LARGE_OBJECT, compress: true
+      assert_compression true, with: { compress: true }
     end
 
     test "low :compress_threshold triggers compression" do
       @cache = lookup_store(compress: true, compress_threshold: 1)
-
-      assert_compressed SMALL_STRING
-      assert_compressed SMALL_OBJECT
-      assert_compressed LARGE_STRING
-      assert_compressed LARGE_OBJECT
+      assert_compression :all
     end
 
     test "high :compress_threshold inhibits compression" do
       @cache = lookup_store(compress: true, compress_threshold: 1.megabyte)
-
-      assert_uncompressed SMALL_STRING
-      assert_uncompressed SMALL_OBJECT
-      assert_uncompressed LARGE_STRING
-      assert_uncompressed LARGE_OBJECT
+      assert_compression false
     end
 
     test ":compress_threshold method option overrides initializer option" do
       @cache = lookup_store(compress: true, compress_threshold: 1)
-
-      assert_uncompressed SMALL_STRING, compress_threshold: 1.megabyte
-      assert_uncompressed SMALL_OBJECT, compress_threshold: 1.megabyte
-      assert_uncompressed LARGE_STRING, compress_threshold: 1.megabyte
-      assert_uncompressed LARGE_OBJECT, compress_threshold: 1.megabyte
+      assert_compression false, with: { compress_threshold: 1.megabyte }
 
       @cache = lookup_store(compress: true, compress_threshold: 1.megabyte)
-
-      assert_compressed SMALL_STRING, compress_threshold: 1
-      assert_compressed SMALL_OBJECT, compress_threshold: 1
-      assert_compressed LARGE_STRING, compress_threshold: 1
-      assert_compressed LARGE_OBJECT, compress_threshold: 1
+      assert_compression :all, with: { compress_threshold: 1 }
     end
 
     test "compression ignores nil" do
-      assert_uncompressed nil
-      assert_uncompressed nil, compress: true, compress_threshold: 1
+      assert_not_compress nil
+      assert_not_compress nil, with: { compress: true, compress_threshold: 1 }
     end
 
     test "compression ignores incompressible data" do
-      assert_uncompressed "", compress: true, compress_threshold: 1
-      assert_uncompressed [*0..127].pack("C*"), compress: true, compress_threshold: 1
+      assert_not_compress "", with: { compress: true, compress_threshold: 1 }
+      assert_not_compress [*0..127].pack("C*"), with: { compress: true, compress_threshold: 1 }
     end
   end
 
@@ -99,19 +62,37 @@ module CacheStoreCompressionBehavior
     SMALL_OBJECT = { data: SMALL_STRING }
     LARGE_OBJECT = { data: LARGE_STRING }
 
-    def assert_compressed(value, **options)
+    def assert_compress(value, **options)
       assert_operator compute_entry_size_reduction(value, **options), :>, 0
     end
 
-    def assert_uncompressed(value, **options)
+    def assert_not_compress(value, **options)
       assert_equal 0, compute_entry_size_reduction(value, **options)
     end
 
-    def compute_entry_size_reduction(value, **options)
+    def assert_compression(compress, **options)
+      if compress == :all
+        assert_compress SMALL_STRING, **options
+        assert_compress SMALL_OBJECT, **options
+      else
+        assert_not_compress SMALL_STRING, **options
+        assert_not_compress SMALL_OBJECT, **options
+      end
+
+      if compress
+        assert_compress LARGE_STRING, **options
+        assert_compress LARGE_OBJECT, **options
+      else
+        assert_not_compress LARGE_STRING, **options
+        assert_not_compress LARGE_OBJECT, **options
+      end
+    end
+
+    def compute_entry_size_reduction(value, with: {})
       entry = ActiveSupport::Cache::Entry.new(value)
 
-      uncompressed = @cache.send(:serialize_entry, entry, **options, compress: false)
-      actual = @cache.send(:serialize_entry, entry, **options)
+      uncompressed = @cache.send(:serialize_entry, entry, **with, compress: false)
+      actual = @cache.send(:serialize_entry, entry, **with)
 
       uncompressed.bytesize - actual.bytesize
     end
