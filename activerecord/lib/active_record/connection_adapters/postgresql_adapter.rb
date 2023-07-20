33611ec45bf6b90f957304ef089ef7698a16fa86
Ibiã Marques <57356836+ibiamscapyba@users.noreply.github.com>
2023-07-20 18:28:52 -0300

    Set client_min_messages as warning

--- activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
@@ -310,7 +310,7 @@ module ActiveRecord
 
       # Enable standard-conforming strings if available.
       def set_standard_conforming_strings
-        old, self.client_min_messages = client_min_messages, 'panic'
+        old, self.client_min_messages = client_min_messages, 'warning'
         execute('SET standard_conforming_strings = on', 'SCHEMA') rescue nil
       ensure
         self.client_min_messages = old
