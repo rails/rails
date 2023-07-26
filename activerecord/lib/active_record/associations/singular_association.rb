9357f7df93ce28b5e8c441f95a009f4730c29a84
zzak <zzakscott@gmail.com>
2023-07-26 14:51:17 +0900

    Revert "Merge pull request #48416 from Shopify/fix-has-one-deletion"
    This reverts commit 6be41aded8c067b2b4af3b05193e551f126fb0e3, reversing
    changes made to 55c3066da325703ff7a9524dbdc479b860db3970.

--- activerecord/lib/active_record/associations/singular_association.rb
+++ activerecord/lib/active_record/associations/singular_association.rb
@@ -57,7 +57,7 @@ module ActiveRecord
           reflection.klass.transaction do
             record = build(attributes, &block)
             saved = record.save
-            replace_keys(record, force: true)
+            set_new_record(record)
             raise RecordInvalid.new(record) if !saved && raise_error
             record
           end
