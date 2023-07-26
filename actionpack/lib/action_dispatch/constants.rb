d1381dc6545981c308341b81be80d89632fd529a
Nuno Silva <nuno.silva@shopify.com>
2023-07-26 12:51:42 +0000

    Add Rack::Lint to ContentSecurityPolicy::Middleware tests
    To ensure Rails is and remains compliant with [the Rack 3
    spec](https://github.com/rack/rack/blob/6d16306192349e665e4ec820a9bfcc0967009b6a/UPGRADE-GUIDE.md)
    we can add `Rack::Lint` to the Rails middleware tests.
    
    This adds additional test coverage to
    `ContentSecurityPolicy::Middleware` to validate that its input and
    output follow the Rack SPEC.
    
    The changes made are because of:
    - [Response Headers must be lower case](https://github.com/rack/rack/blob/6d16306192349e665e4ec820a9bfcc0967009b6a/UPGRADE-GUIDE.md#response-headers-must-be-lower-case)
    
    Added tests to ensure that CSP headers set by an app are not overridden,
    regardless of the casing.
    An example of this is Sidekiq: https://github.com/sidekiq/sidekiq/blob/b3225ce/lib/sidekiq/web/application.rb#L353

--- actionpack/lib/action_dispatch/constants.rb
+++ actionpack/lib/action_dispatch/constants.rb
@@ -8,12 +8,16 @@ module ActionDispatch
     if Gem::Version.new(Rack::RELEASE) < Gem::Version.new("3")
       VARY = "Vary"
       CONTENT_ENCODING = "Content-Encoding"
+      CONTENT_SECURITY_POLICY = "Content-Security-Policy"
+      CONTENT_SECURITY_POLICY_REPORT_ONLY = "Content-Security-Policy-Report-Only"
       LOCATION = "Location"
       FEATURE_POLICY = "Feature-Policy"
       X_REQUEST_ID = "X-Request-Id"
     else
       VARY = "vary"
       CONTENT_ENCODING = "content-encoding"
+      CONTENT_SECURITY_POLICY = "content-security-policy"
+      CONTENT_SECURITY_POLICY_REPORT_ONLY = "content-security-policy-report-only"
       LOCATION = "location"
       FEATURE_POLICY = "feature-policy"
       X_REQUEST_ID = "x-request-id"
