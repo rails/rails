468c86467958f19b16ad898526923cae2243d363
Timur <jarisob@gmail.com>
2023-07-31 10:53:07 +0300

    Fix envelope provider DEK decrypting after previous scheme
    DEK decrypting was failing if you tried to decrypt ciphertext which was previously encrypted with a different scheme without using envelope/DEK approach at all

--- activerecord/test/models/author_encrypted.rb
+++ activerecord/test/models/author_encrypted.rb
@@ -14,3 +14,11 @@ class EncryptedAuthorWithKey < Author
 
   encrypts :name, key: "some secret key!"
 end
+
+class EnvelopeEncryptedAuthor < Author
+  self.table_name = "authors"
+
+  encrypts :name,
+           key_provider: ActiveRecord::Encryption::EnvelopeEncryptionKeyProvider.new,
+           previous: { key: "some secret key!" }
+end
