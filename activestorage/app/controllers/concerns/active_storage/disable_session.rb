b4090cd0d77aa813b7d9acc7615a50dc4e77b3c0
Rafael Mendonça França <rafael@franca.dev>
2023-08-03 15:41:58 -0400

    Remove empty line

--- activestorage/app/controllers/concerns/active_storage/disable_session.rb
+++ activestorage/app/controllers/concerns/active_storage/disable_session.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 # This concern disables the session in order to allow caching by default in some CDNs as CloudFlare.
-
 module ActiveStorage::DisableSession
   extend ActiveSupport::Concern
 
