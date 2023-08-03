36597e2093add6457e213b3691746d1b27aaed07
Eileen M. Uchitelle <eileencodes@users.noreply.github.com>
2023-08-03 11:26:37 -0400

    Merge pull request #48861 from paulreece/correct_missing_and_associated_behavior
    Fixes #47909 when using order or unscope

--- activerecord/lib/active_record/relation/query_methods.rb
+++ activerecord/lib/active_record/relation/query_methods.rb
@@ -77,7 +77,7 @@ module ActiveRecord
         associations.each do |association|
           reflection = scope_association_reflection(association)
           @scope.joins!(association)
-          if @scope.values.size > 1
+          if reflection.options[:class_name]
             self.not(association => { reflection.association_primary_key => nil })
           else
             self.not(reflection.table_name => { reflection.association_primary_key => nil })
@@ -109,7 +109,7 @@ module ActiveRecord
         associations.each do |association|
           reflection = scope_association_reflection(association)
           @scope.left_outer_joins!(association)
-          if @scope.values.size > 1
+          if reflection.options[:class_name]
             @scope.where!(association => { reflection.association_primary_key => nil })
           else
             @scope.where!(reflection.table_name => { reflection.association_primary_key => nil })
