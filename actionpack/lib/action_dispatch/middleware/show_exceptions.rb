6bf2ee09f74af37a50fd28af58b6f33a99ff891b
Hartley McGuire <skipkayhil@gmail.com>
2023-08-16 15:58:51 -0400

    Add test coverage for ShowExceptions failsafe
    This adds additional test coverage to ShowExceptions, since one of the
    possible responses it creates was not previously tested. Because of the
    previous [addition][1] of Rack::Lint, this also demonstrates that the
    Content-Type header needed to be fixed.
    
    [1]: 339dda4a82356d173b62dab144870790618e40c6

--- actionpack/lib/action_dispatch/middleware/show_exceptions.rb
+++ actionpack/lib/action_dispatch/middleware/show_exceptions.rb
@@ -54,7 +54,7 @@ module ActionDispatch
       rescue Exception => failsafe_error
         $stderr.puts "Error during failsafe response: #{failsafe_error}\n  #{failsafe_error.backtrace * "\n  "}"
 
-        [500, { "Content-Type" => "text/plain; charset=utf-8" },
+        [500, { Rack::CONTENT_TYPE => "text/plain; charset=utf-8" },
           ["500 Internal Server Error\n" \
           "If you are the administrator of this website, then please read this web " \
           "application's log file and/or the web server's log file to find out what " \
