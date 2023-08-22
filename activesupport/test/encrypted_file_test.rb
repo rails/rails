3338621efa699fe8eda94b62f328a1472ac02002
Aaron Patterson <aaron@rubyonrails.org>
2023-08-22 09:58:43 -0700

    Use a temporary file for storing unencrypted files while editing
    When we're editing the contents of encrypted files, we should use the
    `Tempfile` class because it creates temporary files with restrictive
    permissions.  This prevents other users on the same system from reading
    the contents of those files while the user is editing them.
    
    [CVE-2023-38037]

--- activesupport/test/encrypted_file_test.rb
+++ activesupport/test/encrypted_file_test.rb
@@ -52,6 +52,14 @@ class EncryptedFileTest < ActiveSupport::TestCase
     assert_equal "#{@content} and went by the lake", @encrypted_file.read
   end
 
+  test "change sets restricted permissions" do
+    @encrypted_file.write(@content)
+    @encrypted_file.change do |file|
+      assert_predicate file, :owned?
+      assert_equal "100600", file.stat.mode.to_s(8), "Incorrect mode for #{file}"
+    end
+  end
+
   test "raise MissingKeyError when key is missing" do
     assert_raise ActiveSupport::EncryptedFile::MissingKeyError do
       encrypted_file(@content_path, key_path: "", env_key: "").read
