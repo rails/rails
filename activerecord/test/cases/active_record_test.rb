9a1d465438980e5e4eac5e8dbdb570e8d0b734a7
Jean Boussier <jean.boussier@gmail.com>
2023-09-01 16:26:17 +0200

    Fix `ActiveRecord.disconnect_all!` to not disable reconnect on pools
    Followup: https://github.com/rails/rails/pull/47856

---
+++ activerecord/test/cases/active_record_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "cases/helper"
+require "rack"
+
+class ActiveRecordTest < ActiveRecord::TestCase
+  unless in_memory_db?
+    test ".disconnect_all! closes all connections" do
+      ActiveRecord::Base.connection.active?
+      assert_predicate ActiveRecord::Base, :connected?
+
+      ActiveRecord.disconnect_all!
+      assert_not_predicate ActiveRecord::Base, :connected?
+
+      ActiveRecord::Base.connection.active?
+      assert_predicate ActiveRecord::Base, :connected?
+    end
+  end
+end
