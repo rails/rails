3c17dab3073d2bd77ecac430fd0593ba014d592a
Rafael Mendonça França <rafael@rubyonrails.org>
2023-09-05 19:37:12 +0000

    We expect queue adapters to be objects, no classes

--- activejob/test/jobs/enqueue_error_job.rb
+++ activejob/test/jobs/enqueue_error_job.rb
@@ -2,18 +2,16 @@
 
 class EnqueueErrorJob < ActiveJob::Base
   class EnqueueErrorAdapter
-    class << self
-      def enqueue(*)
-        raise ActiveJob::EnqueueError, "There was an error enqueuing the job"
-      end
+    def enqueue(*)
+      raise ActiveJob::EnqueueError, "There was an error enqueuing the job"
+    end
 
-      def enqueue_at(*)
-        raise ActiveJob::EnqueueError, "There was an error enqueuing the job"
-      end
+    def enqueue_at(*)
+      raise ActiveJob::EnqueueError, "There was an error enqueuing the job"
     end
   end
 
-  self.queue_adapter = EnqueueErrorAdapter
+  self.queue_adapter = EnqueueErrorAdapter.new
 
   def perform
     raise "This should never be called"
