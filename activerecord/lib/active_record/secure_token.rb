2df70ddb96e7ab0a7c10e411e879ab3a2b1f4d84
Ryuta Kamizono <kamipo@gmail.com>
2023-09-05 17:43:41 +0900

    Generate secure token only once regardless of `on: :initialize` or `on: :create`
    Follow-up to #47420.
    
    Whereas the original behavior (`on: :create`) is invoked only once
    before a record is persisted, the new behavior (`on: :initialize`) is
    invoked not only new record but also persisted records.
    
    It should be invoked only once for new record consistently.

--- activerecord/lib/active_record/secure_token.rb
+++ activerecord/lib/active_record/secure_token.rb
@@ -52,7 +52,9 @@ module ActiveRecord
         require "active_support/core_ext/securerandom"
         define_method("regenerate_#{attribute}") { update! attribute => self.class.generate_unique_secure_token(length: length) }
         set_callback on, on == :initialize ? :after : :before do
-          send("#{attribute}=", self.class.generate_unique_secure_token(length: length)) if has_attribute?(attribute) && !send("#{attribute}?")
+          if new_record? && !query_attribute(attribute)
+            write_attribute(attribute, self.class.generate_unique_secure_token(length: length))
+          end
         end
       end
 
