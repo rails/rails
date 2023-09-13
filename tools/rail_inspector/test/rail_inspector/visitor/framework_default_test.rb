32b8e66d22413c2c1dfbf8520f2349ebd20c23e8
Rafael Mendonça França <rafael@rubyonrails.org>
2023-09-13 01:58:14 +0000

    Autocorrect violations

--- tools/rail_inspector/test/rail_inspector/visitor/framework_default_test.rb
+++ tools/rail_inspector/test/rail_inspector/visitor/framework_default_test.rb
@@ -27,10 +27,10 @@ class FrameworkDefaultTest < Minitest::Test
 
     ["5.0", "5.1"].each { |k| assert_includes(config, k) }
 
-    assert_equal config["5.0"]["ActiveSupport.to_time_preserves_timezone"], "true"
-    assert_equal config["5.0"]["active_record.belongs_to_required_by_default"], "true"
-    assert_equal config["5.0"]["self.ssl_options"], "{ hsts: { subdomains: true } }"
-    assert_equal config["5.1"]["assets.unknown_asset_fallback"], "false"
+    assert_equal("true", config["5.0"]["ActiveSupport.to_time_preserves_timezone"])
+    assert_equal("true", config["5.0"]["active_record.belongs_to_required_by_default"])
+    assert_equal("{ hsts: { subdomains: true } }", config["5.0"]["self.ssl_options"])
+    assert_equal("false", config["5.1"]["assets.unknown_asset_fallback"])
   end
 
   def test_config_wrapped_in_condition
@@ -44,7 +44,7 @@ class FrameworkDefaultTest < Minitest::Test
     RUBY
 
     assert_includes config, "7.1"
-    assert_equal config["7.1"]["self.log_file_size"], "100 * 1024 * 1024"
+    assert_equal("100 * 1024 * 1024", config["7.1"]["self.log_file_size"])
   end
 
   def test_condition_inside_framework
@@ -60,7 +60,7 @@ class FrameworkDefaultTest < Minitest::Test
     RUBY
 
     assert_includes config, "7.1"
-    assert_equal config["7.1"]["action_view.sanitizer_vendor"], "Rails::HTML5::Sanitizer"
+    assert_equal("Rails::HTML5::Sanitizer", config["7.1"]["action_view.sanitizer_vendor"])
   end
 
   def test_nested_frameworks_raise_when_strict
