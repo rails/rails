64184c6f5054d9b1898176107d488f40118aaac1
Hartley McGuire <skipkayhil@gmail.com>
2023-09-22 19:23:28 -0400

    Fix Range#overlap? ignoring empty ranges
    Previously, #overlap? would incorrectly return true when one of the
    ranges is effectively "empty":
    
    ```ruby
    (2...2).overlap? 1..2 # => true
    (1..2).overlap? 2...2 # => true
    ```
    
    This is fixed in the Ruby 3.3 implementation of Range#overlap?, so this
    commit fixes it for Ruby < 3.3 as well.
    
    The tests added are from the Ruby repository and the implementation is
    effectively a Ruby version of the fix in C.
    
    Co-authored-by: Nobuyoshi Nakada <nobu@ruby-lang.org>
    Co-authored-by: Shouichi Kamiya <shouichi.kamiya@gmail.com>

--- activesupport/lib/active_support/core_ext/range/overlap.rb
+++ activesupport/lib/active_support/core_ext/range/overlap.rb
@@ -6,7 +6,33 @@ class Range
   #  (1..5).overlap?(7..9) # => false
   unless Range.method_defined?(:overlap?)
     def overlap?(other)
-      other.begin == self.begin || cover?(other.begin) || other.cover?(self.begin)
+      raise TypeError unless other.is_a? Range
+
+      self_begin = self.begin
+      other_end = other.end
+      other_excl = other.exclude_end?
+
+      return false if _empty_range?(self_begin, other_end, other_excl)
+
+      other_begin = other.begin
+      self_end = self.end
+      self_excl = self.exclude_end?
+
+      return false if _empty_range?(other_begin, self_end, self_excl)
+      return true if self_begin == other_begin
+
+      return false if _empty_range?(self_begin, self_end, self_excl)
+      return false if _empty_range?(other_begin, other_end, other_excl)
+
+      true
+    end
+
+    private
+    def _empty_range?(b, e, excl)
+      return false if b.nil? || e.nil?
+
+      comp = b <=> e
+      comp.nil? || comp > 0 || (comp == 0 && excl)
     end
   end
 
