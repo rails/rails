2be2142e9c8828c824dd07038fdf4d0a53075102
Eugene Kenny <elkenny@gmail.com>
2023-10-03 02:23:26 +0100

    Merge pull request #49462 from Edouard-chin/ec-broadcast-prevent
    Handle case when `config.logger` is already a BroadcastLogger:

--- railties/lib/rails/application/bootstrap.rb
+++ railties/lib/rails/application/bootstrap.rb
@@ -56,9 +56,11 @@ module Rails
         end
         Rails.logger.level = ActiveSupport::Logger.const_get(config.log_level.to_s.upcase)
 
-        broadcast_logger = ActiveSupport::BroadcastLogger.new(Rails.logger)
-        broadcast_logger.formatter = Rails.logger.formatter
-        Rails.logger = broadcast_logger
+        unless Rails.logger.is_a?(ActiveSupport::BroadcastLogger)
+          broadcast_logger = ActiveSupport::BroadcastLogger.new(Rails.logger)
+          broadcast_logger.formatter = Rails.logger.formatter
+          Rails.logger = broadcast_logger
+        end
 
         unless config.consider_all_requests_local
           Rails.error.logger = Rails.logger
