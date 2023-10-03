aa4e713fc07e402aa1bb5acac8144522e96774ab
Rafael Mendonça França <rafael@rubyonrails.org>
2023-10-03 14:04:59 +0000

    Extract public file to maintain test schema so RSpec can use as well
    Since `load_schema` is private API, we don't want it to be explicitly
    called by RSpec.
    
    This feature can be used by RSpec by exposing it in
    `rails/testing/maintain_test_schema` so they don't need to reimplement
    the same thing.

--- railties/lib/rails/test_help.rb
+++ railties/lib/rails/test_help.rb
@@ -14,20 +14,9 @@ require "rails/generators/test_case"
 
 require "active_support/testing/autorun"
 
+require "rails/testing/maintain_test_schema"
+
 if defined?(ActiveRecord::Base)
-  begin
-    ActiveRecord::Migration.maintain_test_schema!
-  rescue ActiveRecord::PendingMigrationError => e
-    puts e.to_s.strip
-    exit 1
-  end
-
-  if Rails.configuration.eager_load
-    ActiveRecord::Base.descendants.each do |model|
-      model.load_schema unless model.abstract_class?
-    end
-  end
-
   ActiveSupport.on_load(:active_support_test_case) do
     include ActiveRecord::TestDatabases
     include ActiveRecord::TestFixtures
