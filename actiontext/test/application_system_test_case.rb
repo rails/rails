b7e619535eaed2f7ad4b6adaecc02c7df9a4303f
zzak <zzakscott@gmail.com>
2023-10-07 17:59:27 +0900

    Enable remote chrome selenium tests for actiontext
    Blocks: rails/buildkite-config#33
    
    Previous works: #47030, #47127
    
    Co-authored-by: Haroon Ahmed <haroon.ahmed25@gmail.com>

--- actiontext/test/application_system_test_case.rb
+++ actiontext/test/application_system_test_case.rb
@@ -3,7 +3,13 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome
+  options = {
+    browser: ENV["SELENIUM_DRIVER_URL"].blank? ? :chrome : :remote,
+    url: ENV["SELENIUM_DRIVER_URL"].blank? ? nil : ENV["SELENIUM_DRIVER_URL"]
+  }
+  driven_by :selenium, using: :headless_chrome, options: options
 end
 
 Capybara.server = :puma, { Silent: true }
+Capybara.server_host = "0.0.0.0" # bind to all interfaces
+Capybara.app_host = "http://#{IPSocket.getaddress(Socket.gethostname)}" if ENV["SELENIUM_DRIVER_URL"].present?
