3a399d97471a29124c1a08be6c14fad94ec62193
Sean Doyle <sean.p.doyle24@gmail.com>
2023-10-17 15:46:09 +0100

    Migrate `ActiveRecord::AttributeAssignment` support for multiparameter attributes to Active Model
    Prior to this commit, the only difference between
    `ActiveRecord::AttributeAssignment` and
    `ActiveModel::AttributeAssignment` was support for multi-parameter
    attributes like those generated by Action View form helpers.
    
    This commit reduces `ActiveRecord::AttributeAssignment`'s
    responsibility, and instead relies on its inclusion of
    `ActiveModel::AttributeAssignment` to implement that behavior.
    
    The added benefit of upstreaming this behavior is that Active Model
    POROs can be designed to be compatible with Action View Date, DateTime,
    and Time `<select>` elements:
    
    ```ruby
    class Topic
      include ActiveModel::Model
      include ActiveModel::Attributes
    
      attribute :last_read_on, :date
    end
    
    topic = Topic.new(
      "last_read_on(1i)" => "2023",
      "last_read_on(2i)" => "10",
      "last_read_on(3i)" => "17"
    )
    topic.last_read_on == Date.new(2023, 10, 17) # => true
    ```
    
    To maintain backwards compatibility, this commit preserves the
    `ActiveRecord::AttributeAssignment` module, despite the fact that it's
    mostly empty. Additionally, it makes sure that
    `ActiveRecord::AttributeAssignmentError` and
    `ActiveRecord::MultiparameterAssignmentErrors` are still thrown. This is
    important for applications that have generic
    `ActiveRecord::ActiveRecordError` handling, since the new
    `ActiveModel::AttributeAssignmentError` and
    `ActiveModel::MultiparameterAssignmentErrors` do not inherit from
    `ActiveRecord::ActiveRecordError`.

--- activemodel/lib/active_model/attribute_assignment.rb
+++ activemodel/lib/active_model/attribute_assignment.rb
@@ -3,6 +3,31 @@
 require "active_support/core_ext/hash/keys"
 
 module ActiveModel
+  # Raised when an error occurred while doing a mass assignment to an attribute through the
+  # {AttributeAssignment#attributes=}[rdoc-ref:AttributeAssignment#attributes=] method.
+  # The exception has an +attribute+ property that is the name of the offending attribute.
+  class AttributeAssignmentError < StandardError
+    attr_reader :exception, :attribute
+
+    def initialize(message = nil, exception = nil, attribute = nil)
+      super(message)
+      @exception = exception
+      @attribute = attribute
+    end
+  end
+
+  # Raised when there are multiple errors while doing a mass assignment through the
+  # {AttributeAssignment#attributes=}[rdoc-ref:AttributeAssignment#attributes=]
+  # method. The exception has an +errors+ property that contains an array of AttributeAssignmentError
+  # objects, each corresponding to the error while assigning to an attribute.
+  class MultiparameterAssignmentErrors < StandardError
+    attr_reader :errors
+
+    def initialize(errors = nil)
+      @errors = errors
+    end
+  end
+
   module AttributeAssignment
     include ActiveModel::ForbiddenAttributesProtection
 
@@ -59,9 +84,22 @@ module ActiveModel
 
     private
       def _assign_attributes(attributes)
+        multi_parameter_attributes = nil
+        nested_parameter_attributes = nil
+
         attributes.each_pair do |k, v|
-          _assign_attribute(k, v)
+          k = k.to_s
+          if k.include?("(")
+            (multi_parameter_attributes ||= {})[k] = v
+          elsif v.is_a?(Hash)
+            (nested_parameter_attributes ||= {})[k] = v
+          else
+            _assign_attribute(k, v)
+          end
         end
+
+        assign_multiparameter_attributes(multi_parameter_attributes) if multi_parameter_attributes
+        assign_nested_parameter_attributes(nested_parameter_attributes) if nested_parameter_attributes
       end
 
       def _assign_attribute(k, v)
@@ -73,6 +111,63 @@ module ActiveModel
         else
           attribute_writer_missing(k.to_s, v)
         end
+      end
+
+      # Assign any deferred nested attributes after the base attributes have been set.
+      def assign_nested_parameter_attributes(pairs)
+        pairs.each { |k, v| _assign_attribute(k, v) }
+      end
+
+      # Instantiates objects for all attribute classes that needs more than one constructor parameter. This is done
+      # by calling new on the column type or aggregation type (through composed_of) object with these parameters.
+      # So having the pairs written_on(1) = "2004", written_on(2) = "6", written_on(3) = "24", will instantiate
+      # written_on (a date type) with Date.new("2004", "6", "24"). You can also specify a typecast character in the
+      # parentheses to have the parameters typecasted before they're used in the constructor. Use i for Integer and
+      # f for Float. If all the values for a given attribute are empty, the attribute will be set to +nil+.
+      def assign_multiparameter_attributes(pairs)
+        execute_callstack_for_multiparameter_attributes(
+          extract_callstack_for_multiparameter_attributes(pairs)
+        )
+      end
+
+      def execute_callstack_for_multiparameter_attributes(callstack)
+        errors = []
+        callstack.each do |name, values_with_empty_parameters|
+          if values_with_empty_parameters.each_value.all?(NilClass)
+            values = nil
+          else
+            values = values_with_empty_parameters
+          end
+          send("#{name}=", values)
+        rescue => ex
+          errors << AttributeAssignmentError.new("error on assignment #{values_with_empty_parameters.values.inspect} to #{name} (#{ex.message})", ex, name)
+        end
+        unless errors.empty?
+          error_descriptions = errors.map(&:message).join(",")
+          raise MultiparameterAssignmentErrors.new(errors), "#{errors.size} error(s) on assignment of multiparameter attributes [#{error_descriptions}]"
+        end
+      end
+
+      def extract_callstack_for_multiparameter_attributes(pairs)
+        attributes = {}
+
+        pairs.each do |(multiparameter_name, value)|
+          attribute_name = multiparameter_name.split("(").first
+          attributes[attribute_name] ||= {}
+
+          parameter_value = value.empty? ? nil : type_cast_attribute_value(multiparameter_name, value)
+          attributes[attribute_name][find_parameter_position(multiparameter_name)] ||= parameter_value
+        end
+
+        attributes
+      end
+
+      def type_cast_attribute_value(multiparameter_name, value)
+        multiparameter_name =~ /\([0-9]*([if])\)/ ? value.send("to_" + $1) : value
+      end
+
+      def find_parameter_position(multiparameter_name)
+        multiparameter_name.scan(/\(([0-9]*).*\)/).first.first.to_i
       end
   end
 end
