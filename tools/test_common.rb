cb09616b94ae3ec83778ea330ecff27a9d16631d
Jean Boussier <jean.boussier@gmail.com>
2023-10-27 09:03:30 +0200

    Avoid exposing ActiveSupport::Testing::NoSkip
    It's only meant for Rails internal use.

--- tools/test_common.rb
+++ tools/test_common.rb
@@ -8,6 +8,13 @@ if ENV["BUILDKITE"]
 end
 
 if ENV["CI"]
-  require File.join(__dir__, "../activesupport/lib/active_support/testing/no_skip")
-  ActiveSupport::TestCase.include(ActiveSupport::Testing::NoSkip)
+  module DisableSkipping # :nodoc:
+    private
+      def skip(message = nil, *)
+        flunk "Skipping tests is not allowed in this environment (#{message})\n" \
+          "Tests should only be skipped when the environment is missing a required dependency.\n" \
+          "This should never happen on CI."
+      end
+  end
+  ActiveSupport::TestCase.include(DisableSkipping)
 end
