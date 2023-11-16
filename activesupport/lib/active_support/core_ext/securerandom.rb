d1ae0241266a728c1b55ddc132cb98f6cd3d32c0
Javier Valencia <javier.valencia@gmail.com>
2023-11-16 16:59:33 +0100

    Add base62 to SecureRandom
    Add base62 to SecureRandom

--- activesupport/lib/active_support/core_ext/securerandom.rb
+++ activesupport/lib/active_support/core_ext/securerandom.rb
@@ -3,8 +3,33 @@
 require "securerandom"
 
 module SecureRandom
-  BASE58_ALPHABET = ("0".."9").to_a + ("A".."Z").to_a + ("a".."z").to_a - ["0", "O", "I", "l"]
+  BASE62_ALPHABET = ("0".."9").to_a + ("A".."Z").to_a + ("a".."z").to_a
+  BASE58_ALPHABET = BASE62_ALPHABET - ["0", "O", "I", "l"]
   BASE36_ALPHABET = ("0".."9").to_a + ("a".."z").to_a
+
+  # SecureRandom.base62 generates a random base62 string.
+  #
+  # The argument _n_ specifies the length of the random string to be generated.
+  #
+  # If _n_ is not specified or is +nil+, 16 is assumed. It may be larger in the future.
+  #
+  # The result may contain alphanumeric characters.
+  #
+  #   p SecureRandom.base62 # => "4kUgL2pdQMSCQtjE"
+  #   p SecureRandom.base62(24) # => "77TMHrHJFvFDwodq8w7Ev2m7"
+  if RUBY_VERSION >= "3.3"
+    def self.base62(n = 16)
+      SecureRandom.alphanumeric(n, chars: BASE62_ALPHABET)
+    end
+  else
+    def self.base62(n = 16)
+      SecureRandom.random_bytes(n).unpack("C*").map do |byte|
+        idx = byte % 64
+        idx = SecureRandom.random_number(58) if idx >= 62
+        BASE62_ALPHABET[idx]
+      end.join
+    end
+  end
 
   # SecureRandom.base58 generates a random base58 string.
   #
