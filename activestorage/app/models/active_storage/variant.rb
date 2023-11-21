rails/rails

commit 1e23fff1a1844c43fc89b277ab4cc290573c1732

Remove unused methods on ActiveStorage::Variant
These methods were added in b221a4dc43368a1b6f00476f7c5f6047c5c7eea4
But they don't seem to be used by Rails internally or have any tests, so
I assume they were added by accident?
As they both seem to be marked as :nodoc: on ActiveStorage::Blob, we can
remove them without a deprecation warning.

If we decide to keep these methods, they should be added to
ActiveStorage::VariantWithRecord as well. No one complaining about there
methods missing on ActiveStorage::VariantWithRecord is another reason
these methods aren't used.

activestorage/app/models/active_storage/variant.rb
@@ -91,12 +91,6 @@ class ActiveStorage::Variant
     ActiveStorage::Filename.new "#{blob.filename.base}.#{variation.format.downcase}"
   end
 
-  alias_method :content_type_for_serving, :content_type
-
-  def forced_disposition_for_serving # :nodoc:
-    nil
-  end
-
   # Returns the receiving variant. Allows ActiveStorage::Variant and ActiveStorage::Preview instances to be used interchangeably.
   def image
     self

commit 78e9db041518df57f808eee217fc86170463fb34

Ensure untracked variants obey config settings
Follow-up to 5aeec6e6f8248f0aa0c70111e7709ec7da5ceccd, which was a
backport of 5d0ab554e3286fe02ae795914ccc23e92f2c73b1.

Untracked variants were broken by 1e23fff1a1844c43fc89b277ab4cc290573c1732,
which only affected 7.1, not 7.0, so the backported fix was not
necessary.  However, the fix also consolidated the implementation of
`content_type_for_serving` and `forced_disposition_for_serving` in
`ActiveStorage::Blob::Servable`, ensuring that those methods obey
`config.active_storage.content_types_to_serve_as_binary` and
`config.active_storage.content_types_allowed_inline`.  But in 7.0, prior
to this commit, `ActiveStorage::Variant` specified its own definition of
those methods, nullifying that benefit.  Therefore, this commit removes
those definitions in favor of `ActiveStorage::Blob::Servable`.

activestorage/app/models/active_storage/variant.rb
@@ -91,12 +91,6 @@ class ActiveStorage::Variant
     ActiveStorage::Filename.new "#{blob.filename.base}.#{variation.format.downcase}"
   end
 
-  alias_method :content_type_for_serving, :content_type
-
-  def forced_disposition_for_serving # :nodoc:
-    nil
-  end
-
   # Returns the receiving variant. Allows ActiveStorage::Variant and ActiveStorage::Preview instances to be used interchangeably.
   def image
     self