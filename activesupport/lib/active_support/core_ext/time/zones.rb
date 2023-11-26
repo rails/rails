504d3509de9d5b445a3f56ab81f591222ab0bf88
Matias Salles <matiassalles99@gmail.com>
2023-11-26 02:26:13 -0300

    check zone presence in find_zone

--- activesupport/lib/active_support/core_ext/time/zones.rb
+++ activesupport/lib/active_support/core_ext/time/zones.rb
@@ -76,10 +76,11 @@ class Time
     #   Time.find_zone! "EST"              # => #<ActiveSupport::TimeZone @name="EST" ...>
     #   Time.find_zone! -5.hours           # => #<ActiveSupport::TimeZone @name="Bogota" ...>
     #   Time.find_zone! nil                # => nil
-    #   Time.find_zone! false              # => false
+    #   Time.find_zone! false              # => nil
+    #   Time.find_zone! ""                 # => nil
     #   Time.find_zone! "NOT-A-TIMEZONE"   # => ArgumentError: Invalid Timezone: NOT-A-TIMEZONE
     def find_zone!(time_zone)
-      return time_zone unless time_zone
+      return unless time_zone.present?
 
       ActiveSupport::TimeZone[time_zone] || raise(ArgumentError, "Invalid Timezone: #{time_zone}")
     end
