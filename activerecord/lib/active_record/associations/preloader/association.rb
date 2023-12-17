def54b22d1640a098243d8167a452a5337304373
Ari Summer <aribsummer@gmail.com>
2023-12-16 17:05:54 -0700

    Replace scope.name with a comparison on both table_name and connection_specification_name

--- activerecord/lib/active_record/associations/preloader/association.rb
+++ activerecord/lib/active_record/associations/preloader/association.rb
@@ -16,12 +16,13 @@ module ActiveRecord
 
           def eql?(other)
             association_key_name == other.association_key_name &&
-              scope.name == other.scope.name &&
+              scope.table_name == other.scope.table_name &&
+              scope.connection_specification_name == other.scope.connection_specification_name &&
               scope.values_for_queries == other.scope.values_for_queries
           end
 
           def hash
-            [association_key_name, scope.name, scope.values_for_queries].hash
+            [association_key_name, scope.table_name, scope.connection_specification_name, scope.values_for_queries].hash
           end
 
           def records_for(loaders)
