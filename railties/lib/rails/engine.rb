f6f6b0542fd9ae6aebe3529baba358d521d64638
Shawn J <phChun@users.noreply.github.com>
2024-01-04 00:19:55 -0500

    Prevent vendorizing gems fixtures in dot directories (like .bundle) from loading when running tests (#50548)
    * Fix Issue with vendorized gems loading fixtures from dot directories
    
    * Fixing matching logic to be specifically any dir starting with a dot on the root
    
    Co-authored-by: Rafael Mendonça França <rafael@rubyonrails.org>

--- railties/lib/rails/engine.rb
+++ railties/lib/rails/engine.rb
@@ -627,7 +627,7 @@ module Rails
       next if is_a?(Rails::Application)
 
       fixtures = config.root.join("test", "fixtures")
-      if fixtures_in_root_and_not_in_vendor?(fixtures)
+      if fixtures_in_root_and_not_in_vendor_or_dot_dir?(fixtures)
         ActiveSupport.on_load(:active_record_fixtures) { self.fixture_paths |= ["#{fixtures}/"] }
       end
     end
@@ -735,9 +735,10 @@ module Rails
         end
       end
 
-      def fixtures_in_root_and_not_in_vendor?(fixtures)
+      def fixtures_in_root_and_not_in_vendor_or_dot_dir?(fixtures)
         fixtures.exist? && fixtures.to_s.start_with?(Rails.root.to_s) &&
-          !fixtures.to_s.start_with?(Rails.root.join("vendor").to_s)
+          !fixtures.to_s.start_with?(Rails.root.join("vendor").to_s) &&
+          !fixtures.to_s.start_with?("#{Rails.root}/.".to_s)
       end
 
       def build_request(env)
