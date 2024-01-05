c5db13056e8169d42af5ac0b11b02c788bd46e18
ooooooo_q <ooooooo-q@users.noreply.github.com>
2024-01-05 12:00:02 +0900

    fix XSS vulnerability when using translation
    [CVE-2024-26143]

--- actionpack/test/abstract/translation_test.rb
+++ actionpack/test/abstract/translation_test.rb
@@ -83,6 +83,22 @@ module AbstractController
         end
       end
 
+      def test_default_translation_as_safe_html
+        @controller.stub :action_name, :index do
+          translation = @controller.t(".twoz", default: ["<tag>"])
+          assert_equal "&lt;tag&gt;", translation
+          assert_equal true, translation.html_safe?
+        end
+      end
+
+      def test_default_translation_with_raise_as_safe_html
+        @controller.stub :action_name, :index do
+          translation = @controller.t(".twoz", raise: true, default: ["<tag>"])
+          assert_equal "&lt;tag&gt;", translation
+          assert_equal true, translation.html_safe?
+        end
+      end
+
       def test_localize
         time, expected = Time.gm(2000), "Sat, 01 Jan 2000 00:00:00 +0000"
         I18n.stub :localize, expected do
@@ -124,6 +140,21 @@ module AbstractController
           translation = @controller.t(".interpolated_html", word: word_struct.new("<World>"))
           assert_equal "<a>Hello &lt;World&gt;</a>", translation
           assert_equal true, translation.html_safe?
+        end
+      end
+
+      def test_translate_marks_translation_with_missing_html_key_as_safe_html
+        @controller.stub :action_name, :index do
+          translation = @controller.t("<tag>.html")
+          assert_equal "translation missing: <tag>.html", translation
+          assert_equal false, translation.html_safe?
+        end
+      end
+      def test_translate_marks_translation_with_missing_nested_html_key_as_safe_html
+        @controller.stub :action_name, :index do
+          translation = @controller.t(".<tag>.html")
+          assert_equal "translation missing: abstract_controller.testing.translation.index.<tag>.html", translation
+          assert_equal false, translation.html_safe?
         end
       end
     end
