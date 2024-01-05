0a51c1a178c203c48499874a8dc2686757e2982a
Jonathan Hefner <jonathan@hefner.pro>
2024-01-05 17:59:08 -0600

    Clean up AS::NumberHelper#number_to_human_size doc [ci-skip]
    (cherry picked from commit 2b801dcec482e2b6228adef2b1f5486ce2b7cca6)

--- activesupport/lib/active_support/number_helper.rb
+++ activesupport/lib/active_support/number_helper.rb
@@ -299,49 +299,55 @@ module ActiveSupport
       NumberToRoundedConverter.convert(number, options)
     end
 
-    # Formats the bytes in +number+ into a more understandable
-    # representation (e.g., giving it 1500 yields 1.46 KB). This
-    # method is useful for reporting file sizes to users. You can
-    # customize the format in the +options+ hash.
+    # Formats +number+ as bytes into a more human-friendly representation.
+    # Useful for reporting file sizes to users.
     #
-    # See <tt>number_to_human</tt> if you want to pretty-print a
-    # generic number.
+    #   number_to_human_size(123)                 # => "123 Bytes"
+    #   number_to_human_size(1234)                # => "1.21 KB"
+    #   number_to_human_size(12345)               # => "12.1 KB"
+    #   number_to_human_size(1234567)             # => "1.18 MB"
+    #   number_to_human_size(1234567890)          # => "1.15 GB"
+    #   number_to_human_size(1234567890123)       # => "1.12 TB"
+    #   number_to_human_size(1234567890123456)    # => "1.1 PB"
+    #   number_to_human_size(1234567890123456789) # => "1.07 EB"
+    #
+    # See #number_to_human if you want to pretty-print a generic number.
     #
     # ==== Options
     #
-    # * <tt>:locale</tt> - Sets the locale to be used for formatting
-    #   (defaults to current locale).
-    # * <tt>:precision</tt> - Sets the precision of the number
-    #   (defaults to 3).
-    # * <tt>:round_mode</tt> - Determine how rounding is performed
-    #   (defaults to :default. See BigDecimal::mode)
-    # * <tt>:significant</tt> - If +true+, precision will be the number
-    #   of significant_digits. If +false+, the number of fractional
-    #   digits (defaults to +true+)
-    # * <tt>:separator</tt> - Sets the separator between the
-    #   fractional and integer digits (defaults to ".").
-    # * <tt>:delimiter</tt> - Sets the thousands delimiter (defaults
-    #   to "").
-    # * <tt>:strip_insignificant_zeros</tt> - If +true+ removes
-    #   insignificant zeros after the decimal separator (defaults to
-    #   +true+)
+    # [+:locale+]
+    #   The locale to use for formatting. Defaults to the current locale.
     #
-    # ==== Examples
+    # [+:precision+]
+    #   The level of precision. Defaults to 3.
     #
-    #   number_to_human_size(123)                                    # => "123 Bytes"
-    #   number_to_human_size(1234)                                   # => "1.21 KB"
-    #   number_to_human_size(12345)                                  # => "12.1 KB"
-    #   number_to_human_size(1234567)                                # => "1.18 MB"
-    #   number_to_human_size(1234567890)                             # => "1.15 GB"
-    #   number_to_human_size(1234567890123)                          # => "1.12 TB"
-    #   number_to_human_size(1234567890123456)                       # => "1.1 PB"
-    #   number_to_human_size(1234567890123456789)                    # => "1.07 EB"
-    #   number_to_human_size(1234567, precision: 2)                  # => "1.2 MB"
-    #   number_to_human_size(483989, precision: 2)                   # => "470 KB"
-    #   number_to_human_size(483989, precision: 2, round_mode: :up)  # => "480 KB"
-    #   number_to_human_size(1234567, precision: 2, separator: ',')  # => "1,2 MB"
-    #   number_to_human_size(1234567890123, precision: 5)            # => "1.1228 TB"
-    #   number_to_human_size(524288000, precision: 5)                # => "500 MB"
+    #     number_to_human_size(123456, precision: 2)  # => "120 KB"
+    #     number_to_human_size(1234567, precision: 2) # => "1.2 MB"
+    #
+    # [+:round_mode+]
+    #   Specifies how rounding is performed. See +BigDecimal.mode+. Defaults to
+    #   +:default+.
+    #
+    #     number_to_human_size(123456, precision: 2, round_mode: :up)
+    #     # => "130 KB"
+    #
+    # [+:significant+]
+    #   Whether +:precision+ should be applied to significant digits instead of
+    #   fractional digits. Defaults to true.
+    #
+    # [+:separator+]
+    #   The decimal separator. Defaults to <tt>"."</tt>.
+    #
+    #     number_to_human_size(1234567, separator: ",")
+    #     # => "1,18 MB"
+    #
+    # [+:delimiter+]
+    #   The thousands delimiter. Defaults to <tt>","</tt>.
+    #
+    # [+:strip_insignificant_zeros+]
+    #   Whether to remove insignificant zeros after the decimal separator.
+    #   Defaults to true.
+    #
     def number_to_human_size(number, options = {})
       NumberToHumanSizeConverter.convert(number, options)
     end
