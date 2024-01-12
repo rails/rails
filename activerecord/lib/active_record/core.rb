8944d804ec060ad00a1d310192f0eb3209545f3d
Petrik de Heus <petrik@deheus.net>
2024-01-12 11:58:23 +0100

    Add examples to #slice and #values_at documentation [ci-skip] (#50679)

--- activerecord/lib/active_record/core.rb
+++ activerecord/lib/active_record/core.rb
@@ -569,6 +569,10 @@ module ActiveRecord
     # Returns a hash of the given methods with their names as keys and returned
     # values as values.
     #
+    #   topic = Topic.new(title: "Budget", author_name: "Jason")
+    #   topic.slice(:title, :author_name)
+    #   => { "title" => "Budget", "author_name" => "Jason" }
+    #
     #--
     # Implemented by ActiveModel::Access#slice.
 
@@ -578,6 +582,10 @@ module ActiveRecord
     # :call-seq: values_at(*methods)
     #
     # Returns an array of the values returned by the given methods.
+    #
+    #   topic = Topic.new(title: "Budget", author_name: "Jason")
+    #   topic.values_at(:title, :author_name)
+    #   => ["Budget", "Jason"]
     #
     #--
     # Implemented by ActiveModel::Access#values_at.
