0ea9748ba6f35f1edca0ed6ce9b2b7c3e110ab91
Stan Lo <stan001212@gmail.com>
2024-01-24 11:07:32 +0000

    Fix prompt colorization test's setup

--- railties/test/commands/console_test.rb
+++ railties/test/commands/console_test.rb
@@ -59,7 +59,8 @@ class Rails::ConsoleTest < ActiveSupport::TestCase
   end
 
   def test_prompt_env_colorization
-    irb_console = Rails::Console::IRBConsole.new(app)
+    app = build_app(nil)
+    irb_console = Rails::Console.new(app).console
     red = "\e[31m"
     green = "\e[32m"
     clear = "\e[0m"
