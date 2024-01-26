cf7d8596c2198a917a2652211109128a71e9ad80
Trevor Vallender <trevor@tsvallender.co.uk>
2024-01-26 09:15:55 +0000

    Document Attached::Model#attachment_changes

--- activestorage/lib/active_storage/attached/model.rb
+++ activestorage/lib/active_storage/attached/model.rb
@@ -251,7 +251,11 @@ module ActiveStorage
       end
     end
 
-    def attachment_changes # :nodoc:
+    ##
+    # :method: attachment_changes
+    #
+    # Returns a Hash of any changes to attachments with unsaved changes, keyed on the attachment’s name.
+    def attachment_changes
       @attachment_changes ||= {}
     end
 
