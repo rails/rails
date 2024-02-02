a71a4ceccb1a9142039ae54a287b6253102af57e
Vladimir Dementyev <palkan@hey.com>
2024-02-02 12:38:17 -0800

    Move async-awareness from channels to pubsub
    Channels (as abstractions) shouldn't care about sync or async nature of pub/sub operations, so should not rely on executor directly. Most adapters already performed operations asynchronously (thus, we doubled it), so this change doesn't affect the existing behaviour.
    
    The only noticeable change is that unsubscribing has been also made async for async adapter (and all adapter using async subscribers map)

--- actioncable/lib/action_cable/subscription_adapter/postgresql.rb
+++ actioncable/lib/action_cable/subscription_adapter/postgresql.rb
@@ -71,12 +71,11 @@ module ActionCable
           end
         end
 
-        class Listener < SubscriberMap
+        class Listener < SubscriberMap::Async
           def initialize(adapter, executor)
-            super()
+            super(executor)
 
             @adapter = adapter
-            @executor = executor
             @queue = Queue.new
 
             @thread = Thread.new do
@@ -122,10 +121,6 @@ module ActionCable
 
           def remove_channel(channel)
             @queue.push([:unlisten, channel])
-          end
-
-          def invoke_callback(*)
-            @executor.post { super }
           end
         end
     end
