25b7cc2b042742a633998a7e7a1cfab19eb8f69f
dhh <david@loudthinking.com>
2024-02-13 11:42:58 -0800

    Default thread count is now 3

--- activestorage/test/dummy/config/puma.rb
+++ activestorage/test/dummy/config/puma.rb
@@ -7,7 +7,7 @@
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 3 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
