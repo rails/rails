53079761a55a7460c75db178bd48bf6b26588408
Eugene Kenny <elkenny@gmail.com>
2024-02-14 23:42:31 +0000

    Add autoload for ActionController::TestRequest
    This constant is referenced in
    actionpack/lib/action_dispatch/testing/assertions/routing.rb,
    which doesn't directly depend on ActionController::TestCase.

--- actionpack/lib/action_controller.rb
+++ actionpack/lib/action_controller.rb
@@ -66,8 +66,11 @@ module ActionController
     autoload :ApiRendering
   end
 
-  autoload :TestCase,           "action_controller/test_case"
-  autoload :TemplateAssertions, "action_controller/test_case"
+  autoload_at "action_controller/test_case" do
+    autoload :TestCase
+    autoload :TestRequest
+    autoload :TemplateAssertions
+  end
 end
 
 # Common Active Support usage in Action Controller
