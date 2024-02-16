214f7c3d16bf72db4ce976a704126e4912a1b420
Ryan Krug <keegnotrub@users.noreply.github.com>
2024-02-16 15:47:29 -0600

    flatten `if` branching on `child_index` lambda check
    both branches set `options[:child_index]` so we just set it as the result of the conditional instead.
    
    Co-authored-by: Sean Doyle <seanpdoyle@users.noreply.github.com>

--- actionview/lib/action_view/helpers/form_helper.rb
+++ actionview/lib/action_view/helpers/form_helper.rb
@@ -2723,11 +2723,12 @@ module ActionView
             association.each do |child|
               if explicit_child_index
                 if explicit_child_index.respond_to?(:call)
-                  if explicit_child_index.arity.zero?
-                    options[:child_index] = explicit_child_index.call
-                  else
-                    options[:child_index] = explicit_child_index.call(child)
-                  end
+                  options[:child_index] =
+                    if explicit_child_index.arity.zero?
+                      explicit_child_index.call
+                    else
+                      explicit_child_index.call(child)
+                    end
                 end
               else
                 options[:child_index] = nested_child_index(name)
