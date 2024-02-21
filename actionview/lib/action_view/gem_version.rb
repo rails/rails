6f0d1ad14b92b9f5906e44740fce8b4f1c7075dc
John Hawthorn <john@hawthorn.email>
2024-02-21 13:42:12 -0800

    Preparing for 7.1.3.2 release

--- actionview/lib/action_view/gem_version.rb
+++ actionview/lib/action_view/gem_version.rb
@@ -10,7 +10,7 @@ module ActionView
     MAJOR = 7
     MINOR = 1
     TINY  = 3
-    PRE   = "1"
+    PRE   = "2"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
--- activesupport/CHANGELOG.md
+++ activesupport/CHANGELOG.md
@@ -1,3 +1,8 @@
+## Rails 7.1.3.2 (February 21, 2024) ##
+
+*   No changes.
+
+
 ## Rails 7.1.3.1 (February 21, 2024) ##
 
 *   No changes.
