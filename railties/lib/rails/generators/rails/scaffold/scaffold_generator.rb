4bc2286f258236d054bcdfe9d2d6d8dcf6d6ca2b
takmar <tak.btbb@gmail.com>
2024-03-03 20:31:21 +0900

    Prevent duplicate invocation of resource_route with Scaffold command

--- railties/lib/rails/generators/rails/scaffold/scaffold_generator.rb
+++ railties/lib/rails/generators/rails/scaffold/scaffold_generator.rb
@@ -6,11 +6,11 @@ module Rails
   module Generators
     class ScaffoldGenerator < ResourceGenerator # :nodoc:
       remove_hook_for :resource_controller
+      remove_hook_for :resource_route
       remove_class_option :actions
 
       class_option :api, type: :boolean,
         desc: "Generate API-only controller and tests, with no view templates"
-      class_option :resource_route, type: :boolean
 
       hook_for :scaffold_controller, required: true
     end
