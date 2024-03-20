1e4f12c12c7d9f4c56eb8acff5f4a4f849f09fd8
John Athayde <john@athayde.com>
2024-03-20 15:50:51 -0400

    Guides visual redesign (#51341)
    Improve the visual presentation of the Rails Guides, bringing them more inline with the new marketing rubyonrails.org site, as part of a project with the Rails Foundation.
    
    The CSS has been recreated from SCSS files, generated into the final pieces. The stylesrc directory contains those commented source files. The generate task will now process those scss => css files.
    
    Co-authored-by: Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>

--- guides/rails_guides/markdown.rb
+++ guides/rails_guides/markdown.rb
@@ -120,7 +120,7 @@ module RailsGuides
                 end
 
                 node[:id] = dom_id(hierarchy) unless node[:id]
-                node.inner_html = "#{node_index(hierarchy)} #{node.inner_html}"
+                node.inner_html = "<span>#{node_index(hierarchy)}</span> #{node.inner_html}"
               end
             end
 
@@ -148,10 +148,20 @@ module RailsGuides
           end.to_html
 
           @index = <<-INDEX.html_safe
-          <div id="subCol">
-            <h3 class="chapter"><img src="images/chapters_icon.gif" alt="" />Chapters</h3>
+          <nav id="subCol">
+            <h3 class="chapter">
+              <picture>
+                <!-- Using the `source`  HTML tag to set the dark theme image -->
+                <source
+                  srcset="images/icon_book-close-bookmark-1-wht.svg"
+                  media="(prefers-color-scheme: dark)"
+                />
+                <img src="images/icon_book-close-bookmark-1.svg" alt="Chapter Icon" />
+              </picture>
+              Chapters
+            </h3>
             #{@index}
-          </div>
+          </nav>
           INDEX
         end
       end
