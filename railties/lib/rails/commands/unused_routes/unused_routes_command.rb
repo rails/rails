e54f869e431372ba30bb6a00c8b58cbba692de7e
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2024-04-02 13:59:18 -0500

    Defer route drawing to the first request, or when url_helpers called.
    Executes the first routes reload in middleware, or when the route set
    url_helpers is called. Previously, this was executed unconditionally on
    boot, which can slow down boot time unnecessarily for larger apps with
    lots of routes.

--- railties/lib/rails/commands/unused_routes/unused_routes_command.rb
+++ railties/lib/rails/commands/unused_routes/unused_routes_command.rb
@@ -41,6 +41,7 @@ module Rails
 
       def perform(*)
         boot_application!
+        Rails.application.routes_reloader.execute_unless_loaded
         require "action_dispatch/routing/inspector"
 
         say(inspector.format(formatter, routes_filter))
