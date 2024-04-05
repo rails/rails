2eb30f08a00e59ebcc4be1c48621e2394a073c50
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2024-04-05 15:18:18 +0200

    Make more info and guides index accessible
    * Adds aria-controls, which binds the control to the content.
    
    * Adds aria-expanded, which signals the current expansion state.
    
    * Adds focus handling, which focuses the first link or the button
      depending on the expansion state.
    
    * Adds keyboard closing for the more info button.
    
    * Adds outside click tracking to close the expanded content.
    
    * Fixes the more info button to not be focusable.

--- guides/assets/javascripts/guides.js
+++ guides/assets/javascripts/guides.js
@@ -24,26 +24,59 @@
   }
 
   document.addEventListener("turbo:load", function() {
-    var guidesMenu = document.getElementById("guidesMenu");
-    var guides     = document.getElementById("guides");
+    // The guides menu anchor is overridden to expand an element with the entire
+    // index on the same page. It is important that both the visibility is
+    // changed and that the aria-expanded attribute is toggled.
+    //
+    // Additionally, keyboard users should be able to close these guides by
+    // pressing escape, which is the standard key to collapse expanded elements.
+    var guidesMenuButton = document.getElementById("guidesMenu");
+    var guides = document.getElementById(guidesMenuButton.getAttribute('aria-controls'));
 
-    guidesMenu.addEventListener("click", function(e) {
-      e.preventDefault();
+    var toggleGuidesMenu = function () {
+      var nextExpanded = guidesMenuButton.getAttribute('aria-expanded') === 'false'
+
       guides.classList.toggle("visible");
+      guidesMenuButton.setAttribute('aria-expanded', nextExpanded ? 'true' : 'false')
+
+      var focusElement = nextExpanded ? guides.querySelector('a') : guidesMenuButton
+      focusElement.focus();
+    }
+
+    guidesMenuButton.addEventListener("click", function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      toggleGuidesMenu();
     });
 
     each(document.querySelectorAll("#guides a"), function(element) {
-      element.addEventListener("click", function(e) {
-        guides.classList.toggle("visible");
+      element.addEventListener("click", function() {
+        toggleGuidesMenu();
       });
     });
 
-    document.addEventListener("keyup", function(e) {
-      if (e.key === "Escape" && guides.classList.contains("visible")) {
-        guides.classList.remove("visible");
+    document.addEventListener('click', function (e) {
+      if (guidesMenuButton.getAttribute('aria-expanded') === 'false') {
+        return;
+      }
+
+      if (e.target instanceof Element) {
+        if (!e.target.closest('#' + guidesMenuButton.getAttribute('aria-controls'))) {
+          toggleGuidesMenu();
+        }
       }
     });
 
+    document.addEventListener("keyup", function(e) {
+      if (e.key === "Escape" && guidesMenuButton.getAttribute('aria-expanded') === 'true') {
+        toggleGuidesMenu();
+      }
+    });
+
+    // If the browser supports the animation timeline CSS feature, JavaScript is
+    // not required for the element to be made visible at a certain scroll
+    // position.
     if (typeof window.CSS === "undefined" || typeof window.CSS.supports === 'undefined' || !CSS.supports("(animation-timeline: scroll())")) {
       var toggleBackToTop = function() {
         if (window.scrollY > 300) {
@@ -56,6 +89,9 @@
       document.addEventListener("scroll", toggleBackToTop);
     }
 
+    // Automatically browse when the version selector is changed. It is
+    // important that this behaviour is communicated to the user, for example
+    // via an accessible label.
     var guidesVersion = document.querySelector("select.guides-version");
     guidesVersion.addEventListener("change", function(e) {
       Turbo.visit(e.target.value);
@@ -64,26 +100,63 @@
     var guidesIndexItem   = document.querySelector("select.guides-index-item");
     var currentGuidePath  = window.location.pathname;
     guidesIndexItem.value = currentGuidePath.substring(currentGuidePath.lastIndexOf("/") + 1) || 'index.html';
-
     guidesIndexItem.addEventListener("change", function(e) {
       Turbo.visit(e.target.value);
     });
 
-    var moreInfoButton = document.querySelector(".more-info-button");
-    var moreInfoLinks  = document.querySelector(".more-info-links");
+    // The move info button expands an element with several links. It is
+    // important that both the visibility is changed and that the aria-expanded
+    // attribute is toggled.
+    //
+    // Additionally, keyboard users should be able to close this menu by
+    // pressing escape, which is the standard key to collapse expanded elements.
+    var moreInfoButton = document.getElementById("more-info");
+    var moreInfoLinks = document.getElementById(moreInfoButton.getAttribute('aria-controls'));
+
+    var toggleMoreInfoContent = function () {
+      var nextExpanded = moreInfoButton.getAttribute('aria-expanded') === 'false';
+      var focusElement = undefined
+
+      if (nextExpanded) {
+        wrap(moreInfoLinks, createElement("div", "more-info-container"));
+        moreInfoButton.setAttribute('aria-expanded', 'true');
+        focusElement = moreInfoLinks.querySelector('a')
+      } else {
+        unwrap(moreInfoLinks);
+        moreInfoButton.setAttribute('aria-expanded', 'false');
+        focusElement = moreInfoButton
+      }
+
+      moreInfoLinks.classList.toggle("s-hidden");
+      focusElement.focus();
+    }
 
     moreInfoButton.addEventListener("click", function(e) {
       e.preventDefault();
+      e.stopPropagation();
 
-      if (moreInfoLinks.classList.contains("s-hidden")) {
-        wrap(moreInfoLinks, createElement("div", "more-info-container"));
-        moreInfoLinks.classList.remove("s-hidden");
-      } else {
-        moreInfoLinks.classList.add("s-hidden");
-        unwrap(moreInfoLinks);
+      toggleMoreInfoContent();
+    });
+
+    document.addEventListener("keyup", function(e) {
+      if (e.key === "Escape" && moreInfoButton.getAttribute('aria-expanded') === 'true') {
+        toggleMoreInfoContent();
       }
     });
 
+    document.addEventListener('click', function (e) {
+      if (moreInfoButton.getAttribute('aria-expanded') === 'false') {
+        return;
+      }
+
+      if (e.target instanceof Element) {
+        if (!e.target.closest('.more-info-container')) {
+          toggleMoreInfoContent();
+        }
+      }
+    });
+
+    //
     var clipboard = new ClipboardJS('.clipboard-button');
     clipboard.on('success', function(e) {
       var trigger = e.trigger;
