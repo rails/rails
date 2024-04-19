2b9f1d60b61f7260b9cf5fe6dae5253625820eec
Mark James <mrj@advancedcontrols.com.au>
2024-04-19 11:56:28 +1000

    Stop showing select prompts for false attributes

--- actionview/lib/action_view/helpers/tags/select_renderer.rb
+++ actionview/lib/action_view/helpers/tags/select_renderer.rb
@@ -40,7 +40,7 @@ module ActionView
               option_tags = tag_builder.content_tag_string("option", content, value: "", label: label) + "\n" + option_tags
             end
 
-            if value.blank? && options[:prompt]
+            if value != false && value.blank? && options[:prompt]
               tag_options = { value: "" }.tap do |prompt_opts|
                 prompt_opts[:disabled] = true if options[:disabled] == ""
                 prompt_opts[:selected] = true if options[:selected] == ""
