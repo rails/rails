5b2977afe09b3e01b261d073a760a1a1a5949174
Rafael Mendonça França <rafael@rubyonrails.org>
2024-04-22 21:21:11 +0000

    Disable rubocop on pattern matching test cases
    Lint/Syntax can't be disabled anymore since https://github.com/rubocop/rubocop/pull/12760.

--- actionpack/test/dispatch/test_response_test/pattern_matching_test_cases.rb
+++ actionpack/test/dispatch/test_response_test/pattern_matching_test_cases.rb
@@ -7,16 +7,12 @@ module TestResponseTest::PatternMatchingTestCases
     test "JSON response Hash pattern matching" do
       response = ActionDispatch::TestResponse.create(200, { "Content-Type" => "application/json" }, '{ "foo": "fighters" }')
 
-      # rubocop:disable Lint/Syntax
       assert_pattern { response.parsed_body => { foo: /fighter/ } }
-      # rubocop:enable Lint/Syntax
     end
 
     test "JSON response Array pattern matching" do
       response = ActionDispatch::TestResponse.create(200, { "Content-Type" => "application/json" }, '[{ "foo": "fighters" }, { "nir": "vana" }]')
-      # rubocop:disable Lint/Syntax
       assert_pattern { response.parsed_body => [{ foo: /fighter/ }, { nir: /vana/ }] }
-      # rubocop:enable Lint/Syntax
     end
 
     test "HTML response pattern matching" do
@@ -30,18 +26,14 @@ module TestResponseTest::PatternMatchingTestCases
       HTML
       html = response.parsed_body
 
-      # rubocop:disable Lint/Syntax
       html.at("main") => {name:, content:}
-      # rubocop:enable Lint/Syntax
 
       assert_equal "main", name
       assert_equal "Some main content", content
 
-      # rubocop:disable Lint/Syntax
       assert_pattern { html.at("main") => { content: "Some main content" } }
       assert_pattern { html.at("main") => { content: /content/ } }
       assert_pattern { html.at("main") => { children: [{ name: "h1", content: /content/ }] } }
-      # rubocop:enable Lint/Syntax
     end
   end
 end
--- actionview/test/template/test_case_test/pattern_matching_test_cases.rb
+++ actionview/test/template/test_case_test/pattern_matching_test_cases.rb
@@ -6,7 +6,6 @@ class ActionView::PatternMatchingTestCases < ActionView::TestCase
 
     render "developers/developer_with_h1", developer: developer
 
-    # rubocop:disable Lint/Syntax
     assert_pattern { document_root_element.at("h1") => { content: "Eloy", attributes: [{ name: "id", value: "name" }] } }
     refute_pattern { document_root_element.at("h1") => { content: "Not Eloy" } }
   end
@@ -16,10 +15,8 @@ class ActionView::PatternMatchingTestCases < ActionView::TestCase
 
     render "developers/developer", developer: developer
 
-    # rubocop:disable Lint/Syntax
     assert_pattern { rendered.html => { content: "Eloy" } }
     refute_pattern { rendered.html => { content: "Not Eloy" } }
-    # rubocop:enable Lint/Syntax
   end
 
   test "rendered.json integrates with pattern matching" do
@@ -27,9 +24,7 @@ class ActionView::PatternMatchingTestCases < ActionView::TestCase
 
     render formats: :json, partial: "developers/developer", locals: { developer: developer }
 
-    # rubocop:disable Lint/Syntax
     assert_pattern { rendered.json => { name: "Eloy" } }
     refute_pattern { rendered.json => { name: "Not Eloy" } }
-    # rubocop:enable Lint/Syntax
   end
 end
