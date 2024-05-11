b3bb3e97fc4157cf10891e586eb06b688a349903
fatkodima <fatkodima123@gmail.com>
2024-05-11 11:23:01 +0300

    Support `touch_all` in batches

--- activerecord/test/cases/batches_test.rb
+++ activerecord/test/cases/batches_test.rb
@@ -4,10 +4,11 @@ require "cases/helper"
 require "models/comment"
 require "models/post"
 require "models/subscriber"
+require "models/developer"
 require "models/cpk"
 
 class EachTest < ActiveRecord::TestCase
-  fixtures :posts, :subscribers, :cpk_orders
+  fixtures :posts, :subscribers, :developers, :cpk_orders
 
   def setup
     @posts = Post.order("id asc")
@@ -382,6 +383,22 @@ class EachTest < ActiveRecord::TestCase
 
   def test_in_batches_update_all_returns_zero_when_no_batches
     assert_equal 0, Post.where("1=0").in_batches(of: 2).update_all(title: "updated-title")
+  end
+
+  def test_in_batches_touch_all_affect_all_records
+    time = Time.new(2000, 1, 1, 0, 0, 0)
+    assert_queries_count(6 + 6) do # 6 selects, 6 updates
+      Developer.in_batches(of: 2).touch_all(time: time)
+    end
+    assert_equal Developer.all.pluck(:updated_at), [time] * Developer.count
+  end
+
+  def test_in_batches_touch_all_returns_rows_affected
+    assert_equal 11, Developer.in_batches(of: 2).touch_all
+  end
+
+  def test_in_batches_touch_all_returns_zero_when_no_batches
+    assert_equal 0, Developer.where("1=0").in_batches(of: 2).touch_all
   end
 
   def test_in_batches_delete_all_should_not_delete_records_in_other_batches
