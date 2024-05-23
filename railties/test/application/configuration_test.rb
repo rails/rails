ad002de1ab458325f12199822046e5aa0be3432a
Yasuo Honda <yasuo.honda@gmail.com>
2024-05-24 08:01:11 +0900

    Merge pull request #51901 from zzak/sqlite-non_existent-errors
    Fix railtie config test for sqlite 3.46.0

--- railties/test/application/configuration_test.rb
+++ railties/test/application/configuration_test.rb
@@ -2893,7 +2893,7 @@ module ApplicationTests
       error = assert_raises(StandardError) do
         Post.lease_connection.add_index :posts, :non_existent
       end
-      assert_match(/no such column: non_existent/, error.message)
+      assert_match(/no such column: "?non_existent"?/, error.message)
     end
 
     test "ActiveSupport::MessageEncryptor.use_authenticated_message_encryption is true by default for new apps" do
