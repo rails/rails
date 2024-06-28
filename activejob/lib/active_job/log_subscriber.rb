912440d8b7319cd5f97eb3b9ad5e55b0b0f8d16b
Dennis Paagman <dennis@paagman.dev>
2024-06-28 20:09:59 +0200

    Improve performance of ActiveJob::LogSubscriber#enqueue_source_location
    In line with the improvements made in #49095, implements the same
    lazily yielding backtrace frame. This will improve the performance of
    `verbose_enqueue_logs`.

--- activejob/lib/active_job/log_subscriber.rb
+++ activejob/lib/active_job/log_subscriber.rb
@@ -189,15 +189,29 @@ module ActiveJob
       end
 
       def log_enqueue_source
-        source = extract_enqueue_source_location(caller)
+        source = enqueue_source_location
 
         if source
           logger.info("↳ #{source}")
         end
       end
 
-      def extract_enqueue_source_location(locations)
-        backtrace_cleaner.clean(locations.lazy).first
+      if Thread.respond_to?(:each_caller_location)
+        def enqueue_source_location
+          Thread.each_caller_location do |location|
+            frame = backtrace_cleaner.clean_frame(location)
+            return frame if frame
+          end
+          nil
+        end
+      else
+        def enqueue_source_location
+          caller_locations(2).each do |location|
+            frame = backtrace_cleaner.clean_frame(location)
+            return frame if frame
+          end
+          nil
+        end
       end
 
       def enqueued_jobs_message(adapter, enqueued_jobs)
