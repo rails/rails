0a080ed6b227d22dcf30c1a2cab0121735d62af1
Reese is on Codeberg <me@reeseric.ci>
2024-07-24 07:47:03 -0500

    Create SubscriptionObserver

---
+++ actioncable/app/javascript/action_cable/subscription_observer.js
@@ -0,0 +1,35 @@
+class SubscriptionObserver {
+  #value = {};
+  #subscribers = new Set();
+  #parser = String;
+  
+  constructor(subscription, initial = {}, parser = String) {
+    this.#value = initial;
+    this.#parser = parser;
+    this.subscription = subscription;
+    this.subscription.received = (data) => {
+      this.value = data;
+    }
+  }
+  
+  set value(val) {
+    this.#value = val
+    this.#subscribers.forEach((subscriber) => {
+      subscriber(this.value)
+    })
+  }
+  
+  get value() {
+    return this.#parser(this.#value)
+  }
+    
+  subscribe(callback) {
+    this.#subscribers.add(callback)
+    this.#subscribers.forEach((subscriber) => {
+      subscriber(this.value)
+    })
+    return () => { this.#subscribers.remove(callback) }
+  }
+}
+
+export default SubscriptionObserver;
