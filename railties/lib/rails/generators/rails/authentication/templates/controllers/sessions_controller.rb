ed553ccce325d5fcbf89ea21d037e7170db442dc
David Heinemeier Hansson <david@hey.com>
2024-07-31 16:22:19 -0700

    Logging out requires current session to be loaded
    Closes #52369. Co-authored-by: yshmarov

--- railties/lib/rails/generators/rails/authentication/templates/controllers/sessions_controller.rb
+++ railties/lib/rails/generators/rails/authentication/templates/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  allow_unauthenticated_access
+  allow_unauthenticated_access only: %i[ new create ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
