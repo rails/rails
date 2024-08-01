52664e97cca4ad60f6d87dbeaed0963a7850e53a
David Heinemeier Hansson <david@hey.com>
2024-08-01 16:08:02 -0700

    Use new automated token generator

--- railties/lib/rails/generators/rails/authentication/templates/mailers/passwords_mailer.rb
+++ railties/lib/rails/generators/rails/authentication/templates/mailers/passwords_mailer.rb
@@ -1,6 +1,6 @@
 class PasswordsMailer < ApplicationMailer
   def reset(user)
-    @token = user.signed_id(purpose: "password", expires_in: 15.minutes)
+    @user = user
     mail subject: "Reset your password", to: user.email_address, from: "passwords@example.com"
   end
 end
