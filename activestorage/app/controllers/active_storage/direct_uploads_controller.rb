0f03da1bcd337dc3df33c8837ccc3e0da7665651
Alex <alex@tanda.co>
2024-08-05 11:25:38 +1000

    Make it possible to secure `ActiveStorage::DirectUploadsController`
    https://guides.rubyonrails.org/active_storage_overview.html#authenticated-controllers discusses how to secure the Active Storage rendering controllers (potentially of interest: [my PR](https://github.com/rails/rails/pull/52388) that improves the docs). These controllers are easy to secure becuase most of the functionality is extracted to modules so they're easy enough to re-implement.
    
    The same isn't true for `DirectUploadsController`. But you should secure that controller too. If you don't, then unauthenticated users can create `ActiveStorage::Blob` records in your database, and get secured URLs they can use to directly upload files into your S3 (etc) bucket.
    
    This PR makes it easier to secure the controller:
    
    - Functionality is extracted to a module.
    - Docs are updated to point you in the right direction.

--- activestorage/app/controllers/active_storage/direct_uploads_controller.rb
+++ activestorage/app/controllers/active_storage/direct_uploads_controller.rb
@@ -3,21 +3,14 @@
 # Creates a new blob on the server side in anticipation of a direct-to-service upload from the client side.
 # When the client-side upload is completed, the signed_blob_id can be submitted as part of the form to reference
 # the blob that was created up front.
+#
+# WARNING: All Active Storage controllers are publicly accessible by default. The
+# generated URLs are hard to guess, but permanent by design. If your files
+# require a higher level of protection consider implementing
+# {Authenticated Controllers}[https://guides.rubyonrails.org/active_storage_overview.html#authenticated-controllers].
+#
+# If you implement your own controller, you will also need to add a new route, and provide that route to the
+# +direct_upload_url+ option for file inputs.
 class ActiveStorage::DirectUploadsController < ActiveStorage::BaseController
-  def create
-    blob = ActiveStorage::Blob.create_before_direct_upload!(**blob_args)
-    render json: direct_upload_json(blob)
-  end
-
-  private
-    def blob_args
-      params.require(:blob).permit(:filename, :byte_size, :checksum, :content_type, metadata: {}).to_h.symbolize_keys
-    end
-
-    def direct_upload_json(blob)
-      blob.as_json(root: false, methods: :signed_id).merge(direct_upload: {
-        url: blob.service_url_for_direct_upload,
-        headers: blob.service_headers_for_direct_upload
-      })
-    end
+  include ActiveStorage::SetDirectUpload
 end
