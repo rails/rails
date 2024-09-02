rails/rails

commit 175bf8259a83853836657c0fbf300dfa3842e535

Dump schema only once after database prepare

activerecord/lib/active_record/tasks/database_tasks.rb
@@ -175,6 +175,7 @@ module ActiveRecord
 
       def prepare_all
         seed = false
+        dump_db_configs = []
 
         each_current_configuration(env) do |db_config|
           with_temporary_pool(db_config) do
@@ -197,12 +198,20 @@ module ActiveRecord
 
         each_current_environment(env) do |environment|
           db_configs_with_versions(environment).sort.each do |version, db_configs|
+            dump_db_configs |= db_configs
+
             db_configs.each do |db_config|
               with_temporary_pool(db_config) do
                 migrate(version)
-                dump_schema(db_config) if ActiveRecord.dump_schema_after_migration
               end
             end
+          end
+        end
+
+        # Dump schema for databases that were migrated.
+        if ActiveRecord.dump_schema_after_migration
+          dump_db_configs.each do |db_config|
+            dump_schema(db_config)
           end
         end
 

railties/test/application/rake/multi_dbs_test.rb
@@ -556,7 +556,6 @@ module ApplicationTests
           end
         MIGRATION
 
-
         Dir.chdir(app_path) do
           rails "db:migrate:up:primary", "VERSION=01_one_migration.rb"
           rails "db:migrate:up:primary", "VERSION=03_three_migration.rb"
@@ -592,6 +591,46 @@ module ApplicationTests
           output = rails "db:prepare"
           entries = output.scan(/^== (\d+).+migrated/).map(&:first).map(&:to_i)
           assert_equal [1, 2, 3, 4] * 2, entries # twice because for test env too
+        end
+      end
+
+      test "db:prepare only dumps schema for migrated databases" do
+        require "#{app_path}/config/environment"
+        app_file "db/migrate/01_one_migration.rb", <<-MIGRATION
+          class OneMigration < ActiveRecord::Migration::Current
+          end
+        MIGRATION
+
+        app_file "db/animals_migrate/02_two_migration.rb", <<-MIGRATION
+          class TwoMigration < ActiveRecord::Migration::Current
+          end
+        MIGRATION
+
+        primary_mtime = nil
+        animals_mtime = nil
+
+        Dir.chdir(app_path) do
+          # Run the first two migrations to get the schema files.
+          rails "db:prepare"
+
+          assert File.exist?("db/schema.rb")
+          assert File.exist?("db/animals_schema.rb")
+
+          primary_mtime = File.mtime("db/schema.rb")
+          animals_mtime = File.mtime("db/animals_schema.rb")
+        end
+
+        app_file "db/animals_migrate/03_three_migration.rb", <<-MIGRATION
+          class ThreeMigration < ActiveRecord::Migration::Current
+          end
+        MIGRATION
+
+        Dir.chdir(app_path) do
+          # Run the new migration and assert that only the animals schema was updated.
+          rails "db:prepare"
+
+          assert_equal primary_mtime, File.mtime("db/schema.rb")
+          assert_not_equal animals_mtime, File.mtime("db/animals_schema.rb")
         end
       end
 

commit 4514d902f97e19b70a2fad99a718deea1977f050

Make sure we are using the correct connection pool when dumping the schema
This was broken by 175bf8259a83853836657c0fbf300dfa3842e535.

Before we were switching the connection pool to use the one for the
correspondent db config, but since we moved the `dump_schema` call to
outside of the `with_temporary_pool` block, we were using the default
connection pool.

Fixes #52776.

activerecord/lib/active_record/tasks/database_tasks.rb
@@ -211,7 +211,9 @@ module ActiveRecord
         # Dump schema for databases that were migrated.
         if ActiveRecord.dump_schema_after_migration
           dump_db_configs.each do |db_config|
-            dump_schema(db_config)
+            with_temporary_pool(db_config) do
+              dump_schema(db_config)
+            end
           end
         end
 