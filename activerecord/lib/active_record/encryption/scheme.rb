1635ff79dc9a7ce44e489d2ab699745c1eaccf47
Yana Agun Siswanto <yana.developer@gmail.com>
2024-09-02 16:24:49 +0700

    Update encryption key configuration error message for clarity
    Updated the error message in `validate_credential` method to provide clearer
    instructions for configuring encryption keys.
    Changed the reference to setting `config.active_record.encryption.<key>` to
    `ActiveRecord::Encryption.config.<key>` in the initializer.
    `config.active_record.encryption` is only supported in Rails 7.1.4.
    
    See #52762 & #50604

--- activerecord/lib/active_record/encryption/scheme.rb
+++ activerecord/lib/active_record/encryption/scheme.rb
@@ -83,7 +83,7 @@ module ActiveRecord
         def validate_credential(key, error_message = "is not configured")
           unless ActiveRecord::Encryption.config.public_send(key).present?
             raise Errors::Configuration, "#{key} #{error_message}. Please configure it via credential "\
-              "active_record_encryption.#{key} or by setting config.active_record.encryption.#{key}"
+              "active_record_encryption.#{key} or by setting ActiveRecord::Encryption.config.#{key} in the initializer."
           end
         end
 
