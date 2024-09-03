8f246640a04429ed4abe8dbcdab4fd47d2451704
David Heinemeier Hansson <david@hey.com>
2024-09-03 14:33:32 -0700

    The remote ip part is actually not necessary

--- railties/lib/rails/rack/silence_request.rb
+++ railties/lib/rails/rack/silence_request.rb
@@ -5,16 +5,16 @@ require "active_support/logger_silence"
 
 module Rails
   module Rack
-    # Allows you to silence requests made to a specific path and optionally by a specific remote IP.
+    # Allows you to silence requests made to a specific path.
     # This is useful for preventing recurring requests like healthchecks from clogging the logging.
-    # This middleware is used to do just that against the path /up from localhost in production by default.
+    # This middleware is used to do just that against the path /up in production by default.
     #
     # Example:
     #
-    #   config.middleware.insert_before Rails::Rack::Logger,
-    #     Rails::Rack::SilenceRequest, remote_ip: "127.0.0.1", path: "/up"
+    #   config.middleware.insert_before \
+    #     Rails::Rack::Logger, Rails::Rack::SilenceRequest, path: "/up"
     #
-    # Note: This should be set before the Rails::Rack::Logger middleware in order to properly silence the entire request.
+    # This middleware can also be configured using `config.silence_healthcheck = "/up"` in Rails.
     class SilenceRequest
       def initialize(app, path:, remote_ip: nil)
         @app, @path, @remote_ip = app, path, remote_ip
@@ -23,7 +23,7 @@ module Rails
       def call(env)
         request = ActionDispatch::Request.new(env)
 
-        if due_silencing?(request)
+        if request.path == @path
           Rails.logger.silence do
             @app.call(env)
           end
@@ -31,19 +31,6 @@ module Rails
           @app.call(env)
         end
       end
-
-      private
-        def due_silencing?(request)
-          remote_ip_due_silencing?(request) && path_due_silencing?(request)
-        end
-
-        def remote_ip_due_silencing?(request)
-          @remote_ip ? request.remote_ip == @remote_ip : true
-        end
-
-        def path_due_silencing?(request)
-          request.path == @path
-        end
     end
   end
 end
--- railties/test/rack_silence_request_test.rb
+++ railties/test/rack_silence_request_test.rb
@@ -9,32 +9,13 @@ class RackSilenceRequestTest < ActiveSupport::TestCase
     mock_logger = Minitest::Mock.new
     mock_logger.expect :silence, nil
 
-    app = app_with_silence(path: "/up")
+    app = Rails::Rack::SilenceRequest.new(lambda { |env| [200, env, "app"] }, path: "/up")
 
     Rails.stub(:logger, mock_logger) do
-      app.call(Rack::MockRequest.env_for("http://example.com/up", "REMOTE_ADDR" => "127.0.0.1"))
-      app.call(Rack::MockRequest.env_for("http://example.com/down", "REMOTE_ADDR" => "127.0.0.1"))
+      app.call(Rack::MockRequest.env_for("http://example.com/up"))
+      app.call(Rack::MockRequest.env_for("http://example.com/down"))
     end
 
     assert mock_logger.verify
   end
-
-  test "silence request only to specific path with ip restriction" do
-    mock_logger = Minitest::Mock.new
-    mock_logger.expect :silence, nil
-
-    app = app_with_silence(path: "/up", remote_ip: "127.0.0.1")
-
-    Rails.stub(:logger, mock_logger) do
-      app.call(Rack::MockRequest.env_for("http://example.com/up", "REMOTE_ADDR" => "127.0.0.1"))
-      app.call(Rack::MockRequest.env_for("http://example.com/up", "REMOTE_ADDR" => "127.0.0.2"))
-    end
-
-    assert mock_logger.verify
-  end
-
-  private
-    def app_with_silence(path:, remote_ip: nil)
-      Rails::Rack::SilenceRequest.new(lambda { |env| [200, env, "app"] }, path:, remote_ip:)
-    end
 end
