f686b75d012cedad15d3707b043b7c6347ae1614
Hartley McGuire <skipkayhil@gmail.com>
2024-09-06 18:23:19 +0000

    Use implication for skip_active_record/skip_solid (#52802)
    * Generator options should default to nil for unset
    
    The generator options are effectively tri-state, with true/false meaning
    set by the user and nil being unset. However, some of these options were
    defaulting to false, meaning they were being treated as set to false by
    the user.
    
    This commit fixes this issue by setting the values to nil by default
    instead of false so that they are properly "unset" by user.
    
    * Use implication for skip_active_record/skip_solid
    
    When skip_solid was added, it used the "old style" option implication
    where skip_*? methods delegate to all the options that should be
    implied. This has a few downsides such as making it unclear to users
    when conflicting options may be specified (for example,
    skip_active_record and no_skip_solid).
    
    This commit changes the skip_solid option to use the new style of
    implication so that error messages are better and the predicate method
    is simpler.
    
    Co-authored-by: Jon Rowe <hello@jonrowe.co.uk>
    
    ---------
    
    Co-authored-by: Jon Rowe <hello@jonrowe.co.uk>

--- railties/lib/rails/generators/app_base.rb
+++ railties/lib/rails/generators/app_base.rb
@@ -111,16 +111,16 @@ module Rails
         class_option :skip_ci,             type: :boolean, default: nil,
                                            desc: "Skip GitHub CI files"
 
-        class_option :skip_kamal,          type: :boolean, default: false,
+        class_option :skip_kamal,          type: :boolean, default: nil,
                                            desc: "Skip Kamal setup"
 
-        class_option :skip_solid,          type: :boolean, default: false,
+        class_option :skip_solid,          type: :boolean, default: nil,
                                            desc: "Skip Solid Cache & Queue setup"
 
         class_option :dev,                 type: :boolean, default: nil,
                                            desc: "Set up the #{name} with Gemfile pointing to your Rails checkout"
 
-        class_option :devcontainer,        type: :boolean, default: false,
+        class_option :devcontainer,        type: :boolean, default: nil,
                                            desc: "Generate devcontainer files"
 
         class_option :edge,                type: :boolean, default: nil,
@@ -206,7 +206,7 @@ module Rails
 
       OPTION_IMPLICATIONS = { # :nodoc:
         skip_active_job:     [:skip_action_mailer, :skip_active_storage],
-        skip_active_record:  [:skip_active_storage],
+        skip_active_record:  [:skip_active_storage, :skip_solid],
         skip_active_storage: [:skip_action_mailbox, :skip_action_text],
         skip_javascript:     [:skip_hotwire],
       }
@@ -432,7 +432,7 @@ module Rails
       end
 
       def skip_solid?
-        options[:skip_active_record] || options[:skip_solid]
+        options[:skip_solid]
       end
 
       class GemfileEntry < Struct.new(:name, :version, :comment, :options, :commented_out)
