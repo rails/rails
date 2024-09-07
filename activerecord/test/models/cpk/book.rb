e07bc2b27b8302cc7d4bb223a535f1243933ae03
Ryuta Kamizono <kamipo@gmail.com>
2024-09-07 14:27:23 +0900

    Merge pull request #52797 from fatkodima/fix-nested-attributes-for-cpk
    Fix updating nested attributes for models with composite primary keys

--- activerecord/test/models/cpk/book.rb
+++ activerecord/test/models/cpk/book.rb
@@ -10,6 +10,7 @@ module Cpk
     belongs_to :author, class_name: "Cpk::Author"
 
     has_many :chapters, foreign_key: [:author_id, :book_id]
+    accepts_nested_attributes_for :chapters
 
     before_destroy :prevent_destroy_if_set
 
