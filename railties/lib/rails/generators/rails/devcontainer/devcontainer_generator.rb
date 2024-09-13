2e5f9a84a3efdb6ffd034c5e631f4b58531eb3fc
Rafael Mendonça França <rafael@rubyonrails.org>
2024-09-13 15:39:57 -0300

    Merge pull request #52913 from koic/suppresss_rubocop_offense_after_rails_new_devcontainer
    Suppress a RuboCop offense after running `rails new` with `--devcontainer`

--- railties/lib/rails/generators/rails/devcontainer/devcontainer_generator.rb
+++ railties/lib/rails/generators/rails/devcontainer/devcontainer_generator.rb
@@ -148,7 +148,7 @@ module Rails
         end
 
         def system_test_configuration
-          optimize_indentation(<<-'RUBY', 2)
+          optimize_indentation(<<-'RUBY', 2).chomp
             if ENV["CAPYBARA_SERVER_PORT"]
               served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
 
