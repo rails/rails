cc94545147d5e32afdc504543a25d3097850826f
Andrew Novoselac <andrew.novoselac@shopify.com>
2024-09-14 17:30:03 -0400

    Do not include redis by default in dev container
    Rails 8 will use the solid gems by default for job queue and action cable, which do not depend on redis. So we won't include redis in the devcontainer by default. Redis will be included in the app is generated with the skip-solid option and uses active job or action cable. For existing apps, we won't use redis if either of the solid gems are in use.

--- railties/lib/rails/generators/rails/app/app_generator.rb
+++ railties/lib/rails/generators/rails/app/app_generator.rb
@@ -265,7 +265,7 @@ module Rails
     def devcontainer
       devcontainer_options = {
         database: options[:database],
-        redis: !(options[:skip_action_cable] && options[:skip_active_job]),
+        redis: options[:skip_solid] && !(options[:skip_action_cable] && options[:skip_active_job]),
         system_test: depends_on_system_test?,
         active_storage: !options[:skip_active_storage],
         dev: options[:dev],
