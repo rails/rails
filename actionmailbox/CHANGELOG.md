34e9d47d8e53786efe4b64e4513aa78eaea757b0
Rafael Mendonça França <rafael@rubyonrails.org>
2024-09-18 05:04:13 +0000

    Preparing for 8.0.0.alpha7 release

--- actionmailbox/CHANGELOG.md
+++ actionmailbox/CHANGELOG.md
@@ -1,3 +1,8 @@
+## Rails 8.0.0.alpha7 (September 18, 2024) ##
+
+*   No changes.
+
+
 ## Rails 8.0.0.alpha6 (September 18, 2024) ##
 
 *   No changes.
--- actiontext/lib/action_text/gem_version.rb
+++ actiontext/lib/action_text/gem_version.rb
@@ -12,7 +12,7 @@ module ActionText
     MAJOR = 8
     MINOR = 0
     TINY  = 0
-    PRE   = "alpha6"
+    PRE   = "alpha7"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
