b6ade9ed8591ad20e9942c3657c5619c4eb8a5c6
lorenzo farnararo <lorenzo.farnararo@suse.com>
2024-09-30 15:51:24 +0200

    Fix user generator to fit initial rubocop rules [FIX #53117]

--- railties/lib/rails/generators/rails/authentication/templates/app/models/user.rb
+++ railties/lib/rails/generators/rails/authentication/templates/app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
-  normalizes :email_address, with: -> e { e.strip.downcase }
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
