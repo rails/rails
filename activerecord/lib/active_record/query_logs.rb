89ce9f60a212d7c0156334fb680cdcbeac90b3a6
Jean Boussier <jean.boussier@gmail.com>
2024-10-02 11:49:06 -0400

    Merge pull request #53150 from fatkodima/fix-query_logs-source_location
    Fix `:source_location` query tag option to print full source locations

--- activerecord/lib/active_record/query_logs.rb
+++ activerecord/lib/active_record/query_logs.rb
@@ -112,7 +112,7 @@ module ActiveRecord
       if Thread.respond_to?(:each_caller_location)
         def query_source_location # :nodoc:
           Thread.each_caller_location do |location|
-            frame = LogSubscriber.backtrace_cleaner.clean_frame(location.path)
+            frame = LogSubscriber.backtrace_cleaner.clean_frame(location)
             return frame if frame
           end
           nil
