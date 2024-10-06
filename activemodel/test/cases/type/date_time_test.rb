2d41e86b7d39c9d991ec304193c2ec6e59758cda
Jean Boussier <jean.boussier@gmail.com>
2024-10-06 10:57:19 -0400

    Fix another Ruby 3.4 Hash#inspect failure

--- activemodel/test/cases/type/date_time_test.rb
+++ activemodel/test/cases/type/date_time_test.rb
@@ -33,7 +33,7 @@ module ActiveModel
       def test_hash_with_wrong_keys
         type = Type::DateTime.new
         error = assert_raises(ArgumentError) { type.cast(a: 1) }
-        assert_equal "Provided hash {:a=>1} doesn't contain necessary keys: [1, 2, 3]", error.message
+        assert_equal "Provided hash #{{ a: 1 }} doesn't contain necessary keys: [1, 2, 3]", error.message
       end
 
       private
