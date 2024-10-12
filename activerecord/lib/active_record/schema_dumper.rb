e403adf8720a77b9481eb6f4e4b3e39cc91356d5
John Duff <duff.john@gmail.com>
2024-10-12 09:56:00 -0400

    Sort table columns by name when dumping schema

--- activerecord/lib/active_record/schema_dumper.rb
+++ activerecord/lib/active_record/schema_dumper.rb
@@ -192,7 +192,7 @@ module ActiveRecord
           tbl.puts ", force: :cascade do |t|"
 
           # then dump all non-primary key columns
-          columns.each do |column|
+          columns.sort_by(&:name).each do |column|
             raise StandardError, "Unknown type '#{column.sql_type}' for column '#{column.name}'" unless @connection.valid_type?(column.type)
             next if column.name == pk
 
