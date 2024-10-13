fb16c1f1e02aa742be2866590041da4e6607b8a4
John Hawthorn <john@hawthorn.email>
2024-10-12 23:07:45 -0700

    Add missing require

--- activesupport/test/core_ext/object/to_query_test.rb
+++ activesupport/test/core_ext/object/to_query_test.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "uri"
 require_relative "../../abstract_unit"
 require "active_support/core_ext/object/to_query"
 require "active_support/core_ext/string/output_safety"
