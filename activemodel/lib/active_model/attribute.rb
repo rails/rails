399af7f3528deeb1b4fb96cd1c2c9b50ef52a43d
John Hawthorn <john@hawthorn.email>
2024-10-16 16:31:16 -0700

    Revert "ActiveModel::Attribute: elide dup for immutable types"

--- activemodel/lib/active_model/attribute.rb
+++ activemodel/lib/active_model/attribute.rb
@@ -96,14 +96,6 @@ module ActiveModel
       end
     end
 
-    def deep_dup # :nodoc:
-      if @type.mutable?
-        dup
-      else
-        self # If the underlying type is immutable we can get away with not duping
-      end
-    end
-
     def type_cast(*)
       raise NotImplementedError
     end
