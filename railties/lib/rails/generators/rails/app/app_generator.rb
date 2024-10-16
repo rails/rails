d3838c3bc3048b2027c51f7ab1b13008c5010ad3
Joé Dupuis <joe@dupuis.io>
2024-10-15 20:37:35 -0700

    Add support for using kamal inside of the generated devcontainer
    Kamal requires docker. The docker-in-docker devcontainer feature allow
    running docker inside the container.

--- railties/lib/rails/generators/rails/app/app_generator.rb
+++ railties/lib/rails/generators/rails/app/app_generator.rb
@@ -266,6 +266,7 @@ module Rails
       devcontainer_options = {
         database: options[:database],
         redis: options[:skip_solid] && !(options[:skip_action_cable] && options[:skip_active_job]),
+        kamal: !options[:skip_kamal],
         system_test: depends_on_system_test?,
         active_storage: !options[:skip_active_storage],
         dev: options[:dev],
