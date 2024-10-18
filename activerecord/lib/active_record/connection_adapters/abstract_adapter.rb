77eef9a3cf3aebb0e77504a51351f7b308f99eae
Alex Baldwin <abaldwintech@gmail.com>
2024-10-18 14:52:20 -0400

    PostgreSQL reload virtual columns on update via RETURNING clause

--- activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/abstract_adapter.rb
@@ -587,6 +587,10 @@ module ActiveRecord
         false
       end
 
+      def supports_update_returning?
+        false
+      end
+
       def supports_insert_on_duplicate_skip?
         false
       end
@@ -612,7 +616,11 @@ module ActiveRecord
       end
 
       def return_value_after_insert?(column) # :nodoc:
-        column.auto_populated?
+        column.auto_populated_on_insert?
+      end
+
+      def return_value_after_update?(column)
+        column.auto_populated_on_update?
       end
 
       def async_enabled? # :nodoc:
