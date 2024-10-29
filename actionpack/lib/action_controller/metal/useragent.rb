e6d10c77a7bb1c3d69e04834fd702c9825863d44
Ankur Patel <ankur@encoredevlabs.com>
2024-10-29 12:11:27 -0700

    Fix for Lighthouse getting a 406 page when it should be ignored as it is a bot

---
+++ actionpack/lib/action_controller/metal/useragent.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "useragent"
+
+# Monkey Patching UserAgent to handle Google PageSpeed Insights
+# Until fix in this PR is merged and gem is updated https://github.com/gshutler/useragent/pull/67
+class UserAgent
+  def bot?
+    # Google PageSpeed Insights adds "Chrome-Lighthouse" to the user agent
+    # https://stackoverflow.com/questions/16403295/what-is-the-name-of-the-google-pagespeed-user-agent
+    if detect_product("Chrome-Lighthouse")
+      true
+    else
+      original_bot?
+    end
+  end
+
+  private
+    alias_method :original_bot?, :bot?
+end
