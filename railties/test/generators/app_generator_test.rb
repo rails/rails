9dc752e70f818c8d41ec09d9b796e291e98000bc
Étienne Barrié <etienne.barrie@gmail.com>
2024-11-08 11:22:40 +0100

    Don't add bin/thrust if thruster is not in Gemfile

--- railties/test/generators/app_generator_test.rb
+++ railties/test/generators/app_generator_test.rb
@@ -304,6 +304,16 @@ class AppGeneratorTest < Rails::Generators::TestCase
     end
   end
 
+  def test_app_update_preserves_skip_thruster
+    run_generator [ destination_root, "--skip-thruster" ]
+
+    FileUtils.cd(destination_root) do
+      assert_no_changes -> { File.exist?("bin/thrust") } do
+        run_app_update
+      end
+    end
+  end
+
   def test_app_update_preserves_skip_test
     run_generator [ destination_root, "--skip-test" ]
 
