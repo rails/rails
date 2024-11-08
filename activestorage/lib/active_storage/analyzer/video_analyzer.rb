dc39ecb5d57e69f6b32ce73bf19665d524218387
Luis Muñoz <l.munoz.m@outlook.com>
2024-11-08 10:55:15 -0300

    Update video_analyzer.rb
    Force round(0) to result of encoded_with * display_height_scale into computed_height method. That for avoid the return of decimals when leave the method.

--- activestorage/lib/active_storage/analyzer/video_analyzer.rb
+++ activestorage/lib/active_storage/analyzer/video_analyzer.rb
@@ -27,131 +27,141 @@ module ActiveStorage
     end
 
     def metadata
-      { width: width, height: height, duration: duration, angle: angle, display_aspect_ratio: display_aspect_ratio, audio: audio?, video: video? }.compact
+      {
+        width: width,
+        height: height,
+        duration: duration,
+        angle: angle,
+        display_aspect_ratio: display_aspect_ratio,
+        audio: audio?,
+        video: video?,
+      }.compact
     end
 
     private
-      def width
-        if rotated?
-          computed_height || encoded_height
-        else
-          encoded_width
+
+    def width
+      rotated? ? computed_height || encoded_height : encoded_width
+    end
+
+    def height
+      rotated? ? encoded_width : computed_height || encoded_height
+    end
+
+    def duration
+      duration = video_stream['duration'] || container['duration']
+      Float(duration) if duration
+    end
+
+    def angle
+      if tags['rotate']
+        Integer(tags['rotate'])
+      elsif display_matrix && display_matrix['rotation']
+        Integer(display_matrix['rotation'])
+      end
+    end
+
+    def display_matrix
+      side_data.detect { |data| data['side_data_type'] == 'Display Matrix' }
+    end
+
+    def display_aspect_ratio
+      if descriptor = video_stream['display_aspect_ratio']
+        if terms = descriptor.split(':', 2)
+          numerator = Integer(terms[0])
+          denominator = Integer(terms[1])
+
+          [numerator, denominator] unless numerator == 0
         end
       end
+    end
 
-      def height
-        if rotated?
-          encoded_width
-        else
-          computed_height || encoded_height
-        end
+    def rotated?
+      angle == 90 || angle == 270 || angle == -90 || angle == -270
+    end
+
+    def audio?
+      audio_stream.present?
+    end
+
+    def video?
+      video_stream.present?
+    end
+
+    def computed_height
+      if encoded_width && display_height_scale
+        (encoded_width * display_height_scale).round(0)
       end
+    end
 
-      def duration
-        duration = video_stream["duration"] || container["duration"]
-        Float(duration) if duration
+    def encoded_width
+      @encoded_width ||= Float(video_stream['width']) if video_stream['width']
+    end
+
+    def encoded_height
+      @encoded_height ||= Float(video_stream['height']) if video_stream[
+        'height'
+      ]
+    end
+
+    def display_height_scale
+      @display_height_scale ||=
+        Float(display_aspect_ratio.last) /
+          display_aspect_ratio.first if display_aspect_ratio
+    end
+
+    def tags
+      @tags ||= video_stream['tags'] || {}
+    end
+
+    def side_data
+      @side_data ||= video_stream['side_data_list'] || {}
+    end
+
+    def video_stream
+      @video_stream ||=
+        streams.detect { |stream| stream['codec_type'] == 'video' } || {}
+    end
+
+    def audio_stream
+      @audio_stream ||=
+        streams.detect { |stream| stream['codec_type'] == 'audio' } || {}
+    end
+
+    def streams
+      probe['streams'] || []
+    end
+
+    def container
+      probe['format'] || {}
+    end
+
+    def probe
+      @probe ||= download_blob_to_tempfile { |file| probe_from(file) }
+    end
+
+    def probe_from(file)
+      instrument(File.basename(ffprobe_path)) do
+        IO.popen(
+          [
+            ffprobe_path,
+            '-print_format',
+            'json',
+            '-show_streams',
+            '-show_format',
+            '-v',
+            'error',
+            file.path,
+          ],
+        ) { |output| JSON.parse(output.read) }
       end
+    rescue Errno::ENOENT
+      logger.info "Skipping video analysis because ffprobe isn't installed"
+      {}
+    end
 
-      def angle
-        if tags["rotate"]
-          Integer(tags["rotate"])
-        elsif display_matrix && display_matrix["rotation"]
-          Integer(display_matrix["rotation"])
-        end
-      end
-
-      def display_matrix
-        side_data.detect { |data| data["side_data_type"] == "Display Matrix" }
-      end
-
-      def display_aspect_ratio
-        if descriptor = video_stream["display_aspect_ratio"]
-          if terms = descriptor.split(":", 2)
-            numerator   = Integer(terms[0])
-            denominator = Integer(terms[1])
-
-            [numerator, denominator] unless numerator == 0
-          end
-        end
-      end
-
-      def rotated?
-        angle == 90 || angle == 270 || angle == -90 || angle == -270
-      end
-
-      def audio?
-        audio_stream.present?
-      end
-
-      def video?
-        video_stream.present?
-      end
-
-      def computed_height
-        if encoded_width && display_height_scale
-          encoded_width * display_height_scale
-        end
-      end
-
-      def encoded_width
-        @encoded_width ||= Float(video_stream["width"]) if video_stream["width"]
-      end
-
-      def encoded_height
-        @encoded_height ||= Float(video_stream["height"]) if video_stream["height"]
-      end
-
-      def display_height_scale
-        @display_height_scale ||= Float(display_aspect_ratio.last) / display_aspect_ratio.first if display_aspect_ratio
-      end
-
-      def tags
-        @tags ||= video_stream["tags"] || {}
-      end
-
-      def side_data
-        @side_data ||= video_stream["side_data_list"] || {}
-      end
-
-      def video_stream
-        @video_stream ||= streams.detect { |stream| stream["codec_type"] == "video" } || {}
-      end
-
-      def audio_stream
-        @audio_stream ||= streams.detect { |stream| stream["codec_type"] == "audio" } || {}
-      end
-
-      def streams
-        probe["streams"] || []
-      end
-
-      def container
-        probe["format"] || {}
-      end
-
-      def probe
-        @probe ||= download_blob_to_tempfile { |file| probe_from(file) }
-      end
-
-      def probe_from(file)
-        instrument(File.basename(ffprobe_path)) do
-          IO.popen([ ffprobe_path,
-            "-print_format", "json",
-            "-show_streams",
-            "-show_format",
-            "-v", "error",
-            file.path
-          ]) do |output|
-            JSON.parse(output.read)
-          end
-        end
-      rescue Errno::ENOENT
-        logger.info "Skipping video analysis because ffprobe isn't installed"
-        {}
-      end
-
-      def ffprobe_path
-        ActiveStorage.paths[:ffprobe] || "ffprobe"
-      end
+    def ffprobe_path
+      ActiveStorage.paths[:ffprobe] || 'ffprobe'
+    end
   end
 end
