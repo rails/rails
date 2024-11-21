shunmugadigialert/ruby6

Allow passing Symbols as aliases in Arel
Table names, column names, and table aliases all allow symbols. This brings back symbol support for `#as` on nodes for consistency with these other methods.

activerecord/lib/arel/alias_predication.rb
@@ -3,6 +3,8 @@
 module Arel # :nodoc: all
   module AliasPredication
     def as(other)
+      other = other.to_s if other.is_a?(Symbol)
+
       Nodes::As.new self, Nodes::SqlLiteral.new(other)
     end
   end

rails/rails

activerecord/lib/arel/alias_predication.rb
@@ -3,6 +3,8 @@
 module Arel # :nodoc: all
   module AliasPredication
     def as(other)
+      other = other.name if other.is_a?(Symbol)
+
       Nodes::As.new self, Nodes::SqlLiteral.new(other, retryable: true)
     end
   end