b4fdd352204e9dc60f5e79772c9188a965f5c5d1
fatkodima <fatkodima123@gmail.com>
2024-12-01 19:10:45 +0200

    Fix add_unique_constraint/add_check_constraint/add_foreign_key to be revertible when given invalid options
    Co-authored-by: Aidan Haran <aidanharan@yahoo.com>

--- activerecord/lib/active_record/connection_adapters/abstract/schema_definitions.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/schema_definitions.rb
@@ -159,6 +159,8 @@ module ActiveRecord
       end
 
       def defined_for?(to_table: nil, validate: nil, **options)
+        options = options.slice(*self.options.keys)
+
         (to_table.nil? || to_table.to_s == self.to_table) &&
           (validate.nil? || validate == self.options.fetch(:validate, validate)) &&
           options.all? { |k, v| Array(self.options[k]).map(&:to_s) == Array(v).map(&:to_s) }
@@ -185,6 +187,8 @@ module ActiveRecord
       end
 
       def defined_for?(name:, expression: nil, validate: nil, **options)
+        options = options.slice(*self.options.keys)
+
         self.name == name.to_s &&
           (validate.nil? || validate == self.options.fetch(:validate, validate)) &&
           options.all? { |k, v| self.options[k].to_s == v.to_s }
