3726030c9a7829033e9acfdb4114f7193b513b8d
Xavier Noria <fxn@hashref.com>
2024-12-03 00:23:43 +0100

    Revert "Add missing require"
    There is a circular require going on and the warning breaks the build. Reverting
    for now to get the fix the build.
    
    This reverts commit dae24cc05ae32389613f07ccd6414e6c7c88467a.

--- activesupport/lib/active_support/core_ext/date_and_time/calculations.rb
+++ activesupport/lib/active_support/core_ext/date_and_time/calculations.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "active_support/core_ext/object/try"
-require "active_support/core_ext/time/calculations"
 require "active_support/core_ext/date_time/conversions"
 
 module DateAndTime
