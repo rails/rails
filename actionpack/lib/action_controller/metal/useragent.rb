dda49bb37a6f15cd76dcc2cea600fe5154afc744
Ankur Patel <ankur@encoredevlabs.com>
2024-12-03 08:32:11 -0800

    Using refinement

--- actionpack/lib/action_controller/metal/useragent.rb
+++ actionpack/lib/action_controller/metal/useragent.rb
@@ -2,19 +2,21 @@
 
 require "useragent"
 
-# Monkey Patching UserAgent to handle Google PageSpeed Insights
-# Until fix in this PR is merged and gem is updated https://github.com/gshutler/useragent/pull/67
-class UserAgent
-  def bot?
-    # Google PageSpeed Insights adds "Chrome-Lighthouse" to the user agent
-    # https://stackoverflow.com/questions/16403295/what-is-the-name-of-the-google-pagespeed-user-agent
-    if detect_product("Chrome-Lighthouse")
-      true
-    else
-      original_bot?
+module PatchedUserAgent
+  # Refining UserAgent to handle Google PageSpeed Insights
+  # Until fix in this PR is merged and gem is updated https://github.com/gshutler/useragent/pull/67
+  refine UserAgent do
+    def bot?
+      # Google PageSpeed Insights adds "Chrome-Lighthouse" to the user agent
+      # https://stackoverflow.com/questions/16403295/what-is-the-name-of-the-google-pagespeed-user-agent
+      if detect_product("Chrome-Lighthouse")
+        true
+      else
+        original_bot?
+      end
     end
+  
+    private
+      alias_method :original_bot?, :bot?
   end
-
-  private
-    alias_method :original_bot?, :bot?
 end
