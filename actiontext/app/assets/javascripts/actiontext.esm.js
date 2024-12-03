1d8ad5ab13c1d39ad08ae4f5d82bbbab74b66a33
Sean Doyle <sean.p.doyle24@gmail.com>
2024-12-03 15:59:44 -0500

    Action Text: Dispatch Active Storage events with `id` and `file`
    Follow-up to [#52680][]
    
    Adjusts the Action Text-dispatched [Active Storage Direct Upload
    JavaScript Events][] to include both `id` and `file` properties as
    documented in the current [Action Text Attachment Direct Upload
    JavaScript Events][]. In addition to those properties, there were two
    events missing from the list:
    
    * `direct-upload:before-blob-request`, `{id, file, xhr}`, Before making
      a request to your application for direct upload metadata.
    * `direct-upload:before-storage-request`, `{id, file, xhr}`, Before
      making a request to store a file.
    
    The code change mimics the style of the Active Storage
    [DirectUploadController][] implementation.
    
    Prior to this commit, the documentation mentioned that an `<input>`
    element would be the event target. In reality, the `AttachmentUpload`
    JavaScript class dispatches events treating the `<trix-editor>` element
    as its target.
    
    This commit changes the documentation to account for that. Alongside the
    code change, this commit expands upon the existing guides documentation
    to also mention the `attachment` property that references a
    [Attachment][] instance.
    
    To account for both _new events_ and _new properties_ in the
    `event.detail` object, this commit includes expanded system test
    coverage asserting that events are dispatched as described .
    
    [#52680]: https://github.com/rails/rails/pull/52680
    [Active Storage Direct Upload JavaScript Events]: https://guides.rubyonrails.org/v8.0.0/active_storage_overview.html#direct-upload-javascript-events
    [Action Text Attachment Direct Upload JavaScript Events]: https://guides.rubyonrails.org/v8.0.0/action_text_overview.html#attachment-direct-upload-javascript-events
    [DirectUploadController]: https://github.com/rails/rails/blob/v8.0.0/activestorage/app/javascript/activestorage/direct_upload_controller.js#L44-L48
    [Attachment]: https://github.com/basecamp/trix/?tab=readme-ov-file#inserting-a-file

--- actiontext/app/assets/javascripts/actiontext.esm.js
+++ actiontext/app/assets/javascripts/actiontext.esm.js
@@ -850,12 +850,21 @@ class AttachmentUpload {
     this.attachment = attachment;
     this.element = element;
     this.directUpload = new DirectUpload(attachment.file, this.directUploadUrl, this);
+    this.dispatch("initialize");
   }
   start() {
     this.directUpload.create(this.directUploadDidComplete.bind(this));
     this.dispatch("start");
   }
+  directUploadWillCreateBlobWithXHR(xhr) {
+    this.dispatch("before-blob-request", {
+      xhr: xhr
+    });
+  }
   directUploadWillStoreFileWithXHR(xhr) {
+    this.dispatch("before-storage-request", {
+      xhr: xhr
+    });
     xhr.upload.addEventListener("progress", (event => {
       const progress = event.loaded / event.total * 100;
       this.attachment.setUploadProgress(progress);
@@ -882,6 +891,8 @@ class AttachmentUpload {
   }
   dispatch(name, detail = {}) {
     detail.attachment = this.attachment;
+    detail.file = this.directUpload.file;
+    detail.id = this.directUpload.id;
     return dispatchEvent(this.element, `direct-upload:${name}`, {
       detail: detail
     });
--- actiontext/test/system/rich_text_editor_test.rb
+++ actiontext/test/system/rich_text_editor_test.rb
@@ -4,19 +4,95 @@ require "application_system_test_case"
 
 class ActionText::RichTextEditorTest < ApplicationSystemTestCase
   test "attaches and uploads image file" do
-    image_file = file_fixture("racecar.jpg")
-
     visit new_message_url
-    attach_file image_file do
-      click_button "Attach Files"
-    end
+    attach_rich_text_file file_fixture("racecar.jpg")
     within :rich_text_area do
-      assert_selector :element, "img", src: %r{/rails/active_storage/blobs/redirect/.*/#{image_file.basename}\Z}
+      assert_selector :element, "img", src: %r{/rails/active_storage/blobs/redirect/.*/racecar.jpg\Z}
     end
     click_button "Create Message"
 
     within class: "trix-content" do
-      assert_selector :element, "img", src: %r{/rails/active_storage/representations/redirect/.*/#{image_file.basename}\Z}
+      assert_selector :element, "img", src: %r{/rails/active_storage/representations/redirect/.*/racecar.jpg\Z}
     end
+  end
+
+  test "dispatches direct-upload:-prefixed events when uploading a File" do
+    visit new_message_url
+    events = capture_direct_upload_events do
+      attach_rich_text_file file_fixture("racecar.jpg")
+
+      assert_selector :element, "img", src: %r{/rails/active_storage/blobs/redirect/.*/racecar.jpg\Z}
+    end
+
+    assert_equal 1, ActiveStorage::Blob.where(filename: "racecar.jpg").count
+    assert_direct_upload_event events[0], "initialize"
+    assert_direct_upload_event events[1], "start"
+    assert_direct_upload_event events[2], "before-blob-request", xhr: "XMLHttpRequest"
+    assert_direct_upload_event events[3], "before-storage-request", xhr: "XMLHttpRequest"
+    assert_direct_upload_event events[4], "progress", progress: "Number"
+    assert_direct_upload_event events[5], "end"
+  end
+
+  test "dispatches direct-upload:error event when uploading fails" do
+    visit new_message_url
+    events = capture_direct_upload_events offline: true do
+      accept_alert 'Error creating Blob for "racecar.jpg". Status: 0' do
+        attach_rich_text_file file_fixture("racecar.jpg")
+      end
+
+      assert_no_selector :element, "img", src: /racecar.jpg\Z/
+    end
+
+    assert_empty ActiveStorage::Blob.where(filename: "racecar.jpg")
+    assert_direct_upload_event events.last, "error", error: "String"
+  end
+
+  def assert_direct_upload_event(event, name, target: find(:rich_text_area), **detail)
+    detail.with_defaults!(id: "Number", file: "File", attachment: "ManagedAttachment")
+
+    assert_equal({ type: "direct-upload:#{name}", target: target, detail: detail }, event)
+  end
+
+  def attach_rich_text_file(*args)
+    attach_file(*args) { click_button "Attach Files" }
+  end
+
+  def capture_direct_upload_events(offline: false, &block)
+    event_names = %w[
+      direct-upload:initialize
+      direct-upload:start
+      direct-upload:before-blob-request
+      direct-upload:before-storage-request
+      direct-upload:progress
+      direct-upload:error
+      direct-upload:end
+    ]
+    execute_script <<~JS, *event_names
+      window.capturedEvents = []
+
+      for (const eventName of arguments) {
+        addEventListener(eventName, ({ target, type, detail }) => {
+          const serialized = {}
+
+          for (const name in detail) {
+            serialized[name] = detail[name].constructor.name
+          }
+
+          window.capturedEvents.push({ target, type, detail: serialized })
+        }, { once: true })
+      }
+    JS
+
+    while_offline(offline, &block)
+
+    evaluate_script("window.capturedEvents").map(&:deep_symbolize_keys!)
+  end
+
+  def while_offline(value, &block)
+    # raises unknown error: network conditions must be set before it can be retrieved
+    # unless set to an initial value
+    page.driver.browser.network_conditions = { offline: !value }
+
+    page.driver.browser.with(network_conditions: { offline: value }, &block)
   end
 end
