9fe84c737b5649798787e6787981b57948820341
Chris Oliver <excid3@gmail.com>
2024-12-09 12:02:17 -0600

    Support <picture> element in guides

--- guides/rails_guides/markdown/renderer.rb
+++ guides/rails_guides/markdown/renderer.rb
@@ -67,6 +67,8 @@ module RailsGuides
         elsif /^(TIP|IMPORTANT|CAUTION|WARNING|NOTE|INFO|TODO)[.:]/.match?(text)
           convert_notes(text)
         elsif text.include?("DO NOT READ THIS FILE ON GITHUB")
+        elsif text.match?(/^<picture>/)
+          text
         elsif text =~ /^\[<sup>(\d+)\]:<\/sup> (.+)$/
           linkback = %(<a href="#footnote-#{$1}-ref"><sup>#{$1}</sup></a>)
           %(<p class="footnote" id="footnote-#{$1}">#{linkback} #{$2}</p>)
