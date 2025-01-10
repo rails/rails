25f9abb02180e38e81e9b85f29ff57461a2cf7f7
Matt Pasquini <matt.pasquini@coupa.com>
2025-01-10 11:21:40 -0800

    Require crc digest classes on use in ActiveStorage::Checksum

--- activestorage/app/models/active_storage/checksum.rb
+++ activestorage/app/models/active_storage/checksum.rb
@@ -2,14 +2,12 @@
 
 class ActiveStorage::Checksum
   require "openssl"
-  require "digest/crc32"
-  require "digest/crc32c"
-  require "digest/crc64"
+
+  attr_accessor :crc32, :crc32c, :crc64
 
   class << self
     # Returns class of integrity checksum algorithm
     #   ActiveStorage::Checksum.for(:MD5) # => OpenSSL::Digest::MD5
-
     def for(checksum_algorithm)
       case checksum_algorithm
       when :MD5
@@ -19,11 +17,29 @@ class ActiveStorage::Checksum
       when :SHA256
         OpenSSL::Digest::SHA256
       when :CRC32
-        Digest::CRC32
+        return @crc32 if @crc32
+        begin
+          require "digest/crc32"
+        rescue LoadError
+          raise LoadError, 'digest/crc32 not loaded. Please add `gem "digest-crc"` to your gemfile.'
+        end
+        @crc32 = Digest::CRC32
       when :CRC32c
-        Digest::CRC32c
+        return @crc32c if @crc32c
+        begin
+          require "digest/crc32c"
+        rescue LoadError
+          raise LoadError, 'digest/crc32c not loaded. Please add `gem "digest-crc"` to your gemfile.'
+        end
+        @crc32c = Digest::CRC32c
       when :CRC64
-        Digest::CRC64
+        return @crc64 if @crc64
+        begin
+          require "digest/crc64"
+        rescue LoadError
+          raise LoadError, 'digest/crc64 not loaded. Please add `gem "digest-crc"` to your gemfile.'
+        end
+        @crc64 = Digest::CRC64
       end
     end
   end
