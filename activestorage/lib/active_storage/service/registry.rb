9fa48de2466c0805dcd453ed71ea180f7918b086
Matt Pasquini <matt.pasquini@coupa.com>
2025-01-21 18:37:07 -0800

    Additional refactoring/cleanup

--- activestorage/lib/active_storage/service/registry.rb
+++ activestorage/lib/active_storage/service/registry.rb
@@ -8,8 +8,6 @@ module ActiveStorage
     end
 
     def fetch(name)
-      return unless name
-
       services.fetch(name.to_sym) do |key|
         if configurations.include?(key)
           services[key] = configurator.build(key)
