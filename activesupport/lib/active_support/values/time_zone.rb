9fe9795756ab6231b2243f6afe7e06691a194366
Schneems <richard.schneeman+foo@gmail.com>
2025-01-29 11:21:29 -0600

    Refactor timezone exception logic into a method
    This centralizes the logic to allow for both exception-raising code and non-exception based lookups to use the same logic by introducing a new private API (as marked via nodoc).

--- activesupport/lib/active_support/values/time_zone.rb
+++ activesupport/lib/active_support/values/time_zone.rb
@@ -230,7 +230,18 @@ module ActiveSupport
       # timezone to find. (The first one with that offset will be returned.)
       # Returns +nil+ if no such time zone is known to the system.
       def [](arg)
-        case arg
+        find!(arg, raise_error: false)
+      end
+
+
+      # Private API, do not use outside of ActiveSupport, it is unstable
+      #
+      # Finda timezone, if `raise_error` is truthy, an
+      # exception is raised.
+      #
+      # The error message can differ based on the class of `self`.
+      def find!(arg, raise_error: true) # :nodoc:
+        value = case arg
         when self
           arg
         when String
@@ -246,6 +257,12 @@ module ActiveSupport
           all.find { |z| z.utc_offset == arg.to_i }
         else
           raise ArgumentError, "invalid argument to TimeZone[]: #{arg.inspect}"
+        end
+
+        if !value && raise_error
+          raise(ArgumentError, "Invalid Timezone: #{time_zone}")
+        else
+          value
         end
       end
 
