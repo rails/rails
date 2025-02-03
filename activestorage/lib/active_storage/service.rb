aa24a6a6d9e29243905d1688a4d6bfa203e29fc9
Matt Pasquini <matt.pasquini@coupa.com>
2025-02-03 11:56:28 -0800

    Make ActiveStorage::Service responsible for checksums

--- activestorage/lib/active_storage/service.rb
+++ activestorage/lib/active_storage/service.rb
@@ -148,6 +148,31 @@ module ActiveStorage
       @public
     end
 
+    def base64digest(io)
+      checksum_implementation.base64digest(io)
+    end
+
+    def file(file)
+      checksum_implementation.file(file).base64digest
+    end
+
+    def compute_checksum_in_chunks(io)
+      raise ArgumentError, "io must be rewindable" unless io.respond_to?(:rewind)
+
+      checksum_implementation.new.tap do |checksum|
+        read_buffer = "".b
+        while io.read(5.megabytes, read_buffer)
+          checksum << read_buffer
+        end
+
+        io.rewind
+      end.base64digest
+    end
+
+    def checksum_implementation
+      ActiveStorage.checksum_implementation
+    end
+
     private
       def private_url(key, expires_in:, filename:, disposition:, content_type:, **)
         raise NotImplementedError
