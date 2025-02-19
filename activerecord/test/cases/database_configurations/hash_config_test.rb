d21a9c10628430ae0de4af801e896a50e89a1743
ChaelCodes <chaelcodes@gmail.com>
2025-02-19 21:24:52 +0000

    Adjust test to use a low keepalive value.

--- activerecord/test/cases/database_configurations/hash_config_test.rb
+++ activerecord/test/cases/database_configurations/hash_config_test.rb
@@ -143,9 +143,9 @@ module ActiveRecord
         assert_equal 60.0, config.reaping_frequency
       end
 
-      def test_reaping_frequency_is_reduced_for_low_idle_timeout
-        config = HashConfig.new("default_env", "primary", idle_timeout: "5", adapter: "abstract")
-        assert_equal 60.0, config.reaping_frequency
+      def test_reaping_frequency_is_reduced_for_low_keepalive
+        config = HashConfig.new("default_env", "primary", keepalive: "15", adapter: "abstract")
+        assert_equal 15.0, config.reaping_frequency
       end
 
       def test_idle_timeout_default_when_nil
