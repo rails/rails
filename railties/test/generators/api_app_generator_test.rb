c0e397d3d8ec3c45222e1a6b7b9013b923d23114
David Heinemeier Hansson <david@37signals.com>
2025-03-05 14:18:35 +0100

    Include in api apps too

--- railties/test/generators/api_app_generator_test.rb
+++ railties/test/generators/api_app_generator_test.rb
@@ -163,6 +163,7 @@ class ApiAppGeneratorTest < Rails::Generators::TestCase
         bin/setup
         config/application.rb
         config/boot.rb
+        config/bundle-audit.yml
         config/cable.yml
         config/environment.rb
         config/environments
