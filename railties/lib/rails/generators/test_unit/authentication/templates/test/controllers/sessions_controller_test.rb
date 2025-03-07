69daa202dba249c2bc8ef33731b1a40cc4748b19
David Heinemeier Hansson <david@hey.com>
2025-03-07 18:04:23 +0100

    Use paths for everything

--- railties/lib/rails/generators/test_unit/authentication/templates/test/controllers/sessions_controller_test.rb
+++ railties/lib/rails/generators/test_unit/authentication/templates/test/controllers/sessions_controller_test.rb
@@ -4,30 +4,30 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   setup { @user = User.take }
 
   test "new" do
-    get new_session_url
+    get new_session_path
     assert_response :success
   end
 
   test "create with valid credentials" do
-    post session_url, params: { email_address: @user.email_address, password: "password" }
+    post session_path, params: { email_address: @user.email_address, password: "password" }
 
-    assert_redirected_to root_url
+    assert_redirected_to root_path
     assert cookies[:session_id]
   end
 
   test "create with invalid credentials" do
-    post session_url, params: { email_address: @user.email_address, password: "wrong" }
+    post session_path, params: { email_address: @user.email_address, password: "wrong" }
 
-    assert_redirected_to new_session_url
+    assert_redirected_to new_session_path
     assert_nil cookies[:session_id]
   end
 
   test "destroy" do
     sign_in_as(User.take)
 
-    delete session_url
+    delete session_path
 
-    assert_redirected_to new_session_url
+    assert_redirected_to new_session_path
     assert_empty cookies[:session_id]
   end
 end
