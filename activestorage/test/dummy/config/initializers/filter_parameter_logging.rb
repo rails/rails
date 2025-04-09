7f9adbf565a14368453e7a76c1175b689d4e1db1
Steve Polito <stevepolito@hey.com>
2025-04-09 16:23:25 -0400

    Use consistent filter parameter values across codebase
    In #52413 we introduced `cvv` and `cvc` as parameters to filter.
    
    This commit simply updates other areas in the codebase to do the same.

--- activestorage/test/dummy/config/initializers/filter_parameter_logging.rb
+++ activestorage/test/dummy/config/initializers/filter_parameter_logging.rb
@@ -4,5 +4,5 @@
 # sensitive information. See the ActiveSupport::ParameterFilter documentation for supported
 # notations and behaviors.
 Rails.application.config.filter_parameters += [
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc
 ]
