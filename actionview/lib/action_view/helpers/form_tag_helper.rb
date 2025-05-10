f392c75cd847784b759e08923e4c3c87807d5d00
Jean Boussier <jean.boussier@gmail.com>
2025-05-10 14:56:08 +0200

    Merge pull request #55037 from Earlopain/cgi-escape-3.5
    Only load from `cgi` what is required for Ruby 3.5 compatibility

--- actionview/lib/action_view/helpers/form_tag_helper.rb
+++ actionview/lib/action_view/helpers/form_tag_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-require "cgi"
+require "cgi/escape"
+require "cgi/util" if RUBY_VERSION < "3.5"
 require "action_view/helpers/content_exfiltration_prevention_helper"
 require "action_view/helpers/url_helper"
 require "action_view/helpers/text_helper"
