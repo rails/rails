5508133d93cb6d0edc8e4ce999ff68009786aceb
Xavier Noria <fxn@hashref.com>
2025-05-27 16:04:10 +0200

    Initial mofications

--- activerecord/lib/active_record/associations/builder/has_many.rb
+++ activerecord/lib/active_record/associations/builder/has_many.rb
@@ -9,9 +9,17 @@ module ActiveRecord::Associations::Builder # :nodoc:
     def self.valid_options(options)
       valid = super + [:counter_cache, :join_table, :index_errors, :as, :through]
       valid += [:foreign_type] if options[:as]
-      valid += [:source, :source_type, :disable_joins] if options[:through]
+      valid += [:source, :source_type, :disable_joins, :_parent_reflection] if options[:through]
       valid += [:ensuring_owner_was] if options[:dependent] == :destroy_async
       valid
+    end
+
+    def self.internal_options(options)
+      if options[:through]
+        super + [:_parent_reflection]
+      else
+        super
+      end
     end
 
     def self.valid_dependent_options
