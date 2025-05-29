37a4e65bde6c967b298ac94b6ed3bf74f595fea1
Ridhwana <Ridhwana.Khan16@gmail.com>
2025-05-29 17:34:05 +0200

    Update activerecord/lib/active_record/connection_adapters/postgresql/schema_definitions.rb
    Co-authored-by: Petrik de Heus <petrik@deheus.net>

--- activerecord/lib/active_record/connection_adapters/postgresql/schema_definitions.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/schema_definitions.rb
@@ -273,7 +273,7 @@ module ActiveRecord
         #     end
         #   end
         #
-        # You can also create the enum and add it to an existing table:
+        # The enum can also be added it to an existing table:
         #
         #   # db/migrate/20230113024409_add_status_to_articles.rb
         #   def change
