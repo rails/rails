shunmugadigialert/ruby6

Introduce `alias_association` feature
Rails 7.1 deprecated using `alias_attribute` for anything other than
an attribute. Many applications end up using `alias_attribute` to
alias associations and removing such usages would be challenging
in many cases. Recognizing this, Rails introduces a new feature to
alias associations called `alias_association`.

activerecord/lib/active_record/relation/query_methods.rb
@@ -1514,10 +1514,10 @@ module ActiveRecord
         when Hash
           opts = opts.transform_keys do |key|
             if key.is_a?(Array)
-              key.map { |k| klass.attribute_aliases[k.to_s] || k.to_s }
+              key.map { |k| klass.querying_aliases[k.to_s] || k.to_s }
             else
               key = key.to_s
-              klass.attribute_aliases[key] || key
+              klass.querying_aliases[key] || key
             end
           end
           references = PredicateBuilder.references(opts)

rails/rails

activerecord/lib/active_record/relation/query_methods.rb
@@ -1635,10 +1635,10 @@ module ActiveRecord
         when Hash
           opts = opts.transform_keys do |key|
             if key.is_a?(Array)
-              key.map { |k| model.attribute_aliases[k.to_s] || k.to_s }
+              key.map { |k| model.querying_aliases[k.to_s] || k.to_s }
             else
               key = key.to_s
-              model.attribute_aliases[key] || key
+              model.querying_aliases[key] || key
             end
           end
           references = PredicateBuilder.references(opts)