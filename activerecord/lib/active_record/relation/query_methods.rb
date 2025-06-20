4360b2b5f38f68afdedc637422cdd3eedc3ad14b
Ryuta Kamizono <kamipo@gmail.com>
2025-06-20 13:58:29 +0900

    Merge pull request #55213 from Yuhi-Sato/fix-typo-with-recursive-example
    [ci skip] Fix typo in with_recursive example

--- activerecord/lib/active_record/relation/query_methods.rb
+++ activerecord/lib/active_record/relation/query_methods.rb
@@ -510,7 +510,7 @@ module ActiveRecord
     #   # WITH RECURSIVE post_and_replies AS (
     #   #   (SELECT * FROM posts WHERE id = 42)
     #   #   UNION ALL
-    #   #   (SELECT * FROM posts JOIN posts_and_replies ON posts.in_reply_to_id = posts_and_replies.id)
+    #   #   (SELECT * FROM posts JOIN post_and_replies ON posts.in_reply_to_id = post_and_replies.id)
     #   # )
     #   # SELECT * FROM posts
     #
