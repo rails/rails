dc000b1e6d7433003e4dc8a761d49650447abed5
git9087 <githubroma96@gmail.com>
2025-07-01 09:22:27 -0300

    Update test_helper.rb

--- test_helper.rb
+++ test_helper.rb
@@ -60,36 +60,34 @@
     #       part.body "<h1>Please join us for a party at Bag End</h1>"
     #     end
     #   end
-    def create_inbound_email_from_mail(status: :processing, **mail_options, &block)
+     create_inbound_email_from_mail(status: :processing, **mail_options, &block)
       mail = Mail.new(mail_options, &block)
       # Bcc header is not encoded by default
       mail[:bcc].include_in_headers = true if mail[:bcc]
 
       create_inbound_email_from_source mail.to_s, status: status
-    end
+    
 
     # Create an InboundEmail using the raw rfc822 +source+ as text.
-    def create_inbound_email_from_source(source, status: :processing)
+     create_inbound_email_from_source(source, status: :processing)
       ActionMailbox::InboundEmail.create_and_extract_message_id! source, status: status
-    end
+    
 
 
     # Create an InboundEmail from fixture using the same arguments as create_inbound_email_from_fixture
     # and immediately route it to processing.
-    def receive_inbound_email_from_fixture(*args)
+     receive_inbound_email_from_fixture(*args)
       create_inbound_email_from_fixture(*args).tap(&:route)
-    end
+    
 
     # Create an InboundEmail using the same options or block as
     # create_inbound_email_from_mail, then immediately route it for processing.
-    def receive_inbound_email_from_mail(**kwargs, &block)
+     receive_inbound_email_from_mail(**kwargs, &block)
       create_inbound_email_from_mail(**kwargs, &block).tap(&:route)
-    end
+    
 
     # Create an InboundEmail using the same arguments as create_inbound_email_from_source and immediately route it
     # to processing.
-    def receive_inbound_email_from_source(*args)
+     receive_inbound_email_from_source(*args)
       create_inbound_email_from_source(*args).tap(&:route)
-    end
-  end
-end
+    
