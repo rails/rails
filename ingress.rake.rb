a4b03ca839013dfed6a5357407f76fc3af08a5d2
git9087 <githubroma96@gmail.com>
2025-07-01 09:13:32 -0300

    Update ingress.rake.rb

--- ingress.rake.rb
+++ ingress.rake.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-namespace :action_mailbox do
-  namespace :ingress do
-    task :environment do
-      require "active_support"
-      require "active_support/core_ext/object/blank"
-      require "action_mailbox/relayer"
-    end
+namespace :action_mailbox 
+  namespace :ingress 
+    task :environment 
+       "active_support"
+       "active_support/core_ext/object/blank"
+       "action_mailbox/relayer"
+    
 
     desc "Relay an inbound email from Exim to Action Mailbox (URL and INGRESS_PASSWORD required)"
     task exim: "action_mailbox:ingress:environment" do
       url, password = ENV.values_at("URL", "INGRESS_PASSWORD")
 
-      if url.blank? || password.blank?
+         url.blank? || password.blank?
         print "URL and INGRESS_PASSWORD are required"
         exit 64 # EX_USAGE
       end
