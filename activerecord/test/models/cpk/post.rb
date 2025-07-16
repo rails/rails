shunmugadigialert/ruby6

Fix inline habtm fixtures for tables with composite primary keys

activerecord/test/models/cpk/post.rb
@@ -4,5 +4,7 @@ module Cpk
   class Post < ActiveRecord::Base
     self.table_name = :cpk_posts
     has_many :comments, class_name: "Cpk::Comment", query_constraints: %i[commentable_title commentable_author], as: :commentable
+    has_many :posts_tags, class_name: "Cpk::PostsTag", query_constraints: %i[post_title post_author]
+    has_many :tags, through: :posts_tags, class_name: "Cpk::Tag"
   end
 end

activerecord/test/models/cpk/posts_tag.rb
@@ -1,0 +1,10 @@
+# frozen_string_literal: true
+
+module Cpk
+  class PostsTag < ActiveRecord::Base
+    self.table_name = :cpk_posts_tags
+
+    belongs_to :post, class_name: "Cpk:Post", query_constraints: %i[post_title post_author]
+    belongs_to :tag, class_name: "Cpk::Tag"
+  end
+end

rails/rails

activerecord/test/models/cpk/post.rb
@@ -4,5 +4,7 @@ module Cpk
   class Post < ActiveRecord::Base
     self.table_name = :cpk_posts
     has_many :comments, class_name: "Cpk::Comment", foreign_key: %i[commentable_title commentable_author], as: :commentable
+    has_many :posts_tags, class_name: "Cpk::PostsTag", foreign_key: %i[post_title post_author]
+    has_many :tags, through: :posts_tags, class_name: "Cpk::Tag"
   end
 end

activerecord/test/models/cpk/posts_tag.rb
@@ -1,0 +1,10 @@
+# frozen_string_literal: true
+
+module Cpk
+  class PostsTag < ActiveRecord::Base
+    self.table_name = :cpk_posts_tags
+
+    belongs_to :post, class_name: "Cpk:Post", foreign_key: %i[post_title post_author]
+    belongs_to :tag, class_name: "Cpk::Tag"
+  end
+end