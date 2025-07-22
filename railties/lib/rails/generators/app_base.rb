1084b228c3c3ad92e2f6a55a6d79741612b32981
Rafael Mendonça França <rafael@rubyonrails.org>
2025-07-22 02:46:18 +0000

    Don't add bundler platforms for JRuby
    The gems used by a Rails application on JRuby can't be resolved in
    other platforms.
    
    Fixes #54894.

--- railties/lib/rails/generators/app_base.rb
+++ railties/lib/rails/generators/app_base.rb
@@ -661,7 +661,7 @@ module Rails
       end
 
       def depend_on_bootsnap?
-        !options[:skip_bootsnap] && !options[:dev] && !defined?(JRUBY_VERSION)
+        !options[:skip_bootsnap] && !options[:dev] && !jruby?
       end
 
       def target_rails_prerelease(self_command = "new")
@@ -737,13 +737,17 @@ module Rails
       end
 
       def add_bundler_platforms
-        if bundle_install?
+        if bundle_install? && !jruby?
           # The vast majority of Rails apps will be deployed on `x86_64-linux`.
           bundle_command("lock --add-platform=x86_64-linux")
 
           # Users that develop on M1 mac may use docker and would need `aarch64-linux` as well.
           bundle_command("lock --add-platform=aarch64-linux") if RUBY_PLATFORM.start_with?("arm64")
         end
+      end
+
+      def jruby?
+        defined?(JRUBY_VERSION)
       end
 
       def empty_directory_with_keep_file(destination, config = {})
