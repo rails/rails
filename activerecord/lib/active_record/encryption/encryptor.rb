4ba3a4b8848ef42e2e09ce4c29932d132e25ecd1
Jonathan Hefner <jonathan@hefner.pro>
2025-08-07 15:21:47 -0500

    Fix missing punctuation [ci-skip]
    (cherry picked from commit 63cb08322d410bd7c9dca279b63118f4357b08aa)

--- activerecord/lib/active_record/encryption/encryptor.rb
+++ activerecord/lib/active_record/encryption/encryptor.rb
@@ -11,7 +11,7 @@ module ActiveRecord
     # It interacts with a KeyProvider for getting the keys, and delegate to
     # ActiveRecord::Encryption::Cipher the actual encryption algorithm.
     class Encryptor
-      # The compressor to use for compressing the payload
+      # The compressor to use for compressing the payload.
       attr_reader :compressor
 
       # === Options
@@ -20,22 +20,22 @@ module ActiveRecord
       #   Defaults to +true+.
       # * <tt>:compressor</tt> - The compressor to use.
       #   1. If compressor is provided, it will be used.
-      #   2. If not, it will use +ActiveRecord::Encryption.config.compressor+ which default value is +Zlib+.
+      #   2. If not, it will use +ActiveRecord::Encryption.config.compressor+, which default value is +Zlib+.
       #   If you want to use a custom compressor, it must respond to +deflate+ and +inflate+.
       def initialize(compress: true, compressor: nil)
         @compress = compress
         @compressor = compressor || ActiveRecord::Encryption.config.compressor
       end
 
-      # Encrypts +clean_text+ and returns the encrypted result
+      # Encrypts +clean_text+ and returns the encrypted result.
       #
       # Internally, it will:
       #
-      # 1. Create a new ActiveRecord::Encryption::Message
-      # 2. Compress and encrypt +clean_text+ as the message payload
+      # 1. Create a new ActiveRecord::Encryption::Message.
+      # 2. Compress and encrypt +clean_text+ as the message payload.
       # 3. Serialize it with +ActiveRecord::Encryption.message_serializer+ (+ActiveRecord::Encryption::SafeMarshal+
-      #    by default)
-      # 4. Encode the result with Base 64
+      #    by default).
+      # 4. Encode the result with Base64.
       #
       # === Options
       #
@@ -45,7 +45,7 @@ module ActiveRecord
       #
       # [:cipher_options]
       #   Cipher-specific options that will be passed to the Cipher configured in
-      #   +ActiveRecord::Encryption.cipher+
+      #   +ActiveRecord::Encryption.cipher+.
       def encrypt(clear_text, key_provider: default_key_provider, cipher_options: {})
         clear_text = force_encoding_if_needed(clear_text) if cipher_options[:deterministic]
 
@@ -53,17 +53,17 @@ module ActiveRecord
         serialize_message build_encrypted_message(clear_text, key_provider: key_provider, cipher_options: cipher_options)
       end
 
-      # Decrypts an +encrypted_text+ and returns the result as clean text
+      # Decrypts an +encrypted_text+ and returns the result as clean text.
       #
       # === Options
       #
       # [:key_provider]
       #   Key provider to use for the encryption operation. It will default to
-      #   +ActiveRecord::Encryption.key_provider+ when not provided
+      #   +ActiveRecord::Encryption.key_provider+ when not provided.
       #
       # [:cipher_options]
       #   Cipher-specific options that will be passed to the Cipher configured in
-      #   +ActiveRecord::Encryption.cipher+
+      #   +ActiveRecord::Encryption.cipher+.
       def decrypt(encrypted_text, key_provider: default_key_provider, cipher_options: {})
         message = deserialize_message(encrypted_text)
         keys = key_provider.decryption_keys(message)
@@ -73,7 +73,7 @@ module ActiveRecord
         raise Errors::Decryption
       end
 
-      # Returns whether the text is encrypted or not
+      # Returns whether the text is encrypted or not.
       def encrypted?(text)
         deserialize_message(text)
         true
