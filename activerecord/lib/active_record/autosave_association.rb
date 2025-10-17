9611865697795e989513a420d55f12cb2362c7b1
Noah Silvera <noah.a.silvera@gmail.com>
2025-10-17 18:36:06 -0400

    Allow autosave association methods to be overwritten
    If you try to override the `autosave` option by calling an association
    method like `has_many(:some_existing_association, autosave: true)` in
    a prepended module, it does not change the behavior of the underlying
    class.
    
    This is because even though you can override the reflection attached
    to the class, the original reflection is still referenced within
    blocks that define the methods related to autosaving associations.
    
    The `add_autosave_association_callbacks` method are only defined when
    an association is first built; or in the example I gave above, when an
    association is rebuilt, so removing this check for method definition
    should have a minimal performance impact.

--- activerecord/lib/active_record/autosave_association.rb
+++ activerecord/lib/active_record/autosave_association.rb
@@ -157,9 +157,7 @@ module ActiveRecord
     module ClassMethods # :nodoc:
       private
         def define_non_cyclic_method(name, &block)
-          return if method_defined?(name, false)
-
-          define_method(name) do |*args|
+          redefine_method(name) do |*args|
             result = true; @_already_called ||= {}
             # Loop prevention for validation of associations
             unless @_already_called[name]
