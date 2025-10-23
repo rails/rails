ca20371acf6d5f823eea734b1c4e0a73798c3f8d
Rafael Mendonça França <rafael@rubyonrails.org>
2025-10-23 21:17:14 +0000

    Don't load sprockets-rails in the dummy app of Active Storage tests

--- activestorage/test/dummy/config/application.rb
+++ activestorage/test/dummy/config/application.rb
@@ -2,14 +2,7 @@
 
 require_relative "boot"
 
-require "rails"
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-require "active_storage/engine"
+require "rails/all"
 
 Bundler.require(*Rails.groups)
 
