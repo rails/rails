f374cfda3529404bb73ae6771bace56f04a9f46c
Rafael Mendonça França <rafael@rubyonrails.org>
2025-10-24 19:28:53 +0000

    Don't need to assert output format details

--- activerecord/test/cases/adapters/mysql2/explain_test.rb
+++ activerecord/test/cases/adapters/mysql2/explain_test.rb
@@ -10,14 +10,11 @@ class Mysql2ExplainTest < ActiveRecord::Mysql2TestCase
   def test_explain_for_one_query
     explain = Author.where(id: 1).explain
     assert_match %(EXPLAIN for: SELECT `authors`.* FROM `authors` WHERE `authors`.`id` = 1), explain
-    assert_match %r(authors |.* const), explain
   end
 
   def test_explain_with_eager_loading
     explain = Author.where(id: 1).includes(:posts).explain
     assert_match %(EXPLAIN for: SELECT `authors`.* FROM `authors` WHERE `authors`.`id` = 1), explain
-    assert_match %r(authors |.* const), explain
     assert_match %(EXPLAIN for: SELECT `posts`.* FROM `posts` WHERE `posts`.`author_id` = 1), explain
-    assert_match %r(posts |.* ALL), explain
   end
 end
