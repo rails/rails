d100e466f68f42d841846781167a66966e0901a2
Rafael Mendonça França <rafael@rubyonrails.org>
2025-10-24 19:35:10 +0000

    Don't load action cable in the activestorage dummy app

--- activestorage/test/dummy/config/application.rb
+++ activestorage/test/dummy/config/application.rb
@@ -1,6 +1,16 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+# require "action_cable/engine"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
