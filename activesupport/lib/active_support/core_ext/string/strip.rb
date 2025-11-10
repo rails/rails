c9fa5d2bccbb549a8a5400f1a00b5205ab7f39ff
Berkan Unal <Berkanunal@gmail.com>
2025-11-10 18:41:20 +0300

    Optimize `String#strip_heredoc`
    Co-Authored-By: Jean Boussier <jean.boussier@gmail.com>

--- activesupport/lib/active_support/core_ext/string/strip.rb
+++ activesupport/lib/active_support/core_ext/string/strip.rb
@@ -20,8 +20,32 @@ class String
   # Technically, it looks for the least indented non-empty line
   # in the whole string, and removes that amount of leading whitespace.
   def strip_heredoc
-    gsub(/^#{scan(/^[ \t]*(?=\S)/).min}/, "").tap do |stripped|
-      stripped.freeze if frozen?
+    scanner = StringScanner.new("")
+
+    min_indent_len = nil
+    lines = self.lines
+
+    lines.each do |line|
+      scanner.string = line
+      indent_len = scanner.skip(/[ \t]*/)
+
+      next unless scanner.match?(/[^\r\n]/)
+
+      min_indent_len = indent_len if indent_len < (min_indent_len || Float::INFINITY)
     end
+
+
+    if min_indent_len.nil? || min_indent_len.zero?
+      return frozen? ? self : dup
+    end
+
+    lines.each do |line|
+      line[0, min_indent_len] = "" unless line == "\n"
+    end
+
+    result = lines.join
+
+    result.freeze if frozen?
+    result
   end
 end
