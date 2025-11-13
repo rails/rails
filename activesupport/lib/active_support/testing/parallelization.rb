8a566aa81e835dc87544ca99ed9501cdbfe3cb56
Jeremy Daer <jeremy@rubyonrails.org>
2025-11-13 10:50:01 -0800

    Make flaky parallel tests easier to diagnose by deterministically assigning tests to workers
    Rails assigns tests to workers in round-robin order so the same `--seed`
    and worker count will result in the same sequence of tests running on each
    worker (whether processes or threads) increasing the odds of reproducing
    test failures caused by test interdependence.
    
    This can make test runtime slower and spikier when one worker gets most of
    the slow tests. Enable `work_stealing: true` to allow idle workers to steal
    tests from busy workers in deterministic order, smoothing out runtime at the
    cost of less reproducible flaky-test failures.

--- activesupport/lib/active_support/testing/parallelization.rb
+++ activesupport/lib/active_support/testing/parallelization.rb
@@ -3,6 +3,8 @@
 require "drb"
 require "drb/unix" unless Gem.win_platform?
 require "active_support/core_ext/module/attribute_accessors"
+require "active_support/testing/parallelization/test_distributor"
+require "active_support/testing/parallelization/thread_pool_executor"
 require "active_support/testing/parallelization/server"
 require "active_support/testing/parallelization/worker"
 
@@ -33,9 +35,13 @@ module ActiveSupport
 
       cattr_reader :run_cleanup_hooks
 
-      def initialize(worker_count)
+      def initialize(worker_count, work_stealing: false)
         @worker_count = worker_count
-        @queue_server = Server.new
+
+        distributor = (work_stealing ? RoundRobinWorkStealingDistributor : RoundRobinDistributor).new \
+          worker_count: worker_count
+
+        @queue_server = Server.new(distributor: distributor)
         @worker_pool = []
         @url = DRb.start_service("drbunix:", @queue_server).uri
       end
