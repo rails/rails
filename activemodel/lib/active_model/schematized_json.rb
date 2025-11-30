f0e5b44de757a6290367ba8f52d27f78ba4a73d0
David Heinemeier Hansson <david@hey.com>
2025-11-30 20:17:52 +0100

    Implementation comments

--- activemodel/lib/active_model/schematized_json.rb
+++ activemodel/lib/active_model/schematized_json.rb
@@ -12,13 +12,16 @@ module ActiveModel
       # directly from the UI as strings, and still have them set with the correct JSON type in the database.
       def has_json(attr, **schema)
         define_method(attr) do
+          # Ensure the attribute is set if nil, so we can pass the reference to the accessor for defaults.
           _write_attribute(attr.to_s, {}) if attribute(attr.to_s).nil?
+
+          # No memoizationen used in order to stay compatible with #reload (and because it's such a thin accessor)
           ActiveModel::SchematizedJson::DataAccessor.new(schema, data: attribute(attr.to_s))
         end
 
         define_method("#{attr}=") { |data| public_send(attr).assign_data_with_type_casting(data) }
 
-        # Ensure default values are set before saving
+        # Ensure default values are set before saving by relying on DataAccessor instantiation to do it.
         before_save -> { send(attr) } if respond_to?(:before_save)
       end
     end
@@ -71,6 +74,8 @@ module ActiveModel
           end
         end
 
+        # Types that are declared using real values, like true/false, 5, or "hello", will be used as defaults.
+        # Types that are declared using symbols, like :boolean, :integer, :string, will be nulled out.
         def update_data_with_schema_defaults
           @data.reverse_merge!(@schema.to_h { |attr, type| [ attr.to_s, type.is_a?(Symbol) ? nil : type ] })
         end
