bf0bcff563ccfc8dd73826cb3259ee1a5325f2b1
Petrik <petrik@deheus.net>
2025-12-10 11:36:15 +0100

    nodoc ActiveRecord::QueryMethods#uniq! [ci skip]
    All other bang methods in QueryMethods are nodoc as well.

--- activerecord/lib/active_record/relation/query_methods.rb
+++ activerecord/lib/active_record/relation/query_methods.rb
@@ -1539,7 +1539,7 @@ module ActiveRecord
     end
 
     # Deduplicate multiple values.
-    def uniq!(name)
+    def uniq!(name) # :nodoc:
       if values = @values[name]
         values.uniq! if values.is_a?(Array) && !values.empty?
       end
