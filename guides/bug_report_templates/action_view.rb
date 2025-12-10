9ec42cd7fe6792dfdf22b4f00d4a5ad521f0335e
Shiva Bhusal <hotline.shiva@gmail.com>
2025-12-10 23:09:56 +0545

    Updates the bug report test template for ActionView
    - It adds tests to validate rendering of templates & partials
    - Adds ActionView tests for template and inline rendering with titleize helper
    - Uses `Pathname.new` for improved path handling
    - Apply suggestion from @seanpdoyle
    - Adds setup and teardown methods for clarity and flexibility
    
    Co-authored-by: Sean Doyle <seanpdoyle@users.noreply.github.com>

--- guides/bug_report_templates/action_view.rb
+++ guides/bug_report_templates/action_view.rb
@@ -16,26 +16,62 @@ require "minitest/autorun"
 
 class TestApp < Rails::Application
   config.load_defaults Rails::VERSION::STRING.to_f
-  config.eager_load = false
-  config.logger = Logger.new($stdout)
-  config.secret_key_base = "secret_key_base"
+  config.eager_load       = false
+  config.logger           = Logger.new($stdout)
+  config.secret_key_base  = "secret_key_base"
 end
+
 Rails.application.initialize!
 
 class BugTest < ActionView::TestCase
+  setup do
+    @view_path = Pathname.new(Dir.mktmpdir)
+    ActionController::Base.prepend_view_path @view_path
+  end
+
+  teardown do
+    @view_path.rmtree
+  end
+
   helper do
-    def upcase(value)
-      value.upcase
+    def titleize(string)
+      string.titleize
     end
   end
 
-  def test_stuff
+  test "Action View template rendering" do
+    view_file "posts/index.html.erb", <<~ERB
+      <h1>Posts</h1>
+
+      <% posts.each do |post| %>
+        <%= render partial: "posts/post", locals: { post: post } %>
+      <% end %>
+    ERB
+
+    view_file "posts/_post.html.erb", <<~ERB
+      <p><%= titleize(post) %></p>
+    ERB
+
+    render template: "posts/index", locals: { posts: ["hello world"] }
+
+    assert_equal "Hello World", rendered.html.at("p").text
+  end
+
+
+  test "Action View inline rendering" do
     render inline: <<~ERB, locals: { key: "value" }
-      <p><%= upcase(key) %></p>
+      <p><%= titleize(key) %></p>
     ERB
 
     element = rendered.html.at("p")
 
-    assert_equal element.text, "VALUE"
+    assert_equal element.text, "Value"
   end
+
+  private
+    def view_file(filename, contents)
+      pathname = @view_path.join(filename)
+      pathname.dirname.mkpath
+      pathname.write(contents.chomp)
+    end
 end
