213f804a7a048ed5de6af11b7d166c4bcbf896bd
Radamés Roriz <radamesroriz@gmail.com>
2025-12-22 01:16:45 -0300

    test: replace url method invoke for @routes.recognize_path

--- activestorage/test/urls/rails_storage_direct_upload_test.rb
+++ activestorage/test/urls/rails_storage_direct_upload_test.rb
@@ -4,9 +4,8 @@ require "test_helper"
 require "database/setup"
 
 class RailsStorageDirectUploadTest < ActiveSupport::TestCase
-  include Rails.application.routes.url_helpers
-
   setup do
+    @routes = Rails.application.routes
     @was_skip_default_direct_uploads_routes = ActiveStorage.skip_default_direct_uploads_routes
   end
 
@@ -17,7 +16,7 @@ class RailsStorageDirectUploadTest < ActiveSupport::TestCase
   test "rails registered direct upload default path" do
     ActiveStorage.skip_default_direct_uploads_routes = false
 
-    assert_includes rails_active_storage_direct_uploads_path(only_path: true), "/rails/active_storage/direct_uploads"
+    assert_equal({ controller: "active_storage/direct_uploads", action: "create" }, @routes.recognize_path("/rails/active_storage/direct_uploads", method: :post))
   end
 
   test "rails skip default direct upload default path" do
