2b504dc07dc2a3816abc1a5ceca79fb7cd4c27e4
Matthew Draper <matthew@trebex.net>
2026-02-11 02:08:05 +1030

    Continue pool disconnect after cleanup failures
    Pool teardown checks in leased connections before disconnecting them so
    normal cleanup callbacks can run. If either that checkin or the adapter
    disconnect raises, report it as handled and move on; leaving later
    connections untouched would strand raw resources with no pool left to
    manage them.

--- activerecord/test/cases/connection_pool_test.rb
+++ activerecord/test/cases/connection_pool_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "cases/helper"
+require "active_support/error_reporter/test_helper"
 require "concurrent/atomic/count_down_latch"
 
 module ActiveRecord
@@ -951,6 +952,60 @@ module ActiveRecord
         end
       ensure
         pool&.disconnect!
+      end
+
+      def test_disconnect_closes_in_use_connections_when_checkin_fails
+        connection = @pool.lease_connection
+        connection.connect!
+
+        checkin_error = StandardError.new("error during checkin")
+        proc_to_raise = -> { raise checkin_error }
+        subscriber = ActiveSupport::ErrorReporter::TestHelper::ErrorSubscriber.new
+        ActiveSupport.error_reporter.subscribe(subscriber)
+        ActiveRecord::ConnectionAdapters::AbstractAdapter.set_callback(:checkin, :before, proc_to_raise)
+
+        assert_nothing_raised do
+          @pool.disconnect!
+        end
+
+        assert_not_predicate connection, :connected?
+        assert_empty @pool.connections
+        assert_equal [[checkin_error, true, :warning, "active_record.connection_pool", {}]], subscriber.events
+      ensure
+        ActiveRecord::ConnectionAdapters::AbstractAdapter.skip_callback(:checkin, :before, proc_to_raise) if proc_to_raise
+        ActiveSupport.error_reporter.unsubscribe(subscriber) if subscriber
+        connection&.disconnect! rescue nil
+      end
+
+      def test_disconnect_continues_when_connection_disconnect_fails
+        first_connection = @pool.checkout
+        second_connection = @pool.checkout
+        disconnect_error = StandardError.new("error during disconnect")
+        disconnected = []
+        subscriber = ActiveSupport::ErrorReporter::TestHelper::ErrorSubscriber.new
+        ActiveSupport.error_reporter.subscribe(subscriber)
+
+        first_connection.define_singleton_method(:disconnect!) do
+          disconnected << self
+          raise disconnect_error
+        end
+        second_connection.define_singleton_method(:disconnect!) do
+          disconnected << self
+        end
+
+        assert_nothing_raised do
+          @pool.disconnect!
+        end
+
+        assert_equal [first_connection, second_connection], disconnected
+        assert_empty @pool.connections
+        assert_equal [[disconnect_error, true, :warning, "active_record.connection_pool", {}]], subscriber.events
+      ensure
+        ActiveSupport.error_reporter.unsubscribe(subscriber) if subscriber
+        first_connection&.singleton_class&.remove_method(:disconnect!) rescue nil
+        second_connection&.singleton_class&.remove_method(:disconnect!) rescue nil
+        first_connection&.disconnect! rescue nil
+        second_connection&.disconnect! rescue nil
       end
 
       def test_pool_sets_connection_visitor
