d926cf38ad420c99b56c64f7633d89082e4aecd2
Duarte Martins <duarteosrm@gmail.com>
2026-02-19 22:49:03 +0100

    Fix ActiveModel::Type::Boolean casting 0.0 (float) as true
    Integer 0 is correctly cast to false, but float 0.0 was cast to true
    due to it not being present in FALSE_VALUES. Add 0.0 to FALSE_VALUES
    for consistent behaviour.
    
    Fixes #56602

--- activemodel/lib/active_model/type/boolean.rb
+++ activemodel/lib/active_model/type/boolean.rb
@@ -7,14 +7,14 @@ module ActiveModel
     # A class that behaves like a boolean type, including rules for coercion of
     # user input.
     #
-    # - <tt>"false"</tt>, <tt>"f"</tt>, <tt>"0"</tt>, +0+ or any other value in
+    # - <tt>"false"</tt>, <tt>"f"</tt>, <tt>"0"</tt>, +0+, +0.0+ or any other value in
     #   +FALSE_VALUES+ will be coerced to +false+.
     # - Empty strings are coerced to +nil+.
     # - All other values will be coerced to +true+.
     class Boolean < Value
       include Helpers::Immutable
       FALSE_VALUES = [
-        false, 0,
+        false, 0, 0.0,
         "0", :"0",
         "f", :f,
         "F", :F,
