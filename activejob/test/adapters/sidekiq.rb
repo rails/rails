ad7e7315077e1fac8463e76051e99393072d2439
Hartley McGuire <skipkayhil@gmail.com>
2026-02-23 23:03:14 -0500

    Merge pull request #56862 from skipkayhil/hm-rtvozytmvnswkpvr
    Fix deprecation of sidekiq/testing/inline

--- activejob/test/adapters/sidekiq.rb
+++ activejob/test/adapters/sidekiq.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
-require "sidekiq/testing/inline"
+require "sidekiq"
+if Sidekiq.respond_to? :testing! # 8.1.1
+  Sidekiq.testing!(:inline)
+else
+  require "sidekiq/testing/inline"
+end
 ActiveJob::Base.queue_adapter = :sidekiq
