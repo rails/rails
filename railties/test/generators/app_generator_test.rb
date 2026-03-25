f3a57d5cc4efe99d5a0ea84390e1f3e6141d2844
Aleksandr Borisov <aishek@gmail.com>
2026-03-25 13:01:10 +0000

    Adjust selenium sessons count for Capybara.using_session

--- railties/test/generators/app_generator_test.rb
+++ railties/test/generators/app_generator_test.rb
@@ -1546,6 +1546,11 @@ class AppGeneratorTest < Rails::Generators::TestCase
       expected_selenium_config = {
         "image" => "selenium/standalone-chromium",
         "restart" => "unless-stopped",
+        "environment" => [
+          "SE_NODE_MAX_SESSIONS=3",
+          "SE_NODE_OVERRIDE_MAX_SESSIONS=true",
+          "SE_START_VNC=false"
+        ]
       }
 
       assert_equal expected_selenium_config, compose_config["services"]["selenium"]
