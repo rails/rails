0b03b3af33ed1b15ecc4f43d4cb9f8331a551051
Andrew Novoselac <andrew.novoselac@shopify.com>
2026-04-01 13:14:27 -0400

    Add named default scopes.
    Default scopes can now be given a name, making them durable. They
    will not be removed by `unscoped` unless explicitly referenced by name.
    
    Co-authored-by: Matthew Draper <matthew@trebex.net>

--- activerecord/lib/active_record/core.rb
+++ activerecord/lib/active_record/core.rb
@@ -423,6 +423,16 @@ module ActiveRecord
         schema_context.cached_find_by_statement(connection, key, &block)
       end
 
+      def raw_relation # :nodoc:
+        relation = Relation.create(self)
+
+        if finder_needs_type_condition? && !ignore_default_scope?
+          relation.where!(type_condition)
+        else
+          relation
+        end
+      end
+
       private
         def inherited(subclass)
           super
@@ -443,16 +453,6 @@ module ActiveRecord
             @inspection_filter = nil
             @filter_attributes ||= nil
             @generated_association_methods ||= nil
-          end
-        end
-
-        def relation
-          relation = Relation.create(self)
-
-          if finder_needs_type_condition? && !ignore_default_scope?
-            relation.where!(type_condition)
-          else
-            relation
           end
         end
 
