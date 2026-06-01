d3afd3145d86f5df4743341bb50bc4ecb0ba77bb
Jean Boussier <jean.boussier@gmail.com>
2026-06-01 10:55:46 +0200

    Merge pull request #57503 from afurm/af/reject-malformed-host-port
    Reject malformed hosts with extra ports

--- actionpack/lib/action_dispatch/middleware/host_authorization.rb
+++ actionpack/lib/action_dispatch/middleware/host_authorization.rb
@@ -74,14 +74,26 @@ module ActionDispatch
 
         def sanitize_string(host)
           if host.start_with?(".")
-            /\A#{SUBDOMAIN_REGEX}?#{Regexp.escape(host[1..-1])}#{PORT_REGEX}?\z/i
+            /\A#{SUBDOMAIN_REGEX}?#{Regexp.escape(host[1..-1])}#{port_regexp(host)}\z/i
           else
-            /\A#{Regexp.escape host}#{PORT_REGEX}?\z/i
+            /\A#{Regexp.escape host}#{port_regexp(host)}\z/i
           end
         end
 
         def extract_hostname(host)
           host.slice(VALID_IP_HOSTNAME, "host") || host
+        end
+
+        def port_regexp(host)
+          host_with_port?(host) ? "" : "#{PORT_REGEX}?"
+        end
+
+        def host_with_port?(host)
+          if host.start_with?("[")
+            host.match?(/\]:\d+\z/)
+          else
+            host.count(":") == 1 && host.match?(/:\d+\z/)
+          end
         end
     end
 
