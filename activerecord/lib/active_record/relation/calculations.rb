313424add67c14e90d5d3cefd23910533cc587bb
Rafael Mendonça França <rafael@rubyonrails.org>
2026-06-02 13:43:23 -0400

    Merge pull request #57532 from 55728/fix-cpk-group-belongs-to-count
    Fix grouped calculations by a `belongs_to` association with a composite primary key model

--- activerecord/lib/active_record/relation/calculations.rb
+++ activerecord/lib/active_record/relation/calculations.rb
@@ -572,7 +572,11 @@ module ActiveRecord
 
           result.then do |calculated_data|
             if association
-              key_ids     = calculated_data.collect { |row| row[group_aliases.first] }
+              key_ids = if association.klass.base_class.composite_primary_key?
+                calculated_data.collect { |row| group_aliases.map { |group_alias| row[group_alias] } }
+              else
+                calculated_data.collect { |row| row[group_aliases.first] }
+              end
               key_records = association.klass.base_class.where(association.klass.base_class.primary_key => key_ids)
               key_records = key_records.index_by(&:id)
             end
