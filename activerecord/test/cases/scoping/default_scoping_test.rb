129bace2c37b5da2078256fd2d720811f23a991a
Andrew Novoselac <andrew.novoselac@shopify.com>
2026-06-05 15:19:19 -0400

    Reloading records properly accounts for named default scopes
    Co-authored by: Matthew Draper <matthew@trebex.net>

--- activerecord/test/cases/scoping/default_scoping_test.rb
+++ activerecord/test/cases/scoping/default_scoping_test.rb
@@ -851,6 +851,48 @@ class DefaultScopingTest < ActiveRecord::TestCase
 
     assert_match(/must be symbols or strings/, error.message)
   end
+
+  def test_named_default_scopes_not_included_on_reload
+    dev = DeveloperWithNamedDefaultScopes.create!(name: "David", mentor_id: 1, firm_id: 2)
+    reload_sql = capture_sql { dev.reload }.first
+
+    assert_no_match(/mentor_id/, reload_sql)
+    assert_no_match(/firm_id/, reload_sql)
+  end
+
+  def test_all_queries_named_defaults_included_on_reload
+    dev = DeveloperWithNamedAllQueriesDefaultScope.create!(name: "David", mentor_id: 1, firm_id: 2)
+    reload_sql = capture_sql { dev.reload }.first
+
+    assert_match(/mentor_id/, reload_sql)
+  end
+
+  def test_named_default_scopes_included_on_unscoped_reload
+    dev = DeveloperWithNamedDefaultScopes.create!(name: "David", mentor_id: 1, firm_id: 1)
+    reload_sql = capture_sql { dev.reload(unscoped: true) }.first
+
+    assert_match(/mentor_id/, reload_sql)
+    assert_match(/firm_id/, reload_sql)
+    assert_no_match(/salary/, reload_sql)
+  end
+
+  def test_unscope_named_default_scopes_from_reload
+    dev = DeveloperWithNamedDefaultScopes.create!(name: "David", mentor_id: 1, firm_id: 1)
+    reload_sql = capture_sql { dev.reload(unscoped: [:firm]) }.first
+
+    assert_match(/mentor_id/, reload_sql)
+    assert_match(/salary/, reload_sql)
+    assert_no_match(/firm_id/, reload_sql)
+  end
+
+  def test_unscope_named_defaults_and_unnamed_default_scopes_from_reload
+    dev = DeveloperWithNamedDefaultScopes.create!(name: "David", mentor_id: 1, firm_id: 1)
+    reload_sql = capture_sql { dev.reload(unscoped: [:firm, :mentor, true]) }.first
+
+    assert_no_match(/mentor_id/, reload_sql)
+    assert_no_match(/firm_id/, reload_sql)
+    assert_no_match(/salary/, reload_sql)
+  end
 end
 
 class DefaultScopingWithThreadTest < ActiveRecord::TestCase
