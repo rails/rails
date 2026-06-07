1fb08d2743fe28ff38204c03529b3dc7af7ed638
Matthew Draper <matthew@trebex.net>
2026-06-07 21:09:54 +0000

    Finalize unfinished intent logs on checkin
    For safety, ensure that all intents that have started do get finished,
    even if the caller somehow forgets about them.

--- activerecord/lib/active_record/connection_adapters/abstract/database_statements.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/database_statements.rb
@@ -697,6 +697,7 @@ module ActiveRecord
         handle = active_record_instrumenter.build_handle("sql.active_record", payload)
         handle.start
         intent.log_handle = handle
+        @unfinalized_intents << intent
       end
 
       def finish_intent_log(intent, exception: nil) # :nodoc:
@@ -704,6 +705,7 @@ module ActiveRecord
         return unless handle
 
         intent.log_handle = nil
+        @unfinalized_intents.delete(intent)
 
         if exception
           payload = intent.notification_payload
@@ -712,6 +714,17 @@ module ActiveRecord
         end
 
         handle.finish
+      end
+
+      def finalize_remaining_intents # :nodoc:
+        intents = @unfinalized_intents
+        @unfinalized_intents = []
+
+        intents.each do |intent|
+          next if intent.finalized?
+
+          intent.finish_log(exception: intent.error)
+        end
       end
 
       # Executes SQL statements in the context of this connection without
