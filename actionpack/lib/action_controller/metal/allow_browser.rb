44b9086ba31dab96753a182dec081c204ce27f09
Andrew Novoselac <andrew.novoselac@shopify.com>
2026-06-15 21:24:41 -0400

    Freeze BrowserBlocker::SETS[:modern]

--- actionpack/lib/action_controller/metal/allow_browser.rb
+++ actionpack/lib/action_controller/metal/allow_browser.rb
@@ -72,7 +72,7 @@ module ActionController # :nodoc:
 
       class BrowserBlocker # :nodoc:
         SETS = {
-          modern: { safari: 17.2, chrome: 120, firefox: 121, opera: 106, ie: false }
+          modern: { safari: 17.2, chrome: 120, firefox: 121, opera: 106, ie: false }.freeze
         }.freeze
 
         attr_reader :request, :versions
