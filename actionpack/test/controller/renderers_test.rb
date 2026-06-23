90af36663769909f43575daf2a4f237ba909635e
Edouard CHIN <chin.edouard@gmail.com>
2026-06-24 00:57:26 +0200

    Stop mutating ActionController Renderers:
    - ### Problem
    
      I'd like to make this codepath Ractor safe, which currently is not
      because one constant isn't frozen (RENDERERS) and we can't froze it
      since we are mutating it at boot time (to add new renderers).
    
      ### Solution
    
      Whenever a new renderer is added, create a new Set with the new
      renderer instead of mutating it. Similar for removing renderer.
    
      ### Side note
    
      This commit unfortunately doesn't make it Ractor safe yet. This
      is because the RENDERERS constant (which is now deprecated) was
      public, so I can't freeze it as I don't know wheter applications
      would mutate it (even though mutating it directly would make 0
      sense).

--- actionpack/test/controller/renderers_test.rb
+++ actionpack/test/controller/renderers_test.rb
@@ -3,8 +3,11 @@
 require "abstract_unit"
 require "controller/fake_models"
 require "active_support/logger"
+require "active_support/testing/ractors_assertions"
 
 class RenderersTest < ActionController::TestCase
+  include ActiveSupport::Testing::RactorsAssertions
+
   class XmlRenderable
     def to_xml(options)
       options[:root] ||= "i-am-xml"
@@ -126,5 +129,15 @@ class RenderersTest < ActionController::TestCase
     end
   ensure
     ActionController::Renderers.remove(:foo)
+  end
+
+  test "set of renderers if frozen" do
+    assert_ractor_shareable(ActionController::Renderers.all)
+
+    ActionController.add_renderer(:rtf) { }
+    assert_ractor_shareable(ActionController::Renderers.all)
+
+    ActionController.remove_renderer(:rtf)
+    assert_ractor_shareable(ActionController::Renderers.all)
   end
 end
