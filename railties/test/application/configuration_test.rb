506e9d771989733a0d171399ee3eec495e24d960
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2026-06-24 00:19:07 -0500

    Freeze Action View configuration with config.freeze_configuration
    When `config.freeze_configuration` is enabled, freeze the relevant Action View
    configuration values after the application has finished initializing so they
    become Ractor-shareable and can no longer be mutated at runtime.

--- railties/test/application/configuration_test.rb
+++ railties/test/application/configuration_test.rb
@@ -2169,6 +2169,19 @@ module ApplicationTests
       assert_ractor_shareable ActionDispatch::Request.parameter_parsers
     end
 
+    test "config.freeze_configuration freezes action_view configurations" do
+      remove_from_config '.*config\.load_defaults.*\n'
+
+      app_file "config/initializers/freeze_configuration.rb", <<-RUBY
+        Rails.application.config.freeze_configuration = true
+      RUBY
+
+      app "production"
+
+      assert_ractor_shareable ActionView::Base.field_error_proc
+      assert_ractor_shareable ActionView::Base.default_formats
+    end
+
     test "config.annotations wrapping SourceAnnotationExtractor::Annotation class" do
       make_basic_app do |application|
         application.config.annotations.register_extensions("coffee") do |tag|
