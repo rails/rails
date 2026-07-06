0e899809b9e8357488bf3bdb84108d68478a18fe
J.J. Green <j.j.green@gmx.co.uk>
2026-07-06 20:56:12 +0100

    rubocop fixes

--- actioncable/test/helpers/action_cable_helper_test.rb
+++ actioncable/test/helpers/action_cable_helper_test.rb
@@ -1,36 +1,34 @@
 # frozen_string_literal: true
 
 require "test_helper"
-require 'action_view/railtie'
+require "action_view/railtie"
 
 class ActionCable::Helpers::ActionCableHelperTest < ActionCable::TestCase
   include ActionCable::Helpers::ActionCableHelper
   include ActionView::Helpers::TagHelper
 
-  setup { ActionCable.server.config.mount_path = '/cable' }
+  setup { ActionCable.server.config.mount_path = "/cable" }
 
-  def with_mock_relative_url_root(value)
+  def with_mock_relative_url_root(value, &block)
     config = Minitest::Mock.new
     config.expect(:relative_url_root, value)
-    Rails.stub(:configuration, config) do
-      yield
-    end
+    Rails.stub(:configuration, config, &block)
   end
 
-  test 'action_cable_meta_tag mount_path without relative_url_root' do
+  test "action_cable_meta_tag mount_path without relative_url_root" do
     with_mock_relative_url_root(nil) do
       assert_equal(
-        action_cable_meta_tag,
-        %Q(<meta name="action-cable-url" content="/cable" />)
+        '<meta name="action-cable-url" content="/cable" />',
+        action_cable_meta_tag
       )
     end
   end
 
-  test 'action_cable_meta_tag mount_path with relative_url_root' do
-    with_mock_relative_url_root('/foo') do
+  test "action_cable_meta_tag mount_path with relative_url_root" do
+    with_mock_relative_url_root("/foo") do
       assert_equal(
-        action_cable_meta_tag,
-        %Q(<meta name="action-cable-url" content="/foo/cable" />)
+        '<meta name="action-cable-url" content="/foo/cable" />',
+        action_cable_meta_tag
       )
     end
   end
