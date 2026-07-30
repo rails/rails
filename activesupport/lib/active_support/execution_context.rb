bcce5106f8b3d074a91d6efaa4a34b7e4a087f3c
Rafael Mendonça França <rafael@rubyonrails.org>
2026-07-30 10:41:19 -0700

    ExecutionContext isn't public API

--- activesupport/lib/active_support/execution_context.rb
+++ activesupport/lib/active_support/execution_context.rb
@@ -4,7 +4,7 @@ require "active_support/core_ext/hash/keys"
 
 module ActiveSupport
   module ExecutionContext # :nodoc:
-    class Record
+    class Record # :nodoc:
       attr_reader :store, :current_attributes_instances
 
       def initialize
