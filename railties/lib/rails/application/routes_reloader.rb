ec059559a9dd6b967aa0bf9fd424ea7cd66082c4
Jean Boussier <byroot@ruby-lang.org>
2026-08-10 08:25:24 +0200

    Fix RoutesReloaderTest to not get stuck

--- railties/lib/rails/application/routes_reloader.rb
+++ railties/lib/rails/application/routes_reloader.rb
@@ -83,7 +83,7 @@ module Rails
             hash[dir.to_s] = %w(rb)
           end
 
-          ActiveSupport::FileUpdateChecker.new(paths, dirs) { reload! }
+          @file_watcher.new(paths, dirs) { reload! }
         end
       end
 
