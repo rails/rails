cc70d62782526a3134d6c2a682fc6b935bbb7d7c
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2026-08-13 11:36:54 -0500

    Make Action Dispatch configs ractor shareable

--- railties/test/application/configuration_test.rb
+++ railties/test/application/configuration_test.rb
@@ -5470,6 +5470,21 @@ module ApplicationTests
       assert_equal "[:active_record_connected_to_stack, :custom_key]", last_response.body
     end
 
+    if RUBY_VERSION >= "4.0"
+      test "ActionDispatch configuration is frozen after boot" do
+        app "development"
+
+        [
+          ActionDispatch::ExceptionWrapper.rescue_responses,
+          ActionDispatch::ExceptionWrapper.rescue_templates,
+          ActionDispatch::ExceptionWrapper.wrapper_exceptions,
+          ActionDispatch::ExceptionWrapper.silent_exceptions,
+        ].each do |config|
+          assert_ractor_shareable(config)
+        end
+      end
+    end
+
     private
       def set_custom_config(contents, config_source = "custom".inspect)
         app_file "config/custom.yml", contents
