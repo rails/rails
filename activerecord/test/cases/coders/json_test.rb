99a9ba3fe57e4ede28a9a6cbcae0058172d8f108
Jean Boussier <jean.boussier@gmail.com>
2026-08-31 09:02:52 +0200

    Merge pull request #58612 from lazerg/fix-json-coder-encode-options
    Fix `NameError` when `ActiveRecord::Coders::JSON` is given encode options

--- activerecord/test/cases/coders/json_test.rb
+++ activerecord/test/cases/coders/json_test.rb
@@ -19,6 +19,16 @@ module ActiveRecord
         coder = JSON.new(decode_options: { symbolize_names: true })
         assert_equal({ foo: "bar" }, coder.load('{"foo":"bar"}'))
       end
+
+      def test_dump_does_not_html_escape
+        coder = JSON.new
+        assert_equal '{"k":"<>&"}', coder.dump({ "k" => "<>&" })
+      end
+
+      def test_dump_with_encode_options
+        coder = JSON.new(encode_options: { escape: true })
+        assert_equal '{"k":"\\u003c\\u003e\\u0026"}', coder.dump({ "k" => "<>&" })
+      end
     end
   end
 end
