3be318dff85081db9072f7ac2c6455e7b9b0d2e5
Jean Boussier <jean.boussier@gmail.com>
2026-09-03 17:12:14 +0200

    Implement IndexedRow::Mutable
    Allows to build another indexed row that shares the same
    index.
    
    It is mutable, but won't accept any key not in the index.
    
    Co-Authored-By: Iliana Hadzhiatanasova <me@iliana.dev>

--- activemodel/test/cases/indexed_row_test.rb
+++ activemodel/test/cases/indexed_row_test.rb
@@ -23,6 +23,10 @@ class IndexedRowTest < ActiveModel::TestCase
     assert_equal @hash.keys, @row.keys
   end
 
+  test "#values" do
+    assert_equal @hash.values, @row.values
+  end
+
   test "#==" do
     assert_equal @hash, @row
   end
@@ -44,5 +48,61 @@ class IndexedRowTest < ActiveModel::TestCase
 
   test "#to_h" do
     assert_equal @hash, @row.to_h
+  end
+end
+
+class EmptyMutableIndexedRowTest < ActiveModel::TestCase
+  setup do
+    @hash = { "first_name" => "George", "last_name" => "Abitbol", "age" => 42 }.freeze
+    @reference = ActiveModel::IndexedRow[@hash]
+    @row = @reference.new_empty_mutable_row
+  end
+
+  test "#[]" do
+    assert_nil @row["first_name"]
+    assert_nil @row["last_name"]
+    assert_nil @row["age"]
+    assert_nil @row["missing"]
+  end
+
+  test "#[]=" do
+    assert_nil @row["age"]
+    @row["age"] = 42
+    assert_equal 42, @row["age"]
+
+    assert_raises KeyError do
+      @row["missing"] = true
+    end
+  end
+
+  test "#size" do
+    assert_equal 0, @row.size
+    @row["age"] = 42
+    assert_equal 1, @row.size
+    @row["first_name"] = "George"
+    assert_equal 2, @row.size
+  end
+
+  test "#keys" do
+    assert_equal [], @row.keys
+    @row["age"] = 42
+    assert_equal ["age"], @row.keys
+    @row["first_name"] = "George"
+    assert_equal ["first_name", "age"], @row.keys
+  end
+
+  test "#values" do
+    assert_equal [], @row.values
+    @row["age"] = 42
+    assert_equal [42], @row.values
+    @row["first_name"] = "George"
+    assert_equal ["George", 42], @row.values
+  end
+
+  test "#fetch" do
+    assert_raises(KeyError) { @row.fetch("age") }
+    assert_equal(4, @row.fetch("age") { 4 })
+    @row["age"] = 42
+    assert_equal 42, @row.fetch("age")
   end
 end
