2d529ab87d21dfc3c11dde4cb95bebefb1e32e9f
don2e4 <don2e4@users.noreply.github.com>
2026-09-14 09:49:00 +0800

    Allow large Mailgun payloads to exceed Rack's query parser limit [Fix #57822]

--- actionmailbox/test/dummy/config/environments/test.rb
+++ actionmailbox/test/dummy/config/environments/test.rb
@@ -60,4 +60,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.action_mailbox.ingress = :mailgun
 end
