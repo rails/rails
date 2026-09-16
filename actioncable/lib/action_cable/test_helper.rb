b12a88175fb248b7b89d75340da642110150fa81
Xavier Noria <fxn@hashref.com>
2026-09-16 08:03:07 +0200

    RDoc to Markdown in some Action Cable files

--- actioncable/lib/action_cable/test_helper.rb
+++ actioncable/lib/action_cable/test_helper.rb
@@ -1,3 +1,4 @@
+# :markup: markdown
 # frozen_string_literal: true
 
 # :markup: markdown
@@ -23,27 +24,31 @@ module ActionCable
     # Asserts that the number of broadcasted messages to the stream matches the
     # given number.
     #
-    #     def test_broadcasts
-    #       assert_broadcasts 'messages', 0
-    #       ActionCable.server.broadcast 'messages', { text: 'hello' }
-    #       assert_broadcasts 'messages', 1
-    #       ActionCable.server.broadcast 'messages', { text: 'world' }
-    #       assert_broadcasts 'messages', 2
-    #     end
+    # ```
+    # def test_broadcasts
+    #   assert_broadcasts 'messages', 0
+    #   ActionCable.server.broadcast 'messages', { text: 'hello' }
+    #   assert_broadcasts 'messages', 1
+    #   ActionCable.server.broadcast 'messages', { text: 'world' }
+    #   assert_broadcasts 'messages', 2
+    # end
+    # ```
     #
     # If a block is passed, that block should cause the specified number of messages
     # to be broadcasted.
     #
-    #     def test_broadcasts_again
-    #       assert_broadcasts('messages', 1) do
-    #         ActionCable.server.broadcast 'messages', { text: 'hello' }
-    #       end
+    # ```
+    # def test_broadcasts_again
+    #   assert_broadcasts('messages', 1) do
+    #     ActionCable.server.broadcast 'messages', { text: 'hello' }
+    #   end
     #
-    #       assert_broadcasts('messages', 2) do
-    #         ActionCable.server.broadcast 'messages', { text: 'hi' }
-    #         ActionCable.server.broadcast 'messages', { text: 'how are you?' }
-    #       end
-    #     end
+    #   assert_broadcasts('messages', 2) do
+    #     ActionCable.server.broadcast 'messages', { text: 'hi' }
+    #     ActionCable.server.broadcast 'messages', { text: 'how are you?' }
+    #   end
+    # end
+    # ```
     #
     def assert_broadcasts(stream, number, &block)
       if block_given?
@@ -59,23 +64,29 @@ module ActionCable
 
     # Asserts that no messages have been sent to the stream.
     #
-    #     def test_no_broadcasts
-    #       assert_no_broadcasts 'messages'
-    #       ActionCable.server.broadcast 'messages', { text: 'hi' }
-    #       assert_broadcasts 'messages', 1
-    #     end
+    # ```
+    # def test_no_broadcasts
+    #   assert_no_broadcasts 'messages'
+    #   ActionCable.server.broadcast 'messages', { text: 'hi' }
+    #   assert_broadcasts 'messages', 1
+    # end
+    # ```
     #
     # If a block is passed, that block should not cause any message to be sent.
     #
-    #     def test_broadcasts_again
-    #       assert_no_broadcasts 'messages' do
-    #         # No job messages should be sent from this block
-    #       end
-    #     end
+    # ```
+    # def test_broadcasts_again
+    #   assert_no_broadcasts 'messages' do
+    #     # No job messages should be sent from this block
+    #   end
+    # end
+    # ```
     #
     # Note: This assertion is simply a shortcut for:
     #
-    #     assert_broadcasts 'messages', 0, &block
+    # ```
+    # assert_broadcasts 'messages', 0, &block
+    # ```
     #
     def assert_no_broadcasts(stream, &block)
       assert_broadcasts stream, 0, &block
@@ -83,15 +94,17 @@ module ActionCable
 
     # Returns the messages that are broadcasted in the block.
     #
-    #     def test_broadcasts
-    #       messages = capture_broadcasts('messages') do
-    #         ActionCable.server.broadcast 'messages', { text: 'hi' }
-    #         ActionCable.server.broadcast 'messages', { text: 'how are you?' }
-    #       end
-    #       assert_equal 2, messages.length
-    #       assert_equal({ text: 'hi' }, messages.first)
-    #       assert_equal({ text: 'how are you?' }, messages.last)
-    #     end
+    # ```
+    # def test_broadcasts
+    #   messages = capture_broadcasts('messages') do
+    #     ActionCable.server.broadcast 'messages', { text: 'hi' }
+    #     ActionCable.server.broadcast 'messages', { text: 'how are you?' }
+    #   end
+    #   assert_equal 2, messages.length
+    #   assert_equal({ text: 'hi' }, messages.first)
+    #   assert_equal({ text: 'how are you?' }, messages.last)
+    # end
+    # ```
     #
     def capture_broadcasts(stream, &block)
       new_broadcasts_from(broadcasts(stream), stream, "capture_broadcasts", &block).map { |m| ActiveSupport::JSON.decode(m) }
@@ -99,19 +112,23 @@ module ActionCable
 
     # Asserts that the specified message has been sent to the stream.
     #
-    #     def test_assert_transmitted_message
-    #       ActionCable.server.broadcast 'messages', text: 'hello'
-    #       assert_broadcast_on('messages', text: 'hello')
-    #     end
+    # ```
+    # def test_assert_transmitted_message
+    #   ActionCable.server.broadcast 'messages', text: 'hello'
+    #   assert_broadcast_on('messages', text: 'hello')
+    # end
+    # ```
     #
     # If a block is passed, that block should cause a message with the specified
     # data to be sent.
     #
-    #     def test_assert_broadcast_on_again
-    #       assert_broadcast_on('messages', text: 'hello') do
-    #         ActionCable.server.broadcast 'messages', text: 'hello'
-    #       end
-    #     end
+    # ```
+    # def test_assert_broadcast_on_again
+    #   assert_broadcast_on('messages', text: 'hello') do
+    #     ActionCable.server.broadcast 'messages', text: 'hello'
+    #   end
+    # end
+    # ```
     #
     def assert_broadcast_on(stream, data, &block)
       # Encode to JSON and back–we want to use this value to compare with decoded
